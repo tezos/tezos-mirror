06e5ea56509fb6b9e72ab657981c18ae9a41a932
Raphaël Proust <code@bnwr.net>
2019-04-22 14:57:51 +0800

    Base: move OS-specific code to node

--- src/bin_node/node_identity_command.ml
+++ src/bin_node/node_identity_command.ml
@@ -34,6 +34,51 @@ let show { Node_config_file.data_dir ; _ } =
   Format.printf "Peer_id: %a.@." P2p_peer.Id.pp id.peer_id ;
   return_unit
 
+module Animation = struct
+
+  let animation = [|
+    "|.....|" ;
+    "|o....|" ;
+    "|oo...|" ;
+    "|ooo..|" ;
+    "|.ooo.|" ;
+    "|..ooo|" ;
+    "|...oo|" ;
+    "|....o|" ;
+    "|.....|" ;
+    "|.....|" ;
+    "|.....|" ;
+    "|.....|" ;
+  |]
+
+  let init = String.make (String.length animation.(0)) '\ '
+  let clean = String.make (String.length animation.(0)) '\b'
+  let animation = Array.map (fun x -> clean ^ x) animation
+  let animation_size = Array.length animation
+  let duration = 1200 / animation_size
+
+  let generate_with_animation ppf target =
+    Format.fprintf ppf "%s%!" init ;
+    let count = ref 10000 in
+    let rec loop n =
+      let start = Mtime_clock.counter () in
+      Format.fprintf ppf "%s%!" animation.(n mod animation_size);
+      try P2p_identity.generate_with_bound ~max:!count target
+      with Not_found ->
+        let time = Mtime.Span.to_ms (Mtime_clock.count start) in
+        count :=
+          if time <= 0. then
+            !count * 10
+          else
+            !count * duration / int_of_float time ;
+        loop (n+1)
+    in
+    let id = loop 0 in
+    Format.fprintf ppf "%s%s\n%!" clean init ;
+    id
+
+end
+
 let generate { Node_config_file.data_dir ; p2p ; _ } =
   let identity_file = identity_file data_dir in
   if Sys.file_exists identity_file then
@@ -42,7 +87,7 @@ let generate { Node_config_file.data_dir ; p2p ; _ } =
     let target = Crypto_box.make_target p2p.expected_pow in
     Format.eprintf "Generating a new identity... (level: %.2f) " p2p.expected_pow ;
     let id =
-      P2p_identity.generate_with_animation Format.err_formatter target in
+      Animation.generate_with_animation Format.err_formatter target in
     Node_identity_file.write identity_file id >>=? fun () ->
     Format.eprintf
       "Stored the new identity (%a) into '%s'.@."
