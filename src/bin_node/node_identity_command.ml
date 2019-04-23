e012d96a1e9dda11bba75766918351a235f66ec6
Raphaël Proust <code@bnwr.net>
2019-04-23 16:04:54 +0800

    Node: move terminal animation code to support library

--- src/bin_node/node_identity_command.ml
+++ src/bin_node/node_identity_command.ml
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2019 Nomadic Labs, <contact@nomadic-labs.com>               *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -34,51 +35,20 @@ let show { Node_config_file.data_dir ; _ } =
   Format.printf "Peer_id: %a.@." P2p_peer.Id.pp id.peer_id ;
   return_unit
 
-module Animation = struct
-
-  let animation = [|
-    "|.....|" ;
-    "|o....|" ;
-    "|oo...|" ;
-    "|ooo..|" ;
-    "|.ooo.|" ;
-    "|..ooo|" ;
-    "|...oo|" ;
-    "|....o|" ;
-    "|.....|" ;
-    "|.....|" ;
-    "|.....|" ;
-    "|.....|" ;
-  |]
-
-  let init = String.make (String.length animation.(0)) '\ '
-  let clean = String.make (String.length animation.(0)) '\b'
-  let animation = Array.map (fun x -> clean ^ x) animation
-  let animation_size = Array.length animation
-  let duration = 1200 / animation_size
-
-  let generate_with_animation ppf target =
-    Format.fprintf ppf "%s%!" init ;
-    let count = ref 10000 in
-    let rec loop n =
-      let start = Mtime_clock.counter () in
-      Format.fprintf ppf "%s%!" animation.(n mod animation_size);
-      try P2p_identity.generate_with_bound ~max:!count target
-      with Not_found ->
-        let time = Mtime_clock.count start in
-        begin if Mtime.Span.(equal zero time) then
-            count := !count * 10
-          else
-            let ms = int_of_float (Mtime.Span.to_ms time) in
-            count := !count * duration / ms
-        end ;
-        loop (n+1)
-    in
-    let id = loop 0 in
-    Format.fprintf ppf "%s%s\n%!" clean init ;
-    id
-
-end
+let generate_with_animation ppf target =
+  let duration = 1200 / Animation.number_of_frames in
+  Animation.make_with_animation
+    ppf
+    ~make:(fun count ->
+        try Ok (P2p_identity.generate_with_bound ~max:count target)
+        with Not_found -> Error count)
+    ~on_retry:(fun time count ->
+        if Mtime.Span.(equal zero time) then
+          max 10 (count * 10)
+        else
+          let ms = int_of_float (Mtime.Span.to_ms time) in
+          count * duration / ms)
+    10000
 
 let generate { Node_config_file.data_dir ; p2p ; _ } =
   let identity_file = identity_file data_dir in
@@ -88,7 +58,7 @@ let generate { Node_config_file.data_dir ; p2p ; _ } =
     let target = Crypto_box.make_target p2p.expected_pow in
     Format.eprintf "Generating a new identity... (level: %.2f) " p2p.expected_pow ;
     let id =
-      Animation.generate_with_animation Format.err_formatter target in
+      generate_with_animation Format.err_formatter target in
     Node_identity_file.write identity_file id >>=? fun () ->
     Format.eprintf
       "Stored the new identity (%a) into '%s'.@."
