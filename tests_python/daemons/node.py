4c3acb8bd9a06507b2d7630f2989c4ef9f3ac5de
Philippe Bidinger <philippe.bidinger@tezcore.com>
2019-05-16 12:00:52 +0200

    Tests/python: extend node/sandbox API
    . Add run/terminate methods to Node in order to start and stop
    a node within a given test, while keeping the persistent state.
    . Allows to start a node with a snapshot

--- tests_python/daemons/node.py
+++ tests_python/daemons/node.py
@@ -1,4 +1,5 @@
 from typing import Dict, List, Tuple
+import time
 import subprocess
 import os
 import tempfile
@@ -6,12 +7,29 @@ import shutil
 from . import utils
 
 
-class Node(subprocess.Popen):
-    """Forks a tezos node and manages its persistent state"""
+class Node:
+    """Forks a tezos node and manages its persistent state.
 
-    # TODO this was initially meant to be a sandbox node only,
-    #      to be more generic, we could allow any command parameters
-    #      and leave the caller setting them.
+    Typical use.
+
+    node = Node(node_bin,
+                sandbox_file,
+                p2p_port=p2p_node,
+                rpc_port=rpc_node,
+                peers=peers_rpc,
+                log_file=log_file,
+                params=params,
+                log_levels=log_levels)
+
+    node.snapshot_import(snapshot) # optional, use a snapshot
+    node.init_id() # generate node id
+    node.init_config() # generate config file based on parameters
+    node.run() # run tezos-node process
+    node.terminate() # terminate process
+    node.run() # re-run using same process
+    node.terminate() # or node.kill()
+    node.cleanup() # cleanup temp files
+    """
 
     def __init__(self,
                  node: str,
@@ -51,21 +69,11 @@ class Node(subprocess.Popen):
         self.node_dir = node_dir
         self.p2p_port = p2p_port
         self.rpc_port = rpc_port
+        self.expected_pow = expected_pow
+        self.node = node
+        self._params = params
+        self._run_called_before = False
 
-        # TODO make this more generic and let the caller fix
-        #  parameters
-        node_config = [node,
-                       'config',
-                       'init',
-                       '--data-dir', node_dir,
-                       '--net-addr', f'127.0.0.1:{p2p_port}',
-                       '--rpc-addr', f'127.0.0.1:{rpc_port}',
-                       '--expected-pow', str(expected_pow)] + params
-        node_identity = [node,
-                         'identity',
-                         'generate',
-                         str(expected_pow),
-                         '--data-dir', node_dir]
         node_run = [node,
                     'run',
                     '--data-dir', node_dir,
@@ -79,11 +87,7 @@ class Node(subprocess.Popen):
                 node_run.append('--peer')
                 node_run.append(f'127.0.0.1:{peer}')
 
-        if use_tls:
-            # We can't create tezos.crt/tezos.key here
-            # as node_dir has to be empty when we run node_config
-            node_config += ['--rpc-tls',
-                            f'{node_dir}/tezos.crt,{node_dir}/tezos.key']
+        self.use_tls = use_tls
 
         new_env = None
         if env is not None:
@@ -94,27 +98,95 @@ class Node(subprocess.Popen):
             lwt_log = ";".join(f'{key} -> {values}' for key, values in
                                log_levels.items())
             new_env['TEZOS_LOG'] = lwt_log
+        self._new_env = new_env
+        self._node_run = node_run
+        self._process = None
+
+    def run(self):
+        node_run_str = utils.format_command(self._node_run)
+        print(node_run_str)
+        # overwrite old log on on first invocation only
+        overwrite_log = not self._run_called_before
+        stdout, stderr = utils.prepare_log(self._node_run,
+                                           self.log_file,
+                                           overwrite_log)
+        self._process = subprocess.Popen(self._node_run, stdout=stdout,
+                                         stderr=stderr, env=self._new_env)
+        time.sleep(0.1)  # make sure node is launched when we access it later
+        self._run_called_before = True
+
+    def init_config(self):
+        node_config = [self.node,
+                       'config',
+                       'init',
+                       '--data-dir', self.node_dir,
+                       '--net-addr', f'127.0.0.1:{self.p2p_port}',
+                       '--rpc-addr', f'127.0.0.1:{self.rpc_port}',
+                       '--expected-pow', str(self.expected_pow)] + self._params
+
+        if self.use_tls:
+            # We can't create tezos.crt/tezos.key here
+            # as node_dir has to be empty when we run node_config
+            node_config += ['--rpc-tls',
+                            (f'{self.node_dir}/tezos.crt,'
+                             f'{self.node_dir}/tezos.key')]
 
         node_config_str = utils.format_command(node_config)
         print(node_config_str)
-        subprocess.run(node_config, check=True, env=new_env)
+        subprocess.run(node_config, check=True)
+
+    def init_id(self):
+        node_identity = [self.node,
+                         'identity',
+                         'generate',
+                         str(self.expected_pow),
+                         '--data-dir', self.node_dir]
         node_identity_str = utils.format_command(node_identity)
         print(node_identity_str)
-        subprocess.run(node_identity, check=True, env=new_env)
-        node_run_str = utils.format_command(node_run)
+        subprocess.run(node_identity, check=True)
+        if self.use_tls:
+            with open(f'{self.node_dir}/tezos.crt', 'w+') as file:
+                file.write(self.use_tls[0])
+            with open(f'{self.node_dir}/tezos.key', 'w+') as file:
+                file.write(self.use_tls[1])
 
-        if use_tls:
-            with open(f'{node_dir}/tezos.crt', 'w+') as file:
-                file.write(use_tls[0])
-            with open(f'{node_dir}/tezos.key', 'w+') as file:
-                file.write(use_tls[1])
+    def upgrade_storage(self):
+        node_upgrade = [self.node, 'upgrade', 'storage', '--data-dir',
+                        self.node_dir]
+        print(utils.format_command(node_upgrade))
+        subprocess.run(node_upgrade, check=True)
 
-        print(node_run_str)
-        stdout, stderr = utils.prepare_log(node_run, log_file)
-        subprocess.Popen.__init__(self, node_run, stdout=stdout, stderr=stderr,
-                                  env=new_env)
+    def snapshot_export(self, file, params=None):
+        if params is None:
+            params = []
+        params = [f'--data-dir', self.node_dir] + params
+        snapshot_cmd = ([self.node, 'snapshot',
+                         'export'] + list(params) + [file])
+        print(utils.format_command(snapshot_cmd))
+        subprocess.run(snapshot_cmd, check=True)
+
+    def snapshot_import(self, file, params=None):
+        if params is None:
+            params = []
+        params = [f'--data-dir', self.node_dir] + params
+        snapshot_cmd = ([self.node, 'snapshot',
+                         'import'] + list(params) + [file])
+        print(utils.format_command(snapshot_cmd))
+        subprocess.run(snapshot_cmd, check=True)
 
     def cleanup(self):
         """Remove node directory (only if generated by constructor)"""
         if self._temp_dir:
             shutil.rmtree(self.node_dir)
+
+    def terminate(self):
+        assert self._process
+        return self._process.terminate()
+
+    def kill(self):
+        assert self._process
+        return self._process.kill()
+
+    def poll(self):
+        assert self._process
+        return self._process.poll()
