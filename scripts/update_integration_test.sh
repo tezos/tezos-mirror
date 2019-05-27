6aba03353c8bbf9e5f33d604cd14c8316ca0b232
Raphaël Proust <code@bnwr.net>
2019-05-27 16:42:17 +0800

    CI: normalize test names
    - no numbers
    - no `test:` prefix

--- scripts/update_integration_test.sh
+++ scripts/update_integration_test.sh
@@ -9,14 +9,11 @@ tmp=$(mktemp)
 
 sed -z 's/^\(.*##BEGIN_INTEGRATION_PYTHON##\n\).*\(\n##END_INTEGRATION_PYTHON##.*\)$/\1/' "$src_dir/.gitlab-ci.yml" > $tmp
 
-cpt=0
 for test in tests_python/tests/test_*.py; do
-    num=$(printf "%02d" $cpt)
-    cpt=$((cpt+1))
     testname=${test##tests_python/tests/test_}
     testname=${testname%%.py}
     cat >> $tmp <<EOF
-integration:$num:$testname:
+integration:$testname:
   <<: *test_definition
   script:
     - pytest $test
