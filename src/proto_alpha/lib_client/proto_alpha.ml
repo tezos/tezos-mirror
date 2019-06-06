f6ef0e58e55ef88c5cd16f9014b1f9f710ca6367
Marco Stronati <marco@stronati.org>
2019-06-06 20:15:05 +0200

    Revert "Proto/Client: change protocol name to avoid conflicts"
    This reverts commit ab952d0a6a21c8a88514fa07afd8621afe4c871e.

--- src/proto_alpha/lib_client/proto_alpha.ml
+++ src/proto_alpha/lib_client/proto_alpha.ml
@@ -23,7 +23,7 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-module Name = struct let name = "alpha-client" end
+module Name = struct let name = "alpha" end
 module T = Tezos_protocol_environment.Make(Tezos_storage.Context)
 module Alpha_environment = T.MakeV1(Name)()
 
@@ -74,3 +74,4 @@ let register_error_kind
   register_error_kind
     category ~id ~title ~description ?pp
     encoding from_error to_error
+
