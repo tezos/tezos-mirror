cb9cea115b9187c912ec9025a1835f7bff449619
Vincent Botbol <vincent.botbol@tezcore.com>
2019-06-12 15:41:34 +0200

    CI: update opam repository hashes

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=300201d4607ea6d1648c77653300676bf0ae16ed
+opam_repository_tag=872258cf064dec03783089695149e283145317dd
 full_opam_repository_tag=2952acf6c0736562d1a94c3fd28ff01a8c38d56b
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
