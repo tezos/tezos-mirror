4a210a4ef905a06c9daf59c2b07b06d9104ecbdb
Marco Stronati <marco@stronati.org>
2019-06-12 18:28:12 +0200

    Flextesa: add all constants

--- src/lib_network_sandbox/tezos_protocol.ml
+++ src/lib_network_sandbox/tezos_protocol.ml
@@ -222,7 +222,24 @@ type t =
   ; blocks_per_voting_period: int
   ; blocks_per_cycle: int
   ; preserved_cycles: int
-  ; proof_of_work_threshold: int }
+  ; proof_of_work_threshold: int
+  ; blocks_per_commitment: int
+  ; endorsers_per_block: int
+  ; hard_gas_limit_per_operation: int
+  ; hard_gas_limit_per_block: int
+  ; tokens_per_roll: int
+  ; michelson_maximum_type_size: int
+  ; seed_nonce_revelation_tip: int
+  ; origination_size: int
+  ; block_security_deposit: int
+  ; endorsement_security_deposit: int
+  ; block_reward: int
+  ; endorsement_reward: int
+  ; hard_storage_limit_per_operation: int
+  ; cost_per_byte: int
+  ; test_chain_duration: int
+  ; quorum_min: int
+  ; quorum_max: int }
 
 let compare a b = String.compare a.id b.id
 
@@ -242,7 +259,25 @@ let default () =
   ; blocks_per_voting_period= 16
   ; blocks_per_cycle= 8
   ; preserved_cycles= 2
-  ; proof_of_work_threshold= -1 }
+  ; proof_of_work_threshold= -1
+  ; blocks_per_commitment= 4
+  ; endorsers_per_block= 32
+  ; hard_gas_limit_per_operation= 800000
+  ; hard_gas_limit_per_block= 8000000
+  ; tokens_per_roll= 8000000000
+  ; michelson_maximum_type_size= 1000
+  ; seed_nonce_revelation_tip= 125000
+  ; origination_size= 257
+  ; block_security_deposit= 512000000
+  ; endorsement_security_deposit= 64000000
+  ; block_reward= 16000000
+  ; endorsement_reward= 2000000
+  ; hard_storage_limit_per_operation= 60000
+  ; cost_per_byte= 1000
+  ; test_chain_duration= 1966080
+  ; quorum_min= 3000
+  ; quorum_max= 7000 }
+
 
 let protocol_parameters_json t : Ezjsonm.t =
   let open Ezjsonm in
@@ -264,8 +299,40 @@ let protocol_parameters_json t : Ezjsonm.t =
     ; ("blocks_per_voting_period", int t.blocks_per_voting_period)
     ; ("blocks_per_cycle", int t.blocks_per_cycle)
     ; ("preserved_cycles", int t.preserved_cycles)
-    ; ( "proof_of_work_threshold"
-      , ksprintf string "%d" t.proof_of_work_threshold ) ]
+    ; ("proof_of_work_threshold"
+      , ksprintf string "%d" t.proof_of_work_threshold)
+    (* which constants are encoded in json as int or string looks a bit arbitrary
+       e.g. michelson_maximum_type_size=1000 is an int but cost_per_byte=1000 is
+       a string *)
+    ; ("blocks_per_commitment", int t.blocks_per_commitment)
+    ; ("endorsers_per_block", int t.endorsers_per_block)
+    ; ("hard_gas_limit_per_operation",
+       string (Int.to_string t.hard_gas_limit_per_operation))
+    ; ("hard_gas_limit_per_block",
+       string (Int.to_string t.hard_gas_limit_per_block))
+    ; ("tokens_per_roll",
+       string (Int.to_string t.tokens_per_roll))
+    ; ("michelson_maximum_type_size", int t.michelson_maximum_type_size)
+    ; ("seed_nonce_revelation_tip",
+       string (Int.to_string t.seed_nonce_revelation_tip))
+    ; ("origination_size", int t.origination_size)
+    ; ("block_security_deposit",
+       string (Int.to_string t.block_security_deposit))
+    ; ("endorsement_security_deposit",
+       string (Int.to_string t.endorsement_security_deposit))
+    ; ("block_reward",
+       string (Int.to_string t.block_reward))
+    ; ("endorsement_reward",
+       string (Int.to_string t.endorsement_reward))
+    ; ("hard_storage_limit_per_operation",
+       string (Int.to_string t.hard_storage_limit_per_operation))
+    ; ("cost_per_byte",
+       string (Int.to_string t.cost_per_byte))
+    ; ("test_chain_duration",
+       string (Int.to_string t.test_chain_duration))
+    ; ("quorum_min", int t.quorum_min)
+    ; ("quorum_max", int t.quorum_max)
+    ]
 
 let sandbox {dictator; _} =
   let pk = Account.pubkey dictator in
