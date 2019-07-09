4d8d8e5dbf0f4206bfaeb6bb94f267a57d024912
Pierre Boutillier <pierre.boutillier@ens-lyon.org>
2019-07-09 14:41:07 +0200

    Dependencies: refresh opam-repository

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=872258cf064dec03783089695149e283145317dd
-full_opam_repository_tag=2952acf6c0736562d1a94c3fd28ff01a8c38d56b
+opam_repository_tag=b961d415ccf9046eafaf2d8cfa83c8bde26854ca
+full_opam_repository_tag=de642cab7342f43cb8f181188312bce2de909c7f
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
