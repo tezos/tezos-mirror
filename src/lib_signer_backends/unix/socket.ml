c1be9d0329284f89b227c2aa0beacd3bc1e25f86
Sebastien Mondet <seb@mondet.org>
2019-08-12 13:49:34 -0400

    Signer: Remove ?interactive, add `import_secret_key`

--- src/lib_signer_backends/unix/socket.ml
+++ src/lib_signer_backends/unix/socket.ml
@@ -155,15 +155,17 @@ struct
           >>=? fun key ->
           return (Lwt_utils_unix.Socket.Unix (Uri.path uri), key)
 
-    let public_key ?interactive:_ uri =
+    let public_key uri =
       parse (uri : pk_uri :> Uri.t) >>=? fun (path, pkh) -> public_key path pkh
 
     let neuterize uri =
       return (Client_keys.make_pk_uri (uri : sk_uri :> Uri.t))
 
-    let public_key_hash ?interactive:_ uri =
+    let public_key_hash uri =
       public_key uri
       >>=? fun pk -> return (Signature.Public_key.hash pk, Some pk)
+
+    let import_secret_key ~io:_ = public_key_hash
 
     let sign ?watermark uri msg =
       parse (uri : sk_uri :> Uri.t)
@@ -210,15 +212,17 @@ struct
                 (path, string_of_int port, [Lwt_unix.AI_SOCKTYPE SOCK_STREAM]),
               pkh )
 
-    let public_key ?interactive:_ uri =
+    let public_key uri =
       parse (uri : pk_uri :> Uri.t) >>=? fun (path, pkh) -> public_key path pkh
 
     let neuterize uri =
       return (Client_keys.make_pk_uri (uri : sk_uri :> Uri.t))
 
-    let public_key_hash ?interactive uri =
-      public_key ?interactive uri
+    let public_key_hash uri =
+      public_key uri
       >>=? fun pk -> return (Signature.Public_key.hash pk, Some pk)
+
+    let import_secret_key ~io:_ = public_key_hash
 
     let sign ?watermark uri msg =
       parse (uri : sk_uri :> Uri.t)
