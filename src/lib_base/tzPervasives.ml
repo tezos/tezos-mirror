5812c0e9ae5f5ca9e76f1da13412a17730a2af42
Pietro Abate <pietro.abate@tezcore.com>
2019-08-25 18:46:01 +0200

    clic: move module scriptable to unix submodule
    and move lwt_exit from lib_base to lib_stdlib_unix

--- src/lib_base/tzPervasives.ml
+++ src/lib_base/tzPervasives.ml
@@ -59,7 +59,6 @@ module P2p_stat = P2p_stat
 module P2p_version = P2p_version
 module Distributed_db_version = Distributed_db_version
 module Network_version = Network_version
-module Lwt_exit = Lwt_exit
 include Utils.Infix
 include Error_monad
 module Internal_event = Internal_event
