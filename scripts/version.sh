752e38b3f80faed7afceb56a4b6d094e5f0b90a4
Pietro Abate <pietro.abate@tezcore.com>
2019-09-18 17:54:12 +0200

    ci: build three different minimal images
    shave 70 Mb from minimal images

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=5e96cbd913014f4c02d1c3ee0908c923ca1c03e0
+opam_repository_tag=5df5d3c1fa73e73a7fbd490a9494fa9a5005caa1
 full_opam_repository_tag=4c2bd0f948ae6a0fc602f73631cc8a8ae6e83c1d
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
