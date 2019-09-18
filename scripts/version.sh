8b4c9499120b522ba8756a85358e3dd37885bc26
Thomas Gazagnaire <thomas@gazagnaire.org>
2019-09-18 15:19:25 +0200

    ci: fix opam dependencies

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=0965ca9934ba0f86a18637984d23e4c0e8dc0100
+opam_repository_tag=5e96cbd913014f4c02d1c3ee0908c923ca1c03e0
 full_opam_repository_tag=4c2bd0f948ae6a0fc602f73631cc8a8ae6e83c1d
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
