3cba41820cd28c49226367fd985ec47f72c70ce2
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2019-09-23 13:35:32 +0200

    Tests: add pytest-regtest

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=060f4a470db0f20af1027b0f93533c37e995992e
+opam_repository_tag=5f775f195983dc7adb0c3591ac495a18d4c5abf5
 full_opam_repository_tag=b394cb53d91e5083ebaab5d5d7b94997ad77d716
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
