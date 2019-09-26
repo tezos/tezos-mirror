891439ca808a95a66945b76267a852206fa280f8
Romain Bardou <romain@nomadic-labs.com>
2019-09-26 12:50:04 +0200

    Base: set chain name to TEZOS_ALPHANET_BABYLON_...

--- src/lib_base/distributed_db_version.ml
+++ src/lib_base/distributed_db_version.ml
@@ -37,7 +37,7 @@ let name_encoding =
     ~description:"A name for the distributed DB protocol"
     string
 
-let chain_name = "TEZOS"
+let chain_name = "TEZOS_ALPHANET_CARTHAGE_2019-11-07T13:39:28"
 
 let sandboxed_chain_name = "SANDBOXED_TEZOS"
 
