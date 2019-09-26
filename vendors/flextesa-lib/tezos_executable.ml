05ad7a8f1f9417a9811090ccd619b6ae9743530f
Romain Bardou <romain@nomadic-labs.com>
2019-09-26 17:16:39 +0200

    remove proto_alpha and replace it with BabyM1

--- vendors/flextesa-lib/tezos_executable.ml
+++ vendors/flextesa-lib/tezos_executable.ml
@@ -26,9 +26,9 @@ let make ?binary ?unix_files_sink ?(environment = []) (kind : [< kind]) =
 
 let kind_string (kind : [< kind]) =
   match kind with
-  | `Accuser -> "accuser-alpha"
-  | `Baker -> "baker-alpha"
-  | `Endorser -> "endorser-alpha"
+  | `Accuser -> "accuser-005-PsBabyM1"
+  | `Baker -> "baker-005-PsBabyM1"
+  | `Endorser -> "endorser-005-PsBabyM1"
   | `Node -> "node"
   | `Client -> "client"
   | `Admin -> "admin-client"
