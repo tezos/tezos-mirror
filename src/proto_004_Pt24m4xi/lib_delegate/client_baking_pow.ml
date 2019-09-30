89f2279180b96f206d43cf9ea88f87243241ac9a
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2019-09-30 12:14:27 +0200

    POW: counter instead of rand

--- src/proto_004_Pt24m4xi/lib_delegate/client_baking_pow.ml
+++ src/proto_004_Pt24m4xi/lib_delegate/client_baking_pow.ml
@@ -36,18 +36,25 @@ let is_updated_constant =
   if String.length commit_hash >= 4 then String.sub commit_hash 0 4
   else default_constant
 
-let is_updated_cstruct = MBytes.of_string is_updated_constant
-
 let is_updated_constant_len = String.length is_updated_constant
 
 (* add a version to the pow *)
-let generate_proof_of_work_nonce () =
-  Bytes.concat
-    (Bytes.of_string "")
-    [ is_updated_cstruct;
-      Rand.generate
-        ( Alpha_context.Constants.proof_of_work_nonce_size
-        - is_updated_constant_len ) ]
+let init_proof_of_work_nonce () =
+  let buf =
+    Bytes.make Alpha_context.Constants.proof_of_work_nonce_size '\000'
+  in
+  Bytes.blit_string is_updated_constant 0 buf 0 is_updated_constant_len ;
+  let max_z_len =
+    Alpha_context.Constants.proof_of_work_nonce_size - is_updated_constant_len
+  in
+  let rec aux z =
+    let z_len = (Z.numbits z + 7) / 8 in
+    if z_len > max_z_len then Seq.Nil
+    else (
+      Bytes.blit_string (Z.to_bits z) 0 buf is_updated_constant_len z_len ;
+      Seq.Cons (buf, fun () -> aux (Z.succ z)) )
+  in
+  aux Z.zero
 
 (* This was used before November 2018 *)
 (* (\* Random proof of work *\)
@@ -55,16 +62,22 @@ let generate_proof_of_work_nonce () =
  *   Rand.generate Alpha_context.Constants.proof_of_work_nonce_size *)
 
 let empty_proof_of_work_nonce =
-  Bytes.of_string (String.make Constants_repr.proof_of_work_nonce_size '\000')
+  Bytes.make Constants_repr.proof_of_work_nonce_size '\000'
 
 let mine cctxt chain block shell builder =
   Alpha_services.Constants.all cctxt (chain, block)
   >>=? fun constants ->
   let threshold = constants.parametric.proof_of_work_threshold in
-  let rec loop () =
-    let block = builder (generate_proof_of_work_nonce ()) in
-    if Baking.check_header_proof_of_work_stamp shell block threshold then
-      return block
-    else loop ()
+  let rec loop nonce_seq =
+    match nonce_seq with
+    | Seq.Nil ->
+        failwith
+          "Client_baking_pow.mine: couldn't find nonce for required proof of \
+           work"
+    | Seq.Cons (nonce, seq) ->
+        let block = builder nonce in
+        if Baking.check_header_proof_of_work_stamp shell block threshold then
+          return block
+        else loop (seq ())
   in
-  loop ()
+  loop (init_proof_of_work_nonce ())
