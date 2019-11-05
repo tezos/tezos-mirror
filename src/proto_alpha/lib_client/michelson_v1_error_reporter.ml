23c5a6d114b575e1f453cef4d82972df465187eb
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2019-11-05 13:05:17 +0100

    Protocol/Client: fix a misleading error message (unexpected big_map)

--- src/proto_alpha/lib_client/michelson_v1_error_reporter.ml
+++ src/proto_alpha/lib_client/michelson_v1_error_reporter.ml
@@ -313,11 +313,7 @@ let report_errors ~details ~show_source ?parsed ppf errs =
         if rest <> [] then Format.fprintf ppf "@," ;
         print_trace locations rest
     | Environment.Ecoproto_error (Unexpected_big_map loc) :: rest ->
-        Format.fprintf
-          ppf
-          "%abig_map type not allowed inside another big_map"
-          print_loc
-          loc ;
+        Format.fprintf ppf "%abig_map type not expected here" print_loc loc ;
         if rest <> [] then Format.fprintf ppf "@," ;
         print_trace locations rest
     | Environment.Ecoproto_error (Unexpected_operation loc) :: rest ->
