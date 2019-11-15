0f0a78e85bc41b20f37175791e21c7098d089eb8
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2019-11-15 18:42:20 +0100

    Use safer url for ocaml-migrate-parsetree in tezos-opam-repository

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=0965ca9934ba0f86a18637984d23e4c0e8dc0100
+opam_repository_tag=d8630743e328ca14a9ad8991768108c29e18d59f
 full_opam_repository_tag=4c2bd0f948ae6a0fc602f73631cc8a8ae6e83c1d
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
