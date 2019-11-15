12002a351e988bc1d2ba5593829171013da92921
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2019-11-15 16:56:37 +0100

    Update tezos-opam-repository to handle ocaml-migrate-parsetree.1.4.0 inplace replacement

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=92376e769e50f3e1483ce186dfa475a3ee58e109
-full_opam_repository_tag=f335c4dcf17eddee73cd2322205429ac6ab88476
+opam_repository_tag=09853d4ae678917990a532ff06e67466d9e50dd3
+full_opam_repository_tag=ac83984312683746df2488f9d2b1e03aaf86efe4
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
