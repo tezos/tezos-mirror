1bc6b098dbf0ecd31d6da80c78c7a3bd8d662fcf
Romain Bardou <romain@nomadic-labs.com>
2019-11-27 16:13:11 +0100

    Node: add bootstrap peer

--- src/bin_node/node_config_file.ml
+++ src/bin_node/node_config_file.ml
@@ -107,7 +107,12 @@ let default_p2p_limits : P2p.limits =
 let default_p2p =
   {
     expected_pow = 26.;
-    bootstrap_peers = ["34.76.169.218"; "34.90.24.160"; "carthagenet.kaml.fr"];
+    bootstrap_peers =
+      [ "tezaria.com";
+        "34.76.169.218";
+        "34.90.24.160";
+        "carthagenet.kaml.fr";
+        "104.248.136.94" ];
     listen_addr = Some ("[::]:" ^ string_of_int default_p2p_port);
     discovery_addr = None;
     private_mode = false;
