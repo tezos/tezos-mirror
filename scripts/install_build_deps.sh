15df20ce0cbe6cb42195d27a7cc028cc28b74a98
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2019-12-02 14:12:12 +0100

    build-dev-deps: add odoc
    The package is needed for building the documentation

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -43,5 +43,5 @@ opam install --yes opam-depext
 
 if [ -n "$dev" ]; then
     opam repository add default --rank=-1 > /dev/null 2>&1 || true
-    opam install merlin --criteria="-changed,-removed"
+    opam install merlin odoc --criteria="-changed,-removed"
 fi
