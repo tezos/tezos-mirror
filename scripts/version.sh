b7eeefe54e7931a6c7533b87ea4483ffe4832e99
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2020-01-07 17:23:50 +0100

    Build: refresh dependencies to 2020-01-07 ocaml/opam-repository

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=f42f3d8042d5ad53605fc4907b19140c01bc678b
-full_opam_repository_tag=ac83984312683746df2488f9d2b1e03aaf86efe4
+opam_repository_tag=060f4a470db0f20af1027b0f93533c37e995992e
+full_opam_repository_tag=b394cb53d91e5083ebaab5d5d7b94997ad77d716
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
