3ce9114e9054b30281554b5de33d061f4c52e0ff
Victor Allombert <victor.allombert@nomadic-labs.com>
2020-01-08 11:32:20 +0100

    Lib_base: introduce placeholder for node's version

--- src/lib_base/node_version.ml
+++ src/lib_base/node_version.ml
@@ -1,7 +1,6 @@
 (*****************************************************************************)
-(*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2020 Nomadic Labs, <contact@nomadic-labs.com>               *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -23,46 +22,11 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-include Tezos_stdlib
-module Error_monad = Tezos_error_monad.Error_monad
-include Tezos_rpc
-include Tezos_clic
-include Tezos_crypto
-include Tezos_micheline
-module Data_encoding = Data_encoding
+type version = {commit_hash : string; commit_date : string}
 
-module List = struct
-  include List
-  include Tezos_stdlib.TzList
-end
-
-module String = struct
-  include String
-  include Tezos_stdlib.TzString
-end
-
-module Time = Time
-module Fitness = Fitness
-module User_activated = User_activated
-module Block_header = Block_header
-module Genesis = Genesis
-module Operation = Operation
-module Protocol = Protocol
-module Test_chain_status = Test_chain_status
-module Preapply_result = Preapply_result
-module Block_locator = Block_locator
-module Mempool = Mempool
-module P2p_addr = P2p_addr
-module P2p_identity = P2p_identity
-module P2p_peer = P2p_peer
-module P2p_point = P2p_point
-module P2p_connection = P2p_connection
-module P2p_stat = P2p_stat
-module P2p_version = P2p_version
-module P2p_rejection = P2p_rejection
-module Distributed_db_version = Distributed_db_version
-module Network_version = Network_version
-module Node_version = Node_version
-include Utils.Infix
-include Error_monad
-module Internal_event = Internal_event
+let version_encoding =
+  let open Data_encoding in
+  conv
+    (fun {commit_hash; commit_date} -> (commit_hash, commit_date))
+    (fun (commit_hash, commit_date) -> {commit_hash; commit_date})
+    (obj2 (req "commit_hash" string) (req "commit_date" string))
