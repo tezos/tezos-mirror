5f395b11ed4e4a53c1b5b0b958ef8b440b9dc538
Raphaël Proust <code@bnwr.net>
2020-01-15 12:22:26 -0300

    Stdlib: fix memory leak in WeakRingTable
    Backport of fix from upstream ringo
    
    Note that the WeakRingTable is not used anywhere in the code. It has
    been merged in in preparation of the change to the new mempool. Thus,
    the leak doesn't currently affect any node.

--- src/lib_stdlib/weakRingTable.ml
+++ src/lib_stdlib/weakRingTable.ml
@@ -22,6 +22,27 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
+
+(* WARNING
+
+   This warning is intended for developers that are modifying this file.
+
+   The correctness of this file (w.r.t. the documentation and w.r.t. its name)
+   requires that the WeakRingTable has *weakness*. I.e., indefinitely inserting
+   bindings in a WeakRingTable should never cause a memory leak.
+
+   In order to test for the weakness of this module, simply run the following
+   command:
+   [dune build @src/lib_stdlib/test/runweaknesstest]
+
+   This command executes the test file [test_ring_weakness.ml] with a few
+   specific environment variables. Unfortunately, because this test is fragile
+   and does not return true/false, it is not executed by the CI. You (YOU!) are
+   required (REQUIRED) to perform the test locally before pushing changes to the
+   remote repository.
+
+   *)
+
 module type S = sig
   type 'a t
 
@@ -31,9 +52,9 @@ module type S = sig
 
   val add : 'a t -> key -> 'a -> unit
 
-  val iter : (key -> 'a -> unit) -> 'a t -> unit
-
   val fold : (key -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
+
+  val iter : (key -> 'a -> unit) -> 'a t -> unit
 
   val find_opt : 'a t -> key -> 'a option
 
@@ -42,66 +63,84 @@ module type S = sig
   val length : 'a t -> int
 end
 
-module Make (M : Hashtbl.HashedType) = struct
+let prng = lazy (Random.State.make_self_init ())
+
+module MakeGeneral (M : Hashtbl.SeededHashedType) = struct
   module Table = Ephemeron.K1.Make (struct
-    type t = int
+    (* NOTE: So that it is scanned by the GC, the type [t] must be an
+       allocated/boxed type. *)
+    type t = nativeint
 
-    let hash a = a
+    (* NOTE: the [nativeint] fed into this hash functions are the result of
+       [Nativeint.of_int @@ M.hash v] (for arbistrary [v]s). We assume [M.hash]
+       already has the expected properties of a good hash, and we assert that
+       [Nativeint.of_int] does not degrade those properties. *)
+    let hash a = Nativeint.to_int a
 
-    let equal = ( = )
-  end)
-
-  module Ring = Ring.MakeTable (struct
-    type t = int * M.t
-
-    let hash (i, _) = i
-
-    let equal = ( = )
+    let equal = Nativeint.equal
   end)
 
   type key = M.t
 
   module Visit_tracking = Set.Make (struct
-    type t = int
+    type t = nativeint
 
-    let compare = Pervasives.compare
+    let compare = Nativeint.compare
   end)
 
-  type 'a t = {table : 'a Table.t; ring : Ring.t}
+  type 'a t = {table : 'a Table.t; ring : (nativeint * M.t) Ring.t; seed : int}
 
-  let create n = {table = Table.create n; ring = Ring.create n}
+  let create r n =
+    let seed = if r then Random.State.bits (Lazy.force prng) else 0 in
+    {table = Table.create n; ring = Ring.create n; seed}
 
-  let add {ring; table} k v =
-    let i = M.hash k in
+  let add {ring; table; seed} k v =
+    let i = Nativeint.of_int @@ M.hash seed k in
     Ring.add ring (i, k) ;
     Table.replace table i v
 
-  let find_opt {table; _} k =
-    let i = M.hash k in
+  let find_opt {table; seed; _} k =
+    let i = Nativeint.of_int @@ M.hash seed k in
     Table.find_opt table i
 
-  let fold f {table; ring} acc =
-    let elts = Ring.elements ring in
-    let (res, _) =
-      List.fold_left
-        (fun (acc, visited) (i, k) ->
-          if Visit_tracking.mem i visited then (acc, visited)
-          else
-            match Table.find_opt table i with
-            | None ->
-                (acc, visited)
-            | Some elt ->
-                (f k elt acc, Visit_tracking.add i visited))
-        (acc, Visit_tracking.empty)
-        elts
-    in
-    res
+  let fold f {table; ring; _} acc =
+    Ring.fold
+      ring
+      ~init:(acc, Visit_tracking.empty)
+      ~f:(fun (acc, visited) (i64, k) ->
+        if Visit_tracking.mem i64 visited then (acc, visited)
+        else
+          match Table.find_opt table i64 with
+          | None ->
+              (acc, visited)
+          | Some elt ->
+              (f k elt acc, Visit_tracking.add i64 visited))
+    |> fst
 
   let iter f t = fold (fun k v () -> f k v) t ()
 
-  let remove t k =
-    let i = M.hash k in
-    Table.remove t.table i
+  let remove {table; seed; _} k =
+    let i = Nativeint.of_int @@ M.hash seed k in
+    Table.remove table i
 
   let length {table; _} = Table.length table
+end
+
+module MakeSeeded (K : Hashtbl.SeededHashedType) : S with type key = K.t =
+struct
+  include MakeGeneral (K)
+
+  let create n = create true n
+end
+
+module Make (K : Hashtbl.HashedType) : S with type key = K.t = struct
+  include MakeGeneral (struct
+    type t = K.t
+
+    let equal = K.equal
+
+    let hash _ k = K.hash k
+  end)
+
+  let create n = create false n
 end
--- src/lib_stdlib/weakRingTable.mli
+++ src/lib_stdlib/weakRingTable.mli
@@ -27,41 +27,76 @@ module type S = sig
 
   type key
 
-  (** [create n] is a table with at most [n] elements except when it has more. *)
+  (** [create n] is a table with at most [n] elements except when it has more.
+      Specifically, the table may contain up to [n] _strongly-held_ bindings and
+      an unlimited number of _weakly-held_ bindings.
+
+      Strongly-held bindings are not garbage collected. Weakly-held bindings may
+      be garbage collected. *)
   val create : int -> 'a t
 
-  (** [add t k v] adds a mapping from key [k] to value [v] in the table.
-      NOTE: when n values are bound to the same key, it may count as up to n
-      elements.
-      However, NOTE: when n values are bound to the same key, only the last
-      binding can be found with [find_opt] or traversed with [fold]. *)
+  (** [add t k v] adds a strongly-held binding from key [k] to value [v] in the
+      table. If the table is a full capacity for strongly-held bindings (i.e.,
+      if there are as many strongly-held bindings as the size specified when the
+      table was [create]d), the oldest strongly-held binding becomes
+      weakly-held.
+
+      Note however, that when inserting multiple bindings to the same key in the
+      table, the previous bindings become unavailable immediately, but they may
+      still count towards the strongly-held binding limits until there are more
+      calls to [add]. In other words, the size limit for the strongly-held
+      bindings may be lowered when inserting bindings from the same key.
+
+      Unlike a usual hash-table, hash collisions are severe. Specifically, when
+      inserting two elements that have the same hash, only the second element
+      will be retrieved by [find_opt], [fold], and [iter].
+
+  *)
   val add : 'a t -> key -> 'a -> unit
 
-  val iter : (key -> 'a -> unit) -> 'a t -> unit
-
-  (** [fold f t acc] folds the function [f] and value [acc] through the recently
-      added elements of [t]. It never folds over more elements than the size
-      bound of the table, even if the table temporarily holds more elements. *)
+  (** [fold f t acc] folds the function [f] and value [acc] through the
+      strongly-held bindings of [t]. It does not fold over the weakly-held
+      bindings. Consequently, [fold] never folds over more bindings than the
+      size bound of the table, even if the table temporarily holds more
+      bindings. *)
   val fold : (key -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
 
-  (** [find_opt t k] is [Some v] if [k] is bound to [v] in [t] and [None]
-      otherwise. A key [k] is bound to a value [v] in [t] if [add t k v] has been
-      called and not too many other bindings have been added since then. *)
+  (** [iter f t] iterates the function [f] through the strongly-held bindings of
+      [t]. It does not fold over the weakly-held bindings. Consequently, [iter]
+      never calls [f] over more bindings than the size bound of the table, even
+      if the table temporarily holds more bindings. *)
+  val iter : (key -> 'a -> unit) -> 'a t -> unit
+
+  (** [find_opt t k] is [Some v] if [t] holds a binding from [k] to [v] and
+      [None] otherwise. Whether the binding is strongly- or weakly-held does not
+      affect [find_opt]. As a result:
+
+      - [find_opt] is able to return values that [fold] or [iter] would miss.
+      - [find_opt] may return a value, and then, if the GC decides to collect
+        some weakly-held bindings, may not be able to return the same value even
+        if the table was not used. *)
   val find_opt : 'a t -> key -> 'a option
 
   (** [remove t k] removes the binding from [key] to the associated element in
-      [t]. Note that you may still be able to find the element using [find_opt]
-      for some time. *)
+      [t]. This works regardless of whether the binding is strongly- or
+      weakly-held.
+
+      Note however, that when removing a strongly-held binding, it may still
+      count towards the limit of strongly-held bindings until further bindings
+      are added. *)
   val remove : 'a t -> key -> unit
 
-  (** [length t] is the number of elements currently in [t], including those
-      that may be garbage collected. *)
+  (** [length t] is the number of bindings currently held by [t]. This include
+      both strongly- and weakly-held bindings. As a result, [length t] is
+      greater or equal to [fold (+) t 0]. *)
   val length : 'a t -> int
 end
 
 (** A bounded table which optimistically cheats on the bound and sometimes
-    counts wrong.
-    Specifically, the table retains a bounded number of elements. It will also
-    retain more if given more than that, but it will always drop back to the
-    bound if the garbage collector intervenes. *)
+    counts wrong. See [S] above for more details.
+
+    For documentation about seeded/non-seeded, check [Hashtbl] in the standard
+    library. *)
+module MakeSeeded (K : Hashtbl.SeededHashedType) : S with type key = K.t
+
 module Make (K : Hashtbl.HashedType) : S with type key = K.t
