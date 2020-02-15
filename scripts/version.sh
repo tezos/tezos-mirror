214b8a572794e62b7241dd4c4921746e170ef0a8
Raphaël Proust <code@bnwr.net>
2020-02-15 13:42:03 -0300

    CI: update build image hashes

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=bd0787b45cf2a439e111dd37fe6d269b49db3027
-full_opam_repository_tag=ab35d2792da1a44fe17a9b467125ac17ac0d314a
+opam_repository_tag=dd2176ca48ba07167b9e9fe8e15e89cc4fc0ba16
+full_opam_repository_tag=6a43c5c7a992879f3081d008157597c01583fcdc
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
