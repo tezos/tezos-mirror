95f2af84f68e2477ebf4882a555caa8ab456f2e1
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2020-02-18 17:36:10 +0100

    Build: Use dune 2

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ocaml_version=4.07.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=dd2176ca48ba07167b9e9fe8e15e89cc4fc0ba16
-full_opam_repository_tag=6a43c5c7a992879f3081d008157597c01583fcdc
+opam_repository_tag=f851c59f551801eaf61c5498c8dccd96f239bd5c
+full_opam_repository_tag=624dc320f077db8d5f5620d557ea0e367460be14
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
