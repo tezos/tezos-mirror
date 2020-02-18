0abbc173ac888617f718b30ab4964182ac44d0c3
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2020-02-18 16:17:05 +0100

    Tests_python: add test for non-comparable type in set update

--- tests_python/tests/test_contract.py
+++ tests_python/tests/test_contract.py
@@ -254,7 +254,10 @@ class TestContracts:
          r'unexpected annotation'),
         # error message for the arity of the chain_id type
         ("chain_id_arity.tz",
-         r'primitive chain_id expects 0 arguments but is given 1')
+         r'primitive chain_id expects 0 arguments but is given 1'),
+        # error message for set update on non-comparable type
+        ("set_update_non_comparable.tz",
+         r'comparable type expected'),
     ])
     def test_ill_typecheck(self, client: Client, contract, error_pattern):
         with utils.assert_run_failure(error_pattern):
