84f1817f23284caa486c6f0fb8363967be246591
Marco Stronati <marco@stronati.org>
2020-03-03 17:03:52 +0100

    OCaml 4.09.0: bump version

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,11 +3,11 @@
 ## `ocaml-version` should be in sync with `README.rst` and
 ## `lib.protocol-compiler/tezos-protocol-compiler.opam`
 
-ocaml_version=4.07.1
+ocaml_version=4.09.0
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=c405b8f9d4b1b00c6fb304230a32e9be7f696bf2
+opam_repository_tag=5aca652afd01bf0d66a9d74aa192309dcb9554b2
 full_opam_repository_tag=624dc320f077db8d5f5620d557ea0e367460be14
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
