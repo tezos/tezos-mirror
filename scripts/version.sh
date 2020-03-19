5c06fd48dcb5d51b9ed1f419ad8ca614e813b59f
Philippe Bidinger <philippe.bidinger@tezcore.com>
2020-03-19 21:45:36 +0100

    Tests/python: update requirements file for compatibility with python3.8

--- scripts/version.sh
+++ scripts/version.sh
@@ -12,6 +12,6 @@ opam_version=2.0
 full_opam_repository_tag=7169f683177ef5f9aebed3af0692d2142bc17664
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=c2d2b7ab8bbf734503b89e40fec179ab8c4e2d6e
+opam_repository_tag=d702e817c3fb0958b0f9666905b8a3e64dc1171d
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
