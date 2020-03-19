58bed14ae9d906d18235f57bf334235ebc04c3ea
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2020-03-19 23:07:00 +0100

    Build: Use OCaml 4.09.1 and incorporate mirage-crypto initialization inefficiency fixup

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,11 +3,11 @@
 ## `ocaml-version` should be in sync with `README.rst` and
 ## `lib.protocol-compiler/tezos-protocol-compiler.opam`
 
-ocaml_version=4.09.0
+ocaml_version=4.09.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=3fa284bda863ab029e801005ab70ec01460ffdb8
-full_opam_repository_tag=06ab3242ed00df1695e8ac04a7a0d8e1b9dd2100
+opam_repository_tag=b81783bc01f71a6f66ae2f63b302bdd0f240f01b
+full_opam_repository_tag=e7a02ec1b7c8fdfd71c834549fa71eb3b984a9fa
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
