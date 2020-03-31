e5f199cf6b79cae8a9d108759caf5c42fcb96ec0
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2020-03-31 16:40:41 +0200

    BIP39: Remove build dependency to Base

--- vendors/ocaml-bip39/src/generator.ml
+++ vendors/ocaml-bip39/src/generator.ml
@@ -3,29 +3,35 @@
    Distributed under the ISC license, see terms at the end of the file.
   ---------------------------------------------------------------------------*)
 
-open Base
-open Stdio
-
 let pp_print_quoted_string ppf str =
-  let open Caml.Format in
-  fprintf ppf "\"%s\"" str
+  Format.fprintf ppf "\"%s\"" str
 
 let pp_print_quoted_string_list ppf strs =
-  let open Caml.Format in
-  pp_print_list ~pp_sep:(fun ppf () -> pp_print_string ppf ";")
+  Format.pp_print_list
+    ~pp_sep:(fun ppf () -> Format.pp_print_string ppf ";")
     pp_print_quoted_string ppf strs
 
+let half_naive_read_lines filename =
+  let ic = open_in filename in
+  let rec aux_read_line ic =
+    match try Some (input_line ic) with End_of_file -> None with
+    | Some x -> x :: aux_read_line ic
+    | None -> [] in
+  let out = aux_read_line ic in
+  let () = close_in ic in
+  out
+
 let gen ml =
-  let txt = "../gen/" ^ (Caml.Filename.remove_extension ml) ^ ".txt" in
-  let words = In_channel.read_lines txt in
-  Out_channel.with_file
-    ~binary:false ~append:false ~fail_if_exists:false ml ~f:begin fun oc ->
-    let ppf = Caml.Format.formatter_of_out_channel oc in
-    Caml.Format.fprintf ppf "let words = [%a]@." pp_print_quoted_string_list words
-  end
+  let txt = "gen/" ^ (Filename.remove_extension ml) ^ ".txt" in
+  let words = half_naive_read_lines txt in
+  let oc = open_out ml in
+  let ppf = Format.formatter_of_out_channel oc in
+  let () =
+    Format.fprintf ppf "let words = [%a]@." pp_print_quoted_string_list words in
+  close_out oc
 
 let () =
-  Array.to_list Caml.Sys.argv |> List.tl_exn |> List.iter ~f:gen
+  Array.to_list Sys.argv |> List.tl |> List.iter gen
 
 (*---------------------------------------------------------------------------
    Copyright (c) 2017 Vincent Bernardoff
