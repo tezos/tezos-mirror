d3b8a8011a2ee995d1ffc15b5e7395732659ee39
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2020-04-02 21:22:23 +0200

    Tests/Python: skip an annotation mismatch test for UNPAIR

--- tests_python/tests/test_contract.py
+++ tests_python/tests/test_contract.py
@@ -339,8 +339,9 @@ class TestContracts:
         ("never_literal.tz",
          r'type never has no inhabitant.'),
         # field annotation mismatch with UNPAIR
-        ("unpair_field_annotation_mismatch.tz",
-         r'The field access annotation does not match'),
+        # Skipped until UNPAIR is promoted as instruction
+        # ("unpair_field_annotation_mismatch.tz",
+        # r'The field access annotation does not match'),
     ])
     def test_ill_typecheck(self, client: Client, contract, error_pattern):
         with utils.assert_run_failure(error_pattern):
