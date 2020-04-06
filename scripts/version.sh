ed8a7714a28441fbb68841e6bde8e8c2f0b807c6
Danny Willems <contact@danny-willems.be>
2020-04-06 13:25:31 +0000

    Build: comment the variable name to avoid confusion

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,11 @@ ocaml_version=4.09.1
 opam_version=2.0
 
 ## Please update `.gitlab-ci.yml` accordingly
-opam_repository_tag=b81783bc01f71a6f66ae2f63b302bdd0f240f01b
+## full_opam_repository is a commit hash of the public OPAM repository, i.e.
+## https://github.com/ocaml/opam-repository
 full_opam_repository_tag=e7a02ec1b7c8fdfd71c834549fa71eb3b984a9fa
+
+## opam_repository is an additional, tezos-specific opam repository.
+opam_repository_tag=b81783bc01f71a6f66ae2f63b302bdd0f240f01b
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
