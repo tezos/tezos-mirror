49125afd9d736c54db27d5ec56523d36a8c75c67
Danny Willems <be.danny.willems@gmail.com>
2020-04-08 19:51:49 +0200

    Vendors ocaml-bls12-381 0.3.13
    But do not plug it yet

--- scripts/version.sh
+++ scripts/version.sh
@@ -12,7 +12,7 @@ opam_version=2.0
 full_opam_repository_tag=1399f13a876c930a5f11b87f7e5e309ab25345e3
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=d0c437853544d0fa15fb152224a734204c92cc5c
+opam_repository_tag=0c374d7d54f4c4dcb39ae94f67b7fb6fb6f9eea6
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
---
+++ vendors/ocaml-bls12-381/src/Fr.mli
@@ -0,0 +1,1 @@
+include Ff.PRIME
