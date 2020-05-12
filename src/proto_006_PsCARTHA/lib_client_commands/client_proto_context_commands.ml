8142d9ea7ba3bcd44d757050a52a8a219624301a
Tomáš Zemanovič <tzemanovic@gmail.com>
2020-05-12 18:41:41 +0200

    Client (006 backport): refactor call command syntax

--- src/proto_006_PsCARTHA/lib_client_commands/client_proto_context_commands.ml
+++ src/proto_006_PsCARTHA/lib_client_commands/client_proto_context_commands.ml
@@ -778,14 +778,13 @@ let commands version () =
          burn_cap_arg
          entrypoint_arg)
       ( prefixes ["call"]
+      @@ ContractAlias.destination_param
+           ~name:"dst"
+           ~desc:"name/literal of the destination contract"
       @@ prefix "from"
       @@ ContractAlias.destination_param
            ~name:"src"
            ~desc:"name of the source contract"
-      @@ prefix "to"
-      @@ ContractAlias.destination_param
-           ~name:"dst"
-           ~desc:"name/literal of the destination contract"
       @@ stop )
       (fun ( fee,
              dry_run,
@@ -802,8 +801,8 @@ let commands version () =
              fee_cap,
              burn_cap,
              entrypoint )
-           (_, source)
            (_, destination)
+           (_, source)
            cctxt ->
         let amount = Tez.zero in
         transfer_command
