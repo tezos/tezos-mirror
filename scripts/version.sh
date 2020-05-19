7edf85e8046a0440fed1fbd5f551ce627b7bd317
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2020-05-19 13:22:30 +0200

    Lib_crypto: add hacl-star
    For now, it is purely artificial to decouple opam-repository update from switching hacl version.

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,10 +9,10 @@ opam_version=2.0
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=7be3578b1bcdc9592c5f235f74d4b5350a95f63b
+full_opam_repository_tag=4dd2620bcc821418bae53669a6c6163964c090a2
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=d2797aa07e8e1b72448ba028cb1e02e9ef32d1f5
+opam_repository_tag=032a8bcaccc6ff9e0dc3dc1379db89d87e73722d
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
