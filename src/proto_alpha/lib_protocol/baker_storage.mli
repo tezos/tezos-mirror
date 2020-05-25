94379e424d66b4fcc4dd79de990e453ac3bc4b20
Tomáš Zemanovič <tomas@cryptium.ch>
2020-05-25 18:38:37 +0200

    Proto: Update and add consensus key logic to baker storage
    Updates `Baker_storage` module with the change of baker identifier from
    `Signature.Public_key_hash.t` to `Baker_hash.t`.
    
    Updates the baker registration function.
    
    Adds the logic for reading and updating consensus key.

--- src/proto_alpha/lib_protocol/baker_storage.mli
+++ src/proto_alpha/lib_protocol/baker_storage.mli
@@ -31,78 +31,103 @@ type frozen_balance = {
 }
 
 type error +=
-  | Balance_too_low_for_deposit of {
-      delegate : Signature.Public_key_hash.t;
+  | (* `Temporary *)
+      Already_active of Baker_hash.t
+  | (* `Temporary *)
+      Already_inactive of Baker_hash.t
+  | (* `Temporary *)
+      Balance_too_low_for_deposit of {
+      baker : Baker_hash.t;
       deposit : Tez_repr.t;
       balance : Tez_repr.t;
     }
+  | (* `Permanent *)
+      Unregistered_baker of Baker_hash.t
+  | (* `Permanent *)
+      Already_registered_baker of Baker_hash.t
+  | (* `Temporary *)
+      Already_used_consensus_key of Signature.Public_key.t
+  | (* `Temporary *)
+      Allocated_consensus_key_account of
+      Signature.Public_key_hash.t
 
-(** Iterate on all registered delegates. *)
-val fold :
+val fresh_baker_from_current_nonce :
+  Raw_context.t -> (Raw_context.t * Baker_hash.t) tzresult Lwt.t
+
+(** Register a new baker contract. *)
+val register :
+  ?baker_hash:Baker_hash.t ->
+  ?prepaid_bootstrap_storage:bool ->
   Raw_context.t ->
-  init:'a ->
-  f:(Signature.Public_key_hash.t -> 'a -> 'a Lwt.t) ->
-  'a Lwt.t
+  balance:Tez_repr.t ->
+  threshold:int ->
+  owner_keys:Signature.Public_key.t list ->
+  consensus_key:Signature.Public_key.t ->
+  (Raw_context.t * Baker_hash.t) tzresult Lwt.t
 
-(** List all registered delegates. *)
-val list : Raw_context.t -> Signature.Public_key_hash.t list Lwt.t
+(** Given baker must registered, fails with [[Unregistered_baker]] otherwise. *)
+val must_be_registered : Raw_context.t -> Baker_hash.t -> unit tzresult Lwt.t
+
+(** Active or deactivate a baker. *)
+val set_active :
+  Raw_context.t -> Baker_hash.t -> bool -> Raw_context.t tzresult Lwt.t
+
+(** Check if a given baker hash is registered as a baker. *)
+val registered : Raw_context.t -> Baker_hash.t -> bool Lwt.t
+
+(** Iterate on all registered bakers. *)
+val fold :
+  Raw_context.t -> init:'a -> f:(Baker_hash.t -> 'a -> 'a Lwt.t) -> 'a Lwt.t
+
+(** List all registered bakers. *)
+val list : Raw_context.t -> Baker_hash.t list Lwt.t
+
+(** Returns the list of contracts (implicit or originated) that delegated towards a given baker *)
+val delegated_contracts :
+  Raw_context.t -> Baker_hash.t -> Contract_repr.t list Lwt.t
 
 (** Various functions to 'freeze' tokens.  A frozen 'deposit' keeps its
     associated rolls. When frozen, 'fees' may trigger new rolls
     allocation. Rewards won't trigger new rolls allocation until
     unfrozen. *)
 val freeze_deposit :
-  Raw_context.t ->
-  Signature.Public_key_hash.t ->
-  Tez_repr.t ->
-  Raw_context.t tzresult Lwt.t
+  Raw_context.t -> Baker_hash.t -> Tez_repr.t -> Raw_context.t tzresult Lwt.t
 
 val freeze_fees :
-  Raw_context.t ->
-  Signature.Public_key_hash.t ->
-  Tez_repr.t ->
-  Raw_context.t tzresult Lwt.t
+  Raw_context.t -> Baker_hash.t -> Tez_repr.t -> Raw_context.t tzresult Lwt.t
 
 val freeze_rewards :
-  Raw_context.t ->
-  Signature.Public_key_hash.t ->
-  Tez_repr.t ->
-  Raw_context.t tzresult Lwt.t
+  Raw_context.t -> Baker_hash.t -> Tez_repr.t -> Raw_context.t tzresult Lwt.t
+
+val init_first_cycles : Raw_context.t -> Raw_context.t tzresult Lwt.t
 
 (** Trigger the context maintenance at the end of cycle 'n', i.e.:
     unfreeze deposit/fees/rewards from 'n - preserved_cycle' ; punish the
-    provided unrevealed seeds (tipically seed from cycle 'n - 1').
+    provided unrevealed seeds (typically seed from cycle 'n - 1').
     Returns a list of account with the amount that was unfrozen for each
-    and the list of deactivated delegates. *)
+    and the list of deactivated bakers. *)
 val cycle_end :
   Raw_context.t ->
   Cycle_repr.t ->
   Nonce_storage.unrevealed list ->
-  ( Raw_context.t
-  * Receipt_repr.balance_updates
-  * Signature.Public_key_hash.t list )
-  tzresult
+  (Raw_context.t * Receipt_repr.balance_updates * Baker_hash.t list) tzresult
   Lwt.t
 
-(** Burn all then frozen deposit/fees/rewards for a delegate at a given
+(** Burn all then frozen deposit/fees/rewards for a baker at a given
     cycle. Returns the burned amounts. *)
 val punish :
   Raw_context.t ->
-  Signature.Public_key_hash.t ->
+  Baker_hash.t ->
   Cycle_repr.t ->
   (Raw_context.t * frozen_balance) tzresult Lwt.t
 
 (** Has the given key some frozen tokens in its implicit contract? *)
 val has_frozen_balance :
-  Raw_context.t ->
-  Signature.Public_key_hash.t ->
-  Cycle_repr.t ->
-  bool tzresult Lwt.t
+  Raw_context.t -> Baker_hash.t -> Cycle_repr.t -> bool tzresult Lwt.t
 
 (** Returns the amount of frozen deposit, fees and rewards associated
-    to a given delegate. *)
-val frozen_balance :
-  Raw_context.t -> Signature.Public_key_hash.t -> Tez_repr.t tzresult Lwt.t
+    to a given baker. *)
+val frozen_balance : Raw_context.t -> Baker_hash.t -> Tez_repr.t tzresult Lwt.t
 
 val frozen_balance_encoding : frozen_balance Data_encoding.t
 
@@ -110,47 +135,35 @@ val frozen_balance_by_cycle_encoding :
   frozen_balance Cycle_repr.Map.t Data_encoding.t
 
 (** Returns the amount of frozen deposit, fees and rewards associated
-    to a given delegate, indexed by the cycle by which at the end the
+    to a given baker, indexed by the cycle by which at the end the
     balance will be unfrozen. *)
 val frozen_balance_by_cycle :
-  Raw_context.t ->
-  Signature.Public_key_hash.t ->
-  frozen_balance Cycle_repr.Map.t Lwt.t
+  Raw_context.t -> Baker_hash.t -> frozen_balance Cycle_repr.Map.t Lwt.t
 
 (** Returns the full 'balance' of the implicit contract associated to
     a given key, i.e. the sum of the spendable balance and of the
     frozen balance. *)
-val full_balance :
-  Raw_context.t -> Signature.Public_key_hash.t -> Tez_repr.t tzresult Lwt.t
+val full_balance : Raw_context.t -> Baker_hash.t -> Tez_repr.t tzresult Lwt.t
 
 val staking_balance :
-  Raw_context.t -> Signature.Public_key_hash.t -> Tez_repr.t tzresult Lwt.t
-
-(** Returns the list of contracts (implicit or originated) that delegated towards a given delegate *)
-val delegated_contracts :
-  Raw_context.t -> Signature.Public_key_hash.t -> Contract_repr.t list Lwt.t
+  Raw_context.t -> Baker_hash.t -> Tez_repr.t tzresult Lwt.t
 
 val delegated_balance :
-  Raw_context.t -> Signature.Public_key_hash.t -> Tez_repr.t tzresult Lwt.t
+  Raw_context.t -> Baker_hash.t -> Tez_repr.t tzresult Lwt.t
 
-val deactivated :
-  Raw_context.t -> Signature.Public_key_hash.t -> bool tzresult Lwt.t
+val deactivated : Raw_context.t -> Baker_hash.t -> bool tzresult Lwt.t
 
-val grace_period :
-  Raw_context.t -> Signature.Public_key_hash.t -> Cycle_repr.t tzresult Lwt.t
+val grace_period : Raw_context.t -> Baker_hash.t -> Cycle_repr.t tzresult Lwt.t
 
 module Proof : sig
   (** Has any proof ever been used at this level for this delegate*)
   val mem :
-    Raw_context.t ->
-    Signature.Public_key_hash.t ->
-    Raw_level_repr.t ->
-    bool tzresult Lwt.t
+    Raw_context.t -> Baker_hash.t -> Raw_level_repr.t -> bool tzresult Lwt.t
 
   (** add a level to already known proofs level for this delegate*)
   val add :
     Raw_context.t ->
-    Signature.Public_key_hash.t ->
+    Baker_hash.t ->
     Raw_level_repr.t ->
     Raw_context.t tzresult Lwt.t
 
@@ -158,7 +171,30 @@ module Proof : sig
   val cleanup : Raw_context.t -> Raw_context.t tzresult Lwt.t
 
   val all :
-    Raw_context.t ->
-    Signature.Public_key_hash.t ->
-    Raw_level_repr.LSet.t tzresult Lwt.t
+    Raw_context.t -> Baker_hash.t -> Raw_level_repr.LSet.t tzresult Lwt.t
 end
+
+(** Find if any baker uses the given public key hash as their pending consensus
+    keys *)
+val is_pending_consensus_key :
+  Raw_context.t -> Contract_repr.t -> bool tzresult Lwt.t
+
+(** Try to find a baker that uses the given public key hash as their
+    consensus keys *)
+val is_consensus_key :
+  Raw_context.t ->
+  Signature.Public_key_hash.t ->
+  Baker_hash.t option tzresult Lwt.t
+
+val get_consensus_key :
+  ?level:Raw_level_repr.t ->
+  ?offset:int32 ->
+  Raw_context.t ->
+  Baker_hash.t ->
+  Signature.Public_key.t tzresult Lwt.t
+
+val set_consensus_key :
+  Raw_context.t ->
+  Baker_hash.t ->
+  Signature.Public_key.t ->
+  Raw_context.t tzresult Lwt.t
