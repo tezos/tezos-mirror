7cc1bab713ba6b2f202e0581f3817a8edbecad8e
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2020-05-27 19:46:57 +0200

    CI: uses a build image with librustzcash

--- scripts/version.sh
+++ scripts/version.sh
@@ -12,7 +12,7 @@ opam_version=2.0
 full_opam_repository_tag=4dd2620bcc821418bae53669a6c6163964c090a2
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=4fa415bcf9dfb36f355ffdb1630c086470c02079
+opam_repository_tag=8605138b07c44ed063d5bee3143a0b8c2bd9d346
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
