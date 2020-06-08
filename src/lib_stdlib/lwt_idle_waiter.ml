d6ec6c0a00236f71c3119ac7302dce89004b7fad
vbot <vincent.botbol@nomadic-labs.com>
2020-06-08 15:13:32 +0200

    Revert "Stdlib: remove unused Lwt_idle_waiter module"
    This reverts commit 8d83436ab116a4bfd3bd1d2a6a14a06a55cc248a.

---
+++ src/lib_stdlib/lwt_idle_waiter.ml
@@ -0,0 +1,107 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Lwt.Infix
+
+type t = {
+  mutable pending_tasks : unit Lwt.u list;
+  mutable pending_idle : (unit -> unit Lwt.t) list;
+  mutable running_tasks : int;
+  mutable running_idle : bool;
+  mutable prevent_tasks : bool;
+}
+
+let create () =
+  {
+    pending_tasks = [];
+    pending_idle = [];
+    running_tasks = 0;
+    running_idle = false;
+    prevent_tasks = false;
+  }
+
+let rec may_run_idle_tasks w =
+  if w.running_tasks = 0 && not w.running_idle then
+    match w.pending_idle with
+    | [] ->
+        ()
+    | pending_idle ->
+        w.running_idle <- true ;
+        w.prevent_tasks <- false ;
+        w.pending_idle <- [] ;
+        (* TODO: can we safely remove async and ignore the created
+           thread? *)
+        Lwt.async (fun () ->
+            let pending_idle = List.rev pending_idle in
+            Lwt_list.iter_s (fun f -> f ()) pending_idle
+            >>= fun () ->
+            w.running_idle <- false ;
+            let pending_tasks = List.rev w.pending_tasks in
+            w.pending_tasks <- [] ;
+            List.iter (fun u -> Lwt.wakeup u ()) pending_tasks ;
+            may_run_idle_tasks w ;
+            Lwt.return_unit)
+
+let wrap_error f =
+  Lwt.catch
+    (fun () -> f () >>= fun r -> Lwt.return_ok r)
+    (fun exn -> Lwt.return_error exn)
+
+let unwrap_error = function Ok r -> Lwt.return r | Error exn -> Lwt.fail exn
+
+let wakeup_error u = function
+  | Ok r ->
+      Lwt.wakeup u r
+  | Error exn ->
+      Lwt.wakeup_exn u exn
+
+let rec task w f =
+  if w.running_idle || w.prevent_tasks then (
+    let (t, u) = Lwt.task () in
+    w.pending_tasks <- u :: w.pending_tasks ;
+    t >>= fun () -> task w f )
+  else (
+    w.running_tasks <- w.running_tasks + 1 ;
+    wrap_error f
+    >>= fun res ->
+    w.running_tasks <- w.running_tasks - 1 ;
+    may_run_idle_tasks w ;
+    unwrap_error res )
+
+let when_idle w f =
+  let (t, u) = Lwt.task () in
+  let canceled = ref false in
+  Lwt.on_cancel t (fun () -> canceled := true) ;
+  let f () =
+    if !canceled then Lwt.return_unit
+    else wrap_error f >>= fun res -> wakeup_error u res ; Lwt.return_unit
+  in
+  w.pending_idle <- f :: w.pending_idle ;
+  may_run_idle_tasks w ;
+  t
+
+let force_idle w f =
+  w.prevent_tasks <- true ;
+  when_idle w f
---
+++ src/lib_stdlib/lwt_idle_waiter.mli
@@ -0,0 +1,49 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** A lightweight scheduler to run tasks concurrently as well as
+    special callbacks that must be run in mutual exclusion with the
+    tasks (and each other). *)
+type t
+
+(** Creates a new task / idle callback scheduler *)
+val create : unit -> t
+
+(** Schedule a task to be run as soon as no idle callback is running,
+   or as soon as the next idle callback has been run if it was
+   scheduled by {!force_idle}. *)
+val task : t -> (unit -> 'a Lwt.t) -> 'a Lwt.t
+
+(** Runs a callback as soon as no task is running. Does not prevent
+    new tasks from being scheduled, the calling code should ensure
+    that some idle time will eventually come. Calling this function
+    from inside the callback will result in a dead lock. *)
+val when_idle : t -> (unit -> 'a Lwt.t) -> 'a Lwt.t
+
+(** Runs a callback as soon as possible. Lets all current tasks
+    finish, but postpones all new tasks until the end of the
+    callback. Calling this function from inside the callback will
+    result in a dead lock. *)
+val force_idle : t -> (unit -> 'a Lwt.t) -> 'a Lwt.t
