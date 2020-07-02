13980e3bab1917983f5ecaf6b7d3da982d4822a1
Marco Stronati <marco@stronati.org>
2020-07-02 17:09:08 +0200

    Proto/Migration: init Sapling storage

--- src/proto_alpha/lib_protocol/init_storage.ml
+++ src/proto_alpha/lib_protocol/init_storage.ml
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2019-2020 Nomadic Labs <contact@nomadic-labs.com>           *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -64,7 +65,9 @@ let prepare_first_block ctxt ~typecheck ~level ~timestamp ~fitness =
       Storage.Vote.Current_period.init
         ctxt
         {index = voting_period_index; kind = Proposal; start_position}
-      >>=? fun ctxt -> Storage.Vote.Pred_period_kind.init ctxt Promotion_vote
+      >>=? fun ctxt ->
+      Storage.Vote.Pred_period_kind.init ctxt Promotion_vote
+      >>=? fun ctxt -> Storage.Sapling.Next.init ctxt
 
 let prepare ctxt ~level ~predecessor_timestamp ~timestamp ~fitness =
   Raw_context.prepare ~level ~predecessor_timestamp ~timestamp ~fitness ctxt
