536b1845cd39805b39d8002cd94656a62c3c736a
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2020-07-06 16:58:28 +0200

    Build: refresh tezos/opam-repository commit hash

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,10 +9,10 @@ opam_version=2.0
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=873cadc9ece33ceb81b6a0aba50ae86a26551de0
+full_opam_repository_tag=ab47b82abdc03e43c67b45b714806ca4d43d7091
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=3ca586100c3774f2317d6099f47381f585104b9e
+opam_repository_tag=3718dd48d5e502c0fe8fc64ed4bf6b0c8040fadc
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
