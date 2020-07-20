0493e43a0a8a4c60233951db73249f3949839b1c
Raphaël Proust <code@bnwr.net>
2020-07-20 11:31:24 +0200

    CI, scripts: update opam hash
    This is for crowbar support

--- scripts/version.sh
+++ scripts/version.sh
@@ -12,7 +12,7 @@ opam_version=2.0
 full_opam_repository_tag=ab47b82abdc03e43c67b45b714806ca4d43d7091
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=06c6af764c3951f59a369b92d98ae0d2068b4cb4
+opam_repository_tag=6d1227cc7e4df7a163aeb0d87e4d75e56de6cb5c
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
