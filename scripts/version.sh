df12b817879737855b82b460a09fa443c3d83ca4
Romain Bardou <romain@nomadic-labs.com>
2020-08-04 17:58:58 +0200

    Docker: use images with libgcc

--- scripts/version.sh
+++ scripts/version.sh
@@ -12,7 +12,7 @@ opam_version=2.0
 full_opam_repository_tag=ab47b82abdc03e43c67b45b714806ca4d43d7091
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=3718dd48d5e502c0fe8fc64ed4bf6b0c8040fadc
+opam_repository_tag=15f1976fa62468dbc91550b36c09d7a87195cad3
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
