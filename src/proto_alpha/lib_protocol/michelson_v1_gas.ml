954ae04ddd121de6da0c0bbfc6f7c285f6468660
Ilias Garnier <ilias.garnier@nomadic-labs.com>
2020-08-15 20:43:43 +0200

    Proto/Gas: add new gas cost functions in dedicated module

--- src/proto_alpha/lib_protocol/michelson_v1_gas.ml
+++ src/proto_alpha/lib_protocol/michelson_v1_gas.ml
@@ -87,6 +87,346 @@ module Cost_of = struct
     let set_update key _presence set = set_access key set *@ alloc_cost 3
   end
 
+  (* FIXME: hardcoded constant, available in next environment version.
+     Set to a reasonable upper bound. *)
+  let public_key_size = 64
+
+  module Generated_costs_007 = struct
+    (* Automatically generated costs functions. *)
+
+    (* model N_Abs_int *)
+    (* Approximating 0.068306 x term *)
+    let cost_N_Abs_int size =
+      let v0 = size in
+      22 + (v0 lsr 4)
+
+    (* model N_Add_intint *)
+    (* Approximating 0.082158 x term *)
+    let cost_N_Add_intint size1 size2 =
+      let v0 = Compare.Int.max size1 size2 in
+      0 + ((v0 lsr 4) + (v0 lsr 6))
+
+    (* model N_Add_tez *)
+    let cost_N_Add_tez = 95
+
+    (* model N_And *)
+    let cost_N_And = 91
+
+    (* model N_And_nat *)
+    (* Approximating 0.079325 x term *)
+    let cost_N_And_nat size1 size2 =
+      let v0 = Compare.Int.min size1 size2 in
+      93 + ((v0 lsr 4) + (v0 lsr 6))
+
+    (* model N_Blake2b *)
+    (* Approximating 1.366428 x term *)
+    let cost_N_Blake2b size =
+      let v0 = size in
+      519 + (v0 + (v0 lsr 2))
+
+    (* model N_Car *)
+    let cost_N_Car = 73
+
+    (* model N_Cdr *)
+    let cost_N_Cdr = 82
+
+    (* model N_Check_signature_ed25519 *)
+    (* Approximating 1.372685 x term *)
+    let cost_N_Check_signature_ed25519 size =
+      let v0 = size in
+      267512 + (v0 + (v0 lsr 2))
+
+    (* model N_Check_signature_p256 *)
+    (* Approximating 1.385771 x term *)
+    let cost_N_Check_signature_p256 size =
+      let v0 = size in
+      590829 + (v0 + (v0 lsr 2) + (v0 lsr 3))
+
+    (* model N_Check_signature_secp256k1 *)
+    (* Approximating 1.372411 x term *)
+    let cost_N_Check_signature_secp256k1 size =
+      let v0 = size in
+      55348 + (v0 + (v0 lsr 2))
+
+    (* model N_Compare_address *)
+    let cost_N_Compare_address size1 size2 =
+      0 + (2 * Compare.Int.min size1 size2)
+
+    (* model N_Compare_bool *)
+    let cost_N_Compare_bool size1 size2 =
+      0 + (128 * Compare.Int.min size1 size2)
+
+    (* model N_Compare_int *)
+    (* Approximating 0.073657 x term *)
+    let cost_N_Compare_int size1 size2 =
+      let v0 = Compare.Int.min size1 size2 in
+      152 + ((v0 lsr 4) + (v0 lsr 7))
+
+    (* model N_Compare_key_hash *)
+    let cost_N_Compare_key_hash size1 size2 =
+      0 + (2 * Compare.Int.min size1 size2)
+
+    (* model N_Compare_mutez *)
+    let cost_N_Compare_mutez size1 size2 =
+      0 + (13 * Compare.Int.min size1 size2)
+
+    (* model N_Compare_string *)
+    (* Approximating 0.039389 x term *)
+    let cost_N_Compare_string size1 size2 =
+      let v0 = Compare.Int.min size1 size2 in
+      121 + ((v0 lsr 5) + (v0 lsr 7))
+
+    (* model N_Compare_timestamp *)
+    (* Approximating 0.072483 x term *)
+    let cost_N_Compare_timestamp size1 size2 =
+      let v0 = Compare.Int.min size1 size2 in
+      141 + ((v0 lsr 4) + (v0 lsr 7))
+
+    (* model N_Concat_string_pair *)
+    (* Approximating 0.068808 x term *)
+    let cost_N_Concat_string_pair size1 size2 =
+      let v0 = size1 + size2 in
+      0 + (v0 lsr 4)
+
+    (* model N_Cons_list *)
+    let cost_N_Cons_list = 89
+
+    (* model N_Cons_none *)
+    let cost_N_Cons_none = 82
+
+    (* model N_Cons_pair *)
+    let cost_N_Cons_pair = 82
+
+    (* model N_Cons_some *)
+    let cost_N_Cons_some = 74
+
+    (* model N_Const *)
+    let cost_N_Const = 85
+
+    (* model N_Dig *)
+    let cost_N_Dig size = 106 + (4 * size)
+
+    (* model N_Dip *)
+    let cost_N_Dip = 100
+
+    (* model N_DipN *)
+    let cost_N_DipN size = 111 + (4 * size)
+
+    (* model N_Drop *)
+    let cost_N_Drop = 81
+
+    (* model N_DropN *)
+    let cost_N_DropN size = 110 + (4 * size)
+
+    (* model N_Dug *)
+    let cost_N_Dug size = 120 + (4 * size)
+
+    (* model N_Dup *)
+    let cost_N_Dup = 75
+
+    let indic_lt x y = if Compare.Int.(x < y) then 1 else 0
+
+    (* model N_Ediv_natnat *)
+    (* Approximating 0.001599 x term *)
+    let cost_N_Ediv_natnat size1 size2 =
+      let q = size1 - size2 in
+      let v0 = indic_lt size2 size1 * q * size2 in
+      (v0 lsr 10) + (v0 lsr 11) + (v0 lsr 13) + 153030
+
+    (* model N_Ediv_tez *)
+    let cost_N_Ediv_tez = 208
+
+    (* model N_Ediv_teznat *)
+    let cost_N_Ediv_teznat size1 size2 =
+      let q = size1 - size2 in
+      (30 * (indic_lt size2 size1 * q * size2)) + 0
+
+    (* model N_Empty_map *)
+    let cost_N_Empty_map = 212
+
+    (* model N_Empty_set *)
+    let cost_N_Empty_set = 238
+
+    (* model N_Eq *)
+    let cost_N_Eq = 85
+
+    (* model N_If *)
+    let cost_N_If = 57
+
+    (* model N_If_cons *)
+    let cost_N_If_cons = 113
+
+    (* model N_If_left *)
+    let cost_N_If_left = 91
+
+    (* model N_If_none *)
+    let cost_N_If_none = 80
+
+    (* model N_Int_nat *)
+    let cost_N_Int_nat = 84
+
+    (* model N_Is_nat *)
+    let cost_N_Is_nat = 93
+
+    (* model N_Left *)
+    let cost_N_Left = 75
+
+    (* model N_List_iter *)
+    let cost_N_List_iter size = 570 + (7 * size)
+
+    (* model N_List_map *)
+    let cost_N_List_map size = 0 + (12 * size)
+
+    (* model N_List_size *)
+    let cost_N_List_size = 78
+
+    (* model N_Loop *)
+    let cost_N_Loop = 72
+
+    (* model N_Loop_left *)
+    let cost_N_Loop_left = 76
+
+    (* model N_Lsl_nat *)
+    (* Approximating 0.129443 x term *)
+    let cost_N_Lsl_nat size =
+      let v0 = size in
+      159 + (v0 lsr 3)
+
+    (* model N_Lsr_nat *)
+    (* Approximating 0.129435 x term *)
+    let cost_N_Lsr_nat size =
+      let v0 = size in
+      103 + (v0 lsr 3)
+
+    (* model N_Map_get *)
+    (* Approximating 0.057548 x term *)
+    let cost_N_Map_get size1 size2 =
+      let v0 = size1 * log2 (1 + size2) in
+      (v0 lsr 5) + (v0 lsr 6) + (v0 lsr 7) + 0
+
+    (* model N_Map_iter *)
+    let cost_N_Map_iter size = 0 + (40 * size)
+
+    (* model N_Map_map *)
+    let cost_N_Map_map size = 0 + (761 * size)
+
+    (* model N_Map_mem *)
+    (* Approximating 0.058563 x term *)
+    let cost_N_Map_mem size1 size2 =
+      let v0 = size1 * log2 (1 + size2) in
+      (v0 lsr 5) + (v0 lsr 6) + (v0 lsr 7) + 0
+
+    (* model N_Map_size *)
+    let cost_N_Map_size = 91
+
+    (* model N_Map_update *)
+    (* Approximating 0.119968 x term *)
+    let cost_N_Map_update size1 size2 =
+      let v0 = size1 * log2 (1 + size2) in
+      (v0 lsr 4) + (v0 lsr 5) + (v0 lsr 6) + (v0 lsr 7) + 0
+
+    (* model N_Mul_intint *)
+    let cost_N_Mul_intint size1 size2 =
+      let a = size1 + size2 in
+      (1 * (a * log2 (1 + a))) + 0
+
+    (* model N_Mul_teznat *)
+    let cost_N_Mul_teznat size = 194 + (133 * size)
+
+    (* model N_Neg_int *)
+    (* Approximating 0.068419 x term *)
+    let cost_N_Neg_int size =
+      let v0 = size in
+      26 + (v0 lsr 4)
+
+    (* model N_Neq *)
+    let cost_N_Neq = 91
+
+    (* model N_Nil *)
+    let cost_N_Nil = 80
+
+    (* model N_Nop *)
+    let cost_N_Nop = 71
+
+    (* model N_Not *)
+    let cost_N_Not = 90
+
+    (* model N_Not_int *)
+    (* Approximating 0.076564 x term *)
+    let cost_N_Not_int size =
+      let v0 = size in
+      55 + ((v0 lsr 4) + (v0 lsr 7))
+
+    (* model N_Or *)
+    let cost_N_Or = 87
+
+    (* model N_Or_nat *)
+    (* Approximating 0.078718 x term *)
+    let cost_N_Or_nat size1 size2 =
+      let v0 = Compare.Int.max size1 size2 in
+      0 + ((v0 lsr 4) + (v0 lsr 6))
+
+    (* model N_Right *)
+    let cost_N_Right = 80
+
+    (* model N_Seq *)
+    let cost_N_Seq = 58
+
+    (* model N_Set_iter *)
+    let cost_N_Set_iter size = 0 + (36 * size)
+
+    (* model N_Set_mem *)
+    (* Approximating 0.059410 x term *)
+    let cost_N_Set_mem size1 size2 =
+      let v0 = size1 * log2 (1 + size2) in
+      (v0 lsr 5) + (v0 lsr 6) + (v0 lsr 7) + (v0 lsr 8) + 0
+
+    (* model N_Set_size *)
+    let cost_N_Set_size = 79
+
+    (* model N_Set_update *)
+    (* Approximating 0.126260 x term *)
+    let cost_N_Set_update size1 size2 =
+      let v0 = size1 * log2 (1 + size2) in
+      (v0 lsr 3) + 0
+
+    (* model N_Sha256 *)
+    let cost_N_Sha256 size = 492 + (5 * size)
+
+    (* model N_Sha512 *)
+    let cost_N_Sha512 size = 457 + (3 * size)
+
+    (* model N_Slice_string *)
+    (* Approximating 0.067048 x term *)
+    let cost_N_Slice_string size =
+      let v0 = size in
+      53 + (v0 lsr 4)
+
+    (* model N_String_size *)
+    let cost_N_String_size = 79
+
+    (* model N_Sub_int *)
+    (* Approximating 0.082399 x term *)
+    let cost_N_Sub_int size1 size2 =
+      let v0 = Compare.Int.max size1 size2 in
+      0 + ((v0 lsr 4) + (v0 lsr 6))
+
+    (* model N_Sub_tez *)
+    let cost_N_Sub_tez = 83
+
+    (* model N_Swap *)
+    let cost_N_Swap = 72
+
+    (* model N_Xor *)
+    let cost_N_Xor = 100
+
+    (* model N_Xor_nat *)
+    (* Approximating 0.078258 x term *)
+    let cost_N_Xor_nat size1 size2 =
+      let v0 = Compare.Int.max size1 size2 in
+      8 + ((v0 lsr 4) + (v0 lsr 6))
+  end
+
   module Interpreter = struct
     let cycle = atomic_step_cost 10
 
