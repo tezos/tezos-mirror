0791bd0ebabeb6b665bfd4fbe23936baec873439
Ilias Garnier <ilias.garnier@nomadic-labs.com>
2020-08-15 23:16:34 +0200

    Proto/Gas: plug new costs on the interpreter

--- src/proto_alpha/lib_protocol/script_interpreter.ml
+++ src/proto_alpha/lib_protocol/script_interpreter.ml
@@ -173,7 +173,7 @@ let unparse_stack ctxt (stack, stack_ty) =
   in
   unparse_stack (stack_ty, stack)
 
-module Interp_costs = Michelson_v1_gas.Cost_of.Interpreter
+module Interp_costs = Michelson_v1_gas.Cost_of.Interpreter_007
 
 let rec interp_stack_prefix_preserving_operation :
     type fbef bef faft aft result.
@@ -284,273 +284,261 @@ end
 
 let cost_of_instr : type b a. (b, a) descr -> b -> Gas.cost =
  fun descr stack ->
-  let cycle_cost = Interp_costs.cycle in
-  let instr_cost =
-    match (descr.instr, stack) with
-    | (Drop, _) ->
-        Interp_costs.stack_op
-    | (Dup, _) ->
-        Interp_costs.stack_op
-    | (Swap, _) ->
-        Interp_costs.stack_op
-    | (Const _, _) ->
-        Interp_costs.push
-    | (Cons_some, _) ->
-        Interp_costs.wrap
-    | (Cons_none _, _) ->
-        Interp_costs.variant_no_data
-    | (If_none _, _) ->
-        Interp_costs.branch
-    | (Cons_pair, _) ->
-        Interp_costs.pair
-    | (Car, _) ->
-        Interp_costs.pair_access
-    | (Cdr, _) ->
-        Interp_costs.pair_access
-    | (Cons_left, _) ->
-        Interp_costs.wrap
-    | (Cons_right, _) ->
-        Interp_costs.wrap
-    | (If_left _, _) ->
-        Interp_costs.branch
-    | (Cons_list, _) ->
-        Interp_costs.cons
-    | (Nil, _) ->
-        Interp_costs.variant_no_data
-    | (If_cons _, _) ->
-        Interp_costs.branch
-    | (List_map _, (list, _)) ->
-        Interp_costs.list_map list
-    | (List_size, _) ->
-        Interp_costs.push
-    | (List_iter _, (l, _)) ->
-        Interp_costs.list_iter l
-    | (Empty_set _, _) ->
-        Interp_costs.empty_set
-    | (Set_iter _, (set, _)) ->
-        Interp_costs.set_iter set
-    | (Set_mem, (v, (set, _))) ->
-        Interp_costs.set_mem v set
-    | (Set_update, (v, (presence, (set, _)))) ->
-        Interp_costs.set_update v presence set
-    | (Set_size, _) ->
-        Interp_costs.set_size
-    | (Empty_map _, _) ->
-        Interp_costs.empty_map
-    | (Map_map _, (map, _)) ->
-        Interp_costs.map_map map
-    | (Map_iter _, (map, _)) ->
-        Interp_costs.map_iter map
-    | (Map_mem, (v, (map, _rest))) ->
-        Interp_costs.map_mem v map
-    | (Map_get, (v, (map, _rest))) ->
-        Interp_costs.map_get v map
-    | (Map_update, (k, (v, (map, _)))) ->
-        Interp_costs.map_update k v map
-    | (Map_size, _) ->
-        Interp_costs.map_size
-    | (Empty_big_map _, _) ->
-        Interp_costs.empty_map
-    | (Big_map_mem, (key, (map, _))) ->
-        Interp_costs.map_mem key map.diff
-    | (Big_map_get, (key, (map, _))) ->
-        Interp_costs.map_get key map.diff
-    | (Big_map_update, (key, (maybe_value, (map, _)))) ->
-        Interp_costs.map_update key (Some maybe_value) map.diff
-    | (Add_seconds_to_timestamp, (n, (t, _))) ->
-        Interp_costs.add_timestamp t n
-    | (Add_timestamp_to_seconds, (t, (n, _))) ->
-        Interp_costs.add_timestamp t n
-    | (Sub_timestamp_seconds, (t, (s, _))) ->
-        Interp_costs.sub_timestamp t s
-    | (Diff_timestamps, (t1, (t2, _))) ->
-        Interp_costs.diff_timestamps t1 t2
-    | (Concat_string_pair, (_x, (_y, _))) ->
-        Interp_costs.concat_string ~length:2
-    | (Concat_string, (ss, _)) ->
-        Interp_costs.concat_string ~length:ss.Script_typed_ir.length
-    | (Slice_string, (_offset, (length, (_s, _)))) ->
-        let length = Script_int.to_zint length in
-        Interp_costs.slice_string (Z.to_int length)
-    | (String_size, _) ->
-        Interp_costs.push
-    | (Concat_bytes_pair, (_x, (_y, _))) ->
-        Interp_costs.concat_bytes ~length:2
-    | (Concat_bytes, (ss, _)) ->
-        Interp_costs.concat_bytes ~length:ss.Script_typed_ir.length
-    | (Slice_bytes, (_offset, (length, (_s, _)))) ->
-        let length = Script_int.to_zint length in
-        Interp_costs.slice_string (Z.to_int length)
-    | (Bytes_size, _) ->
-        Interp_costs.push
-    | (Add_tez, _) ->
-        Interp_costs.int64_op
-    | (Sub_tez, _) ->
-        Interp_costs.int64_op
-    | (Mul_teznat, _) ->
-        Gas.(Interp_costs.int64_op +@ Interp_costs.z_to_int64)
-    | (Mul_nattez, _) ->
-        Gas.(Interp_costs.int64_op +@ Interp_costs.z_to_int64)
-    | (Or, (x, (y, _))) ->
-        Interp_costs.bool_binop x y
-    | (And, (x, (y, _))) ->
-        Interp_costs.bool_binop x y
-    | (Xor, (x, (y, _))) ->
-        Interp_costs.bool_binop x y
-    | (Not, (x, _)) ->
-        Interp_costs.bool_unop x
-    | (Is_nat, (x, _)) ->
-        Interp_costs.abs x
-    | (Abs_int, (x, _)) ->
-        Interp_costs.abs x
-    | (Int_nat, (x, _)) ->
-        Interp_costs.int x
-    | (Neg_int, (x, _)) ->
-        Interp_costs.neg x
-    | (Neg_nat, (x, _)) ->
-        Interp_costs.neg x
-    | (Add_intint, (x, (y, _))) ->
-        Interp_costs.add x y
-    | (Add_intnat, (x, (y, _))) ->
-        Interp_costs.add x y
-    | (Add_natint, (x, (y, _))) ->
-        Interp_costs.add x y
-    | (Add_natnat, (x, (y, _))) ->
-        Interp_costs.add x y
-    | (Sub_int, (x, (y, _))) ->
-        Interp_costs.sub x y
-    | (Mul_intint, (x, (y, _))) ->
-        Interp_costs.mul x y
-    | (Mul_intnat, (x, (y, _))) ->
-        Interp_costs.mul x y
-    | (Mul_natint, (x, (y, _))) ->
-        Interp_costs.mul x y
-    | (Mul_natnat, (x, (y, _))) ->
-        Interp_costs.mul x y
-    | (Ediv_teznat, (x, (y, _))) ->
-        let open Gas in
-        let x = Script_int.of_int64 (Tez.to_mutez x) in
-        Interp_costs.int64_to_z +@ Interp_costs.div x y
-    | (Ediv_tez, (x, (y, _))) ->
-        let open Gas in
-        let x = Script_int.abs (Script_int.of_int64 (Tez.to_mutez x)) in
-        let y = Script_int.abs (Script_int.of_int64 (Tez.to_mutez y)) in
-        Interp_costs.int64_to_z +@ Interp_costs.int64_to_z
-        +@ Interp_costs.div x y
-    | (Ediv_intint, (x, (y, _))) ->
-        Interp_costs.div x y
-    | (Ediv_intnat, (x, (y, _))) ->
-        Interp_costs.div x y
-    | (Ediv_natint, (x, (y, _))) ->
-        Interp_costs.div x y
-    | (Ediv_natnat, (x, (y, _))) ->
-        Interp_costs.div x y
-    | (Lsl_nat, (x, (y, _))) ->
-        Interp_costs.shift_left x y
-    | (Lsr_nat, (x, (y, _))) ->
-        Interp_costs.shift_right x y
-    | (Or_nat, (x, (y, _))) ->
-        Interp_costs.logor x y
-    | (And_nat, (x, (y, _))) ->
-        Interp_costs.logand x y
-    | (And_int_nat, (x, (y, _))) ->
-        Interp_costs.logand x y
-    | (Xor_nat, (x, (y, _))) ->
-        Interp_costs.logxor x y
-    | (Not_int, (x, _)) ->
-        Interp_costs.lognot x
-    | (Not_nat, (x, _)) ->
-        Interp_costs.lognot x
-    | (Seq _, _) ->
-        Gas.free
-    | (If _, _) ->
-        Interp_costs.branch
-    | (Loop _, _) ->
-        Interp_costs.loop_cycle
-    | (Loop_left _, _) ->
-        Interp_costs.loop_cycle
-    | (Dip _, _) ->
-        Interp_costs.stack_op
-    | (Exec, _) ->
-        Interp_costs.exec
-    | (Apply _, _) ->
-        Interp_costs.apply
-    | (Lambda _, _) ->
-        Interp_costs.push
-    | (Failwith _, _) ->
-        Gas.free
-    | (Nop, _) ->
-        Gas.free
-    | (Compare ty, (a, (b, _))) ->
-        Interp_costs.compare ty a b
-    | (Eq, _) ->
-        Interp_costs.compare_res
-    | (Neq, _) ->
-        Interp_costs.compare_res
-    | (Lt, _) ->
-        Interp_costs.compare_res
-    | (Le, _) ->
-        Interp_costs.compare_res
-    | (Gt, _) ->
-        Interp_costs.compare_res
-    | (Ge, _) ->
-        Interp_costs.compare_res
-    | (Pack _, _) ->
-        Gas.free
-    | (Unpack _, _) ->
-        Gas.free
-    | (Address, _) ->
-        Interp_costs.address
-    | (Contract _, _) ->
-        Interp_costs.contract
-    | (Transfer_tokens, _) ->
-        Interp_costs.transfer
-    | (Create_account, _) ->
-        Interp_costs.create_account
-    | (Implicit_account, _) ->
-        Interp_costs.implicit_account
-    | (Create_contract _, _) ->
-        Interp_costs.create_contract
-    | (Create_contract_2 _, _) ->
-        Interp_costs.create_contract
-    | (Set_delegate, _) ->
-        Interp_costs.set_delegate
-    | (Balance, _) ->
-        Interp_costs.balance
-    | (Now, _) ->
-        Interp_costs.now
-    | (Check_signature, (key, (_, (message, _)))) ->
-        Interp_costs.check_signature key message
-    | (Hash_key, _) ->
-        Interp_costs.hash_key
-    | (Blake2b, (bytes, _)) ->
-        Interp_costs.hash_blake2b bytes
-    | (Sha256, (bytes, _)) ->
-        Interp_costs.hash_sha256 bytes
-    | (Sha512, (bytes, _)) ->
-        Interp_costs.hash_sha512 bytes
-    | (Steps_to_quota, _) ->
-        Interp_costs.steps_to_quota
-    | (Source, _) ->
-        Interp_costs.source
-    | (Sender, _) ->
-        Interp_costs.source
-    | (Self _, _) ->
-        Interp_costs.self
-    | (Amount, _) ->
-        Interp_costs.amount
-    | (Dig (n, _), _) ->
-        Interp_costs.stack_n_op n
-    | (Dug (n, _), _) ->
-        Interp_costs.stack_n_op n
-    | (Dipn (n, _, _), _) ->
-        Interp_costs.stack_n_op n
-    | (Dropn (n, _), _) ->
-        Interp_costs.stack_n_op n
-    | (ChainId, _) ->
-        Interp_costs.chain_id
-  in
-  Gas.(cycle_cost +@ instr_cost)
+  match (descr.instr, stack) with
+  | (Drop, _) ->
+      Interp_costs.drop
+  | (Dup, _) ->
+      Interp_costs.dup
+  | (Swap, _) ->
+      Interp_costs.swap
+  | (Const _, _) ->
+      Interp_costs.push
+  | (Cons_some, _) ->
+      Interp_costs.cons_some
+  | (Cons_none _, _) ->
+      Interp_costs.cons_none
+  | (If_none _, _) ->
+      Interp_costs.if_none
+  | (Cons_pair, _) ->
+      Interp_costs.cons_pair
+  | (Car, _) ->
+      Interp_costs.car
+  | (Cdr, _) ->
+      Interp_costs.cdr
+  | (Cons_left, _) ->
+      Interp_costs.cons_left
+  | (Cons_right, _) ->
+      Interp_costs.cons_right
+  | (If_left _, _) ->
+      Interp_costs.if_left
+  | (Cons_list, _) ->
+      Interp_costs.cons_list
+  | (Nil, _) ->
+      Interp_costs.nil
+  | (If_cons _, _) ->
+      Interp_costs.if_cons
+  | (List_map _, (list, _)) ->
+      Interp_costs.list_map list
+  | (List_size, _) ->
+      Interp_costs.list_size
+  | (List_iter _, (l, _)) ->
+      Interp_costs.list_iter l
+  | (Empty_set _, _) ->
+      Interp_costs.empty_set
+  | (Set_iter _, (set, _)) ->
+      Interp_costs.set_iter set
+  | (Set_mem, (v, (set, _))) ->
+      Interp_costs.set_mem v set
+  | (Set_update, (v, (_, (set, _)))) ->
+      Interp_costs.set_update v set
+  | (Set_size, _) ->
+      Interp_costs.set_size
+  | (Empty_map _, _) ->
+      Interp_costs.empty_map
+  | (Map_map _, (map, _)) ->
+      Interp_costs.map_map map
+  | (Map_iter _, (map, _)) ->
+      Interp_costs.map_iter map
+  | (Map_mem, (v, (map, _rest))) ->
+      Interp_costs.map_mem v map
+  | (Map_get, (v, (map, _rest))) ->
+      Interp_costs.map_get v map
+  | (Map_update, (k, (_, (map, _)))) ->
+      Interp_costs.map_update k map
+  | (Map_size, _) ->
+      Interp_costs.map_size
+  | (Empty_big_map _, _) ->
+      Interp_costs.empty_map
+  | (Big_map_mem, (key, (map, _))) ->
+      Interp_costs.map_mem key map.diff
+  | (Big_map_get, (key, (map, _))) ->
+      Interp_costs.map_get key map.diff
+  | (Big_map_update, (key, (_, (map, _)))) ->
+      Interp_costs.map_update key map.diff
+  | (Add_seconds_to_timestamp, (n, (t, _))) ->
+      Interp_costs.add_seconds_timestamp n t
+  | (Add_timestamp_to_seconds, (t, (n, _))) ->
+      Interp_costs.add_seconds_timestamp n t
+  | (Sub_timestamp_seconds, (t, (n, _))) ->
+      Interp_costs.sub_seconds_timestamp n t
+  | (Diff_timestamps, (t1, (t2, _))) ->
+      Interp_costs.diff_timestamps t1 t2
+  | (Concat_string_pair, (x, (y, _))) ->
+      Interp_costs.concat_string_pair x y
+  | (Concat_string, (ss, _)) ->
+      Interp_costs.concat_string_precheck ss
+  | (Slice_string, (_offset, (_length, (s, _)))) ->
+      Interp_costs.slice_string s
+  | (String_size, _) ->
+      Interp_costs.string_size
+  | (Concat_bytes_pair, (x, (y, _))) ->
+      Interp_costs.concat_bytes_pair x y
+  | (Concat_bytes, (ss, _)) ->
+      Interp_costs.concat_string_precheck ss
+  | (Slice_bytes, (_offset, (_length, (s, _)))) ->
+      Interp_costs.slice_bytes s
+  | (Bytes_size, _) ->
+      Interp_costs.bytes_size
+  | (Add_tez, _) ->
+      Interp_costs.add_tez
+  | (Sub_tez, _) ->
+      Interp_costs.sub_tez
+  | (Mul_teznat, (_, (n, _))) ->
+      Interp_costs.mul_teznat n
+  | (Mul_nattez, (n, (_, _))) ->
+      Interp_costs.mul_teznat n
+  | (Or, _) ->
+      Interp_costs.bool_or
+  | (And, _) ->
+      Interp_costs.bool_and
+  | (Xor, _) ->
+      Interp_costs.bool_xor
+  | (Not, _) ->
+      Interp_costs.bool_not
+  | (Is_nat, _) ->
+      Interp_costs.is_nat
+  | (Abs_int, (x, _)) ->
+      Interp_costs.abs_int x
+  | (Int_nat, _) ->
+      Interp_costs.int_nat
+  | (Neg_int, (x, _)) ->
+      Interp_costs.neg_int x
+  | (Neg_nat, (x, _)) ->
+      Interp_costs.neg_nat x
+  | (Add_intint, (x, (y, _))) ->
+      Interp_costs.add_bigint x y
+  | (Add_intnat, (x, (y, _))) ->
+      Interp_costs.add_bigint x y
+  | (Add_natint, (x, (y, _))) ->
+      Interp_costs.add_bigint x y
+  | (Add_natnat, (x, (y, _))) ->
+      Interp_costs.add_bigint x y
+  | (Sub_int, (x, (y, _))) ->
+      Interp_costs.sub_bigint x y
+  | (Mul_intint, (x, (y, _))) ->
+      Interp_costs.mul_bigint x y
+  | (Mul_intnat, (x, (y, _))) ->
+      Interp_costs.mul_bigint x y
+  | (Mul_natint, (x, (y, _))) ->
+      Interp_costs.mul_bigint x y
+  | (Mul_natnat, (x, (y, _))) ->
+      Interp_costs.mul_bigint x y
+  | (Ediv_teznat, (x, (y, _))) ->
+      Interp_costs.ediv_teznat x y
+  | (Ediv_tez, _) ->
+      Interp_costs.ediv_tez
+  | (Ediv_intint, (x, (y, _))) ->
+      Interp_costs.ediv_bigint x y
+  | (Ediv_intnat, (x, (y, _))) ->
+      Interp_costs.ediv_bigint x y
+  | (Ediv_natint, (x, (y, _))) ->
+      Interp_costs.ediv_bigint x y
+  | (Ediv_natnat, (x, (y, _))) ->
+      Interp_costs.ediv_bigint x y
+  | (Lsl_nat, (x, _)) ->
+      Interp_costs.lsl_nat x
+  | (Lsr_nat, (x, _)) ->
+      Interp_costs.lsr_nat x
+  | (Or_nat, (x, (y, _))) ->
+      Interp_costs.or_nat x y
+  | (And_nat, (x, (y, _))) ->
+      Interp_costs.and_nat x y
+  | (And_int_nat, (x, (y, _))) ->
+      Interp_costs.and_nat x y
+  | (Xor_nat, (x, (y, _))) ->
+      Interp_costs.xor_nat x y
+  | (Not_int, (x, _)) ->
+      Interp_costs.not_nat x
+  | (Not_nat, (x, _)) ->
+      Interp_costs.not_nat x
+  | (Seq _, _) ->
+      Interp_costs.seq
+  | (If _, _) ->
+      Interp_costs.if_
+  | (Loop _, _) ->
+      Interp_costs.loop
+  | (Loop_left _, _) ->
+      Interp_costs.loop_left
+  | (Dip _, _) ->
+      Interp_costs.dip
+  | (Exec, _) ->
+      Interp_costs.exec
+  | (Apply _, _) ->
+      Interp_costs.apply
+  | (Lambda _, _) ->
+      Interp_costs.push
+  | (Failwith _, _) ->
+      Gas.free
+  | (Nop, _) ->
+      Interp_costs.nop
+  | (Compare ty, (a, (b, _))) ->
+      Interp_costs.compare ty a b
+  | (Eq, _) ->
+      Interp_costs.neq
+  | (Neq, _) ->
+      Interp_costs.neq
+  | (Lt, _) ->
+      Interp_costs.neq
+  | (Le, _) ->
+      Interp_costs.neq
+  | (Gt, _) ->
+      Interp_costs.neq
+  | (Ge, _) ->
+      Interp_costs.neq
+  | (Pack _, _) ->
+      Gas.free
+  | (Unpack _, _) ->
+      Gas.free
+  | (Address, _) ->
+      Interp_costs.address
+  | (Contract _, _) ->
+      Interp_costs.contract
+  | (Transfer_tokens, _) ->
+      Interp_costs.transfer_tokens
+  | (Implicit_account, _) ->
+      Interp_costs.implicit_account
+  | (Create_contract _, _) ->
+      Interp_costs.create_contract
+  | (Set_delegate, _) ->
+      Interp_costs.set_delegate
+  | (Balance, _) ->
+      Interp_costs.balance
+  | (Now, _) ->
+      Interp_costs.now
+  | (Check_signature, (key, (_, (message, _)))) ->
+      Interp_costs.check_signature key message
+  | (Hash_key, (pk, _)) ->
+      Interp_costs.hash_key pk
+  | (Blake2b, (bytes, _)) ->
+      Interp_costs.blake2b bytes
+  | (Sha256, (bytes, _)) ->
+      Interp_costs.sha256 bytes
+  | (Sha512, (bytes, _)) ->
+      Interp_costs.sha512 bytes
+  | (Source, _) ->
+      Interp_costs.source
+  | (Sender, _) ->
+      Interp_costs.source
+  | (Self _, _) ->
+      Interp_costs.self
+  | (Amount, _) ->
+      Interp_costs.amount
+  | (Dig (n, _), _) ->
+      Interp_costs.dign n
+  | (Dug (n, _), _) ->
+      Interp_costs.dugn n
+  | (Dipn (n, _, _), _) ->
+      Interp_costs.dipn n
+  | (Dropn (n, _), _) ->
+      Interp_costs.dropn n
+  | (ChainId, _) ->
+      Interp_costs.chain_id
+  | (Create_account, _) ->
+      Interp_costs.create_contract
+  | (Create_contract_2 _, _) ->
+      Interp_costs.create_contract
+  | (Steps_to_quota, _) ->
+      Interp_costs.push
 
 let rec step :
     type b a.
