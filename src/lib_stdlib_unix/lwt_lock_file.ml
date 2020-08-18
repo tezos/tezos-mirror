773e9133052c1ee948715e6023c2ef18937af10f
Raphaël Proust <code@bnwr.net>
2020-08-18 17:59:53 +0200

    Stdlib_unix: remove unused functions from Lwt_lock_file

--- src/lib_stdlib_unix/lwt_lock_file.ml
+++ src/lib_stdlib_unix/lwt_lock_file.ml
@@ -25,29 +25,17 @@
 
 open Error_monad
 
-let create_inner lock_command ?(close_on_exec = true) ?(unlink_on_exit = false)
-    fn =
+let create ?(close_on_exec = true) ?(unlink_on_exit = false) fn =
   protect (fun () ->
       Lwt_unix.openfile fn Unix.[O_CREAT; O_WRONLY; O_TRUNC] 0o644
       >>= fun fd ->
       if close_on_exec then Lwt_unix.set_close_on_exec fd ;
-      Lwt_unix.lockf fd lock_command 0
+      Lwt_unix.lockf fd Unix.F_TLOCK 0
       >>= fun () ->
       if unlink_on_exit then Lwt_main.at_exit (fun () -> Lwt_unix.unlink fn) ;
       let pid_str = string_of_int @@ Unix.getpid () in
       Lwt_unix.write_string fd pid_str 0 (String.length pid_str)
       >>= fun _ -> return_unit)
-
-let create = create_inner Unix.F_TLOCK
-
-let blocking_create ?timeout ?(close_on_exec = true) ?(unlink_on_exit = false)
-    fn =
-  let create () = create_inner Unix.F_LOCK ~close_on_exec ~unlink_on_exit fn in
-  match timeout with
-  | None ->
-      create ()
-  | Some duration ->
-      with_timeout (Lwt_unix.sleep duration) (fun _ -> create ())
 
 let is_locked fn =
   if not @@ Sys.file_exists fn then return_false
@@ -62,9 +50,3 @@ let is_locked fn =
               (fun () -> return_false)
               (fun _ -> return_true))
           (fun () -> Lwt_unix.close fd))
-
-let get_pid fn =
-  let open Lwt_io in
-  protect (fun () ->
-      with_file ~mode:Input fn (fun ic ->
-          read ic >>= fun content -> return (int_of_string content)))
