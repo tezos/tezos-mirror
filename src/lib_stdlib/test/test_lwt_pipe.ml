d3d416b8e786c67414212d66d09f6ee0947af6c0
Raphaël Proust <code@bnwr.net>
2020-08-19 15:56:59 +0200

    Stdlib: short explanation for Lwt_pipe test

--- src/lib_stdlib/test/test_lwt_pipe.ml
+++ src/lib_stdlib/test/test_lwt_pipe.ml
@@ -1,7 +1,7 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2020 Nomadic Labs <contact@nomadic-labs.com>                *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -22,6 +22,16 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
+
+(**
+
+  This is a test for the Lwt_pipe module.
+
+  It tests mainly the core functionality: pushing/popping, and size limitation.
+  An additional test (introspection) runs through a pre-made scenario, calling
+  functions one after the other, and checks internal state as it evolves.
+
+*)
 
 open Lwt.Infix
 
