476ab06d244cda6bb250c085b5f951858ba3eda9
Raphaël Proust <code@bnwr.net>
2020-08-19 15:29:22 +0200

    Stdlib: improved doc for Lwt_pipe

--- src/lib_stdlib/lwt_pipe.mli
+++ src/lib_stdlib/lwt_pipe.mli
@@ -30,51 +30,96 @@
 (** Type of queues holding values of type ['a]. *)
 type 'a t
 
-(** [create ~size:(max_size, compute_size)] is an empty queue that can
-    hold max [size] bytes of data, using [compute_size] to compute the
-    size of a datum. If want to count allocated bytes precisely, you
-    need to add [push_overhead] to the result of[compute_size].
-    When no [size] argument is provided, the queue is unbounded. *)
+(** [create ~size:(max_size, compute_size) ()] is an empty queue that can
+    hold max [size] "bytes" of data, using [compute_size] to compute the
+    number of "bytes" in a datum.
+
+    Note that you can use [size] to actually limit the size in byte (i.e., the
+    memory foot-print of the structure (in this case, consider using
+    {!push_overhead} to account for the boilerplate memory), but you can also
+    use [size] to limit the foot-print of the structure for some other resource.
+    E.g., you can spin up tasks in separate processes and limit the number of
+    concurrently running processes.
+
+    Also note that the size bound is not inclusive. So with [size] set to
+    [(2, fun _ -> 1)] you can add one (1) element and then the pipe is full. (It
+    is full because adding any other element would take the total size to [2]
+    which is not strictly smaller than the [max_size] bound.)
+
+    If you do not provide a [size] argument, the queue is unbounded. *)
 val create : ?size:int * ('a -> int) -> unit -> 'a t
 
-(** [push q v] is a promise that blocks while [q] contains more
-    than [size] elements, then adds [v] at the end of [q]. *)
+(** [push q v] is a promise that is pending until there is enough space in [q]
+    to accommodate [v]. When this happens [v] is added to the end of [q] and the
+    promise resolves.
+
+    If there is enough space in [q] to accommodate [v] when the call is made,
+    then the [v] is added immediately and an already resolved promise is
+    returned.
+
+    Note that if several writes are stuck because the pipe is full. These
+    writes will succeed in an order that might be different from the order the
+    write attempts were made. Specifically, when pushing elements of different
+    computed sizes, smaller pushes may be resolved earlier if enough space is
+    freed.
+
+    @raise {!Closed} if [q] is closed. More specifically, the promise is
+    rejected with {!Closed} if [q] is closed. *)
 val push : 'a t -> 'a -> unit Lwt.t
 
-(** [pop q] is a promise that blocks while [q] is empty, then
-    removes and returns the first element in [q].
+(** [pop q] is a promise that is pending until there is an element in [q]. When
+    this happens an element is removed and the promise is fulfilled with it.
 
-    The promise is rejected with [Closed] if [q] has been {!close}d *)
+    If there is already an element in [q] when the call is made, the element is
+    removed immediately and an already resolved promise is returned.
+
+    @raise {!Closed} if [q] is empty and closed. More specifically, the promise
+    is rejected with {!Closed} if [q] is empty and closed. *)
 val pop : 'a t -> 'a Lwt.t
 
-(** [pop_with_timeout t q] is a promise that blocks while [q] is empty, then
-    removes the first element [v] in [q] and returns [Some v].
+(** [pop_with_timeout t q] is a promise that behaves similarly to [pop q]
+    except that it resolves with [None] if [t] resolves before there is an
+    element in [q] to pop.
 
-    If no message can be popped before [t] resolves, it returns [None].
-    As concurrent readers are allowed, [None] does not
-    necessarily mean that no value has been pushed.
+    Note that there can be multiple promises that are awaiting for an element to
+    pop from the queue. As a result, it is possible that [pop_with_timeout] is
+    fulfilled with [None] even though values have been pushed to the [q].
 
     [t] is canceled (i.e., it fails with [Canceled]) if an element is returned.
 
-    The promise is rejected with [Closed] if [q] has been {!close}d
-*)
+    @raise {!Closed} if [q] is empty and closed. More specifically, the promise
+    is rejected with {!Closed} if [q] is empty and closed. *)
 val pop_with_timeout : unit Lwt.t -> 'a t -> 'a option Lwt.t
 
-(** [pop_all q] is a promise that blocks while [q] is empty, then
-    removes and returns all the elements in [q] (in the order in which they
-    were inserted). *)
+(** [pop_all q] is a promise that is pending until there is an element in [q].
+    When this happens, all the elements of [q] are removed and the promise is
+    fulfilled with the list of elements (in the order in which they were
+    inserted).
+
+    If there is already an element in [q] when the call is made, the elements
+    are removed immediately and an already resolved promise is returned.
+
+    @raise {!Closed} if [q] is empty and closed. More specifically, the promise
+    is rejected with {!Closed} if [q] is empty and closed. *)
 val pop_all : 'a t -> 'a list Lwt.t
 
-(** [pop_all_now q] removes and returns all the element in [q] (in the order in
-    which they were inserted). If [q] is empty, [[]] is returned. *)
+(** [pop_all_now q] removes and returns all the elements in [q] (in the order in
+    which they were inserted). If [q] is empty, [[]] is returned.
+
+    @raise {!Closed} if [q] is empty and closed. *)
 val pop_all_now : 'a t -> 'a list
 
-(** [peek q] returns the same value as [pop q] but does not remove the first
-    element. *)
+(** [peek q] returns the same value as [pop q] but does not remove the returned
+    element.
+
+    @raise {!Closed} if [q] is empty and closed. More specifically, the promise
+    is rejected with {!Closed} if [q] is empty and closed. *)
 val peek : 'a t -> 'a Lwt.t
 
-(** [peek_all q] returns the elements in the [q] (oldest first),
-    or [[]] if empty. *)
+(** [peek_all q] returns the elements in the [q] (oldest first), or [[]] if
+    empty. It does not remove elements from [q].
+
+    @raise {!Closed} if [q] is empty and closed. *)
 val peek_all : 'a t -> 'a list
 
 (** [push_now q v] either
@@ -95,24 +140,37 @@ val is_empty : 'a t -> bool
 (** [empty q] is a promise that resolves when [q] becomes empty. *)
 val empty : 'a t -> unit Lwt.t
 
-(** [iter q ~f] pops all elements of [q] and applies [f] on them. *)
+(** [iter q ~f] pops each element of [q] and applies [f] on it until [q] is
+    empty.
+
+    Note that the elements are popped on a by-need basis, i.e., when the
+    previous iteration is resolved. Thus, concurrent [pop] may cause [iter] to
+    miss some elements, and concurrent [push] may cause [iter] to never resolve.
+
+    Also note that an exception raised by [f] will cause the iteration to stop
+    and to be rejected with the same exception.
+
+    Also note that if [q] is closed, the iterations are interrupted when [q] is
+    empty and it resolves with [()].
+
+    If you want to iterate over all the current elements of [q], you need to
+    iterate over the list returned by [pop_all]. *)
 val iter : 'a t -> f:('a -> unit Lwt.t) -> unit Lwt.t
 
 exception Closed
 
 (** [close q] the write-end of [q]:
 
-    * Future write attempts will fail with [Closed].
-    * If there are reads blocked, they will unblock and fail with [Closed].
+    * Future write attempts will fail with {!Closed}.
+    * If there are pending reads, they will become rejected with {!Closed}.
     * Future read attempts will drain the data until there is no data left (at
-      which point [Closed] may be raised).
+      which point {!Closed} may be raised).
 
-    Thus, after a pipe has been closed, reads never block.
-    The [close] function is idempotent.
-*)
+    The [close] function is idempotent. *)
 val close : 'a t -> unit
 
 val is_closed : 'a t -> bool
 
-(** The allocated size in bytes when pushing in the queue. *)
+(** The number of bytes used in the internal representation to hold an element
+    in the queue. *)
 val push_overhead : int
