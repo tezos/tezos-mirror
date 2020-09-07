0ccce29c841cec365d2e5d30efcea3bfae5e854d
Clément Hurlin <clement.hurlin@tweag.io>
2020-09-07 15:48:40 +0200

    Tests/python: proxy: test cache is created at most once
    Cache must be created at most once for every concerned (chain, block) pair.

--- tests_python/tests_alpha/test_proxy.py
+++ tests_python/tests_alpha/test_proxy.py
@@ -2,7 +2,7 @@
 """
 import shutil
 import time
-from typing import Any, Iterator, Tuple
+from typing import Any, Iterator, List, Tuple
 import re
 import pytest
 from client.client import Client
@@ -13,13 +13,33 @@ from tools.constants import ALPHA, CARTHAGE
 
 _OTHER_PROTO = CARTHAGE
 
-_PROXY_RPC_LOG = {"TEZOS_LOG":"alpha.proxy_rpc->debug"}
+_PROXY_RPC_LOG = {"TEZOS_LOG": "alpha.proxy_rpc->debug"}
 _BLOCK_ID = "head"
 _CHAIN_ID = "main"
 _GIVER = "bootstrap1"
 _PROTO = ALPHA
 _RPC_PATH = f'/chains/{_CHAIN_ID}/blocks/{_BLOCK_ID}'
 _PROXY_RPC_CONTEXT_LOG = {"TEZOS_LOG": "proxy_rpc_ctxt->debug"}
+_COMPARED_PATHS = [
+    f'{_RPC_PATH}/context/constants',
+    f'{_RPC_PATH}/helpers/baking_rights',
+    f'{_RPC_PATH}/helpers/baking_rights?&all=true',
+    f'{_RPC_PATH}/helpers/current_level',
+    f'{_RPC_PATH}/minimal_valid_time',
+    f'{_RPC_PATH}/context/constants',
+    f'{_RPC_PATH}/context/constants/errors',
+    f'{_RPC_PATH}/context/delegates',
+    f'{_RPC_PATH}/context/nonces/3',
+    f'{_RPC_PATH}/helpers/endorsing_rights',
+    f'{_RPC_PATH}/helpers/levels_in_current_cycle',
+    f'{_RPC_PATH}/votes/ballot_list',
+    f'{_RPC_PATH}/votes/ballots',
+    f'{_RPC_PATH}/votes/current_period_kind',
+    f'{_RPC_PATH}/votes/current_proposal',
+    f'{_RPC_PATH}/votes/current_quorum',
+    f'{_RPC_PATH}/votes/listings',
+    f'{_RPC_PATH}/votes/proposals',
+]
 
 
 def _create_proxy_for_node(sandbox, node) -> Client:
@@ -137,28 +157,7 @@ def vanilla_and_proxy_clients(sandbox) -> Iterator[Tuple[Client, Client]]:
 
 @pytest.mark.client
 @pytest.mark.slow
-@pytest.mark.parametrize(
-    'path',
-    [
-        f'{_RPC_PATH}/context/constants',
-        f'{_RPC_PATH}/helpers/baking_rights',
-        f'{_RPC_PATH}/helpers/current_level',
-        f'{_RPC_PATH}/minimal_valid_time',
-        f'{_RPC_PATH}/context/constants',
-        f'{_RPC_PATH}/context/constants/errors',
-        f'{_RPC_PATH}/context/delegates',
-        f'{_RPC_PATH}/context/nonces/3',
-        f'{_RPC_PATH}/helpers/endorsing_rights',
-        f'{_RPC_PATH}/helpers/levels_in_current_cycle',
-        f'{_RPC_PATH}/votes/ballot_list',
-        f'{_RPC_PATH}/votes/ballots',
-        f'{_RPC_PATH}/votes/current_period_kind',
-        f'{_RPC_PATH}/votes/current_proposal',
-        f'{_RPC_PATH}/votes/current_quorum',
-        f'{_RPC_PATH}/votes/listings',
-        f'{_RPC_PATH}/votes/proposals',
-    ],
-)
+@pytest.mark.parametrize('path', _COMPARED_PATHS)
 def test_compare(vanilla_and_proxy_clients: Tuple[Client, Client], path: str):
     """Compare the default client and the proxy clients on
     a number of RPCs that are done locally by the proxy client
@@ -199,51 +198,55 @@ def test_wrong_proto(proxy_client):
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize('path',
-                         [f'{_RPC_PATH}/helpers/baking_rights',
-                          f'{_RPC_PATH}/helpers/baking_rights?&all=true',
-                          f'{_RPC_PATH}/context/delegates',
-                          f'{_RPC_PATH}/context/nonces/3',
-                          f'{_RPC_PATH}/helpers/endorsing_rights',
-                          f'{_RPC_PATH}/votes/ballot_list',
-                          f'{_RPC_PATH}/votes/ballots',
-                          f'{_RPC_PATH}/votes/current_period_kind',
-                          f'{_RPC_PATH}/votes/current_proposal',
-                          f'{_RPC_PATH}/votes/current_quorum',
-                          f'{_RPC_PATH}/votes/listings',
-                          f'{_RPC_PATH}/votes/proposals'])
+@pytest.mark.parametrize(
+    'path',
+    [
+        f'{_RPC_PATH}/helpers/baking_rights',
+        f'{_RPC_PATH}/helpers/baking_rights?&all=true',
+        f'{_RPC_PATH}/context/delegates',
+        f'{_RPC_PATH}/context/nonces/3',
+        f'{_RPC_PATH}/helpers/endorsing_rights',
+        f'{_RPC_PATH}/votes/ballot_list',
+        f'{_RPC_PATH}/votes/ballots',
+        f'{_RPC_PATH}/votes/current_period_kind',
+        f'{_RPC_PATH}/votes/current_proposal',
+        f'{_RPC_PATH}/votes/current_quorum',
+        f'{_RPC_PATH}/votes/listings',
+        f'{_RPC_PATH}/votes/proposals',
+    ],
+)
 def test_context_suffix_no_rpc(proxy_client: Client, path: str):
     """
-        This test checks that the proxy client never does a useless RPC.
+    This test checks that the proxy client never does a useless RPC.
 
-        I.e. it checks that if the proxy client requested
-        `/chains/<main>/blocks/<head>/context/raw/bytes/some_path`
-        it doesn't later request
-        `/chains/<main>/blocks/<head>/context/raw/bytes/some_other_path`
+    I.e. it checks that if the proxy client requested
+    `/chains/<main>/blocks/<head>/context/raw/bytes/some_path`
+    it doesn't later request
+    `/chains/<main>/blocks/<head>/context/raw/bytes/some_other_path`
 
-        with `some_other_path` being a suffix of `some_path`. In this
-        scenario, the proxy client should look for the tail of
-        `some_other_path` that is a suffix of `some_path` and use this
-        suffix to get the data within the tree received by the first request.
+    with `some_other_path` being a suffix of `some_path`. In this
+    scenario, the proxy client should look for the tail of
+    `some_other_path` that is a suffix of `some_path` and use this
+    suffix to get the data within the tree received by the first request.
 
-        For this, this test inspects the debug output produced by
-        setting TEZOS_LOG to alphas.proxy_rpc->debug. This causes the client
-        to print the RPCs done to get pieces of the context:
+    For this, this test inspects the debug output produced by
+    setting TEZOS_LOG to alphas.proxy_rpc->debug. This causes the client
+    to print the RPCs done to get pieces of the context:
 
-        alpha.proxy_rpc: P/v1/constants
-        alpha.proxy_rpc: Received tree of size 1
-        alpha.proxy_rpc: P/v1/first_level
-        alpha.proxy_rpc: Received tree of size 1
-        alpha.proxy_rpc: P/cycle/0/random_seed
-        alpha.proxy_rpc: Received tree of size 1
-        alpha.proxy_rpc: P/cycle/0/roll_snapshot
-        alpha.proxy_rpc: Received tree of size 1
-        alpha.proxy_rpc: P/cycle/0/last_roll/0
+    alpha.proxy_rpc: P/v1/constants
+    alpha.proxy_rpc: Received tree of size 1
+    alpha.proxy_rpc: P/v1/first_level
+    alpha.proxy_rpc: Received tree of size 1
+    alpha.proxy_rpc: P/cycle/0/random_seed
+    alpha.proxy_rpc: Received tree of size 1
+    alpha.proxy_rpc: P/cycle/0/roll_snapshot
+    alpha.proxy_rpc: Received tree of size 1
+    alpha.proxy_rpc: P/cycle/0/last_roll/0
 
-        where P is /chains/<main>/blocks/<head>/context/raw/bytes
+    where P is /chains/<main>/blocks/<head>/context/raw/bytes
 
-        In the client, this behavior is implemented by putting the result
-        of RPC calls in a trie.
+    In the client, this behavior is implemented by putting the result
+    of RPC calls in a trie.
     """
     cmd = ["rpc", "get", path]
     (_, stderr, _) = proxy_client.run_generic(cmd, env_change=_PROXY_RPC_LOG)
@@ -263,9 +266,11 @@ def test_context_suffix_no_rpc(proxy_client: Client, path: str):
     assert len(context_queries) >= 2, "There should be at least two queries"
 
     def gen_msg(query_before, query):
-        return f"Query {query_before} should not be followed by query" + \
-               f" {query} because the latter is a suffix of the former." + \
-               " Hence the proxy should reuse the data of the first query."
+        return (
+            f"Query {query_before} should not be followed by query"
+            + f" {query} because the latter is a suffix of the former."
+            + " Hence the proxy should reuse the data of the first query."
+        )
 
     i = len(context_queries) - 1
     while i >= 0:
@@ -273,7 +278,65 @@ def test_context_suffix_no_rpc(proxy_client: Client, path: str):
         j = i - 1
         while j >= 0:
             query_before = context_queries[j]
-            assert not query_before.startswith(query), gen_msg(query_before,
-                                                               query)
+            assert not query_before.startswith(query), gen_msg(
+                query_before, query
+            )
             j -= 1
         i -= 1
+
+
+def _run_cmd_track_caches_creation(
+    client: Client, cmd: List[str]
+) -> List[Tuple[str, str]]:
+    """
+    Run the given command and inspects stdout to keep track of such lines:
+
+    ... alpha.proxy_rpc: proxy cache created for chain main and block head
+
+    This method returns the sequence of pairs (chain, blocks) for which
+    such lines have been recognized
+    """
+    (_, stderr, _) = client.run_generic(cmd, env_change=_PROXY_RPC_LOG)
+    if not stderr:
+        return []
+
+    pattern = (
+        r"^.*proxy_rpc: proxy cache created for "
+        + r"chain (\w+) and block (\w+)"
+    )
+
+    result = []
+    for line in stderr.split("\n"):
+        match = re.search(pattern, line)
+        if match:
+            grps = match.groups()
+            result.append((grps[0], grps[1]))
+
+    return result
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize(
+    'cmd', [["rpc", "get", path] for path in _COMPARED_PATHS]
+)
+def test_cache_at_most_once(proxy_client: Client, cmd: List[str]):
+    """
+    This test checks that the proxy client creates its cache for
+    RPC answers at most once for a given (chain, block) pair.
+    """
+    pairs = _run_cmd_track_caches_creation(proxy_client, cmd)
+    assert (
+        pairs
+    ), "Proxy cache should have been created when executing " + " ".join(cmd)
+
+    def _gen_msg(chain_n_block: Tuple[str, str]) -> str:
+        return (
+            f"proxy RPC cache for chain {chain_n_block[0]}"
+            + f" and block {chain_n_block[1]} created more than once"
+        )
+
+    created = set()
+    for pair in pairs:
+        if pair in created:
+            assert False, _gen_msg(pair)
+        created.add(pair)
