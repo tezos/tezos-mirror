33be80f9ca9aa1ec690f97afdc1f4bf5d6881c5a
Clément Hurlin <clement.hurlin@tweag.io>
2020-09-20 15:03:17 +0200

    proxy: reduce RPC calls when being resilient to head changing on node

--- src/lib_proxy/proxy_services.ml
+++ src/lib_proxy/proxy_services.ml
@@ -24,11 +24,19 @@
 (*****************************************************************************)
 
 module type BLOCK_TO_HASH = sig
+  (** A [None] result value means the caller should use the block passed
+      as third argument, even if not identified by a hash *)
   val hash_of_block :
     #RPC_context.simple ->
     Tezos_shell_services.Shell_services.chain ->
     Tezos_shell_services.Block_services.block ->
-    Block_hash.t tzresult Lwt.t
+    Block_hash.t option tzresult Lwt.t
+
+  val add :
+    Tezos_shell_services.Shell_services.chain ->
+    Tezos_shell_services.Block_services.block ->
+    Block_hash.t ->
+    unit
 end
 
 module Hashtbl = Stdlib.Hashtbl
@@ -44,25 +52,46 @@ module BlockToHash (S : Registration.Proxy_sig) : BLOCK_TO_HASH = struct
 
   let table = Hashtbl.create 17
 
+  let add chain block hash = Hashtbl.add table (chain, block) hash
+
   let hash_of_block (rpc_context : #RPC_context.simple)
       (chain : Tezos_shell_services.Shell_services.chain)
       (block : Tezos_shell_services.Block_services.block) =
     match raw_hash_of_block block with
     | Some h ->
         (* Block is defined by its hash *)
-        return h
+        return_some h
     | None -> (
       match Hashtbl.find_opt table (chain, block) with
       | Some hash ->
           (* Result is in cache *)
-          return hash
+          return_some hash
       | None ->
-          (* Need to do an RPC call to get the hash *)
-          S.hash rpc_context ~chain ~block ()
-          >>=? fun hash ->
-          (* Fill cache with result *)
-          Hashtbl.add table (chain, block) hash ;
-          return hash )
+          if Hashtbl.length table = 0 then
+            (* The table is empty. We do not need to retrieve the hash
+               before retrieving the initial context, because we have
+               no reference hash yet. I mean that, if block is <head>,
+               we are about to retrieve the node's head, no matter its value.
+               Any value is fine and its hash is going to be put right away in
+               the cache (see the call to [B2H.add]).
+               This avoids one RPC call, but it is
+               important because it is the first one: often there is
+               a single call. Skipping it reduces the node's load.
+               
+               In the heavyduty.py scenario (see the proxy's original MR:
+               !1943), it reduces the number of calls to RPC .../hash
+               from 1200 to 700.
+             *)
+            return_none
+          else
+            (* Table is not empty, We need to be consistent with the previous call
+               and we dont have the data available:
+               need to do an RPC call to get the hash *)
+            S.hash rpc_context ~chain ~block ()
+            >>=? fun hash ->
+            (* Fill cache with result *)
+            Hashtbl.add table (chain, block) hash ;
+            return_some hash )
 end
 
 let build_directory (printer : Tezos_client_base.Client_context.printer)
@@ -73,16 +102,26 @@ let build_directory (printer : Tezos_client_base.Client_context.printer)
   let envs_cache = Hashtbl.create 17 in
   let get_env_rpc_context chain block =
     B2H.hash_of_block rpc_context chain block
-    >>=? fun block_hash ->
-    let key = (chain, block_hash) in
+    >>=? fun block_hash_opt ->
+    let (block_key, fill_b2h) =
+      match block_hash_opt with
+      | None ->
+          ( block,
+            fun (rpc_context : Tezos_protocol_environment.rpc_context) ->
+              B2H.add chain block rpc_context.block_hash )
+      | Some block_hash ->
+          (`Hash (block_hash, 0), ignore)
+    in
+    let key = (chain, block_key) in
     match Hashtbl.find_opt envs_cache key with
     | None ->
         Proxy_environment.init_env_rpc_context
           printer
           rpc_context
           chain
-          (`Hash (block_hash, 0))
+          block_key
         >>=? fun rpc_context ->
+        fill_b2h rpc_context ;
         Hashtbl.add envs_cache key rpc_context ;
         return rpc_context
     | Some cached ->
