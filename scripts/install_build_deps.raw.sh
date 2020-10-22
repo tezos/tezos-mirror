cce2d9ca409f12d19812de137ad3015bb7e25534
Romain Bardou <romain@nomadic-labs.com>
2020-10-22 14:38:39 +0200

    Build: do not use opam depext to install cargo

--- scripts/install_build_deps.raw.sh
+++ scripts/install_build_deps.raw.sh
@@ -12,7 +12,12 @@ export OPAMYES=${OPAMYES:=true}
 ## In another ideal world, this list should be extracted from the pinned
 ## packages and filter only conf-* packages
 
-opam depext conf-gmp conf-libev conf-m4 conf-perl conf-pkg-config conf-hidapi ctypes-foreign conf-autoconf conf-libffi conf-rust
+# conf-rust is commented out because we need users to install a specific version of Rust.
+# If we use opam depext, it will probably not install the right version.
+# Note that install_build_deps.sh calls install_build_deps.rust.sh
+# which checks whether Rust is installed with the right version and explains how
+# to install it if needed, so using opam depext is redundant anyway.
+opam depext conf-gmp conf-libev conf-m4 conf-perl conf-pkg-config conf-hidapi ctypes-foreign conf-autoconf conf-libffi #conf-rust
 
 ## In an ideal world, `--with-test` should be present only when using
 ## `--dev`. But this would probably break the CI, so we postponed this
