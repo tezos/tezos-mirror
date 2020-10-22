afa1e2d126526336ede087049a58b53c6fe30b6c
Danny Willems <contact@danny-willems.be>
2020-10-22 15:21:31 +0000

    Add package pycryptodome to poetry
    This is needed to test Keccak and Sha3

--- scripts/version.sh
+++ scripts/version.sh
@@ -12,7 +12,7 @@ opam_version=2.0
 full_opam_repository_tag=1399f13a876c930a5f11b87f7e5e309ab25345e3
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=0c374d7d54f4c4dcb39ae94f67b7fb6fb6f9eea6
+opam_repository_tag=d9dc192fec94252ced7ff5bd6d4e692de99e1d2e
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
