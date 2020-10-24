92afb63fe689b864a059775a8916fb55886b3b47
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2020-10-24 10:31:52 +0200

    Build: refresh opam repo for hacl-star.0.3

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,10 +9,10 @@ opam_version=2.0
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=1399f13a876c930a5f11b87f7e5e309ab25345e3
+full_opam_repository_tag=9f32db57625b3a40a1942b121452fdc3bd0677c4
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=d9dc192fec94252ced7ff5bd6d4e692de99e1d2e
+opam_repository_tag=471779d7aff3d75c3d0b060e2143065d6b3002cd
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
