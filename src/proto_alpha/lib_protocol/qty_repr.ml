458059518f4fb4b2f41a132eeea18835f389b708
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2020-10-30 17:11:02 +0100

    Proto: qty_repr removal, part 3
    Remove Qty_repr.S

--- src/proto_alpha/lib_protocol/qty_repr.ml
+++ src/proto_alpha/lib_protocol/qty_repr.ml
@@ -29,67 +29,7 @@ module type QTY = sig
   val name : string
 end
 
-module type S = sig
-  type qty
-
-  type error +=
-    | Addition_overflow of qty * qty (* `Temporary *)
-    | Subtraction_underflow of qty * qty (* `Temporary *)
-    | Multiplication_overflow of qty * int64 (* `Temporary *)
-    | Negative_multiplicator of qty * int64 (* `Temporary *)
-    | Invalid_divisor of qty * int64
-
-  (* `Temporary *)
-
-  val id : string
-
-  val zero : qty
-
-  val one_mutez : qty
-
-  val one_cent : qty
-
-  val fifty_cents : qty
-
-  val one : qty
-
-  val ( -? ) : qty -> qty -> qty tzresult
-
-  val ( +? ) : qty -> qty -> qty tzresult
-
-  val ( *? ) : qty -> int64 -> qty tzresult
-
-  val ( /? ) : qty -> int64 -> qty tzresult
-
-  val to_mutez : qty -> int64
-
-  (** [of_mutez n] (micro tez) is None if n is negative *)
-  val of_mutez : int64 -> qty option
-
-  (** [of_mutez_exn n] fails if n is negative.
-      It should only be used at toplevel for constants. *)
-  val of_mutez_exn : int64 -> qty
-
-  (** It should only be used at toplevel for constants. *)
-  val add_exn : qty -> qty -> qty
-
-  (** It should only be used at toplevel for constants. *)
-  val mul_exn : qty -> int -> qty
-
-  val encoding : qty Data_encoding.t
-
-  val to_int64 : qty -> int64
-
-  include Compare.S with type t := qty
-
-  val pp : Format.formatter -> qty -> unit
-
-  val of_string : string -> qty option
-
-  val to_string : qty -> string
-end
-
-module Make (T : QTY) : S = struct
+module Make (T : QTY) = struct
   type qty = int64 (* invariant: positive *)
 
   type error +=
