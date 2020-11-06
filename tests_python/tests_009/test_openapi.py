f0651d4e949eda27257a30983ac085956ac14bc6
Danny Willems <be.danny.willems@gmail.com>
2020-11-06 16:41:59 +0100

    Test: Validate OpenAPI spec

--- tests_python/tests_009/test_openapi.py
+++ tests_python/tests_009/test_openapi.py
@@ -9,87 +9,65 @@
 """
 
 import json
-import os
 import subprocess
-import tempfile
+from pathlib import Path
 import requests
+import openapi_spec_validator
+import pytest
+
 from launchers.sandbox import Sandbox
 from tools.constants import NODE_PARAMS
-
 from . import protocol
 
 
-def _get_version() -> str:
+def _get_tezos_node_version() -> str:
     cmd = ["ocaml", "../scripts/print_version.ml"]
-    print(" ".join(cmd))
     process_ret = subprocess.run(
         cmd, check=True, capture_output=True, text=True
     )
-    return process_ret.stdout.strip()
-
-
-def _write_file(path: str, content: str):
-    with open(path, 'w') as handle:
-        handle.write(content)
-
-
-def _try_json_loads(addr: str, content: str):
-    """
-    `addr` is the address from which `content` was obtained. `addr`
-    is only used for generating a faulty's assert message.
-    """
-    try:
-        json.loads(content)
-    except json.JSONDecodeError:
-        assert False, f"{addr} did not return valid json"
-
-
-def test_openapi(sandbox: Sandbox):
-    """Mimicks the script src/openapi/generate.sh. Generates the API
-    and check it generates valid json."""
-    node_idx = 0
-    sandbox.add_node(0, params=NODE_PARAMS)
-    node = sandbox.node(node_idx)
-
-    client = sandbox.client(0)
-    protocol.activate(client)
-
-    api_addr = f"http://localhost:{node.rpc_port}/describe/?recurse=yes"
-    api = requests.get(api_addr).text
-    _try_json_loads(api_addr, api)
-
-    proto_api_addr = (
-        f"http://localhost:{node.rpc_port}"
-        + "/describe/chains/main/blocks/head?recurse=yes"
-    )
-    proto_api = requests.get(proto_api_addr).text
-    _try_json_loads(proto_api_addr, proto_api)
-
-    version = _get_version()
+    version = process_ret.stdout.strip()
     assert version, "version should not be empty"
+    return version
 
-    with tempfile.TemporaryDirectory(prefix='tezos-openapi-') as base_dir:
-        api_path = os.path.join(base_dir, "api.json")
-        proto_api_path = os.path.join(base_dir, "proto_api.json")
 
-        for (path, content) in [(api_path, api), (proto_api_path, proto_api)]:
-            _write_file(path, content)
+class TestOpenAPI:
+    @pytest.fixture(scope="class")
+    def sandbox(self, sandbox: Sandbox):
+        sandbox.add_node(0, params=NODE_PARAMS)
+        client = sandbox.client(0)
+        protocol.activate(client)
+        return sandbox
 
-        for input_ in [api_path, proto_api_path]:
-            # If you need to debug, insert time.sleep(15) in there,
-            # to give you time to inspect generated files before the
-            # enclosing 'with' block finishes or to execute the dune
-            # command manually while the temporary files are still there.
-            cmd = [
-                "dune",
-                "exec",
-                "../src/openapi/rpc_openapi.exe",
-                "--",
-                version,
-                input_,
-            ]
-            print(" ".join(cmd))
-            process_ret = subprocess.run(
-                cmd, check=True, capture_output=True, text=True
-            )
-            _try_json_loads(" ".join(cmd), process_ret.stdout)
+    @pytest.mark.parametrize(
+        "rpc_path", ["describe", "describe/chains/main/blocks/head/"]
+    )
+    def test_validity(self, sandbox: Sandbox, rpc_path: str, tmp_path: Path):
+        """
+        Mimicks the script src/openapi/generate.sh. Generates the API
+        and check it generates a valid OpenAPI specification.
+        """
+        node = sandbox.node(0)
+        addr = f"http://localhost:{node.rpc_port}/{rpc_path}?recurse=yes"
+        json_path = tmp_path / "result.json"
+        with open(json_path, "w") as o_file:
+            json_res = requests.get(addr).json()
+            json.dump(json_res, o_file)
+
+        # If you need to debug, insert time.sleep(15) in there,
+        # to give you time to inspect generated files before the
+        # enclosing 'with' block finishes or to execute the dune
+        # command manually while the temporary files are still there.
+        version = _get_tezos_node_version()
+        cmd = [
+            "dune",
+            "exec",
+            "../src/openapi/rpc_openapi.exe",
+            "--",
+            version,
+            str(json_path.absolute()),
+        ]
+        process_ret = subprocess.run(
+            cmd, check=True, capture_output=True, text=True
+        )
+        res = json.loads(process_ret.stdout)
+        openapi_spec_validator.validate_spec(res)
