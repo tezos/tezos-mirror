a70fc55114e2549e6fa965ad33f8c5a97dc291c6
Nic Volanschi <nic.volanschi@nomadic-labs.com>
2020-11-12 15:04:57 +0000

    constrain Python to v3.8.x for poetry compatibility, as make doc-html fails with Python 3.9

--- scripts/version.sh
+++ scripts/version.sh
@@ -12,7 +12,7 @@ opam_version=2.0
 full_opam_repository_tag=9f32db57625b3a40a1942b121452fdc3bd0677c4
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=1adce2a7a54b71fe114c56c979beb15aec7b313f
+opam_repository_tag=f21f16d5be9bdc0f4a716afce7e7a0d979b1db2e
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
