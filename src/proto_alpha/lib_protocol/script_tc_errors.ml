7293cf9da8e254962bd9eb440ae6914b9a7766a2
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2020-11-12 18:18:16 +0100

    Proto/Michelson: add type to Non_dupable_ty error

--- src/proto_alpha/lib_protocol/script_tc_errors.ml
+++ src/proto_alpha/lib_protocol/script_tc_errors.ml
@@ -185,4 +185,4 @@ type error += Unexpected_ticket of Script.location
 
 type error += Unexpected_forged_value of Script.location
 
-type error += Non_dupable_type of Script.location
+type error += Non_dupable_type of Script.location * Script.expr
