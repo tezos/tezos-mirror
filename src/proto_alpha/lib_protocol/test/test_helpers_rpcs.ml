a2cbadc5e28f6678ab658c92beae94a52b279b05
Marco Stronati <marco@stronati.org>
2020-11-13 09:49:18 +0100

    Proto/test: add test for baking_rights RPC

--- src/proto_alpha/lib_protocol/test/test_helpers_rpcs.ml
+++ src/proto_alpha/lib_protocol/test/test_helpers_rpcs.ml
@@ -1,7 +1,7 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2020 Nomadic Labs, <contact@nomadic-labs.com>               *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -23,29 +23,49 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let () =
-  Alcotest_lwt.run
-    "protocol_alpha"
-    [ ("transfer", Transfer.tests);
-      ("origination", Origination.tests);
-      ("activation", Activation.tests);
-      ("revelation", Reveal.tests);
-      ("endorsement", Endorsement.tests);
-      ("double endorsement", Double_endorsement.tests);
-      ("double baking", Double_baking.tests);
-      ("seed", Seed.tests);
-      ("baking", Baking.tests);
-      ("delegation", Delegation.tests);
-      ("rolls", Rolls.tests);
-      ("combined", Combined_operations.tests);
-      ("qty", Qty.tests);
-      ("voting", Voting.tests);
-      ("interpretation", Interpretation.tests);
-      ("typechecking", Typechecking.tests);
-      ("gas properties", Gas_properties.tests);
-      ("fixed point computation", Fixed_point.tests);
-      ("gas cost functions", Gas_costs.tests);
-      ("lazy storage diff", Lazy_storage_diff.tests);
-      ("sapling", Test_sapling.tests);
-      ("helpers rpcs", Test_helpers_rpcs.tests) ]
-  |> Lwt_main.run
+open Protocol
+open Alpha_context
+
+(* Test the baking_rights RPC.
+   Future levels or cycles are not tested because it's hard in this framework,
+   using only RPCs, to fabricate them. *)
+let test_baking_rights () =
+  Context.init 2
+  >>=? fun (b, contracts) ->
+  let open Alpha_services.Delegate.Baking_rights in
+  (* default max_priority returns 65 results *)
+  get Block.rpc_ctxt b ~all:true
+  >>=? fun rights ->
+  assert (List.length rights = 65) ;
+  (* arbitrary max_priority *)
+  let max_priority = 15 in
+  get Block.rpc_ctxt b ~all:true ~max_priority
+  >>=? fun rights ->
+  assert (List.length rights = max_priority + 1) ;
+  (* filtering by delegate *)
+  let d =
+    Contract.is_implicit (List.nth contracts 0)
+    |> Option.unopt_assert ~loc:__POS__
+  in
+  get Block.rpc_ctxt b ~all:true ~delegates:[d]
+  >>=? fun rights ->
+  assert (List.for_all (fun {delegate; _} -> delegate = d) rights) ;
+  (* filtering by cycle *)
+  Alpha_services.Helpers.current_level Block.rpc_ctxt b
+  >>=? fun {cycle; _} ->
+  get Block.rpc_ctxt b ~all:true ~cycles:[cycle]
+  >>=? fun rights ->
+  Alpha_services.Helpers.levels_in_current_cycle Block.rpc_ctxt b
+  >>=? fun (first, last) ->
+  assert (
+    List.for_all (fun {level; _} -> level >= first && level <= last) rights ) ;
+  (* filtering by level *)
+  Alpha_services.Helpers.current_level Block.rpc_ctxt b
+  >>=? fun {level; _} ->
+  get Block.rpc_ctxt b ~all:true ~levels:[level]
+  >>=? fun rights ->
+  let espected_level = level in
+  assert (List.for_all (fun {level; _} -> level = espected_level) rights) ;
+  return_unit
+
+let tests = [Test.tztest "baking_rights" `Quick test_baking_rights]
