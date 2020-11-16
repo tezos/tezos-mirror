94a3f45624a39563f02dd4a515c6a5687ccdf4a3
Alexandre Doussot <doussot.alexandre@gmail.com>
2020-11-16 20:08:42 +0100

    Tests/Python: add tests for `transfer --simulation`

--- tests_python/conftest.py
+++ tests_python/conftest.py
@@ -1,6 +1,24 @@
 """Pytest configuration
 """
 
+import pytest
+from client.client import Client
+
+
+@pytest.fixture(scope="class")
+def encrypted_account(client: Client):
+    """
+    Import an encrypted account
+    """
+    client.import_secret_key(
+        "encrypted_account",
+        (
+            "encrypted:edesk1n2uGpPtVaeyhWkZzTEcaPRzkQHrqkw5pk8VkZv"
+            "p3rM5KSc3mYNH5cJEuNcfB91B3G3JakKzfLQSmrgF4ht"
+        ),
+        "password",
+    )
+
 
 def pytest_addoption(parser) -> None:
     parser.addoption("--log-dir", action="store", help="specify log directory")
