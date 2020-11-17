Piotr170687/tezos

Use Option.value

src/proto_alpha/lib_protocol/contract_storage.ml
@@ -822,11 +822,11 @@ let init c =
 
 let used_storage_space c contract =
   Storage.Contract.Used_storage_space.get_option c contract
-  >|=? function None -> Z.zero | Some fees -> fees
+  >|=? Option.value ~default:Z.zero
 
 let paid_storage_space c contract =
   Storage.Contract.Paid_storage_space.get_option c contract
-  >|=? function None -> Z.zero | Some paid_space -> paid_space
+  >|=? Option.value ~default:Z.zero
 
 let set_paid_storage_space_and_return_fees_to_pay c contract new_storage_space
     =

src/proto_alpha/lib_protocol/delegate_storage.ml
@@ -274,7 +274,7 @@ let delegated_contracts ctxt delegate =
 
 let get_frozen_deposit ctxt contract cycle =
   Storage.Contract.Frozen_deposits.get_option (ctxt, contract) cycle
-  >|=? function None -> Tez_repr.zero | Some frozen -> frozen
+  >|=? Option.value ~default:Tez_repr.zero
 
 let credit_frozen_deposit ctxt delegate cycle amount =
   let contract = Contract_repr.implicit_contract delegate in
@@ -303,7 +303,7 @@ let freeze_deposit ctxt delegate amount =
 
 let get_frozen_fees ctxt contract cycle =
   Storage.Contract.Frozen_fees.get_option (ctxt, contract) cycle
-  >|=? function None -> Tez_repr.zero | Some frozen -> frozen
+  >|=? Option.value ~default:Tez_repr.zero
 
 let credit_frozen_fees ctxt delegate cycle amount =
   let contract = Contract_repr.implicit_contract delegate in
@@ -337,7 +337,7 @@ let burn_fees ctxt delegate cycle amount =
 
 let get_frozen_rewards ctxt contract cycle =
   Storage.Contract.Frozen_rewards.get_option (ctxt, contract) cycle
-  >|=? function None -> Tez_repr.zero | Some frozen -> frozen
+  >|=? Option.value ~default:Tez_repr.zero
 
 let credit_frozen_rewards ctxt delegate cycle amount =
   let contract = Contract_repr.implicit_contract delegate in

src/proto_alpha/lib_protocol/roll_storage.ml
@@ -225,7 +225,7 @@ let count_rolls ctxt delegate =
 
 let get_change ctxt delegate =
   Storage.Roll.Delegate_change.get_option ctxt delegate
-  >|=? function None -> Tez_repr.zero | Some change -> change
+  >|=? Option.value ~default:Tez_repr.zero
 
 module Delegate = struct
   let fresh_roll ctxt =

src/proto_alpha/lib_protocol/vote_storage.ml
@@ -26,7 +26,7 @@
 
 let recorded_proposal_count_for_delegate ctxt proposer =
   Storage.Vote.Proposals_count.get_option ctxt proposer
-  >|=? function None -> 0 | Some count -> count
+  >|=? Option.value ~default:0
 
 let record_proposal ctxt proposal proposer =
   recorded_proposal_count_for_delegate ctxt proposer
@@ -107,7 +107,7 @@ let update_listings ctxt =
       (* TODO use snapshots *)
       let delegate = Signature.Public_key.hash delegate in
       Storage.Vote.Listings.get_option ctxt delegate
-      >|=? (function None -> 0l | Some count -> count)
+      >|=? Option.value ~default:0l
       >>=? fun count ->
       Storage.Vote.Listings.init_set ctxt delegate (Int32.succ count)
       >|= fun ctxt -> ok (ctxt, Int32.succ total))

tezos/tezos-mirror

src/proto_alpha/lib_protocol/contract_storage.ml
@@ -735,11 +735,11 @@ let init c =
 
 let used_storage_space c contract =
   Storage.Contract.Used_storage_space.get_option c contract
-  >|=? function None -> Z.zero | Some fees -> fees
+  >|=? Option.unopt ~default:Z.zero
 
 let paid_storage_space c contract =
   Storage.Contract.Paid_storage_space.get_option c contract
-  >|=? function None -> Z.zero | Some paid_space -> paid_space
+  >|=? Option.unopt ~default:Z.zero
 
 let set_paid_storage_space_and_return_fees_to_pay c contract new_storage_space
     =

src/proto_alpha/lib_protocol/delegate_storage.ml
@@ -368,7 +368,7 @@ let delegated_contracts ctxt delegate =
 
 let get_frozen_deposit ctxt contract cycle =
   Storage.Contract.Frozen_deposits.get_option (ctxt, contract) cycle
-  >|=? function None -> Tez_repr.zero | Some frozen -> frozen
+  >|=? Option.unopt ~default:Tez_repr.zero
 
 let credit_frozen_deposit ctxt delegate cycle amount =
   let contract = Contract_repr.implicit_contract delegate in
@@ -397,7 +397,7 @@ let freeze_deposit ctxt delegate amount =
 
 let get_frozen_fees ctxt contract cycle =
   Storage.Contract.Frozen_fees.get_option (ctxt, contract) cycle
-  >|=? function None -> Tez_repr.zero | Some frozen -> frozen
+  >|=? Option.unopt ~default:Tez_repr.zero
 
 let credit_frozen_fees ctxt delegate cycle amount =
   let contract = Contract_repr.implicit_contract delegate in
@@ -431,7 +431,7 @@ let burn_fees ctxt delegate cycle amount =
 
 let get_frozen_rewards ctxt contract cycle =
   Storage.Contract.Frozen_rewards.get_option (ctxt, contract) cycle
-  >|=? function None -> Tez_repr.zero | Some frozen -> frozen
+  >|=? Option.unopt ~default:Tez_repr.zero
 
 let credit_frozen_rewards ctxt delegate cycle amount =
   let contract = Contract_repr.implicit_contract delegate in

src/proto_alpha/lib_protocol/roll_storage.ml
@@ -226,7 +226,7 @@ let count_rolls ctxt delegate =
 
 let get_change ctxt delegate =
   Storage.Roll.Delegate_change.get_option ctxt delegate
-  >|=? function None -> Tez_repr.zero | Some change -> change
+  >|=? Option.unopt ~default:Tez_repr.zero
 
 module Delegate = struct
   let fresh_roll ctxt =

src/proto_alpha/lib_protocol/vote_storage.ml
@@ -27,7 +27,7 @@ open Misc.Syntax
 
 let recorded_proposal_count_for_delegate ctxt proposer =
   Storage.Vote.Proposals_count.get_option ctxt proposer
-  >|=? function None -> 0 | Some count -> count
+  >|=? Option.unopt ~default:0
 
 let record_proposal ctxt proposal proposer =
   recorded_proposal_count_for_delegate ctxt proposer
@@ -106,7 +106,7 @@ let freeze_listings ctxt =
       (* TODO use snapshots *)
       let delegate = Signature.Public_key.hash delegate in
       Storage.Vote.Listings.get_option ctxt delegate
-      >|=? (function None -> 0l | Some count -> count)
+      >|=? Option.unopt ~default:0l
       >>=? fun count ->
       Storage.Vote.Listings.init_set ctxt delegate (Int32.succ count)
       >|= fun ctxt -> ok (ctxt, Int32.succ total))