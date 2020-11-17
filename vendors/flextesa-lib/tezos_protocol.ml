icristescu/tezos

Revert "Vendors/flextesa: scale gas limit constants in the context by 128"
This reverts commit 888e2833f9004dc920b73561854ec248e16c9310.

vendors/flextesa-lib/tezos_protocol.ml
@@ -144,8 +144,7 @@ let protocol_parameters_json t : Ezjsonm.t =
     let op_gas_limit, block_gas_limit =
       match subkind with
       | `Babylon -> (800_000, 8_000_000)
-      | `Carthage -> (1_040_000, 10_400_000)
-      | `Alpha -> (128 * 1_040_000, 128 * 10_400_000) in
+      | `Carthage | `Alpha -> (1_040_000, 10_400_000) in
     let open Ezjsonm in
     let list_of_zs = list (fun i -> string (Int.to_string i)) in
     [ ("blocks_per_commitment", int 4)

tezos/tezos-mirror

vendors/flextesa-lib/tezos_protocol.ml
@@ -147,7 +147,7 @@ let protocol_parameters_json t : Ezjsonm.t =
       match subkind with
       | `Babylon -> (800_000, 8_000_000)
       | `Carthage -> (1_040_000, 10_400_000)
-      | `Delphi | `Alpha -> (128 * 1_040_000, 128 * 10_400_000) in
+      | `Delphi | `Alpha -> (1_040_000, 10_400_000) in
     let open Ezjsonm in
     let list_of_zs = list (fun i -> string (Int.to_string i)) in
     [ ("blocks_per_commitment", int 4)