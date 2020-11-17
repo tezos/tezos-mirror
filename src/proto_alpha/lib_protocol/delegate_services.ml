Piotr170687/tezos

Proto: bind -> to

src/proto_alpha/lib_protocol/delegate_services.ml
@@ -262,29 +262,28 @@ let register () =
       Vote.get_voting_power_free ctxt pkh
       >>=? fun voting_power ->
       Delegate.Proof.all ctxt pkh
-      >>=? fun proof_levels ->
-      return
-        {
-          balance;
-          frozen_balance;
-          frozen_balance_by_cycle;
-          staking_balance;
-          delegated_contracts;
-          delegated_balance;
-          deactivated;
-          grace_period;
-          voting_power;
-          proof_levels;
-        }) ;
+      >|=? fun proof_levels ->
+      {
+        balance;
+        frozen_balance;
+        frozen_balance_by_cycle;
+        staking_balance;
+        delegated_contracts;
+        delegated_balance;
+        deactivated;
+        grace_period;
+        voting_power;
+        proof_levels;
+      }) ;
   register1 S.balance (fun ctxt pkh () () -> Delegate.full_balance ctxt pkh) ;
   register1 S.frozen_balance (fun ctxt pkh () () ->
       Delegate.frozen_balance ctxt pkh) ;
   register1 S.frozen_balance_by_cycle (fun ctxt pkh () () ->
-      Delegate.frozen_balance_by_cycle ctxt pkh >>= return) ;
+      Delegate.frozen_balance_by_cycle ctxt pkh >|= ok) ;
   register1 S.staking_balance (fun ctxt pkh () () ->
       Delegate.staking_balance ctxt pkh) ;
   register1 S.delegated_contracts (fun ctxt pkh () () ->
-      Delegate.delegated_contracts ctxt pkh >>= return) ;
+      Delegate.delegated_contracts ctxt pkh >|= ok) ;
   register1 S.delegated_balance (fun ctxt pkh () () ->
       Delegate.delegated_balance ctxt pkh) ;
   register1 S.deactivated (fun ctxt pkh () () -> Delegate.deactivated ctxt pkh) ;
@@ -349,7 +348,7 @@ let requested_levels ~default ctxt cycles levels =
           if Level.(level <= current_level) then return (level, None)
           else
             Baking.earlier_predecessor_timestamp ctxt level
-            >>=? fun timestamp -> return (level, Some timestamp))
+            >|=? fun timestamp -> (level, Some timestamp))
         levels
 
 module Baking_rights = struct
@@ -469,21 +468,21 @@ module Baking_rights = struct
           match q.max_priority with None -> 64 | Some max -> max
         in
         map_s (baking_priorities ctxt max_priority) levels
-        >>=? fun rights ->
+        >|=? fun rights ->
         let rights =
           if q.all then rights else List.map remove_duplicated_delegates rights
         in
         let rights = List.concat rights in
         match q.delegates with
         | [] ->
-            return rights
+            rights
         | _ :: _ as delegates ->
             let is_requested p =
               List.exists
                 (Signature.Public_key_hash.equal p.delegate)
                 delegates
             in
-            return (List.filter is_requested rights))
+            List.filter is_requested rights)
 
   let get ctxt ?(levels = []) ?(cycles = []) ?(delegates = []) ?(all = false)
       ?max_priority block =
@@ -558,13 +557,12 @@ module Endorsing_rights = struct
 
   let endorsement_slots ctxt (level, estimated_time) =
     Baking.endorsement_rights ctxt level
-    >>=? fun rights ->
-    return
-      (Signature.Public_key_hash.Map.fold
-         (fun delegate (_, slots, _) acc ->
-           {level = level.level; delegate; slots; estimated_time} :: acc)
-         rights
-         [])
+    >|=? fun rights ->
+    Signature.Public_key_hash.Map.fold
+      (fun delegate (_, slots, _) acc ->
+        {level = level.level; delegate; slots; estimated_time} :: acc)
+      rights
+      []
 
   let register () =
     let open Services_registration in
@@ -576,18 +574,18 @@ module Endorsing_rights = struct
           q.levels
         >>=? fun levels ->
         map_s (endorsement_slots ctxt) levels
-        >>=? fun rights ->
+        >|=? fun rights ->
         let rights = List.concat rights in
         match q.delegates with
         | [] ->
-            return rights
+            rights
         | _ :: _ as delegates ->
             let is_requested p =
               List.exists
                 (Signature.Public_key_hash.equal p.delegate)
                 delegates
             in
-            return (List.filter is_requested rights))
+            List.filter is_requested rights)
 
   let get ctxt ?(levels = []) ?(cycles = []) ?(delegates = []) block =
     RPC_context.make_call0
@@ -607,7 +605,7 @@ module Endorsing_power = struct
           ctxt
           chain_id
           {shell = operation.shell; protocol_data = data}
-        >>=? fun (_, slots, _) -> return (List.length slots)
+        >|=? fun (_, slots, _) -> List.length slots
     | _ ->
         failwith "Operation is not an endorsement"
 
@@ -719,19 +717,17 @@ let register () =
 
 let endorsement_rights ctxt level =
   Endorsing_rights.endorsement_slots ctxt (level, None)
-  >>=? fun l ->
-  return (List.map (fun {Endorsing_rights.delegate; _} -> delegate) l)
+  >|=? fun l -> List.map (fun {Endorsing_rights.delegate; _} -> delegate) l
 
 let baking_rights ctxt max_priority =
   let max = match max_priority with None -> 64 | Some m -> m in
   let level = Level.current ctxt in
   Baking_rights.baking_priorities ctxt max (level, None)
-  >>=? fun l ->
-  return
-    ( level.level,
-      List.map
-        (fun {Baking_rights.delegate; timestamp; _} -> (delegate, timestamp))
-        l )
+  >|=? fun l ->
+  ( level.level,
+    List.map
+      (fun {Baking_rights.delegate; timestamp; _} -> (delegate, timestamp))
+      l )
 
 let endorsing_power ctxt operation =
   Endorsing_power.endorsing_power ctxt operation

src/proto_alpha/lib_protocol/main.ml
@@ -126,12 +126,12 @@ let begin_partial_application ~chain_id ~ancestor_context:ctxt
   Alpha_context.prepare ~level ~predecessor_timestamp ~timestamp ~fitness ctxt
   >>=? fun (ctxt, migration_balance_updates) ->
   Apply.begin_application ctxt chain_id block_header predecessor_timestamp
-  >>=? fun (ctxt, baker, block_delay) ->
+  >|=? fun (ctxt, baker, block_delay) ->
   let mode =
     Partial_application
       {block_header; baker = Signature.Public_key.hash baker; block_delay}
   in
-  return {mode; chain_id; ctxt; op_count = 0; migration_balance_updates}
+  {mode; chain_id; ctxt; op_count = 0; migration_balance_updates}
 
 let begin_application ~chain_id ~predecessor_context:ctxt
     ~predecessor_timestamp ~predecessor_fitness
@@ -142,12 +142,12 @@ let begin_application ~chain_id ~predecessor_context:ctxt
   Alpha_context.prepare ~level ~predecessor_timestamp ~timestamp ~fitness ctxt
   >>=? fun (ctxt, migration_balance_updates) ->
   Apply.begin_application ctxt chain_id block_header predecessor_timestamp
-  >>=? fun (ctxt, baker, block_delay) ->
+  >|=? fun (ctxt, baker, block_delay) ->
   let mode =
     Application
       {block_header; baker = Signature.Public_key.hash baker; block_delay}
   in
-  return {mode; chain_id; ctxt; op_count = 0; migration_balance_updates}
+  {mode; chain_id; ctxt; op_count = 0; migration_balance_updates}
 
 let begin_construction ~chain_id ~predecessor_context:ctxt
     ~predecessor_timestamp ~predecessor_level:pred_level
@@ -160,22 +160,22 @@ let begin_construction ~chain_id ~predecessor_context:ctxt
   ( match protocol_data with
   | None ->
       Apply.begin_partial_construction ctxt
-      >>=? fun ctxt ->
+      >|=? fun ctxt ->
       let mode = Partial_construction {predecessor} in
-      return (mode, ctxt)
+      (mode, ctxt)
   | Some proto_header ->
       Apply.begin_full_construction
         ctxt
         predecessor_timestamp
         proto_header.contents
-      >>=? fun (ctxt, protocol_data, baker, block_delay) ->
+      >|=? fun (ctxt, protocol_data, baker, block_delay) ->
       let mode =
         let baker = Signature.Public_key.hash baker in
         Full_construction {predecessor; baker; protocol_data; block_delay}
       in
-      return (mode, ctxt) )
-  >>=? fun (mode, ctxt) ->
-  return {mode; chain_id; ctxt; op_count = 0; migration_balance_updates}
+      (mode, ctxt) )
+  >|=? fun (mode, ctxt) ->
+  {mode; chain_id; ctxt; op_count = 0; migration_balance_updates}
 
 let apply_operation ({mode; chain_id; ctxt; op_count; _} as data)
     (operation : Alpha_context.packed_operation) =
@@ -209,9 +209,9 @@ let apply_operation ({mode; chain_id; ctxt; op_count; _} as data)
         baker
         (Alpha_context.Operation.hash operation)
         operation
-      >>=? fun (ctxt, result) ->
+      >|=? fun (ctxt, result) ->
       let op_count = op_count + 1 in
-      return ({data with ctxt; op_count}, Operation_metadata result)
+      ({data with ctxt; op_count}, Operation_metadata result)
 
 let finalize_block {mode; ctxt; op_count; migration_balance_updates} =
   match mode with
@@ -227,20 +227,19 @@ let finalize_block {mode; ctxt; op_count; migration_balance_updates} =
           Alpha_context.Delegate.freeze_deposit ctxt delegate deposit)
         (Alpha_context.get_deposits ctxt)
         (return ctxt)
-      >>=? fun ctxt ->
+      >|=? fun ctxt ->
       let ctxt = Alpha_context.finalize ctxt in
-      return
-        ( ctxt,
-          Apply_results.
-            {
-              baker;
-              level;
-              voting_period_kind;
-              nonce_hash = None;
-              consumed_gas = Z.zero;
-              deactivated = [];
-              balance_updates = migration_balance_updates;
-            } )
+      ( ctxt,
+        Apply_results.
+          {
+            baker;
+            level;
+            voting_period_kind;
+            nonce_hash = None;
+            consumed_gas = Z.zero;
+            deactivated = [];
+            balance_updates = migration_balance_updates;
+          } )
   | Partial_application {block_header; baker; block_delay} ->
       let level = Alpha_context.Level.current ctxt in
       let included_endorsements = Alpha_context.included_endorsements ctxt in
@@ -251,20 +250,19 @@ let finalize_block {mode; ctxt; op_count; migration_balance_updates} =
         included_endorsements
       >>=? fun () ->
       Alpha_context.Vote.get_current_period_kind ctxt
-      >>=? fun voting_period_kind ->
+      >|=? fun voting_period_kind ->
       let ctxt = Alpha_context.finalize ctxt in
-      return
-        ( ctxt,
-          Apply_results.
-            {
-              baker;
-              level;
-              voting_period_kind;
-              nonce_hash = None;
-              consumed_gas = Z.zero;
-              deactivated = [];
-              balance_updates = migration_balance_updates;
-            } )
+      ( ctxt,
+        Apply_results.
+          {
+            baker;
+            level;
+            voting_period_kind;
+            nonce_hash = None;
+            consumed_gas = Z.zero;
+            deactivated = [];
+            balance_updates = migration_balance_updates;
+          } )
   | Application
       { baker;
         block_delay;
@@ -276,7 +274,7 @@ let finalize_block {mode; ctxt; op_count; migration_balance_updates} =
         baker
         ~block_delay
         migration_balance_updates
-      >>=? fun (ctxt, receipt) ->
+      >|=? fun (ctxt, receipt) ->
       let level = Alpha_context.Level.current ctxt in
       let priority = protocol_data.priority in
       let raw_level = Alpha_context.Raw_level.to_int32 level.level in
@@ -290,7 +288,7 @@ let finalize_block {mode; ctxt; op_count; migration_balance_updates} =
           op_count
       in
       let ctxt = Alpha_context.finalize ~commit_message ctxt in
-      return (ctxt, receipt)
+      (ctxt, receipt)
 
 let compare_operations op1 op2 =
   let open Alpha_context in
@@ -378,11 +376,11 @@ let init ctxt block_header =
       Optimized
       parsed_script.storage_type
       storage
-    >>=? fun (storage, ctxt) ->
+    >|=? fun (storage, ctxt) ->
     let storage =
       Alpha_context.Script.lazy_expr (Micheline.strip_locations storage)
     in
-    return (({script with storage}, lazy_storage_diff), ctxt)
+    (({script with storage}, lazy_storage_diff), ctxt)
   in
   Alpha_context.prepare_first_block ~typecheck ~level ~timestamp ~fitness ctxt
-  >>=? fun ctxt -> return (Alpha_context.finalize ctxt)
+  >|=? fun ctxt -> Alpha_context.finalize ctxt

tezos/tezos-mirror

src/proto_alpha/lib_protocol/delegate_services.ml
@@ -24,6 +24,7 @@
 (*****************************************************************************)
 
 open Alpha_context
+open Misc.Syntax
 
 type info = {
   balance : Tez.t;
@@ -228,27 +229,26 @@ let register () =
       Delegate.deactivated ctxt pkh
       >>=? fun deactivated ->
       Delegate.grace_period ctxt pkh
-      >>=? fun grace_period ->
-      return
-        {
-          balance;
-          frozen_balance;
-          frozen_balance_by_cycle;
-          staking_balance;
-          delegated_contracts;
-          delegated_balance;
-          deactivated;
-          grace_period;
-        }) ;
+      >|=? fun grace_period ->
+      {
+        balance;
+        frozen_balance;
+        frozen_balance_by_cycle;
+        staking_balance;
+        delegated_contracts;
+        delegated_balance;
+        deactivated;
+        grace_period;
+      }) ;
   register1 S.balance (fun ctxt pkh () () -> Delegate.full_balance ctxt pkh) ;
   register1 S.frozen_balance (fun ctxt pkh () () ->
       Delegate.frozen_balance ctxt pkh) ;
   register1 S.frozen_balance_by_cycle (fun ctxt pkh () () ->
-      Delegate.frozen_balance_by_cycle ctxt pkh >>= return) ;
+      Delegate.frozen_balance_by_cycle ctxt pkh >|= ok) ;
   register1 S.staking_balance (fun ctxt pkh () () ->
       Delegate.staking_balance ctxt pkh) ;
   register1 S.delegated_contracts (fun ctxt pkh () () ->
-      Delegate.delegated_contracts ctxt pkh >>= return) ;
+      Delegate.delegated_contracts ctxt pkh >|= ok) ;
   register1 S.delegated_balance (fun ctxt pkh () () ->
       Delegate.delegated_balance ctxt pkh) ;
   register1 S.deactivated (fun ctxt pkh () () -> Delegate.deactivated ctxt pkh) ;
@@ -304,7 +304,7 @@ let requested_levels ~default ctxt cycles levels =
           if Level.(level <= current_level) then return (level, None)
           else
             Baking.earlier_predecessor_timestamp ctxt level
-            >>=? fun timestamp -> return (level, Some timestamp))
+            >|=? fun timestamp -> (level, Some timestamp))
         levels
 
 module Baking_rights = struct
@@ -424,21 +424,21 @@ module Baking_rights = struct
           match q.max_priority with None -> 64 | Some max -> max
         in
         map_s (baking_priorities ctxt max_priority) levels
-        >>=? fun rights ->
+        >|=? fun rights ->
         let rights =
           if q.all then rights else List.map remove_duplicated_delegates rights
         in
         let rights = List.concat rights in
         match q.delegates with
         | [] ->
-            return rights
+            rights
         | _ :: _ as delegates ->
             let is_requested p =
               List.exists
                 (Signature.Public_key_hash.equal p.delegate)
                 delegates
             in
-            return (List.filter is_requested rights))
+            List.filter is_requested rights)
 
   let get ctxt ?(levels = []) ?(cycles = []) ?(delegates = []) ?(all = false)
       ?max_priority block =
@@ -513,13 +513,12 @@ module Endorsing_rights = struct
 
   let endorsement_slots ctxt (level, estimated_time) =
     Baking.endorsement_rights ctxt level
-    >>=? fun rights ->
-    return
-      (Signature.Public_key_hash.Map.fold
-         (fun delegate (_, slots, _) acc ->
-           {level = level.level; delegate; slots; estimated_time} :: acc)
-         rights
-         [])
+    >|=? fun rights ->
+    Signature.Public_key_hash.Map.fold
+      (fun delegate (_, slots, _) acc ->
+        {level = level.level; delegate; slots; estimated_time} :: acc)
+      rights
+      []
 
   let register () =
     let open Services_registration in
@@ -531,18 +530,18 @@ module Endorsing_rights = struct
           q.levels
         >>=? fun levels ->
         map_s (endorsement_slots ctxt) levels
-        >>=? fun rights ->
+        >|=? fun rights ->
         let rights = List.concat rights in
         match q.delegates with
         | [] ->
-            return rights
+            rights
         | _ :: _ as delegates ->
             let is_requested p =
               List.exists
                 (Signature.Public_key_hash.equal p.delegate)
                 delegates
             in
-            return (List.filter is_requested rights))
+            List.filter is_requested rights)
 
   let get ctxt ?(levels = []) ?(cycles = []) ?(delegates = []) block =
     RPC_context.make_call0
@@ -562,7 +561,7 @@ module Endorsing_power = struct
           ctxt
           chain_id
           {shell = operation.shell; protocol_data = data}
-        >>=? fun (_, slots, _) -> return (List.length slots)
+        >|=? fun (_, slots, _) -> List.length slots
     | _ ->
         failwith "Operation is not an endorsement"
 
@@ -674,19 +673,17 @@ let register () =
 
 let endorsement_rights ctxt level =
   Endorsing_rights.endorsement_slots ctxt (level, None)
-  >>=? fun l ->
-  return (List.map (fun {Endorsing_rights.delegate; _} -> delegate) l)
+  >|=? fun l -> List.map (fun {Endorsing_rights.delegate; _} -> delegate) l
 
 let baking_rights ctxt max_priority =
   let max = match max_priority with None -> 64 | Some m -> m in
   let level = Level.current ctxt in
   Baking_rights.baking_priorities ctxt max (level, None)
-  >>=? fun l ->
-  return
-    ( level.level,
-      List.map
-        (fun {Baking_rights.delegate; timestamp; _} -> (delegate, timestamp))
-        l )
+  >|=? fun l ->
+  ( level.level,
+    List.map
+      (fun {Baking_rights.delegate; timestamp; _} -> (delegate, timestamp))
+      l )
 
 let endorsing_power ctxt operation =
   Endorsing_power.endorsing_power ctxt operation

src/proto_alpha/lib_protocol/main.ml
@@ -25,6 +25,8 @@
 
 (* Tezos Protocol Implementation - Protocol Signature Instance *)
 
+open Misc.Syntax
+
 type block_header_data = Alpha_context.Block_header.protocol_data
 
 type block_header = Alpha_context.Block_header.t = {
@@ -125,12 +127,12 @@ let begin_partial_application ~chain_id ~ancestor_context:ctxt
   Alpha_context.prepare ~level ~predecessor_timestamp ~timestamp ~fitness ctxt
   >>=? fun ctxt ->
   Apply.begin_application ctxt chain_id block_header predecessor_timestamp
-  >>=? fun (ctxt, baker, block_delay) ->
+  >|=? fun (ctxt, baker, block_delay) ->
   let mode =
     Partial_application
       {block_header; baker = Signature.Public_key.hash baker; block_delay}
   in
-  return {mode; chain_id; ctxt; op_count = 0}
+  {mode; chain_id; ctxt; op_count = 0}
 
 let begin_application ~chain_id ~predecessor_context:ctxt
     ~predecessor_timestamp ~predecessor_fitness
@@ -141,12 +143,12 @@ let begin_application ~chain_id ~predecessor_context:ctxt
   Alpha_context.prepare ~level ~predecessor_timestamp ~timestamp ~fitness ctxt
   >>=? fun ctxt ->
   Apply.begin_application ctxt chain_id block_header predecessor_timestamp
-  >>=? fun (ctxt, baker, block_delay) ->
+  >|=? fun (ctxt, baker, block_delay) ->
   let mode =
     Application
       {block_header; baker = Signature.Public_key.hash baker; block_delay}
   in
-  return {mode; chain_id; ctxt; op_count = 0}
+  {mode; chain_id; ctxt; op_count = 0}
 
 let begin_construction ~chain_id ~predecessor_context:ctxt
     ~predecessor_timestamp ~predecessor_level:pred_level
@@ -159,21 +161,21 @@ let begin_construction ~chain_id ~predecessor_context:ctxt
   ( match protocol_data with
   | None ->
       Apply.begin_partial_construction ctxt
-      >>=? fun ctxt ->
+      >|=? fun ctxt ->
       let mode = Partial_construction {predecessor} in
-      return (mode, ctxt)
+      (mode, ctxt)
   | Some proto_header ->
       Apply.begin_full_construction
         ctxt
         predecessor_timestamp
         proto_header.contents
-      >>=? fun (ctxt, protocol_data, baker, block_delay) ->
+      >|=? fun (ctxt, protocol_data, baker, block_delay) ->
       let mode =
         let baker = Signature.Public_key.hash baker in
         Full_construction {predecessor; baker; protocol_data; block_delay}
       in
-      return (mode, ctxt) )
-  >>=? fun (mode, ctxt) -> return {mode; chain_id; ctxt; op_count = 0}
+      (mode, ctxt) )
+  >|=? fun (mode, ctxt) -> {mode; chain_id; ctxt; op_count = 0}
 
 let apply_operation ({mode; chain_id; ctxt; op_count; _} as data)
     (operation : Alpha_context.packed_operation) =
@@ -207,9 +209,9 @@ let apply_operation ({mode; chain_id; ctxt; op_count; _} as data)
         baker
         (Alpha_context.Operation.hash operation)
         operation
-      >>=? fun (ctxt, result) ->
+      >|=? fun (ctxt, result) ->
       let op_count = op_count + 1 in
-      return ({data with ctxt; op_count}, Operation_metadata result)
+      ({data with ctxt; op_count}, Operation_metadata result)
 
 let finalize_block {mode; ctxt; op_count} =
   match mode with
@@ -225,20 +227,19 @@ let finalize_block {mode; ctxt; op_count} =
           Alpha_context.Delegate.freeze_deposit ctxt delegate deposit)
         (Alpha_context.get_deposits ctxt)
         (return ctxt)
-      >>=? fun ctxt ->
+      >|=? fun ctxt ->
       let ctxt = Alpha_context.finalize ctxt in
-      return
-        ( ctxt,
-          Apply_results.
-            {
-              baker;
-              level;
-              voting_period_kind;
-              nonce_hash = None;
-              consumed_gas = Z.zero;
-              deactivated = [];
-              balance_updates = [];
-            } )
+      ( ctxt,
+        Apply_results.
+          {
+            baker;
+            level;
+            voting_period_kind;
+            nonce_hash = None;
+            consumed_gas = Z.zero;
+            deactivated = [];
+            balance_updates = [];
+          } )
   | Partial_application {block_header; baker; block_delay} ->
       let level = Alpha_context.Level.current ctxt in
       let included_endorsements = Alpha_context.included_endorsements ctxt in
@@ -249,27 +250,26 @@ let finalize_block {mode; ctxt; op_count} =
         included_endorsements
       >>=? fun () ->
       Alpha_context.Vote.get_current_period_kind ctxt
-      >>=? fun voting_period_kind ->
+      >|=? fun voting_period_kind ->
       let ctxt = Alpha_context.finalize ctxt in
-      return
-        ( ctxt,
-          Apply_results.
-            {
-              baker;
-              level;
-              voting_period_kind;
-              nonce_hash = None;
-              consumed_gas = Z.zero;
-              deactivated = [];
-              balance_updates = [];
-            } )
+      ( ctxt,
+        Apply_results.
+          {
+            baker;
+            level;
+            voting_period_kind;
+            nonce_hash = None;
+            consumed_gas = Z.zero;
+            deactivated = [];
+            balance_updates = [];
+          } )
   | Application
       { baker;
         block_delay;
         block_header = {protocol_data = {contents = protocol_data; _}; _} }
   | Full_construction {protocol_data; baker; block_delay; _} ->
       Apply.finalize_application ctxt protocol_data baker ~block_delay
-      >>=? fun (ctxt, receipt) ->
+      >|=? fun (ctxt, receipt) ->
       let level = Alpha_context.Level.current ctxt in
       let priority = protocol_data.priority in
       let raw_level = Alpha_context.Raw_level.to_int32 level.level in
@@ -283,7 +283,7 @@ let finalize_block {mode; ctxt; op_count} =
           op_count
       in
       let ctxt = Alpha_context.finalize ~commit_message ctxt in
-      return (ctxt, receipt)
+      (ctxt, receipt)
 
 let compare_operations op1 op2 =
   let open Alpha_context in
@@ -365,11 +365,11 @@ let init ctxt block_header =
       Optimized
       parsed_script.storage_type
       storage
-    >>=? fun (storage, ctxt) ->
+    >|=? fun (storage, ctxt) ->
     let storage =
       Alpha_context.Script.lazy_expr (Micheline.strip_locations storage)
     in
-    return (({script with storage}, big_map_diff), ctxt)
+    (({script with storage}, big_map_diff), ctxt)
   in
   Alpha_context.prepare_first_block ~typecheck ~level ~timestamp ~fitness ctxt
-  >>=? fun ctxt -> return (Alpha_context.finalize ctxt)
+  >|=? fun ctxt -> Alpha_context.finalize ctxt