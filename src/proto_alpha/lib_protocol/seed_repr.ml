Piotr170687/tezos

Protocol/coq-of-ocaml: renaming to compile alpha_context

src/proto_alpha/lib_protocol/seed_repr.ml
@@ -35,7 +35,7 @@ type nonce = bytes
 
 let nonce_encoding = Data_encoding.Fixed.bytes Constants_repr.nonce_length
 
-let init = "Laissez-faire les proprietaires."
+let initial_seed = "Laissez-faire les proprietaires."
 
 let zero_bytes = Bytes.make Nonce_hash.size '\000'
 
@@ -50,7 +50,7 @@ let seed_encoding =
   let open Data_encoding in
   conv (fun (B b) -> b) (fun b -> B b) state_hash_encoding
 
-let empty = B (State_hash.hash_bytes [Bytes.of_string init])
+let empty = B (State_hash.hash_bytes [Bytes.of_string initial_seed])
 
 let nonce (B state) nonce =
   B (State_hash.hash_bytes [State_hash.to_bytes state; nonce])

tezos/tezos-mirror

src/proto_alpha/lib_protocol/seed_repr.ml
@@ -35,7 +35,7 @@ type nonce = MBytes.t
 
 let nonce_encoding = Data_encoding.Fixed.bytes Constants_repr.nonce_length
 
-let init = "Laissez-faire les proprietaires."
+let initial_seed = "Laissez-faire les proprietaires."
 
 let zero_bytes = MBytes.of_string (String.make Nonce_hash.size '\000')
 
@@ -50,7 +50,7 @@ let seed_encoding =
   let open Data_encoding in
   conv (fun (B b) -> b) (fun b -> B b) state_hash_encoding
 
-let empty = B (State_hash.hash_bytes [MBytes.of_string init])
+let empty = B (State_hash.hash_bytes [MBytes.of_string initial_seed])
 
 let nonce (B state) nonce =
   B (State_hash.hash_bytes [State_hash.to_bytes state; nonce])