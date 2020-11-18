c37dd2215ed6957ecf2f7f8f592535b51af839bb
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2020-11-18 16:15:45 +0100

    Tests/Python: test usage of tickets to implement tokens
    This example combines linear (big) map operators and tickets to
    implement FT and NFT builder and wallet contracts.

--- tests_python/tests/test_contract_onchain_opcodes.py
+++ tests_python/tests/test_contract_onchain_opcodes.py
@@ -3,7 +3,7 @@ import subprocess
 import pytest
 from tools.client_regression import ClientRegression
 from tools import paths
-from tools.paths import OPCODES_CONTRACT_PATH
+from tools.paths import OPCODES_CONTRACT_PATH, MINI_SCENARIOS_CONTRACT_PATH
 from tools.utils import assert_run_failure, assert_storage_contains, bake, \
     init_with_transfer, assert_balance
 from tools.constants import IDENTITIES
@@ -498,6 +498,364 @@ class TestTickets:
             client.transfer(100, 'bootstrap1', 'ticketer_a',
                             ['-arg', params(joiner_addr, 21, 23),
                              '--burn-cap', '10'])
+
+    def test_ticket_fungible_originations(self, client, session):
+        """Test the origination of builder and wallet contracts for fungible
+tokens implemented using tickets."""
+
+        builder_path = path.join(MINI_SCENARIOS_CONTRACT_PATH,
+                                 'ticket_builder_fungible.tz')
+
+        wallet_path = path.join(MINI_SCENARIOS_CONTRACT_PATH,
+                                'ticket_wallet_fungible.tz')
+
+        manager_address = IDENTITIES['bootstrap1']['identity']
+
+        builders = {}
+        wallets = {}
+
+        # Helper functions
+        def originate_builder(name):
+            """Create a fungible token contract managed by bootstrap1."""
+            origination = client.originate(
+                contract_name=f'builder_{name}',
+                amount="0",
+                sender='bootstrap1',
+                contract=builder_path,
+                args=['--init', f'"{manager_address}"',
+                      '--burn-cap', "10"])
+            builders[name] = origination.contract
+            bake(client)
+
+        def originate_wallet(name):
+            """Create a fungible token wallet managed by bootstrap1."""
+            origination = client.originate(
+                contract_name=f'wallet_{name}',
+                amount="0",
+                sender='bootstrap1',
+                contract=wallet_path,
+                args=['--init', f'Pair "{manager_address}" {{}}',
+                      '--burn-cap', "10"])
+            wallets[name] = origination.contract
+            bake(client)
+
+        # Create 3 token contracts "A", "B", and "C".
+        originate_builder("A")
+        originate_builder("B")
+        originate_builder("C")
+
+        # Create 2 wallets "Alice" and "Bob".
+        originate_wallet("Alice")
+        originate_wallet("Bob")
+
+        session['fungible_builders'] = builders
+        session['fungible_wallets'] = wallets
+
+    def test_ticket_fungible_transfers(self, client, session):
+        """Test the life cycle of fungible tokens implemented using tickets."""
+
+        manager_address = IDENTITIES['bootstrap1']['identity']
+
+        builders = session['fungible_builders']
+        wallets = session['fungible_wallets']
+
+        def mint(builder, wallet, amount):
+            """Mint fungible tokens."""
+            wallet_address = wallets[wallet]
+            parameter = f'(Pair "{wallet_address}%receive" {amount})'
+            client.transfer(amount=0,
+                            giver=manager_address,
+                            receiver=f'builder_{builder}',
+                            args=['--burn-cap', '2',
+                                  '--entrypoint', 'mint',
+                                  '--arg', parameter])
+            bake(client)
+
+        def burn(builder, wallet, amount):
+            """Burn fungible tokens."""
+            builder_addr = builders[builder]
+            parameter = f'Pair "{builder_addr}%burn" {amount} "{builder_addr}"'
+            client.transfer(amount=0,
+                            giver=manager_address,
+                            receiver=f'wallet_{wallet}',
+                            args=['--burn-cap', '2',
+                                  '--entrypoint', 'send',
+                                  '--arg', parameter])
+            bake(client)
+
+        def transfer(builder, source_wallet, destination_wallet, amount):
+            """Transfer fungible tokens."""
+            builder_addr = builders[builder]
+            dest_addr = wallets[destination_wallet]
+            parameter = f'Pair "{dest_addr}%receive" {amount} "{builder_addr}"'
+            client.transfer(amount=0,
+                            giver=manager_address,
+                            receiver=f'wallet_{source_wallet}',
+                            args=['--burn-cap', '2',
+                                  '--entrypoint', 'send',
+                                  '--arg', parameter])
+            bake(client)
+
+        # 100A --> Alice
+        mint(builder="A", wallet="Alice", amount=100)
+        # 100B --> Alice
+        mint(builder="B", wallet="Alice", amount=100)
+
+        # Fail: Alice --1C--> Bob
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="C",
+                     source_wallet="Alice",
+                     destination_wallet="Bob",
+                     amount=1)
+
+        # Fail: Alice --0C--> Bob
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="C",
+                     source_wallet="Alice",
+                     destination_wallet="Bob",
+                     amount=0)
+
+        # Fail: Alice --150A--> Bob
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="A",
+                     source_wallet="Alice",
+                     destination_wallet="Bob",
+                     amount=150)
+
+        # Fail: Bob --50A--> Alice
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="A",
+                     source_wallet="Bob",
+                     destination_wallet="Alice",
+                     amount=50)
+
+        # Alice --50A--> Bob
+        transfer(builder="A",
+                 source_wallet="Alice",
+                 destination_wallet="Bob",
+                 amount=50)
+
+        # Alice --50A--> Bob
+        transfer(builder="A",
+                 source_wallet="Alice",
+                 destination_wallet="Bob",
+                 amount=50)
+
+        # Alice --0A--> Bob
+        transfer(builder="A",
+                 source_wallet="Alice",
+                 destination_wallet="Bob",
+                 amount=0)
+
+        # Fail: Alice --1A--> Bob
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="A",
+                     source_wallet="Alice",
+                     destination_wallet="Bob",
+                     amount=1)
+
+        # Bob --100A--> Bob
+        transfer(builder="A",
+                 source_wallet="Bob",
+                 destination_wallet="Bob",
+                 amount=100)
+
+        # Fail: Bob --150A--> Bob
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="A",
+                     source_wallet="Bob",
+                     destination_wallet="Bob",
+                     amount=150)
+
+        # Bob --100A-->
+        burn(builder="A", wallet="Bob", amount=100)
+
+        # Bob --0A-->
+        burn(builder="A", wallet="Bob", amount=0)
+
+        # Fail: Bob --1A-->
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            burn(builder="A", wallet="Bob", amount=1)
+
+    def test_ticket_non_fungible_originations(self, client, session):
+        """Test the origination of builder and wallet contracts for
+non-fungible tokens implemented using tickets."""
+
+        builder_path = path.join(MINI_SCENARIOS_CONTRACT_PATH,
+                                 'ticket_builder_non_fungible.tz')
+
+        wallet_path = path.join(MINI_SCENARIOS_CONTRACT_PATH,
+                                'ticket_wallet_non_fungible.tz')
+
+        manager_address = IDENTITIES['bootstrap1']['identity']
+
+        builders = {}
+        wallets = {}
+
+        # Helper functions
+        def originate_builder(name):
+            """Create a non-fungible token contract managed by bootstrap1."""
+            storage = f'(Pair "{manager_address}" 0)'
+            origination = client.originate(
+                contract_name=f'nft_builder_{name}',
+                amount="0",
+                sender='bootstrap1',
+                contract=builder_path,
+                args=['--init', storage,
+                      '--burn-cap', "10"])
+            builders[name] = origination.contract
+            bake(client)
+
+        def originate_wallet(name):
+            """Create a non-fungible token wallet managed by bootstrap1."""
+            origination = client.originate(
+                contract_name=f'nft_wallet_{name}',
+                amount="0",
+                sender='bootstrap1',
+                contract=wallet_path,
+                args=['--init', f'Pair "{manager_address}" {{}}',
+                      '--burn-cap', "10"])
+            wallets[name] = origination.contract
+            bake(client)
+
+        # Create 3 token contracts "A", "B", and "C".
+        originate_builder("A")
+        originate_builder("B")
+        originate_builder("C")
+
+        # Create 2 wallets "Alice" and "Bob".
+        originate_wallet("Alice")
+        originate_wallet("Bob")
+
+        session['non_fungible_builders'] = builders
+        session['non_fungible_wallets'] = wallets
+
+    def test_ticket_non_fungible_transfers(self, client, session):
+        """Test the life cycle of non-fungible tokens implemented using
+tickets."""
+
+        manager_address = IDENTITIES['bootstrap1']['identity']
+
+        builders = session['non_fungible_builders']
+        wallets = session['non_fungible_wallets']
+
+        def mint(builder, wallet, token_id):
+            """Mint a non-fungible token and assert that it has the expected
+id."""
+            builder_alias = f'nft_builder_{builder}'
+            expected_builder_storage = f'Pair "{manager_address}" {token_id}'
+            actual_builder_storage = client.get_storage(builder_alias)
+            assert expected_builder_storage == actual_builder_storage
+
+            wallet_address = wallets[wallet]
+            parameter = f'"{wallet_address}%receive"'
+            client.transfer(amount=0,
+                            giver=manager_address,
+                            receiver=builder_alias,
+                            args=['--burn-cap', '2',
+                                  '--entrypoint', 'mint_destination',
+                                  '--arg', parameter])
+            bake(client)
+
+        def burn(builder, wallet, token_id):
+            """Burn a non-fungible token."""
+            builder_addr = builders[builder]
+            parameter = \
+                f'Pair "{builder_addr}%burn" "{builder_addr}" {token_id}'
+            client.transfer(amount=0,
+                            giver=manager_address,
+                            receiver=f'nft_wallet_{wallet}',
+                            args=['--burn-cap', '2',
+                                  '--entrypoint', 'send',
+                                  '--arg', parameter])
+            bake(client)
+
+        def transfer(builder, source_wallet, destination_wallet, token_id):
+            """Transfer fungible tokens."""
+            builder_addr = builders[builder]
+            dest_addr = wallets[destination_wallet]
+            parameter = \
+                f'Pair "{dest_addr}%receive" "{builder_addr}" {token_id}'
+            client.transfer(amount=0,
+                            giver=manager_address,
+                            receiver=f'nft_wallet_{source_wallet}',
+                            args=['--burn-cap', '2',
+                                  '--entrypoint', 'send',
+                                  '--arg', parameter])
+            bake(client)
+
+        # A0 --> Alice
+        mint(builder="A", wallet="Alice", token_id=0)
+        # A1 --> Alice
+        mint(builder="A", wallet="Alice", token_id=1)
+        # B0 --> Alice
+        mint(builder="B", wallet="Alice", token_id=0)
+
+        # Fail: Alice --C0--> Bob
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="C",
+                     source_wallet="Alice",
+                     destination_wallet="Bob",
+                     token_id=0)
+
+        # Fail: Alice --A2--> Bob
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="A",
+                     source_wallet="Alice",
+                     destination_wallet="Bob",
+                     token_id=2)
+
+        # Fail: Bob --A0--> Alice
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="A",
+                     source_wallet="Bob",
+                     destination_wallet="Alice",
+                     token_id=0)
+
+        # Fail: Bob --A1--> Bob
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="A",
+                     source_wallet="Bob",
+                     destination_wallet="Bob",
+                     token_id=1)
+
+        # Alice --A1--> Bob
+        transfer(builder="A",
+                 source_wallet="Alice",
+                 destination_wallet="Bob",
+                 token_id=1)
+
+        # Alice --A0--> Bob
+        transfer(builder="A",
+                 source_wallet="Alice",
+                 destination_wallet="Bob",
+                 token_id=0)
+
+        # Fail: Alice --A1--> Bob
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            transfer(builder="A",
+                     source_wallet="Alice",
+                     destination_wallet="Bob",
+                     token_id=1)
+
+        # Bob --A0--> Bob
+        transfer(builder="A",
+                 source_wallet="Bob",
+                 destination_wallet="Bob",
+                 token_id=0)
+
+        # Bob --A0-->
+        burn(builder="A", wallet="Bob", token_id=0)
+
+        # Bob --A1-->
+        burn(builder="A", wallet="Bob", token_id=1)
+
+        # Fail: Bob --B0-->
+        with assert_run_failure(r'script reached FAILWITH instruction'):
+            burn(builder="B", wallet="Bob", token_id=0)
+
+        # Alice --B0-->
+        burn(builder="B", wallet="Alice", token_id=0)
 
 
 ORIGINATE_BIG_MAP_FILE = path.join(OPCODES_CONTRACT_PATH,
