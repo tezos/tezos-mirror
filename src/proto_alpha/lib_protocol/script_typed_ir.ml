Piotr170687/tezos

Comparable types: allow non-comb pairs

src/proto_alpha/lib_protocol/script_typed_ir.ml
@@ -42,29 +42,21 @@ type ('a, 'b) pair = 'a * 'b
 
 type ('a, 'b) union = L of 'a | R of 'b
 
-type comb = Comb
-
-type leaf = Leaf
-
-type (_, _) comparable_struct =
-  | Int_key : type_annot option -> (z num, _) comparable_struct
-  | Nat_key : type_annot option -> (n num, _) comparable_struct
-  | String_key : type_annot option -> (string, _) comparable_struct
-  | Bytes_key : type_annot option -> (MBytes.t, _) comparable_struct
-  | Mutez_key : type_annot option -> (Tez.t, _) comparable_struct
-  | Bool_key : type_annot option -> (bool, _) comparable_struct
-  | Key_hash_key : type_annot option -> (public_key_hash, _) comparable_struct
-  | Timestamp_key :
-      type_annot option
-      -> (Script_timestamp.t, _) comparable_struct
-  | Address_key : type_annot option -> (address, _) comparable_struct
+type _ comparable_ty =
+  | Int_key : type_annot option -> z num comparable_ty
+  | Nat_key : type_annot option -> n num comparable_ty
+  | String_key : type_annot option -> string comparable_ty
+  | Bytes_key : type_annot option -> MBytes.t comparable_ty
+  | Mutez_key : type_annot option -> Tez.t comparable_ty
+  | Bool_key : type_annot option -> bool comparable_ty
+  | Key_hash_key : type_annot option -> public_key_hash comparable_ty
+  | Timestamp_key : type_annot option -> Script_timestamp.t comparable_ty
+  | Address_key : type_annot option -> address comparable_ty
   | Pair_key :
-      (('a, leaf) comparable_struct * field_annot option)
-      * (('b, comb) comparable_struct * field_annot option)
+      ('a comparable_ty * field_annot option)
+      * ('b comparable_ty * field_annot option)
       * type_annot option
-      -> (('a, 'b) pair, comb) comparable_struct
-
-type 'a comparable_ty = ('a, comb) comparable_struct
+      -> ('a, 'b) pair comparable_ty
 
 module type Boxed_set = sig
   type elt

tezos/tezos-mirror

src/proto_alpha/lib_protocol/script_typed_ir.ml
@@ -40,29 +40,21 @@ type ('a, 'b) pair = 'a * 'b
 
 type ('a, 'b) union = L of 'a | R of 'b
 
-type comb = Comb
-
-type leaf = Leaf
-
-type (_, _) comparable_struct =
-  | Int_key : type_annot option -> (z num, _) comparable_struct
-  | Nat_key : type_annot option -> (n num, _) comparable_struct
-  | String_key : type_annot option -> (string, _) comparable_struct
-  | Bytes_key : type_annot option -> (Bytes.t, _) comparable_struct
-  | Mutez_key : type_annot option -> (Tez.t, _) comparable_struct
-  | Bool_key : type_annot option -> (bool, _) comparable_struct
-  | Key_hash_key : type_annot option -> (public_key_hash, _) comparable_struct
-  | Timestamp_key :
-      type_annot option
-      -> (Script_timestamp.t, _) comparable_struct
-  | Address_key : type_annot option -> (address, _) comparable_struct
+type _ comparable_ty =
+  | Int_key : type_annot option -> z num comparable_ty
+  | Nat_key : type_annot option -> n num comparable_ty
+  | String_key : type_annot option -> string comparable_ty
+  | Bytes_key : type_annot option -> Bytes.t comparable_ty
+  | Mutez_key : type_annot option -> Tez.t comparable_ty
+  | Bool_key : type_annot option -> bool comparable_ty
+  | Key_hash_key : type_annot option -> public_key_hash comparable_ty
+  | Timestamp_key : type_annot option -> Script_timestamp.t comparable_ty
+  | Address_key : type_annot option -> address comparable_ty
   | Pair_key :
-      (('a, leaf) comparable_struct * field_annot option)
-      * (('b, comb) comparable_struct * field_annot option)
+      ('a comparable_ty * field_annot option)
+      * ('b comparable_ty * field_annot option)
       * type_annot option
-      -> (('a, 'b) pair, comb) comparable_struct
-
-type 'a comparable_ty = ('a, comb) comparable_struct
+      -> ('a, 'b) pair comparable_ty
 
 module type Boxed_set = sig
   type elt