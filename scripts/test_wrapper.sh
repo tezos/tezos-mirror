7d10d673a99b51cb2577dea140ac57fcc9502a19
Pietro <pietro.abate@tezcore.com>
2020-11-20 16:04:50 +0100

    scripts: return the exit code while wrapping a unit test

--- scripts/test_wrapper.sh
+++ scripts/test_wrapper.sh
@@ -8,6 +8,14 @@ mkdir -p test_results
 echo -n "Running test \"dune build @$nametest/runtest\" ..."
 
 dune build "@$nametest/runtest" > "test_results/$name.log" 2>&1
+EXITCODE=$?
 
-if [ $? ]; then echo "Ok"; else echo "Error"; fi
+if [ $EXITCODE -eq 0 ]; then
+  echo "Ok";
+else
+  echo "Error";
+  echo "Exited with exitcode $EXITCODE"
+  cat "test_results/$name.log"
+fi
 
+exit $EXITCODE
