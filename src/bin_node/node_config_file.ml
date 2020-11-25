7b91820cfcdb0267dae56ccfd529a9aee9bdf524
Romain Bardou <romain@nomadic-labs.com>
2020-11-25 17:10:51 +0100

    Node: remove --network carthagenet

--- src/bin_node/node_config_file.ml
+++ src/bin_node/node_config_file.ml
@@ -102,23 +102,6 @@ let blockchain_network_mainnet =
           "PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS" ) ]
     ~default_bootstrap_peers:["boot.tzbeta.net"]
 
-let blockchain_network_carthagenet =
-  make_blockchain_network
-    ~alias:"carthagenet"
-    {
-      time = Time.Protocol.of_notation_exn "2019-11-28T13:02:13Z";
-      block =
-        Block_hash.of_b58check_exn
-          "BLockGenesisGenesisGenesisGenesisGenesisd6f5afWyME7";
-      protocol =
-        Protocol_hash.of_b58check_exn
-          "PtYuensgYBb3G3x1hLLbCmcav8ue8Kyd2khADcL5LsT5R1hcXex";
-    }
-    ~chain_name:"TEZOS_ALPHANET_CARTHAGE_2019-11-28T13:02:13Z"
-    ~sandboxed_chain_name:"SANDBOXED_TEZOS"
-    ~default_bootstrap_peers:
-      ["34.76.169.218"; "34.90.24.160"; "carthagenet.kaml.fr"; "104.248.136.94"]
-
 let blockchain_network_delphinet =
   make_blockchain_network
     ~alias:"delphinet"
@@ -240,7 +223,6 @@ let blockchain_network_encoding : blockchain_network Data_encoding.t =
 let builtin_blockchain_networks_with_tags =
   [ (1, blockchain_network_sandbox);
     (4, blockchain_network_mainnet);
-    (6, blockchain_network_carthagenet);
     (9, blockchain_network_delphinet) ]
   |> List.map (fun (tag, network) ->
          match network.alias with
