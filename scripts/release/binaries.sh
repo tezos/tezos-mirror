fee2c4b0718f0d8d1c15b758b97098ccba2ae1ed
Roman Melnikov <roman.melnikov@serokell.io>
2020-11-30 17:30:24 +0300

    fixup! Add static x86 Linux binaries building to CI

--- scripts/release/binaries.sh
+++ scripts/release/binaries.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-binaries=("tezos-admin-client" "tezos-client" "tezos-node" "tezos-signer" "tezos-codec" "tezos-sandbox")
+binaries=("tezos-admin-client" "tezos-client" "tezos-node" "tezos-signer" "tezos-codec")
 protocols=("006-PsCARTHA" "007-PsDELPH1" "alpha")
 
 for proto in "${protocols[@]}"; do
