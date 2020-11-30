07f4ba6f029447e8435f8b63112d683d9332f38e
vbot <vincent.botbol@nomadic-labs.com>
2020-11-30 15:38:09 +0100

    Shell: refactor and inline block validator events

--- src/lib_shell/block_validator_process.ml
+++ src/lib_shell/block_validator_process.ml
@@ -72,7 +72,51 @@ type t =
 
 (** The standard block validation method *)
 module Internal_validator_process = struct
-  include Block_validator_process_state.Seq_validator_events
+  module Events = struct
+    open Internal_event.Simple
+
+    let section = ["sequential_block_validator"]
+
+    let init =
+      declare_0
+        ~section
+        ~level:Notice
+        ~name:"seq_initialized"
+        ~msg:"initialized"
+        ()
+
+    let close =
+      declare_0
+        ~section
+        ~level:Notice
+        ~name:"seq_close"
+        ~msg:"shutting down"
+        ()
+
+    let validation_request =
+      declare_2
+        ~section
+        ~level:Debug
+        ~name:"seq_validation_request"
+        ~msg:"requesting validation of {block} for chain {chain}"
+        ~pp1:Block_hash.pp
+        ("block", Block_hash.encoding)
+        ~pp2:Chain_id.pp
+        ("chain", Chain_id.encoding)
+
+    let validation_success =
+      declare_2
+        ~section
+        ~level:Debug
+        ~name:"seq_validation_success"
+        ~msg:"block {block} successfully validated in {timespan}"
+        ~pp1:Block_hash.pp
+        ("block", Block_hash.encoding)
+        ~pp2:Time.System.Span.pp_hum
+        ("timespan", Time.System.Span.encoding)
+
+    let emit = Internal_event.Simple.emit
+  end
 
   type t = {
     context_index : Context.index;
@@ -84,7 +128,7 @@ module Internal_validator_process = struct
   let init
       ({genesis; user_activated_upgrades; user_activated_protocol_overrides} :
         validator_environment) context_index =
-    lwt_emit Init
+    Events.(emit init ())
     >>= fun () ->
     return
       {
@@ -94,7 +138,7 @@ module Internal_validator_process = struct
         user_activated_protocol_overrides;
       }
 
-  let close _ = lwt_emit Close
+  let close _ = Events.(emit close ())
 
   let make_apply_environment
       { user_activated_upgrades;
@@ -133,12 +177,17 @@ module Internal_validator_process = struct
       operations =
     make_apply_environment validator predecessor max_operations_ttl
     >>=? fun env ->
-    lwt_timed_emit
-      (Validation_request (Block_header.hash block_header, env.chain_id))
-    >>= fun event_start ->
+    let now = Systime_os.now () in
+    let block_hash = Block_header.hash block_header in
+    Events.(emit validation_request (block_hash, env.chain_id))
+    >>= fun () ->
     Block_validation.apply env block_header operations
     >>=? fun result ->
-    lwt_emit (Validation_success (Block_header.hash block_header, event_start))
+    let timespan =
+      let then_ = Systime_os.now () in
+      Ptime.diff then_ now
+    in
+    Events.(emit validation_success (block_hash, timespan))
     >>= fun () -> return result
 
   let commit_genesis validator ~chain_id =
@@ -159,7 +208,114 @@ end
 
 (** Block validation using an external process *)
 module External_validator_process = struct
-  include Block_validator_process_state.External_validator_events
+  module Events = struct
+    open Internal_event.Simple
+
+    let section = ["external_block_validator"]
+
+    let init =
+      declare_0
+        ~section
+        ~level:Notice
+        ~name:"proc_initialized"
+        ~msg:"initialized"
+        ()
+
+    let close =
+      declare_0
+        ~section
+        ~level:Notice
+        ~name:"proc_close"
+        ~msg:"shutting down"
+        ()
+
+    let process_exited_abnormally =
+      let open Unix in
+      let process_status_encoding =
+        let open Data_encoding in
+        union
+          [ case
+              (Tag 0)
+              ~title:"wexited"
+              int31
+              (function WEXITED i -> Some i | _ -> None)
+              (fun i -> WEXITED i);
+            case
+              (Tag 1)
+              ~title:"wsignaled"
+              int31
+              (function WSIGNALED i -> Some i | _ -> None)
+              (fun i -> WSIGNALED i);
+            case
+              (Tag 2)
+              ~title:"wstopped"
+              int31
+              (function WSTOPPED i -> Some i | _ -> None)
+              (fun i -> WSTOPPED i) ]
+      in
+      declare_1
+        ~section
+        ~level:Error
+        ~name:"proc_status"
+        ~msg:"{status_msg}"
+        ~pp1:(fun fmt status ->
+          match status with
+          | WEXITED i ->
+              Format.fprintf
+                fmt
+                "process terminated abnormally with exit code %i"
+                i
+          | WSIGNALED i ->
+              Format.fprintf
+                fmt
+                "process was killed by signal %s"
+                (Lwt_exit.signal_name i)
+          | WSTOPPED i ->
+              Format.fprintf
+                fmt
+                "process was stopped by signal %s"
+                (Lwt_exit.signal_name i))
+        ("status_msg", process_status_encoding)
+
+    let process_exited_normally =
+      declare_0
+        ~section
+        ~level:Notice
+        ~name:"proc_exited_normally"
+        ~msg:"process terminated normally"
+        ()
+
+    let validator_started =
+      declare_1
+        ~section
+        ~level:Notice
+        ~name:"proc_validator_started"
+        ~msg:"block validator process started with pid {pid}"
+        ~pp1:Format.pp_print_int
+        ("pid", Data_encoding.int31)
+
+    let request_for =
+      declare_1
+        ~section
+        ~level:Debug
+        ~name:"proc_request"
+        ~msg:"request for {request}"
+        ~pp1:External_validation.request_pp
+        ("request", External_validation.request_encoding)
+
+    let request_result =
+      declare_2
+        ~section
+        ~level:Debug
+        ~name:"proc_request_result"
+        ~msg:"completion of {request_result} in {timespan}"
+        ~pp1:External_validation.request_pp
+        ("request_result", External_validation.request_encoding)
+        ~pp2:Time.System.Span.pp_hum
+        ("timespan", Time.System.Span.encoding)
+
+    let emit = Internal_event.Simple.emit
+  end
 
   type validator_process = {
     process : Lwt_process.process_none;
@@ -243,7 +399,7 @@ module External_validator_process = struct
     >>= fun (process, process_socket) ->
     let process_stdin = Lwt_io.of_fd ~mode:Output process_socket in
     let process_stdout = Lwt_io.of_fd ~mode:Input process_socket in
-    lwt_emit (Validator_started process#pid)
+    Events.(emit validator_started process#pid)
     >>= fun () ->
     let parameters =
       {
@@ -287,7 +443,8 @@ module External_validator_process = struct
           Lwt_canceler.cancel canceler
           >>= fun () ->
           vp.validator_process <- None ;
-          lwt_emit (Process_status status) >>= fun () -> start_process vp )
+          Events.(emit process_exited_abnormally status)
+          >>= fun () -> start_process vp )
     | None ->
         start_process vp )
     >>=? fun (process, process_stdin, process_stdout) ->
@@ -296,8 +453,9 @@ module External_validator_process = struct
         (* Make sure that the promise is not canceled between a send and recv *)
         Lwt.protected
           (Lwt_mutex.with_lock vp.lock (fun () ->
-               lwt_timed_emit (Request request)
-               >>= fun event_start ->
+               let now = Systime_os.now () in
+               Events.(emit request_for request)
+               >>= fun () ->
                External_validation.send
                  process_stdin
                  External_validation.request_encoding
@@ -305,7 +463,11 @@ module External_validator_process = struct
                >>= fun () ->
                External_validation.recv_result process_stdout result_encoding
                >>= fun res ->
-               lwt_emit (Request_result (request, event_start))
+               let timespan =
+                 let then_ = Systime_os.now () in
+                 Ptime.diff then_ now
+               in
+               Events.(emit request_result (request, timespan))
                >>= fun () -> Lwt.return res))
         >>=? fun res ->
         match process#state with
@@ -313,14 +475,15 @@ module External_validator_process = struct
             return res
         | Exited status ->
             vp.validator_process <- None ;
-            lwt_emit (Process_status status) >>= fun () -> return res)
+            Events.(emit process_exited_abnormally status)
+            >>= fun () -> return res)
       (function
         | errors ->
             ( match process#state with
             | Running ->
                 Lwt.return_unit
             | Exited status ->
-                lwt_emit (Process_status status)
+                Events.(emit process_exited_abnormally status)
                 >>= fun () ->
                 vp.validator_process <- None ;
                 Lwt.return_unit )
@@ -330,7 +493,7 @@ module External_validator_process = struct
       ({genesis; user_activated_upgrades; user_activated_protocol_overrides} :
         validator_environment) data_dir context_root protocol_root process_path
       sandbox_parameters =
-    lwt_emit Init
+    Events.(emit init ())
     >>= fun () ->
     let validator =
       {
@@ -389,12 +552,12 @@ module External_validator_process = struct
     send_request validator request Data_encoding.(option int31)
 
   let close vp =
-    lwt_emit Close
+    Events.(emit close ())
     >>= fun () ->
     match vp.validator_process with
     | Some {process; stdin = process_stdin; canceler; _} ->
         let request = External_validation.Terminate in
-        lwt_emit (Request request)
+        Events.(emit request_for request)
         >>= fun () ->
         External_validation.send
           process_stdin
@@ -404,9 +567,11 @@ module External_validator_process = struct
         process#status
         >>= (function
               | Unix.WEXITED 0 ->
-                  Lwt.return_unit
-              | _ ->
-                  process#terminate ; Lwt.return_unit)
+                  Events.(emit process_exited_normally ())
+                  >>= fun () -> Lwt.return_unit
+              | status ->
+                  Events.(emit process_exited_abnormally status)
+                  >>= fun () -> process#terminate ; Lwt.return_unit)
         >>= fun () ->
         Lwt_canceler.cancel canceler
         >>= fun () ->
