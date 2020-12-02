455b48f18c46dab82a8cc921e0966136cedf9e73
vbot <vincent.botbol@nomadic-labs.com>
2020-12-02 13:45:21 +0100

    Shell/Events: refactor costly prevalidator debugging messages

--- src/lib_shell_services/prevalidator_worker_state.mli
+++ src/lib_shell_services/prevalidator_worker_state.mli
@@ -45,7 +45,12 @@ module Event : sig
   type t =
     | Request of
         (Request.view * Worker_types.request_status * error list option)
-    | Debug of string
+    | Invalid_mempool_filter_configuration
+    | Unparsable_operation of Operation_hash.t
+    | Processing_n_operations of int
+    | Fetching_operation of Operation_hash.t
+    | Operation_included of Operation_hash.t
+    | Operations_not_flushed of int
 
   type view = t
 
