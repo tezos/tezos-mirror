842f01e99c11b84fd2e2d577758e4c6102ea460a
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2021-01-06 18:33:10 +0100

    Release: build and publish arm64 static binaries

--- scripts/release/upload-static-binaries-to-package-registry.sh
+++ scripts/release/upload-static-binaries-to-package-registry.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
+ARCH_PREFIX=$1
+
 . scripts/release/binaries.sh
 
 for binary in "${binaries[@]}"; do
     curl --header "JOB-TOKEN: $CI_JOB_TOKEN" \
          --upload-file "install_root/bin/$binary" \
-         "$PACKAGE_REGISTRY_URL/$binary"
+         "$PACKAGE_REGISTRY_URL/${ARCH_PREFIX}$binary"
 done
 
 # Create .tag.gz archive with all binaries and upload it
@@ -16,4 +18,4 @@ tar -czf ../../tezos-binaries.tar.gz .
 cd ../..
 curl --header "JOB-TOKEN: $CI_JOB_TOKEN" \
      --upload-file tezos-binaries.tar.gz \
-     "$PACKAGE_REGISTRY_URL/tezos-binaries.tar.gz"
+     "$PACKAGE_REGISTRY_URL/${ARCH_PREFIX}tezos-binaries.tar.gz"
