cb20c47d0ad0c72196be06ba3a6dd236308bc424
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2021-01-06 15:38:04 +0100

    Build: downgrade for now tls to solve tezos/tezos#1038

--- scripts/version.sh
+++ scripts/version.sh
@@ -10,10 +10,10 @@ recommended_rust_version=1.44.0
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=fb801efd13b2e6370271608f2bfd528f2eea72bb
+full_opam_repository_tag=bf94421703ae6d95113e5b24890f304701e47b78
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=c3537f792ddeb4682b4a5d0716c63dfb9d0995e5
+opam_repository_tag=4eb9728016e05758054c600ddc66c7e295c27a26
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
