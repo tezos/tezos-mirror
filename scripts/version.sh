924facb32c5cb25e045ea71535d995488eb74ca1
Manas <3097018+ManasJayanth@users.noreply.github.com>
2021-01-10 19:32:22 +0530

    Makes Tezos buildable on MacOS arm64
    * Recommend Python 3.9.5 since it works on MacOS arm64
    (note that 3.9.1 is the first version to support Macos arm64)
    * Set compatible python versions in pyproject.toml
    * Recommend rust 1.52.0
    * Regen poetry lock files

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,7 +5,7 @@
 
 ocaml_version=4.12.0
 opam_version=2.0
-recommended_rust_version=1.44.0
+recommended_rust_version=1.52.0
 
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
