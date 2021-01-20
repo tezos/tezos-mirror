64474f01b9e208e4b9a9d3cca7dad871aead73af
Raphaël Proust <code@bnwr.net>
2021-01-20 11:37:53 +0100

    CI: update opam repository commit hash

--- scripts/version.sh
+++ scripts/version.sh
@@ -10,10 +10,10 @@ recommended_rust_version=1.44.0
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=bf94421703ae6d95113e5b24890f304701e47b78
+full_opam_repository_tag=5491aa2960fd7b103b4461772b7badb475061d70
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=4eb9728016e05758054c600ddc66c7e295c27a26
+opam_repository_tag=dd4bc58ba8b5ba8d1fe24796623f51fee6e7a5c3
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
