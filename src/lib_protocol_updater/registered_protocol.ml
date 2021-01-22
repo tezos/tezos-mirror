samoht/tezos

env:add environment v2

src/lib_protocol_updater/registered_protocol.ml
@@ -90,6 +90,27 @@ let build hash =
 
           let complete_b58prefix = Env.Context.complete
         end : T )
+  | Some (V2 protocol) ->
+      let (module F) = protocol in
+      let module Name = struct
+        let name = Protocol_hash.to_b58check hash
+      end in
+      let module Env = Tezos_protocol_environment.MakeV1 (Name) () in
+      Some
+        ( module struct
+          module Raw = F (Env)
+
+          module P = struct
+            let hash = hash
+
+            include Env.Lift (Raw)
+          end
+
+          include P
+          module Block_services = Block_services.Make (P) (P)
+
+          let complete_b58prefix = Env.Context.complete
+        end : T )
 
 module VersionTable = Protocol_hash.Table
 
@@ -184,6 +205,38 @@ end
 
 module Register_embedded_V1
     (Env : Tezos_protocol_environment.V1)
+    (Proto : Env.Updater.PROTOCOL)
+    (Source : Source_sig) =
+struct
+  let hash =
+    match Source.hash with
+    | None ->
+        Protocol.hash Source.sources
+    | Some hash ->
+        hash
+
+  module Self = struct
+    module P = struct
+      let hash = hash
+
+      include Env.Lift (Proto)
+    end
+
+    include P
+    module Block_services = Block_services.Make (P) (P)
+
+    let complete_b58prefix = Env.Context.complete
+  end
+
+  let () =
+    VersionTable.add sources hash Source.sources ;
+    VersionTable.add versions hash (module Self : T)
+
+  include Self
+end
+
+module Register_embedded_V2
+    (Env : Tezos_protocol_environment.V2)
     (Proto : Env.Updater.PROTOCOL)
     (Source : Source_sig) =
 struct

tezos/tezos-mirror

src/lib_protocol_updater/registered_protocol.ml
@@ -90,6 +90,27 @@ let build hash =
 
           let complete_b58prefix = Env.Context.complete
         end : T )
+  | Some (V2 protocol) ->
+      let (module F) = protocol in
+      let module Name = struct
+        let name = Protocol_hash.to_b58check hash
+      end in
+      let module Env = Tezos_protocol_environment.MakeV2 (Name) () in
+      Some
+        ( module struct
+          module Raw = F (Env)
+
+          module P = struct
+            let hash = hash
+
+            include Env.Lift (Raw)
+          end
+
+          include P
+          module Block_services = Block_services.Make (P) (P)
+
+          let complete_b58prefix = Env.Context.complete
+        end : T )
 
 module VersionTable = Protocol_hash.Table
 
@@ -184,6 +205,38 @@ end
 
 module Register_embedded_V1
     (Env : Tezos_protocol_environment.V1)
+    (Proto : Env.Updater.PROTOCOL)
+    (Source : Source_sig) =
+struct
+  let hash =
+    match Source.hash with
+    | None ->
+        Protocol.hash Source.sources
+    | Some hash ->
+        hash
+
+  module Self = struct
+    module P = struct
+      let hash = hash
+
+      include Env.Lift (Proto)
+    end
+
+    include P
+    module Block_services = Block_services.Make (P) (P)
+
+    let complete_b58prefix = Env.Context.complete
+  end
+
+  let () =
+    VersionTable.add sources hash Source.sources ;
+    VersionTable.add versions hash (module Self : T)
+
+  include Self
+end
+
+module Register_embedded_V2
+    (Env : Tezos_protocol_environment.V2)
     (Proto : Env.Updater.PROTOCOL)
     (Source : Source_sig) =
 struct