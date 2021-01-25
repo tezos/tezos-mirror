eea32cc908df18256b994a01aeeac4548da28ea8
Eugen Zalinescu <eugen.zalinescu@nomadic-labs.com>
2021-01-25 14:32:20 +0100

    lib_shell: reduce max_timestamp_drift value

--- src/lib_shell/clock_drift.ml
+++ src/lib_shell/clock_drift.ml
@@ -22,7 +22,7 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let max_timestamp_drift : Ptime.span = Ptime.Span.of_int_s 15
+let max_timestamp_drift : Ptime.span = Ptime.Span.of_int_s 5
 
 let soon : unit -> Time.Protocol.t =
  fun () ->
