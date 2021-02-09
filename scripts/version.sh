271e04701619da723e27dbabf7fbc543a0c157ca
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2021-02-09 20:21:59 +0100

    Build: update opam-repository with fixed hacl.0.3.0-1

--- scripts/version.sh
+++ scripts/version.sh
@@ -10,10 +10,10 @@ recommended_rust_version=1.44.0
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=bf94421703ae6d95113e5b24890f304701e47b78
+full_opam_repository_tag=166a3a212b1c2e088dcedde0442c07514fd9efa6
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=4eb9728016e05758054c600ddc66c7e295c27a26
+opam_repository_tag=b7b6954bce4612682abc76139171caeb2abd1b80
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
