Piotr170687/tezos

Protocol/client: add commands sign/check "message"
Adds two new command to the client :
- sign message <msg> for <src>
  create an failing_noop operation then sign it. By default it will use the
  Genesis block as context to sign it.
- check that message <msg> was signed by <src_pk> to produce <signature>
  create an failing_noop operation then check if the signature provided
  correspond the operation and public key. Use by default the Genesis
  block as context for the operation

src/proto_alpha/lib_client/operation_result.ml
@@ -481,6 +481,8 @@ let rec pp_contents_and_result_list :
         proposal
         Data_encoding.Json.pp
         (Data_encoding.Json.construct Vote.ballot_encoding ballot)
+  | Single_and_result (Failing_noop arbitrary, Failing_noop_result) ->
+      Format.fprintf ppf "@[<v 2>Failing_noop arbitrary: %s@]" arbitrary
   | Single_and_result
       ((Manager_operation _ as op), (Manager_operation_result _ as res)) ->
       Format.fprintf ppf "%a" pp_manager_operation_contents_and_result (op, res)

tezos/tezos-mirror

src/proto_alpha/lib_client/operation_result.ml
@@ -505,6 +505,9 @@ let rec pp_contents_and_result_list :
         proposal
         Data_encoding.Json.pp
         (Data_encoding.Json.construct Vote.ballot_encoding ballot)
+  | Single_and_result (Failing_noop _arbitrary, _) ->
+      (* the Failing_noop operation always fails and can't have result *)
+      .
   | Single_and_result
       ((Manager_operation _ as op), (Manager_operation_result _ as res)) ->
       Format.fprintf ppf "%a" pp_manager_operation_contents_and_result (op, res)