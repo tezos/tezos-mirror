7b0d03b1c17d6fc31f7d59dee2aefc21eacf2224
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2021-02-10 14:33:32 +0100

    Scripts: reflect recent changes in snapshot_alpha

--- scripts/snapshot_alpha.sh
+++ scripts/snapshot_alpha.sh
@@ -107,6 +107,7 @@ sed -i.old -e 's/"hash": "[^"]*",/"hash": "'$long_hash'",/' \
 
 sed -i.old -e s/protocol_alpha/protocol_${version}_${short_hash}/ \
            -e s/protocol-alpha/protocol-${version}-${short_hash}/ \
+           -e s/protocol-functor-alpha/protocol-functor-${version}-${short_hash}/ \
     $(find . -type f)
 
 sed -i.old -e s/-alpha/-${version}-${short_hash}/ \
