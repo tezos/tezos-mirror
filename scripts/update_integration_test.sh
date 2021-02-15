723037a38a1ef5cc119f5a4c88c5bba5a9b23c2e
Raphaël Proust <code@bnwr.net>
2021-02-15 18:20:36 +0100

    CI: filter python tests by marks rather than names

--- scripts/update_integration_test.sh
+++ scripts/update_integration_test.sh
@@ -21,7 +21,7 @@ for PROTO_DIR in $(find tests_python/ -maxdepth 1 -mindepth 1 -iname 'tests_*' |
 integration:${PROTO_DIR_BASE}_fast:
   extends: .integration_python_template
   script:
-    - poetry run pytest ${PROTO_DIR##tests_python/} -k "not slow" -s --log-dir=tmp 2&>1 | tee tmp/${PROTO_DIR_BASE}_fast.out | tail
+    - poetry run pytest ${PROTO_DIR##tests_python/} -m "not slow" -s --log-dir=tmp 2&>1 | tee tmp/${PROTO_DIR_BASE}_fast.out | tail
   stage: test
 
 EOF
