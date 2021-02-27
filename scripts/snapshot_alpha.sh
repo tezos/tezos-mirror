316bd43cef9f6cbd1c182b78078fac70f7d6dc3f
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2021-02-27 14:46:30 +0100

    Scripts: snaphot_alpha registers the protocol as unmutable

--- scripts/snapshot_alpha.sh
+++ scripts/snapshot_alpha.sh
@@ -125,6 +125,9 @@ do
     mv "$file" $(echo "$file" | sed s/alpha/${version}-${short_hash}/g)
 done
 
+# add this protocol to the immutable list
+printf \\n$long_hash >> ../../lib_protocol_compiler/final_protocol_versions
+
 dune exec ../../lib_protocol_compiler/replace.exe ../../lib_protocol_compiler/dune_protocol.template dune.inc ../../lib_protocol_compiler/final_protocol_versions ${version}_${short_hash}
 
 cd ../../..
