d137c9d6b319dda51b8f86b429a9aa3b00e7d2bb
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2021-03-03 17:38:56 +0100

    Florence: (No Baking Accounts) Add vanity nonce

--- src/proto_alpha/lib_protocol/main.ml
+++ src/proto_alpha/lib_protocol/main.ml
@@ -414,3 +414,5 @@ let init ctxt block_header =
   in
   Alpha_context.prepare_first_block ~typecheck ~level ~timestamp ~fitness ctxt
   >|=? fun ctxt -> Alpha_context.finalize ctxt
+
+(* Vanity nonce: 0000006957234545 *)
