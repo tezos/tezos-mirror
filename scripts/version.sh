a304d037ea0d5b85706b513f4bba49f3438586a2
Thomas Letan <lthms@nomadic-labs.com>
2021-03-05 15:56:26 +0100

    OCaml: Bump to 4.10.2

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,17 +3,17 @@
 ## `ocaml-version` should be in sync with `README.rst` and
 ## `lib.protocol-compiler/tezos-protocol-compiler.opam`
 
-ocaml_version=4.09.1
+ocaml_version=4.10.2
 opam_version=2.0
 recommended_rust_version=1.44.0
 
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=7a8c3abfbac416d1e4cd10827642a970da6b0b6f
+full_opam_repository_tag=b3ff02afce0526d4cec91b32bd7b59c1550440ea
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=552c0009110429e5adfd093122ce4eaf6c8b10c2
+opam_repository_tag=ddb7953de1170c81dbadeef0875626d2651571ea
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
