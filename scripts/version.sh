4def37e33c3b2bac2b806adf00b21cdb5c641a99
Julien Debon <julien.debon@tweag.io>
2021-03-25 17:32:05 +0100

    CI: Use dependencies qcheck-core and qcheck-alcotest

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,11 +9,11 @@ recommended_rust_version=1.44.0
 
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=eb6f20daffff6dc5c67b4b55b87bf6b0e600c4d7
+full_opam_repository_tag=b1124400e2d4f93a8becf1315a8e8d205fe0082e
 
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml
-opam_repository_tag=ecb12b00769179e6022ad1547b69950781a45061
+opam_repository_tag=17ecd6f7b4585fdc38b28542523abd9b9b53fde0
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
