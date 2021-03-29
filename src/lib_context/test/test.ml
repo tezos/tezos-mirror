zshipko/tezos

Storage: split context and store into separate libs

src/lib_context/test/test.ml
@@ -23,31 +23,6 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-open Tezos_protocol_environment
-open Context
-
-let ( >>= ) = Lwt.( >>= )
-
-type _ Context.kind += Shell : Tezos_context.Context.t Context.kind
-
-let ops = (module Tezos_context.Context : CONTEXT with type t = 'ctxt)
-
-let checkout index context_hash =
-  Tezos_context.Context.checkout index context_hash
-  >>= function
-  | Some ctxt ->
-      Lwt.return_some (Context.Context {ops; ctxt; kind = Shell})
-  | None ->
-      Lwt.return_none
-
-let checkout_exn index context_hash =
-  Tezos_context.Context.checkout_exn index context_hash
-  >>= fun ctxt -> Lwt.return (Context.Context {ops; ctxt; kind = Shell})
-
-let wrap_disk_context ctxt = Context.Context {ops; ctxt; kind = Shell}
-
-let unwrap_disk_context : t -> Tezos_context.Context.t = function
-  | Context.Context {ctxt; kind = Shell; _} ->
-      ctxt
-  | _ ->
-      assert false
+let () =
+  Lwt_main.run
+    (Alcotest_lwt.run "tezos-storage" [("context", Test_context.tests)])

src/lib_protocol_environment/shell_context.mli
@@ -26,14 +26,14 @@
 open Tezos_protocol_environment
 open Tezos_crypto
 
-type _ Context.kind += Shell : Tezos_storage.Context.t Context.kind
+type _ Context.kind += Shell : Tezos_context.Context.t Context.kind
 
 val checkout :
-  Tezos_storage.Context.index -> Context_hash.t -> Context.t option Lwt.t
+  Tezos_context.Context.index -> Context_hash.t -> Context.t option Lwt.t
 
 val checkout_exn :
-  Tezos_storage.Context.index -> Context_hash.t -> Context.t Lwt.t
+  Tezos_context.Context.index -> Context_hash.t -> Context.t Lwt.t
 
-val wrap_disk_context : Tezos_storage.Context.t -> Context.t
+val wrap_disk_context : Tezos_context.Context.t -> Context.t
 
-val unwrap_disk_context : Context.t -> Tezos_storage.Context.t
+val unwrap_disk_context : Context.t -> Tezos_context.Context.t

src/lib_shell/patch_context.ml
@@ -29,7 +29,7 @@ let patch_context (genesis : Genesis.t) key_json ctxt =
   | None ->
       Lwt.return ctxt
   | Some (key, json) ->
-      Tezos_storage.Context.set
+      Tezos_context.Context.set
         ctxt
         [key]
         (Data_encoding.Binary.to_bytes_exn Data_encoding.json json) )

tezos/tezos-mirror

src/lib_context/test/test.ml
@@ -1,7 +1,7 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2018-2021 Tarides <contact@tarides.com>                     *)
+(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -23,14 +23,6 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** Implementation of Tezos context fully in memory. *)
-
-include Tezos_storage_sigs.Context.S
-
-val empty : t
-
-val encoding : t Data_encoding.t
-
-val get_protocol : t -> Protocol_hash.t Lwt.t
-
-val add_protocol : t -> Protocol_hash.t -> t Lwt.t
+let () =
+  Lwt_main.run
+    (Alcotest_lwt.run "tezos-context" [("context", Test_context.tests)])

src/lib_protocol_environment/shell_context.mli
@@ -26,14 +26,14 @@
 open Tezos_protocol_environment
 open Tezos_crypto
 
-type _ Context.kind += Context : Tezos_storage.Context.t Context.kind
+type _ Context.kind += Context : Tezos_context.Context.t Context.kind
 
 val checkout :
-  Tezos_storage.Context.index -> Context_hash.t -> Context.t option Lwt.t
+  Tezos_context.Context.index -> Context_hash.t -> Context.t option Lwt.t
 
 val checkout_exn :
-  Tezos_storage.Context.index -> Context_hash.t -> Context.t Lwt.t
+  Tezos_context.Context.index -> Context_hash.t -> Context.t Lwt.t
 
-val wrap_disk_context : Tezos_storage.Context.t -> Context.t
+val wrap_disk_context : Tezos_context.Context.t -> Context.t
 
-val unwrap_disk_context : Context.t -> Tezos_storage.Context.t
+val unwrap_disk_context : Context.t -> Tezos_context.Context.t

src/lib_shell/patch_context.ml
@@ -29,7 +29,7 @@ let patch_context (genesis : Genesis.t) key_json ctxt =
   | None ->
       Lwt.return ctxt
   | Some (key, json) ->
-      Tezos_storage.Context.add
+      Tezos_context.Context.add
         ctxt
         [key]
         (Data_encoding.Binary.to_bytes_exn Data_encoding.json json) )