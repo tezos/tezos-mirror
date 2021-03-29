6e03f0a78b542ac273556e3e9ec2178f68abfd3c
vbot <vincent.botbol@nomadic-labs.com>
2021-03-29 16:33:24 +0200

    CI: update dependencies

--- scripts/version.sh
+++ scripts/version.sh
@@ -13,7 +13,7 @@ full_opam_repository_tag=b1124400e2d4f93a8becf1315a8e8d205fe0082e
 
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml
-opam_repository_tag=17ecd6f7b4585fdc38b28542523abd9b9b53fde0
+opam_repository_tag=b2f954608b8e0655b2b52fd9d527614396e02402
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
