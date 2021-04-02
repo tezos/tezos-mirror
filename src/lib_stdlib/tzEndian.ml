7d13fba96e2a04dee3b55d7fb60e099cec009635
Raphaël Proust <code@bnwr.net>
2021-04-02 16:46:16 +0200

    Proto env-v3: Add string reading functions for TzEndian

--- src/lib_stdlib/tzEndian.ml
+++ src/lib_stdlib/tzEndian.ml
@@ -1,7 +1,7 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2019 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(* Copyright (c) 2019-2021 Nomadic Labs, <contact@nomadic-labs.com>          *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -27,26 +27,42 @@ let set_int32 = Bytes.set_int32_be
 
 let get_int32 = Bytes.get_int32_be
 
+let get_int32_string s off = Bytes.get_int32_be (Bytes.unsafe_of_string s) off
+
 let set_int8 = Bytes.set_int8
 
 let get_int8 = Bytes.get_int8
+
+let get_int8_string s off = Bytes.get_int8 (Bytes.unsafe_of_string s) off
 
 let set_int16 = Bytes.set_int16_be
 
 let get_int16 = Bytes.get_int16_be
 
+let get_int16_string s off = Bytes.get_int16_be (Bytes.unsafe_of_string s) off
+
 let set_int64 = Bytes.set_int64_be
 
 let get_int64 = Bytes.get_int64_be
+
+let get_int64_string s off = Bytes.get_int64_be (Bytes.unsafe_of_string s) off
 
 let set_uint8 = Bytes.set_uint8
 
 let get_uint8 = Bytes.get_uint8
 
+let get_uint8_string s off = Bytes.get_uint8 (Bytes.unsafe_of_string s) off
+
 let set_uint16 = Bytes.set_uint16_be
 
 let get_uint16 = Bytes.get_uint16_be
 
+let get_uint16_string s off =
+  Bytes.get_uint16_be (Bytes.unsafe_of_string s) off
+
 let get_double buff i = Int64.float_of_bits (Bytes.get_int64_be buff i)
+
+let get_double_string buff i =
+  Int64.float_of_bits (Bytes.get_int64_be (Bytes.unsafe_of_string buff) i)
 
 let set_double buff i v = Bytes.set_int64_be buff i (Int64.bits_of_float v)
