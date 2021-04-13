6b0eb8828ff78f9f1f2ecd3678711380f053437a
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2021-04-13 16:36:01 +0200

    Tests/CI: split unit:alltest: by proto/non-proto

--- scripts/update_unit_test.sh
+++ scripts/update_unit_test.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
-# This script is for automatically updating the tests in `.gitlab-ci.yml`. This
+# Usage: ./scripts/update_unit_tests.sh
+
+# This script is for automatically updating the tests in `.gitlab/ci/unittest.yml`. This
 # script specifically updates the unit tests, the script
 # `update_integration_test.sh` is for similarly updating the integration
 # tests, and the script `update_opam_test.sh` is for upgrading the packaging
@@ -8,7 +10,19 @@
 # this one stands for the documentation of the other. Note that step 2 varies
 # from script to script, but the logic and the intent is the same.
 
+# In this particular script, unit tests are broken up in a set of
+# jobs: one per protocol containing tests and one containing all
+# remaining jobs.
+
 set -e
+
+# Removes any lines in input that does not correspond to a file
+# tracked by git
+function filter_git_tracked() {
+    while read -r file; do
+        git ls-files --error-unmatch "$file" > /dev/null 2>&1 && echo "$file"
+    done
+}
 
 script_dir="$(cd "$(dirname "$0")" && echo "$(pwd -P)/")"
 src_dir="$(dirname "$script_dir")"
@@ -21,27 +35,51 @@ csplit --quiet --prefix="$tmp" "$src_dir/.gitlab/ci/unittest.yml" /##BEGIN_UNITT
 mv "$tmp"00 "$tmp"
 rm "$tmp"0*
 
+# 2a. Find each proto test add the matching incantation to a temporary
+# file.
+for proto in $(find src/ -mindepth 1 -maxdepth 1 -iname proto_\* -type d | LC_COLLATE=C sort); do
+    name_job=${proto##src/proto_}
+
+    tested_libs=$(find "$proto" -name test -type d 2>/dev/null | filter_git_tracked | LC_COLLATE=C sort)
+    if [ -n "$tested_libs" ]; then
+        cat >> "$tmp" <<EOF
+unit:${name_job}:
+  extends: .unit_test_template
+  script:
+EOF
+        for lib in $tested_libs ; do
+            nametest=${lib%%/test}
+            name=$nametest
+            name=${name##src/proto_}
+            name=${name//\//_}
+            cat >> "$tmp" <<EOF
+    - scripts/test_wrapper.sh $nametest $name
+EOF
+        done
+        echo "" >> "$tmp"
+  fi
+done
+
+# add a job for remaining tests
 cat >> "$tmp" <<EOF
-unit:alltest:
+unit:non-proto:
   extends: .unit_test_template
   script:
 EOF
 
-# 2: Find each test folder and add the matching incantation to the temporary
-# file.
-for lib in $(find src/ vendors/ -name test -type d | LC_COLLATE=C sort) ; do
-  if git ls-files --error-unmatch "$lib"  > /dev/null 2>&1; then
+# 2b: Find each non-proto test folder and add the matching incantation to
+# the temporary file.
+for lib in $(find src/ vendors/ -path src/proto_\* -prune -o -name test -type d -print | filter_git_tracked | LC_COLLATE=C sort) ;
+do
     nametest=${lib%%/test}
     name=$nametest
     name=${name##src/bin_}
     name=${name##src/lib_}
-    name=${name##src/proto_}
     name=${name##vendors/}
     name=${name//\//_}
     cat >> "$tmp" <<EOF
     - scripts/test_wrapper.sh $nametest $name
 EOF
-  fi
 done
 
 
