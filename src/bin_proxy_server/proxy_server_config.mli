8714a4e4b14ca0b7dd42641a2fa34577521a1df9
Clément Hurlin <clement.hurlin@tweag.io>
2021-04-15 11:38:20 +0200

    tezos-proxy-server: MVP 🥁

---
+++ src/bin_proxy_server/proxy_server_config.mli
@@ -0,0 +1,102 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021 Nomadic Labs. <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** This type corresponds to the content of the [CONFIG] file, as
+    well as the aggregations of the corresponding command line arguments.
+    Once it is built and, it is transformed into a value of type
+    [runtime] which represents what the APIs actually needs. *)
+type t = private {
+  endpoint : Uri.t option;
+      (** The node to delegate RPCs to. Name was chosen
+          to be the same as in [tezos-client] *)
+  rpc_addr : Uri.t option;
+      (** The address that the proxy server serves. Name
+          was chosen to be the same as in [tezos-node run] *)
+  rpc_tls : string option;
+      (** A string of the form ["crt,key"] where [crt] is the path
+          to the TLS certificate to use and [key] is the path to the key
+          to use. Name was chosen to be the same as in [tezos-node run] *)
+  sym_block_caching_time : int option;
+      (** The time during which data for a symbolic block identifier
+          (like [head], [head~1]) is kept. Smaller values increase [endpoint]'s
+          load but yield more up-to-data to clients. Higher values
+          decrease [endpoint]'s load but make clients observe slightly deprecated
+          values. If omitted, defaulted to [time_between_blocks].
+
+          If you are not sure what to do, do not specify it; i.e.
+          do not pass [--sym-block-caching-time]. If your node's load
+          is too high, pass a bigger value than [time_between_blocks] (if
+          your clients are fine seeing slightly deprecated values) *)
+}
+
+(** Pretty printer for [t] *)
+val pp : Format.formatter -> t -> unit
+
+(** A value of type [t] after having been appropriately validated and
+    prepared for being passed to the various APIs that need them
+    (hence some types are slightly different than in [t]). *)
+type runtime = private {
+  endpoint : Uri.t;  (** The node to delegate RPCs to. *)
+  rpc_server_address : P2p_addr.t;  (** The address of the server *)
+  rpc_server_port : int;  (** The port of the server *)
+  rpc_server_tls : (string * string) option;
+      (** The paths to the certificate and key to use for TLS *)
+  sym_block_caching_time : int option;
+      (** The duration during which data of symbolic blocks is kept *)
+}
+
+(** [make endpoint rpc_addr rpc_tls sym_block_caching_time]
+    creates an instance of [t] from the flags that are available
+    on the command line. See [t] for the documentation of parameters. *)
+val make :
+  endpoint:Uri.t option ->
+  rpc_addr:Uri.t option ->
+  rpc_tls:string option ->
+  sym_block_caching_time:int option ->
+  t
+
+(** Valid config file: if passed to [destruct_config], [Valid] is returned. *)
+val example_config : string
+
+(** Encoding of what may happen when deserializing a JSON value
+    whose OCaml type is ['a] *)
+type 'a destructed =
+  | Valid of 'a  (** File could be parsed and validated *)
+  | Invalid of string
+      (** File could be parsed but not validated. Message is detailed. *)
+  | CannotDeserialize  (** Deserializing the JSON failed. *)
+
+(** [destruct_config json] returns a [t] from raw [json].
+    Returns [Ok] in case of success, or [Error msg] if validation fails. *)
+val destruct_config : Data_encoding.json -> t destructed
+
+(** [merge_right_bias t1 t2] returns the union of data of [t1] and [t2]
+    (taking the [Some] case on fields of type [option]). If data is
+    present on both side, take data from [t2] *)
+val union_right_bias : t -> t -> t
+
+(** [to_runtime t] returns a [runtime] from a [t].
+    Returns [Ok] in case of success, or [Error msg] if validation fails. *)
+val to_runtime : t -> (runtime, string) result
---
+++ src/bin_proxy_server/proxy_server_main_run.ml
@@ -0,0 +1,106 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+module Events = Proxy_server_run_events
+
+type error += Proxy_server_RPC_Port_already_in_use of P2p_point.Id.t list
+
+type args = {
+  address : P2p_addr.t;
+  port : int;
+  tls_cert_and_key : (string * string) option;
+}
+
+let () =
+  register_error_kind
+    `Permanent
+    ~id:"main.run.port_already_in_use"
+    ~title:"Cannot start proxy server: RPC port already in use"
+    ~description:"Another process is running on the same RPC port."
+    ~pp:(fun ppf addrlist ->
+      Format.fprintf
+        ppf
+        "Another process is probably running on one of these addresses (%a). \
+         Please choose another RPC port."
+        (Format.pp_print_list P2p_point.Id.pp)
+        addrlist)
+    Data_encoding.(obj1 (req "addrlist" (list P2p_point.Id.encoding)))
+    (function
+      | Proxy_server_RPC_Port_already_in_use addrlist ->
+          Some addrlist
+      | _ ->
+          None)
+    (fun addrlist -> Proxy_server_RPC_Port_already_in_use addrlist)
+
+let launch_rpc_server dir {address; port; tls_cert_and_key} =
+  let host = Ipaddr.V6.to_string address in
+  let mode =
+    match tls_cert_and_key with
+    | None ->
+        `TCP (`Port port)
+    | Some (cert, key) ->
+        `TLS (`Crt_file_path cert, `Key_file_path key, `No_password, `Port port)
+  in
+  Lwt.catch
+    (fun () ->
+      Tezos_rpc_http_server.RPC_server.launch
+        ~host
+        mode
+        dir
+        ~media_types:Tezos_rpc_http.Media_type.all_media_types
+      >>= return)
+    (function
+      | Unix.Unix_error (Unix.EADDRINUSE, "bind", "") ->
+          fail (Proxy_server_RPC_Port_already_in_use [(address, port)])
+      | exn ->
+          Lwt.return (error_exn exn))
+
+let run dir ({address; port; _} as args) =
+  let log_cfg = Lwt_log_sink_unix.create_cfg () in
+  Internal_event_unix.init ~lwt_log_sink:log_cfg ()
+  >>= fun () ->
+  let node_downer =
+    Lwt_exit.register_clean_up_callback ~loc:__LOC__ (fun _ ->
+        Events.(emit shutting_down_proxy_server) ())
+  in
+  launch_rpc_server dir args
+  >>=? fun rpc ->
+  let rpc_downer =
+    Lwt_exit.register_clean_up_callback
+      ~loc:__LOC__
+      ~after:[node_downer]
+      (fun _ ->
+        Events.(emit shutting_down_rpc_server) ()
+        >>= fun () -> Tezos_rpc_http_server.RPC_server.shutdown rpc)
+  in
+  Events.(emit starting_rpc_server) (P2p_addr.to_string address, port)
+  >>= fun () ->
+  let _ =
+    Lwt_exit.register_clean_up_callback
+      ~loc:__LOC__
+      ~after:[rpc_downer]
+      (fun _exit_status -> Internal_event_unix.close ())
+  in
+  Lwt_utils.never_ending ()
