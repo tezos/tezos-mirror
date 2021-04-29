0588ce5e57ef16d4e41e597a00d10a7e0db9205d
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2021-04-29 22:25:01 +0200

    Build: Use irmin.2.5.4 and index.1.3.1

--- scripts/version.sh
+++ scripts/version.sh
@@ -10,10 +10,10 @@ recommended_rust_version=1.44.0
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=521b2b782c6e74f8e02e08b3bb4d7aef68428651
+full_opam_repository_tag=3b10ec4f01b65ac56ea15ae94b050d056ba3fbef
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=50872d9c9175f2d1b189790cd8ff584b637c5fa8
+opam_repository_tag=bbe9dcd4d8d867e5b7ba958c9d0823df833339c8
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
