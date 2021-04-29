e0c0059ad02ef8739dbd433cd6f6615ec77e89c4
Romain Bardou <romain@nomadic-labs.com>
2021-04-29 10:04:54 +0200

    Tezt: add Base.show_rex

--- tezt/lib/base.mli
+++ tezt/lib/base.mli
@@ -1,7 +1,7 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2020 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2020-2021 Nomadic Labs <contact@nomadic-labs.com>           *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -71,6 +71,9 @@ type rex
 
 (** Compile a regular expression using Perl syntax. *)
 val rex : string -> rex
+
+(** Convert a regular expression to a string using Perl syntax. *)
+val show_rex : rex -> string
 
 (** Test whether a string matches a regular expression.
 
