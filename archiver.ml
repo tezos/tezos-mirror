364f5387f9dda0502c0c6fe89cfbdf9224b5d4c9
Pierre Boutillier <pierre.boutillier@laposte.net>
2021-05-11 00:10:16 +0200

    Teztale: Edo version of an endorsement tracker

---
+++ archiver.ml
@@ -0,0 +1,384 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2020 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+let levels_per_folder = 4096l
+
+module Endorsement = struct
+  type t =
+    { delegate : Signature.public_key_hash;
+      reception_time : Time.System.t option;
+      errors : error list;
+      block_inclusion : Block_hash.t list
+    }
+
+  let encoding =
+    let open Data_encoding in
+    conv
+      (fun { delegate; reception_time; errors; block_inclusion } ->
+        (delegate, reception_time, errors, block_inclusion))
+      (fun (delegate, reception_time, errors, block_inclusion) ->
+        { delegate; reception_time; errors; block_inclusion })
+      (obj4
+         (req "delegate" Signature.Public_key_hash.encoding)
+         (opt "reception_time" Time.System.encoding)
+         (dft "errors" (list error_encoding) [])
+         (dft "included_in_blocks" (list Block_hash.encoding) []))
+end
+
+module Block = struct
+  type t =
+    { hash : Block_hash.t; reception_time : Time.System.t; nonce : unit option }
+
+  let encoding =
+    let open Data_encoding in
+    conv
+      (fun { hash; reception_time; nonce } -> (hash, reception_time, nonce))
+      (fun (hash, reception_time, nonce) -> { hash; reception_time; nonce })
+      (obj3
+         (req "hash" Block_hash.encoding)
+         (req "reception_time" Time.System.encoding)
+         (opt "nonce" unit))
+end
+
+module Anomaly = struct
+  type problem = Missed | Forgotten | Sequestered | Incorrect
+
+  type t =
+    { level : Int32.t;
+      delegate : Signature.Public_key_hash.t;
+      problem : problem
+    }
+
+  let problem_encoding =
+    Data_encoding.string_enum
+      [ ("missed", Missed);
+        ("forgotten", Forgotten);
+        ("sequestered", Sequestered);
+        ("incorrect", Incorrect) ]
+
+  let encoding =
+    let open Data_encoding in
+    conv
+      (fun { level; delegate; problem } -> (level, delegate, problem))
+      (fun (level, delegate, problem) -> { level; delegate; problem })
+      (obj3
+         (req "level" int32)
+         (req "delegate" Signature.Public_key_hash.encoding)
+         (req "problem" problem_encoding))
+
+  let rec insert_in_ordered_list ({ level; delegate; _ } as anomaly) = function
+    | [] -> [anomaly]
+    | head :: tail as l ->
+        if Compare.Int32.(head.level < level) then anomaly :: l
+        else if
+          Int32.equal head.level level
+          && Signature.Public_key_hash.equal head.delegate delegate
+        then if head.problem = Missed then anomaly :: tail else l
+        else head :: insert_in_ordered_list anomaly tail
+end
+
+type t =
+  { blocks : Block.t list;
+    endorsements : Endorsement.t list;
+    unaccurate : bool
+  }
+
+let encoding =
+  let open Data_encoding in
+  conv
+    (fun { blocks; endorsements; unaccurate } ->
+      (blocks, endorsements, unaccurate))
+    (fun (blocks, endorsements, unaccurate) ->
+      { blocks; endorsements; unaccurate })
+    (obj3
+       (dft "blocks" (list Block.encoding) [])
+       (dft "endorsements" (list Endorsement.encoding) [])
+       (dft "unaccurate" bool false))
+
+let empty = { blocks = []; endorsements = []; unaccurate = true }
+
+let dirname_of_level prefix level =
+  let base = Int32.mul (Int32.div level levels_per_folder) levels_per_folder in
+  Filename.concat
+    prefix
+    (Printf.sprintf
+       "%li-%li"
+       base
+       (Int32.add base (Int32.pred levels_per_folder)))
+
+let filename_of_level prefix level =
+  Filename.concat
+    (dirname_of_level prefix level)
+    (Int32.to_string level ^ ".json")
+
+let load filename encoding empty =
+  Lwt_unix.file_exists filename >>= function
+  | false -> return empty
+  | true ->
+      Lwt_utils_unix.Json.read_file filename
+      >|=? Data_encoding.Json.destruct encoding
+
+let write filename encoding value =
+  Lwt_utils_unix.create_dir (Filename.dirname filename) >>= fun () ->
+  Lwt_utils_unix.Json.write_file
+    filename
+    (Data_encoding.Json.construct encoding value)
+
+let files_in_use = ref []
+
+let get_file_mutex filename =
+  match List.assoc_opt filename !files_in_use with
+  | None ->
+      let x = Lwt_mutex.create () in
+      x
+  | Some x -> x
+
+let drop_file_mutex filename =
+  files_in_use := List.remove_assoc filename !files_in_use
+
+let dump_anomalies path level anomalies =
+  let filename =
+    Filename.concat (dirname_of_level path level) "anomalies.json"
+  in
+  let mutex = get_file_mutex filename in
+  Lwt_mutex.with_lock mutex (fun () ->
+      let () = files_in_use := (filename, mutex) :: !files_in_use in
+      load filename (Data_encoding.list Anomaly.encoding) [] >>=? fun known ->
+      let out =
+        write
+          filename
+          (Data_encoding.list Anomaly.encoding)
+          (List.fold_left
+             (fun x y -> Anomaly.insert_in_ordered_list y x)
+             known
+             anomalies)
+      in
+      let () = drop_file_mutex filename in
+      out)
+
+let extract_anomalies path level infos =
+  if infos.unaccurate then return_unit
+  else
+    let anomalies =
+      List.fold_left
+        (fun acc
+             Endorsement.{ delegate; reception_time; errors; block_inclusion } ->
+          match errors with
+          | _ :: _ ->
+              Anomaly.{ level; delegate; problem = Anomaly.Incorrect } :: acc
+          | [] -> (
+              match (reception_time, block_inclusion) with
+              | (None, []) ->
+                  Anomaly.{ level; delegate; problem = Anomaly.Missed } :: acc
+              | (Some _, []) ->
+                  Anomaly.{ level; delegate; problem = Anomaly.Forgotten }
+                  :: acc
+              | (None, _ :: _) ->
+                  Anomaly.{ level; delegate; problem = Anomaly.Sequestered }
+                  :: acc
+              | (Some _, _ :: _) -> acc ))
+        []
+        infos.endorsements
+    in
+    match anomalies with
+    | [] -> return_unit
+    | _ :: _ -> dump_anomalies path level anomalies
+
+let dump_included_in_block path block_level block_hash reception_time
+    endorsers_pkhs =
+  let open Lwt.Infix in
+  (let endorsements_level = Int32.pred block_level in
+   let filename = filename_of_level path endorsements_level in
+   let mutex = get_file_mutex filename in
+   Lwt_mutex.with_lock mutex (fun () ->
+       let () = files_in_use := (filename, mutex) :: !files_in_use in
+       load filename encoding empty >>=? fun infos ->
+       let (updated_known, unknown) =
+         List.fold_left
+           (fun (acc, missing)
+                Endorsement.(
+                  { delegate; reception_time; errors; block_inclusion } as en) ->
+             match
+               List.partition
+                 (fun pkh -> Signature.Public_key_hash.equal pkh delegate)
+                 missing
+             with
+             | (_ :: _, missing') ->
+                 ( Endorsement.
+                     { delegate;
+                       reception_time;
+                       errors;
+                       block_inclusion = block_hash :: block_inclusion
+                     }
+                   :: acc,
+                   missing' )
+             | ([], _) -> (en :: acc, missing))
+           ([], endorsers_pkhs)
+           infos.endorsements
+       in
+       let endorsements =
+         List.fold_left
+           (fun acc delegate ->
+             Endorsement.
+               { delegate;
+                 reception_time = None;
+                 errors = [];
+                 block_inclusion = [block_hash]
+               }
+             :: acc)
+           updated_known
+           unknown
+       in
+       let out_infos =
+         { blocks = infos.blocks; endorsements; unaccurate = infos.unaccurate }
+       in
+       let out = write filename encoding out_infos in
+       let () = drop_file_mutex filename in
+       out >>=? fun () -> extract_anomalies path endorsements_level out_infos)
+   >>= function
+   | Ok () -> Lwt.return_unit
+   | Error err ->
+       Lwt_io.printl
+         (Format.asprintf
+            "Failed to dump endorsements in block %a at level %li : %a"
+            Block_hash.pp
+            block_hash
+            block_level
+            (Format.pp_print_list Error_monad.pp)
+            err))
+  <&>
+  let filename = filename_of_level path block_level in
+  let mutex = get_file_mutex filename in
+  Lwt_mutex.with_lock mutex (fun () ->
+      let () = files_in_use := (filename, mutex) :: !files_in_use in
+      load filename encoding empty >>=? fun infos ->
+      let blocks =
+        Block.{ hash = block_hash; reception_time; nonce = None }
+        :: infos.blocks
+      in
+      let out =
+        write
+          filename
+          encoding
+          { blocks;
+            endorsements = infos.endorsements;
+            unaccurate = infos.unaccurate
+          }
+      in
+      let () = drop_file_mutex filename in
+      out)
+  >>= function
+  | Ok () -> Lwt.return_unit
+  | Error err ->
+      Lwt_io.printl
+        (Format.asprintf
+           "Failed to dump block %a at level %li : %a"
+           Block_hash.pp
+           block_hash
+           block_level
+           (Format.pp_print_list Error_monad.pp)
+           err)
+
+let dump_received path ?unaccurate level items =
+  let filename = filename_of_level path level in
+  let mutex = get_file_mutex filename in
+  Lwt_mutex.with_lock mutex (fun () ->
+      let () = files_in_use := (filename, mutex) :: !files_in_use in
+      load filename encoding empty >>=? fun infos ->
+      let (updated_known, unknown) =
+        List.fold_left
+          (fun (acc, missing)
+               Endorsement.(
+                 { delegate; reception_time; errors; block_inclusion } as en) ->
+            match
+              List.partition
+                (fun (pkh, _, _) ->
+                  Signature.Public_key_hash.equal pkh delegate)
+                missing
+            with
+            | ((_, err, time) :: _, missing') ->
+                let (reception_time, errors) =
+                  match reception_time with
+                  | Some _ -> (reception_time, errors)
+                  | None -> (time, err)
+                in
+                ( Endorsement.
+                    { delegate; reception_time; errors; block_inclusion }
+                  :: acc,
+                  missing' )
+            | ([], _) -> (en :: acc, missing))
+          ([], items)
+          infos.endorsements
+      in
+      let endorsements =
+        List.fold_left
+          (fun acc (delegate, errors, reception_time) ->
+            Endorsement.
+              { delegate; reception_time; errors; block_inclusion = [] }
+            :: acc)
+          updated_known
+          unknown
+      in
+      let unaccurate = Option.value ~default:infos.unaccurate unaccurate in
+      let out_infos = { blocks = infos.blocks; endorsements; unaccurate } in
+      let out = write filename encoding out_infos in
+      let () = drop_file_mutex filename in
+      out >>=? fun () -> extract_anomalies path level out_infos)
+  >>= function
+  | Ok () -> Lwt.return_unit
+  | Error err ->
+      Lwt_io.printl
+        (Format.asprintf
+           "Failed to dump endorsements at level %li : %a"
+           level
+           (Format.pp_print_list Error_monad.pp)
+           err)
+
+type chunk =
+  | Block of
+      Int32.t * Block_hash.t * Time.System.t * Signature.Public_key_hash.t list
+  | Mempool of
+      bool option
+      * Int32.t
+      * (Signature.Public_key_hash.t * error list * Time.System.t option) list
+
+let (chunk_stream, chunk_feeder) = Lwt_stream.create ()
+
+let launch prefix =
+  Lwt_stream.iter_p
+    (function
+      | Block (block_level, block, reception_time, pkhs) ->
+          dump_included_in_block prefix block_level block reception_time pkhs
+      | Mempool (unaccurate, level, items) ->
+          dump_received prefix ?unaccurate level items)
+    chunk_stream
+
+let stop () = chunk_feeder None
+
+let add_received ?unaccurate level items =
+  chunk_feeder (Some (Mempool (unaccurate, level, items)))
+
+let add_block block_level block_hash reception_time pkhs =
+  chunk_feeder (Some (Block (block_level, block_hash, reception_time, pkhs)))
---
+++ main.ml
@@ -0,0 +1,283 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2019 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+module Block_services = Protocol_client_context.Alpha_block_services
+open Tezos_protocol_008_PtEdo2Zk
+
+let endorsers_pk_at cctxt chain block =
+  Protocol.Delegate_services.Endorsing_rights.get cctxt (chain, block)
+  >>=? fun rights ->
+  List.map_ep
+    (fun Protocol.Delegate_services.Endorsing_rights.{ delegate; _ } ->
+      Protocol.Contract_services.manager_key cctxt (chain, block) delegate
+      >>=? fun x ->
+      Lwt.return (Option.value_e ~error:[Error_monad.Exn Not_found] x))
+    rights
+
+let dump_my_current_endorsements cctxt ?unaccurate ~full chain_id block level
+    errors ops =
+  endorsers_pk_at cctxt cctxt#chain (`Hash (block, 0)) >>=? fun pks ->
+  let (items, missing) =
+    List.fold_left
+      (fun (acc, pks) (delay, op) ->
+        match
+          List.partition
+            (fun delegate_pk ->
+              match
+                Protocol.Alpha_context.Operation.check_signature
+                  delegate_pk
+                  chain_id
+                  op
+              with
+              | Ok () -> true
+              | Error _ -> false)
+            pks
+        with
+        | (([] | _ :: _ :: _), _) -> assert false
+        | ([pk], pks') ->
+            ((Signature.Public_key.hash pk, errors, Some delay) :: acc, pks'))
+      ([], pks)
+      ops
+  in
+  let endorsements =
+    if full then
+      List.fold_left
+        (fun acc pk -> (Signature.Public_key.hash pk, [], None) :: acc)
+        items
+        missing
+    else items
+  in
+  let () = Archiver.add_received ?unaccurate level endorsements in
+  return_unit
+
+let rec check_level_in_endorsements ref_level = function
+  | Block_services.
+      { protocol_data =
+          Protocol.Alpha_context.Operation_data
+            { contents = Single (Endorsement { level }); _ };
+        _
+      }
+    :: tail ->
+      Int32.equal ref_level (Protocol.Alpha_context.Raw_level.to_int32 level)
+      && check_level_in_endorsements ref_level tail
+  | _ :: tail -> check_level_in_endorsements ref_level tail
+  | [] -> true
+
+let extract_endorsement infos
+    (operation_content : Protocol.Alpha_context.packed_operation) =
+  match operation_content with
+  | { Protocol.Main.protocol_data =
+        Protocol.Alpha_context.Operation_data
+          ( { contents = Single (Endorsement { level }); signature = _ } as
+          protocol_data );
+      shell = { branch } as shell
+    } ->
+      let () =
+        assert (
+          match infos with
+          | None -> true
+          | Some (b', l') ->
+              Block_hash.equal branch b'
+              && Protocol.Alpha_context.Raw_level.equal level l' )
+      in
+      ( Some (branch, level),
+        Some { Protocol.Alpha_context.Operation.shell; protocol_data } )
+  | _ -> (infos, None)
+
+let rec valid_endorsements_loop cctxt unaccurate chain_id =
+  Block_services.Mempool.monitor_operations
+    cctxt
+    ~chain:cctxt#chain
+    ~applied:true
+    ~refused:false
+    ~branch_delayed:false
+    ~branch_refused:false
+    ()
+  >>=? fun (ops_stream, _stopper) ->
+  let op_stream = Lwt_stream.flatten ops_stream in
+  Lwt_stream.fold
+    (fun op (infos, l) ->
+      let delay = Systime_os.now () in
+      let (vv, news) = extract_endorsement infos op in
+      (vv, Option.fold ~some:(fun x -> (delay, x) :: l) ~none:l news))
+    op_stream
+    (None, [])
+  >>= fun out ->
+  let next = valid_endorsements_loop cctxt false chain_id in
+  match out with
+  | (None, _) -> next
+  | (Some (block, level), timed_ops) ->
+      let level = Protocol.Alpha_context.Raw_level.to_int32 level in
+      dump_my_current_endorsements
+        cctxt
+        ~unaccurate
+        ~full:true
+        chain_id
+        block
+        level
+        []
+        timed_ops
+      >>=? fun () -> next
+
+let errors_of_operation cctxt op =
+  Block_services.Mempool.pending_operations cctxt ~chain:cctxt#chain ()
+  >|=? fun { refused; branch_refused; branch_delayed; _ } ->
+  let op_hash = Protocol.Alpha_context.Operation.hash_packed op in
+  match Operation_hash.Map.find op_hash branch_delayed with
+  | Some (_, err) -> err
+  | None -> (
+      match Operation_hash.Map.find op_hash branch_refused with
+      | Some (_, err) -> err
+      | None -> (
+          match Operation_hash.Map.find op_hash refused with
+          | Some (_, err) -> err
+          | None -> [] ) )
+
+let rec invalid_endorsements_loop cctxt chain_id =
+  Block_services.Mempool.monitor_operations
+    cctxt
+    ~chain:cctxt#chain
+    ~applied:false
+    ~refused:true
+    ~branch_delayed:true
+    ~branch_refused:true
+    ()
+  >>=? fun (ops_stream, _stopper) ->
+  let op_stream = Lwt_stream.flatten ops_stream in
+  Lwt_stream.fold_s
+    (fun op acc ->
+      let delay = Systime_os.now () in
+      match extract_endorsement None op with
+      | (_, None) -> Lwt.return acc
+      | (None, Some _) -> assert false
+      | (Some (block, level), Some news) ->
+          errors_of_operation cctxt op >>=? fun errors ->
+          Lwt.return
+            (acc >|? fun l -> (block, level, errors, (delay, news)) :: l))
+    op_stream
+    (ok [])
+  >>=? fun out ->
+  let next = invalid_endorsements_loop cctxt chain_id in
+  List.iter_ep
+    (fun (block, level, errors, endorsement) ->
+      let level = Protocol.Alpha_context.Raw_level.to_int32 level in
+      dump_my_current_endorsements
+        cctxt
+        ~full:false
+        chain_id
+        block
+        level
+        errors
+        [endorsement])
+    out
+  >>=? fun () -> next
+
+let blocks_loop cctxt =
+  Shell_services.Monitor.valid_blocks cctxt ~chains:[cctxt#chain] ()
+  >>=? fun (block_stream, _stopper) ->
+  Lwt_stream.fold_s
+    (fun ((chain_id, hash), header) _acc ->
+      let reception_time = Systime_os.now () in
+      Block_services.Operations.operations_in_pass
+        cctxt
+        ~chain:(`Hash chain_id)
+        ~block:(`Hash (hash, 0))
+        0
+      >>=? fun ops ->
+      let block_level = header.Block_header.shell.Block_header.level in
+      (*_assert
+          (check_level_in_endorsements (Int32.pred block_level) ops)
+          __LOC__
+          "Endorsement with the wrong level in "
+        >>=? fun () ->*)
+      let pks =
+        List.filter_map
+          (fun Block_services.{ receipt; _ } ->
+            match receipt with
+            | Some
+                (Protocol.Apply_results.Operation_metadata
+                  { contents =
+                      Single_result
+                        (Protocol.Apply_results.Endorsement_result
+                          { delegate; _ })
+                  }) ->
+                Some delegate
+            | _ -> None)
+          ops
+      in
+      let () = Archiver.add_block block_level hash reception_time pks in
+      return_unit)
+    block_stream
+    (ok ())
+
+let print_failures f =
+  f >|= function
+  | Ok () -> ()
+  | Error e -> Error_monad.pp_print_error Format.err_formatter e
+
+let main cctxt prefix =
+  let dumper = Archiver.launch prefix in
+  let main =
+    Shell_services.Chain.chain_id cctxt ~chain:cctxt#chain ()
+    >>=? fun chain_id ->
+    Lwt.join
+      [ print_failures (valid_endorsements_loop cctxt true chain_id);
+        print_failures (invalid_endorsements_loop cctxt chain_id);
+        print_failures (blocks_loop cctxt) ]
+    >>= fun () ->
+    let () = Archiver.stop () in
+    return_unit
+  in
+  dumper >>= fun () -> main
+
+let group =
+  { Clic.name = "teztale"; Clic.title = "A delegate operation monitor" }
+
+let directory_parameter =
+  Clic.parameter (fun _ p ->
+      if not (Sys.file_exists p && Sys.is_directory p) then
+        failwith "Directory doesn't exist: '%s'" p
+      else return p)
+
+let commands =
+  [ Clic.command
+      ~group
+      ~desc:"Go"
+      Clic.no_options
+      ( Clic.prefixes ["run"; "in"]
+      @@ Clic.param
+           ~name:"archive_path"
+           ~desc:"folder in which to dump files"
+           directory_parameter
+      @@ Clic.stop )
+      (fun () prefix cctxt -> main cctxt prefix) ]
+
+let select_commands _ _ =
+  return
+    (List.map
+       (Clic.map_command (new Protocol_client_context.wrap_full))
+       commands)
+
+let () = Client_main_run.run (module Client_config) ~select_commands
