e08695c2d37e6853f9093a78b871f0238855fe7a
Raphaël Proust <code@bnwr.net>
2021-05-18 11:21:01 +0200

    CI: use newer opam repo with fixed merlin links

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,11 +9,11 @@ recommended_rust_version=1.44.0
 
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=3b10ec4f01b65ac56ea15ae94b050d056ba3fbef
+full_opam_repository_tag=a147c4ecea9e6eaa71a0f3bc152d502695c569c4
 
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml
-opam_repository_tag=17f5f8fd9267b665774722a3f9aac913c672f654
+opam_repository_tag=161654c289ab0c39c425e9b4cc8d7b7558d1d462
 opam_repository_url=https://gitlab.com/tezos/opam-repository
 opam_repository_git=$opam_repository_url.git
 opam_repository=$opam_repository_git\#$opam_repository_tag
