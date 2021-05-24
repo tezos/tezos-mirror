d4hines/marigold-tezos

Proto/tests: parameterize some functions by RNG state in helpers
Co-Authored-By: Ilias Garnier <ilias.garnier@nomadic-labs.com>
Co-Authored-By: Lucas Randazzo <lucas.randazzo@nomadic-labs.com>

src/proto_alpha/lib_protocol/test/helpers/account.mli
@@ -53,7 +53,10 @@ val find_alternate : Signature.Public_key_hash.t -> t
     4.000.000.000 tz (if the list is too short); and add them to the
     global account state *)
 val generate_accounts :
-  ?initial_balances:int64 list -> int -> (t * Tez_repr.t) list
+  ?rng_state:Random.State.t ->
+  ?initial_balances:int64 list ->
+  int ->
+  (t * Tez_repr.t) list
 
 val commitment_secret : Blinded_public_key_hash.activation_code
 

tezos/tezos-mirror

src/proto_alpha/lib_protocol/test/helpers/account.mli
@@ -53,7 +53,12 @@ val find_alternate : Signature.Public_key_hash.t -> t
     [i]th value in the list [initial_balances] or otherwise
     4.000.000.000 tz (if the list is too short); and add them to the
     global account state *)
-val generate_accounts : ?initial_balances:int64 list -> int -> (t * Tez.t) list
+
+val generate_accounts :
+  ?rng_state:Random.State.t ->
+  ?initial_balances:int64 list ->
+  int ->
+  (t * Tez.t) list
 
 val commitment_secret : Blinded_public_key_hash.activation_code
 