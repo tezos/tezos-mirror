4113260ec504657634ccddaf2af3b3da07c22562
Romain <romain@nomadic-labs.com>
2021-05-24 15:50:35 +0000

    Tezt: add Check

---
+++ tezt/lib/check.ml
@@ -0,0 +1,357 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Base
+
+type 'a typ =
+  | E of {pp : Format.formatter -> 'a -> unit; equal : 'a -> 'a -> bool}
+  | C of {pp : Format.formatter -> 'a -> unit; compare : 'a -> 'a -> int}
+
+let pp_print_unit fmt () = Format.pp_print_string fmt "()"
+
+let unit = C {pp = pp_print_unit; compare = Unit.compare}
+
+let bool = C {pp = Format.pp_print_bool; compare = Bool.compare}
+
+let pp_print_quoted_char fmt char = Format.fprintf fmt "%C" char
+
+let char = C {pp = pp_print_quoted_char; compare = Char.compare}
+
+let int = C {pp = Format.pp_print_int; compare = Int.compare}
+
+let pp_print_int32 fmt i = Format.pp_print_string fmt (Int32.to_string i)
+
+let int32 = C {pp = pp_print_int32; compare = Int32.compare}
+
+let pp_print_int64 fmt i = Format.pp_print_string fmt (Int64.to_string i)
+
+let int64 = C {pp = pp_print_int64; compare = Int64.compare}
+
+let float = C {pp = Format.pp_print_float; compare = Float.compare}
+
+let compare_floats_epsilon epsilon a b =
+  if abs_float (a -. b) <= epsilon then 0 else Float.compare a b
+
+let float_epsilon epsilon =
+  C {pp = Format.pp_print_float; compare = compare_floats_epsilon epsilon}
+
+let pp_print_quoted_string fmt string =
+  Format.pp_print_char fmt '"' ;
+  Format.pp_print_string fmt (String.escaped string) ;
+  Format.pp_print_char fmt '"'
+
+let string = C {pp = pp_print_quoted_string; compare = String.compare}
+
+let pp_option pp_item fmt = function
+  | None ->
+      Format.pp_print_string fmt "None"
+  | Some item ->
+      Format.pp_print_string fmt "Some " ;
+      pp_item fmt item
+
+let option = function
+  | E {pp = pp_item; equal = eq_items} ->
+      E {pp = pp_option pp_item; equal = Option.equal eq_items}
+  | C {pp = pp_item; compare = cmp_items} ->
+      C {pp = pp_option pp_item; compare = Option.compare cmp_items}
+
+let pp_list ?(left = "[") ?(right = "]") pp_item fmt list =
+  Format.pp_print_string fmt left ;
+  if list <> [] then (
+    Format.pp_open_box fmt 1 ;
+    Format.pp_print_char fmt ' ' ;
+    let pp_sep fmt () =
+      Format.pp_print_char fmt ';' ;
+      Format.pp_print_space fmt ()
+    in
+    Format.pp_print_list ~pp_sep pp_item fmt list ;
+    Format.pp_print_char fmt ' ' ;
+    Format.pp_close_box fmt () ) ;
+  Format.pp_print_string fmt right
+
+(* Note: available as List.equal in OCaml 4.12. *)
+let rec equal_lists eq_items a b =
+  match (a, b) with
+  | ([], []) ->
+      true
+  | ([], _ :: _) | (_ :: _, []) ->
+      false
+  | (hda :: tla, hdb :: tlb) ->
+      eq_items hda hdb && equal_lists eq_items tla tlb
+
+(* Note: available as List.compare in OCaml 4.12. *)
+let rec compare_lists cmp_items a b =
+  match (a, b) with
+  | ([], []) ->
+      0
+  | ([], _ :: _) ->
+      -1
+  | (_ :: _, []) ->
+      1
+  | (hda :: tla, hdb :: tlb) ->
+      let c = cmp_items hda hdb in
+      if c = 0 then compare_lists cmp_items tla tlb else c
+
+let list = function
+  | E {pp = pp_item; equal = eq_items} ->
+      E {pp = pp_list pp_item; equal = equal_lists eq_items}
+  | C {pp = pp_item; compare = cmp_items} ->
+      C {pp = pp_list pp_item; compare = compare_lists cmp_items}
+
+let pp_array pp_item fmt array =
+  pp_list ~left:"[|" ~right:"|]" pp_item fmt (Array.to_list array)
+
+let equal_arrays eq_items a b =
+  let len_a = Array.length a in
+  let len_b = Array.length b in
+  len_a = len_b
+  &&
+  let rec loop i =
+    if i >= len_a then true
+    else if eq_items a.(i) b.(i) then loop (i + 1)
+    else false
+  in
+  loop 0
+
+let compare_arrays cmp_items a b =
+  let len_a = Array.length a in
+  let len_b = Array.length b in
+  let rec loop i =
+    (* All items up to [i - 1] are equal. *)
+    match (i >= len_a, i >= len_b) with
+    | (true, true) ->
+        (* Both arrays have the same size. *)
+        0
+    | (true, false) ->
+        (* [a] is smaller than [b]. *)
+        -1
+    | (false, true) ->
+        (* [a] is longer than [b]. *)
+        1
+    | (false, false) ->
+        let c = cmp_items a.(i) b.(i) in
+        if c = 0 then loop (i + 1) else c
+  in
+  loop 0
+
+let array = function
+  | E {pp = pp_item; equal = eq_items} ->
+      E {pp = pp_array pp_item; equal = equal_arrays eq_items}
+  | C {pp = pp_item; compare = cmp_items} ->
+      C {pp = pp_array pp_item; compare = compare_arrays cmp_items}
+
+type _ tuple_item = Item : 'b typ * ('a -> 'b) -> 'a tuple_item
+
+let get_pp = function E {pp; _} | C {pp; _} -> pp
+
+let get_equal = function
+  | E {equal; _} ->
+      equal
+  | C {compare; _} ->
+      fun a b -> compare a b = 0
+
+let tuple (type a) (items : a tuple_item list) : a typ =
+  let pp fmt value =
+    Format.pp_open_box fmt 1 ;
+    Format.pp_print_string fmt "(" ;
+    List.iteri
+      (fun i (Item (item_type, get_item)) ->
+        if i > 0 then (
+          Format.pp_print_char fmt ',' ;
+          Format.pp_print_space fmt () ) ;
+        get_pp item_type fmt (get_item value))
+      items ;
+    Format.pp_print_string fmt ")" ;
+    Format.pp_close_box fmt ()
+  in
+  let comparable =
+    List.for_all
+      (function Item (E _, _) -> false | Item (C _, _) -> true)
+      items
+  in
+  if comparable then
+    let compare a b =
+      let rec loop = function
+        | [] ->
+            0
+        | Item (item_type, get_item) :: tail -> (
+          match item_type with
+          | E _ ->
+              assert false (* [comparable] was [true] *)
+          | C {compare; _} ->
+              let c = compare (get_item a) (get_item b) in
+              if c = 0 then loop tail else c )
+      in
+      loop items
+    in
+    C {pp; compare}
+  else
+    let equal a b =
+      let rec loop = function
+        | [] ->
+            true
+        | Item (item_type, get_item) :: tail ->
+            get_equal item_type (get_item a) (get_item b) && loop tail
+      in
+      loop items
+    in
+    E {pp; equal}
+
+let tuple2 a b = tuple [Item (a, fst); Item (b, snd)]
+
+let tuple3 a b c =
+  tuple
+    [ Item (a, fun (x, _, _) -> x);
+      Item (b, fun (_, x, _) -> x);
+      Item (c, fun (_, _, x) -> x) ]
+
+let tuple4 a b c d =
+  tuple
+    [ Item (a, fun (x, _, _, _) -> x);
+      Item (b, fun (_, x, _, _) -> x);
+      Item (c, fun (_, _, x, _) -> x);
+      Item (d, fun (_, _, _, x) -> x) ]
+
+let convert encode = function
+  | E {pp; equal} ->
+      let pp fmt x = pp fmt (encode x) in
+      let equal a b = equal (encode a) (encode b) in
+      E {pp; equal}
+  | C {pp; compare} ->
+      let pp fmt x = pp fmt (encode x) in
+      let compare a b = compare (encode a) (encode b) in
+      C {pp; compare}
+
+let equalable pp equal = E {pp; equal}
+
+let comparable pp compare = C {pp; compare}
+
+module type EQUALABLE = sig
+  type t
+
+  val pp : Format.formatter -> t -> unit
+
+  val equal : t -> t -> bool
+end
+
+let equalable_module (type a) (m : (module EQUALABLE with type t = a)) =
+  let module M = (val m) in
+  equalable M.pp M.equal
+
+module type COMPARABLE = sig
+  type t
+
+  val pp : Format.formatter -> t -> unit
+
+  val compare : t -> t -> int
+end
+
+let comparable_module (type a) (m : (module COMPARABLE with type t = a)) =
+  let module M = (val m) in
+  comparable M.pp M.compare
+
+let fail error_msg pp_a a pp_b b =
+  let error_msg =
+    let substitution group =
+      match Re.Group.get group 0 with
+      | exception Not_found ->
+          (* Should not happen: groups always contain at least group 0
+             denoting the whole match. *)
+          ""
+      | "%L" ->
+          Format.asprintf "%a" pp_a a
+      | "%R" ->
+          Format.asprintf "%a" pp_b b
+      | s ->
+          s
+    in
+    Re.replace (Re.compile (Re.Perl.re "%[LR]")) ~f:substitution error_msg
+  in
+  Test.fail "%s" error_msg
+
+let eq a b typ ~error_msg =
+  if not (get_equal typ a b) then
+    let pp = get_pp typ in
+    fail error_msg pp a pp b
+
+let neq a b typ ~error_msg =
+  if get_equal typ a b then
+    let pp = get_pp typ in
+    fail error_msg pp a pp b
+
+let comparison function_name predicate a b typ ~error_msg =
+  match typ with
+  | E _ ->
+      let pp = get_pp typ in
+      let message =
+        Format.asprintf
+          "Check.%s used on non-comparable type to compare %a and %a with \
+           ~error_msg = %s"
+          function_name
+          pp
+          a
+          pp
+          b
+          error_msg
+      in
+      Test.fail "%s" message
+  | C {pp = _; compare} ->
+      if not (predicate (compare a b)) then
+        let pp = get_pp typ in
+        fail error_msg pp a pp b
+
+let lt x = comparison "(<)" (fun c -> c < 0) x
+
+let le x = comparison "(<=)" (fun c -> c <= 0) x
+
+let gt x = comparison "(>)" (fun c -> c > 0) x
+
+let ge x = comparison "(>=)" (fun c -> c >= 0) x
+
+let pp_rex fmt rex = Format.pp_print_string fmt (show_rex rex)
+
+let like a b ~error_msg =
+  if a =~! b then fail error_msg pp_print_quoted_string a pp_rex b
+
+let not_like a b ~error_msg =
+  if a =~ b then fail error_msg pp_print_quoted_string a pp_rex b
+
+(* We define infix operators at the end to avoid using them accidentally. *)
+
+let ( = ) = eq
+
+let ( <> ) = neq
+
+let ( < ) = lt
+
+let ( <= ) = le
+
+let ( > ) = gt
+
+let ( >= ) = ge
+
+let ( =~ ) = like
+
+let ( =~! ) = not_like
---
+++ tezt/lib/check.mli
@@ -0,0 +1,231 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** Assertions.
+
+    This module defines predicates that call [Test.fail] when they do not hold.
+    Using these predicates gives you more consistent error messages. *)
+
+open Base
+
+(** {2 Comparable Types} *)
+
+(** Type descriptions.
+
+    A [t typ] equips a type [t] with a pretty printer, and either an
+    equality or a comparison function.
+
+    All types can be used with [eq] and [neq], but some types will result
+    in an exception when used with [lt], [le], [gt] and [ge].
+    Such types are said to be non comparable. *)
+type 'a typ
+
+(** The unit type.
+
+    Not very useful on its own, but may be useful inside more structured types.
+
+    This type is comparable. *)
+val unit : unit typ
+
+(** The boolean type.
+
+    Not very useful on its own since one can write [if not b then Test.fail ...],
+    but may be useful inside more structured types.
+
+    This type is comparable with [true > false]. *)
+val bool : bool typ
+
+(** The character type.
+
+    This type is comparable. Characters are compared by comparing their
+    byte representation. *)
+val char : char typ
+
+(** The integer type.
+
+    This type is comparable. *)
+val int : int typ
+
+(** The 32-bit integer type.
+
+    This type is comparable. *)
+val int32 : int32 typ
+
+(** The 64-bit integer type.
+
+    This type is comparable. *)
+val int64 : int64 typ
+
+(** The float type.
+
+    Note that testing float equality or inequality is often a bad idea.
+    Prefer [float_epsilon] when it makes sense.
+
+    This type is comparable. *)
+val float : float typ
+
+(** The float type with an approximative equality function.
+
+    The argument is how much of a difference can two values have and still
+    be considered equal.
+
+    This type is comparable. *)
+val float_epsilon : float -> float typ
+
+(** The string type.
+
+    This type is comparable in lexicographic order, where each character is
+    compared using the same comparison function as [char]. *)
+val string : string typ
+
+(** Make an option type.
+
+    If the item type is comparable, the result is comparable.
+    [None] is always lesser than [Some _]. *)
+val option : 'a typ -> 'a option typ
+
+(** Make a list type.
+
+    If the item type is comparable, the result is comparable in lexicographic order. *)
+val list : 'a typ -> 'a list typ
+
+(** Make an array type.
+
+    If the item type is comparable, the result is comparable in lexicographic order. *)
+val array : 'a typ -> 'a array typ
+
+(** Make a 2-tuple type.
+
+    If both item types are comparable, the result is comparable in lexicographic order. *)
+val tuple2 : 'a typ -> 'b typ -> ('a * 'b) typ
+
+(** Make a 3-tuple type.
+
+    If all item types are comparable, the result is comparable in lexicographic order. *)
+val tuple3 : 'a typ -> 'b typ -> 'c typ -> ('a * 'b * 'c) typ
+
+(** Make a 4-tuple type.
+
+    If all item types are comparable, the result is comparable in lexicographic order. *)
+val tuple4 : 'a typ -> 'b typ -> 'c typ -> 'd typ -> ('a * 'b * 'c * 'd) typ
+
+(** Make a type by encoding to another.
+
+    Usage: [convert encode typ]
+
+    Example: [convert (fun { x; y } -> x, y) (tuple2 int string)]
+
+    Values are converted to [typ] using [encode] before being pretty-printed
+    or compared. The result type is comparable if [typ] is comparable. *)
+val convert : ('a -> 'b) -> 'b typ -> 'a typ
+
+(** Make a custom type from a pretty-printer and an equality function.
+
+    The result is not comparable. *)
+val equalable :
+  (Format.formatter -> 'a -> unit) -> ('a -> 'a -> bool) -> 'a typ
+
+(** Make a custom type from a pretty-printer and a comparison function. *)
+val comparable :
+  (Format.formatter -> 'a -> unit) -> ('a -> 'a -> int) -> 'a typ
+
+module type EQUALABLE = sig
+  type t
+
+  val pp : Format.formatter -> t -> unit
+
+  val equal : t -> t -> bool
+end
+
+(** Same as {!equalable} but takes a module.
+
+    Example: [equalable_module (module String)]. *)
+val equalable_module : (module EQUALABLE with type t = 'a) -> 'a typ
+
+module type COMPARABLE = sig
+  type t
+
+  val pp : Format.formatter -> t -> unit
+
+  val compare : t -> t -> int
+end
+
+(** Same as {!comparable} but takes a module.
+
+    Example: [comparable_module (module String)]. *)
+val comparable_module : (module COMPARABLE with type t = 'a) -> 'a typ
+
+(** {2 Predicates} *)
+
+(** For all functions of this section, if the test fails, the function calls
+    [Test.fail] with the error message given in [~error_msg].
+    This [~error_msg] may contain placeholders [%L] and [%R] where:
+    - [%L] is replaced by the left-hand side value of the operator;
+    - [%R] is replaced by the right-hand side value of the operator.
+
+    Here are some examples of [~error_msg] for inspiration:
+    - ["expected filename = %R, got %L"]
+    - ["expected list size >= %R, got %L"]
+    - ["expected f to be monotonous, got f x = %L and f y = %R with x < y"] *)
+
+(** Check that a value is equal to another.
+
+    Example: [Check.((value = expected) int ~error_msg:"expected value = %R, got %L")] *)
+val ( = ) : 'a -> 'a -> 'a typ -> error_msg:string -> unit
+
+(** Check that a value is not equal to another.
+
+    Example: [Check.((value <> wrong) int ~error_msg:"expected value <> %R")] *)
+val ( <> ) : 'a -> 'a -> 'a typ -> error_msg:string -> unit
+
+(** Check that a value is less than another.
+
+    Example: [Check.((value < threshold) int ~error_msg:"expected value < %R, got %L")] *)
+val ( < ) : 'a -> 'a -> 'a typ -> error_msg:string -> unit
+
+(** Check that a value is less than or equal to another.
+
+    Example: [Check.((value <= threshold) int ~error_msg:"expected value <= %R, got %L")] *)
+val ( <= ) : 'a -> 'a -> 'a typ -> error_msg:string -> unit
+
+(** Check that a value is greater than another.
+
+    Example: [Check.((value > threshold) int ~error_msg:"expected value > %R, got %L")] *)
+val ( > ) : 'a -> 'a -> 'a typ -> error_msg:string -> unit
+
+(** Check that a value is greater than or equal to another.
+
+    Example: [Check.((value >= threshold) int ~error_msg:"expected value >= %R, got %L")] *)
+val ( >= ) : 'a -> 'a -> 'a typ -> error_msg:string -> unit
+
+(** Check that a string matches a regular expression.
+
+    Example: [Check.((value =~ rex) int ~error_msg:"expected value =~ %R, got %L")] *)
+val ( =~ ) : string -> rex -> error_msg:string -> unit
+
+(** Check that a string does not match a regular expression.
+
+    Example: [Check.((value =~! rex) int ~error_msg:"expected value =~! %R, got %L")] *)
+val ( =~! ) : string -> rex -> error_msg:string -> unit
