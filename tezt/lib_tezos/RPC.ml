9a8f15717f1cc850765f390bd4ae034825bfa51d
Albin Coquereau <albin.coquereau@nomadic-labs.com>
2021-06-07 20:42:10 +0200

    Tezt: add run_operation RPC

--- tezt/lib_tezos/RPC.ml
+++ tezt/lib_tezos/RPC.ml
@@ -121,6 +121,20 @@ let get_levels_in_current_cycle ?endpoint ?hooks ?(chain = "main")
   in
   Client.rpc ?endpoint ?hooks GET path client
 
+let post_forge_operations ?endpoint ?hooks ?(chain = "main") ?(block = "head")
+    ~data client =
+  let path =
+    ["chains"; chain; "blocks"; block; "helpers"; "forge"; "operations"]
+  in
+  Client.rpc ?endpoint ?hooks ~data POST path client
+
+let post_run_operation ?endpoint ?hooks ?(chain = "main") ?(block = "head")
+    ~data client =
+  let path =
+    ["chains"; chain; "blocks"; block; "helpers"; "scripts"; "run_operation"]
+  in
+  Client.rpc ?endpoint ?hooks ~data POST path client
+
 module Big_maps = struct
   let get ?endpoint ?hooks ?(chain = "main") ?(block = "head") ~id ~key_hash
       client =
@@ -157,13 +171,6 @@ module Big_maps = struct
     in
     Client.rpc ?endpoint ?hooks ~query_string GET path client
 end
-
-let post_forge_operations ?endpoint ?hooks ?(chain = "main") ?(block = "head")
-    ~data client =
-  let path =
-    ["chains"; chain; "blocks"; block; "helpers"; "forge"; "operations"]
-  in
-  Client.rpc ?endpoint ?hooks ~data POST path client
 
 module Contracts = struct
   let spawn_get_all ?endpoint ?hooks ?(chain = "main") ?(block = "head") client
