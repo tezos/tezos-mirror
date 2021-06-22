082908b23105b1f2d47890eea8cee9e0ac2e2de8
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2021-06-22 14:13:13 +0200

    Build: refresh opam-repository to use tls.0.13.2

--- scripts/version.sh
+++ scripts/version.sh
@@ -10,10 +10,10 @@ recommended_rust_version=1.44.0
 ## Please update `.gitlab-ci.yml` accordingly
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=a147c4ecea9e6eaa71a0f3bc152d502695c569c4
+full_opam_repository_tag=634828f0e488b41f6b0a5ba9dac39fa6496b1904
 
 ## opam_repository is an additional, tezos-specific opam repository.
-opam_repository_tag=0de0cd8a503fbf917116c0e461a13fb2f4f351a6
+opam_repository_tag=b19f263f65febd44d6ef211cebbc46e2b606bdad
 opam_repository_url=https://gitlab.com/tezos/opam-repository.git
 opam_repository=$opam_repository_url\#$opam_repository_tag
 
