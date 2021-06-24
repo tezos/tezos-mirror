b2ea315c15742963fdd3026eacdca4a4eb3e4787
Raphaël Proust <code@bnwr.net>
2021-06-24 09:41:51 +0200

    Everywhere: update data-encoding dependency to 0.4

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,11 +9,11 @@ recommended_rust_version=1.44.0
 
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-full_opam_repository_tag=7026cc03199e7315b5ec592610650ff85c21cd90
+full_opam_repository_tag=9c8cef69e4179de3853f884314649b27b822d4aa
 
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml
-opam_repository_tag=1db0e28de40c6416ed75ee092f7a803fe326069d
+opam_repository_tag=3f540d038574c846e0158483ecd49de374d450c2
 opam_repository_url=https://gitlab.com/tezos/opam-repository
 opam_repository_git=$opam_repository_url.git
 opam_repository=$opam_repository_git\#$opam_repository_tag
