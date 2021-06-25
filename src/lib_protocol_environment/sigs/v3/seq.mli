6dd7d97339e4adf7fd2c6067ee3549d2bc2aa2ed
Clément Hurlin <clement.hurlin@tweag.io>
2021-06-25 16:58:45 +0200

    Protocol-env: fix formatting of doc in seq.mli

--- src/lib_protocol_environment/sigs/v3/seq.mli
+++ src/lib_protocol_environment/sigs/v3/seq.mli
@@ -54,28 +54,28 @@ val unfold : ('b -> ('a * 'b) option) -> 'b -> 'a t
 (** {3 Lwtreslib-specific extensions} *)
 
 (** [first s] is [None] if [s] is empty, it is [Some x] where [x] is the
-      first element of [s] otherwise.
+    first element of [s] otherwise.
 
-      Note that [first] forces the first element of the sequence, which can have
-      side-effects or be computationally expensive. Consider, e.g., the case
-      where [s = filter (fun …) s']: [first s] can force multiple of the values
-      from [s']. *)
+    Note that [first] forces the first element of the sequence, which can have
+    side-effects or be computationally expensive. Consider, e.g., the case
+    where [s = filter (fun …) s']: [first s] can force multiple of the values
+    from [s']. *)
 val first : 'a t -> 'a option
 
 (** Similar to {!fold_left} but wraps the traversal in {!result}. The
-      traversal is interrupted if one of the step returns an [Error _]. *)
+    traversal is interrupted if one of the step returns an [Error _]. *)
 val fold_left_e :
   ('a -> 'b -> ('a, 'trace) result) -> 'a -> 'b t -> ('a, 'trace) result
 
 (** Similar to {!fold_left} but wraps the traversing in {!Lwt}. Each step of
-      the traversal is started after the previous one has resolved. The
-      traversal is interrupted if one of the promise is rejected. *)
+    the traversal is started after the previous one has resolved. The
+    traversal is interrupted if one of the promise is rejected. *)
 val fold_left_s : ('a -> 'b -> 'a Lwt.t) -> 'a -> 'b t -> 'a Lwt.t
 
 (** Similar to {!fold_left} but wraps the traversing in [result Lwt.t].
-      Each step of the traversal is started after the previous one resolved. The
-      traversal is interrupted if one of the step is rejected or is fulfilled
-      with [Error _]. *)
+    Each step of the traversal is started after the previous one resolved. The
+    traversal is interrupted if one of the step is rejected or is fulfilled
+    with [Error _]. *)
 val fold_left_es :
   ('a -> 'b -> ('a, 'trace) result Lwt.t) ->
   'a ->
@@ -83,37 +83,37 @@ val fold_left_es :
   ('a, 'trace) result Lwt.t
 
 (** Similar to {!iter} but wraps the iteration in {!result}. The iteration
-      is interrupted if one of the step returns an [Error _]. *)
+    is interrupted if one of the step returns an [Error _]. *)
 val iter_e : ('a -> (unit, 'trace) result) -> 'a t -> (unit, 'trace) result
 
 (** Similar to {!iter} but wraps the iteration in {!Lwt}. Each step
-      of the iteration is started after the previous one resolved. The iteration
-      is interrupted if one of the promise is rejected. *)
+    of the iteration is started after the previous one resolved. The iteration
+    is interrupted if one of the promise is rejected. *)
 val iter_s : ('a -> unit Lwt.t) -> 'a t -> unit Lwt.t
 
 (** Similar to {!iter} but wraps the iteration in [result Lwt.t]. Each step
-      of the iteration is started after the previous one resolved. The iteration
-      is interrupted if one of the promise is rejected of fulfilled with an
-      [Error _]. *)
+    of the iteration is started after the previous one resolved. The iteration
+    is interrupted if one of the promise is rejected of fulfilled with an
+    [Error _]. *)
 val iter_es :
   ('a -> (unit, 'trace) result Lwt.t) -> 'a t -> (unit, 'trace) result Lwt.t
 
 (** Similar to {!iter} but wraps the iteration in [result Lwt.t]. All the
-      steps of the iteration are started concurrently. The promise [iter_ep]
-      resolves once all the promises of the traversal resolve. At this point it
-      either:
-      - is rejected if at least one of the promises is, otherwise
-      - is fulfilled with [Error _] if at least one of the promises is,
-        otherwise
-      - is fulfilled with [Ok ()] if all the promises are. *)
+    steps of the iteration are started concurrently. The promise [iter_ep]
+    resolves once all the promises of the traversal resolve. At this point it
+    either:
+    - is rejected if at least one of the promises is, otherwise
+    - is fulfilled with [Error _] if at least one of the promises is,
+      otherwise
+    - is fulfilled with [Ok ()] if all the promises are. *)
 val iter_ep :
   ('a -> (unit, 'error Error_monad.trace) result Lwt.t) ->
   'a t ->
   (unit, 'error Error_monad.trace) result Lwt.t
 
 (** Similar to {!iter} but wraps the iteration in {!Lwt}. All the
-      steps of the iteration are started concurrently. The promise [iter_p f s]
-      is resolved only once all the promises of the iteration are. At this point
-      it is either fulfilled if all promises are, or rejected if at least one of
-      them is. *)
+    steps of the iteration are started concurrently. The promise [iter_p f s]
+    is resolved only once all the promises of the iteration are. At this point
+    it is either fulfilled if all promises are, or rejected if at least one of
+    them is. *)
 val iter_p : ('a -> unit Lwt.t) -> 'a t -> unit Lwt.t
