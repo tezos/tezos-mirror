a419e384e5d4a42df1fa387afbda2e0ac9c5879b
Ilias Garnier <ilias.garnier@nomadic-labs.com>
2021-06-25 11:39:36 +0200

    scripts/yes-wallet: fix patch, allow exporting wallet from context

--- scripts/yes-wallet/yes_wallet.ml
+++ scripts/yes-wallet/yes_wallet.ml
@@ -23,12 +23,34 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let _ =
-  let yes_wallet_dir =
-    if Array.length Sys.argv > 1 then Sys.argv.(1) else "yes-wallet"
-  in
+let populate_wallet yes_wallet_dir alias_pkh_pk_list =
   Unix.mkdir yes_wallet_dir 0o750 ;
   Unix.chdir yes_wallet_dir ;
   json_to_file (pkh_list_json alias_pkh_pk_list) "public_key_hashs" ;
   json_to_file (pk_list_json alias_pkh_pk_list) "public_keys" ;
   json_to_file (sk_list_json alias_pkh_pk_list) "secret_keys"
+
+let usage () =
+  Format.printf "> create minimal in <yes_wallet_dir>@." ;
+  Format.printf "    creates a yes-wallet with the foundation baker keys in <yes_wallet_dir>@." ;
+  Format.printf "> create from context <base_dir> in <yes_wallet_dir>@." ;
+  Format.printf "    creates a yes-wallet with all delegates in the head block of the context in <base_dir> and store it in <yes_wallet_dir>@."
+
+let () =
+  match Array.to_list Sys.argv with
+  | [] -> assert false
+  | [_] ->
+    usage () ;
+    exit 0
+  | [_; "create"; "minimal"; "in"; yes_wallet_dir] ->
+    populate_wallet yes_wallet_dir Yes_wallet_lib.alias_pkh_pk_list ;
+    Format.printf "Created minimal wallet in %s@." yes_wallet_dir
+  | [_; "create"; "from"; "context"; base_dir; "in"; yes_wallet_dir] ->
+    let alias_pkh_pk_list =
+      Yes_wallet_lib.load_mainnet_bakers_public_keys base_dir in
+    populate_wallet yes_wallet_dir alias_pkh_pk_list ;
+    Format.printf "Created wallet in %s@." yes_wallet_dir
+  | _ ->
+    Format.eprintf "Invalid command. Usage:@." ;
+    usage () ;
+    exit 1
