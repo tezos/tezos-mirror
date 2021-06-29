5446816442b2329dec90fef1e252674d2a274c41
Craig Ferguson <me@craigfe.io>
2021-06-29 23:42:22 +0100

    CI: update tezos/opam-repository hash

--- scripts/version.sh
+++ scripts/version.sh
@@ -13,7 +13,7 @@ full_opam_repository_tag=9c8cef69e4179de3853f884314649b27b822d4aa
 
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml
-opam_repository_tag=c49c681c432890c52a6ca8b0caeff7bcb2798f66
+opam_repository_tag=357ef1cc5a75657bc0f937f27bd6c61ce24ee37b
 opam_repository_url=https://gitlab.com/tezos/opam-repository
 opam_repository_git=$opam_repository_url.git
 opam_repository=$opam_repository_git\#$opam_repository_tag
