e92afa030d02303fca83b90706697957c4b43a2d
Julien Debon <julien.debon@tweag.io>
2021-07-20 16:50:43 +0200

    Cleanup / improve a bit CircularBuffer doc

--- src/lib_stdlib/circular_buffer.mli
+++ src/lib_stdlib/circular_buffer.mli
@@ -25,14 +25,14 @@
 
 (** This module implements a bufferisation abstraction to store
    temporary raw data chunks (as bytes) when chunks are read
-   sequentially. The function write allows to store chunks in the
+   sequentially. The function [write] allows to store chunks in the
    buffer and the function read to read them from the buffer.
 
    The global contract is that if we write consecutively [d1;d2] onto
-   the buffer. Then we have to fully read d1 and d2, in that order.
+   the buffer, then we have to fully read [d1] and [d2], in that order.
 
    This contract is not enforced by the library, it is the user
-   responsability to respect it.
+   responsibility to respect it.
 
    If the circular buffer is full, a new temporary buffer is
    allocated to store the chunk of data to be written. *)
@@ -43,11 +43,12 @@ type t
 (** An abstraction over a chunk of data written in the buffer. *)
 type data
 
-(** Create a buffer of size maxlength. If the buffer is full, a buffer
-   of size [fresh_buf_size] is allocated (by default `2` kb). *)
+(** [create ?maxlength ?fresh_buf_size ()] creates a buffer of size [maxlength]
+    (by default [32] kb). If the buffer is full, a buffer of size [fresh_buf_size]
+    is allocated (by default [2] kb). *)
 val create : ?maxlength:int -> ?fresh_buf_size:int -> unit -> t
 
-(** [write ~maxlen ~fill_using:f buffer] calls [fill_using buf offset
+(** [write ~maxlen ~fill_using buffer] calls [fill_using buf offset
    maxlen] where [buf] is a buffer that has room for [maxlen] data
    starting from [offset].
 
@@ -64,15 +65,15 @@ val write :
   t ->
   data Lwt.t
 
-(** [read data ~len ~into:buf buffer ~offset] blit [len] data from the [data] chunk.
-    If [len] is not provided, blit all the data.
+(** [read data ~len ~into:buf buffer ~offset] copies [len] data from the [data] chunk into [buf].
+    If [len] is not provided, it copies all the data.
     If [len] is less than the amount of data available, it returns a
-    new handler the the remainder.
+    new handler of the remainder.
 
-    Assumes that data has been produce by a write attempt in [buffer].
-    Assumes that ~len is lesser than [length data].
+    - Assumes that [data] has been produced by a {!write} attempt in [buffer].
+    - Assumes that [len] is less than [length data].
 *)
 val read : data -> ?len:int -> t -> into:Bytes.t -> offset:int -> data option
 
-(** [length data] returns the amount of avalaible bytes in [data] *)
+(** [length data] returns the amount of available bytes in [data] *)
 val length : data -> int
