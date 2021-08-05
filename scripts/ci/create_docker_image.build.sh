4bd96c30accf1d6eb54f597262f69d88422b609b
Pietro <pietro.abate@tezcore.com>
2021-08-05 12:50:23 +0200

    docker: add GIT_VERSION env variable to build.Dockerfile

--- scripts/ci/create_docker_image.build.sh
+++ scripts/ci/create_docker_image.build.sh
@@ -15,6 +15,7 @@ base_image="${3:-registry.gitlab.com/tezos/opam-repository}"
 base_image_version="${4:-runtime-build-dependencies--${opam_repository_tag}}"
 commit_short_sha="${5:-$(git rev-parse --short HEAD)}"
 commit_datetime="${6:-$(git show -s --pretty=format:%ci HEAD)}"
+commit_tag="${7:-$(git describe --tags --always)}"
 
 echo
 echo "### Building tezos..."
@@ -29,6 +30,7 @@ docker build \
   --build-arg "BUILD_IMAGE_VERSION=${base_image_version}" \
   --build-arg "GIT_SHORTREF=${commit_short_sha}" \
   --build-arg "GIT_DATETIME=${commit_datetime}" \
+  --build-arg "GIT_VERSION=${commit_tag}" \
   "$src_dir"
 
 echo
