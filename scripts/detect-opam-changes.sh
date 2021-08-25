500ee1d3e05c1acbc4a58159b142b45fc560c80f
Konstantinos Sideris <siderisk@auth.gr>
2021-08-25 13:36:13 +0300

    CI: Do not depend on jq when creating the opam pipeline

--- scripts/detect-opam-changes.sh
+++ scripts/detect-opam-changes.sh
@@ -7,25 +7,24 @@
 # If one of the files was modified the `TZ_OPAM_FILES_MODIFIED` is set to `true` in order for the opam
 # pipeline to be generated.
 
-FILES="test"
+set -e
 
 if [ -n "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" ]; then
   git fetch origin "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME"
   FILES=$(git diff-tree --no-commit-id --name-only -r HEAD..origin/"$CI_MERGE_REQUEST_TARGET_BRANCH_NAME")
+
+  echo Files changed: "$FILES"
+
+  MATCHES=$(echo "$FILES" | tr ' ' '\n' | grep -E ".*dune|.*dune\.inc|.*\.opam|scripts/version\.sh|\.gitlab-ci\.yml" | cat)
+
+  echo Matches found: "$MATCHES"
+
+  if [ -n "$MATCHES" ]; then
+    export TZ_OPAM_FILES_MODIFIED=true
+  else
+    echo "No matches found"
+  fi
 else
   echo "Not part of a merge request. No commits to compare."
 fi
 
-echo Files changed: "$FILES"
-
-MATCHES=$(echo "$FILES" | tr ' ' '\n' | grep -E ".*dune|.*dune\.inc|.*\.opam|scripts/version\.sh|\.gitlab-ci\.yml")
-
-echo Matches found: "$MATCHES"
-
-if [ -n "$MATCHES" ]; then
-  echo "TZ_OPAM_FILES_MODIFIED=true" >> opam.env
-else
-  echo "No matches found"
-fi
-
-cat opam.env
--- scripts/generate_opam_pipeline.sh
+++ scripts/generate_opam_pipeline.sh
@@ -4,36 +4,32 @@ set -eu
 
 TZ_OPAM_FILES_MODIFIED="${TZ_OPAM_FILES_MODIFIED:-false}"
 
-# shellcheck disable=SC2016
-# We intentionally use single quotes to not allow variable substitution inside the jq query.
-yq -y -n \
-   --arg pkgs "$PACKAGES" \
-   --arg modified_flag "$TZ_OPAM_FILES_MODIFIED" \
-   '( $pkgs | split(" ")) | map(
-{
-  "opam:\(.)": {
-    extends: [
-      ".default_settings_template",
-      ".image_template__runtime_build_test_dependencies_template",
-      ".rules_template__development_opam"
-    ],
-    stage: "packaging",
-    variables: {
-      "TZ_OPAM_FILES_MODIFIED": $modified_flag
-    },
-    script: [
-      "./scripts/opam-pin.sh",
-      "opam depext --yes \(.)",
-      "opam install --yes \(.)",
-      "opam reinstall --yes --with-test \(.)"
-    ]
-  }
-}
-) | add | . + {
-  include: ".gitlab/ci/templates.yml",
-  stages: [ "packaging" ],
-  "opam:pipeline_init": {
-    stage: "packaging",
-    script: "echo Pipeline generated"
-  }
-}'
+cat <<EOF
+include: ".gitlab/ci/templates.yml"
+
+stages:
+  - packaging
+
+opam:pipeline_init:
+  stage: "packaging"
+  script: "echo Pipeline generated"
+EOF
+
+for PKG in $PACKAGES; do
+  cat <<EOF
+
+opam:$PKG:
+  extends:
+    - .default_settings_template
+    - .image_template__runtime_build_test_dependencies_template
+    - .rules_template__development_opam
+  stage: packaging
+  variables:
+    TZ_OPAM_FILES_MODIFIED: "$TZ_OPAM_FILES_MODIFIED"
+  script:
+    - ./scripts/opam-pin.sh
+    - opam depext --yes $PKG
+    - opam install --yes $PKG
+    - opam reinstall --yes --with-test $PKG
+EOF
+done
