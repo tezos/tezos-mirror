3bdc118a93f3d93c484f9b6decad9fb437f4cbc8
Ulrik Strid <ulrik.strid@outlook.com>
2021-09-03 09:35:41 +0000

    protocol environment: Expose BLS signature module
    Exposes Signature module from the BLS12-381 library in environment v4
    It is a prerequesite for Optimistic Rollups

--- src/lib_protocol_environment/environment_V4.mli
+++ src/lib_protocol_environment/environment_V4.mli
@@ -85,6 +85,7 @@ module type V4 = sig
        and type Timelock.chest_key = Timelock.chest_key
        and type Timelock.opening_result = Timelock.opening_result
        and module Sapling = Tezos_sapling.Core.Validator
+       and type Bls_signature.pk = Bls12_381.Signature.pk
 
   (** An [Ecoproto_error e] is a shell error that carry a protocol error.
 
