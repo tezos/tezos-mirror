e19bb5b0428dadb6cbc2ce936a0f976cd0ee9c7b
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2021-09-07 15:27:04 +0200

    scripts/prepare_migration_test.sh: fix call to yes_wallet

--- scripts/prepare_migration_test.sh
+++ scripts/prepare_migration_test.sh
@@ -82,7 +82,7 @@ after deleting spurious files ${yes_wallet}/{blocks,wallet_locks}."
     else
         echo "
 Creating a yes-wallet in directory ${yes_wallet}."
-        dune exec scripts/yes-wallet/yes_wallet.exe "$yes_wallet"
+        dune exec scripts/yes-wallet/yes_wallet.exe -- create minimal in "$yes_wallet"
     fi
     echo "You can now bake for foundation{1..8}."
 }
