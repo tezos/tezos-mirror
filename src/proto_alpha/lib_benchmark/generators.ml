907648f583326c76b09abb0c57bd4b1a95f8bda4
Ilias Garnier <ilias.garnier@nomadic-labs.com>
2021-09-15 11:17:12 +0200

    lib_benchmark/alpha: inline mcmc functor in generators.ml

--- src/proto_alpha/lib_benchmark/generators.ml
+++ src/proto_alpha/lib_benchmark/generators.ml
@@ -23,6 +23,81 @@
 (*                                                                           *)
 (*****************************************************************************)
 
+open StaTz
+
+let print_m (term : Mikhailsky.node) =
+  Mikhailsky.pp Format.str_formatter term ;
+  Format.flush_str_formatter ()
+
+module type Sampler_parameters_sig = sig
+  val initial : State_space.t
+
+  val energy : State_space.t -> float
+
+  val rules : Rules.rule_set list
+
+  val infer : Mikhailsky.node -> Inference.state
+
+  val verbosity : [`Silent | `Progress | `Trace]
+end
+
+(** Generic MCMC michelson sampler (can be used for code and data) *)
+module Make_generic (P : Sampler_parameters_sig) = struct
+  module MH_params : MH.MH_parameters with type t = State_space.t = struct
+    let uniform (l : State_space.t list) : State_space.t Stats.fin_prb =
+      match l with
+      | [] -> assert false
+      | _ ->
+          let arr = Array.of_list l in
+          let emp = Stats.empirical_of_raw_data arr in
+          Stats.fin_prb_of_empirical (module State_space) emp
+
+    let trace state =
+      match P.verbosity with
+      | `Silent | `Progress -> ()
+      | `Trace ->
+          Format.eprintf "@." ;
+          Format.eprintf "%a" State_space.pp state ;
+          Format.eprintf "energy:@." ;
+          Format.eprintf "%f:@." (P.energy state)
+
+    let unrecoverable_failure err current result =
+      Format.eprintf "Error when typechecking term:@." ;
+      Format.eprintf "%a@." Inference.pp_inference_error err ;
+      Format.eprintf "Original state: @[%a@]@." State_space.pp current ;
+      Format.eprintf "Erroneous term: %a@." Mikhailsky.pp result ;
+      Stdlib.failwith "in sampler.ml: unrecoverable failure."
+
+    let rec proposal ({State_space.term; _} as current) =
+      trace current ;
+      let rewriting_options = Rules.rewriting current P.rules in
+      let rewritings =
+        List.fold_left
+          (fun rewritings (path, replacement) ->
+            let result = Kernel.Rewriter.subst ~term ~path ~replacement in
+            let typing =
+              Lazy.from_fun (fun () ->
+                  try P.infer result
+                  with Inference.Ill_typed_script err ->
+                    unrecoverable_failure err current result)
+            in
+            {State_space.typing; term = result} :: rewritings)
+          []
+          rewriting_options
+      in
+      match rewritings with [] -> proposal current | _ -> uniform rewritings
+
+    let log_weight state = -.P.energy state
+
+    include State_space
+  end
+
+  module Sampler = MH.Make (MH_params)
+
+  let generator ~burn_in =
+    Sampler.mcmc ~verbosity:P.verbosity ~initial:P.initial ~burn_in
+end
+
 module Code (X : sig
   module Samplers : Michelson_samplers_base.Full_S
 
@@ -35,7 +110,7 @@ end) =
 struct
   module Autocomp = Autocomp.Make (X.Samplers)
 
-  module MCMC = Sampler.Make (struct
+  module MCMC = Make_generic (struct
     let initial =
       let term = Mikhailsky.Instructions.hole in
       let typing = Lazy.from_val @@ snd (Inference.infer_with_state term) in
@@ -89,7 +164,7 @@ struct
   module Autocomp = Autocomp.Make (X.Samplers)
   module Rewrite_rules = Rules.Data_rewrite_leaves (X.Samplers)
 
-  module MCMC = Sampler.Make (struct
+  module MCMC = Make_generic (struct
     let initial =
       let term = Mikhailsky.Data.hole in
       let typing =
