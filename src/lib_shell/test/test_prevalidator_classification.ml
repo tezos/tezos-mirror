102cc4e42603da4834181314f489e27ad4bec75f
Clément Hurlin <clement.hurlin@tweag.io>
2021-09-15 16:51:37 +0200

    prevalidator: make Test_prevalidation_classification.Generators its own module
    This makes it possible to use the generators from another module.

--- src/lib_shell/test/test_prevalidator_classification.ml
+++ src/lib_shell/test/test_prevalidator_classification.ml
@@ -31,6 +31,7 @@
 *)
 
 open Lib_test.Qcheck_helpers
+module Generators = Prevalidator_generators
 
 module Operation_map = struct
   let pp_with_trace ppf map =
@@ -61,10 +62,6 @@ module Operation_map = struct
         Operation.equal o1 o2 && t1 = t2)
 end
 
-let add_if_not_present classification oph op t =
-  Prevalidator_classification.(
-    if not (is_in_mempool oph t) then add classification oph op t)
-
 type classification_event =
   | Add_if_not_present of
       Prevalidator_classification.classification
@@ -77,107 +74,12 @@ let play_event event t =
   let open Prevalidator_classification in
   match event with
   | Add_if_not_present (classification, oph, op) ->
-      add_if_not_present classification oph op t
+      Generators.add_if_not_present classification oph op t
   | Remove oph -> remove oph t
   | Flush handle_branch_refused -> flush ~handle_branch_refused t
 
-module Generators = struct
+module Extra_generators = struct
   open Prevalidator_classification
-
-  let string_gen = QCheck.Gen.string ?gen:None
-
-  let operation_hash_gen : Operation_hash.t QCheck.Gen.t =
-    let open QCheck.Gen in
-    let+ key = opt (string_size (0 -- 64))
-    and+ path = list_size (0 -- 100) string_gen in
-    Operation_hash.hash_string ?key path
-
-  let block_hash_gen : Block_hash.t QCheck.Gen.t =
-    let open QCheck.Gen in
-    let+ key = opt (string_size (0 -- 64))
-    and+ path = list_size (0 -- 100) string_gen in
-    Block_hash.hash_string ?key path
-
-  (** Operations don't contain "valid" proto bytes but we don't care
-   *  as far as [Prevalidator_classification] is concerned. *)
-  let operation_gen : Operation.t QCheck.Gen.t =
-    let open QCheck.Gen in
-    let+ branch = block_hash_gen
-    and+ proto = string_gen >|= Bytes.unsafe_of_string in
-    Operation.{shell = {branch}; proto}
-
-  (** Do we need richer errors? If so, how to generate those? *)
-  let classification_gen : classification QCheck.Gen.t =
-    QCheck.Gen.oneofl
-      [`Applied; `Branch_delayed []; `Branch_refused []; `Refused []]
-
-  let unrefused_classification_gen : classification QCheck.Gen.t =
-    QCheck.Gen.oneofl [`Applied; `Branch_delayed []; `Branch_refused []]
-
-  let parameters_gen : parameters QCheck.Gen.t =
-    let open QCheck.Gen in
-    let+ map_size_limit = 1 -- 100 in
-    let on_discarded_operation _ = () in
-    {map_size_limit; on_discarded_operation}
-
-  let t_gen ?(can_be_full = true) () : t QCheck.Gen.t =
-    let open QCheck.Gen in
-    let* parameters = parameters_gen in
-    let+ inputs =
-      let limit = parameters.map_size_limit - if can_be_full then 0 else 1 in
-      list_size
-        (0 -- limit)
-        (triple classification_gen operation_hash_gen operation_gen)
-    in
-    let t = Prevalidator_classification.create parameters in
-    List.iter
-      (fun (classification, operation_hash, operation) ->
-        add_if_not_present classification operation_hash operation t)
-      inputs ;
-    t
-
-  (* With probability 1/2, we take an operation hash already present in the
-     classification. This operation is taken uniformly among the
-     different classes. *)
-  let with_t_operation_gen : t -> (Operation_hash.t * Operation.t) QCheck.Gen.t
-      =
-    let module Classification = Prevalidator_classification in
-    let open QCheck.Gen in
-    fun t ->
-      let to_ops map =
-        Operation_hash.Map.bindings map
-        |> List.map (fun (oph, (op, _)) -> (oph, op))
-      in
-      (* If map is empty, it cannot be used as a generator *)
-      let freq_of_map map = if Operation_hash.Map.is_empty map then 0 else 1 in
-      (* If list is empty, it cannot be used as a generator *)
-      let freq_of_list = function [] -> 0 | _ -> 1 in
-      (* If map is not empty, take one of its elements *)
-      let freq_and_gen_of_map map = (freq_of_map map, oneofl (to_ops map)) in
-      (* If list is not empty, take one of its elements *)
-      let freq_and_gen_of_list list = (freq_of_list list, oneofl list) in
-      (* We use max to ensure the ponderation is strictly greater than 0. *)
-      let freq_fresh t =
-        max
-          1
-          (freq_of_list t.applied_rev
-          + freq_of_map (Classification.map t.branch_refused)
-          + freq_of_map (Classification.map t.branch_delayed)
-          + freq_of_map (Classification.map t.refused))
-      in
-      frequency
-        [
-          freq_and_gen_of_list t.applied_rev;
-          freq_and_gen_of_map (Classification.map t.branch_refused);
-          freq_and_gen_of_map (Classification.map t.branch_delayed);
-          freq_and_gen_of_map (Classification.map t.refused);
-          (freq_fresh t, pair operation_hash_gen operation_gen);
-        ]
-
-  let t_with_operation_gen ?can_be_full () :
-      (t * (Operation_hash.t * Operation.t)) QCheck.Gen.t =
-    let open QCheck.Gen in
-    t_gen ?can_be_full () >>= fun t -> pair (return t) (with_t_operation_gen t)
 
   (** Generates an [event].
       The operation hash for [Remove] events is generated using
@@ -188,12 +90,15 @@ module Generators = struct
     let open QCheck.Gen in
     let add_gen =
       let+ (classification, oph, op) =
-        triple classification_gen operation_hash_gen operation_gen
+        triple
+          Generators.classification_gen
+          Generators.operation_hash_gen
+          Generators.operation_gen
       in
       Add_if_not_present (classification, oph, op)
     in
     let remove_gen =
-      let+ (oph, _op) = with_t_operation_gen t in
+      let+ (oph, _op) = Generators.with_t_operation_gen t in
       Remove oph
     in
     let flush_gen =
@@ -210,7 +115,7 @@ module Generators = struct
       obtained by having applied all previous events to [t_initial]. *)
   let t_with_event_sequence_gen =
     let open QCheck.Gen in
-    t_gen () >>= fun t ->
+    Generators.t_gen () >>= fun t ->
     let t_initial = Internal_for_tests.copy t in
     let rec loop acc_gen n =
       if n <= 0 then acc_gen
@@ -419,7 +324,7 @@ let test_invariants =
     ~name:
       "invariants are preserved through any sequence of events (provided we do \
        not [add] already present operations)"
-    (QCheck.make Generators.t_with_event_sequence_gen)
+    (QCheck.make Extra_generators.t_with_event_sequence_gen)
   @@ fun (t, events) ->
   let _ =
     List.fold_left
