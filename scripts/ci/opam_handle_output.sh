c123615ff768eb9408551c35b32238c40e8a171a
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2021-10-11 14:06:29 +0200

    CI: improved capture of logs in opam tests

--- scripts/ci/opam_handle_output.sh
+++ scripts/ci/opam_handle_output.sh
@@ -7,39 +7,53 @@
 # the first 1000 lines of the merged output.
 
 OPAM_LOGS=${OPAM_LOGS:-opam_logs}
+OPAM_DIR="$HOME"/.opam
 
 # artifacts:paths requires a relative path. to avoid calculating the
 # relative path, we use rsync to move all outputs to a sub-directory
 # of the current working directory, as specified by the environment
 # variable OPAM_LOGS, defaulting to opam_logs.
+mkdir -p "$OPAM_LOGS"
+
 rsync --recursive --prune-empty-dirs \
       "$OPAM_SWITCH_PREFIX"/.opam-switch/build \
       --include="*/" \
       --include '*.output' \
       --exclude '*' \
+      "$OPAM_LOGS/output"
+
+rsync --recursive "$OPAM_DIR"/log \
       "$OPAM_LOGS"
 
  # for ease of readability, produce a merged log
 merged=$(mktemp);
-(
-    find "$OPAM_LOGS/" \
-         -iname \*.output \
-         -exec echo "------------------- {} -------------------" ";" \
-         -exec cat "{}" ";"
-) > "$merged"
+
+IFS=$(printf '\n') \
+   find "$OPAM_LOGS" -type f |
+    # Sort files by modification time
+    while read -r file; do
+        printf '%d %s\n' "$(stat -c +%Y "$file")" "$file"
+    done | sort -k1nr | cut -f 2- -d ' ' |
+    while read -r file; do
+        echo "------------------- $file -------------------"
+        cat "$file"
+    done > "$merged"
 
 merged_html="$OPAM_LOGS"/merged_output.html
-echo "<html><body><pre>" > "${merged_html}"
-cat "$merged" >> "${merged_html}"
-echo "</pre></body></html>" >> "${merged_html}"
+(
+    echo "<html><body><pre>"
+    cat "$merged"
+    echo "</pre></body></html>"
+) > "${merged_html}"
 
 if [ "$CI_JOB_STATUS" != 'success' ]; then
-    echo "-- Job was non-successful (job status: $CI_JOB_STATUS), merged output:"
+    merged_output_url=$CI_SERVER_URL/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME/-/jobs/$CI_JOB_ID/artifacts/file/"$OPAM_LOGS"/merged_output.html
+    echo "-- Job was non-successful (job status: ${CI_JOB_STATUS:-N/A}), merged output:"
     echo ""
     cutoff=1000
     head -n $cutoff "$merged"
     line_count=$(wc -l "$merged" | cut -d' ' -f1)
     if [ "$line_count" -gt $cutoff ]; then
-        echo "... see artifacts for full output";
+        echo "... see artifacts for full output at $merged_output_url";
     fi
 fi
