9ebbab03d091459f363551fd5f3af68cb1f94963
Pietro <pietro.abate@tezcore.com>
2021-10-13 10:49:34 +0200

    shellcheck: upload-static-binaries-to-package-registry.sh and release/binaries.sh

--- scripts/release/binaries.sh
+++ scripts/release/binaries.sh
@@ -1,7 +1,11 @@
-#! /usr/bin/env bash
+#!/bin/sh
 
-binaries=("tezos-admin-client" "tezos-client" "tezos-node" "tezos-signer" "tezos-codec")
+binaries="tezos-admin-client tezos-client tezos-node tezos-signer tezos-codec"
 
-for proto in $(cat active_protocol_versions); do
-    binaries+=("tezos-accuser-$proto" "tezos-baker-$proto" "tezos-endorser-$proto")
-done
+while read -r  proto; do
+  if [ "$proto" = "alpha" ] || [ "$proto" = "012-PsiThaCa" ]; then
+    binaries="$binaries tezos-accuser-$proto tezos-baker-$proto"
+  else
+    binaries="$binaries tezos-accuser-$proto tezos-baker-$proto tezos-endorser-$proto"
+  fi
+done < active_protocol_versions
