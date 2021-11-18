mavryk-network/mavryk-ligo

Proto: add Non_empty_string

src/proto_alpha/lib_protocol/non_empty_string.mli
@@ -1,0 +1,45 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** A string that is guaranteed to be non-empty *)
+type t = private string
+
+include Compare.S with type t := t
+
+(** Returns [None] if the original string is empty. *)
+val of_string : string -> t option
+
+(** Fails with [Invalid_argument] if the original string is empty. *)
+val of_string_exn : string -> t
+
+(** [cat2 a b] concatenates [a] and [b].
+    [cat2 a ~sep b] concatenated [a], [sep], and [b]. *)
+val cat2 : t -> ?sep:string -> t -> t
+
+(** [split_on_last c s] finds the last occurrence of [c] in [s] and returns
+    the substring before and the substring after.
+    Returns [None] if [c] is not present in [s] or if one or both substrings
+    would end up being empty. *)
+val split_on_last : char -> t -> (t * t) option

tezos/tezos-mirror

src/proto_alpha/lib_protocol/non_empty_string.mli
@@ -1,0 +1,45 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** A string that is guaranteed to be non-empty *)
+type t = private string
+
+include Compare.S with type t := t
+
+(** Returns [None] if the original string is empty. *)
+val of_string : string -> t option
+
+(** Fails with [Invalid_argument] if the original string is empty. *)
+val of_string_exn : string -> t
+
+(** [cat2 a b] concatenates [a] and [b].
+    [cat2 a ~sep b] concatenates [a], [sep], and [b]. *)
+val cat2 : t -> ?sep:string -> t -> t
+
+(** [split_on_last c s] finds the last occurrence of [c] in [s] and returns
+    the substring before and the substring after.
+    Returns [None] if [c] is not present in [s] or if one or both substrings
+    would end up being empty. *)
+val split_on_last : char -> t -> (t * t) option