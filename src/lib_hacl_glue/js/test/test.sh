8147b04ec849e1432b0a8942b69d8152450e32b5
Hugo Heuzard <hugo.heuzard@nomadic-labs.com>
2021-11-22 14:30:37 +0100

    Javascript: introduce node-wrapper.exe to preload js deps.

--- src/lib_hacl_glue/js/test/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Installing hacl-wasm"
-npm install hacl-wasm
-
-echo "Running test"
-# here we run directly a test in js compiled with jsoo
-# instead of a mocha test
-node -- "$@"
