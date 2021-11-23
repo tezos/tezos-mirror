2a8e2b7e00d9969ac9c7353094167b4cc7fd6c8b
Hugo Heuzard <hugo.heuzard@nomadic-labs.com>
2021-11-23 17:21:29 +0100

    Hacl-js: rename blit_buf_onto_MlBytes into hacl_blit_buf_to_bytes

--- src/lib_hacl_glue/js/src/hacl_stubs.js
+++ src/lib_hacl_glue/js/src/hacl_stubs.js
@@ -13,9 +13,9 @@ function hacl_create_buffer(MlBytes) {
   return buf;
 }
 
-//Provides: blit_buf_onto_MlBytes
+//Provides: hacl_blit_buf_to_bytes
 //Requires: caml_string_unsafe_set
-function blit_buf_onto_MlBytes(buf, MlBytes) {
+function hacl_blit_buf_to_bytes(buf, MlBytes) {
   buf.forEach(function(uint8, index) {
     if(index < MlBytes.l)
       caml_string_unsafe_set(MlBytes, index, uint8)
@@ -24,7 +24,7 @@ function blit_buf_onto_MlBytes(buf, MlBytes) {
 }
 
 //Provides: _1_Lib_RandomBuffer_System_randombytes
-//Requires: blit_buf_onto_MlBytes
+//Requires: hacl_blit_buf_to_bytes
 function _1_Lib_RandomBuffer_System_randombytes(buf) { // eslint-disable-line no-unused-vars
   return ((typeof self !== 'undefined' && (self.crypto || self.msCrypto))
     ? function() { // Browsers
@@ -34,71 +34,71 @@ function _1_Lib_RandomBuffer_System_randombytes(buf) { // eslint-disable-line no
         for (var i = 0; i < n; i += QUOTA) {
           crypto.getRandomValues(result.subarray(i, i + Math.min(n - i, QUOTA)));
         }
-        blit_buf_onto_MlBytes(result, buf);
+        hacl_blit_buf_to_bytes(result, buf);
         return true;
       };
     }
     : function() { // Node
       var result = require("crypto").randomBytes(60);
-      blit_buf_onto_MlBytes(result, buf);
+      hacl_blit_buf_to_bytes(result, buf);
       return true;
     })(buf)
 }
 
 
 //Provides: Hacl_Blake2b_32_blake2b
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_Blake2b_32_blake2b(key, msg, digest_len, digest) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bkey = hacl_create_buffer(key);
   var bmsg = hacl_create_buffer(msg);
   var bret = H.Blake2.blake2b(digest_len, bmsg, bkey);
-  blit_buf_onto_MlBytes(bret[0], digest);
+  hacl_blit_buf_to_bytes(bret[0], digest);
   return 0;
 }
 
 //Provides: Hacl_Hash_SHA2_hash_256
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_Hash_SHA2_hash_256(msg, digest) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bmsg = hacl_create_buffer(msg);
   var bret = H.SHA2.hash_256(bmsg);
-  blit_buf_onto_MlBytes(bret[0], digest);
+  hacl_blit_buf_to_bytes(bret[0], digest);
   return 0;
 }
 
 //Provides: Hacl_Hash_SHA2_hash_512
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_Hash_SHA2_hash_512(msg, digest) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bmsg = hacl_create_buffer(msg);
   var bret = H.SHA2.hash_512(bmsg);
-  blit_buf_onto_MlBytes(bret[0], digest);
+  hacl_blit_buf_to_bytes(bret[0], digest);
   return 0;
 }
 
 //Provides: Hacl_SHA3_sha3_256
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_SHA3_sha3_256(msg, digest) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bmsg = hacl_create_buffer(msg);
   var bret = H.SHA3.hash_256(bmsg);
-  blit_buf_onto_MlBytes(bret[0], digest);
+  hacl_blit_buf_to_bytes(bret[0], digest);
   return 0;
 }
 
 //Provides: Hacl_SHA3_sha3_512
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_SHA3_sha3_512(msg, digest) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bmsg = hacl_create_buffer(msg);
   var bret = H.SHA3.hash_512(bmsg);
-  blit_buf_onto_MlBytes(bret[0], digest);
+  hacl_blit_buf_to_bytes(bret[0], digest);
   return 0;
 }
 
 //Provides: Hacl_Impl_SHA3_keccak
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_Impl_SHA3_keccak(rate, capacity, suffix, msg, digest) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bmsg = hacl_create_buffer(msg);
@@ -106,116 +106,116 @@ function Hacl_Impl_SHA3_keccak(rate, capacity, suffix, msg, digest) { // eslint-
   // since the buffer itself is not passed there is no way to retrive its
   // size in api.js
   var bret = H.SHA3.keccak(rate, capacity, bmsg, suffix, digest.l);
-  blit_buf_onto_MlBytes(bret[0], digest);
+  hacl_blit_buf_to_bytes(bret[0], digest);
   return 0;
 }
 
 //Provides: Hacl_HMAC_compute_sha2_256
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_HMAC_compute_sha2_256 (output, key, msg) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bkey = hacl_create_buffer(key);
   var bmsg = hacl_create_buffer(msg);
   var bret = H.HMAC.sha256(bkey, bmsg);
-  blit_buf_onto_MlBytes(bret[0], output);
+  hacl_blit_buf_to_bytes(bret[0], output);
   return 0;
 }
 
 //Provides: Hacl_HMAC_compute_sha2_512
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_HMAC_compute_sha2_512 (output, key, msg) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bkey = hacl_create_buffer(key);
   var bmsg = hacl_create_buffer(msg);
   var bret = H.HMAC.sha512(bkey, bmsg);
-  blit_buf_onto_MlBytes(bret[0], output);
+  hacl_blit_buf_to_bytes(bret[0], output);
   return 0;
 }
 
 //Provides: Hacl_Curve25519_51_scalarmult
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_Curve25519_51_scalarmult(pk, sk, basepoint) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bsk = hacl_create_buffer(sk);
   var bret = H.Curve25519_51.secret_to_public(bsk);
-  blit_buf_onto_MlBytes(bret[0], pk);
+  hacl_blit_buf_to_bytes(bret[0], pk);
   return 0;
 }
 
 //Provides: Hacl_NaCl_crypto_secretbox_easy
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_NaCl_crypto_secretbox_easy(c, m, n, k) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bm = hacl_create_buffer(m);
   var bn = hacl_create_buffer(n);
   var bk = hacl_create_buffer(k);
   var bret = H.NaCl.secretbox_easy(bm, bn, bk);
-  blit_buf_onto_MlBytes(bret[1], c);
+  hacl_blit_buf_to_bytes(bret[1], c);
   return (bret[0] === 0 ? 1 : 0);
 }
 
 //Provides: Hacl_NaCl_crypto_secretbox_open_easy
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_NaCl_crypto_secretbox_open_easy(m, c, n, k) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bc = hacl_create_buffer(c);
   var bn = hacl_create_buffer(n);
   var bk = hacl_create_buffer(k);
   var bret = H.NaCl.secretbox_open_easy(bc, bn, bk);
-  blit_buf_onto_MlBytes(bret[1], m);
+  hacl_blit_buf_to_bytes(bret[1], m);
   return (bret[0] === 0 ? 1 : 0);
 }
 
 //Provides: Hacl_NaCl_crypto_box_beforenm
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_NaCl_crypto_box_beforenm(k, pk, sk) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bpk = hacl_create_buffer(pk);
   var bsk = hacl_create_buffer(sk);
   var bret = H.NaCl.box_beforenm(bpk, bsk);
-  blit_buf_onto_MlBytes(bret[1], k);
+  hacl_blit_buf_to_bytes(bret[1], k);
   return (bret[0] === 0 ? 1 : 0);
 }
 
 //Provides: Hacl_NaCl_crypto_box_easy_afternm
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_NaCl_crypto_box_easy_afternm(c, m, n, k) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bm = hacl_create_buffer(m);
   var bn = hacl_create_buffer(n);
   var bk = hacl_create_buffer(k);
   var bret = H.NaCl.box_easy_afternm(bm, bn, bk);
-  blit_buf_onto_MlBytes(bret[1], c);
+  hacl_blit_buf_to_bytes(bret[1], c);
   return (bret[0] === 0 ? 1 : 0);
 }
 
 //Provides: Hacl_NaCl_crypto_box_open_easy_afternm
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_NaCl_crypto_box_open_easy_afternm(m, c, n, k) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bc = hacl_create_buffer(c);
   var bn = hacl_create_buffer(n);
   var bk = hacl_create_buffer(k);
   var bret = H.NaCl.box_open_easy_afternm(bc, bn, bk);
-  blit_buf_onto_MlBytes(bret[1], m);
+  hacl_blit_buf_to_bytes(bret[1], m);
   return (bret[0] === 0 ? 1 : 0);
 }
 
 //Provides: Hacl_NaCl_crypto_box_detached_afternm
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_NaCl_crypto_box_detached_afternm(c, tag, m, n, k) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bm = hacl_create_buffer(m);
   var bn = hacl_create_buffer(n);
   var bk = hacl_create_buffer(k);
   var bret = H.NaCl.box_detached_afternm(bm, bn, bk);
-  blit_buf_onto_MlBytes(bret[1], c);
-  blit_buf_onto_MlBytes(bret[2], tag);
+  hacl_blit_buf_to_bytes(bret[1], c);
+  hacl_blit_buf_to_bytes(bret[2], tag);
   return (bret[0] === 0 ? 1 : 0);
 }
 
 //Provides: Hacl_NaCl_crypto_box_open_detached_afternm
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_NaCl_crypto_box_open_detached_afternm(m, c, tag, n, k) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var btag = hacl_create_buffer(tag);
@@ -223,28 +223,28 @@ function Hacl_NaCl_crypto_box_open_detached_afternm(m, c, tag, n, k) { // eslint
   var bn = hacl_create_buffer(n);
   var bk = hacl_create_buffer(k);
   var bret = H.NaCl.box_open_detached_afternm(bc, btag, bn, bk);
-  blit_buf_onto_MlBytes(bret[1], m);
+  hacl_blit_buf_to_bytes(bret[1], m);
   return (bret[0] === 0 ? 1 : 0);
 }
 
 //Provides: Hacl_Ed25519_secret_to_public
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_Ed25519_secret_to_public(out, secret) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bsecret = hacl_create_buffer(secret);
   var bret = H.Ed25519.secret_to_public(bsecret);
-  blit_buf_onto_MlBytes(bret[0], out);
+  hacl_blit_buf_to_bytes(bret[0], out);
   return 0;
 }
 
 //Provides: Hacl_Ed25519_sign
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_Ed25519_sign(signature, sk, msg) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bsk = hacl_create_buffer(sk);
   var bmsg = hacl_create_buffer(msg);
   var bret = H.Ed25519.sign(bsk, bmsg);
-  blit_buf_onto_MlBytes(bret[0], signature);
+  hacl_blit_buf_to_bytes(bret[0], signature);
   return 0;
 }
 
@@ -260,14 +260,14 @@ function Hacl_Ed25519_verify(pk, msg, signature) { // eslint-disable-line no-unu
 }
 
 //Provides: Hacl_P256_ecdsa_sign_p256_without_hash
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_P256_ecdsa_sign_p256_without_hash (privkey, m, k, result) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bm = hacl_create_buffer(m);
   var bprivkey = hacl_create_buffer(privkey);
   var bk = hacl_create_buffer(k);
   var bret = H.P256.ecdsa_sign_without_hash(bm, bprivkey, bk);
-  blit_buf_onto_MlBytes(bret[1], result);
+  hacl_blit_buf_to_bytes(bret[1], result);
   return (bret[0] ? 1 : 0);
 }
 
@@ -302,52 +302,52 @@ function Hacl_P256_verify_q (pk) { // eslint-disable-line no-unused-vars
 }
 
 //Provides: Hacl_P256_ecp256dh_i
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_P256_ecp256dh_i (pk, sk) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bsk = hacl_create_buffer(sk);
   var bret = H.P256.dh_initiator(bsk);
-  blit_buf_onto_MlBytes(bret[1], pk);
+  hacl_blit_buf_to_bytes(bret[1], pk);
   return (bret[0] ? 1 : 0);
 }
 
 //Provides: Hacl_P256_compression_compressed_form
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_P256_compression_compressed_form (pk, out) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bpk = hacl_create_buffer(pk);
   var bret = H.P256.compression_compressed_form(bpk);
-  blit_buf_onto_MlBytes(bret[0], out);
+  hacl_blit_buf_to_bytes(bret[0], out);
   return 0;
 }
 
 //Provides: Hacl_P256_compression_not_compressed_form
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_P256_compression_not_compressed_form (pk, out) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bpk = hacl_create_buffer(pk);
   var bret = H.P256.compression_not_compressed_form(bpk);
-  blit_buf_onto_MlBytes(bret[0], out);
+  hacl_blit_buf_to_bytes(bret[0], out);
   return 0;
 }
 
 //Provides: Hacl_P256_decompression_compressed_form
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_P256_decompression_compressed_form (pk, out) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bpk = hacl_create_buffer(pk);
   var bret = H.P256.decompression_compressed_form(bpk);
-  blit_buf_onto_MlBytes(bret[1], out);
+  hacl_blit_buf_to_bytes(bret[1], out);
   return (bret[0] ? 1 : 0);
 }
 
 //Provides: Hacl_P256_decompression_not_compressed_form
-//Requires: hacl_create_buffer, blit_buf_onto_MlBytes
+//Requires: hacl_create_buffer, hacl_blit_buf_to_bytes
 function Hacl_P256_decompression_not_compressed_form (pk, out) { // eslint-disable-line no-unused-vars
   var H = joo_global_object._HACL;
   var bpk = hacl_create_buffer(pk);
   var bret = H.P256.decompression_not_compressed_form(bpk);
-  blit_buf_onto_MlBytes(bret[1], out);
+  hacl_blit_buf_to_bytes(bret[1], out);
   return (bret[0] ? 1 : 0);
 }
 
