e9b381ab372bd1c56762508b7b7eab43767ee733
Raphaël Proust <code@bnwr.net>
2021-11-29 15:11:11 +0100

    Add test for json-roundtripping
    Encodes-decodes json values in both json and binary, and checks that the
    result is stable.

--- test/json_roundtrip_in_binary.ml
+++ test/json_roundtrip_in_binary.ml
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2020-2021 Nomadic Labs, <contact@nomadic-labs.com>          *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -23,24 +24,73 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let () =
-  Random.self_init () ;
-  Alcotest.run
-    "tezos-data-encoding"
-    [
-      ("success", Success.tests);
-      ("invalid_encoding", Invalid_encoding.tests);
-      ("read_failure", Read_failure.tests);
-      ("write_failure", Write_failure.tests);
-      ("randomized", Randomized.tests);
-      ("versioned", Versioned.tests);
-      ("registration", Registrationed.tests);
-      ("mu", Mu.tests);
-      ("slice", Slice_test.tests);
-      ("conv_with_guard", Guarded_conv.tests);
-      ("with_decoding_guard", Guarded_decode.tests);
-      ("int31_int32", Int31_int32.tests);
-      ("inline_phantom", Reference_check.Inline_phantom.tests);
-      ("mu_phantom", Reference_check.Mu_phantom.tests);
-      ("json_encoding", Json_roundtrip_in_binary.tests);
-    ]
+let ground_values =
+  [
+    `O [];
+    `Bool true;
+    `Bool false;
+    `Float 0.0;
+    `Float 0.1;
+    `Float Float.epsilon;
+    `Float 1.0;
+    `A [];
+    `Null;
+    `String "";
+    `String "0";
+    `String "\"";
+    `String "'";
+    `String ";";
+    `String "0";
+    `String "00";
+  ]
+
+let values_1 =
+  (* [concat_map] is only available since OCaml.4.10, for tests we dont care
+     about perf so we use [concat]+[map] *)
+  List.concat
+  @@ List.map
+       (fun (a, b) ->
+         [
+           `O [("x", a)];
+           `O [("yy", a); ("x", `Null); ("z", `A []); ("t", b)];
+           `A [a];
+           `A [a; b];
+           `A [a; `Float 0.33; `Float 0.33; b; `Float 0.33];
+         ])
+       (List.map2 (fun x y -> (x, y)) ground_values (List.rev ground_values))
+
+let all_values = ground_values @ values_1
+
+(* Setting up the actual tests *)
+let test_json (j : Data_encoding.Json.t) =
+  let json =
+    try Data_encoding.Json.construct Data_encoding.Json.encoding j
+    with exc -> failwith ("Cannot construct: " ^ Printexc.to_string exc)
+  in
+  let jj =
+    try Data_encoding.Json.destruct Data_encoding.Json.encoding json
+    with Data_encoding.Json.Cannot_destruct (_, _) ->
+      failwith "Cannot destruct"
+  in
+  assert (j = jj)
+
+let test_jsons () = List.iter test_json all_values
+
+let test_binary (j : Data_encoding.Json.t) =
+  let s =
+    try Data_encoding.Binary.to_string_exn Data_encoding.Json.encoding j
+    with exc -> failwith ("Cannot construct: " ^ Printexc.to_string exc)
+  in
+  let jj =
+    try Data_encoding.Binary.of_string_exn Data_encoding.Json.encoding s
+    with _ -> failwith "Cannot destruct"
+  in
+  assert (j = jj)
+
+let test_binaries () = List.iter test_binary all_values
+
+let tests =
+  [
+    ("json_roundtrip_in_json", `Quick, test_jsons);
+    ("json_roundtrip_in_binary", `Quick, test_binaries);
+  ]
