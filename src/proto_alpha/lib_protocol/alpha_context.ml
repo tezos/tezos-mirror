c18e46f3f1254a06e61dd7fde7b95eac4dc08c6d
Nicolas Ayache <nicolas.ayache@nomadic-labs.com>
2021-11-30 10:41:37 +0100

    Proto/Micheslon: add Nomadic Labs copyright.

--- src/proto_alpha/lib_protocol/alpha_context.ml
+++ src/proto_alpha/lib_protocol/alpha_context.ml
@@ -2,7 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
-(* Copyright (c) 2019-2020 Nomadic Labs <contact@nomadic-labs.com>           *)
+(* Copyright (c) 2019-2022 Nomadic Labs <contact@nomadic-labs.com>           *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
--- src/proto_alpha/lib_protocol/script_comparable.ml
+++ src/proto_alpha/lib_protocol/script_comparable.ml
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2021-2022 Nomadic Labs <contact@nomadic-labs.com>           *)
 (* Copyright (c) 2020 Metastate AG <hello@metastate.dev>                     *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
