c5b8d2c83d184ee82045adc156ec02b3a123693a
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2021-12-10 19:29:10 +0100

    proto_alpha/lib_protocol/test: move out integration/gas

--- src/proto_alpha/lib_protocol/test/integration/gas/test_gas_levels.ml
+++ src/proto_alpha/lib_protocol/test/integration/gas/test_gas_levels.ml
@@ -26,7 +26,9 @@
 (** Testing
     -------
     Component:  Protocol (Gas levels)
-    Invocation: dune exec src/proto_alpha/lib_protocol/test/main.exe -- test "^gas levels$"
+    Invocation: dune exec \
+                src/proto_alpha/lib_protocol/test/integration/gas/main.exe \
+                -- test "^gas levels$"
     Subject:    On gas consumption and exhaustion.
 *)
 
