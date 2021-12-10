9ade1e51508586a19c40fa36c1b58bf7156e5c79
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2021-12-10 19:15:00 +0100

    proto_alpha/lib_protocol/test: move out transfer and context helpers

---
+++ src/proto_alpha/lib_protocol/test/helpers/transfers.ml
@@ -0,0 +1,65 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Protocol
+open Alpha_context
+open Test_tez
+
+let transfer_and_check_balances ?(with_burn = false) ~loc b ?(fee = Tez.zero)
+    ?expect_failure src dst amount =
+  fee +? amount >>?= fun amount_fee ->
+  Context.Contract.balance (I b) src >>=? fun bal_src ->
+  Context.Contract.balance (I b) dst >>=? fun bal_dst ->
+  Op.transaction
+    ~gas_limit:(Alpha_context.Gas.Arith.integral_of_int_exn 3000)
+    (I b)
+    ~fee
+    src
+    dst
+    amount
+  >>=? fun op ->
+  Incremental.add_operation ?expect_failure b op >>=? fun b ->
+  Context.get_constants (I b)
+  >>=? fun {parametric = {origination_size; cost_per_byte; _}; _} ->
+  cost_per_byte *? Int64.of_int origination_size >>?= fun origination_burn ->
+  let amount_fee_maybe_burn =
+    if with_burn then
+      match Tez.(amount_fee +? origination_burn) with
+      | Ok r -> r
+      | Error _ -> assert false
+    else amount_fee
+  in
+  Assert.balance_was_debited ~loc (I b) src bal_src amount_fee_maybe_burn
+  >>=? fun () ->
+  Assert.balance_was_credited ~loc (I b) dst bal_dst amount >|=? fun () ->
+  (b, op)
+
+let n_transactions n b ?fee source dest amount =
+  List.fold_left_es
+    (fun b _ ->
+      transfer_and_check_balances ~loc:__LOC__ b ?fee source dest amount
+      >|=? fun (b, _) -> b)
+    b
+    (1 -- n)
