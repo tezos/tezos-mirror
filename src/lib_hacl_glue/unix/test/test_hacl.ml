622f898fe4ec896369a29bcdff668f6862ad0cc7
Hugo Heuzard <hugo.heuzard@nomadic-labs.com>
2021-12-12 10:47:56 +0100

    Hacl: update comments

--- src/lib_hacl_glue/unix/test/test_hacl.ml
+++ src/lib_hacl_glue/unix/test/test_hacl.ml
@@ -26,7 +26,7 @@
 (** Testing
     -------
     Component:    Crypto
-    Invocation:   dune build @src/lib_crypto/runtest
+    Invocation:   dune build @src/lib_hacl_glue/runtest
     Subject:      Checking all of the HACL* primitives used in lib_crypto:
                   hashing, HMAC, NaCl, Ed25519, and P-256.
 *)
