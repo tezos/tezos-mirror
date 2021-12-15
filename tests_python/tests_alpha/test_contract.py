9310b15dbf9c619cb53d06d2eff480dfa2cc12c9
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2021-12-15 11:21:34 +0100

    Tests: adapt error message accordingly

--- tests_python/tests_alpha/test_contract.py
+++ tests_python/tests_alpha/test_contract.py
@@ -2123,9 +2123,10 @@ class TestContractTypeChecking:
         client.typecheck_data(f'{address_opt}', 'address')
         client.typecheck_data(f'{address_opt_a}', 'address')
 
+        unexpected_default_error = "unexpected_default_entrypoint"
         unexpected_annotation_error = "unexpected annotation."
 
-        with utils.assert_run_failure(unexpected_annotation_error):
+        with utils.assert_run_failure(unexpected_default_error):
             client.typecheck_data(f'"{address}%default"', 'address')
 
         # 64656661756c74 is "default" in hexa
