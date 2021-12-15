7d5161f1fd49c768036fe20ed0aeba8adaa75597
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-12-15 17:40:02 +0100

    Lib_requester: migrate from qcheck to qcheck2

--- src/lib_requester/test/test_fuzzing_requester.ml
+++ src/lib_requester/test/test_fuzzing_requester.ml
@@ -31,18 +31,18 @@
 *)
 
 include Shared
-open Lib_test.Qcheck_helpers
+open Lib_test.Qcheck2_helpers
+open QCheck2
 
 (** Completely random key generator. Mitigated by also picking keys
  *  in the requester's domain, see [requester_and_keys_gen] below. *)
-let key_arb = QCheck.string
+let key_gen = Gen.small_string ?gen:None
 
-let value_arb = QCheck.int
+let value_gen = Gen.int
 
 (** Creates a requester filled with arbitrary content.
  *  The requester's keys are also returned. *)
-let domain_and_requester_arb : (string list * Test_Requester.t) QCheck.arbitrary
-    =
+let domain_and_requester_gen : (string list * Test_Requester.t) Gen.t =
   let create sets =
     let requester = init_full_requester () in
     let set_one (key, value) =
@@ -51,7 +51,7 @@ let domain_and_requester_arb : (string list * Test_Requester.t) QCheck.arbitrary
     Lwt_main.run @@ Lwt_list.iter_s set_one sets ;
     (List.map fst sets, requester)
   in
-  QCheck.(map create (list (pair key_arb value_arb)))
+  Gen.(map create (list (pair key_gen value_gen)))
 
 (** A generator for a requester and for two keys. We generate them
  *  all simultaneously, to augment chances to have keys that are in
@@ -62,24 +62,24 @@ let domain_and_requester_arb : (string list * Test_Requester.t) QCheck.arbitrary
  *  We could have generated 1 key or 3 keys, or whatever number of keys; but
  *  in practice, in tests below; we need at most two keys.
  *)
-let requester_and_keys_gen : (Test_Requester.t * string * string) QCheck.Gen.t =
-  let open QCheck.Gen in
-  QCheck.gen domain_and_requester_arb >>= fun (domain, requester) ->
+let requester_and_keys_gen : (Test_Requester.t * string * string) Gen.t =
+  let open Gen in
+  let* (domain, requester) = domain_and_requester_gen in
   let key_gen =
     let in_domain_gen = if domain = [] then [] else [oneofl domain] in
     (* Either a random key or a key in the domain *)
-    oneof (QCheck.gen key_arb :: in_domain_gen)
+    oneof (key_gen :: in_domain_gen)
   in
-  pair key_gen key_gen >>= fun (key1, key2) -> pure (requester, key1, key2)
+  let* (key1, key2) = pair key_gen key_gen in
+  pure (requester, key1, key2)
 
-let requester_and_keys_arb =
-  let print = QCheck.Print.(triple (Fun.const "requester") string string) in
-  QCheck.make ~print requester_and_keys_gen
+let print = Print.(triple (Fun.const "requester") string string)
 
 let test_read_read_opt =
-  QCheck.Test.make
+  Test.make
     ~name:"Result.is_ok (read t key) = Option.is_some (read_opt t key)"
-    requester_and_keys_arb
+    ~print
+    requester_and_keys_gen
   @@ fun (t, key, _) ->
   let found_by_read =
     Lwt_main.run @@ (Test_Requester.read t key >|= Result.is_ok)
@@ -90,9 +90,10 @@ let test_read_read_opt =
   qcheck_eq ~pp:Format.pp_print_bool found_by_read found_by_read_opt
 
 let test_read_opt_known =
-  QCheck.Test.make
+  Test.make
     ~name:"known t key = Option.is_some (read_opt t key)"
-    requester_and_keys_arb
+    ~print
+    requester_and_keys_gen
   @@ fun (t, key, _) ->
   let known = Lwt_main.run @@ Test_Requester.known t key in
   let found_by_read_opt =
@@ -101,12 +102,13 @@ let test_read_opt_known =
   qcheck_eq ~pp:Format.pp_print_bool known found_by_read_opt
 
 let test_inject_read_opt =
-  QCheck.Test.make
+  Test.make
     ~name:"read_opt (inject t key value) = (Some value)"
-    QCheck.(pair requester_and_keys_arb value_arb)
+    ~print:Print.(pair print int)
+    Gen.(pair requester_and_keys_gen value_gen)
   @@ fun ((t, key, _), value) ->
   let injected = Lwt_main.run @@ Test_Requester.inject t key value in
-  QCheck.assume injected ;
+  assume injected ;
   let read = Lwt_main.run @@ Test_Requester.read_opt t key in
   qcheck_eq'
     ~pp:(Format.pp_print_option Format.pp_print_int)
@@ -115,11 +117,12 @@ let test_inject_read_opt =
     ()
 
 let test_inject_read_opt_other =
-  QCheck.Test.make
+  Test.make
     ~name:"key <> key' ==> read_opt (inject t key' value) key = read_opt t key"
-    QCheck.(pair requester_and_keys_arb value_arb)
+    ~print:Print.(pair print int)
+    Gen.(pair requester_and_keys_gen value_gen)
   @@ fun ((t, key, key'), value) ->
-  QCheck.assume (key <> key') ;
+  assume (key <> key') ;
   let read_opt_before = Lwt_main.run @@ Test_Requester.read_opt t key in
   let _ = Lwt_main.run @@ Test_Requester.inject t key' value in
   let read_opt_after = Lwt_main.run @@ Test_Requester.read_opt t key in
@@ -133,9 +136,10 @@ let leq_opt opt1 opt2 =
   match (opt1, opt2) with (None, _) | (Some _, Some _) -> true | _ -> false
 
 let test_inject_growth =
-  QCheck.Test.make
+  Test.make
     ~name:"read_opt t key <= read_opt (inject t key' value) key"
-    QCheck.(pair requester_and_keys_arb value_arb)
+    ~print:Print.(pair print int)
+    Gen.(pair requester_and_keys_gen value_gen)
   @@ fun ((t, key, key'), value) ->
   let read_opt_before = Lwt_main.run @@ Test_Requester.read_opt t key in
   let _ = Lwt_main.run @@ Test_Requester.inject t key' value in
@@ -147,9 +151,10 @@ let test_inject_growth =
     ()
 
 let test_inject_memory_table_length =
-  QCheck.Test.make
+  Test.make
     ~name:"memory_table_length t <= memory_table_length (inject t key value)"
-    QCheck.(pair requester_and_keys_arb value_arb)
+    ~print:Print.(pair print int)
+    Gen.(pair requester_and_keys_gen value_gen)
   @@ fun ((t, key, _), value) ->
   let length_before = Test_Requester.memory_table_length t in
   let _ = Lwt_main.run @@ Test_Requester.inject t key value in
@@ -161,9 +166,10 @@ let test_inject_memory_table_length =
     ()
 
 let test_clear_shrink =
-  QCheck.Test.make
+  Test.make
     ~name:"read_opt (clear_or_cancel t key') key <= read_opt t key"
-    requester_and_keys_arb
+    ~print
+    requester_and_keys_gen
   @@ fun (t, key, key') ->
   let read_opt_before = Lwt_main.run @@ Test_Requester.read_opt t key in
   Test_Requester.clear_or_cancel t key' ;
@@ -175,9 +181,10 @@ let test_clear_shrink =
     ()
 
 let test_clear_memory_table_length =
-  QCheck.Test.make
+  Test.make
     ~name:"memory_length (clear_or_cancel t key') key <= memory_length t"
-    requester_and_keys_arb
+    ~print
+    requester_and_keys_gen
   @@ fun (t, key, _) ->
   let length_before = Test_Requester.memory_table_length t in
   Test_Requester.clear_or_cancel t key ;
