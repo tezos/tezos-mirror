OKEAMAH/Git

Tezt,SCORU: Add a failing test for rollup origination
Signed-off-by: Yann Regis-Gianas <yann@nomadic-labs.com>

tezt/lib_tezos/client.ml
@@ -1022,6 +1022,40 @@ let spawn_sign_block client block_hex ~delegate =
 let sign_block client block_hex ~delegate =
   spawn_sign_block client block_hex ~delegate |> Process.check_and_read_stdout
 
+let spawn_originate_rollup ?(wait = "none") ?burn_cap ~src ~kind ~boot_sector
+    client =
+  spawn_command
+    client
+    (["--wait"; wait]
+    @ [
+        "originate";
+        "rollup";
+        "from";
+        src;
+        "of";
+        "kind";
+        kind;
+        "booting";
+        "with";
+        boot_sector;
+      ]
+    @ Option.fold
+        ~none:[]
+        ~some:(fun burn_cap -> ["--burn-cap"; Tez.to_string burn_cap])
+        burn_cap)
+
+let parse_rollup_address_in_receipt output =
+  match output =~* rex "Address: (.*)" with
+  | None -> Test.fail "Cannot extract rollup address from receipt."
+  | Some x -> return x
+
+let originate_rollup ?wait ?burn_cap ~src ~kind ~boot_sector client =
+  let process =
+    spawn_originate_rollup ?wait ?burn_cap ~src ~kind ~boot_sector client
+  in
+  let* output = Process.check_and_read_stdout process in
+  parse_rollup_address_in_receipt output
+
 let init ?path ?admin_path ?name ?color ?base_dir ?endpoint ?media_type () =
   let client =
     create ?path ?admin_path ?name ?color ?base_dir ?endpoint ?media_type ()

tezt/tests/sc_rollup.ml
@@ -23,4 +23,55 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let register ~protocols:(_ : Protocol.t list) = ()
+(*
+
+   Helpers
+   =======
+
+*)
+let test ~__FILE__ ~output_file title =
+  Protocol.register_regression_test
+    ~output_file
+    ~__FILE__
+    ~title
+    ~tags:["sc_rollup"]
+
+let setup f ~protocol =
+  let* (node, client) = Client.init_with_protocol `Client ~protocol () in
+  let bootstrap1_key = Constant.bootstrap1.public_key_hash in
+  f node client bootstrap1_key
+
+(*
+
+   Tests
+   =====
+
+*)
+
+(* Originate a new SCORU of the arithmetic kind
+   --------------------------------------------
+
+   - Rollup addresses are fully determined by operation hashes and origination nonce.
+
+*)
+let test_origination =
+  let output_file = "sc_rollup_origination" in
+  test
+    ~__FILE__
+    ~output_file
+    "origination of a SCORU executes without error"
+    (fun protocol ->
+      setup ~protocol @@ fun _node client bootstrap1_key ->
+      let* rollup_address =
+        Client.originate_rollup
+          ~burn_cap:Tez.(of_int 9999999)
+          ~src:bootstrap1_key
+          ~kind:"arith"
+          ~boot_sector:""
+          client
+      in
+      let* () = Client.bake_for client in
+      Regression.capture rollup_address ;
+      return ())
+
+let register ~protocols = test_origination ~protocols

tezos/tezos-mirror

tezt/lib_tezos/client.ml
@@ -1083,6 +1083,41 @@ let show_voting_period ?endpoint client =
         "tezos-client show voting period did not print the current period"
   | Some period -> return period
 
+let spawn_originate_sc_rollup ?(wait = "none") ?burn_cap ~src ~kind ~boot_sector
+    client =
+  spawn_command
+    client
+    (["--wait"; wait]
+    @ [
+        "originate";
+        "sc";
+        "rollup";
+        "from";
+        src;
+        "of";
+        "kind";
+        kind;
+        "booting";
+        "with";
+        boot_sector;
+      ]
+    @ Option.fold
+        ~none:[]
+        ~some:(fun burn_cap -> ["--burn-cap"; Tez.to_string burn_cap])
+        burn_cap)
+
+let parse_rollup_address_in_receipt output =
+  match output =~* rex "Address: (.*)" with
+  | None -> Test.fail "Cannot extract rollup address from receipt."
+  | Some x -> return x
+
+let originate_sc_rollup ?wait ?burn_cap ~src ~kind ~boot_sector client =
+  let process =
+    spawn_originate_sc_rollup ?wait ?burn_cap ~src ~kind ~boot_sector client
+  in
+  let* output = Process.check_and_read_stdout process in
+  parse_rollup_address_in_receipt output
+
 let init ?path ?admin_path ?name ?color ?base_dir ?endpoint ?media_type () =
   let client =
     create ?path ?admin_path ?name ?color ?base_dir ?endpoint ?media_type ()

tezt/tests/sc_rollup.ml
@@ -23,4 +23,55 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let register ~protocols:(_ : Protocol.t list) = ()
+(*
+
+   Helpers
+   =======
+
+*)
+let test ~__FILE__ ~output_file title =
+  Protocol.register_regression_test
+    ~output_file
+    ~__FILE__
+    ~title
+    ~tags:["sc_rollup"]
+
+let setup f ~protocol =
+  let* (node, client) = Client.init_with_protocol `Client ~protocol () in
+  let bootstrap1_key = Constant.bootstrap1.public_key_hash in
+  f node client bootstrap1_key
+
+(*
+
+   Tests
+   =====
+
+*)
+
+(* Originate a new SCORU of the arithmetic kind
+   --------------------------------------------
+
+   - Rollup addresses are fully determined by operation hashes and origination nonce.
+
+*)
+let test_origination =
+  let output_file = "sc_rollup_origination" in
+  test
+    ~__FILE__
+    ~output_file
+    "origination of a SCORU executes without error"
+    (fun protocol ->
+      setup ~protocol @@ fun _node client bootstrap1_key ->
+      let* rollup_address =
+        Client.originate_sc_rollup
+          ~burn_cap:Tez.(of_int 9999999)
+          ~src:bootstrap1_key
+          ~kind:"arith"
+          ~boot_sector:""
+          client
+      in
+      let* () = Client.bake_for client in
+      Regression.capture rollup_address ;
+      return ())
+
+let register ~protocols = test_origination ~protocols