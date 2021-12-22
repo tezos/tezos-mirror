OKEAMAH/Git

Proto,SCORU: Expose sc rollup origination in CLI
Signed-off-by: Yann Regis-Gianas <yann@nomadic-labs.com>

src/proto_alpha/lib_client/client_proto_context.ml
@@ -745,3 +745,36 @@ let contract_cache_size cctxt ~chain ~block =
 let contract_cache_size_limit cctxt ~chain ~block =
   let cb = (chain, block) in
   Alpha_services.Cache.contract_cache_size_limit cctxt cb
+
+let sc_rollup_originate (cctxt : #full) ~chain ~block ?confirmations ?dry_run
+    ?verbose_signing ?simulation ?fee ?gas_limit ?storage_limit ?counter ~source
+    ~pvm ~boot_sector ~src_pk ~src_sk ~fee_parameter () =
+  let op =
+    Annotated_manager_operation.Single_manager
+      (Injection.prepare_manager_operation
+         ~fee:(Limit.of_option fee)
+         ~gas_limit:(Limit.of_option gas_limit)
+         ~storage_limit:(Limit.of_option storage_limit)
+         (Sc_rollup_originate {pvm; boot_sector}))
+  in
+  Injection.inject_manager_operation
+    cctxt
+    ~chain
+    ~block
+    ?confirmations
+    ?dry_run
+    ?verbose_signing
+    ?simulation
+    ?counter
+    ~source
+    ~fee:(Limit.of_option fee)
+    ~storage_limit:(Limit.of_option storage_limit)
+    ~gas_limit:(Limit.of_option gas_limit)
+    ~src_pk
+    ~src_sk
+    ~fee_parameter
+    op
+  >>=? fun (oph, op, result) ->
+  match Apply_results.pack_contents_list op result with
+  | Apply_results.Single_and_result ((Manager_operation _ as op), result) ->
+      return (oph, op, result)

src/proto_alpha/lib_client/client_proto_context.mli
@@ -384,3 +384,29 @@ val contract_cache_size_limit :
   chain:Shell_services.chain ->
   block:Shell_services.block ->
   int tzresult Lwt.t
+
+val sc_rollup_originate :
+  #Protocol_client_context.full ->
+  chain:Chain_services.chain ->
+  block:Block_services.block ->
+  ?confirmations:int ->
+  ?dry_run:bool ->
+  ?verbose_signing:bool ->
+  ?simulation:bool ->
+  ?fee:Tez.t ->
+  ?gas_limit:Gas.Arith.integral ->
+  ?storage_limit:counter ->
+  ?counter:counter ->
+  source:public_key_hash ->
+  pvm:Sc_rollup.PVM.t ->
+  boot_sector:Sc_rollup.PVM.boot_sector ->
+  src_pk:public_key ->
+  src_sk:Client_keys.sk_uri ->
+  fee_parameter:Injection.fee_parameter ->
+  unit ->
+  ( Operation_hash.t
+    * Kind.sc_rollup_originate Kind.manager contents
+    * Kind.sc_rollup_originate Kind.manager Apply_results.contents_result,
+    tztrace )
+  result
+  Lwt.t

tezos/tezos-mirror

src/proto_alpha/lib_client/client_proto_context.ml
@@ -780,3 +780,36 @@ let originate_tx_rollup (cctxt : #full) ~chain ~block ?confirmations ?dry_run
   match Apply_results.pack_contents_list op result with
   | Apply_results.Single_and_result ((Manager_operation _ as op), result) ->
       return (oph, op, result)
+
+let sc_rollup_originate (cctxt : #full) ~chain ~block ?confirmations ?dry_run
+    ?verbose_signing ?simulation ?fee ?gas_limit ?storage_limit ?counter ~source
+    ~kind ~boot_sector ~src_pk ~src_sk ~fee_parameter () =
+  let op =
+    Annotated_manager_operation.Single_manager
+      (Injection.prepare_manager_operation
+         ~fee:(Limit.of_option fee)
+         ~gas_limit:(Limit.of_option gas_limit)
+         ~storage_limit:(Limit.of_option storage_limit)
+         (Sc_rollup_originate {kind; boot_sector}))
+  in
+  Injection.inject_manager_operation
+    cctxt
+    ~chain
+    ~block
+    ?confirmations
+    ?dry_run
+    ?verbose_signing
+    ?simulation
+    ?counter
+    ~source
+    ~fee:(Limit.of_option fee)
+    ~storage_limit:(Limit.of_option storage_limit)
+    ~gas_limit:(Limit.of_option gas_limit)
+    ~src_pk
+    ~src_sk
+    ~fee_parameter
+    op
+  >>=? fun (oph, op, result) ->
+  match Apply_results.pack_contents_list op result with
+  | Apply_results.Single_and_result ((Manager_operation _ as op), result) ->
+      return (oph, op, result)

src/proto_alpha/lib_client/client_proto_context.mli
@@ -408,3 +408,29 @@ val originate_tx_rollup :
   * Kind.tx_rollup_origination Kind.manager Apply_results.contents_result)
   tzresult
   Lwt.t
+
+val sc_rollup_originate :
+  #Protocol_client_context.full ->
+  chain:Chain_services.chain ->
+  block:Block_services.block ->
+  ?confirmations:int ->
+  ?dry_run:bool ->
+  ?verbose_signing:bool ->
+  ?simulation:bool ->
+  ?fee:Tez.t ->
+  ?gas_limit:Gas.Arith.integral ->
+  ?storage_limit:counter ->
+  ?counter:counter ->
+  source:public_key_hash ->
+  kind:Sc_rollup.Kind.t ->
+  boot_sector:Sc_rollup.PVM.boot_sector ->
+  src_pk:public_key ->
+  src_sk:Client_keys.sk_uri ->
+  fee_parameter:Injection.fee_parameter ->
+  unit ->
+  ( Operation_hash.t
+    * Kind.sc_rollup_originate Kind.manager contents
+    * Kind.sc_rollup_originate Kind.manager Apply_results.contents_result,
+    tztrace )
+  result
+  Lwt.t