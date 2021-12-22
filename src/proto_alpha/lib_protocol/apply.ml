OKEAMAH/Git

Proto,SCORU: Expose smart contract rollup origination as L1 operation
Signed-off-by: Yann Regis-Gianas <yann@nomadic-labs.com>

src/proto_alpha/lib_protocol/apply.ml
@@ -1092,6 +1092,15 @@ let apply_manager_operation_content :
                   consumed_gas = Gas.consumed ~since:before_operation ~until:ctxt;
                 },
               [] ))
+  | Sc_rollup_originate {pvm; boot_sector} ->
+      Sc_rollup.originate ctxt ~pvm ~boot_sector
+      >>=? fun (ctxt, {address; size}) ->
+      let consumed_gas = Gas.consumed ~since:before_operation ~until:ctxt in
+      let result =
+        Sc_rollup_originate_result
+          {address; consumed_gas; size; balance_updates = []}
+      in
+      return (ctxt, result, [])
 
 type success_or_failure = Success of context | Failure
 
@@ -1292,6 +1301,16 @@ let burn_storage_fees :
               global_address = payload.global_address;
             } )
   | Set_deposits_limit_result _ -> return (ctxt, storage_limit, smopr)
+  | Sc_rollup_originate_result payload ->
+      let payer = `Contract payer in
+      Fees.burn_sc_rollup_origination_fees
+        ctxt
+        ~storage_limit
+        ~payer
+        payload.size
+      >>=? fun (ctxt, storage_limit, balance_updates) ->
+      let result = Sc_rollup_originate_result {payload with balance_updates} in
+      return (ctxt, storage_limit, result)
 
 let apply_manager_contents (type kind) ctxt mode chain_id
     ~gas_consumed_in_precheck (op : kind Kind.manager contents) :

src/proto_alpha/lib_protocol/operation_repr.ml
@@ -71,6 +71,8 @@ module Kind = struct
 
   type register_global_constant = Register_global_constant_kind
 
+  type sc_rollup_originate = Sc_rollup_originate_kind
+
   type 'a manager =
     | Reveal_manager_kind : reveal manager
     | Transaction_manager_kind : transaction manager
@@ -78,6 +80,7 @@ module Kind = struct
     | Delegation_manager_kind : delegation manager
     | Register_global_constant_manager_kind : register_global_constant manager
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
+    | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
 end
 
 type 'a consensus_operation_type =
@@ -255,6 +258,11 @@ and _ manager_operation =
   | Set_deposits_limit :
       Tez_repr.t option
       -> Kind.set_deposits_limit manager_operation
+  | Sc_rollup_originate : {
+      pvm : Sc_rollup_repr.PVM.t;
+      boot_sector : Sc_rollup_repr.PVM.boot_sector;
+    }
+      -> Kind.sc_rollup_originate manager_operation
 
 and counter = Z.t
 
@@ -266,6 +274,7 @@ let manager_kind : type kind. kind manager_operation -> kind Kind.manager =
   | Delegation _ -> Kind.Delegation_manager_kind
   | Register_global_constant _ -> Kind.Register_global_constant_manager_kind
   | Set_deposits_limit _ -> Kind.Set_deposits_limit_manager_kind
+  | Sc_rollup_originate _ -> Kind.Sc_rollup_originate_manager_kind
 
 type 'kind internal_operation = {
   source : Contract_repr.contract;
@@ -493,6 +502,28 @@ module Encoding = struct
           inj = (fun key -> Set_deposits_limit key);
         }
 
+    let sc_rollup_operation_tag_offset = 200
+
+    let[@coq_axiom_with_reason "gadt"] sc_rollup_originate_case =
+      MCase
+        {
+          tag = sc_rollup_operation_tag_offset + 0;
+          name = "sc_rollup_originate";
+          encoding =
+            obj2 (req "kind" Sc_rollups.encoding) (req "boot_sector" bytes);
+          select =
+            (function
+            | Manager (Sc_rollup_originate _ as op) -> Some op | _ -> None);
+          proj =
+            (function
+            | Sc_rollup_originate {pvm; boot_sector} ->
+                (Sc_rollups.kind_of pvm, boot_sector));
+          inj =
+            (fun (kind, boot_sector) ->
+              let pvm = Sc_rollups.of_kind kind in
+              Sc_rollup_originate {pvm; boot_sector});
+        }
+
     let encoding =
       let make (MCase {tag; name; encoding; select; proj; inj}) =
         case
@@ -512,6 +543,7 @@ module Encoding = struct
           make delegation_case;
           make register_global_constant_case;
           make set_deposits_limit_case;
+          make sc_rollup_originate_case;
         ]
   end
 
@@ -810,6 +842,9 @@ module Encoding = struct
   let set_deposits_limit_case =
     make_manager_case 112 Manager_operations.set_deposits_limit_case
 
+  let sc_rollup_originate_case =
+    make_manager_case 113 Manager_operations.sc_rollup_originate_case
+
   let contents_encoding =
     let make (Case {tag; name; encoding; select; proj; inj}) =
       case
@@ -838,6 +873,7 @@ module Encoding = struct
            make set_deposits_limit_case;
            make failing_noop_case;
            make register_global_constant_case;
+           make sc_rollup_originate_case;
          ]
 
   let contents_list_encoding =
@@ -1037,6 +1073,8 @@ let equal_manager_operation_kind :
   | (Register_global_constant _, _) -> None
   | (Set_deposits_limit _, Set_deposits_limit _) -> Some Eq
   | (Set_deposits_limit _, _) -> None
+  | (Sc_rollup_originate _, Sc_rollup_originate _) -> Some Eq
+  | (Sc_rollup_originate _, _) -> None
 
 let equal_contents_kind : type a b. a contents -> b contents -> (a, b) eq option
     =
@@ -1128,6 +1166,7 @@ let internal_manager_operation_size (type a) (op : a manager_operation) =
         +! option_size
              (fun _ -> Contract_repr.public_key_hash_in_memory_size)
              pkh_opt )
+  | Sc_rollup_originate _ -> (Nodes.zero, h2w)
   | Reveal _ ->
       (* Reveals can't occur as internal operations *)
       assert false

src/proto_alpha/lib_protocol/operation_repr.mli
@@ -42,6 +42,7 @@
       - origination
       - delegation
       - set deposits limitation
+      - originate smart contract rollup
 
     Each of them can be encoded as raw bytes. Operations are distinguished at
     type level using phantom type parameters. [packed_operation] type allows
@@ -94,6 +95,8 @@ module Kind : sig
 
   type register_global_constant = Register_global_constant_kind
 
+  type sc_rollup_originate = Sc_rollup_originate_kind
+
   type 'a manager =
     | Reveal_manager_kind : reveal manager
     | Transaction_manager_kind : transaction manager
@@ -101,6 +104,7 @@ module Kind : sig
     | Delegation_manager_kind : delegation manager
     | Register_global_constant_manager_kind : register_global_constant manager
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
+    | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
 end
 
 type 'a consensus_operation_type =
@@ -231,6 +235,11 @@ and _ manager_operation =
   | Set_deposits_limit :
       Tez_repr.t option
       -> Kind.set_deposits_limit manager_operation
+  | Sc_rollup_originate : {
+      pvm : Sc_rollup_repr.PVM.t;
+      boot_sector : Sc_rollup_repr.PVM.boot_sector;
+    }
+      -> Kind.sc_rollup_originate manager_operation
 
 and counter = Z.t
 
@@ -350,6 +359,8 @@ module Encoding : sig
 
   val set_deposits_limit_case : Kind.set_deposits_limit Kind.manager case
 
+  val sc_rollup_originate_case : Kind.sc_rollup_originate Kind.manager case
+
   module Manager_operations : sig
     type 'b case =
       | MCase : {
@@ -373,5 +384,7 @@ module Encoding : sig
     val register_global_constant_case : Kind.register_global_constant case
 
     val set_deposits_limit_case : Kind.set_deposits_limit case
+
+    val sc_rollup_originate_case : Kind.sc_rollup_originate case
   end
 end

tezos/tezos-mirror

src/proto_alpha/lib_protocol/apply.ml
@@ -1120,6 +1120,15 @@ let apply_manager_operation_content :
           }
       in
       return (ctxt, result, [])
+  | Sc_rollup_originate {kind; boot_sector} ->
+      let open Sc_rollup_operations in
+      originate ctxt ~kind ~boot_sector >>=? fun (ctxt, {address; size}) ->
+      let consumed_gas = Gas.consumed ~since:before_operation ~until:ctxt in
+      let result =
+        Sc_rollup_originate_result
+          {address; consumed_gas; size; balance_updates = []}
+      in
+      return (ctxt, result, [])
 
 type success_or_failure = Success of context | Failure
 
@@ -1329,6 +1338,16 @@ let burn_storage_fees :
         ( ctxt,
           storage_limit,
           Tx_rollup_origination_result {payload with balance_updates} )
+  | Sc_rollup_originate_result payload ->
+      let payer = `Contract payer in
+      Fees.burn_sc_rollup_origination_fees
+        ctxt
+        ~storage_limit
+        ~payer
+        payload.size
+      >>=? fun (ctxt, storage_limit, balance_updates) ->
+      let result = Sc_rollup_originate_result {payload with balance_updates} in
+      return (ctxt, storage_limit, result)
 
 let apply_manager_contents (type kind) ctxt mode chain_id
     ~gas_consumed_in_precheck (op : kind Kind.manager contents) :

src/proto_alpha/lib_protocol/operation_repr.ml
@@ -73,6 +73,8 @@ module Kind = struct
 
   type tx_rollup_origination = Tx_rollup_origination_kind
 
+  type sc_rollup_originate = Sc_rollup_originate_kind
+
   type 'a manager =
     | Reveal_manager_kind : reveal manager
     | Transaction_manager_kind : transaction manager
@@ -81,6 +83,7 @@ module Kind = struct
     | Register_global_constant_manager_kind : register_global_constant manager
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
     | Tx_rollup_origination_manager_kind : tx_rollup_origination manager
+    | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
 end
 
 type 'a consensus_operation_type =
@@ -259,6 +262,11 @@ and _ manager_operation =
       Tez_repr.t option
       -> Kind.set_deposits_limit manager_operation
   | Tx_rollup_origination : Kind.tx_rollup_origination manager_operation
+  | Sc_rollup_originate : {
+      kind : Sc_rollup_repr.Kind.t;
+      boot_sector : Sc_rollup_repr.PVM.boot_sector;
+    }
+      -> Kind.sc_rollup_originate manager_operation
 
 and counter = Z.t
 
@@ -271,6 +279,7 @@ let manager_kind : type kind. kind manager_operation -> kind Kind.manager =
   | Register_global_constant _ -> Kind.Register_global_constant_manager_kind
   | Set_deposits_limit _ -> Kind.Set_deposits_limit_manager_kind
   | Tx_rollup_origination -> Kind.Tx_rollup_origination_manager_kind
+  | Sc_rollup_originate _ -> Kind.Sc_rollup_originate_manager_kind
 
 type 'kind internal_operation = {
   source : Contract_repr.contract;
@@ -329,6 +338,8 @@ let of_list l =
   | Error s -> error @@ Contents_list_error s
 
 let tx_rollup_operation_tag_offset = 150
+
+let sc_rollup_operation_tag_offset = 200
 
 module Encoding = struct
   open Data_encoding
@@ -513,6 +524,25 @@ module Encoding = struct
           inj = (fun () -> Tx_rollup_origination);
         }
 
+    let[@coq_axiom_with_reason "gadt"] sc_rollup_originate_case =
+      MCase
+        {
+          tag = sc_rollup_operation_tag_offset + 0;
+          name = "sc_rollup_originate";
+          encoding =
+            obj2
+              (req "kind" Sc_rollup_repr.Kind.encoding)
+              (req "boot_sector" Sc_rollup_repr.PVM.boot_sector_encoding);
+          select =
+            (function
+            | Manager (Sc_rollup_originate _ as op) -> Some op | _ -> None);
+          proj =
+            (function
+            | Sc_rollup_originate {kind; boot_sector} -> (kind, boot_sector));
+          inj =
+            (fun (kind, boot_sector) -> Sc_rollup_originate {kind; boot_sector});
+        }
+
     let encoding =
       let make (MCase {tag; name; encoding; select; proj; inj}) =
         case
@@ -533,6 +563,7 @@ module Encoding = struct
           make register_global_constant_case;
           make set_deposits_limit_case;
           make tx_rollup_origination_case;
+          make sc_rollup_originate_case;
         ]
   end
 
@@ -836,6 +867,11 @@ module Encoding = struct
       tx_rollup_operation_tag_offset
       Manager_operations.tx_rollup_origination_case
 
+  let sc_rollup_originate_case =
+    make_manager_case
+      sc_rollup_operation_tag_offset
+      Manager_operations.sc_rollup_originate_case
+
   let contents_encoding =
     let make (Case {tag; name; encoding; select; proj; inj}) =
       case
@@ -865,6 +901,7 @@ module Encoding = struct
            make failing_noop_case;
            make register_global_constant_case;
            make tx_rollup_origination_case;
+           make sc_rollup_originate_case;
          ]
 
   let contents_list_encoding =
@@ -1066,6 +1103,8 @@ let equal_manager_operation_kind :
   | (Set_deposits_limit _, _) -> None
   | (Tx_rollup_origination, Tx_rollup_origination) -> Some Eq
   | (Tx_rollup_origination, _) -> None
+  | (Sc_rollup_originate _, Sc_rollup_originate _) -> Some Eq
+  | (Sc_rollup_originate _, _) -> None
 
 let equal_contents_kind : type a b. a contents -> b contents -> (a, b) eq option
     =
@@ -1157,6 +1196,7 @@ let internal_manager_operation_size (type a) (op : a manager_operation) =
         +! option_size
              (fun _ -> Contract_repr.public_key_hash_in_memory_size)
              pkh_opt )
+  | Sc_rollup_originate _ -> (Nodes.zero, h2w)
   | Reveal _ ->
       (* Reveals can't occur as internal operations *)
       assert false

src/proto_alpha/lib_protocol/operation_repr.mli
@@ -42,7 +42,8 @@
       - origination
       - delegation
       - set deposits limitation
-    - tx rollup origination
+      - tx rollup origination
+      - smart contract rollup origination
 
     Each of them can be encoded as raw bytes. Operations are distinguished at
     type level using phantom type parameters. [packed_operation] type allows
@@ -97,6 +98,8 @@ module Kind : sig
 
   type tx_rollup_origination = Tx_rollup_origination_kind
 
+  type sc_rollup_originate = Sc_rollup_originate_kind
+
   type 'a manager =
     | Reveal_manager_kind : reveal manager
     | Transaction_manager_kind : transaction manager
@@ -105,6 +108,7 @@ module Kind : sig
     | Register_global_constant_manager_kind : register_global_constant manager
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
     | Tx_rollup_origination_manager_kind : tx_rollup_origination manager
+    | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
 end
 
 type 'a consensus_operation_type =
@@ -236,6 +240,11 @@ and _ manager_operation =
       Tez_repr.t option
       -> Kind.set_deposits_limit manager_operation
   | Tx_rollup_origination : Kind.tx_rollup_origination manager_operation
+  | Sc_rollup_originate : {
+      kind : Sc_rollup_repr.Kind.t;
+      boot_sector : Sc_rollup_repr.PVM.boot_sector;
+    }
+      -> Kind.sc_rollup_originate manager_operation
 
 and counter = Z.t
 
@@ -357,6 +366,8 @@ module Encoding : sig
 
   val tx_rollup_origination_case : Kind.tx_rollup_origination Kind.manager case
 
+  val sc_rollup_originate_case : Kind.sc_rollup_originate Kind.manager case
+
   module Manager_operations : sig
     type 'b case =
       | MCase : {
@@ -382,5 +393,7 @@ module Encoding : sig
     val set_deposits_limit_case : Kind.set_deposits_limit case
 
     val tx_rollup_origination_case : Kind.tx_rollup_origination case
+
+    val sc_rollup_originate_case : Kind.sc_rollup_originate case
   end
 end