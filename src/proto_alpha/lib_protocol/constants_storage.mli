OKEAMAH/Git

Proto,SCORU: Add feature flag for SCORU
Signed-off-by: Yann Regis-Gianas <yann@nomadic-labs.com>

src/proto_alpha/lib_protocol/constants_storage.mli
@@ -87,3 +87,5 @@ val double_baking_punishment : Raw_context.t -> Tez_repr.t
 
 val ratio_of_frozen_deposits_slashed_per_double_endorsement :
   Raw_context.t -> Constants_repr.ratio
+
+val enable_sc_rollup : Raw_context.t -> bool

tezos/tezos-mirror

src/proto_alpha/lib_protocol/constants_storage.mli
@@ -93,3 +93,5 @@ val ratio_of_frozen_deposits_slashed_per_double_endorsement :
 val minimal_block_delay : Raw_context.t -> Period_repr.t
 
 val delay_increment_per_round : Raw_context.t -> Period_repr.t
+
+val sc_rollup_enable : Raw_context.t -> bool