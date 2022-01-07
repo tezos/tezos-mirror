OKEAMAH/Git

Proto,SCORU: Add a daemon to the rollup node
Signed-off-by: Yann Regis-Gianas <yann@nomadic-labs.com>

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -2499,4 +2499,6 @@ module Sc_rollup : sig
     pvm:PVM.t ->
     boot_sector:Bytes.t ->
     (context * origination_result) tzresult Lwt.t
+
+  val kind : context -> t -> Sc_rollups.kind option tzresult Lwt.t
 end

tezos/tezos-mirror

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -1919,6 +1919,8 @@ module Sc_rollup : sig
     kind:Kind.t ->
     boot_sector:PVM.boot_sector ->
     (context * t * Z.t) tzresult Lwt.t
+
+  val kind : context -> t -> Kind.t option tzresult Lwt.t
 end
 
 module Kind : sig