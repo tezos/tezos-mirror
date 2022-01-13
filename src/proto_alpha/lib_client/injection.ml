OKEAMAH/Git

Proto,SCORU: Add add_message rollup in L1 operations

src/proto_alpha/lib_client/injection.ml
@@ -320,6 +320,8 @@ let estimated_gas_single (type kind)
         Ok consumed_gas
     | Applied (Set_deposits_limit_result {consumed_gas}) -> Ok consumed_gas
     | Applied (Sc_rollup_originate_result {consumed_gas; _}) -> Ok consumed_gas
+    | Applied (Sc_rollup_add_message_result {consumed_gas; _}) ->
+        Ok consumed_gas
     | Skipped _ -> assert false
     | Backtracked (_, None) ->
         Ok Gas.Arith.zero (* there must be another error for this to happen *)
@@ -352,6 +354,7 @@ let estimated_storage_single (type kind) origination_size
         Ok size_of_constant
     | Applied (Set_deposits_limit_result _) -> Ok Z.zero
     | Applied (Sc_rollup_originate_result {size; _}) -> Ok size
+    | Applied (Sc_rollup_add_message_result _) -> Ok Z.zero
     | Skipped _ -> assert false
     | Backtracked (_, None) ->
         Ok Z.zero (* there must be another error for this to happen *)
@@ -392,6 +395,7 @@ let originated_contracts_single (type kind)
     | Applied (Delegation_result _) -> Ok []
     | Applied (Set_deposits_limit_result _) -> Ok []
     | Applied (Sc_rollup_originate_result _) -> Ok []
+    | Applied (Sc_rollup_add_message_result _) -> Ok []
     | Skipped _ -> assert false
     | Backtracked (_, None) ->
         Ok [] (* there must be another error for this to happen *)

src/proto_alpha/lib_client/operation_result.ml
@@ -174,6 +174,15 @@ let pp_manager_operation_content (type kind) source internal pp_result ppf
         R.pp_boot_sector
         boot_sector
         pp_result
+        result
+  | Sc_rollup_add_message {rollup; messages = _} ->
+      Format.fprintf
+        ppf
+        "@[<v 2>Add a message to the inbox of the smart contract rollup at \
+         address %a%a@]"
+        Sc_rollup.Address.pp
+        rollup
+        pp_result
         result) ;
   Format.fprintf ppf "@]"
 
@@ -399,6 +408,10 @@ let pp_manager_operation_contents_and_result ppf
     Format.fprintf ppf "@,Address: %a" Sc_rollup.Address.pp address ;
     pp_balance_updates_opt ppf balance_updates
   in
+  let pp_sc_rollup_add_message_result
+      (Sc_rollup_add_message_result {consumed_gas}) =
+    Format.fprintf ppf "@,Consumed gas: %a" Gas.Arith.pp consumed_gas
+  in
   let pp_result (type kind) ppf (result : kind manager_operation_result) =
     Format.fprintf ppf "@," ;
     match result with
@@ -468,7 +481,21 @@ let pp_manager_operation_contents_and_result ppf
           "@[<v 0>This rollup creation was BACKTRACKED, its expected effects \
            (as follow) were NOT applied.@]" ;
         pp_sc_rollup_originate_result op
+    | Applied (Sc_rollup_add_message_result _ as op) ->
+        Format.fprintf
+          ppf
+          "This operation sending a message to a smart contract rollup was \
+           successfully applied" ;
+        pp_sc_rollup_add_message_result op
+    | Backtracked ((Sc_rollup_add_message_result _ as op), _errs) ->
+        Format.fprintf
+          ppf
+          "@[<v 0>This operation sending a message to a smart contract rollup \
+           was BACKTRACKED, its expected effects (as follow) were NOT \
+           applied.@]" ;
+        pp_sc_rollup_add_message_result op
   in
+
   Format.fprintf
     ppf
     "@[<v 0>@[<v 2>Manager signed operations:@,\

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -1732,6 +1732,37 @@ module Vote : sig
   val clear_current_proposal : context -> context tzresult Lwt.t
 end
 
+(** See {!Sc_rollup} and {!Sc_rollup_repr}. *)
+module Sc_rollup : sig
+  module PVM : sig
+    type boot_sector = bytes
+
+    module type S = sig
+      val name : string
+
+      val parse_boot_sector : string -> boot_sector option
+
+      val pp_boot_sector : Format.formatter -> boot_sector -> unit
+    end
+
+    type t = (module S)
+  end
+
+  module Address : S.HASH
+
+  type t = Address.t
+
+  type origination_result = {address : Address.t; size : Z.t}
+
+  val originate :
+    context ->
+    pvm:PVM.t ->
+    boot_sector:Bytes.t ->
+    (context * origination_result) tzresult Lwt.t
+
+  val kind : context -> t -> Sc_rollups.kind option tzresult Lwt.t
+end
+
 module Block_payload : sig
   val hash :
     predecessor:Block_hash.t ->
@@ -1908,6 +1939,8 @@ module Kind : sig
 
   type sc_rollup_originate = Sc_rollup_originate_kind
 
+  type sc_rollup_add_message = Sc_rollup_add_message_kind
+
   type 'a manager =
     | Reveal_manager_kind : reveal manager
     | Transaction_manager_kind : transaction manager
@@ -1916,6 +1949,7 @@ module Kind : sig
     | Register_global_constant_manager_kind : register_global_constant manager
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
     | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
+    | Sc_rollup_add_message_manager_kind : sc_rollup_add_message manager
 end
 
 type 'a consensus_operation_type =
@@ -2038,6 +2072,11 @@ and _ manager_operation =
       boot_sector : Sc_rollup_repr.PVM.boot_sector;
     }
       -> Kind.sc_rollup_originate manager_operation
+  | Sc_rollup_add_message : {
+      rollup : Sc_rollup.t;
+      messages : bytes list;
+    }
+      -> Kind.sc_rollup_add_message manager_operation
 
 and counter = Z.t
 
@@ -2182,6 +2221,9 @@ module Operation : sig
 
     val sc_rollup_originate_case : Kind.sc_rollup_originate Kind.manager case
 
+    val sc_rollup_add_message_case :
+      Kind.sc_rollup_add_message Kind.manager case
+
     module Manager_operations : sig
       type 'b case =
         | MCase : {
@@ -2207,6 +2249,8 @@ module Operation : sig
       val set_deposits_limit_case : Kind.set_deposits_limit case
 
       val sc_rollup_originate_case : Kind.sc_rollup_originate case
+
+      val sc_rollup_add_message_case : Kind.sc_rollup_add_message case
     end
   end
 
@@ -2472,33 +2516,3 @@ module Fees : sig
   val check_storage_limit : context -> storage_limit:Z.t -> unit tzresult
 end
 
-(** See {!Sc_rollup} and {!Sc_rollup_repr}. *)
-module Sc_rollup : sig
-  module PVM : sig
-    type boot_sector = bytes
-
-    module type S = sig
-      val name : string
-
-      val parse_boot_sector : string -> boot_sector option
-
-      val pp_boot_sector : Format.formatter -> boot_sector -> unit
-    end
-
-    type t = (module S)
-  end
-
-  module Address : S.HASH
-
-  type t = Address.t
-
-  type origination_result = {address : Address.t; size : Z.t}
-
-  val originate :
-    context ->
-    pvm:PVM.t ->
-    boot_sector:Bytes.t ->
-    (context * origination_result) tzresult Lwt.t
-
-  val kind : context -> t -> Sc_rollups.kind option tzresult Lwt.t
-end

tezos/tezos-mirror

src/proto_alpha/lib_client/injection.ml
@@ -321,6 +321,8 @@ let estimated_gas_single (type kind)
     | Applied (Tx_rollup_origination_result {consumed_gas; _}) ->
         Ok consumed_gas
     | Applied (Sc_rollup_originate_result {consumed_gas; _}) -> Ok consumed_gas
+    | Applied (Sc_rollup_add_messages_result {consumed_gas; _}) ->
+        Ok consumed_gas
     | Skipped _ -> assert false
     | Backtracked (_, None) ->
         Ok Gas.Arith.zero (* there must be another error for this to happen *)
@@ -355,6 +357,7 @@ let estimated_storage_single (type kind) ~tx_rollup_origination_size
     | Applied (Set_deposits_limit_result _) -> Ok Z.zero
     | Applied (Tx_rollup_origination_result _) -> Ok tx_rollup_origination_size
     | Applied (Sc_rollup_originate_result {size; _}) -> Ok size
+    | Applied (Sc_rollup_add_messages_result _) -> Ok Z.zero
     | Skipped _ -> assert false
     | Backtracked (_, None) ->
         Ok Z.zero (* there must be another error for this to happen *)
@@ -403,6 +406,7 @@ let originated_contracts_single (type kind)
     | Applied (Set_deposits_limit_result _) -> Ok []
     | Applied (Tx_rollup_origination_result _) -> Ok []
     | Applied (Sc_rollup_originate_result _) -> Ok []
+    | Applied (Sc_rollup_add_messages_result _) -> Ok []
     | Skipped _ -> assert false
     | Backtracked (_, None) ->
         Ok [] (* there must be another error for this to happen *)

src/proto_alpha/lib_client/operation_result.ml
@@ -184,6 +184,15 @@ let pp_manager_operation_content (type kind) source internal pp_result ppf
         R.pp_boot_sector
         boot_sector
         pp_result
+        result
+  | Sc_rollup_add_messages {rollup; messages = _} ->
+      Format.fprintf
+        ppf
+        "@[<v 2>Add a message to the inbox of the smart contract rollup at \
+         address %a%a@]"
+        Sc_rollup.Address.pp
+        rollup
+        pp_result
         result) ;
 
   Format.fprintf ppf "@]"
@@ -425,6 +434,10 @@ let pp_manager_operation_contents_and_result ppf
     Format.fprintf ppf "@,Address: %a" Sc_rollup.Address.pp address ;
     pp_balance_updates_opt ppf balance_updates
   in
+  let pp_sc_rollup_add_messages_result
+      (Sc_rollup_add_messages_result {consumed_gas}) =
+    Format.fprintf ppf "@,Consumed gas: %a" Gas.Arith.pp consumed_gas
+  in
   let pp_result (type kind) ppf (result : kind manager_operation_result) =
     Format.fprintf ppf "@," ;
     match result with
@@ -505,6 +518,19 @@ let pp_manager_operation_contents_and_result ppf
           "@[<v 0>This rollup origination was BACKTRACKED, its expected \
            effects (as follow) were NOT applied.@]" ;
         pp_sc_rollup_originate_result op
+    | Applied (Sc_rollup_add_messages_result _ as op) ->
+        Format.fprintf
+          ppf
+          "This operation sending a message to a smart contract rollup was \
+           successfully applied" ;
+        pp_sc_rollup_add_messages_result op
+    | Backtracked ((Sc_rollup_add_messages_result _ as op), _errs) ->
+        Format.fprintf
+          ppf
+          "@[<v 0>This operation sending a message to a smart contract rollup \
+           was BACKTRACKED, its expected effects (as follow) were NOT \
+           applied.@]" ;
+        pp_sc_rollup_add_messages_result op
   in
 
   Format.fprintf

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -1773,6 +1773,33 @@ module Vote : sig
   val clear_current_proposal : context -> context tzresult Lwt.t
 end
 
+(** See {!Sc_rollup_storage} and {!Sc_rollup_repr}. *)
+module Sc_rollup : sig
+  module PVM : sig
+    type boot_sector
+
+    val boot_sector_of_string : string -> boot_sector
+  end
+
+  module Address : S.HASH
+
+  type t = Address.t
+
+  module Kind : sig
+    type t = Example_arith
+
+    val encoding : t Data_encoding.t
+  end
+
+  val originate :
+    context ->
+    kind:Kind.t ->
+    boot_sector:PVM.boot_sector ->
+    (t * Z.t * context) tzresult Lwt.t
+
+  val kind : context -> t -> Kind.t option tzresult Lwt.t
+end
+
 module Block_payload : sig
   val hash :
     predecessor:Block_hash.t ->
@@ -1901,33 +1928,6 @@ module Block_header : sig
     unit tzresult
 end
 
-(** See {!Sc_rollup_storage} and {!Sc_rollup_repr}. *)
-module Sc_rollup : sig
-  module PVM : sig
-    type boot_sector
-
-    val boot_sector_of_string : string -> boot_sector
-  end
-
-  module Address : S.HASH
-
-  type t = Address.t
-
-  module Kind : sig
-    type t = Example_arith
-
-    val encoding : t Data_encoding.t
-  end
-
-  val originate :
-    context ->
-    kind:Kind.t ->
-    boot_sector:PVM.boot_sector ->
-    (context * t * Z.t) tzresult Lwt.t
-
-  val kind : context -> t -> Kind.t option tzresult Lwt.t
-end
-
 module Kind : sig
   type preendorsement_consensus_kind = Preendorsement_consensus_kind
 
@@ -1978,6 +1978,8 @@ module Kind : sig
 
   type sc_rollup_originate = Sc_rollup_originate_kind
 
+  type sc_rollup_add_messages = Sc_rollup_add_messages_kind
+
   type 'a manager =
     | Reveal_manager_kind : reveal manager
     | Transaction_manager_kind : transaction manager
@@ -1987,6 +1989,7 @@ module Kind : sig
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
     | Tx_rollup_origination_manager_kind : tx_rollup_origination manager
     | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
+    | Sc_rollup_add_messages_manager_kind : sc_rollup_add_messages manager
 end
 
 type 'a consensus_operation_type =
@@ -2110,6 +2113,11 @@ and _ manager_operation =
       boot_sector : Sc_rollup.PVM.boot_sector;
     }
       -> Kind.sc_rollup_originate manager_operation
+  | Sc_rollup_add_messages : {
+      rollup : Sc_rollup.t;
+      messages : string list;
+    }
+      -> Kind.sc_rollup_add_messages manager_operation
 
 and counter = Z.t
 
@@ -2257,6 +2265,9 @@ module Operation : sig
 
     val sc_rollup_originate_case : Kind.sc_rollup_originate Kind.manager case
 
+    val sc_rollup_add_messages_case :
+      Kind.sc_rollup_add_messages Kind.manager case
+
     module Manager_operations : sig
       type 'b case =
         | MCase : {
@@ -2284,6 +2295,8 @@ module Operation : sig
       val tx_rollup_origination_case : Kind.tx_rollup_origination case
 
       val sc_rollup_originate_case : Kind.sc_rollup_originate case
+
+      val sc_rollup_add_messages_case : Kind.sc_rollup_add_messages case
     end
   end
 