c96bb964bb4f90a101fa5484d40e89d0000d30c5
Richard Bonichon <richard.bonichon@nomadic-labs.com>
2022-01-18 14:53:06 +0100

    Proto/Alpha: revert lost comments in Tez_repr
    These comments were lost for no good reason while merging Tenderbake

--- src/proto_alpha/lib_protocol/tez_repr.mli
+++ src/proto_alpha/lib_protocol/tez_repr.mli
@@ -24,6 +24,14 @@
 (*                                                                           *)
 (*****************************************************************************)
 
+(** Internal representation of the Tez currency. Behaves mostly like a natural
+   number where number 1 represents 1/1,000,000 Tez (1 micro-Tez or mutez).
+   It's protected from ever becoming negative and overflowing by special
+   arithmetic functions, which fail in case something undesired would happen.
+   When divided, it's always rounded down to 1 mutez.
+
+   Internally encoded as [int64], which may be relevant to guard against
+   overflow errors. *)
 type repr
 
 (** [t] is made algebraic in order to distinguish it from the other type
