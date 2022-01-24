OKEAMAH/Git

Proto,tx_rollup: Refactor the [state] type of [Tx_rollup_repr]
This patch gathers several small refactoring regarding this type.

  - We move the definition of [state] in a dedicated module
    ([Tx_rollup_state_repr]).
  - We carbonate the storage of this value in the context.
  - We move the manipulation of this storage in a dedicated module
    ([Tx_rollup_state_storage]).
  - We update [Alpha_context] accordingly.
  - We modify the dedicated RPC to return a value, or a 404 error,
    rather than an option.

Co-authored-by: Marigold <contact@marigold.dev>
Co-authored-by: Nomadic Labs <contact@nomadic-labs.com>
Co-authored-by: Oxhead Alpha <info@oxheadalpha.com>

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -1520,9 +1520,8 @@ module Receipt : sig
   val group_balance_updates : balance_updates -> balance_updates tzresult
 end
 
-(** This simply re-exports [Tx_rollup_repr] and [tx_rollup_storage]. See
-    [tx_rollup_repr] and [tx_rollup_storage] for additional documentation of this
-    module *)
+(** This modules re-exports definitions from {!Tx_rollup_repr} and
+    {!Tx_rollup_storage}. *)
 module Tx_rollup : sig
   include BASIC_DATA
 
@@ -1540,19 +1539,31 @@ module Tx_rollup : sig
 
   val originate : context -> (context * tx_rollup) tzresult Lwt.t
 
-  type state
-
-  val state : context -> tx_rollup -> state option tzresult Lwt.t
-
-  val state_encoding : state Data_encoding.t
-
-  val pp_state : Format.formatter -> state -> unit
-
   module Internal_for_tests : sig
     (** see [tx_rollup_repr.originated_tx_rollup] for documentation *)
     val originated_tx_rollup :
       Origination_nonce.Internal_for_tests.t -> tx_rollup
   end
+end
+
+(** This module re-exports definitions from {!Tx_rollup_state_repr}
+    and {!Tx_rollup_state_storage}. *)
+module Tx_rollup_state : sig
+  type t
+
+  val initial_state : t
+
+  val encoding : t Data_encoding.t
+
+  val pp : Format.formatter -> t -> unit
+
+  val get_opt : context -> Tx_rollup.t -> (context * t option) tzresult Lwt.t
+
+  val get : context -> Tx_rollup.t -> (context * t) tzresult Lwt.t
+
+  type error +=
+    | Tx_rollup_already_exists of Tx_rollup.t
+    | Tx_rollup_does_not_exist of Tx_rollup.t
 end
 
 module Delegate : sig

src/proto_alpha/lib_protocol/tx_rollup_state_storage.ml
@@ -1,0 +1,90 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type error +=
+  | Tx_rollup_already_exists of Tx_rollup_repr.t
+  | Tx_rollup_does_not_exist of Tx_rollup_repr.t
+
+let init : Raw_context.t -> Tx_rollup_repr.t -> Raw_context.t tzresult Lwt.t =
+ fun ctxt tx_rollup ->
+  Storage.Tx_rollup.State.mem ctxt tx_rollup >>=? fun (ctxt, already_exists) ->
+  fail_when already_exists (Tx_rollup_already_exists tx_rollup) >>=? fun () ->
+  Storage.Tx_rollup.State.init ctxt tx_rollup Tx_rollup_state_repr.initial_state
+  >|=? fst
+
+let get_opt :
+    Raw_context.t ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_state_repr.t option) tzresult Lwt.t =
+  Storage.Tx_rollup.State.find
+
+let get :
+    Raw_context.t ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_state_repr.t) tzresult Lwt.t =
+ fun ctxt tx_rollup ->
+  get_opt ctxt tx_rollup >>=? fun (ctxt, state) ->
+  match state with
+  | Some state -> return (ctxt, state)
+  | None -> fail (Tx_rollup_does_not_exist tx_rollup)
+
+(* Error registration *)
+
+let () =
+  let open Data_encoding in
+  (* Tx_rollup_already_exists *)
+  register_error_kind
+    `Permanent
+    ~id:"tx_rollup_already_exists"
+    ~title:"Transaction rollup was already created"
+    ~description:
+      "The protocol tried to originate the same transaction rollup twice"
+    ~pp:(fun ppf addr ->
+      Format.fprintf
+        ppf
+        "Transaction rollup %a is already used for an existing transaction \
+         rollup"
+        Tx_rollup_repr.pp
+        addr)
+    (obj1 (req "rollup_address" Tx_rollup_repr.encoding))
+    (function Tx_rollup_does_not_exist rollup -> Some rollup | _ -> None)
+    (fun rollup -> Tx_rollup_does_not_exist rollup) ;
+  (* Tx_rollup_does_not_exist *)
+  register_error_kind
+    `Temporary
+    ~id:"tx_rollup_does_not_exist"
+    ~title:"Transaction rollup does not exist"
+    ~description:"An invalid transaction rollup address was submitted"
+    ~pp:(fun ppf addr ->
+      Format.fprintf
+        ppf
+        "Invalid transaction rollup address %a"
+        Tx_rollup_repr.pp
+        addr)
+    (obj1 (req "rollup_address" Tx_rollup_repr.encoding))
+    (function Tx_rollup_does_not_exist rollup -> Some rollup | _ -> None)
+    (fun rollup -> Tx_rollup_does_not_exist rollup)

src/proto_alpha/lib_protocol/tx_rollup_state_storage.mli
@@ -1,8 +1,9 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2021 Marigold <contact@marigold.dev>                        *)
-(* Copyright (c) 2021 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -24,10 +25,38 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let fresh_tx_rollup_from_current_nonce ctxt =
-  Raw_context.increment_origination_nonce ctxt >|? fun (ctxt, nonce) ->
-  (ctxt, Tx_rollup_repr.originated_tx_rollup nonce)
+(** A collections of functions to manipulate the state of a
+    transaction rollup.
 
-let originate ctxt =
-  fresh_tx_rollup_from_current_nonce ctxt >>?= fun (ctxt, tx_rollup) ->
-  Tx_rollup_state_storage.init ctxt tx_rollup >|=? fun ctxt -> (ctxt, tx_rollup)
+    Except if the contrary is explicitly stated, the functions of this
+    module are cabonated. *)
+
+type error +=
+  | Tx_rollup_already_exists of Tx_rollup_repr.t
+  | Tx_rollup_does_not_exist of Tx_rollup_repr.t
+
+(** [init ctxt tx_rollup] initializes the state of [tx_rollup].
+
+    This will raises [Tx_rollup_already_exists] if this function has
+    already been called for [tx_rollup], which is definitely something
+    that should not happen, and would indicate a bug in the
+    protocol. *)
+val init : Raw_context.t -> Tx_rollup_repr.t -> Raw_context.t tzresult Lwt.t
+
+(** [get_opt context tx_rollup] returns the current state of
+    [tx_rollup]. If [tx_rollup] is not the address of an existing
+    transaction rollup, [None] is returned instead. *)
+val get_opt :
+  Raw_context.t ->
+  Tx_rollup_repr.t ->
+  (Raw_context.t * Tx_rollup_state_repr.t option) tzresult Lwt.t
+
+(** [get context tx_rollup] returns the current state of [tx_rollup]
+    in the context.
+
+    Raises [Tx_rollup_does_not_exist] iff [tx_rollup] is not the
+    address of an existing transaction rollup. *)
+val get :
+  Raw_context.t ->
+  Tx_rollup_repr.t ->
+  (Raw_context.t * Tx_rollup_state_repr.t) tzresult Lwt.t

tezos/tezos-mirror

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -1519,9 +1519,8 @@ module Receipt : sig
   val group_balance_updates : balance_updates -> balance_updates tzresult
 end
 
-(** This simply re-exports [Tx_rollup_repr] and [tx_rollup_storage]. See
-    [tx_rollup_repr] and [tx_rollup_storage] for additional documentation of this
-    module *)
+(** This module re-exports definitions from {!Tx_rollup_repr} and
+    {!Tx_rollup_storage}. *)
 module Tx_rollup : sig
   include BASIC_DATA
 
@@ -1539,19 +1538,31 @@ module Tx_rollup : sig
 
   val originate : context -> (context * tx_rollup) tzresult Lwt.t
 
-  type state
-
-  val state : context -> tx_rollup -> state option tzresult Lwt.t
-
-  val state_encoding : state Data_encoding.t
-
-  val pp_state : Format.formatter -> state -> unit
-
   module Internal_for_tests : sig
     (** see [tx_rollup_repr.originated_tx_rollup] for documentation *)
     val originated_tx_rollup :
       Origination_nonce.Internal_for_tests.t -> tx_rollup
   end
+end
+
+(** This module re-exports definitions from {!Tx_rollup_state_repr}
+    and {!Tx_rollup_state_storage}. *)
+module Tx_rollup_state : sig
+  type t
+
+  val initial_state : t
+
+  val encoding : t Data_encoding.t
+
+  val pp : Format.formatter -> t -> unit
+
+  val find : context -> Tx_rollup.t -> (context * t option) tzresult Lwt.t
+
+  val get : context -> Tx_rollup.t -> (context * t) tzresult Lwt.t
+
+  type error +=
+    | Tx_rollup_already_exists of Tx_rollup.t
+    | Tx_rollup_does_not_exist of Tx_rollup.t
 end
 
 module Delegate : sig

src/proto_alpha/lib_protocol/tx_rollup_state_storage.ml
@@ -1,0 +1,92 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type error +=
+  | Tx_rollup_already_exists of Tx_rollup_repr.t
+  | Tx_rollup_does_not_exist of Tx_rollup_repr.t
+
+let init : Raw_context.t -> Tx_rollup_repr.t -> Raw_context.t tzresult Lwt.t =
+ fun ctxt tx_rollup ->
+  Storage.Tx_rollup.State.mem ctxt tx_rollup >>=? fun (ctxt, already_exists) ->
+  fail_when already_exists (Tx_rollup_already_exists tx_rollup) >>=? fun () ->
+  Storage.Tx_rollup.State.init ctxt tx_rollup Tx_rollup_state_repr.initial_state
+  >|=? fst
+
+let find :
+    Raw_context.t ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_state_repr.t option) tzresult Lwt.t =
+  Storage.Tx_rollup.State.find
+
+let get :
+    Raw_context.t ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_state_repr.t) tzresult Lwt.t =
+ fun ctxt tx_rollup ->
+  find ctxt tx_rollup >>=? fun (ctxt, state) ->
+  match state with
+  | Some state -> return (ctxt, state)
+  | None -> fail (Tx_rollup_does_not_exist tx_rollup)
+
+(* ------ Error registration ------------------------------------------------ *)
+
+let () =
+  let open Data_encoding in
+  (* Tx_rollup_already_exists *)
+  register_error_kind
+    `Permanent
+    ~id:"tx_rollup_already_exists"
+    ~title:"Transaction rollup was already created"
+    ~description:
+      "The protocol tried to originate the same transaction rollup twice"
+    ~pp:(fun ppf addr ->
+      Format.fprintf
+        ppf
+        "Transaction rollup %a is already used for an existing transaction \
+         rollup. This should not happen, and indicates there is a bug in the \
+         protocol. If you can, please report this bug \
+         (https://gitlab.com/tezos/tezos/-/issues.)"
+        Tx_rollup_repr.pp
+        addr)
+    (obj1 (req "rollup_address" Tx_rollup_repr.encoding))
+    (function Tx_rollup_already_exists rollup -> Some rollup | _ -> None)
+    (fun rollup -> Tx_rollup_already_exists rollup) ;
+  (* Tx_rollup_does_not_exist *)
+  register_error_kind
+    `Temporary
+    ~id:"tx_rollup_does_not_exist"
+    ~title:"Transaction rollup does not exist"
+    ~description:"An invalid transaction rollup address was submitted"
+    ~pp:(fun ppf addr ->
+      Format.fprintf
+        ppf
+        "Invalid transaction rollup address %a"
+        Tx_rollup_repr.pp
+        addr)
+    (obj1 (req "rollup_address" Tx_rollup_repr.encoding))
+    (function Tx_rollup_does_not_exist rollup -> Some rollup | _ -> None)
+    (fun rollup -> Tx_rollup_does_not_exist rollup)

src/proto_alpha/lib_protocol/tx_rollup_state_storage.mli
@@ -1,8 +1,9 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2021 Marigold <contact@marigold.dev>                        *)
-(* Copyright (c) 2021 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -24,25 +25,44 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-open Alpha_context
+(** A collection of functions to manipulate the state of a transaction
+    rollup.
 
-let custom_root =
-  (RPC_path.(open_root / "context" / "tx_rollup")
-    : RPC_context.t RPC_path.context)
+    Except if the contrary is explicitly stated, the functions of this
+    module are carbonated. *)
 
-module S = struct
-  let state =
-    RPC_service.get_service
-      ~description:"Access the state of a rollup."
-      ~query:RPC_query.empty
-      ~output:Tx_rollup_state.encoding
-      RPC_path.(custom_root /: Tx_rollup.rpc_arg / "state")
-end
+type error +=
+  | Tx_rollup_already_exists of Tx_rollup_repr.t
+  | Tx_rollup_does_not_exist of Tx_rollup_repr.t
 
-let register () =
-  let open Services_registration in
-  opt_register1 ~chunked:false S.state (fun ctxt tx_rollup () () ->
-      Tx_rollup_state.find ctxt tx_rollup >|=? snd)
+(** [init ctxt tx_rollup] initializes the state of [tx_rollup].
 
-let state ctxt block tx_rollup =
-  RPC_context.make_call1 S.state ctxt block tx_rollup () ()
+    Returns the error [Tx_rollup_already_exists] iff this function has
+    already been called for [tx_rollup], which is definitely something
+    that should not happen, because the protocol is expected to pick
+    fresh addresses when it originates new transaction rollups (and
+    does so by relying on the “origination nonce” derived from the
+    hash of the operation responsible for the origination, using the
+    same procedure as smart contracts).
+
+    Raising this error would therefore indicate a bug in the
+    protocol. *)
+val init : Raw_context.t -> Tx_rollup_repr.t -> Raw_context.t tzresult Lwt.t
+
+(** [find ctxt tx_rollup] returns the current state of [tx_rollup]. If
+    [tx_rollup] is not the address of an existing transaction rollup,
+    [None] is returned instead. *)
+val find :
+  Raw_context.t ->
+  Tx_rollup_repr.t ->
+  (Raw_context.t * Tx_rollup_state_repr.t option) tzresult Lwt.t
+
+(** [get ctxt tx_rollup] returns the current state of [tx_rollup] in
+    the context.
+
+    Returns the [Tx_rollup_does_not_exist] error iff [tx_rollup] is
+    not the address of an existing transaction rollup. *)
+val get :
+  Raw_context.t ->
+  Tx_rollup_repr.t ->
+  (Raw_context.t * Tx_rollup_state_repr.t) tzresult Lwt.t