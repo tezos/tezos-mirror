OKEAMAH/Git

Proto: Generalize the [destination] argument of [Transaction]
This opens the possibility to reuse [Transaction] and the entrypoint
API to interact with layer-2 solutions such as transaction rollups.

Note that this patch is better reviewed using `git show
--ignore-space-change'.  Indeed, it does not add any additional
functionality. Instead, it adds another layer of
abstraction. Unfortunately, the default diff algorithm used by git is
not well suited to demonstrate it, because this patch increases the
indentation level of large section of code.

Co-authored-by: Marigold <contact@marigold.dev>
Co-authored-by: Nomadic Labs <contact@nomadic-labs.com>
Co-authored-by: Oxhead Alpha <info@oxheadalpha.com>

src/proto_alpha/lib_protocol/destination_repr.mli
@@ -1,8 +1,9 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2021 Trili Tech, <contact@trili.tech>                       *)
-(* Copyright (c) 2021-2022 Nomadic Labs <contact@nomadic-labs.com>           *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxheadalpha.com>                    *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -24,52 +25,36 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-open Alpha_context
+(** The type of the [destination] argument of the
+    {!Operation_repr.Transaction} manager operation.
 
-(* This function extracts nodes of:
-    - Ticketer
-    - Type of content
-    - Content
-    - Owner
-   to generate at ticket-balance key-hash.
-*)
-let ticket_balance_key ctxt ~owner
-    (Ticket_token.Ex_token {ticketer; contents_type; contents}) =
-  let loc = Micheline.dummy_location in
-  Script_ir_translator.unparse_comparable_ty ~loc ctxt contents_type
-  >>?= fun (cont_ty_unstripped, ctxt) ->
-  (* We strip the annotations from the content type in order to map
-     tickets with the same content type, but with different annotations, to the
-     same hash. *)
-  Gas.consume ctxt (Script.strip_annotations_cost cont_ty_unstripped)
-  >>?= fun ctxt ->
-  let typ = Script.strip_annotations cont_ty_unstripped in
-  let ticketer = Destination.Contract ticketer in
-  let ticketer_address =
-    Script_typed_ir.{destination = ticketer; entrypoint = Entrypoint.default}
-  in
-  let owner = Destination.Contract owner in
-  let owner_address =
-    Script_typed_ir.{destination = owner; entrypoint = Entrypoint.default}
-  in
-  let address_t = Script_typed_ir.address_t ~annot:None in
-  Script_ir_translator.unparse_data
-    ctxt
-    Script_ir_translator.Optimized_legacy
-    address_t
-    ticketer_address
-  >>=? fun (ticketer, ctxt) ->
-  Script_ir_translator.unparse_comparable_data
-    ~loc
-    ctxt
-    Script_ir_translator.Optimized_legacy
-    contents_type
-    contents
-  >>=? fun (contents, ctxt) ->
-  Script_ir_translator.unparse_data
-    ctxt
-    Script_ir_translator.Optimized_legacy
-    address_t
-    owner_address
-  >>=? fun (owner, ctxt) ->
-  Lwt.return (Ticket_hash.make ctxt ~ticketer ~typ ~contents ~owner)
+    The introduction of this type allows to interact with emerging
+    layer-2 solutions using the same old API Tezos users and tooling
+    are already used to: contract calls to entrypoint. These solutions
+    cannot be integrated to {!Contract_repr.t} directly, because
+    values of this type are given a balances, which have an impact on
+    the delegation system. *)
+
+(** This type is a superset of the set of contracts ({!Contract_repr.t}).
+
+    {b Note:} It is of key importance that the encoding of this type
+    remains compatible with {!Contract_repr.encoding}, for the
+    introduction to this type to remain transparent from the existing
+    tooling perspective.  *)
+type t = Contract of Contract_repr.t
+
+include Compare.S with type t := t
+
+val to_b58check : t -> string
+
+val of_b58check : string -> t tzresult
+
+val encoding : t Data_encoding.t
+
+val pp : Format.formatter -> t -> unit
+
+(** [in_memory_size contract] returns the number of bytes that are
+    allocated in the RAM for [contract]. *)
+val in_memory_size : t -> Cache_memory_helpers.sint
+
+type error += Invalid_destination_b58check of string

tezos/tezos-mirror

src/proto_alpha/lib_protocol/destination_repr.mli
@@ -1,8 +1,9 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* Open Source License                                                       *)
-(* Copyright (c) 2021 Trili Tech, <contact@trili.tech>                       *)
-(* Copyright (c) 2021-2022 Nomadic Labs <contact@nomadic-labs.com>           *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxheadalpha.com>                    *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -24,51 +25,36 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-open Alpha_context
+(** The type of the [destination] argument of the
+    {!Operation_repr.Transaction} manager operation.
 
-(* This function extracts nodes of:
-    - Ticketer
-    - Type of content
-    - Content
-    - Owner
-   to generate at ticket-balance key-hash.
-*)
-let ticket_balance_key ctxt ~owner
-    (Ticket_token.Ex_token {ticketer; contents_type; contents}) =
-  let loc = Micheline.dummy_location in
-  Script_ir_translator.unparse_comparable_ty ~loc ctxt contents_type
-  >>?= fun (cont_ty_unstripped, ctxt) ->
-  (* We strip the annotations from the content type in order to map
-     tickets with the same content type, but with different annotations, to the
-     same hash. *)
-  Gas.consume ctxt (Script.strip_annotations_cost cont_ty_unstripped)
-  >>?= fun ctxt ->
-  let typ = Script.strip_annotations cont_ty_unstripped in
-  let ticketer = Destination.Contract ticketer in
-  let ticketer_address =
-    Script_typed_ir.{destination = ticketer; entrypoint = Entrypoint.default}
-  in
-  let owner = Destination.Contract owner in
-  let owner_address =
-    Script_typed_ir.{destination = owner; entrypoint = Entrypoint.default}
-  in
-  Script_ir_translator.unparse_data
-    ctxt
-    Script_ir_translator.Optimized_legacy
-    Script_typed_ir.address_t
-    ticketer_address
-  >>=? fun (ticketer, ctxt) ->
-  Script_ir_translator.unparse_comparable_data
-    ~loc
-    ctxt
-    Script_ir_translator.Optimized_legacy
-    contents_type
-    contents
-  >>=? fun (contents, ctxt) ->
-  Script_ir_translator.unparse_data
-    ctxt
-    Script_ir_translator.Optimized_legacy
-    Script_typed_ir.address_t
-    owner_address
-  >>=? fun (owner, ctxt) ->
-  Lwt.return (Ticket_hash.make ctxt ~ticketer ~typ ~contents ~owner)
+    The introduction of this type allows to interact with emerging
+    layer-2 solutions using the API Tezos users and tooling
+    are already used to: contract calls to entrypoint. These solutions
+    cannot be integrated to {!Contract_repr.t} directly, because
+    values of this type are given a balance, which has an impact on
+    the delegation system. *)
+
+(** This type is a superset of the set of contracts ({!Contract_repr.t}).
+
+    {b Note:} It is of key importance that the encoding of this type
+    remains compatible with {!Contract_repr.encoding}, for the
+    introduction to this type to remain transparent from the existing
+    tooling perspective.  *)
+type t = Contract of Contract_repr.t
+
+include Compare.S with type t := t
+
+val to_b58check : t -> string
+
+val of_b58check : string -> t tzresult
+
+val encoding : t Data_encoding.t
+
+val pp : Format.formatter -> t -> unit
+
+(** [in_memory_size contract] returns the number of bytes that are
+    allocated in the RAM for [contract]. *)
+val in_memory_size : t -> Cache_memory_helpers.sint
+
+type error += Invalid_destination_b58check of string