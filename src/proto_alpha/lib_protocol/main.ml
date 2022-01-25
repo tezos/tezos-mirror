mavryk-network/mavryk-ligo

Proto: update ticket balances in migration to Alpha

src/proto_alpha/lib_protocol/main.ml
@@ -781,6 +781,7 @@ let init ctxt block_header =
   init_cache ctxt >>= fun ctxt ->
   Alpha_context.prepare_first_block ~typecheck ~level ~timestamp ctxt
   >>=? fun ctxt ->
+  Ticket_balance_migration.init ctxt >>=? fun ctxt ->
   let cache_nonce =
     cache_nonce_from_block_header
       block_header

tezos/tezos-mirror

src/proto_alpha/lib_protocol/main.ml
@@ -775,6 +775,7 @@ let init ctxt block_header =
   in
   Alpha_context.prepare_first_block ~typecheck ~level ~timestamp ctxt
   >>=? fun ctxt ->
+  Ticket_balance_migration_for_j.init ctxt >>=? fun ctxt ->
   let cache_nonce =
     cache_nonce_from_block_header
       block_header