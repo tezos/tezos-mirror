OKEAMAH/Git

Proto,tx_rollup: Introduce the transaction rollup’s inbox
Note that we had to move the existing [Tx_rollup_*] of [Alpha_context]
due to the dependency of [Tx_rollup_message] to [Ticket_hash].

Co-authored-by: Marigold <contact@marigold.dev>
Co-authored-by: Nomadic Labs <contact@nomadic-labs.com>
Co-authored-by: Oxhead Alpha <info@oxheadalpha.com>

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -1520,52 +1520,6 @@ module Receipt : sig
   val group_balance_updates : balance_updates -> balance_updates tzresult
 end
 
-(** This modules re-exports definitions from {!Tx_rollup_repr} and
-    {!Tx_rollup_storage}. *)
-module Tx_rollup : sig
-  include BASIC_DATA
-
-  type tx_rollup = t
-
-  val rpc_arg : tx_rollup RPC_arg.arg
-
-  val to_b58check : tx_rollup -> string
-
-  val of_b58check : string -> tx_rollup tzresult
-
-  val pp : Format.formatter -> tx_rollup -> unit
-
-  val encoding : tx_rollup Data_encoding.t
-
-  val originate : context -> (context * tx_rollup) tzresult Lwt.t
-
-  module Internal_for_tests : sig
-    (** see [tx_rollup_repr.originated_tx_rollup] for documentation *)
-    val originated_tx_rollup :
-      Origination_nonce.Internal_for_tests.t -> tx_rollup
-  end
-end
-
-(** This module re-exports definitions from {!Tx_rollup_state_repr}
-    and {!Tx_rollup_state_storage}. *)
-module Tx_rollup_state : sig
-  type t
-
-  val initial_state : t
-
-  val encoding : t Data_encoding.t
-
-  val pp : Format.formatter -> t -> unit
-
-  val get_opt : context -> Tx_rollup.t -> (context * t option) tzresult Lwt.t
-
-  val get : context -> Tx_rollup.t -> (context * t) tzresult Lwt.t
-
-  type error +=
-    | Tx_rollup_already_exists of Tx_rollup.t
-    | Tx_rollup_does_not_exist of Tx_rollup.t
-end
-
 module Delegate : sig
   val init :
     context ->
@@ -1980,6 +1934,116 @@ module Ticket_hash : sig
     contents:Script.node ->
     owner:Script.node ->
     (t * context) tzresult
+end
+
+(** This modules re-exports definitions from {!Tx_rollup_repr} and
+    {!Tx_rollup_storage}. *)
+module Tx_rollup : sig
+  include BASIC_DATA
+
+  type tx_rollup = t
+
+  val rpc_arg : tx_rollup RPC_arg.arg
+
+  val to_b58check : tx_rollup -> string
+
+  val of_b58check : string -> tx_rollup tzresult
+
+  val pp : Format.formatter -> tx_rollup -> unit
+
+  val encoding : tx_rollup Data_encoding.t
+
+  val originate : context -> (context * tx_rollup) tzresult Lwt.t
+
+  module Internal_for_tests : sig
+    (** see [tx_rollup_repr.originated_tx_rollup] for documentation *)
+    val originated_tx_rollup :
+      Origination_nonce.Internal_for_tests.t -> tx_rollup
+  end
+end
+
+(** This module re-exports definitions from {!Tx_rollup_state_repr}
+    and {!Tx_rollup_state_storage}. *)
+module Tx_rollup_state : sig
+  type t
+
+  val initial_state : t
+
+  val encoding : t Data_encoding.t
+
+  val pp : Format.formatter -> t -> unit
+
+  val get_opt : context -> Tx_rollup.t -> (context * t option) tzresult Lwt.t
+
+  val get : context -> Tx_rollup.t -> (context * t) tzresult Lwt.t
+
+  type error +=
+    | Tx_rollup_already_exists of Tx_rollup.t
+    | Tx_rollup_does_not_exist of Tx_rollup.t
+end
+
+(** This module re-exports definitions from {!Tx_rollup_message_repr}. *)
+module Tx_rollup_message : sig
+  type deposit = {
+    destination : Tx_rollup_l2_address.t;
+    ticket_hash : Ticket_hash.t;
+    amount : int64;
+  }
+
+  type t = Batch of string | Deposit of deposit
+
+  val size : t -> int
+
+  val encoding : t Data_encoding.t
+
+  val pp : Format.formatter -> t -> unit
+
+  type hash
+
+  val hash_encoding : hash Data_encoding.t
+
+  val pp_hash : Format.formatter -> hash -> unit
+
+  val hash : t -> hash
+end
+
+(** This module re-exports definitions from {!Tx_rollup_inbox_repr} and
+    {!Tx_rollup_inbox_storage}. *)
+module Tx_rollup_inbox : sig
+  type t = {contents : Tx_rollup_message.hash list; cumulated_size : int}
+
+  val pp : Format.formatter -> t -> unit
+
+  val encoding : t Data_encoding.t
+
+  val append_message :
+    context ->
+    Tx_rollup.t ->
+    Tx_rollup_message.t ->
+    (int * context) tzresult Lwt.t
+
+  val messages :
+    context ->
+    ?level:Raw_level.t ->
+    Tx_rollup.t ->
+    (context * Tx_rollup_message.hash list) tzresult Lwt.t
+
+  val size :
+    context ->
+    ?level:Raw_level.t ->
+    Tx_rollup.t ->
+    (context * int) tzresult Lwt.t
+
+  val get :
+    context -> ?level:Raw_level.t -> Tx_rollup.t -> (context * t) tzresult Lwt.t
+
+  val get_opt :
+    context ->
+    ?level:Raw_level.t ->
+    Tx_rollup.t ->
+    (context * t option) tzresult Lwt.t
+
+  type error += Tx_rollup_inbox_does_not_exist of Tx_rollup.t * Raw_level.t
 end
 
 module Kind : sig

src/proto_alpha/lib_protocol/storage.mli
@@ -699,6 +699,37 @@ module Tx_rollup : sig
       with type key = Tx_rollup_repr.t
        and type value = Tx_rollup_state_repr.t
        and type t := Raw_context.t
+
+  (** The number of bytes allocated by the message stored in each inbox. *)
+  module Inbox_cumulated_size :
+    Non_iterable_indexed_carbonated_data_storage
+      with type t := Raw_context.t * Raw_level_repr.t
+       and type key = Tx_rollup_repr.t
+       and type value = int
+
+  (** A carbonated storage to store the hashes of the messages
+      appended in an inbox, in reverse order.
+
+      The actual content is already stored in the block (as part of
+      the operations), so by only storing the hashes we avoid
+      unnecessary storage duplication. *)
+  module Inbox_rev_contents :
+    Non_iterable_indexed_carbonated_data_storage
+      with type t := Raw_context.t * Raw_level_repr.t
+       and type key = Tx_rollup_repr.t
+       and type value = Tx_rollup_message_repr.hash list
+
+  (** [fold (ctxt, level) ~order ~init ~f] traverses all rollups with
+      a nonempty inbox at [level].
+
+      No assurances whatsoever are provided regarding the order of
+      traversal. *)
+  val fold :
+    Raw_context.t ->
+    Raw_level_repr.t ->
+    init:'a ->
+    f:(Tx_rollup_repr.t -> 'a -> 'a Lwt.t) ->
+    'a Lwt.t
 end
 
 (** Smart contract rollup *)

src/proto_alpha/lib_protocol/tx_rollup_inbox_storage.ml
@@ -1,0 +1,153 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type error +=
+  | Tx_rollup_inbox_does_not_exist of Tx_rollup_repr.t * Raw_level_repr.t
+
+let append_message :
+    Raw_context.t ->
+    Tx_rollup_repr.t ->
+    Tx_rollup_message_repr.t ->
+    (int * Raw_context.t) tzresult Lwt.t =
+ fun ctxt rollup message ->
+  let level = (Raw_context.current_level ctxt).level in
+  Storage.Tx_rollup.Inbox_cumulated_size.find (ctxt, level) rollup
+  >>=? fun (ctxt, msize) ->
+  let message_size = Tx_rollup_message_repr.size message in
+  let new_size = Option.value ~default:0 msize + message_size in
+  Storage.Tx_rollup.Inbox_rev_contents.find (ctxt, level) rollup
+  >>=? fun (ctxt, mcontents) ->
+  Storage.Tx_rollup.Inbox_rev_contents.add
+    (ctxt, level)
+    rollup
+    (Tx_rollup_message_repr.hash message :: Option.value ~default:[] mcontents)
+  >>=? fun (ctxt, _, _) ->
+  Storage.Tx_rollup.Inbox_cumulated_size.add (ctxt, level) rollup new_size
+  >>=? fun (ctxt, _, _) -> return (message_size, ctxt)
+
+let messages_opt :
+    Raw_context.t ->
+    ?level:Raw_level_repr.t ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_message_repr.hash list option) tzresult Lwt.t =
+ fun ctxt ?(level = (Raw_context.current_level ctxt).level) tx_rollup ->
+  Storage.Tx_rollup.Inbox_rev_contents.find (ctxt, level) tx_rollup
+  >>=? function
+  | (ctxt, Some rev_contents) -> return (ctxt, Some (List.rev rev_contents))
+  | (ctxt, None) ->
+      (*
+        Prior to returning [None], we check whether or not the
+        transaction rollup address is valid, to raise the appropriate
+        if need be.
+       *)
+      Tx_rollup_state_storage.assert_exist ctxt tx_rollup >>=? fun ctxt ->
+      return (ctxt, None)
+
+let messages :
+    Raw_context.t ->
+    ?level:Raw_level_repr.t ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_message_repr.hash list) tzresult Lwt.t =
+ fun ctxt ?(level = (Raw_context.current_level ctxt).level) tx_rollup ->
+  messages_opt ctxt ~level tx_rollup >>=? function
+  | (ctxt, Some messages) -> return (ctxt, messages)
+  | (_, None) -> fail (Tx_rollup_inbox_does_not_exist (tx_rollup, level))
+
+let size :
+    Raw_context.t ->
+    ?level:Raw_level_repr.t ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * int) tzresult Lwt.t =
+ fun ctxt ?(level = (Raw_context.current_level ctxt).level) tx_rollup ->
+  Storage.Tx_rollup.Inbox_cumulated_size.find (ctxt, level) tx_rollup
+  >>=? function
+  | (ctxt, Some cumulated_size) -> return (ctxt, cumulated_size)
+  | (ctxt, None) ->
+      (*
+        Prior to raising an error related to the missing inbox, we
+        check whether or not the transaction rollup address is valid,
+        to raise the appropriate if need be.
+       *)
+      Tx_rollup_state_storage.assert_exist ctxt tx_rollup >>=? fun _ctxt ->
+      fail (Tx_rollup_inbox_does_not_exist (tx_rollup, level))
+
+let get_opt :
+    Raw_context.t ->
+    ?level:Raw_level_repr.t ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_inbox_repr.t option) tzresult Lwt.t =
+ fun ctxt ?(level = (Raw_context.current_level ctxt).level) tx_rollup ->
+  let open Tx_rollup_inbox_repr in
+  (*
+    [messages_opt] checks whether or not [tx_rollup] is valid, so
+    we do not have to do it here.
+   *)
+  messages_opt ctxt ~level tx_rollup >>=? function
+  | (ctxt, Some contents) ->
+      size ctxt ~level tx_rollup >>=? fun (ctxt, cumulated_size) ->
+      return (ctxt, Some {cumulated_size; contents})
+  | (ctxt, None) -> return (ctxt, None)
+
+let get :
+    Raw_context.t ->
+    ?level:Raw_level_repr.t ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_inbox_repr.t) tzresult Lwt.t =
+ fun ctxt ?(level = (Raw_context.current_level ctxt).level) tx_rollup ->
+  (*
+    [inbox_opt] checks whether or not [tx_rollup] is valid, so we
+    don’t have to do it here.
+   *)
+  get_opt ctxt ~level tx_rollup >>=? function
+  | (ctxt, Some res) -> return (ctxt, res)
+  | (_, None) -> fail (Tx_rollup_inbox_does_not_exist (tx_rollup, level))
+
+(* Error registration *)
+
+let () =
+  let open Data_encoding in
+  (* Tx_rollup_inbox_does_not_exist *)
+  register_error_kind
+    `Permanent
+    ~id:"tx_rollup_inbox_does_not_exist"
+    ~title:"Missing transaction rollup inbox"
+    ~description:"The transaction rollup does not have an inbox at this level"
+    ~pp:(fun ppf (addr, level) ->
+      Format.fprintf
+        ppf
+        "Transaction rollup %a does not have an inbox at level %a"
+        Tx_rollup_repr.pp
+        addr
+        Raw_level_repr.pp
+        level)
+    (obj2
+       (req "tx_rollup_address" Tx_rollup_repr.encoding)
+       (req "raw_level" Raw_level_repr.encoding))
+    (function
+      | Tx_rollup_inbox_does_not_exist (rollup, level) -> Some (rollup, level)
+      | _ -> None)
+    (fun (rollup, level) -> Tx_rollup_inbox_does_not_exist (rollup, level))

tezos/tezos-mirror

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -1521,52 +1521,6 @@ module Receipt : sig
   val group_balance_updates : balance_updates -> balance_updates tzresult
 end
 
-(** This module re-exports definitions from {!Tx_rollup_repr} and
-    {!Tx_rollup_storage}. *)
-module Tx_rollup : sig
-  include BASIC_DATA
-
-  type tx_rollup = t
-
-  val rpc_arg : tx_rollup RPC_arg.arg
-
-  val to_b58check : tx_rollup -> string
-
-  val of_b58check : string -> tx_rollup tzresult
-
-  val pp : Format.formatter -> tx_rollup -> unit
-
-  val encoding : tx_rollup Data_encoding.t
-
-  val originate : context -> (context * tx_rollup) tzresult Lwt.t
-
-  module Internal_for_tests : sig
-    (** see [tx_rollup_repr.originated_tx_rollup] for documentation *)
-    val originated_tx_rollup :
-      Origination_nonce.Internal_for_tests.t -> tx_rollup
-  end
-end
-
-(** This module re-exports definitions from {!Tx_rollup_state_repr}
-    and {!Tx_rollup_state_storage}. *)
-module Tx_rollup_state : sig
-  type t
-
-  val initial_state : t
-
-  val encoding : t Data_encoding.t
-
-  val pp : Format.formatter -> t -> unit
-
-  val find : context -> Tx_rollup.t -> (context * t option) tzresult Lwt.t
-
-  val get : context -> Tx_rollup.t -> (context * t) tzresult Lwt.t
-
-  type error +=
-    | Tx_rollup_already_exists of Tx_rollup.t
-    | Tx_rollup_does_not_exist of Tx_rollup.t
-end
-
 module Delegate : sig
   val init :
     context ->
@@ -2002,6 +1956,119 @@ module Destination : sig
   val in_memory_size : t -> Cache_memory_helpers.sint
 
   type error += Invalid_destination_b58check of string
+end
+
+(** This module re-exports definitions from {!Tx_rollup_repr} and
+    {!Tx_rollup_storage}. *)
+module Tx_rollup : sig
+  include BASIC_DATA
+
+  type tx_rollup = t
+
+  val rpc_arg : tx_rollup RPC_arg.arg
+
+  val to_b58check : tx_rollup -> string
+
+  val of_b58check : string -> tx_rollup tzresult
+
+  val pp : Format.formatter -> tx_rollup -> unit
+
+  val encoding : tx_rollup Data_encoding.t
+
+  val originate : context -> (context * tx_rollup) tzresult Lwt.t
+
+  module Internal_for_tests : sig
+    (** see [tx_rollup_repr.originated_tx_rollup] for documentation *)
+    val originated_tx_rollup :
+      Origination_nonce.Internal_for_tests.t -> tx_rollup
+  end
+end
+
+(** This module re-exports definitions from {!Tx_rollup_state_repr}
+    and {!Tx_rollup_state_storage}. *)
+module Tx_rollup_state : sig
+  type t
+
+  val initial_state : t
+
+  val encoding : t Data_encoding.t
+
+  val pp : Format.formatter -> t -> unit
+
+  val find : context -> Tx_rollup.t -> (context * t option) tzresult Lwt.t
+
+  val get : context -> Tx_rollup.t -> (context * t) tzresult Lwt.t
+
+  type error +=
+    | Tx_rollup_already_exists of Tx_rollup.t
+    | Tx_rollup_does_not_exist of Tx_rollup.t
+end
+
+(** This module re-exports definitions from {!Tx_rollup_message_repr}. *)
+module Tx_rollup_message : sig
+  type deposit = {
+    destination : Tx_rollup_l2_address.Indexable.t;
+    ticket_hash : Ticket_hash.t;
+    amount : int64;
+  }
+
+  type t = Batch of string | Deposit of deposit
+
+  val size : t -> int
+
+  val encoding : t Data_encoding.t
+
+  val pp : Format.formatter -> t -> unit
+
+  type hash
+
+  val hash_encoding : hash Data_encoding.t
+
+  val pp_hash : Format.formatter -> hash -> unit
+
+  val hash : t -> hash
+end
+
+(** This module re-exports definitions from {!Tx_rollup_inbox_repr} and
+    {!Tx_rollup_inbox_storage}. *)
+module Tx_rollup_inbox : sig
+  type t = {contents : Tx_rollup_message.hash list; cumulated_size : int}
+
+  val pp : Format.formatter -> t -> unit
+
+  val encoding : t Data_encoding.t
+
+  val append_message :
+    context ->
+    Tx_rollup.t ->
+    Tx_rollup_message.t ->
+    (int * context) tzresult Lwt.t
+
+  val messages :
+    context ->
+    level:[`Current | `Level of Raw_level.t] ->
+    Tx_rollup.t ->
+    (context * Tx_rollup_message.hash list) tzresult Lwt.t
+
+  val size :
+    context ->
+    level:[`Current | `Level of Raw_level.t] ->
+    Tx_rollup.t ->
+    (context * int) tzresult Lwt.t
+
+  val get :
+    context ->
+    level:[`Current | `Level of Raw_level.t] ->
+    Tx_rollup.t ->
+    (context * t) tzresult Lwt.t
+
+  val find :
+    context ->
+    level:[`Current | `Level of Raw_level.t] ->
+    Tx_rollup.t ->
+    (context * t option) tzresult Lwt.t
+
+  type error += Tx_rollup_inbox_does_not_exist of Tx_rollup.t * Raw_level.t
 end
 
 module Kind : sig

src/proto_alpha/lib_protocol/storage.mli
@@ -699,6 +699,37 @@ module Tx_rollup : sig
       with type key = Tx_rollup_repr.t
        and type value = Tx_rollup_state_repr.t
        and type t := Raw_context.t
+
+  (** The number of bytes allocated by the messages stored in each inbox. *)
+  module Inbox_cumulated_size :
+    Non_iterable_indexed_carbonated_data_storage
+      with type t := Raw_context.t * Raw_level_repr.t
+       and type key = Tx_rollup_repr.t
+       and type value = int
+
+  (** A carbonated storage to store the hashes of the messages
+      appended in an inbox, in reverse order.
+
+      The actual content is already stored in the block (as part of
+      the operations), so by only storing the hashes we avoid
+      unnecessary storage duplication. *)
+  module Inbox_rev_contents :
+    Non_iterable_indexed_carbonated_data_storage
+      with type t := Raw_context.t * Raw_level_repr.t
+       and type key = Tx_rollup_repr.t
+       and type value = Tx_rollup_message_repr.hash list
+
+  (** [fold (ctxt, level) ~order ~init ~f] traverses all rollups with
+      a nonempty inbox at [level].
+
+      No assurances whatsoever are provided regarding the order of
+      traversal. *)
+  val fold :
+    Raw_context.t ->
+    Raw_level_repr.t ->
+    init:'a ->
+    f:(Tx_rollup_repr.t -> 'a -> 'a Lwt.t) ->
+    'a Lwt.t
 end
 
 (** Smart contract rollup *)

src/proto_alpha/lib_protocol/tx_rollup_inbox_storage.ml
@@ -1,0 +1,164 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Marigold <contact@marigold.dev>                        *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type error +=
+  | Tx_rollup_inbox_does_not_exist of Tx_rollup_repr.t * Raw_level_repr.t
+
+let append_message :
+    Raw_context.t ->
+    Tx_rollup_repr.t ->
+    Tx_rollup_message_repr.t ->
+    (int * Raw_context.t) tzresult Lwt.t =
+ fun ctxt rollup message ->
+  let level = (Raw_context.current_level ctxt).level in
+  Storage.Tx_rollup.Inbox_cumulated_size.find (ctxt, level) rollup
+  >>=? fun (ctxt, msize) ->
+  let message_size = Tx_rollup_message_repr.size message in
+  let new_size = Option.value ~default:0 msize + message_size in
+  Storage.Tx_rollup.Inbox_rev_contents.find (ctxt, level) rollup
+  >>=? fun (ctxt, mcontents) ->
+  Storage.Tx_rollup.Inbox_rev_contents.add
+    (ctxt, level)
+    rollup
+    (Tx_rollup_message_repr.hash message :: Option.value ~default:[] mcontents)
+  >>=? fun (ctxt, _, _) ->
+  Storage.Tx_rollup.Inbox_cumulated_size.add (ctxt, level) rollup new_size
+  >>=? fun (ctxt, _, _) -> return (message_size, ctxt)
+
+let get_level :
+    Raw_context.t -> [`Current | `Level of Raw_level_repr.t] -> Raw_level_repr.t
+    =
+ fun ctxt -> function
+  | `Current -> (Raw_context.current_level ctxt).level
+  | `Level lvl -> lvl
+
+let messages_opt :
+    Raw_context.t ->
+    level:[`Current | `Level of Raw_level_repr.t] ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_message_repr.hash list option) tzresult Lwt.t =
+ fun ctxt ~level tx_rollup ->
+  let level = get_level ctxt level in
+  Storage.Tx_rollup.Inbox_rev_contents.find (ctxt, level) tx_rollup
+  >>=? function
+  | (ctxt, Some rev_contents) -> return (ctxt, Some (List.rev rev_contents))
+  | (ctxt, None) ->
+      (*
+        Prior to returning [None], we check whether or not the
+        transaction rollup address is valid, to raise the appropriate
+        if need be.
+       *)
+      Tx_rollup_state_storage.assert_exist ctxt tx_rollup >>=? fun ctxt ->
+      return (ctxt, None)
+
+let messages :
+    Raw_context.t ->
+    level:[`Current | `Level of Raw_level_repr.t] ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_message_repr.hash list) tzresult Lwt.t =
+ fun ctxt ~level tx_rollup ->
+  messages_opt ctxt ~level tx_rollup >>=? function
+  | (ctxt, Some messages) -> return (ctxt, messages)
+  | (_, None) ->
+      fail (Tx_rollup_inbox_does_not_exist (tx_rollup, get_level ctxt level))
+
+let size :
+    Raw_context.t ->
+    level:[`Current | `Level of Raw_level_repr.t] ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * int) tzresult Lwt.t =
+ fun ctxt ~level tx_rollup ->
+  let level = get_level ctxt level in
+  Storage.Tx_rollup.Inbox_cumulated_size.find (ctxt, level) tx_rollup
+  >>=? function
+  | (ctxt, Some cumulated_size) -> return (ctxt, cumulated_size)
+  | (ctxt, None) ->
+      (*
+        Prior to raising an error related to the missing inbox, we
+        check whether or not the transaction rollup address is valid,
+        to raise the appropriate if need be.
+       *)
+      Tx_rollup_state_storage.assert_exist ctxt tx_rollup >>=? fun _ctxt ->
+      fail (Tx_rollup_inbox_does_not_exist (tx_rollup, level))
+
+let find :
+    Raw_context.t ->
+    level:[`Current | `Level of Raw_level_repr.t] ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_inbox_repr.t option) tzresult Lwt.t =
+ fun ctxt ~level tx_rollup ->
+  let open Tx_rollup_inbox_repr in
+  (*
+    [messages_opt] checks whether or not [tx_rollup] is valid, so
+    we do not have to do it here.
+   *)
+  messages_opt ctxt ~level tx_rollup >>=? function
+  | (ctxt, Some contents) ->
+      size ctxt ~level tx_rollup >>=? fun (ctxt, cumulated_size) ->
+      return (ctxt, Some {cumulated_size; contents})
+  | (ctxt, None) -> return (ctxt, None)
+
+let get :
+    Raw_context.t ->
+    level:[`Current | `Level of Raw_level_repr.t] ->
+    Tx_rollup_repr.t ->
+    (Raw_context.t * Tx_rollup_inbox_repr.t) tzresult Lwt.t =
+ fun ctxt ~level tx_rollup ->
+  (*
+    [inbox_opt] checks whether or not [tx_rollup] is valid, so we
+    don’t have to do it here.
+   *)
+  find ctxt ~level tx_rollup >>=? function
+  | (ctxt, Some res) -> return (ctxt, res)
+  | (_, None) ->
+      fail (Tx_rollup_inbox_does_not_exist (tx_rollup, get_level ctxt level))
+
+(* Error registration *)
+
+let () =
+  let open Data_encoding in
+  (* Tx_rollup_inbox_does_not_exist *)
+  register_error_kind
+    `Permanent
+    ~id:"tx_rollup_inbox_does_not_exist"
+    ~title:"Missing transaction rollup inbox"
+    ~description:"The transaction rollup does not have an inbox at this level"
+    ~pp:(fun ppf (addr, level) ->
+      Format.fprintf
+        ppf
+        "Transaction rollup %a does not have an inbox at level %a"
+        Tx_rollup_repr.pp
+        addr
+        Raw_level_repr.pp
+        level)
+    (obj2
+       (req "tx_rollup_address" Tx_rollup_repr.encoding)
+       (req "raw_level" Raw_level_repr.encoding))
+    (function
+      | Tx_rollup_inbox_does_not_exist (rollup, level) -> Some (rollup, level)
+      | _ -> None)
+    (fun (rollup, level) -> Tx_rollup_inbox_does_not_exist (rollup, level))