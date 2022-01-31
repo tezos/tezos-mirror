OKEAMAH/Git

Proto,tx_rollup: Add a RPC to fetch the content of a inbox

src/proto_alpha/lib_protocol/tx_rollup_services.ml
@@ -37,6 +37,13 @@ module S = struct
       ~query:RPC_query.empty
       ~output:Tx_rollup_state.encoding
       RPC_path.(custom_root /: Tx_rollup.rpc_arg / "state")
+
+  let inbox =
+    RPC_service.get_service
+      ~description:"Get the inbox of a transaction rollup"
+      ~query:RPC_query.empty
+      ~output:Tx_rollup_inbox.encoding
+      RPC_path.(custom_root /: Tx_rollup.rpc_arg / "inbox")
 end
 
 let register () =
@@ -51,7 +58,17 @@ let register () =
                 We raise [Not_found] here, and let [resto] turn it
                 into a 404 error.
               *)
-              raise Not_found))
+              raise Not_found)) ;
+  register1 ~chunked:false S.inbox (fun ctxt tx_rollup () () ->
+      let level = (Level.current ctxt).level in
+      Tx_rollup_inbox.get_opt ctxt tx_rollup ~level >|=? function
+      | (_, Some x) -> x
+      | (_, None) ->
+          (*
+            We raise [Not_found] here, and let [resto] turn it
+            into a 404 error.
+           *)
+          raise Not_found)
 
 let state ctxt block tx_rollup =
   RPC_context.make_call1 S.state ctxt block tx_rollup () ()

src/proto_alpha/lib_protocol/tx_rollup_services.mli
@@ -32,4 +32,15 @@ val state :
   Tx_rollup.t ->
   Tx_rollup_state.t shell_tzresult Lwt.t
 
+(** Return the inbox for a transaction rollup for current level.
+
+    Return [Not_found] if the transaction rollup exists, but does not
+    have inbox at that level. Fail if the transaction rollup does not
+    exist. *)
+val inbox :
+  'a #RPC_context.simple ->
+  'a ->
+  Tx_rollup.t ->
+  Tx_rollup_inbox.t shell_tzresult Lwt.t
+
 val register : unit -> unit

tezos/tezos-mirror

Co-authored-by: Marigold <contact@marigold.dev>
Co-authored-by: Nomadic Labs <contact@nomadic-labs.com>
Co-authored-by: Oxhead Alpha <info@oxheadalpha.com>

src/proto_alpha/lib_protocol/tx_rollup_services.ml
@@ -37,12 +37,24 @@ module S = struct
       ~query:RPC_query.empty
       ~output:Tx_rollup_state.encoding
       RPC_path.(custom_root /: Tx_rollup.rpc_arg / "state")
+
+  let inbox =
+    RPC_service.get_service
+      ~description:"Get the inbox of a transaction rollup"
+      ~query:RPC_query.empty
+      ~output:Tx_rollup_inbox.encoding
+      RPC_path.(custom_root /: Tx_rollup.rpc_arg / "inbox")
 end
 
 let register () =
   let open Services_registration in
   opt_register1 ~chunked:false S.state (fun ctxt tx_rollup () () ->
-      Tx_rollup_state.find ctxt tx_rollup >|=? snd)
+      Tx_rollup_state.find ctxt tx_rollup >|=? snd) ;
+  opt_register1 ~chunked:false S.inbox (fun ctxt tx_rollup () () ->
+      Tx_rollup_inbox.find ctxt tx_rollup ~level:`Current >|=? snd)
 
 let state ctxt block tx_rollup =
   RPC_context.make_call1 S.state ctxt block tx_rollup () ()
+
+let inbox ctxt block tx_rollup =
+  RPC_context.make_call1 S.inbox ctxt block tx_rollup () ()

src/proto_alpha/lib_protocol/tx_rollup_services.mli
@@ -32,4 +32,15 @@ val state :
   Tx_rollup.t ->
   Tx_rollup_state.t shell_tzresult Lwt.t
 
+(** Returns the inbox for a transaction rollup for current level.
+
+    Returns [Not_found] if the transaction rollup exists, but does not
+    have inbox at that level. Fails if the transaction rollup does not
+    exist. *)
+val inbox :
+  'a #RPC_context.simple ->
+  'a ->
+  Tx_rollup.t ->
+  Tx_rollup_inbox.t shell_tzresult Lwt.t
+
 val register : unit -> unit