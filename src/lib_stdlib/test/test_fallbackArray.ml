d1176b1c81b7ccfbf723ef4daaebe4fc30c51a03
Richard Bonichon <richard.bonichon@nomadic-labs.com>
2022-02-02 11:23:16 +0100

    Stdlib: add [of_list] to FallbackArray
    As suggested by comment
    https://gitlab.com/tezos/tezos/-/merge_requests/3738#note_726861140
    
    This adds a simple test too.

--- src/lib_stdlib/test/test_fallbackArray.ml
+++ src/lib_stdlib/test/test_fallbackArray.ml
@@ -30,12 +30,42 @@
     Subject:      Fallback arrays
  *)
 
-module Tests = Test_arrays.Make (struct
-  include FallbackArray
+module Tests = struct
+  include Test_arrays.Make (struct
+    include FallbackArray
 
-  let set a i v =
-    set a i v ;
-    a
-end)
+    let set a i v =
+      set a i v ;
+      a
+  end)
+
+  open Alcotest
+
+  let check_of_list () =
+    let l = Array.to_list (Array.init 100 (fun i -> i)) in
+    let open FallbackArray in
+    let fallback = -1 in
+    let farr = of_list ~fallback ~proj:(fun e -> e) l in
+    let farr_fallback = FallbackArray.fallback farr in
+    if not Compare.Int.(farr_fallback = fallback) then
+      fail
+        (Printf.sprintf "fallback should be %d (is %d)" fallback farr_fallback) ;
+    let i = ref 0 in
+    iter
+      (fun e ->
+        let expected = List.nth l !i in
+        if not Compare.Int.(expected = e) then
+          fail
+            (Printf.sprintf
+               "at index %d, fallback array (%d) disagrees with original list \
+                (%d)"
+               !i
+               e
+               expected)
+        else incr i)
+      farr
+
+  let tests = ("of_list", `Quick, check_of_list) :: tests
+end
 
 let () = Alcotest.run "stdlib" [("FallbackArray", Tests.tests)]
