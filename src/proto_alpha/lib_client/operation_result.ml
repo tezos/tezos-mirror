OKEAMAH/Git

Tx_rollup: Commitment operation
Co-author-by: Marigold <contact@marigold.dev>
Co-author-by: Nomadic Labs <contact@nomadic-labs.com>
Co-author-by: Oxhead Alpha <info@oxheadalpha.com>

src/proto_alpha/lib_client/operation_result.ml
@@ -187,6 +187,20 @@ let pp_manager_operation_content (type kind) source internal pp_result ppf
         source
         pp_result
         result
+  | Tx_rollup_commit {rollup; commitment} ->
+      Format.fprintf
+        ppf
+        "@[<v 2>%s:%a, %a@,From: %a%a@]"
+        (if internal then "Internal tx rollup commitment"
+        else "Tx rollup commitment")
+        Tx_rollup.pp
+        rollup
+        Tx_rollup_commitments.Commitment.pp
+        commitment
+        Contract.pp
+        source
+        pp_result
+        result
   | Sc_rollup_originate {kind; boot_sector} ->
       let (module R : Sc_rollups.PVM.S) = Sc_rollups.of_kind kind in
       Format.fprintf
@@ -456,6 +470,15 @@ let pp_manager_operation_contents_and_result ppf
       balance_updates ;
     Format.fprintf ppf "@,Consumed gas: %a" Gas.Arith.pp consumed_gas
   in
+  let pp_tx_rollup_commit_result
+      (Tx_rollup_commit_result {balance_updates; consumed_gas}) =
+    Format.fprintf
+      ppf
+      "@,Balance updates:@,  %a"
+      pp_balance_updates
+      balance_updates ;
+    Format.fprintf ppf "@,Consumed gas: %a" Gas.Arith.pp consumed_gas
+  in
   let pp_sc_rollup_originate_result
       (Sc_rollup_originate_result
         {address; consumed_gas; size; balance_updates}) =
@@ -553,6 +576,17 @@ let pp_manager_operation_contents_and_result ppf
           "@[<v 0>This rollup submit operation was BACKTRACKED, its expected \
            effects (as follow) were NOT applied.@]" ;
         pp_tx_rollup_submit_batch_result op
+    | Applied (Tx_rollup_commit_result _ as op) ->
+        Format.fprintf
+          ppf
+          "This tx rollup commit operation was successfully applied" ;
+        pp_tx_rollup_commit_result op
+    | Backtracked ((Tx_rollup_commit_result _ as op), _err) ->
+        Format.fprintf
+          ppf
+          "@[<v 0>This tx rollup commit rollup operation was BACKTRACKED, its \
+           expected effects (as follow) were NOT applied.@]" ;
+        pp_tx_rollup_commit_result op
     | Applied (Sc_rollup_originate_result _ as op) ->
         Format.fprintf
           ppf

src/proto_alpha/lib_protocol/operation_repr.ml
@@ -75,6 +75,8 @@ module Kind = struct
 
   type tx_rollup_submit_batch = Tx_rollup_submit_batch_kind
 
+  type tx_rollup_commit = Tx_rollup_commit_kind
+
   type sc_rollup_originate = Sc_rollup_originate_kind
 
   type sc_rollup_add_messages = Sc_rollup_add_messages_kind
@@ -88,6 +90,7 @@ module Kind = struct
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
     | Tx_rollup_origination_manager_kind : tx_rollup_origination manager
     | Tx_rollup_submit_batch_manager_kind : tx_rollup_submit_batch manager
+    | Tx_rollup_commit_manager_kind : tx_rollup_commit manager
     | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
     | Sc_rollup_add_messages_manager_kind : sc_rollup_add_messages manager
 end
@@ -273,6 +276,11 @@ and _ manager_operation =
       content : string;
     }
       -> Kind.tx_rollup_submit_batch manager_operation
+  | Tx_rollup_commit : {
+      rollup : Tx_rollup_repr.t;
+      commitment : Tx_rollup_commitments_repr.Commitment.t;
+    }
+      -> Kind.tx_rollup_commit manager_operation
   | Sc_rollup_originate : {
       kind : Sc_rollup_repr.Kind.t;
       boot_sector : Sc_rollup_repr.PVM.boot_sector;
@@ -296,6 +304,7 @@ let manager_kind : type kind. kind manager_operation -> kind Kind.manager =
   | Set_deposits_limit _ -> Kind.Set_deposits_limit_manager_kind
   | Tx_rollup_origination -> Kind.Tx_rollup_origination_manager_kind
   | Tx_rollup_submit_batch _ -> Kind.Tx_rollup_submit_batch_manager_kind
+  | Tx_rollup_commit _ -> Kind.Tx_rollup_commit_manager_kind
   | Sc_rollup_originate _ -> Kind.Sc_rollup_originate_manager_kind
   | Sc_rollup_add_messages _ -> Kind.Sc_rollup_add_messages_manager_kind
 
@@ -543,6 +552,25 @@ module Encoding = struct
               Tx_rollup_submit_batch {tx_rollup; content});
         }
 
+    let[@coq_axiom_with_reason "gadt"] tx_rollup_commit_case =
+      MCase
+        {
+          tag = tx_rollup_operation_tag_offset + 2;
+          name = "tx_rollup_commit";
+          encoding =
+            obj2
+              (req "rollup" Tx_rollup_repr.encoding)
+              (req "commitment" Tx_rollup_commitments_repr.Commitment.encoding);
+          select =
+            (function
+            | Manager (Tx_rollup_commit _ as op) -> Some op | _ -> None);
+          proj =
+            (function
+            | Tx_rollup_commit {rollup; commitment} -> (rollup, commitment));
+          inj =
+            (fun (rollup, commitment) -> Tx_rollup_commit {rollup; commitment});
+        }
+
     let[@coq_axiom_with_reason "gadt"] sc_rollup_originate_case =
       MCase
         {
@@ -603,6 +631,7 @@ module Encoding = struct
           make set_deposits_limit_case;
           make tx_rollup_origination_case;
           make tx_rollup_submit_batch_case;
+          make tx_rollup_commit_case;
           make sc_rollup_originate_case;
           make sc_rollup_add_messages_case;
         ]
@@ -913,6 +942,11 @@ module Encoding = struct
       (tx_rollup_operation_tag_offset + 1)
       Manager_operations.tx_rollup_submit_batch_case
 
+  let tx_rollup_commit_case =
+    make_manager_case
+      (tx_rollup_operation_tag_offset + 2)
+      Manager_operations.tx_rollup_commit_case
+
   let sc_rollup_originate_case =
     make_manager_case
       sc_rollup_operation_origination_tag
@@ -953,6 +987,7 @@ module Encoding = struct
            make register_global_constant_case;
            make tx_rollup_origination_case;
            make tx_rollup_submit_batch_case;
+           make tx_rollup_commit_case;
            make sc_rollup_originate_case;
            make sc_rollup_add_messages_case;
          ]
@@ -1158,6 +1193,8 @@ let equal_manager_operation_kind :
   | (Tx_rollup_origination, _) -> None
   | (Tx_rollup_submit_batch _, Tx_rollup_submit_batch _) -> Some Eq
   | (Tx_rollup_submit_batch _, _) -> None
+  | (Tx_rollup_commit _, Tx_rollup_commit _) -> Some Eq
+  | (Tx_rollup_commit _, _) -> None
   | (Sc_rollup_originate _, Sc_rollup_originate _) -> Some Eq
   | (Sc_rollup_originate _, _) -> None
   | (Sc_rollup_add_messages _, Sc_rollup_add_messages _) -> Some Eq
@@ -1269,6 +1306,9 @@ let internal_manager_operation_size (type a) (op : a manager_operation) =
       assert false
   | Tx_rollup_submit_batch _ ->
       (* Tx_rollup_submit_batch operation can’t occur as internal operations *)
+      assert false
+  | Tx_rollup_commit _ ->
+      (* Tx_rollup_commit operation can’t occur as internal operations *)
       assert false
 
 let packed_internal_operation_in_memory_size :

src/proto_alpha/lib_protocol/operation_repr.mli
@@ -101,6 +101,8 @@ module Kind : sig
 
   type tx_rollup_submit_batch = Tx_rollup_submit_batch_kind
 
+  type tx_rollup_commit = Tx_rollup_commit_kind
+
   type sc_rollup_originate = Sc_rollup_originate_kind
 
   type sc_rollup_add_messages = Sc_rollup_add_messages_kind
@@ -114,6 +116,7 @@ module Kind : sig
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
     | Tx_rollup_origination_manager_kind : tx_rollup_origination manager
     | Tx_rollup_submit_batch_manager_kind : tx_rollup_submit_batch manager
+    | Tx_rollup_commit_manager_kind : tx_rollup_commit manager
     | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
     | Sc_rollup_add_messages_manager_kind : sc_rollup_add_messages manager
 end
@@ -252,6 +255,11 @@ and _ manager_operation =
       content : string;
     }
       -> Kind.tx_rollup_submit_batch manager_operation
+  | Tx_rollup_commit : {
+      rollup : Tx_rollup_repr.t;
+      commitment : Tx_rollup_commitments_repr.Commitment.t;
+    }
+      -> Kind.tx_rollup_commit manager_operation
   | Sc_rollup_originate : {
       kind : Sc_rollup_repr.Kind.t;
       boot_sector : Sc_rollup_repr.PVM.boot_sector;
@@ -386,6 +394,8 @@ module Encoding : sig
   val tx_rollup_submit_batch_case :
     Kind.tx_rollup_submit_batch Kind.manager case
 
+  val tx_rollup_commit_case : Kind.tx_rollup_commit Kind.manager case
+
   val sc_rollup_originate_case : Kind.sc_rollup_originate Kind.manager case
 
   val sc_rollup_add_messages_case :
@@ -418,6 +428,8 @@ module Encoding : sig
     val tx_rollup_origination_case : Kind.tx_rollup_origination case
 
     val tx_rollup_submit_batch_case : Kind.tx_rollup_submit_batch case
+
+    val tx_rollup_commit_case : Kind.tx_rollup_commit case
 
     val sc_rollup_originate_case : Kind.sc_rollup_originate case
 

src/proto_alpha/lib_protocol/test/helpers/op.ml
@@ -551,3 +551,18 @@ let sc_rollup_origination ?counter ?fee ?gas_limit ?storage_limit ctxt
   >>=? fun to_sign_op ->
   Context.Contract.manager ctxt src >|=? fun account ->
   sign account.sk ctxt to_sign_op
+
+let tx_rollup_commit ?counter ?fee ?gas_limit ?storage_limit ctxt
+    (source : Contract.t) (rollup : Tx_rollup.t)
+    (commitment : Tx_rollup_commitments.Commitment.t) =
+  manager_operation
+    ?counter
+    ?fee
+    ?gas_limit
+    ?storage_limit
+    ~source
+    ctxt
+    (Tx_rollup_commit {rollup; commitment})
+  >>=? fun to_sign_op ->
+  Context.Contract.manager ctxt source >|=? fun account ->
+  sign account.sk ctxt to_sign_op

tezos/tezos-mirror

src/proto_alpha/lib_client/operation_result.ml
@@ -187,6 +187,20 @@ let pp_manager_operation_content (type kind) source internal pp_result ppf
         source
         pp_result
         result
+  | Tx_rollup_commit {tx_rollup; commitment} ->
+      Format.fprintf
+        ppf
+        "@[<v 2>%s:%a, %a@,From: %a%a@]"
+        (if internal then "Internal tx rollup commitment"
+        else "Tx rollup commitment")
+        Tx_rollup.pp
+        tx_rollup
+        Tx_rollup_commitments.Commitment.pp
+        commitment
+        Contract.pp
+        source
+        pp_result
+        result
   | Sc_rollup_originate {kind; boot_sector} ->
       let (module R : Sc_rollups.PVM.S) = Sc_rollups.of_kind kind in
       Format.fprintf
@@ -451,6 +465,15 @@ let pp_manager_operation_contents_and_result ppf
       balance_updates ;
     Format.fprintf ppf "@,Consumed gas: %a" Gas.Arith.pp consumed_gas
   in
+  let pp_tx_rollup_commit_result
+      (Tx_rollup_commit_result {balance_updates; consumed_gas}) =
+    Format.fprintf
+      ppf
+      "@,Balance updates:@,  %a"
+      pp_balance_updates
+      balance_updates ;
+    Format.fprintf ppf "@,Consumed gas: %a" Gas.Arith.pp consumed_gas
+  in
   let pp_sc_rollup_originate_result
       (Sc_rollup_originate_result
         {address; consumed_gas; size; balance_updates}) =
@@ -548,6 +571,17 @@ let pp_manager_operation_contents_and_result ppf
           "@[<v 0>This rollup submit operation was BACKTRACKED, its expected \
            effects (as follow) were NOT applied.@]" ;
         pp_tx_rollup_submit_batch_result op
+    | Applied (Tx_rollup_commit_result _ as op) ->
+        Format.fprintf
+          ppf
+          "This tx rollup commit operation was successfully applied" ;
+        pp_tx_rollup_commit_result op
+    | Backtracked ((Tx_rollup_commit_result _ as op), _err) ->
+        Format.fprintf
+          ppf
+          "@[<v 0>This tx rollup commit rollup operation was BACKTRACKED, its \
+           expected effects (as follow) were NOT applied.@]" ;
+        pp_tx_rollup_commit_result op
     | Applied (Sc_rollup_originate_result _ as op) ->
         Format.fprintf
           ppf

src/proto_alpha/lib_protocol/operation_repr.ml
@@ -75,6 +75,8 @@ module Kind = struct
 
   type tx_rollup_submit_batch = Tx_rollup_submit_batch_kind
 
+  type tx_rollup_commit = Tx_rollup_commit_kind
+
   type sc_rollup_originate = Sc_rollup_originate_kind
 
   type sc_rollup_add_messages = Sc_rollup_add_messages_kind
@@ -88,6 +90,7 @@ module Kind = struct
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
     | Tx_rollup_origination_manager_kind : tx_rollup_origination manager
     | Tx_rollup_submit_batch_manager_kind : tx_rollup_submit_batch manager
+    | Tx_rollup_commit_manager_kind : tx_rollup_commit manager
     | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
     | Sc_rollup_add_messages_manager_kind : sc_rollup_add_messages manager
 end
@@ -273,6 +276,11 @@ and _ manager_operation =
       content : string;
     }
       -> Kind.tx_rollup_submit_batch manager_operation
+  | Tx_rollup_commit : {
+      tx_rollup : Tx_rollup_repr.t;
+      commitment : Tx_rollup_commitments_repr.Commitment.t;
+    }
+      -> Kind.tx_rollup_commit manager_operation
   | Sc_rollup_originate : {
       kind : Sc_rollup_repr.Kind.t;
       boot_sector : Sc_rollup_repr.PVM.boot_sector;
@@ -296,6 +304,7 @@ let manager_kind : type kind. kind manager_operation -> kind Kind.manager =
   | Set_deposits_limit _ -> Kind.Set_deposits_limit_manager_kind
   | Tx_rollup_origination -> Kind.Tx_rollup_origination_manager_kind
   | Tx_rollup_submit_batch _ -> Kind.Tx_rollup_submit_batch_manager_kind
+  | Tx_rollup_commit _ -> Kind.Tx_rollup_commit_manager_kind
   | Sc_rollup_originate _ -> Kind.Sc_rollup_originate_manager_kind
   | Sc_rollup_add_messages _ -> Kind.Sc_rollup_add_messages_manager_kind
 
@@ -543,6 +552,26 @@ module Encoding = struct
               Tx_rollup_submit_batch {tx_rollup; content});
         }
 
+    let[@coq_axiom_with_reason "gadt"] tx_rollup_commit_case =
+      MCase
+        {
+          tag = tx_rollup_operation_tag_offset + 2;
+          name = "tx_rollup_commit";
+          encoding =
+            obj2
+              (req "rollup" Tx_rollup_repr.encoding)
+              (req "commitment" Tx_rollup_commitments_repr.Commitment.encoding);
+          select =
+            (function
+            | Manager (Tx_rollup_commit _ as op) -> Some op | _ -> None);
+          proj =
+            (function
+            | Tx_rollup_commit {tx_rollup; commitment} -> (tx_rollup, commitment));
+          inj =
+            (fun (tx_rollup, commitment) ->
+              Tx_rollup_commit {tx_rollup; commitment});
+        }
+
     let[@coq_axiom_with_reason "gadt"] sc_rollup_originate_case =
       MCase
         {
@@ -603,6 +632,7 @@ module Encoding = struct
           make set_deposits_limit_case;
           make tx_rollup_origination_case;
           make tx_rollup_submit_batch_case;
+          make tx_rollup_commit_case;
           make sc_rollup_originate_case;
           make sc_rollup_add_messages_case;
         ]
@@ -913,6 +943,11 @@ module Encoding = struct
       (tx_rollup_operation_tag_offset + 1)
       Manager_operations.tx_rollup_submit_batch_case
 
+  let tx_rollup_commit_case =
+    make_manager_case
+      (tx_rollup_operation_tag_offset + 2)
+      Manager_operations.tx_rollup_commit_case
+
   let sc_rollup_originate_case =
     make_manager_case
       sc_rollup_operation_origination_tag
@@ -953,6 +988,7 @@ module Encoding = struct
            make register_global_constant_case;
            make tx_rollup_origination_case;
            make tx_rollup_submit_batch_case;
+           make tx_rollup_commit_case;
            make sc_rollup_originate_case;
            make sc_rollup_add_messages_case;
          ]
@@ -1158,6 +1194,8 @@ let equal_manager_operation_kind :
   | (Tx_rollup_origination, _) -> None
   | (Tx_rollup_submit_batch _, Tx_rollup_submit_batch _) -> Some Eq
   | (Tx_rollup_submit_batch _, _) -> None
+  | (Tx_rollup_commit _, Tx_rollup_commit _) -> Some Eq
+  | (Tx_rollup_commit _, _) -> None
   | (Sc_rollup_originate _, Sc_rollup_originate _) -> Some Eq
   | (Sc_rollup_originate _, _) -> None
   | (Sc_rollup_add_messages _, Sc_rollup_add_messages _) -> Some Eq
@@ -1269,6 +1307,9 @@ let internal_manager_operation_size (type a) (op : a manager_operation) =
       assert false
   | Tx_rollup_submit_batch _ ->
       (* Tx_rollup_submit_batch operation can’t occur as internal operations *)
+      assert false
+  | Tx_rollup_commit _ ->
+      (* Tx_rollup_commit operation can’t occur as internal operations *)
       assert false
 
 let packed_internal_operation_in_memory_size :

src/proto_alpha/lib_protocol/operation_repr.mli
@@ -101,6 +101,8 @@ module Kind : sig
 
   type tx_rollup_submit_batch = Tx_rollup_submit_batch_kind
 
+  type tx_rollup_commit = Tx_rollup_commit_kind
+
   type sc_rollup_originate = Sc_rollup_originate_kind
 
   type sc_rollup_add_messages = Sc_rollup_add_messages_kind
@@ -114,6 +116,7 @@ module Kind : sig
     | Set_deposits_limit_manager_kind : set_deposits_limit manager
     | Tx_rollup_origination_manager_kind : tx_rollup_origination manager
     | Tx_rollup_submit_batch_manager_kind : tx_rollup_submit_batch manager
+    | Tx_rollup_commit_manager_kind : tx_rollup_commit manager
     | Sc_rollup_originate_manager_kind : sc_rollup_originate manager
     | Sc_rollup_add_messages_manager_kind : sc_rollup_add_messages manager
 end
@@ -252,6 +255,11 @@ and _ manager_operation =
       content : string;
     }
       -> Kind.tx_rollup_submit_batch manager_operation
+  | Tx_rollup_commit : {
+      tx_rollup : Tx_rollup_repr.t;
+      commitment : Tx_rollup_commitments_repr.Commitment.t;
+    }
+      -> Kind.tx_rollup_commit manager_operation
   | Sc_rollup_originate : {
       kind : Sc_rollup_repr.Kind.t;
       boot_sector : Sc_rollup_repr.PVM.boot_sector;
@@ -386,6 +394,8 @@ module Encoding : sig
   val tx_rollup_submit_batch_case :
     Kind.tx_rollup_submit_batch Kind.manager case
 
+  val tx_rollup_commit_case : Kind.tx_rollup_commit Kind.manager case
+
   val sc_rollup_originate_case : Kind.sc_rollup_originate Kind.manager case
 
   val sc_rollup_add_messages_case :
@@ -418,6 +428,8 @@ module Encoding : sig
     val tx_rollup_origination_case : Kind.tx_rollup_origination case
 
     val tx_rollup_submit_batch_case : Kind.tx_rollup_submit_batch case
+
+    val tx_rollup_commit_case : Kind.tx_rollup_commit case
 
     val sc_rollup_originate_case : Kind.sc_rollup_originate case
 

src/proto_alpha/lib_protocol/test/helpers/op.ml
@@ -538,3 +538,18 @@ let sc_rollup_origination ?counter ?fee ?gas_limit ?storage_limit ctxt
   >>=? fun to_sign_op ->
   Context.Contract.manager ctxt src >|=? fun account ->
   sign account.sk ctxt to_sign_op
+
+let tx_rollup_commit ?counter ?fee ?gas_limit ?storage_limit ctxt
+    (source : Contract.t) (tx_rollup : Tx_rollup.t)
+    (commitment : Tx_rollup_commitments.Commitment.t) =
+  manager_operation
+    ?counter
+    ?fee
+    ?gas_limit
+    ?storage_limit
+    ~source
+    ctxt
+    (Tx_rollup_commit {tx_rollup; commitment})
+  >>=? fun to_sign_op ->
+  Context.Contract.manager ctxt source >|=? fun account ->
+  sign account.sk ctxt to_sign_op