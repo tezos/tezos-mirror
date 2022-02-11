379974a97d202685b85184b314d342cabe7a3ee0
xvw <xavier.vdw@gmail.com>
2022-02-11 17:02:25 +0100

    Proto,tx_rollup,tezt: test inboxes reconstruction
    Co-authored-by: Marigold <contact@marigold.dev>
    Co-authored-by: Nomadic Labs <contact@nomadic-labs.com>
    Co-authored-by: Oxhead Alpha <info@oxheadalpha.com>

---
+++ tezt/lib_tezos/tx_rollup_node.mli
@@ -0,0 +1,61 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(* Copyright (c) 2022 Marigold, <contact@marigold.dev>                       *)
+(* Copyright (c) 2022 Oxhead Alpha <info@oxhead-alpha.com>                   *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type t
+
+val create :
+  ?path:string ->
+  ?runner:Runner.t ->
+  ?data_dir:string ->
+  ?addr:string ->
+  ?port:int ->
+  ?dormant_mode:bool ->
+  ?color:Log.Color.t ->
+  ?event_pipe:string ->
+  ?name:string ->
+  rollup_id:string ->
+  rollup_genesis:string ->
+  operator:string ->
+  Client.t ->
+  Node.t ->
+  t
+
+(** Returns the node's endpoint. *)
+val endpoint : t -> string
+
+(** Wait until the node is ready.
+
+    More precisely, wait until a [node_is_ready] event occurs.
+    If such an event already occurred, return immediately. *)
+val wait_for_ready : t -> unit Lwt.t
+
+(** Connected to a tezos node.
+    Returns the name of the configuration file. *)
+val config_init : t -> string -> string -> string Lwt.t
+
+(** [run node] launches the given transaction rollup node. *)
+val run : t -> unit Lwt.t
