7d61e439782f01f7a50a6788af3a946018dd97ec
François Thiré <francois.thire@nomadic-labs.com>
2022-02-12 21:22:38 +0100

    Alpha/Accuser: Patch the accuser to start the event sooner

--- src/proto_alpha/lib_delegate/client_daemon.ml
+++ src/proto_alpha/lib_delegate/client_daemon.ml
@@ -115,12 +115,6 @@ module Accuser = struct
   let run (cctxt : #Protocol_client_context.full) ~chain ~preserved_levels
       ~keep_alive =
     let process () =
-      Client_baking_blocks.monitor_valid_blocks
-        ~next_protocols:(Some [Protocol.hash])
-        cctxt
-        ~chains:[chain]
-        ()
-      >>=? fun valid_blocks_stream ->
       cctxt#message
         "Accuser v%s (%s) for %a started."
         Tezos_version.Version.current_string
@@ -128,6 +122,12 @@ module Accuser = struct
         Protocol_hash.pp_short
         Protocol.hash
       >>= fun () ->
+      Client_baking_blocks.monitor_valid_blocks
+        ~next_protocols:(Some [Protocol.hash])
+        cctxt
+        ~chains:[chain]
+        ()
+      >>=? fun valid_blocks_stream ->
       let canceler = Lwt_canceler.create () in
       let _ =
         Lwt_exit.register_clean_up_callback ~loc:__LOC__ (fun _ ->
