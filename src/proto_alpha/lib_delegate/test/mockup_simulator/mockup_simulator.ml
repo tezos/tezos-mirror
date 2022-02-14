55a49e4984d32e5d3000da87d0df1a2f4b47d630
Alain Mebsout <alain.mebsout@functori.com>
2022-02-14 15:32:01 +0100

    Tests: Mockup simulator: double timeout (at least) Default is now 30 seconds.

--- src/proto_alpha/lib_delegate/test/mockup_simulator/mockup_simulator.ml
+++ src/proto_alpha/lib_delegate/test/mockup_simulator/mockup_simulator.ml
@@ -1053,7 +1053,7 @@ let default_config =
     (* Rounds should be long enough for the bakers to
        exchange all the necessary messages. *)
     round1 = 3L (* No real need to increase round durations. *);
-    timeout = 10;
+    timeout = 30;
     delegate_selection = [];
     initial_seed = None;
     consensus_committee_size =
