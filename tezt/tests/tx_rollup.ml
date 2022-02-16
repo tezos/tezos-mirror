54d609a37d6372dc2dbda4a293bd9af99373fcd8
François Thiré <francois.thire@nomadic-labs.com>
2022-02-16 15:28:48 +0100

    Tezt/Tx_rollups: Refactor regression tests

--- tezt/tests/tx_rollup.ml
+++ tezt/tests/tx_rollup.ml
@@ -32,7 +32,144 @@
    Subject:      .
 *)
 
-(*                               utils                                       *)
+(** To be attached to process whose output needs to be captured by the
+    regression framework. *)
+let hooks = Tezos_regression.hooks
+
+open Rollup
+module Rollup = Tx_rollup
+
+let parameter_file protocol =
+  Protocol.write_parameter_file
+    ~base:(Either.right (protocol, None))
+    [(["tx_rollup_enable"], Some "true")]
+
+(* This module only registers regressions tests. Those regressions
+   tests should be used to ensure there is no regressions with the
+   various RPCs exported by the tx_rollups. *)
+module Regressions = struct
+  type t = {node : Node.t; client : Client.t; rollup : string}
+
+  let init_with_tx_rollup ~protocol =
+    let* parameter_file = parameter_file protocol in
+    let* (node, client) =
+      Client.init_with_protocol ~parameter_file `Client ~protocol ()
+    in
+    (* We originate a dumb rollup to be able to generate a paths for
+       tx_rollups related RPCs. *)
+    let* rollup =
+      Client.originate_tx_rollup
+        ~burn_cap:Tez.(of_int 9999999)
+        ~storage_limit:60_000
+        ~src:Constant.bootstrap1.public_key_hash
+        client
+    in
+    let* () = Client.bake_for client in
+    let* _ = Node.wait_for_level node 2 in
+    return {node; client; rollup}
+
+  let rpc_state ~protocols =
+    Protocol.register_regression_test
+      ~__FILE__
+      ~output_file:"tx_rollup_rpc_state"
+      ~title:"RPC (tx_rollups, regression) - state"
+      ~tags:["tx_rollup"; "rpc"]
+      ~protocols
+    @@ fun protocol ->
+    let* {node = _; client; rollup} = init_with_tx_rollup ~protocol in
+    let* _state = Rollup.get_state ~hooks ~rollup client in
+    return ()
+
+  let submit_batch ~batch {rollup; client; node} =
+    let* () =
+      Client.submit_tx_rollup_batch
+        ~hooks
+        ~content:batch
+        ~rollup
+        ~src:Constant.bootstrap1.public_key_hash
+        client
+    in
+    let current_level = Node.get_level node in
+    let* () = Client.bake_for client in
+    let* _ = Node.wait_for_level node (current_level + 1) in
+    return ()
+
+  let rpc_inbox ~protocols =
+    Protocol.register_regression_test
+      ~__FILE__
+      ~output_file:"tx_rollup_rpc_inbox"
+      ~title:"RPC (tx_rollups, regression) - inbox"
+      ~tags:["tx_rollup"; "rpc"; "inbox"]
+      ~protocols
+    @@ fun protocol ->
+    let* ({rollup; client; node = _} as state) =
+      init_with_tx_rollup ~protocol
+    in
+    (* The content of the batch does not matter for the regression test. *)
+    let batch = "blob" in
+    let* () = submit_batch ~batch state in
+    let* _state = Rollup.get_inbox ~hooks ~rollup client in
+    unit
+
+  let submit_commitment ~level ~roots ~predecessor {rollup; client; node} =
+    let* () =
+      Client.submit_tx_rollup_commitment
+        ~hooks
+        ~level
+        ~roots
+        ~predecessor
+        ~rollup
+        ~src:Constant.bootstrap1.public_key_hash
+        client
+    in
+    let current_level = Node.get_level node in
+    let* () = Client.bake_for client in
+    let* _ = Node.wait_for_level node (current_level + 1) in
+    return ()
+
+  let rpc_commitment ~protocols =
+    Protocol.register_regression_test
+      ~__FILE__
+      ~output_file:"tx_rollup_rpc_commitment"
+      ~title:"RPC (tx_rollups, regression) - commitment"
+      ~tags:["tx_rollup"; "rpc"; "commitment"]
+      ~protocols
+    @@ fun protocol ->
+    let* ({rollup; client; node} as state) = init_with_tx_rollup ~protocol in
+    (* The content of the batch does not matter for the regression test. *)
+    let batch = "blob" in
+    let* () = submit_batch ~batch state in
+    let batch_level = Node.get_level node in
+    (* FIXME https://gitlab.com/tezos/tezos/-/issues/2503
+
+       we introduce two bakes to ensure the block is finalised. This
+       should be removed once we do not rely on Tenderbake anymore. *)
+    let* () = Client.bake_for client in
+    let* () = Client.bake_for client in
+    (* FIXME https://gitlab.com/tezos/tezos/-/issues/2503
+
+       At the same time we remove the dependency to Tenderbake for
+       commitment, we will ensure the root is indeed the root of the
+       previous inbox. I don't know yet how we will be able to do that
+       yes, something is missing. *)
+    let* () =
+      submit_commitment
+        ~level:batch_level
+        ~roots:["root"]
+        ~predecessor:None
+        state
+    in
+    let offset = Node.get_level node - batch_level in
+    let* _state =
+      Rollup.get_commitments ~hooks ~block:"head" ~offset ~rollup client
+    in
+    unit
+
+  let register ~protocols =
+    rpc_state ~protocols ;
+    rpc_inbox ~protocols ;
+    rpc_commitment ~protocols
+end
 
 (** To be attached to process whose output needs to be captured by the
     regression framework. *)
@@ -80,11 +217,6 @@ let get_node_inbox node =
       in
       return {cumulated_size; contents}
 
-let parameter_file protocol =
-  Protocol.write_parameter_file
-    ~base:(Either.right (protocol, None))
-    [(["tx_rollup_enable"], Some "true")]
-
 let test ~__FILE__ ?output_file ?(tags = []) title k =
   match output_file with
   | None ->
@@ -113,13 +245,9 @@ let test_with_setup ~__FILE__ ?output_file ?(tags = []) title f =
       setup ~protocol (fun node client bootstrap_key bootstrap2_key ->
           f protocol node client bootstrap_key bootstrap2_key))
 
-(*                               test                                        *)
-
 let test_submit_batch ~protocols =
-  let open Tezt_tezos in
-  Protocol.register_regression_test
+  Protocol.register_test
     ~__FILE__
-    ~output_file:"tx_rollup_simple_use_case"
     ~title:"Simple use case"
     ~tags:["tx_rollup"]
     ~protocols
@@ -420,6 +548,7 @@ let test_tx_node_store_inbox =
       unit)
 
 let register ~protocols =
+  Regressions.register ~protocols ;
   test_submit_batch ~protocols ;
   test_invalid_rollup_address ~protocols ;
   test_submit_from_originated_source ~protocols ;
