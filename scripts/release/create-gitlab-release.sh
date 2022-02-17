c08e48b89d0d22fdde44bb29ea2f42b9c328cd0f
mujx <374817-mujx@users.noreply.gitlab.com>
2022-02-17 16:24:27 +0200

    Verify generated release links

--- scripts/release/create-gitlab-release.sh
+++ scripts/release/create-gitlab-release.sh
@@ -2,46 +2,48 @@
 
 # release the source tarball and static binaries on the gitlab registry
 
-set -e
+set -eu
 
-binaries=
-. scripts/release/binaries.sh
+# binaries=
+# . scripts/release/binaries.sh
 
 assets=
 
-source="tezos-source-$CI_COMMIT_SHORT_SHA.tgz"
-asset_json="$(jq -n --arg name "$source (source)" \
-                    --arg url "$PACKAGE_REGISTRY_URL/$source" \
-                        '{name: $name, url: $url}')"
-assets="$assets --assets-link='$asset_json'"
+# source="tezos-source-$CI_COMMIT_SHORT_SHA.tgz"
+# asset_json="$(jq -n --arg name "$source (source)" \
+#                     --arg url "$PACKAGE_REGISTRY_URL/$source" \
+#                         '{name: $name, url: $url}')"
+# assets="$assets --assets-link='$asset_json'"
 
-for binary in $binaries; do
-    asset_json="$(jq -n --arg name "$binary (x86_64 Linux)" \
-                        --arg url "$PACKAGE_REGISTRY_URL/x86_64-$binary" \
-                        '{name: $name, url: $url}')"
-    assets="$assets --assets-link='$asset_json'"
-    asset_json="$(jq -n --arg name "$binary (arm64 Linux)" \
-                        --arg url "$PACKAGE_REGISTRY_URL/arm64-$binary" \
-                        '{name: $name, url: $url}')"
-    assets="$assets --assets-link='$asset_json'"
-done
+# for binary in $binaries; do
+#     asset_json="$(jq -n --arg name "$binary (x86_64 Linux)" \
+#                         --arg url "$PACKAGE_REGISTRY_URL/x86_64-$binary" \
+#                         '{name: $name, url: $url}')"
+#     assets="$assets --assets-link='$asset_json'"
+#     asset_json="$(jq -n --arg name "$binary (arm64 Linux)" \
+#                         --arg url "$PACKAGE_REGISTRY_URL/arm64-$binary" \
+#                         '{name: $name, url: $url}')"
+#     assets="$assets --assets-link='$asset_json'"
+# done
 
-archive_json="$(jq -n --arg name "x86_64-linux-tezos-binaries.tar.gz" \
-                      --arg url "$PACKAGE_REGISTRY_URL/x86_64-tezos-binaries.tar.gz" \
-                        '{name: $name, url: $url}')"
-assets="$assets --assets-link='$archive_json'"
-archive_json="$(jq -n --arg name "arm64-linux-tezos-binaries.tar.gz" \
-                      --arg url "$PACKAGE_REGISTRY_URL/arm64-tezos-binaries.tar.gz" \
-                        '{name: $name, url: $url}')"
-assets="$assets --assets-link='$archive_json'"
+# archive_json="$(jq -n --arg name "x86_64-linux-tezos-binaries.tar.gz" \
+#                       --arg url "$PACKAGE_REGISTRY_URL/x86_64-tezos-binaries.tar.gz" \
+#                         '{name: $name, url: $url}')"
+# assets="$assets --assets-link='$archive_json'"
+# archive_json="$(jq -n --arg name "arm64-linux-tezos-binaries.tar.gz" \
+#                       --arg url "$PACKAGE_REGISTRY_URL/arm64-tezos-binaries.tar.gz" \
+#                         '{name: $name, url: $url}')"
+# assets="$assets --assets-link='$archive_json'"
+
+CI_COMMIT_TAG=12.0-rc2
 
 # Add links for the docker images & documentation announcement.
-CI_COMMIT_TAG_NORMALIZED="$(echo "$CI_COMMIT_TAG" | sed -e "s/\./-/g" -e "s/\~/-/g")"
+CI_COMMIT_TAG_NORMALIZED="$(echo "$CI_COMMIT_TAG" | sed -e "s/\./-/g" -e "s/\~/-/g" -e "s/v//g")"
 docs_json="$(jq -n --arg url "https://tezos.gitlab.io/CHANGES.html#version-$CI_COMMIT_TAG_NORMALIZED" \
                      '{name: "Announcement", link_type: "other", url: $url}')"
 assets="$assets --assets-link='$docs_json'"
 
-CI_COMMIT_TAG_WITHOUT_TILDE="$(echo "$CI_COMMIT_TAG" | sed -e "s/\~/-/g")"
+CI_COMMIT_TAG_WITHOUT_TILDE="$(echo "$CI_COMMIT_TAG" | sed -e "s/\~/-/g" -e "s/v//g")"
 
 # Authenticate with dockerhub to be able to fetch the image digest.
 DOCKER_HUB_TOKEN=$(curl --silent "https://auth.docker.io/token?scope=repository:tezos/tezos:pull&service=registry.docker.io"  | jq -r '.token')
@@ -54,12 +56,15 @@ docker_images_json="$(jq -n --arg url "https://hub.docker.com/layers/tezos/tezos
 assets="$assets --assets-link='$docker_images_json'"
 
 # this is for testing if the tag is not set, only on forks
-if [ -z "$MASTER_OR_RELEASE" ] && ! [ "$TEZOS_DEFAULT_NAMESPACE/$TEZOS_DEFAULT_BRANCH" = "tezos/tezos" ]; then
-CI_COMMIT_TAG=${CI_COMMIT_TAG:-"v0.0.1"}
-  echo "Setting fake \$CI_COMMIT_TAG for testing $CI_COMMIT_TAG"
-  curl --request DELETE --header "PRIVATE-TOKEN: $CI_JOB_TOKEN" "$PACKAGE_REGISTRY_URL/releases/$CI_COMMIT_TAG"
-  echo "Remove test release $CI_COMMIT_TAG"
-fi
+# if [ -z "$MASTER_OR_RELEASE" ] && ! [ "$TEZOS_DEFAULT_NAMESPACE/$TEZOS_DEFAULT_BRANCH" = "tezos/tezos" ]; then
+# CI_COMMIT_TAG=${CI_COMMIT_TAG:-"v0.0.1"}
+#   echo "Setting fake \$CI_COMMIT_TAG for testing $CI_COMMIT_TAG"
+#   curl --request DELETE --header "PRIVATE-TOKEN: $CI_JOB_TOKEN" "$PACKAGE_REGISTRY_URL/releases/$CI_COMMIT_TAG"
+#   echo "Remove test release $CI_COMMIT_TAG"
+# fi
 
 #shellcheck disable=SC2086
-release-cli create --name "Release $CI_COMMIT_TAG" --tag-name "$CI_COMMIT_TAG" "$assets"
+test=$(echo release-cli create --name \"Release $CI_COMMIT_TAG\" --tag-name \"$CI_COMMIT_TAG\" $assets)
+echo $test
+
+eval $test
