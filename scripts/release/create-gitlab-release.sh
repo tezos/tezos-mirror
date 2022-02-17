46a7ceeeb92efc903f376e2d847b4ae5355ce161
mujx <374817-mujx@users.noreply.gitlab.com>
2022-02-17 14:44:05 +0200

    Automatically add dockerhub & documentation links to releases

--- scripts/release/create-gitlab-release.sh
+++ scripts/release/create-gitlab-release.sh
@@ -35,6 +35,24 @@ archive_json="$(jq -n --arg name "arm64-linux-tezos-binaries.tar.gz" \
                         '{name: $name, url: $url}')"
 assets="$assets --assets-link='$archive_json'"
 
+# Add links for the docker images & documentation announcement.
+CI_COMMIT_TAG_NORMALIZED="$(echo "$CI_COMMIT_TAG" | sed -e "s/\./-/g" -e "s/\~/-/g")"
+docs_json="$(jq -n --arg url "https://tezos.gitlab.io/CHANGES.html#version-$CI_COMMIT_TAG_NORMALIZED" \
+                     '{name: "Announcement", link_type: "other", url: $url}')"
+assets="$assets --assets-link='$docs_json'"
+
+CI_COMMIT_TAG_WITHOUT_TILDE="$(echo "$CI_COMMIT_TAG" | sed -e "s/\~/-/g")"
+
+# Authenticate with dockerhub to be able to fetch the image digest.
+DOCKER_HUB_TOKEN=$(curl --silent "https://auth.docker.io/token?scope=repository:tezos/tezos:pull&service=registry.docker.io"  | jq -r '.token')
+IMAGE_DIGEST="$(curl -s --header "Accept: application/vnd.docker.distribution.manifest.list.v2+json" \
+                        --header "Authorization: Bearer ${DOCKER_HUB_TOKEN}" "https://registry-1.docker.io/v2/tezos/tezos/manifests/v$CI_COMMIT_TAG_WITHOUT_TILDE" \
+                        | jq -r '.manifests | .[0] | .digest | split(":")[1]')"
+
+docker_images_json="$(jq -n --arg url "https://hub.docker.com/layers/tezos/tezos/v$CI_COMMIT_TAG_WITHOUT_TILDE/images/sha256-$IMAGE_DIGEST" \
+                              '{name: "Docker images", link_type: "image", url: $url}')"
+assets="$assets --assets-link='$docker_images_json'"
+
 # this is for testing if the tag is not set, only on forks
 if [ -z "$MASTER_OR_RELEASE" ] && ! [ "$TEZOS_DEFAULT_NAMESPACE/$TEZOS_DEFAULT_BRANCH" = "tezos/tezos" ]; then
 CI_COMMIT_TAG=${CI_COMMIT_TAG:-"v0.0.1"}
