05da7fb8fe3b81d8cd4c145b32b51293fb0c8794
Raphaël Proust <code@bnwr.net>
2022-02-28 11:09:17 +0100

    Data-encoding: use upstream Compact rather than rolling our own in base

--- manifest/main.ml
+++ manifest/main.ml
@@ -905,8 +905,7 @@ let lib_base_tests ?dep_files names =
     ~modules:names
 
 let _tezos_base_tests_1 =
-  lib_base_tests
-    ["test_bounded"; "test_time"; "test_protocol"; "test_compact_encoding"]
+  lib_base_tests ["test_bounded"; "test_time"; "test_protocol"]
 
 let _tezos_base_tests_2 =
   lib_base_tests ["test_p2p_addr"] ~dep_files:["points.ok"; "points.ko"]
