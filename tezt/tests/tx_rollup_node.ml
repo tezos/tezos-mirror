cf4b0e123fc9b79b651d358ce15efed974bf420f
Victor Allombert <victor.allombert@nomadic-labs.com>
2022-02-28 18:05:41 +0100

    Tezt/Tx_rollup: test the ticket deposit (l1 to l2)

--- tezt/tests/tx_rollup_node.ml
+++ tezt/tests/tx_rollup_node.ml
@@ -196,7 +196,7 @@ let test_tx_node_store_inbox =
         Client.Tx_rollup.submit_batch
           ~content:batch
           ~rollup
-          ~src:Constant.bootstrap1.public_key_hash
+          ~src:operator
           client
       in
       let* () = Client.bake_for client in
@@ -212,7 +212,7 @@ let test_tx_node_store_inbox =
         Client.Tx_rollup.submit_batch
           ~content:snd_batch
           ~rollup
-          ~src:Constant.bootstrap1.public_key_hash
+          ~src:operator
           client
       in
       let* () = Client.bake_for client in
@@ -231,7 +231,187 @@ let test_tx_node_store_inbox =
       check_inbox_equality node_inbox_2 inbox_after_restart ;
       unit)
 
+(* FIXME/TORU: This is a temporary way of querying the node without
+   tx_rollup_client. This aims to be replaced as soon as possible by
+   the dedicated client's RPC. *)
+let raw_tx_node_rpc node ~url =
+  let* rpc = RPC.Curl.get () in
+  match rpc with
+  | None -> assert false
+  | Some curl ->
+      let url =
+        Printf.sprintf
+          "%s/%s"
+          (Rollup_node.rpc_host node ^ ":"
+          ^ Int.to_string (Rollup_node.rpc_port node))
+          url
+      in
+      curl ~url
+
+(* FIXME/TORU: Must be replaced by the Tx_client.get_balance command *)
+let tx_client_get_balance ~tx_node ~block ~ticket_id ~tz4_address =
+  let* wrapped_result =
+    raw_tx_node_rpc
+      tx_node
+      ~url:
+        ("context/" ^ block ^ "/tickets/" ^ ticket_id ^ "/balance/"
+       ^ tz4_address)
+  in
+  let json = JSON.parse ~origin:"tx_client_get_balance" wrapped_result in
+  match JSON.(json |-> "value" |> as_int_opt) with
+  | Some level -> Lwt.return level
+  | None -> Test.fail "Cannot retrieve balance of tz4 address %s" tz4_address
+
+(* Returns the ticket hash, if any, of a given operation. *)
+let get_ticket_hash_from_op op =
+  let metadata = JSON.(op |-> "contents" |=> 0 |-> "metadata") in
+  let result =
+    JSON.(metadata |-> "internal_operation_results" |=> 0 |-> "result")
+  in
+  let kind =
+    JSON.(
+      metadata |-> "internal_operation_results" |=> 0 |-> "parameters"
+      |-> "entrypoint" |> as_string)
+  in
+  let deposit = "deposit" in
+  if not String.(equal kind deposit) then
+    Test.fail
+      "The internal operation was expected to be a %s but is a %s"
+      deposit
+      kind ;
+  let status = JSON.(result |-> "status" |> as_string_opt) in
+  match status with
+  | Some v when String.(equal v "applied") ->
+      let ticket_hash = JSON.(result |-> "ticket_hash" |> as_string) in
+      Lwt.return ticket_hash
+  | None | Some _ -> Test.fail "The contract origination failed"
+
+(* The contract is expecting a parameter of the form:
+   (Pair tx_rollup_txr1_address tx_rollup_tz4_address) *)
+let make_tx_rollup_deposit_argument txr1 tz4 =
+  "( Pair " ^ "\"" ^ txr1 ^ "\" \"" ^ tz4 ^ "\")"
+
+(* FIXME/TORU: we should merge this into Tezos_crypto.Bls *)
+module Bls_public_key_hash = struct
+  include
+    Tezos_crypto.Blake2B.Make
+      (Tezos_crypto.Base58)
+      (struct
+        let name = "Bls12_381.Public_key_hash"
+
+        let title = "A Bls12_381 public key hash"
+
+        let b58check_prefix = "\006\161\166" (* tz4(36) *)
+
+        let size = Some 20
+      end)
+end
+
+let generate_bls_addr ?alias:_ _client =
+  (* FIXME/TORU: we should use the the client to generate keys *)
+  let seed =
+    let rng_state = Random.State.make_self_init () in
+    Bytes.init 32 (fun _ -> char_of_int @@ Random.State.int rng_state 255)
+  in
+  let sk = Bls12_381.Signature.generate_sk seed in
+  let pk = Bls12_381.Signature.MinPk.derive_pk sk in
+  let pkh =
+    Bls_public_key_hash.hash_bytes [Bls12_381.Signature.MinPk.pk_to_bytes pk]
+  in
+  Log.info
+    "A new BLS key was generated: %s"
+    (Bls_public_key_hash.to_b58check pkh) ;
+  (pkh, pk, sk)
+
+let check_tz4_balance ~tx_node ~block ~ticket_id ~tz4_address ~expected_balance
+    =
+  let* tz4_balance =
+    tx_client_get_balance ~tx_node ~block ~ticket_id ~tz4_address
+  in
+  Check.(
+    ( = )
+      tz4_balance
+      expected_balance
+      int
+      ~error_msg:
+        (Format.sprintf
+           "The balance of %s was expected to be %d instead of %d."
+           tz4_address
+           expected_balance
+           tz4_balance)) ;
+  unit
+
+(* Checks that the a ticket can be transfered from the L1 to the rollup. *)
+let test_ticket_deposit_from_l1_to_l2 =
+  Protocol.register_test
+    ~__FILE__
+    ~title:"TX_rollup: deposit ticket from l1 to l2"
+    ~tags:["tx_rollup"; "deposit"; "ticket"]
+    (fun protocol ->
+      let* parameter_file = get_rollup_parameter_file ~protocol in
+      let* (node, client) =
+        Client.init_with_protocol ~parameter_file `Client ~protocol ()
+      in
+      let operator = Constant.bootstrap1.public_key_hash in
+      let* (tx_rollup_hash, tx_node) =
+        init_and_run_rollup_node ~operator node client
+      in
+      let* contract_id =
+        Client.originate_contract
+          ~alias:"rollup_deposit"
+          ~amount:Tez.zero
+          ~src:"bootstrap1"
+          ~prg:"file:./tezt/tests/contracts/proto_alpha/tx_rollup_deposit.tz"
+          ~init:"Unit"
+          ~burn_cap:Tez.(of_int 1)
+          client
+      in
+      let* () = Client.bake_for client in
+      let* _ = Node.wait_for_level node 2 in
+      Log.info
+        "The tx_rollup_deposit %s contract was successfully originated"
+        contract_id ;
+      let (bls_pkh, _, _) = generate_bls_addr client in
+      let bls_pkh_str = Bls_public_key_hash.to_b58check bls_pkh in
+      let arg = make_tx_rollup_deposit_argument tx_rollup_hash bls_pkh_str in
+      (* This smart contract call will transfer 10 tickets to the
+         given address. *)
+      let* () =
+        Client.transfer
+          ~gas_limit:100_000
+          ~fee:Tez.one
+          ~amount:Tez.zero
+          ~burn_cap:Tez.one
+          ~storage_limit:10_000
+          ~giver:"bootstrap1"
+          ~receiver:contract_id
+          ~arg
+          client
+      in
+      let* () = Client.bake_for client in
+      let* _ = Node.wait_for_level node 3 in
+      (* Get the operation containing the ticket transfer. We assume
+         that only one operation is issued in this block. *)
+      let* op =
+        Client.rpc
+          Client.GET
+          ["chains"; "main"; "blocks"; "head"; "operations"; "3"; "0"]
+          client
+      in
+      let* ticket_id = get_ticket_hash_from_op op in
+      Log.info "Ticket %s was successfully emitted" ticket_id ;
+      let* () =
+        check_tz4_balance
+          ~tx_node
+          ~block:"head"
+          ~ticket_id
+          ~tz4_address:bls_pkh_str
+          ~expected_balance:10
+      in
+      unit)
+
 let register ~protocols =
   test_node_configuration protocols ;
   test_tx_node_origination protocols ;
-  test_tx_node_store_inbox protocols
+  test_tx_node_store_inbox protocols ;
+  test_ticket_deposit_from_l1_to_l2 protocols
