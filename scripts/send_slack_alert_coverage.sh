a95f0f31646f1aed306b9b6d8c75aad7cbb172c9
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2022-02-28 10:39:01 +0100

    CI/Coverage: usability improvement in send_slack_alert_coverage.sh
    Only print the response if unsuccessful.

--- scripts/send_slack_alert_coverage.sh
+++ scripts/send_slack_alert_coverage.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-set -e
+set -eu
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     echo "Usage: $0 SLACK_TOKEN CHANNEL_ID PIPELINE_URL"
@@ -19,4 +19,10 @@ curl --silent \
      -H "Authorization: Bearer $SLACK_COVERAGE_TOKEN" \
      -d "channel=$CHANNEL_ID" \
      -d "text=/!\ Corrupted coverage file found in pipeline: $CI_PIPELINE_URL /!\\" \
-     -X POST https://slack.com/api/chat.postMessage
+     -X POST https://slack.com/api/chat.postMessage -o slack-response.json
+jq --exit-status '.ok' < slack-response.json > /dev/null || {
+    echo "Slack notification unsuccessful, response received:";
+    cat slack-response.json;
+    exit 1;
+}
+echo "Slack notification posted to channel $CHANNEL_ID"
