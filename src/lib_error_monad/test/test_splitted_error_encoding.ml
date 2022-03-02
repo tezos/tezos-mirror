70e4e82c9b3c54d1d2b94c8e2fc85906cd7b4495
Raphaël Proust <code@bnwr.net>
2022-03-02 16:01:00 +0100

    Error_monad: add json+registration test

---
+++ src/lib_error_monad/test/test_splitted_error_encoding.ml
@@ -0,0 +1,98 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** Testing
+    -------
+    Component:    Error Monad
+    Invocation:   dune build @src/lib_error_monad/runtest
+    Subject:      On the wrapping of error_encoding in a splitted.
+*)
+
+open TzCore
+
+let splitted_error_encoding =
+  Data_encoding.splitted ~json:error_encoding ~binary:error_encoding
+
+type error += Foo
+
+let msg error = Obj.Extension_constructor.(name @@ of_val error)
+
+let test_unregistered_binary () =
+  let e = Foo in
+  let blob = Data_encoding.Binary.to_string_exn error_encoding e in
+  let ee = Data_encoding.Binary.of_string_exn error_encoding blob in
+  (match ee with Unregistered_error _ -> () | _ -> assert false) ;
+  let sblob = Data_encoding.Binary.to_string_exn splitted_error_encoding e in
+  let see = Data_encoding.Binary.of_string_exn error_encoding sblob in
+  (match see with Unregistered_error _ -> () | _ -> assert false) ;
+  ()
+
+let test_unregistered_json () =
+  let e = Foo in
+  let json = Data_encoding.Json.construct error_encoding e in
+  let ee = Data_encoding.Json.destruct error_encoding json in
+  (match ee with Unregistered_error _ -> () | _ -> assert false) ;
+  let sjson = Data_encoding.Json.construct splitted_error_encoding e in
+  let see = Data_encoding.Json.destruct error_encoding sjson in
+  (match see with Unregistered_error _ -> () | _ -> assert false) ;
+  ()
+
+let test_registered () =
+  register_error_kind
+    `Permanent
+    ~id:"test.Foo"
+    ~title:"test-foo"
+    ~description:"Test Foo"
+    Data_encoding.(obj1 (req "test-foo" Data_encoding.unit))
+    (function Foo -> Some () | _ -> None)
+    (fun () -> Foo) ;
+  let e = Foo in
+  let blob = Data_encoding.Binary.to_string_exn error_encoding e in
+  let ee = Data_encoding.Binary.of_string_exn error_encoding blob in
+  assert (ee = Foo) ;
+  let see = Data_encoding.Binary.of_string_exn splitted_error_encoding blob in
+  assert (see = Foo) ;
+  let sblob = Data_encoding.Binary.to_string_exn splitted_error_encoding e in
+  let ssee = Data_encoding.Binary.of_string_exn error_encoding sblob in
+  assert (ssee = Foo) ;
+  (* and now JSON *)
+  let json = Data_encoding.Json.construct error_encoding e in
+  let ee = Data_encoding.Json.destruct error_encoding json in
+  assert (ee = Foo) ;
+  let sjson = Data_encoding.Json.construct splitted_error_encoding e in
+  let see = Data_encoding.Json.destruct error_encoding sjson in
+  (match see with Unregistered_error _ -> () | _ -> assert false) ;
+  let ssee = Data_encoding.Json.destruct splitted_error_encoding json in
+  (match ssee with Unregistered_error _ -> () | _ -> assert false) ;
+  ()
+
+let tests =
+  [
+    Alcotest.test_case "unregistered(binary)" `Quick test_unregistered_binary;
+    Alcotest.test_case "unregistered(json)" `Quick test_unregistered_json;
+    Alcotest.test_case "registered" `Quick test_registered;
+  ]
+
+let () = Alcotest.run "splitted" [("splitted", tests)]
