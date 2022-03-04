3a8cfe157fad44fac29a976774814e73738c4616
Raphaël Proust <code@bnwr.net>
2022-03-04 17:00:08 +0100

    Base,Proto-env: fix comment indentation

--- src/lib_protocol_environment/sigs/v5/block_header.mli
+++ src/lib_protocol_environment/sigs/v5/block_header.mli
@@ -26,22 +26,20 @@
 type shell_header = {
   level : Int32.t;  (** Height of the block, from the genesis block. *)
   proto_level : int;
-      (** Number of protocol changes since genesis modulo 256. *)
-  (* uint8 *)
+      (** Number (uint8) of protocol changes since genesis modulo 256. *)
   predecessor : Block_hash.t;  (** Hash of the preceding block. *)
   timestamp : Time.t;
       (** Timestamp at which the block is claimed to have been created. *)
   validation_passes : int;
-      (** Number of validation passes (also number of lists of operations). *)
-  (* uint8 *)
+      (** Number (uint8) of validation passes (also number of lists of operations). *)
   operations_hash : Operation_list_list_hash.t;
       (** Hash of the list of lists (actually root hashes of merkle trees)
-      of operations included in the block. There is one list of
-      operations per validation pass. *)
+          of operations included in the block. There is one list of
+          operations per validation pass. *)
   fitness : Bytes.t list;
       (** A sequence of sequences of unsigned bytes, ordered by length and
-      then lexicographically. It represents the claimed fitness of the
-      chain ending in this block. *)
+          then lexicographically. It represents the claimed fitness of the
+          chain ending in this block. *)
   context : Context_hash.t;
       (** Hash of the state of the context after application of this block. *)
 }
