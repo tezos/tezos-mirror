1c74e5644b2b9331a1365acb3c2779b155fa5b20
Joel Bjornson <joel.bjornson@trili.tech>
2022-03-04 08:44:48 +0000

    Tests: test ticket-manager contract

--- src/proto_alpha/lib_protocol/test/integration/michelson/main.ml
+++ src/proto_alpha/lib_protocol/test/integration/michelson/main.ml
@@ -47,6 +47,7 @@ let () =
       ("ticket operations diff", Test_ticket_operations_diff.tests);
       ("ticket accounting", Test_ticket_accounting.tests);
       ("ticket balance", Test_ticket_balance.tests);
+      ("ticket manager", Test_ticket_manager.tests);
       ("timelock", Test_timelock.tests);
       ("typechecking", Test_typechecking.tests);
       ("script cache", Test_script_cache.tests);
