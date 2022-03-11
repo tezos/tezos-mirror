OKEAMAH/Git

Proto: make storage 'carbonated'

src/proto_alpha/lib_protocol/storage.ml
@@ -351,14 +351,14 @@ module Contract = struct
 
   module Rollup_bond_id_index =
     Make_indexed_subcontext
-      (Make_subcontext (Ghost) (Indexed_context.Raw_context)
+      (Make_subcontext (Registered) (Indexed_context.Raw_context)
          (struct
            let name = ["rollup_bond_id_index"]
          end))
          (Make_index (Rollup_bond_id_repr.Index))
 
   module Frozen_rollup_bonds =
-    Rollup_bond_id_index.Make_map
+    Rollup_bond_id_index.Make_carbonated_map
       (struct
         let name = ["rollup_bonds"]
       end)

tezos/tezos-mirror

src/proto_alpha/lib_protocol/storage.ml
@@ -352,18 +352,20 @@ module Contract = struct
 
   module Bond_id_index =
     Make_indexed_subcontext
-      (Make_subcontext (Ghost) (Indexed_context.Raw_context)
+      (Make_subcontext (Registered) (Indexed_context.Raw_context)
          (struct
            let name = ["bond_id_index"]
          end))
          (Make_index (Bond_id_repr.Index))
 
   module Frozen_bonds =
-    Bond_id_index.Make_map
+    Bond_id_index.Make_carbonated_map
       (struct
         let name = ["frozen_bonds"]
       end)
       (Tez_repr)
+
+  let fold_bond_ids = Bond_id_index.fold_keys
 
   module Total_frozen_bonds =
     Indexed_context.Make_map