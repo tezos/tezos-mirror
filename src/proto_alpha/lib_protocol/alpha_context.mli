OKEAMAH/Git

proto/toru: verify withdraw & commit are correct
This commit modify the application of a withdraw operation.

Now it checks that the commitment exists and is final, it also checks
that the withdraw exists and the ticket have not already been withdrawn

Co-authored-by: Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -2177,6 +2177,22 @@ module Tx_rollup_withdraw : sig
   val compute_path : t list -> int -> path
 
   val check_path : path -> t -> list_hash * int
+
+  val add :
+    context ->
+    Tx_rollup.t ->
+    Tx_rollup_level.t ->
+    message_index:int ->
+    withdraw_index:int ->
+    context tzresult Lwt.t
+
+  val mem :
+    context ->
+    Tx_rollup.t ->
+    Tx_rollup_level.t ->
+    message_index:int ->
+    withdraw_index:int ->
+    (bool * context) tzresult Lwt.t
 end
 
 (** This module re-exports definitions from {!Tx_rollup_message_repr}. *)
@@ -2347,6 +2363,13 @@ module Tx_rollup_commitment : sig
 
   val hash : t -> Tx_rollup_commitment_hash.t
 
+  val check_batch_commitment :
+    t ->
+    context_hash:bytes ->
+    Tx_rollup_withdraw.list_hash ->
+    message_index:int ->
+    bool
+
   val add_commitment :
     context ->
     Tx_rollup.t ->
@@ -2364,6 +2387,12 @@ module Tx_rollup_commitment : sig
     (context * Submitted_commitment.t option) tzresult Lwt.t
 
   val get :
+    context ->
+    Tx_rollup.t ->
+    Tx_rollup_level.t ->
+    (context * Submitted_commitment.t) tzresult Lwt.t
+
+  val get_final_existing :
     context ->
     Tx_rollup.t ->
     Tx_rollup_level.t ->
@@ -2443,6 +2472,11 @@ module Tx_rollup_errors : sig
         length : int;
       }
     | Wrong_message_hash
+    | No_finalized_commitment_for_level of {
+        level : Tx_rollup_level.t;
+        window : (Tx_rollup_level.t * Tx_rollup_level.t) option;
+      }
+    | Withdraw_invalid_path
 end
 
 (** This simply re-exports {!Destination_repr}. *)

src/proto_alpha/lib_protocol/tx_rollup_commitment_repr.ml
@@ -152,6 +152,18 @@ let hash c =
   let bytes = Data_encoding.Binary.to_bytes_exn encoding c in
   Commitment_hash.hash_bytes [bytes]
 
+let check_batch_commitment :
+    t ->
+    context_hash:bytes ->
+    Tx_rollup_withdraw_repr.list_hash ->
+    message_index:int ->
+    bool =
+ fun {batches; _} ~context_hash withdraw_list_hash ~message_index ->
+  let computed = batch_commitment context_hash withdraw_list_hash in
+  match List.nth batches message_index with
+  | Some expected -> Message_result_hash.(computed = expected)
+  | None -> false
+
 module Index = struct
   type t = Commitment_hash.t
 

src/proto_alpha/lib_protocol/tx_rollup_state_repr.mli
@@ -161,6 +161,14 @@ val record_commitment_deletion :
   Tx_rollup_commitment_repr.Commitment_hash.t ->
   t tzresult
 
+(** [finalized_commitments_range hash] returns the windows of finalized
+    commitment that have not yet been cleaned out
+
+    This function returns an [Internal_error] if the state is in a incoherent
+    state, this should not be possible. *)
+val finalized_commitments_range :
+  t -> (Tx_rollup_level_repr.t * Tx_rollup_level_repr.t) option tzresult
+
 module Internal_for_tests : sig
   (** [make] returns a state for tests *)
   val make :

tezos/tezos-mirror

src/proto_alpha/lib_protocol/alpha_context.mli
@@ -2204,7 +2204,23 @@ module Tx_rollup_withdraw : sig
 
   val compute_path : t list -> int -> merkle_tree_path
 
-  val check_path : path -> t -> list_hash * int
+  val check_path : merkle_tree_path -> t -> withdrawals_merkle_root * int
+
+  val add :
+    context ->
+    Tx_rollup.t ->
+    Tx_rollup_level.t ->
+    message_index:int ->
+    withdraw_index:int ->
+    context tzresult Lwt.t
+
+  val mem :
+    context ->
+    Tx_rollup.t ->
+    Tx_rollup_level.t ->
+    message_index:int ->
+    withdraw_index:int ->
+    (bool * context) tzresult Lwt.t
 end
 
 (** This module re-exports definitions from {!Tx_rollup_message_repr}. *)
@@ -2369,6 +2385,13 @@ module Tx_rollup_commitment : sig
 
   val hash : t -> Tx_rollup_commitment_hash.t
 
+  val check_batch_commitment :
+    t ->
+    context_hash:bytes ->
+    Tx_rollup_withdraw.withdrawals_merkle_root ->
+    message_index:int ->
+    bool
+
   val add_commitment :
     context ->
     Tx_rollup.t ->
@@ -2386,6 +2409,12 @@ module Tx_rollup_commitment : sig
     (context * Submitted_commitment.t option) tzresult Lwt.t
 
   val get :
+    context ->
+    Tx_rollup.t ->
+    Tx_rollup_level.t ->
+    (context * Submitted_commitment.t) tzresult Lwt.t
+
+  val get_finalized :
     context ->
     Tx_rollup.t ->
     Tx_rollup_level.t ->
@@ -2465,6 +2494,12 @@ module Tx_rollup_errors : sig
         length : int;
       }
     | Wrong_message_hash
+    | No_finalized_commitment_for_level of {
+        level : Tx_rollup_level.t;
+        window : (Tx_rollup_level.t * Tx_rollup_level.t) option;
+      }
+    | Withdraw_invalid_path
+    | Withdraw_already_consumed
 end
 
 (** This simply re-exports {!Destination_repr}. *)

src/proto_alpha/lib_protocol/tx_rollup_commitment_repr.ml
@@ -154,6 +154,18 @@ let hash c =
   let bytes = Data_encoding.Binary.to_bytes_exn encoding c in
   Commitment_hash.hash_bytes [bytes]
 
+let check_batch_commitment :
+    t ->
+    context_hash:bytes ->
+    Tx_rollup_withdraw_repr.withdrawals_merkle_root ->
+    message_index:int ->
+    bool =
+ fun {batches; _} ~context_hash withdraw_merkle_root ~message_index ->
+  let computed = batch_commitment context_hash withdraw_merkle_root in
+  match List.nth batches message_index with
+  | Some expected -> Message_result_hash.(computed = expected)
+  | None -> false
+
 module Index = struct
   type t = Commitment_hash.t
 

src/proto_alpha/lib_protocol/tx_rollup_state_repr.mli
@@ -161,6 +161,14 @@ val record_commitment_deletion :
   Tx_rollup_commitment_repr.Commitment_hash.t ->
   t tzresult
 
+(** [finalized_commitments_range state] returns the window of finalized
+    commitments that have not yet been cleaned out
+
+    This function returns an [Internal_error] if the state is inconsistent, 
+    which should not be possible. *)
+val finalized_commitments_range :
+  t -> (Tx_rollup_level_repr.t * Tx_rollup_level_repr.t) option tzresult
+
 module Internal_for_tests : sig
   (** [make] returns a state for tests *)
   val make :