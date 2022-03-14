07d8089c4da1730eaf25364ca2b8cd05d3e3c573
Raphaël Proust <code@bnwr.net>
2022-03-14 14:28:48 +0100

    TPSeval: convert to newer `let*`

--- src/bin_tps_evaluation/estimate_average_block_command.ml
+++ src/bin_tps_evaluation/estimate_average_block_command.ml
@@ -199,21 +199,22 @@ end
 
 (** Execute the query against the database and formats the result. *)
 let query_db start_date end_date contract_min_percentage conn_str =
-  Lwt.bind (Db.get_operation_summary conn_str start_date end_date ()) (function
-      | Ok rows -> Lwt.return rows
-      | Error e -> Stdlib.failwith (Caqti_error.show e))
-  >>= fun summary ->
-  Lwt.bind
-    (Db.get_top_contracts
-       conn_str
-       start_date
-       end_date
-       contract_min_percentage
-       ())
-    (function
-      | Ok rows -> Lwt.return rows
-      | Error e -> Stdlib.failwith (Caqti_error.show e))
-  >>= fun top_contracts -> Lwt.return (Json.show_summary summary top_contracts)
+  let open Lwt_syntax in
+  let* rows = Db.get_operation_summary conn_str start_date end_date () in
+  let summary =
+    match rows with
+    | Ok rows -> rows
+    | Error e -> Stdlib.failwith (Caqti_error.show e)
+  in
+  let* rows =
+    Db.get_top_contracts conn_str start_date end_date contract_min_percentage ()
+  in
+  let top_contracts =
+    match rows with
+    | Ok rows -> rows
+    | Error e -> Stdlib.failwith (Caqti_error.show e)
+  in
+  Lwt.return (Json.show_summary summary top_contracts)
 
 let register () =
   Long_test.register
