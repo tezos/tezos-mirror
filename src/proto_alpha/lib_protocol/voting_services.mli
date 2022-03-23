0118b0c3d87213cb90fb2e658e1b2a32a8cff754
Nicolas Ayache <nicolas.ayache@nomadic-labs.com>
2022-03-23 23:06:42 +0100

    Proto: doc for Voting_services.

--- src/proto_alpha/lib_protocol/voting_services.mli
+++ src/proto_alpha/lib_protocol/voting_services.mli
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -22,6 +23,8 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
+
+(** This module provides RPC services that return voting-related information. *)
 
 open Alpha_context
 
