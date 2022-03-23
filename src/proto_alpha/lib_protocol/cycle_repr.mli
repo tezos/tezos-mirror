aa437a42997f27e1c77c4385e4a91e3cbb5c907b
Nicolas Ayache <nicolas.ayache@nomadic-labs.com>
2022-03-23 16:31:58 +0100

    Proto: doc for Cycle_repr.

--- src/proto_alpha/lib_protocol/cycle_repr.mli
+++ src/proto_alpha/lib_protocol/cycle_repr.mli
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -22,6 +23,10 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
+
+(** This module provides a type and functions to manipulate cycle numbers.
+
+    Invariant: cycle numbers are always positive. *)
 
 type t
 
