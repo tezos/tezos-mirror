d0276017d1339c179cd33014f97255f45b4bcd6f
Nicolas Ayache <nicolas.ayache@nomadic-labs.com>
2022-03-23 14:06:06 +0100

    Proto: doc for Script_map.

--- src/proto_alpha/lib_protocol/script_map.mli
+++ src/proto_alpha/lib_protocol/script_map.mli
@@ -3,6 +3,7 @@
 (* Open Source License                                                       *)
 (* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
 (* Copyright (c) 2020 Metastate AG <hello@metastate.dev>                     *)
+(* Copyright (c) 2022 Nomadic Labs <contact@nomadic-labs.com>                *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -24,6 +25,13 @@
 (*                                                                           *)
 (*****************************************************************************)
 
+(** Functions to ease the manipulation of Michelson maps.
+
+    A map in Michelson is a type-homegeneous, partial function of keys to
+    values, along with the functions that operate on the structure (through a
+    first-class module).
+*)
+
 open Script_typed_ir
 
 val make :
@@ -34,8 +42,15 @@ val get_module :
   ('key, 'value) map ->
   (module Boxed_map with type key = 'key and type value = 'value)
 
+(** [empty cmp_ty] creates a map module where keys have size
+    [Gas_comparable_input_size.size_of_comparable_value cmp_ty] and are compared
+    with [Script_comparable.compare_comparable cmp_ty] (used for sorting keys,
+    which ensures a reasonable complexity of the map functions).
+    The function returns an empty map packaged as a first-class map module. *)
 val empty : 'a comparable_ty -> ('a, 'b) map
 
+(** [empty_from map] creates an empty map module where the size of keys and the
+    comparison function are those of [map]. *)
 val empty_from : ('a, 'b) map -> ('a, 'c) map
 
 val fold :
@@ -47,6 +62,9 @@ val fold_es :
   'acc ->
   'acc tzresult Lwt.t
 
+(** [update k (Some v) map] associates [v] to [k] in [map] (overwriting the
+    previous value, if any), and [update k None map] removes the potential
+    association to [k] in [map]. *)
 val update : 'a -> 'b option -> ('a, 'b) map -> ('a, 'b) map
 
 val mem : 'key -> ('key, 'value) map -> bool
