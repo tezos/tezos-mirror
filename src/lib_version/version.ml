3ef67b8bb3274c461d51d8acda4cef2cc71f3d7f
Romain Bardou <romain@nomadic-labs.com>
2022-03-30 16:34:43 +0200

    Version: set to 12.1

--- src/lib_version/version.ml
+++ src/lib_version/version.ml
@@ -45,6 +45,6 @@ let pp f {major; minor; additional_info} =
 
 let to_string x = Format.asprintf "%a" pp x
 
-let current = {major = 12; minor = 0; additional_info = Release}
+let current = {major = 12; minor = 1; additional_info = Release}
 
 let current_string = to_string current
