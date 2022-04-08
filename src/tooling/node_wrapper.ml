b5da0961779fbe2c68b6efad2f83978bff7ef67e
Hugo Heuzard <hugo.heuzard@nomadic-labs.com>
2022-04-08 11:41:52 +0200

    JS: change implementation of node_wrapper

--- src/tooling/node_wrapper.ml
+++ src/tooling/node_wrapper.ml
@@ -1,93 +1,36 @@
-module Lib = struct
-  type version = string
-
-  type t = Hacl | Secp256k1 | Bls12_381
-
-  let to_string = function
-    | Hacl -> "hacl"
-    | Secp256k1 -> "secp256k1"
-    | Bls12_381 -> "bls12_381"
-
-  let to_js_lib = function
-    | Hacl -> "hacl-wasm"
-    | Secp256k1 -> "@nomadic-labs/secp256k1-wasm"
-    | Bls12_381 -> "@dannywillems/ocaml-bls12-381"
-
-  let to_load_ident x = Printf.sprintf "load_%s" (to_string x)
-
-  let to_js x =
-    let load_ident = to_load_ident x in
-    let js_lib = to_js_lib x in
-    match x with
-    | Hacl ->
-        Printf.sprintf
-          {|
-function %s() {
+let code_for_lib = function
+  | "hacl-wasm" ->
+      {|
+(function () {
   /* We have to cheat to avoid the noise from hacl-wasm */
   var old_log = console.log;
   console.log = function () {};
-  var loader = require('%s');
+  var loader = require('hacl-wasm');
   console.log = old_log;
   return loader.getInitializedHaclModule().then(function(loaded){
     console.log('hacl loaded');
     global._HACL = loaded})
-}
+})
 |}
-          load_ident
-          js_lib
-    | Secp256k1 ->
-        Printf.sprintf
-          {|
-function %s() {
-  var loader = require('%s');
+  | "secp256k1-wasm" ->
+      {|
+(function () {
+  var loader = require('@nomadic-labs/secp256k1-wasm');
   return loader().then(function(loaded) {
     console.log('secp256k1 loaded');
     global._SECP256K1 = loaded})
-}
+})
 |}
-          load_ident
-          js_lib
-    | Bls12_381 ->
-        Printf.sprintf
-          {|
-function %s() {
-  var loader = require('%s');
+  | "ocaml-bls12-381" ->
+      {|
+(function () {
+  var loader = require('@dannywillems/ocaml-bls12-381');
   return loader().then(function(loaded) {
     console.log('bls12-381 loaded');
     global._BLS12381 = loaded})
-}
-           |}
-          load_ident
-          js_lib
-end
-
-let files = ref []
-
-let libs = ref []
-
-let args = ref []
-
-let () =
-  Arg.parse
-    [
-      ("--hacl", Unit (fun () -> libs := Lib.Hacl :: !libs), "Load hacl-wasm");
-      ( "--secp256k1",
-        Unit (fun () -> libs := Lib.Secp256k1 :: !libs),
-        "Load @nomadic-labs/secp256k1-wasm" );
-      ( "--bls12-381",
-        Unit (fun () -> libs := Lib.Bls12_381 :: !libs),
-        "Load @dannywillems/ocaml-bls12-381" );
-      ( "--",
-        Rest_all (fun l -> args := List.rev_append l !args),
-        "args to pass to the scripts" );
-    ]
-    (fun a ->
-      match Filename.extension a with
-      | ".js" -> files := a :: !files
-      | _ -> args := a :: !args)
-    (Printf.sprintf
-       "%s [FLAGS] FILE.js -- args"
-       (Filename.basename Sys.executable_name))
+})
+|}
+  | _ -> raise Not_found
 
 let run i file args =
   let argv = "node" :: file :: args in
@@ -111,8 +54,40 @@ function run_%d () {
     argv
     file
 
+let files = ref []
+
+let codes = ref []
+
+let args = ref []
+
 let () =
-  let libs = List.rev !libs in
+  let node_preload_var = "NODE_PRELOAD" in
+  let preload =
+    try String.split_on_char ',' (Unix.getenv node_preload_var)
+    with Not_found -> []
+  in
+  List.iter
+    (fun name ->
+      let code =
+        try code_for_lib name
+        with Not_found ->
+          failwith
+            (Printf.sprintf
+               "Unknown preloading library %S in %s"
+               name
+               node_preload_var)
+      in
+      codes := code :: !codes)
+    preload ;
+  List.iter
+    (fun x ->
+      match Filename.extension x with
+      | ".js" -> files := x :: !files
+      | _ -> args := x :: !args)
+    (List.tl (Array.to_list Sys.argv))
+
+let () =
+  let codes = List.rev !codes in
   let files = List.rev !files in
   let args = List.rev !args in
   let b = Buffer.create 1024 in
@@ -130,13 +105,15 @@ if(parseInt(major) < minimum_major){
     process.exit(1);
 }
 |} ;
-  List.iter (fun lib -> Buffer.add_string b (Lib.to_js lib)) libs ;
+  List.iteri
+    (fun i init ->
+      Buffer.add_string b (Printf.sprintf "var init_lib_%d = %s" i init))
+    codes ;
   List.iteri (fun i file -> Buffer.add_string b (run i file args)) files ;
-  let promises = List.map Lib.to_load_ident libs in
   Buffer.add_string b "Promise.resolve('Loading')" ;
-  List.iter
-    (fun p -> Buffer.add_string b (Printf.sprintf ".then(%s)" p))
-    promises ;
+  List.iteri
+    (fun i _ -> Buffer.add_string b (Printf.sprintf ".then(init_lib_%d)" i))
+    codes ;
   List.iteri
     (fun i _file -> Buffer.add_string b (Printf.sprintf ".then(run_%d)" i))
     files ;
@@ -144,6 +121,11 @@ if(parseInt(major) < minimum_major){
     b
     ".catch(function (e) { console.log(e); process.exit(1) })\n" ;
   print_newline () ;
+  let () =
+    match Unix.getenv "ORIGINAL_PATH" with
+    | exception Not_found -> ()
+    | path -> Unix.putenv "PATH" path
+  in
   let oc = Unix.open_process_out "node" in
   Buffer.output_buffer oc b ;
   flush_all () ;
