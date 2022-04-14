b777e03dad3beec1b7bb778a75b64d56b3ad34b8
Sventimir <svantimir@gmail.com>
2022-04-14 12:30:29 +0200

    Proto/Test: Add scripts to logging regression test.

--- src/proto_alpha/lib_protocol/test/regression/test_logging.ml
+++ src/proto_alpha/lib_protocol/test/regression/test_logging.ml
@@ -166,7 +166,14 @@ let run_script {filename; amount; storage; parameter} () =
     Contract_helpers.read_file @@ Base.(("contracts" // filename) ^ ".tz")
   in
   let* ctxt = test_context () in
-  let step_constants = Contract_helpers.{default_step_constants with amount} in
+  let step_constants =
+    Contract_helpers.
+      {
+        default_step_constants with
+        amount;
+        now = Script_timestamp.of_int64 1649939559L;
+      }
+  in
   let* _res, _ctxt =
     Contract_helpers.run_script
       ctxt
@@ -175,6 +182,7 @@ let run_script {filename; amount; storage; parameter} () =
       ~storage
       ~parameter
       ~step_constants
+      ~internal:true (* Allow for forged values (e.g. tickets). *)
       ()
   in
   let* log = get_log () in
@@ -197,17 +205,196 @@ let register_script contract =
     ~__FILE__
     ~title:contract.filename
     ~tags:["protocol"; "regression"; "logging"]
-    ~output_file:(Filename.remove_extension contract.filename)
+    ~output_file:contract.filename
     (fail_on_error @@ run_script contract)
 
+(* These tests should always cover:
+    - every instruction type, which means an example of each group of instructions
+      which are similar to each other with respect to logging; no need to cover every
+      instruction whatsoever, but just every distinct kind ;
+    - every continuation and control structure in Michelson, because those impact
+      what is being logged and what is not.
+   We are not concerned with gas, because that's kept track of by regular regression
+   tests. Actually, gas is unaccounted for in all the tests in this module. *)
 let register () =
   Array.iter
     register_script
     [|
       {
-        filename = "add_to_store";
+        filename = "accounts";
         amount = Tez.zero;
-        storage = "5";
-        parameter = "3";
+        parameter = "Left \"tz1KqTpEZ7Yob7QbPE4Hy4Wo8fHG8LhKxZSx\"";
+        storage = "{}";
+      };
+      {
+        filename = "append";
+        amount = Tez.zero;
+        parameter = "Pair {7; 8; 9} {4; 5; 6}";
+        storage = "{1; 2; 3}";
+      };
+      {
+        filename = "auction";
+        amount = Tez.of_mutez_exn 100_000_000L;
+        parameter = "\"tz1b7tUupMgCNw2cCLpKTkSD1NZzB5TkP2sv\"";
+        storage =
+          "Pair \"2099-12-31T23:59:59Z\" (Pair 50000000 \
+           \"tz1faswCTDciRzE4oJ9jn2Vm2dvjeyA9fUzU\")";
+      };
+      {
+        filename = "big_map_union";
+        amount = Tez.zero;
+        parameter = "{Pair \"string\" 12; Pair \"abc\" 99; Pair \"def\" 3}";
+        storage = "Pair { Elt \"123\" 123 } Unit";
+      };
+      {
+        filename = "check_signature";
+        amount = Tez.zero;
+        parameter = "\"edpkuBknW28nW72KG6RoHtYW7p12T6GKc7nAbwYX5m8Wd9sDVC9yav\"";
+        storage =
+          "Pair \
+           \"edsigu6Ue4mQgPC5aCFqqjitU9pCs5VErXrfPTAZffyJepccGzDEEBExtuPjGuMc2ZRSTBUDR7tJMLVTeJzZn7p9jN9inh4ooV1\" \
+           \"TEZOS\"";
+      };
+      {
+        filename = "comb-get";
+        amount = Tez.zero;
+        parameter = "Pair 1 4 2 Unit";
+        storage = "Unit";
+      };
+      {
+        filename = "comb-set";
+        amount = Tez.zero;
+        parameter = "Unit";
+        storage = "Pair 1 4 2 Unit";
+      };
+      {
+        filename = "concat";
+        amount = Tez.zero;
+        parameter = "\"abcd\"";
+        storage = "\"efgh\"";
+      };
+      {
+        filename = "conditionals";
+        amount = Tez.zero;
+        parameter = "Right (Some 23)";
+        storage = "\"\"";
+      };
+      {
+        filename = "cps_fact";
+        amount = Tez.zero;
+        parameter = "2";
+        storage = "60";
+      };
+      {
+        filename = "dign";
+        amount = Tez.zero;
+        parameter = "Pair (Pair (Pair (Pair 0 1) 2) 3) 4";
+        storage = "7";
+      };
+      {
+        filename = "dipn";
+        amount = Tez.zero;
+        parameter = "Pair (Pair (Pair (Pair 0 1) 2) 3) 4";
+        storage = "7";
+      };
+      {
+        filename = "dugn";
+        amount = Tez.zero;
+        parameter = "Pair (Pair (Pair (Pair 0 1) 2) 3) 4";
+        storage = "7";
+      };
+      {
+        filename = "ediv";
+        amount = Tez.zero;
+        parameter = "Pair 127 11";
+        storage = "Pair None None None None";
+      };
+      {
+        filename = "faucet";
+        amount = Tez.zero;
+        parameter = "\"tz1faswCTDciRzE4oJ9jn2Vm2dvjeyA9fUzU\"";
+        storage = "\"2020-01-01T00:00:00Z\"";
+      };
+      {
+        filename = "get_and_update_map";
+        amount = Tez.zero;
+        parameter = "\"abc\"";
+        storage = "Pair (Some 321) {Elt \"def\" 123}";
+      };
+      {filename = "if"; amount = Tez.zero; parameter = "True"; storage = "None"};
+      {
+        filename = "insertion_sort";
+        amount = Tez.zero;
+        parameter = "{8; 3; 2; 7; 6; 9; 5; 1; 4; 0}";
+        storage = "{}";
+      };
+      {
+        filename = "list_map_block";
+        amount = Tez.zero;
+        parameter = "{1; 2; 3; 4; 5; 6; 7}";
+        storage = "{}";
+      };
+      {
+        filename = "loop_left";
+        amount = Tez.zero;
+        parameter = "{\"abc\"; \"xyz\"}";
+        storage = "{\"zyx\"; \"cba\"}";
+      };
+      {
+        filename = "packunpack";
+        amount = Tez.zero;
+        parameter =
+          "Pair (Pair (Pair \"abc\" {1; 2; 3}) {4; 5; 6}) \
+           0x0507070707010000000361626302000000060001000200030200000006000400050006";
+        storage = "Unit";
+      };
+      {filename = "pexec"; amount = Tez.zero; parameter = "7"; storage = "77"};
+      {
+        filename = "reverse_loop";
+        amount = Tez.zero;
+        parameter = "{\"abc\" ; \"def\" ; \"ghi\"}";
+        storage = "{}";
+      };
+      {
+        filename = "set_delegate";
+        amount = Tez.zero;
+        parameter = "Some \"tz1gjaF81ZRRvdzjobyfVNsAeSC6PScjfQwN\"";
+        storage = "Unit";
+      };
+      {
+        filename = "shifts";
+        amount = Tez.zero;
+        parameter = "Right (Pair 3 2)";
+        storage = "None";
+      };
+      {
+        filename = "spawn_identities";
+        amount = Tez.of_mutez_exn 1_200_00L;
+        parameter = "7";
+        storage = "{}";
+      };
+      {
+        filename = "ticket_join";
+        amount = Tez.zero;
+        parameter = "Pair \"KT1Ln1MPvHDJ1phLL8dNL4jrKF6Q1yQCBG1v\" 17 3";
+        storage = "None";
+      };
+      {
+        filename = "ticket_split";
+        amount = Tez.zero;
+        parameter = "Pair \"KT1Ln1MPvHDJ1phLL8dNL4jrKF6Q1yQCBG1v\" 17 3";
+        storage = "Unit";
+      };
+      {
+        filename = "view_toplevel_lib";
+        amount = Tez.zero;
+        parameter = "5";
+        storage = "3";
+      };
+      {
+        filename = "xor";
+        amount = Tez.zero;
+        parameter = "Left (Pair True False)";
+        storage = "None";
       };
     |]
