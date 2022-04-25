4271cf93b5f94067995362558c3d476d56622263
Alain Mebsout <alain.mebsout@functori.com>
2022-04-25 17:05:53 +0200

    Tx_rollup,Node: Associate ticket indexes to tickets in context

--- src/proto_alpha/lib_tx_rollup/context.ml
+++ src/proto_alpha/lib_tx_rollup/context.ml
@@ -26,7 +26,14 @@
 (* The L2 context for the rollup node has the form of a binary Merkle tree. We
    reuse the implementation provided by [Tezos_context] as it is stable. Proofs
    produced by the node will be verifiable by the protocol. *)
-include Tezos_context.Context_binary
+module Raw = Tezos_context.Context_binary
+include Raw
+
+module Keys = struct
+  let l2_context = ["l2_context"]
+
+  let tickets = ["tickets"]
+end
 
 module Irmin_storage :
   Protocol.Tx_rollup_l2_storage_sig.STORAGE
@@ -36,21 +43,21 @@ module Irmin_storage :
 
   type 'a m = 'a tzresult Lwt.t
 
-  let path k = [Bytes.to_string k]
+  let path k = Keys.l2_context @ [Bytes.to_string k]
 
   let get (ctxt : context) key : bytes option m =
     let open Lwt_result_syntax in
-    let*! res = find ctxt (path key) in
+    let*! res = Raw.find ctxt (path key) in
     return res
 
   let set ctxt key value =
     let open Lwt_result_syntax in
-    let*! ctxt = add ctxt (path key) value in
+    let*! ctxt = Raw.add ctxt (path key) value in
     return ctxt
 
   let remove ctxt key =
     let open Lwt_result_syntax in
-    let*! ctxt = remove ctxt (path key) in
+    let*! ctxt = Raw.remove ctxt (path key) in
     return ctxt
 
   module Syntax = struct
@@ -84,30 +91,30 @@ let context_hash_to_l2 hash =
 let l2_to_context_hash hash =
   Protocol.Tx_rollup_l2_context_hash.to_bytes hash |> Context_hash.of_bytes_exn
 
-let exists index hash = exists index (l2_to_context_hash hash)
+let exists index hash = Raw.exists index (l2_to_context_hash hash)
 
 (* The context hashes are not dependant on the time, we use EPOCH (i.e. 0) to
    commit (and hash). *)
 
 let hash ?(message = "") context =
-  hash ~time:Time.Protocol.epoch ~message context |> context_hash_to_l2
+  Raw.hash ~time:Time.Protocol.epoch ~message context |> context_hash_to_l2
 
 let commit ?(message = "") context =
   let open Lwt_syntax in
-  if is_empty context then
+  if Raw.is_empty context then
     (* We cannot commit empty contexts with Irmin 3 *)
     return (hash ~message context)
   else
-    let+ hash = commit ~time:Time.Protocol.epoch ~message context in
+    let+ hash = Raw.commit ~time:Time.Protocol.epoch ~message context in
     context_hash_to_l2 hash
 
 let checkout_opt index context_hash =
   let open Lwt_syntax in
-  let+ context = checkout index (l2_to_context_hash context_hash) in
+  let+ context = Raw.checkout index (l2_to_context_hash context_hash) in
   match context with
   | Some context -> Some context
   | None ->
-      let empty = empty index in
+      let empty = Raw.empty index in
       let hash_empty = hash empty in
       if Protocol.Tx_rollup_l2_context_hash.(context_hash = hash_empty) then
         Some empty
@@ -152,11 +159,11 @@ module Prover_storage :
 
   let path k = [Bytes.to_string k]
 
-  let get store key = Tree.find store (path key)
+  let get store key = Raw.Tree.find store (path key)
 
-  let set store key value = Tree.add store (path key) value
+  let set store key value = Raw.Tree.add store (path key) value
 
-  let remove store key = Tree.remove store (path key)
+  let remove store key = Raw.Tree.remove store (path key)
 end
 
 module Prover_context = Protocol.Tx_rollup_l2_context.Make (Prover_storage)
@@ -165,28 +172,28 @@ type 'a produce_proof_result = {tree : tree; result : 'a}
 
 let get_tree ctxt =
   let open Lwt_result_syntax in
-  let*! tree_opt = find_tree ctxt [] in
+  let*! tree_opt = Raw.find_tree ctxt Keys.l2_context in
   match tree_opt with
   | Some tree -> return tree
   | None -> fail [Error.Tx_rollup_tree_not_found]
 
 let produce_proof ctxt f =
   let open Lwt_result_syntax in
-  let index = index ctxt in
+  let index = Raw.index ctxt in
   let* tree = get_tree ctxt in
   let* kinded_key =
-    match Tree.kinded_key tree with
+    match Raw.Tree.kinded_key tree with
     | Some kinded_key -> return kinded_key
     | None -> fail [Error.Tx_rollup_tree_kinded_key_not_found]
   in
   let*! (proof, result) =
-    produce_stream_proof index kinded_key (fun tree ->
+    Raw.produce_stream_proof index kinded_key (fun tree ->
         let*! res = f tree in
         Lwt.return (res.tree, res))
   in
   return (proof, result)
 
-let hash_tree = Tree.hash
+let hash_tree = Raw.Tree.hash
 
 let tree_hash_of_context ctxt =
   let open Lwt_result_syntax in
@@ -195,7 +202,7 @@ let tree_hash_of_context ctxt =
 
 let add_tree ctxt tree =
   let open Lwt_syntax in
-  let* ctxt = add_tree ctxt [] tree in
+  let* ctxt = Raw.add_tree ctxt Keys.l2_context tree in
   (* Irmin requires that we commit the context before generating the proof. *)
   let* ctxt_hash = commit ctxt in
   return (ctxt, ctxt_hash)
@@ -208,8 +215,8 @@ let add_tree ctxt tree =
 *)
 let init_context index =
   let open Prover_context.Syntax in
-  let ctxt = empty index in
-  let tree = Tree.empty ctxt in
+  let ctxt = Raw.empty index in
+  let tree = Raw.Tree.empty ctxt in
   let* tree = Prover_context.Address_index.init_counter tree in
   let* tree = Prover_context.Ticket_index.init_counter tree in
   let tree_hash = hash_tree tree in
@@ -218,3 +225,26 @@ let init_context index =
       tree_hash = Protocol.Tx_rollup_message_result_repr.empty_l2_context_hash)) ;
   let* (ctxt, _) = add_tree ctxt tree in
   return ctxt
+
+(** {2 Sub-context for tickets } *)
+
+module Ticket_indexable =
+  Protocol.Indexable.Make (Protocol.Alpha_context.Ticket_hash)
+
+let register_ticket ctxt
+    (ticket_index : Protocol.Tx_rollup_l2_context_sig.ticket_index) ticket =
+  let index_int32 = Protocol.Indexable.to_int32 ticket_index in
+  let key = Keys.tickets @ [Int32.to_string index_int32] in
+  let value = Data_encoding.Binary.to_bytes_exn Ticket.encoding ticket in
+  Raw.add ctxt key value
+
+let get_ticket ctxt
+    (ticket_index : Protocol.Tx_rollup_l2_context_sig.ticket_index) =
+  let open Lwt_syntax in
+  let index_int32 = Protocol.Indexable.to_int32 ticket_index in
+  let key = Keys.tickets @ [Int32.to_string index_int32] in
+  let* value = Raw.find ctxt key in
+  match value with
+  | None -> return_none
+  | Some value ->
+      return_some (Data_encoding.Binary.of_bytes_exn Ticket.encoding value)
