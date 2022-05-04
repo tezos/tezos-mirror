3da76326cb647c03f02499bd5fa64de50e04eea6
Yann Regis-Gianas <yann@nomadic-labs.com>
2022-05-04 16:50:42 +0200

    Tezt,SCORU: Complete an ill-formed TODO
    Signed-off-by: Yann Regis-Gianas <yann@nomadic-labs.com>

--- tezt/tests/sc_rollup.ml
+++ tezt/tests/sc_rollup.ml
@@ -123,10 +123,9 @@ let with_fresh_rollup f tezos_node tezos_client bootstrap1_key =
   let* () = Client.bake_for tezos_client in
   f rollup_address sc_rollup_node configuration_filename
 
-(* TODO: create and insert issue number. Many tests
-   can be refactored using test_scenario.*)
-let test_scenario ?commitment_frequency ?challenge_window
-    {output_file_prefix; variant; tags; description} scenario =
+(* TODO: https://gitlab.com/tezos/tezos/-/issues/2933
+   Many tests can be refactored using test_scenario. *)
+let test_scenario {output_file_prefix; variant; tags; description} scenario =
   let output_file _ = output_file_prefix ^ "_" ^ variant in
   let tags = tags @ [variant] in
   test
