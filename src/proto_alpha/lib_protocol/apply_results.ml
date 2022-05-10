whoistiger/tezos

Proto: split internal Transaction

src/proto_alpha/lib_protocol/apply_results.ml
@@ -87,10 +87,20 @@ let contents_of_internal_operation (type kind)
     kind internal_contents =
   let operation : kind internal_manager_operation =
     match operation with
-    | Transaction {destination; amount; entrypoint; unparsed_parameters; _} ->
+    | Transaction_to_contract
+        {contract; amount; entrypoint; unparsed_parameters; _} ->
         Transaction
           {
-            destination;
+            destination = Contract contract;
+            amount;
+            entrypoint;
+            parameters = Script.lazy_expr unparsed_parameters;
+          }
+    | Transaction_to_tx_rollup
+        {dst_rollup; amount; entrypoint; unparsed_parameters; _} ->
+        Transaction
+          {
+            destination = Tx_rollup dst_rollup;
             amount;
             entrypoint;
             parameters = Script.lazy_expr unparsed_parameters;

src/proto_alpha/lib_protocol/script_interpreter_defs.ml
@@ -515,15 +515,15 @@ let transfer (ctxt, sc) gas amount location parameters_ty parameters destination
       Destination.t ->
       (Kind.transaction manager_operation * context) tzresult =
    fun ctxt tp unparsed_parameters -> function
-    | Contract _ ->
+    | Contract contract ->
         Gas.consume ctxt (Script.strip_locations_cost unparsed_parameters)
         >|? fun ctxt ->
         let unparsed_parameters =
           Micheline.strip_locations unparsed_parameters
         in
-        ( Transaction
+        ( Transaction_to_contract
             {
-              destination;
+              contract;
               amount;
               entrypoint;
               location;
@@ -543,7 +543,7 @@ let transfer (ctxt, sc) gas amount location parameters_ty parameters destination
        rollup to inject the exact type of the entrypoint as used by
        the smart contract. This allows the transaction rollup to extract
        the type of the ticket. *)
-    | Tx_rollup _ -> (
+    | Tx_rollup dst_rollup -> (
         match tp with
         | Pair_t (Ticket_t (tp, _), _, _, _) ->
             Script_ir_translator.unparse_ty
@@ -559,9 +559,9 @@ let transfer (ctxt, sc) gas amount location parameters_ty parameters destination
             let unparsed_parameters =
               Micheline.strip_locations unparsed_parameters
             in
-            ( Transaction
+            ( Transaction_to_tx_rollup
                 {
-                  destination;
+                  dst_rollup;
                   amount;
                   entrypoint;
                   location;

src/proto_alpha/lib_protocol/script_typed_ir.ml
@@ -1367,8 +1367,18 @@ and ('input, 'output) view_signature =
       -> ('input, 'output) view_signature
 
 and 'kind manager_operation =
-  | Transaction : {
-      destination : Destination.t;
+  | Transaction_to_contract : {
+      contract : Contract.t;
+      amount : Tez.tez;
+      entrypoint : Entrypoint.t;
+      location : Script.location;
+      parameters_ty : ('a, _) ty;
+      parameters : 'a;
+      unparsed_parameters : Script.expr;
+    }
+      -> Kind.transaction manager_operation
+  | Transaction_to_tx_rollup : {
+      dst_rollup : Tx_rollup.t;
       amount : Tez.tez;
       entrypoint : Entrypoint.t;
       location : Script.location;
@@ -1409,7 +1419,8 @@ type packed_manager_operation =
 
 let manager_kind : type kind. kind manager_operation -> kind Kind.manager =
   function
-  | Transaction _ -> Kind.Transaction_manager_kind
+  | Transaction_to_contract _ -> Kind.Transaction_manager_kind
+  | Transaction_to_tx_rollup _ -> Kind.Transaction_manager_kind
   | Origination _ -> Kind.Origination_manager_kind
   | Delegation _ -> Kind.Delegation_manager_kind
 

src/proto_alpha/lib_protocol/script_typed_ir.mli
@@ -1495,13 +1495,23 @@ and ('input, 'output) view_signature =
       -> ('input, 'output) view_signature
 
 and 'kind manager_operation =
-  | Transaction : {
+  | Transaction_to_contract : {
       (* The [unparsed_parameters] field may seem useless since we have
          access to a typed version of the field (with [parameters_ty] and
          [parameters]), but we keep it so that we do not have to unparse the
          typed version in order to produce the receipt
          ([Apply_results.internal_manager_operation]). *)
-      destination : Destination.t;
+      contract : Contract.t;
+      amount : Tez.tez;
+      entrypoint : Entrypoint.t;
+      location : Script.location;
+      parameters_ty : ('a, _) ty;
+      parameters : 'a;
+      unparsed_parameters : Script.expr;
+    }
+      -> Kind.transaction manager_operation
+  | Transaction_to_tx_rollup : {
+      dst_rollup : Tx_rollup.t;
       amount : Tez.tez;
       entrypoint : Entrypoint.t;
       location : Script.location;

src/proto_alpha/lib_protocol/test/integration/michelson/test_ticket_operations_diff.ml
@@ -292,12 +292,12 @@ let transfer_operation ~incr ~src ~destination ~parameters_ty ~parameters =
         {
           source = src;
           operation =
-            Transaction
+            Transaction_to_contract
               {
                 amount = Tez.zero;
                 unparsed_parameters = Micheline.strip_locations params_node;
                 entrypoint = Entrypoint.default;
-                destination = Destination.Contract destination;
+                contract = destination;
                 location = Micheline.dummy_location;
                 parameters_ty;
                 parameters;
@@ -324,12 +324,12 @@ let transfer_operation_to_tx_rollup ~incr ~src ~parameters_ty ~parameters
         {
           source = src;
           operation =
-            Transaction
+            Transaction_to_tx_rollup
               {
                 amount = Tez.zero;
                 unparsed_parameters = Micheline.strip_locations params_node;
                 entrypoint = Tx_rollup.deposit_entrypoint;
-                destination = Destination.Tx_rollup tx_rollup;
+                dst_rollup = tx_rollup;
                 location = Micheline.dummy_location;
                 parameters_ty;
                 parameters;

src/proto_alpha/lib_protocol/ticket_operations_diff.ml
@@ -238,12 +238,12 @@ let tickets_of_origination ctxt ~preorigination ~storage_type ~storage =
 let tickets_of_operation ctxt
     (Script_typed_ir.Internal_operation {source = _; operation; nonce = _}) =
   match operation with
-  | Transaction
+  | Transaction_to_contract
       {
         amount = _;
         unparsed_parameters = _;
         entrypoint;
-        destination = Destination.Contract destination;
+        contract = destination;
         location;
         parameters_ty;
         parameters;
@@ -255,9 +255,9 @@ let tickets_of_operation ctxt
         ~location
         ~parameters_ty
         ~parameters
-  | Transaction
+  | Transaction_to_tx_rollup
       {
-        destination = Destination.Tx_rollup tx_rollup_dest;
+        dst_rollup;
         unparsed_parameters = _;
         entrypoint;
         amount = _;
@@ -271,7 +271,7 @@ let tickets_of_operation ctxt
         return
           ( Some
               {
-                destination = Destination.Tx_rollup tx_rollup_dest;
+                destination = Destination.Tx_rollup dst_rollup;
                 tickets = [ex_ticket];
               },
             ctxt )

tezos/tezos-mirror

src/proto_alpha/lib_protocol/apply_results.ml
@@ -87,10 +87,20 @@ let contents_of_internal_operation (type kind)
     kind internal_contents =
   let operation : kind internal_manager_operation =
     match operation with
-    | Transaction {destination; amount; entrypoint; unparsed_parameters; _} ->
+    | Transaction_to_contract
+        {destination; amount; entrypoint; unparsed_parameters; _} ->
         Transaction
           {
-            destination;
+            destination = Contract destination;
+            amount;
+            entrypoint;
+            parameters = Script.lazy_expr unparsed_parameters;
+          }
+    | Transaction_to_tx_rollup
+        {destination; amount; entrypoint; unparsed_parameters; _} ->
+        Transaction
+          {
+            destination = Tx_rollup destination;
             amount;
             entrypoint;
             parameters = Script.lazy_expr unparsed_parameters;

src/proto_alpha/lib_protocol/script_interpreter_defs.ml
@@ -515,13 +515,13 @@ let transfer (ctxt, sc) gas amount location parameters_ty parameters destination
       Destination.t ->
       (Kind.transaction manager_operation * context) tzresult =
    fun ctxt tp unparsed_parameters -> function
-    | Contract _ ->
+    | Contract destination ->
         Gas.consume ctxt (Script.strip_locations_cost unparsed_parameters)
         >|? fun ctxt ->
         let unparsed_parameters =
           Micheline.strip_locations unparsed_parameters
         in
-        ( Transaction
+        ( Transaction_to_contract
             {
               destination;
               amount;
@@ -543,7 +543,7 @@ let transfer (ctxt, sc) gas amount location parameters_ty parameters destination
        rollup to inject the exact type of the entrypoint as used by
        the smart contract. This allows the transaction rollup to extract
        the type of the ticket. *)
-    | Tx_rollup _ -> (
+    | Tx_rollup destination -> (
         match tp with
         | Pair_t (Ticket_t (tp, _), _, _, _) ->
             Script_ir_translator.unparse_ty
@@ -559,7 +559,7 @@ let transfer (ctxt, sc) gas amount location parameters_ty parameters destination
             let unparsed_parameters =
               Micheline.strip_locations unparsed_parameters
             in
-            ( Transaction
+            ( Transaction_to_tx_rollup
                 {
                   destination;
                   amount;

src/proto_alpha/lib_protocol/script_typed_ir.ml
@@ -1367,8 +1367,18 @@ and ('input, 'output) view_signature =
       -> ('input, 'output) view_signature
 
 and 'kind manager_operation =
-  | Transaction : {
-      destination : Destination.t;
+  | Transaction_to_contract : {
+      destination : Contract.t;
+      amount : Tez.tez;
+      entrypoint : Entrypoint.t;
+      location : Script.location;
+      parameters_ty : ('a, _) ty;
+      parameters : 'a;
+      unparsed_parameters : Script.expr;
+    }
+      -> Kind.transaction manager_operation
+  | Transaction_to_tx_rollup : {
+      destination : Tx_rollup.t;
       amount : Tez.tez;
       entrypoint : Entrypoint.t;
       location : Script.location;
@@ -1409,7 +1419,8 @@ type packed_manager_operation =
 
 let manager_kind : type kind. kind manager_operation -> kind Kind.manager =
   function
-  | Transaction _ -> Kind.Transaction_manager_kind
+  | Transaction_to_contract _ -> Kind.Transaction_manager_kind
+  | Transaction_to_tx_rollup _ -> Kind.Transaction_manager_kind
   | Origination _ -> Kind.Origination_manager_kind
   | Delegation _ -> Kind.Delegation_manager_kind
 

src/proto_alpha/lib_protocol/script_typed_ir.mli
@@ -1495,13 +1495,23 @@ and ('input, 'output) view_signature =
       -> ('input, 'output) view_signature
 
 and 'kind manager_operation =
-  | Transaction : {
+  | Transaction_to_contract : {
       (* The [unparsed_parameters] field may seem useless since we have
          access to a typed version of the field (with [parameters_ty] and
          [parameters]), but we keep it so that we do not have to unparse the
          typed version in order to produce the receipt
          ([Apply_results.internal_manager_operation]). *)
-      destination : Destination.t;
+      destination : Contract.t;
+      amount : Tez.tez;
+      entrypoint : Entrypoint.t;
+      location : Script.location;
+      parameters_ty : ('a, _) ty;
+      parameters : 'a;
+      unparsed_parameters : Script.expr;
+    }
+      -> Kind.transaction manager_operation
+  | Transaction_to_tx_rollup : {
+      destination : Tx_rollup.t;
       amount : Tez.tez;
       entrypoint : Entrypoint.t;
       location : Script.location;

src/proto_alpha/lib_protocol/test/integration/michelson/test_ticket_operations_diff.ml
@@ -292,12 +292,12 @@ let transfer_operation ~incr ~src ~destination ~parameters_ty ~parameters =
         {
           source = src;
           operation =
-            Transaction
+            Transaction_to_contract
               {
                 amount = Tez.zero;
                 unparsed_parameters = Micheline.strip_locations params_node;
                 entrypoint = Entrypoint.default;
-                destination = Destination.Contract destination;
+                destination;
                 location = Micheline.dummy_location;
                 parameters_ty;
                 parameters;
@@ -324,12 +324,12 @@ let transfer_operation_to_tx_rollup ~incr ~src ~parameters_ty ~parameters
         {
           source = src;
           operation =
-            Transaction
+            Transaction_to_tx_rollup
               {
                 amount = Tez.zero;
                 unparsed_parameters = Micheline.strip_locations params_node;
                 entrypoint = Tx_rollup.deposit_entrypoint;
-                destination = Destination.Tx_rollup tx_rollup;
+                destination = tx_rollup;
                 location = Micheline.dummy_location;
                 parameters_ty;
                 parameters;

src/proto_alpha/lib_protocol/ticket_operations_diff.ml
@@ -238,12 +238,12 @@ let tickets_of_origination ctxt ~preorigination ~storage_type ~storage =
 let tickets_of_operation ctxt
     (Script_typed_ir.Internal_operation {source = _; operation; nonce = _}) =
   match operation with
-  | Transaction
+  | Transaction_to_contract
       {
         amount = _;
         unparsed_parameters = _;
         entrypoint;
-        destination = Destination.Contract destination;
+        destination;
         location;
         parameters_ty;
         parameters;
@@ -255,9 +255,9 @@ let tickets_of_operation ctxt
         ~location
         ~parameters_ty
         ~parameters
-  | Transaction
+  | Transaction_to_tx_rollup
       {
-        destination = Destination.Tx_rollup tx_rollup_dest;
+        destination;
         unparsed_parameters = _;
         entrypoint;
         amount = _;
@@ -271,7 +271,7 @@ let tickets_of_operation ctxt
         return
           ( Some
               {
-                destination = Destination.Tx_rollup tx_rollup_dest;
+                destination = Destination.Tx_rollup destination;
                 tickets = [ex_ticket];
               },
             ctxt )