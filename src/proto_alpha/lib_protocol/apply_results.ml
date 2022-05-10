whoistiger/tezos

Proto: remove entrypoint field of Transaction_to_tx_rollup

src/proto_alpha/lib_protocol/apply_results.ml
@@ -96,13 +96,12 @@ let contents_of_internal_operation (type kind)
             entrypoint;
             parameters = Script.lazy_expr unparsed_parameters;
           }
-    | Transaction_to_tx_rollup {dst_rollup; entrypoint; unparsed_parameters; _}
-      ->
+    | Transaction_to_tx_rollup {dst_rollup; unparsed_parameters; _} ->
         Transaction
           {
             destination = Tx_rollup dst_rollup;
             amount = Tez.zero;
-            entrypoint;
+            entrypoint = Tx_rollup.deposit_entrypoint;
             parameters = Script.lazy_expr unparsed_parameters;
           }
     | Origination {origination; _} -> Origination origination

src/proto_alpha/lib_protocol/script_interpreter_defs.ml
@@ -552,6 +552,10 @@ let make_transaction_to_tx_rollup (type t tc) ctxt ~dst_rollup ~amount
      the type of the ticket. *)
   error_unless Tez.(amount = zero) Tx_rollup_invalid_transaction_amount
   >>?= fun () ->
+  error_unless
+    Entrypoint.(entrypoint = Tx_rollup.deposit_entrypoint)
+    (Script_tc_errors.No_such_entrypoint entrypoint)
+  >>?= fun () ->
   match parameters_ty with
   | Pair_t (Ticket_t (tp, _), _, _, _) ->
       unparse_data ctxt Optimized parameters_ty parameters
@@ -568,13 +572,7 @@ let make_transaction_to_tx_rollup (type t tc) ctxt ~dst_rollup ~amount
             Micheline.strip_locations unparsed_parameters
           in
           ( Transaction_to_tx_rollup
-              {
-                dst_rollup;
-                entrypoint;
-                parameters_ty;
-                parameters;
-                unparsed_parameters;
-              },
+              {dst_rollup; parameters_ty; parameters; unparsed_parameters},
             ctxt ) )
   | _ ->
       (* TODO: https://gitlab.com/tezos/tezos/-/issues/2455

src/proto_alpha/lib_protocol/ticket_operations_diff.ml
@@ -256,24 +256,16 @@ let tickets_of_operation ctxt
         ~parameters_ty
         ~parameters
   | Transaction_to_tx_rollup
-      {
-        dst_rollup;
-        unparsed_parameters = _;
-        entrypoint;
-        parameters_ty;
-        parameters;
-      } ->
-      if Entrypoint.(entrypoint = Tx_rollup.deposit_entrypoint) then
-        Tx_rollup_parameters.get_deposit_parameters parameters_ty parameters
-        >>?= fun {ex_ticket; l2_destination = _} ->
-        return
-          ( Some
-              {
-                destination = Destination.Tx_rollup dst_rollup;
-                tickets = [ex_ticket];
-              },
-            ctxt )
-      else return (None, ctxt)
+      {dst_rollup; unparsed_parameters = _; parameters_ty; parameters} ->
+      Tx_rollup_parameters.get_deposit_parameters parameters_ty parameters
+      >>?= fun {ex_ticket; l2_destination = _} ->
+      return
+        ( Some
+            {
+              destination = Destination.Tx_rollup dst_rollup;
+              tickets = [ex_ticket];
+            },
+          ctxt )
   | Origination
       {
         origination = {delegate = _; script = _; credit = _};

tezos/tezos-mirror

src/proto_alpha/lib_protocol/apply_results.ml
@@ -96,14 +96,13 @@ let contents_of_internal_operation (type kind)
             entrypoint;
             parameters = Script.lazy_expr unparsed_parameters;
           }
-    | Transaction_to_tx_rollup {destination; entrypoint; unparsed_parameters; _}
-      ->
+    | Transaction_to_tx_rollup {destination; unparsed_parameters; _} ->
         Transaction
           {
             destination = Tx_rollup destination;
             (* Dummy amount used for the external untyped view of internal transactions *)
             amount = Tez.zero;
-            entrypoint;
+            entrypoint = Tx_rollup.deposit_entrypoint;
             parameters = Script.lazy_expr unparsed_parameters;
           }
     | Origination {origination; _} -> Origination origination

src/proto_alpha/lib_protocol/script_interpreter_defs.ml
@@ -552,6 +552,10 @@ let make_transaction_to_tx_rollup (type t tc) ctxt ~destination ~amount
      the type of the ticket. *)
   error_unless Tez.(amount = zero) Tx_rollup_invalid_transaction_amount
   >>?= fun () ->
+  error_unless
+    Entrypoint.(entrypoint = Tx_rollup.deposit_entrypoint)
+    (Script_tc_errors.No_such_entrypoint entrypoint)
+  >>?= fun () ->
   match parameters_ty with
   | Pair_t (Ticket_t (tp, _), _, _, _) ->
       unparse_data ctxt Optimized parameters_ty parameters
@@ -568,13 +572,7 @@ let make_transaction_to_tx_rollup (type t tc) ctxt ~destination ~amount
             Micheline.strip_locations unparsed_parameters
           in
           ( Transaction_to_tx_rollup
-              {
-                destination;
-                entrypoint;
-                parameters_ty;
-                parameters;
-                unparsed_parameters;
-              },
+              {destination; parameters_ty; parameters; unparsed_parameters},
             ctxt ) )
   | _ ->
       (* TODO: https://gitlab.com/tezos/tezos/-/issues/2455

src/proto_alpha/lib_protocol/ticket_operations_diff.ml
@@ -256,24 +256,16 @@ let tickets_of_operation ctxt
         ~parameters_ty
         ~parameters
   | Transaction_to_tx_rollup
-      {
-        destination;
-        unparsed_parameters = _;
-        entrypoint;
-        parameters_ty;
-        parameters;
-      } ->
-      if Entrypoint.(entrypoint = Tx_rollup.deposit_entrypoint) then
-        Tx_rollup_parameters.get_deposit_parameters parameters_ty parameters
-        >>?= fun {ex_ticket; l2_destination = _} ->
-        return
-          ( Some
-              {
-                destination = Destination.Tx_rollup destination;
-                tickets = [ex_ticket];
-              },
-            ctxt )
-      else return (None, ctxt)
+      {destination; unparsed_parameters = _; parameters_ty; parameters} ->
+      Tx_rollup_parameters.get_deposit_parameters parameters_ty parameters
+      >>?= fun {ex_ticket; l2_destination = _} ->
+      return
+        ( Some
+            {
+              destination = Destination.Tx_rollup destination;
+              tickets = [ex_ticket];
+            },
+          ctxt )
   | Origination
       {
         origination = {delegate = _; script = _; credit = _};