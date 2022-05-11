883e236e6db1b70c9a3c100afe215fab6bbd71c2
Raphaël Proust <code@bnwr.net>
2022-05-11 08:40:27 +0200

    Misc: tweak some comments to make upcoming ocamlformat no-op
    These comments are formatted in specific ways which cause the new
    version of ocamlformat to change them. We preemptively change the
    indentation to anticipate the upcoming change.

--- src/lib_stdlib/circular_buffer.ml
+++ src/lib_stdlib/circular_buffer.ml
@@ -53,34 +53,33 @@ let create ?(maxlength = 1 lsl 15) ?(fresh_buf_size = 2000) () =
 
 (* [get_buf_with_offset t write_len] Find a place where [write_len] data can be written onto the buffer [t].
 
-Multiple situtation can arise
+   Multiple situtation can arise
 
-1) STARTS preceeds END,
- _____START____________________END_______
-[_______|ddddddddddddddddddddddd|________]
-|<--Y-->|<----- data ---------->|<---X-->|
+   1) STARTS preceeds END,
+    _____START____________________END_______
+   [_______|ddddddddddddddddddddddd|________]
+   |<--Y-->|<----- data ---------->|<---X-->|
 
-   1.1) either X zone can contain [write_len],
-   1.2) or Y zone can contain [write_len],
-   1.3) or neither is big enough,we create a temporary buffer of size [write_len]
+      1.1) either X zone can contain [write_len],
+      1.2) or Y zone can contain [write_len],
+      1.3) or neither is big enough,we create a temporary buffer of size [write_len]
 
-2) END  preceeds START ,
- ______END____________________START______
-[ddddddd|_______________________|dddddd__]
+   2) END  preceeds START ,
+    ______END____________________START______
+   [ddddddd|_______________________|dddddd__]
 
-   2.1) either the free zone between END and START can contain [write_len],
-   2.2) or we create a temporary buffer of size [write_len]
+      2.1) either the free zone between END and START can contain [write_len],
+      2.2) or we create a temporary buffer of size [write_len]
 
-3) START and END are identical
-  3.1)
- ____START_END___________________________
-[_______|________________________________]=> t.full = false
+   3) START and END are identical
+     3.1)
+    ____START_END___________________________
+   [_______|________________________________]=> t.full = false
 
-  3.2)
- ____END_START___________________________
-[ddddddd|dddddddddddddddddddddddddddddd__] => t.full = true
-
- *)
+     3.2)
+    ____END_START___________________________
+   [ddddddd|dddddddddddddddddddddddddddddd__] => t.full = true
+*)
 (* Pre-condition: write_len > 0 *)
 let get_buf_with_offset t write_len =
   (* Case 3.1 -> put the pointers at the beginning of the buffer which
@@ -115,64 +114,64 @@ let get_buf_with_offset t write_len =
 
    After a correct write the following property holds:
 
-'o' stands for old data
-'_' for free zone
-'w' for just written data
-'r' is the returned record
+   'o' stands for old data
+   '_' for free zone
+   'w' for just written data
+   'r' is the returned record
 
-- initial situation STARTS preceeds END,
- ___________________START___________END_________
-[_____________________|oooooooooooooo|__________]
-|<--------X zone----->|              |<-Y zone->|
+   - initial situation STARTS preceeds END,
+    ___________________START___________END_________
+   [_____________________|oooooooooooooo|__________]
+   |<--------X zone----->|              |<-Y zone->|
 
-   - either X zone can contain [write_len],
- ___________________START_________OLD_END__NEW_END
-[_____________________|oooooooooooooo|wwwwwwww|_] = r.buf
-                                     |<------>|
-                                     |r.length|
-                                     |
-                                     r.offset
+      - either X zone can contain [write_len],
+         ___________________START_________OLD_END__NEW_END
+        [_____________________|oooooooooooooo|wwwwwwww|_] = r.buf
+                                             |<------>|
+                                             |r.length|
+                                             |
+                                             r.offset
 
-   - or Y zone can contain [write_len],
- _________NEW_END____START_________OLD_END______
-[wwwwwwwwwwww|________|oooooooooooooo|__________] = r.buf
-|<-r.length->|
-|
-r.offset=0
-
-   - or neither is big enough,we create a temporary buffer of size [write_len]
- ___________________START___________END_________    |<---max_len---->|
-[_____________________|oooooooooooooo|__________]   [wwwwwwwwwwww____]= r.buf
-                                                    |<-r.length->|
-                                                    |
-                                                    r.offset=0
--  END  preceeds START ,
- ______END____________________START______
-[ooooooo|_______________________|oooooo__]
-
-   - either empty zone can contain [write_len] and a little bit more,
- _____OLD__END______NEW_END___START______
-[ooooooo|wwwwwwwwwwwwwwww|______|oooooo__]
-        |<------>|
-        |r.length|
+      - or Y zone can contain [write_len],
+         _________NEW_END____START_________OLD_END______
+        [wwwwwwwwwwww|________|oooooooooooooo|__________] = r.buf
+        |<-r.length->|
         |
-        r.offset
-   - either empty zone can contain [write_len],
- _____OLD__END___________NEW_END_START___
-[ooooooo|wwwwwwwwwwwwwwwwwwwwwww|oooooo__]
-        |<------>|
-        |r.length|
-        |
-        r.offset
-        t.full = true
+        r.offset=0
 
-   - or we create a temporary buffer of size [max_len]
- ______END____________________START______  |<--------max_len----------->|
-[ooooooo|_______________________|oooooo__] [wwwwwwwwwwwwwwwwwwwwwwww____]
-                                           |<-------r.length----------->|
-                                           |
-                                           r.offset=0
- *)
+      - or neither is big enough,we create a temporary buffer of size [write_len]
+         ___________________START___________END_________    |<---max_len---->|
+        [_____________________|oooooooooooooo|__________]   [wwwwwwwwwwww____]= r.buf
+                                                            |<-r.length->|
+                                                            |
+                                                            r.offset=0
+   -  END  preceeds START ,
+       ______END____________________START______
+      [ooooooo|_______________________|oooooo__]
+
+      - either empty zone can contain [write_len] and a little bit more,
+         _____OLD__END______NEW_END___START______
+        [ooooooo|wwwwwwwwwwwwwwww|______|oooooo__]
+                |<------>|
+                |r.length|
+                |
+                r.offset
+      - either empty zone can contain [write_len],
+         _____OLD__END___________NEW_END_START___
+        [ooooooo|wwwwwwwwwwwwwwwwwwwwwww|oooooo__]
+                |<------>|
+                |r.length|
+                |
+                r.offset
+                t.full = true
+
+      - or we create a temporary buffer of size [max_len]
+        ______END____________________START______  |<--------max_len----------->|
+       [ooooooo|_______________________|oooooo__] [wwwwwwwwwwwwwwwwwwwwwwww____]
+                                                  |<-------r.length----------->|
+                                                  |
+                                                  r.offset=0
+*)
 let write ~maxlen ~fill_using t =
   if maxlen < 0 then invalid_arg "Circular_buffer.write: negative length." ;
   if maxlen = 0 then
@@ -195,62 +194,60 @@ let write ~maxlen ~fill_using t =
    [data.buf]  and update [t.data_start] pointer accordingly.
    data are blit into buffer [into] at [offset].
 
-if data.buf is not the circular buffer, it is supposed to be a
-dedicated buffer allocated at write time and we have no bookkeeping
-to do on the circular buffer.
+   if data.buf is not the circular buffer, it is supposed to be a
+   dedicated buffer allocated at write time and we have no bookkeeping
+   to do on the circular buffer.
 
-Else starting from
+   Else starting from
 
- ______START____________END_____
-[________|ddddd|ddddddddd|______]  [dddddddddddddddddddddddd____]
-         |<--->|<------->|         |<---------------------->|
-           d1       d3                         d2
-It is required to read fully d1, d2, and then d3 in that order.
+    ______START____________END_____
+   [________|ddddd|ddddddddd|______]  [dddddddddddddddddddddddd____]
+            |<--->|<------->|         |<---------------------->|
+              d1       d3                         d2
+   It is required to read fully d1, d2, and then d3 in that order.
 
-We can have a parial read for each chunk leading to a new data chunk d1'
- _________START_________END_____
-[___________|dd|ddddddddd|______]  [dddddddddddddddddddddddd____]
-            |<>|<------->|         |<---------------------->|
-             d1'    d3                         d2
-but the remainder has to be consumed to ensure that further readings
- will succeed.
- ____________START______END_____
-[______________|ddddddddd|______]  [dddddddddddddddddddddddd____]
-               |<------->|         |<---------------------->|
-                    d3                         d2
-When reading extra allocated chunk we don't have to do any bookkeeping
- ____________START______END_____
-[______________|ddddddddd|______]  [___________|dddddddddddd____]
-               |<------->|                     |<---------->|
-                    d3                               d2'
+   We can have a parial read for each chunk leading to a new data chunk d1'
+    _________START_________END_____
+   [___________|dd|ddddddddd|______]  [dddddddddddddddddddddddd____]
+               |<>|<------->|         |<---------------------->|
+                d1'    d3                         d2
+   but the remainder has to be consumed to ensure that further readings
+    will succeed.
+    ____________START______END_____
+   [______________|ddddddddd|______]  [dddddddddddddddddddddddd____]
+                  |<------->|         |<---------------------->|
+                       d3                         d2
+   When reading extra allocated chunk we don't have to do any bookkeeping
+    ____________START______END_____
+   [______________|ddddddddd|______]  [___________|dddddddddddd____]
+                  |<------->|                     |<---------->|
+                       d3                               d2'
 
-Each time we read a chunk in the circular buffer we move start at the
-end of chunk we just read.
+   Each time we read a chunk in the circular buffer we move start at the
+   end of chunk we just read.
 
-Most of the time START points to the begining of the next chunk to
-read, but in one case starting from this situation (where d2
- was to big to fit after d1)
+   Most of the time START points to the begining of the next chunk to
+   read, but in one case starting from this situation (where d2
+    was to big to fit after d1)
 
- _______END____START____
-[dddd|dddd|_____|dddd|__]
-| d2 | d3 |     | d1 |
+    _______END____START____
+   [dddd|dddd|_____|dddd|__]
+   | d2 | d3 |     | d1 |
 
-reading d1 then d2 leads to
+   reading d1 then d2 leads to
 
- _______END___________START
-[dddd|dddd|_____________|__]
+    _______END___________START
+   [dddd|dddd|_____________|__]
 
-Thats why we do
-      t.data_start <- data.offset + len ;
-and not
-      t.data_start <- t.data_start + len ;
+   Thats why we do
+         t.data_start <- data.offset + len ;
+   and not
+         t.data_start <- t.data_start + len ;
 
-An alternative would be to remember that the last bytes of the buffer
-where not used, and to check whether start should be set at the
-begining of the buffer at each read.
-
- *)
-
+   An alternative would be to remember that the last bytes of the buffer
+   where not used, and to check whether start should be set at the
+   begining of the buffer at each read.
+*)
 let read data ?(len = data.length) t ~into ~offset =
   if len > data.length then
     invalid_arg "Circular_buffer.read: len > (length data)." ;
