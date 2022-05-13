c1e656fe317f1bacea889b56ebd20648ba81e925
Rémy El Sibaïe <remy@nomadic-labs.com>
2022-05-13 09:48:45 +0200

    Workers: on chain_validator, replace Event by simple events

--- src/lib_shell/chain_validator.ml
+++ src/lib_shell/chain_validator.ml
@@ -110,6 +110,7 @@ module Logger =
     end)
 
 module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types) (Logger)
+module Events = Chain_validator_events
 open Types
 
 type t = Worker.infinite Worker.queue Worker.t
@@ -181,7 +182,7 @@ let with_activated_peer_validator w peer_id f =
   let nv = Worker.state w in
   let* pv =
     P2p_peer.Error_table.find_or_make nv.active_peers peer_id (fun () ->
-        let* () = Worker.log_event w (Connection peer_id) in
+        let* () = Events.(emit connection) peer_id in
         let* pv =
           Peer_validator.create
             ~notify_new_block:(notify_new_block w (Some peer_id))
@@ -341,7 +342,7 @@ let may_switch_test_chain w active_chains spawn_child block =
   match r with
   | Ok () -> Lwt.return_unit
   | Error err ->
-      Worker.record_event w (Could_not_switch_testchain err) ;
+      let*! () = Events.(emit could_not_switch_testchain) err in
       Lwt.return_unit
 
 let broadcast_head w ~previous block =
@@ -376,9 +377,7 @@ let safe_get_prevalidator_filter hash =
             Protocol_hash.pp_short
             hash
       | Some protocol ->
-          let* () =
-            Chain_validator_event.(emit prevalidator_filter_not_found) hash
-          in
+          let* () = Events.(emit prevalidator_filter_not_found) hash in
           let (module Proto) = protocol in
           let module Filter = Prevalidator_filters.No_filter (Proto) in
           return_ok (module Filter : Prevalidator_filters.FILTER))
@@ -395,9 +394,7 @@ let instantiate_prevalidator parameters set_prevalidator block chain_db =
   in
   match r with
   | Error errs ->
-      let* () =
-        Chain_validator_event.(emit prevalidator_reinstantiation_failure) errs
-      in
+      let* () = Events.(emit prevalidator_reinstantiation_failure) errs in
       set_prevalidator None ;
       Lwt.return_unit
   | Ok prevalidator ->
@@ -599,9 +596,7 @@ let on_error (type a b) w st (request : (a, b) Request.t) (errs : b) :
          example. If there is an error at this level, it certainly
          requires a manual operation from the maintener of the
          node. *)
-      let*! () =
-        Worker.log_event w (Request_failure (request_view, st, errs))
-      in
+      let*! () = Events.(emit request_failure) (request_view, st, errs) in
       Lwt.return_error errs
   (* We do not crash the worker in the following cases mainly for one
      reason: Such request comes from a remote peer. The payload for this
@@ -625,11 +620,8 @@ let on_completion (type a b) w (req : (a, b) Request.t) (update : a)
   Prometheus.Counter.inc_one
     nv.parameters.metrics.worker_counters.worker_completion_count ;
   match req with
-  | Request.Validated {block; _} ->
-      let fitness = Store.Block.fitness block in
-      let request = Request.Hash (Store.Block.hash block) in
+  | Request.Validated {block; _} -> (
       let level = Store.Block.level block in
-      let timestamp = Store.Block.timestamp block in
       let () =
         Shell_metrics.Worker.update_timestamps
           nv.parameters.metrics.worker_timestamps
@@ -657,20 +649,17 @@ let on_completion (type a b) w (req : (a, b) Request.t) (update : a)
                   ~metrics:nv.parameters.metrics
                   (nv.parameters.chain_store, block))
       in
-      Worker.record_event
-        w
-        (Processed_block
-           {request; request_status; update; fitness; level; timestamp}) ;
-      Lwt.return_unit
-  | Request.Notify_head (peer_id, _, _, _) ->
-      Worker.record_event w (Event.Notify_head peer_id) ;
-      Lwt.return_unit
-  | Request.Notify_branch (peer_id, _) ->
-      Worker.record_event w (Event.Notify_branch peer_id) ;
-      Lwt.return_unit
-  | Request.Disconnection peer_id ->
-      Worker.record_event w (Event.Disconnection peer_id) ;
-      Lwt.return_unit
+      let fitness = Store.Block.fitness block in
+      let block_hash = Request.Hash (Store.Block.hash block) in
+      let timestamp = Store.Block.timestamp block in
+      let event_infos = (block_hash, level, timestamp, fitness) in
+      match update with
+      | Ignored_head -> Events.(emit ignore_head) event_infos
+      | Branch_switch -> Events.(emit branch_switch) event_infos
+      | Head_increment -> Events.(emit head_increment) event_infos)
+  | Request.Notify_head (peer_id, _, _, _) -> Events.(emit notify_head) peer_id
+  | Request.Notify_branch (peer_id, _) -> Events.(emit notify_branch) peer_id
+  | Request.Disconnection peer_id -> Events.(emit disconnection) peer_id
 
 let on_close w =
   let open Lwt_syntax in
@@ -706,7 +695,7 @@ let may_load_protocols parameters =
         | false -> return_unit
         | true ->
             (* Only compile protocols that are on-disk *)
-            let*! () = Chain_validator_event.(emit loading_protocol protocol) in
+            let*! () = Events.(emit loading_protocol protocol) in
             trace
               (Validation_errors.Cannot_load_protocol protocol)
               (let* _ =
@@ -741,7 +730,7 @@ let on_launch w _ parameters =
   in
   let prevalidator = ref None in
   let when_status_changes status =
-    let*! () = Worker.log_event w (Event.Sync_status status) in
+    let*! () = Events.(emit synchronisation_status) status in
     Shell_metrics.Chain_validator.update_sync_status
       ~metrics:parameters.metrics
       status ;
@@ -763,7 +752,7 @@ let on_launch w _ parameters =
         Shell_metrics.Chain_validator.update_bootstrapped
           ~metrics:parameters.metrics
           is_bootstrapped ;
-        if is_bootstrapped then Worker.log_event w Event.Bootstrapped
+        if is_bootstrapped then Events.(emit bootstrapped) ()
         else Lwt.return_unit)
       ~when_status_changes
       ~threshold:parameters.limits.synchronisation.threshold
---
+++ src/lib_shell/chain_validator_events.ml
@@ -0,0 +1,223 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2020-2022 Nomadic Labs. <contact@nomadic-labs.com>          *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+include Internal_event.Simple
+module Request = Chain_validator_worker_state.Request
+
+let section = ["validator"; "chain"]
+
+let updated_to_checkpoint =
+  declare_2
+    ~section
+    ~name:"updated_to_checkpoint"
+    ~msg:"updated to checkpoint {block_hash} (running in mode {history_mode})"
+    ~level:Notice
+    ~pp1:Block_hash.pp
+    ~pp2:History_mode.Legacy.pp
+    ("block_hash", Block_hash.encoding)
+    ("history_mode", History_mode.Legacy.encoding)
+
+let prevalidator_filter_not_found =
+  declare_1
+    ~section
+    ~name:"prevalidator_filter_not_found"
+    ~msg:"no prevalidator filter found for protocol {protocol_hash}"
+    ~level:Warning
+    ~pp1:Protocol_hash.pp
+    ("protocol_hash", Protocol_hash.encoding)
+
+let prevalidator_reinstantiation_failure =
+  declare_1
+    ~section
+    ~name:"prevalidator_reinstantiation_failure"
+    ~msg:"failed to reinstantiate prevalidator error {trace}"
+    ~level:Error
+    ~pp1:pp_print_top_error_of_trace
+    ("trace", trace_encoding)
+
+let prevalidator_instantiation_failure =
+  declare_1
+    ~section
+    ~name:"prevalidator_instantiation_failure"
+    ~msg:"failed to instantiate the prevalidator: {trace}"
+    ~level:Error
+    ~pp1:pp_print_top_error_of_trace
+    ("trace", trace_encoding)
+
+let loading_protocol =
+  declare_1
+    ~section
+    ~name:"loading_protocol"
+    ~level:Notice
+    ~msg:"loading non-embedded protocol {protocol} from disk"
+    ~pp1:Protocol_hash.pp
+    ("protocol", Protocol_hash.encoding)
+
+let bootstrapped =
+  declare_0
+    ~section
+    ~name:"bootstrapped"
+    ~msg:"chain is bootstrapped"
+    ~level:Notice
+    ()
+
+let synchronisation_status =
+  declare_1
+    ~section
+    ~name:"synchronisation_status"
+    ~msg:"synchronisation status: {status}"
+    ~level:Notice
+    ("status", Chain_validator_worker_state.Event.sync_status_encoding)
+
+let could_not_switch_testchain =
+  declare_1
+    ~section
+    ~name:"could_not_switch_testchain"
+    ~msg:"error while switching testchina: {trace}"
+    ~level:Error
+    ~pp1:pp_print_top_error_of_trace
+    ("trace", Error_monad.trace_encoding)
+
+let request_failure =
+  declare_3
+    ~section
+    ~name:"request_failure"
+    ~msg:"chain validator request {view} failed ({worker_status}): {errors}"
+    ~level:Notice
+    ~pp1:Request.pp
+    ~pp2:Worker_types.pp_status
+    ~pp3:pp_print_top_error_of_trace
+    ("view", Request.encoding)
+    ("worker_status", Worker_types.request_status_encoding)
+    ("errors", Error_monad.trace_encoding)
+
+let notify_head =
+  declare_1
+    ~section
+    ~name:"notify_head"
+    ~msg:"notify_head from {peer_id}"
+    ~level:Info
+    ~pp1:P2p_peer.Id.pp
+    ("peer_id", P2p_peer.Id.encoding)
+
+let notify_branch =
+  declare_1
+    ~section
+    ~name:"notify_branch"
+    ~msg:"notify branch from {peer_id}"
+    ~level:Notice
+    ~pp1:P2p_peer.Id.pp
+    ("peer_id", P2p_peer.Id.encoding)
+
+let connection =
+  declare_1
+    ~section
+    ~name:"connection"
+    ~msg:"connection of {peer_id}"
+    ~level:Info
+    ~pp1:P2p_peer.Id.pp
+    ("peer_id", P2p_peer.Id.encoding)
+
+let disconnection =
+  declare_1
+    ~section
+    ~name:"disconnection"
+    ~msg:"disconnection of {peer_id}"
+    ~level:Notice
+    ~pp1:P2p_peer.Id.pp
+    ("peer_id", P2p_peer.Id.encoding)
+
+let ignore_head =
+  declare_4
+    ~section
+    ~name:"ignore_head"
+    ~msg:
+      "Current head is better than {view} (level {level}, timestamp \
+       {timestamp}, fitness {fitness}), we do not switch"
+    ~level:Notice
+    ~pp1:Request.pp
+    ~pp2:(fun fmt -> Format.fprintf fmt "%li")
+    ~pp3:Time.Protocol.pp_hum
+    ~pp4:Fitness.pp
+    ("view", Request.encoding)
+    ("level", Data_encoding.int32)
+    ("timestamp", Time.Protocol.encoding)
+    ("fitness", Fitness.encoding)
+
+let branch_switch =
+  declare_4
+    ~section
+    ~name:"branch_switch"
+    ~msg:
+      "Update current head to {view} (level {level}, timestamp {timestamp}, \
+       fitness {fitness}), changing branch"
+    ~level:Notice
+    ~pp1:Request.pp
+    ~pp2:(fun fmt -> Format.fprintf fmt "%li")
+    ~pp3:Time.Protocol.pp_hum
+    ~pp4:Fitness.pp
+    ("view", Request.encoding)
+    ("level", Data_encoding.int32)
+    ("timestamp", Time.Protocol.encoding)
+    ("fitness", Fitness.encoding)
+
+let head_increment =
+  declare_4
+    ~section
+    ~name:"head_increment"
+    ~msg:
+      "Update current head to {view} (level {level}, timestamp {timestamp}, \
+       fitness {fitness}), same branch"
+    ~level:Notice
+    ~pp1:Request.pp
+    ~pp2:(fun fmt -> Format.fprintf fmt "%li")
+    ~pp3:Time.Protocol.pp_hum
+    ~pp4:Fitness.pp
+    ("view", Request.encoding)
+    ("level", Data_encoding.int32)
+    ("timestamp", Time.Protocol.encoding)
+    ("fitness", Fitness.encoding)
+
+let bootstrap_active_peers =
+  declare_2
+    ~section
+    ~name:"bootstrap_active_peers"
+    ~msg:"bootstrap peers: active {active} needed {needed}"
+    ~level:Debug
+    ~pp1:Format.pp_print_int
+    ~pp2:Format.pp_print_int
+    ("active", Data_encoding.int31)
+    ("needed", Data_encoding.int31)
+
+let bootstrap_active_peers_heads_time =
+  declare_2
+    ~section
+    ~name:"bootstrap_active_peers_heads_time"
+    ~msg:"bootstrap peers: active {active} needed {needed}"
+    ~level:Debug
+    ~pp1:Format.pp_print_int
+    ~pp2:Format.pp_print_int
+    ("active", Data_encoding.int31)
+    ("needed", Data_encoding.int31)
