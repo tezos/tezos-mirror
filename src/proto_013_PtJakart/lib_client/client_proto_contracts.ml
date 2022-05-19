1310f842640cadf0924c7590a890277f7485ac64
Alain Mebsout <alain.mebsout@functori.com>
2022-05-19 10:01:13 +0200

    Client/013: check if destination is valid contract before alias lookup

--- src/proto_013_PtJakart/lib_client/client_proto_contracts.ml
+++ src/proto_013_PtJakart/lib_client/client_proto_contracts.ml
@@ -93,12 +93,12 @@ module ContractAlias = struct
         return (s, Contract.implicit_contract v)
     | ["text"; text] -> ContractEntity.of_source text >|=? fun v -> (s, v)
     | _ -> (
-        find cctxt s >>= function
-        | Ok v -> return v
-        | Error k_errs -> (
-            ContractEntity.of_source s >>= function
-            | Ok v -> return (s, v)
-            | Error c_errs -> Lwt.return_error (k_errs @ c_errs)))
+        ContractEntity.of_source s >>= function
+        | Ok v -> return (s, v)
+        | Error c_errs -> (
+            find cctxt s >>= function
+            | Ok v -> return v
+            | Error k_errs -> Lwt.return_error (c_errs @ k_errs)))
 
   let destination_parameter () =
     Clic.parameter
@@ -114,7 +114,7 @@ module ContractAlias = struct
         "\n"
         [
           desc;
-          "Can be an alias, a key, or a literal (autodetected in order).\n\
+          "Can be a literal, an alias, or a key (autodetected in order).\n\
            Use 'text:literal', 'alias:name', 'key:name' to force.";
         ]
     in
@@ -126,7 +126,7 @@ module ContractAlias = struct
         "\n"
         [
           doc;
-          "Can be an alias, a key, or a literal (autodetected in order).\n\
+          "Can be a literal, an alias, or a key (autodetected in order).\n\
            Use 'text:literal', 'alias:name', 'key:name' to force.";
         ]
     in
