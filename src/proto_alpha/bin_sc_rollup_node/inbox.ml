e1e85f9be2e9c8ca2d60a0f28113ccde17aed356
Joel Bjornson <joel.bjornson@trili.tech>
2022-05-26 17:12:00 +0100

    Bin/SCORU: adjust node to apply external msgs

--- src/proto_alpha/bin_sc_rollup_node/inbox.ml
+++ src/proto_alpha/bin_sc_rollup_node/inbox.ml
@@ -116,7 +116,12 @@ let process_head Node_context.({cctxt; rollup_address; _} as node_ctxt) store
          let*! messages_tree = State.get_message_tree store predecessor in
          let*? level = Raw_level.of_int32 level in
          let* messages_tree, history, inbox =
-           Store.Inbox.add_messages history inbox level messages messages_tree
+           Store.Inbox.add_external_messages
+             history
+             inbox
+             level
+             messages
+             messages_tree
          in
          let*! () = State.set_message_tree store head_hash messages_tree in
          let*! () = State.add_inbox store head_hash inbox in
