602978b427956e657015fbb79db8b3debc11f768
Romain Bardou <romain@nomadic-labs.com>
2022-06-02 17:50:07 +0200

    Scripts: fix snapshot_alpha.sh for files with spaces

--- scripts/snapshot_alpha.sh
+++ scripts/snapshot_alpha.sh
@@ -211,10 +211,13 @@ cd lib_protocol
 sed -i.old -e 's/"hash": "[^"]*",/"hash": "'$long_hash'",/' \
     TEZOS_PROTOCOL
 
-sed -i.old -e s/protocol_alpha/protocol_${version}_${short_hash}/ \
-           -e s/protocol-alpha/protocol-${version}-${short_hash}/ \
-           -e s/protocol-functor-alpha/protocol-functor-${version}-${short_hash}/ \
-    $(find . -type f)
+# We use -exec instead of just passing the result of find to sed in order
+# to support spaces in filenames.
+find . -type f -exec \
+  sed -i.old -e s/protocol_alpha/protocol_${version}_${short_hash}/ \
+             -e s/protocol-alpha/protocol-${version}-${short_hash}/ \
+             -e s/protocol-functor-alpha/protocol-functor-${version}-${short_hash}/ \
+             {} \;
 
 # add this protocol to the immutable list
 printf \\n$long_hash >> ../../lib_protocol_compiler/final_protocol_versions
