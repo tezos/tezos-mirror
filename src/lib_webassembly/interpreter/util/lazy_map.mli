f5bd7b7f0977ad18cee7be1311b34465f9421479
Ole Krüger <ole.kruger@trili.tech>
2022-06-06 11:10:40 +0100

    SCORU: WASM: Improve lazy map

--- src/lib_webassembly/interpreter/util/lazy_map.mli
+++ src/lib_webassembly/interpreter/util/lazy_map.mli
@@ -1,4 +1,3 @@
-
 (**
   A lazy map is a fixed-size key-value association where each value is created
   dynamically.
@@ -7,7 +6,11 @@
   logarithmic time complexity and it supports non-int keys - hence "map".
 *)
 
-(** [KeyS] is the qualifier signature for key types in the lazy map. *)
+(** [KeyS] is the qualifier signature for key types in the lazy map.
+    Externally visible and accessible keys of the lazy map are always
+    non-negative. However, the lazy map implementation may internally use
+    negative keys therefore modules of type [KeyS] must support them.
+*)
 module type KeyS = sig
   include Map.OrderedType
 
@@ -16,6 +19,8 @@ module type KeyS = sig
   val add : t -> t -> t
 
   val sub : t -> t -> t
+
+  val pred : t -> t
 
   val succ : t -> t
 
@@ -44,16 +49,30 @@ module type S = sig
       [succ zero] and so on. *)
   val of_list : 'a list -> 'a t
 
-  (** [get key map] retrieves the element at [key]. *)
+  (** [get key map] retrieves the element at [key].
+
+      @raises Memory_exn.Bounds when trying to access an invalid key  *)
   val get : key -> 'a t -> 'a
 
-  (** [set key value map] sets the element at [key] to [value]. *)
+  (** [set key value map] sets the element at [key] to [value].
+
+      @raises Memory_exn.Bounds when trying to set an invalid key *)
   val set : key -> 'a -> 'a t -> 'a t
+
+  (** [cons value map] prepends a value to the front. That value can then be
+      accessed using the [zero] key.
+
+      Time complexity: O(log(instantiated_elements_in_map)) *)
+  val cons : 'a -> 'a t -> 'a t
 
   (** [grow delta ?produce_value map] creates a new lazy map that has [delta]
       more items than [map]. This also retains all values that have previously
-      existed. New values will be created with [produce_values] if provided. *)
+      existed. New values will be created with [produce_values] if provided,
+      starting with [Key.zero] for the new values. *)
   val grow : ?produce_value:(key -> 'a) -> key -> 'a t -> 'a t
+
+  (** [concat lhs rhs] Concatenates two lazy maps. *)
+  val concat : 'a t -> 'a t -> 'a t
 end
 
 (** [UnexpectedAccess] is raised in the default of the [produce_value] argument
@@ -63,6 +82,8 @@ exception UnexpectedAccess
 module Make (Key : KeyS) : S with type key = Key.t
 
 module IntMap : S with type key = int
+
+module Int32Map : S with type key = int32
 
 module Int64Map : S with type key = int64
 
@@ -92,6 +113,8 @@ module Mutable : sig
   module Make (Key : KeyS) : S with type key = Key.t
 
   module IntMap : S with type key = int
+
+  module Int32Map : S with type key = int32
 
   module Int64Map : S with type key = int64
 end
