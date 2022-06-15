2865dcb79644ec21b8a1d86b004e90c3bfd74157
Zaynah Dargaye <zaynah.dargaye@nomadic-labs.com>
2022-06-15 15:18:30 +0200

    Proto/test: add a precheck operation in incremental's helpers
    Co-authored-by: Albin Coquereau <albin.coquereau@nomadic-labs.com>
    Co-authored-by: Zaynah Dargaye <zaynah.dargaye@nomadic-labs.com>

--- src/proto_alpha/lib_protocol/test/helpers/incremental.mli
+++ src/proto_alpha/lib_protocol/test/helpers/incremental.mli
@@ -40,6 +40,15 @@ val validation_state : incremental -> validation_state
 
 val level : incremental -> int32
 
+(** [begin_construction ?mempool_mode predecessor] uses
+    [Main.begin_construction] to create a validation state on top of
+    [predecessor].
+
+    Optional arguments allow to override defaults:
+
+    {ul {li [?mempool_mode:bool]: set the validation state to
+    [partial_construction], [construction] otherwise (default).}}
+*)
 val begin_construction :
   ?timestamp:Time.Protocol.t ->
   ?seed_nonce_hash:Nonce_hash.t ->
@@ -48,6 +57,51 @@ val begin_construction :
   Block.t ->
   incremental tzresult Lwt.t
 
+(** [precheck_operation ?expect_failure ?check_size i op] tries to
+    precheck [op] in the validation state of [i]. If the precheck
+    succeeds, the function returns the incremental value with a
+    validation state updated after the precheck. Otherwise raise the
+    error from the prechecking of [op].
+
+    Optional arguments allow to override defaults:
+
+    {ul {li [?expect_failure:(error list -> unit tzresult Lwt.t)]:
+    precheck of [op] is expected to fail and [expect_failure] should
+    handle the error. In case precheck does not fail and an
+    [expect_failure] is provided, [precheck_operation] fails.}
+
+    {li [?check_size:bool]: enable the check that an operation size
+    should not exceed [Constants_repr.max_operation_data_length].
+    Enabled (set to [true]) by default. }} *)
+val precheck_operation :
+  ?expect_failure:(error list -> unit tzresult Lwt.t) ->
+  ?check_size:bool ->
+  incremental ->
+  Operation.packed ->
+  incremental tzresult Lwt.t
+
+(** [add_operation ?expect_failure ?expect_apply_failure ?check_size i
+    op] tries to apply [op] in the validation state of [i]. If the
+    precheck of [op] succeeds, the function returns the incremental
+    value with a validation state updated after the application of
+    [op]. Otherwise raise the error from the prechecking of [op].
+
+    Optional arguments allow to override defaults:
+
+    {ul {li [?expect_failure:(error list -> unit tzresult Lwt.t)]:
+    precheck of [op] is expected to fail and [expect_failure] should
+    handle the error. In case precheck does not fail and
+    [expect_failure] is provided, [precheck_operation] fails.}
+
+    {ul {li [?expect_apply_failure:(error list -> unit tzresult
+    Lwt.t)]: application of [op] is expected to fail and
+    [expect_apply_failure] should handle the error. In case the
+    application of [op] does not fail and [expect_apply_failure] is
+    provided, [add_operation] fails.}
+
+    {li [?check_size:bool]: enable the check that an operation size
+    should not exceed [Constants_repr.max_operation_data_length].
+    Enabled (set to [true]) by default. }} *)
 val add_operation :
   ?expect_failure:(error list -> unit tzresult Lwt.t) ->
   ?expect_apply_failure:(error list -> unit tzresult Lwt.t) ->
@@ -56,6 +110,10 @@ val add_operation :
   Operation.packed ->
   incremental tzresult Lwt.t
 
+(** [finalize_block i] creates a [Block.t] based on the
+    validation_state and the operations contained in [i]. The function
+    calls [Main.finalize_block] to compute a new context.
+*)
 val finalize_block : incremental -> Block.t tzresult Lwt.t
 
 val rpc_ctxt : incremental Environment.RPC_context.simple
