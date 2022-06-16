0e56624d715960423a887ba59d810b7ecb7ddf7c
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2022-06-16 19:46:04 +0000

    Proto,SCORU: Improve code quality

--- src/proto_alpha/lib_protocol/test/unit/test_sc_rollup_management_protocol.ml
+++ src/proto_alpha/lib_protocol/test/unit/test_sc_rollup_management_protocol.ml
@@ -48,7 +48,6 @@ let check_encode_decode_inbox_message message =
   let*? bytes' =
     Environment.wrap_tzresult @@ Sc_rollup.Inbox.Message.to_bytes message'
   in
-  let bytes' = (bytes' :> string) in
   Assert.equal_string ~loc:__LOC__ (bytes :> string) (bytes' :> string)
 
 let check_encode_decode_outbox_message ctxt message =
