41c511e39ce5c88160361ecea46af0dfa1f58672
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2022-06-28 10:22:00 +0200

    Proto: add Event operation kind

--- src/proto_alpha/lib_protocol/operation_repr.mli
+++ src/proto_alpha/lib_protocol/operation_repr.mli
@@ -99,6 +99,8 @@ module Kind : sig
 
   type delegation = Delegation_kind
 
+  type event = Event_kind
+
   type set_deposits_limit = Set_deposits_limit_kind
 
   type increase_paid_storage = Increase_paid_storage_kind
