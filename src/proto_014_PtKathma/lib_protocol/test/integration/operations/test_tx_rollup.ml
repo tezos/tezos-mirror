6b3d93ab7d05958a685af99ac18ef1e40cebd52f
Fedor Sheremetyev <sheremetyev@gmail.com>
2022-06-29 00:03:18 +0000

    Test: fix OCaml formatting

--- src/proto_014_PtKathma/lib_protocol/test/integration/operations/test_tx_rollup.ml
+++ src/proto_014_PtKathma/lib_protocol/test/integration/operations/test_tx_rollup.ml
@@ -1967,7 +1967,8 @@ let test_full_inbox () =
       baking_reward_fixed_portion = Tez.zero;
       tx_rollup =
         {
-          Tezos_protocol_014_PtKathma_parameters.Default_parameters.constants_test
+          Tezos_protocol_014_PtKathma_parameters.Default_parameters
+          .constants_test
             .tx_rollup
           with
           enable = true;
