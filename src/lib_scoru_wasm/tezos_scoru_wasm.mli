699497d96728a5d393f252dcefae46dab860dcb1
Joel Bjornson <joel.bjornson@trili.tech>
2022-06-30 10:46:35 +0100

    Lib_scoru_wasm: refactor scoru-wasm module

--- src/lib_scoru_wasm/tezos_scoru_wasm.mli
+++ src/lib_scoru_wasm/tezos_scoru_wasm.mli
@@ -23,64 +23,67 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-open Sigs
+(** This module exposes a module type {!S} defining a WASM VM API.
+    It also exposes a functor {!Make} for constructing a concrete implementation
+    of this module type, given an implementation of a {!Sigs.TreeS} module.
 
+    This library acts as a dependency to the protocol environment. Everything
+    WASM VM related that must be exposed to the protocol via the environment
+    shall be added here. *)
+
+(** Represents the location of an input message. *)
 type input = {
   inbox_level : Tezos_base.Bounded.Int32.NonNegative.t;
-  message_counter : Z.t;
+      (** The inbox level at which the message exists.*)
+  message_counter : Z.t;  (** The index of the message in the inbox. *)
 }
 
+(** Represents the location of an output message. *)
 type output = {
   outbox_level : Tezos_base.Bounded.Int32.NonNegative.t;
-  message_index : Z.t;
+      (** The outbox level at which the message exists.*)
+  message_index : Z.t;  (** The index of the message in the outbox. *)
 }
 
-type input_request = No_input_required | Input_required
+(** Represents the state of input requests. *)
+type input_request =
+  | No_input_required  (** The VM does not expect any input. *)
+  | Input_required  (** The VM needs input in order to progress. *)
 
+(** Represents the state of the VM. *)
 type info = {
   current_tick : Z.t;
+      (** The number of ticks processed by the VM, zero for the initial state.
+      [current_tick] must be incremented for each call to [step] *)
   last_input_read : input option;
-  input_request : input_request;
+      (** The last message to be read by the VM, if any. *)
+  input_request : input_request;  (** The current VM input request. *)
 }
 
+(** This module type defines a WASM VM API used for smart-contract rollups. *)
 module type S = sig
   type tree
 
+  (** [compute_step] forwards the VM by one compute tick. If the VM is expecting
+      input, it gets stuck. If the VM is already stuck, this function may
+      raise an exception. *)
   val compute_step : tree -> tree Lwt.t
 
+  (** [set_input_step] forwards the VM by one input tick. If the VM is not
+      expecting input, it gets stuck. If the VM is already stuck, this function
+      may raise an exception. *)
   val set_input_step : input -> string -> tree -> tree Lwt.t
 
+  (** [get_output output state] returns the payload associated with the given
+      output. The result is meant to be deserialized using
+      [Sc_rollup_PVM_sem.output_encoding]. If the output is missing, this
+      function may raise an exception. *)
   val get_output : output -> tree -> string Lwt.t
 
+  (** [get_info] provides a typed view of the current machine state. Should not
+      raise. *)
   val get_info : tree -> info Lwt.t
 end
 
-module Make (T : TreeS) : S with type tree = T.tree = struct
-  type tree = T.tree
-
-  module Decodings = Decodings.Make (T)
-
-  let compute_step = Lwt.return
-
-  let set_input_step _ _ = Lwt.return
-  (* TODO: https://gitlab.com/tezos/tezos/-/issues/3092
-
-     Implement handling of input logic.
-  *)
-
-  let get_output _ _ = Lwt.return ""
-
-  let get_info _ =
-    Lwt.return
-      {
-        current_tick = Z.of_int 0;
-        last_input_read = None;
-        input_request = No_input_required;
-      }
-
-  let _module_instance_of_tree modules =
-    Decodings.Tree.Decoding.run (Decodings.module_instance_decoding modules)
-
-  let _module_instances_of_tree =
-    Decodings.Tree.Decoding.run Decodings.module_instances_decoding
-end
+(** Builds a WASM VM given a concrete implementation of {!Sigs.TreeS}. *)
+module Make (T : Sigs.TreeS) : S with type tree = T.tree
