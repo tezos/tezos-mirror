a4d03d922fb99aab79f006ce696cfb8c0a63b032
Victor Dumitrescu <v.dumitrescu@outlook.com>
2022-06-30 15:32:17 +0200

    opam: bump opam-repository

--- scripts/version.sh
+++ scripts/version.sh
@@ -20,12 +20,12 @@ export recommended_node_version=14.12.0
 
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-export full_opam_repository_tag=439a18a8f373460719aa15ae90cf05933cf81b7b
+export full_opam_repository_tag=1e2c428e84d3ab514163688b396de0fea01e224a
 
 ## opam_repository is an additional, tezos-specific opam repository.
-## This value MUST be the same as `build_deps_image_version` in `.gitlab/ci/templates.yml
+## This value MUST be the same as `build_deps_image_version` in `.gitlab/ci/templates.ym
 export opam_repository_url=https://gitlab.com/tezos/opam-repository
-export opam_repository_tag=db6392ae301aa0bb40b78716e9641e39c8e8b6df
+export opam_repository_tag=d2407c160dabe730ed66ed50a55cf141d4bec7fb
 export opam_repository_git=$opam_repository_url.git
 export opam_repository=$opam_repository_git\#$opam_repository_tag
 
