OKEAMAH/Git

SCORU: WASM: Enhance tree decoding module

src/lib_scoru_wasm/tree_decoding.ml
@@ -23,6 +23,8 @@ module type S = sig
 
   val return : 'a -> 'a t
 
+  val lift : 'a Lwt.t -> 'a t
+
   val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
 
   val ( and+ ) : 'a t -> 'b t -> ('a * 'b) t
@@ -52,6 +54,8 @@ module Make (T : TreeS) : S with type tree = T.tree = struct
   type 'a t = Tree.tree -> prefix_key -> 'a Lwt.t
 
   let return value _tree _prefix = Lwt.return value
+
+  let lift lwt _tree _prefix = lwt
 
   let ( let+ ) dec f tree prefix = Lwt.map f (dec tree prefix)
 

src/lib_scoru_wasm/tree_decoding.mli
@@ -51,6 +51,8 @@ module type S = sig
 
   val return : 'a -> 'a t
 
+  val lift : 'a Lwt.t -> 'a t
+
   val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
 
   val ( and+ ) : 'a t -> 'b t -> ('a * 'b) t

tezos/tezos-mirror

src/lib_scoru_wasm/tree_decoding.ml
@@ -23,6 +23,8 @@ module type S = sig
 
   val return : 'a -> 'a t
 
+  val of_lwt : 'a Lwt.t -> 'a t
+
   val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
 
   val ( and+ ) : 'a t -> 'b t -> ('a * 'b) t
@@ -52,6 +54,8 @@ module Make (T : TreeS) : S with type tree = T.tree = struct
   type 'a t = Tree.tree -> prefix_key -> 'a Lwt.t
 
   let return value _tree _prefix = Lwt.return value
+
+  let of_lwt lwt _tree _prefix = lwt
 
   let ( let+ ) dec f tree prefix = Lwt.map f (dec tree prefix)
 

src/lib_scoru_wasm/tree_decoding.mli
@@ -51,6 +51,8 @@ module type S = sig
 
   val return : 'a -> 'a t
 
+  val of_lwt : 'a Lwt.t -> 'a t
+
   val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
 
   val ( and+ ) : 'a t -> 'b t -> ('a * 'b) t