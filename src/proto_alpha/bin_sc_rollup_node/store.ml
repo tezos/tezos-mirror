fd7656bcfd4ec12b5354267b1f54067f81eaafe1
Yann Regis-Gianas <yann@nomadic-labs.com>
2022-07-06 13:44:29 +0200

    SCORU,Node: Use Hash.of_context_hash
    Signed-off-by: Yann Regis-Gianas <yann@nomadic-labs.com>

--- src/proto_alpha/bin_sc_rollup_node/store.ml
+++ src/proto_alpha/bin_sc_rollup_node/store.ml
@@ -232,8 +232,7 @@ module Inbox = struct
       | Error _ -> assert false
 
     let to_inbox_hash kinded_hash =
-      match kinded_hash with
-      | `Value h | `Node h -> Hash.of_bytes_exn (Context_hash.to_bytes h)
+      match kinded_hash with `Value h | `Node h -> Hash.of_context_hash h
 
     let from_inbox_hash inbox_hash =
       let ctxt_hash = Hash.to_context_hash inbox_hash in
