OKEAMAH/Git

lib_store, lib_shell: use abstract context and Context_ops.index

src/lib_shell/node.ml
@@ -127,7 +127,10 @@ type config = {
   store_root : string;
   context_root : string;
   protocol_root : string;
-  patch_context : (Context.t -> Context.t tzresult Lwt.t) option;
+  patch_context :
+    (Environment_context.Context.t ->
+    Environment_context.Context.t tzresult Lwt.t)
+    option;
   p2p : (P2p.config * P2p.limits) option;
   target : (Block_hash.t * int32) option;
   disable_mempool : bool;

src/lib_shell/node.mli
@@ -37,7 +37,10 @@ type config = {
   store_root : string;
   context_root : string;
   protocol_root : string;
-  patch_context : (Context.t -> Context.t tzresult Lwt.t) option;
+  patch_context :
+    (Environment_context.Context.t ->
+    Environment_context.Context.t tzresult Lwt.t)
+    option;
   p2p : (P2p.config * P2p.limits) option;
   target : (Block_hash.t * int32) option;
   disable_mempool : bool;

src/lib_shell/prevalidation.mli
@@ -162,7 +162,10 @@ module Internal_for_tests : sig
     type chain_store
 
     (** [context store block] checkouts and returns the context of [block] *)
-    val context : chain_store -> Store.Block.t -> Context.t tzresult Lwt.t
+    val context :
+      chain_store ->
+      Store.Block.t ->
+      Environment_context.Context.t tzresult Lwt.t
 
     (** [chain_id store] returns the {!Chain_id.t} to which [store]
         corresponds *)

tezos/tezos-mirror

src/lib_shell/node.ml
@@ -127,7 +127,10 @@ type config = {
   store_root : string;
   context_root : string;
   protocol_root : string;
-  patch_context : (Context.t -> Context.t tzresult Lwt.t) option;
+  patch_context :
+    (Tezos_protocol_environment.Context.t ->
+    Tezos_protocol_environment.Context.t tzresult Lwt.t)
+    option;
   p2p : (P2p.config * P2p.limits) option;
   target : (Block_hash.t * int32) option;
   disable_mempool : bool;

src/lib_shell/node.mli
@@ -37,7 +37,10 @@ type config = {
   store_root : string;
   context_root : string;
   protocol_root : string;
-  patch_context : (Context.t -> Context.t tzresult Lwt.t) option;
+  patch_context :
+    (Tezos_protocol_environment.Context.t ->
+    Tezos_protocol_environment.Context.t tzresult Lwt.t)
+    option;
   p2p : (P2p.config * P2p.limits) option;
   target : (Block_hash.t * int32) option;
   disable_mempool : bool;

src/lib_shell/prevalidation.mli
@@ -162,7 +162,10 @@ module Internal_for_tests : sig
     type chain_store
 
     (** [context store block] checkouts and returns the context of [block] *)
-    val context : chain_store -> Store.Block.t -> Context.t tzresult Lwt.t
+    val context :
+      chain_store ->
+      Store.Block.t ->
+      Tezos_protocol_environment.Context.t tzresult Lwt.t
 
     (** [chain_id store] returns the {!Chain_id.t} to which [store]
         corresponds *)