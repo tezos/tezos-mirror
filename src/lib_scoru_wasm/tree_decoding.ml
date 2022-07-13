OKEAMAH/Git

Lib_scoru_wasm: tagged union for tree decoder

src/lib_scoru_wasm/tree_decoding.ml
@@ -27,12 +27,16 @@ type key = string list
 
 exception Key_not_found of key
 
+exception No_tag_matched
+
 exception Decode_error of {key : key; error : Data_encoding.Binary.read_error}
 
 module type S = sig
   type tree
 
   type 'a t
+
+  type ('tag, 'a) case
 
   val run : 'a t -> tree -> 'a Lwt.t
 
@@ -45,6 +49,14 @@ module type S = sig
   val lazy_mapping : ('i -> key) -> 'a t -> ('i -> 'a Lwt.t) t
 
   val of_lwt : 'a Lwt.t -> 'a t
+
+  val map : ('a -> 'b) -> 'a t -> 'b t
+
+  val map_lwt : ('a -> 'b Lwt.t) -> 'a t -> 'b t
+
+  val case : 'tag -> 'b t -> ('b -> 'a) -> ('tag, 'a) case
+
+  val tagged_union : 'tag t -> ('tag, 'a) case list -> 'a t
 
   module Syntax : sig
     val return : 'a -> 'a t
@@ -82,7 +94,14 @@ module Make (T : Tree.S) : S with type tree = T.tree = struct
 
   type 'a t = Tree.tree -> prefix_key -> 'a Lwt.t
 
+  type ('tag, 'a) case =
+    | Case : {tag : 'tag; extract : 'b -> 'a; decode : 'b t} -> ('tag, 'a) case
+
   let of_lwt lwt _tree _prefix = lwt
+
+  let map f dec tree prefix = Lwt.map f (dec tree prefix)
+
+  let map_lwt f dec tree prefix = Lwt.bind (dec tree prefix) f
 
   module Syntax = struct
     let return value _tree _prefix = Lwt.return value
@@ -92,7 +111,7 @@ module Make (T : Tree.S) : S with type tree = T.tree = struct
 
     let both lhs rhs tree prefix = Lwt.both (lhs tree prefix) (rhs tree prefix)
 
-    let ( let+ ) dec f tree prefix = Lwt.map f (dec tree prefix)
+    let ( let+ ) m f = map f m
 
     let ( and+ ) = both
 
@@ -127,4 +146,17 @@ module Make (T : Tree.S) : S with type tree = T.tree = struct
       tree (to_key index) field_enc input_tree input_prefix
     in
     Lwt.return produce_value
+
+  let case tag decode extract = Case {tag; decode; extract}
+
+  let tagged_union decode_tag cases input_tree prefix =
+    let open Lwt_syntax in
+    let* target_tag = tree ["tag"] decode_tag input_tree prefix in
+    (* Search through the cases to find a matching branch. *)
+    cases
+    |> List.find_map (fun (Case {tag; decode; extract}) ->
+           if tag = target_tag then
+             Some (map extract (tree ["value"] decode) input_tree prefix)
+           else None)
+    |> Option.value_f ~default:(fun _ -> raise No_tag_matched)
 end

src/lib_scoru_wasm/tree_decoding.mli
@@ -28,6 +28,10 @@ type key = string list
 (** Raised when a requested key is not present. *)
 exception Key_not_found of key
 
+(** Raised when an encoder produced by [tagged_union] does not contain a
+    matching branch. *)
+exception No_tag_matched
+
 (** Raised when data-encoding fails to decode a certain value. *)
 exception Decode_error of {key : key; error : Data_encoding.Binary.read_error}
 
@@ -37,7 +41,11 @@ module type S = sig
   (** Tree decoder type *)
   type 'a t
 
-  (** [run decoder tree] runs the tree decoder against the tree. *)
+  (** Represents a partial encoder for a specific constructor of a sum-type. *)
+  type ('tag, 'a) case
+
+  (** [run decoder tree] runs the tree decoder against the tree. May raise a
+      [Key_not_found] or a [No_tag_matched] exception. *)
   val run : 'a t -> tree -> 'a Lwt.t
 
   (** [raw key] retrieves the raw value at the given [key].
@@ -72,6 +80,29 @@ module type S = sig
 
   (** [of_lwt p] lifts the promise [p] into a decoding value. *)
   val of_lwt : 'a Lwt.t -> 'a t
+
+  (** [map f d] maps over the result of the decoder [d] with function [f]. *)
+  val map : ('a -> 'b) -> 'a t -> 'b t
+
+  (** [map_lwt f d] maps over the result of the decoder [d] with the effectful
+      function [f]. *)
+  val map_lwt : ('a -> 'b Lwt.t) -> 'a t -> 'b t
+
+  (** [case tag dec f] return a a partial decoder that represents a case in a
+      variant type. The decoder hides the (existentially bound) type of the
+      parameter to the specific case, provided a converter function [f] and
+      base decoder [dec]. *)
+  val case : 'tag -> 'b t -> ('b -> 'a) -> ('tag, 'a) case
+
+  (** [tagged_union tag_dec cases] returns a decoder that use [tag_dec] for
+      decoding the value of a field [tag]. The decoder searches through the list
+      of cases for a matching branch. When a matching branch is found, it uses
+      its embedded decoder for the value. This function is used for constructing
+      decoders for sum-types.
+
+      If an insufficient list of cases are provided, the resulting encoder may
+      fail with a [No_tag_matched] error when [run].  *)
+  val tagged_union : 'tag t -> ('tag, 'a) case list -> 'a t
 
   (** Syntax module for the {!Tree_decoding}. This is intended to be opened
       locally in functions. Within the scope of this module, the code can

tezos/tezos-mirror

src/lib_scoru_wasm/tree_decoding.ml
@@ -27,12 +27,16 @@ type key = string list
 
 exception Key_not_found of key
 
+exception No_tag_matched_on_decoding
+
 exception Decode_error of {key : key; error : Data_encoding.Binary.read_error}
 
 module type S = sig
   type tree
 
   type 'a t
+
+  type ('tag, 'a) case
 
   val run : 'a t -> tree -> 'a Lwt.t
 
@@ -45,6 +49,14 @@ module type S = sig
   val lazy_mapping : ('i -> key) -> 'a t -> ('i -> 'a Lwt.t) t
 
   val of_lwt : 'a Lwt.t -> 'a t
+
+  val map : ('a -> 'b) -> 'a t -> 'b t
+
+  val map_lwt : ('a -> 'b Lwt.t) -> 'a t -> 'b t
+
+  val case : 'tag -> 'b t -> ('b -> 'a) -> ('tag, 'a) case
+
+  val tagged_union : 'tag t -> ('tag, 'a) case list -> 'a t
 
   module Syntax : sig
     val return : 'a -> 'a t
@@ -82,7 +94,14 @@ module Make (T : Tree.S) : S with type tree = T.tree = struct
 
   type 'a t = Tree.tree -> prefix_key -> 'a Lwt.t
 
+  type ('tag, 'a) case =
+    | Case : {tag : 'tag; extract : 'b -> 'a; decode : 'b t} -> ('tag, 'a) case
+
   let of_lwt lwt _tree _prefix = lwt
+
+  let map f dec tree prefix = Lwt.map f (dec tree prefix)
+
+  let map_lwt f dec tree prefix = Lwt.bind (dec tree prefix) f
 
   module Syntax = struct
     let return value _tree _prefix = Lwt.return value
@@ -92,7 +111,7 @@ module Make (T : Tree.S) : S with type tree = T.tree = struct
 
     let both lhs rhs tree prefix = Lwt.both (lhs tree prefix) (rhs tree prefix)
 
-    let ( let+ ) dec f tree prefix = Lwt.map f (dec tree prefix)
+    let ( let+ ) m f = map f m
 
     let ( and+ ) = both
 
@@ -127,4 +146,17 @@ module Make (T : Tree.S) : S with type tree = T.tree = struct
       tree (to_key index) field_enc input_tree input_prefix
     in
     Lwt.return produce_value
+
+  let case tag decode extract = Case {tag; decode; extract}
+
+  let tagged_union decode_tag cases input_tree prefix =
+    let open Lwt_syntax in
+    let* target_tag = tree ["tag"] decode_tag input_tree prefix in
+    (* Search through the cases to find a matching branch. *)
+    cases
+    |> List.find_map (fun (Case {tag; decode; extract}) ->
+           if tag = target_tag then
+             Some (map extract (tree ["value"] decode) input_tree prefix)
+           else None)
+    |> Option.value_f ~default:(fun _ -> raise No_tag_matched_on_decoding)
 end

src/lib_scoru_wasm/tree_decoding.mli
@@ -28,6 +28,10 @@ type key = string list
 (** Raised when a requested key is not present. *)
 exception Key_not_found of key
 
+(** Raised when an encoder produced by [tagged_union] does not contain a
+    matching branch. *)
+exception No_tag_matched_on_decoding
+
 (** Raised when data-encoding fails to decode a certain value. *)
 exception Decode_error of {key : key; error : Data_encoding.Binary.read_error}
 
@@ -37,7 +41,11 @@ module type S = sig
   (** Tree decoder type *)
   type 'a t
 
-  (** [run decoder tree] runs the tree decoder against the tree. *)
+  (** Represents a partial encoder for a specific constructor of a sum-type. *)
+  type ('tag, 'a) case
+
+  (** [run decoder tree] runs the tree decoder against the tree. May raise a
+      [Key_not_found] or a [No_tag_matched] exception. *)
   val run : 'a t -> tree -> 'a Lwt.t
 
   (** [raw key] retrieves the raw value at the given [key].
@@ -72,6 +80,29 @@ module type S = sig
 
   (** [of_lwt p] lifts the promise [p] into a decoding value. *)
   val of_lwt : 'a Lwt.t -> 'a t
+
+  (** [map f d] maps over the result of the decoder [d] with function [f]. *)
+  val map : ('a -> 'b) -> 'a t -> 'b t
+
+  (** [map_lwt f d] maps over the result of the decoder [d] with the effectful
+      function [f]. *)
+  val map_lwt : ('a -> 'b Lwt.t) -> 'a t -> 'b t
+
+  (** [case tag dec f] returns a partial decoder that represents a case in a
+      variant type. The decoder hides the (existentially bound) type of the
+      parameter to the specific case, provided a converter function [f] and
+      base decoder [dec]. *)
+  val case : 'tag -> 'b t -> ('b -> 'a) -> ('tag, 'a) case
+
+  (** [tagged_union tag_dec cases] returns a decoder that use [tag_dec] for
+      decoding the value of a field [tag]. The decoder searches through the list
+      of cases for a matching branch. When a matching branch is found, it uses
+      its embedded decoder for the value. This function is used for constructing
+      decoders for sum-types.
+
+      If an insufficient list of cases are provided, the resulting encoder may
+      fail with a [No_tag_matched] error when [run].  *)
+  val tagged_union : 'tag t -> ('tag, 'a) case list -> 'a t
 
   (** Syntax module for the {!Tree_decoding}. This is intended to be opened
       locally in functions. Within the scope of this module, the code can