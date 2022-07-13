OKEAMAH/Git

Lib_scoru_wasm: tree encoding module

src/lib_scoru_wasm/tree_encoding.ml
@@ -1,0 +1,139 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 TriliTech <contact@trili.tech>                         *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type key = string list
+
+exception No_tag_matched
+
+module type S = sig
+  type tree
+
+  type -'a t
+
+  val contramap : ('a -> 'b) -> 'b t -> 'a t
+
+  val contramap_lwt : ('a -> 'b Lwt.t) -> 'b t -> 'a t
+
+  val run : 'a t -> 'a -> tree -> tree Lwt.t
+
+  val raw : key -> bytes t
+
+  val value : key -> 'a Data_encoding.t -> 'a t
+
+  val tree : key -> 'a t -> 'a t
+
+  val lazy_mapping : ('k -> key) -> 'v t -> ('k * 'v) list t
+
+  type ('tag, 'a) case
+
+  val case : 'tag -> 'b t -> ('a -> 'b option) -> ('tag, 'a) case
+
+  val tagged_union : 'tag t -> ('tag, 'a) case list -> 'a t
+
+  val lwt : 'a t -> 'a Lwt.t t
+
+  val tup2 : 'a t -> 'b t -> ('a * 'b) t
+
+  val tup3 : 'a t -> 'b t -> 'c t -> ('a * 'b * 'c) t
+end
+
+module Make (T : Tree.S) = struct
+  (** [append_key prefix key] append [key] to [prefix] in order to create a new
+      [prefix_key]. *)
+  let append_key prefix key tail = prefix (List.append key tail)
+
+  type tree = T.tree
+
+  (** Given the tail key, construct a full key. *)
+  type prefix_key = key -> key
+
+  type -'a t = 'a -> prefix_key -> tree -> tree Lwt.t
+
+  let run enc value tree = enc value Fun.id tree
+
+  let lwt enc value prefix tree =
+    let open Lwt_syntax in
+    let* v = value in
+    enc v prefix tree
+
+  let contramap f enc value = enc (f value)
+
+  let contramap_lwt f enc value prefix tree =
+    let open Lwt_syntax in
+    let* v = f value in
+    enc v prefix tree
+
+  let tup2 lhs rhs (l, r) prefix tree =
+    let open Lwt.Syntax in
+    let* tree = lhs l prefix tree in
+    rhs r prefix tree
+
+  let tup3 encode_a encode_b encode_c (a, b, c) prefix tree =
+    let open Lwt.Syntax in
+    let* tree = encode_a a prefix tree in
+    let* tree = encode_b b prefix tree in
+    encode_c c prefix tree
+
+  let raw suffix bytes prefix tree = T.add tree (prefix suffix) bytes
+
+  let value suffix enc =
+    contramap (Data_encoding.Binary.to_bytes_exn enc) (raw suffix)
+
+  let tree key enc value prefix tree = enc value (append_key prefix key) tree
+
+  let lazy_mapping to_key enc_value bindings prefix tree =
+    List.fold_left_s
+      (fun tree (k, v) ->
+        let key = append_key prefix (to_key k) in
+        enc_value v key tree)
+      tree
+      bindings
+
+  type ('tag, 'a) case =
+    | Case : {
+        tag : 'tag;
+        probe : 'a -> 'b option;
+        encode : 'b t;
+      }
+        -> ('tag, 'a) case
+
+  let case tag encode probe = Case {tag; encode; probe}
+
+  let tagged_union encode_tag cases value prefix target_tree =
+    let open Lwt_syntax in
+    let encode_tag = tree ["tag"] encode_tag in
+    let match_case (Case {probe; tag; encode}) =
+      match probe value with
+      | Some value ->
+          let* target_tree = encode_tag tag prefix target_tree in
+          let* x = tree ["value"] encode value prefix target_tree in
+          return (Some x)
+      | None -> return None
+    in
+    let* tree_opt = List.find_map_s match_case cases in
+    match tree_opt with
+    | None -> raise No_tag_matched
+    | Some tree -> return tree
+end

src/lib_scoru_wasm/tree_encoding.mli
@@ -1,0 +1,98 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 TriliTech <contact@trili.tech>                         *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type key = string list
+
+(** Raised when an encoder produced by [tagged_union] does not contain a
+    matching branch. *)
+exception No_tag_matched
+
+module type S = sig
+  type tree
+
+  (** Tree encoder type. *)
+  type -'a t
+
+  (** Represents a partial encoder for specific constructor of a sum-type. *)
+  type ('tag, 'a) case
+
+  (** [contramap f e] is contravariant map operation that creates a new decoder
+      that maps its input using [f] before feeding it to [e]. *)
+  val contramap : ('a -> 'b) -> 'b t -> 'a t
+
+  (** [contramap_lwt f e]. Same as [contramap] except that [f] als produces
+      an Lwt effect. *)
+  val contramap_lwt : ('a -> 'b Lwt.t) -> 'b t -> 'a t
+
+  (** [run enc x tree] encodes the given value [x] using the encoder [enc] and
+      writes it to the tree [tree]. May raise a [Key_not_found] or a
+      [No_tag_matched] exception. *)
+  val run : 'a t -> 'a -> tree -> tree Lwt.t
+
+  (** [raw key] returns an encoder that encodes raw bytes at the given key. *)
+  val raw : key -> bytes t
+
+  (** [value key enc] encodes the value at a given [key] using the provided
+      [enc] encoder for the value. *)
+  val value : key -> 'a Data_encoding.t -> 'a t
+
+  (** [tree key enc] applies a tree encoder for a provided [key]. *)
+  val tree : key -> 'a t -> 'a t
+
+  (** [lazy_mapping to_key enc] returns a key-value list encoder that
+      encodes values from a given key-value list using the key-mapping function
+      [to_key] and the provided encoder [enc] for the values. *)
+  val lazy_mapping : ('k -> key) -> 'v t -> ('k * 'v) list t
+
+  (** [case tag enc f] return a partial encoder that represents a case in a
+      sum-type. The encoder hides the (existentially bound) type of the
+      parameter to the specific case, provided a converter function [f] and
+      base encoder [enc]. *)
+  val case : 'tag -> 'b t -> ('a -> 'b option) -> ('tag, 'a) case
+
+  (** [tagged_union tag_enc cases] returns an encoder that uses [tag_enc] for
+      encoding the value of a field [tag]. The encoder searches through the list
+      of cases for a matching branch. When a matching branch is found, it
+      applies its embedded encoder for the value. This function is used for
+      constructing encoders for sum-types.
+
+      If an insufficient list of cases are provided, the resulting encoder may
+      fail with a [No_tag_matched] error when [run].  *)
+  val tagged_union : 'tag t -> ('tag, 'a) case list -> 'a t
+
+  (** [lwt enc] promotes the given encoder [enc] to one that can handle lwt
+      values. *)
+  val lwt : 'a t -> 'a Lwt.t t
+
+  (** [tup2 e1 e2] creates an encoder that encodes a tuple of elements using
+      [e1] and [e2]. *)
+  val tup2 : 'a t -> 'b t -> ('a * 'b) t
+
+  (** [tup3 e1 e2] creates an encoder that encodes a triple of elements using
+      [e1], [e2], and [e3]. *)
+  val tup3 : 'a t -> 'b t -> 'c t -> ('a * 'b * 'c) t
+end
+
+module Make : functor (T : Tree.S) -> S with type tree = T.tree

tezos/tezos-mirror

src/lib_scoru_wasm/tree_encoding.ml
@@ -1,0 +1,139 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 TriliTech <contact@trili.tech>                         *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type key = string list
+
+exception No_tag_matched_on_encoding
+
+module type S = sig
+  type tree
+
+  type -'a t
+
+  val contramap : ('a -> 'b) -> 'b t -> 'a t
+
+  val contramap_lwt : ('a -> 'b Lwt.t) -> 'b t -> 'a t
+
+  val run : 'a t -> 'a -> tree -> tree Lwt.t
+
+  val raw : key -> bytes t
+
+  val value : key -> 'a Data_encoding.t -> 'a t
+
+  val tree : key -> 'a t -> 'a t
+
+  val lazy_mapping : ('k -> key) -> 'v t -> ('k * 'v) list t
+
+  type ('tag, 'a) case
+
+  val case : 'tag -> 'b t -> ('a -> 'b option) -> ('tag, 'a) case
+
+  val tagged_union : 'tag t -> ('tag, 'a) case list -> 'a t
+
+  val lwt : 'a t -> 'a Lwt.t t
+
+  val tup2 : 'a t -> 'b t -> ('a * 'b) t
+
+  val tup3 : 'a t -> 'b t -> 'c t -> ('a * 'b * 'c) t
+end
+
+module Make (T : Tree.S) = struct
+  (** [append_key prefix key] append [key] to [prefix] in order to create a new
+      [prefix_key]. *)
+  let append_key prefix key tail = prefix (List.append key tail)
+
+  type tree = T.tree
+
+  (** Given the tail key, construct a full key. *)
+  type prefix_key = key -> key
+
+  type -'a t = 'a -> prefix_key -> tree -> tree Lwt.t
+
+  let run enc value tree = enc value Fun.id tree
+
+  let lwt enc value prefix tree =
+    let open Lwt_syntax in
+    let* v = value in
+    enc v prefix tree
+
+  let contramap f enc value = enc (f value)
+
+  let contramap_lwt f enc value prefix tree =
+    let open Lwt_syntax in
+    let* v = f value in
+    enc v prefix tree
+
+  let tup2 lhs rhs (l, r) prefix tree =
+    let open Lwt.Syntax in
+    let* tree = lhs l prefix tree in
+    rhs r prefix tree
+
+  let tup3 encode_a encode_b encode_c (a, b, c) prefix tree =
+    let open Lwt.Syntax in
+    let* tree = encode_a a prefix tree in
+    let* tree = encode_b b prefix tree in
+    encode_c c prefix tree
+
+  let raw suffix bytes prefix tree = T.add tree (prefix suffix) bytes
+
+  let value suffix enc =
+    contramap (Data_encoding.Binary.to_bytes_exn enc) (raw suffix)
+
+  let tree key enc value prefix tree = enc value (append_key prefix key) tree
+
+  let lazy_mapping to_key enc_value bindings prefix tree =
+    List.fold_left_s
+      (fun tree (k, v) ->
+        let key = append_key prefix (to_key k) in
+        enc_value v key tree)
+      tree
+      bindings
+
+  type ('tag, 'a) case =
+    | Case : {
+        tag : 'tag;
+        probe : 'a -> 'b option;
+        encode : 'b t;
+      }
+        -> ('tag, 'a) case
+
+  let case tag encode probe = Case {tag; encode; probe}
+
+  let tagged_union encode_tag cases value prefix target_tree =
+    let open Lwt_syntax in
+    let encode_tag = tree ["tag"] encode_tag in
+    let match_case (Case {probe; tag; encode}) =
+      match probe value with
+      | Some value ->
+          let* target_tree = encode_tag tag prefix target_tree in
+          let* x = tree ["value"] encode value prefix target_tree in
+          return (Some x)
+      | None -> return None
+    in
+    let* tree_opt = List.find_map_s match_case cases in
+    match tree_opt with
+    | None -> raise No_tag_matched_on_encoding
+    | Some tree -> return tree
+end

src/lib_scoru_wasm/tree_encoding.mli
@@ -1,0 +1,98 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 TriliTech <contact@trili.tech>                         *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type key = string list
+
+(** Raised when an encoder produced by [tagged_union] does not contain a
+    matching branch. *)
+exception No_tag_matched_on_encoding
+
+module type S = sig
+  type tree
+
+  (** Tree encoder type. *)
+  type -'a t
+
+  (** Represents a partial encoder for specific constructor of a sum-type. *)
+  type ('tag, 'a) case
+
+  (** [contramap f e] is contravariant map operation that creates a new decoder
+      that maps its input using [f] before feeding it to [e]. *)
+  val contramap : ('a -> 'b) -> 'b t -> 'a t
+
+  (** [contramap_lwt f e]. Same as [contramap] except that [f] als produces
+      an Lwt effect. *)
+  val contramap_lwt : ('a -> 'b Lwt.t) -> 'b t -> 'a t
+
+  (** [run enc x tree] encodes the given value [x] using the encoder [enc] and
+      writes it to the tree [tree]. May raise a [Key_not_found] or a
+      [No_tag_matched] exception. *)
+  val run : 'a t -> 'a -> tree -> tree Lwt.t
+
+  (** [raw key] returns an encoder that encodes raw bytes at the given key. *)
+  val raw : key -> bytes t
+
+  (** [value key enc] encodes the value at a given [key] using the provided
+      [enc] encoder for the value. *)
+  val value : key -> 'a Data_encoding.t -> 'a t
+
+  (** [tree key enc] applies a tree encoder for a provided [key]. *)
+  val tree : key -> 'a t -> 'a t
+
+  (** [lazy_mapping to_key enc] returns a key-value list encoder that
+      encodes values from a given key-value list using the key-mapping function
+      [to_key] and the provided encoder [enc] for the values. *)
+  val lazy_mapping : ('k -> key) -> 'v t -> ('k * 'v) list t
+
+  (** [case tag enc f] return a partial encoder that represents a case in a
+      sum-type. The encoder hides the (existentially bound) type of the
+      parameter to the specific case, provided a converter function [f] and
+      base encoder [enc]. *)
+  val case : 'tag -> 'b t -> ('a -> 'b option) -> ('tag, 'a) case
+
+  (** [tagged_union tag_enc cases] returns an encoder that uses [tag_enc] for
+      encoding the value of a field [tag]. The encoder searches through the list
+      of cases for a matching branch. When a matching branch is found, it
+      applies its embedded encoder for the value. This function is used for
+      constructing encoders for sum-types.
+
+      If an insufficient list of cases are provided, the resulting encoder may
+      fail with a [No_tag_matched] error when [run].  *)
+  val tagged_union : 'tag t -> ('tag, 'a) case list -> 'a t
+
+  (** [lwt enc] promotes the given encoder [enc] to one that can handle lwt
+      values. *)
+  val lwt : 'a t -> 'a Lwt.t t
+
+  (** [tup2 e1 e2] creates an encoder that encodes a tuple of elements using
+      [e1] and [e2]. *)
+  val tup2 : 'a t -> 'b t -> ('a * 'b) t
+
+  (** [tup3 e1 e2] creates an encoder that encodes a triple of elements using
+      [e1], [e2], and [e3]. *)
+  val tup3 : 'a t -> 'b t -> 'c t -> ('a * 'b * 'c) t
+end
+
+module Make : functor (T : Tree.S) -> S with type tree = T.tree