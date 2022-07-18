dcceffe724a02a5cc7fa36125f3e743ab1d23694
Alain Mebsout <alain.mebsout@functori.com>
2022-07-18 12:37:27 +0200

    SCORU,Node: remove unused commitment events

--- src/proto_alpha/bin_sc_rollup_node/commitment_event.ml
+++ src/proto_alpha/bin_sc_rollup_node/commitment_event.ml
@@ -31,76 +31,6 @@ module Simple = struct
 
   let section = ["sc_rollup_node"; "commitment"]
 
-  let event_name ?op_result op_type =
-    let op_type_string =
-      match op_type with
-      | `Cement -> "cement"
-      | `Publish -> "publish"
-      | `Store -> "stored"
-    in
-    let op_result_string_opt =
-      op_result
-      |> Option.map (function
-             | `Injected -> "injected"
-             | `Backtracked -> "backtracked"
-             | `Skipped -> "skipped"
-             | `Failed -> "failed")
-    in
-    match op_result_string_opt with
-    | None -> "sc_rollup_node_commitment_" ^ op_type_string
-    | Some op_result_string ->
-        "sc_rollup_" ^ op_type_string ^ "_commitment_" ^ op_result_string
-
-  let event_msg_prefix ?op_result op_type =
-    let op_type_string =
-      match op_type with
-      | `Cement -> "Cementing"
-      | `Publish -> "Publishing"
-      | `Store -> "was stored"
-    in
-    let op_result_string_opt =
-      op_result
-      |> Option.map (function
-             | `Injected -> "was injected"
-             | `Backtracked -> "was backtracked"
-             | `Skipped -> "was skipped"
-             | `Failed -> "has failed")
-    in
-    match op_result_string_opt with
-    | None -> "Commitment " ^ op_type_string
-    | Some op_result_string ->
-        op_type_string ^ " Commitment " ^ op_result_string
-
-  let commitment_event ?op_result op_type =
-    let name = event_name ?op_result op_type in
-    let msg =
-      event_msg_prefix ?op_result op_type
-      ^ " - predecessor: {predecessor}, inbox_level: {inbox_level}, \
-         compressed_state: {compressed_state}, number_of_ticks: \
-         {number_of_ticks}"
-    in
-    declare_4
-      ~section
-      ~name
-      ~msg
-      ~level:Notice
-      ("predecessor", Sc_rollup.Commitment.Hash.encoding)
-      ("inbox_level", Raw_level.encoding)
-      ("compressed_state", Sc_rollup.State_hash.encoding)
-      ("number_of_ticks", Sc_rollup.Number_of_ticks.encoding)
-
-  let commitment_hash_event ?op_result op_type =
-    let name = event_name ?op_result op_type in
-    let msg =
-      event_msg_prefix ?op_result op_type ^ " - commitment: {commitment_hash}"
-    in
-    declare_1
-      ~section
-      ~name
-      ~msg
-      ~level:Notice
-      ("commitment_hash", Sc_rollup.Commitment.Hash.encoding)
-
   let starting =
     declare_0
       ~section
@@ -169,28 +99,34 @@ module Simple = struct
       ("predecessor_hash", Sc_rollup.Commitment.Hash.encoding)
       ("lcc_hash", Sc_rollup.Commitment.Hash.encoding)
 
-  let commitment_stored = commitment_event `Store
+  let commitment_stored =
+    declare_4
+      ~section
+      ~name:"sc_rollup_node_commitment_stored"
+      ~msg:
+        "Commitment was stored - predecessor: {predecessor}, inbox_level: \
+         {inbox_level}, compressed_state: {compressed_state}, number_of_ticks: \
+         {number_of_ticks}"
+      ~level:Notice
+      ("predecessor", Sc_rollup.Commitment.Hash.encoding)
+      ("inbox_level", Raw_level.encoding)
+      ("compressed_state", Sc_rollup.State_hash.encoding)
+      ("number_of_ticks", Sc_rollup.Number_of_ticks.encoding)
 
-  let publish_commitment_injected =
-    commitment_event `Publish ~op_result:`Injected
-
-  let publish_commitment_backtracked =
-    commitment_event `Publish ~op_result:`Backtracked
-
-  let publish_commitment_skipped = commitment_event `Publish ~op_result:`Skipped
-
-  let publish_commitment_failed = commitment_event `Publish ~op_result:`Failed
-
-  let cement_commitment_injected = commitment_event `Cement ~op_result:`Injected
-
-  let cement_commitment_backtracked =
-    commitment_hash_event `Cement ~op_result:`Backtracked
-
-  let cement_commitment_skipped =
-    commitment_hash_event `Cement ~op_result:`Skipped
-
-  let cement_commitment_failed =
-    commitment_hash_event `Cement ~op_result:`Failed
+  let commitment_injected kind =
+    declare_4
+      ~section
+      ~name:(Printf.sprintf "sc_rollup_%s_commitment_injected" kind)
+      ~msg:
+        (kind
+       ^ " commitment was injected - predecessor: {predecessor}, inbox_level: \
+          {inbox_level}, compressed_state: {compressed_state}, \
+          number_of_ticks: {number_of_ticks}")
+      ~level:Notice
+      ("predecessor", Sc_rollup.Commitment.Hash.encoding)
+      ("inbox_level", Raw_level.encoding)
+      ("compressed_state", Sc_rollup.State_hash.encoding)
+      ("number_of_ticks", Sc_rollup.Number_of_ticks.encoding)
 end
 
 let starting = Simple.(emit starting)
@@ -213,25 +149,10 @@ let commitment_will_not_be_published lcc_level
 let commitment_stored = emit_commitment_event Simple.commitment_stored
 
 let publish_commitment_injected =
-  emit_commitment_event Simple.publish_commitment_injected
-
-let publish_commitment_skipped =
-  emit_commitment_event Simple.publish_commitment_skipped
-
-let publish_commitment_backtracked =
-  emit_commitment_event Simple.publish_commitment_backtracked
-
-let publish_commitment_failed =
-  emit_commitment_event Simple.publish_commitment_failed
+  emit_commitment_event (Simple.commitment_injected "publish")
 
 let cement_commitment_injected =
-  emit_commitment_event Simple.cement_commitment_injected
-
-let cement_commitment_skipped = Simple.(emit cement_commitment_skipped)
-
-let cement_commitment_backtracked = Simple.(emit cement_commitment_backtracked)
-
-let cement_commitment_failed = Simple.(emit cement_commitment_failed)
+  emit_commitment_event (Simple.commitment_injected "cement")
 
 let last_cemented_commitment_updated head level =
   Simple.(emit last_cemented_commitment_updated (head, level))
