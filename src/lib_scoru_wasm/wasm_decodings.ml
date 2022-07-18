OKEAMAH/Git

Wasm: flatten instruction recursion using a per module table of basic blocks

src/lib_scoru_wasm/wasm_decodings.ml
@@ -56,6 +56,18 @@ module Make (T : Tree.S) = struct
        go forward. *)
     of_lwt (Instance.Vector.to_list vector)
 
+  let array_decoding dir item_enc =
+    (* FIXME: temporary code until instructions are stored in vectors *)
+    let open Syntax in
+    let* length = value ["length"] Data_encoding.int32 in
+    let* get_item = lazy_mapping (fun key -> [Int.to_string key]) item_enc in
+    let rec dom acc l =
+      if l = 0 then Lwt.return acc
+      else Lwt.bind (get_item l) (fun c -> dom (c :: acc) (l - 1))
+    in
+    of_lwt (Lwt.map Array.of_list (dom [] (Int32.to_int length - 1)))
+    |> tree [dir]
+
   let ref_decoding_for ref_type modules =
     let open Syntax in
     match ref_type with
@@ -173,7 +185,10 @@ module Make (T : Tree.S) = struct
 
   let data_instance_decoding = lazy_vector_decoding "datas" data_decoding
 
-  let rec instruction_decoding () =
+  let block_label_decoding =
+    value [] Interpreter_encodings.Ast.block_label_encoding
+
+  let instruction_decoding =
     let open Ast in
     let open Syntax in
     let* tag = value ["tag"] Data_encoding.string in
@@ -196,18 +211,18 @@ module Make (T : Tree.S) = struct
       | "Block" ->
           let+ type_ =
             value ["$1"] Interpreter_encodings.Ast.block_type_encoding
-          and+ instrs = tree ["$2"] (instruction_list_decoding ()) in
+          and+ instrs = tree ["$2"] block_label_decoding in
           Block (type_, instrs)
       | "Loop" ->
           let+ type_ =
             value ["$1"] Interpreter_encodings.Ast.block_type_encoding
-          and+ instrs = tree ["$2"] (instruction_list_decoding ()) in
+          and+ instrs = tree ["$2"] block_label_decoding in
           Loop (type_, instrs)
       | "If" ->
           let+ type_ =
             value ["$1"] Interpreter_encodings.Ast.block_type_encoding
-          and+ instrs_if = tree ["$2"] (instruction_list_decoding ())
-          and+ instrs_else = tree ["$3"] (instruction_list_decoding ()) in
+          and+ instrs_if = tree ["$2"] block_label_decoding
+          and+ instrs_else = tree ["$3"] block_label_decoding in
           If (type_, instrs_if, instrs_else)
       | "Br" ->
           let+ var = value ["$1"] Interpreter_encodings.Ast.var_encoding in
@@ -402,12 +417,6 @@ module Make (T : Tree.S) = struct
     in
     Source.(instr @@ no_region)
 
-  and instruction_list_decoding () =
-    (* TODO: #3149
-       Rewrite instruction list encoding using virtual "instruction block"
-       pointers. *)
-    list_decoding (instruction_decoding ())
-
   let func_type_decoding () =
     let open Syntax in
     let* params =
@@ -440,7 +449,7 @@ module Make (T : Tree.S) = struct
           "locals"
           (value [] Interpreter_encodings.Types.value_type_encoding)
       in
-      let+ body = instruction_list_decoding () in
+      let+ body = block_label_decoding in
       let func = Ast.{ftype; locals; body} in
       Func.AstFunc (type_, current_module, Source.(func @@ no_region))
 
@@ -528,6 +537,12 @@ module Make (T : Tree.S) = struct
     in
     Instance.NameMap.create ~produce_value:get_export ()
 
+  let instruction_instance_decoding =
+    array_decoding "instructions" instruction_decoding
+
+  let block_instance_decoding =
+    array_decoding "blocks" instruction_instance_decoding
+
   let module_instance_decoding modules =
     let open Syntax in
     let current_module = ref Instance.empty_module_inst in
@@ -538,9 +553,11 @@ module Make (T : Tree.S) = struct
     let* types = type_instance_decoding in
     let* funcs = function_instance_decoding current_module in
     let* elems = elem_instance_decoding funcs in
-    let+ exports = export_instance_decoding funcs tables memories globals in
+    let* exports = export_instance_decoding funcs tables memories globals in
+    let+ blocks = block_instance_decoding in
     let modul =
-      Instance.{memories; tables; globals; datas; types; funcs; elems; exports}
+      Instance.
+        {memories; tables; globals; datas; types; funcs; elems; exports; blocks}
     in
     current_module := modul ;
     modul

tezos/tezos-mirror

src/lib_scoru_wasm/wasm_decodings.ml
@@ -56,6 +56,18 @@ module Make (T : Tree.S) = struct
        go forward. *)
     of_lwt (Instance.Vector.to_list vector)
 
+  let array_decoding dir item_enc =
+    (* FIXME: temporary code until instructions are stored in vectors *)
+    let open Syntax in
+    let* length = value ["length"] Data_encoding.int32 in
+    let* get_item = lazy_mapping (fun key -> [Int.to_string key]) item_enc in
+    let rec dom acc l =
+      if l = 0 then Lwt.return acc
+      else Lwt.bind (get_item l) (fun c -> dom (c :: acc) (l - 1))
+    in
+    of_lwt (Lwt.map Array.of_list (dom [] (Int32.to_int length - 1)))
+    |> scope [dir]
+
   let ref_decoding_for ref_type modules =
     let open Syntax in
     match ref_type with
@@ -173,7 +185,10 @@ module Make (T : Tree.S) = struct
 
   let data_instance_decoding = lazy_vector_decoding "datas" data_decoding
 
-  let rec instruction_decoding () =
+  let block_label_decoding =
+    value [] Interpreter_encodings.Ast.block_label_encoding
+
+  let instruction_decoding =
     let open Ast in
     let open Syntax in
     let* tag = value ["tag"] Data_encoding.string in
@@ -196,18 +211,18 @@ module Make (T : Tree.S) = struct
       | "Block" ->
           let+ type_ =
             value ["$1"] Interpreter_encodings.Ast.block_type_encoding
-          and+ instrs = scope ["$2"] (instruction_list_decoding ()) in
+          and+ instrs = scope ["$2"] block_label_decoding in
           Block (type_, instrs)
       | "Loop" ->
           let+ type_ =
             value ["$1"] Interpreter_encodings.Ast.block_type_encoding
-          and+ instrs = scope ["$2"] (instruction_list_decoding ()) in
+          and+ instrs = scope ["$2"] block_label_decoding in
           Loop (type_, instrs)
       | "If" ->
           let+ type_ =
             value ["$1"] Interpreter_encodings.Ast.block_type_encoding
-          and+ instrs_if = scope ["$2"] (instruction_list_decoding ())
-          and+ instrs_else = scope ["$3"] (instruction_list_decoding ()) in
+          and+ instrs_if = scope ["$2"] block_label_decoding
+          and+ instrs_else = scope ["$3"] block_label_decoding in
           If (type_, instrs_if, instrs_else)
       | "Br" ->
           let+ var = value ["$1"] Interpreter_encodings.Ast.var_encoding in
@@ -402,12 +417,6 @@ module Make (T : Tree.S) = struct
     in
     Source.(instr @@ no_region)
 
-  and instruction_list_decoding () =
-    (* TODO: #3149
-       Rewrite instruction list encoding using virtual "instruction block"
-       pointers. *)
-    list_decoding (instruction_decoding ())
-
   let func_type_decoding () =
     let open Syntax in
     let* params =
@@ -440,7 +449,7 @@ module Make (T : Tree.S) = struct
           "locals"
           (value [] Interpreter_encodings.Types.value_type_encoding)
       in
-      let+ body = instruction_list_decoding () in
+      let+ body = block_label_decoding in
       let func = Ast.{ftype; locals; body} in
       Func.AstFunc (type_, current_module, Source.(func @@ no_region))
 
@@ -528,6 +537,12 @@ module Make (T : Tree.S) = struct
     in
     Instance.NameMap.create ~produce_value:get_export ()
 
+  let instruction_instance_decoding =
+    array_decoding "instructions" instruction_decoding
+
+  let block_instance_decoding =
+    array_decoding "blocks" instruction_instance_decoding
+
   let module_instance_decoding modules =
     let open Syntax in
     let current_module = ref Instance.empty_module_inst in
@@ -538,9 +553,11 @@ module Make (T : Tree.S) = struct
     let* types = type_instance_decoding in
     let* funcs = function_instance_decoding current_module in
     let* elems = elem_instance_decoding funcs in
-    let+ exports = export_instance_decoding funcs tables memories globals in
+    let* exports = export_instance_decoding funcs tables memories globals in
+    let+ blocks = block_instance_decoding in
     let modul =
-      Instance.{memories; tables; globals; datas; types; funcs; elems; exports}
+      Instance.
+        {memories; tables; globals; datas; types; funcs; elems; exports; blocks}
     in
     current_module := modul ;
     modul