6b9fa9ab7005640e78ee42d04f974f05de0271fc
François Thiré <francois.thire@nomadic-labs.com>
2022-07-27 09:36:36 +0200

    DAL: Move the library to make it depends on `octez-crypto`

--- manifest/main.ml
+++ manifest/main.ml
@@ -823,6 +823,37 @@ let _octez_crypto_tests_unix =
         octez_test_helpers;
       ]
 
+let octez_crypto_dal =
+  public_lib
+    "tezos-crypto-dal"
+    ~path:"src/lib_crypto_dal"
+    ~synopsis:"DAL cryptographic primitives"
+    ~opam:"tezos-crypto-dal"
+    ~deps:
+      [
+        octez_stdlib |> open_;
+        octez_error_monad |> open_;
+        data_encoding |> open_;
+        bls12_381_polynomial;
+      ]
+
+let _octez_crypto_dal_tests =
+  tests
+    ["test_dal_cryptobox"]
+    ~path:"src/lib_crypto_dal/test"
+    ~opam:"tezos-crypto-dal"
+    ~dep_files:["shard_proofs_precomp"]
+    ~deps:
+      [
+        octez_stdlib |> open_;
+        octez_crypto_dal |> open_;
+        octez_error_monad |> open_;
+        data_encoding |> open_;
+        alcotest;
+        qcheck_alcotest;
+        bls12_381_polynomial;
+      ]
+
 let octez_event_logging =
   public_lib
     "tezos-event-logging"
@@ -5126,36 +5157,6 @@ let _octez_codec =
              if link then Protocol.client protocol else None);
          ])
     ~linkall:true
-
-let octez_crypto_dal =
-  public_lib
-    "tezos-crypto.dal"
-    ~path:"src/lib_crypto/dal"
-    ~opam:"tezos-crypto"
-    ~deps:
-      [
-        octez_stdlib |> open_;
-        octez_error_monad |> open_;
-        data_encoding |> open_;
-        bls12_381_polynomial;
-      ]
-
-let _octez_crypto_dal_tests =
-  tests
-    ["test_dal_cryptobox"]
-    ~path:"src/lib_crypto/dal/test"
-    ~opam:"tezos-crypto"
-    ~dep_files:["shard_proofs_precomp"]
-    ~deps:
-      [
-        octez_stdlib |> open_;
-        octez_crypto_dal |> open_;
-        octez_error_monad |> open_;
-        data_encoding |> open_;
-        alcotest;
-        qcheck_alcotest;
-        bls12_381_polynomial;
-      ]
 
 let _octez_proxy_server =
   public_exe
