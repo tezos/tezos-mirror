whoistiger/tezos

Proto: add add_cycle_era to Level_repr

src/proto_alpha/lib_protocol/level_repr.ml
@@ -147,6 +147,8 @@ let create_cycle_eras cycle_eras =
       in
       aux newest_era older_eras >>? fun () -> ok cycle_eras
 
+let add_cycle_era cycle_eras new_era = create_cycle_eras (new_era :: cycle_eras)
+
 let cycle_era_encoding =
   let open Data_encoding in
   conv

src/proto_alpha/lib_protocol/level_repr.mli
@@ -85,6 +85,9 @@ val cycle_eras_encoding : cycle_eras Data_encoding.t
 *)
 val create_cycle_eras : cycle_era list -> cycle_eras tzresult
 
+(** Add a new cycle era *)
+val add_cycle_era : cycle_eras -> cycle_era -> cycle_eras tzresult
+
 (** Returns the current era *)
 val current_era : cycle_eras -> cycle_era
 

tezos/tezos-mirror

src/proto_alpha/lib_protocol/level_repr.ml
@@ -147,6 +147,8 @@ let create_cycle_eras cycle_eras =
       in
       aux newest_era older_eras >>? fun () -> ok cycle_eras
 
+let add_cycle_era new_era cycle_eras = create_cycle_eras (new_era :: cycle_eras)
+
 let cycle_era_encoding =
   let open Data_encoding in
   conv

src/proto_alpha/lib_protocol/level_repr.mli
@@ -85,6 +85,9 @@ val cycle_eras_encoding : cycle_eras Data_encoding.t
 *)
 val create_cycle_eras : cycle_era list -> cycle_eras tzresult
 
+(** Add a new cycle era *)
+val add_cycle_era : cycle_era -> cycle_eras -> cycle_eras tzresult
+
 (** Returns the current era *)
 val current_era : cycle_eras -> cycle_era
 