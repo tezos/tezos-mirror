OKEAMAH/Git

SCORU,Node: Split context and map block hash -> commit hash

src/proto_alpha/bin_sc_rollup_node/context.ml
@@ -1,0 +1,198 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Protocol
+open Alpha_context
+module Maker = Irmin_pack_unix.Maker (Tezos_context_encoding.Context.Conf)
+
+module IStore = struct
+  include Maker.Make (Tezos_context_encoding.Context.Schema)
+  module Schema = Tezos_context_encoding.Context.Schema
+end
+
+type tree = IStore.tree
+
+type index = {path : string; repo : IStore.Repo.t}
+
+type t = {index : index; tree : tree}
+
+type hash = IStore.hash
+
+type path = string list
+
+(* TODO: use (better) encoding with fixed size *)
+let hash_encoding =
+  let open Data_encoding in
+  conv
+    (fun h -> IStore.Hash.to_raw_string h |> Bytes.unsafe_of_string)
+    (fun b -> Bytes.unsafe_to_string b |> IStore.Hash.unsafe_of_raw_string)
+    bytes
+
+let load configuration =
+  let open Lwt_syntax in
+  let open Configuration in
+  let path = default_context_dir configuration.data_dir in
+  let+ repo = IStore.Repo.v (Irmin_pack.config path) in
+  {path; repo}
+
+let flush ctxt = IStore.flush ctxt.repo
+
+let close ctxt = IStore.Repo.close ctxt.repo
+
+let info message =
+  let date = Unix.gettimeofday () |> int_of_float |> Int64.of_int in
+  Irmin.Info.Default.v ~author:"Tezos smart-contract rollup node" ~message date
+
+let raw_commit ?(message = "") index tree =
+  let info = IStore.Info.v ~author:"Tezos" 0L ~message in
+  IStore.Commit.v index.repo ~info ~parents:[] tree
+
+let commit ?message ctxt =
+  let open Lwt_syntax in
+  let+ commit = raw_commit ?message ctxt.index ctxt.tree in
+  IStore.Commit.hash commit
+
+let checkout index key =
+  let open Lwt_syntax in
+  let* o = IStore.Commit.of_hash index.repo key in
+  match o with
+  | None -> return_none
+  | Some commit ->
+      let tree = IStore.Commit.tree commit in
+      return_some {index; tree}
+
+let empty index = {index; tree = IStore.Tree.empty ()}
+
+let is_empty ctxt = IStore.Tree.is_empty ctxt.tree
+
+let make index tree = {index; tree}
+
+module IStoreTree = struct
+  include
+    Tezos_context_helpers.Context.Make_tree
+      (Tezos_context_encoding.Context.Conf)
+      (IStore)
+
+  type nonrec t = index
+
+  type tree = IStore.tree
+
+  type key = path
+
+  type value = bytes
+end
+
+module IStoreProof =
+  Tezos_context_helpers.Context.Make_proof
+    (IStore)
+    (Tezos_context_encoding.Context.Conf)
+
+module Inbox = struct
+  include Sc_rollup.Inbox
+
+  include Sc_rollup.Inbox.MakeHashingScheme (struct
+    module Tree = IStoreTree
+
+    type t = index
+
+    type tree = Tree.tree
+
+    let commit_tree index _key tree =
+      (* TODO: _key ? *)
+      let open Lwt_syntax in
+      let info () = IStore.Info.v ~author:"Tezos" 0L ~message:"" in
+      let* (_ : IStore.commit) =
+        IStore.Commit.v index.repo ~info:(info ()) ~parents:[] tree
+      in
+      return ()
+
+    let to_inbox_hash kinded_hash =
+      match kinded_hash with `Value h | `Node h -> Hash.of_context_hash h
+
+    let from_inbox_hash inbox_hash =
+      let ctxt_hash = Hash.to_context_hash inbox_hash in
+      let store_hash =
+        IStore.Hash.unsafe_of_raw_string (Context_hash.to_string ctxt_hash)
+      in
+      `Node store_hash
+
+    let lookup_tree index hash =
+      IStore.Tree.of_hash index.repo (from_inbox_hash hash)
+
+    type proof = IStoreProof.Proof.tree IStoreProof.Proof.t
+
+    let verify_proof proof f =
+      Lwt.map Result.to_option (IStoreProof.verify_tree_proof proof f)
+
+    let produce_proof index tree f =
+      let open Lwt_syntax in
+      (* TODO: #3381
+         Since committing is required for proof production to work
+         properly, why isn't committing part of the process of proof
+         production? *)
+      let* _commit_key = raw_commit index tree in
+      match IStoreTree.kinded_key tree with
+      | Some k ->
+          let* p = IStoreProof.produce_tree_proof index.repo k f in
+          return (Some p)
+      | None -> return None
+
+    let proof_before proof = to_inbox_hash proof.IStoreProof.Proof.before
+
+    let proof_encoding =
+      Tezos_context_helpers.Merkle_proof_encoding.V1.Tree32.tree_proof_encoding
+  end)
+end
+
+(** Aggregated collection of messages from the L1 inbox *)
+module MessageTrees = struct
+  type value = tree
+
+  let key = ["message_tree"]
+
+  let find ctxt = IStore.Tree.find_tree ctxt.tree key
+
+  let set ctxt tree =
+    let open Lwt_syntax in
+    let* tree = IStore.Tree.add_tree ctxt.tree key tree in
+    let ctxt = {ctxt with tree} in
+    return ctxt
+end
+
+(** State of the PVM that this rollup node deals with *)
+module PVMState = struct
+  type value = tree
+
+  let key = ["pvm_state"]
+
+  let find ctxt = IStore.Tree.find_tree ctxt.tree key
+
+  let exists ctxt = IStore.Tree.mem_tree ctxt.tree key
+
+  let set ctxt state =
+    let open Lwt_syntax in
+    let+ tree = IStore.Tree.add_tree ctxt.tree key state in
+    {ctxt with tree}
+end

src/proto_alpha/bin_sc_rollup_node/inbox.mli
@@ -32,26 +32,21 @@
    The inbox state is persistent.
 
 *)
-open Protocol
 
 (** [process_head node_ctxt head operations] changes the state of the inbox to
     react to [head]. In particular, this process filters the provided
     [operations] of the [head] block. *)
-val process_head : Node_context.t -> Layer1.head -> unit tzresult Lwt.t
+val process_head : Node_context.t -> Layer1.head -> Context.t tzresult Lwt.t
 
 (** [inbox_of_hash node_ctxt block_hash] returns the rollup inbox at the end of
     the given validation of [block_hash]. *)
 val inbox_of_hash :
-  Node_context.t ->
-  Block_hash.t ->
-  Alpha_context.Sc_rollup.Inbox.t tzresult Lwt.t
+  Node_context.t -> Block_hash.t -> Context.Inbox.t tzresult Lwt.t
 
 (** [history_of_hash node_ctxt block_hash] returns the rollup inbox history at
     the end of the given validation of [block_hash]. *)
 val history_of_hash :
-  Node_context.t -> Block_hash.t -> Store.Inbox.history tzresult Lwt.t
-
-val find_message_tree : Store.t -> Block_hash.t -> Store.tree option Lwt.t
+  Node_context.t -> Block_hash.t -> Context.Inbox.history tzresult Lwt.t
 
 (** [start ()] initializes the inbox to track the messages being published. *)
 val start : unit -> unit Lwt.t

src/proto_alpha/bin_sc_rollup_node/interpreter.ml
@@ -25,7 +25,7 @@
 
 open Protocol
 open Alpha_context
-module Inbox = Store.Inbox
+module Inbox = Context.Inbox
 
 module type S = sig
   module PVM : Pvm.S
@@ -33,7 +33,8 @@ module type S = sig
   (** [process_head node_ctxt head] interprets the messages associated
       with a [head] from a chain [event]. This requires the inbox to be updated
       beforehand. *)
-  val process_head : Node_context.t -> Layer1.head -> unit tzresult Lwt.t
+  val process_head :
+    Node_context.t -> Context.t -> Layer1.head -> unit tzresult Lwt.t
 
   (** [state_of_tick node_ctxt tick level] returns [Some (state, hash)]
       for a given [tick] if this [tick] happened before
@@ -194,7 +195,7 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
         in
         return (state, num_messages, inbox_level, fuel)
 
-  let genesis_state block_hash node_ctxt =
+  let genesis_state block_hash node_ctxt ctxt =
     let open Node_context in
     let open Lwt_result_syntax in
     let* boot_sector =
@@ -203,31 +204,33 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
         (node_ctxt.cctxt#chain, `Hash (block_hash, 0))
         node_ctxt.rollup_address
     in
-    let*! initial_state = PVM.initial_state node_ctxt.store in
+    let*! initial_state = PVM.initial_state node_ctxt.context in
     let*! genesis_state = PVM.install_boot_sector initial_state boot_sector in
-    return genesis_state
+    let*! ctxt = Context.PVMState.set ctxt genesis_state in
+    return (ctxt, genesis_state)
 
-  let state_of_hash node_ctxt hash level =
+  let state_of_hash node_ctxt ctxt hash level =
     let open Lwt_result_syntax in
     if Raw_level.(level = node_ctxt.Node_context.genesis_info.level) then
-      genesis_state hash node_ctxt
+      genesis_state hash node_ctxt ctxt
     else
-      let*! state = Store.PVMState.find node_ctxt.store hash in
+      let*! state = Context.PVMState.find ctxt in
       match state with
       | None -> tzfail (Sc_rollup_node_errors.Missing_PVM_state (hash, level))
-      | Some state -> return state
+      | Some state -> return (ctxt, state)
 
   (** [transition_pvm node_ctxt predecessor_hash head] runs a PVM at the
       previous state from block [predecessor_hash] by consuming as many messages
       as possible from block [head]. *)
-  let transition_pvm node_ctxt predecessor_hash (Layer1.Head {hash; level}) =
+  let transition_pvm node_ctxt ctxt predecessor_hash (Layer1.Head {hash; level})
+      =
     let open Lwt_result_syntax in
     (* Retrieve the previous PVM state from store. *)
     let pred_level = Int32.pred level |> Raw_level.of_int32_exn in
-    let* predecessor_state =
+    let* ctxt, predecessor_state =
       if Raw_level.(pred_level <= node_ctxt.Node_context.genesis_info.level)
-      then genesis_state hash node_ctxt
-      else state_of_hash node_ctxt predecessor_hash pred_level
+      then genesis_state hash node_ctxt ctxt
+      else state_of_hash node_ctxt ctxt predecessor_hash pred_level
     in
     let* state, num_messages, inbox_level, _fuel =
       eval_block_inbox
@@ -238,7 +241,9 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
     in
 
     (* Write final state to store. *)
-    let*! () = Store.PVMState.set node_ctxt.store hash state in
+    let*! ctxt = Context.PVMState.set ctxt state in
+    let*! context_hash = Context.commit ctxt in
+    let*! () = Store.Contexts.add node_ctxt.store hash context_hash in
 
     (* Compute extra information about the state. *)
     let*! initial_tick = PVM.get_tick predecessor_state in
@@ -277,12 +282,12 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
     return_unit
 
   (** [process_head node_ctxt head] runs the PVM for the given head. *)
-  let process_head node_ctxt head =
+  let process_head node_ctxt ctxt head =
     let open Lwt_result_syntax in
     let*! predecessor_hash =
       Layer1.predecessor node_ctxt.Node_context.store head
     in
-    transition_pvm node_ctxt predecessor_hash head
+    transition_pvm node_ctxt ctxt predecessor_hash head
 
   (** [run_for_ticks node_ctxt predecessor_hash hash tick_distance] starts the
       evaluation of the inbox at block [hash] for at most [tick_distance]. *)
@@ -291,7 +296,10 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
     let pred_level =
       WithExceptions.Option.get ~loc:__LOC__ (Raw_level.pred level)
     in
-    let* state = state_of_hash node_ctxt predecessor_hash pred_level in
+    let* ctxt = Node_context.checkout_context node_ctxt predecessor_hash in
+    let* _ctxt, state =
+      state_of_hash node_ctxt ctxt predecessor_hash pred_level
+    in
     let* state, _counter, _level, _fuel =
       eval_block_inbox
         node_ctxt.loser_mode

tezos/tezos-mirror

src/proto_alpha/bin_sc_rollup_node/context.ml
@@ -1,0 +1,202 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Protocol
+open Alpha_context
+module Maker = Irmin_pack_unix.Maker (Tezos_context_encoding.Context.Conf)
+
+module IStore = struct
+  include Maker.Make (Tezos_context_encoding.Context.Schema)
+  module Schema = Tezos_context_encoding.Context.Schema
+end
+
+type tree = IStore.tree
+
+type index = {path : string; repo : IStore.Repo.t}
+
+type t = {index : index; tree : tree}
+
+type hash = IStore.hash
+
+type path = string list
+
+let hash_encoding =
+  let open Data_encoding in
+  conv
+    (fun h -> IStore.Hash.to_raw_string h |> Bytes.unsafe_of_string)
+    (fun b -> Bytes.unsafe_to_string b |> IStore.Hash.unsafe_of_raw_string)
+    (Fixed.bytes IStore.Hash.hash_size)
+
+let pp_hash fmt h =
+  IStore.Hash.to_raw_string h
+  |> Hex.of_string |> Hex.show |> Format.pp_print_string fmt
+
+let load configuration =
+  let open Lwt_syntax in
+  let open Configuration in
+  let path = default_context_dir configuration.data_dir in
+  let+ repo = IStore.Repo.v (Irmin_pack.config path) in
+  {path; repo}
+
+let flush ctxt = IStore.flush ctxt.repo
+
+let close ctxt = IStore.Repo.close ctxt.repo
+
+let info message =
+  let date = Unix.gettimeofday () |> int_of_float |> Int64.of_int in
+  Irmin.Info.Default.v ~author:"Tezos smart-contract rollup node" ~message date
+
+let raw_commit ?(message = "") index tree =
+  let info = IStore.Info.v ~author:"Tezos" 0L ~message in
+  IStore.Commit.v index.repo ~info ~parents:[] tree
+
+let commit ?message ctxt =
+  let open Lwt_syntax in
+  let+ commit = raw_commit ?message ctxt.index ctxt.tree in
+  IStore.Commit.hash commit
+
+let checkout index key =
+  let open Lwt_syntax in
+  let* o = IStore.Commit.of_hash index.repo key in
+  match o with
+  | None -> return_none
+  | Some commit ->
+      let tree = IStore.Commit.tree commit in
+      return_some {index; tree}
+
+let empty index = {index; tree = IStore.Tree.empty ()}
+
+let is_empty ctxt = IStore.Tree.is_empty ctxt.tree
+
+let make index tree = {index; tree}
+
+module IStoreTree = struct
+  include
+    Tezos_context_helpers.Context.Make_tree
+      (Tezos_context_encoding.Context.Conf)
+      (IStore)
+
+  type nonrec t = index
+
+  type tree = IStore.tree
+
+  type key = path
+
+  type value = bytes
+end
+
+module IStoreProof =
+  Tezos_context_helpers.Context.Make_proof
+    (IStore)
+    (Tezos_context_encoding.Context.Conf)
+
+module Inbox = struct
+  include Sc_rollup.Inbox
+
+  include Sc_rollup.Inbox.Make_hashing_scheme (struct
+    module Tree = IStoreTree
+
+    type t = index
+
+    type tree = Tree.tree
+
+    let commit_tree index _key tree =
+      (* TODO: _key ? *)
+      let open Lwt_syntax in
+      let info () = IStore.Info.v ~author:"Tezos" 0L ~message:"" in
+      let* (_ : IStore.commit) =
+        IStore.Commit.v index.repo ~info:(info ()) ~parents:[] tree
+      in
+      return ()
+
+    let to_inbox_hash kinded_hash =
+      match kinded_hash with `Value h | `Node h -> Hash.of_context_hash h
+
+    let from_inbox_hash inbox_hash =
+      let ctxt_hash = Hash.to_context_hash inbox_hash in
+      let store_hash =
+        IStore.Hash.unsafe_of_raw_string (Context_hash.to_string ctxt_hash)
+      in
+      `Node store_hash
+
+    let lookup_tree index hash =
+      IStore.Tree.of_hash index.repo (from_inbox_hash hash)
+
+    type proof = IStoreProof.Proof.tree IStoreProof.Proof.t
+
+    let verify_proof proof f =
+      Lwt.map Result.to_option (IStoreProof.verify_tree_proof proof f)
+
+    let produce_proof index tree f =
+      let open Lwt_syntax in
+      (* TODO: #3381
+         Since committing is required for proof production to work
+         properly, why isn't committing part of the process of proof
+         production? *)
+      let* _commit_key = raw_commit index tree in
+      match IStoreTree.kinded_key tree with
+      | Some k ->
+          let* p = IStoreProof.produce_tree_proof index.repo k f in
+          return (Some p)
+      | None -> return None
+
+    let proof_before proof = to_inbox_hash proof.IStoreProof.Proof.before
+
+    let proof_encoding =
+      Tezos_context_merkle_proof_encoding.Merkle_proof_encoding.V1.Tree32
+      .tree_proof_encoding
+  end)
+end
+
+(** Aggregated collection of messages from the L1 inbox *)
+module MessageTrees = struct
+  type value = tree
+
+  let key = ["message_tree"]
+
+  let find ctxt = IStore.Tree.find_tree ctxt.tree key
+
+  let set ctxt tree =
+    let open Lwt_syntax in
+    let* tree = IStore.Tree.add_tree ctxt.tree key tree in
+    let ctxt = {ctxt with tree} in
+    return ctxt
+end
+
+(** State of the PVM that this rollup node deals with *)
+module PVMState = struct
+  type value = tree
+
+  let key = ["pvm_state"]
+
+  let find ctxt = IStore.Tree.find_tree ctxt.tree key
+
+  let exists ctxt = IStore.Tree.mem_tree ctxt.tree key
+
+  let set ctxt state =
+    let open Lwt_syntax in
+    let+ tree = IStore.Tree.add_tree ctxt.tree key state in
+    {ctxt with tree}
+end

src/proto_alpha/bin_sc_rollup_node/inbox.mli
@@ -32,26 +32,23 @@
    The inbox state is persistent.
 
 *)
-open Protocol
+
+open Protocol.Alpha_context
 
 (** [process_head node_ctxt head operations] changes the state of the inbox to
     react to [head]. In particular, this process filters the provided
     [operations] of the [head] block. *)
-val process_head : Node_context.t -> Layer1.head -> unit tzresult Lwt.t
+val process_head : Node_context.t -> Layer1.head -> Context.t tzresult Lwt.t
 
 (** [inbox_of_hash node_ctxt block_hash] returns the rollup inbox at the end of
     the given validation of [block_hash]. *)
 val inbox_of_hash :
-  Node_context.t ->
-  Block_hash.t ->
-  Alpha_context.Sc_rollup.Inbox.t tzresult Lwt.t
+  Node_context.t -> Block_hash.t -> Sc_rollup.Inbox.t tzresult Lwt.t
 
 (** [history_of_hash node_ctxt block_hash] returns the rollup inbox history at
     the end of the given validation of [block_hash]. *)
 val history_of_hash :
-  Node_context.t -> Block_hash.t -> Store.Inbox.history tzresult Lwt.t
-
-val find_message_tree : Store.t -> Block_hash.t -> Store.tree option Lwt.t
+  Node_context.t -> Block_hash.t -> Sc_rollup.Inbox.history tzresult Lwt.t
 
 (** [start ()] initializes the inbox to track the messages being published. *)
 val start : unit -> unit Lwt.t

src/proto_alpha/bin_sc_rollup_node/interpreter.ml
@@ -25,7 +25,7 @@
 
 open Protocol
 open Alpha_context
-module Inbox = Store.Inbox
+module Inbox = Sc_rollup.Inbox
 
 module type S = sig
   module PVM : Pvm.S
@@ -33,7 +33,8 @@ module type S = sig
   (** [process_head node_ctxt head] interprets the messages associated
       with a [head] from a chain [event]. This requires the inbox to be updated
       beforehand. *)
-  val process_head : Node_context.t -> Layer1.head -> unit tzresult Lwt.t
+  val process_head :
+    Node_context.t -> Context.t -> Layer1.head -> unit tzresult Lwt.t
 
   (** [state_of_tick node_ctxt tick level] returns [Some (state, hash)]
       for a given [tick] if this [tick] happened before
@@ -194,7 +195,7 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
         in
         return (state, num_messages, inbox_level, fuel)
 
-  let genesis_state block_hash node_ctxt =
+  let genesis_state block_hash node_ctxt ctxt =
     let open Node_context in
     let open Lwt_result_syntax in
     let* boot_sector =
@@ -203,31 +204,33 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
         (node_ctxt.cctxt#chain, `Hash (block_hash, 0))
         node_ctxt.rollup_address
     in
-    let*! initial_state = PVM.initial_state node_ctxt.store in
+    let*! initial_state = PVM.initial_state node_ctxt.context in
     let*! genesis_state = PVM.install_boot_sector initial_state boot_sector in
-    return genesis_state
+    let*! ctxt = Context.PVMState.set ctxt genesis_state in
+    return (ctxt, genesis_state)
 
-  let state_of_hash node_ctxt hash level =
+  let state_of_hash node_ctxt ctxt hash level =
     let open Lwt_result_syntax in
     if Raw_level.(level = node_ctxt.Node_context.genesis_info.level) then
-      genesis_state hash node_ctxt
+      genesis_state hash node_ctxt ctxt
     else
-      let*! state = Store.PVMState.find node_ctxt.store hash in
+      let*! state = Context.PVMState.find ctxt in
       match state with
       | None -> tzfail (Sc_rollup_node_errors.Missing_PVM_state (hash, level))
-      | Some state -> return state
+      | Some state -> return (ctxt, state)
 
   (** [transition_pvm node_ctxt predecessor_hash head] runs a PVM at the
       previous state from block [predecessor_hash] by consuming as many messages
       as possible from block [head]. *)
-  let transition_pvm node_ctxt predecessor_hash (Layer1.Head {hash; level}) =
+  let transition_pvm node_ctxt ctxt predecessor_hash (Layer1.Head {hash; level})
+      =
     let open Lwt_result_syntax in
     (* Retrieve the previous PVM state from store. *)
     let pred_level = Int32.pred level |> Raw_level.of_int32_exn in
-    let* predecessor_state =
+    let* ctxt, predecessor_state =
       if Raw_level.(pred_level <= node_ctxt.Node_context.genesis_info.level)
-      then genesis_state hash node_ctxt
-      else state_of_hash node_ctxt predecessor_hash pred_level
+      then genesis_state hash node_ctxt ctxt
+      else state_of_hash node_ctxt ctxt predecessor_hash pred_level
     in
     let* state, num_messages, inbox_level, _fuel =
       eval_block_inbox
@@ -238,7 +241,9 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
     in
 
     (* Write final state to store. *)
-    let*! () = Store.PVMState.set node_ctxt.store hash state in
+    let*! ctxt = Context.PVMState.set ctxt state in
+    let*! context_hash = Context.commit ctxt in
+    let*! () = Store.Contexts.add node_ctxt.store hash context_hash in
 
     (* Compute extra information about the state. *)
     let*! initial_tick = PVM.get_tick predecessor_state in
@@ -277,12 +282,12 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
     return_unit
 
   (** [process_head node_ctxt head] runs the PVM for the given head. *)
-  let process_head node_ctxt head =
+  let process_head node_ctxt ctxt head =
     let open Lwt_result_syntax in
     let*! predecessor_hash =
       Layer1.predecessor node_ctxt.Node_context.store head
     in
-    transition_pvm node_ctxt predecessor_hash head
+    transition_pvm node_ctxt ctxt predecessor_hash head
 
   (** [run_for_ticks node_ctxt predecessor_hash hash tick_distance] starts the
       evaluation of the inbox at block [hash] for at most [tick_distance]. *)
@@ -291,7 +296,10 @@ module Make (PVM : Pvm.S) : S with module PVM = PVM = struct
     let pred_level =
       WithExceptions.Option.get ~loc:__LOC__ (Raw_level.pred level)
     in
-    let* state = state_of_hash node_ctxt predecessor_hash pred_level in
+    let* ctxt = Node_context.checkout_context node_ctxt predecessor_hash in
+    let* _ctxt, state =
+      state_of_hash node_ctxt ctxt predecessor_hash pred_level
+    in
     let* state, _counter, _level, _fuel =
       eval_block_inbox
         node_ctxt.loser_mode