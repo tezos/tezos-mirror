e979475ffe2f78b2815eca987c9ce4912be050f6
Romain Bardou <romain@nomadic-labs.com>
2022-08-24 11:22:33 +0200

    Manifest: backport opam fixes from 14.0

--- manifest/main.ml
+++ manifest/main.ml
@@ -89,7 +89,7 @@ let bls12_381 =
     version
 
 let bls12_381_polynomial =
-  let version = V.at_least "0.1.0" in
+  let version = V.exactly "0.1.2" in
   external_lib ~js_compatible:false "tezos-bls12-381-polynomial" version
 
 let camlzip = external_lib "camlzip" V.(at_least "1.11" && less_than "1.12")
@@ -332,7 +332,7 @@ let zarith_stubs_js = external_lib ~js_compatible:true "zarith_stubs_js" V.True
 
 (* VENDORED LIBS *)
 
-let ledgerwallet_tezos = vendored_lib "ledgerwallet-tezos" V.True
+let ledgerwallet_tezos = vendored_lib "ledgerwallet-tezos" V.(at_least "0.2.1")
 
 let vdf =
   external_lib ~js_compatible:true "class_group_vdf" V.(at_least "0.0.4")
@@ -576,13 +576,14 @@ let octez_error_monad =
         lwt;
         octez_lwt_result_stdlib;
       ]
+    ~conflicts:[external_lib "result" V.(less_than "1.5")]
     ~js_compatible:true
 
 let octez_webassembly_interpreter =
   public_lib
     "tezos-webassembly-interpreter"
     ~path:"src/lib_webassembly"
-    ~license:"Apache License 2.0"
+    ~license:"Apache-2.0"
     ~extra_authors:["WebAssembly Authors"]
     ~synopsis:"WebAssembly reference interpreter with tweaks for Tezos"
     ~flags:(Flags.standard ~disable_warnings:[27] ())
