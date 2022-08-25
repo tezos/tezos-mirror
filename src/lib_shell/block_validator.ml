kcommerce/tezos

Workers: remove dead code related to Event parameter

src/lib_shell/block_validator.ml
@@ -118,7 +118,7 @@ module Request = struct
 end
 
 module Events = Block_validator_events
-module Worker = Worker.Make (Name) (Event) (Request) (Types)
+module Worker = Worker.Make (Name) (Request) (Types)
 
 type t = Worker.infinite Worker.queue Worker.t
 

src/lib_shell/chain_validator.ml
@@ -105,7 +105,7 @@ module Types = struct
 end
 
 module Events = Chain_validator_events
-module Worker = Worker.Make (Name) (Event) (Request) (Types)
+module Worker = Worker.Make (Name) (Request) (Types)
 open Types
 
 type t = Worker.infinite Worker.queue Worker.t

src/lib_shell/peer_validator.ml
@@ -89,7 +89,7 @@ module Types = struct
 end
 
 module Events = Peer_validator_events
-module Worker = Worker.Make (Name) (Event) (Request) (Types)
+module Worker = Worker.Make (Name) (Request) (Types)
 open Types
 
 type t = Worker.dropbox Worker.t

tezos/tezos-mirror

src/lib_shell/block_validator.ml
@@ -118,7 +118,7 @@ module Request = struct
 end
 
 module Events = Block_validator_events
-module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types)
+module Worker = Worker.MakeSingle (Name) (Request) (Types)
 
 type t = Worker.infinite Worker.queue Worker.t
 

src/lib_shell/chain_validator.ml
@@ -104,7 +104,7 @@ module Types = struct
 end
 
 module Events = Chain_validator_events
-module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types)
+module Worker = Worker.MakeSingle (Name) (Request) (Types)
 open Types
 
 type t = Worker.infinite Worker.queue Worker.t

src/lib_shell/peer_validator.ml
@@ -89,7 +89,7 @@ module Types = struct
 end
 
 module Events = Peer_validator_events
-module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types)
+module Worker = Worker.MakeSingle (Name) (Request) (Types)
 open Types
 
 type t = Worker.dropbox Worker.t