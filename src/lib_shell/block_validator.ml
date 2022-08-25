kcommerce/tezos

Workers: remove dead code related to Logger

src/lib_shell/block_validator.ml
@@ -118,14 +118,7 @@ module Request = struct
 end
 
 module Events = Block_validator_events
-
-module Logger =
-  Worker_logger.Make (Event) (Request)
-    (struct
-      let worker_name = "node_block_validator"
-    end)
-
-module Worker = Worker.Make (Name) (Event) (Request) (Types) (Logger)
+module Worker = Worker.Make (Name) (Event) (Request) (Types)
 
 type t = Worker.infinite Worker.queue Worker.t
 

src/lib_shell/chain_validator.ml
@@ -104,14 +104,8 @@ module Types = struct
       state.synchronisation_state
 end
 
-module Logger =
-  Worker_logger.Make (Event) (Request)
-    (struct
-      let worker_name = "node_chain_validator"
-    end)
-
 module Events = Chain_validator_events
-module Worker = Worker.Make (Name) (Event) (Request) (Types) (Logger)
+module Worker = Worker.Make (Name) (Event) (Request) (Types)
 open Types
 
 type t = Worker.infinite Worker.queue Worker.t

src/lib_shell/peer_validator.ml
@@ -88,14 +88,8 @@ module Types = struct
     | Some p -> Bootstrap_pipeline.length p
 end
 
-module Logger =
-  Worker_logger.Make (Event) (Request)
-    (struct
-      let worker_name = "node_peer_validator"
-    end)
-
 module Events = Peer_validator_events
-module Worker = Worker.Make (Name) (Event) (Request) (Types) (Logger)
+module Worker = Worker.Make (Name) (Event) (Request) (Types)
 open Types
 
 type t = Worker.dropbox Worker.t

tezos/tezos-mirror

src/lib_shell/block_validator.ml
@@ -118,14 +118,7 @@ module Request = struct
 end
 
 module Events = Block_validator_events
-
-module Logger =
-  Worker_logger.Make (Event) (Request)
-    (struct
-      let worker_name = "node_block_validator"
-    end)
-
-module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types) (Logger)
+module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types)
 
 type t = Worker.infinite Worker.queue Worker.t
 

src/lib_shell/chain_validator.ml
@@ -103,14 +103,8 @@ module Types = struct
       state.synchronisation_state
 end
 
-module Logger =
-  Worker_logger.Make (Event) (Request)
-    (struct
-      let worker_name = "node_chain_validator"
-    end)
-
-module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types) (Logger)
 module Events = Chain_validator_events
+module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types)
 open Types
 
 type t = Worker.infinite Worker.queue Worker.t

src/lib_shell/peer_validator.ml
@@ -88,14 +88,8 @@ module Types = struct
     | Some p -> Bootstrap_pipeline.length p
 end
 
-module Logger =
-  Worker_logger.Make (Event) (Request)
-    (struct
-      let worker_name = "node_peer_validator"
-    end)
-
-module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types) (Logger)
 module Events = Peer_validator_events
+module Worker = Worker.MakeSingle (Name) (Event) (Request) (Types)
 open Types
 
 type t = Worker.dropbox Worker.t