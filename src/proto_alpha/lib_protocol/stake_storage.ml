OKEAMAH/Git

Proto: move `set_active` into `Stake_storage`

src/proto_alpha/lib_protocol/stake_storage.ml
@@ -107,16 +107,21 @@ let add_stake ctxt delegate amount =
        roll now). *)
     return ctxt
 
-let deactivate_only_call_from_delegate_storage ctxt delegate =
+let set_inactive ctxt delegate =
+  Delegate_activation_storage.set_inactive ctxt delegate >>= fun ctxt ->
   Storage.Stake.Active_delegate_with_one_roll.remove ctxt delegate
 
-let activate_only_call_from_delegate_storage ctxt delegate =
-  get_initialized_stake ctxt delegate >>=? fun (staking_balance, ctxt) ->
-  let tokens_per_roll = Constants_storage.tokens_per_roll ctxt in
-  if Tez_repr.(staking_balance >= tokens_per_roll) then
-    Storage.Stake.Active_delegate_with_one_roll.add ctxt delegate ()
-    >>= fun ctxt -> return ctxt
-  else return ctxt
+let set_active ctxt delegate =
+  Delegate_activation_storage.set_active ctxt delegate
+  >>=? fun (ctxt, inactive) ->
+  if not inactive then return ctxt
+  else
+    get_initialized_stake ctxt delegate >>=? fun (staking_balance, ctxt) ->
+    let tokens_per_roll = Constants_storage.tokens_per_roll ctxt in
+    if Tez_repr.(staking_balance >= tokens_per_roll) then
+      Storage.Stake.Active_delegate_with_one_roll.add ctxt delegate ()
+      >>= fun ctxt -> return ctxt
+    else return ctxt
 
 let snapshot ctxt =
   Storage.Stake.Last_snapshot.get ctxt >>=? fun index ->

tezos/tezos-mirror

src/proto_alpha/lib_protocol/stake_storage.ml
@@ -107,16 +107,21 @@ let add_stake ctxt delegate amount =
        would also have it now). *)
     return ctxt
 
-let deactivate_only_call_from_delegate_storage ctxt delegate =
+let set_inactive ctxt delegate =
+  Delegate_activation_storage.set_inactive ctxt delegate >>= fun ctxt ->
   Storage.Stake.Active_delegates_with_minimal_stake.remove ctxt delegate
 
-let activate_only_call_from_delegate_storage ctxt delegate =
-  get_initialized_stake ctxt delegate >>=? fun (staking_balance, ctxt) ->
-  let minimal_stake = Constants_storage.minimal_stake ctxt in
-  if Tez_repr.(staking_balance >= minimal_stake) then
-    Storage.Stake.Active_delegates_with_minimal_stake.add ctxt delegate ()
-    >>= fun ctxt -> return ctxt
-  else return ctxt
+let set_active ctxt delegate =
+  Delegate_activation_storage.set_active ctxt delegate
+  >>=? fun (ctxt, inactive) ->
+  if not inactive then return ctxt
+  else
+    get_initialized_stake ctxt delegate >>=? fun (staking_balance, ctxt) ->
+    let minimal_stake = Constants_storage.minimal_stake ctxt in
+    if Tez_repr.(staking_balance >= minimal_stake) then
+      Storage.Stake.Active_delegates_with_minimal_stake.add ctxt delegate ()
+      >>= fun ctxt -> return ctxt
+    else return ctxt
 
 let snapshot ctxt =
   Storage.Stake.Last_snapshot.get ctxt >>=? fun index ->