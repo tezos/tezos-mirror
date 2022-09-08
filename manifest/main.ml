b2172f9621ed640a75a585a8c386663cf1aaa4a9
Killian Delarue <killian.delarue@nomadic-labs.com>
2022-09-08 11:13:54 +0200

    Manifest: Resto.1.0

--- manifest/main.ml
+++ manifest/main.ml
@@ -293,7 +293,7 @@ let qcheck_core = external_lib "qcheck-core" V.True
 
 let re = external_lib ~js_compatible:true "re" V.(at_least "1.7.2")
 
-let resto_version = V.(at_least "0.10" && less_than "1.0")
+let resto_version = V.(at_least "1.0")
 
 let resto = external_lib ~js_compatible:true "resto" resto_version
 
