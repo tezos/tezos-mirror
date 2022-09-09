ab34cf3fe92b82bd04a20505881d7718e52de2fc
Alain Mebsout <alain.mebsout@functori.com>
2022-09-09 17:38:13 +0200

    Benchmark/012: scope fixes

--- src/proto_012_Psithaca/lib_benchmark/test/test_distribution.ml
+++ src/proto_012_Psithaca/lib_benchmark/test/test_distribution.ml
@@ -1,4 +1,3 @@
-open Tezos_benchmark
 open Michelson_samplers
 open Protocol
 open Internal_for_tests
@@ -47,7 +46,7 @@ module Type_name = struct
   let hash = Stdlib.Hashtbl.hash
 end
 
-module Type_name_hashtbl = Hashtbl.Make (Type_name)
+module Type_name_hashtbl = Stdlib.Hashtbl.Make (Type_name)
 
 let rec tnames_of_type :
     type a. a Script_typed_ir.ty -> type_name list -> type_name list =
