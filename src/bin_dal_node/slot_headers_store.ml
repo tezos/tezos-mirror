7e4551b9376dc4a4e331247a78da9f870eda8e36
Rémy El Sibaïe <remy.el-sibaie@nomadic-labs.com>
2022-09-13 10:15:51 +0200

    Dal/Plugin: add slot headers store

---
+++ src/bin_dal_node/slot_headers_store.ml
@@ -0,0 +1,57 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Protocol.Alpha_context
+include Store_utils
+
+(* Published slot headers per block hash,
+   stored as a list of bindings from `Dal_slot_index.t`
+   to `Dal.Slot.t`. The encoding function converts this
+   list into a `Dal.Slot_index.t`-indexed map. *)
+module Dal_slots = Make_nested_map (struct
+  let path = ["dal"; "slot_headers"]
+
+  (* FIXME/DAL: https://gitlab.com/tezos/tezos/-/issues/3527
+     This value is currently not used, but required by the module type. *)
+  let keep_last_n_entries_in_memory = 10
+
+  type key = Block_hash.t
+
+  let string_of_key = Block_hash.to_b58check
+
+  type secondary_key = Dal.Slot_index.t
+
+  let compare_secondary_keys = Dal.Slot_index.compare
+
+  type value = Dal.Slot.t
+
+  let secondary_key_encoding = Dal.Slot_index.encoding
+
+  let secondary_key_name = "slot_index"
+
+  let value_encoding = Dal.Slot.encoding
+
+  let value_name = "slots_metadata"
+end)
