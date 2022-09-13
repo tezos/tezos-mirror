3a3fd4290c3ac86b3b36c63362b970aa7777a594
Andrea Cerone <andrea.cerone@trili.tech>
2022-09-13 15:43:15 +0100

    Dal/Rollup node: endpoint to retrieve downloaded slot contents

--- src/proto_alpha/lib_sc_rollup/sc_rollup_services.ml
+++ src/proto_alpha/lib_sc_rollup/sc_rollup_services.ml
@@ -207,6 +207,56 @@ module Global = struct
             (req "commitment" Sc_rollup.Commitment.Hash.encoding)
             (req "proof" hex_string))
       (prefix / "proofs" / "outbox")
+
+  let dal_slot_pages () =
+    RPC_service.get_service
+      ~description:
+        "Data availability downloaded slot pages for a given block hash"
+      ~query:RPC_query.empty
+      ~output:
+        (* DAL/FIXME: https://gitlab.com/tezos/tezos/-/issues/3873
+             Estimate size of binary encoding and add a check_size to the
+           encoding. *)
+        Data_encoding.(
+          list
+          @@ obj2
+               (req "index" Dal.Slot_index.encoding)
+               (req "contents" (list @@ option Dal.Page.content_encoding)))
+      (prefix / "dal" / "slot_pages")
+
+  type dal_slot_page_query = {index : Dal.Slot_index.t; page : int}
+
+  let dal_slot_page_query =
+    let open RPC_query in
+    let req name f = function
+      | None ->
+          raise (Invalid (Format.sprintf "Query parameter %s is required" name))
+      | Some arg -> f arg
+    in
+    let invalid_parameter i =
+      raise (Invalid (Format.asprintf "Invalid parameter (%d)" i))
+    in
+    query (fun raw_index raw_page ->
+        let index = req "index" Dal.Slot_index.of_int raw_index in
+        let page = req "page" (fun p -> p) raw_page in
+        match index with
+        | None -> invalid_parameter @@ Option.value ~default:0 raw_index
+        | Some index ->
+            if page < 0 then invalid_parameter page else {index; page})
+    |+ opt_field "index" RPC_arg.int (fun q ->
+           Some (Dal.Slot_index.to_int q.index))
+    |+ opt_field "slot_page" RPC_arg.int (fun q -> Some q.page)
+    |> seal
+
+  let dal_slot_page () =
+    RPC_service.get_service
+      ~description:
+        "Data availability downloaded slot pages for a given block hash"
+      ~query:dal_slot_page_query
+      ~output:
+        Data_encoding.(
+          obj2 (req "result" string) (opt "contents" Dal.Page.content_encoding))
+      (prefix / "dal" / "slot_page")
 end
 
 module Local = struct
