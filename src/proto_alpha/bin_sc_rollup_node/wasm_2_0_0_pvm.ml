bef3e327ebb190795889007838b951df8c262a95
Ole Krüger <ole.kruger@trili.tech>
2022-09-22 16:35:22 +0100

    SCORU: WASM: Establish environment equalities to avoid conversion

--- src/proto_alpha/bin_sc_rollup_node/wasm_2_0_0_pvm.ml
+++ src/proto_alpha/bin_sc_rollup_node/wasm_2_0_0_pvm.ml
@@ -44,63 +44,6 @@ module Wasm_2_0_0_proof_format =
         .tree_proof_encoding
     end)
 
-module Conversions = struct
-  (* BEWARE OF UGLY HACK !!!
-     Environment.Wasm_2_0_0.info/input/output are not compatible with
-      Tezos_scoru_wasm.Wasm_pvm_sig.info/input_info/output_info/
-      although they should be, by their definitions:
-
-      ```
-      type input = Tezos_scoru_wasm.Wasm_pvm_sig.input_info = ...
-      type output = Tezos_scoru_wasm.Wasm_pvm_sig.input_info = ...
-      ```
-
-      Somehow the type equality is not preserved through the protocol environment.
-
-      This is why we need to do the de/reconstruction manualy.
-  *)
-  module EInt32 = Environment.Bounded.Non_negative_int32
-  module SInt32 = Bounded.Non_negative_int32
-  module Opt = Stdlib.Option
-  module Scoru = Tezos_scoru_wasm.Wasm_pvm_sig
-  module Env = Environment.Wasm_2_0_0
-
-  let from_env_int32 (i : EInt32.t) : SInt32.t =
-    EInt32.to_value i |> SInt32.of_value |> Opt.get
-
-  let to_env_int32 (i : SInt32.t) : EInt32.t =
-    SInt32.to_value i |> EInt32.of_value |> Opt.get
-
-  let to_env_input (input : Scoru.input_info) : Env.input =
-    {
-      inbox_level = to_env_int32 input.inbox_level;
-      message_counter = input.message_counter;
-    }
-
-  let to_env_input_request : Scoru.input_request -> Env.input_request = function
-    | Scoru.No_input_required -> Env.No_input_required
-    | Input_required -> Input_required
-
-  let of_env_input (input : Env.input) : Scoru.input_info =
-    {
-      inbox_level = from_env_int32 input.inbox_level;
-      message_counter = input.message_counter;
-    }
-
-  let of_env_output (output : Env.output) : Scoru.output_info =
-    {
-      outbox_level = from_env_int32 output.outbox_level;
-      message_index = output.message_index;
-    }
-
-  let to_env_info (s_info : Scoru.info) : Env.info =
-    {
-      current_tick = s_info.current_tick;
-      last_input_read = Opt.map to_env_input s_info.last_input_read;
-      input_request = to_env_input_request s_info.input_request;
-    }
-end
-
 module type TreeS =
   Tezos_context_sigs.Context.TREE
     with type key = string list
@@ -109,7 +52,7 @@ module type TreeS =
 module Make_backend (Tree : TreeS) = struct
   type Lazy_containers.Lazy_map.tree += PVM_tree of Tree.tree
 
-  module Wasm_pvm = Tezos_scoru_wasm.Wasm_pvm.Make (struct
+  include Tezos_scoru_wasm.Wasm_pvm.Make (struct
     include Tree
 
     let select = function
@@ -118,20 +61,6 @@ module Make_backend (Tree : TreeS) = struct
 
     let wrap t = PVM_tree t
   end)
-
-  let compute_step (tree : Tree.tree) = Wasm_pvm.compute_step tree
-
-  include Conversions
-
-  let set_input_step (input : Environment.Wasm_2_0_0.input) payload
-      (tree : Tree.tree) =
-    Wasm_pvm.set_input_step (of_env_input input) payload tree
-
-  let get_output (output : Environment.Wasm_2_0_0.output) (tree : Tree.tree) =
-    Wasm_pvm.get_output (of_env_output output) tree
-
-  let get_info (tree : Tree.tree) =
-    Lwt.map to_env_info @@ Wasm_pvm.get_info tree
 end
 
 module Impl : Pvm.S = struct
