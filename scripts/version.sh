479838198e059ccd3f36c16412c1222bc2986011
Victor Allombert <victor.allombert@nomadic-labs.com>
2022-10-12 09:31:00 +0200

    Build: update opam deps

--- scripts/version.sh
+++ scripts/version.sh
@@ -20,12 +20,12 @@ export recommended_node_version=14.12.0
 
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-export full_opam_repository_tag=5b15e5ee8adbd7522032018e1694f547555ba2b6
+export full_opam_repository_tag=efe0e53484b1a5a3bde1d762dca7277d0f326ba2
 
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab/ci/templates.ym
 export opam_repository_url=https://gitlab.com/tezos/opam-repository
-export opam_repository_tag=df8d0aa9d9a08a49cf97f27e58662c10fb3f70f2
+export opam_repository_tag=d97ef23a0dadf1a8133e76d3e4cc175dfc9ee8ec
 export opam_repository_git=$opam_repository_url.git
 export opam_repository=$opam_repository_git\#$opam_repository_tag
 
