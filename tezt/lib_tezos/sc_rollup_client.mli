327c6e83a31d63a5740dec4e60a7255edc3e0cac
Alain Mebsout <alain.mebsout@functori.com>
2022-10-14 14:50:04 +0200

    Tezt: test message status

--- tezt/lib_tezos/sc_rollup_client.mli
+++ tezt/lib_tezos/sc_rollup_client.mli
@@ -196,9 +196,10 @@ val batcher_queue :
   ?hooks:Process_hooks.t -> t -> (string * string) list Runnable.process
 
 (** [get_batcher_msg client hash] fetches the message whose hash is [hash] from
-    the queue. It fails if the message is not present. *)
+    the queue. It returns the message together with the full JSON response
+    including the status. *)
 val get_batcher_msg :
-  ?hooks:Process_hooks.t -> t -> string -> string Runnable.process
+  ?hooks:Process_hooks.t -> t -> string -> (string * JSON.t) Runnable.process
 
 (** [generate_keys ~alias client] generates new unencrypted keys for [alias]. *)
 val generate_keys :
