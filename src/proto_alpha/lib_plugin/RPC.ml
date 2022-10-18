aa14c33b6ee0d8397842460fcbf4540df4e7347b
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2022-10-18 16:28:28 +0200

    RPC/Delegate: validators computation takes advantage of caching

--- src/proto_alpha/lib_plugin/RPC.ml
+++ src/proto_alpha/lib_plugin/RPC.ml
@@ -3243,21 +3243,25 @@ module Validators = struct
         path
   end
 
-  let endorsing_slots_at_level ctxt level =
-    Baking.endorsing_rights ctxt level >|=? fun (_, rights) ->
-    Signature.Public_key_hash.Map.fold
-      (fun _pkh {Baking.delegate; consensus_key; slots} acc ->
-        {level = level.level; delegate; consensus_key; slots} :: acc)
-      rights
-      []
+  let add_endorsing_slots_at_level (ctxt, acc) level =
+    Baking.endorsing_rights ctxt level >|=? fun (ctxt, rights) ->
+    ( ctxt,
+      Signature.Public_key_hash.Map.fold
+        (fun _pkh {Baking.delegate; consensus_key; slots} acc ->
+          {level = level.level; delegate; consensus_key; slots} :: acc)
+        rights
+        acc )
 
   let register () =
     Registration.register0 ~chunked:true S.validators (fun ctxt q () ->
         let levels =
           requested_levels ~default_level:(Level.current ctxt) ctxt [] q.levels
         in
-        List.concat_map_es (endorsing_slots_at_level ctxt) levels
-        >|=? fun rights ->
+        List.fold_left_es
+          add_endorsing_slots_at_level
+          (ctxt, [])
+          (List.rev levels)
+        >|=? fun (_ctxt, rights) ->
         let rights =
           match q.delegates with
           | [] -> rights
