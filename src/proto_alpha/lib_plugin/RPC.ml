a6d5cd3f31bfe5e29dc347449a5cd89ae8259575
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2022-10-18 15:43:59 +0200

    RPC/Delegate: endorsing_rights computation takes advantage of caching

--- src/proto_alpha/lib_plugin/RPC.ml
+++ src/proto_alpha/lib_plugin/RPC.ml
@@ -3137,7 +3137,9 @@ module Endorsing_rights = struct
         rights
         []
     in
-    return {level = level.level; delegates_rights = rights; estimated_time}
+    (* returns the ctxt with an updated cache of slot holders *)
+    return
+      (ctxt, {level = level.level; delegates_rights = rights; estimated_time})
 
   let register () =
     Registration.register0 ~chunked:true S.endorsing_rights (fun ctxt q () ->
@@ -3149,8 +3151,8 @@ module Endorsing_rights = struct
             cycles
             q.levels
         in
-        List.map_es (endorsing_rights_at_level ctxt) levels
-        >|=? fun rights_per_level ->
+        List.fold_left_map_es endorsing_rights_at_level ctxt levels
+        >|=? fun (_ctxt, rights_per_level) ->
         let rights_per_level =
           match q.consensus_keys with
           | [] -> rights_per_level
