77d58cd5682a9da7e837b7a365137f4717b0b106
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2022-11-10 16:56:47 +0100

    WASM/REPL: main loop with parsing and typechecking as prelude

--- src/proto_alpha/bin_wasm_repl/main_wasm_repl_alpha.ml
+++ src/proto_alpha/bin_wasm_repl/main_wasm_repl_alpha.ml
@@ -23,8 +23,119 @@
 (*                                                                           *)
 (*****************************************************************************)
 
+open Wasm_utils
+
+(* [parse_binary_module module_name module_stream] parses a binary encoded
+   module. Parsing outside of the PVM allows locations in case of errors. *)
+let parse_binary_module name module_ =
+  let bytes = Tezos_lazy_containers.Chunked_byte_vector.of_string module_ in
+  Tezos_webassembly_interpreter.Decode.decode ~name ~bytes
+
+(* [typecheck_module module_ast] runs the typechecker on the module, which is
+   not done by the PVM. *)
+let typecheck_module module_ =
+  Repl_helpers.trap_exn (fun () ->
+      Tezos_webassembly_interpreter.Valid.check_module module_)
+
+(* [import_pvm_host_functions ()] registers the host functions of the PVM. *)
+let import_pvm_host_functions () =
+  let lookup name = Lwt.return (Tezos_scoru_wasm.Host_funcs.lookup name) in
+  Repl_helpers.trap_exn (fun () ->
+      Lwt.return
+        (Tezos_webassembly_interpreter.Import.register
+           ~module_name:"rollup_safe_core"
+           lookup))
+
+(* [link module_ast] checks a module actually uses the host functions with their
+   correct type, outside of the PVM. *)
+let link module_ =
+  Repl_helpers.trap_exn (fun () ->
+      Tezos_webassembly_interpreter.Import.link module_)
+
+(* Starting point of the module after reading the kernel file: parsing,
+   typechecking and linking for safety before feeding kernel to the PVM, then
+   installation into a tree for the PVM interpreter. *)
+let handle_module binary name module_ =
+  let open Lwt_result_syntax in
+  let* ast =
+    Repl_helpers.trap_exn (fun () ->
+        if binary then parse_binary_module name module_
+        else Lwt.return (parse_module module_))
+  in
+  let* () = typecheck_module ast in
+  let* () = import_pvm_host_functions () in
+  let* _ = link ast in
+  let*! tree = initial_tree ~from_binary:binary module_ in
+  let*! tree = eval_until_input_requested tree in
+  return tree
+
+let start binary file =
+  let open Lwt_result_syntax in
+  let module_name = Filename.(file |> basename |> chop_extension) in
+  let*! buffer = Repl_helpers.read_file file in
+  handle_module binary module_name buffer
+
+(* REPL main loop: reads an input, does something out of it, then loops. *)
+let repl tree inboxes level =
+  let open Lwt_result_syntax in
+  let rec loop tree inboxes level =
+    let*! () = Lwt_io.printf "> " in
+    let* input =
+      Lwt.catch
+        (fun () ->
+          let*! i = Lwt_io.read_line Lwt_io.stdin in
+          return_some i)
+        (fun _ -> return_none)
+    in
+    match input with
+    | Some command ->
+        (* TODO: actually do the command *)
+        loop tree inboxes level
+    | None -> return tree
+  in
+  loop tree (List.to_seq inboxes) level
+
+let file_parameter =
+  Tezos_clic.parameter (fun _ filename ->
+      Repl_helpers.(trap_exn (fun () -> read_file filename)))
+
+let wasm_param =
+  let open Lwt_result_syntax in
+  Tezos_clic.(
+    param
+      ~name:"module"
+      ~desc:"wasm or wast file"
+      (parameter (fun _ filename -> return filename)))
+
+let input_arg =
+  let open Tezos_clic in
+  arg ~doc:"input file" ~long:"inputs" ~placeholder:"inputs.json" file_parameter
+
+let main_command =
+  let open Tezos_clic in
+  let open Lwt_result_syntax in
+  command
+    ~desc:"Start the eval loop"
+    (args1 input_arg)
+    (wasm_param @@ stop)
+    (fun inputs wasm_file () ->
+      let config = Config.config () in
+      let*? binary =
+        if Filename.check_suffix wasm_file ".wasm" then Ok true
+        else if Filename.check_suffix wasm_file ".wast" then Ok false
+        else error_with "Kernels should have .wasm or .wast file extension"
+      in
+      let* tree = start binary wasm_file in
+      let* inboxes =
+        match inputs with
+        | Some inputs -> Messages.parse_inboxes inputs config
+        | None -> return []
+      in
+      let+ _tree = repl tree inboxes 0l in
+      ())
+
 (* List of program commands *)
-let commands = []
+let commands = [main_command]
 
 let () =
   ignore
