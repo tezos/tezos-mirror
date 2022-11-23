06f0ddc5d11e80b57b5b21717e43a377eabd0545
Killian Delarue <killian.delarue@nomadic-labs.com>
2022-11-23 16:17:11 +0100

    Client: Add sc_rollup_node to init-sandboxed-client

--- src/bin_client/octez-init-sandboxed-client.sh
+++ src/bin_client/octez-init-sandboxed-client.sh
@@ -19,12 +19,14 @@ init_sandboxed_client() {
         admin_client="$local_admin_client -base-dir $client_dir -endpoint https://$host:$rpc"
         alpha_baker="$local_alpha_baker -base-dir $client_dir -endpoint https://$host:$rpc"
         alpha_accuser="$local_alpha_accuser -base-dir $client_dir -endpoint https://$host:$rpc"
+        alpha_sc_rollup_node="$local_alpha_sc_rollup_node -base-dir $client_dir -endpoint https://$host:$rpc"
         compiler="$local_compiler"
     else
         client="$local_client -base-dir $client_dir -endpoint http://$host:$rpc"
         admin_client="$local_admin_client -base-dir $client_dir -endpoint http://$host:$rpc"
         alpha_baker="$local_alpha_baker -base-dir $client_dir -endpoint http://$host:$rpc"
         alpha_accuser="$local_alpha_accuser -base-dir $client_dir -endpoint http://$host:$rpc"
+        alpha_sc_rollup_node="$local_alpha_sc_rollup_node -base-dir $client_dir -endpoint http://$host:$rpc"
         compiler="$local_compiler"
     fi
 }
@@ -160,18 +162,25 @@ main () {
         protocol_underscore=$(echo $protocol | tr -- - _)
         local_baker="$bin_dir/../../_build/default/src/proto_$protocol_underscore/bin_baker/main_baker_$protocol_underscore.exe"
         local_accuser="$bin_dir/../../_build/default/src/proto_$protocol_underscore/bin_accuser/main_accuser_$protocol_underscore.exe"
+        local_sc_rollup_node="$bin_dir/../../_build/default/src/proto_$protocol_underscore/bin_sc_rollup_node/main_sc_rollup_node_$protocol_underscore.exe"
 
         if [ -n "$USE_TLS" ]; then
             baker="$local_baker -base-dir $client_dir -endpoint https://$host:$rpc"
             accuser="$local_accuser -base-dir $client_dir -endpoint https://$host:$rpc"
+            sc_rollup_node="$local_sc_rollup_node -base-dir $client_dir -endpoint https://$host:$rpc"
         else
             baker="$local_baker -base-dir $client_dir -endpoint http://$host:$rpc"
             accuser="$local_accuser -base-dir $client_dir -endpoint http://$host:$rpc"
+            sc_rollup_node="$local_sc_rollup_node -base-dir $client_dir -endpoint http://$host:$rpc"
         fi
 
         echo '#!/bin/sh' > $client_dir/bin/octez-baker-$protocol
         echo "exec $baker \"\$@\""  >> $client_dir/bin/octez-baker-$protocol
         chmod +x $client_dir/bin/octez-baker-$protocol
+
+        echo '#!/bin/sh' > $client_dir/bin/octez-sc-rollup-node-$protocol
+        echo "exec $sc_rollup_node \"\$@\""  >> $client_dir/bin/octez-sc-rollup-node-$protocol
+        chmod +x $client_dir/bin/octez-sc-rollup-node-$protocol
 
         echo '#!/bin/sh' > $client_dir/bin/octez-accuser-$protocol
         echo "exec $accuser \"\$@\""  >> $client_dir/bin/octez-accuser-$protocol
