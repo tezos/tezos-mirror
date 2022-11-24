06fef33b2a31cfd2a134a08fb4cc57a3abaaf27d
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2022-11-24 10:41:23 +0100

    WASM/PVM: rename Output_buffer names to help reviews

--- src/lib_webassembly/runtime/output_buffer.ml
+++ src/lib_webassembly/runtime/output_buffer.ml
@@ -11,49 +11,50 @@ exception Invalid_level
 
 exception Invalid_id
 
-module Index_Vector = Lazy_vector.Mutable.ZVector
-module Level_Vector = Lazy_vector.Mutable.Int32Vector
+module Messages_vector = Lazy_vector.Mutable.ZVector
+module Outboxes_vector = Lazy_vector.Mutable.Int32Vector
 
-type t = bytes Index_Vector.t Level_Vector.t
+type t = bytes Messages_vector.t Outboxes_vector.t
 
-let get_level output = Int32.pred (Level_Vector.num_elements output)
+let get_level outboxes = Int32.(pred (Outboxes_vector.num_elements outboxes))
 
-let get_id output =
+let get_id outboxes =
   let open Lwt.Syntax in
-  let level = get_level output in
+  let level = get_level outboxes in
   if level = -1l then raise Empty_output
   else
-    let* last = Level_Vector.(get level output) in
-    Lwt.return @@ (level, Z.pred (Index_Vector.num_elements last))
+    let* last = Outboxes_vector.(get level outboxes) in
+    Lwt.return @@ (level, Z.pred (Messages_vector.num_elements last))
 
-let set_level output level =
-  Level_Vector.grow
-    ~default:(fun () -> Index_Vector.create Z.zero)
-    (Int32.sub level (get_level output))
-    output
+let set_level outboxes level =
+  Outboxes_vector.grow
+    ~default:(fun () -> Messages_vector.create Z.zero)
+    (Int32.sub level (get_level outboxes))
+    outboxes
 
 let index_create a =
-  let init = Index_Vector.create Z.zero in
-  Index_Vector.cons a init ;
+  let init = Messages_vector.create Z.zero in
+  Messages_vector.cons a init ;
   init
 
-let set_value output a =
+let set_value outboxes a =
   let open Lwt.Syntax in
-  let* level, index = get_id output in
-  if level = -1l then Lwt.return @@ Level_Vector.cons (index_create a) output
+  let* level, index = get_id outboxes in
+  if level = -1l then
+    Lwt.return @@ Outboxes_vector.cons (index_create a) outboxes
   else if Z.to_int32 index >= Int32.max_int then raise Id_too_large
   else
-    let* last = Level_Vector.get level output in
-    Lwt.return @@ Index_Vector.grow ~default:(fun () -> a) Z.one last
+    let* last = Outboxes_vector.get level outboxes in
+    Lwt.return @@ Messages_vector.grow ~default:(fun () -> a) Z.one last
 
-let get output level index =
-  let max_level = get_level output in
+let get outboxes level index =
+  let max_level = get_level outboxes in
   if level > max_level || level < 0l then raise Invalid_level
   else
     let open Lwt.Syntax in
-    let* index_vector = Level_Vector.get level output in
-    let max_index = Z.pred @@ Index_Vector.num_elements index_vector in
+    let* index_vector = Outboxes_vector.get level outboxes in
+    let max_index = Z.pred @@ Messages_vector.num_elements index_vector in
     if index > max_index || index < Z.zero then raise Invalid_id
-    else Index_Vector.get index index_vector
+    else Messages_vector.get index index_vector
 
-let alloc () = Level_Vector.create 0l
+let alloc () = Outboxes_vector.create 0l
