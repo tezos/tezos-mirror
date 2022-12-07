4ca011539fa2a094356b1c7efbafeaf296b6b678
Ole Krüger <ole.kruger@trili.tech>
2022-12-07 17:17:48 +0000

    Fix OCaml formatting

--- src/proto_016_PtMumbai/lib_protocol/test/integration/operations/test_tx_rollup.ml
+++ src/proto_016_PtMumbai/lib_protocol/test/integration/operations/test_tx_rollup.ml
@@ -1975,7 +1975,8 @@ let test_full_inbox () =
       baking_reward_fixed_portion = Tez.zero;
       tx_rollup =
         {
-          Tezos_protocol_016_PtMumbai_parameters.Default_parameters.constants_test
+          Tezos_protocol_016_PtMumbai_parameters.Default_parameters
+          .constants_test
             .tx_rollup
           with
           enable = true;
