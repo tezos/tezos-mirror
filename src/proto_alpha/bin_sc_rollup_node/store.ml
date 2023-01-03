fba95e94855a76343fb6454c224071b83e5c8313
Alain Mebsout <alain.mebsout@functori.com>
2023-01-03 19:06:03 +0100

    SCORU/Node: Store uses indexed files
    DAL data remains in an irmin store for now.

--- src/proto_alpha/bin_sc_rollup_node/store.ml
+++ src/proto_alpha/bin_sc_rollup_node/store.ml
@@ -30,123 +30,124 @@ include Store_utils
 (** Aggregated collection of messages from the L1 inbox *)
 open Alpha_context
 
-module IStore = Irmin_store.Make (struct
-  let name = "Tezos smart rollup node"
+module Irmin_store = struct
+  module IStore = Irmin_store.Make (struct
+    let name = "Tezos smart rollup node"
+  end)
+
+  include IStore
+  include Store_utils.Make (IStore)
+end
+
+module Empty_header = struct
+  type t = unit
+
+  let name = "empty"
+
+  let encoding = Data_encoding.unit
+
+  let fixed_size = 0
+end
+
+module Add_empty_header = struct
+  module Header = Empty_header
+
+  let header _ = ()
+end
+
+module Make_hash_index_key (H : Environment.S.HASH) =
+Indexed_store.Make_index_key (struct
+  include Indexed_store.Make_fixed_encodable (H)
+
+  let equal = H.equal
 end)
-
-include Store_utils.Make (IStore)
-
-type 'a store = 'a IStore.t
-
-type 'a t = ([< `Read | `Write > `Read] as 'a) store
-
-type rw = Store_sigs.rw t
-
-type ro = Store_sigs.ro t
-
-let close = IStore.close
-
-let load = IStore.load
-
-let readonly = IStore.readonly
 
 (** L2 blocks *)
 module L2_blocks =
-  Make_append_only_map
+  Indexed_store.Make_indexed_file
     (struct
-      let path = ["state_info"]
+      let name = "l2_blocks"
     end)
+    (Tezos_store_shared.Block_key)
     (struct
-      type key = Block_hash.t
+      type t = (unit, unit) Sc_rollup_block.block
 
-      let to_path_representation = Block_hash.to_b58check
-    end)
-    (struct
-      type value = Sc_rollup_block.t
+      let name = "sc_rollup_block_info"
 
-      let name = "sc_rollup_block"
+      let encoding =
+        Sc_rollup_block.block_encoding Data_encoding.unit Data_encoding.unit
 
-      let encoding = Sc_rollup_block.encoding
+      module Header = struct
+        type t = Sc_rollup_block.header
+
+        let name = "sc_rollup_block_header"
+
+        let encoding = Sc_rollup_block.header_encoding
+
+        let fixed_size = Sc_rollup_block.header_size
+      end
     end)
 
 (** Unaggregated messages per block *)
-module Messages = struct
-  type info = {
-    predecessor : Block_hash.t;
-    predecessor_timestamp : Timestamp.t;
-    messages : Sc_rollup.Inbox_message.t list;
-  }
+module Messages =
+  Indexed_store.Make_indexed_file
+    (struct
+      let name = "messages"
+    end)
+    (Make_hash_index_key (Sc_rollup.Inbox_merkelized_payload_hashes.Hash))
+    (struct
+      type t = Sc_rollup.Inbox_message.t list
 
-  let encoding =
-    let open Data_encoding in
-    conv
-      (fun {predecessor; predecessor_timestamp; messages} ->
-        (predecessor, predecessor_timestamp, messages))
-      (fun (predecessor, predecessor_timestamp, messages) ->
-        {predecessor; predecessor_timestamp; messages})
-    @@ obj3
-         (req "predecessor" Block_hash.encoding)
-         (req "predecessor_timestamp" Timestamp.encoding)
-         (req
-            "messages"
-            (list @@ dynamic_size Sc_rollup.Inbox_message.encoding))
+      let name = "messages_list"
 
-  include
-    Make_append_only_map
-      (struct
-        let path = ["messages"]
-      end)
-      (struct
-        type key = Sc_rollup.Inbox_merkelized_payload_hashes.Hash.t
+      let encoding =
+        Data_encoding.(list @@ dynamic_size Sc_rollup.Inbox_message.encoding)
 
-        let to_path_representation =
-          Sc_rollup.Inbox_merkelized_payload_hashes.Hash.to_b58check
-      end)
-      (struct
-        type value = info
+      module Header = struct
+        type t = Block_hash.t * Timestamp.t
 
-        let name = "messages"
+        let name = "messages_inbox_info"
 
-        let encoding = encoding
-      end)
-end
+        let encoding =
+          let open Data_encoding in
+          obj2
+            (req "predecessor" Block_hash.encoding)
+            (req "predecessor_timestamp" Timestamp.encoding)
+
+        let fixed_size =
+          WithExceptions.Option.get ~loc:__LOC__
+          @@ Data_encoding.Binary.fixed_length encoding
+      end
+    end)
 
 (** Inbox state for each block *)
 module Inboxes =
-  Make_append_only_map
+  Indexed_store.Make_simple_indexed_file
     (struct
-      let path = ["inboxes"]
+      let name = "inboxes"
     end)
+    (Make_hash_index_key (Sc_rollup.Inbox.Hash))
     (struct
-      type key = Sc_rollup.Inbox.Hash.t
-
-      let to_path_representation = Sc_rollup.Inbox.Hash.to_b58check
-    end)
-    (struct
-      type value = Sc_rollup.Inbox.t
+      type t = Sc_rollup.Inbox.t
 
       let name = "inbox"
 
       let encoding = Sc_rollup.Inbox.encoding
+
+      include Add_empty_header
     end)
 
 module Commitments =
-  Make_append_only_map
+  Indexed_store.Make_indexable
     (struct
-      let path = ["commitments"; "computed"]
+      let name = "commitments"
     end)
-    (struct
-      type key = Sc_rollup.Commitment.Hash.t
-
-      let to_path_representation = Sc_rollup.Commitment.Hash.to_b58check
-    end)
-    (struct
-      type value = Sc_rollup.Commitment.t
+    (Make_hash_index_key (Sc_rollup.Commitment.Hash))
+    (Indexed_store.Make_index_value (Indexed_store.Make_fixed_encodable (struct
+      include Sc_rollup.Commitment
 
       let name = "commitment"
-
-      let encoding = Sc_rollup.Commitment.encoding
-    end)
+    end)))
 
 module Commitments_published_at_level = struct
   type element = {
@@ -156,6 +157,12 @@ module Commitments_published_at_level = struct
 
   let element_encoding =
     let open Data_encoding in
+    let opt_level_encoding =
+      conv
+        (function None -> -1l | Some l -> Raw_level.to_int32 l)
+        (fun l -> if l = -1l then None else Some (Raw_level.of_int32_exn l))
+        Data_encoding.int32
+    in
     conv
       (fun {first_published_at_level; published_at_level} ->
         (first_published_at_level, published_at_level))
@@ -163,82 +170,64 @@ module Commitments_published_at_level = struct
         {first_published_at_level; published_at_level})
     @@ obj2
          (req "first_published_at_level" Raw_level.encoding)
-         (opt "published_at_level" Raw_level.encoding)
+         (req "published_at_level" opt_level_encoding)
 
   include
-    Make_updatable_map
+    Indexed_store.Make_indexable
       (struct
-        let path = ["commitments"; "published_at_level"]
+        let name = "commitments"
       end)
-      (struct
-        type key = Sc_rollup.Commitment.Hash.t
-
-        let to_path_representation = Sc_rollup.Commitment.Hash.to_b58check
-      end)
-      (struct
-        type value = element
+      (Make_hash_index_key (Sc_rollup.Commitment.Hash))
+      (Indexed_store.Make_index_value (Indexed_store.Make_fixed_encodable (struct
+        type t = element
 
         let name = "published_levels"
 
         let encoding = element_encoding
-      end)
+      end)))
 end
 
-(* TODO: https://gitlab.com/tezos/tezos/-/issues/4392
-   Use file. *)
-module L2_head =
-  Make_mutable_value
-    (struct
-      let path = ["l2_head"]
-    end)
-    (struct
-      type value = Sc_rollup_block.t
+module L2_head = Indexed_store.Make_singleton (struct
+  type t = Sc_rollup_block.t
 
-      let name = "l2_block"
+  let name = "l2_head"
 
-      let encoding = Sc_rollup_block.encoding
-    end)
+  let encoding = Sc_rollup_block.encoding
+end)
 
-(* TODO: https://gitlab.com/tezos/tezos/-/issues/4392
-   Use file. *)
-module Last_finalized_head =
-  Make_mutable_value
-    (struct
-      let path = ["finalized_head"]
-    end)
-    (struct
-      type value = Sc_rollup_block.t
+module Last_finalized_head = Indexed_store.Make_singleton (struct
+  type t = Sc_rollup_block.t
 
-      let name = "l2_block"
+  let name = "finalized_head"
 
-      let encoding = Sc_rollup_block.encoding
-    end)
+  let encoding = Sc_rollup_block.encoding
+end)
 
 (** Table from L1 levels to blocks hashes. *)
 module Levels_to_hashes =
-  Make_updatable_map
+  Indexed_store.Make_indexable
     (struct
-      let path = ["tezos"; "levels"]
+      let name = "tezos_levels"
     end)
-    (struct
-      type key = int32
+    (Indexed_store.Make_index_key (struct
+      type t = int32
 
-      let to_path_representation = Int32.to_string
-    end)
-    (struct
-      type value = Block_hash.t
+      let encoding = Data_encoding.int32
 
-      let name = "block_hash"
+      let name = "level"
 
-      let encoding = Block_hash.encoding
-    end)
+      let fixed_size = 4
+
+      let equal = Int32.equal
+    end))
+    (Tezos_store_shared.Block_key)
 
 (* Published slot headers per block hash,
    stored as a list of bindings from `Dal_slot_index.t`
    to `Dal.Slot.t`. The encoding function converts this
    list into a `Dal.Slot_index.t`-indexed map. *)
 module Dal_slot_pages =
-  Make_nested_map
+  Irmin_store.Make_nested_map
     (struct
       let path = ["dal"; "slot_pages"]
     end)
@@ -270,7 +259,7 @@ module Dal_slot_pages =
 (** stores slots whose data have been considered and pages stored to disk (if
     they are confirmed). *)
 module Dal_processed_slots =
-  Make_nested_map
+  Irmin_store.Make_nested_map
     (struct
       let path = ["dal"; "processed_slots"]
     end)
@@ -312,7 +301,7 @@ module Dal_processed_slots =
     end)
 
 module Dal_slots_headers =
-  Make_nested_map
+  Irmin_store.Make_nested_map
     (struct
       let path = ["dal"; "slot_headers"]
     end)
@@ -348,7 +337,7 @@ module Dal_slots_headers =
 (** Confirmed DAL slots history. See documentation of
     {Dal_slot_repr.Slots_history} for more details. *)
 module Dal_confirmed_slots_history =
-  Make_append_only_map
+  Irmin_store.Make_append_only_map
     (struct
       let path = ["dal"; "confirmed_slots_history"]
     end)
@@ -370,7 +359,7 @@ module Dal_confirmed_slots_history =
 module Dal_confirmed_slots_histories =
   (* TODO: https://gitlab.com/tezos/tezos/-/issues/4390
      Store single history points in map instead of whole history. *)
-    Make_append_only_map
+    Irmin_store.Make_append_only_map
       (struct
         let path = ["dal"; "confirmed_slots_histories_cache"]
       end)
@@ -386,3 +375,105 @@ module Dal_confirmed_slots_histories =
 
       let encoding = Dal.Slots_history.History_cache.encoding
     end)
+
+type 'a store = {
+  l2_blocks : 'a L2_blocks.t;
+  messages : 'a Messages.t;
+  inboxes : 'a Inboxes.t;
+  commitments : 'a Commitments.t;
+  commitments_published_at_level : 'a Commitments_published_at_level.t;
+  l2_head : 'a L2_head.t;
+  last_finalized_head : 'a Last_finalized_head.t;
+  levels_to_hashes : 'a Levels_to_hashes.t;
+  irmin_store : 'a Irmin_store.t;
+}
+
+type 'a t = ([< `Read | `Write > `Read] as 'a) store
+
+type rw = Store_sigs.rw t
+
+type ro = Store_sigs.ro t
+
+let readonly
+    ({
+       l2_blocks;
+       messages;
+       inboxes;
+       commitments;
+       commitments_published_at_level;
+       l2_head;
+       last_finalized_head;
+       levels_to_hashes;
+       irmin_store;
+     } :
+      _ t) : ro =
+  {
+    l2_blocks = L2_blocks.readonly l2_blocks;
+    messages = Messages.readonly messages;
+    inboxes = Inboxes.readonly inboxes;
+    commitments = Commitments.readonly commitments;
+    commitments_published_at_level =
+      Commitments_published_at_level.readonly commitments_published_at_level;
+    l2_head = L2_head.readonly l2_head;
+    last_finalized_head = Last_finalized_head.readonly last_finalized_head;
+    levels_to_hashes = Levels_to_hashes.readonly levels_to_hashes;
+    irmin_store = Irmin_store.readonly irmin_store;
+  }
+
+let close
+    ({
+       l2_blocks;
+       messages;
+       inboxes;
+       commitments;
+       commitments_published_at_level;
+       l2_head = _;
+       last_finalized_head = _;
+       levels_to_hashes;
+       irmin_store;
+     } :
+      _ t) =
+  let open Lwt_result_syntax in
+  let+ () = L2_blocks.close l2_blocks
+  and+ () = Messages.close messages
+  and+ () = Inboxes.close inboxes
+  and+ () = Commitments.close commitments
+  and+ () = Commitments_published_at_level.close commitments_published_at_level
+  and+ () = Levels_to_hashes.close levels_to_hashes
+  and+ () = Irmin_store.close irmin_store |> Lwt_result.ok in
+  ()
+
+let load (type a) (mode : a mode) data_dir : a store tzresult Lwt.t =
+  let open Lwt_result_syntax in
+  let path name = Filename.concat data_dir name in
+  let cache_size = 10_000 in
+  let* l2_blocks = L2_blocks.load mode ~path:(path "l2_blocks") ~cache_size in
+  let* messages = Messages.load mode ~path:(path "messages") ~cache_size in
+  let* inboxes = Inboxes.load mode ~path:(path "inboxes") ~cache_size in
+  let* commitments = Commitments.load mode ~path:(path "commitments") in
+  let* commitments_published_at_level =
+    Commitments_published_at_level.load
+      mode
+      ~path:(path "commitments_published_at_level")
+  in
+  let* l2_head = L2_head.load mode ~path:(path "l2_head") in
+  let* last_finalized_head =
+    Last_finalized_head.load mode ~path:(path "last_finalized_head")
+  in
+  let* levels_to_hashes =
+    Levels_to_hashes.load mode ~path:(path "levels_to_hashes")
+  in
+  let+ irmin_store =
+    Irmin_store.load mode (path "irmin_store") |> Lwt_result.ok
+  in
+  {
+    l2_blocks;
+    messages;
+    inboxes;
+    commitments;
+    commitments_published_at_level;
+    l2_head;
+    last_finalized_head;
+    levels_to_hashes;
+    irmin_store;
+  }
