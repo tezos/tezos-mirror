465763fcd3e0f0fc3733bc9c64e743b1790f0ec2
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-01-05 16:20:55 +0100

    Tezt: translate pytest [TestStopLoopPassword] to Tezt

--- tezt/tests/transfer.ml
+++ tezt/tests/transfer.ml
@@ -67,6 +67,72 @@ let test_zero_transfer_to_nonexistent_contract =
     client
   |> Process.check_error ~msg:err
 
+(* Tests that the client stops asking for the password after
+   three erroneous passwords given by the user *)
+let test_encrypted_source_stop_loop_password =
+  Protocol.register_test
+    ~__FILE__
+    ~title:"Test invalid inputs when transferring from encrypted source account"
+    ~tags:["client"; "transfer"; "stop"; "loop"; "password"]
+  @@ fun protocol ->
+  Log.info "Import an encrypted account with some tez" ;
+  let* client = Client.init_mockup ~protocol () in
+  let encrypted_account =
+    {
+      alias = "encrypted_account";
+      public_key_hash = "";
+      public_key = "";
+      Account.secret_key =
+        Encrypted
+          "edesk1n2uGpPtVaeyhWkZzTEcaPRzkQHrqkw5pk8VkZvp3rM5KSc3mYNH5cJEuNcfB91B3G3JakKzfLQSmrgF4ht";
+    }
+  in
+  let* () =
+    Client.import_encrypted_secret_key
+      client
+      encrypted_account
+      ~password:"password"
+  in
+  (* Fund the encrypted account *)
+  let* () =
+    Client.transfer
+      ~burn_cap:Tez.one
+      ~amount:(Tez.of_int 100)
+      ~giver:"bootstrap1"
+      ~receiver:encrypted_account.alias
+      client
+  in
+  let giver = "encrypted_account" in
+  let receiver = Constant.bootstrap1.alias in
+  Log.info "Stops after three tries" ;
+  let* () =
+    ["\n\n\n"; "\n\n\nign"; "\n\n\npassword"]
+    |> Lwt_list.iter_s (fun stdin ->
+           let process, output_channel =
+             Client.spawn_command_with_stdin
+               client
+               ["transfer"; "0.1"; "from"; giver; "to"; receiver]
+           in
+           let* () = Lwt_io.write_line output_channel stdin in
+           let* () = Lwt_io.close output_channel in
+           Process.check_error process ~msg:(rex "3 incorrect password attempt"))
+  in
+  Log.info "Password succeed before three tries" ;
+  let* () =
+    ["password\n"; "\npassword\n"; "\n\npassword\n"]
+    |> Lwt_list.iter_s (fun stdin ->
+           let process, output_channel =
+             Client.spawn_command_with_stdin
+               client
+               ["transfer"; "0.1"; "from"; giver; "to"; receiver]
+           in
+           let* () = Lwt_io.write_line output_channel stdin in
+           let* () = Lwt_io.close output_channel in
+           Process.check process)
+  in
+  unit
+
 let register ~protocols =
   test_zero_transfer_to_implicit_contract protocols ;
-  test_zero_transfer_to_nonexistent_contract protocols
+  test_zero_transfer_to_nonexistent_contract protocols ;
+  test_encrypted_source_stop_loop_password protocols
