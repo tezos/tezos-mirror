OKEAMAH/Git

Plugin/Alpha: expose staked_on_commitment

src/proto_alpha/lib_plugin/RPC.ml
@@ -2014,7 +2014,10 @@ module Sc_rollup = struct
           "The hash of the commitment on which the operator has staked on for \
            a smart-contract rollup"
         ~query:RPC_query.empty
-        ~output:(obj1 (req "hash" Sc_rollup.Commitment.Hash.encoding))
+        ~output:
+          (merge_objs
+             (obj1 (req "hash" Sc_rollup.Commitment.Hash.encoding))
+             Sc_rollup.Commitment.encoding)
         RPC_path.(
           path_sc_rollup / "staker" /: Sc_rollup.Staker.rpc_arg
           / "staked_on_commitment")
@@ -2212,10 +2215,16 @@ module Sc_rollup = struct
     Registration.register2 ~chunked:false S.staked_on_commitment
     @@ fun ctxt address staker () () ->
     let open Lwt_result_syntax in
-    let+ branch, _ctxt =
+    let* commitment_hash, _ctxt =
       Alpha_context.Sc_rollup.Stake_storage.find_staker ctxt address staker
     in
-    branch
+    let+ commitment, _ctxt =
+      Alpha_context.Sc_rollup.Commitment.get_commitment
+        ctxt
+        address
+        commitment_hash
+    in
+    (commitment_hash, commitment)
 
   let register_commitment () =
     Registration.register2 ~chunked:false S.commitment
@@ -2333,6 +2342,16 @@ module Sc_rollup = struct
       ctxt
       block
       sc_rollup_address
+      ()
+      ()
+
+  let staked_on_commitment ctxt block sc_rollup_address staker =
+    RPC_context.make_call2
+      S.staked_on_commitment
+      ctxt
+      block
+      sc_rollup_address
+      staker
       ()
       ()
 

tezos/tezos-mirror

src/proto_alpha/lib_plugin/RPC.ml
@@ -2027,12 +2027,15 @@ module Sc_rollup = struct
     let staked_on_commitment =
       RPC_service.get_service
         ~description:
-          "The hash of the newest commitment on which the operator has staked \
-           on for a smart rollup. The hash can be absent if the staked \
-           commitment is before the last cemented commitment, and therefore \
-           the hash no longer exists in the context."
+          "The newest commitment on which the operator has staked on for a \
+           smart rollup. Note that is can return a commitment that is before \
+           the last cemented one."
         ~query:RPC_query.empty
-        ~output:(option Sc_rollup.Commitment.Hash.encoding)
+        ~output:
+          (option
+             (merge_objs
+                (obj1 (req "hash" Sc_rollup.Commitment.Hash.encoding))
+                Sc_rollup.Commitment.encoding))
         RPC_path.(
           path_sc_rollup / "staker" /: Sc_rollup.Staker.rpc_arg
           / "staked_on_commitment")
@@ -2199,10 +2202,19 @@ module Sc_rollup = struct
     Registration.register2 ~chunked:false S.staked_on_commitment
     @@ fun ctxt address staker () () ->
     let open Lwt_result_syntax in
-    let* _ctxt, res =
+    let* ctxt, commitment_hash =
       Alpha_context.Sc_rollup.Stake_storage.find_staker ctxt address staker
     in
-    return res
+    match commitment_hash with
+    | None -> return_none
+    | Some commitment_hash ->
+        let* commitment, _ctxt =
+          Alpha_context.Sc_rollup.Commitment.get_commitment
+            ctxt
+            address
+            commitment_hash
+        in
+        return_some (commitment_hash, commitment)
 
   let register_commitment () =
     Registration.register2 ~chunked:false S.commitment
@@ -2320,6 +2332,16 @@ module Sc_rollup = struct
       ctxt
       block
       sc_rollup_address
+      ()
+      ()
+
+  let staked_on_commitment ctxt block sc_rollup_address staker =
+    RPC_context.make_call2
+      S.staked_on_commitment
+      ctxt
+      block
+      sc_rollup_address
+      staker
       ()
       ()
 