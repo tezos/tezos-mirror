OKEAMAH/Git

WASM: tickify host functions

src/lib_scoru_wasm/test/helpers/ast_generators.ml
@@ -788,8 +788,9 @@ let inv_reveal_tick ~module_reg =
 let inv_stop_gen ~module_reg =
   let* vs = small_vector_gen value_gen in
   let* es = small_vector_gen (admin_instr_gen ~module_reg) in
-  let+ fresh_frame = option (ongoing_frame_stack_gen ~module_reg) in
-  Eval.Inv_stop {code = (vs, es); fresh_frame}
+  let* fresh_frame = option (ongoing_frame_stack_gen ~module_reg) in
+  let+ ticks = map Z.of_int small_nat in
+  Eval.Inv_stop {code = (vs, es); fresh_frame; ticks}
 
 let invoke_step_gen ~module_reg =
   oneof

src/lib_scoru_wasm/test/helpers/ast_printer.ml
@@ -531,18 +531,20 @@ let pp_invoke_step_kont out = function
         reveal
         base_destination
         max_bytes
-  | Inv_stop {code = vs, es; fresh_frame} ->
+  | Inv_stop {code = vs, es; fresh_frame; ticks} ->
       Format.fprintf
         out
         "%@[<v 2>Inv_stop {values = %a;@;\
          instructions = %a;@;\
-         fresh_frame = %a}@]"
+         fresh_frame = %a;@;\
+         ticks = %s}@]"
         (pp_vector Values.pp_value)
         vs
         (pp_vector pp_admin_instr)
         es
         (pp_opt pp_frame_stack)
         fresh_frame
+        (Z.to_string ticks)
 
 let pp_label_step_kont out = function
   | Eval.LS_Start label_kont ->

src/lib_scoru_wasm/wasm_encoding.ml
@@ -1091,16 +1091,18 @@ let invoke_step_kont_encoding =
           Inv_reveal_tick {reveal; base_destination; max_bytes; code = (vs, es)});
       case
         "Inv_stop"
-        (tup3
+        (tup4
            ~flatten:true
            (scope ["values"] values_encoding)
            (lazy_vector_encoding "instructions" admin_instr_encoding)
-           (scope ["fresh_frame"] (option ongoing_frame_stack_encoding)))
+           (scope ["fresh_frame"] (option ongoing_frame_stack_encoding))
+           (value ["ticks"] Data_encoding.z))
         (function
-          | Eval.Inv_stop {code = vs, es; fresh_frame} ->
-              Some (vs, es, fresh_frame)
+          | Eval.Inv_stop {code = vs, es; fresh_frame; ticks} ->
+              Some (vs, es, fresh_frame, ticks)
           | _ -> None)
-        (fun (vs, es, fresh_frame) -> Inv_stop {code = (vs, es); fresh_frame});
+        (fun (vs, es, fresh_frame, ticks) ->
+          Inv_stop {code = (vs, es); fresh_frame; ticks});
     ]
 
 let label_step_kont_encoding =

src/lib_webassembly/exec/eval.mli
@@ -123,7 +123,11 @@ type invoke_step_kont =
       max_bytes : int32;
       code : code;
     }
-  | Inv_stop of {code : code; fresh_frame : ongoing frame_stack option}
+  | Inv_stop of {
+      code : code;
+      fresh_frame : ongoing frame_stack option;
+      ticks : Z.t;
+    }
 
 type label_step_kont =
   | LS_Start : ongoing label_kont -> label_step_kont

tezos/tezos-mirror

src/lib_scoru_wasm/test/helpers/ast_generators.ml
@@ -788,8 +788,9 @@ let inv_reveal_tick ~module_reg =
 let inv_stop_gen ~module_reg =
   let* vs = small_vector_gen value_gen in
   let* es = small_vector_gen (admin_instr_gen ~module_reg) in
-  let+ fresh_frame = option (ongoing_frame_stack_gen ~module_reg) in
-  Eval.Inv_stop {code = (vs, es); fresh_frame}
+  let* fresh_frame = option (ongoing_frame_stack_gen ~module_reg) in
+  let+ remaining_ticks = map Z.of_int small_nat in
+  Eval.Inv_stop {code = (vs, es); fresh_frame; remaining_ticks}
 
 let invoke_step_gen ~module_reg =
   oneof

src/lib_scoru_wasm/test/helpers/ast_printer.ml
@@ -531,18 +531,20 @@ let pp_invoke_step_kont out = function
         reveal
         base_destination
         max_bytes
-  | Inv_stop {code = vs, es; fresh_frame} ->
+  | Inv_stop {code = vs, es; fresh_frame; remaining_ticks} ->
       Format.fprintf
         out
         "%@[<v 2>Inv_stop {values = %a;@;\
          instructions = %a;@;\
-         fresh_frame = %a}@]"
+         fresh_frame = %a;@;\
+         remaining_ticks = %s}@]"
         (pp_vector Values.pp_value)
         vs
         (pp_vector pp_admin_instr)
         es
         (pp_opt pp_frame_stack)
         fresh_frame
+        (Z.to_string remaining_ticks)
 
 let pp_label_step_kont out = function
   | Eval.LS_Start label_kont ->

src/lib_scoru_wasm/wasm_encoding.ml
@@ -1091,16 +1091,18 @@ let invoke_step_kont_encoding =
           Inv_reveal_tick {reveal; base_destination; max_bytes; code = (vs, es)});
       case
         "Inv_stop"
-        (tup3
+        (tup4
            ~flatten:true
            (scope ["values"] values_encoding)
            (lazy_vector_encoding "instructions" admin_instr_encoding)
-           (scope ["fresh_frame"] (option ongoing_frame_stack_encoding)))
+           (scope ["fresh_frame"] (option ongoing_frame_stack_encoding))
+           (value ["remaining_ticks"] Data_encoding.z))
         (function
-          | Eval.Inv_stop {code = vs, es; fresh_frame} ->
-              Some (vs, es, fresh_frame)
+          | Eval.Inv_stop {code = vs, es; fresh_frame; remaining_ticks} ->
+              Some (vs, es, fresh_frame, remaining_ticks)
           | _ -> None)
-        (fun (vs, es, fresh_frame) -> Inv_stop {code = (vs, es); fresh_frame});
+        (fun (vs, es, fresh_frame, remaining_ticks) ->
+          Inv_stop {code = (vs, es); fresh_frame; remaining_ticks});
     ]
 
 let label_step_kont_encoding =

src/lib_webassembly/exec/eval.mli
@@ -123,7 +123,11 @@ type invoke_step_kont =
       max_bytes : int32;
       code : code;
     }
-  | Inv_stop of {code : code; fresh_frame : ongoing frame_stack option}
+  | Inv_stop of {
+      code : code;
+      fresh_frame : ongoing frame_stack option;
+      remaining_ticks : Z.t;
+    }
 
 type label_step_kont =
   | LS_Start : ongoing label_kont -> label_step_kont