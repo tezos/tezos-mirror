f8aa1d63a754463b586fb99390dce0756a2c4f60
lykimq <lykimq@gmail.com>
2023-01-12 10:08:02 +0700

    Tezt: add test client simulation flag

--- tezt/tests/main.ml
+++ tezt/tests/main.ml
@@ -114,6 +114,7 @@ let register_protocol_tests_that_use_supports_correctly () =
   Client_fa12.register ~protocols ;
   Client_keys.register ~protocols ;
   Client_run_view.register ~protocols ;
+  Client_simulation_flag.register ~protocols ;
   Comparable_datatype.register ~protocols ;
   Contract_baker.register ~protocols ;
   Contract_big_map_to_self.register ~protocols ;
