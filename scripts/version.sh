a9f967b6ea6603b7ad98f73ec298b97caf5221d1
Danny Willems <be.danny.willems@gmail.com>
2023-01-13 12:11:50 +0100

    Doc: update definition location for var build_deps_image_version

--- scripts/version.sh
+++ scripts/version.sh
@@ -23,7 +23,7 @@ export recommended_node_version=16.18.1
 export full_opam_repository_tag=7f0579750a745ea92dc2fe3869d491902e2c29dd
 
 ## opam_repository is an additional, tezos-specific opam repository.
-## This value MUST be the same as `build_deps_image_version` in `.gitlab/ci/templates.yml
+## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml`
 export opam_repository_url=https://gitlab.com/tezos/opam-repository
 export opam_repository_tag="${OPAM_REPOSITORY_TAG:-fe5a5a1f731ec89c21b9a104ef7ccd0d5cafae2b}"
 export opam_repository_git="$opam_repository_url.git"
