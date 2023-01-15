43abf62844fe12f97a84c384959f7ae4cde26905
iguerNL@Functori <iguer@functori.com>
2023-01-15 18:00:04 +0100

    DAL/Node: introduce an event for decoding error

--- src/bin_dal_node/event.mli
+++ src/bin_dal_node/event.mli
@@ -32,3 +32,7 @@ val stored_slot_content : Cryptobox.Commitment.t t
 (** Storing a slot's shards event. The given parameters are the slot's
     commitment hash and the number of its shards. *)
 val stored_slot_shards : (string * int) t
+
+(** Decoding a value failed. See {!Types.kind} for kind of considered
+    values. *)
+val decoding_data_failed : Types.kind t
