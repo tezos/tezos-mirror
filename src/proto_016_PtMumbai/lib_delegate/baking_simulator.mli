9288a008f7829d692a3676b01caef3b2eaac5aab
Albin Coquereau <albin.coquereau@nomadic-labs.com>
2023-01-25 17:59:52 +0100

    lib_delegate: add some documentation to baking_simulator

--- src/proto_016_PtMumbai/lib_delegate/baking_simulator.mli
+++ src/proto_016_PtMumbai/lib_delegate/baking_simulator.mli
@@ -41,6 +41,13 @@ val load_context :
 val check_context_consistency :
   Abstract_context_index.t -> Context_hash.t -> unit tzresult Lwt.t
 
+(** [begin_construction ~timestamp ~protocol_data ~force_apply abstract_context
+    predecessor chain_id] creates a new [incremental] value with an empty
+    operation list. A [context] is recovered from the [abstract_index] and the
+    resulting_context_hash from [predecessor]. This context is used to create a
+    [validation_state] and an [application_state] (if [force_apply] is set). A
+    partial [shell_header] is created from [predecessor] information and
+    [timestamp]. *)
 val begin_construction :
   timestamp:Time.Protocol.t ->
   protocol_data:block_header_data ->
@@ -50,11 +57,22 @@ val begin_construction :
   Chain_id.t ->
   incremental tzresult Lwt.t
 
+(** [add_operation incremental op] validates [op] in
+    [incremental.validation_state] without checking its signature. Indeed, the
+    operation has already been validated in the node so it has a correct
+    signature. We still need to validate it again because the context may be
+    different. [op] is also applied if [incremental] has been created with
+    [force_apply] set. This function returns an [incremental] with updated
+    operations list and [validation_state] (and [application_state]). *)
 val add_operation :
   incremental ->
   Operation.packed ->
   (incremental * operation_receipt option) tzresult Lwt.t
 
+(** [finalize_construction incremental] calls the [finalize_validation] of the
+    protocol on the [validation_state] from [incremental]. If [incremental] has
+    been created with [force_apply] set, [finalize_application] is also called
+    and its results returned. *)
 val finalize_construction :
   incremental ->
   (Tezos_protocol_environment.validation_result * block_header_metadata) option
