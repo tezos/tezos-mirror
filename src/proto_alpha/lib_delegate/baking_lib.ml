69b01234463f822a393d041c6a4195536a89caee
Eugen Zalinescu <eugen.zalinescu@nomadic-labs.com>
2023-02-02 14:28:43 +0100

    Baker/DAL: include attestations when baking with 'bake for'

--- src/proto_alpha/lib_delegate/baking_lib.ml
+++ src/proto_alpha/lib_delegate/baking_lib.ml
@@ -468,6 +468,16 @@ let baking_minimal_timestamp state =
       current_mempool
       (List.map snd signed_endorsements)
   in
+  let dal_attestation_level = Int32.succ latest_proposal.block.shell.level in
+  let* own_dal_attestations =
+    Baking_actions.get_dal_attestations state ~level:dal_attestation_level
+  in
+  let* signed_dal_attestations =
+    Baking_actions.sign_dal_attestations state own_dal_attestations
+  in
+  let pool =
+    Operation_pool.add_operations pool (List.map snd signed_dal_attestations)
+  in
   let kind = Baking_actions.Fresh pool in
   let block_to_bake : Baking_actions.block_to_bake =
     {
