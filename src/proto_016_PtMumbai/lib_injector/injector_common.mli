e11ffedb554ea1f51e4671895348aa79f15ebd9c
Alain Mebsout <alain.mebsout@functori.com>
2023-02-03 16:28:24 +0100

    SCORU/Node/Injector: backport !7573 to Mumbai rollup node
    - Injector: parameterize by protocol client
    - Injector: reorg messages contain just block hashes and levels
    - Injector: use Client_context instead of protocol client
    - Injector: remove fee bound parameter
    - Injector: remove dependency on protocol
    - SCORU/Node: use new injector interface

--- src/proto_016_PtMumbai/lib_injector/injector_common.mli
+++ src/proto_016_PtMumbai/lib_injector/injector_common.mli
@@ -23,8 +23,6 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-open Protocol_client_context
-
 (** The type of signers for operations injected by the injector *)
 type signer = {
   alias : string;
@@ -51,44 +49,3 @@ val get_signer :
 val no_reorg : 'a reorg
 
 val reorg_encoding : 'a Data_encoding.t -> 'a reorg Data_encoding.t
-
-type block_info := Alpha_block_services.block_info
-
-type shell_header := Block_header.shell_header
-
-(** [fetch_tezos_shell_header ~find_in_cache cctxt hash] returns [Some
-    shell_header] given a block hash. Looks for the block using [find_in_cache]
-    first, and fetches it from the L1 node otherwise. Returns [None] if no such
-    block hash exists. [find_in_cache] should be from an instance of
-    {!Aches_lwt.Lache.MAP_RESULT}. *)
-val fetch_tezos_shell_header :
-  find_in_cache:
-    (Block_hash.t ->
-    (Block_hash.t -> shell_header option Lwt.t) ->
-    shell_header option Lwt.t) ->
-  #full ->
-  Block_hash.t ->
-  shell_header tzresult Lwt.t
-
-(** [fetch_tezos_block ~find_in_cache cctxt hash] returns [Some block_info]
-    given a block hash. Looks for the block using [find_in_cache] first, and
-    fetches it from the L1 node otherwise. Returns [None] if no such block hash
-    exists. [find_in_cache] should be from an instance of
-    {!Aches_lwt.Lache.MAP_RESULT}. *)
-val fetch_tezos_block :
-  find_in_cache:
-    (Block_hash.t ->
-    (Block_hash.t -> block_info option Lwt.t) ->
-    block_info option Lwt.t) ->
-  #full ->
-  Block_hash.t ->
-  block_info tzresult Lwt.t
-
-(** [tezos_reorg fetch ~old_head_hash ~new_head_hash] computes the
-    reorganization of L1 blocks from the chain whose head is [old_head_hash] and
-    the chain whose head [new_head_hash]. *)
-val tezos_reorg :
-  (Block_hash.t -> block_info tzresult Lwt.t) ->
-  old_head_hash:Block_hash.t ->
-  new_head_hash:Block_hash.t ->
-  block_info reorg tzresult Lwt.t
