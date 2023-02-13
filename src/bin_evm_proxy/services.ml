5392cc47f8e74006bf9c7bab626e0ba09ae8fb29
Rodi-Can Bozman <rbozman.functori@gmail.com>
2023-02-13 16:38:36 +0100

    EVM/Proxy: better dummy simulation

--- src/bin_evm_proxy/services.ml
+++ src/bin_evm_proxy/services.ml
@@ -63,7 +63,9 @@ module Mock = struct
   (* Default chain_id for ethereum custom networks with Ganache. *)
   let chain_id = qty_f (Z.of_int 1337)
 
-  let block_height = block_height_of_z Z.zero
+  let block_height_counter = ref 0
+
+  let block_height () = block_height_of_z @@ Z.of_int !block_height_counter
 
   let balance = qty_f @@ Conv.to_wei 1000
 
@@ -72,16 +74,23 @@ module Mock = struct
   (* Gas limit must be at least 21000 *)
   let gas_price = qty_f @@ Z.of_int 21000
 
-  let transaction_count = qty_f Z.zero
+  let transaction_counter = ref 0
+
+  let transaction_count () = qty_f @@ Z.of_int !transaction_counter
 
   (* Transaction's hash must be an alphanumeric 66 utf8 byte
      hex (chars: a-fA-F) string *)
-  let transaction_hash = hash_f @@ "0x" ^ String.make 64 'a'
+  let transaction_hash =
+    hash_f
+    @@ "0xf837c23ac7150b486be21fc00e3e2ad118e12bec1e2bca401b999f544eabc402"
 
-  let block =
+  let block_hash =
+    "0xd28d009fef5019bd9b353d7d9d881bde4870d3c5e418b1faf05fd9f7540994d8"
+
+  let block () =
     {
-      number = Some (block_height_of_z Z.zero);
-      hash = Some (block_hash_of_string @@ String.make 32 'a');
+      number = Some (block_height ());
+      hash = Some (block_hash_of_string block_hash);
       parent = block_hash_of_string (String.make 32 'a');
       nonce = hash_f @@ String.make 8 'a';
       sha3Uncles = hash_f @@ String.make 32 'a';
@@ -97,18 +106,19 @@ module Mock = struct
       gasLimit = qty_f @@ Z.of_int 1111111;
       gasUsed = qty_f Z.zero;
       timestamp = qty_f Z.zero;
-      transaction = [];
+      transaction = [transaction_hash];
       uncles = [];
     }
 
-  let transaction_receipt =
+  let transaction_receipt () =
     {
       transactionHash = transaction_hash;
-      transactionIndex = qty_f Z.one;
-      blockHash = block_hash_of_string @@ String.make 32 'a';
-      blockNumber = qty_f Z.one;
+      transactionIndex = qty_f Z.zero;
+      blockHash = block_hash_of_string block_hash;
+      blockNumber = qty_f @@ Z.of_int !block_height_counter;
       from = Address "0x6F4d14B90C48bEFb49CA3fe6663dEC70731A8bC7";
       to_ = Address "0xA5A5bf58c7Dc91cBE5005A7E5c6314998Eda479E";
+      contractAddress = None;
       cumulativeGasUsed = gas_price;
       effectiveGasPrice = gas_price;
       gasUsed = gas_price;
@@ -132,17 +142,20 @@ let dispatch dir =
         | Chain_id.Input _ -> return (Chain_id.Output (Ok Mock.chain_id))
         | Get_balance.Input _ -> return (Get_balance.Output (Ok Mock.balance))
         | Block_number.Input _ ->
-            return (Block_number.Output (Ok Mock.block_height))
+            return (Block_number.Output (Ok (Mock.block_height ())))
         | Get_block_by_number.Input _ ->
-            return (Get_block_by_number.Output (Ok Mock.block))
+            return (Get_block_by_number.Output (Ok (Mock.block ())))
         | Get_code.Input _ -> return (Get_code.Output (Ok Mock.code))
         | Gas_price.Input _ -> return (Gas_price.Output (Ok Mock.gas_price))
         | Get_transaction_count.Input _ ->
-            return (Get_transaction_count.Output (Ok Mock.transaction_count))
+            return
+              (Get_transaction_count.Output (Ok (Mock.transaction_count ())))
         | Get_transaction_receipt.Input _ ->
             return
-              (Get_transaction_receipt.Output (Ok Mock.transaction_receipt))
+              (Get_transaction_receipt.Output (Ok (Mock.transaction_receipt ())))
         | Send_raw_transaction.Input _ ->
+            incr Mock.block_height_counter ;
+            incr Mock.transaction_counter ;
             return (Send_raw_transaction.Output (Ok Mock.transaction_hash))
         | Send_transaction.Input _ ->
             return (Send_transaction.Output (Ok Mock.transaction_hash))
