7557f9c005cd0eab6655a5d8777a3393a0c8d39f
Martin Tomazic <martin.tomazic97@gmail.com>
2023-02-13 13:28:14 +0100

    DAC/test: Test closing subscriber stream

--- src/lib_dac_node/test/test_data_streamer.ml
+++ src/lib_dac_node/test/test_data_streamer.ml
@@ -83,8 +83,34 @@ let test_subscription_time () =
   let*! subscriber_2_first = Lwt_stream.next stream_2 in
   return @@ assert_equal_string dac_hash_2 subscriber_2_first
 
+let test_closing_subscriber_stream () =
+  (* 1. subscriber_1 subscribes to the streamer component.
+     2. subscriber_2 subscribes to the streamer component.
+     3. subscriber_1's [stream_1] is closed.
+     4. [dac_hash_1] is published via streamer component.
+
+     As such we expect:
+     - subscriber_1's [stream_1] is empty.
+     - subscriber_2 first element inside the stream is [dac_hash_1].
+  *)
+  let open Lwt_result_syntax in
+  let open Data_streamer in
+  let streamer = init () in
+  let* stream_1, stopper_1 = handle_subscribe streamer in
+  let* stream_2, _stopper_2 = handle_subscribe streamer in
+  let () = Lwt_watcher.shutdown stopper_1 in
+  let* () = publish streamer dac_hash_1 in
+  let*! is_empty = Lwt_stream.is_empty stream_1 in
+  let () = Assert.assert_true "[stream_1]: expected empty stream." is_empty in
+  let*! subscriber_2_first = Lwt_stream.next stream_2 in
+  return @@ assert_equal_string dac_hash_1 subscriber_2_first
+
 let tests =
   [
     Tztest.tztest "Simple pub sub" `Quick test_simple_pub_sub;
     Tztest.tztest "Test order of subscription" `Quick test_subscription_time;
+    Tztest.tztest
+      "Test closing subscriber stream"
+      `Quick
+      test_closing_subscriber_stream;
   ]
