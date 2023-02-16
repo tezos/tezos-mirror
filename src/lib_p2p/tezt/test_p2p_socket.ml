3a631200bc1cea348d9626758160ae3170ee549c
nraymond <nicolas.raymond@nomadic-labs.com>
2023-02-16 10:34:26 +0100

    p2p: Fix ipv4 and ipv6 CI collision

--- src/lib_p2p/tezt/test_p2p_socket.ml
+++ src/lib_p2p/tezt/test_p2p_socket.ml
@@ -39,6 +39,9 @@ end)
 
 let p2p_peer_id_typ = Check.comparable P2p_peer.Id.pp P2p_peer.Id.compare
 
+let set_addr_to_ipv4_localhost () =
+  addr := Ipaddr.V6.of_string_exn "::ffff:127.0.0.1"
+
 (** [connect ?target_id ?proof_of_work_target sched addr port] connect
     and performs [P2p_socket.authenticate] with the given
     [proof_of_work_target] (also checking that the remote point is the
@@ -100,4 +103,13 @@ let nack_test () =
   let* _ = run_nodes client server in
   unit
 
-let () = nack_test ()
+let () =
+  (* TODO: https://gitlab.com/tezos/tezos/-/issues/4882
+     Use ipv4 localhost by default.
+     Setting [addr] to ipv4 localhost is mandatory to
+     perform these tests on the CI. As your tests may be
+     successful using ipv6 on your machine, this will
+     not work on the CI as ipv6 network interface is
+     disabled on CI's executors. *)
+  set_addr_to_ipv4_localhost () ;
+  nack_test ()
