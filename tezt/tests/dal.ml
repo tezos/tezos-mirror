542c39953584da401747013e3e5960cadd1d5cc8
François Thiré <francois.thire@nomadic-labs.com>
2023-02-16 07:51:35 +0100

    Tezt/DAL: Fix flaky test in DAL attestation test
    Co-authored-by: Eugen Zlinescu <eugen.zalinescu@nomadic-labs.com>

--- tezt/tests/dal.ml
+++ tezt/tests/dal.ml
@@ -2040,14 +2040,22 @@ let test_attestor ~with_baker_daemon protocol parameters cryptobox node client
         (slot_idx level = slot_index)
           int
           ~error_msg:"Expected index %L (got %R)") ;
-      let expected_status =
-        if level < first_not_attested_published_level then "attested"
-        else "unattested"
-      in
-      Check.(
-        (expected_status = status)
-          string
-          ~error_msg:"Expected status %L (got %R)") ;
+      (if
+       level < intermediary_level || level >= first_not_attested_published_level
+      then
+       (* We cannot know for sure the status of a slot between
+          [intermediary_level] and
+          [first_not_attested_published_level]. Before
+          [intermediary_level], it should be [attested], and above
+          [first_not_attested_published_level], it should be
+          [unattested]. *)
+       let expected_status =
+         if level < intermediary_level then "attested" else "unattested"
+       in
+       Check.(
+         (expected_status = status)
+           string
+           ~error_msg:"Expected status %L (got %R)")) ;
       check_attestations (level + 1)
   in
   check_attestations first_level
