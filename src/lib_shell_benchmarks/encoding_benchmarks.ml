d84c31ab7040ffa2bd7bd57567129671824ac4db
Pavlo Khrystenko <p.khrystenko@gmail.com>
2023-03-02 16:16:00 +0100

    Snoop: use shell common tags in encoding_benchmark

--- src/lib_shell_benchmarks/encoding_benchmarks.ml
+++ src/lib_shell_benchmarks/encoding_benchmarks.ml
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2020 Nomadic Labs. <contact@nomadic-labs.com>               *)
+(* Copyright (c) 2023 Marigold <contact@marigold.dev>                        *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -22,7 +23,7 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
-
+open Benchmarks_shell
 open Encoding_benchmarks_helpers
 
 (* ------------------------------------------------------------------------- *)
