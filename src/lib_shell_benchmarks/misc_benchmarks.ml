27773b5daf5d99100dcc89787bcd0606b80c5494
Pavlo Khrystenko <p.khrystenko@gmail.com>
2023-03-02 16:18:02 +0100

    Snoop: use shell common tags in misc_benchmarks

--- src/lib_shell_benchmarks/misc_benchmarks.ml
+++ src/lib_shell_benchmarks/misc_benchmarks.ml
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2020 Nomadic Labs. <contact@nomadic-labs.com>               *)
+(* Copyright (c) 2023 Marigold <contact@marigold.dev>                        *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
