12363a3b6ab5fdd486b2bccfd189cf7fe7337f87
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2023-03-08 14:39:24 +0100

    EVM/Kernel: Block validation

--- src/kernel_evm/kernel/src/block.rs
+++ src/kernel_evm/kernel/src/block.rs
@@ -2,12 +2,22 @@
 //
 // SPDX-License-Identifier: MIT
 
-use crate::error::Error;
+use crate::blueprint::Queue;
+use crate::error::{Error, TransferError};
+use crate::helpers::address_to_hash;
+use crate::inbox::Transaction;
 use crate::storage;
-use crate::{blueprint::Queue, inbox::Transaction};
+use host::path::OwnedPath;
 use host::rollup_core::RawRollupCore;
 use host::runtime::Runtime;
-use tezos_ethereum::eth_gen::{L2Level, OwnedHash, Quantity, TransactionHash};
+use tezos_ethereum::eth_gen::{L2Level, OwnedHash, Quantity, RawTransaction, TransactionHash};
+use tezos_ethereum::wei::Wei;
+
+use debug::debug_msg;
+
+// Used by Transaction.nonce.to_u64, the trait must be in scope for the function
+// to be available
+use num_traits::ToPrimitive;
 
 pub struct L2Block {
     // This choice of a L2 block representation is totally
@@ -71,8 +81,52 @@ impl L2Block {
     }
 }
 
-fn validate(block: L2Block) -> Result<L2Block, Error> {
-    // TODO: https://gitlab.com/tezos/tezos/-/issues/4749
+fn get_tx_sender(tx: &RawTransaction) -> Result<(OwnedHash, OwnedPath), Error> {
+    match tx.sender() {
+        // We reencode in hexadecimal, since the accounts hash are encoded in
+        // hexadecimal in the storage.
+        Ok(address) => {
+            let hash = address_to_hash(address);
+            let path = storage::account_path(&hash)?;
+            Ok((hash, path))
+        }
+        Err(_) => Err(Error::Generic),
+    }
+}
+
+// A transaction is valid if the signature is valid, its nonce is valid and it
+// can pay for the gas
+fn validate_transaction<Host: Runtime + RawRollupCore>(
+    host: &mut Host,
+    transaction: &Transaction,
+) -> Result<(), Error> {
+    let tx = &transaction.tx;
+    let (_, src) = get_tx_sender(tx)?;
+    let src_balance = storage::read_account_balance(host, &src).unwrap_or_else(|_| Wei::zero());
+    let src_nonce = storage::read_account_nonce(host, &src).unwrap_or(0u64);
+    let nonce = tx.nonce.to_u64().ok_or(Error::Generic)?;
+    // For now, we consider there's no gas to pay
+    let gas = Wei::zero();
+
+    if !tx.is_valid() {
+        Err(Error::Transfer(TransferError::InvalidSignature))
+    } else if src_nonce != nonce {
+        Err(Error::Transfer(TransferError::InvalidNonce))
+    } else if src_balance < gas {
+        Err(Error::Transfer(TransferError::NotEnoughBalance))
+    } else {
+        Ok(())
+    }
+}
+
+fn validate<Host: Runtime + RawRollupCore>(
+    host: &mut Host,
+    block: L2Block,
+    transactions: &[Transaction],
+) -> Result<L2Block, Error> {
+    transactions
+        .iter()
+        .try_for_each(|tx| validate_transaction(host, tx))?;
     Ok(block)
 }
 
@@ -83,16 +137,17 @@ pub fn produce<Host: Runtime + RawRollupCore>(host: &mut Host, queue: Queue) {
             Ok(current_level) => current_level + 1,
             Err(_) => 0,
         };
-        let transactions = proposal
-            .transactions
-            .iter()
-            .map(|transaction: &Transaction| -> TransactionHash { transaction.tx_hash })
-            .collect();
-        let candidate_block = L2Block::new(next_level, transactions);
-        if let Ok(valid_block) = validate(candidate_block) {
-            storage::store_current_block(host, valid_block).unwrap_or_else(|_| {
-                panic!("Error while storing the current block: stopping the daemon.")
-            })
+
+        let transaction_hashes = proposal.transactions.iter().map(|tx| tx.tx_hash).collect();
+        let candidate_block = L2Block::new(next_level, transaction_hashes);
+
+        match validate(host, candidate_block, &proposal.transactions) {
+            Ok(valid_block) => {
+                storage::store_current_block(host, valid_block).unwrap_or_else(|_| {
+                    panic!("Error while storing the current block: stopping the daemon.")
+                });
+            }
+            Err(e) => debug_msg!(host; "Blueprint is invalid: {:?}\n", e),
         }
     }
 }
