OKEAMAH/Git

Scoru, Tests: implement generative tests against trie
No actual inputs generation in this commit.

src/lib_scoru_wasm/test/helpers/durable_snapshot_util.ml
@@ -70,6 +70,10 @@ module type Testable_durable_sig = sig
     t -> ?edit_readonly:bool -> key -> int64 -> string -> t Lwt.t
 
   val read_value_exn : t -> key -> int64 -> int64 -> string Lwt.t
+
+  module Internal_for_tests : sig
+    val key_to_string : key -> string
+  end
 end
 
 module CBV = Tezos_lazy_containers.Chunked_byte_vector
@@ -398,8 +402,207 @@ end) : Testable_durable_sig with type t = Snapshot.t * Current.t = struct
         add_tree tree_s @@ Snapshot.read_value_exn tree_s key_s offset len)
       (fun () ->
         add_tree tree_c @@ Current.read_value_exn tree_c key_c offset len)
+
+  module Internal_for_tests = struct
+    let key_to_string (_k, k) = Current.Internal_for_tests.key_to_string k
+  end
+end
+
+(* Wrapper around tested durable which keeps track some
+   statistic, also might be used for debug tracing.
+*)
+module Traceable_durable = struct
+  module type S = sig
+    include Testable_durable_sig
+
+    val print_collected_statistic : unit -> unit
+  end
+
+  module Default_traceable_config = struct
+    let print_operations : Durable_operation.Set.t =
+      Durable_operation.Set.of_list Durable_operation.all_operations
+
+    let count_methods_invocations = true
+  end
+
+  module type Traceable_config = module type of Default_traceable_config
+
+  module Make (Config : Traceable_config) (D : Testable_durable_sig) :
+    S with type t = D.t and type key = D.key = struct
+    open Durable_operation
+
+    type st = {succ : int; fails : int}
+
+    let method_invocations : st Map.t ref = ref Durable_operation.Map.empty
+
+    let tot_method_invocations : int ref = ref 0
+
+    type t = D.t
+
+    type key = D.key
+
+    let is_op_printable (op : _ Durable_operation.t) =
+      Set.mem (Some_op op) Config.print_operations
+
+    let inspect_op (type input) (op : input Durable_operation.t) (inp : input)
+        (is_succ : 'a -> bool) (operation : unit -> 'a Lwt.t) : 'a Lwt.t =
+      let inc f =
+        if Config.count_methods_invocations then
+          method_invocations :=
+            Map.update
+              (Some_op op)
+              (Option.fold
+                 ~none:(Some (f {succ = 0; fails = 0}))
+                 ~some:(fun x -> Some (f x)))
+              !method_invocations
+        else ()
+      in
+      let inc_succ () = inc (fun t -> {t with succ = t.succ + 1}) in
+      let inc_fails () = inc (fun t -> {t with fails = t.fails + 1}) in
+      tot_method_invocations := !tot_method_invocations + 1 ;
+      Lwt.try_bind
+        operation
+        (fun a ->
+          if is_succ a then inc_succ () else inc_fails () ;
+          if is_op_printable op then
+            Format.printf
+              "%4d: %a completed normally\n\n"
+              !tot_method_invocations
+              Durable_operation.pp_some_input
+              (Some_input (op, inp)) ;
+          Lwt.return a)
+        (fun exn ->
+          inc_fails () ;
+          if is_op_printable op then
+            Format.printf
+              "%4d: %a completed with an exception: %s\n\n"
+              !tot_method_invocations
+              Durable_operation.pp_some_input
+              (Some_input (op, inp))
+              (Printexc.to_string exn) ;
+          raise exn)
+
+    let encoding = D.encoding
+
+    let max_key_length = D.max_key_length
+
+    let key_of_string_exn = D.key_of_string_exn
+
+    let key_of_string_opt = D.key_of_string_opt
+
+    let find_value dur key =
+      inspect_op
+        Find_value
+        (D.Internal_for_tests.key_to_string key)
+        Option.is_some
+      @@ fun () -> D.find_value dur key
+
+    let find_value_exn dur key =
+      inspect_op
+        Find_value_exn
+        (D.Internal_for_tests.key_to_string key)
+        (Fun.const true)
+      @@ fun () -> D.find_value_exn dur key
+
+    let copy_tree_exn dur ?(edit_readonly = false) key_from key_to =
+      inspect_op
+        Copy_tree_exn
+        ( edit_readonly,
+          D.Internal_for_tests.key_to_string key_from,
+          D.Internal_for_tests.key_to_string key_to )
+        (Fun.const true)
+      @@ fun () -> D.copy_tree_exn dur ~edit_readonly key_from key_to
+
+    let move_tree_exn dur key_from key_to =
+      inspect_op
+        Move_tree_exn
+        ( D.Internal_for_tests.key_to_string key_from,
+          D.Internal_for_tests.key_to_string key_to )
+        (Fun.const true)
+      @@ fun () -> D.move_tree_exn dur key_from key_to
+
+    let list dur key =
+      inspect_op List (D.Internal_for_tests.key_to_string key) (fun l ->
+          not (List.is_empty l))
+      @@ fun () -> D.list dur key
+
+    let count_subtrees dur key =
+      inspect_op
+        Count_subtrees
+        (D.Internal_for_tests.key_to_string key)
+        (fun l -> l > 0)
+      @@ fun () -> D.count_subtrees dur key
+
+    let subtree_name_at dur key sibling_id =
+      inspect_op
+        Substree_name_at
+        (D.Internal_for_tests.key_to_string key, sibling_id)
+        (Fun.const true)
+      @@ fun () -> D.subtree_name_at dur key sibling_id
+
+    let delete ?(edit_readonly = false) dur key =
+      inspect_op
+        Delete
+        (edit_readonly, D.Internal_for_tests.key_to_string key)
+        (Fun.const true)
+      @@ fun () -> D.delete ~edit_readonly dur key
+
+    let hash dur key =
+      inspect_op Hash (D.Internal_for_tests.key_to_string key) Option.is_some
+      @@ fun () -> D.hash dur key
+
+    let hash_exn dur key =
+      inspect_op
+        Hash_exn
+        (D.Internal_for_tests.key_to_string key)
+        (Fun.const true)
+      @@ fun () -> D.hash_exn dur key
+
+    let set_value_exn dur ?(edit_readonly = false) key value =
+      inspect_op
+        Set_value_exn
+        (edit_readonly, D.Internal_for_tests.key_to_string key, value)
+        (Fun.const true)
+      @@ fun () -> D.set_value_exn dur ~edit_readonly key value
+
+    let write_value_exn dur ?(edit_readonly = false) key offset value =
+      inspect_op
+        Write_value_exn
+        (edit_readonly, D.Internal_for_tests.key_to_string key, offset, value)
+        (Fun.const true)
+      @@ fun () -> D.write_value_exn dur ~edit_readonly key offset value
+
+    let read_value_exn dur key offset len =
+      inspect_op
+        Read_value_exn
+        (D.Internal_for_tests.key_to_string key, offset, len)
+        (Fun.const true)
+      @@ fun () -> D.read_value_exn dur key offset len
+
+    module Internal_for_tests = D.Internal_for_tests
+
+    let print_collected_statistic () =
+      let collected = Map.bindings !method_invocations in
+      let sm = !tot_method_invocations in
+      let to_perc (x : int) (tot : int) =
+        Float.(div (of_int (Int.mul 100 x)) (of_int tot))
+      in
+      Format.printf "Methods invocation statistic, total invocations: %d\n" sm ;
+      List.iter
+        (fun (op, st) ->
+          Format.printf
+            "%4s%a: %.1f%% of all ops\n%8sSuccessful: %.1f%% / Fails: %.1f%%\n"
+            ""
+            Durable_operation.pp_some_op
+            op
+            (to_perc (st.succ + st.fails) sm)
+            ""
+            (to_perc st.succ (st.succ + st.fails))
+            (to_perc st.fails (st.succ + st.fails)))
+        collected
+  end
 end
 
 module Paired_durable = Make_paired_durable (struct
-  let compare_durables_after_each_operation = true
+  let compare_durables_after_each_operation = false
 end)

tezos/tezos-mirror

src/lib_scoru_wasm/test/helpers/durable_snapshot_util.ml
@@ -70,6 +70,10 @@ module type Testable_durable_sig = sig
     t -> ?edit_readonly:bool -> key -> int64 -> string -> t Lwt.t
 
   val read_value_exn : t -> key -> int64 -> int64 -> string Lwt.t
+
+  module Internal_for_tests : sig
+    val key_to_list : key -> string list
+  end
 end
 
 module CBV = Tezos_lazy_containers.Chunked_byte_vector
@@ -133,7 +137,7 @@ let compare_durable_storages (ts : Snapshot.t) (tc : Current.t) :
    All the methods are performed on both durables and
    returned values and resulting durables tested on equality.
 *)
-module Make_paired_durable (Config : sig
+module Make_verifiable_current_durable (Config : sig
   val compare_durables_after_each_operation : bool
 end) : Testable_durable_sig with type t = Snapshot.t * Current.t = struct
   type t = Snapshot.t * Current.t
@@ -400,8 +404,215 @@ end) : Testable_durable_sig with type t = Snapshot.t * Current.t = struct
         add_tree tree_s @@ Snapshot.read_value_exn tree_s key_s offset len)
       (fun () ->
         add_tree tree_c @@ Current.read_value_exn tree_c key_c offset len)
+
+  module Internal_for_tests = struct
+    let key_to_list (k1, k2) =
+      let s1 = Snapshot.Internal_for_tests.key_to_list k1 in
+      let s2 = Current.Internal_for_tests.key_to_list k2 in
+      Assert.equal
+        ~eq:(List.equal String.equal)
+        ~loc:__LOC__
+        ~msg:
+          "String key representation are different for Snapshot and Current \
+           storages"
+        s1
+        s2 ;
+      s2
+  end
 end
 
-module Paired_durable = Make_paired_durable (struct
-  let compare_durables_after_each_operation = true
+(* Wrapper around tested durable which keeps track some
+   statistic, also might be used for debug tracing.
+*)
+module Traceable_durable = struct
+  module type S = sig
+    include Testable_durable_sig
+
+    val print_collected_statistic : unit -> unit
+  end
+
+  module Default_traceable_config = struct
+    let print_operations : Durable_operation.Set.t =
+      Durable_operation.Set.of_list Durable_operation.all_operations
+
+    let count_methods_invocations = true
+  end
+
+  module type Traceable_config = module type of Default_traceable_config
+
+  module Make (Config : Traceable_config) (D : Testable_durable_sig) :
+    S with type t = D.t and type key = D.key = struct
+    open Durable_operation
+
+    type st = {succ : int; fails : int}
+
+    let method_invocations : st Map.t ref = ref Durable_operation.Map.empty
+
+    let tot_method_invocations : int ref = ref 0
+
+    type t = D.t
+
+    type key = D.key
+
+    let is_op_printable (op : _ Durable_operation.t) =
+      Set.mem (Some_op op) Config.print_operations
+
+    let inspect_op (type input) (op : input Durable_operation.t) (inp : input)
+        (is_succ : 'a -> bool) (operation : unit -> 'a Lwt.t) : 'a Lwt.t =
+      let inc f =
+        if Config.count_methods_invocations then
+          method_invocations :=
+            Map.update
+              (Some_op op)
+              (Option.fold
+                 ~none:(Some (f {succ = 0; fails = 0}))
+                 ~some:(fun x -> Some (f x)))
+              !method_invocations
+      in
+      let inc_succ () = inc (fun t -> {t with succ = t.succ + 1}) in
+      let inc_fails () = inc (fun t -> {t with fails = t.fails + 1}) in
+      tot_method_invocations := !tot_method_invocations + 1 ;
+      Lwt.try_bind
+        operation
+        (fun a ->
+          if is_succ a then inc_succ () else inc_fails () ;
+          if is_op_printable op then
+            Format.printf
+              "%4d: %a completed normally\n\n"
+              !tot_method_invocations
+              Durable_operation.pp_some_input
+              (Some_input (op, inp)) ;
+          Lwt.return a)
+        (fun exn ->
+          inc_fails () ;
+          if is_op_printable op then
+            Format.printf
+              "%4d: %a completed with an exception: %s\n\n"
+              !tot_method_invocations
+              Durable_operation.pp_some_input
+              (Some_input (op, inp))
+              (Printexc.to_string exn) ;
+          raise exn)
+
+    let encoding = D.encoding
+
+    let max_key_length = D.max_key_length
+
+    let key_of_string_exn = D.key_of_string_exn
+
+    let key_of_string_opt = D.key_of_string_opt
+
+    let find_value dur key =
+      inspect_op
+        Find_value
+        (D.Internal_for_tests.key_to_list key)
+        Option.is_some
+      @@ fun () -> D.find_value dur key
+
+    let find_value_exn dur key =
+      inspect_op
+        Find_value_exn
+        (D.Internal_for_tests.key_to_list key)
+        (Fun.const true)
+      @@ fun () -> D.find_value_exn dur key
+
+    let copy_tree_exn dur ?(edit_readonly = false) key_from key_to =
+      inspect_op
+        Copy_tree_exn
+        ( edit_readonly,
+          D.Internal_for_tests.key_to_list key_from,
+          D.Internal_for_tests.key_to_list key_to )
+        (Fun.const true)
+      @@ fun () -> D.copy_tree_exn dur ~edit_readonly key_from key_to
+
+    let move_tree_exn dur key_from key_to =
+      inspect_op
+        Move_tree_exn
+        ( D.Internal_for_tests.key_to_list key_from,
+          D.Internal_for_tests.key_to_list key_to )
+        (Fun.const true)
+      @@ fun () -> D.move_tree_exn dur key_from key_to
+
+    let list dur key =
+      inspect_op List (D.Internal_for_tests.key_to_list key) (fun l ->
+          not (List.is_empty l))
+      @@ fun () -> D.list dur key
+
+    let count_subtrees dur key =
+      inspect_op Count_subtrees (D.Internal_for_tests.key_to_list key) (fun l ->
+          l > 0)
+      @@ fun () -> D.count_subtrees dur key
+
+    let subtree_name_at dur key sibling_id =
+      inspect_op
+        Substree_name_at
+        (D.Internal_for_tests.key_to_list key, sibling_id)
+        (Fun.const true)
+      @@ fun () -> D.subtree_name_at dur key sibling_id
+
+    let delete ?(edit_readonly = false) dur key =
+      inspect_op
+        Delete
+        (edit_readonly, D.Internal_for_tests.key_to_list key)
+        (Fun.const true)
+      @@ fun () -> D.delete ~edit_readonly dur key
+
+    let hash dur key =
+      inspect_op Hash (D.Internal_for_tests.key_to_list key) Option.is_some
+      @@ fun () -> D.hash dur key
+
+    let hash_exn dur key =
+      inspect_op
+        Hash_exn
+        (D.Internal_for_tests.key_to_list key)
+        (Fun.const true)
+      @@ fun () -> D.hash_exn dur key
+
+    let set_value_exn dur ?(edit_readonly = false) key value =
+      inspect_op
+        Set_value_exn
+        (edit_readonly, D.Internal_for_tests.key_to_list key, value)
+        (Fun.const true)
+      @@ fun () -> D.set_value_exn dur ~edit_readonly key value
+
+    let write_value_exn dur ?(edit_readonly = false) key offset value =
+      inspect_op
+        Write_value_exn
+        (edit_readonly, D.Internal_for_tests.key_to_list key, offset, value)
+        (Fun.const true)
+      @@ fun () -> D.write_value_exn dur ~edit_readonly key offset value
+
+    let read_value_exn dur key offset len =
+      inspect_op
+        Read_value_exn
+        (D.Internal_for_tests.key_to_list key, offset, len)
+        (Fun.const true)
+      @@ fun () -> D.read_value_exn dur key offset len
+
+    module Internal_for_tests = D.Internal_for_tests
+
+    let print_collected_statistic () =
+      let collected = Map.bindings !method_invocations in
+      let sm = !tot_method_invocations in
+      let to_perc (x : int) (tot : int) =
+        Float.(div (of_int (Int.mul 100 x)) (of_int tot))
+      in
+      Format.printf "Methods invocation statistic, total invocations: %d\n" sm ;
+      List.iter
+        (fun (op, st) ->
+          Format.printf
+            "%4s%a: %.1f%% of all ops\n%8sSuccessful: %.1f%% / Fails: %.1f%%\n"
+            ""
+            Durable_operation.pp_some_op
+            op
+            (to_perc (st.succ + st.fails) sm)
+            ""
+            (to_perc st.succ (st.succ + st.fails))
+            (to_perc st.fails (st.succ + st.fails)))
+        collected
+  end
+end
+
+module Verifiable_current_durable = Make_verifiable_current_durable (struct
+  let compare_durables_after_each_operation = false
 end)