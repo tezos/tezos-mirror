a767a6dffac9d62aa65ef79f8178edc5378dacb6
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2023-03-08 15:42:07 +0100

    EVM/Tezt: parse eth responses as JSON

--- tezt/lib_tezos/eth_cli.ml
+++ tezt/lib_tezos/eth_cli.ml
@@ -25,42 +25,40 @@
 
 let path = "eth"
 
-let spawn_command command =
-  Process.spawn path command |> Process.check_and_read_stdout
+let spawn_command command decode =
+  let process = Process.spawn path command in
+  let* output = Process.check_and_read_stdout process in
+  return (JSON.parse ~origin:"eth_spawn_command" output |> decode)
 
 let balance ~account ~endpoint =
-  let* answer =
-    spawn_command ["address:balance"; account; "--network"; endpoint]
-  in
-  return (int_of_string (String.trim answer))
+  spawn_command ["address:balance"; account; "--network"; endpoint] JSON.as_int
 
 let transaction_send ~source_private_key ~to_public_key ~value ~endpoint =
-  let* answer =
-    spawn_command
-      [
-        "transaction:send";
-        "--pk";
-        source_private_key;
-        "--to";
-        to_public_key;
-        "--value";
-        Z.to_string value;
-        "--network";
-        endpoint;
-      ]
-  in
-  return (String.trim answer)
+  spawn_command
+    [
+      "transaction:send";
+      "--pk";
+      source_private_key;
+      "--to";
+      to_public_key;
+      "--value";
+      Z.to_string value;
+      "--network";
+      endpoint;
+    ]
+    JSON.as_string
 
 let get_block ~block_id ~endpoint =
   let exception Could_not_parse_block of string in
-  let* answer = spawn_command ["block:get"; block_id; "--network"; endpoint] in
+  let* answer =
+    spawn_command ["block:get"; block_id; "--network"; endpoint] Fun.id
+  in
   let block_as_json =
-    match Data_encoding.Json.from_string answer with
+    match Data_encoding.Json.from_string (JSON.encode answer) with
     | Ok json -> json
     | Error msg -> raise @@ Could_not_parse_block msg
   in
   return @@ Data_encoding.Json.destruct Eth.Block.encoding block_as_json
 
 let block_number ~endpoint =
-  let* answer = spawn_command ["block:number"; "--network"; endpoint] in
-  return (int_of_string (String.trim answer))
+  spawn_command ["block:number"; "--network"; endpoint] JSON.as_int
