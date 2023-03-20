8f10129859d6be467ee3753fffd2329924d73520
Raphael Toledo <contact@raphael-toledo.com>
2023-03-20 10:33:13 +0000

    Proto_X/test/integration: Removing timelock tests (now in tezt)

--- manifest/main.ml
+++ manifest/main.ml
@@ -4639,7 +4639,6 @@ end = struct
             ("test_ticket_operations_diff", true);
             ("test_ticket_scanner", true);
             ("test_ticket_storage", true);
-            ("test_timelock", true);
             ("test_typechecking", true);
             ("test_lambda_normalization", N.(number >= 016));
           ]
