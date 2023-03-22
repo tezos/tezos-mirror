296ff919662618104cde3f230e23053c264eb814
lin <linoshitani@gmail.com>
2023-03-22 17:25:59 +0900

    Gossipsub/Test: Port [test_join] from rust-libp2p

--- src/lib_gossipsub/test/test_unit.ml
+++ src/lib_gossipsub/test/test_unit.ml
@@ -26,6 +26,7 @@
 (** {2 Unit tests for gossipsub.} *)
 
 open Test_gossipsub_shared
+open Gossipsub_intf
 open Tezt
 open Tezt_core.Base
 
@@ -43,7 +44,7 @@ let assert_subscribed_topics ~__LOC__ ~peer ~expected_topics state =
       ~error_msg:"Expected %R, got %L"
       ~__LOC__)
 
-let many_peers limits = (4 * limits.Gossipsub_intf.degree_optimal) + 1
+let many_peers limits = (4 * limits.degree_optimal) + 1
 
 let make_peers ~number =
   List.init ~when_negative_length:() number (fun i -> i)
@@ -205,6 +206,152 @@ let test_handle_received_subscriptions rng limits parameters =
     state ;
   unit
 
+(* The Join function should:
+   - Fill up mesh with known gossipsub peers in the topic
+   - Returns GRAFT requests for all nodes added to the mesh
+
+   Ported from: https://github.com/libp2p/rust-libp2p/blob/12b785e94ede1e763dd041a107d3a00d5135a213/protocols/gossipsub/src/behaviour/tests.rs#L512
+*)
+let test_join_adds_peers_to_mesh rng limits parameters =
+  Tezt_core.Test.register
+    ~__FILE__
+    ~title:"Gossipsub: Test join adds peers to mesh"
+    ~tags:["gossipsub"; "join"]
+  @@ fun () ->
+  let topics = ["topic0"] in
+  let peers = make_peers ~number:(many_peers limits) in
+  let state =
+    init_state
+      ~rng
+      ~limits
+      ~parameters
+      ~peers
+      ~topics
+      ~to_subscribe:(fun _ -> true)
+      ()
+  in
+  (* leave, then call join to invoke functionality *)
+  let topic = "topic0" in
+  let state, _ = GS.leave {topic} state in
+  (* re-join - there should be peers associated with the topic *)
+  let state, to_graft =
+    match GS.join {topic} state with
+    | state, Joining_topic to_graft -> (state, C.Peer.Set.elements to_graft)
+    | _, _ -> Test.fail ~__LOC__ "Expected Join to succeed"
+  in
+  (* should have added [degree_optimal] nodes to the mesh *)
+  let peers_in_topic =
+    GS.Internal_for_tests.get_peers_in_topic_mesh "topic0" state
+  in
+  Check.(
+    (List.length peers_in_topic = limits.degree_optimal)
+      int
+      ~error_msg:"Expected %R, got %L"
+      ~__LOC__) ;
+  (* there should be [degree_optimal] GRAFT messages. *)
+  Check.(
+    (List.length to_graft = limits.degree_optimal)
+      int
+      ~error_msg:"Expected %R, got %L"
+      ~__LOC__) ;
+  unit
+
+(* The Join function should:
+   - Remove peers from fanout[topic]
+   - Add any fanout[topic] peers to the mesh
+   - Fill up mesh with known gossipsub peers in the topic
+   - Returns GRAFT requests for all nodes added to the mesh
+
+   Ported from: https://github.com/libp2p/rust-libp2p/blob/12b785e94ede1e763dd041a107d3a00d5135a213/protocols/gossipsub/src/behaviour/tests.rs#L512
+*)
+let test_join_adds_fanout_to_mesh rng limits parameters =
+  Tezt_core.Test.register
+    ~__FILE__
+    ~title:"Gossipsub: Test join adds fanout to mesh"
+    ~tags:["gossipsub"; "join"; "fanout"]
+  @@ fun () ->
+  let topics = ["topic0"] in
+  (* We initialize the state with [degree_optimal / 2] peers
+     so the mesh won't be filled with just fanout peers when we call [GS.join]. *)
+  let init_peers, additional_peers =
+    List.split_n (limits.degree_optimal / 2)
+    @@ make_peers ~number:(many_peers limits)
+  in
+  let state =
+    init_state
+      ~rng
+      ~limits
+      ~parameters
+      ~peers:init_peers
+      ~topics
+      ~to_join:(fun _ -> false)
+      ~to_subscribe:(fun _ -> true)
+      ()
+  in
+  (* Publish to topic0.
+     We did not join the topic so the peers should be added to the fanout map.*)
+  let state, _ =
+    GS.publish
+      {sender = None; topic = "topic0"; message_id = 0; message = 0}
+      state
+  in
+  (* Check that all [init_peers] have been added to the fanout.  *)
+  let fanout_peers = GS.Internal_for_tests.get_fanout_peers "topic0" state in
+  Check.(
+    (List.length fanout_peers = limits.degree_optimal / 2)
+      int
+      ~error_msg:"Expected %R, got %L"
+      ~__LOC__) ;
+  (* Add additonal peers *)
+  let state =
+    add_and_subscribe_peers
+      topics
+      additional_peers
+      state
+      ~to_subscribe:(fun _ -> true)
+  in
+  (* Join to topic0 *)
+  let state, to_graft =
+    match GS.join {topic = "topic0"} state with
+    | state, Joining_topic to_graft -> (state, C.Peer.Set.elements to_graft)
+    | _, _ -> Test.fail ~__LOC__ "Expected Join to succeed"
+  in
+  let peers_in_topic =
+    GS.Internal_for_tests.get_peers_in_topic_mesh "topic0" state
+  in
+  (* All [degree_optimal / 2] fanout peers should have been added to the mesh,
+     along with [degree_optimal / 2] more from the pool. *)
+  Check.(
+    (List.length peers_in_topic = limits.degree_optimal)
+      int
+      ~error_msg:"Expected %R, got %L"
+      ~__LOC__) ;
+  List.iter
+    (fun peer ->
+      if not @@ List.mem ~equal:Int.equal peer peers_in_topic then
+        Test.fail
+          "Fanout peer %d should be included in the topic mesh [%a]"
+          peer
+          (Format.pp_print_list
+             ~pp_sep:(fun fmt () -> Format.pp_print_string fmt "; ")
+             Format.pp_print_int)
+          peers_in_topic
+      else ())
+    fanout_peers ;
+  (* There should be [degree_optimal] additional GRAFT messages. *)
+  Check.(
+    (List.length to_graft = limits.degree_optimal)
+      int
+      ~error_msg:"Expected %R, got %L"
+      ~__LOC__) ;
+  (* Check that the fanout map has been cleared.  *)
+  let fanout_peers = GS.Internal_for_tests.get_fanout_peers "topic0" state in
+  Check.(
+    (List.length fanout_peers = 0) int ~error_msg:"Expected %R, got %L" ~__LOC__) ;
+  unit
+
 let register rng limits parameters =
   test_ignore_graft_from_unknown_topic rng limits parameters ;
-  test_handle_received_subscriptions rng limits parameters
+  test_handle_received_subscriptions rng limits parameters ;
+  test_join_adds_peers_to_mesh rng limits parameters ;
+  test_join_adds_fanout_to_mesh rng limits parameters
