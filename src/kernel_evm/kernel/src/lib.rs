b362f1e61611686a62e862f7477091b038eee6e1
Rodi-Can Bozman <rodi.bozman@functori.com>
2023-03-24 11:10:42 +0100

    EVM/Kernel: move all sources of panics into main

--- src/kernel_evm/kernel/src/lib.rs
+++ src/kernel_evm/kernel/src/lib.rs
@@ -10,6 +10,7 @@ use debug::debug_msg;
 use kernel::kernel_entry;
 
 use crate::blueprint::{fetch, Queue};
+use crate::error::Error;
 use crate::storage::{read_smart_rollup_address, store_smart_rollup_address};
 
 mod block;
@@ -33,8 +34,11 @@ pub fn stage_one<Host: Runtime + RawRollupCore>(
     queue
 }
 
-pub fn stage_two<Host: Runtime + RawRollupCore>(host: &mut Host, queue: Queue) {
-    block::produce(host, queue);
+pub fn stage_two<Host: Runtime + RawRollupCore>(
+    host: &mut Host,
+    queue: Queue,
+) -> Result<(), Error> {
+    block::produce(host, queue)?;
 
     if let Ok(L2Block {
         number,
@@ -49,37 +53,56 @@ pub fn stage_two<Host: Runtime + RawRollupCore>(host: &mut Host, queue: Queue) {
             transactions.len()
         )
     }
+
+    Ok(())
 }
 
-fn retrieve_smart_rollup_address<Host: Runtime + RawRollupCore>(host: &mut Host) -> [u8; 20] {
+fn retrieve_smart_rollup_address<Host: Runtime + RawRollupCore>(
+    host: &mut Host,
+) -> Result<[u8; 20], Error> {
     match read_smart_rollup_address(host) {
-        Ok(smart_rollup_address) => smart_rollup_address,
+        Ok(smart_rollup_address) => Ok(smart_rollup_address),
         Err(_) => {
-            let address = Runtime::reveal_metadata(host).unwrap().raw_rollup_address;
-            store_smart_rollup_address(host, &address).unwrap();
-            address
+            let rollup_metadata = Runtime::reveal_metadata(host).map_err(|_| {
+                debug_msg!(host; "Error while storing the transactions receipts.");
+                Error::Generic
+            })?;
+            let address = rollup_metadata.raw_rollup_address;
+            store_smart_rollup_address(host, &address).map_err(|_| {
+                debug_msg!(host; "Error while storing the smart rollup's address.");
+                Error::Generic
+            })?;
+            Ok(address)
         }
     }
 }
 
-fn genesis_initialisation<Host: Runtime + RawRollupCore>(host: &mut Host) {
-    let block_path = storage::block_path(0).unwrap();
+fn genesis_initialisation<Host: Runtime + RawRollupCore>(host: &mut Host) -> Result<(), Error> {
+    let block_path = storage::block_path(0).map_err(|_| {
+        debug_msg!(host; "Error while retrieving genesis block's path.");
+        Error::Generic
+    })?;
     match Runtime::store_has(host, &block_path) {
-        Ok(Some(_)) => (),
-        _ => genesis::init_block(host).unwrap_or_else(|_| {
-            panic!("Error while initializing block genesis: stopping the daemon.")
-        }),
+        Ok(Some(_)) => Ok(()),
+        _ => genesis::init_block(host),
     }
 }
 
 pub fn main<Host: Runtime + RawRollupCore>(host: &mut Host) {
-    let smart_rollup_address = retrieve_smart_rollup_address(host);
+    let smart_rollup_address = match retrieve_smart_rollup_address(host) {
+        Ok(smart_rollup_address) => smart_rollup_address,
+        Err(_) => panic!("Error while retrieving smart rollup's address: stopping the daemon."),
+    };
 
-    genesis_initialisation(host);
+    if genesis_initialisation(host).is_err() {
+        panic!("Error while initializing block genesis: stopping the daemon.")
+    }
 
     let queue = stage_one(host, smart_rollup_address);
 
-    stage_two(host, queue)
+    if stage_two(host, queue).is_err() {
+        panic!("Error during stage two: stopping the daemon.")
+    }
 }
 
 kernel_entry!(main);
