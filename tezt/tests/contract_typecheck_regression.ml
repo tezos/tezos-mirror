4a4412512f8c42ec8bd7728e9f84969037f6d65c
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2023-03-27 09:03:05 +0200

    Tezt/Michelson: move iteration over typechecked scripts

--- tezt/tests/contract_typecheck_regression.ml
+++ tezt/tests/contract_typecheck_regression.ml
@@ -38,52 +38,55 @@ let hooks =
     ~replace_variables:Fun.id
     ()
 
-let test_typecheck_contract protocol script =
-  Protocol.register_regression_test
-    ~__FILE__
-    ~title:(sf "Tc %s" (Michelson_script.name_s script))
-    ~tags:["client"; "michelson"; "typechecking"]
-    (fun _protocol ->
-      (* Register constants for scripts that require it *)
-      let constants =
-        match Michelson_script.name script with
-        | ["mini_scenarios"; "999_constant"] -> ["999"]
-        | ["mini_scenarios"; "constant_unit"] -> ["Unit"; "unit"]
-        | ["mini_scenarios"; "constant_entrypoints"] -> ["unit"]
-        | _ -> []
-      in
-      let* client =
-        match constants with
-        | [] -> return (Client.create_with_mode Mockup)
-        | constants ->
-            let* client = Client.init_mockup ~protocol () in
-            let* (_exprs : string list) =
-              Lwt_list.map_s
-                (fun value ->
-                  Client.register_global_constant
-                    ~src:Constant.bootstrap1.alias
-                    ~value
-                    ~burn_cap:Tez.one
-                    client)
-                constants
-            in
-            return client
-      in
-      Client.typecheck_script
-        ~scripts:[Michelson_script.path script]
-        ~protocol_hash:(Protocol.hash protocol)
-        ~hooks
-        ~no_base_dir_warnings:true
-        ~details:true
-        client)
-    [protocol]
+let test_typecheck_contract protocol scripts =
+  List.iter
+    (fun script ->
+      Protocol.register_regression_test
+        ~__FILE__
+        ~title:(sf "Tc %s" (Michelson_script.name_s script))
+        ~tags:["client"; "michelson"; "typechecking"]
+        (fun _protocol ->
+          (* Register constants for scripts that require it *)
+          let constants =
+            match Michelson_script.name script with
+            | ["mini_scenarios"; "999_constant"] -> ["999"]
+            | ["mini_scenarios"; "constant_unit"] -> ["Unit"; "unit"]
+            | ["mini_scenarios"; "constant_entrypoints"] -> ["unit"]
+            | _ -> []
+          in
+          let* client =
+            match constants with
+            | [] -> return (Client.create_with_mode Mockup)
+            | constants ->
+                let* client = Client.init_mockup ~protocol () in
+                let* (_exprs : string list) =
+                  Lwt_list.map_s
+                    (fun value ->
+                      Client.register_global_constant
+                        ~src:Constant.bootstrap1.alias
+                        ~value
+                        ~burn_cap:Tez.one
+                        client)
+                    constants
+                in
+                return client
+          in
+          Client.typecheck_script
+            ~scripts:[Michelson_script.path script]
+            ~protocol_hash:(Protocol.hash protocol)
+            ~hooks
+            ~no_base_dir_warnings:true
+            ~details:true
+            client)
+        [protocol])
+    scripts
 
 let test_typecheck protocols =
   List.iter
     (fun protocol ->
       (* Type check regression tests for all well-typed contracts *)
-      List.iter
-        (test_typecheck_contract protocol)
+      test_typecheck_contract
+        protocol
         (Michelson_script.find_all_well_typed protocol))
     protocols
 
