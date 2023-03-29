OKEAMAH/Git

Scoru: Move Enocdings_util to lib_tree_encoding

src/bin_wasm_debugger/repl_helpers.ml
@@ -23,6 +23,8 @@
 (*                                                                           *)
 (*****************************************************************************)
 
+open Tezos_scoru_wasm
+
 (* [error loc category msg] fails with the location of an error and a message,
    returned by either the parser of the typechecker of the WASM reference
    interpreter. *)
@@ -80,9 +82,9 @@ let find_key_in_durable tree key =
 let print_durable ?(depth = 10) ?(show_values = true) ?(path = []) tree =
   let open Lwt_syntax in
   let durable_path = "durable" :: path in
-  let* path_exists = Encodings_util.Context.Tree.mem_tree tree durable_path in
+  let* path_exists = Encodings_util.Tree.mem_tree tree durable_path in
   if path_exists then
-    Encodings_util.Context.Tree.fold
+    Encodings_util.Tree.fold
       ~depth:(`Le depth)
       tree
       ("durable" :: path)
@@ -93,7 +95,7 @@ let print_durable ?(depth = 10) ?(show_values = true) ?(path = []) tree =
         (* If we need to show the values, we show every keys, even the root and
            '@'. *)
         if show_values then
-          let+ value = Encodings_util.Context.Tree.find tree [] in
+          let+ value = Encodings_util.Tree.find tree [] in
           let value = Option.value ~default:(Bytes.create 0) value in
           Format.printf "/%s\n  %a\n%!" full_key Hex.pp (Hex.of_bytes value)
         else if key <> [] && key <> ["@"] then

src/lib_scoru_wasm/bench/pvm_instance.mli
@@ -26,7 +26,7 @@
 (** PVM instance used in benchmark*)
 module Wasm :
   Tezos_scoru_wasm.Wasm_pvm_sig.S
-    with type tree = Tezos_scoru_wasm_helpers.Encodings_util.Context.tree
+    with type tree = Tezos_scoru_wasm.Encodings_util.Tree.tree
 
 module Wasm_fast_vm : Tezos_scoru_wasm.Wasm_vm_sig.S
 

src/lib_scoru_wasm/test/test_durable_shapshot.ml
@@ -33,7 +33,7 @@
 
 open Tztest
 open QCheck2
-open Encodings_util
+open Tezos_scoru_wasm.Encodings_util
 open Durable_snapshot_util
 open Durable_operations_generator
 open Probability_utils

src/lib_scoru_wasm/test/test_wasm_pvm.ml
@@ -33,9 +33,9 @@
 *)
 
 open Tezos_scoru_wasm
+open Tezos_scoru_wasm.Encodings_util
 open Wasm_utils
 open Tztest_helper
-open Encodings_util
 
 let should_boot_unreachable_kernel ~version ~batch_size kernel =
   let open Lwt_syntax in

tezos/tezos-mirror

src/bin_wasm_debugger/repl_helpers.ml
@@ -23,6 +23,8 @@
 (*                                                                           *)
 (*****************************************************************************)
 
+open Tezos_scoru_wasm_helpers
+
 (* [error loc category msg] fails with the location of an error and a message,
    returned by either the parser of the typechecker of the WASM reference
    interpreter. *)
@@ -80,9 +82,9 @@ let find_key_in_durable tree key =
 let print_durable ?(depth = 10) ?(show_values = true) ?(path = []) tree =
   let open Lwt_syntax in
   let durable_path = "durable" :: path in
-  let* path_exists = Encodings_util.Context.Tree.mem_tree tree durable_path in
+  let* path_exists = Encodings_util.Tree.mem_tree tree durable_path in
   if path_exists then
-    Encodings_util.Context.Tree.fold
+    Encodings_util.Tree.fold
       ~depth:(`Le depth)
       tree
       ("durable" :: path)
@@ -93,7 +95,7 @@ let print_durable ?(depth = 10) ?(show_values = true) ?(path = []) tree =
         (* If we need to show the values, we show every keys, even the root and
            '@'. *)
         if show_values then
-          let+ value = Encodings_util.Context.Tree.find tree [] in
+          let+ value = Encodings_util.Tree.find tree [] in
           let value = Option.value ~default:(Bytes.create 0) value in
           Format.printf "/%s\n  %a\n%!" full_key Hex.pp (Hex.of_bytes value)
         else if key <> [] && key <> ["@"] then

src/lib_scoru_wasm/bench/pvm_instance.mli
@@ -26,7 +26,7 @@
 (** PVM instance used in benchmark*)
 module Wasm :
   Tezos_scoru_wasm.Wasm_pvm_sig.S
-    with type tree = Tezos_scoru_wasm_helpers.Encodings_util.Context.tree
+    with type tree = Tezos_scoru_wasm_helpers.Encodings_util.Tree.tree
 
 module Wasm_fast_vm : Tezos_scoru_wasm.Wasm_vm_sig.S
 

src/lib_scoru_wasm/test/test_durable_shapshot.ml
@@ -33,7 +33,7 @@
 
 open Tztest
 open QCheck2
-open Encodings_util
+open Tezos_scoru_wasm_helpers.Encodings_util
 open Durable_snapshot_util
 open Probability_utils
 

src/lib_scoru_wasm/test/test_wasm_pvm.ml
@@ -33,9 +33,9 @@
 *)
 
 open Tezos_scoru_wasm
+open Tezos_scoru_wasm_helpers.Encodings_util
 open Wasm_utils
 open Tztest_helper
-open Encodings_util
 
 let should_boot_unreachable_kernel ~version ~batch_size kernel =
   let open Lwt_syntax in