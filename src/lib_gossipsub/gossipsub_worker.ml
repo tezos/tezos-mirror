4a5e23f80b99111576861cd9f316f070d119471d
iguerNL@Functori <iguer@functori.com>
2023-04-04 17:01:56 +0200

    DAL/GS: refactor apply_p2p_event: add apply_p2p_message

--- src/lib_gossipsub/gossipsub_worker.ml
+++ src/lib_gossipsub/gossipsub_worker.ml
@@ -146,6 +146,21 @@ module Make (C : Gossipsub_intf.WORKER_CONFIGURATION) :
         ignore output ;
         gossip_state
 
+  (** Handling messages received from the P2P network. *)
+  let apply_p2p_message ~emit_p2p_msg ~emit_app_msg gossip_state from_peer =
+    function
+    | Publish {message; topic; message_id} ->
+        let publish =
+          {GS.sender = Some from_peer; topic; message_id; message}
+        in
+        GS.publish publish gossip_state
+        |> handle_full_message ~emit_p2p_msg ~emit_app_msg publish
+    | _ ->
+        (* FIXME: https://gitlab.com/tezos/tezos/-/issues/5164
+
+           Handle received p2p messages. *)
+        assert false
+
   (** Handling events received from P2P layer. *)
   let apply_p2p_event ~emit_p2p_msg ~emit_app_msg gossip_state = function
     | New_connection {peer; direct; outbound} ->
@@ -164,19 +179,13 @@ module Make (C : Gossipsub_intf.WORKER_CONFIGURATION) :
            Handle disconnection's output *)
         ignore output ;
         gossip_state
-    | In_message {from_peer; p2p_message = Publish {message; topic; message_id}}
-      ->
-        let publish =
-          {GS.sender = Some from_peer; topic; message_id; message}
-        in
-        GS.publish publish gossip_state
-        |> handle_full_message ~emit_p2p_msg ~emit_app_msg publish
-    | In_message m ->
-        (* FIXME: https://gitlab.com/tezos/tezos/-/issues/5164
-
-           Handle received p2p messages. *)
-        ignore (In_message m) ;
-        assert false
+    | In_message {from_peer; p2p_message} ->
+        apply_p2p_message
+          ~emit_p2p_msg
+          ~emit_app_msg
+          gossip_state
+          from_peer
+          p2p_message
 
   (** This is the main function of the worker. It interacts with the Gossipsub
       automaton given an event. The function possibly sends messages to the P2P
