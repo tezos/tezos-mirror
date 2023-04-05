OKEAMAH/Git

Dac/Node: support all operating modes

src/lib_dac_node/RPC_server.ml
@@ -220,47 +220,99 @@ let register_get_certificate ctx dac_plugin =
     (RPC_services.get_certificate dac_plugin)
     (fun root_hash () () -> handle_get_certificate ctx root_hash)
 
-let register dac_plugin node_context cctxt dac_public_keys_opt dac_sk_uris
-    hash_streamer =
-  let page_store = Node_context.get_page_store node_context in
-  Tezos_rpc.Directory.empty
-  |> register_serialize_dac_store_preimage
-       dac_plugin
-       cctxt
-       dac_sk_uris
-       page_store
-       hash_streamer
-  |> register_verify_external_message_signature dac_plugin dac_public_keys_opt
-  |> register_retrieve_preimage dac_plugin page_store
-  |> register_monitor_root_hashes dac_plugin hash_streamer
-  |> register_store_dac_member_signature node_context dac_plugin cctxt
-  (* TODO: https://gitlab.com/tezos/tezos/-/issues/4934
-     Once profiles are implemented, registration of the coordinator's
-     "/preimage" endpoint should be moved out of the [start_legacy]. *)
-  |> register_coordinator_preimage_endpoint dac_plugin hash_streamer page_store
-  |> register_get_certificate node_context dac_plugin
+module Coordinator = struct
+  let dynamic_rpc_dir dac_plugin
+      (coordinator_node_ctxt :
+        Operating_modes.coordinator Node_context.node_ctxt) =
+    let modal_node_ctxt =
+      match Node_context.mode coordinator_node_ctxt with
+      | Coordinator modal_node_ctxt -> modal_node_ctxt
+    in
 
-(* TODO: https://gitlab.com/tezos/tezos/-/issues/4750
-   Move this to RPC_server.Legacy once all operating modes are supported. *)
-let start_legacy ~rpc_address ~rpc_port ~threshold cctxt ctxt dac_pks_opt
-    dac_sk_uris =
+    let hash_streamer =
+      Node_context.Coordinator.hash_streamer modal_node_ctxt
+    in
+    let node_ctxt = Node_context.Ex coordinator_node_ctxt in
+    let pks_opt = Node_context.Coordinator.pks_opt modal_node_ctxt in
+    let page_store = Node_context.get_page_store node_ctxt in
+    let cctxt = Node_context.get_tezos_node_cctxt node_ctxt in
+
+    Tezos_rpc.Directory.empty
+    |> register_coordinator_preimage_endpoint
+         dac_plugin
+         hash_streamer
+         page_store
+    |> register_verify_external_message_signature dac_plugin pks_opt
+    |> register_retrieve_preimage dac_plugin page_store
+    |> register_monitor_root_hashes dac_plugin hash_streamer
+    |> register_store_dac_member_signature node_ctxt dac_plugin cctxt
+    |> register_get_certificate node_ctxt dac_plugin
+end
+
+module Committee_member = struct
+  let dynamic_rpc_dir dac_plugin
+      (committee_member_node_ctxt : 'a Node_context.node_ctxt) =
+    let node_ctxt = Node_context.Ex committee_member_node_ctxt in
+    let page_store = Node_context.get_page_store node_ctxt in
+    Tezos_rpc.Directory.empty
+    |> register_retrieve_preimage dac_plugin page_store
+end
+
+module Observer = struct
+  let dynamic_rpc_dir dac_plugin
+      (observer_node_ctxt : 'a Node_context.node_ctxt) =
+    let node_ctxt = Node_context.Ex observer_node_ctxt in
+    let page_store = Node_context.get_page_store node_ctxt in
+    Tezos_rpc.Directory.empty
+    |> register_retrieve_preimage dac_plugin page_store
+end
+
+module Legacy = struct
+  let dynamic_rpc_dir dac_plugin (legacy_node_ctxt : 'a Node_context.node_ctxt)
+      =
+    let modal_node_ctxt =
+      match Node_context.mode legacy_node_ctxt with
+      | Legacy modal_node_ctxt -> modal_node_ctxt
+    in
+    let node_ctxt = Node_context.Ex legacy_node_ctxt in
+    let hash_streamer = Node_context.Legacy.hash_streamer modal_node_ctxt in
+    let pks_opt = Node_context.Legacy.pks_opt modal_node_ctxt in
+    let sk_uris_opt = Node_context.Legacy.sk_uris_opt modal_node_ctxt in
+    let page_store = Node_context.get_page_store node_ctxt in
+    let cctxt = Node_context.get_tezos_node_cctxt node_ctxt in
+    Tezos_rpc.Directory.empty
+    |> register_serialize_dac_store_preimage
+         dac_plugin
+         cctxt
+         sk_uris_opt
+         page_store
+         hash_streamer
+    |> register_verify_external_message_signature dac_plugin pks_opt
+    |> register_retrieve_preimage dac_plugin page_store
+    |> register_monitor_root_hashes dac_plugin hash_streamer
+    |> register_store_dac_member_signature node_ctxt dac_plugin cctxt
+    |> register_get_certificate node_ctxt dac_plugin
+end
+
+let start ~rpc_address ~rpc_port node_ctxt =
   let open Lwt_syntax in
+  let (Node_context.Ex modal_node_ctxt) = node_ctxt in
+  let register_dynamic_rpc dac_plugin =
+    match Node_context.mode modal_node_ctxt with
+    | Coordinator _ -> Coordinator.dynamic_rpc_dir dac_plugin modal_node_ctxt
+    | Committee_member _ ->
+        Committee_member.dynamic_rpc_dir dac_plugin modal_node_ctxt
+    | Observer _ -> Observer.dynamic_rpc_dir dac_plugin modal_node_ctxt
+    | Legacy _ -> Legacy.dynamic_rpc_dir dac_plugin modal_node_ctxt
+  in
   let dir =
     Tezos_rpc.Directory.register_dynamic_directory
       Tezos_rpc.Directory.empty
       Tezos_rpc.Path.open_root
       (fun () ->
-        match Node_context.get_status ctxt with
-        | Ready {dac_plugin = (module Dac_plugin); hash_streamer} ->
-            let _threshold = threshold in
-            Lwt.return
-              (register
-                 (module Dac_plugin)
-                 ctxt
-                 cctxt
-                 dac_pks_opt
-                 dac_sk_uris
-                 hash_streamer)
+        match Node_context.get_status node_ctxt with
+        | Ready {dac_plugin = (module Dac_plugin)} ->
+            Lwt.return (register_dynamic_rpc (module Dac_plugin))
         | Starting -> Lwt.return Tezos_rpc.Directory.empty)
   in
   let rpc_address = P2p_addr.of_string_exn rpc_address in

src/lib_dac_node/daemon.ml
@@ -178,55 +178,28 @@ let daemonize handlers =
 let run ~data_dir cctxt =
   let open Lwt_result_syntax in
   let*! () = Event.(emit starting_node) () in
-  let* (Configuration.Ex {rpc_address; rpc_port; reveal_data_dir; mode; _} as
-       config) =
+  let* (Configuration.Ex {rpc_address; rpc_port; reveal_data_dir; _} as config)
+      =
     Configuration.load ~data_dir
   in
   let* () = Dac_manager.Storage.ensure_reveal_data_dir_exists reveal_data_dir in
-  let* addresses, threshold, coordinator_cctxt_opt =
-    match mode with
-    | Configuration.Modal.Legacy configuration ->
-        let committee_members_addresses =
-          Configuration.Legacy.committee_members_addresses configuration
-        in
-        let threshold = Configuration.Legacy.threshold configuration in
-        let dac_cctxt_config =
-          Configuration.Legacy.dac_cctxt_config configuration
-        in
-        return (committee_members_addresses, threshold, dac_cctxt_config)
-    | Coordinator _ -> tzfail @@ Mode_not_supported "coordinator"
-    | Committee_member _ -> tzfail @@ Mode_not_supported "committee_member"
-    | Observer _ -> tzfail @@ Mode_not_supported "observer"
+  let* ctxt = Node_context.init config cctxt in
+  let (Node_context.Ex modal_node_ctxt) = ctxt in
+  let coordinator_cctxt_opt =
+    match Node_context.mode modal_node_ctxt with
+    | Node_context.Modal.Coordinator _ -> None
+    | Node_context.Modal.Committee_member committee_member_ctxt ->
+        Some
+          (Node_context.Committee_member.coordinator_cctxt
+             committee_member_ctxt)
+    | Node_context.Modal.Observer observer_ctxt ->
+        Some (Node_context.Observer.coordinator_cctxt observer_ctxt)
+    | Node_context.Modal.Legacy legacy_ctxt ->
+        Node_context.Legacy.coordinator_cctxt legacy_ctxt
   in
   (* TODO: https://gitlab.com/tezos/tezos/-/issues/4725
      Stop DAC node when in Legacy mode, if threshold is not reached. *)
-  let* dac_accounts =
-    Wallet.Legacy.get_all_committee_members_keys addresses ~threshold cctxt
-  in
-  let dac_pks_opt, dac_sk_uris =
-    dac_accounts
-    |> List.map (fun Wallet.Legacy.{pk_opt; sk_uri_opt; _} ->
-           (pk_opt, sk_uri_opt))
-    |> List.split
-  in
-  let coordinator_cctxt_opt =
-    Option.map
-      (fun Configuration.{host; port} ->
-        Dac_node_client.make_unix_cctxt ~scheme:"http" ~host ~port)
-      coordinator_cctxt_opt
-  in
-  let* ctxt = Node_context.init config cctxt coordinator_cctxt_opt in
-  let* rpc_server =
-    RPC_server.(
-      start_legacy
-        ~rpc_address
-        ~rpc_port
-        ~threshold
-        cctxt
-        ctxt
-        dac_pks_opt
-        dac_sk_uris)
-  in
+  let* rpc_server = RPC_server.start ~rpc_address ~rpc_port ctxt in
   let _ = RPC_server.install_finalizer rpc_server in
   let*! () = Event.(emit rpc_server_is_ready (rpc_address, rpc_port)) in
   (* Start daemon to resolve current protocol plugin *)

tezos/tezos-mirror

src/lib_dac_node/RPC_server.ml
@@ -119,22 +119,6 @@ let handle_get_verify_signature dac_plugin public_keys_opt encoded_l1_message =
 let handle_get_preimage dac_plugin page_store hash =
   Page_store.Filesystem.load dac_plugin page_store hash
 
-module Coordinator = struct
-  let handle_post_preimage dac_plugin page_store hash_streamer payload =
-    let open Lwt_result_syntax in
-    let* root_hash =
-      Pages_encoding.Merkle_tree.V0.Filesystem.serialize_payload
-        dac_plugin
-        ~page_store
-        payload
-    in
-    let () = Data_streamer.publish hash_streamer root_hash in
-    let*! () =
-      Event.emit_root_hash_pushed_to_data_streamer dac_plugin root_hash
-    in
-    return root_hash
-end
-
 (* Handler for subscribing to the streaming of root hashes via
    GET monitor/root_hashes RPC call. *)
 let handle_monitor_root_hashes hash_streamer =
@@ -210,17 +194,6 @@ let register_put_dac_member_signature ctx dac_plugin cctxt =
         cctxt
         dac_member_signature)
 
-let register_coordinator_post_preimage dac_plugin hash_streamer page_store =
-  add_service
-    Tezos_rpc.Directory.register0
-    (RPC_services.Coordinator.post_preimage dac_plugin)
-    (fun () payload ->
-      Coordinator.handle_post_preimage
-        dac_plugin
-        page_store
-        hash_streamer
-        payload)
-
 let register_get_certificate ctx dac_plugin =
   add_service
     Tezos_rpc.Directory.register1
@@ -228,7 +201,7 @@ let register_get_certificate ctx dac_plugin =
     (fun root_hash () () -> handle_get_certificate ctx root_hash)
 
 let register_get_missing_page ctx dac_plugin =
-  match (Node_context.get_config ctx).mode with
+  match Node_context.mode ctx with
   | Legacy _ | Observer _ ->
       add_service
         Tezos_rpc.Directory.register1
@@ -240,48 +213,118 @@ let register_get_missing_page ctx dac_plugin =
           handle_get_missing_page cctxt page_store dac_plugin root_hash)
   | Coordinator _ | Committee_member _ -> Fun.id
 
-let register dac_plugin node_context cctxt dac_public_keys_opt dac_sk_uris
-    hash_streamer =
-  let page_store = Node_context.get_page_store node_context in
-  Tezos_rpc.Directory.empty
-  |> register_post_store_preimage
-       dac_plugin
-       cctxt
-       dac_sk_uris
-       page_store
-       hash_streamer
-  |> register_get_verify_signature dac_plugin dac_public_keys_opt
-  |> register_get_preimage dac_plugin page_store
-  |> register_monitor_root_hashes dac_plugin hash_streamer
-  |> register_put_dac_member_signature node_context dac_plugin cctxt
-  (* TODO: https://gitlab.com/tezos/tezos/-/issues/4934
-     Once profiles are implemented, registration of the coordinator's
-     "/preimage" endpoint should be moved out of the [start_legacy]. *)
-  |> register_coordinator_post_preimage dac_plugin hash_streamer page_store
-  |> register_get_certificate node_context dac_plugin
-  |> register_get_missing_page node_context dac_plugin
+module Coordinator = struct
+  let handle_post_preimage dac_plugin page_store hash_streamer payload =
+    let open Lwt_result_syntax in
+    let* root_hash =
+      Pages_encoding.Merkle_tree.V0.Filesystem.serialize_payload
+        dac_plugin
+        ~page_store
+        payload
+    in
+    let () = Data_streamer.publish hash_streamer root_hash in
+    let*! () =
+      Event.emit_root_hash_pushed_to_data_streamer dac_plugin root_hash
+    in
+    return root_hash
 
-(* TODO: https://gitlab.com/tezos/tezos/-/issues/4750
-   Move this to RPC_server.Legacy once all operating modes are supported. *)
-let start_legacy ~rpc_address ~rpc_port ~threshold cctxt ctxt dac_pks_opt
-    dac_sk_uris =
+  let register_coordinator_post_preimage dac_plugin hash_streamer page_store =
+    add_service
+      Tezos_rpc.Directory.register0
+      (RPC_services.Coordinator.post_preimage dac_plugin)
+      (fun () payload ->
+        handle_post_preimage dac_plugin page_store hash_streamer payload)
+
+  let dynamic_rpc_dir dac_plugin (node_ctxt : Node_context.t) =
+    let modal_node_ctxt =
+      match Node_context.mode node_ctxt with
+      | Coordinator modal_node_ctxt -> modal_node_ctxt
+      (* We only pass [Node_context.t] values whose mode is [Coordinator _] to
+         this function. *)
+      | _ -> assert false
+    in
+
+    let hash_streamer =
+      modal_node_ctxt.Node_context.Coordinator.hash_streamer
+    in
+    let public_keys_opt =
+      Node_context.Coordinator.public_keys_opt modal_node_ctxt
+    in
+    let page_store = Node_context.get_page_store node_ctxt in
+    let cctxt = Node_context.get_tezos_node_cctxt node_ctxt in
+
+    Tezos_rpc.Directory.empty
+    |> register_coordinator_post_preimage dac_plugin hash_streamer page_store
+    |> register_get_verify_signature dac_plugin public_keys_opt
+    |> register_get_preimage dac_plugin page_store
+    |> register_monitor_root_hashes dac_plugin hash_streamer
+    |> register_put_dac_member_signature node_ctxt dac_plugin cctxt
+    |> register_get_certificate node_ctxt dac_plugin
+end
+
+module Committee_member = struct
+  let dynamic_rpc_dir dac_plugin (node_ctxt : Node_context.t) =
+    let page_store = Node_context.get_page_store node_ctxt in
+    Tezos_rpc.Directory.empty |> register_get_preimage dac_plugin page_store
+end
+
+module Observer = struct
+  let dynamic_rpc_dir dac_plugin (node_ctxt : Node_context.t) =
+    let page_store = Node_context.get_page_store node_ctxt in
+    Tezos_rpc.Directory.empty
+    |> register_get_preimage dac_plugin page_store
+    |> register_get_missing_page node_ctxt dac_plugin
+end
+
+module Legacy = struct
+  let dynamic_rpc_dir dac_plugin (node_ctxt : Node_context.t) =
+    let modal_node_ctxt =
+      match Node_context.mode node_ctxt with
+      | Legacy modal_node_ctxt -> modal_node_ctxt
+      (* We only pass [Node_context.t] values whose mode is [Coordinator _] to
+         this function. *)
+      | _ -> assert false
+    in
+    let hash_streamer = modal_node_ctxt.Node_context.Legacy.hash_streamer in
+    let public_keys_opt = Node_context.Legacy.public_keys_opt modal_node_ctxt in
+    let secret_key_uris_opt =
+      Node_context.Legacy.secret_key_uris_opt modal_node_ctxt
+    in
+    let page_store = Node_context.get_page_store node_ctxt in
+    let cctxt = Node_context.get_tezos_node_cctxt node_ctxt in
+    Tezos_rpc.Directory.empty
+    |> register_post_store_preimage
+         dac_plugin
+         cctxt
+         secret_key_uris_opt
+         page_store
+         hash_streamer
+    |> register_get_verify_signature dac_plugin public_keys_opt
+    |> register_get_preimage dac_plugin page_store
+    |> register_monitor_root_hashes dac_plugin hash_streamer
+    |> register_put_dac_member_signature node_ctxt dac_plugin cctxt
+    |> register_get_certificate node_ctxt dac_plugin
+    |> register_get_missing_page node_ctxt dac_plugin
+end
+
+let start ~rpc_address ~rpc_port node_ctxt =
   let open Lwt_syntax in
+  let register_dynamic_rpc dac_plugin =
+    match Node_context.mode node_ctxt with
+    | Coordinator _ -> Coordinator.dynamic_rpc_dir dac_plugin node_ctxt
+    | Committee_member _ ->
+        Committee_member.dynamic_rpc_dir dac_plugin node_ctxt
+    | Observer _ -> Observer.dynamic_rpc_dir dac_plugin node_ctxt
+    | Legacy _ -> Legacy.dynamic_rpc_dir dac_plugin node_ctxt
+  in
   let dir =
     Tezos_rpc.Directory.register_dynamic_directory
       Tezos_rpc.Directory.empty
       Tezos_rpc.Path.open_root
       (fun () ->
-        match Node_context.get_status ctxt with
-        | Ready {dac_plugin = (module Dac_plugin); hash_streamer} ->
-            let _threshold = threshold in
-            Lwt.return
-              (register
-                 (module Dac_plugin)
-                 ctxt
-                 cctxt
-                 dac_pks_opt
-                 dac_sk_uris
-                 hash_streamer)
+        match Node_context.get_status node_ctxt with
+        | Ready {dac_plugin = (module Dac_plugin)} ->
+            Lwt.return (register_dynamic_rpc (module Dac_plugin))
         | Starting -> Lwt.return Tezos_rpc.Directory.empty)
   in
   let rpc_address = P2p_addr.of_string_exn rpc_address in

src/lib_dac_node/daemon.ml
@@ -265,64 +265,26 @@ let run ~data_dir cctxt =
   let open Lwt_result_syntax in
   let*! () = Event.(emit starting_node) () in
   let* (Configuration.
-          {rpc_address; rpc_port; reveal_data_dir; mode; data_dir = _} as
+          {rpc_address; rpc_port; reveal_data_dir; mode = _; data_dir = _} as
        config) =
     Configuration.load ~data_dir
   in
   let* () = Dac_manager.Storage.ensure_reveal_data_dir_exists reveal_data_dir in
-  let* addresses, threshold, coordinator_cctxt_opt, committee_member_address_opt
-      =
-    match mode with
-    | Legacy configuration ->
-        let committee_members_addresses =
-          Configuration.Legacy.committee_members_addresses configuration
-        in
-        let threshold = Configuration.Legacy.threshold configuration in
-        let dac_cctxt_config =
-          Configuration.Legacy.dac_cctxt_config configuration
-        in
-        let committee_member_address_opt =
-          Configuration.Legacy.committee_member_address_opt configuration
-        in
-        return
-          ( committee_members_addresses,
-            threshold,
-            dac_cctxt_config,
-            committee_member_address_opt )
-    | Coordinator _ -> tzfail @@ Mode_not_supported "coordinator"
-    | Committee_member _ -> tzfail @@ Mode_not_supported "committee_member"
-    | Observer _ -> tzfail @@ Mode_not_supported "observer"
+  let* ctxt = Node_context.init config cctxt in
+  let coordinator_cctxt_opt =
+    match Node_context.mode ctxt with
+    | Node_context.Coordinator _ -> None
+    | Committee_member committee_member_ctxt ->
+        Some
+          (Node_context.Committee_member.coordinator_cctxt
+             committee_member_ctxt)
+    | Observer observer_ctxt ->
+        Some (Node_context.Observer.coordinator_cctxt observer_ctxt)
+    | Legacy legacy_ctxt -> Node_context.Legacy.coordinator_cctxt legacy_ctxt
   in
   (* TODO: https://gitlab.com/tezos/tezos/-/issues/4725
      Stop DAC node when in Legacy mode, if threshold is not reached. *)
-  let* committee_members =
-    get_all_committee_members_keys addresses ~threshold cctxt
-  in
-  let dac_pks_opt, dac_sk_uris =
-    committee_members
-    |> List.map
-         (fun Wallet_account.Legacy.{public_key_opt; secret_key_uri_opt; _} ->
-           (public_key_opt, secret_key_uri_opt))
-    |> List.split
-  in
-  let coordinator_cctxt_opt =
-    Option.map
-      (fun Configuration.{host; port} ->
-        Dac_node_client.make_unix_cctxt ~scheme:"http" ~host ~port)
-      coordinator_cctxt_opt
-  in
-  let* ctxt = Node_context.init config cctxt coordinator_cctxt_opt in
-  let* rpc_server =
-    RPC_server.(
-      start_legacy
-        ~rpc_address
-        ~rpc_port
-        ~threshold
-        cctxt
-        ctxt
-        dac_pks_opt
-        dac_sk_uris)
-  in
+  let* rpc_server = RPC_server.start ~rpc_address ~rpc_port ctxt in
   let _ = RPC_server.install_finalizer rpc_server in
   let*! () = Event.(emit rpc_server_is_ready (rpc_address, rpc_port)) in
   (* Start daemon to resolve current protocol plugin *)
@@ -336,7 +298,7 @@ let run ~data_dir cctxt =
         Wallet_account.Legacy.of_committee_member_address
           committee_member_address
           cctxt)
-      committee_member_address_opt
+      None
   in
   match coordinator_cctxt_opt with
   | Some coordinator_cctxt ->