cdca088b5d5249e503bc327523a7348e3b600731
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2023-04-06 16:13:07 +0200

    Alcotezt: remove redundant [protocol]

--- src/proto_016_PtMumbai/lib_protocol/test/unit/test_main.ml
+++ src/proto_016_PtMumbai/lib_protocol/test/unit/test_main.ml
@@ -28,39 +28,39 @@
 let () =
   Alcotest_lwt.run
     ~__FILE__
-    "protocol > unit"
+    (Protocol.name ^ " > unit")
     [
-      (Protocol.name ^ ": Tez_repr.ml", Test_tez_repr.tests);
-      (Protocol.name ^ ": Contract_repr.ml", Test_contract_repr.tests);
-      (Protocol.name ^ ": Destination_repr.ml", Test_destination_repr.tests);
-      (Protocol.name ^ ": Operation_repr.ml", Test_operation_repr.tests);
+      ("Tez_repr.ml", Test_tez_repr.tests);
+      ("Contract_repr.ml", Test_contract_repr.tests);
+      ("Destination_repr.ml", Test_destination_repr.tests);
+      ("Operation_repr.ml", Test_operation_repr.tests);
       ( Protocol.name ^ ": Global_constants_storage.ml",
         Test_global_constants_storage.tests );
-      (Protocol.name ^ ": fitness", Test_fitness.tests);
-      (Protocol.name ^ ": fixed point computation", Test_fixed_point.tests);
-      (Protocol.name ^ ": level module", Test_level_module.tests);
-      (Protocol.name ^ ": qty", Test_qty.tests);
-      (Protocol.name ^ ": round", Test_round_repr.tests);
-      (Protocol.name ^ ": time", Test_time_repr.tests);
-      (Protocol.name ^ ": receipt encodings", Test_receipt.tests);
-      (Protocol.name ^ ": saturation arithmetic", Test_saturation.tests);
-      (Protocol.name ^ ": gas monad", Test_gas_monad.tests);
-      (Protocol.name ^ ": sc rollup storage", Test_sc_rollup_storage.tests);
-      (Protocol.name ^ ": sc rollup game", Test_sc_rollup_game.tests);
-      (Protocol.name ^ ": tx rollup l2", Test_tx_rollup_l2.tests);
-      (Protocol.name ^ ": tx rollup l2 apply", Test_tx_rollup_l2_apply.tests);
-      (Protocol.name ^ ": liquidity baking", Test_liquidity_baking_repr.tests);
-      (Protocol.name ^ ": sc rollup wasm", Test_sc_rollup_wasm.tests);
-      (Protocol.name ^ ": sc rollup arith", Test_sc_rollup_arith.tests);
-      (Protocol.name ^ ": merkle list", Test_merkle_list.tests);
-      (Protocol.name ^ ": sc rollup inbox", Test_sc_rollup_inbox.tests);
-      (Protocol.name ^ ": skip list", Test_skip_list_repr.tests);
+      ("fitness", Test_fitness.tests);
+      ("fixed point computation", Test_fixed_point.tests);
+      ("level module", Test_level_module.tests);
+      ("qty", Test_qty.tests);
+      ("round", Test_round_repr.tests);
+      ("time", Test_time_repr.tests);
+      ("receipt encodings", Test_receipt.tests);
+      ("saturation arithmetic", Test_saturation.tests);
+      ("gas monad", Test_gas_monad.tests);
+      ("sc rollup storage", Test_sc_rollup_storage.tests);
+      ("sc rollup game", Test_sc_rollup_game.tests);
+      ("tx rollup l2", Test_tx_rollup_l2.tests);
+      ("tx rollup l2 apply", Test_tx_rollup_l2_apply.tests);
+      ("liquidity baking", Test_liquidity_baking_repr.tests);
+      ("sc rollup wasm", Test_sc_rollup_wasm.tests);
+      ("sc rollup arith", Test_sc_rollup_arith.tests);
+      ("merkle list", Test_merkle_list.tests);
+      ("sc rollup inbox", Test_sc_rollup_inbox.tests);
+      ("skip list", Test_skip_list_repr.tests);
       ( Protocol.name ^ ": sc rollup management protocol",
         Test_sc_rollup_management_protocol.tests );
-      (Protocol.name ^ ": Bond_id_repr.ml", Test_bond_id_repr.tests);
-      (Protocol.name ^ ": zk rollup storage", Test_zk_rollup_storage.tests);
-      (Protocol.name ^ ": Delegate_consensus_key.ml", Test_consensus_key.tests);
-      (Protocol.name ^ ": local_contexts", Test_local_contexts.tests);
-      (Protocol.name ^ ": dal slot proof", Test_dal_slot_proof.tests);
+      ("Bond_id_repr.ml", Test_bond_id_repr.tests);
+      ("zk rollup storage", Test_zk_rollup_storage.tests);
+      ("Delegate_consensus_key.ml", Test_consensus_key.tests);
+      ("local_contexts", Test_local_contexts.tests);
+      ("dal slot proof", Test_dal_slot_proof.tests);
     ]
   |> Lwt_main.run
