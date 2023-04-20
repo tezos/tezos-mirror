560ad1ca5486aa670604787d8d35682e6102ffba
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-04-20 16:38:30 +0200

    sequencer: build the project a makefile

--- kernels.mk
+++ kernels.mk
@@ -1,4 +1,4 @@
-KERNELS = kernel_sdk evm_kernel.wasm
+KERNELS = kernel_sdk evm_kernel.wasm kernel_sequencer
 
 kernel_sdk:
 	@make -C src/kernel_sdk
@@ -9,13 +9,18 @@ evm_kernel.wasm::
 	@cp src/kernel_evm/target/wasm32-unknown-unknown/release/evm_kernel.wasm $@
 	@wasm-strip $@
 
+kernel_sequencer:
+	@make -C src/kernel_sequencer
+
 build-deps:
 	@make -C src/kernel_sdk build-deps
 	@make -C src/kernel_evm build-deps
+	@make -C src/kernel_sequencer build-deps
 
 test-kernels:
 	@make -C src/kernel_sdk test
 	@make -C src/kernel_evm_mockup tests
+	@make -C src/kernel_sequencer tests
 
 .PHONY: build-kernels
 build-kernels: ${KERNELS}
