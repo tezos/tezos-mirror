9f73366ec1dcc11da0ee7db65b571d6d1a3e0186
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2023-04-21 14:41:53 +0200

    WASM/Debugger: PVM call stack computation

--- src/bin_wasm_debugger/profiling.ml
+++ src/bin_wasm_debugger/profiling.ml
@@ -28,7 +28,7 @@ open Tezos_lazy_containers
 open Tezos_webassembly_interpreter
 module Vector = Lazy_vector.Int32Vector
 
-(** Call graph representation and construction. *)
+(** Call stack representation and construction. *)
 
 (** The call stack computation algorithm is the following:
 
@@ -122,3 +122,197 @@ let call_function called_function current_tick current_function call_stack =
       in
       let func = Node (called_function, current_tick, []) in
       (func, stack)
+
+(** Profiling the execution of the PVM *)
+
+(** A function call can be either a direct call, a call through a reference or
+    an internal step of the PVM. *)
+type function_call =
+  | CallDirect of int32
+  | CallRef of int32
+  | Internal of string
+
+let pp_call ppf = function
+  | CallDirect i -> Format.fprintf ppf "function[%ld]" i
+  | CallRef i -> Format.fprintf ppf "function_ref[%ld]" i
+  | Internal s -> Format.fprintf ppf "%%interpreter(%s)" s
+
+(** [initial_eval_call module_] creates the node with the correct identifier for
+    `kernel_run` according to the profiled module. *)
+let initial_eval_call (module_exports : Ast.export Vector.t) =
+  let open Lwt_syntax in
+  let+ exports = Vector.to_list module_exports in
+  let var =
+    List.find_map
+      (function
+        | {Source.it = {Ast.name; edesc = {it = Ast.FuncExport var; _}}; _}
+          when name = Constants.wasm_entrypoint ->
+            Some var
+        | _ -> None)
+      exports
+  in
+  match var with Some {it = id; _} -> CallDirect id | None -> assert false
+
+(** [update_on_decode current_tick current_call_context] starts and stop
+    `internal` calls related to the {Decode} step of the PVM. *)
+let update_on_decode current_tick (current_node, call_stack) =
+  let open Lwt_syntax in
+  function
+  | Decode.MKStart ->
+      return_some
+      @@ call_function (Internal "decode") current_tick current_node call_stack
+  | Decode.MKStop _ ->
+      let current_node, call_stack =
+        end_function_call current_tick current_node call_stack
+      in
+      return_some
+      @@ call_function (Internal "link") current_tick current_node call_stack
+  | _ -> return_none
+
+(** [update_on_link current_tick current_call_context] starts and stop
+    `internal` call to the {Link} step of the PVM. *)
+let update_on_link current_tick (current_node, call_stack) module_
+    imports_offset =
+  let open Lwt_syntax in
+  if imports_offset >= Vector.num_elements module_.Source.it.Ast.imports then
+    let current_node, call_stack =
+      end_function_call current_tick current_node call_stack
+    in
+    return_some
+    @@ call_function (Internal "init") current_tick current_node call_stack
+  else return_none
+
+(** [update_on_init current_tick current_call_context] starts and stop
+    `internal` call to the {Init} step of the PVM. *)
+let update_on_init current_tick (current_node, call_stack) module_ =
+  let open Lwt_syntax in
+  function
+  | Eval.IK_Stop ->
+      let current_node, call_stack =
+        end_function_call current_tick current_node call_stack
+      in
+      let* start = initial_eval_call module_.Source.it.Ast.exports in
+      return_some @@ call_function start current_tick current_node call_stack
+  | _ -> return_none
+
+(** [update_on_instr current_tick current_node call_stack] handle function calls
+    during the evaluation. *)
+let update_on_instr current_tick current_node call_stack = function
+  | Eval.Plain (Ast.Call f) ->
+      Lwt.return_some
+        (call_function
+           (CallDirect f.Source.it)
+           current_tick
+           current_node
+           call_stack)
+  | Eval.Plain (CallIndirect (f, _)) ->
+      Lwt.return_some
+        (call_function
+           (CallRef f.Source.it)
+           current_tick
+           current_node
+           call_stack)
+  | _ -> Lwt.return_none
+
+(** [update_on_eval current_tick current_call_context] handle function calls and
+    end during the evaluation. *)
+let update_on_eval current_tick (current_node, call_stack) =
+  let open Lwt_syntax in
+  function
+  (* Instruction evaluation step *)
+  | Eval.(SK_Next (_, _, LS_Start (Label_stack (label, _)))) ->
+      let _, es = label.Eval.label_code in
+      if 0l < Vector.num_elements es then
+        let* e = Vector.get 0l es in
+        update_on_instr current_tick current_node call_stack e.Source.it
+      else return_none
+  (* Labels `result` or `trapped` implies the end of a function call and the pop of
+     the current stack frame, this can be interpreted as an end of the current
+     function. *)
+  | SK_Start ({frame_label_kont = Label_trapped _ | Label_result _; _}, _) ->
+      return_some @@ end_function_call current_tick current_node call_stack
+  (* An invocation of function that doesn't return a new stack frame implies the
+     current function is an host function, and it is the end of its call. *)
+  | SK_Next
+      ( _,
+        _,
+        LS_Craft_frame
+          (Label_stack (_, _), Inv_stop {fresh_frame = None; remaining_ticks; _})
+      )
+    when Z.equal Z.zero remaining_ticks ->
+      return_some @@ end_function_call current_tick current_node call_stack
+  | _ -> return_none
+
+(** [update_call_stack current_tick current_context_call state] returns the call
+    context changes for any state. Returns [None] if no change happened. *)
+let update_call_stack current_tick (current_node, call_stack) state =
+  let open Lwt_syntax in
+  match state with
+  | Wasm_pvm_state.Internal_state.Decode {Decode.module_kont; _} ->
+      update_on_decode current_tick (current_node, call_stack) module_kont
+  | Link {ast_module; imports_offset; _} ->
+      update_on_link
+        current_tick
+        (current_node, call_stack)
+        ast_module
+        imports_offset
+  | Init {init_kont; _} ->
+      update_on_init current_tick (current_node, call_stack) init_kont
+  | Eval {config = {step_kont; _}; _} ->
+      update_on_eval current_tick (current_node, call_stack) step_kont
+  | _ -> return_none
+
+(** [eval_and_profile ?write_debug ?reveal_builtins tree] profiles a kernel up
+    to the next result, and returns the call stack. *)
+let eval_and_profile ?write_debug ?reveal_builtins tree =
+  let open Lwt_syntax in
+  (* The call context is built as a side effect of the evaluation. *)
+  let call_stack = ref (Toplevel [], []) in
+
+  let compute_and_snapshot pvm_state =
+    let* updated_stack =
+      update_call_stack
+        pvm_state.Wasm_pvm_state.Internal_state.current_tick
+        !call_stack
+        pvm_state.tick_state
+    in
+    Option.iter
+      (fun (current_node, current_call_stack) ->
+        call_stack := (current_node, current_call_stack))
+      updated_stack ;
+
+    let* input_request_val = Wasm_vm.get_info pvm_state in
+    match (input_request_val.input_request, pvm_state.tick_state) with
+    | Reveal_required _, _ when reveal_builtins = None -> return_false
+    | Input_required, _ | Reveal_required _, _ -> return_false
+    | _ -> return_true
+  in
+  let rec eval_until_input_requested accumulated_ticks tree =
+    let* pvm_state =
+      Encodings_util.Tree_encoding_runner.decode
+        Wasm_pvm.pvm_state_encoding
+        tree
+    in
+    let* info = Wasm_utils.Wasm.get_info tree in
+    match info.Wasm_pvm_state.input_request with
+    | No_input_required ->
+        let* tree, ticks =
+          Wasm_utils.Wasm.Internal_for_tests.compute_step_many_until
+            ?write_debug
+            ?reveal_builtins
+            ~max_steps:(Z.to_int64 pvm_state.max_nb_ticks)
+            compute_and_snapshot
+            tree
+        in
+        eval_until_input_requested
+          (Z.add accumulated_ticks @@ Z.of_int64 ticks)
+          tree
+    | Input_required | Reveal_required _ -> return (tree, accumulated_ticks)
+  in
+  let+ tree, ticks = eval_until_input_requested Z.zero tree in
+  let call_stack =
+    match !call_stack with
+    | Toplevel l, stack -> (Toplevel (List.rev l), stack)
+    | n -> n
+  in
+  (tree, ticks, call_stack)
