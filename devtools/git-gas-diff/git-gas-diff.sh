499138f6f6963c384e2ddaa8a5172bfa46f439f1
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2023-04-28 15:05:57 +0200

    Devtools/git-gas-diff: disable external diff
    For users of external diffing tools in `git diff` (like patdiff for
    example), the git-gas-diff.sh script is currently broken because it
    assumes that the diff is presented in patch format. This commit fixes
    the script by disabling external diff in the call to `git diff`.

--- devtools/git-gas-diff/git-gas-diff.sh
+++ devtools/git-gas-diff/git-gas-diff.sh
@@ -73,7 +73,7 @@ MSG="-- LINE NUMBERS REFER TO TEMPORARY FILE ${DIFF_FILE}. --"
 
 echo -e "$MSG\n"
 
-git diff --diff-filter=ad -U0 --output="$DIFF_FILE" "$OLD" "$NEW" \
+git diff --no-ext-diff --diff-filter=ad -U0 --output="$DIFF_FILE" "$OLD" "$NEW" \
   ../../tezt/tests/expected/
 dune exec git-gas-diff "$DIFF_FILE"
 
