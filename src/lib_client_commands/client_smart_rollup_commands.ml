d7a9509ba1704cc4cb744c3b80854318fed86ece
Alain Mebsout <alain.mebsout@functori.com>
2023-04-28 11:01:28 +0200

    Client: smart rollup commands in base client

--- src/lib_client_commands/client_smart_rollup_commands.ml
+++ src/lib_client_commands/client_smart_rollup_commands.ml
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2023 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2023 Functori, <contact@functori.com>                       *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
@@ -23,10 +24,6 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-open Protocol
-open Alpha_context
-open Client_proto_rollups
-
 let group =
   {
     Tezos_clic.name = "smart rollup";
@@ -39,38 +36,44 @@ let commands () =
     command
       ~group
       ~desc:"Add a smart rollup to the wallet."
-      (args1 (SoruAlias.force_switch ()))
+      (args1 (Smart_rollup_alias.Address.force_switch ()))
       (prefixes ["remember"; "smart"; "rollup"]
-      @@ SoruAlias.fresh_alias_param @@ SoruAlias.source_param @@ stop)
+      @@ Smart_rollup_alias.Address.fresh_alias_param
+      @@ Smart_rollup_alias.Address.source_param @@ stop)
       (fun force name hash cctxt ->
         let open Lwt_result_syntax in
-        let* name = SoruAlias.of_fresh cctxt force name in
-        SoruAlias.add ~force cctxt name hash);
+        let* name = Smart_rollup_alias.Address.of_fresh cctxt force name in
+        Smart_rollup_alias.Address.add ~force cctxt name hash);
     command
       ~group
       ~desc:"Remove a smart rollup from the wallet."
       no_options
-      (prefixes ["forget"; "smart"; "rollup"] @@ SoruAlias.alias_param @@ stop)
-      (fun () (name, _) cctxt -> SoruAlias.del cctxt name);
+      (prefixes ["forget"; "smart"; "rollup"]
+      @@ Smart_rollup_alias.Address.alias_param @@ stop)
+      (fun () (name, _) cctxt -> Smart_rollup_alias.Address.del cctxt name);
     command
       ~group
       ~desc:"Lists all known smart rollup in the wallet."
       no_options
       (fixed ["list"; "known"; "smart"; "rollups"])
-      (fun () (cctxt : Protocol_client_context.full) ->
+      (fun () (cctxt : Client_context.full) ->
         let open Lwt_result_syntax in
-        let* smart_rollups = SoruAlias.load cctxt in
+        let* smart_rollups = Smart_rollup_alias.Address.load cctxt in
         let*! () =
           List.iter_s
             (fun (alias, rollup) ->
-              cctxt#message "%s: %a" alias Sc_rollup.Address.pp rollup)
+              cctxt#message
+                "%s: %a"
+                alias
+                Tezos_crypto.Hashed.Smart_rollup_address.pp
+                rollup)
             smart_rollups
         in
         return_unit);
     command
       ~group
       ~desc:"Forget the entire wallet of known smart rollups."
-      (args1 (SoruAlias.force_switch ()))
+      (args1 (Smart_rollup_alias.Address.force_switch ()))
       (fixed ["forget"; "all"; "smart"; "rollups"])
       (fun force cctxt ->
         let open Lwt_result_syntax in
@@ -79,15 +82,20 @@ let commands () =
             force
             (error_of_fmt "this can only be used with option --force")
         in
-        SoruAlias.set cctxt []);
+        Smart_rollup_alias.Address.set cctxt []);
     command
       ~group
       ~desc:"Display a smart rollup from the wallet."
       no_options
       (prefixes ["show"; "known"; "smart"; "rollup"]
-      @@ SoruAlias.alias_param @@ stop)
-      (fun () (_, rollup) (cctxt : Protocol_client_context.full) ->
+      @@ Smart_rollup_alias.Address.alias_param @@ stop)
+      (fun () (_, rollup) (cctxt : Client_context.full) ->
         let open Lwt_result_syntax in
-        let*! () = cctxt#message "%a\n%!" Sc_rollup.Address.pp rollup in
+        let*! () =
+          cctxt#message
+            "%a\n%!"
+            Tezos_crypto.Hashed.Smart_rollup_address.pp
+            rollup
+        in
         return_unit);
   ]
