d5e1a95acaf1f639ec7f755d133d7d5adf14b04a
vbot <vincent.botbol@nomadic-labs.com>
2023-05-02 17:17:25 +0200

    Alpha/Baker: refactor liquity vote file and arg cli handling

--- src/proto_alpha/lib_delegate/baking_events.ml
+++ src/proto_alpha/lib_delegate/baking_events.ml
@@ -1001,24 +1001,8 @@ module Liquidity_baking = struct
       ~section
       ~name:"reading_per_block"
       ~level:Notice
-      ~msg:"reading per block vote file path: {path}"
+      ~msg:"reading liquidity baking vote file: {path}"
       ("path", Data_encoding.string)
-
-  let per_block_vote_file_notice =
-    declare_1
-      ~section
-      ~name:"per_block_vote_file_notice"
-      ~level:Notice
-      ~msg:"per block vote file {event}"
-      ("event", Data_encoding.string)
-
-  let reading_liquidity_baking =
-    declare_0
-      ~section
-      ~name:"reading_liquidity_baking"
-      ~level:Notice
-      ~msg:"reading liquidity baking toggle vote"
-      ()
 
   let liquidity_baking_toggle_vote =
     declare_1
@@ -1034,26 +1018,10 @@ module Liquidity_baking = struct
     declare_1
       ~section
       ~name:"per_block_vote_file_error"
-      ~level:Notice
+      ~level:Error
       ~msg:"Error reading the block vote file: {errors}"
       ~pp1:pp_print_top_error_of_trace
       ("errors", Error_monad.(TzTrace.encoding error_encoding))
-
-  let liquidity_baking_off =
-    declare_0
-      ~section
-      ~name:"liquidity_baking_off"
-      ~level:Notice
-      ~msg:"Will vote to stop Liquidity Baking"
-      ()
-
-  let liquidity_baking_on =
-    declare_0
-      ~section
-      ~name:"liquidity_baking_on"
-      ~level:Notice
-      ~msg:"Will vote to continue or restart Liquidity Baking"
-      ()
 end
 
 module Selection = struct
