cfce4146f3aaada0a4bb2d1f0f2a5e834a0ee910
Albin Coquereau <albin.coquereau@nomadic-labs.com>
2023-05-02 11:35:47 +0200

    proto_alpha: non legacy encoding use encoding with attestation name

--- src/proto_alpha/lib_protocol/main.ml
+++ src/proto_alpha/lib_protocol/main.ml
@@ -44,8 +44,7 @@ type operation_data = Alpha_context.packed_protocol_data =
       'kind Alpha_context.Operation.protocol_data
       -> operation_data
 
-let operation_data_encoding =
-  Alpha_context.Operation.protocol_data_encoding_with_legacy_attestation_name
+let operation_data_encoding = Alpha_context.Operation.protocol_data_encoding
 
 let operation_data_encoding_with_legacy_attestation_name =
   Alpha_context.Operation.protocol_data_encoding_with_legacy_attestation_name
@@ -56,15 +55,13 @@ type operation_receipt = Apply_results.packed_operation_metadata =
       -> operation_receipt
   | No_operation_metadata : operation_receipt
 
-let operation_receipt_encoding =
-  Apply_results.operation_metadata_encoding_with_legacy_attestation_name
+let operation_receipt_encoding = Apply_results.operation_metadata_encoding
 
 let operation_receipt_encoding_with_legacy_attestation_name =
   Apply_results.operation_metadata_encoding_with_legacy_attestation_name
 
 let operation_data_and_receipt_encoding =
-  Apply_results
-  .operation_data_and_metadata_encoding_with_legacy_attestation_name
+  Apply_results.operation_data_and_metadata_encoding
 
 let operation_data_and_receipt_encoding_with_legacy_attestation_name =
   Apply_results
