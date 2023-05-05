97994b6bfb983c3b950f74673d4f94227896c619
lykimq <lykimq@gmail.com>
2023-05-05 10:34:04 +0200

    Alcotezt: port [src/lib_srs_extraction/test]

--- src/lib_srs_extraction/test/test_main.ml
+++ src/lib_srs_extraction/test/test_main.ml
@@ -1,7 +1,9 @@
 open Libsrs
 
+let path = project_root // Filename.dirname __FILE__
+
 let test_lagrange () =
-  let input_file = "phase1radix2m5" in
+  let input_file = path // "phase1radix2m5" in
   let g1_elements = Powers_of_tau.to_g1s input_file in
   let g2_elements = Powers_of_tau.to_g2s input_file in
   Checks.equality g1_elements g2_elements
@@ -10,4 +12,4 @@ let tests = [("lagrange zcash_srs", test_lagrange)]
 
 let () =
   let wrap = List.map (fun (name, f) -> Alcotest.test_case name `Quick f) in
-  Alcotest.run ~verbose:true "SRS" [("Srs", wrap tests)]
+  Alcotest.run ~__FILE__ "SRS" [("Srs", wrap tests)]
