5804a93d4baef68883d37df1de20e39ad721aa69
Gauthier SEBILLE <gauthier@marigold.dev>
2023-05-05 11:22:54 +0200

    DAC: make Dac_plugin.raw_hash encoding JSON only
    DAC: add ppx_expect inline test for raw_hash encoding
    
    DAC: remove useless change
    
    Revert "DAC: remove useless change"
    
    This reverts commit 1b3a9e4d22a03fc89638a02a38ad8e988fe49f22.
    
    DAC: remove useless change
    
    DAC: trying to revert..
    
    Revert "DAC: trying to revert.."
    
    This reverts commit 0951410f29f7bb040fc9e533ade54cf0637afc29.
    
    DAC: revert snoop-command.t
    
    DAC: switch from ppx_expect to classic tests suite
    
    DAC: remove ppx_expect

---
+++ src/lib_dac/test/test_dac_plugin.ml
@@ -0,0 +1,92 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2023 Marigold <contact@marigold.dev>                        *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** Testing
+    -------
+    Component:  Lib_dac Dac_plugin
+    Invocation: dune exec src/lib_dac/test/main.exe -- --file test_dac_plugin.ml
+    Subject:    Tests for the Dac_plugin.raw_hash.
+*)
+
+let test_encode_decode_json () =
+  let open Lwt_result_syntax in
+  let raw_hash =
+    Dac_plugin.raw_hash_of_bytes
+      (Bytes.of_string
+         "00dce42551fb786c51b29b723f4abba3ea04eb3d239a9a59ec5a5434e151e105e4")
+  in
+  let encoded_raw_hash =
+    Data_encoding.Json.construct Dac_plugin.raw_hash_encoding raw_hash
+  in
+  let decoded_raw_hash =
+    Data_encoding.Json.destruct Dac_plugin.raw_hash_encoding encoded_raw_hash
+  in
+  return @@ assert (raw_hash = decoded_raw_hash)
+
+let test_cannot_encode_binary () =
+  let open Lwt_result_syntax in
+  let raw_hash =
+    Dac_plugin.raw_hash_of_bytes
+      (Bytes.of_string
+         "00dce42551fb786c51b29b723f4abba3ea04eb3d239a9a59ec5a5434e151e105e4")
+  in
+  let encoded_raw_hash =
+    Data_encoding.Binary.to_string Dac_plugin.raw_hash_encoding raw_hash
+  in
+  match encoded_raw_hash with
+  | Error _ -> return @@ assert true
+  | Ok _ -> return @@ assert false
+
+let test_cannot_decode_binary () =
+  let open Lwt_result_syntax in
+  let raw_hash =
+    "00dce42551fb786c51b29b723f4abba3ea04eb3d239a9a59ec5a5434e151e105e4"
+  in
+  let encoded_raw_hash =
+    Data_encoding.Binary.of_string Dac_plugin.raw_hash_encoding raw_hash
+  in
+  match encoded_raw_hash with
+  | Error _ -> return @@ assert true
+  | Ok _ -> return @@ assert false
+
+let tests =
+  [
+    Tztest.tztest
+      "Encode and decode to JSON leads to the same Dac_plugin.raw_hash"
+      `Quick
+      test_encode_decode_json;
+    Tztest.tztest
+      "Cannot encode a raw_hash to binary"
+      `Quick
+      test_cannot_encode_binary;
+    Tztest.tztest
+      "Cannot decode a raw_hash to binary"
+      `Quick
+      test_cannot_decode_binary;
+  ]
+
+let () =
+  Alcotest_lwt.run ~__FILE__ "lib_dac" [("Dac_plugin.ml", tests)]
+  |> Lwt_main.run
