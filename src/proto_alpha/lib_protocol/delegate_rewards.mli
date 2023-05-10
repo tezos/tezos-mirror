9094f6ff0f54b9a6d11e6846306303f9949b0779
Lucas Randazzo <lucas@nomadic-labs.com>
2023-05-10 16:40:03 +0200

    Proto: add Delegate_rewards module
    This module returns the tez values for the different kinds of rewards

---
+++ src/proto_alpha/lib_protocol/delegate_rewards.mli
@@ -0,0 +1,36 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2023 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(* These functions return the amount of tez rewarded for each action *)
+
+val baking_reward_fixed_portion : Raw_context.t -> Tez_repr.t
+
+val baking_reward_bonus_per_slot : Raw_context.t -> Tez_repr.t
+
+val endorsing_reward_per_slot : Raw_context.t -> Tez_repr.t
+
+val liquidity_baking_subsidy : Raw_context.t -> Tez_repr.t
+
+val seed_nonce_revelation_tip : Raw_context.t -> Tez_repr.t
