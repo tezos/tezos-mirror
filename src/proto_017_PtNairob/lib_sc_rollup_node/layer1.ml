7339416325c1fec3c74f678a2134e435dfe0cd34
Alain Mebsout <alain.mebsout@functori.com>
2023-05-16 11:20:38 +0200

    SCORU/Node/Nairobi: port !8767 to Nairobi rollup node
    - SCORU/Node: use blocks cache when fetching headers
    - SCORU/Node: use configurable L1 cache size
    - SCORU/Node: prefetch Tezos blocks
    - SCORU/Node: show processing time of blocks in events
    - SCORU/Node: only check inbox for commitments when catching up

--- src/proto_017_PtNairob/lib_sc_rollup_node/layer1.ml
+++ src/proto_017_PtNairob/lib_sc_rollup_node/layer1.ml
@@ -92,21 +92,67 @@ type blocks_cache = Alpha_block_services.block_info Blocks_cache.t
 
 type headers_cache = Block_header.shell_header Blocks_cache.t
 
-(** Global blocks cache for the smart rollup node. *)
-let blocks_cache : blocks_cache = Blocks_cache.create 32
+open Octez_crawler.Layer_1
 
-(** Global block headers cache for the smart rollup node. *)
-let headers_cache : headers_cache = Blocks_cache.create 32
+type nonrec t = {
+  l1 : t;
+  cctxt : Client_context.full;
+  blocks_cache : blocks_cache;
+      (** Global blocks cache for the smart rollup node. *)
+  headers_cache : headers_cache;
+      (** Global block headers cache for the smart rollup node. *)
+  prefetch_blocks : int;  (** Number of blocks to prefetch by default. *)
+}
 
-include Octez_crawler.Layer_1
+let start ~name ~reconnection_delay ~l1_blocks_cache_size ?protocols
+    ?(prefetch_blocks = l1_blocks_cache_size) cctxt =
+  let open Lwt_result_syntax in
+  let*? () =
+    if prefetch_blocks > l1_blocks_cache_size then
+      error_with
+        "Blocks to prefetch must be less than the cache size: %d"
+        l1_blocks_cache_size
+    else Ok ()
+  in
+  let*! l1 = start ~name ~reconnection_delay ?protocols cctxt in
+  let blocks_cache = Blocks_cache.create l1_blocks_cache_size in
+  let headers_cache = Blocks_cache.create l1_blocks_cache_size in
+  let cctxt = (cctxt :> Client_context.full) in
+  return {l1; cctxt; blocks_cache; headers_cache; prefetch_blocks}
 
-let cache_shell_header hash header =
+let shutdown {l1; _} = shutdown l1
+
+let cache_shell_header {headers_cache; _} hash header =
   Blocks_cache.put headers_cache hash (Lwt.return_some header)
 
 let iter_heads l1_ctxt f =
-  iter_heads l1_ctxt @@ fun (hash, {shell = {level; _} as header; _}) ->
-  cache_shell_header hash header ;
+  iter_heads l1_ctxt.l1 @@ fun (hash, {shell = {level; _} as header; _}) ->
+  cache_shell_header l1_ctxt hash header ;
   f {hash; level; header}
+
+let wait_first l1_ctxt =
+  let open Lwt_syntax in
+  let+ hash, {shell = {level; _} as header; _} = wait_first l1_ctxt.l1 in
+  {hash; level; header}
+
+let get_predecessor_opt ?max_read {l1; _} = get_predecessor_opt ?max_read l1
+
+let get_predecessor ?max_read {l1; _} = get_predecessor ?max_read l1
+
+let get_tezos_reorg_for_new_head {l1; _} ?get_old_predecessor old_head new_head
+    =
+  get_tezos_reorg_for_new_head l1 ?get_old_predecessor old_head new_head
+
+module Internal_for_tests = struct
+  let dummy cctxt =
+    {
+      l1 = Internal_for_tests.dummy cctxt;
+      cctxt = (cctxt :> Client_context.full);
+      blocks_cache = Blocks_cache.create 1;
+      headers_cache = Blocks_cache.create 1;
+      prefetch_blocks = 0;
+    }
+end
 
 (**
 
@@ -118,7 +164,7 @@ let iter_heads l1_ctxt f =
 (** [fetch_tezos_block cctxt hash] returns a block shell header of
     [hash]. Looks for the block in the blocks cache first, and fetches it from
     the L1 node otherwise. *)
-let fetch_tezos_shell_header cctxt hash =
+let fetch_tezos_shell_header {cctxt; blocks_cache; headers_cache; _} hash =
   let open Lwt_syntax in
   trace (Cannot_find_block hash)
   @@
@@ -138,7 +184,21 @@ let fetch_tezos_shell_header cctxt hash =
     | Ok shell_header -> return_some shell_header
   in
   let+ shell_header =
-    Blocks_cache.bind_or_put headers_cache hash fetch Lwt.return
+    let do_cached_header_fetch () =
+      Blocks_cache.bind_or_put headers_cache hash fetch Lwt.return
+    in
+    match Blocks_cache.bind blocks_cache hash Lwt.return with
+    | None -> do_cached_header_fetch ()
+    | Some block -> (
+        (* There is already a full block being fetched. *)
+        let* block in
+        match block with
+        | None ->
+            (* Fetching full block failed. *)
+            do_cached_header_fetch ()
+        | Some block ->
+            (* The full block is already in the cache. *)
+            return_some block.header.shell)
   in
   match (shell_header, !errors) with
   | None, None ->
@@ -154,7 +214,7 @@ let fetch_tezos_shell_header cctxt hash =
 (** [fetch_tezos_block cctxt hash] returns a block info given a block
     hash. Looks for the block in the blocks cache first, and fetches it from the
     L1 node otherwise. *)
-let fetch_tezos_block cctxt hash =
+let fetch_tezos_block {cctxt; blocks_cache; headers_cache; _} hash =
   let open Lwt_syntax in
   trace (Cannot_find_block hash)
   @@
@@ -184,4 +244,32 @@ let fetch_tezos_block cctxt hash =
         Block_hash.pp
         hash
   | None, Some errs -> Error errs
-  | Some block, _ -> Ok block
+  | Some block, _ ->
+      Blocks_cache.put headers_cache hash (Lwt.return_some block.header.shell) ;
+      Ok block
+
+let make_prefetching_schedule {prefetch_blocks; _} blocks =
+  let blocks_with_prefetching, _, first_prefetch =
+    List.fold_left
+      (fun (acc, nb_prefetch, prefetch) b ->
+        let nb_prefetch = nb_prefetch + 1 in
+        let prefetch = b :: prefetch in
+        if nb_prefetch >= prefetch_blocks then ((b, prefetch) :: acc, 0, [])
+        else ((b, []) :: acc, nb_prefetch, prefetch))
+      ([], 0, [])
+      (List.rev blocks)
+  in
+  match (blocks_with_prefetching, first_prefetch) with
+  | [], _ | _, [] -> blocks_with_prefetching
+  | (first, _) :: rest, _ -> (first, first_prefetch) :: rest
+
+let prefetch_tezos_blocks l1_ctxt = function
+  | [] -> ()
+  | blocks ->
+      Lwt.async @@ fun () ->
+      List.iter_p
+        (fun {hash; _} ->
+          let open Lwt_syntax in
+          let+ _maybe_block = fetch_tezos_block l1_ctxt hash in
+          ())
+        blocks
