fd65b58909ce5b19407010bd849290f47bbc44e0
iguerNL@Functori <iguer@functori.com>
2023-05-17 09:14:48 +0200

    DAL/Node: implement GS messages validation

--- src/bin_dal_node/daemon.ml
+++ src/bin_dal_node/daemon.ml
@@ -100,6 +100,46 @@ module Handler = struct
     in
     return (go (), stopper)
 
+  (* [gossipsub_app_messages_validation cryptobox message message_id] allows
+     checking whether the given [message] identified by [message_id] is valid
+     with the current [cryptobox] parameters. The validity check is done by
+     verifying that the shard in the message effectively belongs to the
+     commitment given by [message_id]. *)
+  let gossipsub_app_messages_validation cryptobox message message_id =
+    let open Gossipsub in
+    let {share; shard_proof} = message in
+    let {commitment; shard_index; _} = message_id in
+    let shard = Cryptobox.{share; index = shard_index} in
+    match Cryptobox.verify_shard cryptobox commitment shard shard_proof with
+    | Ok () -> `Valid
+    | Error err ->
+        let err =
+          match err with
+          | `Invalid_degree_strictly_less_than_expected {given; expected} ->
+              Format.sprintf
+                "Invalid_degree_strictly_less_than_expected. Given: %d, \
+                 expected: %d"
+                given
+                expected
+          | `Invalid_shard -> "Invalid_shard"
+          | `Shard_index_out_of_range s ->
+              Format.sprintf "Shard_index_out_of_range(%s)" s
+          | `Shard_length_mismatch -> "Shard_length_mismatch"
+        in
+        Event.(
+          emit__dont_wait__use_with_care
+            message_validation_error
+            (message_id, err)) ;
+        `Invalid
+    | exception exn ->
+        (* Don't crash if crypto raised an exception. *)
+        let err = Printexc.to_string exn in
+        Event.(
+          emit__dont_wait__use_with_care
+            message_validation_error
+            (message_id, err)) ;
+        `Invalid
+
   let resolve_plugin_and_set_ready config ctxt cctxt =
     (* Monitor heads and try resolve the DAL protocol plugin corresponding to
        the protocol of the targeted node. *)
@@ -124,6 +164,12 @@ module Handler = struct
             init_cryptobox config.Configuration.use_unsafe_srs proto_parameters
           in
           Node_context.set_ready ctxt plugin cryptobox proto_parameters ;
+          (* FIXME: https://gitlab.com/tezos/tezos/-/issues/4441
+
+             The hook below should be called each time cryptobox parameters
+             change. *)
+          Gossipsub.Worker.Validate_message_hook.set
+            (gossipsub_app_messages_validation cryptobox) ;
           let*! () = Event.(emit node_is_ready ()) in
           stopper () ;
           return_unit
