24ba80fafb04a22760efda6909d59046c932ab91
pecornilleau <pe.cornilleau@marigold.dev>
2023-05-17 12:06:22 +0200

    SORU: EVM: exe for chunking tx

--- src/bin_evm_proxy/lib/rollup_node.mli
+++ src/bin_evm_proxy/lib/rollup_node.mli
@@ -22,11 +22,14 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
-(** [make_encoded_messages ~smart_rollup_address raw_tx] returns the hash of the
-    transaction, and a list of transactions to include in the inbox.
+
+(** [make_encoded_messages ~smart_rollup_address raw_tx] returns the
+    hash of the transaction, and a list of transactions to include in the inbox.
     - [smart_rollup_address] is encoded on 20 bytes
-    - [raw_tx] is an ethereum transaction in hex format
-    (without the 0x prefix)*)
+    - [raw_tx] is an ethereum transaction in hex format (without the 0x prefix).
+
+    All messages go through the same encoding, but will only be chunked if
+    necessary. *)
 val make_encoded_messages :
   smart_rollup_address:string ->
   Ethereum_types.hash ->
