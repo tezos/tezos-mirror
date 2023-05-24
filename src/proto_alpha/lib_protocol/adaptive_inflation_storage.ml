9fdbd44a54dbf7ba3235032ecff878b5d3b101d6
Lucas Randazzo <lucas@nomadic-labs.com>
2023-05-24 14:06:12 +0200

    Proto: Add module for Adaptive Inflation Storage

---
+++ src/proto_alpha/lib_protocol/adaptive_inflation_storage.ml
@@ -0,0 +1,36 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2023 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(* Default reward coefficient when AI is not in effect, chosen so that
+   rewards * coeff = rewards *)
+let default = Q.one
+
+let get_reward_coeff ctxt ~current_cycle =
+  let open Lwt_result_syntax in
+  let ai_enable = (Raw_context.constants ctxt).adaptive_inflation.enable in
+  if ai_enable then
+    let* k_opt = Storage.Reward_coeff.find ctxt current_cycle in
+    return (Option.value ~default k_opt)
+  else return default
