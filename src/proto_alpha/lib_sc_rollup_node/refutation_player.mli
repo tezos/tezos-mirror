OKEAMAH/Git

SCORU/Node: defunctorize refutation games

src/proto_alpha/lib_sc_rollup_node/refutation_player.mli
@@ -26,43 +26,34 @@
 open Protocol
 open Alpha_context
 
-(** Worker module for a signle refutation game player.
-    The node's refutation coordinator will spawn a new refutation player
-    for each refutation game.
+(** Worker module for a signle refutation game player.  The node's refutation
+    coordinator will spawn a new refutation player for each refutation game.
 *)
 module Worker : Worker.T
 
 (** Type for a refutation game player.  *)
 type worker = Worker.infinite Worker.queue Worker.t
 
-module type S = sig
-  (** [init_and_play node_ctxt ~self ~conflict ~game ~level]
-      initializes  a new refutation game player for signer [self].
-      After initizialization, the worker will play the next move
-      depending on the [game] state.
-      If no [game] is passed, the worker will play the opening
-      move for [conflict].
-  *)
-  val init_and_play :
-    Node_context.rw ->
-    self:public_key_hash ->
-    conflict:Sc_rollup.Refutation_storage.conflict ->
-    game:Sc_rollup.Game.t option ->
-    level:int32 ->
-    unit tzresult Lwt.t
+(** [init_and_play node_ctxt ~self ~conflict ~game ~level] initializes a new
+    refutation game player for signer [self].  After initizialization, the
+    worker will play the next move depending on the [game] state.  If no [game]
+    is passed, the worker will play the opening move for [conflict].  *)
+val init_and_play :
+  Node_context.rw ->
+  self:public_key_hash ->
+  conflict:Sc_rollup.Refutation_storage.conflict ->
+  game:Sc_rollup.Game.t option ->
+  level:int32 ->
+  unit tzresult Lwt.t
 
-  (** [play worker game ~level] makes the [worker] play the next move depending
+(** [play worker game ~level] makes the [worker] play the next move depending
       on the [game] state for their conflict.
   *)
-  val play : worker -> Sc_rollup.Game.t -> level:int32 -> unit Lwt.t
+val play : worker -> Sc_rollup.Game.t -> level:int32 -> unit Lwt.t
 
-  (** Shutdown a refutaiton game player. *)
-  val shutdown : worker -> unit Lwt.t
+(** Shutdown a refutaiton game player. *)
+val shutdown : worker -> unit Lwt.t
 
-  (** [current_games ()] lists the opponents' this node is playing
-      refutation games against, alongside the worker that takes care
-      of each game. *)
-  val current_games : unit -> (public_key_hash * worker) list
-end
-
-module Make (Interpreter : Pvm.S) : S
+(** [current_games ()] lists the opponents' this node is playing refutation
+    games against, alongside the worker that takes care of each game. *)
+val current_games : unit -> (public_key_hash * worker) list

tezos/tezos-mirror

src/proto_alpha/lib_sc_rollup_node/refutation_player.mli
@@ -26,43 +26,34 @@
 open Protocol
 open Alpha_context
 
-(** Worker module for a signle refutation game player.
-    The node's refutation coordinator will spawn a new refutation player
-    for each refutation game.
+(** Worker module for a single refutation game player.  The node's refutation
+    coordinator will spawn a new refutation player for each refutation game.
 *)
 module Worker : Worker.T
 
 (** Type for a refutation game player.  *)
 type worker = Worker.infinite Worker.queue Worker.t
 
-module type S = sig
-  (** [init_and_play node_ctxt ~self ~conflict ~game ~level]
-      initializes  a new refutation game player for signer [self].
-      After initizialization, the worker will play the next move
-      depending on the [game] state.
-      If no [game] is passed, the worker will play the opening
-      move for [conflict].
-  *)
-  val init_and_play :
-    Node_context.rw ->
-    self:public_key_hash ->
-    conflict:Sc_rollup.Refutation_storage.conflict ->
-    game:Sc_rollup.Game.t option ->
-    level:int32 ->
-    unit tzresult Lwt.t
+(** [init_and_play node_ctxt ~self ~conflict ~game ~level] initializes a new
+    refutation game player for signer [self].  After initizialization, the
+    worker will play the next move depending on the [game] state.  If no [game]
+    is passed, the worker will play the opening move for [conflict].  *)
+val init_and_play :
+  Node_context.rw ->
+  self:public_key_hash ->
+  conflict:Sc_rollup.Refutation_storage.conflict ->
+  game:Sc_rollup.Game.t option ->
+  level:int32 ->
+  unit tzresult Lwt.t
 
-  (** [play worker game ~level] makes the [worker] play the next move depending
+(** [play worker game ~level] makes the [worker] play the next move depending
       on the [game] state for their conflict.
   *)
-  val play : worker -> Sc_rollup.Game.t -> level:int32 -> unit Lwt.t
+val play : worker -> Sc_rollup.Game.t -> level:int32 -> unit Lwt.t
 
-  (** Shutdown a refutaiton game player. *)
-  val shutdown : worker -> unit Lwt.t
+(** Shutdown a refutaiton game player. *)
+val shutdown : worker -> unit Lwt.t
 
-  (** [current_games ()] lists the opponents' this node is playing
-      refutation games against, alongside the worker that takes care
-      of each game. *)
-  val current_games : unit -> (public_key_hash * worker) list
-end
-
-module Make (Interpreter : Pvm.S) : S
+(** [current_games ()] lists the opponents' this node is playing refutation
+    games against, alongside the worker that takes care of each game. *)
+val current_games : unit -> (public_key_hash * worker) list