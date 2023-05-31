b6ef883bc7ed835c904eddacc52a0b78625e566b
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-05-31 20:48:49 +0200

    Proto/Tests/Frozen_deposits: fix test_may_bake_again_after_full_deposit_slash

--- src/proto_alpha/lib_protocol/test/integration/consensus/test_frozen_deposits.ml
+++ src/proto_alpha/lib_protocol/test/integration/consensus/test_frozen_deposits.ml
@@ -263,7 +263,7 @@ let test_cannot_bake_with_zero_deposits_limit () =
   (* Assert.proto_error_with_info ~loc:__LOC__ b1 "Zero frozen deposits" *)
   Assert.error ~loc:__LOC__ b1 (fun _ -> true)
 
-let test_may_bake_again_after_full_deposit_slash () =
+let test_may_not_bake_again_after_full_deposit_slash () =
   let order_ops op1 op2 =
     let oph1 = Operation.hash op1 in
     let oph2 = Operation.hash op2 in
@@ -311,11 +311,13 @@ let test_may_bake_again_after_full_deposit_slash () =
   Block.bake ~policy:(By_account slashed_account) b >>= fun res ->
   Assert.error ~loc:__LOC__ res (fun _ -> true) >>=? fun () ->
   Block.bake_until_cycle_end ~policy:(By_account good_account) b >>=? fun b ->
-  (* Assert that the [slashed_account]'s deposit is correctly reset *)
+  (* Assert that the [slashed_account]'s deposit is still zero without manual
+     staking. *)
   Context.Delegate.current_frozen_deposits (B b) slashed_account >>=? fun fd ->
-  Assert.not_equal_tez ~loc:__LOC__ fd Tez.zero >>=? fun () ->
-  (* Check that we are now allowed to bake with [slashed_account] *)
-  Block.bake ~policy:(By_account slashed_account) b >>=? fun _b -> return_unit
+  Assert.equal_tez ~loc:__LOC__ fd Tez.zero >>=? fun () ->
+  (* Check that we are still not allowed to bake with [slashed_account] *)
+  Block.bake ~policy:(By_account slashed_account) b >>= fun res ->
+  Assert.error ~loc:__LOC__ res (fun _ -> true) >>=? fun () -> return_unit
 
 let test_deposits_after_stake_removal () =
   Context.init_with_constants2 constants >>=? fun (genesis, contracts) ->
@@ -692,9 +694,9 @@ let tests =
         `Quick
         test_cannot_bake_with_zero_deposits_limit;
       tztest
-        "may bake again after full slash"
+        "may not bake again after full slash"
         `Quick
-        test_may_bake_again_after_full_deposit_slash;
+        test_may_not_bake_again_after_full_deposit_slash;
       tztest
         "deposits after stake removal"
         `Quick
