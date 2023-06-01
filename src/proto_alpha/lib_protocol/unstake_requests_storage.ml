9c9c3ce4d8f369b5907b5ffc4247360cc6ec18b5
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-06-01 15:00:35 +0200

    Proto/Unstake_requests_storage: add prepare_finalize_unstake

--- src/proto_alpha/lib_protocol/unstake_requests_storage.ml
+++ src/proto_alpha/lib_protocol/unstake_requests_storage.ml
@@ -22,3 +22,76 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
+
+type prepared_finalize_unstake = {
+  finalizable : (Signature.Public_key_hash.t * Cycle_repr.t * Tez_repr.t) list;
+  unfinalizable : Signature.Public_key_hash.t * (Cycle_repr.t * Tez_repr.t) list;
+}
+
+let z100 = Z.of_int 100
+
+let apply_slashes ~preserved_plus_slashing slashing_history ~from_cycle amount =
+  let first_cycle_to_apply_slash = from_cycle in
+  let last_cycle_to_apply_slash =
+    Cycle_repr.add from_cycle (preserved_plus_slashing - 1)
+  in
+  (* TODO https://gitlab.com/tezos/tezos/-/issues/5768
+     All slashings are applied multiplicatively so the order should have
+     no impact. But it does a little because of rounding down. Let's make sure
+     slashings are always applied in the same order, from oldest to newest. *)
+  let amount = Z.of_int64 (Tez_repr.to_mutez amount) in
+  let amount =
+    List.fold_left
+      (fun amount (slashing_cycle, slashing_percentage) ->
+        if
+          Cycle_repr.(
+            slashing_cycle >= first_cycle_to_apply_slash
+            && slashing_cycle <= last_cycle_to_apply_slash)
+        then Z.div (Z.mul amount (Z.of_int (100 - slashing_percentage))) z100
+        else amount)
+      amount
+      slashing_history
+  in
+  Tez_repr.of_mutez_exn (Z.to_int64 amount)
+
+let prepare_finalize_unstake ctxt contract =
+  let open Lwt_result_syntax in
+  let preserved_cycles = Constants_storage.preserved_cycles ctxt in
+  let max_slashing_period = Constants_storage.max_slashing_period ctxt in
+  let preserved_plus_slashing = preserved_cycles + max_slashing_period in
+  let current_cycle = (Raw_context.current_level ctxt).cycle in
+  match Cycle_repr.sub current_cycle preserved_plus_slashing with
+  | None (* no finalizable cycle *) -> return None
+  | Some greatest_finalizable_cycle -> (
+      let* requests_opt =
+        Storage.Contract.Unstake_requests.find ctxt contract
+      in
+      match requests_opt with
+      | None | Some {delegate = _; requests = []} -> return None
+      | Some {delegate; requests} ->
+          let* slashing_history_opt =
+            Storage.Contract.Slashed_deposits.find
+              ctxt
+              (Contract_repr.Implicit delegate)
+          in
+          let slashing_history =
+            Option.value slashing_history_opt ~default:[]
+          in
+          let finalizable, unfinalizable =
+            List.partition_map
+              (fun request ->
+                let request_cycle, request_amount = request in
+                if Cycle_repr.(request_cycle <= greatest_finalizable_cycle) then
+                  let new_amount =
+                    apply_slashes
+                      ~preserved_plus_slashing
+                      slashing_history
+                      ~from_cycle:request_cycle
+                      request_amount
+                  in
+                  Left (delegate, request_cycle, new_amount)
+                else Right request)
+              requests
+          in
+          let unfinalizable = (delegate, unfinalizable) in
+          return_some {finalizable; unfinalizable})
--- src/proto_alpha/lib_protocol/unstake_requests_storage.mli
+++ src/proto_alpha/lib_protocol/unstake_requests_storage.mli
@@ -27,3 +27,21 @@
 
     This module is responsible for maintaining the
     {!Storage.Contract.Unstake_requests} table. *)
+
+type prepared_finalize_unstake = {
+  finalizable : (Signature.Public_key_hash.t * Cycle_repr.t * Tez_repr.t) list;
+  unfinalizable : Signature.Public_key_hash.t * (Cycle_repr.t * Tez_repr.t) list;
+}
+
+(** [prepare_finalize_unstake ctxt contract] preprocesses a [finalize_unstake]
+    for [contract]. It returns a list of transfers [(d, c, a)] to do from
+    delegate's [d] unstaked frozen deposits for cycle [c] of amount [a] in
+    the [finalizable_field] as well as the remaining unfinalizable requests
+    that should be kept in the storage in [unfinalizable].
+
+    It returns [None] if there are no finalizable unstake requests (regardless
+    of whether there are unstake requests at all). *)
+val prepare_finalize_unstake :
+  Raw_context.t ->
+  Contract_repr.t ->
+  prepared_finalize_unstake option tzresult Lwt.t
