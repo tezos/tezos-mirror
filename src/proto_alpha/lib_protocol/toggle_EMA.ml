a23d940d45d50d2b73f99876bae05e17ba34cbad
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2023-06-07 23:04:54 +0200

    Proto/block votes: name the parameters of EMA update computation
    This commit refactors the computation of EMA update by giving names to
    the parameters of the computation.

--- src/proto_alpha/lib_protocol/toggle_EMA.ml
+++ src/proto_alpha/lib_protocol/toggle_EMA.ml
@@ -24,18 +24,55 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** Exponential moving average of toggle votes. Represented as an int64 between
-    0 and 2,000,000,000. It is an exponential moving average of the "off" votes
-    over a window of the most recent 2000 blocks that did not vote "pass". *)
+(** Block headers contain some fields whose values represent the block
+   producer's opinion on some topics. The votes are averaged to get an
+   estimation of the will of the stake-weighted majority of bakers on
+   these topics. The protocol can then perform automatic actions
+   depending on the values of these averages; typically activating or
+   deactivating some features.
 
-(* The exponential moving average is represented as an Int64 between 0L and 2_000_000_000L *)
-type t = Int64.t (* Invariant 0 <= ema <= 2_000_000_000L *)
+   This module is about the computation of these averages.
+
+   We use exponential moving averages (EMA for short) because they can
+   easily and efficiently be implemented because a single value needs to
+   be stored in the context for each average. Each EMA is updated once per
+   block and stored in the context. It is represented using a 64-bit
+   signed integer but it can only take non-negative values in a range
+   of the form 0...ema_max where the constant ema_max, the maximum
+   value of the EMA, is a parameter of this module. To update an EMA,
+   we multiply the EMA computed in the previous block by a constant
+   factor slightly less than 1 called the attenuation factor, and then
+   we either add or remove (depending on the vote that was casted in
+   the block header) another constant called the baker's contribution
+   to the EMA. The baker contribution is also a parameter of this
+   module. When multiplying by the attenuation factor, we round toward
+   the middle of the 0...ema_max range. The update formula is thus:
+
+   new_ema = ((old_ema - ema_max/2) * attenuation_factor) +- baker_contribution + ema_max/2
+
+*)
+
+module EMA_parameters = struct
+  let baker_contribution = Z.of_int 500_000
+
+  let ema_max = 2_000_000_000L
+
+  (* We don't need to parameterize by the attenuation factor because
+     it can be computed from the two other parameters with the
+     following formula:
+
+     attenuation_factor = (ema_max - 2 baker_contribution) / ema_max
+  *)
+end
+
+type t = Int64.t
+(* Invariant 0L <= ema <= EMA_Parameters.ema_max *)
 
 (* This error is not registered because we don't expect it to be
    raised. *)
 type error += Toggle_ema_out_of_bound of Int64.t
 
-let check_bounds x = Compare.Int64.(0L <= x && x <= 2_000_000_000L)
+let check_bounds x = Compare.Int64.(0L <= x && x <= EMA_parameters.ema_max)
 
 let of_int64 (x : Int64.t) : t tzresult Lwt.t =
   if check_bounds x then return x else tzfail @@ Toggle_ema_out_of_bound x
@@ -50,29 +87,37 @@ let to_int64 (ema : t) : Int64.t = ema
 
 (* We perform the computations in Z to avoid overflows. *)
 
-let z_1999 : Z.t = Z.of_int 1999
+let ema_max_z = Z.of_int64 EMA_parameters.ema_max
 
-let z_2000 : Z.t = Z.of_int 2000
+let attenuation_numerator =
+  Z.(sub ema_max_z (mul (of_int 2) EMA_parameters.baker_contribution))
 
-let attenuate z = Z.(div (mul z_1999 z) z_2000)
+let attenuation_denominator = ema_max_z
 
-let z_1_000_000_000 = Z.of_int 1_000_000_000
+let attenuate z = Z.(div (mul attenuation_numerator z) attenuation_denominator)
 
-(* Outside of this module, the EMA is always between 0 and 2,000,000,000.
-   This [recenter] wrappers, puts it in between -1,000,000,000 and 1,000,000,000.
-   The goal of this recentering around zero is to make [update_ema_off] and
-   [update_ema_on] behave symmetrically with respect to rounding. *)
-let recenter f ema = Z.(add z_1_000_000_000 (f (sub ema z_1_000_000_000)))
+let half_ema_max_z = Z.(div ema_max_z (of_int 2))
 
-let z_500_000 = Z.of_int 500_000
+(* Outside of this module, the EMA is always between 0L and ema_max.
+   This [recenter] wrappers, puts it in between -ema_max/2 and
+   ema_max/2.  The goal of this recentering around zero is to make
+   [update_ema_off] and [update_ema_on] behave symmetrically with
+   respect to rounding. *)
+let recenter f ema = Z.(add half_ema_max_z (f (sub ema half_ema_max_z)))
 
 let update_ema_off (ema : t) : t =
   let ema = Z.of_int64 ema in
-  recenter (fun ema -> Z.add (attenuate ema) z_500_000) ema |> Z.to_int64
+  recenter
+    (fun ema -> Z.add (attenuate ema) EMA_parameters.baker_contribution)
+    ema
+  |> Z.to_int64
 
 let update_ema_on (ema : t) : t =
   let ema = Z.of_int64 ema in
-  recenter (fun ema -> Z.sub (attenuate ema) z_500_000) ema |> Z.to_int64
+  recenter
+    (fun ema -> Z.sub (attenuate ema) EMA_parameters.baker_contribution)
+    ema
+  |> Z.to_int64
 
 let ( < ) : t -> Int64.t -> bool = Compare.Int64.( < )
 
