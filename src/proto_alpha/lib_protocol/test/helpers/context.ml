f68deebcdf97e8b3a1125244cb78d3f8103f0c41
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2023-06-08 21:35:22 +0200

    Proto test helpers/AI: helper fun for new launch_cycle RPC

--- src/proto_alpha/lib_protocol/test/helpers/context.ml
+++ src/proto_alpha/lib_protocol/test/helpers/context.ml
@@ -241,6 +241,9 @@ let get_liquidity_baking_subsidy ctxt =
 let get_liquidity_baking_cpmm_address ctxt =
   Alpha_services.Liquidity_baking.get_cpmm_address rpc_ctxt ctxt
 
+let get_adaptive_inflation_launch_cycle ctxt =
+  Adaptive_inflation_services.launch_cycle rpc_ctxt ctxt
+
 let get_seed_nonce_revelation_tip ctxt =
   get_constants ctxt >>=? fun {Constants.parametric = csts; _} ->
   return
