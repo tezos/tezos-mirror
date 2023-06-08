f9333ebfedb315915f6ee608ddbc27375c1945b0
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-06-08 12:42:17 +0200

    Proto/Tez: mul_ratio

--- src/proto_alpha/lib_protocol/tez_repr.mli
+++ src/proto_alpha/lib_protocol/tez_repr.mli
@@ -70,6 +70,10 @@ val div2 : t -> t
 (** [div2_sub tez] returns [(⌊tez / 2⌋, tez - ⌊tez / 2⌋)]. *)
 val div2_sub : t -> t * t
 
+(** [mul_ratio tez ~num ~den] returns [tez * num / den] without failing
+    when [tez * num] overflows. *)
+val mul_ratio : t -> num:int64 -> den:int64 -> t tzresult
+
 val to_mutez : t -> int64
 
 (** [of_mutez n] (micro tez) is None if n is negative *)
