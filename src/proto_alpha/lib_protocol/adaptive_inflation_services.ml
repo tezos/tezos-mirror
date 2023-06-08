49327509d0f9a037e2c38e5614e5444f4b05ef44
Lucas Randazzo <lucas@nomadic-labs.com>
2023-06-08 14:14:40 +0200

    Proto/AI/RPC: add helper functions

--- src/proto_alpha/lib_protocol/adaptive_inflation_services.ml
+++ src/proto_alpha/lib_protocol/adaptive_inflation_services.ml
@@ -45,6 +45,41 @@ module S = struct
       RPC_path.(context_path / "total_frozen_stake")
 end
 
+let q_to_float_string q =
+  let offset = 1000 in
+  let unit = Z.div q.Q.num q.den in
+  let q = Q.(sub q (unit /// Z.one)) in
+  let q = Q.(mul q (offset // 1)) in
+  let dec = Z.div q.num q.den in
+  let padded_dec_string = Format.asprintf "%03d" (Z.to_int dec) in
+  Format.asprintf "%a.%s" Z.pp_print unit padded_dec_string
+
+let current_rewards_per_minute ctxt =
+  let open Lwt_result_syntax in
+  let base_total_rewards_per_minute =
+    (Constants.reward_weights ctxt).base_total_rewards_per_minute
+  in
+  let q_base_total_rewards_per_minute =
+    Tez.to_mutez base_total_rewards_per_minute |> Q.of_int64
+  in
+  let cycle = (Level.current ctxt).cycle in
+  let* f = Delegate.Rewards.For_RPC.get_reward_coeff ctxt ~cycle in
+  let f = Q.mul f q_base_total_rewards_per_minute (* rewards per minute *) in
+  return f
+
+(* Does the reverse operations of [compute_coeff] in [adaptive_inflation_storage.ml] *)
+let current_yearly_rate_value ~formatter ctxt =
+  let open Lwt_result_syntax in
+  let q_min_per_year = Q.of_int 525600 in
+  let* total_supply = Contract.get_total_supply ctxt in
+  let q_total_supply = Tez.to_mutez total_supply |> Q.of_int64 in
+  let* f = current_rewards_per_minute ctxt in
+  let f = Q.div f q_total_supply (* inflation per minute *) in
+  let f = Q.mul f q_min_per_year (* inflation per year *) in
+  (* transform into a string *)
+  let f = Q.(mul f (100 // 1)) in
+  return (formatter f)
+
 let register () =
   let open Services_registration in
   register0 ~chunked:false S.total_supply (fun ctxt () () ->
