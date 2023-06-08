19517a3b72e9609faf16c92d749a9b79e03ae665
Alain Mebsout <alain.mebsout@functori.com>
2023-06-08 11:35:28 +0200

    SCORU/Node/Mumbai: use Layer1 from octez_smart_rollup_node

---
+++ src/proto_016_PtMumbai/lib_sc_rollup_node/layer1_helpers.mli
@@ -0,0 +1,40 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2023 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(* Copyright (c) 2023 Functori, <contact@functori.com>                       *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Octez_smart_rollup_node.Layer1
+
+(** [fetch_tezos_block cctxt hash] returns a block info given a block hash.
+    Looks for the block in the blocks cache first, and fetches it from the L1
+    node otherwise. *)
+val fetch_tezos_block :
+  t ->
+  Block_hash.t ->
+  Protocol_client_context.Alpha_block_services.block_info tzresult Lwt.t
+
+(** [prefetch_tezos_blocks l1_ctxt blocks] prefetches the blocks
+    asynchronously. NOTE: the number of blocks to prefetch must not be greater
+    than the size of the blocks cache otherwise they will be lost. *)
+val prefetch_tezos_blocks : t -> head list -> unit
