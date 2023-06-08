4af05a427727df482b1e93b462948fa7b40838ea
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-06-08 08:35:40 +0200

    [scripts/version.sh]: Bump [full_opam_repository_tag]

--- scripts/version.sh
+++ scripts/version.sh
@@ -20,7 +20,7 @@ export recommended_node_version=16.18.1
 
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-export full_opam_repository_tag=0e89b5b0195c6f2597c95251896e1ef8b199c1d2
+export full_opam_repository_tag=8f3e687679802b20b0ddcca7e9031108751f8e08
 
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml`
