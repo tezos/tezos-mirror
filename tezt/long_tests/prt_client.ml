19087a15fb00019c9b10c6609d795baaa4adb134
François Thiré <francois.thire@nomadic-labs.com>
2023-06-09 10:05:05 +0200

    Tezt/PRT: Increase the sample size

--- tezt/long_tests/prt_client.ml
+++ tezt/long_tests/prt_client.ml
@@ -60,7 +60,7 @@ let client_load_time ~executors () =
     ~executors
   @@ fun () ->
   let client = Client.create () in
-  Long_test.time_lwt ~repeat:5 load_time @@ fun () -> Client.version client
+  Long_test.time_lwt ~repeat:20 load_time @@ fun () -> Client.version client
 
 let get_blocks_response_time ~executors ~protocol =
   Long_test.register
@@ -71,7 +71,7 @@ let get_blocks_response_time ~executors ~protocol =
     ~executors
   @@ fun () ->
   let* _node, client = Client.init_with_protocol `Client ~protocol () in
-  Long_test.time_lwt response_time_measurement @@ fun () ->
+  Long_test.time_lwt ~repeat:20 response_time_measurement @@ fun () ->
   let* _ = RPC.Client.call client @@ RPC.get_chain_block () in
   unit
 
