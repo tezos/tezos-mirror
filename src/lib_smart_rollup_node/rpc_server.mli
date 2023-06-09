e088da16a6980048637328c986f58365d3003b6f
Alain Mebsout <alain.mebsout@functori.com>
2023-06-09 22:23:22 +0200

    SCORU/Node: RPC server with changeable directory
    Start and stop.

---
+++ src/lib_smart_rollup_node/rpc_server.mli
@@ -0,0 +1,40 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2022 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(* Copyright (c) 2022-2023 TriliTech <contact@trili.tech>                    *)
+(* Copyright (c) 2023 Functori, <contact@functori.com>                       *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type t
+
+(** [start node_ctxt config] starts an RPC server listening for requests on the
+    port [config.rpc_port] and address [config.rpc_addr]. *)
+val start : Configuration.t -> unit Tezos_rpc.Directory.t -> t tzresult Lwt.t
+
+(** Shutdown a running RPC server. When this function is called, the rollup node
+    will stop listening to incoming requests. *)
+val shutdown : t -> unit Lwt.t
+
+(** Changes the directory for the RPC server. WARNING: this function stops and
+    restarts the RPC server. *)
+val change_directory : t -> unit Tezos_rpc.Directory.t -> t tzresult Lwt.t
