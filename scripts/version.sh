21f36849ea2f63fe65141106ca6f078289fa3d67
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2023-06-11 17:33:30 +0200

    Doc: Alpine uses python 3.10.12 bugfix release

--- scripts/version.sh
+++ scripts/version.sh
@@ -25,7 +25,7 @@ export full_opam_repository_tag=0e89b5b0195c6f2597c95251896e1ef8b199c1d2
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml`
 export opam_repository_url=https://gitlab.com/tezos/opam-repository
-export opam_repository_tag="${OPAM_REPOSITORY_TAG:-74aaa3a741443be2f5ee4d08e6b63898ec99f450}"
+export opam_repository_tag="${OPAM_REPOSITORY_TAG:-cf7920c4fe635f92bc703fe7ae6fd38c9b4ba8a3}"
 export opam_repository_git="$opam_repository_url.git"
 export opam_repository="$opam_repository_git"\#"$opam_repository_tag"
 
