6b1d89cd7fc8f4b4eded71307a3f325a0f8cafcd
iguerNL@Functori <iguer@functori.com>
2023-06-13 15:45:07 +0200

    DAL/Client: add a function dal_publish to send slot headers to mempool

--- src/proto_alpha/lib_client/client_proto_context.ml
+++ src/proto_alpha/lib_client/client_proto_context.ml
@@ -1311,3 +1311,34 @@ let zk_rollup_update (cctxt : #full) ~chain ~block ?confirmations ?dry_run
   match Apply_results.pack_contents_list op result with
   | Apply_results.Single_and_result ((Manager_operation _ as op), result) ->
       return (oph, op, result)
+
+let dal_publish (cctxt : #full) ~chain ~block ?confirmations ?dry_run
+    ?verbose_signing ?simulation ?fee ?gas_limit ?storage_limit ?counter ~source
+    ~slot_header ~src_pk ~src_sk ~fee_parameter () =
+  Annotated_manager_operation.Single_manager
+    (Injection.prepare_manager_operation
+       ~fee:(Limit.of_option fee)
+       ~gas_limit:(Limit.of_option gas_limit)
+       ~storage_limit:(Limit.of_option storage_limit)
+       (Dal_publish_slot_header slot_header))
+  |> Injection.inject_manager_operation
+       cctxt
+       ~chain
+       ~block
+       ?confirmations
+       ?dry_run
+       ?verbose_signing
+       ?simulation
+       ?counter
+       ~source
+       ~fee:(Limit.of_option fee)
+       ~storage_limit:(Limit.of_option storage_limit)
+       ~gas_limit:(Limit.of_option gas_limit)
+       ~src_pk
+       ~src_sk
+       ~fee_parameter
+       ~successor_level:true
+  >>=? fun (oph, _, op, result) ->
+  match Apply_results.pack_contents_list op result with
+  | Apply_results.Single_and_result ((Manager_operation _ as op), result) ->
+      return (oph, op, result)
