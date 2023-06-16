746a3c920f4cb6c2e30d9094efaa3b7a93dddd35
Martin Tomazic <martin.tomazic97@gmail.com>
2023-06-16 15:24:38 +0200

    DAC: Ensure `Unknown` variant of `Aggregate_signature.t`

--- src/lib_dac/signature_repr.mli
+++ src/lib_dac/signature_repr.mli
@@ -24,49 +24,34 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-type t = {
-  root_hash : Dac_plugin.raw_hash;
-  signature : Tezos_crypto.Aggregate_signature.t;
-  signer_pkh : Tezos_crypto.Aggregate_signature.public_key_hash;
-}
+(** [t] is a representation of committee member signature. *)
+type t
 
-let encoding =
-  Data_encoding.(
-    conv
-      (fun {root_hash; signature; signer_pkh} ->
-        (root_hash, signature, signer_pkh))
-      (fun (root_hash, signature, signer_pkh) ->
-        {root_hash; signature; signer_pkh})
-      (obj3
-         (req "root_hash" Dac_plugin.raw_hash_encoding)
-         (req "signature" Tezos_crypto.Aggregate_signature.encoding)
-         (req
-            "signer_pkh"
-            Tezos_crypto.Aggregate_signature.Public_key_hash.encoding)))
+(** [encoding dac_plugin] returns the [t Data_encoding.t] given a [dac_plugin]. *)
+val encoding : t Data_encoding.t
 
-(** [ensure_unknown_aggregate_sig_variant sig_rep] ensures that the underlying
-    [Aggregate_signature.t] always corresponds to [Unknown] variant. 
+(** [make root_hash signature pkh] creates a [t]. In addition, it ensures that
+    the underlying [Aggregate_signature.t] always corresponds to [Unknown]
+    variant.
     
-    Motivation: 
-    [Aggregate_signature.t] has two variants: [Bls12_381 of Bls.t] and
-    [Unknown of Bytes.t]. Their binary repr is exactly the same. In json though,
-    the encoding adds a prefix. For [Bls12_381] variant it adds the "BLsig"
-    prefix and for [Unknown] variant it adds the "asig" prefix.
-    In practice this means that when signature is computed by Committee member,
-    it will have "Blsig" json prefix. When read by Coordinator from the store,
-    it will be read as [Unknown] variant and later served with "asig" prefix in
-    JSON. This helper is used to guarantee consistent "asig" prefix. *)
-let ensure_unknown_sig_variant signature =
-  let open Tezos_crypto.Aggregate_signature in
-  match signature with
-  | Unknown _ as unknown -> unknown
-  | Bls12_381 _ as bls -> Unknown (to_bytes bls)
+    Motivation for the [Unknown] variant:
+    [Unknown] variant uses "asig" JSON prefix, whereas [Bls12_381] variant uses
+    "BLsig" JSON prefix. To ensure consistent signature prefix, when data is
+    exchanged accross the DAC network we use [Unknown] variant only. *)
+val make :
+  Dac_plugin.raw_hash ->
+  Tezos_crypto.Aggregate_signature.signature ->
+  Tezos_crypto.Aggregate_signature.public_key_hash ->
+  t
 
-let make root_hash signature signer_pkh =
-  {root_hash; signature = ensure_unknown_sig_variant signature; signer_pkh}
+(** [get_root_hash signature] returns a root hash of a given signature
+    representation. *)
+val get_root_hash : t -> Dac_plugin.raw_hash
 
-let get_root_hash {root_hash; _} = root_hash
+(** [get_signature signature] returns an aggregated signature of a given
+    signature representation. *)
+val get_signature : t -> Tezos_crypto.Aggregate_signature.signature
 
-let get_signature {signature; _} = signature
-
-let get_signer_pkh {signer_pkh; _} = signer_pkh
+(** [get_signer_pkh signature] returns a signer public key hash of a given
+    signature representation. *)
+val get_signer_pkh : t -> Tezos_crypto.Aggregate_signature.public_key_hash
