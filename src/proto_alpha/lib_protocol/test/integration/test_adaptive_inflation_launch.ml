5cd68e2377eb7817423366ec682b8fdd86a05925
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2023-06-20 21:27:45 +0200

    Tests/AI/Launch: check total_frozen_stake changes after costaking
    `preserved_cycles` after a costaking event, the total_frozen_stake
    increases by the costaked amount.

--- src/proto_alpha/lib_protocol/test/integration/test_adaptive_inflation_launch.ml
+++ src/proto_alpha/lib_protocol/test/integration/test_adaptive_inflation_launch.ml
@@ -132,6 +132,7 @@ let test_launch threshold expected_vote_duration () =
     let consensus_threshold = 0 in
     {default_constants with consensus_threshold; adaptive_inflation}
   in
+  let preserved_cycles = constants.preserved_cycles in
   let* block, delegate = Context.init_with_constants1 constants in
   let delegate_pkh = Context.Contract.pkh delegate in
   let* () = assert_is_not_yet_set_to_launch ~loc:__LOC__ block in
@@ -287,7 +288,14 @@ let test_launch threshold expected_vote_duration () =
   let* balance = Context.Contract.balance (B block) wannabe_costaker in
   let*?@ balance_to_stake = Protocol.Alpha_context.Tez.(balance -? one) in
   let* operation = stake (B block) wannabe_costaker balance_to_stake in
-  let* (_block : Block.t) = Block.bake ~operation block in
+  let* block = Block.bake ~operation block in
+  let* block = Block.bake_until_n_cycle_end (preserved_cycles + 1) block in
+  let* () =
+    assert_total_frozen_stake
+      ~loc:__LOC__
+      block
+      (Protocol.Alpha_context.Tez.of_mutez_exn 3_999_999_000_000L)
+  in
   return_unit
 
 let tests =
