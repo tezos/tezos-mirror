mavryk-network/mavryk-protocol

kernel/sequencer: add message definition

src/kernel_sequencer/src/message.rs
@@ -81,6 +81,7 @@ impl Tag for SetSequencer {
 pub enum KernelMessage {
     Sequence(Framed<Sequence>),
     SetSequencer(Framed<SetSequencer>),
+    Message(Vec<u8>),
 }
 
 impl<P> NomReader for Framed<P>
@@ -137,6 +138,10 @@ impl NomReader for KernelMessage {
                 preceded(tag([1]), Framed::<SetSequencer>::nom_read),
                 KernelMessage::SetSequencer,
             )),
+            map(
+                |bytes: &[u8]| Ok(([].as_slice(), bytes.to_vec())),
+                KernelMessage::Message,
+            ),
         )))(input)
     }
 }
@@ -154,6 +159,7 @@ impl BinWriter for KernelMessage {
                 enc::put_byte(&0x01, output);
                 set_sequencer.bin_write(output)?;
             }
+            KernelMessage::Message(message) => enc::put_bytes(message, output),
         }
         Ok(())
     }
@@ -166,7 +172,7 @@ mod tests {
     use super::{KernelMessage, Sequence};
     use crate::message::SetSequencer;
     use tezos_crypto_rs::hash::{SecretKeyEd25519, SeedEd25519};
-    use tezos_data_encoding::enc::BinWriter;
+    use tezos_data_encoding::enc::{self, BinWriter};
     use tezos_data_encoding::nom::NomReader;
     use tezos_smart_rollup_encoding::public_key::PublicKey;
     use tezos_smart_rollup_encoding::smart_rollup::SmartRollupAddress;
@@ -233,5 +239,49 @@ mod tests {
         let (_, msg_read) = KernelMessage::nom_read(&bin).expect("deserialization should work");
 
         assert_eq!(msg_read, sequence);
+    }
+
+    #[test]
+    fn test_user_message_serialization() {
+        let sequence = KernelMessage::Message(vec![0x01, 0x0, 0x01, 0x02, 0x02]);
+
+        // Serializing
+        let mut bin: Vec<u8> = Vec::new();
+        sequence.bin_write(&mut bin).unwrap();
+
+        // Deserializing
+        let (_, msg_read) = KernelMessage::nom_read(&bin).expect("deserialization should work");
+
+        assert_eq!(msg_read, sequence);
+    }
+
+    #[test]
+    fn test_message_default() {
+        let (_, secret) = key_pair("edsk3a5SDDdMWw3Q5hPiJwDXUosmZMTuKQkriPqY6UqtSfdLifpZbB");
+        let signature = secret.sign([0x0]).expect("sign should work");
+
+        let sequence = KernelMessage::Sequence(Framed {
+            destination: SmartRollupAddress::from_b58check("sr1EzLeJYWrvch2Mhvrk1nUVYrnjGQ8A4qdb")
+                .expect("decoding should work"),
+            payload: Sequence {
+                signature,
+                delayed_messages: 5,
+                messages: Vec::default(),
+            },
+        });
+
+        // Serializing
+        let mut bin: Vec<u8> = Vec::new();
+        sequence.bin_write(&mut bin).unwrap();
+        enc::put_bytes(&[0x01, 0x02, 0x03, 0x04], &mut bin);
+
+        println!("{:?}", bin);
+
+        // Deserializing
+        let (remaining, msg_read) =
+            KernelMessage::nom_read(&bin).expect("deserialization should work");
+
+        assert!(remaining.is_empty());
+        assert_eq!(msg_read, KernelMessage::Message(bin))
     }
 }

tezos/tezos-mirror

src/kernel_sequencer/src/message.rs
@@ -2,7 +2,12 @@
 //
 // SPDX-License-Identifier: MIT
 
-use nom::{bytes::complete::tag, combinator::map, sequence::preceded};
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    combinator::{all_consuming, map},
+    sequence::preceded,
+};
 use tezos_crypto_rs::hash::Signature;
 use tezos_data_encoding::{
     enc::{self, BinResult, BinWriter},
@@ -62,6 +67,7 @@ pub enum SequencerMsg {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum KernelMessage {
     Sequencer(Framed<SequencerMsg>),
+    DelayedMessage(Vec<u8>),
 }
 
 impl<P> NomReader for Framed<P>
@@ -105,10 +111,16 @@ where
 
 impl NomReader for KernelMessage {
     fn nom_read(input: &[u8]) -> NomResult<Self> {
-        map(
-            preceded(tag([1]), Framed::<SequencerMsg>::nom_read),
-            KernelMessage::Sequencer,
-        )(input)
+        all_consuming(alt((
+            all_consuming(map(
+                preceded(tag([1]), Framed::<SequencerMsg>::nom_read),
+                KernelMessage::Sequencer,
+            )),
+            map(
+                |bytes: &[u8]| Ok(([].as_slice(), bytes.to_vec())),
+                KernelMessage::DelayedMessage,
+            ),
+        )))(input)
     }
 }
 
@@ -120,6 +132,7 @@ impl BinWriter for KernelMessage {
                 enc::put_byte(&0x01, output);
                 sequencer_framed_msg.bin_write(output)?;
             }
+            KernelMessage::DelayedMessage(message) => enc::put_bytes(message, output),
         }
         Ok(())
     }
@@ -132,7 +145,7 @@ mod tests {
     use super::{KernelMessage, Sequence};
     use crate::message::SetSequencer;
     use tezos_crypto_rs::hash::{SecretKeyEd25519, SeedEd25519};
-    use tezos_data_encoding::enc::BinWriter;
+    use tezos_data_encoding::enc::{self, BinWriter};
     use tezos_data_encoding::nom::NomReader;
     use tezos_smart_rollup_encoding::public_key::PublicKey;
     use tezos_smart_rollup_encoding::smart_rollup::SmartRollupAddress;
@@ -201,5 +214,50 @@ mod tests {
         let (_, msg_read) = KernelMessage::nom_read(&bin).expect("deserialization should work");
 
         assert_eq!(msg_read, sequence);
+    }
+
+    #[test]
+    fn test_user_message_serialization() {
+        let sequence = KernelMessage::DelayedMessage(vec![0x01, 0x0, 0x01, 0x02, 0x02]);
+
+        // Serializing
+        let mut bin: Vec<u8> = Vec::new();
+        sequence.bin_write(&mut bin).unwrap();
+
+        // Deserializing
+        let (_, msg_read) = KernelMessage::nom_read(&bin).expect("deserialization should work");
+
+        assert_eq!(msg_read, sequence);
+    }
+
+    #[test]
+    fn test_message_default() {
+        let (_, secret) = key_pair("edsk3a5SDDdMWw3Q5hPiJwDXUosmZMTuKQkriPqY6UqtSfdLifpZbB");
+        let signature = secret.sign([0x0]).expect("sign should work");
+
+        let sequence = KernelMessage::Sequencer(Framed {
+            destination: SmartRollupAddress::from_b58check("sr1EzLeJYWrvch2Mhvrk1nUVYrnjGQ8A4qdb")
+                .expect("decoding should work"),
+            payload: SequencerMsg::Sequence(Sequence {
+                nonce: 0,
+                signature,
+                delayed_messages: 5,
+                messages: Vec::default(),
+            }),
+        });
+
+        // Serializing
+        let mut bin: Vec<u8> = Vec::new();
+        sequence.bin_write(&mut bin).unwrap();
+        enc::put_bytes(&[0x01, 0x02, 0x03, 0x04], &mut bin);
+
+        println!("{:?}", bin);
+
+        // Deserializing
+        let (remaining, msg_read) =
+            KernelMessage::nom_read(&bin).expect("deserialization should work");
+
+        assert!(remaining.is_empty());
+        assert_eq!(msg_read, KernelMessage::DelayedMessage(bin))
     }
 }