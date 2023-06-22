a497c36dd7a9acd7af044680630f2c067fbd6c24
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2023-06-22 17:25:11 +0200

    EVM/Kernel: index new accounts during transaction application

--- src/kernel_evm/kernel/src/apply.rs
+++ src/kernel_evm/kernel/src/apply.rs
@@ -16,6 +16,8 @@ use tezos_ethereum::transaction::TransactionHash;
 use tezos_smart_rollup_debug::{debug_msg, Runtime};
 
 use crate::error::{Error, TransferError};
+use crate::indexable_storage::IndexableStorage;
+use crate::storage::index_account;
 
 /// This defines the needed function to apply a transaction.
 pub trait ApplicableTransaction {
@@ -184,6 +186,28 @@ fn make_object_info(
     }
 }
 
+// From a receipt, indexes the caller, recipient and the new address if needs
+// be.
+fn index_new_accounts<Host: Runtime>(
+    host: &mut Host,
+    accounts_index: &mut IndexableStorage,
+    receipt: &TransactionReceiptInfo,
+) -> Result<(), Error> {
+    index_account(host, &receipt.caller, accounts_index)?;
+    if let Some(to) = receipt.to {
+        index_account(host, &to, accounts_index)?
+    };
+    match receipt
+        .execution_outcome
+        .as_ref()
+        .and_then(|o| o.new_address)
+    {
+        Some(to) => index_account(host, &to, accounts_index),
+        None => Ok(()),
+    }
+}
+
+#[allow(clippy::too_many_arguments)]
 pub fn apply_transaction<Host: Runtime>(
     host: &mut Host,
     block_constants: &BlockConstants,
@@ -192,6 +216,7 @@ pub fn apply_transaction<Host: Runtime>(
     transaction_hash: TransactionHash,
     index: u32,
     evm_account_storage: &mut EthereumAccountStorage,
+    accounts_index: &mut IndexableStorage,
 ) -> Result<Option<(TransactionReceiptInfo, TransactionObjectInfo)>, Error> {
     let caller = match transaction.caller() {
         Ok(caller) => caller,
@@ -245,6 +270,7 @@ pub fn apply_transaction<Host: Runtime>(
         make_receipt_info(transaction_hash, index, execution_outcome, caller, to);
     let object_info =
         make_object_info(transaction, transaction_hash, caller, index, gas_used);
+    index_new_accounts(host, accounts_index, &receipt_info)?;
 
     Ok(Some((receipt_info, object_info)))
 }
