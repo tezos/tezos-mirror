OKEAMAH/Git

Crypto/Timelock: remove rsa_public from chest

src/lib_crypto/timelock.ml
@@ -78,8 +78,6 @@ let decrypt symmetric_key ciphertext =
     ciphertext.nonce
 
 (* -------- Timelock types, conversion functions and encodings -------- *)
-(* RSA group rsa2048 = p * q where p and q are prime numbers *)
-type rsa_public = Z.t
 
 (* default RSA rsa2048: the 2048 bit RSA rsa2048 challenge
    c.f. https://en.wikipedia.org/wiki/RSA_numbers#RSA-2048 *)
@@ -267,25 +265,20 @@ let puzzle_to_symmetric_key ~time puzzle proof =
   else None
 
 (* -------- Timelock high level functions (used in Tezos) -------- *)
-type chest = {puzzle : puzzle; rsa_public : rsa_public; ciphertext : ciphertext}
+type chest = {puzzle : puzzle; ciphertext : ciphertext}
 
 let chest_encoding =
   let open Data_encoding in
   def "timelock.chest"
   @@ conv_with_guard
-       (fun chest -> (chest.puzzle, chest.rsa_public, chest.ciphertext))
-       (fun (puzzle, rsa_public, ciphertext) ->
-         if Z.Compare.(puzzle < Z.zero || puzzle >= rsa_public) then
+       (fun chest -> (chest.puzzle, chest.ciphertext))
+       (fun (puzzle, ciphertext) ->
+         if Z.Compare.(puzzle < Z.zero || puzzle >= rsa2048) then
            Error "puzzle is not in the RSA group"
-         else if not @@ Z.equal rsa_public rsa2048 then
-           Error "not RSA2048 rsa2048"
          else if not @@ (Bytes.length ciphertext.payload > Crypto_box.tag_length)
          then Error "unexpected payload (smaller than expected tag length)"
-         else Ok {puzzle; rsa_public; ciphertext})
-       (obj3
-          (req "puzzle" n)
-          (req "rsa_public" n)
-          (req "ciphertext" ciphertext_encoding))
+         else Ok {puzzle; ciphertext})
+       (obj2 (req "puzzle" n) (req "ciphertext" ciphertext_encoding))
 
 type chest_key = timelock_proof
 
@@ -300,7 +293,7 @@ let create_chest_and_chest_key ?(precompute_path = None) ~payload ~time () =
   in
   let sym_key = timelock_proof_to_symmetric_key proof in
   let ciphertext = encrypt sym_key payload in
-  ({puzzle; rsa_public = rsa2048; ciphertext}, proof)
+  ({puzzle; ciphertext}, proof)
 
 let create_chest_key chest ~time = unlock_and_prove ~time chest.puzzle
 

src/lib_crypto/timelock.mli
@@ -47,11 +47,6 @@
 (** We will time-lock symmetric keys to then handle arbitrary bytes *)
 type symmetric_key
 
-(** RSA public key to define a group in which we will work.
-    The key is an integer n = p*q with p, q primes number. The group we work in
-    is the set of inversible mod n. *)
-type rsa_public
-
 (** Locked value that can be accessed with a number of sequential operations.
     It is concretely a member of the RSA group. *)
 type puzzle
@@ -151,7 +146,7 @@ val proof_encoding : timelock_proof Data_encoding.t
 
 (** Contains a value (the decryption of the ciphertext) that can be provably
     recovered in [time] sequential operation. *)
-type chest = {puzzle : puzzle; rsa_public : rsa_public; ciphertext : ciphertext}
+type chest = {puzzle : puzzle; ciphertext : ciphertext}
 
 val chest_encoding : chest Data_encoding.t
 

tezos/tezos-mirror

src/lib_crypto/timelock.ml
@@ -78,8 +78,6 @@ let decrypt symmetric_key ciphertext =
     ciphertext.nonce
 
 (* -------- Timelock types, conversion functions and encodings -------- *)
-(* RSA group rsa2048 = p * q where p and q are prime numbers *)
-type rsa_public = Z.t
 
 (* default RSA rsa2048: the 2048 bit RSA rsa2048 challenge
    c.f. https://en.wikipedia.org/wiki/RSA_numbers#RSA-2048 *)
@@ -296,29 +294,20 @@ let locked_value_to_symmetric_key ~time locked_value proof =
   else None
 
 (* -------- Timelock high level functions (used in Tezos) -------- *)
-type chest = {
-  locked_value : locked_value;
-  rsa_public : rsa_public;
-  ciphertext : ciphertext;
-}
+type chest = {locked_value : locked_value; ciphertext : ciphertext}
 
 let chest_encoding =
   let open Data_encoding in
   def "timelock.chest"
   @@ conv_with_guard
-       (fun chest -> (chest.locked_value, chest.rsa_public, chest.ciphertext))
-       (fun (locked_value, rsa_public, ciphertext) ->
-         if Z.Compare.(locked_value < Z.zero || locked_value >= rsa_public) then
+       (fun chest -> (chest.locked_value, chest.ciphertext))
+       (fun (locked_value, ciphertext) ->
+         if Z.Compare.(locked_value < Z.zero) then
            Error "locked value is not in the rsa group"
-         else if not @@ Z.equal rsa_public rsa2048 then
-           Error "not RSA2048 rsa2048"
          else if not @@ (Bytes.length ciphertext.payload > Crypto_box.tag_length)
          then Error "unexpected payload (smaller than expected tag length)"
-         else Ok {locked_value; rsa_public; ciphertext})
-       (obj3
-          (req "locked_value" n)
-          (req "rsa_public" n)
-          (req "ciphertext" ciphertext_encoding))
+         else Ok {locked_value; ciphertext})
+       (obj2 (req "locked_value" n) (req "ciphertext" ciphertext_encoding))
 
 type chest_key = timelock_proof
 
@@ -333,7 +322,7 @@ let create_chest_and_chest_key ?(precompute_path = None) ~payload ~time () =
   in
   let sym_key = timelock_proof_to_symmetric_key proof in
   let ciphertext = encrypt sym_key payload in
-  ({locked_value; rsa_public = rsa2048; ciphertext}, proof)
+  ({locked_value; ciphertext}, proof)
 
 let create_chest_key chest ~time = unlock_and_prove ~time chest.locked_value
 

src/lib_crypto/timelock.mli
@@ -47,11 +47,6 @@
 (** We will time-lock symmetric keys to then handle arbitrary bytes *)
 type symmetric_key
 
-(** RSA public key to define a group in which we will work.
-    The key is an integer n = p*q with p, q primes number. The group we work in
-    is the set of inversible mod n. *)
-type rsa_public
-
 (** Locked value that can be accessed with a number of sequential operations.
     It is concretely a member of the RSA group. *)
 type locked_value
@@ -167,11 +162,7 @@ val proof_encoding : timelock_proof Data_encoding.t
 
 (** Contains a value (the decryption of the ciphertext) that can be provably
     recovered in [time] sequential operation. *)
-type chest = {
-  locked_value : locked_value;
-  rsa_public : rsa_public;
-  ciphertext : ciphertext;
-}
+type chest = {locked_value : locked_value; ciphertext : ciphertext}
 
 val chest_encoding : chest Data_encoding.t
 