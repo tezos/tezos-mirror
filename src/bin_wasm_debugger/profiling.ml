ef00a6792b7e5cf17c77e1e8eb1ba49f95844662
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2023-06-29 17:46:01 +0200

    WASM/Profile: profile time

--- src/bin_wasm_debugger/profiling.ml
+++ src/bin_wasm_debugger/profiling.ml
@@ -38,7 +38,11 @@ module Vector = Lazy_vector.Int32Vector
     A node contains:
     - [id]: a function call representation (an identifier)
     - [t]: the ticks elapsed during the call
+    - [time]: the time elapsed during the call
     - [sub]: the subcalls.
+
+    Note that for the rest of the algorithm, `time` will be eluded as its
+    computation is equivalent to the ticks.
 
     The algorithm starts with an empty toplevel and an empty continuation.
     - on function call (id, current_tick, current_node, continuation):
@@ -88,44 +92,65 @@ module Vector = Lazy_vector.Int32Vector
 *)
 
 type 'function_call call_stack =
-  | Node of 'function_call * Z.t * 'function_call call_stack list
+  | Node of
+      'function_call * Z.t * Ptime.span option * 'function_call call_stack list
   | Toplevel of 'function_call call_stack list
 
 let rec fold_call_stack f acc = function
-  | Node (call, ticks, substacks) ->
-      List.fold_left (fold_call_stack f) (f acc call ticks) substacks
+  | Node (call, ticks, time, substacks) ->
+      List.fold_left (fold_call_stack f) (f acc call ticks time) substacks
   | Toplevel substacks -> List.fold_left (fold_call_stack f) acc substacks
+
+(* [sub_opt_time t1 t2] returns `Some (t1 - t2)` if both are not `None`, and returns
+   `None` otherwise. This function propagates the non usage of time if
+   `--without-time` is specified by the profiler. *)
+let sub_opt_times t1 t2 =
+  match (t1, t2) with
+  | Some t1, Some t2 -> Some (Ptime.Span.sub t1 t2)
+  | _, _ -> None
 
 (** [end_function_call current_tick current_function call_stack] implements an
     ending call. Please refer to the prelude of the file. *)
-let end_function_call current_tick current_function call_stack =
+let end_function_call current_tick current_time current_function call_stack =
+  let current_time = current_time () in
   match current_function with
-  | Node (call, starting_tick, subcalls) -> (
-      let final_node =
-        Node (call, Z.sub current_tick starting_tick, List.rev subcalls)
-      in
+  | Node (call, starting_tick, starting_time, subcalls) -> (
+      let tick = Z.sub current_tick starting_tick in
+      let time = sub_opt_times current_time starting_time in
+      let final_node = Node (call, tick, time, List.rev subcalls) in
       match call_stack with
       | [] -> assert false
       | Toplevel finalized :: stack ->
           (Toplevel (final_node :: finalized), stack)
-      | Node (call, ticks, subcalls) :: stack ->
-          (Node (call, Z.sub current_tick ticks, final_node :: subcalls), stack)
-      )
+      | Node (call, ticks, time, subcalls) :: stack ->
+          ( Node
+              ( call,
+                Z.sub current_tick ticks,
+                sub_opt_times current_time time,
+                final_node :: subcalls ),
+            stack ))
   (* A toplevel call cannot reduce. *)
   | Toplevel _ -> (current_function, call_stack)
 
 (** [call_function called_function current_tick current_function call_stack]
     implements a function start. Please refere to the prelude of the module. *)
-let call_function called_function current_tick current_function call_stack =
+let call_function called_function current_tick current_time current_function
+    call_stack =
+  let current_time = current_time () in
   match current_function with
   | Toplevel _ as top ->
-      let func = Node (called_function, current_tick, []) in
+      let func = Node (called_function, current_tick, current_time, []) in
       (func, top :: call_stack)
-  | Node (current_call, ticks, subcalls) ->
+  | Node (current_call, ticks, time, subcalls) ->
       let stack =
-        Node (current_call, Z.sub current_tick ticks, subcalls) :: call_stack
+        Node
+          ( current_call,
+            Z.sub current_tick ticks,
+            sub_opt_times current_time time,
+            subcalls )
+        :: call_stack
       in
-      let func = Node (called_function, current_tick, []) in
+      let func = Node (called_function, current_tick, current_time, []) in
       (func, stack)
 
 (** Profiling the execution of the PVM *)
@@ -149,68 +174,92 @@ let initial_eval_call = Function Constants.wasm_entrypoint
 
 (** [update_on_decode current_tick current_call_context] starts and stop
     `internal` calls related to the {Decode} step of the PVM. *)
-let update_on_decode current_tick (current_node, call_stack) =
+let update_on_decode current_tick current_time (current_node, call_stack) =
   let open Lwt_syntax in
   function
   | Decode.MKStart ->
       return_some
-      @@ call_function (Internal "decode") current_tick current_node call_stack
+      @@ call_function
+           (Internal "decode")
+           current_tick
+           current_time
+           current_node
+           call_stack
   | Decode.MKStop _ ->
       let current_node, call_stack =
-        end_function_call current_tick current_node call_stack
+        end_function_call current_tick current_time current_node call_stack
       in
       return_some
-      @@ call_function (Internal "link") current_tick current_node call_stack
+      @@ call_function
+           (Internal "link")
+           current_tick
+           current_time
+           current_node
+           call_stack
   | _ -> return_none
 
 (** [update_on_link current_tick current_call_context] starts and stop
     `internal` call to the {Link} step of the PVM. *)
-let update_on_link current_tick (current_node, call_stack) module_
+let update_on_link current_tick current_time (current_node, call_stack) module_
     imports_offset =
   let open Lwt_syntax in
   if imports_offset >= Vector.num_elements module_.Source.it.Ast.imports then
     let current_node, call_stack =
-      end_function_call current_tick current_node call_stack
+      end_function_call current_tick current_time current_node call_stack
     in
     return_some
-    @@ call_function (Internal "init") current_tick current_node call_stack
+    @@ call_function
+         (Internal "init")
+         current_tick
+         current_time
+         current_node
+         call_stack
   else return_none
 
 (** [update_on_init current_tick current_call_context] starts and stop
     `internal` call to the {Init} step of the PVM. *)
-let update_on_init current_tick (current_node, call_stack) =
+let update_on_init current_tick current_time (current_node, call_stack) =
   let open Lwt_syntax in
   function
   | Eval.IK_Stop ->
       let current_node, call_stack =
-        end_function_call current_tick current_node call_stack
+        end_function_call current_tick current_time current_node call_stack
       in
       return_some
-      @@ call_function initial_eval_call current_tick current_node call_stack
+      @@ call_function
+           initial_eval_call
+           current_tick
+           current_time
+           current_node
+           call_stack
   | _ -> return_none
 
 (** [update_on_instr current_tick current_node call_stack] handle function calls
     during the evaluation. *)
-let update_on_instr current_tick current_node call_stack symbols = function
+let update_on_instr current_tick current_time current_node call_stack symbols =
+  function
   | Eval.Plain (Ast.Call f) ->
       let id =
         match Custom_section.FuncMap.find f.Source.it symbols with
         | None -> CallDirect f.Source.it
         | Some f -> Function f
       in
-      Lwt.return_some (call_function id current_tick current_node call_stack)
+      Lwt.return_some
+        (call_function id current_tick current_time current_node call_stack)
   | Eval.Plain (CallIndirect (f, _)) ->
       Lwt.return_some
         (call_function
            (CallRef f.Source.it)
            current_tick
+           current_time
            current_node
            call_stack)
   | _ -> Lwt.return_none
 
 (** [update_on_eval current_tick current_call_context] handle function calls and
     end during the evaluation. *)
-let update_on_eval current_tick (current_node, call_stack) symbols =
+let update_on_eval current_tick current_time (current_node, call_stack) symbols
+    =
   let open Lwt_syntax in
   function
   (* Instruction evaluation step *)
@@ -218,13 +267,20 @@ let update_on_eval current_tick (current_node, call_stack) symbols =
       let _, es = label.Eval.label_code in
       if 0l < Vector.num_elements es then
         let* e = Vector.get 0l es in
-        update_on_instr current_tick current_node call_stack symbols e.Source.it
+        update_on_instr
+          current_tick
+          current_time
+          current_node
+          call_stack
+          symbols
+          e.Source.it
       else return_none
   (* Labels `result` or `trapped` implies the end of a function call and the pop of
      the current stack frame, this can be interpreted as an end of the current
      function. *)
   | SK_Start ({frame_label_kont = Label_trapped _ | Label_result _; _}, _) ->
-      return_some @@ end_function_call current_tick current_node call_stack
+      return_some
+      @@ end_function_call current_tick current_time current_node call_stack
   (* An invocation of function that doesn't return a new stack frame implies the
      current function is an host function, and it is the end of its call. *)
   | SK_Next
@@ -234,41 +290,64 @@ let update_on_eval current_tick (current_node, call_stack) symbols =
           (Label_stack (_, _), Inv_stop {fresh_frame = None; remaining_ticks; _})
       )
     when Z.equal Z.zero remaining_ticks ->
-      return_some @@ end_function_call current_tick current_node call_stack
+      return_some
+      @@ end_function_call current_tick current_time current_node call_stack
   | _ -> return_none
 
 (** [update_call_stack current_tick current_context_call symbols state] returns
     the call context changes for any state. Returns [None] if no change
     happened. *)
-let update_call_stack current_tick (current_node, call_stack) symbols state =
+let update_call_stack current_tick current_time (current_node, call_stack)
+    symbols state =
   let open Lwt_syntax in
   match state with
   | Wasm_pvm_state.Internal_state.Decode {Decode.module_kont; _} ->
-      update_on_decode current_tick (current_node, call_stack) module_kont
+      update_on_decode
+        current_tick
+        current_time
+        (current_node, call_stack)
+        module_kont
   | Link {ast_module; imports_offset; _} ->
       update_on_link
         current_tick
+        current_time
         (current_node, call_stack)
         ast_module
         imports_offset
   | Init {init_kont; _} ->
-      update_on_init current_tick (current_node, call_stack) init_kont
+      update_on_init
+        current_tick
+        current_time
+        (current_node, call_stack)
+        init_kont
   | Eval {config = {step_kont; _}; _} ->
-      update_on_eval current_tick (current_node, call_stack) symbols step_kont
+      update_on_eval
+        current_tick
+        current_time
+        (current_node, call_stack)
+        symbols
+        step_kont
   | _ -> return_none
 
 module Make (Wasm_utils : Wasm_utils_intf.S) = struct
   (** [eval_and_profile ?write_debug ?reveal_builtins symbols tree] profiles a
     kernel up to the next result, and returns the call stack. *)
-  let eval_and_profile ?write_debug ?reveal_builtins symbols tree =
+  let eval_and_profile ?write_debug ?reveal_builtins with_time symbols tree =
     let open Lwt_syntax in
     (* The call context is built as a side effect of the evaluation. *)
     let call_stack = ref (Toplevel [], []) in
+
+    (* [current_time] is defined as a closure instead as a direct call to avoid
+       calling it at each tick and avoid a non necessary system call. *)
+    let current_time () =
+      if with_time then Some (Time.System.now () |> Ptime.to_span) else None
+    in
 
     let compute_and_snapshot pvm_state =
       let* updated_stack =
         update_call_stack
           pvm_state.Wasm_pvm_state.Internal_state.current_tick
+          current_time
           !call_stack
           symbols
           pvm_state.tick_state
@@ -329,7 +408,7 @@ module StringMap = Map.Make (String)
     is used to print the identifiers. *)
 let collapse_stack ~max_depth pp_call call_stack =
   let rec handle_node ~prefix ~depth ~max_depth accumulated_nodes = function
-    | Node (name, ticks, subnodes) ->
+    | Node (name, ticks, _time, subnodes) ->
         let prefix =
           if prefix = "" then Format.asprintf "%a" pp_call name
           else Format.asprintf "%s;%a" prefix pp_call name
@@ -361,6 +440,10 @@ let collapse_stack ~max_depth pp_call call_stack =
     [depth]. *)
 let pp_indent ppf depth = Format.fprintf ppf "%s" (String.make (depth * 2) ' ')
 
+let pp_time_opt ppf = function
+  | None -> ()
+  | Some time -> Format.fprintf ppf " (%a)" Ptime.Span.pp time
+
 let rec pp_nodes ?(max_depth = 10) depth pp_call ppf nodes =
   if depth > max_depth then ()
   else
@@ -373,16 +456,18 @@ let rec pp_nodes ?(max_depth = 10) depth pp_call ppf nodes =
       nodes
 
 and pp_stack ?max_depth depth pp_call ppf = function
-  | Node (call, ticks, sub_nodes) ->
+  | Node (call, ticks, time, sub_nodes) ->
       Format.fprintf
         ppf
-        "%a- %a : %a ticks%a"
+        "%a- %a : %a ticks%a%a"
         pp_indent
         depth
         pp_call
         call
         Z.pp_print
         ticks
+        pp_time_opt
+        time
         (pp_nodes ?max_depth depth pp_call)
         sub_nodes
   | Toplevel nodes -> pp_nodes ?max_depth depth pp_call ppf nodes
@@ -392,7 +477,7 @@ and pp_stack ?max_depth depth pp_call ppf = function
 let pp_stack ?max_depth = pp_stack ?max_depth 0
 
 let rec pp_flame_callstack_node ~prefix ~depth ~max_depth pp_call ppf = function
-  | Node (call, ticks, subnodes) ->
+  | Node (call, ticks, _time, subnodes) ->
       let prefix =
         if prefix = "" then Format.asprintf "%a" pp_call call
         else Format.asprintf "%s;%a" prefix pp_call call
