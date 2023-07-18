94ed5cf1af1d59c7204955b03edaf3be2b339ffe
taiseiKMC <hiromasa.saito@dailambda.jp>
2023-07-18 18:16:03 +0900

    Proto: Add script_repr_costs.ml
    Move micheline costs to script_repr_costs.ml
    Remove functions that turned to be unused by this change

---
+++ src/proto_alpha/lib_protocol/script_repr_costs.ml
@@ -0,0 +1,58 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2018 Dynamic Ledger Solutions, Inc. <contact@tezos.com>     *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
+module S = Saturation_repr
+
+(* model encoding/DECODING_MICHELINE *)
+let cost_DECODING_MICHELINE size1 size2 size3 =
+  let open S.Syntax in
+  let traversal_cost = S.safe_int 60 in
+  let string_per_byte_cost = S.safe_int 10 in
+  let z_per_byte_cost = S.safe_int 10 in
+  (traversal_cost * size1) + (z_per_byte_cost * size2)
+  + (string_per_byte_cost * size3)
+
+(* model encoding/DECODING_MICHELINE *)
+let cost_DECODING_MICHELINE_bytes size =
+  let open S.Syntax in
+  S.safe_int 20 * size
+
+(* model encoding/ENCODING_MICHELINE *)
+let cost_ENCODING_MICHELINE size1 size2 size3 =
+  let open S.Syntax in
+  let traversal_cost = S.safe_int 100 in
+  let string_per_byte_cost = S.safe_int 10 in
+  let z_per_byte_cost = S.safe_int 25 in
+  (traversal_cost * size1) + (z_per_byte_cost * size2)
+  + (string_per_byte_cost * size3)
+
+(* model encoding/ENCODING_MICHELINE *)
+let cost_ENCODING_MICHELINE_bytes size =
+  let open S.Syntax in
+  S.safe_int 33 * size
+
+(* model micheline/strip_locations_micheline *)
+let cost_strip_locations_micheline size =
+  Gas_limit_repr.atomic_step_cost @@ S.mul (S.safe_int size) (S.safe_int 51)
