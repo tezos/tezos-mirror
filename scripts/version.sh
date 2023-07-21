OKEAMAH/Git

scripts/version.sh: bump opam_repository tag
Bump to version where [runtime-prebuild-dependencies] contains
[sccache].

scripts/version.sh
@@ -25,7 +25,7 @@ export full_opam_repository_tag=01f6a0905760e389a567ee124839f7b2e4c3864e
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml`
 export opam_repository_url=https://gitlab.com/tezos/opam-repository
-export opam_repository_tag="${OPAM_REPOSITORY_TAG:-21b28468b394f9bdaf0383a89ebf0cc96b3b8d02}"
+export opam_repository_tag="${OPAM_REPOSITORY_TAG:-cbddfa8c468320a0ac519f4c95ae102a26e4b402}"
 export opam_repository_git="$opam_repository_url.git"
 export opam_repository="$opam_repository_git"\#"$opam_repository_tag"
 

tezos/tezos-mirror

scripts/version.sh
@@ -25,7 +25,7 @@ export full_opam_repository_tag=7109cb085f1064ae0722bdbdd0edf2d51bbe603b
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml`
 export opam_repository_url=https://gitlab.com/tezos/opam-repository
-export opam_repository_tag="${OPAM_REPOSITORY_TAG:-e3dcadd9d522d11c035df3c572dd6008beb2d5e8}"
+export opam_repository_tag="${OPAM_REPOSITORY_TAG:-ca3944346d5e89132605f0338a5874df19886fde}"
 export opam_repository_git="$opam_repository_url.git"
 export opam_repository="$opam_repository_git"\#"$opam_repository_tag"
 