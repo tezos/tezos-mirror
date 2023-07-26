16540bd9f20b94310709dbf129d55af3eee20fe8
Alain Mebsout <alain.mebsout@functori.com>
2023-07-26 19:02:42 +0200

    SCORU/Node/Store: recompute correct level associations

--- src/lib_smart_rollup_node/store_version.mli
+++ src/lib_smart_rollup_node/store_version.mli
@@ -23,7 +23,7 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-type t = V0 | V1 | V2
+type t = V0 | V1 | V2 | V3
 
 (** Pretty-printer for store versions *)
 val pp : Format.formatter -> t -> unit
