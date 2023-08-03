0e6ff9d04c3f8f23a7400220a5b683112bbced8a
Eugen Zalinescu <eugen.zalinescu@nomadic-labs.com>
2023-08-03 08:09:38 +0200

    Tezt/DAL: rename Dal_node.endpoint

--- tezt/lib_tezos/dal_node.mli
+++ tezt/lib_tezos/dal_node.mli
@@ -59,7 +59,13 @@ val listen_addr : t -> string
 (** Get the node's metrics server point pair "address:port" given as [--metrics-addr] to a dal node. *)
 val metrics_addr : t -> string
 
-(** Return the endpoint of the dal node, i.e., http://rpc_host:rpc_port. *)
+(* TODO: https://gitlab.com/tezos/tezos/-/issues/6164
+   Maybe improve naming, see doc-string below *)
+
+(** Return the endpoint of the DAL node's RPC server, i.e.,
+    http://rpc_host:rpc_port. This should not be confused with the "endpoint"
+    argument of the DAL node (and the homonymous config file field) , which
+    refers to endpoint of L1 node. *)
 val endpoint : t -> string
 
 (** Get the data-dir of an dal node. *)
