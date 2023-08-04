2b7a2f73463ecd8312ab548667dc9184a50cf4bf
lin <linoshitani@gmail.com>
2023-08-04 16:29:46 +0800

    Dal/Infra: Use custom docker image

--- devtools/cloud-infrastructure/projects/nl-dal/main/scripts/startup_script.sh
+++ devtools/cloud-infrastructure/projects/nl-dal/main/scripts/startup_script.sh
@@ -18,5 +18,5 @@ EOF
 
 docker run \
   -p 30000-30999:30000-30999 \
-  --name tezos debian \
+  --name tezos europe-west1-docker.pkg.dev/nl-dal/docker-registry/debian-tezos@sha256:latest \
   bash -c "$(cat /tmp/start.sh)"
