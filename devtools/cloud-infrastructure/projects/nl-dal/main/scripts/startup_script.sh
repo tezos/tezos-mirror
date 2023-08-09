000b78ee37e1d19530a2aa06dcbc9d9518c5b793
lin <linoshitani@gmail.com>
2023-08-09 13:52:58 +0800

    Dal/Infra: Remove unnecessary startup script

--- devtools/cloud-infrastructure/projects/nl-dal/main/scripts/startup_script.sh
+++ devtools/cloud-infrastructure/projects/nl-dal/main/scripts/startup_script.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# Force re-apply
-
-# Parameters
-
-# Docker deployment script
-##########################
-
-cat > /tmp/start.sh <<EOF
-apt update
-apt install libgmp-dev curl libev-dev libhidapi-dev python3 openssh-server -y
-mkdir -p /root/.ssh
-mkdir -p /run/sshd
-
-/usr/sbin/sshd -D -p 30000 -e
-EOF
-
 docker run \
   -p 30000-30999:30000-30999 \
   --name tezos europe-west1-docker.pkg.dev/nl-dal/docker-registry/debian-tezos@sha256:latest \
