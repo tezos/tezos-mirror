c035d3f15c5abe795cda09a87a0c295fc4ac50d0
Gabriel Moise <gabriel.moise@trili.tech>
2023-08-10 18:18:00 +0300

    Testnet_experiments: Fixup_2

--- devtools/testnet_experiment_tools/testnet_experiment_tools.ml
+++ devtools/testnet_experiment_tools/testnet_experiment_tools.ml
@@ -31,31 +31,32 @@ let ensure_dir_exists dir =
     (fun () ->
       let* () = Lwt_utils_unix.create_dir ~perm:0o744 dir in
       Lwt.return_unit)
-    (function
-      | Failure s ->
-          if String.equal s "Not a directory" then
-            Test.fail "%s exists but is not a directory." dir
-          else Test.fail "Cannot create directory %s" dir
-      | _ -> Test.fail "Cannot create directory %s" dir)
+    (fun exn ->
+      Test.fail
+        "Failed to create directory '%s': %s"
+        dir
+        (Printexc.to_string exn))
 
 let default_number_of_bakers = 10
+
+let bakers = "BAKERS"
 
 let baker_alias n = Printf.sprintf "baker_%d" n
 
 let number_of_bakers =
-  Sys.getenv_opt "BAKERS" |> Option.map int_of_string
+  Sys.getenv_opt bakers |> Option.map int_of_string
   |> Option.value ~default:default_number_of_bakers
 
 let generate_baker_accounts n client =
-  let rec go i =
+  let rec generate_baker_account i =
     if i = 0 then Lwt.return_unit
     else
       let* _alias = Client.gen_keys ~alias:(baker_alias i) client in
       let* () = Lwt_io.printf "." in
-      go (i - 1)
+      generate_baker_account (i - 1)
   in
   let* () = Lwt_io.printf "Generating accounts" in
-  let* () = go n in
+  let* () = generate_baker_account n in
   Lwt_io.printf "\n\n"
 
 (* These tests can be run locally to generate the data needed to run a
