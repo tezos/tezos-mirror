OKEAMAH/Git

WASM: Add `reveal_dal_page` to the Rust SDK

src/kernel_sdk/core/src/smart_rollup_core.rs
@@ -313,6 +313,22 @@ pub unsafe trait SmartRollupCore {
     /// - `destination_addr` must point to a mutable slice of bytes with
     ///   `capacity >= max_bytes`
     unsafe fn reveal_metadata(&self, destination_addr: *mut u8, max_bytes: usize) -> i32;
+
+    /// Loads the result of an arbitrary reveal request to memory.
+    /// If the preimage is larger than `max_bytes`, its contents is trimmed.
+    ///
+    /// # Safety
+    /// - `payload_addr` must be a ptr to a slice containing a hash.
+    /// - `payload_len` must be the length of the slice.
+    /// - `destination_addr `must point to a mutable slice of bytes with
+    ///   `capacity >= max_size`.
+    unsafe fn reveal(
+        &self,
+        payload_addr: *const u8,
+        payload_len: usize,
+        destination_addr: *mut u8,
+        destination_len: usize,
+    ) -> i32;
 }
 
 /// Information about message level & id.

src/kernel_sdk/host/src/runtime.rs
@@ -186,6 +186,15 @@ pub trait Runtime {
         destination: &mut [u8],
     ) -> Result<usize, RuntimeError>;
 
+    /// Reveal a DAL page.
+    fn reveal_dal_page(
+        &self,
+        published_level: i32,
+        slot_index: u8,
+        message_index: i16,
+        destination: &mut [u8],
+    ) -> Result<usize, RuntimeError>;
+
     /// Return the size of value stored at `path`
     fn store_value_size(&self, path: &impl Path) -> Result<usize, RuntimeError>;
 
@@ -578,6 +587,38 @@ where
                                              SDK developers at https://gitlab.com/tezos/tezos");
 
         RollupMetadata::from(destination)
+    }
+
+    fn reveal_dal_page(
+        &self,
+        published_level: i32,
+        slot_index: u8,
+        message_index: i16,
+        destination: &mut [u8],
+    ) -> Result<usize, RuntimeError> {
+        // This will match the encoding declared for a DAL page slot in the Tezos protocol.
+        let payload: &[u8] = &[
+            &[2u8], // tag
+            published_level.to_be_bytes().as_ref(),
+            &[slot_index],
+            message_index.to_be_bytes().as_ref(),
+        ]
+        .concat();
+
+        let res = unsafe {
+            SmartRollupCore::reveal(
+                self,
+                payload.as_ptr(),
+                payload.len(),
+                destination.as_mut_ptr(),
+                destination.len(),
+            )
+        };
+
+        match Error::wrap(res) {
+            Ok(size) => Ok(size),
+            Err(e) => Err(RuntimeError::HostErr(e)),
+        }
     }
 
     fn store_value_size(&self, path: &impl Path) -> Result<usize, RuntimeError> {

tezos/tezos-mirror

src/kernel_sdk/core/src/smart_rollup_core.rs
@@ -134,6 +134,7 @@ extern "C" {
     /// `payload_len` is the same as the one used by the Tezos protocol.
     ///
     /// Returns the size of the data loaded in memory.
+    #[cfg(feature = "proto-alpha")]
     pub fn reveal(
         payload_addr: *const u8,
         payload_len: usize,
@@ -313,6 +314,23 @@ pub unsafe trait SmartRollupCore {
     /// - `destination_addr` must point to a mutable slice of bytes with
     ///   `capacity >= max_bytes`
     unsafe fn reveal_metadata(&self, destination_addr: *mut u8, max_bytes: usize) -> i32;
+
+    /// Loads the result of a raw reveal request to memory.
+    /// If the preimage is larger than `max_bytes`, its contents is trimmed.
+    ///
+    /// # Safety
+    /// - `payload_addr` must be a ptr to a slice containing a hash.
+    /// - `payload_len` must be the length of the slice.
+    /// - `destination_addr `must point to a mutable slice of bytes with
+    ///   `capacity >= max_bytes`.
+    #[cfg(feature = "proto-alpha")]
+    unsafe fn reveal(
+        &self,
+        payload_addr: *const u8,
+        payload_len: usize,
+        destination_addr: *mut u8,
+        max_bytes: usize,
+    ) -> i32;
 }
 
 /// Information about message level & id.

src/kernel_sdk/host/src/runtime.rs
@@ -186,6 +186,16 @@ pub trait Runtime {
         destination: &mut [u8],
     ) -> Result<usize, RuntimeError>;
 
+    /// Reveal a DAL page.
+    #[cfg(all(feature = "alloc", feature = "proto-alpha"))]
+    fn reveal_dal_page(
+        &self,
+        published_level: i32,
+        slot_index: u8,
+        page_index: i16,
+        destination: &mut [u8],
+    ) -> Result<usize, RuntimeError>;
+
     /// Return the size of value stored at `path`
     fn store_value_size(&self, path: &impl Path) -> Result<usize, RuntimeError>;
 
@@ -578,6 +588,39 @@ where
                                              SDK developers at https://gitlab.com/tezos/tezos");
 
         RollupMetadata::from(destination)
+    }
+
+    #[cfg(all(feature = "alloc", feature = "proto-alpha"))]
+    fn reveal_dal_page(
+        &self,
+        published_level: i32,
+        slot_index: u8,
+        page_index: i16,
+        destination: &mut [u8],
+    ) -> Result<usize, RuntimeError> {
+        // This will match the encoding declared for a DAL page in the Tezos protocol.
+        let payload: &[u8] = &[
+            &[2u8], // tag
+            published_level.to_be_bytes().as_ref(),
+            &[slot_index],
+            page_index.to_be_bytes().as_ref(),
+        ]
+        .concat();
+
+        let res = unsafe {
+            SmartRollupCore::reveal(
+                self,
+                payload.as_ptr(),
+                payload.len(),
+                destination.as_mut_ptr(),
+                destination.len(),
+            )
+        };
+
+        match Error::wrap(res) {
+            Ok(size) => Ok(size),
+            Err(e) => Err(RuntimeError::HostErr(e)),
+        }
     }
 
     fn store_value_size(&self, path: &impl Path) -> Result<usize, RuntimeError> {