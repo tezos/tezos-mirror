OKEAMAH/Git

EVM: Bench: remove hardcoded paths

src/kernel_evm/benchmarks/scripts/run_benchmarks.js
@@ -5,7 +5,7 @@
 // This script runs the benchmarks for the EVM kernel and writes the result to benchmark_result.csv
 
 // Before running this script, run the following commands to build the debugger and the benchmark kernel
-// $ make 
+// $ make
 // $ make -C src/kernel_evm/
 
 // Then run this script using the following command
@@ -18,23 +18,24 @@ var fs = require('fs');
 var readline = require('readline');
 const { spawn } = require('child_process');
 const { execSync } = require('child_process');
+const external = require("./lib/external")
+const path = require('node:path')
 
-// TODO: Remove Hardcoded Paths
-// issue: https://gitlab.com/tezos/tezos/-/issues/6175
-const RUN_DEBUGGER_COMMAND = './octez-smart-rollup-wasm-debugger';
-const RUN_INSTALLER_COMMAND = 'src/kernel_sdk/target/release/smart-rollup-installer'
+const RUN_DEBUGGER_COMMAND = external.bin('./octez-smart-rollup-wasm-debugger');
+const RUN_INSTALLER_COMMAND = external.bin('src/kernel_sdk/target/release/smart-rollup-installer')
 const EVM_INSTALLER_KERNEL_PATH = 'evm_installer.wasm';
 const PREIMAGE_DIR = 'preimages';
-const SETUP_FILE_PATH = 'src/kernel_evm/config/benchmarking.yaml';
-const EVM_KERNEL_PATH = 'src/kernel_evm/target/wasm32-unknown-unknown/release/evm_kernel.wasm';
+const SETUP_FILE_PATH = external.ressource('src/kernel_evm/config/benchmarking.yaml')
+const EVM_KERNEL_PATH = external.ressource('src/kernel_evm/target/wasm32-unknown-unknown/release/evm_kernel.wasm')
+
 
 function sumArray(arr) {
     return arr.reduce((acc, curr) => acc + curr, 0);
 }
-  
+
 function run_profiler(path) {
 
-    profiler_result = new Promise ((resolve, _) => {
+    profiler_result = new Promise((resolve, _) => {
 
         var gas_used = [];
 
@@ -54,19 +55,19 @@ function run_profiler(path) {
 
         childProcess.stdout.on('data', (data) => {
             const output = data.toString();
-                const profiler_output_path_regex = /Profiling result can be found in (.+)/;
-                const profiler_output_path_match = output.match(profiler_output_path_regex);
-                const profiler_output_path_result = profiler_output_path_match 
-                    ? profiler_output_path_match[1] 
-                    : null;
-                if (profiler_output_path_result !== null) {
-                    profiler_output_path = profiler_output_path_result;
-                }
-                const gas_used_regex = /\bgas_used:\s*(\d+)/g;
-                var match;
-                while ((match = gas_used_regex.exec(output))) {
-                    gas_used.push(match[1]);
-                }
+            const profiler_output_path_regex = /Profiling result can be found in (.+)/;
+            const profiler_output_path_match = output.match(profiler_output_path_regex);
+            const profiler_output_path_result = profiler_output_path_match
+                ? profiler_output_path_match[1]
+                : null;
+            if (profiler_output_path_result !== null) {
+                profiler_output_path = profiler_output_path_result;
+            }
+            const gas_used_regex = /\bgas_used:\s*(\d+)/g;
+            var match;
+            while ((match = gas_used_regex.exec(output))) {
+                gas_used.push(match[1]);
+            }
         });
         childProcess.on('close', _ => {
             if (profiler_output_path == "") {
@@ -90,9 +91,9 @@ async function get_ticks(path, function_call_keyword) {
     const rl = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity
-      });
+    });
 
-      for await (const l of rl) {
+    for await (const l of rl) {
         if (l !== "") {
             tokens = l.split(" ");
             calls = tokens[0];
@@ -108,7 +109,7 @@ async function get_ticks(path, function_call_keyword) {
                 previous_row_is_given_function_call = false;
             }
         }
-      }
+    }
 
     return ticks_count_for_transactions;
 }
@@ -134,7 +135,7 @@ async function analyze_profiler_output(path) {
     };
 }
 
-// Run given benchmark 
+// Run given benchmark
 async function run_benchmark(path) {
     run_profiler_result = await run_profiler(path);
     profiler_output_path = run_profiler_result[0];
@@ -155,7 +156,8 @@ function build_evm_installer_kernel_for_benchmark() {
 
 function build_benchmark_scenario(benchmark_script) {
     try {
-        execSync(`node ${benchmark_script} > transactions.json`);
+        let bench_path = path.format({ dir: __dirname, base: benchmark_script })
+        execSync(`node ${bench_path} > transactions.json`);
     } catch (error) {
         console.log(`Error running script ${benchmark_script}. Please fixed the error in the script before running this benchmark script`)
         console.error(error);
@@ -166,7 +168,7 @@ function log_benchmark_result(benchmark_name, run_benchmark_result) {
     rows = [];
     gas_costs = run_benchmark_result.gas_costs;
     kernel_run_ticks = run_benchmark_result.kernel_run_ticks;
-    run_transaction_ticks=run_benchmark_result.run_transaction_ticks;
+    run_transaction_ticks = run_benchmark_result.run_transaction_ticks;
     signature_verification_ticks = run_benchmark_result.signature_verification_ticks;
     store_transaction_object_ticks = run_benchmark_result.store_transaction_object_ticks;
     interpreter_init_ticks = run_benchmark_result.interpreter_init_ticks;
@@ -186,10 +188,9 @@ function log_benchmark_result(benchmark_name, run_benchmark_result) {
     return rows;
 }
 
-// Run the benchmark suite and write the result to benchmark_result.csv
 async function run_all_benchmarks(benchmark_scripts) {
     console.log(`Running benchmarks on: [${benchmark_scripts.join('\n  ')}]`);
-    var fields = ["benchmark_name", "gas_cost", "run_transaction_ticks", "signature_verification_ticks", "store_transaction_object_ticks" ,"interpreter_init_ticks", "interpreter_decode_ticks", "fetch_blueprint_ticks", "kernel_run_ticks", "unaccounted_ticks"];
+    var fields = ["benchmark_name", "gas_cost", "run_transaction_ticks", "signature_verification_ticks", "store_transaction_object_ticks", "interpreter_init_ticks", "interpreter_decode_ticks", "fetch_blueprint_ticks", "kernel_run_ticks", "unaccounted_ticks"];
     fs.writeFileSync('benchmark_result.csv', fields.join(",") + "\n");
     for (var i = 0; i < benchmark_scripts.length; i++) {
         var benchmark_script = benchmark_scripts[i];
@@ -198,7 +199,7 @@ async function run_all_benchmarks(benchmark_scripts) {
         console.log(`Benchmarking ${benchmark_script}`);
         build_benchmark_scenario(benchmark_script);
         run_benchmark_result = await run_benchmark("transactions.json");
-        benchmark_log  = log_benchmark_result(benchmark_name, run_benchmark_result);
+        benchmark_log = log_benchmark_result(benchmark_name, run_benchmark_result);
         fs.appendFileSync('benchmark_result.csv', benchmark_log.map(row => row.join(",")).join("\n") + "\n");
     }
     console.log("Benchmarking complete");
@@ -206,17 +207,15 @@ async function run_all_benchmarks(benchmark_scripts) {
 }
 
 benchmark_scripts = [
-    // TODO: Remove Hardcoded Paths
-    // issue: https://gitlab.com/tezos/tezos/-/issues/6175
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_storage_1.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_storage_2.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_transfers_1.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_transfers_2.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_transfers_3.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_keccak.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_verifySignature.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_erc20tok.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_loop.js",
+    "benchmarks/bench_storage_1.js",
+    "benchmarks/bench_storage_2.js",
+    "benchmarks/bench_transfers_1.js",
+    "benchmarks/bench_transfers_2.js",
+    "benchmarks/bench_transfers_3.js",
+    "benchmarks/bench_keccak.js",
+    "benchmarks/bench_verifySignature.js",
+    "benchmarks/bench_erc20tok.js",
+    "benchmarks/bench_loop.js",
 ]
 build_evm_installer_kernel_for_benchmark();
 run_all_benchmarks(benchmark_scripts);

tezos/tezos-mirror

src/kernel_evm/benchmarks/scripts/run_benchmarks.js
@@ -5,7 +5,7 @@
 // This script runs the benchmarks for the EVM kernel and writes the result to benchmark_result.csv
 
 // Before running this script, run the following commands to build the debugger and the benchmark kernel
-// $ make 
+// $ make
 // $ make -C src/kernel_evm/
 
 // Then run this script using the following command
@@ -18,23 +18,24 @@ var fs = require('fs');
 var readline = require('readline');
 const { spawn } = require('child_process');
 const { execSync } = require('child_process');
+const external = require("./lib/external")
+const path = require('node:path')
 
-// TODO: Remove Hardcoded Paths
-// issue: https://gitlab.com/tezos/tezos/-/issues/6175
-const RUN_DEBUGGER_COMMAND = './octez-smart-rollup-wasm-debugger';
-const RUN_INSTALLER_COMMAND = 'src/kernel_sdk/target/release/smart-rollup-installer'
+const RUN_DEBUGGER_COMMAND = external.bin('./octez-smart-rollup-wasm-debugger');
+const RUN_INSTALLER_COMMAND = external.bin('src/kernel_sdk/target/release/smart-rollup-installer')
 const EVM_INSTALLER_KERNEL_PATH = 'evm_installer.wasm';
 const PREIMAGE_DIR = 'preimages';
-const SETUP_FILE_PATH = 'src/kernel_evm/config/benchmarking.yaml';
-const EVM_KERNEL_PATH = 'src/kernel_evm/target/wasm32-unknown-unknown/release/evm_kernel.wasm';
+const SETUP_FILE_PATH = external.resource('src/kernel_evm/config/benchmarking.yaml')
+const EVM_KERNEL_PATH = external.resource('src/kernel_evm/target/wasm32-unknown-unknown/release/evm_kernel.wasm')
+
 
 function sumArray(arr) {
     return arr.reduce((acc, curr) => acc + curr, 0);
 }
-  
+
 function run_profiler(path) {
 
-    profiler_result = new Promise ((resolve, _) => {
+    profiler_result = new Promise((resolve, _) => {
 
         var gas_used = [];
 
@@ -54,19 +55,19 @@ function run_profiler(path) {
 
         childProcess.stdout.on('data', (data) => {
             const output = data.toString();
-                const profiler_output_path_regex = /Profiling result can be found in (.+)/;
-                const profiler_output_path_match = output.match(profiler_output_path_regex);
-                const profiler_output_path_result = profiler_output_path_match 
-                    ? profiler_output_path_match[1] 
-                    : null;
-                if (profiler_output_path_result !== null) {
-                    profiler_output_path = profiler_output_path_result;
-                }
-                const gas_used_regex = /\bgas_used:\s*(\d+)/g;
-                var match;
-                while ((match = gas_used_regex.exec(output))) {
-                    gas_used.push(match[1]);
-                }
+            const profiler_output_path_regex = /Profiling result can be found in (.+)/;
+            const profiler_output_path_match = output.match(profiler_output_path_regex);
+            const profiler_output_path_result = profiler_output_path_match
+                ? profiler_output_path_match[1]
+                : null;
+            if (profiler_output_path_result !== null) {
+                profiler_output_path = profiler_output_path_result;
+            }
+            const gas_used_regex = /\bgas_used:\s*(\d+)/g;
+            var match;
+            while ((match = gas_used_regex.exec(output))) {
+                gas_used.push(match[1]);
+            }
         });
         childProcess.on('close', _ => {
             if (profiler_output_path == "") {
@@ -90,9 +91,9 @@ async function get_ticks(path, function_call_keyword) {
     const rl = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity
-      });
+    });
 
-      for await (const l of rl) {
+    for await (const l of rl) {
         if (l !== "") {
             tokens = l.split(" ");
             calls = tokens[0];
@@ -108,7 +109,7 @@ async function get_ticks(path, function_call_keyword) {
                 previous_row_is_given_function_call = false;
             }
         }
-      }
+    }
 
     return ticks_count_for_transactions;
 }
@@ -134,7 +135,7 @@ async function analyze_profiler_output(path) {
     };
 }
 
-// Run given benchmark 
+// Run given benchmark
 async function run_benchmark(path) {
     run_profiler_result = await run_profiler(path);
     profiler_output_path = run_profiler_result[0];
@@ -155,7 +156,8 @@ function build_evm_installer_kernel_for_benchmark() {
 
 function build_benchmark_scenario(benchmark_script) {
     try {
-        execSync(`node ${benchmark_script} > transactions.json`);
+        let bench_path = path.format({ dir: __dirname, base: benchmark_script })
+        execSync(`node ${bench_path} > transactions.json`);
     } catch (error) {
         console.log(`Error running script ${benchmark_script}. Please fixed the error in the script before running this benchmark script`)
         console.error(error);
@@ -166,7 +168,7 @@ function log_benchmark_result(benchmark_name, run_benchmark_result) {
     rows = [];
     gas_costs = run_benchmark_result.gas_costs;
     kernel_run_ticks = run_benchmark_result.kernel_run_ticks;
-    run_transaction_ticks=run_benchmark_result.run_transaction_ticks;
+    run_transaction_ticks = run_benchmark_result.run_transaction_ticks;
     signature_verification_ticks = run_benchmark_result.signature_verification_ticks;
     store_transaction_object_ticks = run_benchmark_result.store_transaction_object_ticks;
     interpreter_init_ticks = run_benchmark_result.interpreter_init_ticks;
@@ -186,10 +188,9 @@ function log_benchmark_result(benchmark_name, run_benchmark_result) {
     return rows;
 }
 
-// Run the benchmark suite and write the result to benchmark_result.csv
 async function run_all_benchmarks(benchmark_scripts) {
     console.log(`Running benchmarks on: [${benchmark_scripts.join('\n  ')}]`);
-    var fields = ["benchmark_name", "gas_cost", "run_transaction_ticks", "signature_verification_ticks", "store_transaction_object_ticks" ,"interpreter_init_ticks", "interpreter_decode_ticks", "fetch_blueprint_ticks", "kernel_run_ticks", "unaccounted_ticks"];
+    var fields = ["benchmark_name", "gas_cost", "run_transaction_ticks", "signature_verification_ticks", "store_transaction_object_ticks", "interpreter_init_ticks", "interpreter_decode_ticks", "fetch_blueprint_ticks", "kernel_run_ticks", "unaccounted_ticks"];
     fs.writeFileSync('benchmark_result.csv', fields.join(",") + "\n");
     for (var i = 0; i < benchmark_scripts.length; i++) {
         var benchmark_script = benchmark_scripts[i];
@@ -198,7 +199,7 @@ async function run_all_benchmarks(benchmark_scripts) {
         console.log(`Benchmarking ${benchmark_script}`);
         build_benchmark_scenario(benchmark_script);
         run_benchmark_result = await run_benchmark("transactions.json");
-        benchmark_log  = log_benchmark_result(benchmark_name, run_benchmark_result);
+        benchmark_log = log_benchmark_result(benchmark_name, run_benchmark_result);
         fs.appendFileSync('benchmark_result.csv', benchmark_log.map(row => row.join(",")).join("\n") + "\n");
     }
     console.log("Benchmarking complete");
@@ -206,17 +207,15 @@ async function run_all_benchmarks(benchmark_scripts) {
 }
 
 benchmark_scripts = [
-    // TODO: Remove Hardcoded Paths
-    // issue: https://gitlab.com/tezos/tezos/-/issues/6175
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_storage_1.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_storage_2.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_transfers_1.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_transfers_2.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_transfers_3.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_keccak.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_verifySignature.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_erc20tok.js",
-    "src/kernel_evm/benchmarks/scripts/benchmarks/bench_loop.js",
+    "benchmarks/bench_storage_1.js",
+    "benchmarks/bench_storage_2.js",
+    "benchmarks/bench_transfers_1.js",
+    "benchmarks/bench_transfers_2.js",
+    "benchmarks/bench_transfers_3.js",
+    "benchmarks/bench_keccak.js",
+    "benchmarks/bench_verifySignature.js",
+    "benchmarks/bench_erc20tok.js",
+    "benchmarks/bench_loop.js",
 ]
 build_evm_installer_kernel_for_benchmark();
 run_all_benchmarks(benchmark_scripts);