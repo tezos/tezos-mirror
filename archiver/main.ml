136226afe74c5fb6c4bbaef4da96b3ba837378fa
Pierre Boutillier <pierre.boutillier@nomadic-labs.com>
2023-08-28 16:03:10 +0200

    Teztale: An archiver for Proxford

--- archiver/main.ml
+++ archiver/main.ml
@@ -247,5 +247,6 @@ module M014 = PtKathma_machine.M
 module M015 = PtLimaPt_machine.M
 module M016 = PtMumbai_machine.M
 module M017 = PtNairob_machine.M
+module M018 = Proxford_machine.M
 
 let () = Client_main_run.run (module Client_config) ~select_commands
