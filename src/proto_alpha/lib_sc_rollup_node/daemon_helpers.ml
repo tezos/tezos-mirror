1456b4c8b738a7f3476eab8d7326ff8b0f55ef2a
Sylvain Ribstein <sylvain.ribstein@gmail.com>
2023-09-01 17:24:29 +0200

    rollup/node: fails with operating key is not in whitelist anymore

--- src/proto_alpha/lib_sc_rollup_node/daemon_helpers.ml
+++ src/proto_alpha/lib_sc_rollup_node/daemon_helpers.ml
@@ -285,6 +285,18 @@ let process_included_l1_operation (type kind) (node_ctxt : Node_context.rw)
             Signature.Public_key_hash.(operating_pkh = staker)
             Sc_rollup_node_errors.Exit_bond_recovered_bailout_mode
       | _ -> return_unit)
+  | ( Sc_rollup_execute_outbox_message _,
+      Sc_rollup_execute_outbox_message_result
+        {whitelist_update = Some whitelist_update; _} ) -> (
+      match whitelist_update with
+      | Public -> return_unit
+      | Private whitelist_update ->
+          let*? () =
+            Node_context.check_op_in_whitelist_or_bailout_mode
+              node_ctxt
+              whitelist_update
+          in
+          return_unit)
   | _, _ ->
       (* Other manager operations *)
       return_unit
