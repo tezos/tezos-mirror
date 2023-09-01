0930f5b7eca20c1b9bad599946859c09c7fbef77
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2023-09-01 09:51:21 +0200

    EVM: remove legacy signer

--- manifest/main.ml
+++ manifest/main.ml
@@ -8071,21 +8071,6 @@ let _evm_proxy =
       ]
     ~bisect_ppx:Yes
 
-let _octez_evm_signer_exe =
-  private_exe
-    "octez_evm_upgrade_signer"
-    ~path:"src/bin_evm_proxy/upgrade_signer"
-    ~synopsis:"EVM upgrade message"
-    ~opam:"octez-evm-upgrade-signer"
-    ~deps:
-      [
-        octez_base |> open_ ~m:"TzPervasives";
-        octez_base_unix;
-        octez_clic;
-        evm_proxy_lib_prod;
-        evm_proxy_lib_dev;
-      ]
-
 let octez_scoru_wasm_regressions =
   private_lib
     "tezos_scoru_wasm_regressions"
