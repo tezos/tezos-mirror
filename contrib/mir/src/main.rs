0fdeb2090494c66ef41e3a7f868403f38318795b
Sandeep.C.R <sandeep.chandrika@serokell.io>
2023-09-07 13:30:23 +0530

    MIR: Add two tests for the Michelson parser

--- contrib/mir/src/main.rs
+++ contrib/mir/src/main.rs
@@ -22,5 +22,40 @@
 /* DEALINGS IN THE SOFTWARE.                                                  */
 /*                                                                            */
 /******************************************************************************/
+mod parser;
+mod syntax;
 
 fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser;
+
+    #[test]
+    fn parser_test_expect_success() {
+        let src = "{ INT ; PUSH int 0 ; DUP 2 ; GT ;
+       IF { DIP { PUSH int -1 ; ADD } ;
+            PUSH int 1 ;
+            DUP 3 ;
+            GT ;
+            LOOP { SWAP ; DUP 2 ; ADD ; DIP 2 { PUSH int -1 ; ADD } ; DUP 3 ; GT } ;
+            DIP { DROP 2 } }
+          { DIP { DROP } } }";
+
+        assert!(parser::parse(&src));
+    }
+
+    #[test]
+    fn parser_test_expect_fail() {
+        let src = "{ INT ; PUSH int 0 ; DUP 2 ; GT ;
+       IF { DIP { PUSH int -1 ; ADD } ;
+            PUSH int 1 ;
+            DUP 3
+            GT ;
+            LOOP { SWAP ; DUP 2 ; ADD ; DIP 2 { PUSH int -1 ; ADD } ; DUP 3 ; GT } ;
+            DIP { DROP 2 } }
+          { DIP { DROP } } }";
+
+        assert!(!parser::parse(&src));
+    }
+}
