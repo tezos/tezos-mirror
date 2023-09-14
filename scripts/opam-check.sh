530c048c6ebf327dc989b424522bf46516d5cbab
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-09-14 09:54:48 +0200

    Scripts: shellcheck [scripts/opam-check.sh]

--- scripts/opam-check.sh
+++ scripts/opam-check.sh
@@ -2,6 +2,7 @@
 
 script_dir="$(cd "$(dirname "$0")" && echo "$(pwd -P)/")"
 
+# shellcheck source=scripts/version.sh
 . "$script_dir"/version.sh
 
 echo "## Checking installed dependencies..."
@@ -13,11 +14,15 @@ if ! opam install opam/virtual/octez-deps.opam --deps-only --with-test --show-ac
     echo
     opam install opam/virtual/octez-deps.opam --deps-only --with-test --show-actions
     echo
+    # We really want literal backticks here, not command substitution.
+    # shellcheck disable=SC2016
     echo 'Failed! Please read the doc in `./scripts/update_opam_repo.sh` and act accordingly.'
     echo
     exit 1
 fi
 
+# We really want literal backticks here, not command substitution.
+# shellcheck disable=SC2016
 echo '## Running `./scripts/update_opam_repo.sh`'
 echo
 ./scripts/update_opam_repo.sh || exit 1
@@ -28,6 +33,8 @@ if [ -n "$(cat opam_repo.patch)" ] ; then
     cat opam_repo.patch
     echo "##################################################"
 
+    # We really want literal backticks here, not command substitution.
+    # shellcheck disable=SC2016
     echo 'Failed! The variables `opam_repository_tag` and `full_opam_repository_tag` are not synchronized. Please read the doc in `./scripts/update_opam_repo.sh` and act accordingly.'
     echo
     exit 1
