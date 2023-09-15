3b41b6039c7a423008b5b68d21c972e8a041fe52
Ole Krüger <ole.kruger@trili.tech>
2023-09-15 17:39:04 +0100

    Nix: Add RISC-V target for Rust and Clang

--- nix/sources.nix
+++ nix/sources.nix
@@ -13,13 +13,18 @@ let
     }
   );
 
-  pkgs =
-    import
-    (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/cd07839e2e61f8b7c467f20a896c3f9e63a04918.tar.gz";
-      sha256 = "1xr250f9z72v560pkvi25iwclnysjn8h9mw8cdnjl4izq0milmzi";
-    })
-    {overlays = [opam-nix-integration.overlay rust-overlay];};
+  pkgsSrc = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/cd07839e2e61f8b7c467f20a896c3f9e63a04918.tar.gz";
+    sha256 = "1xr250f9z72v560pkvi25iwclnysjn8h9mw8cdnjl4izq0milmzi";
+  };
+
+  pkgs = import pkgsSrc {
+    overlays = [opam-nix-integration.overlay rust-overlay];
+  };
+
+  riscv64Pkgs = import pkgsSrc {
+    crossSystem.config = "riscv64-unknown-linux-gnu";
+  };
 in {
-  inherit opam-nix-integration rust-overlay pkgs;
+  inherit opam-nix-integration rust-overlay pkgs riscv64Pkgs;
 }
