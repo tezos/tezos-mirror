6c2a806612386b63bc985c009125e4239505309e
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-09-15 15:19:15 +0200

    Proto/Stake_repr: move staker to a separate module

--- src/proto_alpha/lib_protocol/stake_repr.mli
+++ src/proto_alpha/lib_protocol/stake_repr.mli
@@ -23,22 +23,7 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** Adding and removing stake can be done from/toward a delegate, one
-   of its staker, or both the delegate and all its stakers at
-   once. We need to distinguish these cases to enforce the staking
-   over baking limit. *)
-type staker =
-  | Single of Contract_repr.t * Signature.public_key_hash
-    (* A single staker, either the delegate itself or one of its
-       staker. *)
-  | Shared of Signature.public_key_hash
-(* The delegate and all its stakers simultaneously. *)
-
-val staker_encoding : staker Data_encoding.t
-
-val compare_staker : staker -> staker -> int
-
-val staker_delegate : staker -> Signature.public_key_hash
+include module type of Staker_repr
 
 (** Stake of a delegate. *)
 type t = private {frozen : Tez_repr.t; delegated : Tez_repr.t}
