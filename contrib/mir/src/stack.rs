b7d698c00404776ff1f163f96a898d45c4174a1b
Nikolay Yakimov <root@livid.pp.ru>
2023-09-21 15:54:05 +0300

    MIR: Move typechecking helpers to typechecker module

--- contrib/mir/src/stack.rs
+++ contrib/mir/src/stack.rs
@@ -8,41 +8,6 @@
 use std::collections::VecDeque;
 
 use crate::ast::*;
-use crate::gas;
-use crate::gas::Gas;
-use crate::typechecker::TcError;
 
 pub type TypeStack = VecDeque<Type>;
-
-/// Ensures type stack is at least of the required length, otherwise returns
-/// `Err(StackTooShort)`.
-pub fn ensure_stack_len(stack: &TypeStack, l: usize) -> Result<(), TcError> {
-    if stack.len() >= l {
-        Ok(())
-    } else {
-        Err(TcError::StackTooShort)
-    }
-}
-
-/// Ensures two type stacks compare equal, otherwise returns
-/// `Err(StacksNotEqual)`. If runs out of gas, returns `Err(OutOfGas)` instead.
-pub fn ensure_stacks_eq(gas: &mut Gas, stack1: &[Type], stack2: &[Type]) -> Result<(), TcError> {
-    if stack1.len() != stack2.len() {
-        return Err(TcError::StacksNotEqual);
-    }
-    for (ty1, ty2) in stack1.iter().zip(stack2.iter()) {
-        ensure_ty_eq(gas, ty1, ty2)?;
-    }
-    Ok(())
-}
-
-fn ensure_ty_eq(gas: &mut Gas, ty1: &Type, ty2: &Type) -> Result<(), TcError> {
-    gas.consume(gas::tc_cost::ty_eq(ty1.size_for_gas(), ty2.size_for_gas())?)?;
-    if ty1 != ty2 {
-        Err(TcError::StacksNotEqual)
-    } else {
-        Ok(())
-    }
-}
-
 pub type IStack = VecDeque<Value>;
--- contrib/mir/src/typechecker.rs
+++ contrib/mir/src/typechecker.rs
@@ -147,6 +147,37 @@ fn typecheck_value(gas: &mut Gas, t: &Type, v: &Value) -> Result<(), TcError> {
     }
 }
 
+/// Ensures type stack is at least of the required length, otherwise returns
+/// `Err(StackTooShort)`.
+fn ensure_stack_len(stack: &TypeStack, l: usize) -> Result<(), TcError> {
+    if stack.len() >= l {
+        Ok(())
+    } else {
+        Err(TcError::StackTooShort)
+    }
+}
+
+/// Ensures two type stacks compare equal, otherwise returns
+/// `Err(StacksNotEqual)`. If runs out of gas, returns `Err(OutOfGas)` instead.
+fn ensure_stacks_eq(gas: &mut Gas, stack1: &[Type], stack2: &[Type]) -> Result<(), TcError> {
+    if stack1.len() != stack2.len() {
+        return Err(TcError::StacksNotEqual);
+    }
+    for (ty1, ty2) in stack1.iter().zip(stack2.iter()) {
+        ensure_ty_eq(gas, ty1, ty2)?;
+    }
+    Ok(())
+}
+
+fn ensure_ty_eq(gas: &mut Gas, ty1: &Type, ty2: &Type) -> Result<(), TcError> {
+    gas.consume(gas::tc_cost::ty_eq(ty1.size_for_gas(), ty2.size_for_gas())?)?;
+    if ty1 != ty2 {
+        Err(TcError::StacksNotEqual)
+    } else {
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod typecheck_tests {
     use std::collections::VecDeque;
