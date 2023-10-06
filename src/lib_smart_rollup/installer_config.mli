OKEAMAH/Git

Lib_smart_rollup: parse set instruction of installer configuration
Co-Authored-By: Pierrick Couderc <pierrick.couderc@nomadic-labs.com>

src/lib_smart_rollup/installer_config.mli
@@ -1,0 +1,41 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2023 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(**
+   Handling of YAML configuration file used by the kernel installer.
+
+   A configuration has the form:
+   ```
+   instructions:
+     - set:
+         value: <hexadecimal value>
+         to: <string>
+     ...
+   ```
+*)
+
+type error +=
+  | Installer_config_yaml_error of string
+  | Installer_config_invalid
+  | Installer_config_invalid_instruction of int
+
+(* Instructions of the installer configuration. *)
+type instr = Set of {value : string; to_ : string}
+
+(* A configuration is a set of instructions. *)
+type t = instr list
+
+val pp : Format.formatter -> t -> unit
+
+val instr_encoding : instr Data_encoding.t
+
+val encoding : t Data_encoding.t
+
+(** [parse_yaml content] parses [content] as a YAML representing the
+    configuration of the kernel installer, and returns the corresponding set of
+    instructions. *)
+val parse_yaml : string -> t tzresult

tezos/tezos-mirror

src/lib_smart_rollup/installer_config.mli
@@ -1,0 +1,41 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2023 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(**
+   Handling of YAML configuration file used by the kernel installer.
+
+   A configuration has the form:
+   ```
+   instructions:
+     - set:
+         value: <hexadecimal value>
+         to: <string>
+     ...
+   ```
+*)
+
+type error +=
+  | Installer_config_yaml_error of string
+  | Installer_config_invalid
+  | Installer_config_invalid_instruction of int
+
+(* Instructions of the installer configuration. *)
+type instr = Set of {value : string; to_ : string}
+
+(* A configuration is a list of instructions. *)
+type t = instr list
+
+val pp : Format.formatter -> t -> unit
+
+val instr_encoding : instr Data_encoding.t
+
+val encoding : t Data_encoding.t
+
+(** [parse_yaml content] parses [content] as a YAML representing the
+    configuration of the kernel installer, and returns the corresponding set of
+    instructions. *)
+val parse_yaml : string -> t tzresult