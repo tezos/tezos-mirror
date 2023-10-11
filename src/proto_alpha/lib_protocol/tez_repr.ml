f9edc87a5460f8dd40a3d019474c66b30052929d
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-10-11 16:44:03 +0200

    Proto/Tez_repr: add mul_percentage

--- src/proto_alpha/lib_protocol/tez_repr.ml
+++ src/proto_alpha/lib_protocol/tez_repr.ml
@@ -182,6 +182,13 @@ let mul_ratio tez ~num ~den =
     if Z.fits_int64 z then return (Tez_tag (Z.to_int64 z))
     else tzfail (Multiplication_overflow (tez, num))
 
+let mul_percentage =
+  let z100 = Z.of_int 100 in
+  fun (Tez_tag t) (percentage : Int_percentage.t) ->
+    (* Guaranteed to produce no errors by the invariants on {!Int_percentage.t}. *)
+    Tez_tag
+      Z.(to_int64 (div (mul (of_int64 t) (of_int (percentage :> int))) z100))
+
 let of_mutez t = if t < 0L then None else Some (Tez_tag t)
 
 let of_mutez_exn x =
