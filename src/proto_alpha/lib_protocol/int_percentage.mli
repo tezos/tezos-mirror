7de8f2876132c8767b493fbba6c79a26448157b2
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-10-11 16:17:19 +0200

    Proto: add module Int_percentage

---
+++ src/proto_alpha/lib_protocol/int_percentage.mli
@@ -0,0 +1,9 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2023 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** An integer value between 0 and 100, inclusive. *)
+type t = private int
