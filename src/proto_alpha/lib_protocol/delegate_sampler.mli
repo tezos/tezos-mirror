9a0cc67062716c94f7405ea0cbc3e6705581502a
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-10-12 11:53:05 +0200

    Proto/Delegate_sampler: remove dead delegate_baking_power_for_cycle

--- src/proto_alpha/lib_protocol/delegate_sampler.mli
+++ src/proto_alpha/lib_protocol/delegate_sampler.mli
@@ -85,14 +85,6 @@ val select_distribution_for_cycle :
   Raw_context.t tzresult Lwt.t
 
 module For_RPC : sig
-  (** The baking power for a given delegate from the selected stake
-    snapshot of the current cycle. *)
-  val delegate_baking_power_for_cycle :
-    Raw_context.t ->
-    Cycle_repr.t ->
-    Signature.public_key_hash ->
-    int64 tzresult Lwt.t
-
   (** The baking power for a given delegate computed from its current
     stake. *)
   val delegate_current_baking_power :
