mavryk-network/mavryk-protocol

evm/kernel: test pkey is returned from validation

tezt/tests/evm_rollup.ml
@@ -2136,6 +2136,39 @@ let test_rpc_sendRawTransaction =
     ~error_msg:"Unexpected returned hash, should be %R, but got %L" ;
   unit
 
+let test_validation_result =
+  Protocol.register_test
+    ~__FILE__
+    ~tags:["evm"; "simulate"]
+    ~title:
+      "Ensure validation returns appropriate address for a given transaction."
+  @@ fun protocol ->
+  let* {sc_rollup_client; _} = setup_past_genesis ~admin:None protocol in
+
+  let tx =
+    "f867808080940000000000000000000000000000000000000000880de0b6b3a764000080820a95a0ed19fdcb200e950226eec84d2790bed3af7339c4be1a272023ddddfcc00d2db0a04fb15e1c4d2826e0d53431262a1b8d3e7accfce1325f632eb127db8a1b6d5f41"
+  in
+  let simulation_msg = "ff0101" ^ tx in
+
+  let*! simulation_result =
+    Sc_rollup_client.simulate
+      ~insight_requests:
+        [
+          `Durable_storage_key ["evm"; "simulation_status"];
+          `Durable_storage_key ["evm"; "simulation_result"];
+        ]
+      sc_rollup_client
+      [Hex.to_string @@ `Hex "ff"; Hex.to_string @@ `Hex simulation_msg]
+  in
+
+  let expected_insights =
+    [Some "01"; Some "9dcb74adb59a1eb4590a5aa4f71455c35e0933a0"]
+  in
+  Check.(
+    (simulation_result.insights = expected_insights) (list @@ option string))
+    ~error_msg:"Expected result %R, but got %L" ;
+  unit
+
 type storage_migration_results = {
   transfer_result : transfer_result;
   block_result : Block.t;
@@ -2542,7 +2575,8 @@ let register_evm_proxy_server ~protocols =
   test_deposit_dailynet protocols ;
   test_rpc_sendRawTransaction_nonce_too_low protocols ;
   test_rpc_sendRawTransaction_nonce_too_high protocols ;
-  test_rpc_sendRawTransaction_invalid_chain_id protocols
+  test_rpc_sendRawTransaction_invalid_chain_id protocols ;
+  test_validation_result protocols
 
 let register ~protocols =
   register_evm_proxy_server ~protocols ;

tezos/tezos-mirror

tezt/tests/evm_rollup.ml
@@ -2316,6 +2316,36 @@ let test_rpc_getTransactionByBlockNumberAndIndex =
     ~title:"RPC method eth_getTransactionByBlockNumberAndIndex"
   @@ test_rpc_getTransactionByBlockArgAndIndex ~by:`Number
 
+let test_validation_result =
+  Protocol.register_test
+    ~__FILE__
+    ~tags:["evm"; "simulate"]
+    ~title:
+      "Ensure validation returns appropriate address for a given transaction."
+  @@ fun protocol ->
+  let* {sc_rollup_client; _} = setup_past_genesis ~admin:None protocol in
+  let tx =
+    "f867808080940000000000000000000000000000000000000000880de0b6b3a764000080820a95a0ed19fdcb200e950226eec84d2790bed3af7339c4be1a272023ddddfcc00d2db0a04fb15e1c4d2826e0d53431262a1b8d3e7accfce1325f632eb127db8a1b6d5f41"
+  in
+  let simulation_msg = "ff0101" ^ tx in
+  let*! simulation_result =
+    Sc_rollup_client.simulate
+      ~insight_requests:
+        [
+          `Durable_storage_key ["evm"; "simulation_status"];
+          `Durable_storage_key ["evm"; "simulation_result"];
+        ]
+      sc_rollup_client
+      [Hex.to_string @@ `Hex "ff"; Hex.to_string @@ `Hex simulation_msg]
+  in
+  let expected_insights =
+    [Some "01"; Some "9dcb74adb59a1eb4590a5aa4f71455c35e0933a0"]
+  in
+  Check.(
+    (simulation_result.insights = expected_insights) (list @@ option string))
+    ~error_msg:"Expected result %R, but got %L" ;
+  unit
+
 type storage_migration_results = {
   transfer_result : transfer_result;
   block_result : Block.t;
@@ -3192,7 +3222,8 @@ let register_evm_proxy_server ~protocols =
   test_cover_fees protocols ;
   test_rpc_gasPrice protocols ;
   test_rpc_getStorageAt protocols ;
-  test_accounts_double_indexing protocols
+  test_accounts_double_indexing protocols ;
+  test_validation_result protocols
 
 let register ~protocols =
   register_evm_proxy_server ~protocols ;