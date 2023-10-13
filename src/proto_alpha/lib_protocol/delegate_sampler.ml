96541cb3cd55e48a650aad6990867d0e18a397d2
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-10-13 13:55:35 +0200

    Proto/Delegate_sampler: do not compute snapshot index

--- src/proto_alpha/lib_protocol/delegate_sampler.ml
+++ src/proto_alpha/lib_protocol/delegate_sampler.ml
@@ -213,11 +213,7 @@ let compute_snapshot_index ctxt cycle ~max_snapshot_index =
 
 let select_distribution_for_cycle ctxt ~slashings cycle =
   let open Lwt_result_syntax in
-  let* max_snapshot_index = Stake_storage.max_snapshot_index ctxt in
   let* seed = Seed_storage.raw_for_cycle ctxt cycle in
-  let* _selected_index =
-    compute_snapshot_index_for_seed ~max_snapshot_index seed
-  in
   let* stakes, total_stake = get_stakes ctxt ~slashings in
   let* ctxt =
     Stake_storage.set_selected_distribution_for_cycle
