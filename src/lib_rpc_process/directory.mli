OKEAMAH/Git

RPC process: handle config rpc endpoint

src/lib_rpc_process/directory.mli
@@ -24,8 +24,10 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** [build_rpc_directory ~node_version] builds the Tezos RPC directory for the
-    rpc process. RPC handled here are not forwarded to the node.
+(** [build_rpc_directory node_version config] builds the Tezos RPC directory for
+    the rpc process. RPC handled here are not forwarded to the node.
 *)
 val build_rpc_directory :
-  Tezos_version.Node_version.t -> unit Tezos_rpc.Directory.t
+  Tezos_version.Node_version.t ->
+  Octez_node_config.Config_file.t ->
+  unit Tezos_rpc.Directory.t

tezos/tezos-mirror

src/lib_rpc_process/directory.mli
@@ -5,8 +5,10 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** [build_rpc_directory ~node_version] builds the Tezos RPC directory for the
-    rpc process. RPCs handled here are not forwarded to the node.
+(** [build_rpc_directory node_version config] builds the Tezos RPC directory for
+    the rpc process. RPCs handled here are not forwarded to the node.
 *)
 val build_rpc_directory :
-  Tezos_version.Node_version.t -> unit Tezos_rpc.Directory.t
+  Tezos_version.Node_version.t ->
+  Octez_node_config.Config_file.t ->
+  unit Tezos_rpc.Directory.t