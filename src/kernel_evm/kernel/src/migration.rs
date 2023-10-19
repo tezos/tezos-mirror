0d2443217a5397fea291dc8f3fdafa098b68282c
Antonio Locascio <antonio.locascio@nomadic-labs.com>
2023-10-19 08:50:34 +0200

    EVM/Kernel: write migration for new Tx receipt and object encoding

--- src/kernel_evm/kernel/src/migration.rs
+++ src/kernel_evm/kernel/src/migration.rs
@@ -4,21 +4,27 @@
 // SPDX-License-Identifier: MIT
 
 use crate::error::Error;
+use crate::error::StorageError::InvalidLoadValue;
 use crate::error::UpgradeProcessError::Fallback;
 use crate::storage::{
-    block_path, index_block, init_blocks_index, read_current_block_number,
-    read_storage_version, store_rlp, store_storage_version, STORAGE_VERSION,
+    block_path, index_block, init_blocks_index, init_transaction_hashes_index,
+    object_path, read_current_block_number, read_storage_version, receipt_path,
+    store_rlp, store_storage_version, STORAGE_VERSION,
 };
 use ethbloom::Bloom;
-use primitive_types::{H256, U256};
+use ethereum::Log;
+use primitive_types::{H160, H256, U256};
 use rlp::{Decodable, DecoderError, Rlp};
 use tezos_ethereum::block::L2Block;
 use tezos_ethereum::eth_gen::OwnedHash;
-use tezos_ethereum::rlp_helpers::{
-    decode_field, decode_field_h256, decode_field_u256_le, decode_transaction_hash_list,
-    next, FromRlpBytes,
+use tezos_ethereum::transaction::{
+    TransactionHash, TransactionObject, TransactionReceipt, TransactionStatus,
+    TransactionType, TRANSACTION_HASH_SIZE,
 };
-use tezos_ethereum::transaction::TransactionHash;
+use tezos_ethereum::{
+    rlp_helpers::*,
+    tx_signature::{rlp_decode_opt, TxSignature},
+};
 use tezos_smart_rollup_encoding::timestamp::Timestamp;
 use tezos_smart_rollup_host::runtime::Runtime;
 
@@ -130,6 +136,7 @@ fn migrate_one_block<Host: Runtime>(
 ) -> Result<(), Error> {
     let path = block_path(block_hash)?;
     let bytes = host.store_read_all(&path)?;
+    host.store_delete(&path)?;
     // Read old block
     let old_block = OldL2Block::from_rlp_bytes(&bytes)?;
     let new_block = L2Block {
@@ -216,6 +223,204 @@ fn fix_block_indexes<Host: Runtime>(host: &mut Host) -> Result<(), Error> {
     Ok(())
 }
 
+// The Transaction Receipt used to include the block hash.
+// This had to be mocked, as the block hash should depend on
+// the receipts. This is why this field was removed.
+#[derive(Debug, PartialEq, Clone)]
+pub struct OldTransactionReceipt {
+    pub hash: TransactionHash,
+    pub index: u32,
+    pub block_hash: H256,
+    pub block_number: U256,
+    pub from: H160,
+    pub to: Option<H160>,
+    pub cumulative_gas_used: U256,
+    pub effective_gas_price: U256,
+    pub gas_used: U256,
+    pub contract_address: Option<H160>,
+    pub logs: Vec<Log>,
+    pub logs_bloom: Bloom,
+    pub type_: TransactionType,
+    pub status: TransactionStatus,
+}
+
+impl Decodable for OldTransactionReceipt {
+    fn decode(decoder: &Rlp<'_>) -> Result<Self, DecoderError> {
+        if !decoder.is_list() {
+            return Err(DecoderError::RlpExpectedToBeList);
+        }
+        if Ok(14) != decoder.item_count() {
+            return Err(DecoderError::RlpIncorrectListLen);
+        }
+
+        let mut it = decoder.iter();
+        let hash: TransactionHash = decode_transaction_hash(&next(&mut it)?)?;
+        let index: u32 = decode_field(&next(&mut it)?, "index")?;
+        let block_hash: H256 = decode_field(&next(&mut it)?, "block_hash")?;
+        let block_number: U256 = decode_field_u256_le(&next(&mut it)?, "block_number")?;
+        let from: H160 = decode_field(&next(&mut it)?, "from")?;
+        let to: Option<H160> = decode_option(&next(&mut it)?, "to")?;
+        let cumulative_gas_used: U256 =
+            decode_field_u256_le(&next(&mut it)?, "cumulative_gas_used")?;
+        let effective_gas_price: U256 =
+            decode_field_u256_le(&next(&mut it)?, "effective_gas_price")?;
+        let gas_used: U256 = decode_field_u256_le(&next(&mut it)?, "gas_used")?;
+        let contract_address: Option<H160> =
+            decode_option(&next(&mut it)?, "contract_address")?;
+        let logs = decode_list(&next(&mut it)?, "logs")?;
+        let logs_bloom = decode_field(&next(&mut it)?, "logs_bloom")?;
+        let type_: TransactionType = decode_transaction_type(&next(&mut it)?)?;
+        let status: TransactionStatus = decode_transaction_status(&next(&mut it)?)?;
+        Ok(OldTransactionReceipt {
+            hash,
+            index,
+            block_hash,
+            block_number,
+            from,
+            to,
+            cumulative_gas_used,
+            effective_gas_price,
+            gas_used,
+            contract_address,
+            logs,
+            logs_bloom,
+            type_,
+            status,
+        })
+    }
+}
+
+// The Transaction Object used to include the block hash.
+// This had to be mocked, as the block hash should depend on
+// the objects. This is why this field was removed.
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct OldTransactionObject {
+    pub block_hash: H256,
+    pub block_number: U256,
+    pub from: H160,
+    pub gas_used: U256,
+    pub gas_price: U256,
+    pub hash: TransactionHash,
+    pub input: Vec<u8>,
+    pub nonce: U256,
+    pub to: Option<H160>,
+    pub index: u32,
+    pub value: U256,
+    pub signature: Option<TxSignature>,
+}
+
+impl Decodable for OldTransactionObject {
+    fn decode(decoder: &Rlp<'_>) -> Result<Self, DecoderError> {
+        if decoder.is_list() {
+            if Ok(14) == decoder.item_count() {
+                let mut it = decoder.iter();
+                let block_hash: H256 = decode_field_h256(&next(&mut it)?, "block_hash")?;
+                let block_number: U256 =
+                    decode_field_u256_le(&next(&mut it)?, "block_number")?;
+                let from: H160 = decode_field(&next(&mut it)?, "from")?;
+                let gas_used: U256 = decode_field_u256_le(&next(&mut it)?, "gas_used")?;
+                let gas_price: U256 = decode_field_u256_le(&next(&mut it)?, "gas_price")?;
+                let hash: TransactionHash = decode_transaction_hash(&next(&mut it)?)?;
+                let input: Vec<u8> = decode_field(&next(&mut it)?, "input")?;
+                let nonce: U256 = decode_field_u256_le(&next(&mut it)?, "nonce")?;
+                let to: Option<H160> = decode_option(&next(&mut it)?, "to")?;
+                let index: u32 = decode_field(&next(&mut it)?, "index")?;
+                let value: U256 = decode_field_u256_le(&next(&mut it)?, "value")?;
+                let signature = rlp_decode_opt(&mut it)?;
+                Ok(OldTransactionObject {
+                    block_hash,
+                    block_number,
+                    from,
+                    gas_used,
+                    gas_price,
+                    hash,
+                    input,
+                    nonce,
+                    to,
+                    index,
+                    value,
+                    signature,
+                })
+            } else {
+                Err(DecoderError::RlpIncorrectListLen)
+            }
+        } else {
+            Err(DecoderError::RlpExpectedToBeList)
+        }
+    }
+}
+
+fn migrate_one_receipt<Host: Runtime>(
+    host: &mut Host,
+    receipt_hash: &TransactionHash,
+) -> Result<(), Error> {
+    let path = receipt_path(receipt_hash)?;
+    let bytes = host.store_read_all(&path)?;
+    host.store_delete(&path)?;
+    // Read old receipt
+    let old_receipt = OldTransactionReceipt::from_rlp_bytes(&bytes)?;
+    let new_receipt = TransactionReceipt {
+        hash: old_receipt.hash,
+        index: old_receipt.index,
+        block_number: old_receipt.block_number,
+        from: old_receipt.from,
+        to: old_receipt.to,
+        cumulative_gas_used: old_receipt.cumulative_gas_used,
+        effective_gas_price: old_receipt.effective_gas_price,
+        gas_used: old_receipt.gas_used,
+        contract_address: old_receipt.contract_address,
+        logs: old_receipt.logs,
+        logs_bloom: old_receipt.logs_bloom,
+        type_: old_receipt.type_,
+        status: old_receipt.status,
+    };
+    // Write new receipt
+    store_rlp(&new_receipt, host, &path)
+}
+
+fn migrate_one_object<Host: Runtime>(
+    host: &mut Host,
+    tx_hash: &TransactionHash,
+) -> Result<(), Error> {
+    let path = object_path(tx_hash)?;
+    let bytes = host.store_read_all(&path)?;
+    host.store_delete(&path)?;
+    // Read old object
+    let old_object = OldTransactionObject::from_rlp_bytes(&bytes)?;
+    let new_receipt = TransactionObject {
+        block_number: old_object.block_number,
+        from: old_object.from,
+        gas_used: old_object.gas_used,
+        gas_price: old_object.gas_price,
+        hash: old_object.hash,
+        input: old_object.input,
+        nonce: old_object.nonce,
+        to: old_object.to,
+        index: old_object.index,
+        value: old_object.value,
+        signature: old_object.signature,
+    };
+    // Write new receipt
+    store_rlp(&new_receipt, host, &path)
+}
+
+fn migrate_receipts_and_objects<Host: Runtime>(host: &mut Host) -> Result<(), Error> {
+    let indexed_transaction_hashes = init_transaction_hashes_index()?;
+    let tx_len = indexed_transaction_hashes.length(host)?;
+    for index in 0..tx_len {
+        let bytes = indexed_transaction_hashes.unsafe_get_value(host, index)?;
+        let tx_hash: TransactionHash =
+            bytes.as_slice().try_into().map_err(|_| InvalidLoadValue {
+                expected: TRANSACTION_HASH_SIZE,
+                actual: bytes.len(),
+            })?;
+        migrate_one_receipt(host, &tx_hash)?;
+        migrate_one_object(host, &tx_hash)?;
+    }
+    Ok(())
+}
+
 // The workflow for migration is the following:
 //
 // - bump `storage::STORAGE_VERSION` by one
@@ -229,6 +434,8 @@ fn migration<Host: Runtime>(host: &mut Host) -> Result<MigrationStatus, Error> {
         // MIGRATION CODE - START
 
         migrate_blocks(host)?;
+
+        migrate_receipts_and_objects(host)?;
 
         replace_genesis_parent_hash(host)?;
 
