OKEAMAH/Git

rpc-process: fix ACL handling by filtering during the forwarding

src/lib_rpc_http/RPC_middleware.ml
@@ -82,6 +82,33 @@ let make_transform_callback ?ctx ?on_forwarding forwarding_endpoint callback
     Lwt.return (`Response answer)
   else Lwt.return answer
 
+let make_transform_callback_with_acl ~acl ?ctx ?on_forwarding
+    forwarding_endpoint callback conn req body =
+  let allowed =
+    let path =
+      Resto.Utils.decode_split_path (Uri.path @@ Cohttp.Request.uri req)
+    in
+    match Cohttp.Request.meth req with
+    | #Resto.meth as meth -> RPC_server.Acl.allowed acl ~meth ~path
+    | `HEAD | `OPTIONS | `Other _ | `CONNECT | `TRACE -> true
+  in
+  if allowed then
+    make_transform_callback
+      ?ctx
+      ?on_forwarding
+      forwarding_endpoint
+      callback
+      conn
+      req
+      body
+  else
+    let response =
+      let body, encoding = (Cohttp_lwt.Body.empty, Cohttp.Transfer.Fixed 0L) in
+      let status = `Unauthorized in
+      (Cohttp.Response.make ~status ~encoding (), body)
+    in
+    Lwt.return (`Response response)
+
 let rpc_metrics_transform_callback ~update_metrics dir callback conn req body =
   let open Lwt_result_syntax in
   let do_call () = callback conn req body in
@@ -109,5 +136,12 @@ let rpc_metrics_transform_callback ~update_metrics dir callback conn req body =
       (* Otherwise, the call must be done anyway. *)
       do_call ()
 
-let proxy_server_query_forwarder ?ctx ?on_forwarding forwarding_endpoint =
-  make_transform_callback ?ctx ?on_forwarding forwarding_endpoint
+let proxy_server_query_forwarder ?acl ?ctx ?on_forwarding forwarding_endpoint =
+  match acl with
+  | Some acl ->
+      make_transform_callback_with_acl
+        ~acl
+        ?ctx
+        ?on_forwarding
+        forwarding_endpoint
+  | None -> make_transform_callback ?ctx ?on_forwarding forwarding_endpoint

src/lib_rpc_process/main.ml
@@ -104,7 +104,9 @@ let launch_rpc_server (config : Parameters.t) (addr, port) =
       ~media_types:(Media_type.Command_line.of_command_line media_types)
       dir
   in
-  let callback = Forward_handler.callback server config.rpc_comm_socket_path in
+  let callback =
+    Forward_handler.callback ~acl server config.rpc_comm_socket_path
+  in
   Lwt.catch
     (fun () ->
       let*! () = RPC_server.launch ~host server ~callback mode in

tezos/tezos-mirror

src/lib_rpc_http/RPC_middleware.ml
@@ -95,6 +95,33 @@ let make_transform_callback ?ctx ?forwarder_events forwarding_endpoint callback
     in
     Lwt.return answer
 
+let make_transform_callback_with_acl ~acl ?ctx ?forwarder_events
+    forwarding_endpoint callback conn req body =
+  let allowed =
+    let path =
+      Resto.Utils.decode_split_path (Uri.path @@ Cohttp.Request.uri req)
+    in
+    match Cohttp.Request.meth req with
+    | #Resto.meth as meth -> RPC_server.Acl.allowed acl ~meth ~path
+    | `HEAD | `OPTIONS | `Other _ | `CONNECT | `TRACE -> true
+  in
+  if allowed then
+    make_transform_callback
+      ?ctx
+      ?forwarder_events
+      forwarding_endpoint
+      callback
+      conn
+      req
+      body
+  else
+    let response =
+      let body, encoding = (Cohttp_lwt.Body.empty, Cohttp.Transfer.Fixed 0L) in
+      let status = `Unauthorized in
+      (Cohttp.Response.make ~status ~encoding (), body)
+    in
+    Lwt.return (`Response response)
+
 let rpc_metrics_transform_callback ~update_metrics dir callback conn req body =
   let open Lwt_result_syntax in
   let do_call () = callback conn req body in
@@ -122,5 +149,13 @@ let rpc_metrics_transform_callback ~update_metrics dir callback conn req body =
       (* Otherwise, the call must be done anyway. *)
       do_call ()
 
-let proxy_server_query_forwarder ?ctx ?forwarder_events forwarding_endpoint =
-  make_transform_callback ?ctx ?forwarder_events forwarding_endpoint
+let proxy_server_query_forwarder ?acl ?ctx ?forwarder_events forwarding_endpoint
+    =
+  match acl with
+  | Some acl ->
+      make_transform_callback_with_acl
+        ~acl
+        ?ctx
+        ?forwarder_events
+        forwarding_endpoint
+  | None -> make_transform_callback ?ctx ?forwarder_events forwarding_endpoint

src/lib_rpc_process/main.ml
@@ -109,7 +109,9 @@ let launch_rpc_server (params : Parameters.t) (addr, port) =
       ~media_types:(Media_type.Command_line.of_command_line media_types)
       dir
   in
-  let callback = Forward_handler.callback server params.rpc_comm_socket_path in
+  let callback =
+    Forward_handler.callback ~acl server params.rpc_comm_socket_path
+  in
   Lwt.catch
     (fun () ->
       let*! () = RPC_server.launch ~host server ~callback mode in