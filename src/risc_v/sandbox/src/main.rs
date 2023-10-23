78de1e1f786a7af7593a2afbf733817317cde531
Ole Krüger <ole.kruger@trili.tech>
2023-10-23 13:15:30 +0100

    RISC-V: Sandbox

---
+++ src/risc_v/sandbox/src/main.rs
@@ -0,0 +1,58 @@
+use rvemu::{cpu::Mode, emulator::Emulator, exception::Exception};
+use std::error::Error;
+
+mod cli;
+mod input;
+mod syscall;
+
+/// Convert a RISC-V exception into an error.
+pub fn exception_to_error(exc: Exception) -> Box<dyn Error> {
+    format!("{:?}", exc).into()
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = cli::parse();
+
+    let mut emu = Emulator::new();
+    emu.cpu.mode = Mode::User;
+
+    // Load the ELF binary into the emulator.
+    input::Input::load_file(&cli.input)?
+        .configure_emulator(&mut emu)
+        .map_err(exception_to_error)?;
+
+    let mut prev_pc = emu.cpu.pc;
+    loop {
+        emu.cpu.devices_increment();
+
+        if let Some(interrupt) = emu.cpu.check_pending_interrupt() {
+            interrupt.take_trap(&mut emu.cpu);
+
+            // We don't do anything with the devices at the moment. So we'll
+            // just panic if they magically come alive.
+            panic!("Interrupt {:?}", interrupt);
+        }
+
+        emu.cpu
+            .execute()
+            .map(|_| ())
+            .or_else(|exception| match exception {
+                Exception::EnvironmentCallFromUMode => syscall::handle(&mut emu),
+
+                _ => {
+                    let trap = exception.take_trap(&mut emu.cpu);
+
+                    // Don't bother handling other exceptions. For now they're
+                    // all fatal.
+                    panic!("Exception {:?} at {:#x}: {:?}", exception, prev_pc, trap)
+                }
+            })?;
+
+        // If the program loops in place we assume it is stuck.
+        if prev_pc == emu.cpu.pc {
+            panic!("Stuck at {:#x}", prev_pc);
+        }
+
+        prev_pc = emu.cpu.pc;
+    }
+}
