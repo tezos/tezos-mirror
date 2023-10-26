d3eb2deb6b32e337f1ea7d8a61b157a9fd9f1197
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-10-26 16:17:56 +0200

    evm/benchmarks: add gasPrice parameter

--- src/kernel_evm/benchmarks/scripts/benchmarks/bench_verifySignature.js
+++ src/kernel_evm/benchmarks/scripts/benchmarks/bench_verifySignature.js
@@ -50,6 +50,6 @@ txs.push(utils.send(player1, create.addr, 0, call_getEthSignedMessageHash))
 
 // following tx needs more than 3999000 gas
 // tick model can't accept more for now
-txs.push(utils.send(player1, create.addr, 0, call_verify, gasLimit = 3999000))
+txs.push(utils.send(player1, create.addr, 0, call_verify, { gasLimit: 3999000 }))
 
 utils.print_bench([txs])
