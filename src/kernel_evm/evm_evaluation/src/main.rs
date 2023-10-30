64a3c04187ad53ef4c055dce21b0a7871fa5fb39
Rodi-Can Bozman <rodi.bozman@functori.com>
2023-10-30 17:19:36 +0100

    EVM/Eval: update report and skip test that are taking too long
    Since the fix for inter contract calls bug has been fixed, loads of test
    from the evm evaluation are finally behaving as they should.
    Some of them don't need to be skipped anymore.
    Some of them are now taking way too long and need to be skipped.

--- src/kernel_evm/evm_evaluation/src/main.rs
+++ src/kernel_evm/evm_evaluation/src/main.rs
@@ -23,35 +23,27 @@ pub fn find_all_json_tests(path: &Path) -> Vec<PathBuf> {
         .collect::<Vec<PathBuf>>()
 }
 
-/* REPORT - 23/10/2023 - 14:40 (UTC+2)
+/* REPORT - 30/10/2023 - 17:18 (UTC+2)
 
 # Number of tests:
-    - Runned: 2595
-    - Skipped: 69
+    - Runned: 2474
+    - Skipped: 194
 
 # Ethereum transactions:
-    - 17571 in total with Shanghai config
+    - 17404 in total with Shanghai config
 
 # EVM execution outcomes: (just for stats, not relevant)
-    - 5266 SUCCESS
-    - 12137 FAILURE
+    - 7565 SUCCESS
+    - 5666 FAILURE
     - 168 INVALID
 
 # EVM evaluation interpretation:
 
-## Per sub-checks:
-    - 61945 CORRECT STATE
-    - 97773 INVALID STATE
-
-## Per runned ethereum transactions:
-    - 5188 GOOD STATE
-    - 12383 BAD STATE
-
 ## Per test interpreted:
-    - 850 SUCCESS (33.25%)
-    - 1705 FAILURE (66.75%)
+    - 746 SUCCESS (30.65%)
+    - 1688 FAILURE (69.35%)
 
-EVM confidence : 0.33 (target: 1.00) */
+EVM confidence : 0.31 (target: 1.00) */
 
 pub fn main() {
     // Preliminary step:
@@ -70,7 +62,7 @@ pub fn main() {
                 continue;
             }
 
-            // The following test are failing they need in depth debugging
+            // The following test(s) is/are failing they need in depth debugging
             // Reason: panicked at 'arithmetic operation overflow'
             if test_file.file_name() == Some(OsStr::new("HighGasPrice.json"))
                 || test_file.file_name() == Some(OsStr::new("randomStatetest32.json"))
@@ -78,22 +70,13 @@ pub fn main() {
                 || test_file.file_name() == Some(OsStr::new("randomStatetest50.json"))
                 || test_file.file_name() == Some(OsStr::new("randomStatetest468.json"))
                 || test_file.file_name() == Some(OsStr::new("gasCostBerlin.json"))
+                || test_file.file_name() == Some(OsStr::new("underflowTest.json"))
             {
                 println!("\nSKIPPED\n");
                 continue;
             }
 
-            // The following test are failing they need in depth debugging
-            // Reason: stack overflow
-            if test_file.file_name() == Some(OsStr::new("CallRecursiveContract.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new("RecursiveCreateContracts.json"))
-            {
-                println!("\nSKIPPED\n");
-                continue;
-            }
-
-            // The following test are failing they need in depth debugging
+            // The following test(s) is/are failing they need in depth debugging
             // Reason: memory allocation of X bytes failed | 73289 IOT instruction (core dumped)
             if test_file.file_name() == Some(OsStr::new("sha3.json")) {
                 println!("\nSKIPPED\n");
@@ -108,6 +91,46 @@ pub fn main() {
 
             // Oddly long checks on a test that do no relevant check (passing)
             if test_file.file_name() == Some(OsStr::new("intrinsic.json")) {
+                println!("\nSKIPPED\n");
+                continue;
+            }
+
+            // Long tests ~ (outcome is unknown)
+            if test_file.file_name() == Some(OsStr::new("static_Call50000_sha256.json"))
+                || test_file.file_name()
+                    == Some(OsStr::new("static_Call50000_ecrec.json"))
+                || test_file.file_name() == Some(OsStr::new("static_Call50000.json"))
+            {
+                println!("\nSKIPPED\n");
+                continue;
+            }
+
+            // Reason: panicked at 'attempt to add with overflow'
+            if let Some(file_name) = test_file.to_str() {
+                if file_name.contains("DiffPlaces.json") {
+                    println!("\nSKIPPED\n");
+                    continue;
+                }
+            }
+
+            // Reason: panicked at 'attempt to multiply with overflow'
+            if test_file.file_name()
+                == Some(OsStr::new("static_Call1024BalanceTooLow.json"))
+                || test_file.file_name()
+                    == Some(OsStr::new("static_Call1024BalanceTooLow2.json"))
+                || test_file.file_name()
+                    == Some(OsStr::new("static_Call1024PreCalls3.json"))
+            {
+                println!("\nSKIPPED\n");
+                continue;
+            }
+
+            // Reason: panicked at 'attempt to add with overflow'
+            if test_file.file_name() == Some(OsStr::new("static_Call1024PreCalls.json"))
+                || test_file.file_name()
+                    == Some(OsStr::new("static_Call1024PreCalls2.json"))
+                || test_file.file_name() == Some(OsStr::new("diffPlaces.json"))
+            {
                 println!("\nSKIPPED\n");
                 continue;
             }
@@ -215,6 +238,7 @@ pub fn main() {
                 continue;
             }
         }
+
         runner::run_test(&test_file).unwrap();
     }
     println!("@@@@@ END OF TESTING @@@@@");
