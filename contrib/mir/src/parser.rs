mavryk-network/mavryk-protocol

MIR: parse full contracts (no views yet)

contrib/mir/src/parser.rs
@@ -6,7 +6,7 @@
 /******************************************************************************/
 
 use crate::ast::*;
-use crate::lexer::{LexerError, Tok};
+use crate::lexer::{LexerError, Prim, Tok};
 use crate::syntax;
 use lalrpop_util::ParseError;
 use logos::Logos;
@@ -17,11 +17,61 @@ pub enum ParserError {
     ExpectedU10(i128),
     #[error(transparent)]
     LexerError(#[from] LexerError),
+    #[error("no {0} field")]
+    NoField(Prim),
+    #[error("duplicate {0} field")]
+    DuplicateField(Prim),
 }
 
 #[allow(dead_code)]
 pub fn parse(src: &str) -> Result<ParsedInstructionBlock, ParseError<usize, Tok, ParserError>> {
     syntax::instructionBlockParser::new().parse(spanned_lexer(src))
+}
+
+#[allow(dead_code)]
+pub fn parse_contract(
+    src: &str,
+) -> Result<Contract<ParsedStage>, ParseError<usize, Tok, ParserError>> {
+    syntax::ContractParser::new().parse(spanned_lexer(src))
+}
+
+/// Helper type to parse contract fields
+pub enum ContractEntity {
+    Parameter(Type),
+    Storage(Type),
+    Code(ParsedInstruction),
+}
+
+impl TryFrom<Vec<ContractEntity>> for Contract<ParsedStage> {
+    type Error = crate::parser::ParserError;
+    fn try_from(value: Vec<ContractEntity>) -> Result<Self, Self::Error> {
+        use crate::lexer::Prim as P;
+        use crate::parser::ParserError as Err;
+        use ContractEntity as CE;
+        let mut param: Option<Type> = None;
+        let mut storage: Option<Type> = None;
+        let mut code: Option<ParsedInstruction> = None;
+        fn set_if_none<T>(x: &mut Option<T>, y: T, e: P) -> Result<(), Err> {
+            if x.is_none() {
+                *x = Some(y);
+                Ok(())
+            } else {
+                Err(Err::DuplicateField(e))
+            }
+        }
+        for i in value {
+            match i {
+                CE::Parameter(p) => set_if_none(&mut param, p, P::parameter),
+                CE::Storage(p) => set_if_none(&mut storage, p, P::storage),
+                CE::Code(p) => set_if_none(&mut code, p, P::code),
+            }?;
+        }
+        Ok(Contract {
+            parameter: param.ok_or(Err::NoField(P::parameter))?,
+            storage: storage.ok_or(Err::NoField(P::storage))?,
+            code: code.ok_or(Err::NoField(P::code))?,
+        })
+    }
 }
 
 fn spanned_lexer(
@@ -153,6 +203,49 @@ mod tests {
         assert_eq!(
             parse("{CAR @var :ty %field :ty.2 @var.2 %field.2}").unwrap(),
             vec![Car],
+        );
+    }
+
+    #[test]
+    fn parse_contract_test() {
+        use crate::lexer::Prim::{code, parameter, storage};
+        use Instruction::*;
+        use ParserError as Err;
+        use Type as T;
+
+        assert_eq!(
+            parse_contract("parameter unit; storage unit; code FAILWITH"),
+            Ok(Contract {
+                parameter: T::Unit,
+                storage: T::Unit,
+                code: Failwith
+            })
+        );
+        // duplicate
+        assert_eq!(
+            parse_contract("parameter unit; parameter int; storage unit; code FAILWITH"),
+            Err(Err::DuplicateField(parameter).into())
+        );
+        assert_eq!(
+            parse_contract("parameter unit; storage unit; storage int; code FAILWITH"),
+            Err(Err::DuplicateField(storage).into())
+        );
+        assert_eq!(
+            parse_contract("code INT; parameter unit; storage unit; code FAILWITH"),
+            Err(Err::DuplicateField(code).into())
+        );
+        // missing
+        assert_eq!(
+            parse_contract("storage unit; code FAILWITH"),
+            Err(Err::NoField(parameter).into())
+        );
+        assert_eq!(
+            parse_contract("parameter unit; code FAILWITH"),
+            Err(Err::NoField(storage).into())
+        );
+        assert_eq!(
+            parse_contract("parameter unit; storage unit"),
+            Err(Err::NoField(code).into())
         );
     }
 }

tezos/tezos-mirror

contrib/mir/src/parser.rs
@@ -6,7 +6,7 @@
 /******************************************************************************/
 
 use crate::ast::*;
-use crate::lexer::{LexerError, Tok};
+use crate::lexer::{LexerError, Prim, Tok};
 use crate::syntax;
 use lalrpop_util::ParseError;
 use logos::Logos;
@@ -17,11 +17,61 @@ pub enum ParserError {
     ExpectedU10(i128),
     #[error(transparent)]
     LexerError(#[from] LexerError),
+    #[error("no {0} field")]
+    NoField(Prim),
+    #[error("duplicate {0} field")]
+    DuplicateField(Prim),
 }
 
 #[allow(dead_code)]
 pub fn parse(src: &str) -> Result<ParsedInstructionBlock, ParseError<usize, Tok, ParserError>> {
     syntax::instructionBlockParser::new().parse(spanned_lexer(src))
+}
+
+#[allow(dead_code)]
+pub fn parse_contract_script(
+    src: &str,
+) -> Result<ContractScript<ParsedStage>, ParseError<usize, Tok, ParserError>> {
+    syntax::ContractScriptParser::new().parse(spanned_lexer(src))
+}
+
+/// Helper type to parse contract fields
+pub enum ContractScriptEntity {
+    Parameter(Type),
+    Storage(Type),
+    Code(ParsedInstruction),
+}
+
+impl TryFrom<Vec<ContractScriptEntity>> for ContractScript<ParsedStage> {
+    type Error = crate::parser::ParserError;
+    fn try_from(value: Vec<ContractScriptEntity>) -> Result<Self, Self::Error> {
+        use crate::lexer::Prim as P;
+        use crate::parser::ParserError as Err;
+        use ContractScriptEntity as CE;
+        let mut param: Option<Type> = None;
+        let mut storage: Option<Type> = None;
+        let mut code: Option<ParsedInstruction> = None;
+        fn set_if_none<T>(x: &mut Option<T>, y: T, e: P) -> Result<(), Err> {
+            if x.is_none() {
+                *x = Some(y);
+                Ok(())
+            } else {
+                Err(Err::DuplicateField(e))
+            }
+        }
+        for i in value {
+            match i {
+                CE::Parameter(p) => set_if_none(&mut param, p, P::parameter),
+                CE::Storage(p) => set_if_none(&mut storage, p, P::storage),
+                CE::Code(p) => set_if_none(&mut code, p, P::code),
+            }?;
+        }
+        Ok(ContractScript {
+            parameter: param.ok_or(Err::NoField(P::parameter))?,
+            storage: storage.ok_or(Err::NoField(P::storage))?,
+            code: code.ok_or(Err::NoField(P::code))?,
+        })
+    }
 }
 
 fn spanned_lexer(
@@ -153,6 +203,49 @@ mod tests {
         assert_eq!(
             parse("{CAR @var :ty %field :ty.2 @var.2 %field.2}").unwrap(),
             vec![Car],
+        );
+    }
+
+    #[test]
+    fn parse_contract_script_test() {
+        use crate::lexer::Prim::{code, parameter, storage};
+        use Instruction::*;
+        use ParserError as Err;
+        use Type as T;
+
+        assert_eq!(
+            parse_contract_script("parameter unit; storage unit; code FAILWITH"),
+            Ok(ContractScript {
+                parameter: T::Unit,
+                storage: T::Unit,
+                code: Failwith
+            })
+        );
+        // duplicate
+        assert_eq!(
+            parse_contract_script("parameter unit; parameter int; storage unit; code FAILWITH"),
+            Err(Err::DuplicateField(parameter).into())
+        );
+        assert_eq!(
+            parse_contract_script("parameter unit; storage unit; storage int; code FAILWITH"),
+            Err(Err::DuplicateField(storage).into())
+        );
+        assert_eq!(
+            parse_contract_script("code INT; parameter unit; storage unit; code FAILWITH"),
+            Err(Err::DuplicateField(code).into())
+        );
+        // missing
+        assert_eq!(
+            parse_contract_script("storage unit; code FAILWITH"),
+            Err(Err::NoField(parameter).into())
+        );
+        assert_eq!(
+            parse_contract_script("parameter unit; code FAILWITH"),
+            Err(Err::NoField(storage).into())
+        );
+        assert_eq!(
+            parse_contract_script("parameter unit; storage unit"),
+            Err(Err::NoField(code).into())
         );
     }
 }