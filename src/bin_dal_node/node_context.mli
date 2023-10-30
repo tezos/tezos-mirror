62f085d753694fe7f9b5d2846ef9bb7a6fe199a0
François Thiré <francois.thire@nomadic-labs.com>
2023-10-30 11:24:35 +0100

    DAL/Node: Add some docstrings

--- src/bin_dal_node/node_context.mli
+++ src/bin_dal_node/node_context.mli
@@ -170,8 +170,18 @@ module P2P : sig
       it may occur that a discconnection takes several minutes. *)
   val disconnect_peer : t -> ?wait:bool -> P2p_peer.Id.t -> unit Lwt.t
 
+  (** [get_points ?connected t] returns a list of points. If
+      [connected] is [true] (default), it returns only points we are
+      currently connected. Otherwise, it returns a list of known
+      points (points for which we were already successfully connected
+      in the past.) *)
   val get_points : ?connected:bool -> t -> P2p_point.Id.t list tzresult Lwt.t
 
+  (** [get_points_info ?connected t] returns a list of info for
+      points. If [connected] is [true] (default), it returns only info
+      for points we are currently connected. Otherwise, it returns a
+      list of infos for known points (points for which we were already
+      successfully connected in the past.) *)
   val get_points_info :
     ?connected:bool ->
     t ->
