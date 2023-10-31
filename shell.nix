e1890196c3ced6e093645328fb6e7c1a30df7c5e
Emma Turner <emma.turner@trili.tech>
2023-10-31 13:40:48 +0000

    nix: fix compilation on linux
    the libiconvReal unintentionally broke builds (including top-level make) on linux

--- shell.nix
+++ shell.nix
@@ -31,8 +31,8 @@ let
 
     # Cross-compilation for RISC-V
     sources.riscv64Pkgs.clangStdenv.cc
-    sources.riscv64Pkgs.libiconvReal
-  ];
+  ]
+  ++ (pkgs.lib.optional pkgs.stdenv.isDarwin sources.riscv64Pkgs.libiconvReal);
 
   mainPackage = (import ./default.nix).overrideAttrs (old: {
     # This makes the shell load faster.
