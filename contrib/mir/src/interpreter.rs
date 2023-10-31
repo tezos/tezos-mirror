mavryk-network/mavryk-protocol

MIR: implement contract typecheck/interpret as assoc. functions

contrib/mir/src/interpreter.rs
@@ -20,6 +20,38 @@ pub enum InterpretError {
     FailedWith(TypedValue),
 }
 
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+pub enum ContractInterpretError {
+    #[error("failed typechecking input: {0}")]
+    TcError(#[from] crate::typechecker::TcError),
+    #[error("runtime failure while running the contract: {0}")]
+    InterpretError(#[from] crate::interpreter::InterpretError),
+}
+
+#[allow(dead_code)]
+impl Contract<TypecheckedStage> {
+    pub fn interpret(
+        &self,
+        ctx: &mut crate::context::Ctx,
+        parameter: Value,
+        storage: Value,
+    ) -> Result<(Vec<TypedValue>, TypedValue), ContractInterpretError> {
+        let in_ty = Type::new_pair(self.parameter.clone(), self.storage.clone());
+        let in_val = Value::new_pair(parameter, storage);
+        let tc_val = in_val.typecheck(ctx, &in_ty)?;
+        let mut stack = stk![tc_val];
+        self.code.interpret(ctx, &mut stack)?;
+        use TypedValue as V;
+        match stack.pop().expect("empty execution stack") {
+            V::Pair(bx, storage) => match *bx {
+                V::List(vec) => Ok((vec, *storage)),
+                v => panic!("expected `list operation`, got {:?}", v),
+            },
+            v => panic!("expected `pair 'a 'b`, got {:?}", v),
+        }
+    }
+}
+
 #[allow(dead_code)]
 pub fn interpret(
     ast: &TypecheckedAST,

contrib/mir/src/main.rs
@@ -25,6 +25,7 @@ mod tests {
     use crate::gas::Gas;
     use crate::interpreter;
     use crate::parser;
+    use crate::parser::parse_contract;
     use crate::stack::stk;
     use crate::typechecker;
 
@@ -211,6 +212,30 @@ mod tests {
         );
     }
 
+    #[test]
+    fn vote_contract() {
+        let mut ctx = Ctx {
+            amount: 5_000_000,
+            ..Ctx::default()
+        };
+        let interp_res = parse_contract(VOTE_SRC)
+            .unwrap()
+            .typecheck(&mut ctx)
+            .unwrap()
+            .interpret(
+                &mut ctx,
+                "foo".into(),
+                vec![Elt("bar", 0), Elt("baz", 0), Elt("foo", 0)].into(),
+            );
+        use TypedValue as TV;
+        match interp_res.unwrap() {
+            (_, TV::Map(m)) => {
+                assert_eq!(m.get(&TV::String("foo".to_owned())).unwrap(), &TV::Int(1))
+            }
+            _ => panic!("unexpected contract output"),
+        }
+    }
+
     const FIBONACCI_SRC: &str = "{ INT ; PUSH int 0 ; DUP 2 ; GT ;
            IF { DIP { PUSH int -1 ; ADD } ;
             PUSH int 1 ;
@@ -237,4 +262,22 @@ mod tests {
             LOOP { SWAP ; DUP 2 ; ADD ; DIP 2 { PUSH int -1 ; ADD } ; DUP 3 ; GT } ;
             DIP { DROP 2 } }
           { DIP { DROP } } }";
+
+    const VOTE_SRC: &str = "{
+          parameter (string %vote);
+          storage (map string int);
+          code {
+              AMOUNT;
+              PUSH mutez 5000000;
+              COMPARE; GT;
+              IF { { UNIT; FAILWITH } } {};
+              DUP; DIP { CDR; DUP }; CAR; DUP;
+              DIP {
+                  GET; { IF_NONE { { UNIT ; FAILWITH } } {} };
+                  PUSH int 1; ADD; SOME
+              };
+              UPDATE;
+              NIL operation; PAIR
+          }
+      }";
 }

contrib/mir/src/typechecker.rs
@@ -77,6 +77,33 @@ pub enum StacksNotEqualReason {
 pub struct TypesNotEqual(Type, Type);
 
 #[allow(dead_code)]
+impl Contract<ParsedStage> {
+    pub fn typecheck(
+        self,
+        ctx: &mut crate::context::Ctx,
+    ) -> Result<Contract<TypecheckedStage>, crate::typechecker::TcError> {
+        let Contract {
+            parameter, storage, ..
+        } = self;
+        let mut stack = stk![Type::new_pair(parameter.clone(), storage.clone())];
+        let code = self.code.typecheck(ctx, &mut stack)?;
+        ensure_stacks_eq(
+            ctx,
+            &stk![Type::new_pair(
+                Type::new_list(Type::Operation),
+                storage.clone()
+            )],
+            &stack,
+        )?;
+        Ok(Contract {
+            code,
+            parameter,
+            storage,
+        })
+    }
+}
+
+#[allow(dead_code)]
 pub fn typecheck(
     ast: ParsedAST,
     ctx: &mut Ctx,

tezos/tezos-mirror

contrib/mir/src/interpreter.rs
@@ -20,6 +20,41 @@ pub enum InterpretError {
     FailedWith(TypedValue),
 }
 
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
+pub enum ContractInterpretError {
+    #[error("failed typechecking input: {0}")]
+    TcError(#[from] crate::typechecker::TcError),
+    #[error("runtime failure while running the contract: {0}")]
+    InterpretError(#[from] crate::interpreter::InterpretError),
+}
+
+#[allow(dead_code)]
+impl ContractScript<TypecheckedStage> {
+    /// Interpret a typechecked contract script using the provided parameter and
+    /// storage. Parameter and storage are given as untyped `Value`s, as this
+    /// allows ensuring they satisfy the types expected by the script.
+    pub fn interpret(
+        &self,
+        ctx: &mut crate::context::Ctx,
+        parameter: Value,
+        storage: Value,
+    ) -> Result<(Vec<TypedValue>, TypedValue), ContractInterpretError> {
+        let in_ty = Type::new_pair(self.parameter.clone(), self.storage.clone());
+        let in_val = Value::new_pair(parameter, storage);
+        let tc_val = in_val.typecheck(ctx, &in_ty)?;
+        let mut stack = stk![tc_val];
+        self.code.interpret(ctx, &mut stack)?;
+        use TypedValue as V;
+        match stack.pop().expect("empty execution stack") {
+            V::Pair(bx, storage) => match *bx {
+                V::List(vec) => Ok((vec, *storage)),
+                v => panic!("expected `list operation`, got {:?}", v),
+            },
+            v => panic!("expected `pair 'a 'b`, got {:?}", v),
+        }
+    }
+}
+
 #[allow(dead_code)]
 pub fn interpret(
     ast: &TypecheckedAST,

contrib/mir/src/main.rs
@@ -25,6 +25,7 @@ mod tests {
     use crate::gas::Gas;
     use crate::interpreter;
     use crate::parser;
+    use crate::parser::parse_contract_script;
     use crate::stack::stk;
     use crate::typechecker;
 
@@ -211,6 +212,30 @@ mod tests {
         );
     }
 
+    #[test]
+    fn vote_contract() {
+        let mut ctx = Ctx {
+            amount: 5_000_000,
+            ..Ctx::default()
+        };
+        let interp_res = parse_contract_script(VOTE_SRC)
+            .unwrap()
+            .typecheck(&mut ctx)
+            .unwrap()
+            .interpret(
+                &mut ctx,
+                "foo".into(),
+                vec![Elt("bar", 0), Elt("baz", 0), Elt("foo", 0)].into(),
+            );
+        use TypedValue as TV;
+        match interp_res.unwrap() {
+            (_, TV::Map(m)) => {
+                assert_eq!(m.get(&TV::String("foo".to_owned())).unwrap(), &TV::Int(1))
+            }
+            _ => panic!("unexpected contract output"),
+        }
+    }
+
     const FIBONACCI_SRC: &str = "{ INT ; PUSH int 0 ; DUP 2 ; GT ;
            IF { DIP { PUSH int -1 ; ADD } ;
             PUSH int 1 ;
@@ -237,4 +262,22 @@ mod tests {
             LOOP { SWAP ; DUP 2 ; ADD ; DIP 2 { PUSH int -1 ; ADD } ; DUP 3 ; GT } ;
             DIP { DROP 2 } }
           { DIP { DROP } } }";
+
+    const VOTE_SRC: &str = "{
+          parameter (string %vote);
+          storage (map string int);
+          code {
+              AMOUNT;
+              PUSH mutez 5000000;
+              COMPARE; GT;
+              IF { { UNIT; FAILWITH } } {};
+              DUP; DIP { CDR; DUP }; CAR; DUP;
+              DIP {
+                  GET; { IF_NONE { { UNIT ; FAILWITH } } {} };
+                  PUSH int 1; ADD; SOME
+              };
+              UPDATE;
+              NIL operation; PAIR
+          }
+      }";
 }

contrib/mir/src/typechecker.rs
@@ -77,6 +77,33 @@ pub enum StacksNotEqualReason {
 pub struct TypesNotEqual(Type, Type);
 
 #[allow(dead_code)]
+impl ContractScript<ParsedStage> {
+    pub fn typecheck(
+        self,
+        ctx: &mut crate::context::Ctx,
+    ) -> Result<ContractScript<TypecheckedStage>, crate::typechecker::TcError> {
+        let ContractScript {
+            parameter, storage, ..
+        } = self;
+        let mut stack = stk![Type::new_pair(parameter.clone(), storage.clone())];
+        let code = self.code.typecheck(ctx, &mut stack)?;
+        ensure_stacks_eq(
+            ctx,
+            &stk![Type::new_pair(
+                Type::new_list(Type::Operation),
+                storage.clone()
+            )],
+            &stack,
+        )?;
+        Ok(ContractScript {
+            code,
+            parameter,
+            storage,
+        })
+    }
+}
+
+#[allow(dead_code)]
 pub fn typecheck(
     ast: ParsedAST,
     ctx: &mut Ctx,