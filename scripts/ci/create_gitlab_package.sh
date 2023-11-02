756a6aedaba5037080faeb7bf39407ca43480151
Killian Delarue <killian.delarue@nomadic-labs.com>
2023-11-02 17:19:15 +0100

    WIP

--- scripts/ci/create_gitlab_package.sh
+++ scripts/ci/create_gitlab_package.sh
@@ -83,7 +83,8 @@ git --version
 # Verify the placeholder %(describe:tags) is available
 git describe --tags
 # Create tarball
-git archive "${CI_COMMIT_TAG}" --format=tar | bzip2 > "${source_tarball}"
+stash="$(git stash create)"
+git archive "${stash}" --format=tar "$SOURCE_PATH" | bzip2 > "${source_tarball}"
 
 # Check tarball is valid
 tar -tjf "${source_tarball}" > /dev/null
