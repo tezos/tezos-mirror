mavryk-network/mavryk-protocol

rollup/node: make config for mode, purpose and operators more strict and robust

src/lib_smart_rollup_node/configuration.ml
@@ -76,33 +76,9 @@ type t = {
   cors : Resto_cohttp.Cors.t;
 }
 
-type error +=
-  | Missing_mode_operators of {mode : string; missing_operators : string list}
-  | Empty_operation_kinds_for_custom_mode
+type error += Empty_operation_kinds_for_custom_mode
 
 let () =
-  register_error_kind
-    ~id:"sc_rollup.node.missing_mode_operators"
-    ~title:"Missing operators for the chosen mode"
-    ~description:"Missing operators for the chosen mode."
-    ~pp:(fun ppf (mode, missing_operators) ->
-      Format.fprintf
-        ppf
-        "@[<hov>Missing operators %a for mode %s.@]"
-        (Format.pp_print_list
-           ~pp_sep:(fun ppf () -> Format.fprintf ppf ",@ ")
-           Format.pp_print_string)
-        missing_operators
-        mode)
-    `Permanent
-    Data_encoding.(
-      obj2 (req "mode" string) (req "missing_operators" (list string)))
-    (function
-      | Missing_mode_operators {mode; missing_operators} ->
-          Some (mode, missing_operators)
-      | _ -> None)
-    (fun (mode, missing_operators) ->
-      Missing_mode_operators {mode; missing_operators}) ;
   register_error_kind
     ~id:"sc_rollup_node.empty_operation_kinds_for_custom_mode"
     ~title:"Empty operation kinds for custom mode"
@@ -619,30 +595,8 @@ let operation_kinds_of_mode mode =
       let purposes = purposes_of_mode mode in
       List.map Purpose.operation_kind purposes |> List.flatten
 
-let check_mode config =
-  let open Result_syntax in
-  let check_purposes purposes =
-    let missing_operators =
-      List.filter (fun p -> not (Purpose.Map.mem p config.operators)) purposes
-    in
-    if missing_operators <> [] then
-      let mode = string_of_mode config.mode in
-      let missing_operators = List.map Purpose.to_string missing_operators in
-      tzfail (Missing_mode_operators {mode; missing_operators})
-    else return_unit
-  in
-  let narrow_purposes purposes =
-    let* () = check_purposes purposes in
-    let operators =
-      Purpose.Map.filter
-        (fun op_purpose _ -> List.mem ~equal:Stdlib.( = ) op_purpose purposes)
-        config.operators
-    in
-    return {config with operators}
-  in
-  match config.mode with
-  | Custom [] -> tzfail Empty_operation_kinds_for_custom_mode
-  | _ as mode -> narrow_purposes (purposes_of_mode mode)
+let check_custom_mode mode =
+  error_when (mode = Custom []) Empty_operation_kinds_for_custom_mode
 
 let can_inject mode (op_kind : Operation_kind.t) =
   let allowed_operations = operation_kinds_of_mode mode in
@@ -689,21 +643,18 @@ let load ~data_dir =
   config
 
 module Cli = struct
-  let make_operators operators =
-    let purposed_operators, default_operators =
-      List.partition_map
-        (function
-          | `Purpose p_operator -> Left p_operator
-          | `Default operator -> Right operator)
-        operators
-    in
-    let default_operator =
-      match default_operators with
-      | [] -> None
-      | [default_operator] -> Some default_operator
-      | _ -> Stdlib.failwith "Multiple default operators"
-    in
-    Purpose.make_map purposed_operators ~default:default_operator
+  let get_purpose_and_default_operator operator =
+    let open Result_syntax in
+    List.fold_left_e
+      (fun (purposed_operator, default_operator_opt) -> function
+        | `Purpose p_operator ->
+            return (p_operator :: purposed_operator, default_operator_opt)
+        | `Default operator ->
+            if Option.is_none default_operator_opt then
+              return (purposed_operator, Some operator)
+            else tzfail (error_of_fmt "Multiple default operators"))
+      ([], None)
+      operator
 
   let configuration_from_args ~rpc_addr ~rpc_port ~metrics_addr ~loser_mode
       ~reconnection_delay ~dal_node_endpoint ~dac_observer_endpoint ~dac_timeout
@@ -711,61 +662,68 @@ module Cli = struct
       ~sc_rollup_address ~boot_sector_file ~operators ~index_buffer_size
       ~irmin_cache_size ~log_kernel_debug ~no_degraded ~gc_frequency
       ~history_mode ~allowed_origins ~allowed_headers =
-    let operators = make_operators operators in
-    let config =
-      {
-        sc_rollup_address;
-        boot_sector_file;
-        operators;
-        rpc_addr = Option.value ~default:default_rpc_addr rpc_addr;
-        rpc_port = Option.value ~default:default_rpc_port rpc_port;
-        reconnection_delay =
-          Option.value ~default:default_reconnection_delay reconnection_delay;
-        dal_node_endpoint;
-        dac_observer_endpoint;
-        dac_timeout;
-        metrics_addr;
-        fee_parameters = Operation_kind.Map.empty;
-        mode;
-        loser_mode = Option.value ~default:Loser_mode.no_failures loser_mode;
-        batcher = default_batcher;
-        injector =
-          {
-            retention_period =
-              Option.value
-                ~default:default_injector.retention_period
-                injector_retention_period;
-            attempts =
-              Option.value ~default:default_injector.attempts injector_attempts;
-            injection_ttl =
-              Option.value ~default:default_injector.injection_ttl injection_ttl;
-          };
-        l1_blocks_cache_size = default_l1_blocks_cache_size;
-        l2_blocks_cache_size = default_l2_blocks_cache_size;
-        prefetch_blocks = None;
-        index_buffer_size;
-        irmin_cache_size;
-        log_kernel_debug;
-        no_degraded;
-        gc_parameters =
-          {
-            frequency_in_blocks =
-              Option.value
-                ~default:default_gc_parameters.frequency_in_blocks
-                gc_frequency;
-          };
-        history_mode = Option.value ~default:default_history_mode history_mode;
-        cors =
-          Resto_cohttp.Cors.
-            {
-              allowed_headers =
-                Option.value ~default:default.allowed_headers allowed_headers;
-              allowed_origins =
-                Option.value ~default:default.allowed_origins allowed_origins;
-            };
-      }
+    let open Result_syntax in
+    let* purposed_operator, default_operator =
+      get_purpose_and_default_operator operators
     in
-    check_mode config
+    let* operators =
+      Purpose.make_operator
+        ?default_operator
+        ~needed_purpose:(purposes_of_mode mode)
+        purposed_operator
+    in
+    let+ () = check_custom_mode mode in
+    {
+      sc_rollup_address;
+      boot_sector_file;
+      operators;
+      rpc_addr = Option.value ~default:default_rpc_addr rpc_addr;
+      rpc_port = Option.value ~default:default_rpc_port rpc_port;
+      reconnection_delay =
+        Option.value ~default:default_reconnection_delay reconnection_delay;
+      dal_node_endpoint;
+      dac_observer_endpoint;
+      dac_timeout;
+      metrics_addr;
+      fee_parameters = Operation_kind.Map.empty;
+      mode;
+      loser_mode = Option.value ~default:Loser_mode.no_failures loser_mode;
+      batcher = default_batcher;
+      injector =
+        {
+          retention_period =
+            Option.value
+              ~default:default_injector.retention_period
+              injector_retention_period;
+          attempts =
+            Option.value ~default:default_injector.attempts injector_attempts;
+          injection_ttl =
+            Option.value ~default:default_injector.injection_ttl injection_ttl;
+        };
+      l1_blocks_cache_size = default_l1_blocks_cache_size;
+      l2_blocks_cache_size = default_l2_blocks_cache_size;
+      prefetch_blocks = None;
+      index_buffer_size;
+      irmin_cache_size;
+      log_kernel_debug;
+      no_degraded;
+      gc_parameters =
+        {
+          frequency_in_blocks =
+            Option.value
+              ~default:default_gc_parameters.frequency_in_blocks
+              gc_frequency;
+        };
+      history_mode = Option.value ~default:default_history_mode history_mode;
+      cors =
+        Resto_cohttp.Cors.
+          {
+            allowed_headers =
+              Option.value ~default:default.allowed_headers allowed_headers;
+            allowed_origins =
+              Option.value ~default:default.allowed_origins allowed_origins;
+          };
+    }
 
   let patch_configuration_from_args configuration ~rpc_addr ~rpc_port
       ~metrics_addr ~loser_mode ~reconnection_delay ~dal_node_endpoint
@@ -774,16 +732,20 @@ module Cli = struct
       ~boot_sector_file ~operators ~index_buffer_size ~irmin_cache_size
       ~log_kernel_debug ~no_degraded ~gc_frequency ~history_mode
       ~allowed_origins ~allowed_headers =
-    let new_operators = make_operators operators in
-    (* Merge operators *)
-    let operators =
-      Purpose.Map.merge
-        (fun _purpose -> Option.either)
-        new_operators
+    let open Result_syntax in
+    let mode = Option.value ~default:configuration.mode mode in
+    let* () = check_custom_mode mode in
+    let* purposed_operator, default_operator =
+      get_purpose_and_default_operator operators
+    in
+    let* operators =
+      Purpose.replace_operator
+        ?default_operator
+        ~needed_purpose:(purposes_of_mode mode)
+        purposed_operator
         configuration.operators
     in
-
-    let configuration =
+    return
       {
         configuration with
         sc_rollup_address =
@@ -793,7 +755,7 @@ module Cli = struct
         boot_sector_file =
           Option.either boot_sector_file configuration.boot_sector_file;
         operators;
-        mode = Option.value ~default:configuration.mode mode;
+        mode;
         rpc_addr = Option.value ~default:configuration.rpc_addr rpc_addr;
         rpc_port = Option.value ~default:configuration.rpc_port rpc_port;
         dal_node_endpoint =
@@ -848,8 +810,6 @@ module Cli = struct
                   allowed_origins;
             };
       }
-    in
-    check_mode configuration
 
   let create_or_read_config ~data_dir ~rpc_addr ~rpc_port ~metrics_addr
       ~loser_mode ~reconnection_delay ~dal_node_endpoint ~dac_observer_endpoint

src/lib_smart_rollup_node/rollup_node_daemon.ml
@@ -335,12 +335,7 @@ let maybe_recover_bond ({node_ctxt; configuration; _} as state) =
     match operator with
     | None ->
         (* this case can't happen because the bailout mode needs a operator to start*)
-        tzfail
-          (Configuration.Missing_mode_operators
-             {
-               mode = Configuration.string_of_mode Bailout;
-               missing_operators = [Purpose.to_string Operating];
-             })
+        tzfail (Purpose.Missing_operator Operating)
     | Some operating_pkh -> (
         let module Plugin = (val state.plugin) in
         let* last_published_commitment =
@@ -471,7 +466,8 @@ let run ({node_ctxt; configuration; plugin; _} as state) =
   protect start ~on_error:(function
       | Rollup_node_errors.(
           ( Lost_game _ | Unparsable_boot_sector _ | Invalid_genesis_state _
-          | Operator_not_in_whitelist | Configuration.Missing_mode_operators _ ))
+          | Operator_not_in_whitelist | Purpose.Missing_operator _
+          | Purpose.Too_many_operator _ ))
         :: _ as e ->
           fatal_error_exit e
       | Rollup_node_errors.Could_not_open_preimage_file _ :: _ as e ->

tezos/tezos-mirror

src/lib_smart_rollup_node/configuration.ml
@@ -76,33 +76,9 @@ type t = {
   cors : Resto_cohttp.Cors.t;
 }
 
-type error +=
-  | Missing_mode_operators of {mode : string; missing_operators : string list}
-  | Empty_operation_kinds_for_custom_mode
+type error += Empty_operation_kinds_for_custom_mode
 
 let () =
-  register_error_kind
-    ~id:"sc_rollup.node.missing_mode_operators"
-    ~title:"Missing operators for the chosen mode"
-    ~description:"Missing operators for the chosen mode."
-    ~pp:(fun ppf (mode, missing_operators) ->
-      Format.fprintf
-        ppf
-        "@[<hov>Missing operators %a for mode %s.@]"
-        (Format.pp_print_list
-           ~pp_sep:(fun ppf () -> Format.fprintf ppf ",@ ")
-           Format.pp_print_string)
-        missing_operators
-        mode)
-    `Permanent
-    Data_encoding.(
-      obj2 (req "mode" string) (req "missing_operators" (list string)))
-    (function
-      | Missing_mode_operators {mode; missing_operators} ->
-          Some (mode, missing_operators)
-      | _ -> None)
-    (fun (mode, missing_operators) ->
-      Missing_mode_operators {mode; missing_operators}) ;
   register_error_kind
     ~id:"sc_rollup_node.empty_operation_kinds_for_custom_mode"
     ~title:"Empty operation kinds for custom mode"
@@ -619,30 +595,8 @@ let operation_kinds_of_mode mode =
       let purposes = purposes_of_mode mode in
       List.map Purpose.operation_kind purposes |> List.flatten
 
-let check_mode config =
-  let open Result_syntax in
-  let check_purposes purposes =
-    let missing_operators =
-      List.filter (fun p -> not (Purpose.Map.mem p config.operators)) purposes
-    in
-    if missing_operators <> [] then
-      let mode = string_of_mode config.mode in
-      let missing_operators = List.map Purpose.to_string missing_operators in
-      tzfail (Missing_mode_operators {mode; missing_operators})
-    else return_unit
-  in
-  let narrow_purposes purposes =
-    let* () = check_purposes purposes in
-    let operators =
-      Purpose.Map.filter
-        (fun op_purpose _ -> List.mem ~equal:Stdlib.( = ) op_purpose purposes)
-        config.operators
-    in
-    return {config with operators}
-  in
-  match config.mode with
-  | Custom [] -> tzfail Empty_operation_kinds_for_custom_mode
-  | _ as mode -> narrow_purposes (purposes_of_mode mode)
+let check_custom_mode mode =
+  error_when (mode = Custom []) Empty_operation_kinds_for_custom_mode
 
 let can_inject mode (op_kind : Operation_kind.t) =
   let allowed_operations = operation_kinds_of_mode mode in
@@ -689,21 +643,18 @@ let load ~data_dir =
   config
 
 module Cli = struct
-  let make_operators operators =
-    let purposed_operators, default_operators =
-      List.partition_map
-        (function
-          | `Purpose p_operator -> Left p_operator
-          | `Default operator -> Right operator)
-        operators
-    in
-    let default_operator =
-      match default_operators with
-      | [] -> None
-      | [default_operator] -> Some default_operator
-      | _ -> Stdlib.failwith "Multiple default operators"
-    in
-    Purpose.make_map purposed_operators ?default:default_operator
+  let get_purposed_and_default_operators operators =
+    let open Result_syntax in
+    List.fold_left_e
+      (fun (purposed_operator, default_operator_opt) -> function
+        | `Purpose p_operator ->
+            return (p_operator :: purposed_operator, default_operator_opt)
+        | `Default operator ->
+            if Option.is_none default_operator_opt then
+              return (purposed_operator, Some operator)
+            else tzfail (error_of_fmt "Multiple default operators"))
+      ([], None)
+      operators
 
   let configuration_from_args ~rpc_addr ~rpc_port ~metrics_addr ~loser_mode
       ~reconnection_delay ~dal_node_endpoint ~dac_observer_endpoint ~dac_timeout
@@ -711,61 +662,68 @@ module Cli = struct
       ~sc_rollup_address ~boot_sector_file ~operators ~index_buffer_size
       ~irmin_cache_size ~log_kernel_debug ~no_degraded ~gc_frequency
       ~history_mode ~allowed_origins ~allowed_headers =
-    let operators = make_operators operators in
-    let config =
-      {
-        sc_rollup_address;
-        boot_sector_file;
-        operators;
-        rpc_addr = Option.value ~default:default_rpc_addr rpc_addr;
-        rpc_port = Option.value ~default:default_rpc_port rpc_port;
-        reconnection_delay =
-          Option.value ~default:default_reconnection_delay reconnection_delay;
-        dal_node_endpoint;
-        dac_observer_endpoint;
-        dac_timeout;
-        metrics_addr;
-        fee_parameters = Operation_kind.Map.empty;
-        mode;
-        loser_mode = Option.value ~default:Loser_mode.no_failures loser_mode;
-        batcher = default_batcher;
-        injector =
-          {
-            retention_period =
-              Option.value
-                ~default:default_injector.retention_period
-                injector_retention_period;
-            attempts =
-              Option.value ~default:default_injector.attempts injector_attempts;
-            injection_ttl =
-              Option.value ~default:default_injector.injection_ttl injection_ttl;
-          };
-        l1_blocks_cache_size = default_l1_blocks_cache_size;
-        l2_blocks_cache_size = default_l2_blocks_cache_size;
-        prefetch_blocks = None;
-        index_buffer_size;
-        irmin_cache_size;
-        log_kernel_debug;
-        no_degraded;
-        gc_parameters =
-          {
-            frequency_in_blocks =
-              Option.value
-                ~default:default_gc_parameters.frequency_in_blocks
-                gc_frequency;
-          };
-        history_mode = Option.value ~default:default_history_mode history_mode;
-        cors =
-          Resto_cohttp.Cors.
-            {
-              allowed_headers =
-                Option.value ~default:default.allowed_headers allowed_headers;
-              allowed_origins =
-                Option.value ~default:default.allowed_origins allowed_origins;
-            };
-      }
+    let open Result_syntax in
+    let* purposed_operator, default_operator =
+      get_purposed_and_default_operators operators
     in
-    check_mode config
+    let* operators =
+      Purpose.make_operator
+        ?default_operator
+        ~needed_purposes:(purposes_of_mode mode)
+        purposed_operator
+    in
+    let+ () = check_custom_mode mode in
+    {
+      sc_rollup_address;
+      boot_sector_file;
+      operators;
+      rpc_addr = Option.value ~default:default_rpc_addr rpc_addr;
+      rpc_port = Option.value ~default:default_rpc_port rpc_port;
+      reconnection_delay =
+        Option.value ~default:default_reconnection_delay reconnection_delay;
+      dal_node_endpoint;
+      dac_observer_endpoint;
+      dac_timeout;
+      metrics_addr;
+      fee_parameters = Operation_kind.Map.empty;
+      mode;
+      loser_mode = Option.value ~default:Loser_mode.no_failures loser_mode;
+      batcher = default_batcher;
+      injector =
+        {
+          retention_period =
+            Option.value
+              ~default:default_injector.retention_period
+              injector_retention_period;
+          attempts =
+            Option.value ~default:default_injector.attempts injector_attempts;
+          injection_ttl =
+            Option.value ~default:default_injector.injection_ttl injection_ttl;
+        };
+      l1_blocks_cache_size = default_l1_blocks_cache_size;
+      l2_blocks_cache_size = default_l2_blocks_cache_size;
+      prefetch_blocks = None;
+      index_buffer_size;
+      irmin_cache_size;
+      log_kernel_debug;
+      no_degraded;
+      gc_parameters =
+        {
+          frequency_in_blocks =
+            Option.value
+              ~default:default_gc_parameters.frequency_in_blocks
+              gc_frequency;
+        };
+      history_mode = Option.value ~default:default_history_mode history_mode;
+      cors =
+        Resto_cohttp.Cors.
+          {
+            allowed_headers =
+              Option.value ~default:default.allowed_headers allowed_headers;
+            allowed_origins =
+              Option.value ~default:default.allowed_origins allowed_origins;
+          };
+    }
 
   let patch_configuration_from_args configuration ~rpc_addr ~rpc_port
       ~metrics_addr ~loser_mode ~reconnection_delay ~dal_node_endpoint
@@ -774,16 +732,20 @@ module Cli = struct
       ~boot_sector_file ~operators ~index_buffer_size ~irmin_cache_size
       ~log_kernel_debug ~no_degraded ~gc_frequency ~history_mode
       ~allowed_origins ~allowed_headers =
-    let new_operators = make_operators operators in
-    (* Merge operators *)
-    let operators =
-      Purpose.Map.merge
-        (fun _purpose -> Option.either)
-        new_operators
+    let open Result_syntax in
+    let mode = Option.value ~default:configuration.mode mode in
+    let* () = check_custom_mode mode in
+    let* purposed_operator, default_operator =
+      get_purposed_and_default_operators operators
+    in
+    let* operators =
+      Purpose.replace_operator
+        ?default_operator
+        ~needed_purposes:(purposes_of_mode mode)
+        purposed_operator
         configuration.operators
     in
-
-    let configuration =
+    return
       {
         configuration with
         sc_rollup_address =
@@ -793,7 +755,7 @@ module Cli = struct
         boot_sector_file =
           Option.either boot_sector_file configuration.boot_sector_file;
         operators;
-        mode = Option.value ~default:configuration.mode mode;
+        mode;
         rpc_addr = Option.value ~default:configuration.rpc_addr rpc_addr;
         rpc_port = Option.value ~default:configuration.rpc_port rpc_port;
         dal_node_endpoint =
@@ -848,8 +810,6 @@ module Cli = struct
                   allowed_origins;
             };
       }
-    in
-    check_mode configuration
 
   let create_or_read_config ~data_dir ~rpc_addr ~rpc_port ~metrics_addr
       ~loser_mode ~reconnection_delay ~dal_node_endpoint ~dac_observer_endpoint

src/lib_smart_rollup_node/rollup_node_daemon.ml
@@ -331,12 +331,7 @@ let maybe_recover_bond ({node_ctxt; configuration; _} as state) =
     match operator with
     | None ->
         (* this case can't happen because the bailout mode needs a operator to start*)
-        tzfail
-          (Configuration.Missing_mode_operators
-             {
-               mode = Configuration.string_of_mode Bailout;
-               missing_operators = [Purpose.to_string Operating];
-             })
+        tzfail (Purpose.Missing_operator Operating)
     | Some operating_pkh -> (
         let module Plugin = (val state.plugin) in
         let* last_published_commitment =
@@ -467,7 +462,8 @@ let run ({node_ctxt; configuration; plugin; _} as state) =
   protect start ~on_error:(function
       | Rollup_node_errors.(
           ( Lost_game _ | Unparsable_boot_sector _ | Invalid_genesis_state _
-          | Operator_not_in_whitelist | Configuration.Missing_mode_operators _ ))
+          | Operator_not_in_whitelist | Purpose.Missing_operator _
+          | Purpose.Too_many_operators _ ))
         :: _ as e ->
           fatal_error_exit e
       | Rollup_node_errors.Could_not_open_preimage_file _ :: _ as e ->