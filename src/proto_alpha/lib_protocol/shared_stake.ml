591ef389332d84cfe95b91b622384e3cb5070f6c
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-11-07 18:46:48 +0100

    Proto/Shared_stake/compute_reward_distrib: split the frozen part

--- src/proto_alpha/lib_protocol/shared_stake.ml
+++ src/proto_alpha/lib_protocol/shared_stake.ml
@@ -29,7 +29,11 @@ let share ~rounding ~full_staking_balance amount =
     let* stakers_part = Tez_repr.(amount -? baker_part) in
     return {baker_part; stakers_part}
 
-type reward_distrib = {to_frozen : Tez_repr.t; to_spendable : Tez_repr.t}
+type reward_distrib = {
+  to_baker_from_staking : Tez_repr.t;
+  to_stakers : Tez_repr.t;
+  to_spendable : Tez_repr.t;
+}
 
 (** Compute the reward distribution between frozen and spendable according to:
     - the [full_staking_balance] of the delegate composed of the [own_frozen]
@@ -44,25 +48,37 @@ Preconditions:
 
 Rounding favors:
   - staking over delegation
+  - baking over staking
 
 In case the delegate's stake is zero, everything goes to the spendable balance.
 *)
-let compute_reward_distrib ~full_staking_balance:_ ~stake
+let compute_reward_distrib ~full_staking_balance ~stake
     ~edge_of_baking_over_staking_billionth:_ ~(rewards : Tez_repr.t) =
   let open Result_syntax in
   let ({frozen; weighted_delegated} : Stake_repr.t) = stake in
   let* total_stake = Tez_repr.(frozen +? weighted_delegated) in
   if Tez_repr.(total_stake <= zero) then
-    return {to_spendable = rewards; to_frozen = Tez_repr.zero}
+    return
+      {
+        to_spendable = rewards;
+        to_baker_from_staking = Tez_repr.zero;
+        to_stakers = Tez_repr.zero;
+      }
   else
     let* to_spendable =
       Tez_repr.mul_ratio
+        ~rounding:`Down
         rewards
         ~num:(Tez_repr.to_mutez weighted_delegated)
         ~den:(Tez_repr.to_mutez total_stake)
     in
     let* to_frozen = Tez_repr.(rewards -? to_spendable) in
-    return {to_frozen; to_spendable}
+    let* {baker_part; stakers_part} =
+      share ~rounding:`Towards_baker ~full_staking_balance to_frozen
+    in
+    let to_baker_from_staking = baker_part in
+    let to_stakers = stakers_part in
+    return {to_baker_from_staking; to_stakers; to_spendable}
 
 let share ~rounding ctxt delegate amount =
   let open Lwt_result_syntax in
@@ -103,25 +119,22 @@ let pay_rewards ctxt ?active_stake ~source ~delegate rewards =
              (Signature.Public_key_hash.Map.find delegate stake_distrib)
              ~default:Stake_repr.zero)
   in
-  let* {to_frozen; to_spendable} =
+  let* {to_baker_from_staking; to_stakers; to_spendable} =
     compute_reward_distrib ctxt delegate active_stake rewards
-  in
-  let* {baker_part; stakers_part} =
-    share ~rounding:`Towards_stakers ctxt delegate to_frozen
   in
   let* ctxt, balance_updates_frozen_rewards_baker =
     Token.transfer
       ctxt
       source
       (`Frozen_deposits (Frozen_staker_repr.baker delegate))
-      baker_part
+      to_baker_from_staking
   in
   let* ctxt, balance_updates_frozen_rewards_stakers =
     Token.transfer
       ctxt
       source
       (`Frozen_deposits (Frozen_staker_repr.shared_between_stakers ~delegate))
-      stakers_part
+      to_stakers
   in
   let+ ctxt, balance_updates_spendable_rewards =
     Token.transfer
