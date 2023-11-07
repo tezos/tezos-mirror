268ff33f3f4c0da8c05979c53d78a626fa923916
Eugen Zalinescu <eugen.zalinescu@nomadic-labs.com>
2023-11-07 11:53:18 +0100

    DAL/GS: rename field

--- src/lib_gossipsub/tezos_gossipsub.ml
+++ src/lib_gossipsub/tezos_gossipsub.ml
@@ -179,7 +179,7 @@ module Make (C : AUTOMATON_CONFIG) :
   }
 
   (** [Connections] implements a bidirectional map from peers to connections and from
-      topics to peer.
+      topics to peers.
 
       Invariant:
         forall (c : Connections.t),
@@ -220,10 +220,11 @@ module Make (C : AUTOMATON_CONFIG) :
   end = struct
     type t = {
       peer_to_conn : connection Peer.Map.t;
-      topic_to_peer : Peer.Set.t Topic.Map.t;
+      topic_to_peers : Peer.Set.t Topic.Map.t;
     }
 
-    let empty = {peer_to_conn = Peer.Map.empty; topic_to_peer = Topic.Map.empty}
+    let empty =
+      {peer_to_conn = Peer.Map.empty; topic_to_peers = Topic.Map.empty}
 
     let bindings map = Peer.Map.bindings map.peer_to_conn
 
@@ -251,15 +252,15 @@ module Make (C : AUTOMATON_CONFIG) :
             {connection with topics = Topic.Set.add topic connection.topics}
           in
           let peer_to_conn = Peer.Map.add peer connection map.peer_to_conn in
-          let topic_to_peer =
+          let topic_to_peers =
             Topic.Map.update
               topic
               (function
                 | None -> Some (Peer.Set.singleton peer)
                 | Some peers_in_topic -> Some (Peer.Set.add peer peers_in_topic))
-              map.topic_to_peer
+              map.topic_to_peers
           in
-          `subscribed {peer_to_conn; topic_to_peer}
+          `subscribed {peer_to_conn; topic_to_peers}
 
     let unsubscribe peer topic map =
       match find peer map with
@@ -269,16 +270,16 @@ module Make (C : AUTOMATON_CONFIG) :
             {connection with topics = Topic.Set.remove topic connection.topics}
           in
           let peer_to_conn = Peer.Map.add peer connection map.peer_to_conn in
-          let topic_to_peer =
+          let topic_to_peers =
             Topic.Map.update
               topic
               (function
                 | None -> None
                 | Some peers_in_topic ->
                     Some (Peer.Set.remove peer peers_in_topic))
-              map.topic_to_peer
+              map.topic_to_peers
           in
-          `unsubscribed {peer_to_conn; topic_to_peer}
+          `unsubscribed {peer_to_conn; topic_to_peers}
 
     let remove peer map =
       match find peer map with
@@ -286,9 +287,9 @@ module Make (C : AUTOMATON_CONFIG) :
       | Some conn ->
           let topics = conn.topics in
           let peer_to_conn = Peer.Map.remove peer map.peer_to_conn in
-          let topic_to_peer =
+          let topic_to_peers =
             Topic.Set.fold
-              (fun topic topic_to_peer ->
+              (fun topic topic_to_peers ->
                 Topic.Map.update
                   topic
                   (function
@@ -297,18 +298,18 @@ module Make (C : AUTOMATON_CONFIG) :
                         None
                     | Some peers_in_topic ->
                         Some (Peer.Set.remove peer peers_in_topic))
-                  topic_to_peer)
+                  topic_to_peers)
               topics
-              map.topic_to_peer
+              map.topic_to_peers
           in
-          {peer_to_conn; topic_to_peer}
+          {peer_to_conn; topic_to_peers}
 
     let fold f map acc = Peer.Map.fold f map.peer_to_conn acc
 
     let iter f map = Peer.Map.iter f map.peer_to_conn
 
     let peers_in_topic topic map =
-      Topic.Map.find topic map.topic_to_peer
+      Topic.Map.find topic map.topic_to_peers
       |> Option.value ~default:Peer.Set.empty
   end
 
