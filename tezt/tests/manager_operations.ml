0ddfcb5aad41630c2e72d4eff91e2d0dbb5ed386
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-11-08 15:04:08 +0100

    Tezt/Manager_operations: simplify gas_to_execute_rest_noop

--- tezt/tests/manager_operations.ml
+++ tezt/tests/manager_operations.ml
@@ -861,8 +861,7 @@ module Deserialisation = struct
     return contract
 
   (* Gas to execute call to noop contract without deserialization *)
-  let gas_to_execute_rest_noop = function
-    | Protocol.Nairobi | Oxford | Alpha -> 1941
+  let gas_to_execute_rest_noop (_ : Protocol.t) = 1941
 
   let inject_call_with_bytes ?(source = Constant.bootstrap5) ?protocol ~contract
       ~size_kB ~gas_limit client =
