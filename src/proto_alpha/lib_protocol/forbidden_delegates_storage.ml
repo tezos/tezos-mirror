c382694a34a5d8af2690e7732903bf631ec82f38
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2023-11-14 10:27:01 +0100

    Proto/Forbidden_delegates: enhance criteria at cycle end

--- src/proto_alpha/lib_protocol/forbidden_delegates_storage.ml
+++ src/proto_alpha/lib_protocol/forbidden_delegates_storage.ml
@@ -64,32 +64,58 @@ let set_forbidden_delegates ctxt forbidden_delegates =
   in
   return ctxt
 
-let reset ctxt =
-  if
-    Signature.Public_key_hash.Set.is_empty
-      (Raw_context.Consensus.forbidden_delegates ctxt)
-  then Lwt.return ctxt
-  else set_forbidden_delegates ctxt Signature.Public_key_hash.Set.empty
-
 let update_at_cycle_end ctxt ~new_cycle =
   let open Lwt_result_syntax in
-  let*! ctxt = reset ctxt in
-  let* selection_for_new_cycle =
-    Stake_storage.get_selected_distribution ctxt new_cycle
-  in
-  List.fold_left_es
-    (fun ctxt (delegate, _stake) ->
-      let* current_deposits =
-        Delegate_storage.current_frozen_deposits ctxt delegate
-      in
-      if Tez_repr.(current_deposits = zero) then
-        (* If the delegate's current deposit remains at zero then we add it to
-           the forbidden set. *)
-        let*! ctxt = forbid ctxt delegate in
+  let forbidden_delegates = Raw_context.Consensus.forbidden_delegates ctxt in
+  if Signature.Public_key_hash.Set.is_empty forbidden_delegates then return ctxt
+  else
+    let* selection_for_new_cycle =
+      Stake_storage.get_selected_distribution_as_map ctxt new_cycle
+    in
+    let* forbidden_delegates =
+      Signature.Public_key_hash.Set.fold_es
+        (fun delegate acc ->
+          let* slash_history_opt =
+            Storage.Contract.Slashed_deposits.find ctxt (Implicit delegate)
+          in
+          let slash_history = Option.value slash_history_opt ~default:[] in
+          (* To be unforbidden in the new cycle, a delegate must not meet the
+             criterion used on denunciation anymore... *)
+          if should_forbid ~current_cycle:new_cycle slash_history then
+            return acc
+          else
+            (* ...and must have, either no rights (in which case there is no reason
+               to keep it forbidden), or have at least half the frozen deposits it
+               had when rights were computed (probably coming from autostaking). *)
+            let+ unforbid =
+              match
+                Signature.Public_key_hash.Map.find
+                  delegate
+                  selection_for_new_cycle
+              with
+              | None -> return_true
+              | Some {frozen; _} ->
+                  let+ current_deposits =
+                    Delegate_storage.current_frozen_deposits ctxt delegate
+                  in
+                  Tez_repr.(current_deposits >= div2 frozen)
+            in
+            if unforbid then
+              `Changed
+                (Signature.Public_key_hash.Set.remove
+                   delegate
+                   (match acc with
+                   | `Unchanged -> forbidden_delegates
+                   | `Changed forbidden_delegates -> forbidden_delegates))
+            else acc)
+        forbidden_delegates
+        `Unchanged
+    in
+    match forbidden_delegates with
+    | `Unchanged -> return ctxt
+    | `Changed forbidden_delegates ->
+        let*! ctxt = set_forbidden_delegates ctxt forbidden_delegates in
         return ctxt
-      else return ctxt)
-    ctxt
-    selection_for_new_cycle
 
 let init_for_genesis_and_oxford ctxt =
   Storage.Tenderbake.Forbidden_delegates.init
