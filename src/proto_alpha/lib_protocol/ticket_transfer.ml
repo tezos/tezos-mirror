c49dcee3bbe476515ea7c8fdcf68b356d9e06486
lin <linoshitani@gmail.com>
2023-11-19 17:43:12 +0900

    Proto: Introduce transfer_tickets_in_parameters

--- src/proto_alpha/lib_protocol/ticket_transfer.ml
+++ src/proto_alpha/lib_protocol/ticket_transfer.ml
@@ -108,3 +108,45 @@ let transfer_ticket ctxt ~sender ~dst ex_token qty =
     Ticket_balance_key.of_ex_token ctxt ~owner:dst ex_token
   in
   transfer_ticket_with_hashes ctxt ~sender_hash ~dst_hash qty
+
+let transfer_tickets_in_parameters ctxt parameter parameter_ty ~source ~dst =
+  let open Lwt_result_syntax in
+  let*? has_tickets, ctxt = Ticket_scanner.type_has_tickets ctxt parameter_ty in
+  let* tickets, ctxt =
+    Ticket_scanner.tickets_of_value
+      ~include_lazy:true
+      ctxt
+      has_tickets
+      parameter
+  in
+  let* ctxt, ticket_receipt, paid_storage_diff =
+    List.fold_left_es
+      (fun (ctxt, ticket_receipt_acc, paid_storage_diff_acc) ticket ->
+        let ticket_token, amount =
+          Ticket_scanner.ex_token_and_amount_of_ex_ticket ticket
+        in
+        let* ctxt, paid_storage_diff =
+          transfer_ticket ctxt ~sender:source ~dst ticket_token amount
+        in
+        let* ticket_token, ctxt =
+          Ticket_token_unparser.unparse ctxt ticket_token
+        in
+        let amount = Script_int.(to_zint (amount :> n num)) in
+        let ticket_receipt_item =
+          Ticket_receipt.
+            {
+              ticket_token;
+              updates = [{account = source; amount = Z.neg amount}];
+              (* We only handle negation from the [source] in this receipt.
+                 The addition to [dst] will be taken into account in the receipts
+                 generated from the subsequent contract execution. *)
+            }
+        in
+        return
+          ( ctxt,
+            ticket_receipt_item :: ticket_receipt_acc,
+            Z.add paid_storage_diff_acc paid_storage_diff ))
+      (ctxt, [], Z.zero)
+      tickets
+  in
+  return (ctxt, ticket_receipt, paid_storage_diff)
