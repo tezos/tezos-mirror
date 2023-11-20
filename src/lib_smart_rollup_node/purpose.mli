mavryk-network/mavryk-protocol

rollup/node: allow multiple batcher key

src/lib_smart_rollup_node/purpose.mli
@@ -10,7 +10,7 @@
     operations that they sign. *)
 type 'a purpose_kind =
   | Operating : Signature.public_key_hash purpose_kind
-  | Batching : Signature.public_key_hash purpose_kind
+  | Batching : Signature.public_key_hash list purpose_kind
   | Cementing : Signature.public_key_hash purpose_kind
   | Recovering : Signature.public_key_hash purpose_kind
   | Executing_outbox : Signature.public_key_hash purpose_kind

tezos/tezos-mirror

src/lib_smart_rollup_node/purpose.mli
@@ -10,7 +10,7 @@
     operations that they sign. *)
 type 'a t =
   | Operating : Signature.public_key_hash t
-  | Batching : Signature.public_key_hash t
+  | Batching : Signature.public_key_hash list t
   | Cementing : Signature.public_key_hash t
   | Recovering : Signature.public_key_hash t
   | Executing_outbox : Signature.public_key_hash t