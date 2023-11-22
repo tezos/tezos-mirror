8e2acaf0eedb3cedd45893099d6a7615c9830f71
Hugo Heuzard <hugo.heuzard@lambda-coins.com>
2023-11-22 13:41:44 +0100

    Clent-libs: expose bug with empty redirection

--- contrib/lib_kaitai_of_data_encoding/test/test_translation_of_objs.ml
+++ contrib/lib_kaitai_of_data_encoding/test/test_translation_of_objs.ml
@@ -215,3 +215,31 @@ let%expect_test "test nested object translation" =
   - id: replacement_protocol
     type: protocol_hash
   |}]
+
+let%expect_test "test object translation with empty fields" =
+  let open Data_encoding in
+  let encoding =
+    obj3
+      (req "cst1" (constant "cst1"))
+      (opt "cst2" (constant "cst2"))
+      (dft "cst3" (constant "cst3") ())
+  in
+  let s =
+    Kaitai_of_data_encoding.Translate.from_data_encoding
+      ~id:"nested_objects"
+      encoding
+  in
+  print_endline (Kaitai.Print.print s) ;
+  [%expect.unreachable]
+[@@expect.uncaught_exn {|
+  (* CR expect_test_collector: This test expectation appears to contain a backtrace.
+     This is strongly discouraged as backtraces are fragile.
+     Please change this test to not include a backtrace. *)
+
+  (Failure "redirect_if_many: empty list not supported")
+  Raised at Stdlib.failwith in file "stdlib.ml", line 29, characters 17-33
+  Called from Kaitai_of_data_encoding__Translate.seq_field_of_field in file "contrib/lib_kaitai_of_data_encoding/translate.ml", line 460, characters 8-130
+  Called from Kaitai_of_data_encoding__Translate.seq_field_of_data_encoding in file "contrib/lib_kaitai_of_data_encoding/translate.ml", line 266, characters 24-64
+  Called from Kaitai_of_data_encoding__Translate.from_data_encoding in file "contrib/lib_kaitai_of_data_encoding/translate.ml", line 705, characters 8-63
+  Called from Kaitai_of_data_encoding_test__Test_translation_of_objs.(fun) in file "contrib/lib_kaitai_of_data_encoding/test/test_translation_of_objs.ml", line 228, characters 4-98
+  Called from Expect_test_collector.Make.Instance_io.exec in file "collector/expect_test_collector.ml", line 234, characters 12-19 |}]
