OKEAMAH/Git

Client-libs: support for string_enum - regenerate

contrib/kaitai-struct-files/alpha__script__prim.ksy
@@ -1,0 +1,402 @@
+meta:
+  id: alpha__script__prim
+  endian: be
+enums:
+  alpha__michelson__v1__primitives:
+    0: parameter
+    1: storage
+    2: code
+    3:
+      id: false
+      doc: False
+    4:
+      id: elt
+      doc: Elt
+    5:
+      id: left
+      doc: Left
+    6:
+      id: none_
+      doc: None
+    7:
+      id: pair__
+      doc: Pair
+    8:
+      id: right
+      doc: Right
+    9:
+      id: some_
+      doc: Some
+    10:
+      id: true
+      doc: True
+    11:
+      id: unit_
+      doc: Unit
+    12:
+      id: pack
+      doc: PACK
+    13:
+      id: unpack
+      doc: UNPACK
+    14:
+      id: blake2b
+      doc: BLAKE2B
+    15:
+      id: sha256
+      doc: SHA256
+    16:
+      id: sha512
+      doc: SHA512
+    17:
+      id: abs
+      doc: ABS
+    18:
+      id: add
+      doc: ADD
+    19:
+      id: amount
+      doc: AMOUNT
+    20:
+      id: and
+      doc: AND
+    21:
+      id: balance
+      doc: BALANCE
+    22:
+      id: car
+      doc: CAR
+    23:
+      id: cdr
+      doc: CDR
+    24:
+      id: check_signature
+      doc: CHECK_SIGNATURE
+    25:
+      id: compare
+      doc: COMPARE
+    26:
+      id: concat
+      doc: CONCAT
+    27:
+      id: cons
+      doc: CONS
+    28:
+      id: create_account
+      doc: CREATE_ACCOUNT
+    29:
+      id: create_contract
+      doc: CREATE_CONTRACT
+    30:
+      id: implicit_account
+      doc: IMPLICIT_ACCOUNT
+    31:
+      id: dip
+      doc: DIP
+    32:
+      id: drop
+      doc: DROP
+    33:
+      id: dup
+      doc: DUP
+    34:
+      id: ediv
+      doc: EDIV
+    35:
+      id: empty_map
+      doc: EMPTY_MAP
+    36:
+      id: empty_set
+      doc: EMPTY_SET
+    37:
+      id: eq
+      doc: EQ
+    38:
+      id: exec
+      doc: EXEC
+    39:
+      id: failwith
+      doc: FAILWITH
+    40:
+      id: ge
+      doc: GE
+    41:
+      id: get
+      doc: GET
+    42:
+      id: gt
+      doc: GT
+    43:
+      id: hash_key
+      doc: HASH_KEY
+    44:
+      id: if
+      doc: IF
+    45:
+      id: if_cons
+      doc: IF_CONS
+    46:
+      id: if_left
+      doc: IF_LEFT
+    47:
+      id: if_none
+      doc: IF_NONE
+    48:
+      id: int_
+      doc: INT
+    49:
+      id: lambda_
+      doc: LAMBDA
+    50:
+      id: le
+      doc: LE
+    51:
+      id: left_
+      doc: LEFT
+    52:
+      id: loop
+      doc: LOOP
+    53:
+      id: lsl
+      doc: LSL
+    54:
+      id: lsr
+      doc: LSR
+    55:
+      id: lt
+      doc: LT
+    56:
+      id: map_
+      doc: MAP
+    57:
+      id: mem
+      doc: MEM
+    58:
+      id: mul
+      doc: MUL
+    59:
+      id: neg
+      doc: NEG
+    60:
+      id: neq
+      doc: NEQ
+    61:
+      id: nil
+      doc: NIL
+    62:
+      id: none
+      doc: NONE
+    63:
+      id: not
+      doc: NOT
+    64:
+      id: now
+      doc: NOW
+    65:
+      id: or_
+      doc: OR
+    66:
+      id: pair_
+      doc: PAIR
+    67:
+      id: push
+      doc: PUSH
+    68:
+      id: right_
+      doc: RIGHT
+    69:
+      id: size
+      doc: SIZE
+    70:
+      id: some
+      doc: SOME
+    71:
+      id: source
+      doc: SOURCE
+    72:
+      id: sender
+      doc: SENDER
+    73:
+      id: self
+      doc: SELF
+    74:
+      id: steps_to_quota
+      doc: STEPS_TO_QUOTA
+    75:
+      id: sub
+      doc: SUB
+    76:
+      id: swap
+      doc: SWAP
+    77:
+      id: transfer_tokens
+      doc: TRANSFER_TOKENS
+    78:
+      id: set_delegate
+      doc: SET_DELEGATE
+    79:
+      id: unit__
+      doc: UNIT
+    80:
+      id: update
+      doc: UPDATE
+    81:
+      id: xor
+      doc: XOR
+    82:
+      id: iter
+      doc: ITER
+    83:
+      id: loop_left
+      doc: LOOP_LEFT
+    84:
+      id: address_
+      doc: ADDRESS
+    85:
+      id: contract_
+      doc: CONTRACT
+    86:
+      id: isnat
+      doc: ISNAT
+    87:
+      id: cast
+      doc: CAST
+    88:
+      id: rename
+      doc: RENAME
+    89: bool
+    90: contract
+    91: int
+    92: key
+    93: key_hash
+    94: lambda
+    95: list
+    96: map
+    97: big_map
+    98: nat
+    99: option
+    100: or
+    101: pair
+    102: set
+    103: signature
+    104: string
+    105: bytes
+    106: mutez
+    107: timestamp
+    108: unit
+    109: operation
+    110: address
+    111:
+      id: slice
+      doc: SLICE
+    112:
+      id: dig
+      doc: DIG
+    113:
+      id: dug
+      doc: DUG
+    114:
+      id: empty_big_map
+      doc: EMPTY_BIG_MAP
+    115:
+      id: apply
+      doc: APPLY
+    116: chain_id
+    117:
+      id: chain_id_
+      doc: CHAIN_ID
+    118:
+      id: level
+      doc: LEVEL
+    119:
+      id: self_address
+      doc: SELF_ADDRESS
+    120: never
+    121:
+      id: never_
+      doc: NEVER
+    122:
+      id: unpair
+      doc: UNPAIR
+    123:
+      id: voting_power
+      doc: VOTING_POWER
+    124:
+      id: total_voting_power
+      doc: TOTAL_VOTING_POWER
+    125:
+      id: keccak
+      doc: KECCAK
+    126:
+      id: sha3
+      doc: SHA3
+    127:
+      id: pairing_check
+      doc: PAIRING_CHECK
+    128: bls12_381_g1
+    129: bls12_381_g2
+    130: bls12_381_fr
+    131: sapling_state
+    132: sapling_transaction_deprecated
+    133:
+      id: sapling_empty_state
+      doc: SAPLING_EMPTY_STATE
+    134:
+      id: sapling_verify_update
+      doc: SAPLING_VERIFY_UPDATE
+    135: ticket
+    136:
+      id: ticket_deprecated
+      doc: TICKET_DEPRECATED
+    137:
+      id: read_ticket
+      doc: READ_TICKET
+    138:
+      id: split_ticket
+      doc: SPLIT_TICKET
+    139:
+      id: join_tickets
+      doc: JOIN_TICKETS
+    140:
+      id: get_and_update
+      doc: GET_AND_UPDATE
+    141: chest
+    142: chest_key
+    143:
+      id: open_chest
+      doc: OPEN_CHEST
+    144:
+      id: view_
+      doc: VIEW
+    145: view
+    146: constant
+    147:
+      id: sub_mutez
+      doc: SUB_MUTEZ
+    148: tx_rollup_l2_address
+    149:
+      id: min_block_time
+      doc: MIN_BLOCK_TIME
+    150: sapling_transaction
+    151:
+      id: emit
+      doc: EMIT
+    152:
+      id: lambda_rec
+      doc: Lambda_rec
+    153:
+      id: lambda_rec_
+      doc: LAMBDA_REC
+    154:
+      id: ticket_
+      doc: TICKET
+    155:
+      id: bytes_
+      doc: BYTES
+    156:
+      id: nat_
+      doc: NAT
+seq:
+- id: alpha__michelson__v1__primitives
+  type: u1
+  enum: alpha__michelson__v1__primitives

contrib/kaitai-struct-files/id_010__ptgranad__script__prim.ksy
@@ -1,0 +1,366 @@
+meta:
+  id: id_010__ptgranad__script__prim
+  endian: be
+enums:
+  id_010__ptgranad__michelson__v1__primitives:
+    0: parameter
+    1: storage
+    2: code
+    3:
+      id: false
+      doc: False
+    4:
+      id: elt
+      doc: Elt
+    5:
+      id: left_
+      doc: Left
+    6:
+      id: none
+      doc: None
+    7:
+      id: pair__
+      doc: Pair
+    8:
+      id: right_
+      doc: Right
+    9:
+      id: some
+      doc: Some
+    10:
+      id: true
+      doc: True
+    11:
+      id: unit_
+      doc: Unit
+    12:
+      id: pack
+      doc: PACK
+    13:
+      id: unpack
+      doc: UNPACK
+    14:
+      id: blake2b
+      doc: BLAKE2B
+    15:
+      id: sha256
+      doc: SHA256
+    16:
+      id: sha512
+      doc: SHA512
+    17:
+      id: abs
+      doc: ABS
+    18:
+      id: add
+      doc: ADD
+    19:
+      id: amount
+      doc: AMOUNT
+    20:
+      id: and
+      doc: AND
+    21:
+      id: balance
+      doc: BALANCE
+    22:
+      id: car
+      doc: CAR
+    23:
+      id: cdr
+      doc: CDR
+    24:
+      id: check_signature
+      doc: CHECK_SIGNATURE
+    25:
+      id: compare
+      doc: COMPARE
+    26:
+      id: concat
+      doc: CONCAT
+    27:
+      id: cons
+      doc: CONS
+    28:
+      id: create_account
+      doc: CREATE_ACCOUNT
+    29:
+      id: create_contract
+      doc: CREATE_CONTRACT
+    30:
+      id: implicit_account
+      doc: IMPLICIT_ACCOUNT
+    31:
+      id: dip
+      doc: DIP
+    32:
+      id: drop
+      doc: DROP
+    33:
+      id: dup
+      doc: DUP
+    34:
+      id: ediv
+      doc: EDIV
+    35:
+      id: empty_map
+      doc: EMPTY_MAP
+    36:
+      id: empty_set
+      doc: EMPTY_SET
+    37:
+      id: eq
+      doc: EQ
+    38:
+      id: exec
+      doc: EXEC
+    39:
+      id: failwith
+      doc: FAILWITH
+    40:
+      id: ge
+      doc: GE
+    41:
+      id: get
+      doc: GET
+    42:
+      id: gt
+      doc: GT
+    43:
+      id: hash_key
+      doc: HASH_KEY
+    44:
+      id: if
+      doc: IF
+    45:
+      id: if_cons
+      doc: IF_CONS
+    46:
+      id: if_left
+      doc: IF_LEFT
+    47:
+      id: if_none
+      doc: IF_NONE
+    48:
+      id: int_
+      doc: INT
+    49:
+      id: lambda_
+      doc: LAMBDA
+    50:
+      id: le
+      doc: LE
+    51:
+      id: left
+      doc: LEFT
+    52:
+      id: loop
+      doc: LOOP
+    53:
+      id: lsl
+      doc: LSL
+    54:
+      id: lsr
+      doc: LSR
+    55:
+      id: lt
+      doc: LT
+    56:
+      id: map_
+      doc: MAP
+    57:
+      id: mem
+      doc: MEM
+    58:
+      id: mul
+      doc: MUL
+    59:
+      id: neg
+      doc: NEG
+    60:
+      id: neq
+      doc: NEQ
+    61:
+      id: nil
+      doc: NIL
+    62:
+      id: none_
+      doc: NONE
+    63:
+      id: not
+      doc: NOT
+    64:
+      id: now
+      doc: NOW
+    65:
+      id: or_
+      doc: OR
+    66:
+      id: pair_
+      doc: PAIR
+    67:
+      id: push
+      doc: PUSH
+    68:
+      id: right
+      doc: RIGHT
+    69:
+      id: size
+      doc: SIZE
+    70:
+      id: some_
+      doc: SOME
+    71:
+      id: source
+      doc: SOURCE
+    72:
+      id: sender
+      doc: SENDER
+    73:
+      id: self
+      doc: SELF
+    74:
+      id: steps_to_quota
+      doc: STEPS_TO_QUOTA
+    75:
+      id: sub
+      doc: SUB
+    76:
+      id: swap
+      doc: SWAP
+    77:
+      id: transfer_tokens
+      doc: TRANSFER_TOKENS
+    78:
+      id: set_delegate
+      doc: SET_DELEGATE
+    79:
+      id: unit__
+      doc: UNIT
+    80:
+      id: update
+      doc: UPDATE
+    81:
+      id: xor
+      doc: XOR
+    82:
+      id: iter
+      doc: ITER
+    83:
+      id: loop_left
+      doc: LOOP_LEFT
+    84:
+      id: address_
+      doc: ADDRESS
+    85:
+      id: contract_
+      doc: CONTRACT
+    86:
+      id: isnat
+      doc: ISNAT
+    87:
+      id: cast
+      doc: CAST
+    88:
+      id: rename
+      doc: RENAME
+    89: bool
+    90: contract
+    91: int
+    92: key
+    93: key_hash
+    94: lambda
+    95: list
+    96: map
+    97: big_map
+    98: nat
+    99: option
+    100: or
+    101: pair
+    102: set
+    103: signature
+    104: string
+    105: bytes
+    106: mutez
+    107: timestamp
+    108: unit
+    109: operation
+    110: address
+    111:
+      id: slice
+      doc: SLICE
+    112:
+      id: dig
+      doc: DIG
+    113:
+      id: dug
+      doc: DUG
+    114:
+      id: empty_big_map
+      doc: EMPTY_BIG_MAP
+    115:
+      id: apply
+      doc: APPLY
+    116: chain_id
+    117:
+      id: chain_id_
+      doc: CHAIN_ID
+    118:
+      id: level
+      doc: LEVEL
+    119:
+      id: self_address
+      doc: SELF_ADDRESS
+    120: never
+    121:
+      id: never_
+      doc: NEVER
+    122:
+      id: unpair
+      doc: UNPAIR
+    123:
+      id: voting_power
+      doc: VOTING_POWER
+    124:
+      id: total_voting_power
+      doc: TOTAL_VOTING_POWER
+    125:
+      id: keccak
+      doc: KECCAK
+    126:
+      id: sha3
+      doc: SHA3
+    127:
+      id: pairing_check
+      doc: PAIRING_CHECK
+    128: bls12_381_g1
+    129: bls12_381_g2
+    130: bls12_381_fr
+    131: sapling_state
+    132: sapling_transaction
+    133:
+      id: sapling_empty_state
+      doc: SAPLING_EMPTY_STATE
+    134:
+      id: sapling_verify_update
+      doc: SAPLING_VERIFY_UPDATE
+    135: ticket
+    136:
+      id: ticket_
+      doc: TICKET
+    137:
+      id: read_ticket
+      doc: READ_TICKET
+    138:
+      id: split_ticket
+      doc: SPLIT_TICKET
+    139:
+      id: join_tickets
+      doc: JOIN_TICKETS
+    140:
+      id: get_and_update
+      doc: GET_AND_UPDATE
+seq:
+- id: id_010__ptgranad__michelson__v1__primitives
+  type: u1
+  enum: id_010__ptgranad__michelson__v1__primitives

tezos/tezos-mirror

contrib/kaitai-struct-files/alpha__script__prim.ksy
@@ -1,0 +1,402 @@
+meta:
+  id: alpha__script__prim
+  endian: be
+enums:
+  alpha__michelson__v1__primitives:
+    0: parameter
+    1: storage
+    2: code
+    3:
+      id: false
+      doc: False
+    4:
+      id: elt
+      doc: Elt
+    5:
+      id: left
+      doc: Left
+    6:
+      id: none_0
+      doc: None
+    7:
+      id: pair_1
+      doc: Pair
+    8:
+      id: right
+      doc: Right
+    9:
+      id: some_0
+      doc: Some
+    10:
+      id: true
+      doc: True
+    11:
+      id: unit_0
+      doc: Unit
+    12:
+      id: pack
+      doc: PACK
+    13:
+      id: unpack
+      doc: UNPACK
+    14:
+      id: blake2b
+      doc: BLAKE2B
+    15:
+      id: sha256
+      doc: SHA256
+    16:
+      id: sha512
+      doc: SHA512
+    17:
+      id: abs
+      doc: ABS
+    18:
+      id: add
+      doc: ADD
+    19:
+      id: amount
+      doc: AMOUNT
+    20:
+      id: and
+      doc: AND
+    21:
+      id: balance
+      doc: BALANCE
+    22:
+      id: car
+      doc: CAR
+    23:
+      id: cdr
+      doc: CDR
+    24:
+      id: check_signature
+      doc: CHECK_SIGNATURE
+    25:
+      id: compare
+      doc: COMPARE
+    26:
+      id: concat
+      doc: CONCAT
+    27:
+      id: cons
+      doc: CONS
+    28:
+      id: create_account
+      doc: CREATE_ACCOUNT
+    29:
+      id: create_contract
+      doc: CREATE_CONTRACT
+    30:
+      id: implicit_account
+      doc: IMPLICIT_ACCOUNT
+    31:
+      id: dip
+      doc: DIP
+    32:
+      id: drop
+      doc: DROP
+    33:
+      id: dup
+      doc: DUP
+    34:
+      id: ediv
+      doc: EDIV
+    35:
+      id: empty_map
+      doc: EMPTY_MAP
+    36:
+      id: empty_set
+      doc: EMPTY_SET
+    37:
+      id: eq
+      doc: EQ
+    38:
+      id: exec
+      doc: EXEC
+    39:
+      id: failwith
+      doc: FAILWITH
+    40:
+      id: ge
+      doc: GE
+    41:
+      id: get
+      doc: GET
+    42:
+      id: gt
+      doc: GT
+    43:
+      id: hash_key
+      doc: HASH_KEY
+    44:
+      id: if
+      doc: IF
+    45:
+      id: if_cons
+      doc: IF_CONS
+    46:
+      id: if_left
+      doc: IF_LEFT
+    47:
+      id: if_none
+      doc: IF_NONE
+    48:
+      id: int_0
+      doc: INT
+    49:
+      id: lambda_0
+      doc: LAMBDA
+    50:
+      id: le
+      doc: LE
+    51:
+      id: left_0
+      doc: LEFT
+    52:
+      id: loop
+      doc: LOOP
+    53:
+      id: lsl
+      doc: LSL
+    54:
+      id: lsr
+      doc: LSR
+    55:
+      id: lt
+      doc: LT
+    56:
+      id: map_0
+      doc: MAP
+    57:
+      id: mem
+      doc: MEM
+    58:
+      id: mul
+      doc: MUL
+    59:
+      id: neg
+      doc: NEG
+    60:
+      id: neq
+      doc: NEQ
+    61:
+      id: nil
+      doc: NIL
+    62:
+      id: none
+      doc: NONE
+    63:
+      id: not
+      doc: NOT
+    64:
+      id: now
+      doc: NOW
+    65:
+      id: or_0
+      doc: OR
+    66:
+      id: pair_0
+      doc: PAIR
+    67:
+      id: push
+      doc: PUSH
+    68:
+      id: right_0
+      doc: RIGHT
+    69:
+      id: size
+      doc: SIZE
+    70:
+      id: some
+      doc: SOME
+    71:
+      id: source
+      doc: SOURCE
+    72:
+      id: sender
+      doc: SENDER
+    73:
+      id: self
+      doc: SELF
+    74:
+      id: steps_to_quota
+      doc: STEPS_TO_QUOTA
+    75:
+      id: sub
+      doc: SUB
+    76:
+      id: swap
+      doc: SWAP
+    77:
+      id: transfer_tokens
+      doc: TRANSFER_TOKENS
+    78:
+      id: set_delegate
+      doc: SET_DELEGATE
+    79:
+      id: unit_1
+      doc: UNIT
+    80:
+      id: update
+      doc: UPDATE
+    81:
+      id: xor
+      doc: XOR
+    82:
+      id: iter
+      doc: ITER
+    83:
+      id: loop_left
+      doc: LOOP_LEFT
+    84:
+      id: address_0
+      doc: ADDRESS
+    85:
+      id: contract_0
+      doc: CONTRACT
+    86:
+      id: isnat
+      doc: ISNAT
+    87:
+      id: cast
+      doc: CAST
+    88:
+      id: rename
+      doc: RENAME
+    89: bool
+    90: contract
+    91: int
+    92: key
+    93: key_hash
+    94: lambda
+    95: list
+    96: map
+    97: big_map
+    98: nat
+    99: option
+    100: or
+    101: pair
+    102: set
+    103: signature
+    104: string
+    105: bytes
+    106: mutez
+    107: timestamp
+    108: unit
+    109: operation
+    110: address
+    111:
+      id: slice
+      doc: SLICE
+    112:
+      id: dig
+      doc: DIG
+    113:
+      id: dug
+      doc: DUG
+    114:
+      id: empty_big_map
+      doc: EMPTY_BIG_MAP
+    115:
+      id: apply
+      doc: APPLY
+    116: chain_id
+    117:
+      id: chain_id_0
+      doc: CHAIN_ID
+    118:
+      id: level
+      doc: LEVEL
+    119:
+      id: self_address
+      doc: SELF_ADDRESS
+    120: never
+    121:
+      id: never_0
+      doc: NEVER
+    122:
+      id: unpair
+      doc: UNPAIR
+    123:
+      id: voting_power
+      doc: VOTING_POWER
+    124:
+      id: total_voting_power
+      doc: TOTAL_VOTING_POWER
+    125:
+      id: keccak
+      doc: KECCAK
+    126:
+      id: sha3
+      doc: SHA3
+    127:
+      id: pairing_check
+      doc: PAIRING_CHECK
+    128: bls12_381_g1
+    129: bls12_381_g2
+    130: bls12_381_fr
+    131: sapling_state
+    132: sapling_transaction_deprecated
+    133:
+      id: sapling_empty_state
+      doc: SAPLING_EMPTY_STATE
+    134:
+      id: sapling_verify_update
+      doc: SAPLING_VERIFY_UPDATE
+    135: ticket
+    136:
+      id: ticket_deprecated
+      doc: TICKET_DEPRECATED
+    137:
+      id: read_ticket
+      doc: READ_TICKET
+    138:
+      id: split_ticket
+      doc: SPLIT_TICKET
+    139:
+      id: join_tickets
+      doc: JOIN_TICKETS
+    140:
+      id: get_and_update
+      doc: GET_AND_UPDATE
+    141: chest
+    142: chest_key
+    143:
+      id: open_chest
+      doc: OPEN_CHEST
+    144:
+      id: view_0
+      doc: VIEW
+    145: view
+    146: constant
+    147:
+      id: sub_mutez
+      doc: SUB_MUTEZ
+    148: tx_rollup_l2_address
+    149:
+      id: min_block_time
+      doc: MIN_BLOCK_TIME
+    150: sapling_transaction
+    151:
+      id: emit
+      doc: EMIT
+    152:
+      id: lambda_rec
+      doc: Lambda_rec
+    153:
+      id: lambda_rec_0
+      doc: LAMBDA_REC
+    154:
+      id: ticket_0
+      doc: TICKET
+    155:
+      id: bytes_0
+      doc: BYTES
+    156:
+      id: nat_0
+      doc: NAT
+seq:
+- id: alpha__michelson__v1__primitives
+  type: u1
+  enum: alpha__michelson__v1__primitives

contrib/kaitai-struct-files/id_010__ptgranad__script__prim.ksy
@@ -1,0 +1,366 @@
+meta:
+  id: id_010__ptgranad__script__prim
+  endian: be
+enums:
+  id_010__ptgranad__michelson__v1__primitives:
+    0: parameter
+    1: storage
+    2: code
+    3:
+      id: false
+      doc: False
+    4:
+      id: elt
+      doc: Elt
+    5:
+      id: left_0
+      doc: Left
+    6:
+      id: none
+      doc: None
+    7:
+      id: pair_1
+      doc: Pair
+    8:
+      id: right_0
+      doc: Right
+    9:
+      id: some
+      doc: Some
+    10:
+      id: true
+      doc: True
+    11:
+      id: unit_0
+      doc: Unit
+    12:
+      id: pack
+      doc: PACK
+    13:
+      id: unpack
+      doc: UNPACK
+    14:
+      id: blake2b
+      doc: BLAKE2B
+    15:
+      id: sha256
+      doc: SHA256
+    16:
+      id: sha512
+      doc: SHA512
+    17:
+      id: abs
+      doc: ABS
+    18:
+      id: add
+      doc: ADD
+    19:
+      id: amount
+      doc: AMOUNT
+    20:
+      id: and
+      doc: AND
+    21:
+      id: balance
+      doc: BALANCE
+    22:
+      id: car
+      doc: CAR
+    23:
+      id: cdr
+      doc: CDR
+    24:
+      id: check_signature
+      doc: CHECK_SIGNATURE
+    25:
+      id: compare
+      doc: COMPARE
+    26:
+      id: concat
+      doc: CONCAT
+    27:
+      id: cons
+      doc: CONS
+    28:
+      id: create_account
+      doc: CREATE_ACCOUNT
+    29:
+      id: create_contract
+      doc: CREATE_CONTRACT
+    30:
+      id: implicit_account
+      doc: IMPLICIT_ACCOUNT
+    31:
+      id: dip
+      doc: DIP
+    32:
+      id: drop
+      doc: DROP
+    33:
+      id: dup
+      doc: DUP
+    34:
+      id: ediv
+      doc: EDIV
+    35:
+      id: empty_map
+      doc: EMPTY_MAP
+    36:
+      id: empty_set
+      doc: EMPTY_SET
+    37:
+      id: eq
+      doc: EQ
+    38:
+      id: exec
+      doc: EXEC
+    39:
+      id: failwith
+      doc: FAILWITH
+    40:
+      id: ge
+      doc: GE
+    41:
+      id: get
+      doc: GET
+    42:
+      id: gt
+      doc: GT
+    43:
+      id: hash_key
+      doc: HASH_KEY
+    44:
+      id: if
+      doc: IF
+    45:
+      id: if_cons
+      doc: IF_CONS
+    46:
+      id: if_left
+      doc: IF_LEFT
+    47:
+      id: if_none
+      doc: IF_NONE
+    48:
+      id: int_0
+      doc: INT
+    49:
+      id: lambda_0
+      doc: LAMBDA
+    50:
+      id: le
+      doc: LE
+    51:
+      id: left
+      doc: LEFT
+    52:
+      id: loop
+      doc: LOOP
+    53:
+      id: lsl
+      doc: LSL
+    54:
+      id: lsr
+      doc: LSR
+    55:
+      id: lt
+      doc: LT
+    56:
+      id: map_0
+      doc: MAP
+    57:
+      id: mem
+      doc: MEM
+    58:
+      id: mul
+      doc: MUL
+    59:
+      id: neg
+      doc: NEG
+    60:
+      id: neq
+      doc: NEQ
+    61:
+      id: nil
+      doc: NIL
+    62:
+      id: none_0
+      doc: NONE
+    63:
+      id: not
+      doc: NOT
+    64:
+      id: now
+      doc: NOW
+    65:
+      id: or_0
+      doc: OR
+    66:
+      id: pair_0
+      doc: PAIR
+    67:
+      id: push
+      doc: PUSH
+    68:
+      id: right
+      doc: RIGHT
+    69:
+      id: size
+      doc: SIZE
+    70:
+      id: some_0
+      doc: SOME
+    71:
+      id: source
+      doc: SOURCE
+    72:
+      id: sender
+      doc: SENDER
+    73:
+      id: self
+      doc: SELF
+    74:
+      id: steps_to_quota
+      doc: STEPS_TO_QUOTA
+    75:
+      id: sub
+      doc: SUB
+    76:
+      id: swap
+      doc: SWAP
+    77:
+      id: transfer_tokens
+      doc: TRANSFER_TOKENS
+    78:
+      id: set_delegate
+      doc: SET_DELEGATE
+    79:
+      id: unit_1
+      doc: UNIT
+    80:
+      id: update
+      doc: UPDATE
+    81:
+      id: xor
+      doc: XOR
+    82:
+      id: iter
+      doc: ITER
+    83:
+      id: loop_left
+      doc: LOOP_LEFT
+    84:
+      id: address_0
+      doc: ADDRESS
+    85:
+      id: contract_0
+      doc: CONTRACT
+    86:
+      id: isnat
+      doc: ISNAT
+    87:
+      id: cast
+      doc: CAST
+    88:
+      id: rename
+      doc: RENAME
+    89: bool
+    90: contract
+    91: int
+    92: key
+    93: key_hash
+    94: lambda
+    95: list
+    96: map
+    97: big_map
+    98: nat
+    99: option
+    100: or
+    101: pair
+    102: set
+    103: signature
+    104: string
+    105: bytes
+    106: mutez
+    107: timestamp
+    108: unit
+    109: operation
+    110: address
+    111:
+      id: slice
+      doc: SLICE
+    112:
+      id: dig
+      doc: DIG
+    113:
+      id: dug
+      doc: DUG
+    114:
+      id: empty_big_map
+      doc: EMPTY_BIG_MAP
+    115:
+      id: apply
+      doc: APPLY
+    116: chain_id
+    117:
+      id: chain_id_0
+      doc: CHAIN_ID
+    118:
+      id: level
+      doc: LEVEL
+    119:
+      id: self_address
+      doc: SELF_ADDRESS
+    120: never
+    121:
+      id: never_0
+      doc: NEVER
+    122:
+      id: unpair
+      doc: UNPAIR
+    123:
+      id: voting_power
+      doc: VOTING_POWER
+    124:
+      id: total_voting_power
+      doc: TOTAL_VOTING_POWER
+    125:
+      id: keccak
+      doc: KECCAK
+    126:
+      id: sha3
+      doc: SHA3
+    127:
+      id: pairing_check
+      doc: PAIRING_CHECK
+    128: bls12_381_g1
+    129: bls12_381_g2
+    130: bls12_381_fr
+    131: sapling_state
+    132: sapling_transaction
+    133:
+      id: sapling_empty_state
+      doc: SAPLING_EMPTY_STATE
+    134:
+      id: sapling_verify_update
+      doc: SAPLING_VERIFY_UPDATE
+    135: ticket
+    136:
+      id: ticket_0
+      doc: TICKET
+    137:
+      id: read_ticket
+      doc: READ_TICKET
+    138:
+      id: split_ticket
+      doc: SPLIT_TICKET
+    139:
+      id: join_tickets
+      doc: JOIN_TICKETS
+    140:
+      id: get_and_update
+      doc: GET_AND_UPDATE
+seq:
+- id: id_010__ptgranad__michelson__v1__primitives
+  type: u1
+  enum: id_010__ptgranad__michelson__v1__primitives