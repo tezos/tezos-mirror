9809fc35bb011459d8d0e5ad17ef907f45ad496f
Mathias Bourgoin <mathias.bourgoin@nomadic-labs.com>
2023-11-30 16:27:26 +0100

    rollup/node: introduce automatic handling of pvm_contexts
    This commit introduces a (currently unused) module that automatically
    dispatches context calls to (upcoming) pvm_plugin contexts.

--- src/lib_smart_rollup_node/context.ml
+++ src/lib_smart_rollup_node/context.ml
@@ -23,4 +23,196 @@
 (*                                                                           *)
 (*****************************************************************************)
 
+let err_implementation_mismatch ~expected ~got =
+  Format.kasprintf
+    invalid_arg
+    "Context implementation mismatch: expecting %s, got %s"
+    expected
+    got
+
+open Context_sigs
+
+module Context = struct
+  type ('repo, 'tree) pvm_context_impl =
+    (module Context_sigs.S with type repo = 'repo and type tree = 'tree)
+
+  let equiv (a, b) (c, d) = (Equality_witness.eq a c, Equality_witness.eq b d)
+
+  type hash = Smart_rollup_context_hash.t
+
+  type 'a t =
+    | Context : {
+        index : ('a, 'repo) index;
+        pvm_context_impl : ('repo, 'tree) pvm_context_impl;
+        impl_name : string;
+        tree : 'tree;
+        equality_witness : ('repo, 'tree) equality_witness;
+      }
+        -> 'a t
+
+  type ro = [`Read] t
+
+  type rw = [`Read | `Write] t
+
+  let make ~index ~tree ~pvm_context_impl ~equality_witness ~impl_name =
+    Context {index; tree; pvm_context_impl; equality_witness; impl_name}
+
+  let load :
+      type tree repo.
+      (repo, tree) pvm_context_impl ->
+      cache_size:int ->
+      'a Store_sigs.mode ->
+      string ->
+      'a t tzresult Lwt.t =
+   fun (module Pvm_Context_Impl) ~cache_size mode path ->
+    let open Lwt_result_syntax in
+    let* index = Pvm_Context_Impl.load ~cache_size mode path in
+    let equality_witness = Pvm_Context_Impl.equality_witness in
+    let impl_name = Pvm_Context_Impl.impl_name in
+    return
+    @@ make
+         ~index
+         ~tree:(Pvm_Context_Impl.PVMState.empty ())
+         ~pvm_context_impl:(module Pvm_Context_Impl)
+         ~equality_witness
+         ~impl_name
+
+  let index c = c
+
+  let close (type a)
+      (Context {pvm_context_impl = (module Pvm_Context_Impl); index; _} : a t) :
+      unit Lwt.t =
+    Pvm_Context_Impl.close index
+
+  let readonly (type a)
+      (Context ({pvm_context_impl = (module Pvm_Context_Impl); index; _} as o) :
+        a t) : ro =
+    Context {o with index = Pvm_Context_Impl.readonly index}
+
+  let checkout (type a)
+      (Context ({pvm_context_impl = (module Pvm_Context_Impl); index; _} as o) :
+        a t) hash : a t option Lwt.t =
+    let open Lwt_syntax in
+    let+ ctx = Pvm_Context_Impl.checkout index hash in
+    match ctx with
+    | None -> None
+    | Some {index; tree} -> Some (Context {o with index; tree})
+
+  let empty (type a)
+      (Context ({pvm_context_impl = (module Pvm_Context_Impl); index; _} as o) :
+        a t) : a t =
+    let {index; tree} = Pvm_Context_Impl.empty index in
+    Context {o with index; tree}
+
+  let commit ?message
+      (Context {pvm_context_impl = (module Pvm_Context_Impl); index; tree; _} :
+        [> `Write] t) =
+    Pvm_Context_Impl.commit ?message {index; tree}
+
+  let is_gc_finished
+      (Context {pvm_context_impl = (module Pvm_Context_Impl); index; _} :
+        [> `Write] t) =
+    Pvm_Context_Impl.is_gc_finished index
+
+  let gc
+      (Context {pvm_context_impl = (module Pvm_Context_Impl); index; _} :
+        [> `Write] t) ?callback hash =
+    Pvm_Context_Impl.gc index ?callback hash
+
+  let wait_gc_completion
+      (Context {pvm_context_impl = (module Pvm_Context_Impl); index; _} :
+        [> `Write] t) =
+    Pvm_Context_Impl.wait_gc_completion index
+
+  type pvmstate =
+    | PVMState : {
+        pvm_context_impl : ('repo, 'tree) pvm_context_impl;
+        impl_name : string;
+        pvmstate : 'tree;
+        equality_witness : ('repo, 'tree) equality_witness;
+      }
+        -> pvmstate
+
+  let make_pvmstate ~pvm_context_impl ~equality_witness ~impl_name ~pvmstate =
+    PVMState {pvm_context_impl; impl_name; pvmstate; equality_witness}
+
+  (** State of the PVM that this rollup node deals with *)
+  module PVMState = struct
+    type value = pvmstate
+
+    let empty : type a. a t -> value =
+     fun (Context
+           {
+             pvm_context_impl = (module Pvm_Context_Impl);
+             equality_witness;
+             impl_name;
+             _;
+           }) ->
+      make_pvmstate
+        ~pvm_context_impl:(module Pvm_Context_Impl)
+        ~equality_witness
+        ~pvmstate:(Pvm_Context_Impl.PVMState.empty ())
+        ~impl_name
+
+    let find : type a. a t -> value option Lwt.t =
+     fun (Context
+           {
+             pvm_context_impl = (module Pvm_Context_Impl);
+             index;
+             tree;
+             equality_witness;
+             impl_name;
+             _;
+           }) ->
+      let open Lwt_syntax in
+      let+ pvmstate = Pvm_Context_Impl.PVMState.find {index; tree} in
+      match pvmstate with
+      | None -> None
+      | Some pvmstate ->
+          Some
+            (make_pvmstate
+               ~pvm_context_impl:(module Pvm_Context_Impl)
+               ~equality_witness
+               ~pvmstate
+               ~impl_name)
+
+    let lookup : value -> string list -> bytes option Lwt.t =
+     fun (PVMState {pvm_context_impl = (module Pvm_Context_Impl); pvmstate; _})
+         path ->
+      Pvm_Context_Impl.PVMState.lookup pvmstate path
+
+    let set : type a. a t -> value -> a t Lwt.t =
+     fun (Context
+           ({pvm_context_impl = (module Pvm_Context_Impl); index; tree; _} as
+           o1))
+         (PVMState o2) ->
+      let open Lwt_syntax in
+      match equiv o1.equality_witness o2.equality_witness with
+      | Some Refl, Some Refl ->
+          let+ ctxt = Pvm_Context_Impl.PVMState.set {index; tree} o2.pvmstate in
+          Context {o1 with index = ctxt.index; tree = ctxt.tree}
+      | _ ->
+          err_implementation_mismatch ~expected:o1.impl_name ~got:o2.impl_name
+  end
+
+  module Internal_for_tests = struct
+    let get_a_tree : type a. a t -> string -> pvmstate Lwt.t =
+     fun (Context
+           {
+             pvm_context_impl = (module Pvm_Context_Impl);
+             equality_witness;
+             impl_name;
+             _;
+           })
+         key ->
+      let open Lwt_syntax in
+      let+ tree = Pvm_Context_Impl.Internal_for_tests.get_a_tree key in
+      make_pvmstate
+        ~pvm_context_impl:(module Pvm_Context_Impl)
+        ~equality_witness
+        ~impl_name
+        ~pvmstate:tree
+  end
+end
+
 include Irmin_context
--- src/lib_smart_rollup_node/context.mli
+++ src/lib_smart_rollup_node/context.mli
@@ -22,5 +22,142 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
+(** This module is largely inspired from {!module:Tezos_protocol_environment.Environement_context} *)
+open Store_sigs
+
+open Context_sigs
+
+(** This module dipatches context calls to contexts/pvm_states
+    corresponding to the used pvm *)
+module Context : sig
+  (** See {!module:Tezos_protocol_environment.Environement_context.ops} *)
+  type ('repo, 'tree) pvm_context_impl =
+    (module Context_sigs.S with type repo = 'repo and type tree = 'tree)
+
+  (* Context existential that embeds the context_module embedded
+     associated to pvm protocol_plugins *)
+
+  (** See {!module:Tezos_protocol_environment.Environement_context.t} *)
+  type 'a t = private
+    | Context : {
+        index : ('a, 'repo) index;
+        pvm_context_impl : ('repo, 'tree) pvm_context_impl;
+        impl_name : string;
+        tree : 'tree;
+        equality_witness : ('repo, 'tree) equality_witness;
+      }
+        -> 'a t
+
+  (** Read/write context {!t}. *)
+  type rw = [`Read | `Write] t
+
+  (** Read-only context {!t}. *)
+  type ro = [`Read] t
+
+  val make :
+    index:('a, 'b) index ->
+    tree:'c ->
+    pvm_context_impl:('b, 'c) pvm_context_impl ->
+    equality_witness:('b, 'c) equality_witness ->
+    impl_name:string ->
+    'a t
+
+  val equiv :
+    'a Equality_witness.t * 'b Equality_witness.t ->
+    'c Equality_witness.t * 'd Equality_witness.t ->
+    ('a, 'c) Equality_witness.eq option * ('b, 'd) Equality_witness.eq option
+
+  type hash = Smart_rollup_context_hash.t
+
+  (** [load cache_size path] initializes from disk a context from [path].
+    [cache_size] allows to change the LRU cache size of Irmin
+    (100_000 by default at irmin-pack/config.ml *)
+  val load :
+    ('repo, 'tree) pvm_context_impl ->
+    cache_size:int ->
+    ([< `Read | `Write > `Read] as 'a) mode ->
+    string ->
+    'a t tzresult Lwt.t
+
+  (** [index context] is the repository of the context [context]. *)
+  val index : 'a t -> 'a t
+
+  (** [close ctxt] closes the context index [ctxt]. *)
+  val close : 'a t -> unit Lwt.t
+
+  (** [readonly index] returns a read-only version of the index. *)
+  val readonly : [> `Read] t -> [`Read] t
+
+  (** [checkout ctxt hash] checkouts the content that corresponds to the commit
+    hash [hash] in the repository [ctxt] and returns the corresponding
+    context. If there is no commit that corresponds to [hash], it returns
+    [None].  *)
+  val checkout : 'a t -> hash -> 'a t option Lwt.t
+
+  (** [empty ctxt] is the context with an empty content for the repository [ctxt]. *)
+  val empty : 'a t -> 'a t
+
+  (** [commit ?message context] commits content of the context [context] on disk,
+    and return the commit hash. *)
+  val commit : ?message:string -> [`Read | `Write] t -> hash Lwt.t
+
+  (** [is_gc_finished index] returns true if a GC is finished (or idle) and false
+    if a GC is running for [index]. *)
+  val is_gc_finished : [`Read | `Write] t -> bool
+
+  (** [gc index ?callback hash] removes all data older than [hash] from disk.
+    If passed, [callback] will be executed when garbage collection finishes. *)
+  val gc :
+    [`Read | `Write] t -> ?callback:(unit -> unit Lwt.t) -> hash -> unit Lwt.t
+
+  (** [wait_gc_completion index] will return a blocking thread if a
+    GC run is currently ongoing. *)
+  val wait_gc_completion : [`Read | `Write] t -> unit Lwt.t
+
+  (* Pvm_state that embeds the context_module embedded associated to pvm
+     protocol_plugins *)
+  type pvmstate =
+    | PVMState : {
+        pvm_context_impl : ('repo, 'tree) pvm_context_impl;
+        impl_name : string;
+        pvmstate : 'tree;
+        equality_witness : ('repo, 'tree) equality_witness;
+      }
+        -> pvmstate
+
+  val make_pvmstate :
+    pvm_context_impl:('a, 'b) pvm_context_impl ->
+    equality_witness:('a, 'b) equality_witness ->
+    impl_name:string ->
+    pvmstate:'b ->
+    pvmstate
+
+  (** State of the PVM that this rollup node deals with *)
+  module PVMState : sig
+    (** The value of a PVM state *)
+    type value = pvmstate
+
+    (** [empty ()] is the empty PVM state. *)
+    val empty : 'a t -> value
+
+    (** [find context] returns the PVM state stored in the [context], if any. *)
+    val find : 'a t -> value option Lwt.t
+
+    (** [lookup state path] returns the data stored for the path [path] in the PVM
+      state [state].  *)
+    val lookup : value -> string list -> bytes option Lwt.t
+
+    (** [set context state] saves the PVM state [state] in the context and returns
+      the updated context. Note: [set] does not perform any write on disk, this
+      information must be committed using {!val:commit}. *)
+    val set : 'a t -> value -> 'a t Lwt.t
+  end
+
+  module Internal_for_tests : sig
+    (** [get_a_tree key] provides a value of internal type [tree] which can be
+      used as a state to be set in the context directly. *)
+    val get_a_tree : 'a t -> string -> pvmstate Lwt.t
+  end
+end
 
 include module type of Irmin_context
