c1efc4a417afbc756842463d6686ec8a57a80a48
Alain Mebsout <alain.mebsout@functori.com>
2023-12-01 14:50:37 +0100

    Rollup node: check snapshot integrity after export

--- src/lib_smart_rollup_node/snapshots.ml
+++ src/lib_smart_rollup_node/snapshots.ml
@@ -78,6 +78,76 @@ let pre_export_checks_and_get_snapshot_metadata ~data_dir =
       last_commitment = Sc_rollup_block.most_recent_commitment head.header;
     }
 
+let first_available_level ~data_dir store =
+  let open Lwt_result_syntax in
+  let* gc_levels = Store.Gc_levels.read store.Store.gc_levels in
+  match gc_levels with
+  | Some {first_available_level; _} -> return first_available_level
+  | None -> (
+      let* metadata = Metadata.read_metadata_file ~dir:data_dir in
+      match metadata with
+      | None -> failwith "No metadata (needs rollup genesis info)."
+      | Some {genesis_info = {level; _}; _} -> return level)
+
+let check_some hash what = function
+  | Some x -> Ok x
+  | None ->
+      error_with "Could not read %s at %a after export." what Block_hash.pp hash
+
+let check_l2_chain ~data_dir (store : _ Store.t) context
+    (head : Sc_rollup_block.t) =
+  let open Lwt_result_syntax in
+  let* first_available_level = first_available_level ~data_dir store in
+  let rec check_block hash =
+    let* b = Store.L2_blocks.read store.l2_blocks hash in
+    let*? _b, header = check_some hash "L2 block" b in
+    let* messages = Store.Messages.read store.messages header.inbox_witness in
+    let*? _messages = check_some hash "messages" messages in
+    let* inbox = Store.Inboxes.read store.inboxes header.inbox_hash in
+    let*? _inbox = check_some hash "inbox" inbox in
+    let* () =
+      match header.commitment_hash with
+      | None -> return_unit
+      | Some commitment_hash ->
+          let* commitment =
+            Store.Commitments.read store.commitments commitment_hash
+          in
+          let*? _commitment = check_some hash "commitment" commitment in
+          return_unit
+    in
+    (* Ensure head context is available. *)
+    let*! head_ctxt = Context.checkout context header.context in
+    let*? _head_ctxt = check_some hash "context" head_ctxt in
+    if header.level <= first_available_level then return_unit
+    else check_block header.predecessor
+  in
+  check_block head.header.block_hash
+
+let post_import_checks ~dest =
+  let open Lwt_result_syntax in
+  let store_dir = Configuration.default_storage_dir dest in
+  let context_dir = Configuration.default_context_dir dest in
+  (* Load context and stores in read-only to run checks. *)
+  let* () = check_store_version store_dir in
+  let* context = Context.load ~cache_size:100 Read_only context_dir in
+  let* store =
+    Store.load
+      Read_only
+      ~index_buffer_size:1000
+      ~l2_blocks_cache_size:100
+      store_dir
+  in
+  let* head = check_head store context in
+  let* () = check_l2_chain ~data_dir:dest store context head in
+  let*! () = Context.close context in
+  let* () = Store.close store in
+  return_unit
+
+let post_export_checks ~snapshot_file =
+  Lwt_utils_unix.with_tempdir "snapshot_checks_" @@ fun dest ->
+  extract gzip_reader stdlib_writer (fun _ -> ()) ~snapshot_file ~dest ;
+  post_import_checks ~dest
+
 let operator_local_file_regexp =
   Re.Str.regexp "^storage/\\(commitments_published_at_level.*\\|lpc$\\)"
 
@@ -127,4 +197,5 @@ let export ~data_dir ~dest =
     return dest_file
   in
   let snapshot_file = compress ~snapshot_file:uncompressed_snapshot in
+  let* () = post_export_checks ~snapshot_file in
   return snapshot_file
