OKEAMAH/Git

MIR: tzt runner: compare against typed output stack
Comparing against untyped output stack is simply impossible in general,
a lot of stacks we may be interested in don't have a unique untyped
representation, or don't have an untyped representation at all. Case in
point: big_map, address, contract, operation &c

contrib/mir/src/tzt.rs
@@ -21,10 +21,7 @@ pub type TestStack = Vec<(Type, TypedValue)>;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum TztTestError {
-    StackMismatch(
-        (FailingTypeStack, Stack<Value>),
-        (FailingTypeStack, Stack<Value>),
-    ),
+    StackMismatch((FailingTypeStack, IStack), (FailingTypeStack, IStack)),
     UnexpectedError(TestError),
     UnexpectedSuccess(IStack),
     ExpectedDifferentError(ErrorExpectation, TestError),
@@ -91,10 +88,7 @@ impl TryFrom<Vec<TztEntity>> for TztTest {
                     "output",
                     &mut m_output,
                     match tzt_output {
-                        Success(stk) => {
-                            typecheck_stack(stk.clone())?;
-                            ExpectSuccess(stk)
-                        }
+                        Success(stk) => ExpectSuccess(typecheck_stack(stk)?),
                         Fail(v) => ExpectError(InterpreterError(
                             InterpreterErrorExpectation::FailedWith(v),
                         )),
@@ -142,7 +136,7 @@ pub enum TestError {
 /// the code in a test.
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum TestExpectation {
-    ExpectSuccess(Vec<(Type, Value)>),
+    ExpectSuccess(Vec<(Type, TypedValue)>),
     ExpectError(ErrorExpectation),
 }
 

tezos/tezos-mirror

contrib/mir/src/tzt.rs
@@ -23,10 +23,7 @@ pub type TestStack = Vec<(Type, TypedValue)>;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub enum TztTestError {
-    StackMismatch(
-        (FailingTypeStack, Stack<Value>),
-        (FailingTypeStack, Stack<Value>),
-    ),
+    StackMismatch((FailingTypeStack, IStack), (FailingTypeStack, IStack)),
     UnexpectedError(TestError),
     UnexpectedSuccess(ErrorExpectation, IStack),
     ExpectedDifferentError(ErrorExpectation, TestError),
@@ -118,10 +115,7 @@ impl TryFrom<Vec<TztEntity>> for TztTest {
                     "output",
                     &mut m_output,
                     match tzt_output {
-                        TztSuccess(stk) => {
-                            typecheck_stack(stk.clone())?;
-                            ExpectSuccess(stk)
-                        }
+                        TztSuccess(stk) => ExpectSuccess(typecheck_stack(stk)?),
                         TztError(error_exp) => ExpectError(error_exp),
                     },
                 )?,
@@ -161,7 +155,7 @@ pub enum TestError {
 /// the code in a test.
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum TestExpectation {
-    ExpectSuccess(Vec<(Type, Value)>),
+    ExpectSuccess(Vec<(Type, TypedValue)>),
     ExpectError(ErrorExpectation),
 }
 