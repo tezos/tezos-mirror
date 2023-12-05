ea68861bd3b880fe8cc0b233a1a0dda8a747c6a9
Ole Krüger <ole.kruger@trili.tech>
2023-12-05 17:23:15 +0000

    Build: Dev deps for HermitOS target

---
+++ src/risc_v/dummy_kernel/install-rust-std-hermit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+version="$(rustc --version | awk '{print $2}')"
+url="https://github.com/hermit-os/rust-std-hermit/releases/download/${version}/rust-std-${version}-riscv64gc-unknown-hermit.tar.gz"
+
+td="$(mktemp -d)"
+mkdir -p "$td"
+
+curl -L "$url" | tar xz -C "$td" --strip-components=1
+"$td/install.sh"
+
+rm -rf "$td"
