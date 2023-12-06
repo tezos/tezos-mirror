713f77b2075e1ca856e5d5a134bf6b4651e1f690
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-12-06 08:47:40 +0100

    [.gitlab/ci/jobs/test/tezt.yml]: Refactor

--- scripts/ci/merge_coverage.sh
+++ scripts/ci/merge_coverage.sh
@@ -6,8 +6,9 @@ set -eu
 # compatibility.
 COVERAGE_MERGED=$(echo "$CI_JOB_NAME" | tr -s '[\/_ @[]+' '-')
 
-# If the ci--no-coverage label is set, we do not attempt to merge the coverage files
-if echo "${CI_MERGE_REQUEST_LABELS:-}" | grep -q '\(^\|,\)ci--no-coverage\($\|,\)' ; then
+# If BISECT_FILE is not set, or the ci--no-coverage label is set, we
+# do not attempt to merge the coverage files
+if [ -z "${BISECT_FILE:-}" ] || echo "${CI_MERGE_REQUEST_LABELS:-}" | grep -q '\(^\|,\)ci--no-coverage\($\|,\)' ; then
     rm -f "$BISECT_FILE"*.coverage
     echo "Coverage is disabled."
 elif bisect-ppx-report merge --coverage-path "$BISECT_FILE" "$COVERAGE_MERGED".coverage; then
