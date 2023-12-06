00cfc32a624185e40984e8541ff100b4e7a4ed04
Julien Tesson <julien.tesson@nomadic-labs.com>
2023-12-06 11:29:05 +0100

    devtools/du: use octez-node-config

--- devtools/proto_context_du/config.ml
+++ devtools/proto_context_du/config.ml
@@ -5,29 +5,8 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let mainnet_genesis =
-  Genesis.
-    {
-      time = Time.Protocol.of_notation_exn "2018-06-30T16:07:32Z";
-      block =
-        Block_hash.of_b58check_exn
-          "BLockGenesisGenesisGenesisGenesisGenesisf79b5d1CoW2";
-      protocol =
-        Protocol_hash.of_b58check_exn
-          "Ps9mPmXaRzmzk35gbAYNCAw6UXdE2qoABTHbN2oEEc1qM7CwT9P";
-    }
-
-let ghostnet_genesis =
-  Genesis.
-    {
-      time = Time.Protocol.of_notation_exn "2022-01-25T15:00:00Z";
-      block =
-        Block_hash.of_b58check_exn
-          "BLockGenesisGenesisGenesisGenesisGenesis1db77eJNeJ9";
-      protocol =
-        Protocol_hash.of_b58check_exn
-          "Ps9mPmXaRzmzk35gbAYNCAw6UXdE2qoABTHbN2oEEc1qM7CwT9P";
-    }
-
 let known_networks =
-  [("mainnet", mainnet_genesis); ("ghostnet", ghostnet_genesis)]
+  List.map
+    (fun (alias, (net : Octez_node_config.Config_file.blockchain_network)) ->
+      (alias, net.genesis))
+    Octez_node_config.Config_file.builtin_blockchain_networks
