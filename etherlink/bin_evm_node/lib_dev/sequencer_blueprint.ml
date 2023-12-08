0eb3f04d5e887efb6ae5443a5d2905c3414a1c2a
Antonio Locascio <antonio.locascio@nomadic-labs.com>
2023-12-08 09:56:14 +0100

    EVM/Sequencer: chunk large blueprints

--- etherlink/bin_evm_node/lib_dev/sequencer_blueprint.ml
+++ etherlink/bin_evm_node/lib_dev/sequencer_blueprint.ml
@@ -7,6 +7,36 @@
 
 open Ethereum_types
 
+(* U256 *)
+let blueprint_number_size = 32
+
+(* U16 *)
+let nb_chunks_size = 2
+
+(* U16 *)
+let chunk_index_size = 2
+
+let blueprint_tag_size = 1
+
+(* Tags added by RLP encoding for the sequencer blueprint.
+    The sequencer blueprint follows the format:
+     [ chunk, <- max size around 4kb, requires tag of 3 bytes
+       number, <- 32 bytes, requires a tag of 1 byte
+       nb_chunks, <- 2 bytes, requires a tag of 1 byte
+       chunk_index <- 2 bytes, requires a tag of 1 byte
+     ] <- outer list requires tag of 3 bytes.
+
+   In total, the tags take 9 bytes. We use 16 to be safe.
+*)
+let rlp_tags_size = 16
+
+let max_chunk_size =
+  let open Transaction_format in
+  (* max_input_size already considers the external tag *)
+  max_input_size - framing_protocol_tag_size - smart_rollup_address_size
+  - blueprint_tag_size - blueprint_number_size - nb_chunks_size
+  - chunk_index_size - rlp_tags_size
+
 let now_bytes () =
   let now = Ptime_clock.now () in
   let now = Ptime.to_rfc3339 now in
@@ -14,10 +44,6 @@ let now_bytes () =
   Time.Protocol.to_seconds timestamp
   |> Z.of_int64 |> Z.to_bits |> Bytes.of_string
 
-(* TODO: https://gitlab.com/tezos/tezos/-/issues/6692
-   Allow large blueprints in the kernel.
-   For now, we only produce one chunk.
-*)
 let make_blueprint_chunks ~transactions =
   let open Rlp in
   let messages =
@@ -38,7 +64,13 @@ let make_blueprint_chunks ~transactions =
   in
   let timestamp = Value (now_bytes ()) in
   let blob = List [messages; timestamp] |> encode in
-  [blob]
+  match String.chunk_bytes max_chunk_size blob with
+  | Ok chunks -> chunks
+  | Error _ ->
+      (* [chunk_bytes] can only return an [Error] if the optional
+         argument [error_on_partial_chunk] is passed. As this is not
+         the case in this call, this branch is impossible. *)
+      assert false
 
 let encode_u16_le i =
   let bytes = Bytes.make 2 '\000' in
@@ -49,12 +81,11 @@ let create ~smart_rollup_address ~number ~transactions =
   let open Rlp in
   let number = Value (encode_u256_le number) in
   let chunks = make_blueprint_chunks ~transactions in
-  (* TODO: to be calculated properly in a future commit *)
   let nb_chunks = Rlp.Value (encode_u16_le @@ List.length chunks) in
   let message_from_chunk chunk_index chunk =
     let chunk_index = Rlp.Value (encode_u16_le chunk_index) in
     let rlp_sequencer_blueprint =
-      List [Value chunk; number; nb_chunks; chunk_index]
+      List [Value (Bytes.of_string chunk); number; nb_chunks; chunk_index]
       |> encode |> Bytes.to_string
     in
     `External
