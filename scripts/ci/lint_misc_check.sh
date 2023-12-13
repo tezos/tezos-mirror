3aa11f8accd9334f42a8cb086228e0f371ef8743
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2023-12-13 15:08:49 +0100

    [scripts/ci/lint_misc_check.sh]: fetch [CI_MERGE_REQUEST_DIFF_BASE_SHA]

--- scripts/ci/lint_misc_check.sh
+++ scripts/ci/lint_misc_check.sh
@@ -10,9 +10,14 @@ diff poetry.lock /home/tezos/poetry.lock
 diff pyproject.toml /home/tezos/pyproject.toml
 
 make check-linting
+
+# Make sure that CI_MERGE_REQUEST_DIFF_BASE_SHA is present.
+if [ -n "${CI_MERGE_REQUEST_DIFF_BASE_SHA:-}" ] &&
+       ! git cat-file -t "$CI_MERGE_REQUEST_DIFF_BASE_SHA" > /dev/null 2>&1; then
+    git fetch origin "$CI_MERGE_REQUEST_DIFF_BASE_SHA"
+fi
 CHECK_LICENSES_DIFF_BASE=${CI_MERGE_REQUEST_DIFF_BASE_SHA:-} \
   ./scripts/lint.sh  --check-licenses-git-new
-
 
 # python checks
 make check-python-linting
