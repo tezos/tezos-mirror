fc28407a1c1e063327ccf133cce654fa4c9aee08
Victor Dumitrescu <v.dumitrescu@outlook.com>
2023-12-18 10:42:31 +0100

    RISC-V jstz kernel: initialisation

---
+++ src/risc_v/jstz/src/main.rs
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Nomadic Labs <contact@nomadic-labs.com>
+//
+// SPDX-License-Identifier: MIT
+
+use tezos_smart_rollup::kernel_entry;
+
+kernel_entry!(jstz_kernel::entry);
