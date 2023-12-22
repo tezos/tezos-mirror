9d66f3d6394622a6a401d45a4c65c28ab37541b2
Eugen Zalinescu <eugen.zalinescu@nomadic-labs.com>
2023-12-22 10:09:54 +0100

    Alpha/Baker: enable protocol logging

--- src/proto_alpha/lib_delegate/client_daemon.ml
+++ src/proto_alpha/lib_delegate/client_daemon.ml
@@ -139,6 +139,8 @@ module Baker = struct
         let* dal_config = Node_rpc.fetch_dal_config cctxt in
         Cryptobox.Config.init_dal ~find_srs_files dal_config
       in
+      let consumer = Protocol_logging.make_log_message_consumer () in
+      Lifted_protocol.set_log_message_consumer consumer ;
       Baking_scheduling.run cctxt ~canceler ~chain ~constants config delegates
     in
     let* () =
