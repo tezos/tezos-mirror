f18ac01094245a2378f8b731e49561c47748135d
pikatos <steve.sanches@nomadic-labs.com>
2024-01-09 09:43:22 +0100

    EVM/Kernel: add unit tests for blake2f
    EVM/Kernel: add more tests from spec

--- etherlink/kernel_evm/evm_execution/src/precompiles.rs
+++ etherlink/kernel_evm/evm_execution/src/precompiles.rs
@@ -193,7 +193,7 @@ fn ecrecover_precompile<Host: Runtime>(
 ) -> Result<PrecompileOutcome, EthereumError> {
     log!(handler.borrow_host(), Info, "Calling ecrecover precompile");
 
-    // check that enough ressources to execute (gas / ticks) are available
+    // check that enough resources to execute (gas / ticks) are available
     let estimated_ticks = fail_if_too_much!(tick_model::ticks_of_ecrecover(), handler);
     let cost = 3000;
     if let Err(err) = handler.record_cost(cost) {
@@ -1148,6 +1148,106 @@ mod tests {
             hex::encode(expected_output),
             hex::encode(outcome.result.unwrap())
         );
+    }
+
+    #[test]
+    fn test_blake2f_invalid_empty() {
+        // act
+        let input = [0; 0];
+        let result =
+            execute_precompiled(H160::from_low_u64_be(9), &input, None, Some(25000));
+
+        // assert
+        // expected outcome is OK and empty output
+
+        assert!(result.is_ok());
+        let outcome = result.unwrap();
+        assert!(outcome.is_success);
+        assert_eq!(Some(vec![]), outcome.result);
+    }
+
+    #[test]
+    fn text_blake2f_invalid_flag() {
+        let input = hex::decode(
+            "0000000c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbab\
+            d9831f79217e1319cde05b616263000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000300000000000000000000000000000002"
+        ).unwrap();
+
+        let result =
+            execute_precompiled(H160::from_low_u64_be(9), &input, None, Some(25000));
+
+        assert!(result.is_ok());
+        let outcome = result.unwrap();
+        assert!(outcome.is_success);
+        assert_eq!(Some(vec![]), outcome.result);
+    }
+
+    struct Blake2fTest {
+        input: &'static str,
+        expected: &'static str,
+    }
+
+    const BLAKE2F_TESTS: [Blake2fTest; 4] = [
+        Blake2fTest {
+            input: "\
+            0000000048c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbab\
+            d9831f79217e1319cde05b616263000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000300000000000000000000000000000001",
+            expected: "\
+            08c9bcf367e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d282e6ad7f520e511f6c3e2b8c68059b9442be0454267ce079\
+            217e1319cde05b"
+        },
+        Blake2fTest {
+            input: "\
+            0000000c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbab\
+            d9831f79217e1319cde05b616263000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000300000000000000000000000000000001",
+            expected: "\
+            ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab9\
+            2386edd4009923"
+        },
+        Blake2fTest {
+            input: "\
+            0000000c48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbab\
+            d9831f79217e1319cde05b616263000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000300000000000000000000000000000000",
+            expected: "\
+            75ab69d3190a562c51aef8d88f1c2775876944407270c42c9844252c26d2875298743e7f6d5ea2f2d3e8d226039cd31b4e426ac4f2d3d666a6\
+            10c2116fde4735"
+        },
+        Blake2fTest {
+            input: "\
+            0000000148c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbab\
+            d9831f79217e1319cde05b616263000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\
+            0000000000000000000000000000000000000000000000000300000000000000000000000000000001",
+            expected: "\
+            b63a380cb2897d521994a85234ee2c181b5f844d2c624c002677e9703449d2fba551b3a8333bcdf5f2f7e08993d53923de3d64fcc68c034e71\
+            7b9293fed7a421"
+        }
+    ];
+
+    #[test]
+    fn test_blake2f_input_spec() {
+        for test in BLAKE2F_TESTS.iter() {
+            let input = hex::decode(test.input).unwrap();
+            let result =
+                execute_precompiled(H160::from_low_u64_be(9), &input, None, Some(25000));
+
+            assert!(result.is_ok());
+            let outcome = result.unwrap();
+            println!("{}", outcome.gas_used);
+            assert!(outcome.is_success);
+
+            let expected = hex::decode(test.expected).unwrap();
+
+            assert_eq!(Some(expected), outcome.result);
+        }
     }
 
     struct ModexpTestCase {
