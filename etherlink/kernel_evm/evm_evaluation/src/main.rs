f8eea486854dbab7c28dab586875b8e82bbb1b27
Rodi-Can Bozman <rodi.bozman@functori.com>
2024-01-12 13:14:15 +0100

    EVM/Eval: refacto skipped checks
    Between of parsing reason and others. Also use a macro to match the
    skipped test's name (lines are shorter).

--- etherlink/kernel_evm/evm_evaluation/src/main.rs
+++ etherlink/kernel_evm/evm_evaluation/src/main.rs
@@ -21,8 +21,6 @@ use walkdir::{DirEntry, WalkDir};
 use fillers::TestResult;
 use helpers::{construct_folder_path, OutputOptions};
 
-const SKIP_ANY: bool = true;
-
 pub fn find_all_json_tests(path: &Path) -> Vec<PathBuf> {
     WalkDir::new(path)
         .into_iter()
@@ -306,6 +304,144 @@ fn generate_diff(
     }
 }
 
+pub fn check_skip(test_file_path: &Path) -> bool {
+    let file_name = test_file_path.file_name().unwrap().to_str().unwrap();
+
+    // Reason: panicked at 'attempt to add with overflow'
+    if file_name.contains("DiffPlaces.json") {
+        return true;
+    }
+
+    matches!(
+        file_name,
+        // Reason: chainId is tested for ethereum mainnet (1) not for etherlink (1337)
+        | "chainId.json"
+
+        // The following test(s) is/are failing they need in depth debugging
+        // Reason: memory allocation of X bytes failed | 73289 IOT instruction (core dumped)
+        | "sha3.json"
+
+        // Long tests ✔ (passing)
+        | "loopMul.json"
+
+        // Oddly long checks on a test that do no relevant check (passing)
+        | "intrinsic.json"
+
+        // Long tests ~ (outcome is unknown)
+        | "static_Call50000_sha256.json"
+        | "static_Call50000_ecrec.json"
+        | "static_Call50000.json"
+
+        // Reason: panicked at 'attempt to multiply with overflow'
+        | "static_Call1024BalanceTooLow.json"
+        | "static_Call1024BalanceTooLow2.json"
+        | "static_Call1024PreCalls3.json"
+
+        // Reason: panicked at 'attempt to add with overflow'
+        | "static_Call1024PreCalls.json"
+        | "static_Call1024PreCalls2.json"
+        | "diffPlaces.json"
+
+        // The following test(s) is/are failing they need in depth debugging
+        // Reason: panicked at 'arithmetic operation overflow'
+        | "HighGasPrice.json"
+        | "randomStatetest32.json"
+        | "randomStatetest7.json"
+        | "randomStatetest50.json"
+        | "randomStatetest468.json"
+        | "underflowTest.json"
+        | "randomStatetest384.json"
+        | "201503110226PYTHON_DUP6.json"
+    )
+}
+
+pub fn check_skip_parsing(test_file_path: &Path) -> bool {
+    let file_name = test_file_path.file_name().unwrap().to_str().unwrap();
+
+    matches!(
+        file_name,
+        // SKIPPED BECAUSE OF WRONG PARSING OF FILLER FILES
+
+        // ********** JSON ********** //
+
+        // The following test(s) is/are failing they need in depth debugging
+        // Reason: panicked at 'Invalid character 'N' at position 62'
+        | "gasCostBerlin.json"
+
+        // Reason: comments in the result field
+        | "static_CREATE_EmptyContractAndCallIt_0wei.json"
+        | "static_CREATE_EmptyContractWithStorageAndCallIt_0wei.json"
+        | "callToNonExistent.json"
+        | "CreateAndGasInsideCreate.json"
+        | "add11.json"
+
+        // Reason: inconsistent hex/dec field value
+        | "TouchToEmptyAccountRevert.json"
+        | "CREATE_EContract_ThenCALLToNonExistentAcc.json"
+        | "CREATE_EmptyContract.json"
+        | "StoreGasOnCreate.json"
+        | "OverflowGasRequire2.json"
+        | "StackDepthLimitSEC.json"
+
+        // ********** YAML ********** //
+
+        // Reason: invalid hex character: _
+        | "doubleSelfdestructTest.json"
+        | "clearReturnBuffer.json"
+        | "gasCost.json"
+
+        // Reason: invalid length 0, expected a (both 0x-prefixed or not) hex string or
+        // byte array containing between (0; 32] bytes
+        | "ZeroValue_SUICIDE_ToOneStorageKey.json"
+        | "ValueOverflow.json"
+
+        // Reason: invalid length 0, expected a (both 0x-prefixed or not) hex string or
+        // byte array containing between (0; 32] bytes
+        | "eqNonConst.json"
+        | "mulmodNonConst.json"
+        | "addmodNonConst.json"
+        | "smodNonConst.json"
+        | "callcodeNonConst.json"
+        | "mstoreNonConst.json"
+        | "modNonConst.json"
+        | "extcodesizeNonConst.json"
+        | "log1NonConst.json"
+        | "extcodecopyNonConst.json"
+        | "log2NonConst.json"
+        | "andNonConst.json"
+        | "log3NonConst.json"
+        | "sgtNonConst.json"
+        | "expNonConst.json"
+        | "mloadNonConst.json"
+        | "log0NonConst.json"
+        | "byteNonConst.json"
+        | "orNonConst.json"
+        | "codecopyNonConst.json"
+        | "gtNonConst.json"
+        | "signextNonConst.json"
+        | "ltNonConst.json"
+        | "sltNonConst.json"
+        | "balanceNonConst.json"
+        | "mstore8NonConst.json"
+        | "delegatecallNonConst.json"
+        | "iszeroNonConst.json"
+        | "subNonConst.json"
+        | "calldatacopyNonConst.json"
+        | "sha3NonConst.json"
+        | "sdivNonConst.json"
+        | "addNonConst.json"
+        | "notNonConst.json"
+        | "createNonConst.json"
+        | "xorNonConst.json"
+        | "calldataloadNonConst.json"
+        | "divNonConst.json"
+        | "returnNonConst.json"
+        | "mulNonConst.json"
+        | "callNonConst.json"
+        | "twoOps.json"
+    )
+}
+
 pub fn main() {
     let opt = Opt::from_args();
     let diff = opt.diff.is_some();
@@ -393,193 +529,6 @@ pub fn main() {
             };
             skip = true
         };
-
-        if SKIP_ANY {
-            // The following test(s) is/are failing they need in depth debugging
-            // Reason: panicked at 'arithmetic operation overflow'
-            if test_file.file_name() == Some(OsStr::new("HighGasPrice.json"))
-                || test_file.file_name() == Some(OsStr::new("randomStatetest32.json"))
-                || test_file.file_name() == Some(OsStr::new("randomStatetest7.json"))
-                || test_file.file_name() == Some(OsStr::new("randomStatetest50.json"))
-                || test_file.file_name() == Some(OsStr::new("randomStatetest468.json"))
-                || test_file.file_name() == Some(OsStr::new("underflowTest.json"))
-                || test_file.file_name() == Some(OsStr::new("randomStatetest384.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new("201503110226PYTHON_DUP6.json"))
-            {
-                process_skip();
-            }
-
-            // The following test(s) is/are failing they need in depth debugging
-            // Reason: memory allocation of X bytes failed | 73289 IOT instruction (core dumped)
-            if test_file.file_name() == Some(OsStr::new("sha3.json")) {
-                process_skip();
-            }
-
-            // Long tests ✔ (passing)
-            if test_file.file_name() == Some(OsStr::new("loopMul.json")) {
-                process_skip();
-            }
-
-            // Oddly long checks on a test that do no relevant check (passing)
-            if test_file.file_name() == Some(OsStr::new("intrinsic.json")) {
-                process_skip();
-            }
-
-            // Long tests ~ (outcome is unknown)
-            if test_file.file_name() == Some(OsStr::new("static_Call50000_sha256.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new("static_Call50000_ecrec.json"))
-                || test_file.file_name() == Some(OsStr::new("static_Call50000.json"))
-            {
-                process_skip();
-            }
-
-            // Reason: panicked at 'attempt to add with overflow'
-            if let Some(file_name) = test_file.to_str() {
-                if file_name.contains("DiffPlaces.json") {
-                    process_skip();
-                }
-            }
-
-            // Reason: panicked at 'attempt to multiply with overflow'
-            if test_file.file_name()
-                == Some(OsStr::new("static_Call1024BalanceTooLow.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new("static_Call1024BalanceTooLow2.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new("static_Call1024PreCalls3.json"))
-            {
-                process_skip();
-            }
-
-            // Reason: panicked at 'attempt to add with overflow'
-            if test_file.file_name() == Some(OsStr::new("static_Call1024PreCalls.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new("static_Call1024PreCalls2.json"))
-                || test_file.file_name() == Some(OsStr::new("diffPlaces.json"))
-            {
-                process_skip();
-            }
-
-            // Reason: chainId is tested for ethereum mainnet (1) not for etherlink (1337)
-            if let Some(file_name) = test_file.to_str() {
-                if file_name.contains("chainId.json") {
-                    process_skip();
-                }
-            }
-
-            // SKIPPED BECAUSE OF WRONG PARSING OF FILLER FILES
-
-            // ********** JSON ********** //
-
-            // Reason: comments in the result field
-            if test_file.file_name() == Some(OsStr::new("add11.json"))
-                || test_file.file_name() == Some(OsStr::new("add11.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new("static_CREATE_EmptyContractAndCallIt_0wei.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new(
-                        "static_CREATE_EmptyContractWithStorageAndCallIt_0wei.json",
-                    ))
-                || test_file.file_name() == Some(OsStr::new("callToNonExistent.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new("CreateAndGasInsideCreate.json"))
-            {
-                process_skip();
-                continue;
-            }
-
-            // Reason: invalid length 0, expected a (both 0x-prefixed or not) hex string or
-            // byte array containing between (0; 32] bytes
-            if test_file.file_name()
-                == Some(OsStr::new("ZeroValue_SUICIDE_ToOneStorageKey.json"))
-                || test_file.file_name() == Some(OsStr::new("ValueOverflow.json"))
-            {
-                process_skip();
-                continue;
-            }
-
-            // Reason: inconsistent hex/dec field value
-            if test_file.file_name() == Some(OsStr::new("TouchToEmptyAccountRevert.json"))
-                || test_file.file_name()
-                    == Some(OsStr::new("CREATE_EContract_ThenCALLToNonExistentAcc.json"))
-                || test_file.file_name() == Some(OsStr::new("CREATE_EmptyContract.json"))
-                || test_file.file_name() == Some(OsStr::new("StoreGasOnCreate.json"))
-                || test_file.file_name() == Some(OsStr::new("OverflowGasRequire2.json"))
-                || test_file.file_name() == Some(OsStr::new("StackDepthLimitSEC.json"))
-            {
-                process_skip();
-                continue;
-            }
-
-            // The following test(s) is/are failing they need in depth debugging
-            // Reason: panicked at 'Invalid character 'N' at position 62'
-            if test_file.file_name() == Some(OsStr::new("gasCostBerlin.json")) {
-                process_skip();
-                continue;
-            }
-
-            // ********** YAML ********** //
-
-            // Reason: invalid hex character: _
-            if test_file.file_name() == Some(OsStr::new("doubleSelfdestructTest.json"))
-                || test_file.file_name() == Some(OsStr::new("clearReturnBuffer.json"))
-                || test_file.file_name() == Some(OsStr::new("gasCost.json"))
-            {
-                process_skip();
-                continue;
-            }
-
-            // Reason: invalid length 0, expected a (both 0x-prefixed or not) hex string or
-            // byte array containing between (0; 32] bytes
-            if test_file.file_name() == Some(OsStr::new("eqNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("mulmodNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("addmodNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("smodNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("callcodeNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("mstoreNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("modNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("extcodesizeNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("log1NonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("extcodecopyNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("log2NonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("andNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("log3NonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("sgtNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("expNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("mloadNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("log0NonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("byteNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("orNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("codecopyNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("gtNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("signextNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("ltNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("sltNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("balanceNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("mstore8NonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("delegatecallNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("iszeroNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("subNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("calldatacopyNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("sha3NonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("sdivNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("addNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("notNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("createNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("xorNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("calldataloadNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("divNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("returnNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("mulNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("callNonConst.json"))
-                || test_file.file_name() == Some(OsStr::new("twoOps.json"))
-            {
-                process_skip();
-                continue;
-            }
-        }
 
         runner::run_test(
             &test_file,
