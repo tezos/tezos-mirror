aac1df70841ef23a04a851f1e301f3e22942c0b9
Thomas Letan <lthms@nomadic-labs.com>
2024-01-29 10:44:07 +0100

    EVM Node: Rework [Evm_services] public API

--- etherlink/bin_node/lib_dev/evm_services.ml
+++ etherlink/bin_node/lib_dev/evm_services.ml
@@ -16,9 +16,20 @@ let get_blueprint_service =
     ~output:Blueprint_types.payload_encoding
     Path.(evm_services_root / "blueprint" /: Arg.uint63)
 
-let register ctxt dir =
+let register_get_blueprint_service ctxt dir =
   Directory.opt_register1 dir get_blueprint_service (fun level () () ->
       let open Lwt_syntax in
       let number = Ethereum_types.Qty (Z.of_int64 level) in
       let* blueprint = Evm_context.find_blueprint ctxt number in
       return_ok blueprint)
+
+let register ctxt dir = register_get_blueprint_service ctxt dir
+
+let get_blueprint ~evm_node_endpoint Ethereum_types.(Qty level) =
+  Tezos_rpc_http_client_unix.RPC_client_unix.call_service
+    [Media_type.json]
+    ~base:evm_node_endpoint
+    get_blueprint_service
+    ((), Z.to_int64 level)
+    ()
+    ()
