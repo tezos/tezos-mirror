f71f0d1646752ae79e64118ff9f9d0a77bc3fdc2
Felix Puscasu <felix.puscasu@trili.tech>
2024-01-31 17:20:59 +0000

    RISC-V: lui instruction

--- src/risc_v/machine_state/src/interpreter/rv32i.rs
+++ src/risc_v/machine_state/src/interpreter/rv32i.rs
@@ -19,6 +19,15 @@ where
         let result = rval.wrapping_add(imm as u64);
         self.write(rd, result);
     }
+
+    /// `LUI` U-type instruction
+    ///
+    /// Set the upper 20 bits of the `rd` register with the `U-type` formatted immediate `imm`
+    pub fn run_lui(&mut self, imm: i64, rd: XRegister) {
+        // Being a `U-type` operation, the immediate is correctly formatted
+        // (lower 12 bits cleared and the value is sign-extended)
+        self.write(rd, imm as u64);
+    }
 }
 
 #[cfg(test)]
@@ -26,12 +35,14 @@ pub mod tests {
     use crate::{
         backend::tests::TestBackendFactory,
         create_backend, create_state,
-        registers::{a1, a2, t2, t3, t4},
+        registers::{a1, a2, a3, a4, t2, t3, t4, XRegisters, XRegistersLayout},
         HartState, HartStateLayout,
     };
+    use proptest::{prelude::any, prop_assert_eq, proptest};
 
     pub fn test<F: TestBackendFactory>() {
         test_addi::<F>();
+        test_lui::<F>();
     }
 
     fn test_addi<F: TestBackendFactory>() {
@@ -63,5 +74,23 @@ pub mod tests {
             // check against wrapping addition performed on the lowest 32 bits
             assert_eq!(state.xregisters.read(rd), res)
         }
+    }
+
+    fn test_lui<F: TestBackendFactory>() {
+        proptest!(|(imm in any::<i64>())| {
+            let mut backend = create_backend!(XRegistersLayout, F);
+            let mut xregs = create_state!(XRegisters, F, backend);
+            xregs.write(a2, 0);
+            xregs.write(a4, 0);
+
+            // U-type immediate sets imm[31:20]
+            let imm = imm & 0xFFFF_F000;
+            xregs.run_lui(imm, a3);
+            // read value is the expected one
+            prop_assert_eq!(xregs.read(a3), imm as u64);
+            // it doesn't modify other registers
+            prop_assert_eq!(xregs.read(a2), 0);
+            prop_assert_eq!(xregs.read(a4), 0);
+        });
     }
 }
