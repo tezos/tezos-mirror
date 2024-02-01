9a0db44a01c4e82bdc71e49ec28a3b2b6894fb29
Thomas Letan <lthms@nomadic-labs.com>
2024-02-01 15:35:41 +0100

    EVM Node: The Observer applies the blueprint its receives

--- etherlink/bin_node/lib_dev/observer.ml
+++ etherlink/bin_node/lib_dev/observer.ml
@@ -7,8 +7,30 @@
 
 open Ethereum_types
 
-let on_new_blueprint (blueprint : Blueprint_types.t) =
+let on_new_blueprint (ctxt : Evm_context.t) (blueprint : Blueprint_types.t) =
+  let (Qty level) = ctxt.next_blueprint_number in
+  let (Qty number) = ctxt.next_blueprint_number in
+  if Z.(equal level number) then
+    Evm_context.apply_blueprint ctxt blueprint.payload
+  else failwith "Received a blueprint with an unexpected number."
+
+let main (ctxt : Evm_context.t) ~evm_node_endpoint =
   let open Lwt_result_syntax in
-  let (Qty level) = blueprint.number in
-  let*! () = Blueprint_events.blueprint_applied level in
-  return_unit
+  let rec loop ctxt stream =
+    let*! candidate = Lwt_stream.get stream in
+    match candidate with
+    | Some blueprint ->
+        let* ctxt = on_new_blueprint ctxt blueprint in
+        loop ctxt stream
+    | None -> return_unit
+  in
+
+  (* TODO: https://gitlab.com/tezos/tezos/-/issues/6876
+     Should be resilient to errors from the EVM node endpoint *)
+  let*! blueprints_stream =
+    Evm_services.monitor_blueprints
+      ~evm_node_endpoint
+      ctxt.next_blueprint_number
+  in
+
+  loop ctxt blueprints_stream
--- etherlink/bin_node/lib_dev/observer.mli
+++ etherlink/bin_node/lib_dev/observer.mli
@@ -5,4 +5,6 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-val on_new_blueprint : Blueprint_types.t -> unit tzresult Lwt.t
+(** [main ctxt ~evm_node_endpoint] starts the main event-loop of the Observer,
+    consuming the blueprints received from [evm_node_endpoint]. *)
+val main : Evm_context.t -> evm_node_endpoint:Uri.t -> unit tzresult Lwt.t
