1d3bd9efd2dab700ee9da4fe0f38649e8dc5c155
Julien Tesson <julien.tesson@nomadic-labs.com>
2024-02-08 13:14:50 +0100

    proto/pending_denunciations: encapsulate find

--- src/proto_alpha/lib_protocol/pending_denunciations_storage.ml
+++ src/proto_alpha/lib_protocol/pending_denunciations_storage.ml
@@ -4,3 +4,8 @@
 (* Copyright (c) 2024 Nomadic Labs. <contact@nomadic-labs.com>               *)
 (*                                                                           *)
 (*****************************************************************************)
+
+let find ctxt delegate =
+  let open Lwt_result_syntax in
+  let* denunciations_opt = Storage.Pending_denunciations.find ctxt delegate in
+  return @@ Option.value denunciations_opt ~default:[]
--- src/proto_alpha/lib_protocol/pending_denunciations_storage.mli
+++ src/proto_alpha/lib_protocol/pending_denunciations_storage.mli
@@ -4,3 +4,11 @@
 (* Copyright (c) 2024 Nomadic Labs. <contact@nomadic-labs.com>               *)
 (*                                                                           *)
 (*****************************************************************************)
+
+(** Returns the pending denunciations list of the given delegate.
+    It returns an empty list if none are registered.
+  *)
+val find :
+  Raw_context.t ->
+  Signature.public_key_hash ->
+  Denunciations_repr.item list tzresult Lwt.t
