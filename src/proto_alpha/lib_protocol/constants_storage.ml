OKEAMAH/Git

proto/constants: add slashable_deposits_period constant

src/proto_alpha/lib_protocol/constants_storage.ml
@@ -56,6 +56,10 @@ let consensus_key_activation_delay c =
   let constants = Raw_context.constants c in
   constants.preserved_cycles
 
+let slashable_deposits_period c =
+  let constants = Raw_context.constants c in
+  constants.preserved_cycles
+
 let blocks_per_cycle c =
   let constants = Raw_context.constants c in
   constants.blocks_per_cycle

src/proto_alpha/lib_protocol/constants_storage.mli
@@ -175,3 +175,7 @@ val tolerated_inactivity_period : Raw_context.t -> int
 
 (** Delay before the activation of a consensus key, in cycles *)
 val consensus_key_activation_delay : Raw_context.t -> int
+
+(** Period, in cycles, during which frozen tokens remains slashable after the
+    cycle of their deposit *)
+val slashable_deposits_period : Raw_context.t -> int

tezos/tezos-mirror

src/proto_alpha/lib_protocol/constants_storage.ml
@@ -56,6 +56,10 @@ let consensus_key_activation_delay c =
   let constants = Raw_context.constants c in
   constants.consensus_rights_delay
 
+let slashable_deposits_period c =
+  let constants = Raw_context.constants c in
+  constants.consensus_rights_delay
+
 let blocks_per_cycle c =
   let constants = Raw_context.constants c in
   constants.blocks_per_cycle

src/proto_alpha/lib_protocol/constants_storage.mli
@@ -182,3 +182,7 @@ val tolerated_inactivity_period : Raw_context.t -> int
 
 (** Delay before the activation of a consensus key, in cycles *)
 val consensus_key_activation_delay : Raw_context.t -> int
+
+(** Period, in cycles, during which frozen tokens remain slashable after the
+    cycle of their deposit *)
+val slashable_deposits_period : Raw_context.t -> int