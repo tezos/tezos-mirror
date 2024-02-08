OKEAMAH/Git

proto/constants,ai_storage: add adaptive_issuance_activation_delay constant

src/proto_alpha/lib_protocol/constants_storage.ml
@@ -44,6 +44,10 @@ let issuance_modification_delay c =
   let constants = Raw_context.constants c in
   constants.consensus_rights_delay
 
+let adaptive_issuance_activation_delay c =
+  let constants = Raw_context.constants c in
+  1 + constants.preserved_cycles + Constants_repr.max_slashing_period
+
 let blocks_per_cycle c =
   let constants = Raw_context.constants c in
   constants.blocks_per_cycle

tezos/tezos-mirror

src/proto_alpha/lib_protocol/constants_storage.ml
@@ -44,6 +44,10 @@ let issuance_modification_delay c =
   let constants = Raw_context.constants c in
   constants.consensus_rights_delay
 
+let adaptive_issuance_activation_delay c =
+  let constants = Raw_context.constants c in
+  1 + constants.consensus_rights_delay + Constants_repr.max_slashing_period
+
 let blocks_per_cycle c =
   let constants = Raw_context.constants c in
   constants.blocks_per_cycle