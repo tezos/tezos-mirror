OKEAMAH/Git

shell_services/history_mode: change comment referencing preserved_cycles

src/lib_shell_services/history_mode.ml
@@ -42,9 +42,9 @@ type t =
   | Rolling of additional_cycles option
 
 (* The default_offset value defines a window of stored cycles which is
-   suitable for baking services. It currently corresponds to 6 as we
+   suitable for baking services. It currently corresponds to 2 as we
    store 1 cycle below the last allowed fork level of the current
-   head, which is set to [preserved_cycles] cycles in the past.
+   head, which is set to [preserved_blocks_cycles] cycles in the past.
    TODO: https://gitlab.com/tezos/tezos/-/issues/1406
    As this value is potentially both network and protocol specific, it
    could be lifted as a protocol value or an hardcoded node

tezos/tezos-mirror

src/lib_shell_services/history_mode.ml
@@ -42,10 +42,9 @@ type t =
   | Rolling of additional_cycles option
 
 (* The default_offset value defines a window of stored cycles which is
-   suitable for baking services. It currently corresponds to 6 as we
+   suitable for baking services. It currently corresponds to 2 as we
    store 1 cycle below the last preserved block level of the current
-   head, which is set to [blocks_preservation_cycles] cycles in the
-   past.
+   head, which is set to [blocks_preservation_cycles] cycles in the past.
    TODO: https://gitlab.com/tezos/tezos/-/issues/1406
    As this value is potentially both network and protocol specific, it
    could be lifted as a protocol value or an hardcoded node