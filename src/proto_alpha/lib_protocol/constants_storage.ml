OKEAMAH/Git

proto/constants,delegate_consensus_key: add key_activation_delay constant

src/proto_alpha/lib_protocol/constants_storage.ml
@@ -52,6 +52,10 @@ let tolerated_inactivity_period c =
   let constants = Raw_context.constants c in
   1 + constants.preserved_cycles
 
+let consensus_key_activation_delay c =
+  let constants = Raw_context.constants c in
+  constants.preserved_cycles
+
 let blocks_per_cycle c =
   let constants = Raw_context.constants c in
   constants.blocks_per_cycle

tezos/tezos-mirror

src/proto_alpha/lib_protocol/constants_storage.ml
@@ -52,6 +52,10 @@ let tolerated_inactivity_period c =
   let constants = Raw_context.constants c in
   1 + constants.consensus_rights_delay
 
+let consensus_key_activation_delay c =
+  let constants = Raw_context.constants c in
+  constants.consensus_rights_delay
+
 let blocks_per_cycle c =
   let constants = Raw_context.constants c in
   constants.blocks_per_cycle