7a6df736b218427b5182c1704ee9cd90d63288e6
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2024-02-13 21:44:31 +0100

    Tezt/DAL: publish_store_and_wait_slot also returns commitment

--- tezt/tests/dal.ml
+++ tezt/tests/dal.ml
@@ -1221,10 +1221,10 @@ let publish_and_store_slot ?with_proof ?counter ?force ?(fee = 1_200) client
 
 (* Similar to [publish_and_store_slot] but additionally bakes [1 +
    number_of_extra_blocks] blocks to trigger the publication of the
-   shards of the published slot. Moreover, the [wait_slot] argument
-   can be used to wait for the shards to be received by one or several
-   other DAL nodes. Returns the level at which the slot was
-   published. *)
+   shards of the published slot commitment. Moreover, the [wait_slot]
+   argument can be used to wait for the shards to be received by one
+   or several other DAL nodes. Returns the published commitment and
+   the level at which it was published. *)
 let publish_store_and_wait_slot ?counter ?force ?(fee = 1_200) node client
     slot_producer_dal_node source ~index ~wait_slot
     ~number_of_extra_blocks_to_bake content =
@@ -1266,7 +1266,7 @@ let publish_store_and_wait_slot ?counter ?force ?(fee = 1_200) node client
   let* () = bake_for ~count:number_of_extra_blocks_to_bake client in
   (* Wait for the shards to be received *)
   let* () = p in
-  return level
+  return (level, commitment)
 
 let publish_store_and_attest_slot ?with_proof ?counter ?force ?fee client node
     dal_node source ~index ~content ~attestation_lag ~number_of_slots =
@@ -4340,7 +4340,7 @@ let test_attestation_through_p2p _protocol dal_parameters _cryptobox node client
          all_shard_indices
   in
   let attester_dal_node_endpoint = Dal_node.rpc_endpoint attester in
-  let* publication_level =
+  let* publication_level, _commitment =
     publish_store_and_wait_slot
       node
       client
