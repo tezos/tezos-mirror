d500501fb2d1f5777a9a4b04ad95f7852ec0bc72
Mathias Bourgoin <mathias.bourgoin@nomadic-labs.com>
2024-02-13 18:20:42 +0100

    devtools/yes_wallet: add Contract and Commitment modules
    - used to compute total supply

--- devtools/yes_wallet/get_delegates_017_PtNairob.ml
+++ devtools/yes_wallet/get_delegates_017_PtNairob.ml
@@ -29,6 +29,8 @@ module Get_delegates = struct
 
   type context = Alpha_context.t
 
+  type contract = Alpha_context.Contract.t
+
   let hash = hash
 
   module Tez = struct
@@ -42,6 +44,50 @@ module Get_delegates = struct
     module To_latest = Tezos_crypto.Signature.Of_V1
   end
 
+  module Contract = struct
+    open Alpha_context.Contract
+
+    let fold context ~init ~f =
+      let open Lwt_syntax in
+      let* l = list context in
+      Lwt_list.fold_left_s f init l
+
+    let balance ctxt t = get_balance ctxt t |> Lwt.map Environment.wrap_tzresult
+
+    let frozen_bonds ctxt t =
+      get_frozen_bonds ctxt t |> Lwt.map Environment.wrap_tzresult
+
+    let get_staked_balance _ctxt _t = Lwt_result_syntax.return None
+
+    let get_unstaked_frozen_balance _ctxt _t = Lwt_result_syntax.return None
+
+    let get_unstaked_finalizable_balance _ctxt _t =
+      Lwt_result_syntax.return None
+
+    let get_full_balance _ctxt _t = Lwt_result_syntax.return Tez.zero
+
+    let contract_address contract = Alpha_context.Contract.to_b58check contract
+
+    let total_supply _ctxt = Lwt_result_syntax.return Tez.zero
+  end
+
+  module Commitment = struct
+    include Alpha_context.Commitment
+
+    type t = Blinded_public_key_hash.t
+
+    (* Use Obj.magic to access commitments from raw context without modifying Protocol 017 *)
+    let fold ctxt ~order ~init ~f =
+      let context : Tezos_protocol_017_PtNairob.Protocol.Raw_context.t =
+        Obj.magic ctxt
+      in
+      Tezos_protocol_017_PtNairob.Protocol.Storage.Commitments.fold
+        context
+        ~order
+        ~init
+        ~f:(fun c t acc -> f c (Tez_repr.to_mutez t) acc)
+  end
+
   module Delegate = struct
     open Alpha_context.Delegate
 
@@ -53,6 +99,10 @@ module Get_delegates = struct
 
     let staking_balance ctxt pkh =
       staking_balance ctxt pkh |> Lwt.map Environment.wrap_tzresult
+
+    let current_frozen_deposits _ctxt _pkh = Lwt_result_syntax.return Tez.zero
+
+    let unstaked_frozen_deposits _ctxt _pkh = Lwt_result_syntax.return Tez.zero
 
     let deactivated ctxt pkh =
       deactivated ctxt pkh |> Lwt.map Environment.wrap_tzresult
--- devtools/yes_wallet/get_delegates_018_Proxford.ml
+++ devtools/yes_wallet/get_delegates_018_Proxford.ml
@@ -29,6 +29,8 @@ module Get_delegates = struct
 
   type context = Alpha_context.t
 
+  type contract = Alpha_context.Contract.t
+
   let hash = hash
 
   module Tez = struct
@@ -42,6 +44,57 @@ module Get_delegates = struct
     module To_latest = Tezos_crypto.Signature.Of_V1
   end
 
+  module Contract = struct
+    open Alpha_context.Contract
+
+    let fold context ~init ~f =
+      let open Lwt_syntax in
+      let* l = list context in
+      Lwt_list.fold_left_s f init l
+
+    let balance ctxt t = get_balance ctxt t |> Lwt.map Environment.wrap_tzresult
+
+    let frozen_bonds ctxt t =
+      get_frozen_bonds ctxt t |> Lwt.map Environment.wrap_tzresult
+
+    let get_staked_balance ctxt t =
+      For_RPC.get_staked_balance ctxt t |> Lwt.map Environment.wrap_tzresult
+
+    let get_unstaked_frozen_balance ctxt t =
+      For_RPC.get_unstaked_frozen_balance ctxt t
+      |> Lwt.map Environment.wrap_tzresult
+
+    let get_unstaked_finalizable_balance ctxt t =
+      For_RPC.get_unstaked_finalizable_balance ctxt t
+      |> Lwt.map Environment.wrap_tzresult
+
+    let get_full_balance ctxt t =
+      For_RPC.get_full_balance ctxt t |> Lwt.map Environment.wrap_tzresult
+
+    let contract_address contract = Alpha_context.Contract.to_b58check contract
+
+    let total_supply ctxt =
+      Alpha_context.Contract.get_total_supply ctxt
+      |> Lwt.map Environment.wrap_tzresult
+  end
+
+  module Commitment = struct
+    include Alpha_context.Commitment
+
+    type t = Blinded_public_key_hash.t
+
+    (* Use Obj.magic to access commitments from raw context without modifying Protocol 018 *)
+    let fold ctxt ~order ~init ~f =
+      let context : Tezos_protocol_018_Proxford.Protocol.Raw_context.t =
+        Obj.magic ctxt
+      in
+      Tezos_protocol_018_Proxford.Protocol.Storage.Commitments.fold
+        context
+        ~order
+        ~init
+        ~f:(fun c t acc -> f c (Tez_repr.to_mutez t) acc)
+  end
+
   module Delegate = struct
     open Alpha_context.Delegate
 
@@ -53,6 +106,16 @@ module Get_delegates = struct
 
     let staking_balance ctxt pkh =
       For_RPC.staking_balance ctxt pkh |> Lwt.map Environment.wrap_tzresult
+
+    let current_frozen_deposits ctxt pkh =
+      current_frozen_deposits ctxt pkh |> Lwt.map Environment.wrap_tzresult
+
+    let unstaked_frozen_deposits ctxt pkh =
+      Alpha_context.Unstaked_frozen_deposits.balance
+        ctxt
+        pkh
+        Alpha_context.Level.(current ctxt).cycle
+      |> Lwt.map Environment.wrap_tzresult
 
     let deactivated ctxt pkh =
       deactivated ctxt pkh |> Lwt.map Environment.wrap_tzresult
