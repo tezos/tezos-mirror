f6b5d5edee49e304de73e8f9b76be99f07642cec
Diane Gallois-Wong <diane.gallois-wong@nomadic-labs.com>
2024-02-14 12:00:33 +0100

    Proto/alpha_context/tests: expose functions to convert to/from repr

--- src/proto_alpha/lib_protocol/alpha_context.mli
+++ src/proto_alpha/lib_protocol/alpha_context.mli
@@ -264,6 +264,10 @@ module Raw_level : sig
     val add : raw_level -> int -> raw_level
 
     val sub : raw_level -> int -> raw_level option
+
+    val from_repr : Raw_level_repr.t -> raw_level
+
+    val to_repr : raw_level -> Raw_level_repr.t
   end
 end
 
@@ -376,6 +380,12 @@ module Round : sig
 
   (* store a round in context *)
   val update : context -> t -> context tzresult Lwt.t
+
+  module Internal_for_tests : sig
+    val from_repr : Round_repr.t -> t
+
+    val to_repr : t -> Round_repr.t
+  end
 end
 
 module Gas : sig
