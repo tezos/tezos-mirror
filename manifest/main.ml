b1e8209d0dcddc124968ab2f867f6595de11f489
Lucas Randazzo <lucas@nomadic-labs.com>
2024-02-20 13:56:18 +0100

    Proto/tests: move reward tests in dedicated file

--- manifest/main.ml
+++ manifest/main.ml
@@ -5423,6 +5423,7 @@ end = struct
             ("test_adaptive_issuance_roundtrip", N.(number >= 018));
             ("test_scenario_base", N.(number >= 019));
             ("test_scenario_stake", N.(number >= 019));
+            ("test_scenario_rewards", N.(number >= 019));
             ("test_liquidity_baking", true);
             ("test_storage_functions", true);
             ("test_storage", true);
