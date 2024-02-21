41e5afc854941d2c54ce11e5c847c7964e8daaaa
Alain Mebsout <alain.mebsout@functori.com>
2024-02-21 17:39:42 +0100

    CI: releases for etherlink based on tag

--- scripts/ci/release.sh
+++ scripts/ci/release.sh
@@ -10,27 +10,37 @@ scripts_dir=$(dirname "$current_dir")
 src_dir=$(dirname "$scripts_dir")
 script_inputs_dir="$src_dir/script-inputs"
 
-binaries="$(cat "$script_inputs_dir/released-executables")"
-deb_packages="$(find . -maxdepth 1 -name octez-\*.deb)"
-rpm_packages="$(find . -maxdepth 1 -name octez-\*.rpm)"
+
+gitlab_package=$((echo "${CI_COMMIT_TAG}" | grep -Eq "^etherlink-") \
+                     && echo "etherlink" || echo "octez")
+
+if [ "${gitlab_package}" = "etherlink" ]; then
+    binaries="$(cat "$script_inputs_dir/etherlink-executables")"
+    deb_packages="$(find . -maxdepth 1 \( -name octez-evm\*.deb -o -name octez-smartrollup\*.deb\))"
+    rpm_packages="$(find . -maxdepth 1 \( -name octez-evm\*.rpm -o -name octez-smartrollup\*.rpm\))"
+else
+    binaries="$(cat "$script_inputs_dir/released-executables")"
+    deb_packages="$(find . -maxdepth 1 -name octez-\*.deb)"
+    rpm_packages="$(find . -maxdepth 1 -name octez-\*.rpm)"
+fi
 
 ### Compute GitLab release names
 
 # Remove the 'v' in front
 # X.Y or X.Y-rcZ
-gitlab_release_no_v=$(echo "${CI_COMMIT_TAG}" | sed -e 's/^v//g')
+gitlab_package_version=$(echo "${CI_COMMIT_TAG}" | sed -e 's/^\(v\|etherlink-\)//g')
 
 # Replace '.' with '-'
 # X-Y or X-Y-rcZ
 # shellcheck disable=SC2034
-gitlab_release_no_dot=$(echo "${gitlab_release_no_v}" | sed -e 's/\./-/g')
+gitlab_release_no_dot=$(echo "${gitlab_package_version}" | sed -e 's/\./-/g')
 
 # X
-gitlab_release_major_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^v([0-9]+)\.([0-9]+)(-rc[0-9]+)?$/\1/p')
+gitlab_release_major_version=$(echo "${gitlab_package_version}" | sed -nE 's/^([0-9]+)\.([0-9]+)(-rc[0-9]+)?$/\1/p')
 # Y
-gitlab_release_minor_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^v([0-9]+)\.([0-9]+)(-rc[0-9]+)?$/\2/p')
+gitlab_release_minor_version=$(echo "${gitlab_package_version}" | sed -nE 's/^([0-9]+)\.([0-9]+)(-rc[0-9]+)?$/\2/p')
 # Z
-gitlab_release_rc_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^v([0-9]+)\.([0-9]+)(-rc)?([0-9]+)?$/\4/p')
+gitlab_release_rc_version=$(echo "${gitlab_package_version}" | sed -nE 's/^([0-9]+)\.([0-9]+)(-rc)?([0-9]+)?$/\4/p')
 
 # Is this a release candidate?
 if [ -n "${gitlab_release_rc_version}" ]; then
@@ -41,15 +51,12 @@ if [ -n "${gitlab_release_rc_version}" ]; then
 else
   # No, release name: Release X.Y
   # shellcheck disable=SC2034
-  gitlab_release_name="Release ${gitlab_release_major_version}.${gitlab_release_minor_version}"
+  gitlab_release_name="Release ${gitlab_package} ${gitlab_release_major_version}.${gitlab_release_minor_version}"
   opam_release_tag="${gitlab_release_major_version}.${gitlab_release_minor_version}"
 fi
 
 ### Compute GitLab generic package names
 
-gitlab_octez_package_name="octez-${gitlab_release_no_v}"
-gitlab_octez_deb_package_name="octez-debian-${gitlab_release_no_v}"
-gitlab_octez_rpm_package_name="octez-redhat-${gitlab_release_no_v}"
-
-# X.Y or X.Y-rcZ
-gitlab_package_version="${gitlab_release_no_v}"
+gitlab_octez_package_name="${gitlab_package}-${gitlab_package_version}"
+gitlab_octez_deb_package_name="${gitlab_package}-debian-${gitlab_package_version}"
+gitlab_octez_rpm_package_name="${gitlab_package}-redhat-${gitlab_package_version}"
