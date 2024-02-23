fd1d75f8ed411f7ee1083d71155169cb7a7d6fb3
Thomas Letan <lthms@nomadic-labs.com>
2024-02-23 21:49:05 +0100

    CI: Prepare split of test_kernels
    Move rules that are very RISC-V specific from kernels.mk to the Makefile of the
    RISC-V projects directly, so that kernels.mk just calls this Makefile. This
    will make the next step a lot easier to review.

--- kernels.mk
+++ kernels.mk
@@ -4,13 +4,10 @@
 #
 # SPDX-License-Identifier: MIT
 
-KERNELS=tx_kernel.wasm tx_kernel_dal.wasm dal_echo_kernel.wasm risc-v-dummy risc-v-dummy.elf jstz
+KERNELS=tx_kernel.wasm tx_kernel_dal.wasm dal_echo_kernel.wasm
 SDK_DIR=src/kernel_sdk
-RISC_V_DIR=src/risc_v
-RISC_V_DUMMY_DIR=src/risc_v/dummy_kernel
-RISC_V_JSTZ_DIR=src/risc_v/jstz
-RISC_V_TESTS_DIR=src/risc_v/tests
 DEMO_DIR=src/kernel_tx_demo
+RISC_V_DIR=src/risc_v
 
 .PHONY: all
 all: build-dev-deps check test build
@@ -20,11 +17,6 @@ all: build-dev-deps check test build
 kernel_sdk:
 	@make -C src/kernel_sdk build
 	@cp src/kernel_sdk/target/$(NATIVE_TARGET)/release/smart-rollup-installer .
-
-.PHONY: risc-v-sandbox
-risc-v-sandbox:
-	@make -C $(RISC_V_DIR) build-sandbox
-	@ln -f $(RISC_V_DIR)/target/$(NATIVE_TARGET)/release/risc-v-sandbox $@
 
 .PHONY: tx_demo_collector
 tx_demo_collector:
@@ -48,25 +40,10 @@ dal_echo_kernel.wasm:
 	@cp src/kernel_dal_echo/target/wasm32-unknown-unknown/release/dal_echo_kernel.wasm $@
 	@wasm-strip $@
 
-.PHONY: risc-v-dummy.elf
-risc-v-dummy.elf:
-	@make -C ${RISC_V_DUMMY_DIR} build
-	@ln -f ${RISC_V_DUMMY_DIR}/target/riscv64gc-unknown-hermit/release/risc-v-dummy $@
-
-.PHONY: risc-v-dummy
-risc-v-dummy:
-	@make -C ${RISC_V_DUMMY_DIR} build
-
-.PHONY: jstz
-jstz:
-	@make -C ${RISC_V_JSTZ_DIR} build
-
-.PHONY: risc-v-tests
-risc-v-tests:
-	@make -C ${RISC_V_TESTS_DIR} build
 
 .PHONY: build
-build: ${KERNELS} kernel_sdk risc-v-sandbox risc-v-tests
+build: ${KERNELS} kernel_sdk
+	@make -C ${RISC_V_DIR} build
 
 .PHONY: clang-supports-wasm
 clang-supports-wasm:
@@ -90,15 +67,14 @@ build-deps:
 .PHONY: test
 test:
 	@make -C ${SDK_DIR} test
-	@make -C ${RISC_V_DIR} test
 	@make -C ${DEMO_DIR} test
+	@make -C ${RISC_V_DIR} test
 
 .PHONY: check
 check: build-dev-deps
 	@make -C ${SDK_DIR} check
-	@make -C ${RISC_V_DIR} check
 	@make -C ${DEMO_DIR} check
-	@make -C ${RISC_V_TESTS_DIR} check
+	@make -C ${RISC_V_DIR} check
 
 	# Check formatting of all crates.
 	@exec scripts/check-format-rust.sh
@@ -115,7 +91,5 @@ publish-sdk:
 clean:
 	@rm -f ${KERNELS}
 	@make -C ${SDK_DIR} clean
-	@make -C ${RISC_V_DIR} clean
 	@make -C ${DEMO_DIR} clean
-	@rm -f risc-v-sandbox
-	@make -C ${RISC_V_TESTS_DIR} clean
+	@make -C ${RISC_V_DIR} clean
