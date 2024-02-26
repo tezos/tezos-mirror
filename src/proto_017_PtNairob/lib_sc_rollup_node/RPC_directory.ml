87665567e04c505a4902248b828dad257376ee33
Alain Mebsout <alain.mebsout@functori.com>
2024-02-26 10:18:20 +0100

    Rollup node/Nairobi: remove outbox proof RPC
    Backport of 14ff11ce9858afdc2cd6e807e573b1ec8a0a5b1e
    
    Co-authored-by: Valentin Chaboche <valentin.chaboche@nomadic-labs.com>

--- src/proto_017_PtNairob/lib_sc_rollup_node/RPC_directory.ml
+++ src/proto_017_PtNairob/lib_sc_rollup_node/RPC_directory.ml
@@ -249,14 +249,6 @@ let () =
   return outbox
 
 let () =
-  Block_helpers_directory.register0
-    Sc_rollup_services.Block.Helpers.outbox_proof
-  @@ fun (node_ctxt, _block_hash) output () ->
-  let open Lwt_result_syntax in
-  let+ commitment, proof = Outbox.proof_of_output node_ctxt output in
-  (Sc_rollup_proto_types.Commitment_hash.of_octez commitment, proof)
-
-let () =
   Block_helpers_directory.register1
     Sc_rollup_services.Block.Helpers.outbox_proof_simple
   @@ fun (node_ctxt, _block_hash) outbox_level message_index () ->
