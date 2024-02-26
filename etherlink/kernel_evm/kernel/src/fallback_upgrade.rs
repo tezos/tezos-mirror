12e2fdb367f40273d801aebf7a45227d7b4b6f61
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2024-02-26 10:59:01 +0100

    EVM/Kernel: move internal_storage to its own module
    It will be needed by yet another modified storage

--- etherlink/kernel_evm/kernel/src/fallback_upgrade.rs
+++ etherlink/kernel_evm/kernel/src/fallback_upgrade.rs
@@ -2,7 +2,8 @@
 //
 // SPDX-License-Identifier: MIT
 
-use crate::safe_storage::{safe_path, InternalStorage, SafeStorage};
+use crate::internal_storage::InternalStorage;
+use crate::safe_storage::{safe_path, SafeStorage};
 use tezos_evm_logging::{log, Level::*};
 use tezos_smart_rollup_host::runtime::Runtime;
 use tezos_smart_rollup_host::{path::RefPath, runtime::RuntimeError, KERNEL_BOOT_PATH};
@@ -11,53 +12,63 @@ const BACKUP_KERNEL_BOOT_PATH: RefPath =
     RefPath::assert_from(b"/__backup_kernel/boot.wasm");
 
 fn backup_current_kernel<Host: Runtime>(
-    host: &mut SafeStorage<&mut Host, &mut InternalStorage>,
+    safe_host: &mut SafeStorage<&mut Host, &mut InternalStorage>,
 ) -> Result<(), RuntimeError> {
     // Fallback preparation detected
     // Storing the current kernel boot path under a temporary path in
     // order to fallback on it if something goes wrong in the upcoming
     // upgraded kernel.
     log!(
-        host,
+        safe_host.host,
         Info,
         "Preparing potential fallback by backing up the current kernel."
     );
-    host.store_copy(&KERNEL_BOOT_PATH, &BACKUP_KERNEL_BOOT_PATH)
+    safe_host
+        .host
+        .store_copy(&KERNEL_BOOT_PATH, &BACKUP_KERNEL_BOOT_PATH)
 }
 
 pub fn fallback_backup_kernel<Host: Runtime>(
-    host: &mut SafeStorage<&mut Host, &mut InternalStorage>,
+    safe_host: &mut SafeStorage<&mut Host, &mut InternalStorage>,
 ) -> Result<(), RuntimeError> {
     log!(
-        host,
+        safe_host.host,
         Error,
         "Something went wrong, fallback mechanism is triggered."
     );
-    host.store_move(&BACKUP_KERNEL_BOOT_PATH, &KERNEL_BOOT_PATH)
+    safe_host
+        .host
+        .store_move(&BACKUP_KERNEL_BOOT_PATH, &KERNEL_BOOT_PATH)
 }
 
 fn clean_backup_kernel<Host: Runtime>(
-    host: &mut SafeStorage<&mut Host, &mut InternalStorage>,
+    safe_host: &mut SafeStorage<&mut Host, &mut InternalStorage>,
 ) -> Result<(), RuntimeError> {
-    log!(host, Info, "Cleaning the backup kernel.");
-    host.store_delete(&BACKUP_KERNEL_BOOT_PATH)
+    log!(safe_host.host, Info, "Cleaning the backup kernel.");
+    safe_host.host.store_delete(&BACKUP_KERNEL_BOOT_PATH)
 }
 
 pub fn promote_upgrade<Host: Runtime>(
-    host: &mut SafeStorage<&mut Host, &mut InternalStorage>,
+    safe_host: &mut SafeStorage<&mut Host, &mut InternalStorage>,
 ) -> Result<(), RuntimeError> {
     let safe_kernel_boot_path = safe_path(&KERNEL_BOOT_PATH)?;
-    match host.store_read(&safe_kernel_boot_path, 0, 0) {
+    match safe_host.host.store_read(&safe_kernel_boot_path, 0, 0) {
         Ok(_) => {
             // Upgrade detected
-            log!(host, Info, "Upgrade activated.");
-            backup_current_kernel(host)?;
-            host.store_move(&safe_kernel_boot_path, &KERNEL_BOOT_PATH)
+            log!(safe_host.host, Info, "Upgrade activated.");
+            backup_current_kernel(safe_host)?;
+            safe_host
+                .host
+                .store_move(&safe_kernel_boot_path, &KERNEL_BOOT_PATH)
         }
         Err(_) => {
             // No on-going upgrade detected
-            if host.store_read(&BACKUP_KERNEL_BOOT_PATH, 0, 0).is_ok() {
-                clean_backup_kernel(host)?
+            if safe_host
+                .host
+                .store_read(&BACKUP_KERNEL_BOOT_PATH, 0, 0)
+                .is_ok()
+            {
+                clean_backup_kernel(safe_host)?
             };
             Ok(())
         }
--- etherlink/kernel_evm/kernel/src/safe_storage.rs
+++ etherlink/kernel_evm/kernel/src/safe_storage.rs
@@ -5,6 +5,7 @@
 //
 // SPDX-License-Identifier: MIT
 
+use crate::internal_storage::{ExtendedRuntime, InternalRuntime};
 use tezos_smart_rollup_core::PREIMAGE_HASH_SIZE;
 #[cfg(feature = "proto-alpha")]
 use tezos_smart_rollup_host::dal_parameters::RollupDalParameters;
@@ -13,66 +14,9 @@ use tezos_smart_rollup_host::{
     metadata::RollupMetadata,
     path::{concat, OwnedPath, Path, RefPath},
     runtime::{Runtime, RuntimeError, ValueType},
-    Error,
 };
 
 pub const TMP_PATH: RefPath = RefPath::assert_from(b"/tmp");
-
-const STORE_HASH_SIZE: usize = 32;
-
-// The [__internal_store_get_hash] host function is not made available by the
-// SDK. We expose it through an [InternalRuntime] trait.
-
-#[link(wasm_import_module = "smart_rollup_core")]
-extern "C" {
-    pub fn __internal_store_get_hash(
-        path: *const u8,
-        path_len: usize,
-        dst: *mut u8,
-        max_size: usize,
-    ) -> i32;
-}
-
-pub trait InternalRuntime {
-    fn __internal_store_get_hash<T: Path>(
-        &mut self,
-        path: &T,
-    ) -> Result<Vec<u8>, RuntimeError>;
-}
-
-// Wrapper for InternalRuntime, this will be added
-// to the Runtime for the Kernel to use.
-// The path is optional to be able to get the hash
-// of the root directory.
-pub trait ExtendedRuntime {
-    fn store_get_hash<T: Path>(
-        &mut self,
-        path: Option<T>,
-    ) -> Result<Vec<u8>, RuntimeError>;
-}
-
-pub struct InternalStorage();
-
-impl InternalRuntime for InternalStorage {
-    fn __internal_store_get_hash<T: Path>(
-        &mut self,
-        path: &T,
-    ) -> Result<Vec<u8>, RuntimeError> {
-        let mut buffer = [0u8; STORE_HASH_SIZE];
-        let result = unsafe {
-            __internal_store_get_hash(
-                path.as_ptr(),
-                path.size(),
-                buffer.as_mut_ptr(),
-                STORE_HASH_SIZE,
-            )
-        };
-        match Error::wrap(result) {
-            Ok(_i) => Ok(buffer.to_vec()),
-            Err(e) => Err(RuntimeError::HostErr(e)),
-        }
-    }
-}
 
 // The kernel runtime requires both the standard Runtime and the
 // new Extended one.
