6fcc97b85ad5e4404f5cbf128fb650006c0aecdf
Gabriel Moise <gabriel.moise@trili.tech>
2024-02-27 11:02:46 +0000

    Baking_nonces: Add TriliTech copyright

--- src/proto_alpha/lib_delegate/baking_nonces.mli
+++ src/proto_alpha/lib_delegate/baking_nonces.mli
@@ -2,6 +2,7 @@
 (*                                                                           *)
 (* Open Source License                                                       *)
 (* Copyright (c) 2021 Nomadic Labs <contact@nomadic-labs.com>                *)
+(* Copyright (c) 2024 TriliTech <contact@trili.tech>                         *)
 (*                                                                           *)
 (* Permission is hereby granted, free of charge, to any person obtaining a   *)
 (* copy of this software and associated documentation files (the "Software"),*)
