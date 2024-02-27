9790b31e01fd799f6873b1067a3067da10aa3dc2
Sylvain Ribstein <sylvain.ribstein@nomadic-labs.com>
2024-02-27 08:50:36 +0100

    evm/kernel: add blueprint applied event

--- etherlink/kernel_evm/kernel/src/event.rs
+++ etherlink/kernel_evm/kernel/src/event.rs
@@ -3,16 +3,20 @@
 // SPDX-License-Identifier: MIT
 
 use crate::{storage, upgrade};
+use primitive_types::{H256, U256};
 use rlp::{Encodable, RlpStream};
+use tezos_ethereum::rlp_helpers::append_u256_le;
 use tezos_smart_rollup_host::runtime::Runtime;
 
 pub const UPGRADE_TAG: u8 = 0x01;
 pub const SEQUENCER_UPGRADE_TAG: u8 = 0x02;
+pub const BLUEPRINT_APPLIED_TAG: u8 = 0x03;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Event {
     Upgrade(upgrade::KernelUpgrade),
     SequencerUpgrade(upgrade::SequencerUpgrade),
+    BlueprintApplied { number: U256, hash: H256 },
 }
 
 impl Encodable for Event {
@@ -26,6 +30,12 @@ impl Encodable for Event {
             Event::SequencerUpgrade(sequencer_upgrade) => {
                 stream.append(&SEQUENCER_UPGRADE_TAG);
                 stream.append(sequencer_upgrade);
+            }
+            Event::BlueprintApplied { number, hash } => {
+                stream.append(&BLUEPRINT_APPLIED_TAG);
+                stream.begin_list(2);
+                append_u256_le(stream, number);
+                stream.append(hash);
             }
         }
     }
