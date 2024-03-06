7038861b8db3eef20d581cd3e005f840e9c5035f
Romain Bardou <romain@nomadic-labs.com>
2024-03-06 16:44:00 +0100

    CI: remove check that uses update_opam_repo.sh

--- scripts/opam-check.sh
+++ scripts/opam-check.sh
@@ -21,23 +21,4 @@ if ! opam install opam/virtual/octez-deps.opam.locked --deps-only --with-test --
   exit 1
 fi
 
-# We really want literal backticks here, not command substitution.
-# shellcheck disable=SC2016
-echo '## Running `./scripts/update_opam_repo.sh`'
-echo
-./scripts/update_opam_repo.sh || exit 1
-
-if [ -n "$(cat opam_repo.patch)" ]; then
-
-  echo "##################################################"
-  cat opam_repo.patch
-  echo "##################################################"
-
-  # We really want literal backticks here, not command substitution.
-  # shellcheck disable=SC2016
-  echo 'Failed! The variables `opam_repository_tag` and `full_opam_repository_tag` are not synchronized. Please read the doc in `./scripts/update_opam_repo.sh` and act accordingly.'
-  echo
-  exit 1
-fi
-
 echo "Ok."
