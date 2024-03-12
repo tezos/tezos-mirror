14cb6b609097ab67841bcfc996d1fab19157462d
François Thiré <francois.thire@nomadic-labs.com>
2024-03-12 12:05:30 +0100

    Dal_node/Amplificator: fork reconstruction
    Co-authored-by: Anne-Laure Schmitt <al.schmitt@protonmail.com>
    Co-authored-by: Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>

--- src/bin_dal_node/amplificator.ml
+++ src/bin_dal_node/amplificator.ml
@@ -53,6 +53,12 @@ let with_amplification_lock (ready_ctxt : Node_context.ready_ctxt) slot_id
     in
     return_unit
 
+let proved_shards_encoding =
+  let open Data_encoding in
+  let shards_encoding = list Cryptobox.shard_encoding in
+  let shards_proofs_encoding = array Cryptobox.shard_proof_encoding in
+  obj2 (req "shards" shards_encoding) (req "proofs" shards_proofs_encoding)
+
 let amplify (shard_store : Store.Shards.t) (slot_store : Store.node_store)
     commitment ~published_level ~slot_index gs_worker node_ctxt =
   let open Lwt_result_syntax in
@@ -75,7 +81,7 @@ let amplify (shard_store : Store.Shards.t) (slot_store : Store.node_store)
          shards_proofs_precomputation = Some precomputation;
          proto_parameters;
          _;
-       } as ready_ctxt) ->
+       } as ready_ctxt) -> (
       let dal_parameters = Cryptobox.parameters cryptobox in
       let number_of_shards = dal_parameters.number_of_shards in
       let redundancy_factor = dal_parameters.redundancy_factor in
@@ -150,38 +156,89 @@ let amplify (shard_store : Store.Shards.t) (slot_store : Store.node_store)
                    | _, index, Ok share -> Some Cryptobox.{index; share}
                    | _ -> None)
           in
-          let* polynomial =
-            Slot_manager_legacy.polynomial_from_shards_lwt
-              cryptobox
-              shards
-              ~number_of_needed_shards
-          in
-          let shards = Cryptobox.shards_from_polynomial cryptobox polynomial in
-          let shard_proofs =
-            Cryptobox.prove_shards cryptobox ~precomputation ~polynomial
-          in
-          let* () =
-            Store.(
-              Shards.save_and_notify
-                slot_store.shard_store
-                slot_store.shards_watcher
-                commitment
-                shards)
-            |> Errors.to_tzresult
-          in
-          Store.save_shard_proofs slot_store commitment shard_proofs ;
-          let* () =
-            Slot_manager.publish_slot_data
-              ~level_committee:(Node_context.fetch_committee node_ctxt)
-              slot_store
-              gs_worker
-              cryptobox
-              proto_parameters
-              commitment
-              published_level
-              slot_index
-          in
-          let*! () =
-            Event.(emit reconstruct_finished (published_level, slot_index))
-          in
-          return_unit
+          (* We fork a new process to handle the cryptographic aspects of
+             amplification (reconstruction of the polynomial + generation of
+             the proofs for all the shards). The parent and the forked child
+             communicate using a pair of pipes and a very simple protocol:
+
+             - child computes shards, shard proofs, and serializes them,
+             - child->parent: length of serialized proved shards,
+             - child->parent: serialized proved shards,
+             - parent stores shards and shard proofs,
+             - child waits a bit and exits *)
+          let*! () = Lwt_io.flush_all () in
+          let ic_parent, oc_child = Lwt_io.pipe ~cloexec:true () in
+          match Lwt_unix.fork () with
+          | 0 ->
+              (* Child *)
+              let*! () = Lwt_io.close ic_parent in
+              let* polynomial =
+                Slot_manager_legacy.polynomial_from_shards_lwt
+                  cryptobox
+                  shards
+                  ~number_of_needed_shards
+              in
+              let shards =
+                Cryptobox.shards_from_polynomial cryptobox polynomial
+                |> List.of_seq
+              in
+              let shard_proofs =
+                Cryptobox.prove_shards cryptobox ~precomputation ~polynomial
+              in
+              let proved_shards_encoded =
+                Data_encoding.Binary.to_bytes_exn
+                  proved_shards_encoding
+                  (shards, shard_proofs)
+              in
+              let buffer = Bytes.create 4 in
+              let len = Bytes.length proved_shards_encoded in
+              Bytes.set_int32_ne buffer 0 (Int32.of_int len) ;
+              let*! _ = Lwt_io.write_from oc_child buffer 0 4 in
+              let*! _ =
+                Lwt_io.write_from oc_child proved_shards_encoded 0 len
+              in
+              let*! () = Lwt_io.close oc_child in
+              let*! () = Lwt_unix.sleep 60. in
+              exit 0
+          | _n ->
+              (* Parent *)
+              let*! () = Lwt_io.close oc_child in
+              let* shards, shard_proofs =
+                let buffer = Bytes.create 4 in
+                let*! () = Lwt_io.read_into_exactly ic_parent buffer 0 4 in
+                let len = Bytes.get_int32_ne buffer 0 |> Int32.to_int in
+                let encoded = Bytes.create len in
+                let*! () = Lwt_io.read_into_exactly ic_parent encoded 0 len in
+                let*! () = Lwt_io.close ic_parent in
+                let shards, shard_proofs =
+                  Data_encoding.Binary.of_bytes_exn
+                    proved_shards_encoding
+                    encoded
+                in
+                return (List.to_seq shards, shard_proofs)
+              in
+              let* () =
+                Store.(
+                  Shards.save_and_notify
+                    slot_store.shard_store
+                    slot_store.shards_watcher
+                    commitment
+                    shards)
+                |> Errors.to_tzresult
+              in
+              Store.save_shard_proofs slot_store commitment shard_proofs ;
+              let* () =
+                Slot_manager.publish_slot_data
+                  ~level_committee:(Node_context.fetch_committee node_ctxt)
+                  slot_store
+                  gs_worker
+                  cryptobox
+                  proto_parameters
+                  commitment
+                  published_level
+                  slot_index
+              in
+              let*! () =
+                Event.(emit reconstruct_finished (published_level, slot_index))
+              in
+              return_unit)
