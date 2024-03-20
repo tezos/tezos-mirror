f4316a99224e9b11dfe33118ab42bd8e4b90a7c5
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2024-03-20 15:50:29 +0100

    [contrib/ai-simulator/script-scrap-weeklynet.sh]: shfmt

--- contrib/ai-simulator/script-scrap-weeklynet.sh
+++ contrib/ai-simulator/script-scrap-weeklynet.sh
@@ -13,39 +13,39 @@ l=$1
 
 batch_size=10
 
-for (( i=0; i<=$l; i+=batch_size )); do
+for ((i = 0; i <= $l; i += batch_size)); do
 
-    echo "Launching data request for batch starting with cycle: $i"
+  echo "Launching data request for batch starting with cycle: $i"
 
-    for (( n=i; n<$((i + batch_size)) && n<=$l; n++ )); do
-        block=$((128 * (1 + n)))
+  for ((n = i; n < $((i + batch_size)) && n <= $l; n++)); do
+    block=$((128 * (1 + n)))
 
-        (curl -s https://rpc.weeklynet-2024-03-13.teztnets.com/chains/main/blocks/$block/context/total_supply > "/tmp/ai-sim-supply/$n.txt" &)
+    (curl -s https://rpc.weeklynet-2024-03-13.teztnets.com/chains/main/blocks/$block/context/total_supply > "/tmp/ai-sim-supply/$n.txt" &)
 
-        (curl -s https://rpc.weeklynet-2024-03-13.teztnets.com/chains/main/blocks/$block/context/total_frozen_stake > "/tmp/ai-sim-frozen/$n.txt" &)
+    (curl -s https://rpc.weeklynet-2024-03-13.teztnets.com/chains/main/blocks/$block/context/total_frozen_stake > "/tmp/ai-sim-frozen/$n.txt" &)
+  done
+
+  for ((n = i; n < $((i + batch_size)) && n <= $l; n++)); do
+    echo -ne "Looking for data $n\r"
+
+    until [ -s "/tmp/ai-sim-supply/$n.txt" ]; do
+      sleep 0.1
+    done
+    until [ -s "/tmp/ai-sim-frozen/$n.txt" ]; do
+      sleep 0.1
     done
 
-    for (( n=i; n<$((i + batch_size)) && n<=$l; n++ )); do
-        echo -ne "Looking for data $n\r"
-
-        until [ -s "/tmp/ai-sim-supply/$n.txt" ]; do
-            sleep 0.1
-        done
-        until [ -s "/tmp/ai-sim-frozen/$n.txt" ]; do
-            sleep 0.1
-        done
-
-        echo -ne "\r"
-    done
+    echo -ne "\r"
+  done
 done
 
 for n in $(seq 0 $l); do
 
-    supply=$(cat "/tmp/ai-sim-supply/$n.txt")
-    frozen=$(cat "/tmp/ai-sim-frozen/$n.txt")
+  supply=$(cat "/tmp/ai-sim-supply/$n.txt")
+  frozen=$(cat "/tmp/ai-sim-frozen/$n.txt")
 
-    echo "$supply," >> src/total_supply_storage.js
-    echo "$frozen," >> src/total_frozen_stake_storage.js
+  echo "$supply," >> src/total_supply_storage.js
+  echo "$frozen," >> src/total_frozen_stake_storage.js
 done
 
 echo "];" >> src/total_supply_storage.js
