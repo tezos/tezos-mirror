c45671c0c2d9a2e87e4c6e87eb1e0f60ab115d35
Diana Savatina <diana.savvatina@trili.tech>
2024-03-26 15:20:41 +0000

    profiler_lib: fix for unit tests
    applied comments:
     - added --verbose flag
     - added checks for the profiler output
     - use Temp.file instead of the hardcoded path

--- src/lib_base/unix/test/test_simple_profiler.ml
+++ src/lib_base/unix/test/test_simple_profiler.ml
@@ -1,25 +1,7 @@
 (*****************************************************************************)
 (*                                                                           *)
-(* Open Source License                                                       *)
-(* Copyright (c) 2023 Nomadic Labs <contact@nomadic-labs.com>                *)
-(*                                                                           *)
-(* Permission is hereby granted, free of charge, to any person obtaining a   *)
-(* copy of this software and associated documentation files (the "Software"),*)
-(* to deal in the Software without restriction, including without limitation *)
-(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
-(* and/or sell copies of the Software, and to permit persons to whom the     *)
-(* Software is furnished to do so, subject to the following conditions:      *)
-(*                                                                           *)
-(* The above copyright notice and this permission notice shall be included   *)
-(* in all copies or substantial portions of the Software.                    *)
-(*                                                                           *)
-(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
-(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
-(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
-(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
-(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
-(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
-(* DEALINGS IN THE SOFTWARE.                                                 *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2024 Trilitech <contact@trili.tech>                         *)
 (*                                                                           *)
 (*****************************************************************************)
 
@@ -27,32 +9,58 @@
    -------
    Component:    Base, Unix
    Invocation:   dune exec src/lib_base/unix/test/main.exe -- --file test_simple_profiler.ml
-   Subject:      Check syslog primitives
+                 Use --verbose flag for detailed logs.
+   Subject:      Testing simple profiler
 *)
+open Tezt
 
 open Tezos_base.TzPervasives
 open Profiler
 module WithExceptions = Tezos_error_monad.TzLwtreslib.WithExceptions
 open Lwt_syntax
 
-let print_file_content file_path =
-  let rec read_and_print chan =
-    try
-      let line = input_line chan in
-      print_endline line ;
-      read_and_print chan
-    with
-    | End_of_file -> close_in chan
+let check_line_matches line (pattern: string) =
+  if not (Re.Str.string_match (Re.Str.regexp pattern) line 0) then
+    Test.fail ~__LOC__ "No match: pattern={%s}\n%s" pattern line
+
+let check_file_matches (file_path : string) (patterns : (int * string) list)
+    =
+  let get_line file =
+    try input_line file with
+    | End_of_file ->
+        close_in file ;
+        Test.fail ~__LOC__ "The file is shorter than expected: %s" file_path
     | ex ->
-        close_in chan ;
+        close_in file ;
         raise ex
   in
+  let check_eof file =
+    try
+      let line = input_line file in
+      close_in file ;
+      Test.fail ~__LOC__ "The file is longer than expected: \n%s (%s)" line file_path
+    with
+    | End_of_file -> close_in file
+    | ex ->
+        close_in file ;
+        raise ex
+  in
+  let rec read_and_match file patterns_left =
+    match patterns_left with
+    | [] -> check_eof file
+    | (count, pattern) :: rest -> (
+        match count with
+        | 0 -> read_and_match file rest
+        | n ->
+            let line = get_line file in
+            Log.info "%s" line ;
+            check_line_matches line pattern ;
+            read_and_match file ((n - 1, pattern) :: rest))
+  in
   try
-    let chan = open_in file_path in
-    read_and_print chan
-  with Sys_error msg ->
-    Printf.eprintf "Error: %s\n" msg ;
-    Sys.remove file_path
+    let file = open_in file_path in
+    read_and_match file patterns
+  with Sys_error msg -> Printf.eprintf "Error [%s] %s\n" file_path msg
 
 let noop profiler =
   Profiler.record profiler "noop" ;
@@ -60,14 +68,14 @@ let noop profiler =
 
 let sleep10ms profiler =
   Profiler.record profiler "sleep10ms" ;
-  print_endline "---sleep10---" ;
+  Log.info "---sleep10---" ;
   Unix.sleepf 0.01 ;
   noop profiler ;
   Profiler.stop profiler
 
 let sleep70ms profiler =
   Profiler.record profiler "sleep70ms" ;
-  print_endline "---sleep70---" ;
+  Log.info "---sleep70---" ;
   Unix.sleepf 0.07 ;
   Profiler.stop profiler
 
@@ -87,11 +95,11 @@ let bar profiler =
   Profiler.stop profiler
 
 let test70 profiler =
-  print_endline "\nTEST: test 70\n" ;
+  Log.info "\nTEST: test 70\n" ;
   Profiler.record profiler "test70" ;
   bar profiler ;
   foo profiler ;
-  Profiler.stop profiler
+  Profiler.stop profiler 
 
 let sleep_until_next_second () =
   let current_time = Unix.gettimeofday () in
@@ -103,53 +111,70 @@ let sleep_until_next_second () =
 let slp tag profiler =
   let* () =
     Profiler.record_s profiler ("slp " ^ tag) (fun () ->
-        let ts = Unix.gettimeofday () in
-        let ts_ptime =
-          WithExceptions.Option.get ~loc:__LOC__ (Ptime.of_float_s ts)
-        in
-        Format.printf "%a: sleep in %s\n" Time.System.pp_hum ts_ptime tag ;
+        Log.info "sleep in %s" tag ;
         let* () = Lwt_unix.sleep 0.01 in
-        let ts = Unix.gettimeofday () in
-        let ts_ptime =
-          WithExceptions.Option.get ~loc:__LOC__ (Ptime.of_float_s ts)
-        in
-        Format.printf "%a: sleep done\n" Time.System.pp_hum ts_ptime ;
+        Log.info "sleep done";
         Lwt.return_unit)
   in
   return_unit
 
 let sequences profiler =
-  let* () = Profiler.record_s profiler "a_seq" (fun () -> slp "a_seq" profiler) in
+  let* () =
+    Profiler.record_s profiler "a_seq" (fun () -> slp "a_seq" profiler)
+  in
   let* () =
     Profiler.record_s profiler "b_seq" (fun () ->
-        let* () = Profiler.record_s profiler "b_a_seq" (fun () -> slp "b_a_seq" profiler) in
-        let* () = Profiler.record_s profiler "b_b_seq" (fun () -> slp "b_b_seq" profiler) in
+        let* () =
+          Profiler.record_s profiler "b_a_seq" (fun () ->
+              slp "b_a_seq" profiler)
+        in
+        let* () =
+          Profiler.record_s profiler "b_b_seq" (fun () ->
+              slp "b_b_seq" profiler)
+        in
         slp "b_seq" profiler)
   in
   return_unit
 
 let aggregates profiler =
-  let* () = Profiler.aggregate_s profiler "a_aggr" (fun () -> slp "a_aggr" profiler) in
-  let* () = Profiler.aggregate_s profiler "b_aggr" (fun () -> slp "b_aggr" profiler) in
+  let* () =
+    Profiler.aggregate_s profiler "a_aggr" (fun () -> slp "a_aggr" profiler)
+  in
+  let* () =
+    Profiler.aggregate_s profiler "b_aggr" (fun () -> slp "b_aggr" profiler)
+  in
   let* () =
     Profiler.aggregate_s profiler "a_aggr" (fun () ->
-        let* () = Profiler.aggregate_s profiler "a_a_aggr" (fun () -> slp "a_a_aggr" profiler) in
-        let* () = Profiler.aggregate_s profiler "a_b_aggr" (fun () -> slp "a_b_aggr" profiler) in
-        let* () = Profiler.aggregate_s profiler "a_c_aggr" (fun () -> slp "a_c_aggr" profiler) in
+        let* () =
+          Profiler.aggregate_s profiler "a_a_aggr" (fun () ->
+              slp "a_a_aggr" profiler)
+        in
+        let* () =
+          Profiler.aggregate_s profiler "a_b_aggr" (fun () ->
+              slp "a_b_aggr" profiler)
+        in
+        let* () =
+          Profiler.aggregate_s profiler "a_c_aggr" (fun () ->
+              slp "a_c_aggr" profiler)
+        in
         slp "a_aggr1" profiler)
   in
   let* () =
     Profiler.aggregate_s profiler "a_aggr" (fun () ->
         let* () =
           Profiler.aggregate_s profiler "a_a_aggr" (fun () ->
-              Profiler.aggregate_s profiler "a_a_a_aggr" (fun () -> slp "a_a_a_aggr" profiler))
+              Profiler.aggregate_s profiler "a_a_a_aggr" (fun () ->
+                  slp "a_a_a_aggr" profiler))
         in
         slp "a_aggr2" profiler)
   in
   return_unit
 
 let buggy_aggregates profiler =
-  let t1 = Profiler.aggregate_s profiler "bug_aggr1" (fun () -> slp "bug_aggr1" profiler) in
+  let t1 =
+    Profiler.aggregate_s profiler "bug_aggr1" (fun () ->
+        slp "bug_aggr1" profiler)
+  in
   let t2 =
     Profiler.aggregate_s profiler "bug_aggr2" (fun () ->
         let* () = slp "bug_aggr2-1" profiler in
@@ -158,7 +183,9 @@ let buggy_aggregates profiler =
   Lwt.join [t1; t2]
 
 let spans profiler =
-  let t1 = Profiler.span_s profiler ["span1"] (fun () -> slp "span1" profiler) in
+  let t1 =
+    Profiler.span_s profiler ["span1"] (fun () -> slp "span1" profiler)
+  in
   let t2 =
     Profiler.span_s profiler ["span2"; "inner1"] (fun () ->
         let* () = slp "span2, inner1/1" profiler in
@@ -172,21 +199,33 @@ let spans profiler =
   Lwt.join [t1; t2; t3]
 
 let sequences_with_mark_and_stamp profiler =
-  let* () = Profiler.record_s profiler "a_seq" (fun () -> slp "a_seq" profiler ) in
+  let* () =
+    Profiler.record_s profiler "a_seq" (fun () -> slp "a_seq" profiler)
+  in
   let* () =
     Profiler.record_s profiler "b_seq" (fun () ->
-        let* () = Profiler.record_s profiler "b_a_seq" (fun () -> slp "b_a_seq" profiler) in
-        let () = List.iter (fun _ -> Profiler.mark profiler ["mark"]) (1 -- 10) in
+        let* () =
+          Profiler.record_s profiler "b_a_seq" (fun () ->
+              slp "b_a_seq" profiler)
+        in
+        let () =
+          List.iter (fun _ -> Profiler.mark profiler ["mark"]) (1 -- 10)
+        in
         let () = Profiler.stamp profiler "stamp" in
-        let* () = Profiler.record_s profiler "b_b_seq" (fun () -> slp "b_b_seq" profiler) in
-        let () = List.iter (fun _ -> Profiler.mark profiler ["mark"]) (1 -- 10) in
+        let* () =
+          Profiler.record_s profiler "b_b_seq" (fun () ->
+              slp "b_b_seq" profiler)
+        in
+        let () =
+          List.iter (fun _ -> Profiler.mark profiler ["mark"]) (1 -- 10)
+        in
         let () = Profiler.stamp profiler "stamp" in
         slp "b_seq" profiler)
   in
   return_unit
 
 let test_profiler_actions profiler =
-  print_endline "\nTEST: test_profiler_actions\n" ;
+  Log.info "\nTEST: test_profiler_actions\n" ;
   let main () =
     let* () = sleep_until_next_second () in
     Profiler.record_s profiler "main" (fun () ->
@@ -195,46 +234,71 @@ let test_profiler_actions profiler =
         let* () = buggy_aggregates profiler in
         let* () = spans profiler in
         let* () = sequences_with_mark_and_stamp profiler in
-        Lwt.return_unit) in
+        Lwt.return_unit)
+  in
   Lwt_main.run (main ())
 
-let get_profiler test_name =  
+let ms_pattern = {| +[0-9]+\.[0-9][0-9][0-9]ms +[0-9]+%.*|}
+let test70_patterns : ((int * string) list) = 
+  [
+    (1, {|20[0-9\-]+T[0-9:\.]+-00:00|});  (* Timestamp pattern *)
+    (1, {|test70 \.+ 1|} ^ ms_pattern);
+    (22, {| +[a-z0-9_ ]+ \.+ [0-9]+|} ^ ms_pattern);
+  ]
+let test_actions_patterns : ((int * string) list) = 
+  [
+    (1, {|20[0-9\-]+T[0-9:\.]+-00:00|});  (* Timestamp pattern *)
+    (1, {|main \.+ 1|} ^ ms_pattern);
+    (1, {|  a_aggr \.+ 3|} ^ ms_pattern);
+    (1, {|    a_a_aggr \.+ 2|} ^ ms_pattern);
+    (17, {| +[a-z0-9_\- ]+ \.+ 1|} ^ ms_pattern);
+    (1, {|  span2 \.+ 0|});
+    (10, {| +[a-z0-9_\- ]+ \.+ 1|} ^ ms_pattern);
+    (1, {|    span2 \.+ 0|});
+    (9, {| +[a-z0-9_\- ]+ \.+ 1|} ^ ms_pattern);
+    (1, {|    mark \.+ 20|});
+    (2, {| +[a-z0-9_\- ]+ \.+ 1|} ^ ms_pattern);
+    (1, {|    stamp \.+ 1.*|});
+    (2, {| +[a-z0-9_\- ]+ \.+ 1|} ^ ms_pattern);
+    (1, {|    stamp \.+ 1.*|});
+    (1, {| +[a-z0-9_\- ]+ \.+ 1|} ^ ms_pattern);
+  ]
+  
+let get_profiler file_name=
   let profiler = unplugged () in
   let test_profiler_instance =
     Profiler.instance
-      Tezos_base_unix.Simple_profiler.auto_write_to_txt_file
-      ("/tmp/output_test_simple_profiling_" ^ test_name ^ ".txt", Profiler.Detailed)
+      Tezos_base_unix.Simple_profiler.auto_write_to_txt_file ( file_name, Profiler.Detailed )
   in
   plug profiler test_profiler_instance ;
   profiler
 
-let print_test_results test_name = 
-  print_endline ("\nProfiling result for " ^ test_name) ;
-  print_endline ("==================================") ;
-  print_file_content ("/tmp/output_test_simple_profiling_" ^ test_name ^ ".txt") 
-
-let run_test_with_profiler test_name test_fn =
-  let profiler = get_profiler test_name in
+let run_test_with_profiler test_name test_fn test_patterns =
+  let file_name = 
+    (* "/tmp/output_test_simple_profiling_" ^ test_name ^ ".txt" in *)
+    Temp.file ("output_test_simple_profiling_" ^ test_name ^ ".txt") in
+  let profiler = get_profiler file_name in
+    
   test_fn profiler ;
-  print_test_results test_name
+  Log.info "\nProfiling result for %s" test_name ;
+  Log.info "==================================" ;
+  check_file_matches file_name test_patterns
 
 let () =
-  Tezt_core.Test.register
+  Tezt_core.Regression.register
     ~__FILE__
     ~title:"Simple profiler: basic profiler test"
     ~tags:["unix"; "profiler"]
   @@ fun () ->
-
-  run_test_with_profiler "test_main" test70 ;
+  run_test_with_profiler "test70" test70 test70_patterns;
   Tezt_core.Base.unit
 
 let () =
-  Tezt_core.Test.register
+  Tezt_core.Regression.register
     ~__FILE__
     ~title:"Simple profiler: all profiler actions"
     ~tags:["unix"; "profiler"]
   @@ fun () ->
-
-  run_test_with_profiler "test_main" test_profiler_actions ;
+  run_test_with_profiler "test_actions" test_profiler_actions test_actions_patterns;
 
   Tezt_core.Base.unit
