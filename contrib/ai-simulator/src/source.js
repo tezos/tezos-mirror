4be25bccde05e70c0d60cfc261bb793bab5d02a6
phink <paul.laforgue@nomadic-labs.com>
2024-03-26 07:32:06 +0100

    Contrib/ai-simulator: total_delegated method

--- contrib/ai-simulator/src/source.js
+++ contrib/ai-simulator/src/source.js
@@ -123,6 +123,8 @@ const safe_get = (array, cycle) => {
  * of the given cycle. The ratio is clipped between 0 and 1.
  * @property {function(number):bigInt} total_supply - Takes a cycle and
  * returns the total supply (in mutez) available on the chain
+ * @property {function(number):bigInt} total_delegated_balance - Takes a cycle and
+ * returns returns the total delegated amount (in mutez) on the chain
  * @property {function(number):bigInt} total_staked_balance - Takes a cycle and
  * returns the total stake (in mutez) frozen on the chain.
  */
@@ -150,6 +152,7 @@ const safe_get = (array, cycle) => {
  * @param {number} config.chain.ai_activation_cycle - Adaptive Issuance activation cycle.
  * @param {array(number)} config.chain.total_supply.
  * @param {array(number)} config.chain.total_frozen_stake.
+ * @param {array(number)} config.chain.total_delegated.
  *
  * @return {Simulator}. The Adaptive Issuance simulator.
  */
@@ -171,6 +174,10 @@ export class Simulator {
 
   total_staked_balance(cycle) {
     return bigInt(this.config.chain.total_frozen_stake[cycle]);
+  }
+
+  total_delegated_balance(cycle) {
+    return bigInt(this.config.chain.total_delegated[cycle]);
   }
 
   #compute_extremum(cycle, initial_value, final_value) {
