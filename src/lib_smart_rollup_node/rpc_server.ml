30bab513bed1e7aa91dfa715e1fa00de21400e9f
Alain Mebsout <alain.mebsout@functori.com>
2024-03-29 10:32:37 +0100

    Rollup node/RPC: /stats is not available in secure ACL

--- src/lib_smart_rollup_node/rpc_server.ml
+++ src/lib_smart_rollup_node/rpc_server.ml
@@ -50,6 +50,7 @@ module Acl = struct
               "GET /global/block/*/durable/wasm_2_0_0/subkeys";
               "/local/batcher/**";
               "/admin/**";
+              "/stats/**";
             ];
       }
 
