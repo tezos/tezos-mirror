3aed5d43451a430a909113a16e2d745ce5a5380e
saroupille <francois.thire@nomadic-labs.com>
2024-04-04 08:55:12 +0200

    Tezt/DAL: A scenario for benchmarking performances

--- tezt/tests/main.ml
+++ tezt/tests/main.ml
@@ -229,7 +229,9 @@ let register_protocol_tests_that_use_supports_correctly () =
 let register_protocol_specific_because_regression_tests () = ()
 
 (* Declaration of tests using tezt cloud. *)
-let () = Tezt_cloud.register ~tags:[Tag.cloud]
+let () =
+  Tezt_cloud.register ~tags:[Tag.cloud] ;
+  Dal_cloud.register ()
 
 let () =
   register_protocol_independent_tests () ;
