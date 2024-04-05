36a266df51c4d136d825daa71b8b2ac2bea3859e
Alain Mebsout <alain.mebsout@functori.com>
2024-04-05 19:50:59 +0200

    Doc: license header format

--- src/lib_smart_rollup/dictionary_encoding.ml
+++ src/lib_smart_rollup/dictionary_encoding.ml
@@ -1,7 +1,7 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* SPDX-License-Identifier: MIT                                              *)
-(* Copyright (c) 2024 Functori <contact@functori.com>                        *)
+(* SPDX-FileCopyrightText: 2024 Functori <contact@functori.com>              *)
 (*                                                                           *)
 (*****************************************************************************)
 
