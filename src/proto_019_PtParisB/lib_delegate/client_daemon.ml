484b1db57da98a43d08e850419a5b8251c9eab9a
Killian Delarue <killian.delarue@nomadic-labs.com>
2024-04-11 15:22:50 +0200

    Baker: Fix version in starting log

--- src/proto_019_PtParisB/lib_delegate/client_daemon.ml
+++ src/proto_019_PtParisB/lib_delegate/client_daemon.ml
@@ -118,8 +118,8 @@ module Baker = struct
       in
       let*! () =
         cctxt#message
-          "Baker v%a (%s) for %a started."
-          Tezos_version.Version.pp
+          "Baker %a (%s) for %a started."
+          Tezos_version.Version.pp_simple
           Tezos_version_value.Current_git_info.octez_version
           Tezos_version_value.Current_git_info.abbreviated_commit_hash
           Protocol_hash.pp_short
