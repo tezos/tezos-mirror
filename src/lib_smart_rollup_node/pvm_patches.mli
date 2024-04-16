cdd717c9ad152a61f5643eab59c0c8f6a269e4e7
Alain Mebsout <alain.mebsout@functori.com>
2024-04-16 11:33:26 +0200

    Rollup node: hardcoded PVM patches

--- src/lib_smart_rollup_node/pvm_patches.mli
+++ src/lib_smart_rollup_node/pvm_patches.mli
@@ -9,3 +9,13 @@
 type unsafe_patch =
   | Increase_max_nb_ticks of int64
       (** Increase the maximum number of ticks.  *)
+
+(** The type of registered patches for the PVM. *)
+type t = private unsafe_patch list
+
+(** Encoding for unsafe patches. *)
+val unsafe_patch_encoding : unsafe_patch Data_encoding.t
+
+(** [make address patches] builds the patches from the provided list [patches]
+    and adds the hardcoded PVM patches for the rollup [address]. *)
+val make : Address.t -> unsafe_patch list -> t
