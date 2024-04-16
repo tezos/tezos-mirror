0c5dccf355f5d3bf8fd5de97ff07aa4165383b34
Alain Mebsout <alain.mebsout@functori.com>
2024-04-16 14:32:40 +0200

    Rollup node: type unsafe pvm patch

---
+++ src/lib_smart_rollup_node/pvm_patches.ml
@@ -0,0 +1,8 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2024 Functori <contact@functori.com>                        *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type unsafe_patch = Increase_max_nb_ticks of int64
