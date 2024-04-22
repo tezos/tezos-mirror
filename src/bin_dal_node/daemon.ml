90f26f2f5bfa3ccbc65e4e59901621e8b518940b
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2024-04-22 09:54:16 +0200

    DAL: rename remove_old_level_shards
    Because it now also removes slots

--- src/bin_dal_node/daemon.ml
+++ src/bin_dal_node/daemon.ml
@@ -357,12 +357,12 @@ module Handler = struct
           return_unit
     else return_unit
 
-  (* This function removes the shards corresponding to the commitments at level
-     exactly [Node_context.next_level_to_gc ~head_level]. In the future
-     we may want to remove the shards from all preceeding levels, not only this
-     one. Also, removing could be done more efficiently than iterating on all
-     the slots. *)
-  let remove_old_level_shards proto_parameters ctxt head_level =
+  (* This function removes from the store all the slots (and their
+     shards) published at level exactly [Node_context.next_level_to_gc
+     ~head_level]. In the future we may want to remove the shards from
+     all preceeding levels, not only this one. Also, removing could be
+     done more efficiently than iterating on all the slots. *)
+  let remove_old_level_slots_and_shards proto_parameters ctxt head_level =
     let open Lwt_result_syntax in
     let oldest_level =
       Node_context.next_level_to_gc ctxt ~current_level:head_level
@@ -554,7 +554,9 @@ module Handler = struct
                cryptobox
                head_level
                proto_parameters.attestation_lag) ;
-          let* () = remove_old_level_shards proto_parameters ctxt head_level in
+          let* () =
+            remove_old_level_slots_and_shards proto_parameters ctxt head_level
+          in
           let process_block =
             process_block
               ctxt
