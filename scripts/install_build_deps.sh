456ded6c1cb4575ade35e2a0c437db61d72974ef
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2024-04-30 12:07:31 +0200

    [scripts/install_build_deps.sh]: clarify usage of [opam_repository]
    - The variable [opam_repository] is read from the environment but it
      is then overwritten -- we remove this read.
    - The variable [opam_repository] is renamed to [full_opam_repository]
      to avoid confusion.

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -10,7 +10,6 @@ src_dir="$(dirname "$script_dir")"
 
 # make sure that this variable is declared
 ocaml_version=${ocaml_version:?}
-opam_repository=${opam_repository:?}
 
 create_opam_switch() {
   [ -n "$1" ] || {
@@ -33,9 +32,9 @@ opamswitch="$OPAMSWITCH"
 unset OPAMSWITCH
 
 echo "Use opam repository commit: $full_opam_repository_tag"
-opam_repository="https://github.com/ocaml/opam-repository.git#$full_opam_repository_tag"
-opam repository set-url tezos --dont-select "$opam_repository" ||
-  opam repository add tezos --dont-select "$opam_repository" > /dev/null 2>&1
+full_opam_repository="https://github.com/ocaml/opam-repository.git#$full_opam_repository_tag"
+opam repository set-url tezos --dont-select "$full_opam_repository" ||
+  opam repository add tezos --dont-select "$full_opam_repository" > /dev/null 2>&1
 
 # Note: there should be no need to 'opam update' since 'opam repository add/set-url'
 # should have fetched already and the repository cannot change since it is
