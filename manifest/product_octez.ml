fd1ca84437dbe85d5e4e249acd8a0e1e5f8348b7
Killian Delarue <killian.delarue@nomadic-labs.com>
2024-04-30 11:00:29 +0200

    Manifest: Fix a license typo

--- manifest/product_octez.ml
+++ manifest/product_octez.ml
@@ -2,7 +2,7 @@
 (*                                                                           *)
 (* SPDX-License-Identifier: MIT                                              *)
 (* Copyright (c) 2021-2023 Nomadic Labs <contact@nomadic-labs.com>           *)
-(* Copyright (c) 2022-2024 TriliTech <contact@trili.tech>                   *)
+(* Copyright (c) 2022-2024 TriliTech <contact@trili.tech>                    *)
 (* Copyright (c) 2023 Marigold <contact@marigold.dev>                        *)
 (*                                                                           *)
 (*****************************************************************************)
