5bd4c19f67748aac8969a122a05661149c22030c
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2024-05-06 16:50:28 +0200

    EVM/Node: move "one or two" encoding to an helper module

---
+++ etherlink/bin_node/lib_dev/encodings/helpers.ml
@@ -0,0 +1,26 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2024 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+let encoding_with_optional_second_param encoding second_param_encoding
+    default_second_param =
+  let open Data_encoding in
+  let encoding = if is_tup encoding then encoding else tup1 encoding in
+  union
+    [
+      case
+        ~title:"with_second_param"
+        (Tag 0)
+        (merge_tups encoding (tup1 second_param_encoding))
+        (fun (t, second_param) -> Some (t, second_param))
+        (fun (t, second_param) -> (t, second_param));
+      case
+        ~title:"without_second_param"
+        (Tag 1)
+        encoding
+        (fun (t, _) -> Some t)
+        (fun t -> (t, default_second_param));
+    ]
