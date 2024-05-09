dde9221f9b5b692f118c04040c6e0e17ce776fde
Ryan Tan <ryan.tan@trili.tech>
2024-05-09 11:12:07 +0100

    Shell: expose http cache header tools from node

---
+++ src/lib_shell/http_cache_headers.ml
@@ -0,0 +1,40 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2024 TriliTech <contact@trili.tech>                         *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type tools = {
+  get_estimated_time_to_next_level : unit -> Ptime.span option Lwt.t;
+}
+
+(** [get_estimated_time_to_next_level chain_store] gets the estimated time 
+    to the next level of the main chain *)
+let get_estimated_time_to_next_level chain_store =
+  let open Lwt_option_syntax in
+  let*! block = Store.Chain.current_head chain_store in
+  let header = Store.Block.shell_header block in
+  let*! proto_hash = Store.Block.protocol_hash_exn chain_store block in
+  let*? (module Http_cache_headers) =
+    Protocol_plugin.find_http_cache_headers proto_hash
+  in
+  let* round_end =
+    Http_cache_headers.get_round_end_time
+      ~get_context:(fun () -> Store.Block.context_exn chain_store block)
+      header
+  in
+  let now = Time.System.now () in
+  if round_end < now then
+    (* If the round has ended, then the next level block is late.
+       We cannot be sure when it will arrive. *)
+    Lwt.return_none
+  else
+    let timespan = Ptime.diff round_end now in
+    return timespan
+
+let make_tools chain_store =
+  {
+    get_estimated_time_to_next_level =
+      (fun () -> get_estimated_time_to_next_level chain_store);
+  }
---
+++ src/lib_shell/http_cache_headers.mli
@@ -0,0 +1,16 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2024 TriliTech <contact@trili.tech>                         *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(**  Expose helper tools used for Http cache header middleware. *)
+
+type tools = {
+  (* [get_estimated_time_to_next_level ()] gets the estimated time
+     to the next level of the main chain *)
+  get_estimated_time_to_next_level : unit -> Ptime.span option Lwt.t;
+}
+
+val make_tools : Store.chain_store -> tools
