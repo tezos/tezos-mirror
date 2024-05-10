4ed735e03136f3c387816978fc3ad8f3b51c033d
Thomas Pécseli <thomas.pecseli@trili.tech>
2024-05-10 16:10:00 +0100

    RISC-V: PVM module for the rollup node

---
+++ src/lib_risc_v/pvm/backend.ml
@@ -0,0 +1,30 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2024 TriliTech <contact@trili.tech>                         *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type reveals = unit
+
+type write_debug = string -> unit Lwt.t
+
+type input_info
+
+type state = Storage.tree
+
+let compute_step_many ?reveal_builtins:_ ?write_debug:_ ?stop_at_snapshot:_
+    ~max_steps:_ state =
+  Lwt.return (state, 0L)
+
+let compute_step = Lwt.return
+
+let compute_step_with_debug ?write_debug:_ = Lwt.return
+
+let get_tick _state = Lwt.return (Z.of_int 0)
+
+type status = Riscv_dummy_status
+
+let get_status _state = Lwt.return Riscv_dummy_status
+
+let string_of_status _status = "riscv_dummy_status"
