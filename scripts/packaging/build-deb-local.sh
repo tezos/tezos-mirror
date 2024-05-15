b0e511dbc42debf31576b85ab376006437ba8a99
Pietro Abate <pietro.abate@nomadic-labs.com>
2024-05-15 13:51:19 +0200

    packaging post-19.1: remove leftovers when building packages locally

--- scripts/packaging/build-deb-local.sh
+++ scripts/packaging/build-deb-local.sh
@@ -11,12 +11,14 @@ make all
 # Prepare the packaging by copying all the freshly compiled binaries
 mkdir -p scripts/packaging/octez/binaries
 mkdir -p scripts/packaging/octez/zcash-params
+rm -Rf scripts/packaging/octez/binaries/*
 cp octez-* scripts/packaging/octez/binaries/
 
 # Copy the zcash parametes to be packaged
 cp -a _opam/share/zcash-params scripts/packaging/octez/
 
 # Build the debian packages
+rm -f scripts/packaging/*.deb
 cd scripts/packaging/octez/ &&
   DEB_BUILD_OPTIONS=noautodbgsym dpkg-buildpackage -b --no-sign -sa
 
