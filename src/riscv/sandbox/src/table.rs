1f33d28824c00cfbb4a94be5072af154d0bc975c
Felix Puscasu <felix.puscasu@trili.tech>
2024-05-15 16:04:40 +0100

    RISC-V: Benchmark with sorting options

--- src/riscv/sandbox/src/table.rs
+++ src/riscv/sandbox/src/table.rs
@@ -2,8 +2,13 @@
 //
 // SPDX-License-Identifier: MIT
 
-use self::utils::{prepend_cell, tableify_bench_stats, thousand_format, vec_cell_factory_ref};
-use crate::commands::bench::{BenchStats, NamedStats};
+use self::utils::{
+    prepend_cell, tableify_bench_stats, thousand_format, vec_cell_factory_ref, NamedBenchInstrStats,
+};
+use crate::{
+    cli::{SortInstr, SortRuns, TableSortArgs},
+    commands::bench::{BenchStats, NamedStats},
+};
 use comfy_table::{
     modifiers::UTF8_ROUND_CORNERS, presets::UTF8_FULL, Attribute, Cell, CellAlignment, Color,
     ContentArrangement, Table,
@@ -87,14 +92,14 @@ fn section_interpreter_stats(table: &mut Table, data: &[(&BenchStats, &String)])
     }
 }
 
-fn section_instruction_stats(table: &mut Table, data: &[(&BenchStats, &String)]) {
+fn section_instruction_stats(table: &mut Table, data: &[&[&NamedBenchInstrStats]]) {
     table.add_row(Vec::<Cell>::new());
 
     table.add_row(vec![Cell::new("Distinct Instructions").fg(Color::DarkGreen)]);
 
-    for instr_data in tableify_bench_stats(&data.iter().map(|d| d.0).collect_vec()) {
+    for instr_data in data {
         // instruction name + props
-        let name = match instr_data.iter().flatten().next() {
+        let name = match instr_data.iter().filter_map(|item| item.1).next() {
             Some(s) => &s.name,
             _ => "No instruction data",
         };
@@ -105,14 +110,78 @@ fn section_instruction_stats(table: &mut Table, data: &[(&BenchStats, &String)])
         ));
 
         // content
-        for (i, instr_stat) in instr_data.iter().enumerate() {
-            let bench_name = data[i].1;
-            table.add_row(content_stats_line_opt(bench_name, instr_stat));
+        for instr_stat in instr_data.iter() {
+            table.add_row(content_stats_line_opt(instr_stat.0, &instr_stat.1));
         }
     }
 }
 
-pub fn table_from_stats(data: &[(&BenchStats, &String)]) -> Table {
+/// Sort the instruction groups.
+fn sort_instruction_groups<'a, 'b, 'c>(
+    ordering: &SortInstr,
+    data: &'c [Vec<NamedBenchInstrStats<'a, 'b>>],
+) -> Vec<&'c Vec<NamedBenchInstrStats<'a, 'b>>> {
+    // Obtain an array of references to the lines that will be sorted.
+    let mut data = data.iter().collect_vec();
+
+    let instr_name = |instr_data: &Vec<NamedBenchInstrStats<'a, 'b>>| match instr_data.iter().next()
+    {
+        None => "No instruction data",
+        Some(el) => el.0,
+    };
+
+    let max_avg = |instr_data: &Vec<NamedBenchInstrStats>| {
+        instr_data.iter().map(|el| el.1.map(|e| e.average)).max()
+    };
+
+    let total_count = |instr_data: &Vec<NamedBenchInstrStats>| {
+        instr_data
+            .iter()
+            .map(|el| match el.1 {
+                None => 0,
+                Some(stats) => stats.count,
+            })
+            .sum::<usize>()
+    };
+
+    data.sort_by(|&a, &b| match ordering {
+        SortInstr::Alphabetic => instr_name(a).cmp(instr_name(b)),
+        SortInstr::MaxAvg => max_avg(a).cmp(&max_avg(b)).reverse(),
+        SortInstr::TotalCount => total_count(a).cmp(&total_count(b)).reverse(),
+    });
+
+    data
+}
+
+fn sort_runs_per_instr<'a, 'b, 'c>(
+    ordering: &SortRuns,
+    data: &[&'c Vec<NamedBenchInstrStats<'a, 'b>>],
+) -> Vec<Vec<&'c NamedBenchInstrStats<'a, 'b>>> {
+    let bench_name = |stats: &NamedBenchInstrStats<'a, 'b>| stats.0;
+
+    let average = |stats: &NamedBenchInstrStats| stats.1.map(|s| s.average);
+
+    let total = |stats: &NamedBenchInstrStats| stats.1.map(|s| s.total);
+
+    data.iter()
+        .map(|group| {
+            let mut group = group.iter().collect_vec();
+            group.sort_by(|&a, &b| match ordering {
+                SortRuns::Alphabetic => bench_name(a).cmp(bench_name(b)),
+                SortRuns::Input => std::cmp::Ordering::Equal,
+                SortRuns::Speed => average(a).cmp(&average(b)).reverse(),
+                SortRuns::Total => total(a).cmp(&total(b)).reverse(),
+            });
+            group
+        })
+        .collect_vec()
+}
+
+pub fn table_from_stats(sort_opts: &TableSortArgs, data: &[(&BenchStats, &String)]) -> Table {
+    let by_instr_data = tableify_bench_stats(data);
+    let sorted_data = sort_instruction_groups(&sort_opts.sort_instr, &by_instr_data);
+    let sorted_data = sort_runs_per_instr(&sort_opts.sort_runs, &sorted_data);
+
     let mut table = Table::new();
     table
         .load_preset(UTF8_FULL)
@@ -121,7 +190,10 @@ pub fn table_from_stats(data: &[(&BenchStats, &String)]) -> Table {
 
     section_summary(&mut table, data);
     section_interpreter_stats(&mut table, data);
-    section_instruction_stats(&mut table, data);
+    section_instruction_stats(
+        &mut table,
+        &sorted_data.iter().map(|line| line.as_slice()).collect_vec(),
+    );
 
     table
 }
