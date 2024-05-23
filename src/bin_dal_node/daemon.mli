43369876aa79c7a590a76f2bdcf58070001faaee
iguerNL@Functori <iguer@functori.com>
2024-05-23 07:19:19 +0200

    Dal/Node: add a daemon.mli file

---
+++ src/bin_dal_node/daemon.mli
@@ -0,0 +1,12 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* SPDX-FileCopyrightText: 2024 Functori, <contact@functori.com>             *)
+(* SPDX-FileCopyrightText: 2024 Nomadic Labs, <contact@nomadic-labs.com>     *)
+(*                                                                           *)
+(*****************************************************************************)
+
+val run :
+  data_dir:string ->
+  (Configuration_file.t -> Configuration_file.t) ->
+  unit tzresult Lwt.t
