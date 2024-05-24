9f663509517f9a857d2f16534031b3b2aa33a417
Albin Coquereau <albin.coquereau@nomadic-labs.com>
2024-05-24 09:26:33 +0200

    shell/block_validator: extract with_retry_to_load_protocol from
    on_validation_request function

--- src/lib_shell/block_validator.ml
+++ src/lib_shell/block_validator.ml
@@ -181,6 +181,26 @@ let check_operations_merkle_root hash header operations =
          found = computed_hash;
        })
 
+(* [with_retry_to_load_protocol bv peer f] tries to call [f], if it fails with an
+   [Unavailable_protocol] error, it fetches the protocol from the [peer] and retries
+   to call [f] *)
+let with_retry_to_load_protocol (bv : Types.state) ~peer f =
+  let open Lwt_syntax in
+  let* r = f () in
+  match r with
+  (* [Unavailable_protocol] is expected to be the
+     first error in the trace *)
+  | Error (Unavailable_protocol {protocol; _} :: _) ->
+      let* _ =
+        Protocol_validator.fetch_and_compile_protocol
+          bv.protocol_validator
+          ?peer
+          ~timeout:bv.limits.protocol_timeout
+          protocol
+      in
+      f ()
+  | _ -> Lwt.return r
+
 let on_validation_request w
     {
       Request.chain_db;
@@ -221,22 +241,6 @@ let on_validation_request w
                 let* pred =
                   Store.Block.read_block chain_store header.shell.predecessor
                 in
-                let with_retry_to_load_protocol f =
-                  let*! r = f () in
-                  match r with
-                  (* [Unavailable_protocol] is expected to be the
-                     first error in the trace *)
-                  | Error (Unavailable_protocol {protocol; _} :: _) ->
-                      let* _ =
-                        Protocol_validator.fetch_and_compile_protocol
-                          bv.protocol_validator
-                          ?peer
-                          ~timeout:bv.limits.protocol_timeout
-                          protocol
-                      in
-                      f ()
-                  | _ -> Lwt.return r
-                in
                 let*! mempool = Store.Chain.mempool chain_store in
                 let bv_operations =
                   List.map
@@ -248,7 +252,7 @@ let on_validation_request w
                 let*! r =
                   protect ~canceler:(Worker.canceler w) (fun () ->
                       protect ?canceler (fun () ->
-                          with_retry_to_load_protocol (fun () ->
+                          with_retry_to_load_protocol bv ~peer (fun () ->
                               validate_block
                                 bv.validation_process
                                 chain_db
@@ -270,7 +274,7 @@ let on_validation_request w
                       protect ~canceler:(Worker.canceler w) (fun () ->
                           protect ?canceler (fun () ->
                               let*! () = Events.(emit applying_block) hash in
-                              with_retry_to_load_protocol (fun () ->
+                              with_retry_to_load_protocol bv ~peer (fun () ->
                                   Block_validator_process.apply_block
                                     ~should_validate:false
                                     bv.validation_process
