db195ee7da2be1fd1c4cabcd5485879ef236c5d0
Romain <romain@nomadic-labs.com>
2024-05-28 11:06:55 +0200

    Scripts: format script:snapshot_alpha_and_link.sh

--- scripts/ci/script:snapshot_alpha_and_link.sh
+++ scripts/ci/script:snapshot_alpha_and_link.sh
@@ -2,7 +2,7 @@
 
 last_proto_name=$(find src -name "proto_[0-9][0-9][0-9]_*" | awk -F'/' '{print $$NF}' | sort -r | head -1)
 last_proto_version=$(echo "${last_proto_name}" | cut -d'_' -f2 | sed 's/^0*//')
-new_proto_version=$(printf "%03d" $(( last_proto_version + 1 )))
+new_proto_version=$(printf "%03d" $((last_proto_version + 1)))
 
 make octez-protocol-compiler
 ./scripts/snapshot_alpha_and_link.sh "${new_proto_version}" next
