ced7c5e0e14494df17fa7b21b3057c68b0fc3e98
Killian Delarue <killian.delarue@nomadic-labs.com>
2024-05-29 11:45:50 +0200

    Scripts, CI: Use [gitlab_release] in [docker_promote_to_latest.sh]

--- scripts/ci/docker_promote_to_latest.sh
+++ scripts/ci/docker_promote_to_latest.sh
@@ -10,18 +10,18 @@ current_dir=$(cd "$(dirname "${0}")" && pwd)
 # shellcheck source=./scripts/ci/docker.sh
 . "${current_dir}/docker.sh"
 
+# shellcheck source=./scripts/ci/octez-release.sh
+. ./scripts/ci/octez-release.sh
+
 ## The goal of this script is to retag existing Docker images (do not rebuild them)
 target_tag='latest'
 
-# Find tag with format vX.Y at branch HEAD
-tag_to_promote=$(git tag --points-at HEAD | grep -oE '^v[0-9]{1,3}\.[0-9]{1,3}$' || :)
-
-if [ -z "${tag_to_promote}" ]; then
-  echo "Error: could not find valid tag like vX.Y at branch HEAD"
+if [ -z "${gitlab_release}" ]; then
+  echo "Error: could not find valid tag like octez-vX.Y at branch HEAD"
   exit 1
 fi
 
-echo "### Promoting docker images with tag '${tag_to_promote}' to tag '${target_tag}'"
+echo "### Promoting docker images with tag '${gitlab_release}' to tag '${target_tag}'"
 
 # Loop over images
 for docker_image in ${docker_images}; do
@@ -30,8 +30,8 @@ for docker_image in ${docker_images}; do
   # Loop over architectures
   amends=''
   for docker_architecture in ${docker_architectures}; do
-    docker pull "${docker_image}:${docker_architecture}_${tag_to_promote}"
-    amends="${amends} --amend ${docker_image}:${docker_architecture}_${tag_to_promote}"
+    docker pull "${docker_image}:${docker_architecture}_${gitlab_release}"
+    amends="${amends} --amend ${docker_image}:${docker_architecture}_${gitlab_release}"
   done
 
   # Because of the variable amends, we use eval here to first construct the command
