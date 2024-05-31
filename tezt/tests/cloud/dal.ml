7d6c92d9d380b386c4573974f5a2ada3e8daa4ff
Anne-Laure <al.schmitt@protonmail.com>
2024-05-31 15:49:33 +0200

    Tezt/Test/Cloud/Dal: disconnection is handled in type t

--- tezt/tests/cloud/dal.ml
+++ tezt/tests/cloud/dal.ml
@@ -8,48 +8,69 @@
 module Cryptobox = Dal_common.Cryptobox
 module Helpers = Dal_common.Helpers
 
-module Disconnected = struct
-  (* The number of block during which the baker stays disconnected *)
-  let time_disconnected = 10
-
-  (* Each [disconnect_frequency] blocks, a baker is disconnected *)
-  let disconnect_frequency = 8
-
+module Disconnect = struct
   module IMap = Map.Make (Int)
 
-  (* The state contains bakers indexes that have been disconnected associated
-     with the level at where they have been disconnected *)
-  let state = ref IMap.empty
+  (* The [disconnected_bakers] map contains bakers indexes whose DAL node
+     have been disconnected, associated with the level at which they have
+     been disconnected.
+     Each [frequency] number of levels, a baker, chosen in a round-robin
+     fashion, is disconnected.
+     A disconnected baker reconnects after [reconnection_delay] levels.
+     The next baker to disconnect is stored in [next_to_disconnect];
+     it is 0 when no baker has been disconnected yet *)
+  type t = {
+    disconnected_bakers : int IMap.t;
+    frequency : int;
+    reconnection_delay : int;
+    next_to_disconnect : int;
+  }
 
-  (* The next baker to disconnected is stored there *)
-  let next_to_disconnect = ref 0
+  let init (frequency, reconnection_delay) =
+    {
+      disconnected_bakers = IMap.empty;
+      frequency;
+      reconnection_delay;
+      next_to_disconnect = 0;
+    }
 
-  (* This function only does something when a relevant level is reached. In
-     that case, the next baker to disconnect is added with the current level in
-     the state and [f] is applied that baker *)
-  let disconnect frequency level f =
-    if level mod frequency <> 0 then Lwt.return_unit
+  (* When a relevant level is reached, [disconnect t level f] puts the baker of
+     index [t.next_to_disconnect] in [t.disconnected_bakers] and applies [f] to
+     this baker. If it is already disconnected, the function does nothing and
+     returns [t] unchanged *)
+  let disconnect t level f =
+    if level mod t.frequency <> 0 then Lwt.return t
     else
-      match IMap.find_opt !next_to_disconnect !state with
-      | Some _ -> Lwt.return_unit
+      match IMap.find_opt t.next_to_disconnect t.disconnected_bakers with
+      | Some _ ->
+          Log.info
+            "disconnect: all bakers have been disconnected, waiting for next \
+             baker to reconnect." ;
+          Lwt.return t
       | None ->
-          state := IMap.add !next_to_disconnect level !state ;
-          let res = f !next_to_disconnect in
-          next_to_disconnect := !next_to_disconnect + 1 ;
-          res
+          let* () = f t.next_to_disconnect in
+          Lwt.return
+            {
+              t with
+              disconnected_bakers =
+                IMap.add t.next_to_disconnect level t.disconnected_bakers;
+              next_to_disconnect = t.next_to_disconnect + 1;
+            }
 
   (* Applies [f] on the bakers DAL nodes that have been disconnected for long
      enough *)
-  let reconnect reconnection_delay level f =
+  let reconnect t level f =
     let bakers_to_reconnect, bakers_to_keep_disconnected =
       IMap.partition
-        (fun _ disco_level -> level >= disco_level + reconnection_delay)
-        !state
+        (fun _ disco_level -> level >= disco_level + t.reconnection_delay)
+        t.disconnected_bakers
     in
-    state := bakers_to_keep_disconnected ;
-    IMap.to_seq bakers_to_reconnect
-    |> List.of_seq
-    |> Lwt_list.iter_p (fun (b, _) -> f b)
+    let* () =
+      IMap.to_seq bakers_to_reconnect
+      |> List.of_seq
+      |> Lwt_list.iter_p (fun (b, _) -> f b)
+    in
+    Lwt.return {t with disconnected_bakers = bakers_to_keep_disconnected}
 end
 
 module Cli = struct
@@ -267,6 +288,7 @@ type t = {
   parameters : Dal_common.Parameters.t;
   infos : (int, per_level_info) Hashtbl.t;
   metrics : (int, metrics) Hashtbl.t;
+  disconnection_state : Disconnect.t option;
 }
 
 let pp_metrics t
@@ -954,6 +976,9 @@ let init ~(configuration : configuration) cloud next_agent =
   let infos = Hashtbl.create 101 in
   let metrics = Hashtbl.create 101 in
   Hashtbl.replace metrics 1 default_metrics ;
+  let disconnection_state =
+    Option.map Disconnect.init configuration.disconnect
+  in
   Lwt.return
     {
       cloud;
@@ -965,6 +990,7 @@ let init ~(configuration : configuration) cloud next_agent =
       parameters;
       infos;
       metrics;
+      disconnection_state;
     }
 
 let wait_for_gossipsub_worker_event ~name dal_node lambda =
@@ -1015,22 +1041,25 @@ let on_new_level t level =
   pp_metrics t metrics ;
   push_metrics t metrics ;
   Hashtbl.replace t.metrics level metrics ;
-  match t.configuration.disconnect with
-  | None -> Lwt.return_unit
-  | Some (disconnect_frequency, time_disconnected) ->
+  match t.disconnection_state with
+  | None -> Lwt.return t
+  | Some disconnection_state ->
       let nb_bakers = List.length t.bakers in
-      let* () =
-        Disconnected.disconnect disconnect_frequency level (fun b ->
+      let* disconnection_state =
+        Disconnect.disconnect disconnection_state level (fun b ->
             let baker_to_disconnect =
               (List.nth t.bakers (b mod nb_bakers)).dal_node
             in
             Dal_node.terminate baker_to_disconnect)
       in
-      Disconnected.reconnect time_disconnected level (fun b ->
-          let baker_to_reconnect =
-            (List.nth t.bakers (b mod nb_bakers)).dal_node
-          in
-          connect_nodes_via_p2p t.bootstrap.dal_node baker_to_reconnect)
+      let* disconnection_state =
+        Disconnect.reconnect disconnection_state level (fun b ->
+            let baker_to_reconnect =
+              (List.nth t.bakers (b mod nb_bakers)).dal_node
+            in
+            connect_nodes_via_p2p t.bootstrap.dal_node baker_to_reconnect)
+      in
+      Lwt.return {t with disconnection_state = Some disconnection_state}
 
 let produce_slot t level i =
   let producer = List.nth t.producers i in
@@ -1075,7 +1104,7 @@ let rec loop t level =
       |> Seq.map (fun i -> produce_slot t level i)
       |> List.of_seq |> Lwt.join
   in
-  let* () = p in
+  let* t = p in
   loop t (level + 1)
 
 let configuration =
