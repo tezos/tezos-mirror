0e58d3a6c35b050674553c12d7ccbc8f76ef0e70
Killian Delarue <killian.delarue@nomadic-labs.com>
2024-05-31 11:28:52 +0200

    CI: Add [dependencies] option to [job_docker_promote_to_latest]

--- ci/bin/octez_latest_release.ml
+++ ci/bin/octez_latest_release.ml
@@ -14,13 +14,14 @@
 open Tezos_ci
 open Common
 
-let job_docker_promote_to_latest ~ci_docker_hub : tezos_job =
+let job_docker_promote_to_latest ?dependencies ~ci_docker_hub () : tezos_job =
   job_docker_authenticated
     ~__POS__
+    ?dependencies
     ~stage:Stages.publish_release
     ~name:"docker:promote_to_latest"
     ~ci_docker_hub
     ["./scripts/ci/docker_promote_to_latest.sh"]
 
 let jobs ?(test = false) () =
-  [job_docker_promote_to_latest ~ci_docker_hub:(not test)]
+  [job_docker_promote_to_latest ~ci_docker_hub:(not test) ()]
