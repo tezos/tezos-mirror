bd2d4674ec2149125f98847014d3404758a8c9a9
Emma Turner <emma.turner@trili.tech>
2024-05-31 13:51:54 +0100

    RISC-V: jstz-bench static opt

--- src/riscv/scripts/jstz-bench.sh
+++ src/riscv/scripts/jstz-bench.sh
@@ -8,22 +8,28 @@
 
 set -e
 
-TX=""
+USAGE="Usage: -t <num_transfers> [ -s: static inbox ]"
 
-while getopts "t:" OPTION; do
+TX=""
+STATIC_INBOX=""
+
+while getopts "st:" OPTION; do
   case "$OPTION" in
   t)
     TX="$OPTARG"
     ;;
+  s)
+    STATIC_INBOX="y"
+    ;;
   *)
-    echo "Usage: -t <num_transfers>"
+    echo "$USAGE"
     exit 1
     ;;
   esac
 done
 
 if [ -z "$TX" ]; then
-  echo "Usage: -t <num_transfers>"
+  echo "$USAGE"
   exit 1
 fi
 
@@ -33,23 +39,34 @@ RISCV_DIR=$(dirname "$0")/..
 
 cd "$RISCV_DIR"
 
-echo "[INFO]: building deps"
+echo "[INFO]: building sandbox"
 make riscv-sandbox &> /dev/null
-make -C jstz build &> /dev/null
+echo "[INFO]: building bench tool"
+make -C jstz inbox-bench &> /dev/null
 
 DATA_DIR=${DATA_DIR:=$(mktemp -d)}
 
 echo "[INFO]: generating $TX transfers"
 INBOX_FILE="${DATA_DIR}/inbox.json"
+RUN_INBOX="$INBOX_FILE"
 ./jstz/inbox-bench generate --inbox-file "$INBOX_FILE" --transfers "$TX"
+
+echo "[INFO]: building jstz"
+if [ "$STATIC_INBOX" = "y" ]; then
+  INBOX_FILE="$INBOX_FILE" make -C jstz build-kernel-static &> /dev/null
+  RUN_INBOX="$DATA_DIR"/empty.json
+  echo "[]" > "$RUN_INBOX"
+else
+  make -C jstz build-kernel &> /dev/null
+fi
 
 echo "[INFO]: running $TX transfers"
 LOG="${DATA_DIR}/log.out"
 ./riscv-sandbox rvemu \
   --input ../../tezt/tests/riscv-tests/hermit-loader \
   --initrd jstz/target/riscv64gc-unknown-hermit/release/jstz \
-  --inbox-file "$INBOX_FILE" \
-  --timings > "$LOG"
+  --inbox-file "$RUN_INBOX" \
+  --timings > "$LOG" 2> /dev/null
 
 echo "[INFO]: collecting results"
 echo -e "\033[1m"
