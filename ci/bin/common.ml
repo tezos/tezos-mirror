775a0dd0eb4cb9a7b9c342b63e228c96c2f15e5b
Romain <romain@nomadic-labs.com>
2024-06-03 07:52:56 +0000

    CI: recursively check for changes in the manifest

--- ci/bin/common.ml
+++ ci/bin/common.ml
@@ -347,7 +347,7 @@ let changeset_opam_jobs =
         "**/*.opam";
         ".gitlab/ci/jobs/packaging/opam:prepare.yml";
         ".gitlab/ci/jobs/packaging/opam_package.yml";
-        "manifest/*.ml*";
+        "manifest/**/*.ml*";
         "scripts/opam-prepare-repo.sh";
         "scripts/version.sh";
       ])
