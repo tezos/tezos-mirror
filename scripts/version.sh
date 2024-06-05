6773d0acc1f72d82775644744fad30a045b6c292
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2024-06-05 09:05:43 +0200

    [images/opam-repository]: remove [scripts/version.sh] redundancy
    This commit removes the reduncancies between
    [images/opam-repository/scripts/version.sh] and [scripts/version.sh]
    by merging the former into the latter.
    
    The variable [$opam_repository_commit_hash] from
    [images/opam-repository/scripts/version.sh] equals
    [$full_opam_repository_tag] from [scripts/version.], and
    this[images/opam-repository/scripts/create_opam_repo.sh] is adapted to
    use the latter.

--- scripts/version.sh
+++ scripts/version.sh
@@ -22,12 +22,6 @@ export opam_version_major=2
 export recommended_rust_version=1.74.0
 export recommended_node_version=18.18.2
 
-# The Alpine minor version used to build the opam-repository images
-# and used to run the `trigger` job in the CI. This value SHOULD
-# correspond to the Alpine minor version given by the `trigger` job's
-# `image:`.
-export alpine_version='3.19'
-
 ## full_opam_repository is a commit hash of the public opam repository, i.e.
 ## https://github.com/ocaml/opam-repository
 export full_opam_repository_tag=0bdad08fb7e8f889d600aa06619fdb88cd179258
@@ -46,7 +40,36 @@ export opam_repository_tag="${OPAM_REPOSITORY_TAG:-b114b38e93159016d4f956dc9fce8
 export dal_srsu_g1_sha=c48ce4add1de2a7561108f17bf0c16bc1e93c0bff24bc7da465c24e0b4b2653e
 export dal_srsu_g2_sha=e7fbe747ae3648a5b664d8f8bd7c524996f7ed07f3331f905d2e73767d580f7c
 
+##
+## Versions installed in the images/opam-repository
+##
+
+# The builds of these image also rely on variables set above.
+
+# The Alpine minor version used to build the opam-repository images
+# and used to run the `trigger` job in the CI. This value SHOULD
+# correspond to the Alpine minor version given by the `trigger` job's
+# `image:`.
+export alpine_version='3.19'
+
+# Installed via apk rust
+export rust_version='1.76.0'
+
+# Installed via apk cargo
+export cargo_version='1.76.0'
+
+# Installed via apk rust
+export opam_version='2.1.5'
+
+# Installed via apk python3-dev
+export python_version='3.11.9'
+
+# Installed via apk
+export poetry_version='1.7.1'
+
+##
 ## Other variables, used both in Makefile and scripts
+##
 export COVERAGE_OUTPUT=_coverage_output
 
 ##
