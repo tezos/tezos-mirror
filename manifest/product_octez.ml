f1c63cee714ba942f18fe6aab9f87c68cda9b55d
Romain <romain@nomadic-labs.com>
2024-06-05 14:32:53 +0200

    Revert "Build: Build Rust dependencies with immutable lock file to improve error messages"
    This reverts commit 64a5dbdb3830fbb8a7ab0f15ddcf2443d1e49a7e.
    
    This is because this commit depends on the actual commit that we want to revert.

--- manifest/product_octez.ml
+++ manifest/product_octez.ml
@@ -606,14 +606,12 @@ let octez_rust_deps =
                   S "progn";
                   of_atom_list
                     ["run"; "rm"; "-f"; "wasmer-3.3.0/lib/c-api/wasmer.h"];
-                  of_atom_list
-                    ["run"; "cargo"; "build"; "--release"; "--locked"];
+                  of_atom_list ["run"; "cargo"; "build"; "--release"];
                   [
                     S "setenv";
                     S "OBJCOPY";
                     S "objcopy";
-                    of_atom_list
-                      ["run"; "cargo"; "run"; "--release"; "--locked"];
+                    of_atom_list ["run"; "cargo"; "run"; "--release"];
                   ];
                   of_atom_list ["run"; "strip"; "-x"; "liboctez_rust_deps.a"];
                   of_atom_list
