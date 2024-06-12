5e28794d403775eaf71a80d98ccac62d89d4f40f
Victor Allombert <victor.allombert@nomadic-labs.com>
2024-06-12 15:47:29 +0200

    Devtools: improve RPC bench progress display

--- devtools/benchmarks-tools/bench_RPS/rps.sh
+++ devtools/benchmarks-tools/bench_RPS/rps.sh
@@ -30,25 +30,26 @@ for id in $(seq 1 1 "$2"); do
   mkdir -p "$metrics_output"
 
   for concurrency in 1 10; do
-    echo "-------------"
+    echo "------------------------------------------------------------------------------------"
+
     echo "Concurrency=$concurrency"
 
-    echo "Health:"
+    echo "Health (run $id/$2):"
     $cassowary -t "$timeout" -u http://127.0.0.1:8732/health/ready --json-metrics --json-metrics-file="$metrics_output/health-$concurrency-th.json" -c "$concurrency" -n 100000 -H "$header"
 
-    echo "Header:"
+    echo "Header (run $id/$2):"
     $cassowary -t "$timeout" -u http://127.0.0.1:8732/chains/main/blocks/header --json-metrics --json-metrics-file="$metrics_output/header-$concurrency-th.json" -c "$concurrency" -n 100000 -H "$header"
 
-    echo "Baking rights:"
+    echo "Baking rights (run $id/$2):"
     $cassowary -t "$timeout" -u http://127.0.0.1:8732/chains/main/blocks/head/helpers/baking_rights --json-metrics --json-metrics-file="$metrics_output/baking-$concurrency-th.json" -c "$concurrency" -n 10000 -H "$header"
 
-    echo "Attestation rights:"
+    echo "Attestation rights (run $id/$2):"
     $cassowary -t "$timeout" -u http://127.0.0.1:8732/chains/main/blocks/head/helpers/attestation_rights --json-metrics --json-metrics-file="$metrics_output/attestation-$concurrency-th.json" -c "$concurrency" -n 1000 -H "$header"
 
   done
 
   echo "-------------"
-  echo -n "RPS: "
+  echo -n "RPS of run $id: "
   cat "$metrics_output"/*.json | jq .requests_per_second | awk '{sum+=$0;count+=1} END{print sum/count}'
 
 done
