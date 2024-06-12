98931b9a30818e9e49b94818a7717731d4f09bd4
Ole Krüger <ole.kruger@trili.tech>
2024-06-12 13:57:42 +0100

    RISC-V: Remove ExecEnv trait

--- src/riscv/lib/src/exec_env.rs
+++ src/riscv/lib/src/exec_env.rs
@@ -5,24 +5,6 @@
 pub mod posix;
 pub mod pvm;
 
-use crate::{
-    machine_state::{bus::main_memory::MainMemoryLayout, MachineState},
-    state_backend::{self, AllocatedOf, Manager},
-    traps::EnvironException,
-};
-
-/// An execution environment
-pub trait ExecutionEnvironment {
-    /// Layout of the EE state
-    type Layout: state_backend::Layout;
-
-    /// EE state type
-    type State<M: Manager>: ExecutionEnvironmentState<M, ExecutionEnvironment = Self>;
-
-    /// Runtime configuration
-    type Config<'a>: Default;
-}
-
 /// Outcome of handling an ECALL
 #[derive(Debug)]
 pub enum EcallOutcome {
@@ -31,26 +13,4 @@ pub enum EcallOutcome {
 
     /// ECALL was handled successfully
     Handled { continue_eval: bool },
-}
-
-/// State for an execution environment
-pub trait ExecutionEnvironmentState<M: Manager> {
-    /// Matching execution environment
-    type ExecutionEnvironment: ExecutionEnvironment;
-
-    /// Bind the EE state to the allocated regions.
-    fn bind(
-        space: AllocatedOf<<Self::ExecutionEnvironment as ExecutionEnvironment>::Layout, M>,
-    ) -> Self;
-
-    /// Reset the EE state.
-    fn reset(&mut self);
-
-    /// Handle a call to the execution environment.
-    fn handle_call<ML: MainMemoryLayout>(
-        &mut self,
-        machine: &mut MachineState<ML, M>,
-        config: &mut <Self::ExecutionEnvironment as ExecutionEnvironment>::Config<'_>,
-        exception: EnvironException,
-    ) -> EcallOutcome;
 }
