a3676976ff1f8a341344117f65531d0651a48276
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2024-06-14 15:23:43 +0200

    CIAO: can add dune cache

--- ci/bin/common.ml
+++ ci/bin/common.ml
@@ -300,6 +300,40 @@ let enable_cargo_cache job =
   (* Allow Cargo to access the network *)
   |> enable_networked_cargo
 
+(** Add variable enabling dune cache.
+
+    This function can be applied to jobs that run dune.
+
+    - [key] and [path] configure the key under which the cache is
+    stored, and the path that will be cached. By default, the [key]
+    contains the name of the job, thus scoping the cache to all
+    instances of that job. By default, [path] is the folder
+    ["$CI_PROJECT_DIR/_dune_cache"], and this function also sets the
+    environment dir [DUNE_CACHE_ROOT] such that dune stores its caches
+    there.
+
+    - [copy_mode], if [true] (default is [false]) sets
+    {{:https://dune.readthedocs.io/en/stable/caching.html#cache-storage-mode}Dune
+    Cache Storage Mode} to [copy]. If [false], [hardlink] mode is
+    used, which is typically more performant but requires that the
+    build and cache folder be on the same volume. *)
+let enable_dune_cache ?key ?(path = "$CI_PROJECT_DIR/_dune_cache")
+    ?(copy_mode = false) ?policy job =
+  let key =
+    Option.value
+      ~default:
+        ("dune_cache-" ^ Gitlab_ci.Predefined_vars.(show ci_job_name_slug))
+      key
+  in
+  job
+  |> append_variables
+       [
+         ("DUNE_CACHE", "enabled");
+         ("DUNE_CACHE_STORAGE_MODE", if copy_mode then "copy" else "hardlink");
+         ("DUNE_CACHE_ROOT", path);
+       ]
+  |> append_cache (cache ?policy ~key [path])
+
 (** {2 Changesets} *)
 
 (** Modifying these files will unconditionally execute all conditional jobs. *)
