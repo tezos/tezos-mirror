6958a2c55fed593c039f5b4add01a284a78613c7
Nic Volanschi <nic.volanschi@nomadic-labs.com>
2024-06-14 15:15:49 +0200

    doc: switch to sphinx-book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ release = (
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -109,19 +109,21 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+#html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'logo_only': True, 'sticky_navigation': False}
+#html_theme_options = {'logo_only': True, 'sticky_navigation': False}
+
 html_logo = "logo.svg"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_title = "Octez & Protocol products documentation"
 html_css_files = [
     'css/custom.css',
 ]
