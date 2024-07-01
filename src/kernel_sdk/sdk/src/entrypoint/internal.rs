398d70cc78b4d37ff00f0d841034d5c3cade0fcc
Felix Puscasu <felix.puscasu@trili.tech>
2024-07-01 15:51:49 +0100

    Kernel/SDK: compilation of kernels to native

--- src/kernel_sdk/sdk/src/entrypoint/internal.rs
+++ src/kernel_sdk/sdk/src/entrypoint/internal.rs
@@ -7,7 +7,3 @@ mod static_input_host;
 
 #[cfg(feature = "extra")]
 pub use static_input_host::StaticInbox;
-
-// Used by the entrypoint::main proc-macro
-pub use tezos_smart_rollup_entrypoint::set_panic_hook;
-pub use tezos_smart_rollup_entrypoint::RollupHost;
