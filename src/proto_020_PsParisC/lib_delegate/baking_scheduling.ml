0830504a2d2bcf4f4e0ea668c0b0511af443e0a3
Albin Coquereau <albin.coquereau@nomadic-labs.com>
2024-07-08 14:14:15 +0200

    parisC/baker: add an event at baker start to list the delegate that will be used

--- src/proto_020_PsParisC/lib_delegate/baking_scheduling.ml
+++ src/proto_020_PsParisC/lib_delegate/baking_scheduling.ml
@@ -941,6 +941,7 @@ let run cctxt ?canceler ?(stop_on_event = fun _ -> false)
     ?(on_error = fun _ -> Lwt_result_syntax.return_unit) ?constants ~chain
     config delegates =
   let open Lwt_result_syntax in
+  let*! () = Events.(emit Baking_events.Delegates.delegates_used delegates) in
   let* chain_id = Shell_services.Chain.chain_id cctxt ~chain () in
   let* () = perform_sanity_check cctxt ~chain_id in
   let cache = Baking_cache.Block_cache.create 10 in
