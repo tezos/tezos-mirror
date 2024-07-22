b8ef4ab3cae5bbe03b26474808e343ab9de8d2a5
Pietro Abate <pietro.abate@nomadic-labs.com>
2024-07-22 13:06:31 +0200

    packaging next: split octez data packages

--- scripts/packaging/build-deb-local.sh
+++ scripts/packaging/build-deb-local.sh
@@ -1,31 +1,66 @@
 #!/bin/sh
 
+TYPE=${1}
+
 set -eu
 
-# Build tezos as usual
-# shellcheck disable=SC1091
-. "$HOME/.cargo/env"
-eval "$(opam env)"
-make all
+packages() {
+  # Build tezos as usual
+  # shellcheck disable=SC1091
+  . "$HOME/.cargo/env"
+  eval "$(opam env)"
+  make all
 
-# Prepare the packaging by copying all the freshly compiled binaries
-mkdir -p scripts/packaging/octez/binaries
-mkdir -p scripts/packaging/octez/zcash-params
-rm -Rf scripts/packaging/octez/binaries/*
+  # Prepare the packaging by copying all the freshly compiled binaries
+  mkdir -p scripts/packaging/octez/binaries
+  mkdir -p scripts/packaging/octez/zcash-params
+  rm -Rf scripts/packaging/octez/binaries/*
 
-EXECUTABLES=$(cat script-inputs/*-executables)
-for ex in $EXECUTABLES; do
-  cp "$ex" scripts/packaging/octez/binaries/
-done
+  EXECUTABLES=$(cat script-inputs/*-executables)
+  for ex in $EXECUTABLES; do
+    cp "$ex" scripts/packaging/octez/binaries/
+  done
 
-# Copy the zcash parametes to be packaged
-cp -a _opam/share/zcash-params scripts/packaging/octez/
+  # Build the debian packages
+  cd scripts/packaging/octez/
+  DEB_BUILD_OPTIONS=noautodbgsym dpkg-buildpackage -tc -b --no-sign -sa
+  cd -
+}
 
-# Build the debian packages
+zcash() {
+  # Link the zcash dans DAL parametes to be packaged
+  if [ ! -e scripts/packaging/octez-data/zcash-params ]; then
+    ln -s ../../../_opam/share/zcash-params scripts/packaging/octez-data/
+  fi
+
+  if [ ! -e scripts/packaging/octez-data/dal-trusted-setup ]; then
+    scripts/install_dal_trusted_setup.sh
+    ln -s ../../../_opam/share/dal-trusted-setup scripts/packaging/octez-data/
+  fi
+
+  cd scripts/packaging/octez-data
+  DEB_BUILD_OPTIONS=noautodbgsym dpkg-buildpackage -tc -b --no-sign -sa
+  cd -
+
+}
+
+# Cleanup old leftover packages
 rm -f scripts/packaging/*.deb
-cd scripts/packaging/octez/
-DEB_BUILD_OPTIONS=noautodbgsym dpkg-buildpackage -tc -b --no-sign -sa
-cd -
+
+case $TYPE in
+"binaries")
+  echo "Building binary packages only"
+  packages
+  ;;
+"zcash")
+  echo "Building DATA packages only"
+  zcash
+  ;;
+*)
+  zcash
+  packages
+  ;;
+esac
 
 echo "All packages are available in ./scripts/packaging"
 
