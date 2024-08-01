8f77e5d9f770f646bbcd8ca98c04965858079488
Anne-Laure <al.schmitt@protonmail.com>
2024-08-01 15:44:55 +0200

    Kzg/Kate_amortized: add some comments

--- src/lib_kzg/kate_amortized.mli
+++ src/lib_kzg/kate_amortized.mli
@@ -1,5 +1,9 @@
 open Bls
 
+(** Public_parameters are needed for commit, preprocess, prove & verify
+    Notably the type is the same for both prover & verifier, but the needed SRS
+    size is shorter for the verifier (= max_polynomial_length - shard_length)
+    than for the prover (= max_polynomial_length) *)
 type public_parameters = {
   max_polynomial_length : int;
   shard_length : int;
@@ -7,6 +11,8 @@ type public_parameters = {
   number_of_shards : int;
 }
 
+(** The preprocess is the SRS under a certain form. It is a necessary step for
+    proving *)
 type preprocess
 
 val preprocess_encoding : preprocess t
@@ -15,6 +21,7 @@ type shard_proof = Commitment.Single_G1.t
 
 type commitment = Commitment.Single_G1.t
 
+(** Comparison function for preprocess, used for test purposes *)
 val preprocess_equal : preprocess -> preprocess -> bool
 
 val commit : public_parameters -> Poly.t -> commitment
@@ -37,6 +44,9 @@ val verify :
   proof:shard_proof ->
   bool
 
+(** Verifies shard proofs in a batch : compared to using verify several times,
+    this reduces the time spend to commit (only one commit for all proofs)
+    and the time in pairing (only one pairing for all proofs). *)
 val verify_multi :
   public_parameters ->
   commitment:commitment ->
