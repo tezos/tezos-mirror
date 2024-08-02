c31a39f7b0308bb07a140eb195540c96a825020e
arnaud <arnaud.bihan@functori.com>
2024-08-02 16:41:30 +0200

    EVM/Node: Compute gas for da_fees with access list

--- etherlink/bin_node/lib_dev/fees.ml
+++ etherlink/bin_node/lib_dev/fees.ml
@@ -6,7 +6,7 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let gas_for_fees ?da_fee_per_byte ~gas_price tx_data =
+let gas_for_fees ?da_fee_per_byte ~gas_price ?(access_list = []) tx_data =
   (* The computation of the gas_for_fees comes from the kernel in fees.rs
      in the gas_for_fees function *)
   (* Constants defined in the kernel: *)
@@ -20,7 +20,22 @@ let gas_for_fees ?da_fee_per_byte ~gas_price tx_data =
   in
   (* Computation of da fee based on da fee per byte and variable tx data. *)
   let da_fee da_fee_per_byte tx_data =
-    let size = Bytes.length tx_data + assumed_tx_encoded_size |> Z.of_int in
+    (* Compute access_list size for da_fees *)
+    let size_address = Z.of_int 20 in
+    let size_slot = Z.of_int 32 in
+    let access_list_size =
+      List.fold_left
+        (fun acc (_, slots) ->
+          let nb_slots = Z.of_int (List.length slots) in
+          let slots_cost = Z.mul size_slot nb_slots in
+          Z.add (Z.add acc slots_cost) size_address)
+        Z.zero
+        access_list
+    in
+    let size_wo_access_list =
+      Bytes.length tx_data + assumed_tx_encoded_size |> Z.of_int
+    in
+    let size = Z.add size_wo_access_list access_list_size in
     Z.mul da_fee_per_byte size
   in
   let fees = da_fee da_fee_per_byte tx_data in
--- etherlink/bin_node/lib_dev/fees.mli
+++ etherlink/bin_node/lib_dev/fees.mli
@@ -6,10 +6,14 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** [gas_for_fees ~da_fee_per_byte ~gas_price tx_data] returns the DA fees,
-      i.e. the gas unit necessary for the data availability.
+(** [gas_for_fees ~da_fee_per_byte ~gas_price ?access_list tx_data] returns
+      the DA fees, i.e. the gas unit necessary for the data availability.
 
       The whole point of this function is to avoid an unncessary call
       to the WASM PVM to improve the performances. *)
 val gas_for_fees :
-  ?da_fee_per_byte:Ethereum_types.quantity -> gas_price:Z.t -> bytes -> Z.t
+  ?da_fee_per_byte:Ethereum_types.quantity ->
+  gas_price:Z.t ->
+  ?access_list:Transaction.access_list_item list ->
+  bytes ->
+  Z.t
