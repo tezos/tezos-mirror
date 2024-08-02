dd7a79b0353c0784c24f98232f8e60b7333174e4
Pietro Abate <pietro.abate@nomadic-labs.com>
2024-08-02 12:51:31 +0200

    packages next: preseed octez-node configuration

--- docs/introduction/install-bin-deb.sh
+++ docs/introduction/install-bin-deb.sh
@@ -63,12 +63,31 @@ set -x
 
 # [install prerequisites]
 apt-get update
-apt-get install -y sudo gpg curl
+apt-get install -y sudo gpg curl debconf-utils
 # [add repository]
 REPO="deb https://$bucket.storage.googleapis.com/$distribution $release main"
-sudo curl "https://$bucket.storage.googleapis.com/$distribution/octez.asc" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/octez.gpg
+curl "https://$bucket.storage.googleapis.com/$distribution/octez.asc" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/octez.gpg
 echo "$REPO" | sudo tee /etc/apt/sources.list.d/octez.list
 sudo apt-get update
+
+# [ preeseed octez ]
+if [ -n "$PREFIX" ]; then
+  # preseed octez-node for debconf. Notice we set purge_warning to yes,
+  # to make the `autopurge` pass and remove all the node data at the end of this
+  # script.
+  cat << EOF > preseed.cfg
+octez-node octez-node/configure string yes
+octez-node octez-node/history-mode string full
+octez-node octez-node/network string mainnet
+octez-node octez-node/purge_warning string yes
+debconf debconf/frontend select Noninteractive
+EOF
+  # preseed the package
+  sudo debconf-set-selections preseed.cfg
+
+  # check the package configuration
+  sudo debconf-get-selections | grep octez
+fi
 
 # [install tezos]
 sudo apt-get install -y octez-client
@@ -76,6 +95,7 @@ sudo apt-get install -y octez-node
 sudo apt-get install -y octez-baker
 sudo apt-get install -y octez-dal-node
 
+# [install octez additional packages]
 if [ -n "$PREFIX" ]; then
   # [install octez NEXT packages]
   sudo apt-get install -y octez-smart-rollup-node
@@ -91,5 +111,31 @@ octez-node --version
 "octez-baker-$protocol" --version
 "octez-accuser-$protocol" --version
 
-# [test autoremove]
-sudo apt autoremove -y octez-node octez-client octez-baker octez-dal-node
+# [test autopurge]
+sudo apt-get autopurge -y octez-node octez-client octez-baker octez-dal-node
+
+# [check autopurge]
+if [ -n "$PREFIX" ]; then
+  # check the package configuration
+  sudo debconf-get-selections | if grep -q octez ; then \
+    echo "Leftovers in debconf db"; \
+    exit 1 ; \
+  fi
+
+  printf "Check if the user tezos was removed:"
+  if id tezos > /dev/null 2>&1; then
+    echo "Tezos user not correctly removed"
+    id tezos
+    exit 1
+  else
+    echo "Ok."
+  fi
+
+  printf "Check if the datadir was correctly removed:"
+  if [ -e /var/tezos ]; then
+    echo "Datadir /var/tezos not correctly removed"
+    ls -la /var/tezos
+  else
+    echo "Ok."
+  fi
+fi
