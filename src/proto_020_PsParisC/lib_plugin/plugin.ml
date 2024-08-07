c245764d3d83ef4d17311c161eabfb1c5d8bfa55
Victor Allombert <victor.allombert@nomadic-labs.com>
2024-08-07 10:00:48 +0200

    Plugin/020_PsParisC: backport Shell_helpers protocol plugin

--- src/proto_020_PsParisC/lib_plugin/plugin.ml
+++ src/proto_020_PsParisC/lib_plugin/plugin.ml
@@ -31,3 +31,4 @@ module RPC = RPC
 module Metrics = Metrics_plugin
 module Script_interpreter_logging = Script_interpreter_logging
 module Http_cache_headers = Http_cache_headers
+module Shell_helpers = Shell_helpers
