a3d7257976126f3e4b6d67ced0e48393aa5e56f2
Killian Delarue <killian.delarue@nomadic-labs.com>
2024-08-08 16:45:47 +0200

    Quebec/src: adapt Quebec predecessors

--- src/proto_021/lib_protocol/raw_context.mli
+++ src/proto_021/lib_protocol/raw_context.mli
@@ -95,8 +95,8 @@ val prepare :
 
 type previous_protocol =
   | Genesis of Parameters_repr.t
-  | Beta
-  | (* Beta predecessor *) ParisC_020 (* Beta predecessor *)
+  | Quebec
+  | (* Quebec predecessor *) ParisC_020 (* Quebec predecessor *)
 
 val prepare_first_block :
   level:int32 ->
