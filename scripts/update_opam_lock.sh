6c4be4bc0d121e94028db6f33402c2b58e8fc1f3
Thomas Letan <lthms@nomadic-labs.com>
2024-08-15 22:23:29 +0200

    Deps: Fix `update_opam_lock.sh` to work on macOS too
    GNU sed and BSD sed, while sharing a binary name, are not compatible
    wrt. options (such as -i) or more complex regexps.
    
    Instead we use the editor ed which is on all unixes, to simply delete
    the lines we want to remove, matching the given pattern.
    
    Co-authored-by: Alain Mebsout <alain.mebsout@functori.com>

--- scripts/update_opam_lock.sh
+++ scripts/update_opam_lock.sh
@@ -106,8 +106,11 @@ opam lock opam/virtual/octez-deps.opam
 # depends on system libraries. We need to remove them from our lockfile,
 # otherwise they won’t be portable.
 # See https://github.com/ocaml/opam-repository/commit/0b240d2960133fd3d8aa8f008d7aa79534caa3b9
-sed -i -E 's/.*"host-arch-.*?".*//' octez-deps.opam.locked
-sed -i -E 's/.*"host-system-.*?".*//' octez-deps.opam.locked
+ed -s octez-deps.opam.locked << EOF
+g/host-arch-/d
+g/host-system-/d
+w
+EOF
 
 mv octez-deps.opam.locked opam/virtual
 echo "---- Updated: opam/virtual/octez-deps.opam.locked"
