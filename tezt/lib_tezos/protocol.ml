c279b5b46e9192f990f21a93031643d8689f0577
Julien Tesson <julien.tesson@nomadic-labs.com>
2024-08-21 13:23:14 +0200

    proto/beta: Reset beta protocol hash after reformat

--- tezt/lib_tezos/protocol.ml
+++ tezt/lib_tezos/protocol.ml
@@ -60,7 +60,7 @@ let tag protocol = String.lowercase_ascii (name protocol)
 let hash = function
   | Alpha -> "ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK"
   | ParisC -> "PsParisCZo7KAh1Z1smVd9ZMZ1HHn5gkzbM94V3PLCpknFWhUAi"
-  | Beta -> "PtQ2DrEGKLgFVzti6LSwK9c6SHHANWi1Wdzy4X4AY2BGoazwaAW"
+  | Beta -> "PsPxPKEtaNmZbEArSYm2HdPk419cHLqe8DpkxDmMWpToKHwu2Bg"
 (* DO NOT REMOVE, AUTOMATICALLY ADD STABILISED PROTOCOL HASH HERE *)
 
 let genesis_hash = "ProtoGenesisGenesisGenesisGenesisGenesisGenesk612im"
