8874d4b7d6aafdf3789b92fe8d0ade7615c81c3d
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2024-08-29 15:20:33 +0200

    EVM/Kernel: block hash opcode use index storage
    Why would we duplicate this information

--- etherlink/kernel_evm/evm_execution/src/storage.rs
+++ etherlink/kernel_evm/evm_execution/src/storage.rs
@@ -121,7 +121,7 @@ pub mod tracer {
 
 /// API to interact with blocks storage
 pub mod blocks {
-    use host::path::{OwnedPath, Path};
+    use host::path::OwnedPath;
     use host::runtime::{Runtime, RuntimeError};
     use primitive_types::{H256, U256};
     use thiserror::Error;
@@ -164,23 +164,6 @@ pub mod blocks {
     // (opcode 0x40: BLOCKHASH)
     const BLOCKS_STORED: usize = 256;
 
-    /// Add new block with provided hash and timestamp.
-    /// Drop a block which gets out of BLOCKS_STORED window.
-    pub fn add_new_block_hash(
-        host: &mut impl Runtime,
-        block_number: U256,
-        hash: H256,
-    ) -> Result<(), EvmBlockStorageError> {
-        let block_hash_path = to_block_hash_path(block_number)?;
-        write_h256(host, &block_hash_path, hash)?;
-
-        // Clean up old block hash if any
-        match block_number.checked_sub(U256::from(BLOCKS_STORED)) {
-            Some(old_block_num) => delete_block_hash(host, old_block_num),
-            None => Ok(()),
-        }
-    }
-
     /// Get block hash by block number.
     pub fn get_block_hash(
         host: &impl Runtime,
@@ -198,31 +181,9 @@ pub mod blocks {
 
     fn to_block_hash_path(block_number: U256) -> Result<OwnedPath, EvmBlockStorageError> {
         let path: Vec<u8> =
-            format!("/evm/world_state/blocks/{}/hash", block_number).into();
+            format!("/evm/world_state/indexes/blocks/{}", block_number).into();
         let owned_path = OwnedPath::try_from(path)?;
         Ok(owned_path)
-    }
-
-    fn delete_block_hash(
-        host: &mut impl Runtime,
-        block_number: U256,
-    ) -> Result<(), EvmBlockStorageError> {
-        let block_hash_path = to_block_hash_path(block_number)?;
-
-        match host.store_delete(&block_hash_path) {
-            Ok(()) | Err(RuntimeError::PathNotFound) => Ok(()),
-            Err(err) => Err(EvmBlockStorageError::RuntimeError(err)),
-        }
-    }
-
-    fn write_h256(
-        host: &mut impl Runtime,
-        path: &impl Path,
-        hash: H256,
-    ) -> Result<(), EvmBlockStorageError> {
-        let hash_bytes = hash.to_fixed_bytes();
-        host.store_write(path, &hash_bytes, 0)
-            .map_err(EvmBlockStorageError::RuntimeError)
     }
 
     /// Test utilities for block storage
@@ -240,56 +201,6 @@ pub mod blocks {
                 BlockHash::try_from(level_bytes.repeat(8))
                     .expect("Hash expected to be valid")
             })
-        }
-    }
-
-    #[cfg(test)]
-    pub mod tests {
-        use super::test_utils::blocks_iter;
-        use super::*;
-        use primitive_types::U256;
-        use sha3::{Digest, Keccak256};
-        use tezos_smart_rollup_mock::MockHost;
-
-        #[test]
-        fn blocks_cleaned_up() {
-            let mut mock_host = MockHost::default();
-            const DELETE_BLOCKS_N: usize = 5;
-            let mut last_kept_block_hash = None;
-
-            blocks_iter()
-                .take(BLOCKS_STORED + DELETE_BLOCKS_N)
-                .enumerate()
-                .for_each(|(i, hash)| {
-                    let keccak_hash =
-                        H256::from_slice(Keccak256::digest(hash.as_ref()).as_slice());
-                    if i == DELETE_BLOCKS_N {
-                        last_kept_block_hash = Some(keccak_hash)
-                    }
-                    add_new_block_hash(&mut mock_host, U256::from(i as i32), keccak_hash)
-                        .unwrap()
-                });
-
-            // Make sure that blocks are cleaned up
-            (0..DELETE_BLOCKS_N).for_each(|i| {
-                assert_eq!(
-                    get_block_hash(&mock_host, U256::from(i as i32))
-                        .expect_err("Blocks should be cleaned up"),
-                    EvmBlockStorageError::RuntimeError(
-                        host::runtime::RuntimeError::PathNotFound
-                    )
-                );
-            });
-
-            // Make sure that last block is kept
-            assert_eq!(
-                get_block_hash(&mock_host, U256::from(DELETE_BLOCKS_N as i32))
-                    .unwrap_or_else(|_| panic!(
-                        "Block with number {} should be still kept",
-                        DELETE_BLOCKS_N
-                    )),
-                last_kept_block_hash.unwrap()
-            );
         }
     }
 }
