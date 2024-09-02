7fe625d7d7241e5012337bdb3185c718ec46382c
Julien Tesson <julien.tesson@nomadic-labs.com>
2024-09-02 18:17:15 +0200

    Quebeca/manifest: link protocol in the node, client and codec

--- manifest/product_octez.ml
+++ manifest/product_octez.ml
@@ -6988,7 +6988,7 @@ let hash = Protocol.hash
 
   let _020_PsParisC = active (Name.v "PsParisC" 020)
 
-  let _beta = active (Name.dev "beta")
+  let _021_PsquebeC = active (Name.v "PsquebeC" 021)
 
   let alpha = active (Name.dev "alpha")
 
