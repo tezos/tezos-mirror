b9ab209c45681f1db45030c43203ed886e3ed7d8
Julien Sagot <julien.sagot@lambda-coins.com>
2024-09-03 17:42:13 +0200

    Tezt/Teztale: add documentation

--- tezt/lib_tezos/teztale.mli
+++ tezt/lib_tezos/teztale.mli
@@ -5,11 +5,14 @@
 (*                                                                           *)
 (*****************************************************************************)
 
+(** A user is either an archiver or an admin *)
 type user = {login : string; password : string}
 
 type interface = {address : string; port : int}
 
 module Server : sig
+
+  (** See parameters of {!run} function for details. *)
   type conf = {
     name : string;
     interface : interface;
@@ -17,10 +20,27 @@ module Server : sig
     admin : user;
   }
 
+  (** Expose paths of the files involved in the test:
+      - Server configuration file
+      - SQLite database
+  *)
   type filenames = {conf_filename : string; db_filename : string}
 
   type t = {process : Process.t; filenames : filenames; conf : conf}
 
+  (** [run ?runner ?path ?name ?address ?port ?users ?admin ()]
+
+      Spawn a teztale server with some given parameters:
+      - runner: runner used to spawn the process
+      - path: path of the teztale server executable
+      - name: name the the server used in logs
+      - address: where teztale server will be listening on (default is [127.0.0.1])
+      - port: port associated with [address]
+      - users: list of archivers allowed to feed the server. You can add
+        users once the server is started using the {!add_user} function.
+      - admin: credential used for adming tasks such as adding an allowed archiver
+        default is [admin:password]
+  *)
   val run :
     ?runner:Runner.t ->
     ?path:string ->
@@ -32,14 +52,33 @@ module Server : sig
     unit ->
     t Lwt.t
 
+  (** [add_user server user]
+      Add an archiver (its credentials) allowed to feed the database.
+  *)
   val add_user : t -> user -> (unit, exn) Result.t Lwt.t
 end
 
 module Archiver : sig
+
+  (** See parameters of {!run} function for details. *)
   type conf = {name : string; user : user; feed : interface list}
 
   type t = {process : Process.t; conf : conf}
 
+  (** [run ?runner ?path ?name ~node_port user feed]
+
+      Spawn a teztale archiver with some given parameters:
+      - runner: runner used to spawn the process
+      - path: path of the teztale server executable
+      - name: name the the server used in logs
+      - node_port: port used for the octez node RPCs
+      - user: login and password used by the archiver. Make sure that
+        login and password used have been allowed in the server. See
+        [?users] argument of {!Server.run} or {!Server.add_user}.
+      - feed: list of interfaces for teztale servers to feed. If the archiver
+        is feeding multiple servers, every server must accept login info defined
+        by [user] parameter.
+  *)
   val run :
     ?runner:Runner.t ->
     ?path:string ->
