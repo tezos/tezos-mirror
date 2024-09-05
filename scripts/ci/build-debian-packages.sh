9c1e5d3903a06016b89f0dd76ade51f15a6567b4
Pietro Abate <pietro.abate@nomadic-labs.com>
2024-09-05 14:43:19 +0200

    packages: consider git tags while building packages

--- scripts/ci/build-debian-packages.sh
+++ scripts/ci/build-debian-packages.sh
@@ -7,11 +7,18 @@ set -e
 BUILDDIR=$(pwd)
 export BLST_PORTABLE=true
 
+# fetch tags for releases
+git fetch -q --tags
+
 # Prepare the building area: copying all files from
 # the dependency image a staging area. This is necessary
 # to build on arm64 where the BUILDDIR is in ram.
 cp -a ./* /root/tezos/
+cp -a ./.git /root/tezos/
 cd /root/tezos/
+
+eval "$(opam env)"
+eval "$(dpkg-architecture)"
 
 export DEBEMAIL="contact@nomadic-labs.com"
 
