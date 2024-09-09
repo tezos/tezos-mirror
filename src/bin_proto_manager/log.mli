005fe5b23d311036cd3e2c37f2866a77295af5a9
Vivien <vivien.pelletier@nomadic-labs.com>
2024-09-09 17:30:38 +0200

    proto_manager: add a log module

---
+++ src/bin_proto_manager/log.mli
@@ -0,0 +1,52 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* SPDX-FileCopyrightText: 2024 Nomadic Labs <contact@nomadic-labs.com>      *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** This module permits to log using colors when the output is a terminal. It
+    is base on [Format].
+
+    Some semantic tags are defined to handle ANSI color and blinking:
+    - <yellow>
+    - <red>
+    - <blue>
+    - <green>
+    - <cyan>
+    - <magenta>
+    - <blinking>
+*)
+
+(* Same as [Format.printf]. *)
+val printf : ('a, Format.formatter, unit) format -> 'a
+
+(* Same as [Format.eprintf]. *)
+val eprintf : ('a, Format.formatter, unit) format -> 'a
+
+(* Same as [Format.printf] but add a newline at the end. *)
+val printfln : ('a, Format.formatter, unit) format -> 'a
+
+(* Same as [Format.eprintf] but add a newline at the end. *)
+val eprintfln : ('a, Format.formatter, unit) format -> 'a
+
+(* [error ?name msg] prints ["Error:"] followed by [msg] in red and a newline
+   in the standard error.
+   If [~name] is provided [msg] is prefixed by [~name] in yellow. *)
+val error : ?name:string -> ('a, Format.formatter, unit) format -> 'a
+
+(* [warning msg] prints ["Warning:"] followed by [msg] in yellow and a newline
+   in the standard error. *)
+val warning : ('a, Format.formatter, unit) format -> 'a
+
+(* Same as [eprintln] but prints in blue. *)
+val blue : ('a, Format.formatter, unit) format -> 'a
+
+(* Same as [eprintln] but prints in green. *)
+val green : ('a, Format.formatter, unit) format -> 'a
+
+(* Same as [eprintln] but prints in cyan. *)
+val cyan : ('a, Format.formatter, unit) format -> 'a
+
+(* Same as [eprintln] but prints in magenta. *)
+val magenta : ('a, Format.formatter, unit) format -> 'a
