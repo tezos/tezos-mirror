a174b949144a559e277fd94ff30f1c64d9006b41
Vivien <vivien.pelletier@nomadic-labs.com>
2024-09-09 17:27:15 +0200

    manifest: create proto_manager

---
+++ src/bin_proto_manager/main.ml
@@ -0,0 +1,27 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* SPDX-FileCopyrightText: 2024 Nomadic Labs <contact@nomadic-labs.com>      *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Cmdliner
+
+let wip () = Format.printf "This application is under development@."
+
+let wip_t = Term.(const wip $ const ())
+
+let cmd =
+  let doc =
+    "help to automatize most of the process involved in the new \
+     stabilisation/release of protocol amendments"
+  in
+  let info =
+    Cmd.info
+      "octez-proto-manager"
+      ~version:Tezos_version_value.Bin_version.octez_version_string
+      ~doc
+  in
+  Cmd.v info wip_t
+
+let () = exit (Cmd.eval cmd)
