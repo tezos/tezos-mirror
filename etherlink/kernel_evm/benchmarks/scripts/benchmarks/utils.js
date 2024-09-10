35f80ae64f6d6d3b60feb1892298e4363a04b1bf
Rodi-Can Bozman <rodi.bozman@functori.com>
2024-09-10 14:47:08 +0200

    EVM/Benchmarks: remove devmode arg from chunker (doesn't exist anymore)
    Thanks Pierrick for debugging this.
    
    Co-authored-by: Pierrick Couderc <pierrick.couderc@nomadic-labs.com>

--- etherlink/kernel_evm/benchmarks/scripts/benchmarks/utils.js
+++ etherlink/kernel_evm/benchmarks/scripts/benchmarks/utils.js
@@ -120,8 +120,8 @@ function temporary_data_file(data) {
 }
 
 function chunk_data_gen(data, extra_args) {
-    let tmp_data_file = temporary_data_file(data, "--devmode")
-    run_chunker_command = `${CHUNKER} chunk data file:${tmp_data_file} --devmode ${extra_args}`;
+    let tmp_data_file = temporary_data_file(data)
+    run_chunker_command = `${CHUNKER} chunk data file:${tmp_data_file} ${extra_args}`;
     // The buffer for the output is chosen really big to prevent ENOBUF errors, as the output is proportional to the input
     chunked_message = new Buffer.from(execSync(run_chunker_command, { maxBuffer: data.length * 5 })).toString();
     fs.unlinkSync(tmp_data_file);
