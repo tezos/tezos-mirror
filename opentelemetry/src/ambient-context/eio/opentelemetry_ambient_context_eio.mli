237fb65cf459366b5045fd9e6d4e7732b94808a5
François Thiré <francois.thire@nomadic-labs.com>
2024-09-12 14:34:18 +0200

    Opentelemetry: Add source code in the repository

---
+++ opentelemetry/src/ambient-context/eio/opentelemetry_ambient_context_eio.mli
@@ -0,0 +1,2 @@
+val storage : unit -> Opentelemetry_ambient_context.storage
+(** Storage using Eio's fibers local storage *)
---
+++ opentelemetry/src/ambient-context/hmap_key_.new.ml
@@ -0,0 +1,1 @@
+let key : Hmap.t Thread_local_storage.t = Thread_local_storage.create ()
