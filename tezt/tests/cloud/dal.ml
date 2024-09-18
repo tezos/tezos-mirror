f07b826ef7c4b7740b7b0693734121f2afd20c8c
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2024-09-18 23:07:31 +0200

    Tezt cloud/DAL+Etherlink: use NginX as reverse proxy

--- tezt/tests/cloud/dal.ml
+++ tezt/tests/cloud/dal.ml
@@ -334,6 +334,148 @@ module Teztale = struct
     Lwt.return_unit
 end
 
+module Dal_reverse_proxy = struct
+  (** This module allows to configure NginX as a reverse proxy in
+      front of a collection of DAL nodes. This allows to balance the
+      load on the DAL nodes based on which DAL slot index is used
+      while complying with the interface of the rollup node, which
+      expects a single DAL node endpoint.
+
+      The NginX reverse-proxy configuration is added as an NginX site
+      (in directory /etc/nginx/sites-available/ which is symlinked in
+      /etc/nginx/sites-enabled/). This module makes no assumption
+      about the existence of other NginX sites and should be invisible
+      to them except that:
+
+      - it disables the "default" NginX site (the file
+        /etc/nginx/sites-enabled/default is removed if it exists),
+      - it overrides the "reverse_proxy" NgninX site if there is some.
+  *)
+
+  (* An NginX configuration file is made of directives inside
+     potentially nested context blocks. *)
+  type config = Directive of string | Context_block of string * config list
+
+  let rec pp_config out = function
+    | Directive s -> Format.fprintf out "%s;" s
+    | Context_block (head, l) ->
+        Format.fprintf
+          out
+          "@[<v 2>%s {@;%a@]@;}"
+          head
+          (Format.pp_print_list
+             ~pp_sep:(fun out () -> Format.fprintf out "@;")
+             pp_config)
+          l
+
+  let generate_nginx_config ~port out ~default_endpoint
+      (producers : (int * string) Seq.t) =
+    Format.fprintf
+      out
+      "@[<v 0>%a@]@."
+      pp_config
+      (Context_block
+         ( "server",
+           [
+             Directive (sf "listen 0.0.0.0:%d" port);
+             (* When fetching data about a slot the RPC are of the
+                form (GET /levels/<level>/slots/<slot_index>/...); we
+                direct the request to a DAL node subscribed to the
+                topics of the queried slot index. *)
+             Context_block
+               ( "location /levels/",
+                 producers
+                 |> Seq.map (fun (slot_index, endpoint) ->
+                        Context_block
+                          ( sf "location ~ ^/levels/[0-9]+?/slots/%d/" slot_index,
+                            [Directive (sf "proxy_pass %s" endpoint)] ))
+                 |> List.of_seq );
+             (* When posting a DAL slot, the format of the RPC is POST
+                /slots/?slot_index=<slot_index>. In this case too we
+                redirect the request to a DAL node subscribed to the
+                topics of the queried slot index. *)
+             Context_block
+               ( "location /slots",
+                 producers
+                 |> Seq.map (fun (slot_index, endpoint) ->
+                        Context_block
+                          ( sf
+                              {|if ($query_string ~ "slot_index=%d")|}
+                              slot_index,
+                            [Directive (sf "proxy_pass %s" endpoint)] ))
+                 |> List.of_seq );
+             (* Other queries can be answered by any DAL node. *)
+             Context_block
+               ("location /", [Directive (sf "proxy_pass %s" default_endpoint)]);
+           ] ))
+
+  let init_reverse_proxy ~next_agent ~default_endpoint
+      (proxified_dal_nodes : (int * string) Seq.t) =
+    (* A NGINX reverse proxy which balances load between producer DAL
+       nodes based on the requested slot index. *)
+    let* agent = next_agent ~name:"dal-reverse-proxy" in
+    let runner = Agent.runner agent in
+    let port = Agent.next_available_port agent in
+    let () = toplog "Launching reverse proxy" in
+    let () = toplog "Generating nginx reverse proxy config" in
+    let config_filename = "nginx_reverse_proxy_config" in
+    let out_chan = Stdlib.open_out config_filename in
+    let config_ppf = Format.formatter_of_out_channel out_chan in
+    Format.fprintf
+      config_ppf
+      "%a"
+      (generate_nginx_config ~default_endpoint ~port)
+      proxified_dal_nodes ;
+    close_out out_chan ;
+
+    (* Upload the configuration file. *)
+    let* (_ : string) =
+      Agent.copy
+        ~destination:"/etc/nginx/sites-available/reverse_proxy"
+        ~source:config_filename
+        agent
+    in
+
+    (* Disable the default configuration *)
+    let* () =
+      Process.spawn ?runner "rm" ["-f"; "/etc/nginx/sites-enabled/default"]
+      |> Process.check
+    in
+
+    (* Enable the reverse proxy configuration *)
+    let* () =
+      Process.spawn
+        ?runner
+        "ln"
+        [
+          "-s";
+          "/etc/nginx/sites-available/reverse_proxy";
+          "/etc/nginx/sites-enabled/reverse_proxy";
+        ]
+      |> Process.check
+    in
+
+    (* Start the NginX service *)
+    let* () =
+      Process.spawn ?runner "service" ["nginx"; "restart"] |> Process.check
+    in
+    (* In order to pass the reverse proxy to the various Tezt helpers we
+       need to pretend to be a DAL node. The simplest way to do so is to
+       call Dal_node.Agent.create_from_endpoint with the appropriate
+       rpc_port and never call Dal_node.run on the result.
+
+       Since the DAL node never runs, it does not call it's L1 endpoint. *)
+    let l1_node_endpoint : Endpoint.t = {host = ""; scheme = ""; port = 0} in
+    let* dal_node =
+      Dal_node.Agent.create_from_endpoint
+        ~name:"bootstrap-dal-node"
+        ~rpc_port:port
+        agent
+        ~l1_node_endpoint
+    in
+    return dal_node
+end
+
 module Cli = struct
   include Cli
 
@@ -1701,8 +1843,9 @@ let init_etherlink_dal_node ~bootstrap ~next_agent ~name ~dal_slots ~network =
           default_dal_node
       in
       let* () = Dal_node.Agent.run ~memtrace:Cli.memtrace default_dal_node in
+      let default_endpoint = Dal_node.rpc_endpoint default_dal_node in
 
-      let* _dal_slots_and_nodes =
+      let* dal_slots_and_nodes =
         dal_slots
         |> Lwt_list.map_p (fun slot_index ->
                let name =
@@ -1727,7 +1870,13 @@ let init_etherlink_dal_node ~bootstrap ~next_agent ~name ~dal_slots ~network =
                let* () = Dal_node.Agent.run ~memtrace:Cli.memtrace dal_node in
                return (slot_index, Dal_node.rpc_endpoint dal_node))
       in
-      some default_dal_node
+      let* reverse_proxy_dal_node =
+        Dal_reverse_proxy.init_reverse_proxy
+          ~next_agent
+          ~default_endpoint
+          (List.to_seq dal_slots_and_nodes)
+      in
+      some reverse_proxy_dal_node
 
 let init_etherlink_operator_setup cloud configuration name ~bootstrap ~dal_slots
     account agent next_agent =
