50286e105cdf87d58598e49daf3e0c425d7ce011
Marina Polubelova <marina.polubelova@nomadic-labs.com>
2024-09-18 11:40:20 +0200

    Proto/Tests: add new file for staking operations tests

---
+++ src/proto_021_PsquebeC/lib_protocol/test/unit/test_staking_operations.ml
@@ -0,0 +1,14 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2024 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** Testing
+    -------
+    Component:    protocol
+    Invocation:   dune exec src/proto_021_PsquebeC/lib_protocol/test/unit/main.exe \
+                  -- --file test_staking_operations.ml
+    Subject:      test staking operations
+*)
