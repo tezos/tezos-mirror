b8f7b839001a0422a75036f3ff7bfbf245464e08
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2024-09-19 16:28:48 +0200

    EVM/Kernel: test a bit more the linked list

--- etherlink/kernel_evm/kernel/src/linked_list.rs
+++ etherlink/kernel_evm/kernel/src/linked_list.rs
@@ -35,7 +35,7 @@ struct LinkedListPointer<Id, Elt> {
 }
 
 /// Each element in the list has a pointer
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 struct Pointer<Id, Elt> {
     /// Current index of the pointer
     id: Id,
@@ -503,18 +503,17 @@ where
 
 #[cfg(test)]
 mod tests {
-    use super::LinkedList;
+    use super::*;
     use proptest::prelude::*;
     use proptest::proptest;
     use rlp::{Decodable, DecoderError, Encodable};
     use std::collections::HashMap;
-    use tezos_ethereum::transaction::TRANSACTION_HASH_SIZE;
     use tezos_evm_runtime::runtime::MockKernelHost;
     use tezos_smart_rollup_debug::Runtime;
     use tezos_smart_rollup_host::path::RefPath;
 
-    #[derive(Clone, PartialEq)]
-    struct Hash([u8; TRANSACTION_HASH_SIZE]);
+    #[derive(Clone, PartialEq, Debug)]
+    struct Hash([u8; 8]);
 
     impl Encodable for Hash {
         fn rlp_append(&self, s: &mut rlp::RlpStream) {
@@ -538,6 +537,62 @@ mod tests {
         }
     }
 
+    fn traverse_f(
+        host: &MockKernelHost,
+        list_path: &OwnedPath,
+        pointer: Pointer<Hash, u8>,
+        f: &dyn Fn(&Pointer<Hash, u8>) -> Option<Hash>,
+    ) -> (Pointer<Hash, u8>, usize) {
+        let mut size = 0;
+        let mut cell = pointer.clone();
+        loop {
+            size += 1;
+            match f(&cell) {
+                None => break,
+                Some(next) => {
+                    match Pointer::<Hash, u8>::read(host, list_path, &next).unwrap() {
+                        Some(next) => cell = next,
+                        None => {
+                            panic!("Pointer should exist in storage");
+                        }
+                    }
+                }
+            }
+        }
+        (cell, size)
+    }
+
+    fn traverse(host: &MockKernelHost, list: &LinkedList<Hash, u8>) -> usize {
+        let Some(LinkedListPointer {
+            ref front,
+            ref back,
+        }) = list.pointers
+        else {
+            return 0;
+        };
+
+        let (found_front, size_from_back) =
+            traverse_f(host, &list.path, back.clone(), &|cell: &Pointer<
+                Hash,
+                u8,
+            >| {
+                cell.previous.clone()
+            });
+        let (found_back, size_from_front) =
+            traverse_f(host, &list.path, front.clone(), &|cell: &Pointer<
+                Hash,
+                u8,
+            >| {
+                cell.next.clone()
+            });
+
+        assert_eq!(found_front, front.clone());
+        assert_eq!(found_back, back.clone());
+        assert_eq!(size_from_back, size_from_front);
+
+        size_from_back
+    }
+
     fn assert_length(
         host: &MockKernelHost,
         list: &LinkedList<Hash, u8>,
@@ -551,7 +606,12 @@ mod tests {
         assert_eq!(
             expected_len, actual_length,
             "Unexpected length for the list"
-        )
+        );
+        let traverse_length = traverse(host, list);
+        assert_eq!(
+            expected_len, traverse_length as u64,
+            "Traverse size is unexpected"
+        );
     }
 
     #[test]
@@ -568,7 +628,7 @@ mod tests {
         let host = MockKernelHost::default();
         let path = RefPath::assert_from(b"/list");
         let list = LinkedList::new(&path, &host).expect("list should be created");
-        let hash = Hash([0; TRANSACTION_HASH_SIZE]);
+        let hash = Hash([0; 8]);
         let read: Option<u8> = list.find(&host, &hash).expect("storage should work");
         assert!(read.is_none())
     }
@@ -578,7 +638,7 @@ mod tests {
         let mut host = MockKernelHost::default();
         let path = RefPath::assert_from(b"/list");
         let mut list = LinkedList::new(&path, &host).expect("list should be created");
-        let id = Hash([0x0; TRANSACTION_HASH_SIZE]);
+        let id = Hash([0x0; 8]);
         let elt = 0x32_u8;
 
         list.push(&mut host, &id, &elt)
@@ -597,7 +657,7 @@ mod tests {
         let mut host = MockKernelHost::default();
         let path = RefPath::assert_from(b"/list");
         let mut list = LinkedList::new(&path, &host).expect("list should be created");
-        let id = Hash([0x0; TRANSACTION_HASH_SIZE]);
+        let id = Hash([0x0; 8]);
         let elt = 0x32_u8;
 
         assert_length(&host, &list, 0u64);
@@ -617,7 +677,7 @@ mod tests {
         let mut host = MockKernelHost::default();
         let path = RefPath::assert_from(b"/list");
         let mut list = LinkedList::new(&path, &host).expect("list should be created");
-        let id = Hash([0x0; TRANSACTION_HASH_SIZE]);
+        let id = Hash([0x0; 8]);
 
         let removed: Option<u8> =
             list.remove(&mut host, &id).expect("storage should work");
@@ -630,7 +690,7 @@ mod tests {
         let mut host = MockKernelHost::default();
         let path = RefPath::assert_from(b"/list");
         let mut list = LinkedList::new(&path, &host).expect("list should be created");
-        let id = Hash([0x0; TRANSACTION_HASH_SIZE]);
+        let id = Hash([0x0; 8]);
         let elt = 0x32_u8;
 
         list.push(&mut host, &id, &elt)
@@ -649,8 +709,8 @@ mod tests {
         let mut host = MockKernelHost::default();
         let path = RefPath::assert_from(b"/list");
         let mut list = LinkedList::new(&path, &host).expect("list should be created");
-        let id_1 = Hash([0x0; TRANSACTION_HASH_SIZE]);
-        let id_2 = Hash([0x1; TRANSACTION_HASH_SIZE]);
+        let id_1 = Hash([0x0; 8]);
+        let id_2 = Hash([0x1; 8]);
 
         list.push(&mut host, &id_1, &0x32_u8)
             .expect("storage should workd");
@@ -670,8 +730,8 @@ mod tests {
         let mut host = MockKernelHost::default();
         let path = RefPath::assert_from(b"/list");
         let mut list = LinkedList::new(&path, &host).expect("list should be created");
-        let id_1 = Hash([0x0; TRANSACTION_HASH_SIZE]);
-        let id_2 = Hash([0x1; TRANSACTION_HASH_SIZE]);
+        let id_1 = Hash([0x0; 8]);
+        let id_2 = Hash([0x1; 8]);
 
         list.push(&mut host, &id_1, &0x32_u8)
             .expect("storage should workd");
@@ -693,8 +753,8 @@ mod tests {
         let path = RefPath::assert_from(b"/list");
         let mut list: LinkedList<Hash, u8> =
             LinkedList::new(&path, &host).expect("list should be created");
-        let id_1 = Hash([0x0; TRANSACTION_HASH_SIZE]);
-        let id_2 = Hash([0x1; TRANSACTION_HASH_SIZE]);
+        let id_1 = Hash([0x0; 8]);
+        let id_2 = Hash([0x1; 8]);
         list.push(&mut host, &id_1, &0x32_u8)
             .expect("push should have worked");
         list.push(&mut host, &id_2, &0x33_u8)
@@ -706,29 +766,114 @@ mod tests {
     }
 
     fn fill_list(
-        elements: &HashMap<[u8; TRANSACTION_HASH_SIZE], u8>,
+        elements: &HashMap<[u8; 8], u8>,
     ) -> (MockKernelHost, LinkedList<Hash, u8>) {
         let mut host = MockKernelHost::default();
         let path = RefPath::assert_from(b"/list");
         let mut list = LinkedList::new(&path, &host).expect("list should be created");
-        for (id, elt) in elements {
+        for (pos, (id, elt)) in elements.iter().enumerate() {
             list.push(&mut host, &Hash(*id), elt)
                 .expect("storage should work");
-            assert!(!list.is_empty())
+
+            assert_length(&host, &list, pos as u64 + 1);
         }
         (host, list)
     }
 
-    fn elements() -> impl Strategy<Value = HashMap<[u8; TRANSACTION_HASH_SIZE], u8>> {
-        prop::collection::hash_map(any::<[u8; 32]>(), any::<u8>(), 1..10)
+    fn make_list(n: u8, host: &mut MockKernelHost) -> (LinkedList<Hash, u8>, Vec<Hash>) {
+        let path = RefPath::assert_from(b"/list");
+        let mut list = LinkedList::new(&path, host).expect("list should be created");
+        let mut elements = vec![];
+        for i in 0..n {
+            let id = Hash([i; 8]);
+            list.push(host, &id, &i).unwrap();
+            elements.push(id);
+        }
+
+        (list, elements)
+    }
+
+    #[test]
+    fn test_push_4_remove_pos_0() {
+        let mut host = MockKernelHost::default();
+        let (mut list, elements) = make_list(4, &mut host);
+
+        assert_length(&host, &list, 4);
+        let _: Option<u8> = list
+            .remove(&mut host, &elements[0])
+            .expect("storage should work");
+        assert_length(&host, &list, 3);
+    }
+
+    #[test]
+    fn test_push_4_remove_pos_1() {
+        let mut host = MockKernelHost::default();
+        let (mut list, elements) = make_list(4, &mut host);
+
+        assert_length(&host, &list, 4);
+        let _: Option<u8> = list
+            .remove(&mut host, &elements[1])
+            .expect("storage should work");
+        assert_length(&host, &list, 3);
+    }
+
+    #[test]
+    fn test_push_4_remove_pos_2() {
+        let mut host = MockKernelHost::default();
+        let (mut list, elements) = make_list(4, &mut host);
+
+        assert_length(&host, &list, 4);
+        let _: Option<u8> = list
+            .remove(&mut host, &elements[2])
+            .expect("storage should work");
+        assert_length(&host, &list, 3);
+    }
+
+    #[test]
+    fn test_push_4_remove_pos_3() {
+        let mut host = MockKernelHost::default();
+        let (mut list, elements) = make_list(4, &mut host);
+
+        assert_length(&host, &list, 4);
+        let _: Option<u8> = list
+            .remove(&mut host, &elements[3])
+            .expect("storage should work");
+        assert_length(&host, &list, 3);
+    }
+
+    fn elements() -> impl Strategy<Value = (HashMap<[u8; 8], u8>, usize)> {
+        // Generate a HashMap with keys of size [u8; 32] and u8 values
+        prop::collection::hash_map(any::<[u8; 8]>(), any::<u8>(), 1..10).prop_flat_map(
+            |map| {
+                let map_len = map.len(); // Get the length of the HashMap
+
+                // Ensure there's at least one element in the map
+                let random_index_strategy = 0..map_len; // Strategy to pick a random index
+
+                // Return the HashMap and the randomly selected index value
+                (Just(map.clone()), random_index_strategy)
+                    .prop_map(move |(map, idx)| (map, idx))
+            },
+        )
     }
 
     proptest! {
 
-        #![proptest_config(ProptestConfig::with_cases(50))]
+        #![proptest_config(ProptestConfig::with_cases(200))]
 
         #[test]
-        fn test_pushed_elements_are_present(elements in elements()) {
+        fn test_push_remove_consistency((elements, idx) in elements()) {
+            let (mut host, mut list) = fill_list(&elements);
+
+            let elt = elements.keys().nth(idx).unwrap();
+            list.remove(&mut host, &Hash(*elt)).unwrap();
+
+            assert_length(&host, &list, (elements.len() as u64) - 1);
+
+        }
+
+        #[test]
+        fn test_pushed_elements_are_present((elements, _) in elements()) {
             let (host, list) = fill_list(&elements);
             for (id, elt) in & elements {
                 let read: u8 = list.find(&host, &Hash(*id)).expect("storage should work").expect("element should be present");
@@ -737,7 +882,7 @@ mod tests {
         }
 
         #[test]
-        fn test_push_element_create_non_empty_list(elements in elements()) {
+        fn test_push_element_create_non_empty_list((elements, _) in elements()) {
             let mut host = MockKernelHost::default();
             let path = RefPath::assert_from(b"/list");
             let mut list = LinkedList::new(&path, &host).expect("list should be created");
@@ -749,7 +894,7 @@ mod tests {
         }
 
         #[test]
-        fn test_remove_from_empty_creates_empty_list(elements: Vec<[u8; TRANSACTION_HASH_SIZE]>) {
+        fn test_remove_from_empty_creates_empty_list(elements: Vec<[u8; 8]>) {
             let mut host = MockKernelHost::default();
             let path = RefPath::assert_from(b"/list");
             let mut list = LinkedList::new(&path, &host).expect("list should be created");
@@ -761,7 +906,7 @@ mod tests {
         }
 
         #[test]
-        fn test_remove_returns_the_appropriate_element(elements in elements()) {
+        fn test_remove_returns_the_appropriate_element((elements, _) in elements()) {
             let (mut host, mut list) = fill_list(&elements);
             let mut length : u64 = elements.len().try_into().unwrap();
             for (id, elt) in &elements {
@@ -773,7 +918,7 @@ mod tests {
         }
 
         #[test]
-        fn test_remove_everything_creates_the_empty_list(elements in elements()) {
+        fn test_remove_everything_creates_the_empty_list((elements, _) in elements()) {
             let (mut host, mut list) = fill_list(&elements);
             for (id, _) in elements {
                 let _: u8 = list.remove(&mut host, &Hash(id)).expect("storage should work").expect("element should be present");
@@ -782,7 +927,7 @@ mod tests {
         }
 
         #[test]
-        fn test_list_is_kept_between_reboots(elements in elements()) {
+        fn test_list_is_kept_between_reboots((elements, _) in elements()) {
             let mut host = MockKernelHost::default();
             let path = RefPath::assert_from(b"/list");
             for (id, elt) in &elements {
@@ -802,7 +947,7 @@ mod tests {
         }
 
         #[test]
-        fn test_pop_first_after_push(elements in elements()) {
+        fn test_pop_first_after_push((elements, _) in elements()) {
             let mut host = MockKernelHost::default();
             let path = RefPath::assert_from(b"/list");
             let mut list = LinkedList::new(&path, &host).expect("list should be created");
@@ -815,7 +960,7 @@ mod tests {
         }
 
         #[test]
-        fn test_pop_first_keep_the_order(elements in elements()) {
+        fn test_pop_first_keep_the_order((elements, _) in elements()) {
             let mut host = MockKernelHost::default();
             let path = RefPath::assert_from(b"/list");
             let mut list = LinkedList::new(&path, &host).expect("list should be created");
