488791872b822bf0989db6995d0ffe03d96a7332
Rodi-Can Bozman <rodi.bozman@functori.com>
2024-09-23 10:46:10 +0200

    Etherlink/Metrics: add governance contracts entrypoints encodings

--- etherlink/governance-metrics/src/node_helpers.ml
+++ etherlink/governance-metrics/src/node_helpers.ml
@@ -11,6 +11,197 @@ open Tezos_rpc
 let failwith_unused_encoding ~__FUNCTION__ =
   Stdlib.failwith @@ __FUNCTION__ ^ ": Unused encoding"
 
+type operation_result = {status : string}
+
+let operation_result_encoding =
+  conv
+    (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+    (fun (status, ()) -> {status})
+    (merge_objs (obj1 (req "status" string)) unit)
+
+type contract_metadata = {operation_result : operation_result}
+
+let contract_metadata_encoding =
+  conv
+    (fun {operation_result} -> (operation_result, ()))
+    (fun (operation_result, ()) -> {operation_result})
+    (merge_objs (obj1 (req "operation_result" operation_result_encoding)) unit)
+
+type sequencer_proposal_args = {
+  sequencer_pk : string;
+  pool_address : string; (* repr: bytes *)
+}
+
+type sequencer_proposal_args_opt = {
+  sequencer_pk : string option;
+  pool_address : string option; (* repr: bytes *)
+}
+
+type sequencer_entrypoints_parameters =
+  | Trigger_committee_upgrade of string (* repr: address *)
+  | Vote of string
+  | Upvote_proposal of sequencer_proposal_args
+  | New_proposal of sequencer_proposal_args
+
+type kernel_entrypoint_parameters =
+  | Trigger_kernel_upgrade of string (* repr: address *)
+  | Vote of string
+  | Upvote_proposal of string (* repr: bytes *)
+  | New_proposal of string (* repr: bytes *)
+
+type entrypoint_parameters =
+  | Sequencer of sequencer_entrypoints_parameters
+  | Kernel of kernel_entrypoint_parameters
+  | Security_kernel of kernel_entrypoint_parameters
+
+let sequencer_proposal_encoding =
+  conv
+    (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+    (fun (sequencer_pk, pool_address) -> {sequencer_pk; pool_address})
+    (obj2 (opt "string" string) (opt "bytes" string))
+
+let entrypoint_parameters_encoding ~contracts =
+  let open Configuration in
+  union
+    [
+      case
+        ~title:"string_entrypoint_parameters"
+        Json_only
+        (merge_objs (obj1 (req "string" string)) unit)
+        (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+        (fun (value, ()) ~entrypoint ~destination ->
+          match (entrypoint, destination) with
+          | "vote", dest when dest = contracts.kernel_governance ->
+              Some (Kernel (Vote value))
+          | "vote", dest when dest = contracts.sequencer_governance ->
+              Some (Sequencer (Vote value))
+          | "vote", dest when dest = contracts.security_kernel_governance ->
+              Some (Security_kernel (Vote value))
+          | "trigger_kernel_upgrade", dest
+            when dest = contracts.kernel_governance ->
+              Some (Kernel (Trigger_kernel_upgrade value))
+          | "trigger_committee_upgrade", dest
+            when dest = contracts.sequencer_governance ->
+              Some (Sequencer (Trigger_committee_upgrade value))
+          | "trigger_kernel_upgrade", dest
+            when dest = contracts.security_kernel_governance ->
+              Some (Security_kernel (Trigger_kernel_upgrade value))
+          | _ -> None);
+      case
+        ~title:"bytes_entrypoint_parameters"
+        Json_only
+        (merge_objs (obj1 (req "bytes" string)) unit)
+        (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+        (fun (value, ()) ~entrypoint ~destination ->
+          match (entrypoint, destination) with
+          | "upvote_proposal", dest when dest = contracts.kernel_governance ->
+              Some (Kernel (Upvote_proposal value))
+          | "new_proposal", dest when dest = contracts.kernel_governance ->
+              Some (Kernel (New_proposal value))
+          | "upvote_proposal", dest
+            when dest = contracts.security_kernel_governance ->
+              Some (Security_kernel (Upvote_proposal value))
+          | "new_proposal", dest
+            when dest = contracts.security_kernel_governance ->
+              Some (Security_kernel (New_proposal value))
+          | _ -> None);
+      case
+        ~title:"sequencer_proposal_entrypoint_parameters"
+        Json_only
+        (merge_objs (obj1 (req "args" (list sequencer_proposal_encoding))) unit)
+        (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+        (fun (proposal_arguments, ()) ~entrypoint ~destination ->
+          match (proposal_arguments, entrypoint, destination) with
+          | [{sequencer_pk; _}; {pool_address; _}], "upvote_proposal", dest
+            when dest = contracts.sequencer_governance -> (
+              match (sequencer_pk, pool_address) with
+              | Some sequencer_pk, Some pool_address ->
+                  Some
+                    (Sequencer (Upvote_proposal {sequencer_pk; pool_address}))
+              | _ -> None)
+          | [{sequencer_pk; _}; {pool_address; _}], "new_proposal", dest
+            when dest = contracts.sequencer_governance -> (
+              match (sequencer_pk, pool_address) with
+              | Some sequencer_pk, Some pool_address ->
+                  Some (Sequencer (New_proposal {sequencer_pk; pool_address}))
+              | _ -> None)
+          | _ -> None);
+      case
+        ~title:"ignored_entrypoint_parameters"
+        Json_only
+        unit
+        (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+        (fun _ ~entrypoint:_ ~destination:_ -> None);
+    ]
+
+type contract_parameters = {value : entrypoint_parameters}
+
+let contract_parameters_encoding ~contracts =
+  conv
+    (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+    (fun ((entrypoint, get_value), ()) ~destination ->
+      Option.map (fun value -> {value}) (get_value ~entrypoint ~destination))
+    (merge_objs
+       (obj2
+          (req "entrypoint" string)
+          (req "value" (entrypoint_parameters_encoding ~contracts)))
+       unit)
+
+type contract_transaction = {source : string; parameters : contract_parameters}
+
+let contract_transaction_encoding ~contracts =
+  conv
+    (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+    (fun ((source, destination, get_parameters, metadata), ()) ->
+      if metadata.operation_result.status = "applied" then
+        Option.map
+          (fun parameters -> Some {source; parameters})
+          (get_parameters ~destination)
+      else None)
+    (merge_objs
+       (obj4
+          (req "source" string)
+          (req "destination" string)
+          (req "parameters" (contract_parameters_encoding ~contracts))
+          (req "metadata" contract_metadata_encoding))
+       unit)
+
+let contract_transaction_opt_encoding ~contracts =
+  union
+    [
+      case
+        ~title:"contract_transaction"
+        Json_only
+        (merge_objs
+           (obj1 (req "kind" (constant "transaction")))
+           (contract_transaction_encoding ~contracts))
+        (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+        (fun ((), contract_transaction_tuple) -> contract_transaction_tuple);
+      case
+        ~title:"ignored_operation"
+        Json_only
+        unit
+        (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+        (fun _ -> None);
+    ]
+
+let operation_encoding ~contracts =
+  conv
+    (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+    (fun (contents, ()) -> List.filter_some @@ List.filter_some contents)
+    (merge_objs
+       (obj1
+          (req "contents" (list (contract_transaction_opt_encoding ~contracts))))
+       unit)
+
+let operations_encoding ~contracts =
+  conv
+    (fun _ -> failwith_unused_encoding ~__FUNCTION__)
+    (fun (operations, ()) -> List.concat_map List.concat operations)
+    (merge_objs
+       (obj1 (req "operations" (list (list (operation_encoding ~contracts)))))
+       unit)
+
 let micheline_encoding =
   let open Tezos_micheline.Micheline in
   let open Tezos_micheline.Micheline_encoding in
