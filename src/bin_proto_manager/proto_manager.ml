d49dc8d5d8c5f71aeb90beb0354672e72eb133d0
Vivien <vivien.pelletier@nomadic-labs.com>
2024-09-23 19:14:03 +0200

    proto_manager: add command line definition

---
+++ src/bin_proto_manager/proto_manager.ml
@@ -0,0 +1,12 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* SPDX-FileCopyrightText: 2024 Nomadic Labs <contact@nomadic-labs.com>      *)
+(*                                                                           *)
+(*****************************************************************************)
+
+let stabilise _from _target = Log.warning "Stabilisation is under development"
+
+let snapshot _from _target = Log.warning "Snapshot is under development"
+
+let hash _from = Log.warning "Updating hash is under development"
