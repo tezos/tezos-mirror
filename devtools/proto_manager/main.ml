d93077af7d59d41d15f07f442f038808fb5905ae
Vivien <vivien.pelletier@nomadic-labs.com>
2024-09-27 12:00:18 +0200

    Proto_manager: improve cmd agrument & add git-dir

--- devtools/proto_manager/main.ml
+++ devtools/proto_manager/main.ml
@@ -7,36 +7,72 @@
 
 open Cmdliner
 
-let from =
-  let doc = "The source protocol to stabilise, snapshot or update the hash." in
+let from action =
+  let doc =
+    Format.asprintf
+      "The source protocol to %s. It must be of the form '%a'."
+      action
+      Proto_manager.pp_pattern
+      Proto_manager.protocol_pattern
+  in
   Arg.(
     required
     & opt (some string) None
     & info ["f"; "from"] ~docv:"<protocol_source>" ~doc)
 
-let target =
-  let doc = "The target protocol to stabilise or snapshot." in
+let target action pattern =
+  let doc =
+    Format.asprintf
+      "The target protocol to %s. It must be different than <protocol_source> \
+       and of the form '%a'."
+      action
+      Proto_manager.pp_pattern
+      pattern
+  in
   Arg.(
     required
     & opt (some string) None
     & info ["t"; "to"] ~docv:"<protocol_target>" ~doc)
 
+let git_dir =
+  let doc =
+    "The root directory of a clone of tezos/tezos git repository. By default, \
+     it is the current working directory."
+  in
+  Arg.(
+    value
+    & opt dir (Sys.getcwd ())
+    & info ["g"; "git-directory"] ~docv:"<git_directory>" ~doc)
+
 let stabilise =
+  let action = "stabilise" in
   let doc = "Stabilise a protocol." in
   let info = Cmd.info "stabilise" ~doc in
-  let cmd = Term.(const Proto_manager.stabilise $ from $ target) in
+  let cmd =
+    Term.(
+      const Proto_manager.Stabilise.run
+      $ git_dir $ from action
+      $ target action Proto_manager.protocol_pattern)
+  in
   Cmd.v info cmd
 
 let snapshot =
+  let action = "snapshot" in
   let doc = "Snapshot a protocol." in
   let info = Cmd.info "snapshot" ~doc in
-  let cmd = Term.(const Proto_manager.snapshot $ from $ target) in
+  let cmd =
+    Term.(
+      const Proto_manager.Snapshot.run
+      $ git_dir $ from action
+      $ target action Proto_manager.Snapshot.target_pattern)
+  in
   Cmd.v info cmd
 
 let hash =
+  let action = "update the hash" in
   let doc = "Update the hash of a protocol." in
   let info = Cmd.info "hash" ~doc in
-  let cmd = Term.(const Proto_manager.hash $ from) in
+  let cmd = Term.(const Proto_manager.Hash.run $ git_dir $ from action) in
   Cmd.v info cmd
 
 let cmd =
--- devtools/proto_manager/proto_manager.ml
+++ devtools/proto_manager/proto_manager.ml
@@ -5,8 +5,26 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let stabilise _from _target = Log.warning "Stabilisation is under development"
+type pattern = {string : string; regex : Re.re}
 
-let snapshot _from _target = Log.warning "Snapshot is under development"
+let pp_pattern fmt {string; _} = Format.pp_print_string fmt string
 
-let hash _from = Log.warning "Updating hash is under development"
+let protocol_pattern =
+  let string = {|[a-z]+[0-9]*|} in
+  {string; regex = Re.Perl.(compile @@ re ("^" ^ string ^ "$"))}
+
+module Stabilise = struct
+  let run _git_dir _protocol_source _protocol_target = ()
+end
+
+module Snapshot = struct
+  let target_pattern =
+    let string = {|[a-z]+_[0-9][0-9][0-9]|} in
+    {string; regex = Re.Perl.(compile @@ re ("^" ^ string ^ "$"))}
+
+  let run _git_dir _protocol_source _protocol_target = ()
+end
+
+module Hash = struct
+  let run _git_dir _from = Log.warning "Updating hash is under development"
+end
