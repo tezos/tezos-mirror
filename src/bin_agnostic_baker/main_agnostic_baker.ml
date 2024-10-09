fc17fc51d856ca73bab390ef5f4ed30a4718ad9f
Victor Allombert <victor.allombert@nomadic-labs.com>
2024-10-09 11:37:05 +0200

    Agnostic_baker: parameters as standalone module

--- src/bin_agnostic_baker/main_agnostic_baker.ml
+++ src/bin_agnostic_baker/main_agnostic_baker.ml
@@ -104,7 +104,8 @@ module Events = struct
       ~level:Notice
       ~name:"protocol_encountered"
       ~msg:"the {status} protocol {proto_hash} was encountered"
-      ("status", string)
+      ("status", Parameters.status_encoding)
+      ~pp1:Parameters.pp_status
       ("proto_hash", Protocol_hash.encoding)
       ~pp2:Protocol_hash.pp_short
 
@@ -127,71 +128,6 @@ module Events = struct
         "new block on {period} period (remaining period duration {remaining})"
       ("period", string)
       ("remaining", int31)
-end
-
-module Parameters = struct
-  let default_node_addr = "http://127.0.0.1:8732"
-
-  let default_daily_logs_path = Some "octez-agnostic-baker"
-
-  let log_config ~base_dir =
-    let base_dir : string =
-      match base_dir with
-      | Some p -> p
-      | None -> Tezos_client_base_unix.Client_config.Cfg_file.default.base_dir
-    in
-    let daily_logs_path =
-      default_daily_logs_path
-      |> Option.map Filename.Infix.(fun logdir -> base_dir // "logs" // logdir)
-    in
-    Tezos_base_unix.Internal_event_unix.make_with_defaults
-      ?enable_default_daily_logs_at:daily_logs_path
-      ~log_cfg:Tezos_base_unix.Logs_simple_config.default_cfg
-      ()
-
-  type status = Active | Frozen
-
-  let pp_status fmt status =
-    Format.fprintf
-      fmt
-      "%s"
-      (match status with Active -> "active" | Frozen -> "frozen")
-
-  (* From Manifest/Product_octez/Protocol*)
-  let protocol_info = function
-    | ( "ProtoGenesisGenesisGenesisGenesisGenesisGenesk612im"
-      | "Ps9mPmXaRzmzk35gbAYNCAw6UXdE2qoABTHbN2oEEc1qM7CwT9P"
-      | "PtCJ7pwoxe8JasnHY8YonnLYjcVHmhiARPJvqcC6VfHT5s8k8sY"
-      | "PsYLVpVvgbLhAhoqAkMFUo6gudkJ9weNXhUYCiLDzcUpFpkk8Wt"
-      | "PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP"
-      | "Pt24m4xiPbLDhVgVfABUjirbmda3yohdN82Sp9FeuAXJ4eV9otd"
-      | "PsBABY5HQTSkA4297zNHfsZNKtxULfL18y95qb3m53QJiXGmrbU"
-      | "PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS"
-      | "PsCARTHAGazKbHtnKfLzQg3kms52kSRpgnDY982a9oYsSXRLQEb"
-      | "PsDELPH1Kxsxt8f9eWbxQeRxkjfbxoqM52jvs5Y5fBxWWh4ifpo"
-      | "PtEdoTezd3RHSC31mpxxo1npxFjoWWcFgQtxapi51Z8TLu6v6Uq"
-      | "PtEdo2ZkT9oKpimTah6x2embF25oss54njMuPzkJTEi5RqfdZFA"
-      | "PsFLorenaUUuikDWvMDr6fGBRG8kt3e3D3fHoXK1j1BFRxeSH4i"
-      | "PtGRANADsDU8R9daYKAgWnQYAJ64omN1o3KMGVCykShA97vQbvV"
-      | "PtHangz2aRngywmSRGGvrcTyMbbdpWdpFKuS4uMWxg2RaH9i1qx"
-      | "Psithaca2MLRFYargivpo7YvUr7wUDqyxrdhC5CQq78mRvimz6A"
-      | "PtJakart2xVj7pYXJBXrqHgd82rdkLey5ZeeGwDgPp9rhQUbSqY"
-      | "PtKathmankSpLLDALzWw7CGD2j2MtyveTwboEYokqUCP4a1LxMg"
-      | "PtLimaPtLMwfNinJi9rCfDPWea8dFgTZ1MeJ9f1m2SRic6ayiwW"
-      | "PtMumbai2TmsJHNGRkD8v8YDbtao7BLUC3wjASn1inAKLFCjaH1"
-      | "PtNairobiyssHuh87hEhfVBGCVrK3WnS8Z2FT4ymB5tAa4r1nQf"
-      | "ProxfordYmVfjWnRcgjWH36fW6PArwqykTFzotUxRs6gmTcZDuH"
-      | "PtParisBxoLz5gzMmn3d9WBQNoPSZakgnkMC2VNuQ3KXfUtUQeZ" ) as full_hash ->
-        (String.sub full_hash 0 8, Frozen)
-    | ( "PsParisCZo7KAh1Z1smVd9ZMZ1HHn5gkzbM94V3PLCpknFWhUAi"
-      | "PsquebeCaYyvBEESCaXL8B8Tn8BcEhps2Zke1xMVtyr7X4qMfxT" ) as full_hash ->
-        (String.sub full_hash 0 8, Active)
-    | "ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK" -> ("alpha", Active)
-    | _ -> (*We assume that unmatched protocols are beta ones*) ("beta", Active)
-
-  let protocol_short_hash h = fst (protocol_info h)
-
-  let protocol_status h = snd (protocol_info h)
 end
 
 module Baker = struct
@@ -421,9 +357,7 @@ module Daemon = struct
         | None -> Lwt.return_unit
         | Some h ->
             if not (Protocol_hash.equal h protocol_hash) then
-              Events.(emit protocol_encountered)
-                ( Format.asprintf "%a" Parameters.pp_status proto_status,
-                  protocol_hash )
+              Events.(emit protocol_encountered) (proto_status, protocol_hash)
             else Lwt.return_unit
       in
       match proto_status with
@@ -443,8 +377,7 @@ module Daemon = struct
             | None ->
                 let*! () =
                   Events.(emit protocol_encountered)
-                    ( Format.asprintf "%a" Parameters.pp_status proto_status,
-                      protocol_hash )
+                    (proto_status, protocol_hash)
                 in
                 let*! () = Events.(emit waiting_for_active_protocol) () in
                 monitor_heads ~node_addr:state.node_endpoint
