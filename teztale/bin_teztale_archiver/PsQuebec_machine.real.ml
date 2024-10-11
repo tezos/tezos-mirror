7d4876f1749235f3a78aa20fe05e996752ae932b
Vivien Pelletier <vivien.pelletier@nomadic-labs.com>
2024-10-11 17:28:29 +0200

    Quebec/teztale: update teztale_archiver_main.ml

--- teztale/bin_teztale_archiver/PsQuebec_machine.real.ml
+++ teztale/bin_teztale_archiver/PsQuebec_machine.real.ml
@@ -6,19 +6,19 @@
 (*****************************************************************************)
 
 module Block_services =
-  Tezos_client_021_PtQenaB1.Protocol_client_context.Alpha_block_services
+  Tezos_client_021_PsQuebec.Protocol_client_context.Alpha_block_services
 
 open Lwt_result_syntax
-open Tezos_protocol_021_PtQenaB1
-open Tezos_protocol_plugin_021_PtQenaB1
+open Tezos_protocol_021_PsQuebec
+open Tezos_protocol_plugin_021_PsQuebec
 
 module Services : Protocol_machinery.PROTOCOL_SERVICES = struct
   let hash = Protocol.hash
 
-  type wrap_full = Tezos_client_021_PtQenaB1.Protocol_client_context.wrap_full
+  type wrap_full = Tezos_client_021_PsQuebec.Protocol_client_context.wrap_full
 
   let wrap_full cctxt =
-    new Tezos_client_021_PtQenaB1.Protocol_client_context.wrap_full cctxt
+    new Tezos_client_021_PsQuebec.Protocol_client_context.wrap_full cctxt
 
   let slot_to_int x =
     (* YES this is Fun.x ! *)
