1c5c546e6daaf6c873e3f0623ac620f7cd43dbc9
Victor Allombert <victor.allombert@nomadic-labs.com>
2024-10-17 09:51:20 +0200

    Agnostic_baker: introduce Parameters interface

---
+++ src/bin_agnostic_baker/parameters.mli
@@ -0,0 +1,29 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2024 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** Default endpoint to contact the node. Based on the
+    [Octez_node_config.Config_file.default_rpc_port]. *)
+val default_node_addr : string
+
+val log_config : base_dir:string option -> Tezos_base.Internal_event_config.t
+
+(** Status of a protocol, based on Manifest/Product_octez/Protocol. A
+    protocol is considered as Active while it is running on a network,
+    and thus, have dedicated binaries. Otherwise, the protocol is
+    Frozen as not running anymore and no associated binaries.
+
+    Warning, it is needed to update status for each new protocol added.
+*)
+type status = Active | Frozen
+
+val pp_status : Format.formatter -> status -> unit
+
+val status_encoding : status t
+
+val protocol_short_hash : string -> string
+
+val protocol_status : string -> status
