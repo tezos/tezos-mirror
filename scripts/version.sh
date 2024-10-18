de59dc9901ac41dc35600b0b2d1132cc6458abc2
Victor Dumitrescu <v.dumitrescu@outlook.com>
2024-10-18 09:43:46 +0200

    Deps: update opam-repository tag
    Update in order to use `class_group_vdf.0.0.5`

--- scripts/version.sh
+++ scripts/version.sh
@@ -24,7 +24,7 @@ export recommended_node_version=18.18.2
 
 ## opam_repository is a commit hash of the public opam repository, i.e.
 ## https://github.com/ocaml/opam-repository
-export opam_repository_tag=a70ac3366bdaaf354374c8ac8929d09c17e6f494
+export opam_repository_tag=6be71e78343d97c4343a15c3c24a5b39cd162fd6
 
 # SHA-256 hashes of the DAL SRSs, as used in 'scripts/install_dal_trusted_setup.sh' to verify
 # integrity of downloaded SRS.
