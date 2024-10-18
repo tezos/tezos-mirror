948a39e8ba6f4b4db8230d91cb51d42fb1ca753b
iguerNL@Functori <iguer@functori.com>
2024-10-18 11:25:43 +0200

    DAL/Proto: rework the interface of compute_attested_slot_headers
    The function becomes compute_slot_headers_statuses, and it's not used/
    accessed from alpha_context anymore, since indexing is done in a simpler
    manner since MR https://gitlab.com/tezos/tezos/-/merge_requests/15320

--- src/proto_alpha/lib_protocol/alpha_context.mli
+++ src/proto_alpha/lib_protocol/alpha_context.mli
@@ -2880,11 +2880,6 @@ module Dal : sig
 
     val finalize_pending_slot_headers :
       context -> number_of_slots:int -> (context * Attestation.t) tzresult Lwt.t
-
-    val compute_attested_slot_headers :
-      is_slot_attested:(Header.t -> Attestation.attestation_status) ->
-      Header.t list ->
-      Header.t list * Attestation.t
   end
 
   module Operations : sig
