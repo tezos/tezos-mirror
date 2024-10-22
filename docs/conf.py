4cd16b2ba2db1db5e8d7917747c26ad464debc2a
Tim McMackin <timothymcmackin@yahoo.com>
2024-10-22 16:22:39 +0000

    Doc: Add copy button to doc code blocks

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'tezos_custom_roles',
     'michelsonlexer',
     'sphinxcontrib.jquery',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
