3e52dd162e2aa85d850df2560cfb62be7f04e7b1
Alain Mebsout <alain.mebsout@functori.com>
2024-10-28 17:42:53 +0100

    code

--- ci/bin/main.ml
+++ ci/bin/main.ml
@@ -138,9 +138,15 @@ let () =
   let octez_release_tag_re = "/^octez-v\\d+\\.\\d+(?:\\-rc\\d+)?$/" in
   (* Matches Octez beta release tags, e.g. [octez-v1.2-beta5]. *)
   let octez_beta_release_tag_re = "/^octez-v\\d+\\.\\d+\\-beta\\d*$/" in
-  (* Matches Etherlink release tags, e.g. [etherlink-v1.2] or [etherlink-v1.2-rc4]. *)
+  (* Matches EVM node release tags, e.g. [octez-evm-node-v1.2] or
+     [octez-evm-node-v1.2-rc4]. *)
   let octez_evm_node_release_tag_re =
     "/^octez-evm-node-v\\d+\\.\\d+(?:\\-rc\\d+)?$/"
+  in
+  (* Matches smart rollup node release tags, e.g. [octez-smart-rollup-node-v1.2]
+     or [rollup-node-v1.2-rc4]. *)
+  let octez_smart_rollup_node_release_tag_re =
+    "/^(?:octez-smart-)?rollup-node-v\\d+\\.\\d+(?:\\-rc\\d+)?$/"
   in
   let open Rules in
   let open Pipeline in
@@ -230,6 +236,16 @@ let () =
         octez-evm-node-vX.Y(-rcN). Creates and publishes a release on GitLab \
         with associated etherlink artifacts (static binaries and Docker \
         image)." ^ release_description) ;
+  register
+    "octez_smart_rollup_node_release_tag"
+    If.(push && has_tag_match octez_smart_rollup_node_release_tag_re)
+    ~jobs:(Release_tag.octez_smart_rollup_node_jobs ())
+    ~description:
+      ("Release tag pipelines for Etherlink.\n\n\
+        Created when the release manager pushes a tag in the format \
+        octez-smart-rollup-node-vX.Y(-rcN). Creates and publishes a release on \
+        GitLab with associated artifacts for the smart rollup node (static \
+        binaries and Docker image)." ^ release_description) ;
   register
     "non_release_tag"
     If.(on_tezos_namespace && push && has_non_release_tag)
