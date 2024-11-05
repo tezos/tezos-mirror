f26e37b7d92be6cd75e93b989a9ed34fb0f2f7be
Vivien <vivien.pelletier@nomadic-labs.com>
2024-11-05 10:25:03 +0100

    Proto_manager: rework State

--- devtools/proto_manager/state.ml
+++ devtools/proto_manager/state.ml
@@ -5,26 +5,63 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-type snapshot_label = {name : string; version : int}
+open Utils.Infix
 
-type label = Snapshot of snapshot_label | Stabilise of string
+type protocol = {name : string; version : int option; hash : string option}
 
-type t = {
-  commits : int;
-  protocol_source : string;
-  protocol_target : string;
-  target_label : label;
-  capitalize_label : string;
-  git_dir : string;
-}
+type t = {git_dir : string; commits : int; source : protocol; target : protocol}
 
-let pp_label fmt = function
-  | Stabilise label -> Format.pp_print_string fmt label
-  | Snapshot label -> Format.fprintf fmt "%03d_%s" label.version label.name
+let get_git_dir {git_dir; _} = git_dir
 
-let pp_label_name fmt = function
-  | Stabilise label -> Format.pp_print_string fmt label
-  | Snapshot label -> Format.fprintf fmt "%s" label.name
+let eq_source_target {source; target; _} = source = target
+
+let incr_commit_count state = {state with commits = state.commits + 1}
+
+module type Dir_sig = sig
+  val get_src_proto : t -> string
+
+  val get_lib_protocol : t -> string
+
+  val get_doc : t -> string
+end
+
+module type Protocol_sig = sig
+  module Dir : sig
+    module Relative : Dir_sig
+
+    module Absolute : Dir_sig
+  end
+
+  val get_name : t -> string
+
+  val get_version_value : t -> string
+
+  val get_constructor : t -> string
+
+  val get_hash : __LOC__:string -> ?short:unit -> t -> string
+
+  val get_bin_suffix : __LOC__:string -> t -> string
+
+  val get_module_name_root : __LOC__:string -> t -> string
+
+  val get_dune_name : __LOC__:string -> t -> string
+
+  val get_version : __LOC__:string -> t -> int
+end
+
+module type Get_protocol = sig
+  val get_protocol : t -> protocol
+end
+
+let get_src_proto_dir proto =
+  match (proto.version, proto.hash) with
+  | None, _ -> ("src" // "proto_") ^ proto.name
+  | Some version, None ->
+      Format.asprintf "%s_%03d_%s" ("src" // "proto_") version proto.name
+  | Some version, Some hash ->
+      Format.asprintf "%s_%03d_%s" ("src" // "proto_") version hash
+
+let get_doc_dir proto = "docs" // proto.name
 
 let exit ~__LOC__ (state : t) =
   let open Log in
@@ -32,40 +69,125 @@ let exit ~__LOC__ (state : t) =
   if state.commits > 0 then
     printfln
       "@[<v 2>@{<green>To clean up git commits run:@}@,\
-       git reset -- hard  HEAD~%i"
+       git reset --hard  HEAD~%i"
       state.commits ;
   printfln
     "@[<v 0>@[<v 2>@{<green>To clean up other created files:@}@,\
-     rm -rf docs/%a@,\
-     rm -rf src/proto_%a@]@,\
+     rm -rf %s@,\
+     rm -rf src/proto_%s@]@,\
      exiting ...@]"
-    pp_label_name
-    state.target_label
-    pp_label
-    state.target_label ;
+    (get_doc_dir state.target)
+    (get_src_proto_dir state.target) ;
   exit 1
 
-let create ~git_dir ~protocol_source ~protocol_target ~target_pattern =
-  let target_label =
-    match String.split_on_char '_' protocol_target with
+let make_absolute relative state = state.git_dir // relative
+
+module Protocol (G : Get_protocol) = struct
+  module Dir = struct
+    module Relative = struct
+      let get_src_proto state = get_src_proto_dir (G.get_protocol state)
+
+      let get_lib_protocol state = get_src_proto state // "lib_protocol"
+
+      let get_doc state = get_doc_dir (G.get_protocol state)
+    end
+
+    module Absolute = struct
+      let get_src_proto state =
+        make_absolute (get_src_proto_dir (G.get_protocol state)) state
+
+      let get_lib_protocol state = get_src_proto state // "lib_protocol"
+
+      let get_doc state =
+        make_absolute (get_doc_dir (G.get_protocol state)) state
+    end
+  end
+
+  let get_name state = (G.get_protocol state).name
+
+  let get_version_value state =
+    let proto = G.get_protocol state in
+    match proto.version with
+    | None -> proto.name
+    | Some version -> Format.asprintf "%s_%03d" proto.name version
+
+  let get_constructor state =
+    String.capitalize_ascii (G.get_protocol state).name
+
+  let get_hash ~__LOC__ ?short state =
+    let proto = G.get_protocol state in
+    match proto.hash with
+    | Some hash -> (
+        match short with None -> hash | Some () -> String.sub hash 0 8)
+    | None ->
+        Log.error "Protocol %s does not contain a hash" proto.name ;
+        exit ~__LOC__ state
+
+  let get_bin_suffix ~__LOC__ state =
+    let proto = G.get_protocol state in
+    match (proto.version, proto.hash) with
+    | None, _ -> proto.name
+    | Some version, Some hash -> Format.asprintf "%03d_%s" version hash
+    | Some _, None ->
+        Log.error "Protocol %s does not contain a hash" proto.name ;
+        exit ~__LOC__ state
+
+  let get_module_name_root ~__LOC__ state =
+    let proto = G.get_protocol state in
+    match (proto.version, proto.hash) with
+    | None, _ -> "protocol_" ^ proto.name
+    | Some version, Some hash -> Format.asprintf "protocol_%03d_%s" version hash
+    | Some _, None ->
+        Log.error "Protocol %s does not contain a hash" proto.name ;
+        exit ~__LOC__ state
+
+  let get_dune_name ~__LOC__ state =
+    let proto = G.get_protocol state in
+    match (proto.version, proto.hash) with
+    | None, _ -> proto.name
+    | Some version, Some hash -> Format.asprintf "%03d_%s" version hash
+    | Some _, None ->
+        Log.error "Protocol %s does not contain a hash" proto.name ;
+        exit ~__LOC__ state
+
+  let get_version ~__LOC__ state =
+    let proto = G.get_protocol state in
+    match proto.version with
+    | None ->
+        Log.error "%s protocol does not contain a version" proto.name ;
+        exit ~__LOC__ state
+    | Some version -> version
+end
+
+module Source = Protocol (struct
+  let get_protocol {source; _} = source
+end)
+
+module Target = Protocol (struct
+  let get_protocol {target; _} = target
+end)
+
+let create ~git_dir ~source ~target ~target_pattern =
+  let parse_protocol proto =
+    match String.split_on_char '_' proto with
     | [name; version] -> (
         match int_of_string_opt version with
-        | Some version -> Snapshot {name; version}
+        | Some _ as version -> {name; version; hash = None}
         | None ->
             Log.error "Cannot parse protocol target version." ;
             Log.error "Protocol target should be of form '%s'." target_pattern ;
             Stdlib.exit 1)
-    | [name] -> Stabilise name
+    | [name] -> {name; version = None; hash = None}
     | _ ->
         Log.error "Cannot parse protocol target." ;
         Log.error "Protocol target should be of form '%s'." target_pattern ;
         Stdlib.exit 1
   in
   {
-    commits = 0;
-    protocol_source;
-    protocol_target;
-    target_label;
-    capitalize_label = String.capitalize_ascii protocol_source;
     git_dir;
+    commits = 0;
+    source = parse_protocol source;
+    target = parse_protocol target;
   }
+
+let set_target_hash ~hash state = {state with target = {state.target with hash}}
--- devtools/proto_manager/state.mli
+++ devtools/proto_manager/state.mli
@@ -5,26 +5,95 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-(** Type of target label representation for snapshot. *)
-type snapshot_label = {name : string; version : int}
+(** Type that represents a protocol. *)
+type protocol
 
-(** Type of different target label representation for different actions. *)
-type label = Snapshot of snapshot_label | Stabilise of string
+(** Type that represents the state of the protocol manager. *)
+type t
 
-type t = {
-  commits : int;  (** Count of commit done by this process. *)
-  protocol_source : string;
-  protocol_target : string;
-  target_label : label;
-  capitalize_label : string;
-  git_dir : string;
-}
+(** [eq_source_target state] is true if source and target protocols designate
+    the same protocol. *)
+val eq_source_target : t -> bool
 
-(** [pp_label fmt l] prints the full representation of [l] in [fmt]. *)
-val pp_label : Format.formatter -> label -> unit
+(** [get_git_dir state] returns the absolute path of the tezos git repository
+    directory where the protocol manager operates. *)
+val get_git_dir : t -> string
 
-(** [pp_label_name fmt l] prints the name of [l] in [fmt]. *)
-val pp_label_name : Format.formatter -> label -> unit
+(** [incr_commit_count state] increments by one the count of commits made by
+    the protocol manager. *)
+val incr_commit_count : t -> t
+
+(** [make_absolute path state] returns the absolute path of [path] considering
+    that it is relative to the git repository directory. *)
+val make_absolute : string -> t -> string
+
+(** Module type that contains functions to get protocol related directories. *)
+module type Dir_sig = sig
+  (** [get_src_proto state] returns the protocol source directory. *)
+  val get_src_proto : t -> string
+
+  (** [get_lib_protocol state] returns the lib_protocol directory of the
+      protocol. *)
+  val get_lib_protocol : t -> string
+
+  (** [get_doc state] returns the doc directory of the protocol. *)
+  val get_doc : t -> string
+end
+
+(** Module type that contains functions to get informations of a protocol. *)
+module type Protocol_sig = sig
+  (** Module that contains functions to get the directories of the protocol. *)
+  module Dir : sig
+    (** Module that contains functions to get the paths relative to the git
+        repository directory. *)
+    module Relative : Dir_sig
+
+    (** Module that contains the functions to get the absolute paths. *)
+    module Absolute : Dir_sig
+  end
+
+  (** [get_name state] returns the name of the protocol in lowercase. *)
+  val get_name : t -> string
+
+  (** [get_version_value state] returns the protocol version_value string. It
+      is of the form quebec_021 or alpha. *)
+  val get_version_value : t -> string
+
+  (** [get_constructor state] returns a string representing an Ocaml
+      constructor of the protocol. *)
+  val get_constructor : t -> string
+
+  (** [get_hash ~__LOC__ ?short state] returns the hash of the protocol if
+      exists or fails. If [short] is provided returns the 8 first characters of
+      the hash. *)
+  val get_hash : __LOC__:string -> ?short:unit -> t -> string
+
+  (** [get_bin_suffix ~__LOC__ state] returns the suffix used for protocol
+      binaries. If the protocol has a version but not a hash it fails. *)
+  val get_bin_suffix : __LOC__:string -> t -> string
+
+  (** [get_module_name_root ~__LOC__ state] returns the root of protocol module
+      names.  If the protocol has a version but not a hash it fails. *)
+  val get_module_name_root : __LOC__:string -> t -> string
+
+  (** [get_dune_name ~__LOC__ state] returns the name of the protocol used in
+      dune files. If the protocol has a version but not a hash it fails. *)
+  val get_dune_name : __LOC__:string -> t -> string
+
+  (** [get_version ~__LOC__ state] returns ther version of the protocol. If the
+      protocol has not a version it fails. *)
+  val get_version : __LOC__:string -> t -> int
+end
+
+(** Module to get information about the source protocol. *)
+module Source : Protocol_sig
+
+(** Module to get information about the target protocol. *)
+module Target : Protocol_sig
+
+(** [set_target_hash ~hash state] returns [state] but with [hash] as hash of
+    the target protocol. *)
+val set_target_hash : hash:string option -> t -> t
 
 (** Log an error message and how to clean up created files and exits the
     process. *)
@@ -32,8 +101,4 @@ val exit : __LOC__:string -> t -> 'a
 
 (** Creates a state. *)
 val create :
-  git_dir:string ->
-  protocol_source:string ->
-  protocol_target:string ->
-  target_pattern:string ->
-  t
+  git_dir:string -> source:string -> target:string -> target_pattern:string -> t
