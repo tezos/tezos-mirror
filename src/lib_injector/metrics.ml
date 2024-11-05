c13569eee8a7d5908f3052b4b6ab493e17382906
Antoine Lanco <antoine.lanco@functori.com>
2024-11-05 11:13:12 +0100

    SORU/Metrics: Add labeled gauge metrics injector

--- src/lib_injector/metrics.ml
+++ src/lib_injector/metrics.ml
@@ -32,6 +32,9 @@ module Make (P : P) = struct
   (** Registers a gauge in [injector_registry] *)
   let v_gauge = Gauge.v ~registry ~namespace ~subsystem
 
+  (** Registers a labeled gauge in [injector_registry] *)
+  let v_label_gauge = Gauge.v_labels ~registry ~namespace ~subsystem
+
   module Tags = Injector_tags.Make (P.Tag)
 
   module Gauge_table = Hashtbl.Make (struct
@@ -43,6 +46,8 @@ module Make (P : P) = struct
   let concat_tags tags =
     String.concat ", "
     @@ List.map (fun t -> Format.asprintf "%a" P.Tag.pp t) tags
+
+  let batchers_balance_gauge_table = Gauge_table.create 7
 
   let queue_gauge_table = Gauge_table.create 7
 
@@ -91,24 +96,33 @@ module Make (P : P) = struct
     Gauge_table.add
       included_operations_gauge_table
       tags
-      included_operations_gauge
+      included_operations_gauge ;
+    let batchers_balance_gauge =
+      v_label_gauge
+        ~help:(Format.sprintf "injector %s (%d) batcher balance" tags_str i)
+        ~label_names:["pkh"; "tags"]
+        (Format.asprintf "%d_batcher_balance" i)
+    in
+    Gauge_table.add batchers_balance_gauge_table tags batchers_balance_gauge
 
-  let set_queue_size tags len =
-    match Gauge_table.find_opt queue_gauge_table (Tags.of_list tags) with
-    | None -> ()
-    | Some gauge -> Gauge.set gauge (Int.to_float len)
+  let set_gauge_batcher_balance tags key balance =
+    Option.iter
+      (fun labeled_gauge ->
+        Gauge.set
+          (Gauge.labels labeled_gauge [key; concat_tags tags])
+          (Int64.to_float balance /. 1_000_000.))
+      (Gauge_table.find_opt batchers_balance_gauge_table (Tags.of_list tags))
 
-  let set_injected_operations_size tags len =
-    match
-      Gauge_table.find_opt injected_operations_gauge_table (Tags.of_list tags)
-    with
-    | None -> ()
-    | Some gauge -> Gauge.set gauge (Int.to_float len)
+  let set_gauge_find_opt table tags len =
+    Option.iter
+      (fun gauge -> Gauge.set gauge (Int.to_float len))
+      (Gauge_table.find_opt table (Tags.of_list tags))
 
-  let set_included_operations_size tags len =
-    match
-      Gauge_table.find_opt included_operations_gauge_table (Tags.of_list tags)
-    with
-    | None -> ()
-    | Some gauge -> Gauge.set gauge (Int.to_float len)
+  let set_queue_size = set_gauge_find_opt queue_gauge_table
+
+  let set_injected_operations_size =
+    set_gauge_find_opt injected_operations_gauge_table
+
+  let set_included_operations_size =
+    set_gauge_find_opt included_operations_gauge_table
 end
