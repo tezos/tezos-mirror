621d20c7f1a22d5ad1e96dfbb8704ee9eee18f4b
Gabriel Moise <gabriel.moise@trili.tech>
2024-11-11 10:57:15 +0000

    Profiler: Add verbosity to profiler test
    This commit fulfils more purposes:
    - adds `Info` verbosity to all profiler calls
    - refactors the uses of `let* () = blah in return_unit` to just `blah`
    - gets rid of `Profiler.` syntax as we already have the `open Profiler` at the beginning of the file
    - removes the unnecessary (in my opinion) and confusing use of `let text = "text"` where `text` is used 2-3 times in the profiling logs, I believe this improves readability

--- src/lib_base/unix/test/test_simple_profiler.ml
+++ src/lib_base/unix/test/test_simple_profiler.ml
@@ -53,45 +53,45 @@ let check_file_content file_path =
     Sys.remove file_path
 
 let noop profiler =
-  Profiler.record profiler ("noop", []) ;
-  Profiler.stop profiler
+  record profiler Info ("noop", []) ;
+  stop profiler
 
 let sleep10ms profiler =
   let tag = "sleep10ms" in
-  Profiler.record profiler (tag, []) ;
+  record profiler Info (tag, []) ;
   Log.info "%s" tag ;
   Unix.sleepf 0.01 ;
   noop profiler ;
-  Profiler.stop profiler
+  stop profiler
 
 let sleep70ms profiler =
   let tag = "sleep70" in
-  Profiler.record profiler (tag, []) ;
+  record profiler Info (tag, []) ;
   Log.info "%s" tag ;
   Unix.sleepf 0.07 ;
-  Profiler.stop profiler
+  stop profiler
 
 let foo profiler =
-  Profiler.record profiler ("foo", []) ;
+  record profiler Info ("foo", []) ;
   sleep10ms profiler ;
   sleep70ms profiler ;
   sleep10ms profiler ;
-  Profiler.stop profiler
+  stop profiler
 
 let bar profiler =
-  Profiler.record profiler ("bar", []) ;
+  record profiler Info ("bar", []) ;
   sleep70ms profiler ;
   sleep10ms profiler ;
   foo profiler ;
   foo profiler ;
-  Profiler.stop profiler
+  stop profiler
 
 let test70 profiler =
   Log.info "\nTEST: test 70\n" ;
-  Profiler.record profiler ("test70", []) ;
+  record profiler Info ("test70", []) ;
   bar profiler ;
   foo profiler ;
-  Profiler.stop profiler
+  stop profiler
 
 let sleep_until_next_second () =
   let current_time = Unix.gettimeofday () in
@@ -101,164 +101,126 @@ let sleep_until_next_second () =
   Lwt_unix.sleep remaining_time
 
 let slp tag profiler =
-  let* () =
-    Profiler.record_s
-      profiler
-      ("slp " ^ tag, [])
-      (fun () ->
-        Log.info "sleep in %s" tag ;
-        let* () = Lwt_unix.sleep 0.01 in
-        Log.info "sleep done" ;
-        Lwt.return_unit)
-  in
-  return_unit
+  record_s
+    profiler
+    Info
+    ("slp " ^ tag, [])
+    (fun () ->
+      Log.info "sleep in %s" tag ;
+      let* () = Lwt_unix.sleep 0.01 in
+      Log.info "sleep done" ;
+      Lwt.return_unit)
 
 let sequences profiler =
-  let a_seq = "a_seq" in
-  let b_seq = "b_seq" in
-  let b_a_seq = "b_a_seq" in
-  let b_b_seq = "b_b_seq" in
   let* () =
-    Profiler.record_s profiler (a_seq, []) (fun () -> slp a_seq profiler)
+    record_s profiler Info ("a_seq", []) (fun () -> slp "a_seq" profiler)
   in
-  let* () =
-    Profiler.record_s profiler (b_seq, []) (fun () ->
-        let* () =
-          Profiler.record_s profiler (b_a_seq, []) (fun () ->
-              slp b_a_seq profiler)
-        in
-        let* () =
-          Profiler.record_s profiler (b_b_seq, []) (fun () ->
-              slp b_b_seq profiler)
-        in
-        slp b_seq profiler)
-  in
-  return_unit
+  record_s profiler Info ("b_seq", []) (fun () ->
+      let* () =
+        record_s profiler Info ("b_a_seq", []) (fun () ->
+            slp "b_a_seq" profiler)
+      in
+      let* () =
+        record_s profiler Info ("b_b_seq", []) (fun () ->
+            slp "b_b_seq" profiler)
+      in
+      slp "b_seq" profiler)
 
 let aggregates profiler =
-  let a_aggr = "a_aggr" in
-  let b_aggr = "b_aggr" in
-  let a_a_aggr = "a_a_aggr" in
-  let a_b_aggr = "a_b_aggr" in
-  let a_c_aggr = "a_c_aggr" in
-  let a_a_a_aggr = "a_a_a_aggr" in
   let* () =
-    Profiler.aggregate_s profiler (a_aggr, []) (fun () -> slp a_aggr profiler)
+    aggregate_s profiler Info ("a_aggr", []) (fun () -> slp "a_aggr" profiler)
   in
   let* () =
-    Profiler.aggregate_s profiler (b_aggr, []) (fun () -> slp b_aggr profiler)
+    aggregate_s profiler Info ("b_aggr", []) (fun () -> slp "b_aggr" profiler)
   in
   let* () =
-    Profiler.aggregate_s profiler (a_aggr, []) (fun () ->
+    aggregate_s profiler Info ("a_aggr", []) (fun () ->
         let* () =
-          Profiler.aggregate_s profiler (a_a_aggr, []) (fun () ->
-              slp a_a_aggr profiler)
+          aggregate_s profiler Info ("a_a_aggr", []) (fun () ->
+              slp "a_a_aggr" profiler)
         in
         let* () =
-          Profiler.aggregate_s profiler (a_b_aggr, []) (fun () ->
-              slp a_b_aggr profiler)
+          aggregate_s profiler Info ("a_b_aggr", []) (fun () ->
+              slp "a_b_aggr" profiler)
         in
         let* () =
-          Profiler.aggregate_s profiler (a_c_aggr, []) (fun () ->
-              slp a_c_aggr profiler)
+          aggregate_s profiler Info ("a_c_aggr", []) (fun () ->
+              slp "a_c_aggr" profiler)
         in
-        slp a_aggr profiler)
+        slp "a_aggr" profiler)
   in
-  let* () =
-    Profiler.aggregate_s profiler (a_aggr, []) (fun () ->
-        let* () =
-          Profiler.aggregate_s profiler (a_a_aggr, []) (fun () ->
-              Profiler.aggregate_s profiler (a_a_a_aggr, []) (fun () ->
-                  slp a_a_a_aggr profiler))
-        in
-        slp a_aggr profiler)
-  in
-  return_unit
+  aggregate_s profiler Info ("a_aggr", []) (fun () ->
+      let* () =
+        aggregate_s profiler Info ("a_a_aggr", []) (fun () ->
+            aggregate_s profiler Info ("a_a_a_aggr", []) (fun () ->
+                slp "a_a_a_aggr" profiler))
+      in
+      slp "a_aggr" profiler)
 
 let buggy_aggregates profiler =
-  let bug_aggr_a = "bug_aggr_a" in
-  let bug_aggr_b = "bug_aggr_b" in
-  let bug_aggr_b_a = "bug_aggr_b_a" in
-  let bug_aggr_b_b = "bug_aggr_b_b" in
-
   let t1 =
-    Profiler.aggregate_s profiler (bug_aggr_a, []) (fun () ->
-        slp bug_aggr_a profiler)
+    aggregate_s profiler Info ("bug_aggr_a", []) (fun () ->
+        slp "bug_aggr_a" profiler)
   in
   let t2 =
-    Profiler.aggregate_s profiler (bug_aggr_b, []) (fun () ->
-        let* () = slp bug_aggr_b_a profiler in
-        slp bug_aggr_b_b profiler)
+    aggregate_s profiler Info ("bug_aggr_b", []) (fun () ->
+        let* () = slp "bug_aggr_b_a" profiler in
+        slp "bug_aggr_b_b" profiler)
   in
   Lwt.join [t1; t2]
 
 let spans profiler =
-  let span1 = "span1" in
-  let span2 = "span2" in
-  let inner1 = "inner1" in
-  let inner2 = "inner2" in
-  let inner11 = "inner1/1" in
-  let inner12 = "inner1/2" in
-  let inner21 = "inner2/1" in
-  let inner22 = "inner2/2" in
   let t1 =
-    Profiler.span_s profiler ([span1], []) (fun () -> slp span1 profiler)
+    span_s profiler Info (["span1"], []) (fun () -> slp "span1" profiler)
   in
   let t2 =
-    Profiler.span_s
+    span_s
       profiler
-      ([span2; inner1], [])
+      Info
+      (["span2"; "inner1"], [])
       (fun () ->
-        let* () = slp (span2 ^ ", " ^ inner11) profiler in
-        slp (span2 ^ ", " ^ inner12) profiler)
+        let* () = slp "span2, inner1/1" profiler in
+        slp "span2, inner1/2" profiler)
   in
   let t3 =
-    Profiler.span_s
+    span_s
       profiler
-      ([span2; inner2], [])
+      Info
+      (["span2"; "inner2"], [])
       (fun () ->
-        let* () = slp (span2 ^ ", " ^ inner21) profiler in
-        slp (span2 ^ ", " ^ inner22) profiler)
+        let* () = slp "span2, inner2/1" profiler in
+        slp "span2, inner2/2" profiler)
   in
   Lwt.join [t1; t2; t3]
 
 let sequences_with_mark_and_stamp profiler =
-  let a_seq = "a_seq" in
-  let b_seq = "b_seq" in
-  let b_a_seq = "b_a_seq" in
-  let b_b_seq = "b_b_seq" in
-  let mark = "mark" in
-  let stamp = "stamp" in
   let* () =
-    Profiler.record_s profiler (a_seq, []) (fun () -> slp a_seq profiler)
+    record_s profiler Info ("a_seq", []) (fun () -> slp "a_seq" profiler)
   in
-  let* () =
-    Profiler.record_s profiler (b_seq, []) (fun () ->
-        let* () =
-          Profiler.record_s profiler (b_a_seq, []) (fun () ->
-              slp b_a_seq profiler)
-        in
-        let () =
-          List.iter (fun _ -> Profiler.mark profiler ([mark], [])) (1 -- 10)
-        in
-        let () = Profiler.stamp profiler (stamp, []) in
-        let* () =
-          Profiler.record_s profiler (b_b_seq, []) (fun () ->
-              slp b_b_seq profiler)
-        in
-        let () =
-          List.iter (fun _ -> Profiler.mark profiler ([mark], [])) (1 -- 10)
-        in
-        let () = Profiler.stamp profiler (stamp, []) in
-        slp b_seq profiler)
-  in
-  return_unit
+  record_s profiler Info ("b_seq", []) (fun () ->
+      let* () =
+        record_s profiler Info ("b_a_seq", []) (fun () ->
+            slp "b_a_seq" profiler)
+      in
+      let () =
+        List.iter (fun _ -> mark profiler Info (["mark"], [])) (1 -- 10)
+      in
+      stamp profiler Info ("stamp", []) ;
+      let* () =
+        record_s profiler Info ("b_b_seq", []) (fun () ->
+            slp "b_b_seq" profiler)
+      in
+      let () =
+        List.iter (fun _ -> mark profiler Info (["mark"], [])) (1 -- 10)
+      in
+      stamp profiler Info ("stamp", []) ;
+      slp "b_seq" profiler)
 
 let test_profiler_actions profiler =
   Log.info "\nTEST: test_profiler_actions\n" ;
   let main () =
     let* () = sleep_until_next_second () in
-    Profiler.record_s profiler ("main", []) (fun () ->
+    record_s profiler Info ("main", []) (fun () ->
         let* () = sequences profiler in
         let* () = aggregates profiler in
         let* () = buggy_aggregates profiler in
@@ -271,11 +233,11 @@ let test_profiler_actions profiler =
 let get_profiler file_name =
   let profiler = unplugged () in
   let test_profiler_instance =
-    Profiler.instance
+    instance
       (* The default driver is a text driver writing to a file
          without suffixing it *)
       Tezos_base_unix.Simple_profiler.auto_write_as_txt_to_file
-      (file_name, Profiler.Info)
+      (file_name, Info)
   in
   plug profiler test_profiler_instance ;
   profiler
