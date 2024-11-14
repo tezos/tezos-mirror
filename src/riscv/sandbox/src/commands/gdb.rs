5d312b67e22cb2efb878f6c6fd729c17b91a4f0d
Emma Turner <emma.turner@trili.tech>
2024-11-14 17:05:14 +0000

    RISC-V: gdb can read XRegisters

--- src/riscv/sandbox/src/commands/gdb.rs
+++ src/riscv/sandbox/src/commands/gdb.rs
@@ -16,21 +16,43 @@ use gdbstub::target::ext::base::BaseOps;
 use gdbstub::target::ext::breakpoints::{
     Breakpoints, BreakpointsOps, SwBreakpoint, SwBreakpointOps,
 };
-use gdbstub::target::{Target, TargetResult};
+use gdbstub::target::{Target, TargetError, TargetResult};
+use gdbstub_arch::riscv::reg::RiscvCoreRegs;
+use octez_riscv::machine_state::bus::main_memory::M1G;
+use octez_riscv::machine_state::bus::OutOfBounds;
+use octez_riscv::machine_state::mode::Mode;
+use octez_riscv::machine_state::registers::XRegister;
+use octez_riscv::stepper::test::TestStepper;
+use octez_riscv::stepper::Stepper;
 
 use crate::cli::GdbServerOptions;
 use std::error::Error;
-use std::io;
+use std::marker::PhantomData;
 use std::net::{TcpListener, TcpStream};
+use std::{fs, io};
 
 /// Run a gdb server that can be used for debugging RISC-V programs.
 pub fn gdb_server(opts: GdbServerOptions) -> Result<(), Box<dyn Error>> {
-    let mut target = RiscvGdb;
+    let fname = opts
+        .input
+        .file_name()
+        .ok_or("Invalid program path")?
+        .to_str()
+        .ok_or("File name cannot be converted to string")?;
+    let program = fs::read(&opts.input)?;
+    let (mut stepper, _symbols) =
+        TestStepper::<M1G>::new_with_parsed_program(program.as_slice(), None, Mode::User)?;
+
+    // loop
+    let mut target = RiscvGdb {
+        stepper: &mut stepper,
+    };
+
     let connection = wait_for_gdb_connection(opts.port)?;
     let connection: Box<dyn ConnectionExt<Error = std::io::Error>> = Box::new(connection);
     let mut debugger = GdbStub::new(connection);
 
-    let disconnect = debugger.run_blocking::<RiscVEventLoop>(&mut target)?;
+    let disconnect = debugger.run_blocking::<RiscvEventLoop<_>>(&mut target)?;
 
     eprintln!("{:?}", disconnect);
     Ok(())
@@ -48,12 +70,14 @@ fn wait_for_gdb_connection(port: u16) -> io::Result<TcpStream> {
     Ok(stream) // `TcpStream` implements `gdbstub::Connection`
 }
 
-struct RiscvGdb;
+struct RiscvGdb<'a, S: Stepper> {
+    stepper: &'a mut S,
+}
 
-impl RiscvGdb {
+impl<'a, S: Stepper> RiscvGdb<'a, S> {
     fn run_and_check_for_incoming_data(
         &mut self,
-        conn: &mut <RiscVEventLoop as run_blocking::BlockingEventLoop>::Connection,
+        conn: &mut <RiscvEventLoop<S> as run_blocking::BlockingEventLoop>::Connection,
     ) -> RiscvGdbEvent {
         let mut poll_incoming_data = || {
             // gdbstub takes ownership of the underlying connection, so the `borrow_conn`
@@ -72,7 +96,7 @@ enum RiscvGdbEvent {
     IncomingData,
 }
 
-impl Target for RiscvGdb {
+impl<'a, S: Stepper> Target for RiscvGdb<'a, S> {
     type Error = String;
     type Arch = gdbstub_arch::riscv::Riscv64;
 
@@ -88,12 +112,16 @@ impl Target for RiscvGdb {
     }
 }
 
-impl SingleThreadBase for RiscvGdb {
-    fn read_registers(
-        &mut self,
-        regs: &mut <Self::Arch as Arch>::Registers,
-    ) -> TargetResult<(), Self> {
-        todo!()
+impl<'a, S: Stepper> SingleThreadBase for RiscvGdb<'a, S> {
+    fn read_registers(&mut self, regs: &mut RiscvCoreRegs<u64>) -> TargetResult<(), Self> {
+        let state = self.stepper.machine_state();
+        regs.pc = state.hart.pc.read();
+
+        let array = state.hart.xregisters.struct_ref();
+        for i in 0..31 {
+            regs.x[i + 1] = array.read(i);
+        }
+        Ok(())
     }
 
     fn write_registers(
@@ -104,7 +132,17 @@ impl SingleThreadBase for RiscvGdb {
     }
 
     fn read_addrs(&mut self, start_addr: u64, data: &mut [u8]) -> TargetResult<usize, Self> {
-        todo!()
+        const EFAULT: u8 = 14;
+        eprintln!("read_addrs: {start_addr} -> {}", data.len());
+
+        let state = self.stepper.machine_state();
+
+        use octez_riscv::machine_state::bus::AddressableRead;
+
+        match state.main_memory.read_all(start_addr, data) {
+            Ok(()) => Ok(data.len()),
+            Err(OutOfBounds) => Err(TargetError::Errno(EFAULT)),
+        }
     }
 
     fn write_addrs(&mut self, start_addr: u64, data: &[u8]) -> TargetResult<(), Self> {
@@ -117,7 +155,7 @@ impl SingleThreadBase for RiscvGdb {
     }
 }
 
-impl SingleThreadResume for RiscvGdb {
+impl<'a, S: Stepper> SingleThreadResume for RiscvGdb<'a, S> {
     fn resume(&mut self, signal: Option<Signal>) -> Result<(), Self::Error> {
         todo!()
     }
@@ -128,13 +166,14 @@ impl SingleThreadResume for RiscvGdb {
     }
 }
 
-impl SingleThreadSingleStep for RiscvGdb {
+impl<'a, S: Stepper> SingleThreadSingleStep for RiscvGdb<'a, S> {
     fn step(&mut self, signal: Option<Signal>) -> Result<(), Self::Error> {
         todo!()
     }
 }
 
-impl Breakpoints for RiscvGdb {
+impl<'a, S: Stepper> Breakpoints for RiscvGdb<'a, S> {
+    // there are several kinds of breakpoints - this target uses software breakpoints
     // We implement support for Software Breakpoints, in a similar fashion to
     // the debugger.
     #[inline(always)]
@@ -143,7 +182,7 @@ impl Breakpoints for RiscvGdb {
     }
 }
 
-impl SwBreakpoint for RiscvGdb {
+impl<'a, S: Stepper> SwBreakpoint for RiscvGdb<'a, S> {
     fn add_sw_breakpoint(
         &mut self,
         addr: u64,
@@ -161,12 +200,12 @@ impl SwBreakpoint for RiscvGdb {
     }
 }
 
-enum RiscVEventLoop {}
+struct RiscvEventLoop<'a, S: Stepper>(PhantomData<&'a S>);
 
 // The `run_blocking::BlockingEventLoop` groups together various callbacks
 // the `GdbStub::run_blocking` event loop requires you to implement.
-impl run_blocking::BlockingEventLoop for RiscVEventLoop {
-    type Target = RiscvGdb;
+impl<'a, S: Stepper> run_blocking::BlockingEventLoop for RiscvEventLoop<'a, S> {
+    type Target = RiscvGdb<'a, S>;
     type Connection = Box<dyn ConnectionExt<Error = std::io::Error>>;
 
     // or MultiThreadStopReason on multi threaded targets
@@ -176,7 +215,7 @@ impl run_blocking::BlockingEventLoop for RiscVEventLoop {
     // called. The implementation should block until either the target
     // reports a stop reason, or if new data was sent over the connection.
     fn wait_for_stop_reason(
-        target: &mut RiscvGdb,
+        target: &mut Self::Target,
         conn: &mut Self::Connection,
     ) -> Result<
         run_blocking::Event<SingleThreadStopReason<u64>>,
@@ -193,7 +232,6 @@ impl run_blocking::BlockingEventLoop for RiscVEventLoop {
         //
         // in this example, lets assume the target has a magic method that handles
         // this for us.
-
         let event = match target.run_and_check_for_incoming_data(conn) {
             RiscvGdbEvent::IncomingData => {
                 let byte = conn
@@ -209,46 +247,13 @@ impl run_blocking::BlockingEventLoop for RiscVEventLoop {
 
     // Invoked when the GDB client sends a Ctrl-C interrupt.
     fn on_interrupt(
-        target: &mut RiscvGdb,
-    ) -> Result<Option<SingleThreadStopReason<u64>>, <RiscvGdb as Target>::Error> {
+        target: &mut Self::Target,
+    ) -> Result<Option<SingleThreadStopReason<u64>>, <Self::Target as Target>::Error> {
         // notify the target that a ctrl-c interrupt has occurred.
         // target.stop_in_response_to_ctrl_c_interrupt()?;
 
         // a pretty typical stop reason in response to a Ctrl-C interrupt is to
         // report a "Signal::SIGINT".
         Ok(Some(SingleThreadStopReason::Signal(Signal::SIGINT).into()))
-    }
-}
-
-fn gdb_event_loop_thread(
-    debugger: GdbStub<RiscvGdb, Box<dyn ConnectionExt<Error = std::io::Error>>>,
-    mut target: RiscvGdb,
-) {
-    match debugger.run_blocking::<RiscVEventLoop>(&mut target) {
-        Ok(disconnect_reason) => match disconnect_reason {
-            DisconnectReason::Disconnect => {
-                println!("Client disconnected")
-            }
-            DisconnectReason::TargetExited(code) => {
-                println!("Target exited with code {}", code)
-            }
-            DisconnectReason::TargetTerminated(sig) => {
-                println!("Target terminated with signal {}", sig)
-            }
-            DisconnectReason::Kill => println!("GDB sent a kill command"),
-        },
-        Err(e) => {
-            if e.is_target_error() {
-                println!(
-                    "target encountered a fatal error: {:?}",
-                    e.into_target_error().unwrap()
-                )
-            } else if e.is_connection_error() {
-                let (e, kind) = e.into_connection_error().unwrap();
-                println!("connection error: {:?} - {}", kind, e,)
-            } else {
-                println!("gdbstub encountered a fatal error: {:?}", e)
-            }
-        }
     }
 }
