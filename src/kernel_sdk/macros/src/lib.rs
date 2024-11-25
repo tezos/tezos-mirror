32735c60db69e671690605135d879f1c89f8e751
Ole Krüger <ole.kruger@trili.tech>
2024-11-25 18:54:09 +0100

    Kernel/SDK: Allow disabling runtime options macro with feature flag

--- src/kernel_sdk/macros/src/lib.rs
+++ src/kernel_sdk/macros/src/lib.rs
@@ -6,13 +6,12 @@
 //! You should not depend on this crate directly, but instead use them as exported
 //! from `tezos_smart_rollup`.
 
+#[cfg(feature = "runtime-options")]
 mod runtime;
 
 use proc_macro::TokenStream;
 use proc_macro_error2::{abort, proc_macro_error};
 use quote::{quote, ToTokens};
-use runtime::RuntimeConfig;
-use syn::{parse_macro_input, ItemFn};
 
 /// Mark a function of type `fn(&mut impl Runtime)` as the entrypoint of the kernel.
 ///
@@ -101,62 +100,8 @@ pub fn main(attr: TokenStream, item: TokenStream) -> TokenStream {
 ///     // do nothing
 /// }
 /// ```
+#[cfg(feature = "runtime-options")]
 #[proc_macro_attribute]
 pub fn runtime(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let runtime: RuntimeConfig = parse_macro_input!(attr);
-
-    let code: TokenStream = match runtime.static_inbox {
-        None => {
-            // No inbox path given, behave as a no-op
-            item
-        }
-        Some(path) => {
-            let ItemFn {
-                attrs,
-                vis,
-                sig,
-                block,
-            } = parse_macro_input!(item);
-            let fn_stmts = &block.stmts;
-            let fn_name = sig.ident.to_token_stream();
-            // An inbox path was given, evaluate in the shell for environment variables.
-            let input_file = shellexpand::full(&path).unwrap();
-            quote! {
-                // Function attributes are passed to the nested function to keep
-                // "intuitive" style of calling syntactic modifiers.
-                // More detailed expalantion in tezos_smart_rollup::entrypoint module.
-                #vis fn #fn_name(host: &mut impl Runtime) {
-                    // Override the function name so that we call into the nested kernel.
-                    // While keeping the name for the outer function the same so that
-                    // the outer macros keep operating on the same name (but modified contents)
-                    #(#attrs)*
-                    #sig {
-                        #(#fn_stmts)*
-                    }
-
-                    mod __tezos_runtime_static_inbox {
-                        use tezos_smart_rollup::entrypoint::internal::StaticInbox;
-                        use std::cell::RefCell;
-                        use std::thread_local;
-                        use tezos_smart_rollup::prelude::*;
-
-                        const INPUT: &str = include_str!(#input_file);
-
-                        thread_local! {
-                            pub static STATIC_INBOX: RefCell<StaticInbox> = RefCell::new(StaticInbox::new_from_json(INPUT));
-                        }
-
-                    }
-                    __tezos_runtime_static_inbox::STATIC_INBOX.with_borrow_mut(|inbox| {
-                        let mut host = inbox.wrap_runtime(host);
-
-                        #fn_name(&mut host)
-                    });
-                }
-            }
-            .into()
-        }
-    };
-
-    code
+    runtime::runtime_attribute(attr, item)
 }
