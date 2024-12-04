43b7ed30a50d7a977a5164fb3d18cc30b506d391
Pietro Abate <pietro.abate@nomadic-labs.com>
2024-12-04 11:53:55 +0100

    rpm packages: add installation script

---
+++ docs/introduction/install-bin-rpm.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+distribution=$1
+release=$2
+
+bucket="$GCP_LINUX_PACKAGES_BUCKET"
+protocol=$(head -1 script-inputs/active_protocol_versions_without_number)
+
+. scripts/ci/octez-packages-version.sh
+
+case "$RELEASETYPE" in
+ReleaseCandidate | TestReleaseCandidate)
+  distribution="RC/${distribution}"
+  ;;
+Release | TestRelease)
+  : nop
+  ;;
+Master)
+  distribution="master/${distribution}"
+  ;;
+SoftRelease)
+  distribution="${CI_COMMIT_TAG}/${distribution}"
+  ;;
+TestBranch)
+  distribution="${CI_COMMIT_REF_NAME}/${distribution}"
+  ;;
+*)
+  echo "Cannot test packages on this branch"
+  exit 1
+  ;;
+esac
+
+set -e
+set -x
+
+if [ "$RELEASETYPE" = "Master" ]; then
+  # [add repository]
+  # Update and install the config-mananger plugin
+  dnf -y update
+  dnf -y install 'dnf-command(config-manager)'
+
+  # Add the repository
+  dnf -y config-manager --add-repo "https://packages.nomadic-labs.com/next/$distribution/dists/$release/"
+  dnf -y config-manager --set-enabled devel
+  dnf -y update
+
+  # Install public key
+  rpm --import "https://packages.nomadic-labs.com/next/$distribution/octez.asc"
+  # [end add repository]
+else
+  # Update and install the config-mananger plugin
+  dnf -y update
+  dnf -y install 'dnf-command(config-manager)'
+
+  # Add the repository
+  dnf -y config-manager --add-repo "https://storage.googleapis.com/$bucket/next/$distribution/dists/$release/"
+  dnf -y config-manager --set-enabled devel
+  dnf -y update
+
+  # Install public key
+  rpm --import "https://storage.googleapis.com/$bucket/next/$distribution/octez.asc"
+
+fi
+
+dnf -y install sudo
+
+# [install tezos]
+sudo dnf -y install octez-node
+sudo dnf -y install octez-client
+sudo dnf -y install octez-node
+sudo dnf -y install octez-baker
+sudo dnf -y install octez-dal-node
+sudo dnf -y install octez-smart-rollup-node
+
+# [test executables]
+octez-client --version
+octez-node --version
+"octez-baker-$protocol" --version
+"octez-accuser-$protocol" --version
+
+# [test autopurge]
+sudo dnf -y remove octez-node octez-client octez-baker octez-dal-node
