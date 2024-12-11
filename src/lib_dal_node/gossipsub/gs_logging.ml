6810b9b142a86061a35d0a1bcef9f20b6df3aaaa
iguerNL@Functori <iguer@functori.com>
2024-12-11 07:04:27 +0100

    DAL: show bootstrap flag of connections in Gs_logging after its removal from peer

--- src/lib_dal_node/gossipsub/gs_logging.ml
+++ src/lib_dal_node/gossipsub/gs_logging.ml
@@ -82,17 +82,18 @@ module Events = struct
       ("topic", Types.Topic.encoding)
 
   let new_connection =
-    declare_3
+    declare_4
       ~section
       ~name:(prefix "new_connection")
       ~msg:
         "Process New_connection from/to {peer} (direct={direct}, \
-         trusted={trusted})"
+         trusted={trusted}, bootstrap={bootstrap})"
       ~level:Notice
       ~pp1:Types.Peer.pp
       ("peer", Types.Peer.encoding)
       ("direct", bool)
       ("trusted", bool)
+      ("bootstrap", bool)
 
   let disconnection =
     declare_1
@@ -205,8 +206,8 @@ let event =
       | Leave topic -> emit leave topic)
   | P2P_input event -> (
       match event with
-      | New_connection {peer; direct; trusted; bootstrap = _} ->
-          emit new_connection (peer, direct, trusted)
+      | New_connection {peer; direct; trusted; bootstrap} ->
+          emit new_connection (peer, direct, trusted, bootstrap)
       | Disconnection {peer} -> emit disconnection peer
       | In_message {from_peer; p2p_message} -> (
           match p2p_message with
