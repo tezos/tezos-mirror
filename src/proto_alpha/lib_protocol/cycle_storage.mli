597c89a0fe69ce4d50c78fcc03537a7e0ef762f1
Diane Gallois-Wong <diane.gallois-wong@nomadic-labs.com>
2024-12-13 16:54:04 +0100

    Proto: factorize and comment last cycle with sampling data and
    cycle to be cleared when a new cycle starts

--- src/proto_alpha/lib_protocol/cycle_storage.mli
+++ src/proto_alpha/lib_protocol/cycle_storage.mli
@@ -10,6 +10,18 @@
 (** Current cycle, that is, the cycle of the current level. *)
 val current : Raw_context.t -> Cycle_repr.t
 
+(** The oldest cycle on which the context keeps sampling data. *)
+val oldest_cycle_with_sampling_data : Raw_context.t -> Cycle_repr.t
+
+(** The cycle whose sampling data should be cleared when [new_cycle]
+    starts.
+
+    Returns [None] when the computed cycle would be negative: this
+    means there are no cycles that should be cleared when [new_cycle]
+    starts. *)
+val cycle_to_clear_of_sampling_data :
+  new_cycle:Cycle_repr.t -> Cycle_repr.t option
+
 (** Highest cycle whose unstake requests are currently finalizable.
 
     Returns [None] when the computed cycle would be negative: this
