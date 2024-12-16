8008141afcac273ed201843f78d347caee0963b5
Alain Mebsout <alain.mebsout@functori.com>
2024-12-16 16:10:46 +0100

    Tezt: don't force dream for websocket tests

--- etherlink/tezt/tests/evm_sequencer.ml
+++ etherlink/tezt/tests/evm_sequencer.ml
@@ -8791,7 +8791,6 @@ let test_websocket_rpcs =
        |> List.map (fun a -> a.Eth_account.address))
       @ Eth_account.lots_of_address)
     ~minimum_base_fee_per_gas:base_fee_for_hardcoded_tx
-    ~rpc_server:Dream (* Websockets only available in Dream *)
     ~websockets:true
   @@ fun {sequencer; _} _protocol ->
   Log.info "Opening a websocket connection with the node" ;
@@ -8866,7 +8865,6 @@ let test_websocket_subscription_rpcs_cant_be_called_via_http_requests =
        |> List.map (fun a -> a.Eth_account.address))
       @ Eth_account.lots_of_address)
     ~minimum_base_fee_per_gas:base_fee_for_hardcoded_tx
-    ~rpc_server:Dream (* Websockets only available in Dream *)
     ~websockets:true
   @@ fun {sequencer; _} _protocol ->
   let* () =
@@ -8917,7 +8915,6 @@ let test_websocket_newHeads_event =
        |> List.map (fun a -> a.Eth_account.address))
       @ Eth_account.lots_of_address)
     ~minimum_base_fee_per_gas:base_fee_for_hardcoded_tx
-    ~rpc_server:Dream (* Websockets only available in Dream *)
     ~websockets:true
   @@ fun {sequencer; observer; _} _protocol ->
   let scenario evm_node (lvl, lvl') =
@@ -8952,7 +8949,6 @@ let test_websocket_cleanup =
        |> List.map (fun a -> a.Eth_account.address))
       @ Eth_account.lots_of_address)
     ~minimum_base_fee_per_gas:base_fee_for_hardcoded_tx
-    ~rpc_server:Dream (* Websockets only available in Dream *)
     ~websockets:true
   @@ fun {sequencer; _} _protocol ->
   let* websocket = Evm_node.open_websocket sequencer in
@@ -8985,14 +8981,12 @@ let test_websocket_newPendingTransactions_event =
        |> List.map (fun a -> a.Eth_account.address))
       @ Eth_account.lots_of_address)
     ~minimum_base_fee_per_gas:base_fee_for_hardcoded_tx
-    ~rpc_server:Dream (* Websockets only available in Dream *)
     ~websockets:true
   @@ fun {sequencer; sc_rollup_node; _} _protocol ->
   let* observer =
     run_new_observer_node
       ~finalized_view:false
       ~sc_rollup_node
-      ~rpc_server:Dream (* Websockets only available in Dream *)
       ~websockets:true
       sequencer
   in
@@ -9037,7 +9031,6 @@ let test_websocket_logs_event =
        |> List.map (fun a -> a.Eth_account.address))
       @ Eth_account.lots_of_address)
     ~minimum_base_fee_per_gas:base_fee_for_hardcoded_tx
-    ~rpc_server:Dream
     ~websockets:true
   @@ fun {sequencer; observer; _} _protocol ->
   let scenario evm_node =
