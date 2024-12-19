167146f00e8228c63e8fc5aeeb0312fc44c74bb8
Victor Dumitrescu <v.dumitrescu@outlook.com>
2024-12-19 11:44:35 +0100

    Proto/RISC-V: Dummy protocol environment interface for the PVM

--- src/proto_alpha/lib_protocol/sc_rollup_riscv.ml
+++ src/proto_alpha/lib_protocol/sc_rollup_riscv.ml
@@ -1,10 +1,111 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* SPDX-License-Identifier: MIT                                              *)
-(* Copyright (c) 2023 Nomadic Labs <contact@nomadic-labs.com>                *)
-(* Copyright (c) 2024 TriliTech, <contact@trili.tech>                        *)
+(* Copyright (c) 2023 - 2024 Nomadic Labs <contact@nomadic-labs.com>         *)
+(* Copyright (c) 2024 - 2025 TriliTech, <contact@trili.tech>                 *)
 (*                                                                           *)
 (*****************************************************************************)
+
+(* TODO RV-374: Move `Riscv_proto_env_sig` and `Riscv_proto_env` to protocol
+   environment *)
+module type Riscv_proto_env_sig = sig
+  type state
+
+  type proof
+
+  type output
+
+  type output_proof
+
+  type hash = Smart_rollup.State_hash.t
+
+  type input
+
+  type input_request
+
+  val state_hash : state -> hash
+
+  val empty_state : unit -> state
+
+  val proof_start_state : proof -> hash
+
+  val proof_stop_state : proof -> hash
+
+  val proof_to_bytes : proof -> bytes
+
+  val bytes_to_proof : bytes -> (proof, string) result
+
+  val install_boot_sector : state -> string -> state
+
+  val verify_proof : input option -> proof -> input_request option
+
+  val output_of_output_proof : output_proof -> output
+
+  val state_of_output_proof : output_proof -> hash
+
+  val verify_output_proof : output_proof -> output option
+
+  val output_proof_to_bytes : output_proof -> bytes
+
+  val bytes_to_output_proof : bytes -> (output_proof, string) result
+
+  val get_current_level : state -> int32 option
+end
+
+module Riscv_proto_env : Riscv_proto_env_sig = struct
+  type state = {
+    payload : string;
+    level : Raw_level_repr.t option;
+    message_counter : Z.t;
+    tick : Z.t;
+  }
+
+  type proof = unit
+
+  type output = unit
+
+  type output_proof = unit
+
+  type hash = Smart_rollup.State_hash.t
+
+  type input = unit
+
+  type input_request = unit
+
+  (* In order to synchronise with the node implementation of the PVM at genesis,
+     * we set the state hash to be the initial state hash of the node
+     * implementation. *)
+  let state_hash _state =
+    Smart_rollup.State_hash.of_b58check_exn
+      "srs129JscUr3XsPcNFUEiKqVNP38tn8oksbGir1qYXgQs8QD7bcNNd"
+
+  let empty_state () =
+    {payload = ""; level = None; message_counter = Z.zero; tick = Z.zero}
+
+  let proof_start_state _proof = assert false
+
+  let proof_stop_state _proof = assert false
+
+  let proof_to_bytes _proof = assert false
+
+  let bytes_to_proof _bytes = assert false
+
+  let install_boot_sector state boot_sector = {state with payload = boot_sector}
+
+  let verify_proof _input _proof = assert false
+
+  let output_of_output_proof _output_proof = assert false
+
+  let state_of_output_proof _output_proof = assert false
+
+  let verify_output_proof _output_proof = assert false
+
+  let output_proof_to_bytes _output_proof = assert false
+
+  let bytes_to_output_proof _bytes = assert false
+
+  let get_current_level _state = assert false
+end
 
 open Sc_rollup_repr
 module PS = Sc_rollup_PVM_sig
--- src/proto_alpha/lib_protocol/sc_rollup_riscv.mli
+++ src/proto_alpha/lib_protocol/sc_rollup_riscv.mli
@@ -1,10 +1,58 @@
 (*****************************************************************************)
 (*                                                                           *)
 (* SPDX-License-Identifier: MIT                                              *)
-(* Copyright (c) 2023 Nomadic Labs <contact@nomadic-labs.com>                *)
-(* Copyright (c) 2024 TriliTech <contact@trili.tech>                        *)
+(* Copyright (c) 2023 - 2024 Nomadic Labs <contact@nomadic-labs.com>         *)
+(* Copyright (c) 2024 - 2025 TriliTech <contact@trili.tech>                  *)
 (*                                                                           *)
 (*****************************************************************************)
+
+(* TODO RV-374: Move `Riscv_proto_env_sig` and `Riscv_proto_env` to protocol
+   environment *)
+module type Riscv_proto_env_sig = sig
+  type state
+
+  type proof
+
+  type output
+
+  type output_proof
+
+  type hash = Smart_rollup.State_hash.t
+
+  type input
+
+  type input_request
+
+  val state_hash : state -> hash
+
+  val empty_state : unit -> state
+
+  val proof_start_state : proof -> hash
+
+  val proof_stop_state : proof -> hash
+
+  val proof_to_bytes : proof -> bytes
+
+  val bytes_to_proof : bytes -> (proof, string) result
+
+  val install_boot_sector : state -> string -> state
+
+  val verify_proof : input option -> proof -> input_request option
+
+  val output_of_output_proof : output_proof -> output
+
+  val state_of_output_proof : output_proof -> hash
+
+  val verify_output_proof : output_proof -> output option
+
+  val output_proof_to_bytes : output_proof -> bytes
+
+  val bytes_to_output_proof : bytes -> (output_proof, string) result
+
+  val get_current_level : state -> int32 option
+end
+
+module Riscv_proto_env : Riscv_proto_env_sig
 
 type void = |
 
