3e7d10c38a7e67bfcee2732c3af3cfae905c8b49
Rodi-Can Bozman <rodi.bozman@functori.com>
2025-01-07 16:22:40 +0100

    EVM/Evaluation: activate Cancun in evaluation

--- etherlink/kernel_evm/evm_evaluation/src/runner.rs
+++ etherlink/kernel_evm/evm_evaluation/src/runner.rs
@@ -339,6 +339,7 @@ pub fn run_test(
         for (spec_name, tests) in &unit.post {
             let config = match spec_name {
                 SpecName::Shanghai => Config::shanghai(),
+                SpecName::Cancun => Config::cancun(),
                 // TODO: enable future configs when parallelization is enabled.
                 // Other tests are ignored
                 _ => continue,
