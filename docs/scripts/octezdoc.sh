8425396774a95433441da7e4ca7cfc2327590954
Nic Volanschi <nic.volanschi@nomadic-labs.com>
2025-01-17 12:45:49 +0100

    doc: order pages in octezdoc.txt

---
+++ docs/scripts/octezdoc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Generate a single-file text version of the documentation usable by bots
+
+MYDIR="$(dirname "$0")"
+BUILDDIR="$MYDIR"/../_build
+
+grep 'toctree-l\d' $BUILDDIR/index.html | \
+  sed 's|^.*href="||; s|.html">.*$|.txt|'|\
+  grep -v -f $MYDIR/octezdoc-excludes.txt | \
+  while read f
+  do
+    cat _build_txt/$f
+    # insert blank line
+    echo ""
+  done
