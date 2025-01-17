af3e557e85218964cd64ed6d65094272ada883ad
Nic Volanschi <nic.volanschi@nomadic-labs.com>
2025-01-17 14:32:13 +0100

    doc: fix format & syntax of octezdoc.sh

--- docs/scripts/octezdoc.sh
+++ docs/scripts/octezdoc.sh
@@ -4,12 +4,11 @@
 MYDIR="$(dirname "$0")"
 BUILDDIR="$MYDIR"/../_build
 
-grep 'toctree-l\d' $BUILDDIR/index.html | \
-  sed 's|^.*href="||; s|.html">.*$|.txt|'|\
-  grep -v -f $MYDIR/octezdoc-excludes.txt | \
-  while read f
-  do
-    cat _build_txt/$f
+grep 'toctree-l\d' "$BUILDDIR"/index.html |
+  sed 's|^.*href="||; s|.html">.*$|.txt|' |
+  grep -v -f "$MYDIR"/octezdoc-excludes.txt |
+  while read -r f; do
+    cat _build_txt/"$f"
     # insert blank line
     echo ""
   done
