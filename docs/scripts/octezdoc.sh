fd36f869c63e72b94fc936c44e006436e1f95dd0
Nic Volanschi <nic.volanschi@nomadic-labs.com>
2025-01-22 16:33:59 +0100

    doc: add comments to octezdoc.sh

--- docs/scripts/octezdoc.sh
+++ docs/scripts/octezdoc.sh
@@ -4,11 +4,13 @@
 MYDIR="$(dirname "$0")"
 BUILDDIR="$MYDIR"/../_build
 
+# Extract order of pages from the HTML root index,
+# then exclude blacklisted pages
 grep 'toctree-l\d' "$BUILDDIR"/index.html |
   sed 's|^.*href="||; s|.html">.*$|.txt|' |
   grep -v -f "$MYDIR"/octezdoc-excludes.txt |
   while read -r f; do
     cat _build_txt/"$f"
-    # insert blank line
+    # insert blank line at the end of each file
     echo ""
   done
