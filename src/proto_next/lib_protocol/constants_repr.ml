b32ef6374e4dd0c0594f42866002112a9832f95b
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2025-01-28 18:48:16 +0100

    Next/src: set current version

--- src/proto_next/lib_protocol/constants_repr.ml
+++ src/proto_next/lib_protocol/constants_repr.ml
@@ -24,7 +24,7 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
-let version_value = "alpha_current"
+let version_value = "next"
 
 let version = "v1"
 
