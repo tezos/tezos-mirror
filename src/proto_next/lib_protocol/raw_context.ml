7a1b17db852bb20b414bb28be91e5b3450ed03bd
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2025-01-28 18:48:16 +0100

    Next/src: adapt Next predecessors

--- src/proto_next/lib_protocol/raw_context.ml
+++ src/proto_next/lib_protocol/raw_context.ml
@@ -889,8 +889,8 @@ let prepare ~level ~predecessor_timestamp ~timestamp ~adaptive_issuance_enable
 
 type previous_protocol =
   | Genesis of Parameters_repr.t
-  | Alpha
-  | (* Alpha predecessor *) Quebec (* Alpha predecessor *)
+  | Next
+  | (* Next predecessor *) Quebec (* Next predecessor *)
 
 let check_and_update_protocol_version ctxt =
   let open Lwt_result_syntax in
@@ -906,9 +906,9 @@ let check_and_update_protocol_version ctxt =
         else if Compare.String.(s = "genesis") then
           let+ param, ctxt = get_proto_param ctxt in
           (Genesis param, ctxt)
-        else if Compare.String.(s = "alpha_current") then return (Alpha, ctxt)
-        else if (* Alpha predecessor *) Compare.String.(s = "quebec_021") then
-          return (Quebec, ctxt) (* Alpha predecessor *)
+        else if Compare.String.(s = "next") then return (Next, ctxt)
+        else if (* Next predecessor *) Compare.String.(s = "quebec_021") then
+          return (Quebec, ctxt) (* Next predecessor *)
         else Lwt.return @@ storage_error (Incompatible_protocol_version s)
   in
   let*! ctxt =
@@ -996,10 +996,10 @@ let prepare_first_block ~level ~timestamp chain_id ctxt =
         let* ctxt = set_cycle_eras ctxt cycle_eras in
         let*! result = add_constants ctxt param.constants in
         return (result, None)
-    (* Start of Alpha stitching. Comment used for automatic snapshot *)
-    | Alpha ->
+    (* Start of Next stitching. Comment used for automatic snapshot *)
+    | Next ->
         (*
-            FIXME chain_id is used for Q to Alpha migration and nomore after.
+            FIXME chain_id is used for Q to Next migration and nomore after.
             We ignored for automatic stabilisation, should it be removed in
             Beta?
         *)
@@ -1290,7 +1290,7 @@ let prepare_first_block ~level ~timestamp chain_id ctxt =
            it should be removed in quebec when stabilising *)
         let*! c = get_previous_protocol_constants ctxt in
         return (ctxt, Some c)
-        (* End of Alpha stitching. Comment used for automatic snapshot *)
+        (* End of Next stitching. Comment used for automatic snapshot *)
         (* Start of alpha predecessor stitching. Comment used for automatic snapshot *)
     | Quebec ->
         (* See {!Constants_parametric_repr} for instructions on how to
