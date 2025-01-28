91940a49bad42dc8196d005f38db6ed084a04d45
François Thiré <francois.thire@nomadic-labs.com>
2025-01-28 14:55:49 +0100

    Baker/Alpha: Nit when the DAL node is not healthy

--- src/proto_alpha/lib_delegate/baking_events.ml
+++ src/proto_alpha/lib_delegate/baking_events.ml
@@ -99,9 +99,8 @@ module Commands = struct
       ~level:Error
       ~msg:
         "The DAL node running on {endpoint} is not healthy. DAL attestations \
-         cannot be sent.\n\
-         Its health is {health}.\n\
-         Please check your DAL node and possibly restart it."
+         cannot be sent. Its health is {health}. Please check that your DAL \
+         node is configured correctly."
       ~pp1:Uri.pp
       ("endpoint", Tezos_rpc.Encoding.uri_encoding)
       ~pp2:Tezos_dal_node_services.Types.Health.pp
