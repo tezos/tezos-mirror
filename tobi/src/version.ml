0e1d8db66c84a2fbe155cc3d6836faafb0cc08c8
Romain <romain@nomadic-labs.com>
2025-01-29 12:57:27 +0100

    Tobi: add Version module

---
+++ tobi/src/version.ml
@@ -0,0 +1,12 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2025 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type t = Dev | Old of string
+
+let parse = function "dev" -> Dev | v -> Old v
+
+let show = function Dev -> "dev" | Old v -> v
