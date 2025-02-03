e33187973f69662761aba10f48ef188aa9bbe41e
Rodi-Can Bozman <rodi.bozman@functori.com>
2025-02-03 13:51:16 +0100

    EVM/Kernel: fast_withdrawal_parameters abi decoder
    This is hack-ish like the rest of this file, we should get rid of it
    and use a proper crate for it.

--- etherlink/kernel_evm/evm_execution/src/abi.rs
+++ etherlink/kernel_evm/evm_execution/src/abi.rs
@@ -9,8 +9,11 @@
 //! at
 //! [Contract ABI specification](https://docs.soliditylang.org/en/develop/abi-spec.html)
 
-// TODO investigate if we can use this Rust package instead:
-// https://docs.rs/ethabi/latest/ethabi/
+// TODO: https://gitlab.com/tezos/tezos/-/issues/7722
+// This whole file is hack-ish.
+// In the long term we should get rid of this file completely and rely on a proper
+// implementation for abi parameters (for instance see cast's implementation to
+// depend on the same crates).
 
 use primitive_types::{H160, U256};
 
@@ -42,6 +45,41 @@ pub fn bytes_parameter(input_data: &[u8], parameter_number: usize) -> Option<&[u
 /// Get a string parameter from a contract call, input data buffer
 pub fn string_parameter(input_data: &[u8], parameter_number: usize) -> Option<&str> {
     core::str::from_utf8(bytes_parameter(input_data, parameter_number)?).ok()
+}
+
+pub fn fast_withdrawal_parameters(input_data: &[u8]) -> Option<(&str, &str, Vec<u8>)> {
+    // Disclaimer: The following implementation is REALLY hack-ish. But it's the most straighforward
+    // decoding function until we find a better crate to handle these encodings.
+    // We ignore the first three slices of 32 bytes, they are the position of the parameters.
+    // In our case, since it's a hack-ish implementation, we can directly infer them.
+    const U256_SIZE: usize = 32;
+
+    let first_arg_size: usize = U256::from(input_data.get(3 * U256_SIZE..4 * U256_SIZE)?)
+        .try_into()
+        .ok()?;
+    let number_of_slice = first_arg_size.div_ceil(U256_SIZE);
+    let read_to = first_arg_size + 4 * U256_SIZE;
+    let arg_1 = &input_data.get(4 * U256_SIZE..read_to)?;
+    let arg_1 = core::str::from_utf8(arg_1).ok()?;
+
+    let from = U256_SIZE * number_of_slice + 4 * U256_SIZE;
+    let second_args_size: usize = U256::from(input_data.get(from..(from + U256_SIZE))?)
+        .try_into()
+        .ok()?;
+    let number_of_slice = second_args_size.div_ceil(U256_SIZE);
+    let read_to = (from + U256_SIZE) + second_args_size;
+    let arg_2 = &input_data.get((from + U256_SIZE)..read_to)?;
+    let arg_2 = core::str::from_utf8(arg_2).ok()?;
+
+    let from = U256_SIZE * number_of_slice + (from + U256_SIZE);
+    let third_args_size: usize = U256::from(input_data.get(from..(from + U256_SIZE))?)
+        .try_into()
+        .ok()?;
+    let read_to = third_args_size + (from + U256_SIZE);
+    let arg_3 = &input_data.get((from + U256_SIZE)..read_to)?;
+    let arg_3 = arg_3.to_vec();
+
+    Some((arg_1, arg_2, arg_3))
 }
 
 /// Get an address parameter from the input data buffer
@@ -84,5 +122,19 @@ pub fn fixed_bytes_parameter<const N: usize>(
         }
     } else {
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::fast_withdrawal_parameters;
+
+    #[test]
+    fn test_fast_withdrawal_parameters() {
+        let test_case = hex::decode("000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000024747a316670356e63446d7159775943353638665245597a3969775154674751754b5a7158000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000024747a316670356e63446d7159775943353638665245597a3969775154674751754b5a7158000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+        let (arg_1, arg_2, arg_3) = fast_withdrawal_parameters(&test_case).unwrap();
+        assert_eq!("tz1fp5ncDmqYwYC568fREYz9iwQTgGQuKZqX", arg_1);
+        assert_eq!("tz1fp5ncDmqYwYC568fREYz9iwQTgGQuKZqX", arg_2);
+        assert_eq!(Vec::<u8>::new(), arg_3)
     }
 }
