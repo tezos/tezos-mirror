6e7b244a1b2b8d179f42738b8b3d118626a41d96
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2025-02-05 13:11:23 +0100

    Bees: add the Hive to handle starting worker loops
    Co-authored-by: Mathias Bourgoin <mathias.bourgoin@nomadic-labs.com>
    Co-authored-by: mattiasdrp <mattias@kojin.tech>
    Co-authored-by: Julien Sagot <julien.sagot@lambda-coins.com>

---
+++ src/lib_bees/hive.ml
@@ -0,0 +1,44 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* SPDX-FileCopyrightText: 2025 Nomadic Labs <contact@nomadic-labs.com>      *)
+(*                                                                           *)
+(*****************************************************************************)
+
+type worker =
+  | Worker : {
+      worker : _;
+      launched : Time.System.t;
+      switch : Eio.Switch.t;
+      subdomains : int;
+    }
+      -> worker
+
+type t = {workers : worker String.Hashtbl.t}
+
+let hive = {workers = String.Hashtbl.create 64}
+
+exception Unknown_worker of string
+
+let launch_worker (type worker) (worker : worker) ~bee_name ~domains worker_loop
+    =
+  let {workers} = hive in
+  let env = Tezos_base_unix.Event_loop.env_exn () in
+  let switch = Tezos_base_unix.Event_loop.main_switch_exn () in
+  for i = 0 to domains - 1 do
+    Eio.Fiber.fork_daemon ~sw:switch (fun () ->
+        Eio.Domain_manager.run env#domain_mgr (fun () -> worker_loop i worker))
+  done ;
+  String.Hashtbl.add
+    workers
+    bee_name
+    (Worker
+       {worker; launched = Time.System.now (); subdomains = domains; switch})
+
+let get_error bee_name =
+  let {workers} = hive in
+  match String.Hashtbl.find_opt workers bee_name with
+  (* Note that this branch shouldn't be reachable, as this function is supposed
+     to be called from within an existing worker loop. *)
+  | None -> raise (Unknown_worker bee_name)
+  | Some (Worker {switch; _}) -> Eio.Switch.get_error switch
