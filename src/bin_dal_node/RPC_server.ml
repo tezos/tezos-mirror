5acde32e1b54f64b7a44baa0ddb420c778a0e05e
François Thiré <francois.thire@nomadic-labs.com>
2025-02-14 16:13:52 +0100

    DAL/Node: Register and implement the `get_fanout` RPC

--- src/bin_dal_node/RPC_server.ml
+++ src/bin_dal_node/RPC_server.ml
@@ -496,6 +496,10 @@ module P2P = struct
            ~subscribed:q#subscribed
            ctxt
 
+    let get_fanout ctxt () () =
+      let open Lwt_result_syntax in
+      return @@ Node_context.P2P.Gossipsub.get_fanout ctxt
+
     let get_slot_indexes_peers ctxt q () =
       let open Lwt_result_syntax in
       return
@@ -611,6 +615,10 @@ let register :
        Tezos_rpc.Directory.register0
        Services.P2P.Gossipsub.get_topics_peers
        (P2P.Gossipsub.get_topics_peers ctxt)
+  |> add_service
+       Tezos_rpc.Directory.register0
+       Services.P2P.Gossipsub.get_fanout
+       (P2P.Gossipsub.get_fanout ctxt)
   |> add_service
        Tezos_rpc.Directory.register0
        Services.P2P.Gossipsub.get_slot_indexes_peers
