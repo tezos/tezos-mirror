50828a1c1cbdea4224945bf2ad615693099998d9
Diane Gallois-Wong <diane.gallois-wong@nomadic-labs.com>
2025-02-18 15:20:42 +0100

    Proto/init_storage: init AI earlier

--- src/proto_alpha/lib_protocol/init_storage.ml
+++ src/proto_alpha/lib_protocol/init_storage.ml
@@ -222,6 +222,7 @@ let prepare_first_block chain_id ctxt ~typecheck_smart_contract
             param.bootstrap_contracts
             param.bootstrap_smart_rollups
         in
+        let* ctxt = Adaptive_issuance_storage.init_from_genesis ctxt in
         let* ctxt = Delegate_cycles.init_first_cycles ctxt in
         let* ctxt =
           Vote_storage.init
@@ -241,7 +242,6 @@ let prepare_first_block chain_id ctxt ~typecheck_smart_contract
             operation_results
         in
         let* ctxt = Sc_rollup_inbox_storage.init_inbox ~predecessor ctxt in
-        let* ctxt = Adaptive_issuance_storage.init_from_genesis ctxt in
         let* ctxt = Address_registry_storage.init ctxt in
         return (ctxt, commitments_balance_updates @ bootstrap_balance_updates)
         (* Start of Alpha stitching. Comment used for automatic snapshot *)
