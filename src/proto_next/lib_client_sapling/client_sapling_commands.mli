3c679c2522309606c3adb7cb5af1516dd32b7a44
mbourgoin <mathias.bourgoin@nomadic-labs.com>
2025-02-19 09:30:25 +0100

    R022/src: remove proto_next

--- src/proto_next/lib_client_sapling/client_sapling_commands.mli
+++
@@ -1,8 +0,0 @@
-(*****************************************************************************)
-(*                                                                           *)
-(* SPDX-License-Identifier: MIT                                              *)
-(* Copyright (c) 2019-2020 Nomadic Labs, <contact@nomadic-labs.com>          *)
-(*                                                                           *)
-(*****************************************************************************)
-
-val commands : unit -> Protocol_client_context.full Tezos_clic.command list
