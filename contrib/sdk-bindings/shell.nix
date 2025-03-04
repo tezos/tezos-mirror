ba0dc25631921e66074519882db019f07c8cfeb7
Sébastien Palmer <sebastien.palmer@functori.com>
2025-03-04 09:35:19 +0100

    SDK/bindings: add a shell.nix for dependencies

---
+++ contrib/sdk-bindings/shell.nix
@@ -0,0 +1,15 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+  buildInputs = [
+    # Rust
+    pkgs.rustup
+
+    # Python
+    pkgs.python39
+    pkgs.yapf
+  ];
+}
