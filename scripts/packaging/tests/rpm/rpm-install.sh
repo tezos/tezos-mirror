14660e888ece701734f7e798177020c105133883
Pietro Abate <pietro.abate@nomadic-labs.com>
2025-03-05 21:14:40 +0100

    packages: download ghostnet snapshot while testing

--- scripts/packaging/tests/rpm/rpm-install.sh
+++ scripts/packaging/tests/rpm/rpm-install.sh
@@ -37,6 +37,21 @@ dnf -y install sudo procps
 sudo dnf -y install octez-client
 dnf -y install octez-node
 
+#shellcheck disable=SC2129,SC1091
+echo "NETWORK=ghostnet" >> /etc/default/octez-node
+echo "HISTORY_MODE=rolling" >> /etc/default/octez-node
+echo "SNAPSHOT_NO_CHECK=" >> /etc/default/octez-node
+#shellcheck disable=SC1091
+. /etc/default/octez-node
+
+rm "$DATADIR/config.json"
+su tezos -c "/usr/bin/octez-node config init \
+      --data-dir=$DATADIR \
+      --network=$NETWORK \
+      --history-mode=$HISTORY_MODE \
+      --net-addr=\"[::]:9732\" \
+      --rpc-addr=\"127.0.0.1:8732\""
+
 # if systemd is available we test the service scripts
 if [ "$(ps --no-headers -o comm 1)" = "systemd" ]; then
   systemctl enable octez-node
