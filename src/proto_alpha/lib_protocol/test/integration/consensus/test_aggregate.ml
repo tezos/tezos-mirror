76f7f0a19059b84d4e1d1505980ed85162df3ae9
Adam Allombert-Goget <adam.allombert@nomadic-labs.com>
2025-03-09 12:49:08 +0100

    proto-unit-test: add aggregation test for a single delegate

--- src/proto_alpha/lib_protocol/test/integration/consensus/test_aggregate.ml
+++ src/proto_alpha/lib_protocol/test/integration/consensus/test_aggregate.ml
@@ -48,6 +48,89 @@ let aggregate_unimplemented_error = function
   | Validate_errors.Consensus.Aggregate_not_implemented -> true
   | _ -> false
 
+let find_aggregate_result receipt =
+  let result_opt =
+    List.find_map
+      (function
+        | Tezos_protocol_alpha__Protocol.Apply_results.Operation_metadata
+            {
+              contents =
+                Single_result (Attestations_aggregate_result _ as result);
+            } ->
+            Some result
+        | _ -> None)
+      receipt
+  in
+  match result_opt with
+  | Some res -> res
+  | None -> Test.fail "No aggregate result found"
+
+(* [check_attestations_aggregate_result ~committee result] verifies that
+   [result] has the following properties:
+   - [balance_update] is empty;
+   - [voting_power] equals the sum of slots owned by attesters in [committee];
+   - the public key hashes in [result] committee match those of [committee]. *)
+let check_attestations_aggregate_result ~committee
+    (result :
+      Alpha_context.Kind.attestations_aggregate
+      Tezos_protocol_alpha__Protocol.Apply_results.contents_result) =
+  let open Lwt_result_syntax in
+  match result with
+  | Attestations_aggregate_result
+      {balance_updates; committee = resulting_committee; consensus_power} ->
+      (* Check balance updates *)
+      let* () =
+        match balance_updates with
+        | [] -> return_unit
+        | _ -> Test.fail "Unexpected non-empty balance updates list"
+      in
+      (* Check voting power *)
+      let* () =
+        let voting_power =
+          List.fold_left
+            (fun acc (delegate : RPC.Validators.t) ->
+              List.length delegate.slots + acc)
+            0
+            committee
+        in
+        if voting_power = consensus_power then return_unit
+        else
+          Test.fail
+            "Wrong voting power : expected %d, found %d"
+            voting_power
+            consensus_power
+      in
+      (* Check committee *)
+      let committee_pkhs =
+        List.map
+          (fun (consensus_key : RPC.Validators.t) -> consensus_key.delegate)
+          committee
+      in
+      let resulting_committee_pkhs =
+        List.map
+          (fun (attester : Alpha_context.Consensus_key.t) -> attester.delegate)
+          resulting_committee
+      in
+      if
+        List.equal
+          Tezos_crypto.Signature.Public_key_hash.equal
+          resulting_committee_pkhs
+          committee_pkhs
+      then return_unit
+      else
+        let pp =
+          Format.(
+            pp_print_list
+              ~pp_sep:pp_print_cut
+              Tezos_crypto.Signature.Public_key_hash.pp)
+        in
+        Test.fail
+          "@[<v 0>Wrong commitee@,@[<v 2>expected:@,%a@]@,@[<v 2>found:@,%a@]@]"
+          pp
+          committee_pkhs
+          pp
+          resulting_committee_pkhs
+
 let test_aggregate_feature_flag_enabled () =
   let open Lwt_result_syntax in
   let* _genesis, attested_block =
@@ -72,6 +155,33 @@ let test_aggregate_feature_flag_disabled () =
     ~mempool_error:aggregate_in_mempool_error
     Aggregate
 
+let test_aggregate_attestation_with_a_single_bls_attestation () =
+  let open Lwt_result_syntax in
+  let* _genesis, block =
+    init_genesis_with_some_bls_accounts ~aggregate_attestation:true ()
+  in
+  let* attesters = Context.get_attesters (B block) in
+  let attester, slot =
+    (* Find an attester with a BLS consensus key. *)
+    WithExceptions.Option.get
+      ~loc:__LOC__
+      (List.find_map
+         (fun (attester : RPC.Validators.t) ->
+           match (attester.consensus_key, attester.slots) with
+           | Bls _, slot :: _ -> Some (attester, slot)
+           | _ -> None)
+         attesters)
+  in
+  let* attestation =
+    Op.raw_attestation ~delegate:attester.RPC.Validators.delegate ~slot block
+  in
+  let operation =
+    WithExceptions.Option.get ~loc:__LOC__ (Op.aggregate [attestation])
+  in
+  let* _, (_, receipt) = Block.bake_with_metadata ~operation block in
+  let result = find_aggregate_result receipt in
+  check_attestations_aggregate_result ~committee:[attester] result
+
 let tests =
   [
     Tztest.tztest
@@ -82,6 +192,10 @@ let tests =
       "test_aggregate_feature_flag_disabled"
       `Quick
       test_aggregate_feature_flag_disabled;
+    Tztest.tztest
+      "test_aggregate_attestation_with_a_single_bls_attestation"
+      `Quick
+      test_aggregate_attestation_with_a_single_bls_attestation;
   ]
 
 let () =
