d9a2d23c9b675957feb0d46dd1362a3b01b6ebdb
mbourgoin <mathias.bourgoin@nomadic-labs.com>
2025-03-10 16:03:44 +0100

    lib_base/event_loop: don't create a main eio switch

--- src/lib_bees/hive.ml
+++ src/lib_bees/hive.ml
@@ -32,16 +32,22 @@ let launch_worker (type worker) (worker : worker) ~bee_name ~domains worker_loop
     =
   let {workers} = hive in
   let env = Tezos_base_unix.Event_loop.env_exn () in
-  let switch = Tezos_base_unix.Event_loop.main_switch_exn () in
-  for i = 0 to domains - 1 do
-    Eio.Fiber.fork_daemon ~sw:switch (fun () ->
-        Eio.Domain_manager.run env#domain_mgr (fun () -> worker_loop i worker))
-  done ;
-  String.Hashtbl.add
-    workers
-    bee_name
-    (Worker
-       {worker; launched = Time.System.now (); subdomains = domains; switch})
+  Eio.Switch.run (fun sw ->
+      for i = 0 to domains - 1 do
+        Eio.Fiber.fork_daemon ~sw (fun () ->
+            Eio.Domain_manager.run env#domain_mgr (fun () ->
+                worker_loop i worker))
+      done ;
+      String.Hashtbl.add
+        workers
+        bee_name
+        (Worker
+           {
+             worker;
+             launched = Time.System.now ();
+             subdomains = domains;
+             switch = sw;
+           }))
 
 let get_error bee_name =
   let {workers} = hive in
