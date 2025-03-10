c9dba2ce5b09f04cf3cadfdc53ef68b98ed20b30
Neelay Sant <neelay.sant@trili.tech>
2025-03-10 16:52:27 +0000

    RISC-V: Support JrImm over ICB

--- src/riscv/lib/src/jit.rs
+++ src/riscv/lib/src/jit.rs
@@ -826,6 +826,43 @@ mod tests {
         }
     });
 
+    backend_test!(test_jr_imm, F, {
+        use crate::machine_state::registers::NonZeroXRegister::*;
+
+        let scenarios: &[Scenario<'_, F>] = &[
+            ScenarioBuilder::default()
+                // Jumping to the next instruction should exit the block
+                .set_instructions(&[
+                    I::new_li(x2, 10, Uncompressed),
+                    I::new_jr_imm(x2, 10, Compressed),
+                    I::new_nop(Uncompressed),
+                ])
+                .set_assert_hook(&|core: &MachineCoreState<M4K, F::Manager>| {
+                    assert_eq!(core.hart.pc.read(), 20);
+                })
+                .set_expected_steps(2)
+                .build(),
+            ScenarioBuilder::default()
+                // Jumping to start of the block should still exit.
+                .set_instructions(&[
+                    I::new_li(x6, 10, Compressed),
+                    I::new_jr_imm(x6, -10, Uncompressed),
+                ])
+                .set_assert_hook(&|core: &MachineCoreState<M4K, F::Manager>| {
+                    assert_eq!(core.hart.pc.read(), 0);
+                })
+                .set_expected_steps(2)
+                .build(),
+        ];
+
+        let mut jit = JIT::<M4K, F::Manager>::new().unwrap();
+        let mut interpreted_bb = InterpretedBlockBuilder;
+
+        for scenario in scenarios {
+            scenario.run(&mut jit, &mut interpreted_bb);
+        }
+    });
+
     backend_test!(test_addi, F, {
         use Instruction as I;
 
