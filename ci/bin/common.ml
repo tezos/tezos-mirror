d36dbfef1b91c2b906b4289b2c580f92724a8226
Pietro Abate <pietro.abate@nomadic-labs.com>
2025-03-11 12:15:55 +0100

    l1 profile: enable slim mode

--- ci/bin/common.ml
+++ ci/bin/common.ml
@@ -980,6 +980,7 @@ let job_build_layer1_profiling ?(expire_in = Duration (Days 1)) () =
          [])
     ~variables:[("TEZOS_PPX_PROFILER", "profiling"); ("PROFILE", "static")]
     [
+      "scripts/slim-mode.sh on";
       "make build OCTEZ_EXECUTABLES?=octez-node";
       "mkdir -p octez-binaries/x86_64/";
       "mv octez-node octez-binaries/x86_64/";
