7151f42ced95ce1b08e2e2f583a55c78d044fbf7
Rodi-Can Bozman <rodi.bozman@functori.com>
2025-03-13 14:00:18 +0100

    EVM/Kernel: add migration code to set EVM config if it was never set before

--- etherlink/kernel_evm/kernel/src/storage.rs
+++ etherlink/kernel_evm/kernel/src/storage.rs
@@ -60,6 +60,7 @@ pub enum StorageVersion {
     V25,
     V26,
     V27,
+    V28,
 }
 
 impl From<StorageVersion> for u64 {
@@ -74,7 +75,7 @@ impl StorageVersion {
     }
 }
 
-pub const STORAGE_VERSION: StorageVersion = StorageVersion::V27;
+pub const STORAGE_VERSION: StorageVersion = StorageVersion::V28;
 
 pub const PRIVATE_FLAG_PATH: RefPath = RefPath::assert_from(b"/evm/remove_whitelist");
 
