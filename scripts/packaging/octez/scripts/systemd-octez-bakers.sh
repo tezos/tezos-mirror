2c9fde4dd5aa1d7bdad637863ed1493894efebe7
Pietro Abate <pietro.abate@nomadic-labs.com>
2025-03-13 15:08:47 +0100

    packages: refactor of systemd tests

--- scripts/packaging/octez/scripts/systemd-octez-bakers.sh
+++ scripts/packaging/octez/scripts/systemd-octez-bakers.sh
@@ -20,9 +20,13 @@ for file in /usr/bin/octez-baker-P*; do
       systemctl start octez-dal-node
     fi
     systemctl enable "octez-baker@$proto"
+    systemctl enable "octez-accuser@$proto"
     systemctl start "octez-baker@$proto"
+    systemctl start "octez-accuser@$proto"
+
   elif [ "$1" = "stop" ]; then
     systemctl stop "octez-baker@$proto"
+    systemctl stop "octez-accuser@$proto"
     if grep -q "\-\-dal-node" /etc/default/octez-baker; then
       systemctl stop octez-dal-node
     fi
