2ccf022eecf0b29711266a9885b916a4adb5310e
iguerNL@Functori <iguer@functori.com>
2025-03-17 16:20:49 +0100

    DAL/Node: refactor fetch_dal_config. Introduce a helper fetch_info_from_l1

--- src/bin_dal_node/daemon.ml
+++ src/bin_dal_node/daemon.ml
@@ -23,36 +23,35 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let fetch_dal_config cctxt =
+let fetch_info_from_l1 cctxt ~what ~rpc =
   let open Lwt_syntax in
   let delay = 0.1 in
   let delay_max = 20.0 in
+  let endpoint = Uri.to_string cctxt#base in
   let rec retry delay =
-    let* r = Config_services.dal_config cctxt in
+    let* r = rpc cctxt in
     match r with
     | Error
         [RPC_client_errors.(Request_failed {error = Connection_failed _; _})] ->
         let delay = min delay_max (delay *. 2.) in
-        let event_kind = if delay < delay_max then `Notice else `Warning in
         let* () =
           Event.emit_retry_fetching_info_from_l1
-            ~endpoint:(Uri.to_string cctxt#base)
+            ~endpoint
             ~delay
-            ~what:"DAL config"
-            ~event_kind
+            ~what
+            ~event_kind:(if delay < delay_max then `Notice else `Warning)
         in
         let* () = Lwt_unix.sleep delay in
         retry delay
     | Error err -> return_error err
-    | Ok dal_config ->
-        let* () =
-          Event.emit_fetched_l1_info_success
-            ~endpoint:(Uri.to_string cctxt#base)
-            ~what:"DAL config"
-        in
-        return_ok dal_config
+    | Ok res ->
+        let* () = Event.emit_fetched_l1_info_success ~endpoint ~what in
+        return_ok res
   in
   retry delay
+
+let fetch_dal_config cctxt =
+  fetch_info_from_l1 cctxt ~rpc:Config_services.dal_config ~what:"DAL config"
 
 let init_cryptobox config proto_parameters =
   let open Lwt_result_syntax in
