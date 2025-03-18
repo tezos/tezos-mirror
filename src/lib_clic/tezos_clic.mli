ea33040f0cc9e95ab851e6b726416b10401c010d
Thomas Letan <lthms@nomadic-labs.com>
2025-03-18 11:11:06 +0100

    EVM Node: Support setting key options with env variables
    With this patch, we extend the Octez EVM node to support setting some key
    options (`--data-dir`, `--config-file`, and `--network`) using environment
    variables.
    
    This change is implemented by modifying `Tezos_clic`. While we do not try to
    make use of this new feature in another component than the EVM node, it seems
    clear other binaries could leverage this. The Rollup Node is an obvious
    candidate.
    
    We prefix the environment variable names with `EVM_NODE_` to avoid having two
    binaries using the same environment variable. That is, `--data-dir` can also be
    set using `EVM_NODE_DATA_DIR` instead of `DATA_DIR`.

--- src/lib_clic/tezos_clic.mli
+++ src/lib_clic/tezos_clic.mli
@@ -91,12 +91,16 @@ val constant : 'a -> ('a, 'ctx) arg
 (** [arg ~doc ~long ?short converter] creates an argument to a command.
     The [~long] argument is the long format, without the double dashes.
     The [?short] argument is the optional one letter shortcut.
+    The [?env] argument is the optional environment variable name that can be
+    used to provide a default value. If the environment variable is set and
+    the CLI argument is passed, the CLI argument is used.
     If the argument is not provided, [None] is returned. *)
 val arg :
   doc:string ->
   ?short:char ->
   long:string ->
   placeholder:string ->
+  ?env:string ->
   ('a, 'ctx) parameter ->
   ('a option, 'ctx) arg
 
@@ -114,13 +118,16 @@ val multiple_arg :
   ('a, 'ctx) parameter ->
   ('a list option, 'ctx) arg
 
-(** Create an argument that will contain the [~default] value if it is not provided. *)
+(** Create an argument that will contain the [~default] value if it is not provided.
+    [?env] is the optional environment variable name that can be
+    used to override the default value. *)
 val default_arg :
   doc:string ->
   ?short:char ->
   long:string ->
   placeholder:string ->
   default:string ->
+  ?env:string ->
   ('a, 'ctx) parameter ->
   ('a, 'ctx) arg
 
