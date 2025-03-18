f4a5926676eae67b55e59ea35c2942485addacf0
iguerNL@Functori <iguer@functori.com>
2025-03-18 13:48:09 +0100

    DAL/Node: show the network name when emitting node_is_ready event

--- src/bin_dal_node/daemon.ml
+++ src/bin_dal_node/daemon.ml
@@ -1764,6 +1764,6 @@ let run ~data_dir ~configuration_override =
   (* Register topics with gossipsub worker. *)
   let* () = update_and_register_profiles ctxt in
   (* Start never-ending monitoring daemons *)
-  let*! () = Event.emit_node_is_ready () in
+  let*! () = Event.emit_node_is_ready ~network_name in
   let* () = daemonize [Handler.new_finalized_head ctxt cctxt crawler] in
   return_unit
