62e56095e701ee690f21daf04bd8a8cb7e89d4e3
Gabriel Moise <gabriel.moise@trili.tech>
2025-03-26 14:32:32 +0000

    Agnostic_baker: Refactor Rpc_services module

--- src/lib_agnostic_baker/rpc_services.mli
+++ src/lib_agnostic_baker/rpc_services.mli
@@ -2,30 +2,34 @@
 (*                                                                           *)
 (* SPDX-License-Identifier: MIT                                              *)
 (* Copyright (c) 2024 Nomadic Labs, <contact@nomadic-labs.com>               *)
+(* Copyright (c) 2025 Trilitech <contact@trili.tech>                         *)
 (*                                                                           *)
 (*****************************************************************************)
 
-(** [request_uri ~node_addr ~uri] is a raw call that will return the
-    Cohttp response of an RPC call, given a [~uri], against the
-    [~node_addr]. *)
+(** [request_uri ~node_addr ~uri] issues an HTTP [GET] request to the [~uri] and
+    returns the response and its body. In case the connection is refused,
+    it fails with a connection error on [~node_addr]. *)
 val request_uri :
   node_addr:string ->
   uri:string ->
   (Cohttp_lwt_unix.Response.t * Cohttp_lwt.Body.t) tzresult Lwt.t
 
-(** [get_level ~node_addr] returns the level of the block. *)
+(** [get_level ~node_addr] retrieves the current block level from the node at
+    [~node_addr]. *)
 val get_level : node_addr:string -> (int, error trace) result Lwt.t
 
-(** [get_level ~node_addr] returns the hash of the block. *)
+(** [get_block_hash ~node_addr] retrieves the hash of the current block from the node
+    at [~node_addr]. *)
 val get_block_hash :
   node_addr:string -> (Block_hash.t, error trace) result Lwt.t
 
-(** [get_next_protocol_hash ~node_addr] returns the protocol hash
-    contained in the [next_protocol] field of the metadata of a
-    block. *)
+(** [get_next_protocol_hash ~node_addr] retrieves the protocol hash from the [next_protocol]
+    field in the metadata of the current block, as seen by the node at [~node_addr]. *)
 val get_next_protocol_hash : node_addr:string -> Protocol_hash.t tzresult Lwt.t
 
-(** [get_current_period ~node_addr] returns the current voting
-    period in addition to the number of remaining blocks until the end
-    of the period. *)
+(** [get_current_period ~node_addr] retrieves the current voting period information
+    from the node at [~node_addr]. It returns a pair [(kind, remaining)] where:
+    - [kind] is a string representing the voting period kind, and
+    - [remaining] is the number of blocks remaining until the end of the voting period. *)
+
 val get_current_period : node_addr:string -> (string * int) tzresult Lwt.t
