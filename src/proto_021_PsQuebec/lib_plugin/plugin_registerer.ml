b1f1ce7ede4718cd5550ef69cded060230624a75
Diane Gallois-Wong <diane.gallois-wong@nomadic-labs.com>
2025-03-28 19:09:05 +0100

    021_PsQuebec/plugin: add delegators_contribution plugin
    Porting to proto 021_PsQuebec 4d5dbe7178df12718721270eaad1c1f78f8f9ea4 - plugin: add delegators_contribution plugin
    
    + adapt code to Quebec

--- src/proto_021_PsQuebec/lib_plugin/plugin_registerer.ml
+++ src/proto_021_PsQuebec/lib_plugin/plugin_registerer.ml
@@ -53,3 +53,69 @@ let () =
   Protocol_plugin.register_http_cache_headers_plugin (module Http_cache_headers)
 
 let () = Protocol_plugin.register_shell_helpers (module Shell_helpers)
+
+module Delegators_contribution_plugin = struct
+  let hash = Registerer.Registered.hash
+
+  let convert_pkh :
+      Tezos_crypto.Signature.public_key_hash ->
+      Tezos_crypto__.Signature_v1.public_key_hash = function
+    | Ed25519 x -> Ed25519 x
+    | Secp256k1 x -> Secp256k1 x
+    | P256 x -> P256 x
+    | Bls x -> Bls x
+
+  let delegated_breakdown_at_sampling context ~cycle ~delegate_pkh =
+    let open Lwt_result_syntax in
+    let* output =
+      Delegators_contribution.delegated_breakdown_at_sampling
+        context
+        ~cycle
+        ~delegate_pkh:(convert_pkh delegate_pkh)
+    in
+    match output with
+    | `Ok
+        {
+          min_delegated_amount;
+          min_delegated_level;
+          overstaked;
+          total_delegated_including_overdelegated;
+          total_delegated_after_limits;
+          overdelegated;
+        } ->
+        return
+          (`Ok
+            {
+              Protocol_plugin.min_delegated_amount;
+              min_delegated_level;
+              overstaked;
+              total_delegated_including_overdelegated;
+              total_delegated_after_limits;
+              overdelegated;
+            })
+    | (`Retry_at_level _ | `Cycle_too_far_in_future) as x -> return x
+
+  let min_delegated_breakdown context ~delegate_pkh =
+    let open Lwt_result_syntax in
+    let* {
+           total_delegated;
+           own_delegated;
+           delegators_contributions;
+           former_delegators_unstake_requests;
+         } =
+      Delegators_contribution.min_delegated_breakdown
+        context
+        ~delegate_pkh:(convert_pkh delegate_pkh)
+    in
+    return
+      {
+        Protocol_plugin.total_delegated;
+        own_delegated;
+        delegators_contributions;
+        former_delegators_unstake_requests;
+      }
+end
+
+let () =
+  Protocol_plugin.register_delegators_contribution
+    (module Delegators_contribution_plugin)
