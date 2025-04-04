5295884386121cc37a6d7eb6d3b895b83b394133
Lucas Randazzo <lucas@nomadic-labs.com>
2025-04-04 15:09:37 +0200

    Proto/tests: simple ck update test

--- src/proto_alpha/lib_protocol/test/integration/consensus/test_companion_key.ml
+++ src/proto_alpha/lib_protocol/test/integration/consensus/test_companion_key.ml
@@ -30,3 +30,258 @@
                   -- --file test_companion_key.ml
     Subject:    Companion key test: registration, usage, etc
  *)
+
+open Scenario
+
+let check_cks delegate_name : t -> unit tzresult Lwt.t =
+ fun (block, state) ->
+  let current_cycle = current_cycle block in
+  Account_helpers.assert_CKs
+    ~loc:__LOC__
+    (B block)
+    current_cycle
+    delegate_name
+    state.State.account_map
+
+type key_status = Active | Pending | Unregistered
+
+type key_kind = Protocol.Operation_repr.consensus_key_kind =
+  | Consensus
+  | Companion
+
+let check_ck_status ~loc ~ck ~registered_for (status : key_status)
+    (kind : key_kind) : (t, t) scenarios =
+  let open Lwt_result_syntax in
+  exec_unit (fun (block, state) ->
+      let delegate = State.find_account registered_for state in
+      let ck = State.find_account ck state in
+      let* info = Context.Delegate.info (B block) delegate.pkh in
+      let {
+        Delegate_services.active_consensus_key;
+        active_companion_key;
+        pending_consensus_keys;
+        pending_companion_keys;
+        _;
+      } =
+        info
+      in
+      match (status, kind) with
+      | Active, Consensus ->
+          Assert.equal
+            ~loc
+            Signature.Public_key_hash.equal
+            "Active consensus key"
+            Signature.Public_key_hash.pp
+            active_consensus_key
+            ck.pkh
+      | Active, Companion -> (
+          match active_companion_key with
+          | None -> failwith "Companion key is not yet active"
+          | Some active_companion_key ->
+              Assert.equal
+                ~loc
+                Signature.Public_key_hash.equal
+                "Active companion key"
+                Signature.Public_key_hash.pp
+                (Bls active_companion_key)
+                ck.pkh)
+      | Pending, Consensus ->
+          Assert.is_true
+            ~loc
+            (List.exists
+               (fun (_, pending_key) ->
+                 Signature.Public_key_hash.equal pending_key ck.pkh)
+               pending_consensus_keys)
+      | Pending, Companion ->
+          Assert.is_true
+            ~loc
+            (List.exists
+               (fun (_, pending_key) ->
+                 Signature.Public_key_hash.equal (Bls pending_key) ck.pkh)
+               pending_companion_keys)
+      | Unregistered, Consensus ->
+          let* () =
+            Assert.not_equal
+              ~loc
+              Signature.Public_key_hash.equal
+              "Active consensus key"
+              Signature.Public_key_hash.pp
+              active_consensus_key
+              ck.pkh
+          in
+          Assert.is_true
+            ~loc
+            (List.for_all
+               (fun (_, pending_key) ->
+                 not (Signature.Public_key_hash.equal pending_key ck.pkh))
+               pending_consensus_keys)
+      | Unregistered, Companion ->
+          let* () =
+            match active_companion_key with
+            | None -> return_unit
+            | Some ack ->
+                Assert.not_equal
+                  ~loc
+                  Signature.Public_key_hash.equal
+                  "Active companion key"
+                  Signature.Public_key_hash.pp
+                  (Bls ack)
+                  ck.pkh
+          in
+          Assert.is_true
+            ~loc
+            (List.for_all
+               (fun (_, pending_key) ->
+                 not (Signature.Public_key_hash.equal (Bls pending_key) ck.pkh))
+               pending_companion_keys))
+
+let check_no_active_companion ~loc ~delegate : (t, t) scenarios =
+  let open Lwt_result_syntax in
+  exec_unit (fun (block, state) ->
+      let delegate = State.find_account delegate state in
+      let* info = Context.Delegate.info (B block) delegate.pkh in
+      Assert.is_none
+        ~loc
+        ~pp:Signature.Bls.Public_key_hash.pp
+        info.active_companion_key)
+
+let update_consensus_key_any_algo_with_fresh_key ~ck_name src =
+  fold_tag
+    (fun algo ->
+      add_account ~algo ck_name --> update_consensus_key ~ck_name src)
+    [
+      ("Ed25519", Ed25519);
+      ("Secp256k1", Secp256k1);
+      ("P256", P256);
+      ("BLS", Bls);
+    ]
+
+let update_companion_key_with_fresh_key ~ck_name src =
+  add_account ~algo:Bls ck_name --> update_companion_key ~ck_name src
+
+let test_simple_register_consensus_and_companion_keys =
+  let bootstrap_accounts = ["bootstrap1"; "bootstrap2"] in
+  let delegate = "delegate" in
+  let new_ck = "ck" in
+  let consensus_rights_delay =
+    Default_parameters.constants_mainnet.consensus_rights_delay
+  in
+  let check_finalized_block = check_cks delegate in
+  init_constants ()
+  --> set S.allow_tz4_delegate_enable true
+  --> set S.consensus_rights_delay consensus_rights_delay
+  --> (Tag "is bootstrap"
+       --> begin_test
+             ~force_attest_all:true
+             ~check_finalized_block
+             (delegate :: bootstrap_accounts)
+      |+ Tag "is created"
+         --> begin_test
+               ~force_attest_all:true
+               ~check_finalized_block
+               ~algo:Bls
+               bootstrap_accounts
+         --> add_account_with_funds
+               delegate
+               ~funder:(Stdlib.List.hd bootstrap_accounts)
+               (Amount (Tez_helpers.of_mutez 2_000_000_000_000L))
+         --> set_delegate delegate (Some delegate)
+         --> (Tag "active" --> stake delegate Half
+              --> wait_n_cycles consensus_rights_delay
+             |+ Tag "inactive" --> noop))
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:delegate
+        ~registered_for:delegate
+        Active
+        Consensus
+  --> (Tag "update consensus key"
+       --> update_consensus_key_any_algo_with_fresh_key ~ck_name:new_ck delegate
+       (* It is not a companion key *)
+       --> check_ck_status
+             ~loc:__LOC__
+             ~ck:new_ck
+             ~registered_for:delegate
+             Unregistered
+             Companion
+       --> loop
+             (consensus_rights_delay + 1)
+             (check_ck_status
+                ~loc:__LOC__
+                ~ck:delegate
+                ~registered_for:delegate
+                Active
+                Consensus
+             --> check_ck_status
+                   ~loc:__LOC__
+                   ~ck:new_ck
+                   ~registered_for:delegate
+                   Pending
+                   Consensus
+             --> next_cycle)
+       (* After consensus_rights_delay + 1 cycles, new key becomes active, and old key
+          becomes unregistered *)
+       --> check_ck_status
+             ~loc:__LOC__
+             ~ck:delegate
+             ~registered_for:delegate
+             Unregistered
+             Consensus
+       --> check_ck_status
+             ~loc:__LOC__
+             ~ck:new_ck
+             ~registered_for:delegate
+             Active
+             Consensus
+      |+ Tag "update companion key"
+         --> update_companion_key_with_fresh_key ~ck_name:new_ck delegate
+             (* It is not a consensus key *)
+         --> check_ck_status
+               ~loc:__LOC__
+               ~ck:new_ck
+               ~registered_for:delegate
+               Unregistered
+               Consensus
+         --> loop
+               (consensus_rights_delay + 1)
+               (check_ck_status
+                  ~loc:__LOC__
+                  ~ck:delegate
+                  ~registered_for:delegate
+                  Active
+                  Consensus
+               --> check_ck_status
+                     ~loc:__LOC__
+                     ~ck:new_ck
+                     ~registered_for:delegate
+                     Pending
+                     Companion
+               --> check_no_active_companion ~loc:__LOC__ ~delegate
+               --> next_cycle)
+         (* After consensus_rights_delay + 1 cycles, new key becomes active *)
+         --> check_ck_status
+               ~loc:__LOC__
+               ~ck:delegate
+               ~registered_for:delegate
+               Active
+               Consensus
+         --> check_ck_status
+               ~loc:__LOC__
+               ~ck:new_ck
+               ~registered_for:delegate
+               Active
+               Companion)
+
+let tests =
+  tests_of_scenarios
+  @@ [
+       ( "Simple update ck for delegate",
+         test_simple_register_consensus_and_companion_keys );
+     ]
+
+let () =
+  register_tests
+    ~__FILE__
+    ~tags:
+      ["protocol"; "scenario"; "consensus"; "bls"; "attestation"; "companion"]
+    tests
