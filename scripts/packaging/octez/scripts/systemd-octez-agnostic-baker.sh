59b8f726429db0cf46ea29652c826f4a4db653d6
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2025-04-07 14:12:40 +0200

    Packages/Debian: agnostic baker package use agnostic baker

--- scripts/packaging/octez/scripts/systemd-octez-agnostic-baker.sh
+++ scripts/packaging/octez/scripts/systemd-octez-agnostic-baker.sh
@@ -9,9 +9,9 @@ fi
 
 # Loop through each file matching the pattern
 # NB: we avoid selecting `-alpha` or `-next` bakers
-for file in /usr/bin/octez-baker-P*; do
+for file in /usr/bin/octez-accuser-P*; do
   file=$(basename "$file")
-  proto=$(echo "$file" | sed 's/^octez-baker-//')
+  proto=$(echo "$file" | sed 's/^octez-accuser-//')
 
   # Enable or start/stop the systemd service based on the argument
   if [ "$1" = "start" ]; then
@@ -19,16 +19,17 @@ for file in /usr/bin/octez-baker-P*; do
       systemctl enable octez-dal-node
       systemctl start octez-dal-node
     fi
-    systemctl enable "octez-baker@$proto"
+    systemctl enable "octez-agnostic-baker-bin"
+    systemctl start "octez-agnostic-baker-bin"
     systemctl enable "octez-accuser@$proto"
-    systemctl start "octez-baker@$proto"
     systemctl start "octez-accuser@$proto"
 
   elif [ "$1" = "stop" ]; then
-    systemctl stop "octez-baker@$proto"
     systemctl stop "octez-accuser@$proto"
     if grep -q "\-\-dal-node" /etc/default/octez-baker; then
       systemctl stop octez-dal-node
     fi
   fi
 done
+
+echo "systemd-octez-agnostic-baker.sh done"
