a20f28a95a6595b13a7c5f7704fb914884bd8423
Luciano Freitas <luciano.freitas@lambda-coins.com>
2025-04-08 14:57:18 +0200

    Tezlink: implement constants RPC

--- etherlink/bin_node/lib_dev/tezlink_services_impl.ml
+++ etherlink/bin_node/lib_dev/tezlink_services_impl.ml
@@ -50,6 +50,59 @@ let current_level (module Backend : Services_backend_sig.S) chain block
       cycle_position = Int32.rem level constants.blocks_per_cycle;
     }
 
+let _constants chain block =
+  let open Lwt_result_syntax in
+  (* TODO: #7831
+     take chain into account
+     For the moment this implementation only supports the main chain, once
+     the rpc support of tezlink is more stable, we can add support for other chains *)
+  let* () =
+    match chain with `Main -> return_unit | _ -> failwith "Unsupported chain"
+  in
+
+  (* TODO: #7831
+     take block into account
+     For the moment this implementation only supports the head block, once
+     the rpc support of tezlink is more stable, we can add support for other blocks *)
+  let* () =
+    match block with
+    | `Head _ -> return_unit
+    | _ -> failwith "Unsupported block"
+  in
+
+  let fixed_values =
+    Tezlink_constants.
+      ( ( proof_of_work_nonce_size,
+          nonce_length,
+          max_anon_ops_per_block,
+          max_operation_data_length,
+          max_proposals_per_delegate,
+          max_micheline_node_count,
+          max_micheline_bytes_limit,
+          max_allowed_global_constant_depth,
+          cache_layout_size,
+          michelson_maximum_type_size ),
+        ( max_slashing_period,
+          sc_max_wrapped_proof_binary_size,
+          sc_rollup_message_size_limit,
+          sc_rollup_max_number_of_messages_per_level ) )
+  in
+
+  let* fixed =
+    match
+      Tezos_services.Protocol_types.Constants.values_to_fixed fixed_values
+    with
+    | Ok fixed -> return fixed
+    | Error err ->
+        failwith
+          "Failed to get fixed constants: %a"
+          Error_monad.pp_print_trace
+          err
+  in
+  return
+    Tezos_services.Protocol_types.Constants.
+      {fixed; parametric = Tezlink_constants.mainnet}
+
 let michelson_services_methods backend =
   {
     current_level = current_level backend;
