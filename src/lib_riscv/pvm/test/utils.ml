f932525975688927ed1e0a0d96bb3390da9791fe
Victor Dumitrescu <v.dumitrescu@outlook.com>
2025-04-09 10:55:52 +0200

    RISC-V: Introduce basic tests for the OCaml API

---
+++ src/lib_riscv/pvm/test/utils.ml
@@ -0,0 +1,20 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2025 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(* Get a string pointing to the path of a RISC-V kernel which the RISC-V PVM
+ * can interpret as an initial boot sector. The PVM will then load the kernel
+ * directly. This is to bypass origination of large kernels, which is not
+ * currently supported (RV-109) *)
+let read_riscv_kernel (kernel_path : string) (checksum_path : string) =
+  match String.split_on_char ' ' (read_file checksum_path) with
+  | checksum :: _ -> Lwt.return ("kernel:" ^ kernel_path ^ ":" ^ checksum)
+  | _ -> Lwt.fail_with "Kernel found"
+
+let read_riscv_dummy_kernel () =
+  read_riscv_kernel
+    (project_root // "src/riscv/assets/riscv-dummy.elf")
+    (project_root // "src/riscv/assets/riscv-dummy.elf.checksum")
