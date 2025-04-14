5b8c74b3944fb966dbbec0930426296f92040c95
Lucas Randazzo <lucas@nomadic-labs.com>
2025-04-14 18:19:45 +0200

    Proto/tests: test steal consensus keys

--- src/proto_alpha/lib_protocol/test/integration/consensus/test_companion_key.ml
+++ src/proto_alpha/lib_protocol/test/integration/consensus/test_companion_key.ml
@@ -43,6 +43,25 @@ let check_cks delegate_name : t -> unit tzresult Lwt.t =
     delegate_name
     state.State.account_map
 
+let check_all_cks : t -> unit tzresult Lwt.t =
+ fun (block, state) ->
+  let all_delegates =
+    String.Map.filter
+      (fun name _ -> State.is_self_delegate name state)
+      state.State.account_map
+    |> String.Map.bindings |> List.map fst
+  in
+  let current_cycle = current_cycle block in
+  List.iter_es
+    (fun delegate_name ->
+      Account_helpers.assert_CKs
+        ~loc:__LOC__
+        (B block)
+        current_cycle
+        delegate_name
+        state.State.account_map)
+    all_delegates
+
 type key_status = Active | Pending | Unregistered
 
 type key_kind = Protocol.Operation_repr.consensus_key_kind =
@@ -272,11 +291,140 @@ let test_simple_register_consensus_and_companion_keys =
                Active
                Companion)
 
+let test_register_other_accounts_as_ck =
+  let consensus_rights_delay =
+    Default_parameters.constants_mainnet.consensus_rights_delay
+  in
+  init_constants ()
+  --> set S.allow_tz4_delegate_enable true
+  --> set S.consensus_rights_delay consensus_rights_delay
+  --> begin_test
+        ~algo:Bls
+        ~check_finalized_block:check_all_cks
+        ~force_attest_all:true
+        ["delegate"; "victim_1"; "victim_2"]
+  (* Both victims start with themselves as their own consensus_keys *)
+  --> assert_failure
+        ~loc:__LOC__
+        ~expected_error:(fun _ err ->
+          Error_helpers.check_error_constructor_name
+            ~loc:__LOC__
+            ~expected:
+              Protocol.Delegate_consensus_key
+              .Invalid_consensus_key_update_active
+            err)
+        (update_consensus_key ~ck_name:"victim_1" "delegate")
+  --> assert_failure
+        ~loc:__LOC__
+        ~expected_error:(fun _ err ->
+          Error_helpers.check_error_constructor_name
+            ~loc:__LOC__
+            ~expected:
+              Protocol.Delegate_consensus_key
+              .Invalid_consensus_key_update_active
+            err)
+        (update_companion_key ~ck_name:"victim_2" "delegate")
+  (* ... So we give them other consensus keys.
+     Note that the algo is not defined, so it is chosen at random. *)
+  --> add_account "consensus_key_1"
+  --> update_consensus_key ~ck_name:"consensus_key_1" "victim_1"
+  --> add_account "consensus_key_2"
+  --> update_consensus_key ~ck_name:"consensus_key_2" "victim_2"
+  (* We do not need to wait to steal the newly available keys *)
+  --> update_consensus_key ~ck_name:"victim_1" "delegate"
+  --> update_companion_key ~ck_name:"victim_2" "delegate"
+  (* Check that everything is as expected:
+     - both victims have themselves as active consensus keys,
+       and have a pending companion key each
+     - The delegate has two keys pending
+     - After the consensus rights delay, all pendings become active,
+       and the victims do not use their own keys as consensus keys. *)
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"victim_1"
+        ~registered_for:"victim_1"
+        Active
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"victim_2"
+        ~registered_for:"victim_2"
+        Active
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"consensus_key_1"
+        ~registered_for:"victim_1"
+        Pending
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"consensus_key_2"
+        ~registered_for:"victim_2"
+        Pending
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"victim_1"
+        ~registered_for:"delegate"
+        Pending
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"victim_2"
+        ~registered_for:"delegate"
+        Pending
+        Companion
+  (* Activation *)
+  --> wait_n_cycles (consensus_rights_delay + 1)
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"victim_1"
+        ~registered_for:"victim_1"
+        Unregistered
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"victim_2"
+        ~registered_for:"victim_2"
+        Unregistered
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"consensus_key_1"
+        ~registered_for:"victim_1"
+        Active
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"consensus_key_2"
+        ~registered_for:"victim_2"
+        Active
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"victim_1"
+        ~registered_for:"delegate"
+        Active
+        Consensus
+  --> check_ck_status
+        ~loc:__LOC__
+        ~ck:"victim_2"
+        ~registered_for:"delegate"
+        Active
+        Companion
+  (* Bake a cycle, for fun. Note that for each block, the RPCs also check for the complete
+     status of registration of consensus and companion keys, and all delegates attest
+     with their active consensus key. In this new cycle, the newly activated keys are thus
+     used for the first time. *)
+  --> next_cycle
+
 let tests =
   tests_of_scenarios
   @@ [
        ( "Simple update ck for delegate",
          test_simple_register_consensus_and_companion_keys );
+       ("Register other accounts as ck", test_register_other_accounts_as_ck);
      ]
 
 let () =
