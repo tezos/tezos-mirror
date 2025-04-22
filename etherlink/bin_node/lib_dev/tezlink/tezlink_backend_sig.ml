0e7dcbbb17a75178634eacc9f2b9053a8ef27246
Dibassi Brahima <brahima.dibassi@lambda-coins.com>
2025-04-22 16:38:50 +0200

    Tezlink/Node/RPC : Refactor manager_key RPC

--- etherlink/bin_node/lib_dev/tezlink/tezlink_backend_sig.ml
+++ etherlink/bin_node/lib_dev/tezlink/tezlink_backend_sig.ml
@@ -5,8 +5,6 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-open Tezos_types
-
 module type S = sig
   val current_level :
     [> `Main] ->
@@ -18,13 +16,17 @@ module type S = sig
     [> `Main] -> [> `Head of 'a] -> Tezlink_constants.t tzresult Lwt.t
 
   val balance :
-    [> `Main] -> [> `Head of 'a] -> Contract.t -> Tez.t tzresult Lwt.t
+    [> `Main] ->
+    [> `Head of 'a] ->
+    Tezos_types.Contract.t ->
+    Tezos_types.Tez.t tzresult Lwt.t
 
   val manager_key :
     [> `Main] ->
     [> `Head of 'a] ->
-    Contract.t ->
-    Signature.public_key option tzresult Lwt.t
+    Tezos_types.Contract.t ->
+    Signature.V1.Public_key.t option tzresult Lwt.t
 
-  val counter : [> `Main] -> [> `Head of 'a] -> Contract.t -> Z.t tzresult Lwt.t
+  val counter :
+    [> `Main] -> [> `Head of 'a] -> Tezos_types.Contract.t -> Z.t tzresult Lwt.t
 end
