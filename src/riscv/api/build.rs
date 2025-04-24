bc445ce54362766f06a12e54f426a85f1e5fb77b
Ole Krüger <ole.kruger@trili.tech>
2025-04-24 12:38:21 +0100

    RISC-V: Relax mutual exclusivity of `ci` and `default`

--- src/riscv/api/build.rs
+++ src/riscv/api/build.rs
@@ -7,10 +7,6 @@ fn generate_ocaml_sigs() {
 }
 
 pub fn main() {
-    if env::var("CARGO_FEATURE_DEFAULT").is_ok() && env::var("CARGO_FEATURE_CI").is_ok() {
-        panic!("Feature 'default' and 'ci' are mutually exclusive");
-    }
-
     if env::var("INSIDE_DUNE").is_err() {
         // Generate the Ocaml signatures only when building outside the Dune rule.
         generate_ocaml_sigs();
