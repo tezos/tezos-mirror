c76cf1ab54835d490f9176aa25acf1e30bebb470
arnaud <arnaud.bihan@functori.com>
2025-05-20 17:48:48 +0200

    L2/Node: Introduce tezlink_durable_storage module

--- etherlink/bin_node/lib_dev/tezlink_services_impl.ml
+++ etherlink/bin_node/lib_dev/tezlink_services_impl.ml
@@ -101,35 +101,19 @@ module Make (Backend : Backend) : Tezlink_backend_sig.S = struct
 
   let balance chain block c =
     let `Main = chain in
-
-    Durable_storage.inspect_durable_and_decode_default
-      ~default:Tezos_types.Tez.zero
-      (read ~block)
-      (Path.balance c)
-      (Data_encoding.Binary.of_bytes_exn Tez.encoding)
+    Tezlink_durable_storage.balance (read ~block) c
 
   let manager_key chain block c =
     (* TODO: #7831 !17664
        Support non-default chain and block parameters. *)
     let `Main = chain in
-
-    Durable_storage.inspect_durable_and_decode_opt
-      (read ~block)
-      (Path.manager_key c)
-      (Data_encoding.Binary.of_bytes_exn Signature.V1.Public_key.encoding)
+    Tezlink_durable_storage.manager_key (read ~block) c
 
   let counter chain block c =
     (* TODO: #7831 !17664
        Support non-default chain and block parameters. *)
     let `Main = chain in
-
-    Durable_storage.inspect_durable_and_decode_default
-    (* FIXME: #7960
-       This default should be the global counter *)
-      ~default:Z.one
-      (read ~block)
-      (Path.counter c)
-      (Data_encoding.Binary.of_bytes_exn Data_encoding.z)
+    Tezlink_durable_storage.counter (read ~block) c
 
   let header chain block =
     let open Lwt_result_syntax in
