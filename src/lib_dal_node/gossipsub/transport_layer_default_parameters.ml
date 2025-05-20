4ed2360c87316478afba6080e1bb789dffa4a1c5
Eugen Zalinescu <eugen.zalinescu@nomadic-labs.com>
2025-05-20 15:58:09 +0200

    DAL/Node: remove reference to issue #6391

--- src/lib_dal_node/gossipsub/transport_layer_default_parameters.ml
+++ src/lib_dal_node/gossipsub/transport_layer_default_parameters.ml
@@ -32,12 +32,8 @@ module P2p_limits = struct
 
   let greylist_timeout = Time.System.Span.of_seconds_exn 86400. (* one day *)
 
-  (* Some of the default values below taken from L1 node instantiation of
-     Octez-p2p library. We amplify the value by this constant. *)
-  (* FIXME: https://gitlab.com/tezos/tezos/-/issues/6391
-
-     Decide which default value we should choose for P2P parameters and wheter we
-     specialize them per profile kind. *)
+  (* Some of the values taken from the L1 node's instantiation of the
+     octez-p2p library are amplified by the following constant. *)
   let dal_amplification_factor = 8
 
   (* [maintenance_idle_time] is set to [None] to disable the internal maintenance
