63080a12cd2fb02576ca50e0aae7ea7711860f99
Victor Allombert <victor.allombert@nomadic-labs.com>
2025-05-20 10:50:22 +0200

    Lib_bees: task worker now uses a shared pool of domains
    Co-authored-by: Mathias Bourgoin <mathias.bourgoin@nomadic-labs.com>
    Co-authored-by: Julien SAGOT <julien.sagot@lambda-coins.com>

--- src/lib_bees/task_worker.mli
+++ src/lib_bees/task_worker.mli
@@ -6,60 +6,43 @@
 (*****************************************************************************)
 
 (** This module provides an easy-to-use generic worker in order to run
-    computations in parallel.
+    computations in parallel. It uses a dedicated pool of domains that is shared
+    amongst all components of the same process.
+    Important: the task worker inherits the main event loop switch that is
+    available when it is created. If this switch is terminated, it will result
+    in an inoperative task worker.
  *)
-
-type task_worker
-
-type launch_error
 
 type 'a message_error =
   | Closed of error list option
   | Request_error of 'a
   | Any of exn
 
-(** [worker name domains] creates a worker to be used with {!val-launch_task}
-    and {!val-launch_tasks} functions.
-
-    Typically, one worker should be initialised at program launch, and reused
-    during the whole program lifetime for parallelised computation. *)
-val worker : string -> int -> (task_worker, launch_error) result
-
-(** [launch_task_and_wait worker name func ?on_completion arg] create a request named
-    [name] and executing [func args], and push it to [worker] queue, returning a
+(** [launch_task_and_wait name func ?on_completion arg] create a request named
+    [name] and executing [func args], and push it to a [worker] queue. [worker]
+    is created at toplevel and shared amongst all the components. Returns a
     promise for the request result. An optional callback [on_completion] can be
     used to trigger specific computations once the request is completed. *)
 val launch_task_and_wait :
-  task_worker ->
   string ->
   ('a -> 'b) ->
   ?on_completion:('b -> unit) ->
   'a ->
   ('b, 'c message_error) result Eio.Promise.t
 
-(** [launch_tasks_and_wait worker name func ?on_completion args] runs
+(** [launch_tasks_and_wait name func ?on_completion args] runs
     {!val-launch_task_and_wait} for each each [arg] in [args], in parallel. *)
 val launch_tasks_and_wait :
-  task_worker ->
   string ->
   ('a -> 'b) ->
   ?on_completion:('b -> unit) ->
   'a list ->
   ('b, 'c message_error) result list
 
-(** [launch_task worker name func ?on_completion arg] create a request named
-    [name] and executing [func args], and push it to [worker] queue, true if the
-    request has been pushed successfully and false otherwise. An optional
-    callback [on_completion] can be used to trigger specific computations once
-    the request is completed *)
+(** [launch_task name func ?on_completion arg] create a request named [name] and
+    executing [func args], and push it to [worker] queue, true if the request
+    has been pushed successfully and false otherwise. An optional callback
+    [on_completion] can be used to trigger specific computations once the
+    request is completed *)
 val launch_task :
-  task_worker ->
-  string ->
-  ('a -> 'b) ->
-  ?on_completion:('b -> unit) ->
-  'a ->
-  bool
-
-(** [shutdown worker] waits for all requests to be completed 
-    then closes the worker. *)
-val shutdown : task_worker -> unit
+  string -> ('a -> 'b) -> ?on_completion:('b -> unit) -> 'a -> bool
--- src/lib_bees/test/test_bees_task_worker.ml
+++ src/lib_bees/test/test_bees_task_worker.ml
@@ -26,18 +26,15 @@ let tztest label fn =
 let tests_fibonacci =
   let test_fibonacci domains =
     let fib () =
-      match Tezos_bees.Task_worker.worker "worker" domains with
-      | Error e -> Lwt_result_syntax.tzfail e
-      | Ok worker ->
-          let rec fib n = if n <= 1 then n else fib (n - 1) + fib (n - 2) in
-          let input = Stdlib.List.init 5 (fun i -> i + 10) in
-          let expected = List.map fib input in
-          let output =
-            Tezos_bees.Task_worker.launch_tasks_and_wait worker "fib" fib input
-          in
-          let output = List.filter_map Result.to_option output in
-          Assert.equal_list expected output ;
-          Lwt.return_ok ()
+      let rec fib n = if n <= 1 then n else fib (n - 1) + fib (n - 2) in
+      let input = Stdlib.List.init 5 (fun i -> i + 10) in
+      let expected = List.map fib input in
+      let output =
+        Tezos_bees.Task_worker.launch_tasks_and_wait "fib" fib input
+      in
+      let output = List.filter_map Result.to_option output in
+      Assert.equal_list expected output ;
+      Lwt.return_ok ()
     in
     let name = Printf.sprintf "Fibonacci[11;12;13;14;15] %d domains" domains in
     tztest name fib
@@ -48,61 +45,43 @@ let tests_reuse =
   (* Check that the same task worker can be used with totally different tasks and types. *)
   let test =
     tztest "reuse" @@ fun () ->
-    match Tezos_bees.Task_worker.worker "worker" 1 with
-    | Error e -> Lwt_result_syntax.tzfail e
-    | Ok worker ->
-        let int_input = 0 in
-        let int_expected = succ int_input in
-        let int_output =
-          Tezos_bees.Task_worker.launch_task_and_wait
-            worker
-            "succ(int)"
-            succ
-            int_input
-        in
-        let str_input = "0" in
-        let succ s = int_of_string s |> succ |> string_of_int in
-        let str_expected = succ str_input in
-        let str_output =
-          Tezos_bees.Task_worker.launch_task_and_wait
-            worker
-            "succ(str)"
-            succ
-            str_input
-        in
-        let int_output = Eio.Promise.await int_output in
-        let str_output = Eio.Promise.await str_output in
-        Assert.equal (Ok int_expected) int_output ;
-        Assert.equal (Ok str_expected) str_output ;
-        Lwt.return_ok ()
+    let int_input = 0 in
+    let int_expected = succ int_input in
+    let int_output =
+      Tezos_bees.Task_worker.launch_task_and_wait "succ(int)" succ int_input
+    in
+    let str_input = "0" in
+    let succ s = int_of_string s |> succ |> string_of_int in
+    let str_expected = succ str_input in
+    let str_output =
+      Tezos_bees.Task_worker.launch_task_and_wait "succ(str)" succ str_input
+    in
+    let int_output = Eio.Promise.await int_output in
+    let str_output = Eio.Promise.await str_output in
+    Assert.equal (Ok int_expected) int_output ;
+    Assert.equal (Ok str_expected) str_output ;
+    Lwt.return_ok ()
   in
   ("Reuse a worker for a different kind of computation", [test])
 
 let tests_on_completion_callback =
   let test =
     tztest "on_completion_handler" @@ fun () ->
-    match Tezos_bees.Task_worker.worker "worker" 1 with
-    | Error e -> Lwt_result_syntax.tzfail e
-    | Ok worker ->
-        let r = ref 0 in
-        let noop () = () in
-        let _ =
-          Tezos_bees.Task_worker.launch_task_and_wait worker "callback" noop ()
-          |> Eio.Promise.await
-        in
-        Assert.equal !r 0 ;
-        let on_completion () = incr r in
-        let tasks = 2 in
-        let _ =
-          Tezos_bees.Task_worker.launch_tasks_and_wait
-            ~on_completion
-            worker
-            "callback"
-            noop
-            (Stdlib.List.init tasks (fun _ -> ()))
-        in
-        Assert.equal !r tasks ;
-        Lwt.return_ok ()
+    let r = ref 0 in
+    let noop () = () in
+    let _ = Tezos_bees.Task_worker.launch_task_and_wait "callback" noop () in
+    Assert.equal !r 0 ;
+    let on_completion () = incr r in
+    let tasks = 2 in
+    let _ =
+      Tezos_bees.Task_worker.launch_tasks_and_wait
+        ~on_completion
+        "callback"
+        noop
+        (Stdlib.List.init tasks (fun _ -> ()))
+    in
+    Assert.equal !r tasks ;
+    Lwt.return_ok ()
   in
   ("Run on_completion callback", [test])
 
