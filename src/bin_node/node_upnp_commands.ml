b66583223fe5d5029f686576e1f81cb83a885a6f
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2025-05-22 12:15:10 +0200

    Node/UPNP: add parameters to search-gateway command
    While the command should disappear, the parameters will remain anyway.

--- src/bin_node/node_upnp_commands.ml
+++ src/bin_node/node_upnp_commands.ml
@@ -5,13 +5,21 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let search_gateway () =
+type options = {
+  bind_addr : string option;
+  broadcast_addr : string option;
+  timeout : float option;
+  single_search_timeout : float option;
+}
+
+let search_gateway {bind_addr; broadcast_addr; timeout; single_search_timeout}
+    () =
   match
     Octez_igd_next.Igd_next_gen.search_gateway
-      ~bind_addr:None
-      ~broadcast_address:None
-      ~timeout:None
-      ~single_search_timeout:None
+      ~bind_addr
+      ~broadcast_address:broadcast_addr
+      ~timeout
+      ~single_search_timeout
   with
   | Ok _ ->
       Format.printf "Gateway found\n%!" ;
@@ -20,27 +28,78 @@ let search_gateway () =
       Format.eprintf "Cannot find gateway: %s\n%!" e ;
       Lwt.return_unit
 
-let search_gateway () = Tezos_base_unix.Event_loop.main_run search_gateway
+let search_gateway options () =
+  Tezos_base_unix.Event_loop.main_run (search_gateway options)
 
 module Term = struct
-  let process _ = `Ok (search_gateway ())
+  open Cmdliner
 
   let docs = "UPNP OPTIONS"
 
-  let search_gateway =
-    let doc = "" in
-    Cmdliner.Arg.(value & flag & info ~docs ~doc ["search-gateway"])
+  let bind_addr =
+    let doc = "The URL at which this instance can be reached." in
+    Arg.(
+      value
+      & opt (some string) None
+      & info ~docs ~doc ~docv:"ADDR:PORT" ["listen-addr"])
 
-  let term = Cmdliner.Term.(ret (const process $ search_gateway))
+  let broadcast_addr =
+    let doc =
+      "The address used for broadcasting the UPNP discovery requests."
+    in
+    Arg.(
+      value
+      & opt (some string) None
+      & info ~docs ~doc ~docv:"ADDR:PORT" ["broadcast-addr"])
+
+  let discovery_timeout =
+    let doc = "Timeout until UPNP discovery stops looking for a gateway." in
+    Arg.(
+      value
+      & opt (some float) None
+      & info ~docs ~doc ~docv:"SEC" ["discovery-timeout"])
+
+  let single_search_timeout =
+    let doc =
+      "Timeout until the node stops waiting for the first discovery response."
+    in
+    Arg.(
+      value
+      & opt (some float) None
+      & info ~docs ~doc ~docv:"SEC" ["single-search-timeout"])
+
+  let options =
+    let open Term.Syntax in
+    let+ bind_addr
+    and+ broadcast_addr
+    and+ discovery_timeout
+    and+ single_search_timeout in
+    {
+      bind_addr;
+      broadcast_addr;
+      timeout = discovery_timeout;
+      single_search_timeout;
+    }
+
+  let process options = `Ok (search_gateway options ())
+
+  let term = Term.(ret (const process $ options))
 end
 
 module Manpage = struct
-  let command_description = "Experimental"
+  let command_description =
+    "Port mapping through UPNP/IGD. The gateway must support UPNP and accept \
+     port mapping requests, and the local firewall should accept messages from \
+     it on 1900/UDP, otherwise gateway discovery will fail. Default values \
+     should suffice in general setup."
 
   let description = [`S "DESCRIPTION"; `P command_description]
 
   let man = description
 
+  (* Note that this command is temporary as a proof of concept, and will evolve
+     into a full-fledge port mapping command. As such, the documentation is
+     minimal for now as it doesn't make sense. *)
   let info = Cmdliner.Cmd.info ~doc:"Experimental" ~man "search-gateway"
 end
 
