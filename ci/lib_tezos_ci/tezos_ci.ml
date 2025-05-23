5bde9a0fecfa6d1413c2631ddcc66b79078b0c78
Bruno Bernardo <bruno@nomadic-labs.com>
2025-05-23 14:46:04 +0200

    CI: merge [publish_release_gitlab] stage into [publish]
    We need only one stage for jobs that publish assets.

--- ci/lib_tezos_ci/tezos_ci.ml
+++ ci/lib_tezos_ci/tezos_ci.ml
@@ -1299,8 +1299,6 @@ module Stages = struct
 
   let prepare_release = Stage.register "prepare_release"
 
-  let publish_release_gitlab = Stage.register "publish_release_gitlab"
-
   let publish_release = Stage.register "publish_release"
 
   let publish_package_gitlab = Stage.register "publish_package_gitlab"
