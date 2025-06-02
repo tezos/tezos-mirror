2acffa290330d294db863a55d5775f2d30e9a880
Marina Polubelova <marina.polubelova@nomadic-labs.com>
2025-06-02 15:40:39 +0200

    Tezt: update tests for threshold bls signatures

--- tezt/lib_tezos/RPC.mli
+++ tezt/lib_tezos/RPC.mli
@@ -822,7 +822,8 @@ val post_bls_aggregate_public_keys :
   (string * string) list -> (string * string) t
 
 (** RPC: [POST /bls/threshold_signatures] *)
-val post_bls_threshold_signatures : (int * string) list -> string t
+val post_bls_threshold_signatures :
+  pk:string -> msg:string -> (int * string) list -> string t
 
 (** {2 Big maps RPC module} *)
 
