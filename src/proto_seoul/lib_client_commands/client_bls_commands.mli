df356ee6fbd23dde70b43b38486b2baa00c5fe7e
mbourgoin <mathias.bourgoin@nomadic-labs.com>
2025-06-06 09:45:49 +0200

    S023/src: remove proto_seoul

--- src/proto_seoul/lib_client_commands/client_bls_commands.mli
+++
@@ -1,8 +0,0 @@
-(*****************************************************************************)
-(*                                                                           *)
-(* SPDX-License-Identifier: MIT                                              *)
-(* SPDX-FileCopyrightText: 2025 Nomadic Labs <contact@nomadic-labs.com>      *)
-(*                                                                           *)
-(*****************************************************************************)
-
-val commands : unit -> Protocol_client_context.full Tezos_clic.command list
