af9c2124085191721d70f88c4a647a756deeafed
mbourgoin <mathias.bourgoin@nomadic-labs.com>
2025-06-06 09:41:36 +0200

    Seoul/devtools: update testnet_experiment_tools

--- devtools/testnet_experiment_tools/testnet_experiment_tools.ml
+++ devtools/testnet_experiment_tools/testnet_experiment_tools.ml
@@ -89,6 +89,10 @@ let network_parameters_templates_dir =
   Filename.current_dir_name // "devtools" // "testnet_experiment_tools"
   // "activation_parameters_templates"
 
+let protocol_seoul_parameters_template =
+  Filename.current_dir_name // "src" // "proto_seoul" // "parameters"
+  // "mainnet_parameters.json"
+
 let protocol_alpha_parameters_template =
   Filename.current_dir_name // "src" // "proto_alpha" // "parameters"
   // "mainnet_parameters.json"
@@ -100,6 +104,7 @@ let network_activation_parameters_templates protocol_hash =
         (Filename.concat
            network_parameters_templates_dir
            "proto_022_PsRiotum_mainnet.json")
+  | Tezt_tezos.Protocol.Seoul -> Some protocol_seoul_parameters_template
   | Tezt_tezos.Protocol.Alpha ->
       (* Fetching the network parameters from the src/proto_alpha directory,
          to be sure that we are in synch with current protocl parameters. *)
