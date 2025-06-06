ebd17fed05259913e406d227771f99d3e56f6786
Bruno Bernardo <bruno@nomadic-labs.com>
2025-06-06 10:03:47 +0200

    CI: add [container_scanning_merge_reports] jobs in [security_scans]
    This job merges the scanning reports from previous jobs using [jq].
    
    The job runs on the [CI.monitoring] image because this image contains [datadog-ci] and [jq].
    This implies that the [oc.docker:ci:amd64] is added to the pipeline.

--- ci/bin/security_scans.ml
+++ ci/bin/security_scans.ml
@@ -41,5 +41,52 @@ let jobs : tezos_job list =
       [". ./scripts/ci/container_scanning_generate_reports.sh"]
   in
 
-  Common.job_datadog_pipeline_trace
-  :: List.map job_container_scanning image_tags
+  let job_list_container_scanning =
+    List.map job_container_scanning image_tags
+  in
+
+  (* Merges reports from individual image scans into a single report
+     compatible with Gitlab's vulnerability report.
+     https://gitlab.com/tezos/tezos/-/security/vulnerability_report *)
+  let job_container_scanning_merge_reports : tezos_job =
+    job
+      ~__POS__
+      ~name:"container_scanning_merge_reports"
+      ~description:
+        "Merge container scanning reports in a single one fitted for Gitlab \
+         Vulnerability report"
+        (* needs the reports produced in previous [container_scanning] jobs *)
+      ~dependencies:
+        (Dependent
+           (List.map (fun job -> Artifacts job) job_list_container_scanning))
+      ~stage:Stages.test
+      ~image:Images.CI.monitoring
+      ~artifacts:
+        (artifacts
+           ~reports:
+             (reports
+                ~container_scanning:"gl-container-scanning-report.json"
+                ())
+           [])
+      ((* Merge of all container scanning reports: the vulnerability
+          arrays are merged into a single one. *)
+       let jq_slurp_filter =
+         "'{ version: .[0].version, scan: .[0].scan, vulnerabilities: \
+          map(.vulnerabilities[]), remediations: map(.remediations[])}'"
+       in
+       (* String of all individual image scan reports *)
+       let input_scan_reports =
+         let concat_reports tag report_name : string =
+           Format.sprintf " gl-container-scanning-report-"
+           ^ tag ^ ".json" ^ report_name
+         in
+         List.fold_right concat_reports image_tags " "
+       in
+       [
+         "jq --slurp " ^ jq_slurp_filter ^ input_scan_reports
+         ^ "> gl-container-scanning-report.json";
+       ])
+  in
+
+  (Common.job_datadog_pipeline_trace :: job_list_container_scanning)
+  @ [job_container_scanning_merge_reports]
