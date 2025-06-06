0ba4a751b6ba21ae5237674cfc89a7191886e956
mbourgoin <mathias.bourgoin@nomadic-labs.com>
2025-06-06 09:37:59 +0200

    Seoul/src: set current version

--- src/proto_seoul/lib_protocol/constants_repr.ml
+++ src/proto_seoul/lib_protocol/constants_repr.ml
@@ -24,7 +24,7 @@
 (* DEALINGS IN THE SOFTWARE.                                                 *)
 (*                                                                           *)
 (*****************************************************************************)
-let version_value = "alpha_current"
+let version_value = "seoul"
 
 let version = "v1"
 
