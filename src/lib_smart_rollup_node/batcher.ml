8bf0f0c81f91f002490de3c85c652eced12de5df
Alain Mebsout <alain.mebsout@functori.com>
2025-06-10 17:04:59 +0200

    Rollup node: opentelemetry traces for batcher

--- src/lib_smart_rollup_node/batcher.ml
+++ src/lib_smart_rollup_node/batcher.ml
@@ -430,6 +430,8 @@ let handle_request_error rq =
 let register_messages ?order ~drop_duplicate messages =
   let open Lwt_result_syntax in
   let*? w = worker () in
+  Opentelemetry_lwt.Trace.with_ ~service_name:"Batcher" "register_messages"
+  @@ fun _ ->
   Worker.Queue.push_request_and_wait
     w
     (Request.Register {order; messages; drop_duplicate})
@@ -443,6 +445,8 @@ let produce_batches () =
       return_unit
   | Error e -> fail e
   | Ok w ->
+      Opentelemetry_lwt.Trace.with_ ~service_name:"Batcher" "produce_batches"
+      @@ fun _ ->
       Worker.Queue.push_request_and_wait w Request.Produce_batches
       |> handle_request_error
 
