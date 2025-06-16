4e0c3e6e6447e1196de0114f9f55cf74735a472d
Thomas Letan <lthms@nomadic-labs.com>
2025-06-16 14:53:33 +0200

    Octez_telemetry: Introduce helpers for tracing
    * What
    
    This commit introduces a new `Octez_telemetry` library. This library
    provides convenient wrappers around the `Opentelemetry_lwt` library. Its
    primary purpose is to simplify the integration of OpenTelemetry-based
    tracing within Octez libraries and applications.
    
    * Why
    
    The new library aims to streamline the process of instrumenting code with
    tracing capabilities, especially when dealing with the Tezos error monad. By
    offering specialized wrappers, it reduces boilerplate and promotes consistent
    tracing practices across the codebase, improving observability and debugging
    efforts.
    
    * How
    
    A new `src/lib_telemetry` directory has been created, containing `trace.ml` and
    `trace.mli`. These files define `with_tzresult` which automatically handle span
    status and error messages based on the outcome of a computation in the Error
    monad. The library has been integrated into `product_octez.ml` and
    `product_etherlink.ml` manifests and `dune` files.

---
+++ src/lib_telemetry/trace.ml
@@ -0,0 +1,42 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2025 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+let message_on_tztrace tztrace =
+  Format.asprintf "%a" Error_monad.pp_print_trace tztrace
+
+let with_result ?(message_on_success = Fun.const "Success")
+    ?(message_on_error = Fun.const "Something went wrong") ?force_new_trace_id
+    ?trace_state ?service_name ?attrs ?kind ?trace_id ?parent ?scope ?links
+    span_name k =
+  Opentelemetry_lwt.Trace.with_
+    ?force_new_trace_id
+    ?trace_state
+    ?service_name
+    ?attrs
+    ?kind
+    ?trace_id
+    ?parent
+    ?scope
+    ?links
+    span_name
+  @@ fun scope ->
+  let open Lwt_result_syntax in
+  let*! res = k scope in
+  match res with
+  | Ok res when Option.is_none (Opentelemetry.Scope.status scope) ->
+      Opentelemetry.Scope.set_status
+        scope
+        {code = Status_code_ok; message = message_on_success res} ;
+      return res
+  | Error err when Option.is_none (Opentelemetry.Scope.status scope) ->
+      Opentelemetry.Scope.set_status
+        scope
+        {code = Status_code_error; message = message_on_error err} ;
+      fail err
+  | res -> Lwt.return res
+
+let with_tzresult = with_result ~message_on_error:message_on_tztrace
