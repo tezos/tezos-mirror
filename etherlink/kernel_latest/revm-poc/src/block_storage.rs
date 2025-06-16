afbdedab9d995bfce32da7f35688cbf8938be892
Rodi-Can Bozman <rodi.bozman@functori.com>
2025-06-16 16:50:44 +0200

    Etherlink/Revm: readapt block storage with errors

--- etherlink/kernel_latest/revm-poc/src/block_storage.rs
+++ etherlink/kernel_latest/revm-poc/src/block_storage.rs
@@ -3,26 +3,29 @@
 // SPDX-License-Identifier: MIT
 
 use revm::primitives::B256;
-use tezos_smart_rollup_host::{path::OwnedPath, runtime::Runtime};
+use tezos_evm_runtime::runtime::Runtime;
+use tezos_smart_rollup_host::path::OwnedPath;
+
+use crate::Error;
 
 // Ref. https://www.evm.codes/?fork=cancun#40
 // (opcode 0x40: BLOCKHASH)
 pub const BLOCKS_STORED: u64 = 256;
 
-fn to_block_hash_path(block_number: u64) -> OwnedPath {
+fn to_block_hash_path(block_number: u64) -> Result<OwnedPath, Error> {
     let path: Vec<u8> =
         format!("/evm/world_state/indexes/blocks/{}", block_number).into();
-    OwnedPath::try_from(path).unwrap()
+    OwnedPath::try_from(path).map_err(|err| Error::Custom(err.to_string()))
 }
 
 /// Get block hash by block number.
-pub fn get_block_hash(host: &impl Runtime, block_number: u64) -> B256 {
-    let block_path = to_block_hash_path(block_number);
-    let block_hash = host.store_read_all(&block_path).unwrap();
+pub fn get_block_hash(host: &impl Runtime, block_number: u64) -> Result<B256, Error> {
+    let block_path = to_block_hash_path(block_number)?;
+    let block_hash = host.store_read_all(&block_path)?;
 
     if block_hash.len() == 32 {
-        B256::from_slice(&block_hash)
+        Ok(B256::from_slice(&block_hash))
     } else {
-        panic!("Malformed block hash")
+        Err(Error::Custom("Malformed block hash".to_owned()))
     }
 }
