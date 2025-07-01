74f9283af7780d4cd22c7ce9d00304e17f885daf
Sébastien Palmer <sebastien.palmer@functori.com>
2025-07-01 11:39:30 +0200

    Rust SDK: add a contract crate
    Duplicated from `src/kernel_sdk/encoding/src/contract.rs`

--- sdk/rust/protocol/src/lib.rs
+++ sdk/rust/protocol/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// SPDX-FileCopyrightText: 2025 Functori <contact@functori.com>
+//
+// SPDX-License-Identifier: MIT
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod contract;
