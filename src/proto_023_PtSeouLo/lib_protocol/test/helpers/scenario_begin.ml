ffe22dcc6c0e46955bd538bcd6cf8a85b2db65e4
Lucas Randazzo <lucas@nomadic-labs.com>
2025-07-07 17:00:07 +0200

    023_PtSeouLo/Proto/tests/scenario: upgrade end of block checks with metadata
    Porting to proto 023_PtSeouLo 4092305bee378bb967e665b5f6af623018fff9f4 - Proto/tests/scenario: upgrade end of block checks with metadata

--- src/proto_023_PtSeouLo/lib_protocol/test/helpers/scenario_begin.ml
+++ src/proto_023_PtSeouLo/lib_protocol/test/helpers/scenario_begin.ml
@@ -82,8 +82,8 @@ let init_constants ?(default = Test) ?(reward_per_block = 0L)
 
 (** Initialize the test, given some initial parameters *)
 let begin_test ?algo ?(burn_rewards = false) ?(force_attest_all = false)
-    ?(check_finalized_block = fun _ -> Lwt_result_syntax.return_unit)
-    delegates_name_list : (constants, t) scenarios =
+    ?(check_finalized_block_perm = []) delegates_name_list :
+    (constants, t) scenarios =
   exec (fun (constants : constants) ->
       let open Lwt_result_syntax in
       let bootstrap = "__bootstrap__" in
@@ -152,7 +152,8 @@ let begin_test ?algo ?(burn_rewards = false) ?(force_attest_all = false)
             pending_slashes = [];
             double_signings = [];
             force_attest_all;
-            check_finalized_block;
+            check_finalized_block_perm;
+            check_finalized_block_temp = [];
             operation_mode = Bake;
           }
       in
