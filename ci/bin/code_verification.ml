0940edaba5de6ece246669d59157946540c92901
Bruno Bernardo <bruno@nomadic-labs.com>
2025-07-15 14:08:06 +0200

    Revert "CI: [Tezos_ci.trigger_job] in [Stages.start] by construction"
    This reverts commit 8e3977edff65fb3f742f729a3a1ab37607a02fc6.

--- ci/bin/code_verification.ml
+++ ci/bin/code_verification.ml
@@ -931,6 +931,7 @@ let jobs pipeline_type =
       trigger_job
         ~__POS__
         ~rules:(make_rules ~manual:No ~changes:changeset_debian_packages ())
+        ~stage:Stages.start
         ~dependencies:dependencies_needs_start
         Debian_repository.child_pipeline_partial_auto
     in
@@ -938,6 +939,7 @@ let jobs pipeline_type =
       trigger_job
         ~__POS__
         ~dependencies:(Dependent [])
+        ~stage:Stages.start
         Debian_repository.child_pipeline_full
     in
 
@@ -946,6 +948,7 @@ let jobs pipeline_type =
       trigger_job
         ~__POS__
         ~rules:(make_rules ~manual:No ~changes:changeset_rpm_packages ())
+        ~stage:Stages.start
         ~dependencies:dependencies_needs_start
         Rpm_repository.child_pipeline_partial_auto
     in
@@ -953,6 +956,7 @@ let jobs pipeline_type =
       trigger_job
         ~__POS__
         ~dependencies:(Dependent [])
+        ~stage:Stages.start
         Rpm_repository.child_pipeline_full
     in
 
@@ -960,6 +964,7 @@ let jobs pipeline_type =
       trigger_job
         ~__POS__
         ~rules:(make_rules ~manual:No ~changes:changeset_homebrew ())
+        ~stage:Stages.start
         ~dependencies:dependencies_needs_start
         Homebrew.child_pipeline_full_auto
     in
@@ -967,6 +972,7 @@ let jobs pipeline_type =
       trigger_job
         ~__POS__
         ~rules:(make_rules ~manual:No ~changes:changeset_homebrew ())
+        ~stage:Stages.start
         ~dependencies:(Dependent [])
         Homebrew.child_pipeline_full
     in
@@ -975,6 +981,7 @@ let jobs pipeline_type =
       trigger_job
         ~__POS__
         ~rules:(make_rules ~manual:No ~changes:changeset_base_images ())
+        ~stage:Stages.start
         ~dependencies:(Dependent [])
         Base_images.child_pipeline
     in
@@ -2074,6 +2081,7 @@ let jobs pipeline_type =
         ~__POS__
         ~rules:(make_rules ~manual:Yes ())
         ~dependencies:(Dependent [])
+        ~stage:Stages.start
         Debian_repository.child_pipeline_partial
     in
     let job_rpm_repository_trigger_partial : tezos_job =
@@ -2084,6 +2092,7 @@ let jobs pipeline_type =
         ~__POS__
         ~rules:(make_rules ~manual:Yes ())
         ~dependencies:(Dependent [])
+        ~stage:Stages.start
         Rpm_repository.child_pipeline_partial
     in
     let job_homebrew_repository_trigger : tezos_job =
@@ -2093,12 +2102,14 @@ let jobs pipeline_type =
         ~__POS__
         ~rules:(make_rules ~manual:Yes ())
         ~dependencies:(Dependent [])
+        ~stage:Stages.start
         Homebrew.child_pipeline_full
     in
     let job_base_images_trigger =
       trigger_job
         ~__POS__
         ~rules:(make_rules ~manual:Yes ())
+        ~stage:Stages.start
         ~dependencies:(Dependent [])
         Base_images.child_pipeline
     in
