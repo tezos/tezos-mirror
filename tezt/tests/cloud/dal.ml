06b2fea89387ee021dd6ec802fcc0add65b2f059
Gabriel Moise <gabriel.moise@trili.tech>
2025-07-18 21:23:47 +0100

    Tezt_cloud: Refactor echo_rollup

--- tezt/tests/cloud/dal.ml
+++ tezt/tests/cloud/dal.ml
@@ -165,14 +165,6 @@ type baker = {
   stake : int;
 }
 
-type echo_operator = {
-  node : Node.t;
-  client : Client.t;
-  sc_rollup_node : Sc_rollup_node.t;
-  sc_rollup_address : string;
-  operator : Account.key;
-}
-
 type etherlink_operator_setup = {
   node : Node.t;
   client : Client.t;
@@ -259,7 +251,7 @@ type t = {
       (* NOTE: they have the observer profile*)
   observers : Dal_node_helpers.observer list;
   etherlink : etherlink option;
-  echo_rollup : echo_operator option;
+  echo_rollup : Echo_rollup.operator option;
   time_between_blocks : int;
   parameters : Dal_common.Parameters.t;
   infos : (int, per_level_info) Hashtbl.t;
@@ -1961,19 +1953,6 @@ let init_teztale (configuration : configuration) cloud agent =
   if configuration.teztale then init_teztale cloud agent |> Lwt.map Option.some
   else Lwt.return_none
 
-let init_echo_rollup_account (bootstrap : bootstrap)
-    (configuration : configuration) =
-  if configuration.echo_rollup then
-    let () = toplog "Initializing the echo rollup key" in
-    let* key =
-      Client.stresstest_gen_keys
-        ~alias_prefix:"echo_operator"
-        1
-        bootstrap.client
-    in
-    Lwt.return_some (List.hd key)
-  else Lwt.return_none
-
 let init_etherlink_operators (bootstrap : bootstrap) etherlink_configuration =
   let* etherlink_rollup_operator_key =
     if etherlink_configuration <> None then
@@ -2144,7 +2123,12 @@ let init_public_network cloud (configuration : configuration)
   let* etherlink_rollup_operator_key, etherlink_batching_operator_keys =
     init_etherlink_operators bootstrap etherlink_configuration
   in
-  let* echo_rollup_key = init_echo_rollup_account bootstrap configuration in
+  let* echo_rollup_key =
+    Echo_rollup.init_echo_rollup_account
+      ~client:bootstrap.client
+      ~echo_rollup:configuration.echo_rollup
+      ~alias_prefix:"echo_operator"
+  in
   let accounts_to_fund =
     (if configuration.producer_key = None then
        List.map (fun producer -> (producer, 10 * 1_000_000)) producer_accounts
@@ -2420,14 +2404,11 @@ let init_sandbox_and_activate_protocol cloud (configuration : configuration)
     else Lwt.return []
   in
   let* echo_rollup_key =
-    if configuration.echo_rollup then
-      let* keys =
-        Client.stresstest_gen_keys ~alias_prefix:"echo_rollup_key" 1 client
-      in
-      Lwt.return_some (List.hd keys)
-    else Lwt.return_none
+    Echo_rollup.init_echo_rollup_account
+      ~client
+      ~echo_rollup:configuration.echo_rollup
+      ~alias_prefix:"echo_rollup_key"
   in
-
   let* () =
     if configuration.simulate_network = Disabled then
       let* parameter_file =
@@ -3199,173 +3180,6 @@ let init_etherlink cloud configuration etherlink_configuration ~bootstrap
   in
   return {configuration = etherlink_configuration; operator; accounts}
 
-let init_echo_rollup cloud configuration ~bootstrap operator dal_slots
-    next_agent =
-  let name = name_of Echo_rollup_operator in
-  let* agent = next_agent ~name in
-  let data_dir =
-    configuration.data_dir |> Option.map (fun data_dir -> data_dir // name)
-  in
-  let env, with_yes_crypto =
-    may_set_yes_crypto_env configuration.simulate_network
-  in
-  let* node =
-    Node_helpers.init
-      ?env
-      ?data_dir
-      ~name
-      ~arguments:
-        [
-          Peer bootstrap.node_p2p_endpoint;
-          refutation_game_minimal_rolling_history_mode;
-        ]
-      ~rpc_external:configuration.external_rpc
-      configuration.network
-      ~snapshot:configuration.snapshot
-      ~with_yes_crypto
-      cloud
-      agent
-  in
-  let endpoint = Client.Node node in
-  let* client = Client.Agent.create ~endpoint agent in
-  let () = toplog "Init Echo rollup: importing the operator secret key" in
-  let* () =
-    Client.import_secret_key
-      client
-      ~endpoint
-      operator.Account.secret_key
-      ~alias:operator.Account.alias
-  in
-
-  let l = Node.get_last_seen_level node in
-  let () = toplog "Init Echo rollup: revealing the operator account" in
-  let*! () = Client.reveal client ~endpoint ~src:operator.Account.alias in
-  let () = toplog "Init Echo rollup: waiting for level %d" (l + 2) in
-  let* _ = Node.wait_for_level node (l + 2) in
-  let () = toplog "Init Echo rollup: waiting for level %d: done" (l + 2) in
-
-  let otel = Cloud.open_telemetry_endpoint cloud in
-  let* dal_node =
-    match dal_slots with
-    | [] ->
-        toplog "Echo rollup doesn't follow any slot" ;
-        none
-    | [slot_index] ->
-        let name = name_of (Echo_rollup_dal_observer {slot_index}) in
-        let* agent = next_agent ~name in
-        let* dal_node = Dal_node.Agent.create ~name ~node cloud agent in
-        let* () =
-          Dal_node.init_config
-            ~expected_pow:(Network.expected_pow configuration.network)
-            ~observer_profiles:dal_slots
-            ~peers:(Option.to_list bootstrap.dal_node_p2p_endpoint)
-            dal_node
-        in
-        let* () =
-          Dal_node.Agent.run
-            ?otel
-            ~ppx_profiling:configuration.ppx_profiling
-            ~ppx_profiling_backends:configuration.ppx_profiling_backends
-            dal_node
-        in
-        some dal_node
-    | _ ->
-        let* dal_reverse_proxy_with_observers =
-          Dal_reverse_proxy.init_dal_reverse_proxy_observers
-            ~external_rpc:configuration.external_rpc
-            ~network:configuration.network
-            ~snapshot:configuration.snapshot
-            ~ppx_profiling:configuration.ppx_profiling
-            ~ppx_profiling_backends:configuration.ppx_profiling_backends
-            ~memtrace:configuration.memtrace
-            ~simulate_network:configuration.simulate_network
-            ~name_of:(fun slot_index ->
-              name_of (Echo_rollup_dal_observer {slot_index}))
-            ~default_endpoint:None
-            ~node_p2p_endpoint:bootstrap.node_p2p_endpoint
-            ~dal_node_p2p_endpoint:bootstrap.dal_node_p2p_endpoint
-            ~dal_slots
-            ~next_agent
-            ~otel
-            ~cloud
-        in
-        some dal_reverse_proxy_with_observers
-  in
-  let* sc_rollup_node =
-    Sc_rollup_node.Agent.create
-      ~name:(Format.asprintf "%s-rollup-node" name)
-      ~base_dir:(Client.base_dir client)
-      ~default_operator:operator.alias
-      ~operators:[(Sc_rollup_node.Operating, operator.Account.alias)]
-      ?dal_node
-      cloud
-      agent
-      Operator
-      node
-  in
-  let preimages_dir =
-    Filename.concat (Sc_rollup_node.data_dir sc_rollup_node) "wasm_2_0_0"
-  in
-  let slots_bitvector =
-    List.fold_left
-      (fun vec slot -> Z.logor vec (Z.of_int (1 lsl slot)))
-      Z.zero
-      dal_slots
-  in
-  let config =
-    Format.sprintf
-      {|instructions:
-- set:
-    value: %s
-    to: /slots
-      |}
-      (Z.to_bits slots_bitvector |> Hex.of_string |> Hex.show)
-  in
-  let output_config = Temp.file "config.yaml" in
-  write_file output_config ~contents:config ;
-  let* remote_output_config = Agent.copy agent ~source:output_config in
-  let* {output; _} =
-    Sc_rollup_helpers.Agent.prepare_installer_kernel
-      ~config:(`Path remote_output_config)
-      ~preimages_dir
-      Constant.WASM.dal_echo_kernel_bandwidth
-      agent
-  in
-  let pvm_kind = "wasm_2_0_0" in
-  let l = Node.get_last_seen_level node in
-  let () = toplog "Init Echo rollup: originating the rollup" in
-  let* sc_rollup_address =
-    Sc_rollup_helpers.Agent.originate_sc_rollup
-      ~kind:pvm_kind
-      ~boot_sector:output
-      ~parameters_ty:"unit"
-      ~src:operator.alias
-      client
-  in
-  let () = toplog "Init Echo rollup: waiting again, for level %d" (l + 2) in
-  let* _ = Node.wait_for_level node (l + 2) in
-  let () =
-    toplog "Init Echo rollup: waiting again, for level %d: done" (l + 2)
-  in
-  let () = toplog "Init Echo rollup: launching the rollup node" in
-  let* () =
-    Sc_rollup_node.run sc_rollup_node sc_rollup_address [Log_kernel_debug]
-  in
-  let () = toplog "Init Echo rollup: launching the rollup node: done" in
-  let operator : echo_operator =
-    {node; client; sc_rollup_node; operator; sc_rollup_address}
-  in
-  let* () =
-    add_prometheus_source
-      ?dal_node
-      ~node
-      ~sc_rollup_node
-      cloud
-      agent
-      (Format.asprintf "echo-%s" name)
-  in
-  return operator
-
 let obtain_some_node_rpc_endpoint agent network (bootstrap : bootstrap)
     (bakers : baker list) (producers : Dal_node_helpers.producer list)
     (observers : Dal_node_helpers.observer list) etherlink =
@@ -3591,25 +3405,22 @@ let init ~(configuration : configuration) etherlink_configuration cloud
   in
   let () = toplog "Init: all producers and observers have been initialized" in
   let* echo_rollup =
-    match echo_rollup_key with
-    | Some operator ->
-        let dal_slots =
-          List.map (fun p -> p.Dal_node_helpers.slot_index) producers
-        in
-        let* echo_rollup =
-          init_echo_rollup
-            cloud
-            configuration
-            ~bootstrap
-            operator
-            dal_slots
-            next_agent
-        in
-        Lwt.return_some echo_rollup
-    | _ -> Lwt.return_none
+    Echo_rollup.init_echo_rollup
+      cloud
+      ~data_dir:configuration.data_dir
+      ~simulate_network:configuration.simulate_network
+      ~external_rpc:configuration.external_rpc
+      ~network:configuration.network
+      ~snapshot:configuration.snapshot
+      ~ppx_profiling:configuration.ppx_profiling
+      ~ppx_profiling_backends:configuration.ppx_profiling_backends
+      ~memtrace:configuration.memtrace
+      ~node_p2p_endpoint:bootstrap.node_p2p_endpoint
+      ~dal_node_p2p_endpoint:bootstrap.dal_node_p2p_endpoint
+      ~next_agent
+      producers
+      echo_rollup_key
   in
-
-  let () = toplog "Init: Echo rollup has been initialized" in
   let* etherlink =
     match etherlink_configuration with
     | Some etherlink_configuration ->
---
+++ tezt/tests/cloud/echo_rollup.mli
@@ -0,0 +1,42 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2025 Trilitech <contact@trili.tech>                         *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** This module handles the initialization and orchestration of an Echo rollup
+    within a Tezt-cloud scenario.
+
+    An Echo rollup is a smart rollup running a WASM kernel that echoes back
+    messages published via DAL slots.
+*)
+
+type operator
+
+(** [init_echo_rollup_account ~client ~echo_rollup ~alias_prefix] generates
+    a new rollup operator key if [~echo_rollup] is [true]. The key alias will
+    start with [~alias_prefix]. *)
+val init_echo_rollup_account :
+  client:Client.t ->
+  echo_rollup:bool ->
+  alias_prefix:string ->
+  Account.key option Lwt.t
+
+(** Initialize an Echo rollup. *)
+val init_echo_rollup :
+  Cloud.t ->
+  data_dir:string option ->
+  simulate_network:Scenarios_cli.network_simulation_config ->
+  external_rpc:bool ->
+  network:Network.t ->
+  snapshot:Snapshot_helpers.t ->
+  ppx_profiling:bool ->
+  ppx_profiling_backends:string list ->
+  memtrace:bool ->
+  node_p2p_endpoint:string ->
+  dal_node_p2p_endpoint:string option ->
+  next_agent:(name:string -> Agent.t Lwt.t) ->
+  Dal_node_helpers.producer list ->
+  Account.key option ->
+  operator option Lwt.t
