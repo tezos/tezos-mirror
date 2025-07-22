cd8480a82f30407d67dc78e4e9e35f78719749d7
Guillaume Bau <guillaume.bau@nomadic-labs.com>
2025-07-22 20:59:54 +0200

    Tezt/Cloud: do not depend on logrotate to handle sighup

--- tezt/lib_cloud/env.ml
+++ tezt/lib_cloud/env.ml
@@ -118,19 +118,18 @@ let init () =
      This allows logrotate to use a better strategy than copytruncate
      https://incoherency.co.uk/blog/stories/logrotate-copytruncate-race-condition.html
   *)
-  (if log_rotation <> 0 then
-     match Tezt_core.Cli.Logs.file with
-     | None -> ()
-     | Some logfile ->
-         Log.report "Installing signal handler for SIGHUP" ;
-         let signal_hup_handler signal =
-           if signal = Sys.sighup then (
-             (* TODO: Not sure if set_file is async-signal-safe.
-                Maybe implement a better solution if it causes issues *)
-             Tezt_core.Log.set_file logfile ;
-             Log.report "Logfile was reopened")
-         in
-         Sys.set_signal Sys.sighup (Sys.Signal_handle signal_hup_handler)) ;
+  (match Tezt_core.Cli.Logs.file with
+  | None -> ()
+  | Some logfile ->
+      Log.report "Installing signal handler for SIGHUP" ;
+      let signal_hup_handler signal =
+        if signal = Sys.sighup then (
+          (* TODO: Not sure if set_file is async-signal-safe.
+             Maybe implement a better solution if it causes issues *)
+          Tezt_core.Log.set_file logfile ;
+          Log.report "Logfile was reopened")
+      in
+      Sys.set_signal Sys.sighup (Sys.Signal_handle signal_hup_handler)) ;
   match mode with
   | `Local_orchestrator_local_agents | `Ssh_host _ -> Lwt.return_unit
   | `Remote_orchestrator_local_agents ->
