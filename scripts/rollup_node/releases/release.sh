abcc14377c06380bef92a10ea4020067576b6408
Killian Delarue <killian.delarue@nomadic-labs.com>
2025-07-29 15:17:24 +0200

    Scripts, Rollup-node: Remove prefix [gitlab] in some variables names
    This is more consistent with the other scripts

--- scripts/rollup_node/releases/release.sh
+++ scripts/rollup_node/releases/release.sh
@@ -5,21 +5,21 @@
 # shellcheck disable=SC2034
 architectures='x86_64 arm64'
 
-# Full octez release tag
+# Full release tag
 # octez-smart-rollup-node-vX.Y, octez-smart-rollup-node-vX.Y-rcZ or octez-smart-rollup-node-vX.Y-betaZ
-gitlab_release=$(echo "${CI_COMMIT_TAG}" | grep -oE '^(octez-smart-rollup-node|octez)-v[0-9]+(\.[0-9]+)?(-(rc|beta)([0-9]+))?$' || :)
+release=$(echo "${CI_COMMIT_TAG}" | grep -oE '^(octez-smart-rollup-node|octez)-v[0-9]+(\.[0-9]+)?(-(rc|beta)([0-9]+))?$' || :)
 
 # Strips the leading 'octez-smart-rollup-node-v'
 # X.Y, X.Y-rcZ or X.Y-betaZ
-gitlab_release_no_v=$(echo "${gitlab_release}" | sed -E 's/^(octez-smart-rollup-node|octez)-v//g')
+release_no_v=$(echo "${release}" | sed -E 's/^(octez-smart-rollup-node|octez)-v//g')
 
 # Replace '.' with '-'
 # X-Y or X-Y-rcZ
 # shellcheck disable=SC2034
-gitlab_release_no_dot=$(echo "${gitlab_release_no_v}" | sed -e 's/\./-/g')
+release_no_dot=$(echo "${release_no_v}" | sed -e 's/\./-/g')
 
 # shellcheck disable=SC2034
-gitlab_release_tilda=$(echo "${gitlab_release_no_v}" | sed -e 's/-/~/g')
+release_tilda=$(echo "${release_no_v}" | sed -e 's/-/~/g')
 
 # X
 release_major_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^(octez-smart-rollup-node|octez)-v([0-9]+)\.([0-9]+)((-rc[0-9]+)?|(-beta[0-9]+)?)$/\2/p')
@@ -34,19 +34,19 @@ release_beta_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^(octez-smart-rollup
 if [ -n "${release_rc_version}" ]; then
   # Yes, release name: X.Y~rcZ
   # shellcheck disable=SC2034
-  gitlab_release_name="Octez Smart Rollup Node Release Candidate v${gitlab_release_tilda}"
+  gitlab_release_name="Octez Smart Rollup Node Release Candidate v${release_tilda}"
 # Is this a beta ?
 elif [ -n "${release_beta_version}" ]; then
-  gitlab_release_name="Octez Smart Rollup Node Beta v${gitlab_release_tilda}"
+  gitlab_release_name="Octez Smart Rollup Node Beta v${release_tilda}"
 else
   # No, release name: Octez SMART-ROLLUP Node Release X.Y
   # shellcheck disable=SC2034
-  gitlab_release_name="Octez Smart Rollup Node Release v${gitlab_release_tilda}"
+  gitlab_release_name="Octez Smart Rollup Node Release v${release_tilda}"
 fi
 
 ### Compute GitLab generic package names
 
-suffix="${gitlab_release_no_v}"
+suffix="${release_no_v}"
 gitlab_octez_binaries_package_name="octez-smart-rollup-node-${suffix}"
 
 # X.Y or X.Y-rcZ
