4f51711c58debc68c91f48e9a5e01a5a4fe90a76
Thomas Plisson <thomas.plisson@lambda-coins.com>
2025-07-30 10:55:22 +0200

    REVM: Tezt scenario confirms EIP-2537 is enabled

--- etherlink/tezt/tests/evm_sequencer.ml
+++ etherlink/tezt/tests/evm_sequencer.ml
@@ -13598,6 +13598,31 @@ let test_eip7702 =
   Check.((code = expected_code) string) ~error_msg:"Expected code of %R, got %L" ;
   unit
 
+let test_eip2537 =
+  register_all
+    ~__FILE__
+    ~tags:["evm"; "bls"; "precompile"; "eip2537"]
+    ~title:"EIP-2537 is activated on Etherlink"
+    ~kernels:[Latest]
+  @@ fun {sequencer; _} _protocol ->
+  let bls_g1add = "0x000000000000000000000000000000000000000b" in
+  (* First entry of BLS12_G1ADD at https://eips.ethereum.org/assets/eip-2537/test-vectors *)
+  let input =
+    "0x0000000000000000000000000000000017f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb0000000000000000000000000000000008b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e100000000000000000000000000000000112b98340eee2777cc3c14163dea3ec97977ac3dc5c70da32e6e87578f44912e902ccef9efe28d4a78b8999dfbca942600000000000000000000000000000000186b28d92356c4dfec4b5201ad099dbdede3781f8998ddf929b4cd7756192185ca7b8f4ef7088f813270ac3d48868a21"
+  in
+  let expected =
+    "0x000000000000000000000000000000000a40300ce2dec9888b60690e9a41d3004fda4886854573974fab73b046d3147ba5b7a5bde85279ffede1b45b3918d82d0000000000000000000000000000000006d3d887e9f53b9ec4eb6cedf5607226754b07c01ace7834f57f3e7315faefb739e59018e22c492006190fba4a870025"
+  in
+  let* value =
+    Cast.raw_call
+      ~endpoint:(Evm_node.endpoint sequencer)
+      ~address:bls_g1add
+      ~arg:input
+  in
+  Check.((value = expected) string)
+    ~error_msg:(Format.sprintf "BLS12_G1ADD mismatch for test bls_g1add_g1+p1") ;
+  unit
+
 let protocols = Protocol.all
 
 let () =
@@ -13769,4 +13794,5 @@ let () =
   test_fa_deposit_can_be_claimed [Alpha] ;
   test_claim_deposit_event [Alpha] ;
   test_eip2930_storage_access [Alpha] ;
-  test_eip7702 [Alpha]
+  test_eip7702 [Alpha] ;
+  test_eip2537 [Alpha]
