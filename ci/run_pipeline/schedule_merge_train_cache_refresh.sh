0d961a890ff1bb1ece582e951473fb04fe7b9d85
Bruno Bernardo <bruno@nomadic-labs.com>
2025-09-02 12:44:13 +0200

    (temp-not working) CI/run_pipeline: add dummy values when running [schedule_merge_train_cache_refresh]
    Needed to prevent mistakes such as:
    https://gitlab.com/tezos/tezos/-/jobs/11214681520 (CHECK_LICENSES_DIFF_BASE in [oc.misc_checks])
    https://gitlab.com/tezos/tezos/-/jobs/11214681533 (CI_MERGE_REQUEST_TARGET_BRANCH_NAME in [commit_titles])
    
    edit: not working

--- ci/run_pipeline/schedule_merge_train_cache_refresh.sh
+++ ci/run_pipeline/schedule_merge_train_cache_refresh.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && echo "$(pwd -P)/")"
 cd "$SCRIPT_DIR" || exit
 
-TZ_SCHEDULE_KIND=CACHE_REFRESH ./scheduled.sh
+CI_MERGE_REQUEST_TARGET_BRANCH_NAME="origin/master" CHECK_LICENSES_DIFF_BASE="origin/master" TZ_SCHEDULE_KIND=CACHE_REFRESH ./scheduled.sh
