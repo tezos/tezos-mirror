1c733d3b71efb2da020ad3df3017974ca658b094
Luciano Freitas <luciano.freitas@nomadic-labs.com>
2025-09-16 18:09:20 +0200

    Tezlink/Kernel: create functions to get big_map durable paths
    Co-authored-by: Arnaud Bihan <arnaud.bihan@functori.com>

--- etherlink/kernel_latest/tezos_execution/src/context.rs
+++ etherlink/kernel_latest/tezos_execution/src/context.rs
@@ -2,6 +2,11 @@
 //
 // SPDX-License-Identifier: MIT
 
+use mir::{
+    ast::{big_map::BigMapId, IntoMicheline, TypedValue},
+    parser::Parser,
+};
+use tezos_crypto_rs::blake2b::digest_256;
 use tezos_smart_rollup_host::path::{concat, OwnedPath, Path, PathError, RefPath};
 
 // TODO: https://gitlab.com/tezos/tezos/-/issues/7867: add the missing paths
@@ -30,9 +35,7 @@ impl Context {
 }
 
 pub mod contracts {
-    use tezos_smart_rollup_host::path::{concat, OwnedPath, PathError, RefPath};
-
-    use super::Context;
+    use super::*;
 
     const ROOT: RefPath = RefPath::assert_from(b"/contracts");
 
@@ -50,5 +53,64 @@ pub mod contracts {
 
     pub fn global_counter(context: &Context) -> Result<OwnedPath, PathError> {
         concat(&root(context)?, &GLOBAL_COUNTER)
+    }
+}
+
+pub mod big_maps {
+    use super::*;
+
+    const BIG_MAP_PATH: RefPath = RefPath::assert_from(b"/big_map");
+
+    const KEY_TYPE_PATH: RefPath = RefPath::assert_from(b"/key_type");
+
+    const VALUE_TYPE_PATH: RefPath = RefPath::assert_from(b"/value_type");
+
+    const NEXT_ID_PATH: RefPath = RefPath::assert_from(b"/next_id");
+
+    fn root(context: &Context) -> Result<OwnedPath, PathError> {
+        concat(&context.path, &BIG_MAP_PATH)
+    }
+
+    pub fn next_id_path(context: &Context) -> Result<OwnedPath, PathError> {
+        concat(&root(context)?, &NEXT_ID_PATH)
+    }
+
+    pub fn big_map_path(
+        context: &Context,
+        id: &BigMapId,
+    ) -> Result<OwnedPath, PathError> {
+        concat(&root(context)?, &OwnedPath::try_from(format!("/{id}"))?)
+    }
+
+    pub fn key_type_path(
+        context: &Context,
+        id: &BigMapId,
+    ) -> Result<OwnedPath, PathError> {
+        concat(&big_map_path(context, id)?, &KEY_TYPE_PATH)
+    }
+
+    pub fn value_type_path(
+        context: &Context,
+        id: &BigMapId,
+    ) -> Result<OwnedPath, PathError> {
+        concat(&big_map_path(context, id)?, &VALUE_TYPE_PATH)
+    }
+
+    pub fn value_path(
+        context: &Context,
+        id: &BigMapId,
+        key: &TypedValue,
+    ) -> Result<OwnedPath, PathError> {
+        let parser = Parser::new();
+        let key_encoded = key
+            .clone()
+            .into_micheline_optimized_legacy(&parser.arena)
+            .encode();
+        let key_hashed = digest_256(&key_encoded);
+        let key_hex = hex::encode(key_hashed);
+        concat(
+            &big_map_path(context, id)?,
+            &OwnedPath::try_from(format!("/{key_hex}"))?,
+        )
     }
 }
