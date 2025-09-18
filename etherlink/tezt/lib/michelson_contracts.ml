3e97d1941dba20ba503477ac2d99200b6616a701
Luciano Freitas <luciano.freitas@nomadic-labs.com>
2025-09-18 12:21:15 +0200

    Tezlink/Tezt: add comment on how to obtain bootstrap addresses

--- etherlink/tezt/lib/michelson_contracts.ml
+++ etherlink/tezt/lib/michelson_contracts.ml
@@ -7,6 +7,32 @@
 
 let tezlink_protocol = Protocol.S023
 
+(* How to add a new contract to this list to serve as bootstrap contracts in
+   Tezlink integration tests.
+
+   To add a contract to this list, we need to pick a fresh KT1 address. It's
+   also a good idea to manually check that originating the new contract on Tezos
+   succeeds. Note that a fresh KT1 address is part of the result of this manual
+   origination test. The most convenient way to run this manual test and generate
+   the address is to use the mockup mode of octez-client:
+
+
+   1) Create a convenient alias for the client:
+      alias mockup-client='/path/to/octez-client --mode mockup --base-dir /tmp/mockup'
+      mockup-client create mockup
+
+   2) Originate the contract; the address will be printed on success:
+      mockup-client originate contract <contract_name> \
+        transferring 0 from bootstrap1 \
+        running <path/to/contract> \
+        --init '<initial_storage>' \
+        --burn-cap 1 \
+        --force
+
+   3) The address can be found on the bottom of the receipt and it can be later consulted by
+      mockup-client list known contracts
+*)
+
 let concat_hello () =
   Evm_node.
     {
