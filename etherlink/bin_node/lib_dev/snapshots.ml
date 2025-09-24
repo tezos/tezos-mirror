c34baf5c812fa8dda0612e2079a73701bd7ca3f6
Alain Mebsout <alain.mebsout@functori.com>
2025-09-24 20:53:59 +0200

    EVM node: snapshots with deduplication using desync

--- etherlink/bin_node/lib_dev/snapshots.ml
+++ etherlink/bin_node/lib_dev/snapshots.ml
@@ -214,3 +214,202 @@ let interpolate_snapshot_provider ?rollup_address ?network history_mode provider
   @@ Misc.interpolate
        provider
        [rollup_address_short; rollup_address_long; history_mode; network]
+
+let default_index_file = "evm-%h-snapshot-%r-%l.caidx"
+
+let backup_items =
+  [
+    "store";
+    Evm_store.sqlite_file_name;
+    Evm_store.sqlite_file_name ^ "-shm";
+    Evm_store.sqlite_file_name ^ "-wal";
+    "wasm_2_0_0";
+  ]
+
+let export ?desync_path ?chunk_size ?progress ~target_store ?target_dir
+    ?(index_file = default_index_file) ~data_dir () =
+  let open Lwt_result_syntax in
+  let* () = Data_dir.lock ~data_dir in
+  let* {
+         rollup_address;
+         current_number = current_level;
+         legacy_block_storage = _;
+         history_mode;
+         first_number = first_level;
+       } =
+    Data_dir.store_info ~data_dir
+  in
+  let metadata =
+    V1 {rollup_address; current_level; history_mode; first_level}
+  in
+  let index_file =
+    match target_dir with
+    | None -> index_file
+    | Some target_dir -> Filename.concat target_dir index_file
+  in
+  let*? index_file =
+    interpolate_snapshot_file
+      current_level
+      rollup_address
+      history_mode
+      index_file
+  in
+  let*! () = Events.exporting_snapshot index_file in
+  let* () =
+    Desync_snapshots.export
+      ?desync_path
+      ?chunk_size
+      ?progress
+      ~metadata
+      ~metadata_encoding
+      ~target_store
+      ~index_file
+      ~backup_items
+      data_dir
+  in
+  let*! () = Events.finished_exporting_snapshot index_file in
+  return index_file
+
+let info ?desync_path ~source_store ?index_dir ~index_file () =
+  let open Lwt_result_syntax in
+  let index_file =
+    match index_dir with
+    | None -> index_file
+    | Some index_dir -> Filename.concat index_dir index_file
+  in
+  let* index_info = Desync_snapshots.info ?desync_path index_file in
+  let* metadata =
+    Desync_snapshots.read_metadata
+      ?desync_path
+      ~source_store
+      ~index_file
+      metadata_encoding
+  in
+  let size = Ezjsonm.find index_info ["size"] |> Ezjsonm.get_int in
+  return (metadata, size)
+
+let check_metadata ~populated ~data_dir metadata : unit tzresult Lwt.t =
+  let open Lwt_result_syntax in
+  let open Ethereum_types in
+  let metadata_rollup_address, Qty metadata_current_level, metadata_history =
+    match metadata with
+    | V1 {rollup_address; current_level; history_mode; first_level} ->
+        (rollup_address, current_level, Some (history_mode, first_level))
+  in
+  when_ populated @@ fun () ->
+  let* store = Evm_store.init ~data_dir ~perm:(Read_only {pool_size = 1}) () in
+  Evm_store.use store @@ fun conn ->
+  let* metadata = Evm_store.Metadata.find conn in
+  let* () =
+    match metadata with
+    | None -> return_unit
+    | Some {smart_rollup_address = r; history_mode = _} ->
+        fail_unless
+          (metadata_rollup_address = r)
+          (Incorrect_rollup (metadata_rollup_address, r))
+  in
+  let* () =
+    match (metadata_history, metadata) with
+    | None, _ | _, None -> return_unit
+    | Some (metadata_hist, _), Some {history_mode; _}
+      when metadata_hist <> history_mode ->
+        failwith
+          "Cannot import %a snapshot into %a EVM node."
+          Configuration.pp_history_mode_debug
+          metadata_hist
+          Configuration.pp_history_mode_debug
+          history_mode
+    | _ -> (* Same history mode *) return_unit
+  in
+  let* first_context = Evm_store.Context_hashes.find_earliest conn in
+  let* () =
+    match (first_context, metadata_history) with
+    | None, _ | _, None -> return_unit
+    | Some (Qty first_number, _), Some (Archive, Qty metadata_first_level)
+      when Z.Compare.(metadata_first_level > first_number) ->
+        failwith
+          "Snapshot history starts at %a but the archive node has history from \
+           %a locally. Backup configuration and start from an empty data dir \
+           if you want to proceed."
+          Z.pp_print
+          metadata_first_level
+          Z.pp_print
+          first_number
+    | _ -> return_unit
+  in
+  let* latest_context = Evm_store.Context_hashes.find_latest conn in
+  let* () =
+    match latest_context with
+    | None -> return_unit
+    | Some (Qty current_number, _) ->
+        fail_when
+          Z.Compare.(metadata_current_level <= current_number)
+          (Outdated_snapshot (metadata_current_level, current_number))
+  in
+  return_unit
+
+let import ?desync_path ~force ?history_mode ~source_store ?index_dir
+    ~index_file ~data_dir () =
+  let open Lwt_result_syntax in
+  let open Filename.Infix in
+  Data_dir.use ~data_dir @@ fun () ->
+  let index_file =
+    match index_dir with
+    | None -> index_file
+    | Some index_dir -> Filename.concat index_dir index_file
+  in
+  let*! () = Events.importing_snapshot index_file in
+  Lwt_utils_unix.with_tempdir ~temp_dir:data_dir ".octez_evm_node_import_"
+  @@ fun dest ->
+  let* metadata, _size =
+    info ?desync_path ~source_store ?index_dir ~index_file ()
+  in
+  let* store_history_mode =
+    match (history_mode, metadata) with
+    | Some h1, V1 {history_mode = h2; _} ->
+        if Configuration.history_mode_partial_eq h1 h2 then return_some h1
+        else tzfail (History_mode_mismatch (h1, h2))
+    | _ -> return_none
+  in
+  let*! populated = Data_dir.populated ~data_dir in
+  let*? () =
+    error_when ((not force) && populated) (Data_dir_populated data_dir)
+  in
+  let*! () = Lwt_utils_unix.create_dir data_dir in
+  let* () = Data_dir.lock ~data_dir in
+  let* () =
+    (* This is safe because we took the lock. This cannot be moved before
+       `Data_dir.lock`. *)
+    when_ populated @@ fun () ->
+    Format.printf "Delete previous contents from the data-dir\n%!" ;
+    let*! () = Lwt_utils_unix.remove_dir (Data_dir.store_path ~data_dir) in
+    return_unit
+  in
+  let* () = check_metadata ~populated ~data_dir metadata in
+  let* _metadata =
+    Desync_snapshots.import
+      ?desync_path
+      ~metadata_encoding
+      ~source_store
+      ~index_file
+      dest
+  in
+  let*! () =
+    List.iter_s
+      (fun item ->
+        let orig = data_dir // item in
+        let dest = dest // item in
+        let*! to_rename = Lwt_unix.file_exists dest in
+        if not to_rename then Lwt.return_unit
+        else
+          let*! () =
+            match Sys.is_directory orig with
+            | false -> Lwt_unix.unlink orig
+            | true -> Lwt_utils_unix.remove_dir orig
+            | exception Sys_error _ -> Lwt.return_unit
+          in
+          Lwt_unix.rename dest orig)
+      backup_items
+  in
+  let*! () = Events.import_finished () in
+  return store_history_mode
--- etherlink/bin_node/lib_dev/snapshots.mli
+++ etherlink/bin_node/lib_dev/snapshots.mli
@@ -15,6 +15,14 @@ type error +=
   | Incorrect_rollup of Address.t * Address.t
   | Outdated_snapshot of Z.t * Z.t
 
+type metadata =
+  | V1 of {
+      rollup_address : Address.t;
+      current_level : Ethereum_types.quantity;
+      history_mode : Configuration.history_mode;
+      first_level : Ethereum_types.quantity;
+    }
+
 (** [interpolate_snapshot_file block_number rollup_address history_mode path]
     replaces placeholders in the given snapshot [path] with the provided values.
     The supported placeholders are:
@@ -56,3 +64,74 @@ val interpolate_snapshot_provider :
   Configuration.history_mode ->
   string ->
   string tzresult
+
+(** [export ?desync_path ?chunk_size ?progress ~target_store ?target_dir
+    ?index_file ~data_dir ()] exports the data from an EVM node data directory
+    to a snapshot index file and snapshot data in the [target_store] for
+    deduplication.
+
+    @param desync_path Path to the desync tool executable. If not provided,
+      it will be looked for in the system's PATH.
+    @param chunk_size Specifies the chunking strategy for the desync tool.
+    @param progress Displays a progress bar if true.
+    @param target_store The path to the desync store where snapshot data will
+      be stored.
+    @param target_dir The directory where the snapshot index file will be
+      created.
+    @param index_file The name of the snapshot index file.
+    @param data_dir The path to the EVM node data directory.
+*)
+val export :
+  ?desync_path:string ->
+  ?chunk_size:Desync_snapshots.chunk_size ->
+  ?progress:bool ->
+  target_store:string ->
+  ?target_dir:string ->
+  ?index_file:string ->
+  data_dir:string ->
+  unit ->
+  string tzresult Lwt.t
+
+(** [info ?desync_path ~source_store ?index_dir ~index_file ()]
+    returns the metadata and the size of a snapshot.
+
+    @param desync_path Path to the desync tool executable. If not provided,
+      it will be looked for in the system's PATH.
+    @param source_store The path to the desync store containing the snapshot
+      data.
+    @param index_dir The directory where the snapshot index file is located.
+    @param index_file The name of the snapshot index file.
+*)
+val info :
+  ?desync_path:string ->
+  source_store:string ->
+  ?index_dir:string ->
+  index_file:string ->
+  unit ->
+  (metadata * int) tzresult Lwt.t
+
+(** [import ?desync_path ~force ?history_mode ~source_store ?index_dir
+    ~index_file ~data_dir ()] imports a snapshot into an EVM node data
+    directory.
+
+    @param desync_path Path to the desync tool executable. If not provided,
+      it will be looked for in the system's PATH.
+    @param force If true, existing data in the data directory will be
+      overwritten.
+    @param history_mode The history mode for the imported data.
+    @param source_store The path to the desync store containing the snapshot
+      data.
+    @param index_dir The directory where the snapshot index file is located.
+    @param index_file The name of the snapshot index file.
+    @param data_dir The path to the EVM node data directory.
+*)
+val import :
+  ?desync_path:string ->
+  force:bool ->
+  ?history_mode:Configuration.history_mode ->
+  source_store:string ->
+  ?index_dir:string ->
+  index_file:string ->
+  data_dir:string ->
+  unit ->
+  Configuration.history_mode option tzresult Lwt.t
