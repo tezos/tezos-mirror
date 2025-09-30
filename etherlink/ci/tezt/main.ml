ed6076733ffce6b7feeb919d259f6ec6be7d1826
Romain <romain@nomadic-labs.com>
2025-09-30 12:05:32 +0200

    Tezt: split out Etherlink main executable

---
+++ etherlink/ci/tezt/main.ml
@@ -0,0 +1,16 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2025 Nomadic Labs. <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(* DO NOT ADD TESTS TO THIS FILE DIRECTLY
+   This file should only run [Test.run].
+
+   Add your tests to a library such as the one in [etherlink/tezt/tests],
+   and link this library with this executable.
+   Make sure the library is compiled with [-linkall] so that tests
+   are actually registered. *)
+
+let () = Test.run ()
