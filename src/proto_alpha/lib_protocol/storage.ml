ead83577a4f074a0d4800e2c3a4d436c5ac55008
Eugen Zalinescu <eugen.zalinescu@nomadic-labs.com>
2025-10-04 09:27:13 +0200

    Proto/DAL: update doc-strings to mention invariant exceptions in Storage.DAL

--- src/proto_alpha/lib_protocol/storage.ml
+++ src/proto_alpha/lib_protocol/storage.ml
@@ -2355,9 +2355,6 @@ module Dal = struct
           let name = ["slot_headers"]
         end)
         (struct
-          (* The size of the list below is at most equal to the
-             [number_of_slots] as declared in the DAL parameters of the
-             protocol. *)
           type t = (Dal_slot_repr.Header.t * Contract_repr.t) list
 
           let encoding =
@@ -2385,8 +2382,6 @@ module Dal = struct
           let name = ["slot_headers_successive_histories_of_level"]
         end)
         (struct
-          (* The size of the list below is always equal to the [number_of_slots]
-             as declared in the DAL parameters of the protocol. *)
           type t =
             (Dal_slot_repr.History.Pointer_hash.t * Dal_slot_repr.History.t)
             list
