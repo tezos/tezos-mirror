ac802563e240e0a56057ab346d5025eaa2e930dd
Pierre-Emmanuel Cornilleau <pierre-emmanuel.cornilleau@lambda-coins.com>
2025-10-08 12:43:13 +0200

    L2Node/Tezlink: add errors to pk validation

--- etherlink/bin_node/lib_dev/tezlink_prevalidation.ml
+++ etherlink/bin_node/lib_dev/tezlink_prevalidation.ml
@@ -5,6 +5,7 @@
 (* SPDX-FileCopyrightText: 2025 Functori <contact@functori.com>              *)
 (*                                                                           *)
 (*****************************************************************************)
+open Tezlink_imports
 open Tezlink_imports.Alpha_context
 
 let ( let** ) v f =
@@ -12,14 +13,46 @@ let ( let** ) v f =
   let* r = v in
   match r with Ok v -> f v | Error err -> return (Error err)
 
-let validate_manager_info ~read (Contents op : packed_contents) =
+type clue = Operation_hash.t
+
+let pp_clue = Operation_hash.pp
+
+let clue_encoding = Operation_hash.encoding
+
+let tzfail_p e = Lwt_result_syntax.tzfail @@ Imported_env.Ecoproto_error e
+
+type error += Not_a_manager_operation of clue
+
+let () =
+  register_error_kind
+    `Permanent
+    ~id:"evm_node.dev.tezlink.not_a_manager_operation"
+    ~title:
+      "Failed to prevalidate an operation: only manager operation are allowed"
+    ~description:
+      "Prevalidation of the operation failed because it was not recognized as \
+       a manager operation."
+    ~pp:(fun ppf raw ->
+      Format.fprintf
+        ppf
+        "Failed to prevaliate an operation: not a manager_operation %a"
+        pp_clue
+        raw)
+    Data_encoding.(obj1 (req "operation" clue_encoding))
+    (function Not_a_manager_operation raw -> Some raw | _ -> None)
+    (fun raw -> Not_a_manager_operation raw)
+
+let validate_manager_info ~read ~error_clue (Contents op : packed_contents) =
   let open Lwt_result_syntax in
   match op with
   | Manager_operation {source; operation; _} -> (
       let contract = Tezos_types.Contract.of_implicit source in
       let* manager = Tezlink_durable_storage.manager read contract in
       match (manager, operation) with
-      | Some (Public_key _), Reveal _ -> failwith "TODO already revealed"
+      | Some (Public_key _), Reveal _ ->
+          tzfail_p
+          @@ Imported_protocol.Contract_manager_storage.Previously_revealed_key
+               (Implicit source)
       | Some (Public_key pk), _op -> return (Ok (pk, source))
       | Some (Hash _), Reveal {public_key; _} ->
           (* the revealed public key might be the one we're searching for *)
@@ -27,10 +60,23 @@ let validate_manager_info ~read (Contents op : packed_contents) =
           let pkh_revealed = Public_key.hash public_key in
           if Public_key_hash.equal source pkh_revealed then
             return @@ Ok (public_key, source)
-          else failwith "TODO not revealed"
-      | Some (Hash _), _op -> failwith "TODO not revealed"
-      | None, _ -> failwith "TODO unknown source")
-  | _ -> failwith "TODO not a manager operation"
+          else
+            tzfail_p
+            @@ Imported_protocol.Contract_manager_storage.(
+                 Inconsistent_hash
+                   {
+                     public_key;
+                     expected_hash = source;
+                     provided_hash = pkh_revealed;
+                   })
+      | Some (Hash _), _op ->
+          tzfail_p
+          @@ Imported_protocol.Contract_manager_storage.Unrevealed_manager_key
+               (Implicit source)
+      | None, _ ->
+          tzfail_p
+          @@ Imported_protocol.Contract_storage.Empty_implicit_contract source)
+  | _ -> tzfail @@ Not_a_manager_operation error_clue
 
 (** Information required to validate an operation. Some will change while folding
    on the batch. We'll need :
@@ -67,15 +113,16 @@ let rec validate_batch ~(ctxt : batch_validation_context)
 let validate_tezlink_operation ~read raw =
   let open Tezlink_imports.Alpha_context in
   let open Lwt_result_syntax in
-  ignore read ;
   (* TODO check size *)
+  let raw = Bytes.of_string raw in
+  let error_clue = Operation_hash.hash_bytes [raw] in
   (* Operation deserialization. To avoid breakage during the prevalidation
        implementation we use the `Operation.decode` helper but we'll need to
        simplify it when the validation can return all the necessary information. *)
   (* TODO: simplify decoding, at the moment it does some verification as
        a side-effect *)
   let*? (operation : Tezos_types.Operation.t) =
-    raw |> Bytes.of_string |> Tezos_types.Operation.decode
+    raw |> Tezos_types.Operation.decode
   in
   let {protocol_data = Operation_data {contents; signature}; _} =
     operation.op
@@ -86,7 +133,7 @@ let validate_tezlink_operation ~read raw =
     | Cons (first_operation, rest) ->
         (Contents first_operation, Operation.to_list (Contents_list rest))
   in
-  let** pk, source = validate_manager_info ~read first in
+  let** pk, source = validate_manager_info ~error_clue ~read first in
   ignore source ;
   (* TODO build validation context *)
   let** _ = validate_batch ~ctxt:() (first :: rest) in
