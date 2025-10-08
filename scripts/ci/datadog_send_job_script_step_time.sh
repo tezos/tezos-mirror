847db1369f71f332038ce4559883d169089eaec6
Bruno Bernardo <bruno@nomadic-labs.com>
2025-10-08 11:36:26 +0200

    CI,Scripts: use [measure] instead of [tag] for [script_step_time] in [datadog-ci]
    According to the doc [0], [measure] should be used for numerical tags.
    
    [0] https://docs.datadoghq.com/continuous_integration/pipelines/custom_tags_and_measures/#add-measures-to-pipeline-traces

--- scripts/ci/datadog_send_job_script_step_time.sh
+++ scripts/ci/datadog_send_job_script_step_time.sh
@@ -8,7 +8,7 @@ echo "SCRIPT_STEP_TIME=$SCRIPT_STEP_TIME"
 
 if command -v datadog-ci > /dev/null 2>&1; then
   echo "Sending job-level info to Datadog"
-  DATADOG_SITE=datadoghq.eu datadog-ci tag --level job --tags script_step_time:"$SCRIPT_STEP_TIME"
+  DATADOG_SITE=datadoghq.eu datadog-ci measure --level job --measures script_step_time:"$SCRIPT_STEP_TIME"
 else
   echo "'datadog-ci' not installed, no job info sent to Datadog"
 fi
