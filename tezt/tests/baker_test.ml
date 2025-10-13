694fa2f424260d309c8eda100de3fcd3b80240a2
Adam Allombert-Goget <adam.allombert@nomadic-labs.com>
2025-10-13 17:36:35 +0200

    tezt/baker_test: add a test for stream refresh on timeout

--- tezt/tests/baker_test.ml
+++ tezt/tests/baker_test.ml
@@ -1311,6 +1311,67 @@ let unable_to_reach_node_mempool =
         "The baker failed to shut down after being unable to reach the node \
          mempool for 2 minutes"
 
+let stream_is_refreshed_on_timeout =
+  Protocol.register_test
+    ~__FILE__
+    ~title:"Operation stream is refreshed on timeout"
+    ~tags:[team; "operation_worker"; "timeout"]
+    ~uses:(fun _protocol -> [Constant.octez_agnostic_baker])
+    ~supports:Protocol.(From_protocol 023)
+  @@ fun protocol ->
+  log_step 1 "Initialize a node with mainnet constants" ;
+  let* parameter_file =
+    Protocol.write_parameter_file
+      ~base:(Either.Right (protocol, Some Constants_mainnet))
+      []
+  in
+  let* node, client =
+    Client.init_with_protocol
+      `Client
+      ~protocol
+      ~parameter_file
+      ~timestamp:Now
+      ()
+  in
+  log_step 2 "Run a baker" ;
+  let* baker =
+    Agnostic_baker.init
+      ~delegates:Constant.[activator.public_key_hash]
+      node
+      client
+  in
+  log_step 3 "Bake 2 levels" ;
+  let* () =
+    Client.bake_for_and_wait ~keys:[] ~minimal_timestamp:true ~count:2 client
+  in
+  (* Since the node will neither switch heads nor forward new operations, the
+     operation worker is expected to refresh the monitor_operations stream after
+     the equivalent of two rounds of inactivity. *)
+  log_step 4 "Wait for stream_timeout event" ;
+  let wait_for_stream_timeout () =
+    Agnostic_baker.wait_for
+      baker
+      "monitor_operations_stream_timeout.v0"
+      (fun _ -> Some ())
+  in
+  let* waiter =
+    Lwt.choose
+      [
+        (let* () = wait_for_stream_timeout () in
+         return `Stream_timeout);
+        (* Saves some CI time by failing after 2 minutes if the baker fails to
+           refresh the stream as expected.*)
+        (let* () = Lwt_unix.sleep 120. in
+         return `Timeout);
+      ]
+  in
+  match waiter with
+  | `Stream_timeout -> unit
+  | `Timeout ->
+      Test.fail
+        "The baker did not refresh the streamed RPC after prolonged inactivity \
+         from the mempool"
+
 let register ~protocols =
   check_node_version_check_bypass_test protocols ;
   check_node_version_allowed_test protocols ;
@@ -1329,4 +1390,5 @@ let register ~protocols =
   attestations_aggregation_on_reproposal_local_context protocols ;
   attestations_aggregation_on_reproposal_remote_node protocols ;
   aggregated_operations_retrival_from_block_content protocols ;
-  unable_to_reach_node_mempool protocols
+  unable_to_reach_node_mempool protocols ;
+  stream_is_refreshed_on_timeout protocols
