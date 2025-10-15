44db8892f7cece14ae7b091bc00df2808e638640
Dibassi Brahima <brahima.dibassi@lambda-coins.com>
2025-10-15 17:52:23 +0200

    Tezlink/Kernel/Gas : Add Tezlink operation gas start from non default value

--- etherlink/kernel_latest/tezos_execution/src/gas.rs
+++ etherlink/kernel_latest/tezos_execution/src/gas.rs
@@ -10,13 +10,49 @@
 /// Every Gas Related Function or Constant should be defined here.
 use mir::gas;
 use tezos_data_encoding::types::Narith;
+use thiserror::Error;
 
 pub struct TezlinkOperationGas {
     milligas_limit: u32,
     pub current_gas: gas::Gas,
 }
 
+#[derive(Debug, Error)]
+pub enum TezlinkGasError {
+    #[error(
+        "The gas limit provided is too high the limit is {0} gas units but {1} gas units were given"
+    )]
+    GasLimitTooHigh(u32, num_bigint::BigUint),
+    #[error("Failed gas limit conversion: {0}")]
+    CannotConvertToU32(num_bigint::TryFromBigIntError<num_bigint::BigUint>),
+}
+
 impl TezlinkOperationGas {
+    /// This corresponds to the default value of the `hard_gas_limit_per_operation` parametric constant.
+    /// Currently can be found in Tezos source code at: src/proto_023_PtSeouLo/lib_parameters/default_parameter.ml
+    pub const MAX_GAS_UNIT_AMOUNT: u32 = 1_040_000;
+
+    pub fn start(
+        operation_gas_limit: &tezos_data_encoding::types::Narith,
+    ) -> Result<Self, TezlinkGasError> {
+        if operation_gas_limit.0 > num_bigint::BigUint::from(Self::MAX_GAS_UNIT_AMOUNT) {
+            return Err(TezlinkGasError::GasLimitTooHigh(
+                Self::MAX_GAS_UNIT_AMOUNT,
+                operation_gas_limit.0.clone(),
+            ));
+        }
+
+        let operation_miligas_limit = &operation_gas_limit.0 * 1000u32;
+        // Should never fail because of the previous check
+        let milligas_limit = operation_miligas_limit
+            .try_into()
+            .map_err(TezlinkGasError::CannotConvertToU32)?;
+        Ok(Self {
+            milligas_limit,
+            current_gas: gas::Gas::new(milligas_limit),
+        })
+    }
+
     fn start_internal(&mut self) {
         self.milligas_limit = self.current_gas.milligas();
     }
@@ -63,12 +99,11 @@ impl TezlinkOperationGas {
 }
 
 impl Default for TezlinkOperationGas {
-    /// Constructs [Gas] with [DEFAULT_GAS_AMOUNT] gas remaining.
+    /// Constructs [Gas] with [MAX_GAS_UNIT_AMOUNT] gas remaining.
     fn default() -> Self {
-        let current_gas = gas::Gas::default();
         Self {
-            milligas_limit: current_gas.milligas(),
-            current_gas,
+            milligas_limit: Self::MAX_GAS_UNIT_AMOUNT * 1000,
+            current_gas: gas::Gas::new(Self::MAX_GAS_UNIT_AMOUNT * 1000),
         }
     }
 }
