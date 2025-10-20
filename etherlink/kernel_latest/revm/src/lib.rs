b57d26f12abc470ba9fd88f7ce2e859a259500ed
Rodi-Can Bozman <rodi.bozman@functori.com>
2025-10-20 14:30:43 +0200

    Etherlink/Revm: fix simulation with traceCalls

--- etherlink/kernel_latest/revm/src/lib.rs
+++ etherlink/kernel_latest/revm/src/lib.rs
@@ -203,6 +203,7 @@ type EvmContext<'a, Host> = Evm<
     EthFrame<EthInterpreter>,
 >;
 
+#[allow(clippy::too_many_arguments)]
 #[instrument(skip_all)]
 fn evm_inspect<'a, Host: Runtime>(
     db: EtherlinkVMDB<'a, Host>,
@@ -212,8 +213,10 @@ fn evm_inspect<'a, Host: Runtime>(
     chain_id: u64,
     spec_id: SpecId,
     inspector: EtherlinkInspector,
+    is_simulation: bool,
 ) -> EvmInspection<'a, Host> {
-    let cfg = CfgEnv::new().with_chain_id(chain_id).with_spec(spec_id);
+    let mut cfg = CfgEnv::new().with_chain_id(chain_id).with_spec(spec_id);
+    cfg.disable_eip3607 = is_simulation;
 
     Context::<
         BlockEnv,
@@ -301,6 +304,7 @@ pub fn run_transaction<'a, Host: Runtime>(
             block_constants.chain_id.as_u64(),
             spec_id,
             inspector,
+            is_simulation,
         );
 
         let result = evm.inspect_tx_commit(&tx)?;
