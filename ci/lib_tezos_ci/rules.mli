3f1dd5e922deca11400ccdb9e39202bdbb1dc5a7
Bruno Bernardo <bruno@nomadic-labs.com>
2025-10-21 18:12:42 +0200

    CI: add [debian.daily] scheduled pipeline

--- ci/lib_tezos_ci/rules.mli
+++ ci/lib_tezos_ci/rules.mli
@@ -60,6 +60,12 @@ val merge_train : If.t
     [TZ_SCHEDULE_KIND] set to [EXTENDED_TESTS]. *)
 val schedule_extended_tests : If.t
 
+(** A rule that is true for daily debian test pipelines.
+
+    Such pipelines have [CI_PIPELINE_SOURCE] set to [scheduled] and
+    [TZ_SCHEDULE_KIND] set to [debian.daily]. *)
+val debian_daily : If.t
+
 (** A rule that is true for scheduled extended RPC test pipelines.
 
     Such pipelines have [CI_PIPELINE_SOURCE] set to [scheduled] and
