a4291f5625ec15c2be50125f4e21d02f21d6e53f
Ole Krüger <ole.kruger@trili.tech>
2025-10-26 18:55:01 +0000

    Nix: Use official formatter

--- contrib/sdk-bindings/shell.nix
+++ contrib/sdk-bindings/shell.nix
@@ -1,48 +1,53 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import nixpkgs {
+    config = { };
+    overlays = [ ];
+  };
 in
 
-pkgs.mkShell.override {
-  inherit (pkgs.swift) stdenv;
-} {
-  buildInputs = [
-    # Rust
-    pkgs.rustup
+pkgs.mkShell.override
+  {
+    inherit (pkgs.swift) stdenv;
+  }
+  {
+    buildInputs = [
+      # Rust
+      pkgs.rustup
 
-    # Swift
-    pkgs.swift
-    pkgs.swiftpm
-    pkgs.swift-format
-    pkgs.swiftPackages.Foundation
-    pkgs.swiftPackages.XCTest
+      # Swift
+      pkgs.swift
+      pkgs.swiftpm
+      pkgs.swift-format
+      pkgs.swiftPackages.Foundation
+      pkgs.swiftPackages.XCTest
 
-    # Python
-    pkgs.python39
-    pkgs.python39Packages.pytest
-    pkgs.yapf
-    pkgs.maturin
+      # Python
+      pkgs.python39
+      pkgs.python39Packages.pytest
+      pkgs.yapf
+      pkgs.maturin
 
-    # Kotlin
-    pkgs.kotlin
-    pkgs.ktlint
-    pkgs.gradle
-  ];
+      # Kotlin
+      pkgs.kotlin
+      pkgs.ktlint
+      pkgs.gradle
+    ];
 
-  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-    pkgs.swiftPackages.Dispatch
-    pkgs.swiftPackages.Foundation
-    pkgs.swiftPackages.XCTest
-  ];
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.swiftPackages.Dispatch
+      pkgs.swiftPackages.Foundation
+      pkgs.swiftPackages.XCTest
+    ];
 
-  # Maturin requires a python environment to install
-  shellHook = ''
-    if [ ! -d .venv ]; then
-      python -m venv .venv
-      source .venv/bin/activate
-      pip install pytest
-    else
-      source .venv/bin/activate
-    fi
-  '';
-}
+    # Maturin requires a python environment to install
+    shellHook = ''
+      if [ ! -d .venv ]; then
+        python -m venv .venv
+        source .venv/bin/activate
+        pip install pytest
+      else
+        source .venv/bin/activate
+      fi
+    '';
+  }
