4cd04571263e67f29017f71be7ab236be6825326
Romain <romain@nomadic-labs.com>
2025-12-03 18:08:08 +0100

    CIAO: share code for oc.unit ~needs_legacy

--- ci/lib_tezos_ci_jobs/misc.ml
+++ ci/lib_tezos_ci_jobs/misc.ml
@@ -84,6 +84,16 @@ let job_script_test_gen_genesis =
 let dune_cache_pull_from_pipeline =
   Cacio.dune_cache ~key:Pipeline ~policy:Pull ()
 
+(* Add these jobs to the [needs_legacy] of jobs that use [dune_cache_pull_from_pipeline]
+   so that the cache is actually available. *)
+let needs_cache_from_x86_64_build_jobs =
+  [
+    (* We don't need the artifacts, but we do want the cache from the build jobs. *)
+    (Cacio.Job, Code_verification.job_build_x86_64_release Before_merging);
+    (Job, Code_verification.job_build_x86_64_exp Before_merging);
+    (Job, Code_verification.job_build_x86_64_extra_dev Before_merging);
+  ]
+
 let job_script_snapshot_alpha_and_link =
   CI.job
     "oc.script:snapshot_alpha_and_link"
@@ -278,13 +288,7 @@ let job_oc_unit_non_proto_x86_64 =
     ~image:Tezos_ci.Images.CI.test
       (* use the test image because [lib_benchmark] require Python *)
     ~arch:Amd64
-    ~needs_legacy:
-      [
-        (* We don't need the artifacts, but we do want the cache from the build jobs. *)
-        (Job, Code_verification.job_build_x86_64_release Before_merging);
-        (Job, Code_verification.job_build_x86_64_exp Before_merging);
-        (Job, Code_verification.job_build_x86_64_extra_dev Before_merging);
-      ]
+    ~needs_legacy:needs_cache_from_x86_64_build_jobs
     ~variables:[("DUNE_ARGS", "-j 12")]
     ~artifacts:(artifacts_test_results_xml Amd64)
     ~dune_cache:dune_cache_pull_from_pipeline
@@ -342,13 +346,7 @@ let job_oc_unit_proto_x86_64 =
     ~image:Tezos_ci.Images.CI.build
     ~arch:Amd64
     ~cpu:Very_high
-    ~needs_legacy:
-      [
-        (* We don't need the artifacts, but we do want the cache from the build jobs. *)
-        (Job, Code_verification.job_build_x86_64_release Before_merging);
-        (Job, Code_verification.job_build_x86_64_exp Before_merging);
-        (Job, Code_verification.job_build_x86_64_extra_dev Before_merging);
-      ]
+    ~needs_legacy:needs_cache_from_x86_64_build_jobs
     ~variables:[("DUNE_ARGS", "-j 12")]
     ~artifacts:(artifacts_test_results_xml Amd64)
     ~dune_cache:dune_cache_pull_from_pipeline
@@ -368,13 +366,7 @@ let job_oc_unit_other_x86_64 =
     ~image:Tezos_ci.Images.CI.build
     ~arch:Amd64
     ~cpu:High
-    ~needs_legacy:
-      [
-        (* We don't need the artifacts, but we do want the cache from the build jobs. *)
-        (Job, Code_verification.job_build_x86_64_release Before_merging);
-        (Job, Code_verification.job_build_x86_64_exp Before_merging);
-        (Job, Code_verification.job_build_x86_64_extra_dev Before_merging);
-      ]
+    ~needs_legacy:needs_cache_from_x86_64_build_jobs
     ~variables:[("DUNE_ARGS", "-j 12")]
     ~artifacts:(artifacts_test_results_xml Amd64)
     ~dune_cache:dune_cache_pull_from_pipeline
