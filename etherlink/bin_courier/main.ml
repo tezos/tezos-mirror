c30adb169822be0585a63f0d4daf88112ed0cba2
Luciano Freitas <luciano.freitas@nomadic-labs.com>
2025-12-11 10:40:47 +0100

    Bin_courier: use Etherlink's Tx_queue

--- etherlink/bin_courier/main.ml
+++ etherlink/bin_courier/main.ml
@@ -135,9 +135,8 @@ module Constant = struct
     Efunc_core.Types.a "ff00000000000000000000000000000000000001"
 end
 
-let start_blueprint_follower ~relay_endpoint =
+let start_blueprint_follower ~container ~relay_endpoint ~rpc_endpoint =
   let open Lwt_result_syntax in
-  let open Floodgate_lib in
   let* next_blueprint_number =
     Batch.call
       (module Rpc_encodings.Block_number)
@@ -149,9 +148,13 @@ let start_blueprint_follower ~relay_endpoint =
   let* time_between_blocks =
     Evm_services.get_time_between_blocks
       ~fallback:(Time_between_blocks 10.)
-      ~evm_node_endpoint:relay_endpoint
-      ~timeout:10.
+      ~evm_node_endpoint:rpc_endpoint
+      ~timeout:Network_info.timeout
       ()
+  in
+  let (Evm_node_lib_dev.Services_backend_sig.Evm_tx_container
+         (module Tx_container)) =
+    container
   in
   Blueprints_follower.start
     ~multichain:false
@@ -164,7 +167,10 @@ let start_blueprint_follower ~relay_endpoint =
       let*! () = Floodgate_events.received_blueprint number in
       let* () =
         match Blueprint_decoder.transaction_hashes blueprint with
-        | Ok hashes -> List.iter_es Tx_queue.confirm hashes
+        | Ok hashes ->
+            Tx_container.confirm_transactions
+              ~clear_pending_queue_after:false
+              ~confirmed_txs:(List.to_seq hashes)
         | Error _ -> return_unit
       in
       return (`Continue Blueprints_follower.{sbl_callbacks_activated = false}))
@@ -179,29 +185,18 @@ let withdraw_data receiver =
   let receiver = Tezos_crypto.Signature.Public_key_hash.to_b58check receiver in
   Efunc_core.Evm.encode ~name:"withdraw_base58" [`string] [`string receiver]
 
-let withdraw ~endpoint ~infos value from receiver =
+let withdraw ~container ~endpoint ~infos value from receiver =
   let open Lwt_result_syntax in
+  let (Evm_node_lib_dev.Services_backend_sig.Evm_tx_container
+         (module Tx_container)) =
+    container
+  in
   let result, rwaker = Lwt.task () in
-  let hash, hwaker = Lwt.task () in
-  let*! () =
-    Tx_queue.transfer
-      ~callback:
-        (let open Lwt_syntax in
-         function
-         | `Confirmed ->
-             let* () = Events.confirmed_withdrawal () in
-             Lwt.wakeup rwaker (Ok ()) ;
-             return_unit
-         | `Refused ->
-             Lwt.wakeup rwaker (error_with "Withdrawal refused") ;
-             return_unit
-         | `Dropped ->
-             Lwt.wakeup rwaker (error_with "Withdrawal dropped") ;
-             return_unit
-         | `Accepted hash ->
-             Lwt.wakeup hwaker hash ;
-             Events.accepted_transaction hash)
-      ~gas_limit:(Z.of_int 16_150_912)
+  let gas_limit = Z.of_int 16_150_912 in
+  let fees = Z.(gas_limit * infos.Network_info.base_fee_per_gas) in
+  let* raw_tx, transaction_object =
+    Craft.transfer_with_obj_exn
+      ~gas_limit
       ~infos
       ~from
       ~to_:Constant.withdrawal_contract
@@ -209,15 +204,43 @@ let withdraw ~endpoint ~infos value from receiver =
       ~data:(withdraw_data receiver)
       ()
   in
+  let txn_hash = Transaction_object.hash transaction_object in
+  let callback reason =
+    match reason with
+    | `Confirmed ->
+        let*! () = Events.confirmed_withdrawal () in
+        Lwt.wakeup rwaker (Ok ()) ;
+        Account.debit from Z.(value + fees) ;
+        Account.increment_nonce from ;
+        Lwt.return_unit
+    | `Refused ->
+        Lwt.wakeup rwaker (error_with "Withdrawal refused") ;
+        Lwt.return_unit
+    | `Dropped ->
+        Lwt.wakeup rwaker (error_with "Withdrawal dropped") ;
+        Lwt.return_unit
+    | `Accepted ->
+        let*! () = Events.accepted_transaction txn_hash in
+        Lwt.return_unit
+  in
+  let next_nonce = Ethereum_types.quantity_of_z from.nonce in
+  let* add_res =
+    Tx_container.add ~callback ~next_nonce transaction_object ~raw_tx
+  in
+  let* () =
+    match add_res with
+    | Ok (_ : Ethereum_types.hash) -> return_unit
+    | Error e ->
+        Lwt.fail_with @@ Format.asprintf "Error adding to the tx_queue: %s" e
+  in
   let* () = result in
-  let*! hash in
   let* receipt_rpc_result =
     Batch.call
       (module Rpc_encodings.Get_transaction_receipt)
       ~keep_alive:true
       ~timeout:10.
       ~evm_node_endpoint:endpoint
-      hash
+      txn_hash
   in
   match receipt_rpc_result with
   | None -> failwith "Could not find the receipt"
@@ -252,18 +275,37 @@ let command =
     @@ stop)
     (fun network amount signer receiver _ ->
       let open Lwt_result_syntax in
-      let* () =
-        Tx_queue.start
-          ~relay_endpoint:(Network.relay network)
-          ~max_transaction_batch_length:None
-          ~inclusion_timeout:5.
-          ()
+      let start_container, container =
+        Evm_node_lib_dev.Tx_queue.tx_container ~chain_family:EVM
       in
-
+      let (Evm_node_lib_dev.Services_backend_sig.Evm_tx_container
+             (module Tx_container)) =
+        container
+      in
+      let max_size = 999_999 in
+      let tx_per_addr_limit = Int64.of_int 999_999 in
+      let max_transaction_batch_length = Some 300 in
+      let max_lifespan_s = 2 in
+      let config : Evm_node_config.Configuration.tx_queue =
+        {
+          max_size;
+          max_transaction_batch_length;
+          max_lifespan_s;
+          tx_per_addr_limit;
+        }
+      in
+      let* () = start_container ~config ~keep_alive:true ~timeout:10. () in
       let _ =
-        start_blueprint_follower ~relay_endpoint:(Network.relay network)
+        start_blueprint_follower
+          ~container
+          ~relay_endpoint:(Network.relay network)
+          ~rpc_endpoint:(Network.endpoint network)
       in
-      let _ = Tx_queue.beacon ~tick_interval:0.1 in
+      let _ =
+        Tx_container.tx_queue_beacon
+          ~evm_node_endpoint:(Rpc (Network.endpoint network))
+          ~tick_interval:0.1
+      in
       let* infos =
         Floodgate_lib.Network_info.fetch
           ~rpc_endpoint:(Network.endpoint network)
@@ -272,7 +314,13 @@ let command =
       let* from =
         Account.from_signer ~evm_node_endpoint:(Network.endpoint network) signer
       in
-      withdraw ~endpoint:(Network.endpoint network) ~infos amount from receiver)
+      withdraw
+        ~container
+        ~endpoint:(Network.endpoint network)
+        ~infos
+        amount
+        from
+        receiver)
 
 let global_options = Tezos_clic.no_options
 
