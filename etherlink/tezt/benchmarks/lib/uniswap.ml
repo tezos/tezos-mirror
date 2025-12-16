b2a396b93a6f04bd1498c4c63a916efaa16b1091
Alain Mebsout <alain.mebsout@functori.com>
2025-12-16 10:04:51 +0100

    Etherlink/benchmark: fix issue introduced by new tx queue

--- etherlink/tezt/benchmarks/lib/uniswap.ml
+++ etherlink/tezt/benchmarks/lib/uniswap.ml
@@ -400,9 +400,9 @@ let setup ~accounts ~nb_tokens ~nb_hops ~sequencer ~rpc_node =
     container
   in
   let max_size = 999_999 in
-  let tx_per_addr_limit = Int64.of_int 999_999 in
-  let max_transaction_batch_length = Some 300 in
-  let max_lifespan_s = 2 in
+  let tx_per_addr_limit = Int64.max_int in
+  let max_transaction_batch_length = None in
+  let max_lifespan_s = int_of_float parameters.timeout in
   let config : Evm_node_config.Configuration.tx_queue =
     {max_size; max_transaction_batch_length; max_lifespan_s; tx_per_addr_limit}
   in
--- etherlink/tezt/benchmarks/snailtracer.ml
+++ etherlink/tezt/benchmarks/snailtracer.ml
@@ -253,7 +253,7 @@ let test_snailtracer () =
   let max_size = 999_999 in
   let tx_per_addr_limit = Int64.of_int 999_999 in
   let max_transaction_batch_length = Some 300 in
-  let max_lifespan_s = 2 in
+  let max_lifespan_s = int_of_float parameters.timeout in
   let config : Evm_node_config.Configuration.tx_queue =
     {max_size; max_transaction_batch_length; max_lifespan_s; tx_per_addr_limit}
   in
@@ -355,7 +355,7 @@ let test_full_image_raytracing () =
   let max_size = 999_999 in
   let tx_per_addr_limit = Int64.of_int 999_999 in
   let max_transaction_batch_length = Some 300 in
-  let max_lifespan_s = 2 in
+  let max_lifespan_s = int_of_float parameters.timeout in
   let config : Evm_node_config.Configuration.tx_queue =
     {max_size; max_transaction_batch_length; max_lifespan_s; tx_per_addr_limit}
   in
