1bafafd9cb58363dea24b948cf82c16a30d66633
Bruno Bernardo <bruno@nomadic-labs.com>
2026-01-12 09:41:10 +0100

    CI/Packaging: allow failure of [oc.upgrade_bin_debian_bookworm-systemd]
    Has been flaky, which is blocking/slowing down the landing of MRs.
    
    We don't modify the same jobs for ubuntu (jammy, noble) as they are only present in daily pipelines.
    
    We also do not touch the newly added [oc.upgrade_bin_debian_bookworm-systemd_no_restart] job as it seems reliable.

--- ci/lib_tezos_ci_jobs/debian_repository.ml
+++ ci/lib_tezos_ci_jobs/debian_repository.ml
@@ -523,6 +523,7 @@ let jobs ?(limit_dune_build_jobs = false) pipeline_type =
                ("DISTRIBUTION", "debian");
                ("RELEASE", "bookworm");
              ])
+        ~allow_failure:Yes
         [
           "./scripts/ci/systemd-packages-test.sh \
            scripts/packaging/tests/deb/upgrade-systemd-test.sh \
