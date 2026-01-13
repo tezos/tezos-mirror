fc5095dbf287cf8ecea1c7e75778078880b74e3d
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2026-01-13 11:07:23 +0100

    Packages/CI: drop no restart test
    It's the default behavior, we dropped the `--restart-on-upgrade` from the debian
    packages, we don't need this test.

--- ci/lib_tezos_ci_jobs/debian_repository.ml
+++ ci/lib_tezos_ci_jobs/debian_repository.ml
@@ -371,24 +371,6 @@ let jobs ?(limit_dune_build_jobs = false) ?(manual = false) pipeline_type =
         ];
       job_install_systemd_bin
         ~__POS__
-        ~name:"oc.upgrade_bin_ubuntu_jammy_systemd_test_no_restart"
-        ~dependencies:
-          (Dependent
-             [
-               Job job_docker_systemd_test_ubuntu_dependencies;
-               Job job_apt_repo_ubuntu;
-             ])
-        ~variables:
-          (Common.Packaging.make_variables
-             ~kind:"systemd-tests"
-             [("PREFIX", ""); ("DISTRIBUTION", "ubuntu"); ("RELEASE", "jammy")])
-        [
-          "./scripts/ci/systemd-packages-test.sh \
-           scripts/packaging/tests/deb/upgrade-systemd-test-no-restart.sh \
-           images/packages/debian-systemd-tests.Dockerfile";
-        ];
-      job_install_systemd_bin
-        ~__POS__
         ~name:"oc.upgrade_bin_ubuntu_noble_systemd_test"
         ~dependencies:
           (Dependent
@@ -403,24 +385,6 @@ let jobs ?(limit_dune_build_jobs = false) ?(manual = false) pipeline_type =
         [
           "./scripts/ci/systemd-packages-test.sh \
            scripts/packaging/tests/deb/upgrade-systemd-test.sh \
-           images/packages/debian-systemd-tests.Dockerfile";
-        ];
-      job_install_systemd_bin
-        ~__POS__
-        ~name:"oc.upgrade_bin_ubuntu_noble_systemd_test_no_restart"
-        ~dependencies:
-          (Dependent
-             [
-               Job job_docker_systemd_test_ubuntu_dependencies;
-               Job job_apt_repo_ubuntu;
-             ])
-        ~variables:
-          (Common.Packaging.make_variables
-             ~kind:"systemd-tests"
-             [("PREFIX", ""); ("DISTRIBUTION", "ubuntu"); ("RELEASE", "noble")])
-        [
-          "./scripts/ci/systemd-packages-test.sh \
-           scripts/packaging/tests/deb/upgrade-systemd-test-no-restart.sh \
            images/packages/debian-systemd-tests.Dockerfile";
         ];
     ]
@@ -482,28 +446,6 @@ let jobs ?(limit_dune_build_jobs = false) ?(manual = false) pipeline_type =
         [
           "./scripts/ci/systemd-packages-test.sh \
            scripts/packaging/tests/deb/upgrade-systemd-test.sh \
-           images/packages/debian-systemd-tests.Dockerfile";
-        ];
-      job_install_systemd_bin
-        ~__POS__
-        ~name:"oc.upgrade_bin_debian_bookworm-systemd_no_restart"
-        ~dependencies:
-          (Dependent
-             [
-               Job job_docker_systemd_test_debian_dependencies;
-               Job job_apt_repo_debian;
-             ])
-        ~variables:
-          (Common.Packaging.make_variables
-             ~kind:"systemd-tests"
-             [
-               ("PREFIX", "");
-               ("DISTRIBUTION", "debian");
-               ("RELEASE", "bookworm");
-             ])
-        [
-          "./scripts/ci/systemd-packages-test.sh \
-           scripts/packaging/tests/deb/upgrade-systemd-test-no-restart.sh \
            images/packages/debian-systemd-tests.Dockerfile";
         ];
     ]
