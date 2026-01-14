4d944bd7f93917dca64109f6e6a4991c18c63967
Albin Coquereau <albin.coquereau@nomadic-labs.com>
2026-01-14 16:42:25 +0100

    lib_ml_dsa: cleaning high level interface

--- src/lib_ml_dsa/ml_dsa_44.ml
+++ src/lib_ml_dsa/ml_dsa_44.ml
@@ -5,52 +5,63 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-type key_pair = Octez_libcrux_ml_dsa.key_pair
-
 type signing_key = Octez_libcrux_ml_dsa.signing_key
 
 type verification_key = Octez_libcrux_ml_dsa.verification_key
 
-let generate_key_pair =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_generate_key_pair
-
-let key_pair_get_signing_key =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_key_pair_get_signing_key
-
-let key_pair_get_verification_key =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_key_pair_get_verification_key
-
 type signature = Octez_libcrux_ml_dsa.signature
 
-let sign signing_key message context randomness =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_sign
+open Octez_libcrux_ml_dsa
+
+let signing_key_size = 2560
+
+let verification_key_size = 1312
+
+let signature_size = 2420
+
+let key_gen_randomness_size = 32
+
+let signing_randomness_size = 32
+
+let generate_key_pair ~seed =
+  if Bytes.length seed <> key_gen_randomness_size then
+    Error
+      (Format.sprintf "wrong seed size, should be %d" key_gen_randomness_size)
+  else
+    let get_keys key_pair =
+      ( octez_libcrux_ml_dsa_44_key_pair_get_signing_key key_pair,
+        octez_libcrux_ml_dsa_44_key_pair_get_verification_key key_pair )
+    in
+    Result.map get_keys (octez_libcrux_ml_dsa_44_generate_key_pair seed)
+
+let sign ~randomness signing_key message =
+  octez_libcrux_ml_dsa_44_sign
     signing_key
     message
-    context
+    Bytes.empty (* Empty context *)
     randomness
 
-let verify verification_key message context signature =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_verify
+let verify verification_key message signature =
+  octez_libcrux_ml_dsa_44_verify
     verification_key
     message
-    context
+    Bytes.empty (* Empty context *)
     signature
 
 let verification_key_to_bytes verification_key =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_verification_key_to_bytes
-    verification_key
+  octez_libcrux_ml_dsa_44_verification_key_to_bytes verification_key
 
 let verification_key_from_bytes bytes =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_verification_key_from_bytes bytes
+  octez_libcrux_ml_dsa_44_verification_key_from_bytes bytes
 
 let signing_key_to_bytes signing_key =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_signing_key_to_bytes signing_key
+  octez_libcrux_ml_dsa_44_signing_key_to_bytes signing_key
 
 let signing_key_from_bytes bytes =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_signing_key_from_bytes bytes
+  octez_libcrux_ml_dsa_44_signing_key_from_bytes bytes
 
 let signature_to_bytes signature =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_signature_to_bytes signature
+  octez_libcrux_ml_dsa_44_signature_to_bytes signature
 
 let signature_from_bytes bytes =
-  Octez_libcrux_ml_dsa.octez_libcrux_ml_dsa_44_signature_from_bytes bytes
+  octez_libcrux_ml_dsa_44_signature_from_bytes bytes
--- src/lib_ml_dsa/ml_dsa_44.mli
+++ src/lib_ml_dsa/ml_dsa_44.mli
@@ -5,33 +5,38 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-type key_pair
-
 type signing_key
 
-type verification_key
-
-type signature
-
-val generate_key_pair : bytes -> (key_pair, string) result
-
-val key_pair_get_signing_key : key_pair -> signing_key
-
-val key_pair_get_verification_key : key_pair -> verification_key
-
-val sign : signing_key -> bytes -> bytes -> bytes -> (signature, string) result
-
-val verify :
-  verification_key -> bytes -> bytes -> signature -> (unit, string) result
-
-val verification_key_to_bytes : verification_key -> bytes
-
-val verification_key_from_bytes : bytes -> (verification_key, string) result
+val signing_key_size : int
 
 val signing_key_to_bytes : signing_key -> bytes
 
 val signing_key_from_bytes : bytes -> (signing_key, string) result
 
+type verification_key
+
+val verification_key_size : int
+
+val verification_key_to_bytes : verification_key -> bytes
+
+val verification_key_from_bytes : bytes -> (verification_key, string) result
+
+type signature
+
+val signature_size : int
+
 val signature_to_bytes : signature -> bytes
 
 val signature_from_bytes : bytes -> (signature, string) result
+
+val key_gen_randomness_size : int
+
+val generate_key_pair :
+  seed:bytes -> (signing_key * verification_key, string) result
+
+val signing_randomness_size : int
+
+val sign :
+  randomness:bytes -> signing_key -> bytes -> (signature, string) result
+
+val verify : verification_key -> bytes -> signature -> (unit, string) result
