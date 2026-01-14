b28f0ead356751e7378650cf0a4f32a70fe1eca2
Rodi-Can Bozman <rodi.bozman@functori.com>
2026-01-14 14:13:16 +0100

    Etherlink/FarfadetDEV: backport !20271
    Only backports relevant parts of !20271, so everything
    related to CI was omitted.
    
    It is a hard requirement to backport the fix for deposits.
    
    Co-authored-by: Thomas Letan <lthms@nomadic-labs.com>

--- etherlink/kernel_farfadet_dev/revm/src/precompiles/initializer.rs
+++ etherlink/kernel_farfadet_dev/revm/src/precompiles/initializer.rs
@@ -4,14 +4,13 @@
 // SPDX-License-Identifier: MIT
 
 use revm::{
-    primitives::{hex::FromHex, Address, Bytes, FixedBytes, KECCAK_EMPTY},
+    primitives::{Address, Bytes, KECCAK_EMPTY},
     state::Bytecode,
 };
 use tezos_evm_runtime::runtime::Runtime;
 
 use crate::{
     custom,
-    helpers::storage::bytes_hash,
     precompiles::constants::{
         FA_BRIDGE_SOL_ADDR, FA_BRIDGE_SOL_CONTRACT, INTERNAL_FORWARDER_SOL_CONTRACT,
         WITHDRAWAL_SOL_ADDR, WITHDRAWAL_SOL_CONTRACT,
@@ -20,43 +19,23 @@ use crate::{
     Error,
 };
 
-use super::constants::{
-    FA_BRIDGE_SOL_CODE_HASH, INTERNAL_FORWARDER_SOL_CODE_HASH, WITHDRAWAL_SOL_CODE_HASH,
-};
+use super::constants::PredeployedContract;
 
 pub fn init_precompile_bytecodes<Host: Runtime>(host: &'_ mut Host) -> Result<(), Error> {
-    init_precompile_bytecode(
-        host,
-        &Address::ZERO,
-        INTERNAL_FORWARDER_SOL_CONTRACT,
-        &INTERNAL_FORWARDER_SOL_CODE_HASH,
-    )?;
-    init_precompile_bytecode(
-        host,
-        &WITHDRAWAL_SOL_ADDR,
-        WITHDRAWAL_SOL_CONTRACT,
-        &WITHDRAWAL_SOL_CODE_HASH,
-    )?;
-    init_precompile_bytecode(
-        host,
-        &FA_BRIDGE_SOL_ADDR,
-        FA_BRIDGE_SOL_CONTRACT,
-        &FA_BRIDGE_SOL_CODE_HASH,
-    )?;
-
-    Ok(())
+    init_precompile_bytecode(host, &Address::ZERO, &INTERNAL_FORWARDER_SOL_CONTRACT)?;
+    init_precompile_bytecode(host, &WITHDRAWAL_SOL_ADDR, &WITHDRAWAL_SOL_CONTRACT)?;
+    init_precompile_bytecode(host, &FA_BRIDGE_SOL_ADDR, &FA_BRIDGE_SOL_CONTRACT)
 }
 
 fn init_precompile_bytecode<Host: Runtime>(
     host: &'_ mut Host,
     addr: &Address,
-    hex_bytes: &str,
-    code_hash: &FixedBytes<32>,
+    predeployed: &'static PredeployedContract,
 ) -> Result<(), Error> {
     let mut created_account = StorageAccount::from_address(addr)?;
     let mut account_info = created_account.info(host).map_err(custom)?;
 
-    if account_info.code_hash == *code_hash {
+    if account_info.code_hash == predeployed.code_hash {
         return Ok(());
     }
 
@@ -64,12 +43,16 @@ fn init_precompile_bytecode<Host: Runtime>(
         CodeStorage::delete(host, &account_info.code_hash)?;
     }
 
-    let code = Bytecode::new_legacy(Bytes::from_hex(hex_bytes).map_err(custom)?);
-    let code_hash = bytes_hash(code.original_byte_slice());
-    account_info.code_hash = code_hash;
+    let code = Bytecode::new_legacy(Bytes::from_static(predeployed.code));
+    account_info.code_hash = predeployed.code_hash;
     created_account
         .set_info(host, account_info)
         .map_err(custom)?;
-    CodeStorage::add(host, code.original_byte_slice(), Some(code_hash))?;
+    CodeStorage::add(
+        host,
+        code.original_byte_slice(),
+        Some(predeployed.code_hash),
+    )?;
+
     Ok(())
 }
