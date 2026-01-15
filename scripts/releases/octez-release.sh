9d4992fc70bb5605f94c16bb2a3c52462dd37e66
Killian Delarue <killian.delarue@nomadic-labs.com>
2026-01-15 15:28:13 +0100

    Scripts, Releases: Introduce [gitlab_packaging_revision_version]

--- scripts/releases/octez-release.sh
+++ scripts/releases/octez-release.sh
@@ -20,7 +20,7 @@ octez_source_content="$script_inputs_dir/octez-source-content"
 
 # Full octez release tag
 # octez-vX.Y, octez-vX.Y-rcZ or octez-vX.Y-betaZ
-gitlab_release=$(echo "${CI_COMMIT_TAG}" | grep -oE '^octez-v([0-9]+)\.([0-9]+)(-(rc|beta)([0-9]+))?$' || :)
+gitlab_release=$(echo "${CI_COMMIT_TAG}" | grep -oE '^octez-v([0-9]+)\.([0-9]+)(-(rc|beta)?([0-9]+))?$' || :)
 
 # Strips the leading 'octez-v'
 # X.Y, X.Y-rcZ or X.Y-betaZ
@@ -32,13 +32,15 @@ gitlab_release_no_v=$(echo "${gitlab_release}" | sed -e 's/^octez-v//g')
 gitlab_release_no_dot=$(echo "${gitlab_release_no_v}" | sed -e 's/\./-/g')
 
 # X
-gitlab_release_major_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^octez-v([0-9]+)\.([0-9]+)((-rc[0-9]+)?|(-beta[0-9]+)?)$/\1/p')
+gitlab_release_major_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^octez-v([0-9]+)\.([0-9]+)((-[0-9]+)?|(-rc[0-9]+)?|(-beta[0-9]+)?)$/\1/p')
 # Y
-gitlab_release_minor_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^octez-v([0-9]+)\.([0-9]+)((-rc[0-9]+)?|(-beta[0-9]+)?)$/\2/p')
+gitlab_release_minor_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^octez-v([0-9]+)\.([0-9]+)((-[0-9]+)?|(-rc[0-9]+)?|(-beta[0-9]+)?)$/\2/p')
 # Z
 gitlab_release_rc_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^octez-v([0-9]+)\.([0-9]+)(-rc)?([0-9]+)?$/\4/p')
 # Beta
 gitlab_release_beta_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^octez-v([0-9]+)\.([0-9]+)(-beta)?([0-9]+)?$/\4/p')
+# Revision
+gitlab_packaging_revision_version=$(echo "${CI_COMMIT_TAG}" | sed -nE 's/^octez-v([0-9]+)\.([0-9]+)-([0-9]+)?$/\3/p')
 
 # Is this a release candidate?
 if [ -n "${gitlab_release_rc_version}" ]; then
