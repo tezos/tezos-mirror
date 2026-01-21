57d9fcce39a13645f7911c64f0cc1cf39dab7896
Eugen Zalinescu <eugen.zalinescu@nomadic-labs.com>
2026-01-21 06:41:56 +0100

    Scripts: update check-format-rust.sh to be able to apply formatting changes
    ...via the CLI arguments --fix or --apply

--- scripts/check-format-rust.sh
+++ scripts/check-format-rust.sh
@@ -5,6 +5,30 @@ set -e
 self=$(dirname "$0")
 repo_args=()
 ls_args=()
+fmt_args="--check"
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --fix)
+    fmt_args=""
+    shift
+    ;;
+  -h | --help)
+    echo "Usage: $0 [--fix]"
+    echo ""
+    echo "Options:"
+    echo "  --fix             Apply formatting changes instead of just checking"
+    echo "  -h, --help        Show this help message"
+    exit 0
+    ;;
+  *)
+    echo "Unknown option: $1"
+    echo "Use --help for usage information"
+    exit 1
+    ;;
+  esac
+done
 
 # Are we using Jujutsu?
 if ! git rev-parse --show-toplevel 2> /dev/null && jj root 2> /dev/null > /dev/null; then
@@ -18,7 +42,7 @@ fi
 
 format_all() {
   # shellcheck disable=SC2016
-  xargs -0 -I{} sh -c "cd $self/../\$(dirname {}) && cargo fmt --check"
+  xargs -0 -I{} sh -c "cd $self/../\$(dirname {}) && cargo fmt $fmt_args"
 }
 
 find_files() {
