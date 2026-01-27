8feb6ecaa52ee3586fb5c8e3c0ed3eabe122d7f1
Dibassi Brahima <brahima.dibassi@lambda-coins.com>
2026-01-27 11:17:32 +0100

    SDK/Rust/EncondingDerive : Regenerated the macro regression tests

--- sdk/rust/encoding-derive/tests/expand/struct2.expanded.rs
+++ sdk/rust/encoding-derive/tests/expand/struct2.expanded.rs
@@ -12,8 +12,7 @@ where
         tezos_data_encoding::encoding::Encoding::Obj(
             "Struct2",
             <[_]>::into_vec(
-                #[rustc_box]
-                ::alloc::boxed::Box::new([
+                ::alloc::boxed::box_new([
                     tezos_data_encoding::encoding::Field::new(
                         "a",
                         #[allow(clippy::redundant_clone)]
