d12dab687620fded66646cff3e7e20e934ab7505
Arnaud <arnaud.bihan@functori.com>
2026-01-30 17:31:27 +0100

    Tezlink/Store: Introduce store migration

---
+++ etherlink/bin_node/tezlink_migrations/tezlink_node_migrations.ml
@@ -0,0 +1,50 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2024 Nomadic Labs <contact@nomadic-labs.com>                *)
+(*                                                                           *)
+(*****************************************************************************)
+
+open Sqlite.Request
+open Caqti_type.Std
+
+let re_t = Re.(compile @@ Posix.re "[0-9]{3}_([a-z0-9_]+).sql")
+
+module type S = sig
+  val name : string
+
+  val up : (unit, unit, [`Zero]) Sqlite.Request.t list
+end
+
+type migration = (module S)
+
+let make_tezlink_migration path =
+  let read_exn path =
+    match Tezlink_migrations.read path with
+    | Some content -> content
+    | None -> raise (Invalid_argument "read_exn")
+  in
+
+  let migration_name =
+    let open Re in
+    let (group : Group.t) = exec re_t path in
+    Group.get group 1
+  in
+
+  let migration_step = ( @@ ) (unit ->. unit) in
+
+  let make_migration_requests str =
+    String.split ';' str
+    |> List.filter_map (fun i ->
+           match String.trim i with "" -> None | x -> Some (migration_step x))
+  in
+
+  (module struct
+    let name = migration_name
+
+    let up = make_migration_requests (read_exn path)
+  end : S)
+
+let tezlink_migrations version =
+  List.take_n (version + 1) Tezlink_migrations.file_list
+  |> List.map make_tezlink_migration
