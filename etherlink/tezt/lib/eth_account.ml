c65bd1b63a0b39424291516a6ed6ef8582035b0a
Guillaume Genestier <guillaume.genestier@lambda-coins.com>
2026-02-04 11:41:47 +0100

    Etherlink/test: DAL slots are bigger, so we craft more transactions

--- etherlink/tezt/lib/eth_account.ml
+++ etherlink/tezt/lib/eth_account.ml
@@ -59,6 +59,21 @@ let bootstrap_accounts =
       private_key =
         "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
     };
+    {
+      address = "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC";
+      private_key =
+        "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a";
+    };
+    {
+      address = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
+      private_key =
+        "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6";
+    };
+    {
+      address = "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65";
+      private_key =
+        "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a";
+    };
   |]
 
 let lots_of_address =
