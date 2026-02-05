492684e58a53d6c2d9a568c05b38564cdc27211b
Palmer Sébastien <sebastien.palmer@functori.com>
2026-02-05 09:27:11 +0100

    Tezlink/Kernel: implement Rlp::Encodable for Protocol

--- etherlink/kernel_latest/tezos/src/protocol.rs
+++ etherlink/kernel_latest/tezos/src/protocol.rs
@@ -2,10 +2,30 @@
 //
 // SPDX-License-Identifier: MIT
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug, Clone, Copy)]
+#[repr(u8)]
 pub enum Protocol {
-    S023,
+    S023 = 23,
+}
+
+pub const TARGET_TEZOS_PROTOCOL: Protocol = Protocol::S023;
+
+impl rlp::Encodable for Protocol {
+    fn rlp_append(&self, s: &mut rlp::RlpStream) {
+        let val = *self as u8;
+        s.append_internal(&val);
+    }
 }
 
 #[cfg(test)]
-pub const TARGET_TEZOS_PROTOCOL: Protocol = Protocol::S023;
+mod tests {
+    use super::Protocol;
+
+    #[test]
+    pub fn rpl_encode_protocol() {
+        let mut stream = rlp::RlpStream::new();
+        stream.append(&Protocol::S023);
+        let out = stream.out();
+        assert_eq!(out, vec![23]);
+    }
+}
