846c866dadc6d97be8dce6e7a7e7b75812d01322
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2026-02-11 16:29:41 +0100

    TezosX/Node: refactor blueprint decoding
    Separate extraction of fields from the RLP structure from conversion
    so that the logic for the latter can be shared between versioned and
    unversioned blueprints.

--- etherlink/bin_node/lib_dev/blueprint_decoder.ml
+++ etherlink/bin_node/lib_dev/blueprint_decoder.ml
@@ -5,38 +5,40 @@
 (*                                                                           *)
 (*****************************************************************************)
 
-let from_rlp =
+let from_rlp rlp =
   let open Result_syntax in
   let open Rlp in
-  function
-  | List
-      [
-        Value _parent_hash;
-        List delayed_transactions;
-        List transactions;
-        Value _timestamp;
-      ] ->
-      let* delayed_transaction_hashes =
-        List.map_e
-          (function
-            | Value hash_tx ->
-                let (`Hex hex) = Hex.of_bytes hash_tx in
-                return (Ethereum_types.Hash (Hex hex), None)
-            | _ -> tzfail Sequencer_blueprint.Not_a_blueprint)
-          delayed_transactions
-      in
-
-      let* transaction_hashes =
-        List.map_e
-          (function
-            | Value bytes ->
-                let raw_tx = Bytes.to_string bytes in
-                return (Ethereum_types.hash_raw_tx raw_tx, Some raw_tx)
-            | _ -> tzfail Sequencer_blueprint.Not_a_blueprint)
-          transactions
-      in
-      return (delayed_transaction_hashes @ transaction_hashes)
-  | _rlp -> tzfail Sequencer_blueprint.Not_a_blueprint
+  let* delayed_transactions, transactions =
+    match rlp with
+    | List
+        [
+          Value _parent_hash;
+          List delayed_transactions;
+          List transactions;
+          Value _timestamp;
+        ] ->
+        return (delayed_transactions, transactions)
+    | _rlp -> tzfail Sequencer_blueprint.Not_a_blueprint
+  in
+  let* delayed_transaction_hashes =
+    List.map_e
+      (function
+        | Value hash_tx ->
+            let (`Hex hex) = Hex.of_bytes hash_tx in
+            return (Ethereum_types.Hash (Hex hex), None)
+        | _ -> tzfail Sequencer_blueprint.Not_a_blueprint)
+      delayed_transactions
+  in
+  let* transaction_hashes =
+    List.map_e
+      (function
+        | Value bytes ->
+            let raw_tx = Bytes.to_string bytes in
+            return (Ethereum_types.hash_raw_tx raw_tx, Some raw_tx)
+        | _ -> tzfail Sequencer_blueprint.Not_a_blueprint)
+      transactions
+  in
+  return (delayed_transaction_hashes @ transaction_hashes)
 
 let transactions payload =
   let open Result_syntax in
