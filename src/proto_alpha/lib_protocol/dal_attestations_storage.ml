8f3b589a8ce95f59a5b2ef3c99fe1ed86849cf6d
Julien <julien.tesson@nomadic-labs.com>
2026-02-13 22:33:51 +0100

    Alpha/Dal_attestations_storage: add committee_level_of

--- src/proto_alpha/lib_protocol/dal_attestations_storage.ml
+++ src/proto_alpha/lib_protocol/dal_attestations_storage.ml
@@ -27,3 +27,17 @@ let record_number_of_attested_shards ctxt ~delegate ~attested_level attestation
 let attestations = Dal.attestations
 
 let record_attestation = Dal.record_attestation
+
+(** [committee_level_of ctxt ~published_level] computes the committee level
+    (a.k.a. shard assignment level) for a given [published_level].
+
+    The committee level is defined as
+    [published_level + attestation_lag - 1], where [attestation_lag] is
+    retrieved from the DAL parameters that were active at [published_level].
+    This makes the function protocol-change-aware: if a protocol migration
+    changed [attestation_lag], the correct value for the given
+    [published_level] is used (via {!Dal_storage.parameters}). *)
+let committee_level_of ctxt ~published_level =
+  let open Lwt_result_syntax in
+  let* dal_params = Dal_storage.parameters ctxt published_level in
+  return (Raw_level_repr.add published_level (dal_params.attestation_lag - 1))
