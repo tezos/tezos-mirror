61af83c0d12b1987b9c461a650f04375c6d3eb82
Marina Polubelova <marina.polubelova@nomadic-labs.com>
2026-02-23 18:27:49 +0100

    Proto/CLST/Tests: add test for export_ticket entrypoint

--- src/proto_alpha/lib_protocol/test/helpers/op.ml
+++ src/proto_alpha/lib_protocol/test/helpers/op.ml
@@ -1713,3 +1713,55 @@ let clst_update_operator ?force_reveal ?counter ?fee ?gas_limit ?storage_limit
     src
     (Contract.Originated clst_hash)
     Tez.zero
+
+let clst_export_ticket ?force_reveal ?counter ?fee ?gas_limit ?storage_limit
+    (ctxt : Context.t) ~(src : Contract.t) ~(dst : Contract.t) (amount : int64)
+    =
+  let open Lwt_result_wrap_syntax in
+  let* alpha_ctxt = Context.get_alpha_ctxt ctxt in
+  let*@ clst_hash = Contract.get_clst_contract_hash alpha_ctxt in
+  let open Environment.Micheline in
+  let ticket_to_export =
+    Prim
+      ( dummy_location,
+        Script.D_Pair,
+        [
+          String (dummy_location, Contract.to_b58check src);
+          Int (dummy_location, Z.zero);
+          Int (dummy_location, Z.of_int64 amount);
+        ],
+        [] )
+  in
+  let tx =
+    Prim
+      ( dummy_location,
+        Script.D_Pair,
+        [
+          String (dummy_location, Contract.to_b58check dst);
+          Seq (dummy_location, [ticket_to_export]);
+        ],
+        [] )
+  in
+  let elm =
+    Prim
+      ( dummy_location,
+        Script.D_Pair,
+        [Prim (dummy_location, D_None, [], []); Seq (dummy_location, [tx])],
+        [] )
+  in
+  let parameters =
+    Alpha_context.Script.lazy_expr
+      Environment.Micheline.(Seq (dummy_location, [elm]) |> strip_locations)
+  in
+  unsafe_transaction
+    ?force_reveal
+    ?counter
+    ?fee
+    ?gas_limit
+    ?storage_limit
+    ~entrypoint:(Entrypoint.of_string_strict_exn "export_ticket")
+    ~parameters
+    ctxt
+    src
+    (Contract.Originated clst_hash)
+    Tez.zero
