479de3f28d70243c4f3f0a70ef9eaded52f437c9
Eugen Zalinescu <eugen.zalinescu@nomadic-labs.com>
2026-02-24 08:32:31 +0100

    Tezt/Cloud/DAL: fix typo in baker_helpers.mli

--- tezt/tests/cloud/baker_helpers.mli
+++ tezt/tests/cloud/baker_helpers.mli
@@ -30,7 +30,7 @@ type per_baker_dal_summary = {
   attested_slots : int;
   in_committee : bool;
   (* [attestation_with_dal] is [None] if one is out of the DAL committee or did
-     not send any attestation.
+     not send an attestation.
      Otherwise, it is [Some the_sent_attestation_is_"with_dal"]. *)
   attestation_with_dal : bool option;
 }
