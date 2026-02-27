c50d768817e08e647b4a8844da25e0f4aec9dbea
Marina Polubelova <marina.polubelova@nomadic-labs.com>
2026-02-27 18:46:16 +0100

    Proto/CLST/Tests: add test for lambda_export entrypoint

--- src/proto_alpha/lib_protocol/test/helpers/error_helpers.ml
+++ src/proto_alpha/lib_protocol/test/helpers/error_helpers.ml
@@ -285,6 +285,18 @@ let expect_clst_empty_ticket ~loc errs =
         true
     | _ -> false)
 
+let expect_clst_standard_error ~loc ~str errs =
+  Assert.expect_error ~loc errs (function
+    (* CLST is interacted with as a Michelson contract, as such the trace is
+       always part of the interpreter error trace. *)
+    | Script_interpreter_errors.Runtime_contract_error _
+      :: Script_interpreter_errors.Reject (_, node, _)
+      :: _ -> (
+        match Environment.Micheline.root node with
+        | String (_, str_) -> String.equal str_ str
+        | _ -> false)
+    | _ -> false)
+
 let expect_tz5_account_disabled ~loc errs =
   Assert.expect_error ~loc errs (function
     | [Validate_errors.Manager.Tz5_account_disabled] -> true
