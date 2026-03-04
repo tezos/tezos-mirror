e89a380c2e3e92936f8b2d62fe4122f1dea1b9c4
Killian Delarue <killian.delarue@nomadic-labs.com>
2026-03-04 14:36:39 +0100

    Release-page, Tests: Introduce [bin_release_page/tezt/]

---
+++ ci/bin_release_page/tezt/test_version_manager.ml
@@ -0,0 +1,13 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2026 Nomadic Labs. <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** Testing
+    -------
+    Component:    Release Page / version_manager
+    Invocation:   dune exec ci/bin_release_page/tezt/main.exe --file test_version_manager.exe
+    Subject:      Tests for the version_manager tool
+*)
