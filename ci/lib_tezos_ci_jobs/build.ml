0ebed63d36edcd988536d456f482427c699f498f
Romain <romain@nomadic-labs.com>
2026-03-05 12:15:13 +0100

    CI: generate artifact list semi-automatically

--- ci/lib_tezos_ci_jobs/build.ml
+++ ci/lib_tezos_ci_jobs/build.ml
@@ -42,8 +42,8 @@ let build_octez_source =
     ]
 
 (* Common features of build jobs that use [build_full_unreleased.sh]. *)
-let build_job ~__POS__ ~arch ?storage ~executable_files ?(extra = []) ~artifacts
-    ?dune_cache name =
+let build_job ~__POS__ ~arch ?storage ~executable_files ?(extra = [])
+    ?(extra_artifacts = []) ?dune_cache name =
   CI.job
     name
     ~__POS__
@@ -67,7 +67,9 @@ let build_job ~__POS__ ~arch ?storage ~executable_files ?(extra = []) ~artifacts
               (Tezos_ci.Runner.Arch.show_easy_to_distinguish arch))
          ~when_:On_success
          ~expire_in:(Duration (Days 1))
-         artifacts)
+         (read_lines_from_file executable_files
+         @ List.map (( // ) "_build/default") extra
+         @ extra_artifacts))
     ?dune_cache
     ~cargo_cache:true
     ~sccache:(Cacio.sccache ~policy:Pull_push ())
@@ -89,7 +91,7 @@ let job_build_released =
     ~arch
     ~storage:Ramfs
     ~executable_files:"script-inputs/released-executables"
-    ~artifacts:["octez-*"; "src/proto_*/parameters/*.json"]
+    ~extra_artifacts:["src/proto_*/parameters/*.json"]
 
 let job_build_extra_dev =
   Cacio.parameterize @@ fun arch ->
@@ -114,13 +116,12 @@ let job_build_extra_dev =
       match arch with
       | Amd64 -> ["contrib/octez_injector_server/octez_injector_server.exe"]
       | _ -> [])
-    ~artifacts:
+    ~extra_artifacts:
       [
-        "octez-*";
         "src/proto_*/parameters/*.json";
+        (* TODO: the protocol compiler executable is already at the root
+           as octez-protocol-compiler, do we actually need the following line? *)
         "_build/default/src/lib_protocol_compiler/bin/main_native.exe";
-        "_build/default/tezt/tests/main.exe";
-        "_build/default/contrib/octez_injector_server/octez_injector_server.exe";
       ]
     ~dune_cache:(match arch with Amd64 -> true | Arm64 -> false)
 
@@ -132,14 +133,7 @@ let job_build_exp =
     ~description:"Build the set of experimental executables."
     ~arch
     ~executable_files:"script-inputs/experimental-executables"
-    ~artifacts:
-      [
-        "octez-*";
-        "octez-teztale-*";
-        "src/proto_*/parameters/*.json";
-        "etherlink-governance-observer";
-        "fa-bridge-watchtower";
-      ]
+    ~extra_artifacts:["src/proto_*/parameters/*.json"]
     ~dune_cache:(match arch with Amd64 -> true | Arm64 -> false)
 
 let job_build_layer1_profiling =
