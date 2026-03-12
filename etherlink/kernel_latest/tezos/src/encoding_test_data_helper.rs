56f8d85a7ddf6e851329e4794c4575f808392986
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2026-03-12 13:23:11 +0100

    Tezlink/Kernel/Tests: fallback directory for regression traces

--- etherlink/kernel_latest/tezos/src/encoding_test_data_helper.rs
+++ etherlink/kernel_latest/tezos/src/encoding_test_data_helper.rs
@@ -10,23 +10,51 @@ pub mod test_helpers {
 
     use crate::protocol::Protocol;
 
+    const ENCODING_REGRESSION_TRACES_DIRECTORY: &str =
+        "../../../tezt/tests/expected/encoding.ml";
+    const FALLBACK_ENCODING_REGRESSION_TRACES_DIRECTORY: &str = "regressions";
+
+    pub fn regression_trace(
+        dir: &str,
+        protocol: Protocol,
+        encoding: &str,
+    ) -> Result<String, std::io::Error> {
+        let filepath = format!(
+            "{}/{}/{:?}-- {}- {}.out",
+            env!("CARGO_MANIFEST_DIR"),
+            dir,
+            protocol,
+            "protocol encoding regression test",
+            encoding
+        );
+        println!("{}", &filepath);
+        fs::read_to_string(filepath)
+    }
+
     pub fn fetch_generated_data(
         protocol: Protocol,
         encoding: &str,
         sample_name: &str,
     ) -> Vec<u8> {
-        let filepath = format!(
-            "{}/{}/{:?}-- {}- {}.out",
-            env!("CARGO_MANIFEST_DIR"),
-            "regressions",
+        let file_content = match regression_trace(
+            ENCODING_REGRESSION_TRACES_DIRECTORY,
             protocol,
-            "protocol encoding regression test",
-            encoding
-        );
-
-        println!("{}", &filepath);
-        let file_content =
-            fs::read_to_string(filepath).expect("Should have been able to read the file");
+            encoding,
+        ) {
+            Ok(file_content) => file_content,
+            Err(_) => {
+                // For frozen protocols, the regression traces are
+                // moved to the legacy directory because Tezt
+                // checks that all regression traces correspond to
+                // regression tests which still exist.
+                regression_trace(
+                    FALLBACK_ENCODING_REGRESSION_TRACES_DIRECTORY,
+                    protocol,
+                    encoding,
+                )
+                .expect("Should have been able to read the file")
+            }
+        };
 
         let re =
             Regex::new(&format!("{}: ([0-9a-f]+)", regex::escape(sample_name))).unwrap();
