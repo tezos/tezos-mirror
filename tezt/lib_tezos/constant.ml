8ccbf3846fe42640bfd7e62fe3383bff496f9821
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2026-03-19 14:39:35 +0100

    U025/tezt: add unused u025_025 baker

--- tezt/lib_tezos/constant.ml
+++ tezt/lib_tezos/constant.ml
@@ -99,6 +99,9 @@ let _octez_baker_seoul =
 let _octez_baker_tallinn =
   Uses.make ~tag:"baker_psd5wvtj" ~path:"./octez-baker-PtTALLiN" ()
 
+let _octez_baker_ushuaia =
+  Uses.make ~tag:"baker_psgxqmsv" ~path:"./octez-baker-PsGXQmSv" ()
+
 let _octez_baker_alpha =
   Uses.make ~tag:"baker_alpha" ~path:"./octez-baker-alpha" ()
 
