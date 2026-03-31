a565e1176caef064ff26b1f64a3e8ee3805c6092
Romain <romain@nomadic-labs.com>
2026-03-31 12:07:52 +0200

    CI: migrate docker build jobs in master pipelines to Cacio

---
+++ ci/lib_tezos_ci_jobs/docker.mli
@@ -0,0 +1,9 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* SPDX-License-Identifier: MIT                                              *)
+(* Copyright (c) 2026 Nomadic Labs. <contact@nomadic-labs.com>               *)
+(*                                                                           *)
+(*****************************************************************************)
+
+(** Register Docker build and related jobs. *)
+val register : unit -> unit
