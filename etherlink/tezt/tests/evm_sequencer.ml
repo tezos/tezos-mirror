76038b781c39b10ba988e60ff7270946258d1ed5
Rodi-Can Bozman <rodi.bozman@functori.com>
2026-04-20 17:37:58 +0200

    TezosX/Tezt: remove fast withdraw feature flag test (not used anymore)

--- etherlink/tezt/tests/evm_sequencer.ml
+++ etherlink/tezt/tests/evm_sequencer.ml
@@ -9063,21 +9063,6 @@ let test_multichain_feature_flag =
       "Multichain feature flag in the durable storage is %L, expected %R" ;
   unit
 
-let test_fast_withdrawal_feature_flag =
-  register_all
-    ~__FILE__
-    ~tags:["fast_withdrawal"; "feature_flag"]
-    ~title:"Fast withdrawal feature is set in storage"
-    ~enable_fast_withdrawal:true
-  @@ fun {sequencer; kernel; _} _protocol ->
-  (* We simply check that the flag is set in the storage. *)
-  let path = Durable_storage_path.enable_fast_withdrawal kernel in
-  let*@ flag = Rpc.state_value sequencer path in
-  Check.is_true
-    (Option.is_some flag)
-    ~error_msg:(sf "Expected to have a value at %s" path) ;
-  unit
-
 let test_evm_node_flag =
   register_all
     ~__FILE__
@@ -16426,7 +16411,6 @@ let () =
   test_make_l2_kernel_installer_config "EVM" ;
   test_make_l2_kernel_installer_config "Michelson" ;
   test_mainnet_kernel_name_matches_root_hash () ;
-  test_fast_withdrawal_feature_flag protocols ;
   test_deposit_and_fast_withdraw protocols ;
   test_deposit_and_fa_fast_withdraw protocols ;
   test_fast_withdrawal_l2_caller protocols ;
