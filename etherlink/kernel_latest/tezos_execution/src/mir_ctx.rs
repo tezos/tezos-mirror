96bffffa616a54993faf6cee8733cce311edd149
Mohamed IGUERNLALA <iguer@functori.com>
2026-05-13 13:42:39 +0200

    Tezos X: thread gas through hash_micheline_expr
    `hash_micheline_expr(expr)` becomes `hash_micheline_expr(expr,
    gas: &mut Gas)`. Its internal `encode_for_pack` now consumes the
    passed gas instead of `Gas::unmetered()`. Callers updated: `hash_key`
    forwards its own gas; `big_map_update` passes
    `&mut self.operation_gas.remaining`.

--- etherlink/kernel_latest/tezos_execution/src/mir_ctx.rs
+++ etherlink/kernel_latest/tezos_execution/src/mir_ctx.rs
@@ -739,8 +739,11 @@ pub fn clear_temporary_big_maps<Host: StorageV1, C: Context>(
 /// Hashes a Micheline expression using the packed format (with 0x05 prefix)
 /// to match L1's Script_expr_hash.
 /// See: https://gitlab.com/tezos/tezos/-/blob/master/src/proto_023_PtSeouLo/lib_protocol/script_ir_translator.ml#L159
-fn hash_micheline_expr(expr: &Micheline<'_>) -> Result<ScriptExprHash, LazyStorageError> {
-    let bytes = expr.encode_for_pack(&mut Gas::unmetered())??;
+fn hash_micheline_expr(
+    expr: &Micheline<'_>,
+    gas: &mut Gas,
+) -> Result<ScriptExprHash, LazyStorageError> {
+    let bytes = expr.encode_for_pack(gas)??;
     Ok(digest_256(&bytes).into())
 }
 
@@ -764,7 +767,7 @@ fn hash_key(
 ) -> Result<ScriptExprHash, LazyStorageError> {
     let parser = Parser::new();
     let key_micheline = key.into_micheline_optimized_legacy(&parser.arena, gas)?;
-    hash_micheline_expr(&key_micheline)
+    hash_micheline_expr(&key_micheline, gas)
 }
 
 /// Function to convert a BtreeMap that represent the lazy_storage_diff
@@ -995,7 +998,8 @@ impl<'a, Host: StorageV1, C: Context> LazyStorage<'a> for TcCtx<'a, Host, C> {
         let key_encoded = micheline_expr.encode(&mut self.operation_gas.remaining)??;
         // key_hashed: hash of packed encoding (with 0x05 prefix), used for storage path
         // See: https://gitlab.com/tezos/tezos/-/blob/master/src/proto_023_PtSeouLo/lib_protocol/script_ir_translator.ml#L5563
-        let key_hashed = hash_micheline_expr(&micheline_expr)?;
+        let key_hashed =
+            hash_micheline_expr(&micheline_expr, &mut self.operation_gas.remaining)?;
         let value_path = value_path(self.context, id, &key_hashed)?;
         match value {
             None => {
