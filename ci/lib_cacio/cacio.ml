0410b633027b0b32dd4f90c63e0303ba4997023f
Romain <romain@nomadic-labs.com>
2026-07-08 16:08:59 +0200

    Cacio: cache as a list instead of a list option

--- ci/lib_cacio/cacio.ml
+++ ci/lib_cacio/cacio.ml
@@ -208,7 +208,7 @@ type job = {
   variables : Gitlab_ci.Types.variables option;
   script : string list;
   artifacts : Gitlab_ci.Types.artifacts option;
-  cache : Gitlab_ci.Types.cache list option;
+  cache : Gitlab_ci.Types.cache list;
   cargo_cache : bool;
   sccache : sccache_config option;
   dune_cache : bool;
@@ -706,7 +706,7 @@ let convert_graph ?(interruptible_pipeline = true)
                 ?artifacts
                 ~datadog:(not disable_datadog)
                 ?allow_failure
-                ?cache
+                ?cache:(match cache with [] -> None | _ :: _ -> Some cache)
                 script
               |> maybe_enable_cargo_cache |> maybe_enable_sccache
               |> maybe_enable_dune_cache
@@ -1062,10 +1062,10 @@ module Make (Component : COMPONENT) : COMPONENT_API = struct
   let job ~__POS__:source_location ~stage ~description ?provider ?arch ?cpu
       ?storage ?tag ?image ?only_if_changed ?(force = false)
       ?(force_if_label = []) ?(needs = []) ?(needs_legacy = []) ?parallel
-      ?environment ?variables ?artifacts ?cache ?(cargo_cache = false) ?sccache
-      ?(dune_cache = false) ?(disable_datadog = false) ?allow_failure ?retry
-      ?timeout ?(image_dependencies = []) ?services ?id_tokens ?(script = [])
-      name =
+      ?environment ?variables ?artifacts ?(cache = []) ?(cargo_cache = false)
+      ?sccache ?(dune_cache = false) ?(disable_datadog = false) ?allow_failure
+      ?retry ?timeout ?(image_dependencies = []) ?services ?id_tokens
+      ?(script = []) name =
     let name = make_name name in
     declared_jobs := String_map.add name source_location !declared_jobs ;
     (* Check that no dependency is in an ulterior stage. *)
