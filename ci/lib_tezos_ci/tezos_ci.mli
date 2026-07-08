7446d991f8cbc1d4c6d06d0f22f4b31caaa9ca90
Romain <romain@nomadic-labs.com>
2026-07-08 15:27:30 +0200

    CIAO: remove unused Tezos_ci.append_before_script from mli

--- ci/lib_tezos_ci/tezos_ci.mli
+++ ci/lib_tezos_ci/tezos_ci.mli
@@ -491,11 +491,6 @@ val append_script : string list -> tezos_job -> tezos_job
     Throws error when applied to {!trigger_job}s. *)
 val append_cache : Gitlab_ci.Types.cache -> tezos_job -> tezos_job
 
-(** Append to the [before_script:] section of a job.
-
-    Throws error when applied to {!trigger_job}s. *)
-val append_before_script : string list -> tezos_job -> tezos_job
-
 (** Override the [interruptible:] flag of a job.
 
     Has no effect on {!trigger_job}s. *)
