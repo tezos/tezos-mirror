f8cb6728e4c483583492ec93e402562c4baed97f
Joel Bjornson <joel.bjornson@trili.tech>
2026-07-27 09:36:44 +0100

    EVM/Gov: add regression tests for self-delegation double-count fix, pin pytezos 3.17.0

--- etherlink/tezos_contracts/governance/tests/sequencer_governance/entrypoints/test_vote.py
+++ etherlink/tezos_contracts/governance/tests/sequencer_governance/entrypoints/test_vote.py
@@ -380,6 +380,39 @@ class CommitteeGovernanceNewProposalTestCase(BaseTestCase):
         assert storage['voting_context']['period']['promotion']['yea_voting_power'] == DEFAULT_VOTING_POWER
         assert storage['voting_context']['period']['promotion']['total_voting_power'] == DEFAULT_TOTAL_VOTING_POWER
     
+    def test_self_delegated_voter_voting_power_not_doubled(self) -> None:
+        proposer = self.bootstrap_baker()
+        baker = self.bootstrap_baker()
+        delegation = self.deploy_delegated_governance()
+        governance_started_at_level = self.get_current_level() + 1
+        governance = self.deploy_sequencer_governance(custom_config={
+            'started_at_level': governance_started_at_level,
+            'period_length': 5,
+            'proposal_quorum': 20,
+            'delegation_contract': delegation.address
+        })
+
+        whitelist = {governance.address}
+        delegation.using(baker).propose_voting_key(pkh(baker), True, whitelist).send()
+        self.bake_block()
+        delegation.using(baker).claim_voting_rights(pkh(baker)).send()
+        self.bake_block()
+
+        assert delegation.list_voters(pkh(baker), governance.address) == [pkh(baker)]
+
+        sequencer_pk = 'edpkurcgafZ2URyB6zsm5d1YqmLt9r1Lk89J81N6KpyMaUzXWEsv1X'
+        pool_address = 'B7A97043983f24991398E5a82f63F4C58a417185'
+        governance.using(proposer).new_proposal(sequencer_pk, pool_address).send()
+        self.bake_blocks(4)
+
+        assert governance.get_voting_state()['period_type'] == PROMOTION_PERIOD
+
+        governance.using(baker).vote(YEA_VOTE).send()
+        self.bake_block()
+
+        storage = governance.contract.storage()
+        assert storage['voting_context']['period']['promotion']['yea_voting_power'] == DEFAULT_VOTING_POWER
+
     def test_delegate_vote_should_only_apply_for_non_voted_baker(self) -> None:
         proposer = self.bootstrap_baker()
         baker1 = self.bootstrap_baker()
