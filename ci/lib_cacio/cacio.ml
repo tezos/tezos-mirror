68b0fea2f7f7e00521909ee3b88d223c91f27a5b
Romain <romain@nomadic-labs.com>
2026-09-01 15:17:46 +0200

    CI: inline TEZT_PARALLEL

--- ci/lib_cacio/cacio.ml
+++ ci/lib_cacio/cacio.ml
@@ -1209,10 +1209,6 @@ module Make (Component : COMPONENT) : COMPONENT_API = struct
     let junit_xml = "tezt-junit.xml" in
     let variables =
       [
-        (* The following variables are only used in the YAML.
-           We can inline them later but for now the diff with the old Tezt.job function
-           is easier to review with them. *)
-        ("TEZT_PARALLEL", string_of_int parallel_tests);
         (* The following variable must be an environment variable
            because it is not only used in the YAML. *)
         ("TEZT_NO_NPX", "true");
@@ -1221,7 +1217,7 @@ module Make (Component : COMPONENT) : COMPONENT_API = struct
     let variables_to_echo =
       (* Later this could be computed from [variables], but for now we try to reduce
          the diff as much as possible to ease reviewing. *)
-      ["CI_NODE_INDEX"; "CI_NODE_TOTAL"; "TEZT_PARALLEL"]
+      ["CI_NODE_INDEX"; "CI_NODE_TOTAL"]
     in
     let cmd_echo_variables =
       let message =
@@ -1316,7 +1312,7 @@ module Make (Component : COMPONENT) : COMPONENT_API = struct
           ];
           ["--job"; "${CI_NODE_INDEX:-1}/${CI_NODE_TOTAL:-1}"];
           ["--record"; "tezt-results.json"];
-          ["--job-count"; "${TEZT_PARALLEL}"];
+          ["--job-count"; string_of_int parallel_tests];
           ["--retry"; string_of_int retry_tests];
           ["--record-mem-peak"];
           ["--mem-warn"; "5_000_000_000"];
